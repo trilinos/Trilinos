46254e7e352431eb0e64195a377b0a60a185beaf
mmstjohn <mmstjohn>
1999-05-12 23:25:36 +0000

    Fixed a couple of things that lint picked up. Changed it so that the parallel find median function is easier to include in new algorithms.

--- packages/zoltan/src/par/par_const.h
+++ packages/zoltan/src/par/par_const.h
@@ -22,6 +22,25 @@ static char *cvs_par_const_h = "$Id$";
 
 #include <mpi.h>
 
+/*
+ * Function prototype for utility to find the median
+ * of an array of weighted coordinates.
+ */
+extern int LB_find_median(
+  double *dots,         /* array of coordinates                              */
+  double *wgts,         /* array of weights associated with dots             */
+  int *dotmark,         /* returned list of which side of the median
+                           each dot is on:
+                                0 - dot is < valuehalf
+                                1 - dot is > valuehalf                       */
+  int dotnum,           /* number of dots (length of three previous arrays   */
+  int proc,             /* this proc number (rank)                           */
+  double fractionlo,    /* fraction of weight that should be in bottom half  */
+  MPI_Comm local_comm,  /* MPI communicator on which to find median          */
+  double *valuehalf,    /* on entry - first guess at median (if first_guess set)                           on exit - the median value                        */
+  int first_guess,      /* if set, use value in valuehalf as first guess     */
+  int *counter          /* returned for stats, # of median interations       */
+);
 
 extern void LB_print_sync_start(LB *, int);
 extern void LB_print_sync_end(LB *, int);
