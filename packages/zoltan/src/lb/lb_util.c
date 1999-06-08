ff16f6eb96c009c73cc4adbc494b3e81864b8b35
Erik G. Boman <egboman@sandia.gov>
1999-06-08 17:24:30 +0000

    Changed  LB_Get_Obj_List() to be compatible with the new API for weights.

--- packages/zoltan/src/lb/lb_util.c
+++ packages/zoltan/src/lb/lb_util.c
@@ -37,21 +37,24 @@ void LB_perform_error_checking(LB *lb)
  * i.e., by calling either Get_Obj_List or Get_First_Obj+Get_Next_Obj.
  */
 
-void LB_Get_Obj_List(LB *lb, LB_GID *global_ids, LB_LID *local_ids, int *ierr)
+void LB_Get_Obj_List(LB *lb, LB_GID *global_ids, LB_LID *local_ids, 
+     int wdim, float *objwgts, int *ierr)
 {
   int i;
 
   if (lb->Get_Obj_List != NULL){
     /* Get object list directly */
-    lb->Get_Obj_List(lb->Get_Obj_List_Data, global_ids, local_ids, ierr);
+    lb->Get_Obj_List(lb->Get_Obj_List_Data, global_ids, local_ids, 
+                     wdim, objwgts, ierr);
   }
   else if ((lb->Get_First_Obj != NULL) && (lb->Get_Next_Obj != NULL)){
     /* Use iterator functions to loop through object list */
     if (lb->Get_First_Obj(lb->Get_Obj_List_Data, global_ids, local_ids, 
-        ierr)){
+        wdim, objwgts, ierr)){
       i = 0;
       while (!ierr && lb->Get_Next_Obj(lb->Get_Obj_List_Data, 
-        global_ids[i], local_ids[i], &global_ids[i+1], &local_ids[i+1], ierr))
+        global_ids[i], local_ids[i], &global_ids[i+1], &local_ids[i+1], 
+        wdim, &objwgts[(i+1)*wdim], ierr))
         i++;
     }
   }
