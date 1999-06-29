198e065714d42ec619c0c7813b6d88ecdbd2e7ad
Karen D. Devine <kddevin@sandia.gov>
1999-06-29 19:13:44 +0000

    Prototypes for load-balancing in driver.

---
+++ packages/zoltan/src/driver/dr_loadbal_const.h
@@ -0,0 +1,28 @@
+/*====================================================================
+ * ------------------------
+ * | CVS File Information |
+ * ------------------------
+ *
+ * $RCSfile$
+ *
+ * $Author$
+ *
+ * $Date$
+ *
+ * $Revision$
+ *
+ * $Name$
+ *====================================================================*/
+#ifndef lint
+static char *cvs_dr_loadbal_const = "$Id$";
+#endif
+
+#ifndef _DR_LOADBAL_CONST_H_
+#define _DR_LOADBAL_CONST_H_
+
+extern int run_zoltan(int, PROB_INFO_PTR, ELEM_INFO **);
+extern int migrate_elements(int, ELEM_INFO **, struct LB_Struct *, int,
+                            LB_GID *, LB_LID *, int *, int, LB_GID *,
+                            LB_LID *, int *);
+
+#endif /* _DR_LOADBAL_CONST_H_ */
