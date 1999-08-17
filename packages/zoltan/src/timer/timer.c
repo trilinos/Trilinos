d5169e64b3ee8caa52bcf5aa05173bdd59bd854c
Erik G. Boman <egboman@sandia.gov>
1999-08-17 21:08:32 +0000

    Lots of changes in timer:  - Fixed a bug in Set_Timer_Param.  - CPU timer now tries to guess the number of rollovers    when using clock().  - Added two new timer types: "user" and "usersys".    "user" is user time, and "usersys" is user plus system time.    These options may use different function calls on different    machines, and may be unavailable on some systems.    Currently we use times() on POSIX systems and getrusage()    on BSD-like systems. The logic to determine the local    system characteristics can certainly be improved.

--- packages/zoltan/src/timer/timer.c
+++ packages/zoltan/src/timer/timer.c
@@ -21,7 +21,7 @@ static char *cvs_timerc_id = "$Id$";
 #include "timer_const.h"
 
 /* Static variables */
-static int TIMER = TIME_WALL;
+static int Timer = LB_TIME_WALL;
 
 /* Timer parameters */
 
@@ -33,70 +33,140 @@ char *val)                      /* value of variable */
     PARAM_UTYPE result;         /* value returned from Check_Param */
     int index;                  /* index returned from Check_Param */
     PARAM_VARS timer_params[] = {
-        { "TIMER", &TIMER, "INT" },
+        { "TIMER", &Timer, "STRING" },
         { NULL, NULL, NULL }
     };
 
     status = LB_Check_Param(name, val, timer_params, &result, &index);
+/*
+    printf("Debug: In LB_Set_Timer_Param, name=%s, val=%s, status=%d, index=%d\n", 
+      name, val, status, index);
+*/
+
     if (status == 0 && index == 0) {
         status = 3; /* Don't add to params list */
         if (strcasecmp(result.sval, "wall")==0)
-          TIMER = TIME_WALL;
+          Timer = LB_TIME_WALL;
         else if (strcasecmp(result.sval, "cpu")==0) {
-#ifndef SMOS
-          TIMER = TIME_CPU;
-#else  /* SMOS */
-          fprintf(stderr, "LB_Set_Timer_Param warning:  CPU time not available"
-                          "for SMOS; Wall clock time will be used.\n");
-          TIMER = TIME_WALL;
-#endif /* SMOS */
+#if (defined(_CLOCK_T) && ! defined(SMOS))
+          Timer = LB_TIME_CPU;
+#else  /* SMOS or !_CLOCK_T */
+          fprintf(stderr, "LB_Set_Timer_Param warning: CPU time not available;"
+                          " Wall clock time will be used.\n");
+          Timer = LB_TIME_WALL;
+#endif /* SMOS or !_CLOCK_T */
         }
-        else
-          status = 2; /* Unknown timer */
+        else if (strcasecmp(result.sval, "user")==0){
+#if (defined(HAVE_TIMES) || defined(HAVE_RUSAGE))
+          Timer = LB_TIME_USER;
+#else
+          fprintf(stderr, "LB_Set_Timer_Param warning: user time not available;"
+                          " Wall clock time will be used.\n");
+          Timer = LB_TIME_WALL;
+#endif
+        }
+        else if (strcasecmp(result.sval, "usersys")==0){
+#if (defined(HAVE_TIMES) || defined(HAVE_RUSAGE))
+          Timer = LB_TIME_USERSYS;
+#else
+          fprintf(stderr, "LB_Set_Timer_Param warning: usersys time not "
+                          "available; Wall clock time will be used.\n");
+          Timer = LB_TIME_WALL;
+#endif
+        }
+        else{
+          fprintf(stderr, "LB_Set_Timer_Param warning: Unknown timer option "
+                          "%s\n", result.sval);
+          status = 2; /* Illegal parameter */
+        }
     }
 
     return(status);
 }
 
-/* Timer routine that returns either CPU or wall-clock time */
-
-#ifndef CLOCKS_PER_SEC
-/* Some non-ANSI systems return clock() in microseconds 
- * and do not define CLOCKS_PER_SEC.
- */
-#define CLOCKS_PER_SEC 1000000
-#endif
+/* Timer routine that returns either CPU or wall-clock time.
+   The ANSI function clock() might roll over at approx 36 minutes,
+   so we try to determine the number of rollovers.
+*/
 
 double LB_Time()
 {
-  double t;
+  double t = 0.0;
+#if (defined(_CLOCK_T) && ! defined(SMOS))
+  clock_t num_ticks;
+  static clock_t last_num_ticks = 0;
+  static double  inv_clocks_per_sec = 1./(double)CLOCKS_PER_SEC;
+  static double  clock_width =
+    (double)(1L<<((int)sizeof(clock_t)*8-2))*4./(double)CLOCKS_PER_SEC;
+  static int     clock_rollovers = 0;
+#endif /* !SMOS */
+#ifdef HAVE_TIMES
+  static double  inv_clk_tck = 1./(double)CLK_TCK;
+#endif
 
-  if (TIMER==TIME_WALL)
+  if (Timer==LB_TIME_WALL)
     /* Wall clock */
     t = MPI_Wtime();
-  else if (TIMER==TIME_CPU) {
+  else if (Timer==LB_TIME_CPU) {
     /* CPU time */
-#ifndef SMOS  /* CPU Time not available for SMOS */
-    t = ((double) clock())/CLOCKS_PER_SEC;
+#if (defined(_CLOCK_T) && ! defined(SMOS))
+    num_ticks = clock();
+    if (num_ticks < last_num_ticks) clock_rollovers++;
+    t = num_ticks * inv_clocks_per_sec;
+    if (clock_rollovers) t += clock_rollovers * clock_width;
+    last_num_ticks = num_ticks;
 #endif /* !SMOS */
   }
-  else
-    t = 0.0;  /* Error */
+#if defined(HAVE_TIMES)
+  else if (Timer==LB_TIME_USER) {
+    struct tms tms;
+    times(&tms);
+    t = tms.tms_utime * inv_clk_tck;
+  }
+  else if (Timer==LB_TIME_USERSYS) {
+    struct tms tms;
+    times(&tms);
+    t = (tms.tms_utime + tms.tms_stime) * inv_clk_tck;
+  }
+#elif defined(HAVE_RUSAGE)
+  else if (Timer==LB_TIME_USER) {
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    t = (rusage.ru_utime.tv_sec + 1.0e-6 * rusage.ru_utime.tv_usec);
+  }
+  else if (Timer==LB_TIME_USERSYS) {
+    struct rusage rusage;
+    getrusage(RUSAGE_SELF, &rusage);
+    t = ((rusage.ru_utime.tv_sec + rusage.ru_stime.tv_sec) +
+        1.0e-6 * (rusage.ru_utime.tv_usec + rusage.ru_stime.tv_usec));
+  }
+#endif /* HAVE_TIMES or HAVE_RUSAGE */
 
   return t;
 }
 
-/* Precision of timer */
+/* Precision of timer. If the precision is unknown, zero is returned.  */
 double LB_Time_Resolution()
 {
-  double t;
+  double t = 0.0;
 
-  if (TIMER==TIME_WALL)
+  if (Timer==LB_TIME_WALL)
     t = MPI_Wtick();
-  else if (TIMER==TIME_CPU)
-    t = 1.0/CLOCKS_PER_SEC;
-  else
-    t = 0.0;  /* Error */
+  else if (Timer==LB_TIME_CPU)
+    t = 1.0/(double)CLOCKS_PER_SEC;
+#if defined(HAVE_TIMES)
+  else if ((Timer==LB_TIME_USER)||(Timer==LB_TIME_USERSYS)) {
+    t = 1.0/(double)CLK_TCK;
+  }
+#elif defined(HAVE_RUSAGE)
+  else if ((Timer==LB_TIME_USER)||(Timer==LB_TIME_USERSYS)) {
+#ifdef SUN
+    /* Use Sun-specific variable */
+    extern int usec_per_tick; 
+    t = 1.0e-6 * usec_per_tick;
+#endif /* !SUN */
+  }
+#endif /* HAVE_TIMES or HAVE_RUSAGE */
 
   return t;
 }
--- packages/zoltan/src/timer/timer_const.h
+++ packages/zoltan/src/timer/timer_const.h
@@ -20,11 +20,45 @@
 static char *cvs_timerconsth_id = "$Id$";
 #endif
 
-#include <time.h>
+#include <time.h> /* ANSI C; defines clock_t and clock() */
 
-/* Constants used in timer routines */
-#define TIME_WALL 1
-#define TIME_CPU 2
+/* POSIX.1 compliant systems should use times() for user+system timing */
+#if (defined(_POSIX_) || defined(POSIX) || defined(POSIX1))
+#define HAVE_TIMES
+#endif
+
+/* BSD-like systems should use getrusage() for user+system timing */
+#if (defined(BSD) || defined(BSD_COMP) || defined(sun))
+#define HAVE_RUSAGE
+#endif
+
+/* Include more header files depending on HAVE_* */
+#if defined(HAVE_TIMES)
+#include <sys/types.h>
+#include <sys/times.h>
+extern clock_t times();
+#elif defined(HAVE_RUSAGE)
+#include <sys/time.h>
+#include <sys/resource.h>
+extern int getrusage();
+#endif
+
+/* Some non-ANSI systems return clock() in microseconds
+ * and do not define CLOCKS_PER_SEC.
+ */
+#ifndef CLOCKS_PER_SEC
+#define CLOCKS_PER_SEC 1000000 /* Correct for SunOs 4.1 */
+#endif
+/* Similarly, guess the value of CLK_TCK if it is not defined */
+#ifndef CLK_TCK
+#define CLK_TCK 60 /* Correct for SunOs 4.1 */
+#endif
+
+/* Constants used in LB timer routines */
+#define LB_TIME_WALL 1
+#define LB_TIME_CPU  2
+#define LB_TIME_USER 3
+#define LB_TIME_USERSYS 4
 
 /* Function prototypes */
 double LB_Time();
