53ea71ceda8d597707ceb70c291c1d0b91d8190a
Erik G. Boman <egboman@sandia.gov>
2000-02-14 21:32:33 +0000

    Added prototype for LB_clean_string().

--- packages/zoltan/src/lb/lb_util_const.h
+++ packages/zoltan/src/lb/lb_util_const.h
@@ -23,6 +23,7 @@ extern void LB_perform_error_checking(LB *);
 extern void LB_Get_Obj_List(LB *, LB_GID *, LB_LID *, int, float *, int *);
 extern int LB_pad_for_alignment(int);
 extern unsigned int LB_hashf(LB_GID, int);
+extern int LB_clean_string(char *, char **);
 
 /*****************************************************************************/
 /*****************************************************************************/
