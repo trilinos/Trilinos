a42b99137b73f9235cc89c635d5cbed445747da0
Karen D. Devine <kddevin@sandia.gov>
2000-03-24 23:28:51 +0000

    Added copyright information to all files.

--- packages/zoltan/src/comm/comm_do.c
+++ packages/zoltan/src/comm/comm_do.c
@@ -1,17 +1,16 @@
-/*====================================================================
- * ------------------------
- * | CVS File Information |
- * ------------------------
- *
- * $RCSfile$
- *
- * $Author$
- *
- * $Date$
- *
- * $Revision$
- *
- *====================================================================*/
+/*****************************************************************************
+ * Zoltan Dynamic Load-Balancing Library for Parallel Applications           *
+ * Copyright (c) 2000, Sandia National Laboratories.                         *
+ * Zoltan is distributed under the GNU Lesser General Public License 2.1.    * 
+ * For more info, see the README file in the top-level Zoltan directory.     *  
+ *****************************************************************************/
+/*****************************************************************************
+ * CVS File Information :
+ *    $RCSfile$
+ *    $Author$
+ *    $Date$
+ *    $Revision$
+ ****************************************************************************/
 
 #include <stdio.h>
 #include <memory.h>
