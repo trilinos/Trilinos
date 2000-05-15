7295249998562478b8c156b37bf7098e85477c85
Karen D. Devine <kddevin@sandia.gov>
2000-05-15 23:30:48 +0000

    Removed overly optimistic reference to GNU Lesser GPL. Adjusted copyright notice for NIST contributions.

--- packages/zoltan/src/parmetis/scatter_graph.c
+++ packages/zoltan/src/parmetis/scatter_graph.c
@@ -1,3 +1,16 @@
+/*****************************************************************************
+ * Zoltan Dynamic Load-Balancing Library for Parallel Applications           *
+ * Copyright (c) 2000, Sandia National Laboratories.                         *
+ * For more info, see the README file in the top-level Zoltan directory.     *
+ *****************************************************************************/
+/*****************************************************************************
+ * CVS File Information :
+ *    $RCSfile$
+ *    $Author$
+ *    $Date$
+ *    $Revision$
+ ****************************************************************************/
+
 #include "lb_const.h"
 #include "all_allo_const.h"
 #include "comm_const.h"
