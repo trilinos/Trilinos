98c0e2b0286516ff45c80f0916c69486c8f7b77b
Karen D. Devine <kddevin@sandia.gov>
2000-08-30 19:54:04 +0000

    Logically separated memory-management package from Zoltan-specific memory management routines.
      New files:  mem.c, mem_const.h define memory management package.
      Old files:  all_allo.c, all_allo_const.h, free_structure.c define
                  Zoltan-specific routines.
    
    New routine in memory management packages was added to allow DEBUG_MEMORY
    to be set; this new routine provides separation between the memory package
    and the parameter setting code.

--- packages/zoltan/src/all/all_allo_const.h
+++ packages/zoltan/src/all/all_allo_const.h
@@ -15,45 +15,10 @@
 #define __ALL_ALLO_H
 
 #include "lb_const.h"
-
-#ifndef HAVE_PROTOTYPES
-#   if defined(__STDC__) || defined(__GNUC__) || defined(__cplusplus) || defined(c_plusplus)
-#       define	HAVE_PROTOTYPES
-#   endif
-#endif
-
-#undef PROTO
-#ifdef HAVE_PROTOTYPES
-#   define	PROTO(x)	x
-#else
-#   define	PROTO(x)	()
-#endif
-
-#define LB_MALLOC(a) LB_Malloc((a), __FILE__, __LINE__)
-#define LB_REALLOC(a, b) LB_Realloc((a), (b), __FILE__, __LINE__)
-#define LB_FREE(a) LB_Free((void **) (a), __FILE__, __LINE__)
-
-/* function declarations for dynamic array allocation */
-
-#ifdef __STDC__
-extern double *LB_Array_Alloc(char *file, int lineno, int numdim, ...);
-#else
-extern double *LB_Array_Alloc();
-#endif
+#include "mem_const.h"
 
 extern int LB_Set_Malloc_Param(char *, char *);
-extern void LB_Free(void **ptr, char *file, int lineno);
-extern double *LB_Malloc(int n, char *file, int lineno);
-extern double *LB_Realloc(void *ptr, int n, char *filename, int lineno);
-extern void LB_Memory_Stats();
-extern int LB_Memory_Num();
 extern void LB_Free_Structure(LB *);
-
-#ifdef __STDC__
-extern void LB_Multifree(int n, ...);
-#else
-extern void LB_Multifree();
-#endif
 
 /* function prototypes for Fortran allocation functions */
 
