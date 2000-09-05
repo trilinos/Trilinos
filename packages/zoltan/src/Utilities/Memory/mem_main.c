05b95030901fb027acc3e3dc3d5f1933098eb8d0
Karen D. Devine <kddevin@sandia.gov>
2000-09-05 22:40:11 +0000

    Stub driver for memory management package.

---
+++ packages/zoltan/src/Utilities/Memory/mem_main.c
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * Zoltan Dynamic Load-Balancing Library for Parallel Applications           *
+ * Copyright (c) 2000, Sandia National Laboratories.                         *
+ * For more info, see the README file in the top-level Zoltan directory.     *  
+ *****************************************************************************/
+/*****************************************************************************
+ * CVS File Information :
+ *    $RCSfile$
+ *    $Author$
+ *    $Date$
+ *    $Revision$
+ ****************************************************************************/
+
+#include <stdio.h>
+#include "mem_const.h"
+
+int main(int argc, char *argv[])
+{
+printf("This is a stub driver for the Memory Management package.\n");
+return 0;
+}
