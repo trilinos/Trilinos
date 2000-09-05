9c99935868dac0fc3cf97db860b9ac9c65561f14
Karen D. Devine <kddevin@sandia.gov>
2000-09-05 22:39:28 +0000

    Stub driver for communications package.

---
+++ packages/zoltan/src/Utilities/Communication/comm_main.c
@@ -0,0 +1,21 @@
+/*****************************************************************************
+ * Zoltan Dynamic Load-Balancing Library for Parallel Applications           *
+ * Copyright (c) 2000, Sandia National Laboratories.                         *
+ * For more info, see the README file in the top-level Zoltan directory.     *  
+ *****************************************************************************/
+/*****************************************************************************
+ * CVS File Information :
+ *    $RCSfile$
+ *    $Author$
+ *    $Date$
+ *    $Revision$
+ ****************************************************************************/
+
+#include <stdio.h>
+#include "comm_const.h"
+
+int main(int argc, char *argv[])
+{
+printf("This is a stub driver for the Unstructured Communications package.\n");
+return 0;
+}
