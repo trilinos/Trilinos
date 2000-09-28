faba8d0754117bd003324ebf61ff1a58c30a1e56
Erik G. Boman <egboman@sandia.gov>
2000-09-28 22:10:41 +0000

    Removed a comment.

--- packages/zoltan/src/driver/dr_loadbal.c
+++ packages/zoltan/src/driver/dr_loadbal.c
@@ -87,8 +87,6 @@ int run_zoltan(int Proc, PROB_INFO_PTR prob, MESH_INFO_PTR mesh)
     return 0;
   }
 
-  /* LB_Set_Param(lb, "CHECK_GRAPH", "1"); */
-
   /* Set the user-specified parameters */
   for (i = 0; i < prob->num_params; i++) {
     ierr = LB_Set_Param(lb, prob->params[i][0], prob->params[i][1]);
