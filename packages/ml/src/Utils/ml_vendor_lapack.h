40e6edf53883440317675daa039a6f07042e7775
Raymond Tuminaro <rstumin@sandia.gov>
2000-10-25 20:38:41 +0000

    still trying to get blas right on TFLOP

--- packages/ml/src/Utils/ml_vendor_lapack.h
+++ packages/ml/src/Utils/ml_vendor_lapack.h
@@ -1,3 +1,10 @@
+#ifndef __MLVENDORLAPACK__
+#define __MLVENDORLAPACK__
+
+
+
+
+
 #ifdef USE_VENDOR_BLAS
 #define ML_IDAMAX_FUNC
 #define ML_DSWAP_FUNC
@@ -14,24 +21,59 @@
 
 #ifdef USE_VENDOR_LAPACK
 
+#ifndef ML_DTRSV_FUNC
 #define ML_DTRSV_FUNC
+#endif
+#ifndef ML_DGETRF_FUNC
 #define ML_DGETRF_FUNC
+#endif
+#ifndef ML_DGETRS_FUNC
 #define ML_DGETRS_FUNC
+#endif
+#ifndef ML_XERBLA_FUNC
 #define ML_XERBLA_FUNC
+#endif
+#ifndef ML_LSAME_FUNC
 #define ML_LSAME_FUNC
+#endif
+#ifndef ML_DLASWP_FUNC
 #define ML_DLASWP_FUNC
+#endif
+#ifndef ML_DGETF2_FUNC
 #define ML_DGETF2_FUNC
+#endif
+#ifndef ML_DGER_FUNC
 #define ML_DGER_FUNC
+#endif
+#ifndef ML_ILAENV_FUNC
 #define ML_ILAENV_FUNC
+#endif
+#ifndef ML_DGEMV_FUNC
 #define ML_DGEMV_FUNC
+#endif
+#ifndef ML_DTRMV_FUNC
 #define ML_DTRMV_FUNC
+#endif
+#ifndef ML_DLAMCH_FUNC
 #define ML_DLAMCH_FUNC
+#endif
+#ifndef ML_DLAMC1_FUNC
 #define ML_DLAMC1_FUNC
+#endif
+#ifndef ML_DLAMC2_FUNC
 #define ML_DLAMC2_FUNC
+#endif
+#ifndef ML_DLAMC3_FUNC
 #define ML_DLAMC3_FUNC
+#endif
+#ifndef ML_DLAMC4_FUNC
 #define ML_DLAMC4_FUNC
+#endif
+#ifndef ML_DLAMC5_FUNC
 #define ML_DLAMC5_FUNC
+#endif
 
+#ifndef TFLOP
 #define ML_DPOTRS_FUNC
 #define ML_DGELS_FUNC
 #define ML_DGELQ2_FUNC
@@ -45,5 +87,7 @@
 #define ML_DORMLQ_FUNC
 #define ML_DORM2R_FUNC
 #define ML_DORML2_FUNC
+#endif
 
+#endif
 #endif
