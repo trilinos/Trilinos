e61f362955de201eefeef8a31f5cd933f8742bfb
Karen D. Devine <kddevin@sandia.gov>
2000-11-13 22:38:26 +0000

    Oops, I accidentally wiped out this file last week. This commit puts it back.

---
+++ packages/zoltan/src/fort/lb_user_const.f90
@@ -0,0 +1,42 @@
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+! Zoltan Dynamic Load-Balancing Library for Parallel Applications            !
+! For more info, see the README file in the top-level Zoltan directory.      ! 
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!  CVS File Information :
+!     $RCSfile$
+!     $Author$
+!     $Date$
+!     $Revision$
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+module lb_user_const
+use zoltan_types
+implicit none
+
+
+! User defined data types for passing data to the query functions.  These can
+! be used any way you want, but one suggestion is to use them as "wrapper"
+! types for your own user defined types, e.g.
+! type LB_User_Data_1 
+!    type(your_type), pointer :: ptr
+! end type
+! Exactly four data types must be defined, but you don't have to use them.
+
+type LB_User_Data_1
+   integer :: dummy
+end type LB_User_Data_1
+
+type LB_User_Data_2
+   integer :: dummy
+end type LB_User_Data_2
+
+type LB_User_Data_3
+   integer :: dummy
+end type LB_User_Data_3
+
+type LB_User_Data_4
+   integer :: dummy
+end type LB_User_Data_4
+
+end module lb_user_const
