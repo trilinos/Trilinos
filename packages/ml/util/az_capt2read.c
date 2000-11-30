1cdfa6c57b089a708c44c218da8a918b87647be7
Raymond Tuminaro <rstumin@sandia.gov>
2000-11-30 21:39:39 +0000

    changed to try and accomodate several different types of input including both vectors & matrices with different types of headers and preceeding string information.

--- packages/ml/util/az_capt2read.c
+++ packages/ml/util/az_capt2read.c
@@ -1,72 +1,96 @@
 #include <stdio.h>
+/*
+ *  Convert matlab-type sparse matrix data to AZ_read_msr()
+ *  format. This routine is mostly used for matrices (though
+ *  it can be used for vectors as well). Each matrix entry
+ *  should appear on a line of the form
+ *      row_index col_index value
+ *
+ *  Note: 
+ *     1) This routine should work with AZ_capture_matrix.dat and
+ *        AZ_capture_rhs.dat files.
+ *     2) row and column indices start from 1 (not 0).
+ *     3) rows should be sorted. That is all entries for
+ *        row i must appear before row i+k (k > 0).
+ *     4) The number of rows must be given. This can either
+ *        be passed on the command line (i.e. argv[1])
+ *        or by preceeding the matrix data with a single
+ *        line containing 2 numbers (the first is the number
+ *        of matrix rows).
+ *     5) String data proceeding the matrix/vector numbers is ignored.
+ *
+ *
+ *  This routine can be used for vectors instead of matrices. In this
+ *  case, each line should contain only 1 entry correspond to the vector 
+ *  element value.
+ *
+ */
 int main(int argc, char *argv[]) {
 
    int Nrows, Nnzs, current_row, row, col, i;
-   int flag, first_time = 1, j;
+   int flag, first_time = 1, j, ch;
    char string[80];
    double val;
    extern int mygetline(char string[]);
 
-#ifdef adams
-#define both
-#endif
-#ifdef day
-#define both
-#endif
-#ifndef both
-    /* read of the header 'Start of MSR matrix' */
+   /* We need to determine if there is some kind of header in the  */
+   /* file. First strip off any strings.                           */ 
 
-
-    flag = mygetline(string);
-
-    /* read of the number of rows and nonzeros  */
-
-    if ((flag != 0) && (string[0] >= '0') && (string[0] <= '9'))
-       flag = sscanf(string,"%d%d",&Nrows, &Nnzs);
-    else 
-       flag = scanf("%d%d",&Nrows, &Nnzs);
-
-    if (flag != 2) {
-       printf("Error: Couldn't read number of rows and nonzeros\n");
-       exit(1);
-    }
-#else
-   if (argc != 2) {
-       printf("Usage: az_capt2read Nrows\n");
-       exit(1);
+   
+   ch = getchar();
+   while ( ((ch >= 'a') && (ch <= 'z')) || ((ch >= 'A') && (ch <= 'Z'))) {
+      while (ch != '\n') ch = getchar();
+      ch = getchar();
    }
-   if (sscanf(argv[1],"%d",&Nrows) != 1) {
-      printf("Couldn't parse command line\n");
+
+   /* Now strip off any values indicating the size and number of nonzeros. */
+   i = 0;
+   string[i++] = ch;
+   while (ch != '\n') {
+      ch = getchar();
+      string[i++] = (char ) ch;
+   }
+   string[i] = '\0';
+
+   flag = 0;
+   if ( (i = sscanf(string,"%d%d%lf",&Nrows,&j,&val)) != 2) {
+             /* No header found (line with just 2 numbers) */
+      flag = 1;
+      if (argc == 2) {
+         if (sscanf(argv[1],"%d",&Nrows) != 1) {
+            printf("Couldn't parse command line\n");
+            exit(1);
+         }
+      }
+      else {
+         printf("No header found nor was the number of rows \n");
+         printf("specified on the command line. You must either \n");
+         printf("do\n\naz_capt2read Nrows\n\nor the file must have a line ");
+         printf("preceeding the\nmatrix data with just two numbers. The ");
+         printf("first\nindicating the number of matrix rows.\n");
+         exit(1);
+      }
+   }
+   printf("%d\n",Nrows);
+
+   if (i == 1) {
+      if (sscanf(string,"%lf",&val) != 1) {
+         printf("can not parse first number?\n");
+         exit(1);
+      }
+      printf("%d  %20.13e\n-1\n",0,val);
+      for (i = 1; i < Nrows; i++) {
+         scanf("%lf",&val);
+         printf("%d  %20.13e\n-1\n",i,val);
+      }
       exit(1);
    }
-#ifdef adams
-#ifdef old
-flag = scanf("%d%d",&Nrows, &Nnzs);
-#endif
-#endif
-#endif
-    printf("%d\n",Nrows);
-    flag = mygetline(string);
-    if (flag == 0) flag = mygetline(string);
 
-    current_row = 1;
-    while ( flag != 0) {
-#ifdef day
-/*
-string[0] = ' ';
-string[1] = ' ';
-string[2] = ' ';
-string[3] = ' ';
-j = 4; while ( string[j] != ',') j++;
-string[j] = ' ';
-j = 4; while ( string[j] != ')') j++;
-string[j] = ' ';
-while ( string[j] != '=') j++;
-string[j] = ' ';
-while ( string[j] != ';') j++;
-string[j] = ' ';
-*/
-#endif
+
+   if (flag == 0) flag = mygetline(string);
+
+   current_row = 1;
+   while ( flag != 0) {
        sscanf(string,"%d%d%lf",&row,&col,&val);
        if (row > Nrows) {
           fprintf(stderr,"Error: row (%d) exceeds total number of rows (%d).\n",
