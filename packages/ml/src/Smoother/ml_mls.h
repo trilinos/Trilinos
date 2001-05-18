c7627d2295d3636bf8734b3c115fa538b06f5983
Raymond Tuminaro <rstumin@sandia.gov>
2001-05-18 18:55:28 +0000

    new smoother (Cheby)

---
+++ packages/ml/src/Smoother/ml_mls.h
@@ -0,0 +1,42 @@
+#ifndef __MLMLS__
+#define __MLMLS__
+
+#define MLS_MAX_DEG  5 /* max. degree of MLS smoother        */
+
+struct MLSthing {
+ /*
+  * set degree and 
+  * the precomputed coefficients used in MLS application
+  */
+  int     mlsReady;  
+  int     mlsLevel;  
+  int     mlsDeg; /* degree for current level */
+  double  mlsBoost; 
+  double  mlsOver; 
+  double  mlsOm[MLS_MAX_DEG];
+  double  mlsOm2;
+  double  mlsCf[MLS_MAX_DEG];
+  int     nIsolNod; 
+  double *pIsolNod; 
+  double *res0, *res, *y;  /* workarrays allocated in .... to be reused */
+};
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int   ML_MLS_GetIsoln(int lv); 
+int   ML_MLS_SetOm(int lv, int deg); 
+int   ML_MLS_Smooth0(double b[], double vx[], double vy[], int deg, 
+		     double *om, double *cf, int nit, double over, 
+		     double wk);
+int   ML_MLS_Smooth1(double b[], double vx[], double vy[], int deg, 
+		     double *om, double *cf, int nit, double over, 
+		     double wk);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
