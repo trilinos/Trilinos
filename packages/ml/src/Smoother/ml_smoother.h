110d711e60f13a7e07e583c85088ec637fcb7d74
Jonathan Hu <jhu@sandia.gov>
2001-08-01 18:06:40 +0000

    Changed prototype for ML_Smoother_Gen_Hiptmair_Data.

--- packages/ml/src/Smoother/ml_smoother.h
+++ packages/ml/src/Smoother/ml_smoother.h
@@ -198,7 +198,7 @@ extern  int ML_Smoother_Hiptmair(void *, int, double *, int, double *);
 extern  int ML_Smoother_Create_Hiptmair_Data(ML_Sm_Hiptmair_Data **data);
 extern  int ML_Smoother_Gen_Hiptmair_Data(ML_Sm_Hiptmair_Data**,
                          ML_Operator*, ML_Operator*, ML_Operator*,
-                         ML_Operator*, int, int*);
+                         ML_Operator*, int, int*, double);
 extern void ML_Smoother_Destroy_Hiptmair_Data(void *data);
 extern  int ML_Smoother_Create_BGS_Data(ML_Sm_BGS_Data **data);
 extern void ML_Smoother_Destroy_BGS_Data(void *data);
