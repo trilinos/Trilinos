0f9455115dd6a96e742ea222724382f0a24dc964
Tamara G. Kolda <tgkolda@sandia.gov>
2001-10-16 20:03:55 +0000

    Changed NLS_MethodManager so that it is dervied from NLS_Method.

--- packages/nox/NLS_MethodManager.C
+++ packages/nox/NLS_MethodManager.C
@@ -1,67 +1,63 @@
 //-----------------------------------------------------------------------------
 // ASCI_NLS_NonlinearSolver
 //-----------------------------------------------------------------------------
-#include <string>
-//#include "NLS_Interface.H"
-#include "NLS_ParameterList.H"
-#include "NLS_Method.H"
-#include "NLS_MethodManager.H"
-//-----------------------------------------------------------------------------
 
-//-----------------------------------------------------------------------------
-// Constructors/Destructors
-//-----------------------------------------------------------------------------
-NLS_MethodManager::NLS_MethodManager(NLS_ParameterList& params)
-  :  ParameterList(params)
-{ Method=0; }
+#include "NLS_MethodManager.H"
+
+NLS_MethodManager::NLS_MethodManager(NLS_Group& i, NLS_ParameterList& p) :
+  method(NULL)
+{
+
+  string method = ParameterList.getParameter("Nonlinear Solver", "Newton");
+
+  cout << "Nonlinear Solver: " << method << endl; 
+  
+  if (method == "Newton") {
+    ptr = new NLS_Newton(i, p);
+  } 
+  else {
+    cout << "ERROR: invalid choice for nonlinear solver "
+	 << "in NLS_MethodManager constructor" << endl;
+    throw 1;
+  }
+
+}
 
 NLS_MethodManager::~NLS_MethodManager()
-{ if (Method != 0) delete Method; }
-
-//-----------------------------------------------------------------------------
-// Create the Global Nonlinear Solver Method
-//-----------------------------------------------------------------------------
-
-void NLS_MethodManager::createMethod()
 {
-  string SearchMethod = ParameterList.getParameter("Search Method", "default");
-  
-  if (SearchMethod=="Newton") {
-    //Method = new NLS_Newton(InitialGuess, ParameterList );
-    cout << "Global Method: Newton" << endl; 
-
-  } else if (SearchMethod=="LineSearchNewton") {
-    //Method = new NLS_Method(InitialGuess, ParameterList );
-    cout << "Global Method: LineSearchNewton" << endl; 
-
-  } else if (SearchMethod=="default") {
-    //Method = new NLS_Newton(InitialGuess, ParameterList );
-    cout << "WARNING: In NLS_MethodManager - no global method found!" << endl;
-    cout << "         Defaulting to Full Newton Method" << endl;
-  } else {
-    cout << "ERROR: In NLS_MethodManager - no global method found!" << endl; 
-    exit (-1);
-  }
+  delete ptr;
 }
-//-----------------------------------------------------------------------------
-// Solve one nonlinear iteration of the Nonlinear System
-//-----------------------------------------------------------------------------
+
+
+void NLS_MethodManager::resetParameters(NLS_ParameterList& p)
+{
+  ptr->resetParameters(p);
+}
+
+bool NLS_MethodManager::isConverged()
+{
+  return ptr->isConverged();
+}
+
 int NLS_MethodManager::iterate()
 {
-  int status=0;
-  status = Method->iterate();
-  return status;
+  return ptr->iterate();
 }
-//-----------------------------------------------------------------------------
-// Solve the Nonlinear System
-//-----------------------------------------------------------------------------
+
 int NLS_MethodManager::solve()
 {
-  int status=0;
-  status = Method->solve();
-  return status;
+  return ptr->solve();
 }
-//-----------------------------------------------------------------------------
+
+NLS_Group& NLS_MethodManager::getSolutionGroup() const
+{
+  return ptr->getSolutionGroup();
+}
+
+bool NLS_MethodManager::getProfileInfo(string& name, NLS_Parameter& p) const
+{
+  return ptr->getProfileInfo(name, p);
+}
 
 
 
