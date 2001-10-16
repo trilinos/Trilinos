01f81e3c6c95e9bf76d66e4e520957d688a5c5ad
Jonathan Hu <jhu@sandia.gov>
2001-10-16 22:36:11 +0000

    Some routines for analyzing two grid convergence.  Based on estimates in "Introduction to Algebraic Multigrid" by Christian Wagner (U. of Heidelberg).

---
+++ packages/ml/src/Utils/ml_twogrid_analysis.h
@@ -0,0 +1,44 @@
+/* ******************************************************************** */
+/* See the file COPYRIGHT for a complete copyright notice, contact      */
+/* person and disclaimer.                                               */        
+/* ******************************************************************** */
+
+/* ******************************************************************** */
+/* Some tools for two grid analysis.                                    */
+/* ******************************************************************** */
+/* Author        : Jonathan Hu (SNL)                                    */
+/* Date          : September, 2001                                      */
+/* ******************************************************************** */
+
+#ifndef __MLTWOGRID__
+#define __MLTWOGRID__
+
+#ifdef __cplusplus
+   extern "C" {
+#endif
+
+#include "ml_include.h"
+
+extern double ML_gdot_H0(ML_Operator *Amat, double *vec1, double *vec2);
+extern double ML_gdot_H1(ML_Operator *Amat, double *vec1, double *vec2);
+extern double ML_gdot_H2(ML_Operator *Amat, double *vec1, double *vec2);
+extern double ML_GetCoarseGridConst(ML_Operator *Amat, ML_Operator *Rmat,
+                                    ML_Operator *Pmat, double *err_h);
+extern double ML_GetSmoothingConst(ML_Operator *Amat, double *err_h,
+                                    ML_Smoother *sm);
+/*
+extern double ML_GetTwoLevelConvergenceFactor(ML_Operator *Amat,
+                                    ML_Operator *Rmat, ML_Operator *Pmat,
+                                    ML_Smoother *sm,
+                                    double *approx_soln, double *exact_soln);
+*/
+double ML_GetTwoLevelConvergenceFactor(ML *ml, double *approx_soln,
+									   double *exact_soln);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /*ifdef __MLTWOGRID__*/
+
