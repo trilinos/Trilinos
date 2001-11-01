d5bbb2a72cd4a1b3ff25dba1e2570bb826c82f41
Karen D. Devine <kddevin@sandia.gov>
2001-11-01 16:39:07 +0000

    New file containing macros and definitions to be shared across Zoltan utilities.  Currently contains only PRINT macros for printing error messages, warnings, trace information (enter/exit subroutines) and debugging info.

---
+++ packages/zoltan/src/Utilities/shared/zoltan_util.h
@@ -0,0 +1,51 @@
+/*****************************************************************************
+ * Zoltan Dynamic Load-Balancing Library for Parallel Applications           *
+ * Copyright (c) 2000, Sandia National Laboratories.                         *
+ * For more info, see the README file in the top-level Zoltan directory.     *  
+ *****************************************************************************/
+/*****************************************************************************
+ * CVS File Information :
+ *    $RCSfile$
+ *    $Author$
+ *    $Date$
+ *    $Revision$
+ ****************************************************************************/
+
+#ifndef __ZOLTAN_UTIL_H
+#define __ZOLTAN_UTIL_H
+
+/*****************************************************************************/
+/* 
+ *  Macros and definitions that are common to all Zoltan modules and 
+ *  utilities.
+ */
+/*****************************************************************************/
+
+/*****************************************************************************/
+/*
+ *  Macros for consistently printing error and warning messages.
+ */
+/*****************************************************************************/
+
+#define ZOLTAN_PRINT_ERROR(proc,yo,str) \
+  fprintf(stderr, "[%d] Zoltan ERROR in %s (line %d of %s):  %s\n", \
+          proc, yo, __LINE__, __FILE__, str);
+
+#define ZOLTAN_PRINT_WARN(proc,yo,str) \
+  fprintf(stderr, "[%d] Zoltan WARNING in %s (line %d of %s):  %s\n", \
+          proc, yo, __LINE__, __FILE__, str);
+
+#define ZOLTAN_TRACE_ENTER(proc,yo,str) \
+  printf("ZOLTAN (Processor %d) Entering %s  %s\n", (proc), (yo), \
+         ((str) ? (str) : " "));
+
+#define ZOLTAN_TRACE_EXIT(proc,yo,str) \
+  printf("ZOLTAN (Processor %d) Leaving %s  %s\n", (proc), (yo), \
+         ((str) ? (str) : " "));
+
+#define ZOLTAN_PRINT_INFO(proc,yo,str) \
+  printf("ZOLTAN (Processor %d) %s: %s\n", (lb)->Proc, (yo), \
+         ((str) ? (str) : " "));
+
+
+#endif /* !__ZOLTAN_UTIL_H */
