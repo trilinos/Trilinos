a8b30141d04d0c500a5038ffcc108409f1df55ec
Tamara G. Kolda <tgkolda@sandia.gov>
2001-11-27 20:38:14 +0000

    Changed to a functor and changed arguments

--- packages/nox/NLS_ForcingTerm.C
+++ packages/nox/NLS_ForcingTerm.C
@@ -13,115 +13,96 @@
 
 #include "NLS_ForcingTerm.H"
 
-NLS_ForcingTerm::NLS_ForcingTerm() 
+double NLS_ForcingTerm::operator()(NLS_ParameterList& p, double normrhs, 
+				   double normoldrhs, double normpredrhs)
 {
-  eta_max = 0.01;
-}
+  const double eta_min = p.getParameter("Minimum Linear Solver Tolerance", 1.0e-6);
+  const double eta_km1 = p.getParameter("Linear Solver Tolerance", 0.0);
+  const double eta_max = 0.01;
 
-NLS_ForcingTerm::~NLS_ForcingTerm() 
-{
-  
-}
-
-double NLS_ForcingTerm::getForcingTerm(NLS_ParameterList& p)
-{
-  eta_min = p.getParameter("Minimum Linear Solver Tolerance", 1.0e-6);
-  eta_km1 = p.getParameter("Linear Solver Tolerance", 0.0);
-
-  string Method = p.getParameter("Forcing Term Method","Failed!");
+  const string Method = p.getParameter("Forcing Term Method", "Failed!");
 
   if (Method == "Constant") {    
 
-    eta_k = eta_min;
-
     if (NLS_Utilities::doPrint(3)) 
-      cout << "      Forcing Term: Constant = " << eta_k << endl << endl;
+      cout << "      Forcing Term: Constant = " << eta_min << "\n" << endl;
 
-    return eta_k;
+    return eta_min;
   }        
 
   else if (Method == "Type 1") {
     
-    residual_norm_km1 = p.getParameter("Residual Norm km1",0.0);
-    residual_norm_k = p.getParameter("Residual Norm k",0.0);
-    residual_norm_linear_model = p.getParameter("Linearized Residual Norm",0.0);
-    
-    eta_k = fabs(residual_norm_k -
-		 residual_norm_linear_model)/residual_norm_km1;
+    double eta_k = fabs(normrhs - normpredrhs) / normoldrhs;
      
     if (NLS_Utilities::doPrint(4)) {
-      cout << "      Forcing Term Type 1" << endl;
+      cout << "      Forcing Term Type 1" << "\n";
       cout << "      Residual Norm k-1 =            " 
-	   << residual_norm_km1 << endl;
+	   << normoldrhs << "\n";
       cout << "      Residual Norm Linear Model k = " 
-	   << residual_norm_linear_model << endl;
+	   << normpredrhs << "\n";
       cout << "      Residual Norm k =              " 
-	   << residual_norm_k << endl;
+	   << normrhs << "\n";
       cout << "      Calculated eta_k =             " << eta_k << endl;
     }
   
     // Impose safeguard and constraints ...
-    alpha = (1.0 + sqrt(5.0))/2.0;
-    eta_k_alpha = pow(eta_km1,alpha);
+    const double alpha = (1.0 + sqrt(5.0)) / 2.0;
+    const double eta_k_alpha = pow(eta_km1, alpha);
     eta_k = max(eta_k, eta_k_alpha);
     eta_k = max(eta_k, eta_min);
     eta_k = min(eta_max, eta_k);
     
     if (NLS_Utilities::doPrint(4)) {
-      cout << "      Applying bounds...             " << endl;
+      cout << "      Applying bounds...             " << "\n";
       cout << "      Final eta_k =                  " << eta_k << endl;
     }
   
     if (NLS_Utilities::doPrint(3)) 
-      cout << "      Forcing Term: Type 1 = "<< eta_k << endl << endl;
+      cout << "      Forcing Term: Type 1 = "<< eta_k << "\n" << endl;
      
     return eta_k;
   }
     
   else if (Method == "Type 2") {  
     
-    alpha = p.getParameter("Forcing Term alpha", 1.5);
-    gamma = p.getParameter("Forcing Term gamma", 0.9);
-    residual_norm_km1 = p.getParameter("Residual Norm km1",0.0);
-    residual_norm_k = p.getParameter("Residual Norm k",0.0);
-    residual_ratio = residual_norm_k/residual_norm_km1;
+    const double alpha = p.getParameter("Forcing Term alpha", 1.5);
+    const double gamma = p.getParameter("Forcing Term gamma", 0.9);
+    const double residual_ratio = normrhs / normoldrhs;
     
-    eta_k = gamma * pow(residual_ratio, alpha);
+    double eta_k = gamma * pow(residual_ratio, alpha);
      
     if (NLS_Utilities::doPrint(4)) {
-      cout << "      Forcing Term Type 2" << endl;
+      cout << "      Forcing Term Type 2" << "\n";
       cout << "      Residual Norm k-1 =            " 
-	   << residual_norm_km1 << endl;
+	   << normoldrhs << "\n";
       cout << "      Residual Norm k =              " 
-	   << residual_norm_k << endl;
+	   << normrhs << "\n";
       cout << "      Calculated eta_k =             " << eta_k << endl;
     }
   
     // Impose safeguard and constraints ... 
-    eta_k_alpha = gamma * pow(eta_km1,alpha);
+    const double eta_k_alpha = gamma * pow(eta_km1, alpha);
     eta_k = max(eta_k, eta_k_alpha);
     eta_k = max(eta_k, eta_min);
     eta_k = min(eta_max, eta_k);
     
     if (NLS_Utilities::doPrint(4)) {
-      cout << "      Applying bounds...             " << endl;
+      cout << "      Applying bounds...             " << "\n";
       cout << "      Final eta_k =                  " << eta_k << endl;
     }
   
     if (NLS_Utilities::doPrint(3)) 
-      cout << "      Forcing Term: Type 2 = "<< eta_k << endl << endl;
+      cout << "      Forcing Term: Type 2 = "<< eta_k << "\n" << endl;
       
     return eta_k;
   }
 
   // Default 
 
-  eta_k = eta_min;
-
   if (NLS_Utilities::doPrint(3)) 
-    cout << "Warning: Forcing Term Method was not set."  << endl 
+    cout << "Warning: Forcing Term Method was not set."  << "\n" 
 	 << "Returning default linear solver tolerance of " 
-	 << eta_k << "." << endl << endl;
+	 << eta_min << "." << "\n" << endl;
 
-  return eta_k;
+  return eta_min;
 }
