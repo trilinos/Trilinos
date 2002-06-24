cad738821f443a273a3048dab0716dbfebd50aa9
Tamara G. Kolda <tgkolda@sandia.gov>
2002-06-24 18:30:02 +0000

    Changed PrecMatrix to Preconditioner throughout.
    Added new function applyJacobianInverse.
    
    Changed some of the functions from pure virtual to virtual - this is
    to allow simple defaults for functions that might not be supported in
    derived objects. This will also be useful as we add additional
    functionality to the Group.
    
    Added new file NOX_Abstract_Group.C to define defaults for the virtual
    functions.

---
+++ packages/nox/src/NOX_Abstract_Group.C
@@ -0,0 +1,104 @@
+// $Id$ 
+// $Source$ 
+
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//   
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//   
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// 
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov).
+// 
+// ************************************************************************
+//@HEADER
+
+#include "NOX_Abstract_Group.H"
+
+using namespace NOX;
+using namespace NOX::Abstract;
+
+bool Group::computeJacobian()
+{
+  return false;
+}
+
+bool Group::computeGradient()
+{
+  return false;
+}
+
+bool Group::computeNewton(NOX::Parameter::List& params)
+{
+  return false;
+}
+
+bool Group::computePreconditioner()
+{
+  return false;
+}
+
+bool Group::applyJacobian(const Vector& input, Vector& result) const
+{
+  return false;
+}
+
+bool Group::applyJacobianTranspose(const Vector& input, Vector& result) const
+{
+  return false;
+}
+
+bool Group::applyJacobianInverse(const Vector& input, Vector& result) const
+{
+  return false;
+}
+
+bool Group::applyPreconditionerInverse(const Vector& input, Vector& result) const
+{
+  return false;
+}
+
+bool Group::isJacobian() const
+{
+  return false;
+}
+
+bool Group::isGradient() const
+{
+  return false;
+}
+
+bool Group::isNewton() const
+{
+  return false;
+}
+
+bool Group::isPreconditioner() const
+{
+  return false;
+}
+
+
+
+
+
+
+
+
