a3b765ffafe4fbc15a24abc1546ebf7ec88a79ab
Robert Heaphy <rheaphy>
2002-08-07 19:06:53 +0000

    Continued replacing zz->Proc with an integer proc. Now reads Umit's test files (however this has not been tested with multiple weights for an object (edge or vertex), and some combination of vertex and edge weights.) Reading Umit's testfiles is transparent to the user, the software detects the type of testfile and automatically adjusts.

--- packages/zoltan/src/hg/zoltan_hg_readfile.c
+++ packages/zoltan/src/hg/zoltan_hg_readfile.c
@@ -21,13 +21,43 @@ extern "C" {
 
 #define BUF_LEN 1000000
 
+
 int Zoltan_HG_Readfile   (int, FILE*, int*, int*, int*, int**, int**, int*, float**, int*, float**) ;
-static int ibm_readfile  (int, FILE*, int*, int*, int*, int**, int**, int*, float**, int*, float**) ;
+static int old_readfile  (int, FILE*, int*, int*, int*, int**, int**, int*, float**, int*, float**) ;
 static int umit_readfile (int, FILE*, int*, int*, int*, int**, int**, int*, float**, int*, float**) ;
+
 
 
 int Zoltan_HG_Readfile (
  int Proc,
+ FILE *f,
+ int *nVtx, int *nEdge, int *nPin,
+ int **hindex,   int **hvertex,
+ int *vwgt_dim, float **vwgt,
+ int *ewgt_dim, float **ewgt)
+    {
+    int err = ZOLTAN_OK ;
+    char string[81], *s, errstr[200];
+    char *yo = "Zoltan_HG_Readfile" ;
+
+    if (!fgets (string, 80, f))
+       {
+       sprintf(errstr, "ERROR...not able to read input file\n");
+       ZOLTAN_PRINT_WARN (Proc, yo, errstr) ;
+       return ZOLTAN_WARN;
+       }
+    s = strtok(string, " \t\n") ;
+    if (*s == '%' || atoi(s) < 2)
+       err = umit_readfile (Proc,f,nVtx,nEdge,nPin,hindex,hvertex,vwgt_dim,vwgt,ewgt_dim,ewgt) ;
+    else if (atoi(s) > 1)
+       err = old_readfile  (Proc,f,nVtx,nEdge,nPin,hindex,hvertex,vwgt_dim,vwgt,ewgt_dim,ewgt) ;
+
+    return  err ;
+    }
+
+
+
+int old_readfile (int Proc,
  FILE *f,
  int *nVtx, int *nEdge, int *nPin,
  int **index,   int **vertex,
@@ -38,7 +68,7 @@ int Zoltan_HG_Readfile (
     char errstr[200] ;
     int Hedge=0, code=0, pin, i;
     char string[BUF_LEN], *s;
-    char *yo = "ibm_readfile" ;
+    char *yo = "old_readfile" ;
 
     /* TODO: edge weights, multiple edge/vertex weights */
 
@@ -121,12 +151,130 @@ int Zoltan_HG_Readfile (
        ZOLTAN_PRINT_WARN (Proc, yo, errstr) ;
        return ZOLTAN_WARN;
        }
-printf ("friday returning from ibm read routine\n") ;
-
     return ZOLTAN_OK;
     }
 
 
+
+int umit_readfile (int Proc,
+ FILE *f,
+ int *nVtx, int *nEdge, int *nPin,
+ int **index,   int **vertex,
+ int *vwgt_dim, float **vwgt,
+ int *ewgt_dim, float **ewgt)
+    {
+    int i, j ;
+    int count ;
+    char errstr[200], tmpstr[200] ;
+    int Hedge=0, code=0, dims=1, pin, offset=0;
+    char string[BUF_LEN], *s;
+    char *yo = "old_readfile" ;
+
+    /* TODO: edge weights, multiple edge/vertex weights */
+
+    rewind(f) ;
+    while (fgets (string, 80, f)!=NULL)
+       {
+       sscanf (string, "%s", tmpstr) ;
+       if (tmpstr[0] == '%')
+          continue ;
+
+       count = sscanf (string, "%d %d %d %d %d %d", &offset, nVtx, nEdge, nPin, &code, &dims) ;
+       if (count <  4)
+          {
+          sprintf (errstr, "ERROR, control line of file must be: offset |V| |E| |P| (code) (dims)\n");
+          ZOLTAN_PRINT_WARN (Proc, yo, errstr) ;
+          return ZOLTAN_WARN ;
+          }
+       break ;
+       }
+
+    /* nEdge HYPEREDGE LINES */
+    if (!((*index)  = (int *) ZOLTAN_MALLOC (sizeof (int) * (*nEdge+1))) ||
+        !((*vertex) = (int *) ZOLTAN_MALLOC (sizeof (int) *  *nPin)))
+           {
+           ZOLTAN_FREE ((void **) index) ;
+           ZOLTAN_FREE ((void **) vertex) ;
+           ZOLTAN_PRINT_ERROR(zz->Proc, yo, "Insufficient memory.");
+           return ZOLTAN_MEMERR;
+           }
+    if (code==2 || code==3)
+       {
+       *ewgt = (float *) ZOLTAN_MALLOC (sizeof (float) * *nEdge) ;
+       if (*ewgt == NULL)
+          {
+          ZOLTAN_PRINT_ERROR(Proc, yo, "Insufficient memory.");
+          return ZOLTAN_MEMERR;
+          }
+       }
+
+    Hedge = 0 ;
+    j = 0 ;
+    for (i = 0 ; i < *nEdge; i++)
+       {
+       (*index)[i] = Hedge;
+
+       if (!(fgets (string, BUF_LEN, f)))
+          {
+          sprintf(errstr, "ERROR... read hvertex %d\n",i);
+          ZOLTAN_PRINT_WARN (Proc, yo, errstr) ;
+          return ZOLTAN_WARN;
+          }
+
+       s = strtok(string," \n");
+       if (s == NULL)
+          continue ;
+       if (*s == '%')
+          {
+          i-- ;       /* don't count comment line in data file */
+          continue ;
+          }
+       if (code == 2 || code == 3)
+          {
+          (*ewgt)[j++] = (float) atoi(s) ;
+          s = strtok (NULL, " \t\n") ;
+          }
+       while (s != NULL)
+          {
+          pin = atoi(s)-offset ;
+          s = strtok(NULL," \n\t");
+          if (s == NULL)
+             break ;
+          if (pin < 0 || pin >= *nVtx)
+              {
+              sprintf(errstr, "ERROR...pin %d of vertex %d is out of range [%d,%d]!\n",
+               pin,i+1,1-offset, *nVtx-offset);
+              ZOLTAN_PRINT_WARN (Proc, yo, errstr) ;
+              return ZOLTAN_WARN;
+              }
+          (*vertex)[Hedge++] = pin ;
+          }
+       }
+    (*index)[*nEdge] = Hedge;
+
+    if (code == 0  || code == 2)
+       return ZOLTAN_OK ;
+
+    /* nVtx vertex weights */
+    if (!((*vwgt) = (float *) ZOLTAN_MALLOC (sizeof (float) * *nVtx)))
+        {
+        ZOLTAN_PRINT_ERROR (Proc, yo, "Insufficient memory for vwgt.");
+        return ZOLTAN_MEMERR;
+        }
+    i = 0 ;
+    while (fgets (string, BUF_LEN, f) != NULL && i < *nVtx)
+        {
+        s = strtok (string, " \n") ;
+        if (*s == '%')
+            continue ;
+        while (s != NULL && i < *nVtx)
+           {
+           (*vwgt)[i++] = (float)(atoi(s));
+           s = strtok (NULL, " \t\n") ;
+           }
+        }
+    return ZOLTAN_OK ;
+    }
 
 #ifdef __cplusplus
 } /* closing bracket for extern "C" */
