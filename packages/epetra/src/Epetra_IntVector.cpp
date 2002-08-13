ad035a60d587abc1fb5e4a1e92da87118cef2677
Michael A. Heroux <maherou@sandia.gov>
2002-08-13 05:20:16 +0000

    ----------------------------------------------------------------------
     Added Files:
     	Epetra_IntVector.cpp Epetra_IntVector.h
     ----------------------------------------------------------------------
    
    Added a simple distributed integer vector class.  I have used Epetra_Vector as
    a crutch once too often today, and finally broke down and wrote an integer equivalent.
    The primary purpose of this class is to distribute integer information across the
    parallel machine.  This kind of functionality is needed as a tool in many practical
    settings.  Until now I have loaded and unloaded the integers into a double Epetra_Vector,
    but this is much cleaner and only took a couple hours to write.

---
+++ packages/epetra/src/Epetra_IntVector.cpp
@@ -0,0 +1,489 @@
+
+/* Copyright (2001) Sandia Corportation. Under the terms of Contract 
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this 
+ * work by or on behalf of the U.S. Government.  Export of this program
+ * may require a license from the United States Government. */
+
+
+/* NOTICE:  The United States Government is granted for itself and others
+ * acting on its behalf a paid-up, nonexclusive, irrevocable worldwide
+ * license in ths data to reproduce, prepare derivative works, and
+ * perform publicly and display publicly.  Beginning five (5) years from
+ * July 25, 2001, the United States Government is granted for itself and
+ * others acting on its behalf a paid-up, nonexclusive, irrevocable
+ * worldwide license in this data to reproduce, prepare derivative works,
+ * distribute copies to the public, perform publicly and display
+ * publicly, and to permit others to do so.
+ * 
+ * NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED STATES DEPARTMENT
+ * OF ENERGY, NOR SANDIA CORPORATION, NOR ANY OF THEIR EMPLOYEES, MAKES
+ * ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LEGAL LIABILITY OR
+ * RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR USEFULNESS OF ANY
+ * INFORMATION, APPARATUS, PRODUCT, OR PROCESS DISCLOSED, OR REPRESENTS
+ * THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS. */
+
+
+#include "Epetra_IntVector.h"
+#include "Epetra_Map.h"
+#include "Epetra_Comm.h"
+//=============================================================================
+Epetra_IntVector::Epetra_IntVector(const Epetra_BlockMap& Map)
+  : Epetra_DistObject(Map, "Epetra::IntVector"),
+    UserAllocated_(false),
+    Allocated_(false)
+{
+  AllocateForCopy();
+}
+//=============================================================================
+Epetra_IntVector::Epetra_IntVector(const Epetra_IntVector& Source)
+  : Epetra_DistObject(Source),
+    UserAllocated_(Source.UserAllocated_),
+    Allocated_(Source.Allocated_)
+{
+  AllocateForCopy();
+  DoCopy(Source.Values_);
+}
+//=============================================================================
+Epetra_IntVector::Epetra_IntVector(Epetra_DataAccess CV, const Epetra_BlockMap& Map, int *V)
+  : Epetra_DistObject(Map, "Epetra::IntVector"),
+    UserAllocated_(false),
+    Allocated_(false)
+{
+  if (CV==Copy) {
+    AllocateForCopy();
+    DoCopy(V);
+  }
+  else {
+    AllocateForView();
+    DoView(V);
+  }
+}
+//=========================================================================
+Epetra_IntVector::~Epetra_IntVector(){}
+
+//=========================================================================
+int Epetra_IntVector::AllocateForCopy()
+{
+  
+  if (Allocated_) return(0);
+    
+  Values_ = new int[MyLength()];
+
+  Allocated_ = true;
+  UserAllocated_ = false;
+  return(0);
+}
+
+//=========================================================================
+int Epetra_IntVector::DoCopy(int * V)
+{
+  int iend = MyLength();
+  for (int i=0; i<iend; i++) Values_[i] = V[i];
+
+  return(0);
+}
+//=========================================================================
+int Epetra_IntVector::AllocateForView()
+{
+
+  Allocated_ = true;
+  UserAllocated_ = true;
+  
+  return(0);
+}
+
+//=========================================================================
+int Epetra_IntVector::DoView(int * V)
+{
+  
+  Values_ = V;
+
+  return(0);
+}
+//=============================================================================
+int Epetra_IntVector::ExtractCopy(int *V) const
+{
+  
+  int iend = MyLength();
+  for (int i=0; i<iend; i++) V[i] = Values_[i];
+  return(0);
+}
+
+//=============================================================================
+int Epetra_IntVector::ExtractView(int **V) const
+{
+  *V = Values_;
+  return(0);
+}
+//========================================================================
+Epetra_IntVector& Epetra_IntVector::operator = (const Epetra_IntVector& V) {
+  
+
+  if (MyLength() != V.MyLength())
+    throw ReportError("Length of IntVectors incompatible in Assign.  The this IntVector has MyLength = " + toString(MyLength())
+		      + ".  The V IntVector has MyLength = " + toString(V.MyLength()), -1);
+  
+  int iend = MyLength();
+  for (int i=0; i<iend; i++) Values_[i] =V[i];
+  return(*this);
+}
+
+void Epetra_IntVector::Print(ostream& os) const {
+  int MyPID = Map().Comm().MyPID();
+  int NumProc = Map().Comm().NumProc();
+  
+  for (int iproc=0; iproc < NumProc; iproc++) {
+    if (MyPID==iproc) {
+      int NumMyElements1 =Map(). NumMyElements();
+      int MaxElementSize1 = Map().MaxElementSize();
+      int * MyGlobalElements1 = Map().MyGlobalElements();
+      int * FirstPointInElementList1;
+      if (MaxElementSize1!=1) FirstPointInElementList1 = Map().FirstPointInElementList();
+
+      if (MyPID==0) {
+	os.width(8);
+	os <<  "     MyPID"; os << "    ";
+	os.width(12);
+	if (MaxElementSize1==1)
+	  os <<  "GID  ";
+	else
+	  os <<  "     GID/Point";
+	os.width(20);
+	os <<  "Value  ";
+	os << endl;
+      }
+      for (int i=0; i < NumMyElements1; i++) {
+	for (int ii=0; ii< Map().ElementSize(ii); ii++) {
+	  int iii;
+	  os.width(10);
+	  os <<  MyPID; os << "    ";
+	  os.width(10);
+	  if (MaxElementSize1==1) {
+	    os << MyGlobalElements1[i] << "    ";
+	    iii = i;
+	  }
+	  else {
+	    os <<  MyGlobalElements1[i]<< "/" << ii << "    ";
+	    iii = FirstPointInElementList1[i]+ii;
+	  }
+	      os.width(20);
+	      os <<  Values_[iii];
+	  os << endl;
+	}
+      }
+      os << flush; 
+    }
+
+    // Do a few global ops to give I/O a chance to complete
+    Map().Comm().Barrier();
+    Map().Comm().Barrier();
+    Map().Comm().Barrier();
+  }
+  return;
+}
+//=========================================================================
+int Epetra_IntVector::CheckSizes(const Epetra_DistObject& Source) {
+  return(0);
+}
+
+//=========================================================================
+int Epetra_IntVector::CopyAndPermute(const Epetra_DistObject& Source, int NumSameIDs, 
+				       int NumPermuteIDs, int * PermuteToLIDs, 
+				       int *PermuteFromLIDs) {
+
+  const Epetra_IntVector & A = dynamic_cast<const Epetra_IntVector &>(Source);
+
+  int * From;
+  A.ExtractView(&From);
+  int *To = Values_;
+
+  int * ToFirstPointInElementList = 0;
+  int * FromFirstPointInElementList = 0;
+  int * FromElementSizeList = 0;
+  int MaxElementSize = Map().MaxElementSize();
+  bool ConstantElementSize = Map().ConstantElementSize();
+
+  if (!ConstantElementSize) {
+    ToFirstPointInElementList =   Map().FirstPointInElementList();
+    FromFirstPointInElementList = A.Map().FirstPointInElementList();
+    FromElementSizeList = A.Map().ElementSizeList();
+  }
+  int j, jj, jjj, k;
+  
+  int NumSameEntries;
+
+  bool Case1 = false;
+  bool Case2 = false;
+  // bool Case3 = false;
+
+  if (MaxElementSize==1) {
+    Case1 = true;
+    NumSameEntries = NumSameIDs;
+  }
+  else if (ConstantElementSize) {
+    Case2 = true;
+    NumSameEntries = NumSameIDs * MaxElementSize;
+  }
+  else {
+    // Case3 = true;
+    NumSameEntries = FromFirstPointInElementList[NumSameIDs];
+  }
+
+  // Short circuit for the case where the source and target vector is the same.
+  if (To==From) NumSameEntries = 0;
+  
+  // Do copy first
+  if (NumSameIDs>0)
+    if (To!=From) {
+	for (j=0; j<NumSameEntries; j++)
+	  To[j] = From[j];
+    }
+  // Do local permutation next
+  if (NumPermuteIDs>0) {
+  
+    // Point entry case
+    if (Case1) {
+      
+      for (j=0; j<NumPermuteIDs; j++) 
+	To[PermuteToLIDs[j]] = From[PermuteFromLIDs[j]];
+    }
+    // constant element size case
+    else if (Case2) {
+      
+      for (j=0; j<NumPermuteIDs; j++) {
+	jj = MaxElementSize*PermuteToLIDs[j];
+	jjj = MaxElementSize*PermuteFromLIDs[j];
+	  for (k=0; k<MaxElementSize; k++)
+	    To[jj+k] = From[jjj+k];
+      }
+    }
+    
+    // variable element size case
+    else {
+      
+      for (j=0; j<NumPermuteIDs; j++) {
+	jj = ToFirstPointInElementList[PermuteToLIDs[j]];
+	jjj = FromFirstPointInElementList[PermuteFromLIDs[j]];
+	int ElementSize = FromElementSizeList[PermuteFromLIDs[j]];
+	  for (k=0; k<ElementSize; k++)
+	    To[jj+k] = From[jjj+k];
+      }
+    }
+  }
+  return(0);
+}
+
+//=========================================================================
+int Epetra_IntVector::PackAndPrepare(const Epetra_DistObject & Source, int NumExportIDs, int * ExportLIDs,
+				      int Nsend, int Nrecv,
+				      int & LenExports, char * & Exports, int & LenImports, 
+				      char * & Imports, 
+				      int & SizeOfPacket, Epetra_Distributor & Distor) {
+
+
+  const Epetra_IntVector & A = dynamic_cast<const Epetra_IntVector &>(Source);
+
+  int i, j, jj, k;
+
+  int  * From;
+  A.ExtractView(&From);
+  int MaxElementSize = Map().MaxElementSize();
+  bool ConstantElementSize = Map().ConstantElementSize();
+
+  int * FromFirstPointInElementList = 0;
+  int * FromElementSizeList = 0;
+
+  if (!ConstantElementSize) {
+    FromFirstPointInElementList = A.Map().FirstPointInElementList();
+    FromElementSizeList = A.Map().ElementSizeList();
+  }
+
+  int * IntExports = 0;
+  int * IntImports = 0;
+
+  if (Nsend>LenExports) {
+    if (LenExports>0) delete [] Exports;
+    LenExports = Nsend;
+    IntExports = new int[LenExports];
+    Exports = (char *) IntExports;
+  }
+
+  if (Nrecv>LenImports) {
+    if (LenImports>0) delete [] Imports;
+    LenImports = Nrecv;
+    IntImports = new int[LenImports];
+    Imports = (char *) IntImports;
+  }
+
+  SizeOfPacket = Map().MaxElementSize() * sizeof(int); 
+
+  int * ptr;
+
+  if (NumExportIDs>0) {
+    ptr = (int *) Exports;
+    
+    
+    // Point entry case
+    if (MaxElementSize==1) for (j=0; j<NumExportIDs; j++) *ptr++ = From[ExportLIDs[j]];
+
+    // constant element size case
+    else if (ConstantElementSize) {
+      
+      for (j=0; j<NumExportIDs; j++) {
+	jj = MaxElementSize*ExportLIDs[j];
+	  for (k=0; k<MaxElementSize; k++)
+	    *ptr++ = From[jj+k];
+      }
+    }
+    
+    // variable element size case
+    else {
+      
+      int SizeOfPacket = MaxElementSize;
+      for (j=0; j<NumExportIDs; j++) {
+	ptr = (int *) Exports + j*SizeOfPacket;
+	jj = FromFirstPointInElementList[ExportLIDs[j]];
+	int ElementSize = FromElementSizeList[ExportLIDs[j]];
+	  for (k=0; k<ElementSize; k++)
+	    *ptr++ = From[jj+k];
+      }
+    }
+  }
+
+  return(0);
+}
+
+//=========================================================================
+int Epetra_IntVector::UnpackAndCombine(const Epetra_DistObject & Source,
+					 int NumImportIDs, int * ImportLIDs, 
+					char * Imports, int & SizeOfPacket, 
+					 Epetra_Distributor & Distor, 
+					 Epetra_CombineMode CombineMode ) {
+  int i, j, jj, k;
+  
+  if(    CombineMode != Add
+      && CombineMode != Zero
+      && CombineMode != Insert
+      && CombineMode != Average
+      && CombineMode != AbsMax )
+    EPETRA_CHK_ERR(-1); //Unsupported CombinedMode, will default to Zero
+
+  if (NumImportIDs<=0) return(0);
+
+  int * To = Values_;
+  int MaxElementSize = Map().MaxElementSize();
+  bool ConstantElementSize = Map().ConstantElementSize();
+
+  int * ToFirstPointInElementList = 0;
+  int * ToElementSizeList = 0;
+
+  if (!ConstantElementSize) {
+    ToFirstPointInElementList = Map().FirstPointInElementList();
+    ToElementSizeList = Map().ElementSizeList();
+  }
+  
+  int * ptr;
+  // Unpack it...
+
+  ptr = (int *) Imports;
+    
+  // Point entry case
+  if (MaxElementSize==1) {
+      
+      if (CombineMode==Add)
+	for (j=0; j<NumImportIDs; j++) To[ImportLIDs[j]] += *ptr++; // Add to existing value
+      else if(CombineMode==Insert)
+	for (j=0; j<NumImportIDs; j++) To[ImportLIDs[j]] = *ptr++;
+      else if(CombineMode==AbsMax)
+        for (j=0; j<NumImportIDs; j++) To[ImportLIDs[j]] = EPETRA_MAX( To[ImportLIDs[j]],abs(*ptr++)); //
+      // Note:  The following form of averaging is not a true average if more that one value is combined.
+      //        This might be an issue in the future, but we leave this way for now.
+      else if(CombineMode==Average)
+	for (j=0; j<NumImportIDs; j++) {To[ImportLIDs[j]] += *ptr++; To[ImportLIDs[j]] /= 2;}
+  }
+
+  // constant element size case
+
+  else if (ConstantElementSize) {
+   
+    if (CombineMode==Add) {
+      for (j=0; j<NumImportIDs; j++) {
+	jj = MaxElementSize*ImportLIDs[j];
+	  for (k=0; k<MaxElementSize; k++)
+	    To[jj+k] += *ptr++; // Add to existing value
+      }
+    }
+    else if(CombineMode==Insert) {
+      for (j=0; j<NumImportIDs; j++) {
+	jj = MaxElementSize*ImportLIDs[j];
+	  for (k=0; k<MaxElementSize; k++)
+	    To[jj+k] = *ptr++;
+      }
+    }
+    else if(CombineMode==AbsMax) {
+      for (j=0; j<NumImportIDs; j++) {
+	jj = MaxElementSize*ImportLIDs[j];
+	  for (k=0; k<MaxElementSize; k++)
+	    To[jj+k] = EPETRA_MAX( To[jj+k], abs(*ptr++) );
+      }
+    }
+    // Note:  The following form of averaging is not a true average if more that one value is combined.
+    //        This might be an issue in the future, but we leave this way for now.
+    else if(CombineMode==Average) {
+      for (j=0; j<NumImportIDs; j++) {
+	jj = MaxElementSize*ImportLIDs[j];
+	  for (k=0; k<MaxElementSize; k++)
+	    { To[jj+k] += *ptr++; To[jj+k] /= 2;}
+      }
+    }
+  }
+    
+  // variable element size case
+
+  else {
+      
+    int SizeOfPacket = MaxElementSize;
+
+    if (CombineMode==Add) {
+      for (j=0; j<NumImportIDs; j++) {
+	ptr = (int *) Imports + j*SizeOfPacket;
+	jj = ToFirstPointInElementList[ImportLIDs[j]];
+	int ElementSize = ToElementSizeList[ImportLIDs[j]];
+	  for (k=0; k<ElementSize; k++)
+	    To[jj+k] += *ptr++; // Add to existing value
+      }
+    }
+    else  if(CombineMode==Insert){
+      for (j=0; j<NumImportIDs; j++) {
+	ptr = (int *) Imports + j*SizeOfPacket;
+	jj = ToFirstPointInElementList[ImportLIDs[j]];
+	int ElementSize = ToElementSizeList[ImportLIDs[j]];
+	  for (k=0; k<ElementSize; k++)
+	    To[jj+k] = *ptr++;
+      }
+    }
+    else  if(CombineMode==AbsMax){
+      for (j=0; j<NumImportIDs; j++) {
+	ptr = (int *) Imports + j*SizeOfPacket;
+	jj = ToFirstPointInElementList[ImportLIDs[j]];
+	int ElementSize = ToElementSizeList[ImportLIDs[j]];
+	  for (k=0; k<ElementSize; k++)
+	    To[jj+k] = EPETRA_MAX( To[jj+k], abs(*ptr++) );
+      }
+    }
+    // Note:  The following form of averaging is not a true average if more that one value is combined.
+    //        This might be an issue in the future, but we leave this way for now.
+    else if(CombineMode==Average) {
+      for (j=0; j<NumImportIDs; j++) {
+	ptr = (int *) Imports + j*SizeOfPacket;
+	jj = ToFirstPointInElementList[ImportLIDs[j]];
+	int ElementSize = ToElementSizeList[ImportLIDs[j]];
+	  for (k=0; k<ElementSize; k++)
+	    { To[jj+k] += *ptr++; To[jj+k] /= 2;}
+      }
+    }
+  }
+  
+  return(0);
+}
+
---
+++ packages/epetra/src/Epetra_IntVector.h
@@ -0,0 +1,236 @@
+
+/* Copyright (2001) Sandia Corportation. Under the terms of Contract 
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this 
+ * work by or on behalf of the U.S. Government.  Export of this program
+ * may require a license from the United States Government. */
+
+
+/* NOTICE:  The United States Government is granted for itself and others
+ * acting on its behalf a paid-up, nonexclusive, irrevocable worldwide
+ * license in ths data to reproduce, prepare derivative works, and
+ * perform publicly and display publicly.  Beginning five (5) years from
+ * July 25, 2001, the United States Government is granted for itself and
+ * others acting on its behalf a paid-up, nonexclusive, irrevocable
+ * worldwide license in this data to reproduce, prepare derivative works,
+ * distribute copies to the public, perform publicly and display
+ * publicly, and to permit others to do so.
+ * 
+ * NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED STATES DEPARTMENT
+ * OF ENERGY, NOR SANDIA CORPORATION, NOR ANY OF THEIR EMPLOYEES, MAKES
+ * ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LEGAL LIABILITY OR
+ * RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR USEFULNESS OF ANY
+ * INFORMATION, APPARATUS, PRODUCT, OR PROCESS DISCLOSED, OR REPRESENTS
+ * THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS. */
+
+#ifndef _EPETRA_INTVECTOR_H_
+#define _EPETRA_INTVECTOR_H_
+
+#include "Epetra_DistObject.h"
+#include "Epetra_BlockMap.h"
+class Epetra_Map;
+
+//! Epetra_IntVector: A class for constructing and using dense integer vectors on a parallel computer.
+
+/*! The Epetra_IntVector class enables the construction and use of integer
+     dense vectors in a distributed memory environment.  The distribution of the dense
+    vector is determined in part by a Epetra_Comm object and a Epetra_Map (or Epetra_LocalMap
+    or Epetra_BlockMap).
+
+
+<b> Distributed Global vs. Replicated Local</b>
+<ul>
+  <li> Distributed Global Vectors - In most instances, a multi-vector will be partitioned
+       across multiple memory images associated with multiple processors.  In this case, there is 
+       a unique copy of each element and elements are spread across all processors specified by 
+       the Epetra_Comm communicator.
+  <li> Replicated Local Vectors - Some algorithms use vectors that are too small to
+       be distributed across all processors.  Replicated local vectors handle
+       these types of situation.
+</ul>
+
+<b>Constructing Epetra_IntVectors</b>
+
+There are four Epetra_IntVector constructors.  The first is a basic constructor that allocates
+space and sets all values to zero, the second is a 
+copy constructor. The third and fourth constructors work with user data.  These constructors have
+two data access modes:
+<ol>
+  <li> Copy mode - Allocates memory and makes a copy of the user-provided data. In this case, the
+       user data is not needed after construction.
+  <li> View mode - Creates a "view" of the user data. In this case, the
+       user data is required to remain intact for the life of the vector.
+</ol>
+
+\warning View mode is \e extremely dangerous from a data hiding perspective.
+Therefore, we strongly encourage users to develop code using Copy mode first and 
+only use the View mode in a secondary optimization phase.
+
+All Epetra_IntVector constructors require a map argument that describes the layout of elements
+on the parallel machine.  Specifically, 
+\c map is a Epetra_Map, Epetra_LocalMap or Epetra_BlockMap object describing the desired
+memory layout for the vector.
+
+There are four different Epetra_IntVector constructors:
+<ul>
+  <li> Basic - All values are zero.
+  <li> Copy - Copy an existing vector.
+  <li> Copy from or make view of user int array.
+</ul>
+
+<b>Extracting Data from Epetra_IntVectors</b>
+
+Once a Epetra_IntVector is constructed, it is possible to extract a copy of the values or create
+a view of them.
+
+\warning ExtractView functions are \e extremely dangerous from a data hiding perspective.
+For both ExtractView fuctions, there is a corresponding ExtractCopy function.  We
+strongly encourage users to develop code using ExtractCopy functions first and 
+only use the ExtractView functions in a secondary optimization phase.
+
+There are two Extract functions:
+<ul>
+  <li> ExtractCopy - Copy values into a user-provided array.
+  <li> ExtractView - Set user-provided array to point to Epetra_IntVector data.
+</ul>
+
+
+\warning A Epetra_Map, Epetra_LocalMap or Epetra_BlockMap object is required for all 
+  Epetra_IntVector constructors.
+
+*/
+
+//=========================================================================
+class Epetra_IntVector : public Epetra_DistObject {
+
+  public:
+
+  //@{ \name Constructors/destructors.
+  //! Basic Epetra_IntVector constuctor.
+  /*! Creates a Epetra_IntVector object and fills with zero values.  
+
+    \param In 
+           Map - A Epetra_LocalMap, Epetra_Map or Epetra_BlockMap.
+
+	   \warning Note that, because Epetra_LocalMap
+	   derives from Epetra_Map and Epetra_Map derives from Epetra_BlockMap, this constructor works
+	   for all three types of Epetra map classes.
+
+    \return Pointer to a Epetra_IntVector.
+
+  */
+  Epetra_IntVector(const Epetra_BlockMap& Map);
+
+  //! Epetra_IntVector copy constructor.
+  
+  Epetra_IntVector(const Epetra_IntVector& Source);
+  
+  //! Set vector values from user array.
+  /*!
+    \param In 
+           Epetra_DataAccess - Enumerated type set to Copy or View.
+    \param In 
+           Map - A Epetra_LocalMap, Epetra_Map or Epetra_BlockMap.
+    \param In
+           V - Pointer to an array of integer numbers..
+
+    \return Integer error code, set to 0 if successful.
+
+	   See Detailed Description section for further discussion.
+  */
+  Epetra_IntVector(Epetra_DataAccess CV, const Epetra_BlockMap& Map, int *V);
+
+  //! Epetra_IntVector destructor.  
+    virtual ~Epetra_IntVector ();
+  //@}
+  
+
+  //@{ \name Extraction methods
+
+
+  //! Put vector values into user-provided array.
+  /*!
+    \param Out
+           V - Pointer to memory space that will contain the vector values.  
+
+    \return Integer error code, set to 0 if successful.
+  */
+  int ExtractCopy(int *V) const;
+  
+  //! Set user-provided address of V.
+  /*!
+    \param Out
+           V - Address of a pointer to that will be set to point to the values of the vector.  
+
+    \return Integer error code, set to 0 if successful.
+  */
+  int ExtractView(int **V) const;
+  //@}
+
+  //@{ \name Overloaded operators
+
+  //! = Operator.
+  /*!
+    \param In
+           A - Epetra_IntVector to copy.
+
+    \return Epetra_IntVector.
+  */
+  Epetra_IntVector& operator = (const Epetra_IntVector& Source);
+  
+  //! Element access function.
+  /*!
+    \return V[Index].
+  */
+    int& operator [] (int index) { return Values_[index]; }
+  //! Element access function.
+  /*!
+    \return V[Index].
+  */
+    const int& operator [] (int index) const { return Values_[index]; }
+    //@}
+    
+  //@{ \name Attribute access functions
+
+  //! Returns the local vector length on the calling processor of vectors in the multi-vector.
+  int MyLength() const {return(Map().NumMyPoints());};
+
+  //! Returns the global vector length of vectors in the multi-vector.
+  int GlobalLength() const {return(Map().NumGlobalPoints());};
+  //@}
+
+  //@{ \name I/O methods
+
+  //! Print method
+  virtual void Print(ostream & os) const;
+    //@}
+ private:
+
+  int AllocateForCopy();
+  int DoCopy(int * V);
+  int AllocateForView();
+  int DoView(int * V);
+
+   // Routines to implement Epetra_DistObject virtual methods
+
+  int CheckSizes(const Epetra_DistObject& A);
+  int CopyAndPermute(const Epetra_DistObject & Source, int NumSameIDs, 
+			 int NumPermuteIDs, int * PermuteToLIDs, int * PermuteFromLIDs);
+
+  int PackAndPrepare(const Epetra_DistObject & Source, int NumExportIDs, int * ExportLIDs,
+				      int Nsend, int Nrecv,
+				      int & LenExports, char * & Exports, int & LenImports, 
+				      char * & Imports, 
+				      int & SizeOfPacket, Epetra_Distributor & Distor);
+  
+  int UnpackAndCombine(const Epetra_DistObject & Source,
+		       int NumImportIDs, int * ImportLIDs, 
+		       char * Imports, int & SizeOfPacket, 
+		       Epetra_Distributor & Distor, Epetra_CombineMode CombineMode );
+  int * Values_;
+  int MyLength_;
+  int GlobalLength_;
+  bool UserAllocated_;
+  bool Allocated_;
+};
+
+#endif /* _EPETRA_INTVECTOR_H_ */
