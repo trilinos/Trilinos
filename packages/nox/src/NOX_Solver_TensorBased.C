7c1468c87311a1f77a387fe65b9b3ed84ed1d7ad
Brett W. Bader <bwbader@sandia.gov>
2002-08-15 21:36:31 +0000

    Lots of changes here.  Basically, everything is pretty much self-contained now.  There are 2 line search options (Standard or Curvilinear) that are contained within the class.  Code is also cleaned up and commented better.  I tried to follow the format layed out in Linesearch::Quadratic since that one has had the most number of eyeballs looking at it...
    To use this solver, instantiate a new object of type
    NOX::Solver::TensorBased since the solver is not an option from within
    the Solver::Manager.
    
    Doxygen comments are not fully tested, but I think it is okay.  If you
    find, wrong comments, feel free to modify.  If any algorithmic
    bugs/changes, then please let me know.

--- packages/nox/src/NOX_Solver_TensorBased.C
+++ packages/nox/src/NOX_Solver_TensorBased.C
@@ -56,7 +56,7 @@ void TensorBased::init()
   minStep = lsparams.getParameter("Minimum Step", 1.0e-12);
   defaultStep = lsparams.getParameter("Default Step", 1.0);
   recoveryStep = lsparams.getParameter("Recovery Step", defaultStep);
-  maxiters = lsparams.getParameter("Max Iters", 100);
+  maxIters = lsparams.getParameter("Max Iters", 40);
   alpha = lsparams.getParameter("Alpha Factor", 1.0e-4);
 
   // Set up utilities (i.e., set print processor, etc)
@@ -83,7 +83,7 @@ void TensorBased::init()
     if (Utils::doPrint(Utils::Warning)) {
       cout << "Warning: NOX::Solver::TensorBased::init() - The solution passed "
 	   << "into the solver (either through constructor or reset method) "
-	   << "is already converged!  The solver wil not "
+	   << "is already converged!  The solver will not "
 	   << "attempt to solve this system since status is flagged as "
 	   << "converged." << endl;
     }
@@ -134,7 +134,8 @@ NOX::StatusTest::StatusType TensorBased::iterate()
   bool ok;
   ok = direction.compute(dir, soln, *this);
   if (!ok) {
-    cout << "NOX::Solver::TensorBased::iterate - unable to calculate direction" << endl;
+    cout << "NOX::Solver::TensorBased::iterate - "
+	 << "unable to calculate direction" << endl;
     status = StatusTest::Failed;
     return status;
   }
@@ -146,109 +147,18 @@ NOX::StatusTest::StatusType TensorBased::iterate()
   oldsoln = soln;
 
   // Do line search and compute new soln.
-  /*    ok = linesearch.compute(soln, step, oldsoln, dir);  */
-  {
-    bool isFailed = false;
-    double newf;
-    double oldf = 0.5*oldsoln.getNormF()*oldsoln.getNormF();  
-                            // Redefined f() = 0.5 * F'*F = 0.5*||F||^2
-
-
-    // Computation of directional derivative used in curvature condition
-    // Note that for Newton direction, fprime = -2.0*oldf
-    Abstract::Vector* tmpvecptr = oldsoln.getX().clone(ShapeCopy);
-    oldsoln.applyJacobian(dir,*tmpvecptr);
-    double fprime = tmpvecptr->dot(oldsoln.getF());
-
-    if (Utils::doPrint(Utils::InnerIteration)) {
-      cout << "\n" << Utils::fill(72) << "\n";
-      cout << "-- Std. Tensor Line Search -- \n";
-    }
-  
-    int lsIterations = 1;
-    step = defaultStep;
-    soln.computeX(oldsoln, dir, step);
-    soln.computeF();    
-    
-    newf = 0.5*soln.getNormF()*soln.getNormF();  
-
-    if (newf >= oldf + alpha*step*fprime) {
-
-      totalNumLSSteps++;
-
-      if (Utils::doPrint(Utils::InnerIteration)) {
-	cout << setw(3) << lsIterations << ":";
-	cout << " step = " << Utils::sci(step);
-	cout << " oldf = " << Utils::sci(oldf);
-	cout << " newf = " << Utils::sci(newf);
-	cout << endl;
-      }
-      
-      // Is the tensor step a descent direction?  If not, switch to Newton
-      if (fprime >= 0) {
-	dir = oldsoln.getNewton();
-	oldsoln.applyJacobian(dir,*tmpvecptr);
-	fprime = tmpvecptr->dot(oldsoln.getF());
-      }
-      else {
-	step = step * 0.5;
-      }
-
-      // Update the number of linesearch iterations
-      lsIterations ++;
-      
-      soln.computeX(oldsoln, dir, step);
-      soln.computeF();
-      newf = 0.5*soln.getNormF()*soln.getNormF(); 
-
-      //Iterate until Armijo-Goldstein condition is satisfied
-      while (newf >= oldf + alpha*step*fprime  &&  !isFailed) {  
-      
-	if (Utils::doPrint(Utils::InnerIteration)) {
-	  cout << setw(3) << lsIterations << ":";
-	  cout << " step = " << Utils::sci(step);
-	  cout << " oldf = " << Utils::sci(oldf);
-	  cout << " newf = " << Utils::sci(newf);
-	  cout << endl;
-	}
-	
-	lsIterations ++;
-	step = step * 0.5;
-
-	if ((step < minStep) || (lsIterations > maxiters)) {
-	  totalNumFailedLineSearches++;
-	  isFailed = true;
-	  step = recoveryStep;
-	  cout << Utils::fill(5,' ') << "step = " << Utils::sci(step);
-	  cout << Utils::fill(1,' ') << "oldf = " << Utils::sci(sqrt(2.*oldf));
-	  cout << Utils::fill(1,' ') << "newf = " << Utils::sci(sqrt(2.*newf));
-	  cout << " (USING RECOVERY STEP!)" << endl;
-	  cout << Utils::fill(72) << "\n" << endl;
-	}
-
-	soln.computeX(oldsoln, dir, step);
-	soln.computeF();    
-
-	newf = 0.5*soln.getNormF()*soln.getNormF();
-      }
-    }
-
-    if (Utils::doPrint(Utils::InnerIteration)) {
-      cout << setw(3) << lsIterations << ":";
-      cout << " step = " << Utils::sci(step);
-      cout << " oldf = " << Utils::sci(oldf);
-      cout << " newf = " << Utils::sci(newf);
-      if (isFailed)
-	cout << " (USING RECOVERY STEP!)" << endl;
-      else
-	cout << " (STEP ACCEPTED!)" << endl;
-      cout << Utils::fill(72) << "\n" << endl;
-    }
-
-    ok = !isFailed;
-    totalNumLSIterations += lsIterations - 1;
+  string choice = params.sublist("Line Search").
+    getParameter("Method", "Curvilinear");
+  if (choice == "Curvilinear") 
+    ok = performCurvilinearLinesearch(); 
+  else if (choice == "Standard")
+    ok = performStandardLinesearch();
+  else {
+    cout << "ERROR: NOX::Direction::Tensor::iterate() - the choice of "
+	 << "\"Line Search\" parameter is invalid." << endl;
+    //throw "NOX Error";
+    ok = performCurvilinearLinesearch(); 
   }
-
 
 
   if (!ok) {
@@ -258,7 +168,8 @@ NOX::StatusTest::StatusType TensorBased::iterate()
       return status;
     }
     else if (Utils::doPrint(Utils::Warning))
-      cout << "NOX::Solver::TensorBased::iterate - using recovery step for linesearch" << endl;
+      cout << "NOX::Solver::TensorBased::iterate - "
+	   << "using recovery step for linesearch" << endl;
   }
       
 
@@ -288,16 +199,6 @@ NOX::StatusTest::StatusType TensorBased::solve()
   }
 
   setOutputParameters();
-
-  /*
-  // Print out initialization information
-  if (Utils::doPrint(Utils::Parameters)) {
-    cout << "\n" << Utils::fill(72) << "\n";
-    cout << "\n-- Parameters Used in Nonlinear Solver --\n\n";
-    params.print(cout,5);
-    cout << "\n" << Utils::fill(72) << "\n";
-  }
-  */
 
   return status;
 }
@@ -368,6 +269,7 @@ void TensorBased::printUpdate()
   }
 }
 
+
 bool TensorBased::setOutputParameters() 
 {
   NOX::Parameter::List& outputList = params.sublist("Output");
@@ -382,6 +284,251 @@ bool TensorBased::setOutputParameters()
 			  totalNumLSSteps);
 
   return true;
+}
+
+bool TensorBased::performStandardLinesearch()
+{
+  // Copy pointers into temporary references
+  Abstract::Group& newsoln = *solnptr;
+
+  // Local variables
+  int lsIterations = 1;
+  bool ok;
+  bool isFailed = false;
+  double newf;
+  double oldf = 0.5*oldsoln.getNormF()*oldsoln.getNormF();  
+                            // Redefined f() = 0.5 * F'*F = 0.5*||F||^2
+
+
+  // Computation of directional derivative used in curvature condition
+  // Note that for Newton direction, fprime = -2.0*oldf
+  Abstract::Vector* tmpvecptr = oldsoln.getX().clone(ShapeCopy);
+  oldsoln.applyJacobian(dir,*tmpvecptr);
+  double fprime = tmpvecptr->dot(oldsoln.getF());
+
+  if (Utils::doPrint(Utils::InnerIteration)) {
+    cout << "\n" << Utils::fill(72) << "\n";
+    cout << "-- Std. Tensor Line Search -- \n";
+  }
+
+  // Compute first trial point and its function value
+  step = defaultStep;
+  newsoln.computeX(oldsoln, dir, step);
+  newsoln.computeF();    
+  newf = 0.5*newsoln.getNormF()*newsoln.getNormF();  
+
+  // Compute the convergence criteria for the line search 
+  double convergence = oldf + alpha*step*fprime;
+
+  // Is a linesearch needed?
+  if (newf >= convergence) {
+
+    totalNumLSSteps++;
+
+    if (Utils::doPrint(Utils::InnerIteration)) {
+      cout << setw(3) << lsIterations << ":";
+      cout << " step = " << Utils::sci(step);
+      cout << " oldf = " << Utils::sci(sqrt(2.*oldf));
+      cout << " newf = " << Utils::sci(sqrt(2.*newf));
+      cout << endl;
+    }
+      
+    // Is the tensor step a descent direction?  If not, switch to Newton
+    if (fprime >= 0) {
+      dir = oldsoln.getNewton();
+      oldsoln.applyJacobian(dir,*tmpvecptr);
+      fprime = tmpvecptr->dot(oldsoln.getF());
+    }
+    else {
+      step = step * 0.5;
+    }
+
+    // Update the number of linesearch iterations
+    lsIterations ++;
+      
+    // Compute trial point and its function value
+    newsoln.computeX(oldsoln, dir, step);
+    newsoln.computeF();
+    newf = 0.5*newsoln.getNormF()*newsoln.getNormF(); 
+
+    // Recompute convergence criteria based on new step or new fprime
+    convergence = oldf + alpha*step*fprime;
+
+    //Iterate until Armijo-Goldstein condition is satisfied
+    while (newf >= convergence) {  
+      
+      if (Utils::doPrint(Utils::InnerIteration)) {
+	cout << setw(3) << lsIterations << ":";
+	cout << " step = " << Utils::sci(step);
+	cout << " oldf = " << Utils::sci(sqrt(2.*oldf));
+	cout << " newf = " << Utils::sci(sqrt(2.*newf));
+	cout << endl;
+      }
+	
+      lsIterations ++;
+      step = step * 0.5;
+
+      // Check for linesearch failure: if true, exit the method
+      if ((step < minStep) || (lsIterations > maxIters)) {
+
+	totalNumFailedLineSearches += 1;
+	step = recoveryStep;
+
+	// Update the group and exit linesearch
+	newsoln.computeX(oldsoln, dir, step);
+	newsoln.computeF();    
+	newf = 0.5*newsoln.getNormF()*newsoln.getNormF();
+
+	// Print out info
+	cout << Utils::fill(5,' ') << "step = " << Utils::sci(step);
+	cout << Utils::fill(1,' ') << "oldf = " << Utils::sci(sqrt(2.*oldf));
+	cout << Utils::fill(1,' ') << "newf = " << Utils::sci(sqrt(2.*newf));
+	cout << " (USING RECOVERY STEP!)" << endl;
+	cout << Utils::fill(72) << "\n" << endl;
+
+	isFailed = true;
+	return (!isFailed);
+      }
+
+      // Compute new trial point and its function value
+      newsoln.computeX(oldsoln, dir, step);
+      newsoln.computeF();    
+      newf = 0.5*newsoln.getNormF()*newsoln.getNormF();
+
+      // Recompute convergence criteria based on new step
+      convergence = oldf + alpha*step*fprime;
+
+    }
+  }
+
+  if (Utils::doPrint(Utils::InnerIteration)) {
+    cout << setw(3) << lsIterations << ":";
+    cout << " step = " << Utils::sci(step);
+    cout << " oldf = " << Utils::sci(sqrt(2.*oldf));
+    cout << " newf = " << Utils::sci(sqrt(2.*newf));
+    if (isFailed)
+      cout << " (USING RECOVERY STEP!)" << endl;
+    else
+      cout << " (STEP ACCEPTED!)" << endl;
+    cout << Utils::fill(72) << "\n" << endl;
+  }
+  
+  ok = !isFailed;
+  totalNumLSIterations += lsIterations - 1;
+
+  delete tmpvecptr;
+
+  return ok;
+}
+
+
+bool TensorBased::performCurvilinearLinesearch()
+{
+  // Copy pointer into temporary reference
+  Abstract::Group& newsoln = *solnptr;
+
+  // local variables
+  int lsIterations = 1;
+  bool ok;
+  bool isFailed = false;
+  double newf;
+  double oldf = 0.5*oldsoln.getNormF()*oldsoln.getNormF();  
+                            // Redefined f() = 0.5 * F'*F = 0.5*||F||^2
+
+  // Computation of directional derivative used in curvature condition
+  Abstract::Vector* tmpvecptr = oldsoln.getX().clone(ShapeCopy);
+  oldsoln.applyJacobian(oldsoln.getNewton(),*tmpvecptr);   // bwb - dNewton 
+  double fprime = tmpvecptr->dot(oldsoln.getF());
+  
+  if (Utils::doPrint(Utils::InnerIteration)) {
+    cout << "\n" << Utils::fill(72) << "\n";
+    cout << "-- Curvilinear Linesearch -- \n";
+  }
+  
+  // Compute curvilinear step, new trial point, and its function value
+  step = defaultStep;
+  newsoln.computeX(oldsoln, dir, step);
+  newsoln.computeF();    
+  newf = 0.5*newsoln.getNormF()*newsoln.getNormF();  
+  
+  // Compute the convergence criteria for the line search 
+  double convergence = oldf + alpha*step*fprime;
+
+  // Increment the counter of Newton steps requiring a linesearch
+  if (newf >= convergence)
+    totalNumLSSteps++;
+
+  //Iterate until Armijo-Goldstein condition is satisfied
+  while (newf >= convergence) {  
+    
+    if (Utils::doPrint(Utils::InnerIteration)) {
+      cout << setw(3) << lsIterations << ":";
+      cout << " step = " << Utils::sci(step);
+      cout << " oldf = " << Utils::sci(sqrt(2.*oldf));
+      cout << " newf = " << Utils::sci(sqrt(2.*newf));
+      cout << endl;
+    }
+    
+    lsIterations ++;
+
+    // Backtrack by halving the step
+    step = step * 0.5;
+
+    // Check for linesearch failure: if true, exit the method
+    if ((step < minStep) || (lsIterations > maxIters)) {
+
+      totalNumFailedLineSearches += 1;
+      step = recoveryStep;
+
+      // Update the group, and exit linesearch
+      ok = direction.computeCurvilinearStep(dir, newsoln, *this, step);
+      newsoln.computeX(oldsoln, dir, 1.0);
+      newsoln.computeF();    
+      newf = 0.5*newsoln.getNormF()*newsoln.getNormF();
+
+      // Print out info
+      cout << Utils::fill(5,' ') << "step = " << Utils::sci(step);
+      cout << Utils::fill(1,' ') << "oldf = " << Utils::sci(sqrt(2.*oldf));
+      cout << Utils::fill(1,' ') << "newf = " << Utils::sci(sqrt(2.*newf));
+      cout << " (USING RECOVERY STEP!)" << endl;
+      cout << Utils::fill(72) << "\n" << endl;
+
+      isFailed = true;
+      return (!isFailed);
+    }
+
+    // Compute curvilinear step, new trial point, and its function value
+    ok = direction.computeCurvilinearStep(dir, newsoln, *this, step);   
+    newsoln.computeX(oldsoln, dir, 1.0);
+    newsoln.computeF();    
+    newf = 0.5*newsoln.getNormF()*newsoln.getNormF();
+
+    // Recompute convergence criteria based on new step
+    convergence = oldf + alpha*step*fprime;
+
+  }  // end while loop
+
+
+  if (Utils::doPrint(Utils::InnerIteration)) {
+    cout << setw(3) << lsIterations << ":";
+    cout << " step = " << Utils::sci(step);
+    cout << " oldf = " << Utils::sci(sqrt(2.*oldf));
+    cout << " newf = " << Utils::sci(sqrt(2.*newf));
+    cout << " (STEP ACCEPTED!)" << endl;
+    cout << Utils::fill(72) << "\n" << endl;
+  }
+
+#if DEBUG_LEVEL > 1
+  cout << "Current point: ";
+  newsoln.getX().print();
+#endif
+
+  ok = !isFailed;
+  totalNumLSIterations += lsIterations - 1;
+
+  delete tmpvecptr;
+
+  return ok;
 }
 
 
