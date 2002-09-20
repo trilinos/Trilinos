f0427b7e91639b368fabb7ad1d268236621d70a7
Jonathan Hu <jhu@sandia.gov>
2002-09-20 22:29:37 +0000

    Wrappers so that ML can manipulate Epetra operators.  Also necessary (but not sufficient, see Epetra source) for AztecOO to be able to use ML as a preconditioner.

---
+++ packages/ml/src/Utils/ml_epetra_utils.h
@@ -0,0 +1,35 @@
+#ifndef _ML_EPETRA_UTILS_H_
+#define _ML_EPETRA_UTILS_H_
+
+class Epetra_Comm;
+class Epetra_BlockMap;
+class Epetra_MultiVector;
+class Epetra_RowMatrix;
+#include "Epetra_LinearProblem.h"
+#include "Epetra_Object.h"
+#include "Epetra_RowMatrix.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_Map.h"
+#include "Epetra_Vector.h"
+#include "Epetra_Comm.h"
+#include "Epetra_Import.h"
+
+#ifdef PETRA_MPI
+#include "mpi.h"
+#endif
+#include "ml_include.h"
+
+// External prototypes
+extern "C" int Epetra_ML_matvec(void *data, int in, double *p, int out,
+                 double *ap);
+
+extern "C" int Epetra_ML_getrow(void *data, int N_requested_rows,
+                 int requested_rows[], int allocated_space, int columns[],
+                 double values[], int row_lengths[]);
+
+extern "C" int Epetra_ML_comm_wrapper(double vec[], void *data);
+
+extern "C" int EpetraMatrix2MLMatrix(ML *ml_handle, int level,
+                                Epetra_RowMatrix * Amat);
+
+#endif /* _ML_EPETRA_UTILS_H_ */
