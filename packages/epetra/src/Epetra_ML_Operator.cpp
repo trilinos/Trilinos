bad5cf9faa48ac263f61bbb2f5990fd90036fc3c
Michael A. Heroux <maherou@sandia.gov>
2002-09-27 15:17:22 +0000

    ----------------------------------------------------------------------
     Added Files:
     	Epetra_ML_Operator.h Epetra_ML_Operator.cpp
     ----------------------------------------------------------------------
    
    Addresses bug 53.
    
    Adding a new class Epetra_ML_Operator that implements the
    Epetra_Operator abstract interface.  This new class allows ML to be
    used as an Epetra_Operator and therefore as a preconditioner for
    AztecOO.

---
+++ packages/epetra/src/Epetra_ML_Operator.cpp
@@ -0,0 +1,87 @@
+/* Copyright (2001) Sandia Corportation. Under the terms of Contract 
+ * DE-AC04-94AL85000, there is a non-exclusive license for use of this 
+ * work by or on behalf of the U.S. Government.  Export of this program
+ * may require a license from the United States Government. */
+
+
+/* NOTICE:  The United States Government is granted for itself and others
+ * acting on its behalf a paid-up, nonexclusive, irrevocable worldwide
+ * license in ths data to reproduce, prepare derivative works, and
+ * perform publicly and display publicly.  Beginning five (5) years from
+ * July 25, 2001, the United States Government is granted for itself and
+ * others acting on its behalf a paid-up, nonexclusive, irrevocable
+ * worldwide license in this data to reproduce, prepare derivative works,
+ * distribute copies to the public, perform publicly and display
+ * publicly, and to permit others to do so.
+ * 
+ * NEITHER THE UNITED STATES GOVERNMENT, NOR THE UNITED STATES DEPARTMENT
+ * OF ENERGY, NOR SANDIA CORPORATION, NOR ANY OF THEIR EMPLOYEES, MAKES
+ * ANY WARRANTY, EXPRESS OR IMPLIED, OR ASSUMES ANY LEGAL LIABILITY OR
+ * RESPONSIBILITY FOR THE ACCURACY, COMPLETENESS, OR USEFULNESS OF ANY
+ * INFORMATION, APPARATUS, PRODUCT, OR PROCESS DISCLOSED, OR REPRESENTS
+ * THAT ITS USE WOULD NOT INFRINGE PRIVATELY OWNED RIGHTS. */
+
+#ifdef EPETRA_MPI
+#include "Epetra_MpiComm.h"
+#include "mpi.h"
+#endif
+#include "/home/jhu/ml/trilinos/ml/Obj/ml_include.h"
+
+#include "Epetra_ML_Operator.h"
+#include "Epetra_MultiVector.h"
+#include "Epetra_Vector.h"
+#include "Epetra_BlockMap.h"
+
+//==============================================================================
+// constructor -- it's presumed that the user has constructed the ML
+// object somewhere else
+Epetra_ML_Operator::Epetra_ML_Operator(ML *ml_handle, const Epetra_Comm &myComm,const Epetra_BlockMap &dm, const Epetra_BlockMap &rm)
+  : solver_(ml_handle),
+    Label_(0),
+    Comm_(myComm),
+    DomainMap_(dm),
+    RangeMap_(rm) {
+  Label_ = "Epetra ML_Operator";
+}
+//==============================================================================
+Epetra_ML_Operator::~Epetra_ML_Operator() {
+}
+//==============================================================================
+int Epetra_ML_Operator::ApplyInverse(const Epetra_MultiVector& X, Epetra_MultiVector& Y) const {
+
+  if (!X.Map().SameAs(DomainMap())) EPETRA_CHK_ERR(-1);
+  if (!Y.Map().SameAs(RangeMap())) EPETRA_CHK_ERR(-2);
+  if (Y.NumVectors()!=X.NumVectors()) EPETRA_CHK_ERR(-3);
+
+  Epetra_MultiVector xtmp(X); // Make copy of X (needed in case X is scaled
+                              // in solver or if X = Y
+  Y.PutScalar(0.0); // Always start with Y = 0
+  // ML_iterate doesn't handle multivectors, so extract and iterate one at
+  // a time on them.
+  double **xvectors;
+  double **yvectors;
+  int ierr = xtmp.ExtractView(&xvectors);
+  ierr = Y.ExtractView(&yvectors);
+
+  //note: solver_ is the ML handle
+  for (int i=0; i < X.NumVectors(); i++)
+  {
+    switch(solver_->ML_scheme) {
+      case(ML_MGFULLV):
+        ML_Solve_MGFull(solver_,
+                        xvectors[i],  //rhs
+                        yvectors[i]); //solution
+        break;
+      case(ML_SAAMG): //Marian Brezina's solver
+        ML_Solve_AMGV(solver_,
+                      xvectors[i],  //rhs
+                      yvectors[i]); //solution
+        break;
+      default:
+        ML_Solve_MGV(solver_,
+                     xvectors[i],  //rhs
+                     yvectors[i]); //solution
+    }
+  }
+
+}
