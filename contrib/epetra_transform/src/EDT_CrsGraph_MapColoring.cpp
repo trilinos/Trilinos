0b117840be34a22a41068e28ea660b89a4d1d81e
Robert J. Hoekstra <rjhoeks@sandia.gov>
2002-10-22 17:36:43 +0000

    Updated to fully support coloring in parallel environments.
    The additional functionality required was the addition of the connectivity
    to bounding (ghost) indices since they represent distinct non-zeroes in the
    local jacobian.  These bounding indices are now included in the coloring and
    the resulting color map for parallel applications represents the coloring of
    the columns in the original graph.

--- contrib/epetra_transform/src/EDT_CrsGraph_MapColoring.cpp
+++ contrib/epetra_transform/src/EDT_CrsGraph_MapColoring.cpp
@@ -19,56 +19,84 @@ std::auto_ptr<Epetra_MapColoring> CrsGraph_MapColoring::operator()( const Epetra
 
   const Epetra_BlockMap & RowMap = original.RowMap();
   int nRows = RowMap.NumMyElements();
+  const Epetra_BlockMap & ColMap = original.ColMap();
+  int nCols = ColMap.NumMyElements();
+
+  if( verbose_ ) cout << "RowMap:\n" << RowMap;
+  if( verbose_ ) cout << "ColMap:\n" << ColMap;
+
+  Epetra_CrsGraph * base = &( const_cast<Epetra_CrsGraph&>(original) );
 
   int NumIndices;
   int * Indices;
 
+  // For parallel applications, add in boundaries to coloring
+  bool distributedGraph = RowMap.DistributedGlobal();
+  if( distributedGraph )
+  {
+    base = new Epetra_CrsGraph( Copy, ColMap, ColMap, 0 );
+
+    for( int i = 0; i < nRows; ++i )
+    {
+      assert( original.ExtractMyRowView( i, NumIndices, Indices ) == 0 );
+      assert( base->InsertMyIndices( i, NumIndices, Indices ) >= 0 );
+
+      for( int j = 0; j < NumIndices; ++j )
+        if( Indices[j] >= nRows )
+          assert( base->InsertMyIndices( Indices[j], 1, &i ) >= 0 );
+    } 
+
+    base->TransformToLocal();
+  }
+
+  if( verbose_ ) cout << "Base Graph:\n" << *base << endl;
+
   //Generate Local Distance-1 Adjacency Graph
   //(Transpose of original excluding non-local column indices)
-  std::auto_ptr<Epetra_CrsGraph> Adj1 = CrsGraph_Transpose( true )( original );
+  std::auto_ptr<Epetra_CrsGraph> Adj1 = CrsGraph_Transpose( true )( *base );
   if( verbose_ ) cout << "Adjacency 1 Graph!\n" << *Adj1;
 
   int Delta = Adj1->MaxNumIndices();
   cout << endl << "Delta: " << Delta << endl;
 
   //Generation of Local Distance-2 Adjacency Graph
-  Epetra_CrsGraph Adj2( Copy, RowMap, RowMap, 0 );
+  Epetra_CrsGraph Adj2( Copy, ColMap, ColMap, 0 );
   int NumAdj1Indices;
   int * Adj1Indices;
-  for( int i = 0; i < nRows; ++i )
+  for( int i = 0; i < nCols; ++i )
   {
     assert( Adj1->ExtractMyRowView( i, NumAdj1Indices, Adj1Indices ) == 0 );
 
     for( int j = 0; j < NumAdj1Indices; ++j )
     {
-      assert( original.ExtractMyRowView( Adj1Indices[j], NumIndices, Indices ) == 0 );
+      assert( base->ExtractMyRowView( Adj1Indices[j], NumIndices, Indices ) == 0 );
       int NumLocalIndices = 0;
       for( int k = 0; k < NumIndices; ++k )
-        if( Indices[k] < nRows ) NumLocalIndices++; 
+        if( Indices[k] < nCols ) NumLocalIndices++; 
       assert( Adj2.InsertMyIndices( i, NumLocalIndices, Indices ) >= 0 );
     }
   }
   assert( Adj2.TransformToLocal() == 0 );
   if( verbose_ ) cout << "Adjacency 2 Graph!\n" << Adj2;
 
-  vector<int> rowOrder( nRows );
+  vector<int> rowOrder( nCols );
   //Simple reordering
   {
     multimap<int,int> adjMap;
-    for( int i = 0; i < nRows; ++i )
+    for( int i = 0; i < nCols; ++i )
       adjMap.insert( pair<const int,int>( Adj2.NumMyIndices(i), i ) );
     multimap<int,int>::iterator iter = adjMap.begin();
     multimap<int,int>::iterator end = adjMap.end();
     for( int i = 1; iter != end; ++iter, ++i )
-      rowOrder[ nRows - i ] = iter->second;
+      rowOrder[ nCols - i ] = iter->second;
   }
 
-  Epetra_MapColoring * ColorMap = new Epetra_MapColoring( RowMap );
+  Epetra_MapColoring * ColorMap = new Epetra_MapColoring( ColMap );
 
   //Application of Greedy Algorithm to generate Color Map
   int Size = Delta * Delta + 1;
   set<int> allowedColors;
-  for( int col = 0; col < nRows; ++col )
+  for( int col = 0; col < nCols; ++col )
   {
     for( int i = 0; i < Size; ++i ) allowedColors.insert( i+1 ); 
 
@@ -81,6 +109,8 @@ std::auto_ptr<Epetra_MapColoring> CrsGraph_MapColoring::operator()( const Epetra
   }
 
   if( verbose_ ) cout << "ColorMap!\n" << *ColorMap;
+
+  if( distributedGraph ) delete base;
 
   return std::auto_ptr<Epetra_MapColoring> ( ColorMap );
 }
