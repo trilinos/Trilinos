d4cd979251aa057d488015921ffc75822d872277
Roger P. Pawlowski <rppawlo@sandia.gov>
2002-11-20 22:35:37 +0000

    All problems in the "example" directory (including prerelese broyden) have been added to the "example-lapack" directory.  They produce exactly the same results as in the example directory.  To enable the examples for lapack use the following in the configure script:
    --enable-lapack-examples

---
+++ packages/nox/example-lapack/Linear.C
@@ -0,0 +1,255 @@
+// $Id$ 
+// $Source$ 
+
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//   
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//   
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// 
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov).
+// 
+// ************************************************************************
+//@HEADER
+
+/*! 
+  \file Linear.C
+  \brief A simple linear example based on NOX::LAPACK.
+  
+   This is an example of using %NOX with the NOX::LAPACK::Group and
+   NOX::LAPACK::Vector classes. These are very basic classes intended
+   only to illustrate and test NOX. They are based on a combination of
+   C++ STL and LAPACK.
+
+   This example is a simple linear problem.
+   
+   \f[
+   F(x) = Ax -b 
+   \f]
+
+   \f[
+   A = 
+   \left[
+   \begin{array}{cccc}
+   7 & 1 & 2 & 1\\
+   1 & 8 & 3 & 1\\
+   1 & 3 & 9 & 1\\
+   1 & 1 & 1 & 10
+   \end{array}
+   \right]
+
+   b = \left[
+   \begin{array}{c}
+   1 \\
+   1 \\
+   1 \\
+   1
+   \end{array}
+   \right]
+
+   \f]
+
+   The initial guess is given by
+   \f[
+   x^{(0)} = \left[
+   \begin{array}{c}
+   4\\
+   4\\
+   4\\
+   4
+   \end{array}
+   \right]
+   \f]
+
+   The solution is
+   \f[
+   x^{(*)} \approx \left[
+   \begin{array}{c}
+   0.1022\\
+   0.0783\\
+   0.0653\\
+   0.0754
+   \end{array}
+   \right]
+   \f]
+*/
+
+
+#include "NOX.H"
+#include "NOX_LAPACK_Group.H"
+
+//! An interface to the linear example described in Linear.C
+class Linear : public NOX::LAPACK::Interface {
+
+public:
+ 
+  //! Constructor
+  Linear() : 
+    initialGuess(4),
+    solution(4),
+    A(4, 4),
+    b(4)
+  {
+    initialGuess.init(4);
+
+    b.init(1);
+
+    A(0,0) = 7; 
+    A(0,1) = 1;
+    A(0,2) = 2;
+    A(0,3) = 1;
+    A(1,0) = 1; 
+    A(1,1) = 8;
+    A(1,2) = 3;
+    A(1,3) = 1;
+    A(2,0) = 1; 
+    A(2,1) = 3;
+    A(2,2) = 9;
+    A(2,3) = 1;
+    A(3,0) = 1; 
+    A(3,1) = 1;
+    A(3,2) = 1;
+    A(3,3) = 10;
+
+    solution(0) = 0.1022;
+    solution(1) = 0.0783;
+    solution(2) = 0.0653;
+    solution(3) = 0.0754;
+  };
+  
+  //! Destructor
+  ~Linear() {};
+
+  const NOX::LAPACK::Vector& getInitialGuess()
+  {
+    return initialGuess;
+  };
+
+  const NOX::LAPACK::Vector& getSolution()
+  {
+    return solution;
+  };
+
+  bool computeF(NOX::LAPACK::Vector& f, const NOX::LAPACK::Vector &x)
+  {
+    int n = f.length();
+    for (int i = 0; i < n; i ++) 
+    {
+      f(i) = 0    ;
+      for (int j = 0; j < n; j ++) {
+	f(i) += A(i,j) * x(j);
+      }
+      f(i) -= b(i);
+    }
+    return true;
+  };
+  
+  bool computeJacobian(NOX::LAPACK::Matrix& J, const NOX::LAPACK::Vector & x)
+  {
+    J = A;
+    return true;
+  };
+
+private:
+
+  //! Initial guess
+  NOX::LAPACK::Vector initialGuess;
+  //! Correct solution
+  NOX::LAPACK::Vector solution;
+  //! Matrix for linear problem
+  /*! See Linear.C */
+  NOX::LAPACK::Matrix A;
+  //! RHS for linear problem
+  /*! See Linear.C */
+  NOX::LAPACK::Vector b;
+
+};
+
+//! Main subroutine in example Linear.C
+int main()
+{
+  // Set up the problem interface
+  Linear linear;;
+  
+  // Create a group which uses that problem interface. The group will
+  // be initialized to contain the default initial guess for the
+  // specified problem.
+  NOX::LAPACK::Group grp(linear);
+
+  // Set up the status tests
+  NOX::StatusTest::NormF statusTestA(grp, 1.0e-4);
+  NOX::StatusTest::MaxIters statusTestB(20);
+  NOX::StatusTest::Combo statusTestsCombo(NOX::StatusTest::Combo::OR, statusTestA, statusTestB);
+
+  // Create the list of solver parameters
+  NOX::Parameter::List solverParameters;
+
+  // Set the level of output (this is the default)
+  solverParameters.setParameter("Output Information", 
+		     NOX::Utils::Warning + 
+		     NOX::Utils::OuterIteration + 
+		     NOX::Utils::InnerIteration + 
+		     NOX::Utils::Parameters);
+
+  // Set the solver (this is the default)
+  solverParameters.setParameter("Nonlinear Solver", "Line Search Based");
+
+  // Create direction sublist
+  NOX::Parameter::List& directionParameters = solverParameters.sublist("Direction");
+  
+  // Set direction
+  directionParameters.setParameter("Method", "Quasi-Newton");
+
+  // Create the line search parameters sublist
+  NOX::Parameter::List& lineSearchParameters = solverParameters.sublist("Line Search");
+
+  // Set the line search method
+  lineSearchParameters.setParameter("Method", "Polynomial");
+  lineSearchParameters.setParameter("Force Interpolation", true);
+  lineSearchParameters.setParameter("Convergence Criteria", "None");
+  lineSearchParameters.setParameter("Default Step", 1.0);
+  lineSearchParameters.setParameter("Min Bounds Factor", 0.0);
+
+
+  // Create the solver
+  NOX::Solver::Manager solver(grp, statusTestsCombo, solverParameters);
+
+  // Solve the nonlinesar system
+  NOX::StatusTest::StatusType status = solver.solve();
+
+  // Print the answer
+  cout << "\n" << "-- Parameter List From Solver --" << "\n";
+  solver.getParameterList().print(cout);
+
+  // Get the answer
+  grp = solver.getSolutionGroup();
+  
+
+  // Print the answer
+  cout << "\n" << "-- Final Solution From Solver --" << "\n";
+  grp.print();
+
+  // Print the expected answer
+  grp.setX(linear.getSolution());
+  grp.computeF();
+  cout << "\n" << "-- Expected Solution --" << "\n";
+  grp.print();
+}
---
+++ packages/nox/example-lapack/Rosenbrock.C
@@ -0,0 +1,199 @@
+// $Id$ 
+// $Source$ 
+
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//   
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//   
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// 
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov).
+// 
+// ************************************************************************
+//@HEADER
+
+/*!
+   \file Rosenbrock.C
+   \brief A simple 2D example using %Rosenbrock's function based on NOX::Example
+
+   This is an example of using %NOX with the NOX::LAPACK::Group and
+   NOX::LAPACK::Vector classes. These are very basic classes intended
+   only to illustrate and test NOX. They are based on a combination of
+   C++ STL and LAPACK.
+
+   This example is the "%Rosenbrock function" from Jorge J. More',
+   Burton S. Garbow, and Kenneth E. Hillstrom, Testing Unconstrained
+   Optimization Software, ACM TOMS, Vol. 7, No. 1, March 1981,
+   pp. 14-41.
+
+   It comes originally from H. H. %Rosenbrock, An Automatic Method for
+   Finding the Greatest or Least Value of a Function, J. Comput.
+   3(1960):175-184.
+
+   The function is defined as
+   \f[
+   F(x) = \left[
+   \begin{array}{c}
+   10 (x[2] - x[1]^2) \\
+   1 - x[1]
+   \end{array}
+   \right]
+   \f]
+
+   The initial guess is given by
+   \f[
+   x_0 = \left[
+   \begin{array}{c}
+   -1.2\\
+   1
+   \end{array}
+   \right]
+   \f]
+
+   The solution is
+   \f[
+   x_* = \left[
+   \begin{array}{c}
+   1\\
+   1
+   \end{array}
+   \right]
+   \f]
+
+*/
+
+
+#include "NOX.H"
+#include "NOX_LAPACK_Group.H"
+
+//! An interface to the example described in Rosenbrock.C
+class Rosenbrock : public NOX::LAPACK::Interface {
+
+public:
+ 
+  //! Constructor
+  Rosenbrock() : 
+    initialGuess(2),
+    solution(2)
+  {
+    initialGuess(0) = -1.2;
+    initialGuess(1) = 1;
+    solution(0) = 1;
+    solution(1) = 1;
+  };
+
+  //! Destructor
+  ~Rosenbrock() {};
+
+  const NOX::LAPACK::Vector& getInitialGuess()
+  {
+    return initialGuess;
+  };
+
+  const NOX::LAPACK::Vector& getSolution()
+  {
+    return solution;
+  };
+
+  bool computeF(NOX::LAPACK::Vector& f, const NOX::LAPACK::Vector &x)
+  {
+    f(0) = 10 * (x(1) - x(0) * x(0));
+    f(1) = 1 - x(0);
+    return true;
+  };
+  
+  bool computeJacobian(NOX::LAPACK::Matrix& J, const NOX::LAPACK::Vector & x)
+  {
+    J(0,0) = -20 * x(0);
+    J(0,1) = 10;
+    J(1,0) = -1;
+    J(1,1) = 0;
+    return true;
+  };
+
+private:
+
+  //! Initial guess
+  NOX::LAPACK::Vector initialGuess;
+  //! Correct solution
+  NOX::LAPACK::Vector solution;
+
+};
+
+//! Main subroutine of Rosenbrock.C
+int main()
+{
+  // Set up the problem interface
+  Rosenbrock rosenbrock;;
+  
+  // Create a group which uses that problem interface. The group will
+  // be initialized to contain the default initial guess for the
+  // specified problem.
+  NOX::LAPACK::Group grp(rosenbrock);
+
+  // Set up the status tests
+  NOX::StatusTest::NormF statusTestA(grp, 1.0e-4);
+  NOX::StatusTest::MaxIters statusTestB(20);
+  NOX::StatusTest::Combo statusTestsCombo(NOX::StatusTest::Combo::OR, statusTestA, statusTestB);
+
+  // Create the list of solver parameters
+  NOX::Parameter::List solverParameters;
+
+  // Set the level of output (this is the default)
+  solverParameters.setParameter("Output Information", 
+		     NOX::Utils::Warning + 
+		     NOX::Utils::OuterIteration + 
+		     NOX::Utils::InnerIteration + 
+		     NOX::Utils::Parameters);
+
+  // Set the solver (this is the default)
+  solverParameters.setParameter("Nonlinear Solver", "Line Search Based");
+
+  // Create the line search parameters sublist
+  NOX::Parameter::List& lineSearchParameters = solverParameters.sublist("Line Search");
+
+  // Set the line search method
+  lineSearchParameters.setParameter("Method","More'-Thuente");
+
+  // Create the solver
+  NOX::Solver::Manager solver(grp, statusTestsCombo, solverParameters);
+
+  // Solve the nonlinesar system
+  NOX::StatusTest::StatusType status = solver.solve();
+
+  // Print the answer
+  cout << "\n" << "-- Parameter List From Solver --" << "\n";
+  solver.getParameterList().print(cout);
+
+  // Get the answer
+  grp = solver.getSolutionGroup();
+  
+
+  // Print the answer
+  cout << "\n" << "-- Final Solution From Solver --" << "\n";
+  grp.print();
+
+  // Print the expected answer
+  grp.setX(rosenbrock.getSolution());
+  grp.computeF();
+  cout << "\n" << "-- Expected Solution --" << "\n";
+  grp.print();
+}
