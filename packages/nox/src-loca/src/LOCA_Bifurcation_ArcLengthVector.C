25b4c3ccc58cb54e47bb710ae6b94f334c6ed445
Eric T. Phipps <etphipp@sandia.gov>
2002-11-25 22:27:57 +0000

    Added LOCA::MultiVector class which provides a common implementation for LOCA::Bifurcation::ArcLengthVector, LOCA::Bifurcation::TPBordVector, and any future composite "super-vector" classes.  TPBordVector and ArcLengthVector now only provide interfaces for this class.

--- packages/nox/src-loca/src/LOCA_Bifurcation_ArcLengthVector.C
+++ packages/nox/src-loca/src/LOCA_Bifurcation_ArcLengthVector.C
@@ -32,208 +32,66 @@
 
 #include "LOCA_Bifurcation_ArcLengthVector.H"  // Class definition
 
-#include "NOX_Common.H"    // for abs()
-
 using namespace LOCA;
 using namespace LOCA::Bifurcation; 
 
 ArcLengthVector::ArcLengthVector(const NOX::Abstract::Vector& xVec,
-			   double arcParam) :
-  xVectorPtr(xVec.clone()),
-  arcParam(arcParam) 
+				 double arcParam) :
+  MultiVector(1,1)
 {
-
+  setVector(0, xVec);
+  setScalar(0, arcParam);
 }
 
-ArcLengthVector::ArcLengthVector(const ArcLengthVector& source) :
-  xVectorPtr(source.xVectorPtr->clone(NOX::DeepCopy)),
-  arcParam(source.arcParam)  
+ArcLengthVector::ArcLengthVector(const ArcLengthVector& source, 
+				 NOX::CopyType type) :
+  MultiVector(source, type)
 {
-  
 }
 
 ArcLengthVector::~ArcLengthVector()
 {
-  delete xVectorPtr;
-}
-
-NOX::Abstract::Vector& ArcLengthVector::init(double gamma)
-{
-  xVectorPtr->init(gamma);
-  arcParam = gamma;
-  return *this;
-}
-
-NOX::Abstract::Vector& ArcLengthVector::abs(const NOX::Abstract::Vector& y)
-{
-  const ArcLengthVector* tpY = 0;
-  tpY = dynamic_cast<const ArcLengthVector*>(&y);
-  
-  xVectorPtr->abs(*(tpY->xVectorPtr));
-  arcParam = fabs(tpY->getArcParam());
-  return *this;
 }
 
 NOX::Abstract::Vector& ArcLengthVector::operator=(const NOX::Abstract::Vector& y)
 {
- const LOCA::Bifurcation::ArcLengthVector* aly = dynamic_cast<const LOCA::Bifurcation::ArcLengthVector*>(&y);
-
-  return (*this = *aly);
+ return operator=(dynamic_cast<const ArcLengthVector&>(y));
 }
 
-LOCA::Bifurcation::ArcLengthVector& ArcLengthVector::operator=(const LOCA::Bifurcation::ArcLengthVector& y)
+ArcLengthVector& ArcLengthVector::operator=(const ArcLengthVector& y)
 {
-  const ArcLengthVector* tpY = 0;
-  tpY = dynamic_cast<const ArcLengthVector*>(&y);
-  
-  *xVectorPtr = *(tpY->xVectorPtr);
-  arcParam = tpY->arcParam;
-  return *this;
-}
-
-NOX::Abstract::Vector& ArcLengthVector::reciprocal(const NOX::Abstract::Vector& y)
-{
-  const ArcLengthVector* tpY = 0;
-  tpY = dynamic_cast<const ArcLengthVector*>(&y);
-  
-  xVectorPtr->reciprocal(*(tpY->xVectorPtr));
-  arcParam = 1.0 / (tpY->arcParam);
-  return *this;
-}
-
-NOX::Abstract::Vector& ArcLengthVector::scale(double gamma)
-{
-  xVectorPtr->scale(gamma);
-  arcParam *= gamma;
-  return *this;
-}
-
-NOX::Abstract::Vector& ArcLengthVector::scale(const NOX::Abstract::Vector& y)
-{
-  const ArcLengthVector* tpY = 0;
-  tpY = dynamic_cast<const ArcLengthVector*>(&y);
-  
-  xVectorPtr->scale(*(tpY->xVectorPtr));
-  arcParam *= tpY->arcParam;
-  return *this;
-}
-
-NOX::Abstract::Vector& ArcLengthVector::update(double alpha, const NOX::Abstract::Vector& y, double gamma)
-{
-  const ArcLengthVector* tpY = 0;
-  tpY = dynamic_cast<const ArcLengthVector*>(&y);
-  
-  xVectorPtr->update(alpha, *(tpY->xVectorPtr), gamma);
-  arcParam = alpha * tpY->arcParam + gamma;
-  return *this;
-}
-
-NOX::Abstract::Vector& ArcLengthVector::update(double alpha, 
-			 const NOX::Abstract::Vector& y, 
-                         double beta, const NOX::Abstract::Vector& b,
-                         double gamma)
-{
-  const ArcLengthVector* tpY = 0;
-  tpY = dynamic_cast<const ArcLengthVector*>(&y);
-
-  const ArcLengthVector* tpB = 0;
-  tpB = dynamic_cast<const ArcLengthVector*>(&b);  
-
-  xVectorPtr->update(alpha, *(tpY->xVectorPtr), beta, 
-		     *(tpB->xVectorPtr), gamma);
-
-  arcParam = alpha * tpY->arcParam + beta * tpB->arcParam + gamma;
+  MultiVector::operator=(y);
   return *this;
 }
 
 NOX::Abstract::Vector* ArcLengthVector::clone(NOX::CopyType type) const
 {
-  ArcLengthVector* newCopy = 0;
-
-  newCopy = new ArcLengthVector(*this);
-  
-  if (type != NOX::DeepCopy)
-    newCopy->init(0.0);
-      
-  return newCopy;
-}
-
-double ArcLengthVector::norm(NormType type) const
-{
-  double n, nx;
-  switch (type) {
-  case NOX::Abstract::Vector::MaxNorm:
-    nx = xVectorPtr->norm(NOX::Abstract::Vector::MaxNorm);
-    n = max(nx, arcParam);
-    break;
-  case NOX::Abstract::Vector::OneNorm:
-    nx = xVectorPtr->norm(NOX::Abstract::Vector::OneNorm);
-    n = nx + fabs(arcParam);
-    break;
-  case NOX::Abstract::Vector::TwoNorm:
-  default:
-   nx = xVectorPtr->norm(NOX::Abstract::Vector::TwoNorm);
-   n = sqrt(nx*nx + arcParam*arcParam);
-   break;
-  }
-  return n;
-}
-
-double ArcLengthVector::norm(const NOX::Abstract::Vector& weights) const
-{
-  cout << "ERROR: LOCA::Bifurcation::ArcLengthVector::norm(weights) - "
-       << "Not implemented yet!!!" << endl;
-  throw "LOCA Error";
-}
-
-double ArcLengthVector::dot(const NOX::Abstract::Vector& y) const
-{
-  const ArcLengthVector* tpY = 0;
-  tpY = dynamic_cast<const ArcLengthVector*>(&y);
-
-  return( xVectorPtr->dot(*tpY->xVectorPtr) + arcParam*arcParam);
-
-}
-
-int ArcLengthVector::length() const
-{
-  return (xVectorPtr->length() + 1);
-}
-
-bool ArcLengthVector::print() const
-{
-  xVectorPtr->print();
-  cout << "ArcLength Parameter = " << arcParam << endl;
-  return true;
+  return new ArcLengthVector(*this, type);
 }
 
 void ArcLengthVector::setVec(const NOX::Abstract::Vector& xVec,
                              double arcPar)
 {
-  *xVectorPtr = xVec;
-  arcParam = arcPar;
+  setVector(0, xVec);
+  setScalar(0, arcPar);
 }
 
-  //! Returns the solution vector component of extended vector
 const NOX::Abstract::Vector& ArcLengthVector::getXVec() const
 {
-  return *xVectorPtr;
+  return getVector(0);
 }
 
-  //! Get Arclength parameter
 double ArcLengthVector::getArcParam() const
 {
-  return arcParam;
+  return getScalar(0);
 }
 
-  //! Returns the solution vector component of extended vector
 NOX::Abstract::Vector& ArcLengthVector::getXVec()
 {
-  return *xVectorPtr;
+  return getVector(0);
 }
 
-  //! Get Arclength parameter
 double& ArcLengthVector::getArcParam()
 {
-  return arcParam;
+  return getScalar(0);
 }
---
+++ packages/nox/src-loca/src/LOCA_MultiVector.C
@@ -0,0 +1,285 @@
+// $Id$ 
+// $Source$ 
+
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//   
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//   
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// 
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov).
+// 
+// ************************************************************************
+//@HEADER
+
+#include "LOCA_MultiVector.H"  // Class definition
+
+using namespace LOCA;
+
+MultiVector::MultiVector(int nvecs, int nscalars) :
+  vectorPtrs(nvecs), scalars(nscalars)
+{
+}
+
+MultiVector::MultiVector(const MultiVector& source, NOX::CopyType type) :
+  vectorPtrs(source.vectorPtrs.size()), 
+  scalars(source.scalars)
+{
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i] = source.vectorPtrs[i]->clone(type);
+
+  if (type != NOX::DeepCopy)
+    init(0.0);
+}
+
+
+MultiVector::~MultiVector()
+{
+  for (int i=0; i<vectorPtrs.size(); i++)
+    delete vectorPtrs[i];
+}
+
+NOX::Abstract::Vector& MultiVector::operator=(const NOX::Abstract::Vector& y)
+{
+  return operator=(dynamic_cast<const MultiVector&>(y));
+}
+
+MultiVector& MultiVector::operator=(const MultiVector& y)
+{ 
+  if (this != &y) {
+    for (int i=0; i<vectorPtrs.size(); i++)
+      delete vectorPtrs[i];
+
+    vectorPtrs = y.vectorPtrs;
+    scalars = y.scalars;
+
+    for (int i=0; i<vectorPtrs.size(); i++)
+      vectorPtrs[i] = y.vectorPtrs[i]->clone(NOX::DeepCopy);
+  }
+  return *this;
+}
+
+NOX::Abstract::Vector* MultiVector::clone(NOX::CopyType type) const
+{
+  return new MultiVector(*this, type);
+}
+
+NOX::Abstract::Vector& MultiVector::init(double gamma)
+{
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->init(gamma);
+  return *this;
+}
+
+NOX::Abstract::Vector& MultiVector::abs(const NOX::Abstract::Vector& y)
+{
+  const MultiVector& Y = dynamic_cast<const MultiVector&>(y);
+  
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->abs(*(Y.vectorPtrs[i]));
+  for (int i=0; i<scalars.size(); i++)
+    scalars[i] = fabs(Y.scalars[i]);
+
+  return *this;
+}
+
+NOX::Abstract::Vector& MultiVector::reciprocal(const NOX::Abstract::Vector& y)
+{
+  const MultiVector& Y = dynamic_cast<const MultiVector&>(y);
+  
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->reciprocal(*(Y.vectorPtrs[i]));
+  for (int i=0; i<scalars.size(); i++)
+    scalars[i] = 1.0 / Y.scalars[i];
+  
+  return *this;
+}
+
+NOX::Abstract::Vector& MultiVector::scale(double gamma)
+{
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->scale(gamma);
+  for (int i=0; i<scalars.size(); i++)
+    scalars[i] *= gamma;
+
+  return *this;
+}
+
+NOX::Abstract::Vector& MultiVector::scale(const NOX::Abstract::Vector& y)
+{
+  const MultiVector& Y = dynamic_cast<const MultiVector&>(y);
+  
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->scale(*(Y.vectorPtrs[i]));
+  for (int i=0; i<scalars.size(); i++)
+    scalars[i] *= Y.scalars[i];
+
+  return *this;
+}
+
+NOX::Abstract::Vector& MultiVector::update(double alpha, const NOX::Abstract::Vector& y, double gamma)
+{
+  const MultiVector& Y = dynamic_cast<const MultiVector&>(y);
+  
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->update(alpha, *(Y.vectorPtrs[i]), gamma);
+  for (int i=0; i<scalars.size(); i++)
+    scalars[i] = alpha*(Y.scalars[i]) + gamma*scalars[i];
+
+  return *this;
+}
+
+NOX::Abstract::Vector& MultiVector::update(double alpha, 
+			 const NOX::Abstract::Vector& y, 
+                         double beta, const NOX::Abstract::Vector& b,
+                         double gamma)
+{
+  const MultiVector& Y = dynamic_cast<const MultiVector&>(y);
+  const MultiVector& B = dynamic_cast<const MultiVector&>(b);
+  
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->update(alpha, *(Y.vectorPtrs[i]), beta, *(B.vectorPtrs[i]),
+			  gamma);
+  for (int i=0; i<scalars.size(); i++)
+    scalars[i] = alpha*(Y.scalars[i]) + beta*(B.scalars[i]) + gamma*scalars[i];
+
+  return *this;
+}
+
+double MultiVector::norm(NormType type) const
+{
+  double n = 0.0;
+  double nv;
+
+  switch (type) {
+
+  case NOX::Abstract::Vector::MaxNorm:
+    for (int i=0; i<vectorPtrs.size(); i++) 
+      n = max(n, vectorPtrs[i]->norm(type));
+    for (int i=0; i<scalars.size(); i++) 
+      n = max(n, fabs(scalars[i]));
+    break;
+
+  case NOX::Abstract::Vector::OneNorm:
+    for (int i=0; i<vectorPtrs.size(); i++) 
+      n += vectorPtrs[i]->norm(type);
+    for (int i=0; i<scalars.size(); i++) 
+      n += fabs(scalars[i]);
+    break;
+
+  case NOX::Abstract::Vector::TwoNorm:
+  default:
+    for (int i=0; i<vectorPtrs.size(); i++) {
+      nv = vectorPtrs[i]->norm(type);
+      n += nv*nv;
+    }
+    for (int i=0; i<scalars.size(); i++) 
+      n += scalars[i]*scalars[i];
+    n = sqrt(n);
+   break;
+
+  }
+
+  return n;
+}
+
+double MultiVector::norm(const NOX::Abstract::Vector& weights) const
+{
+  const MultiVector& W = dynamic_cast<const MultiVector&>(weights);
+  double n = 0.0;
+  double nv;
+
+  for (int i=0; i<vectorPtrs.size(); i++) {
+    nv = vectorPtrs[i]->norm(*(W.vectorPtrs[i]));
+    n += nv*nv;
+  }
+  for (int i=0; i<scalars.size(); i++) 
+    n += W.scalars[i]*scalars[i]*scalars[i];
+  n = sqrt(n);
+
+  return n;
+}
+
+double MultiVector::dot(const NOX::Abstract::Vector& y) const
+{
+  const MultiVector& Y = dynamic_cast<const MultiVector&>(y);
+  double d = 0.0;
+  
+  for (int i=0; i<vectorPtrs.size(); i++)
+    d += vectorPtrs[i]->dot(*(Y.vectorPtrs[i]));
+  for (int i=0; i<scalars.size(); i++)
+    d += scalars[i]*(Y.scalars[i]);
+
+  return d;
+}
+
+int MultiVector::length() const
+{
+  int l = 0;
+  for (int i=0; i<vectorPtrs.size(); i++)
+    l += vectorPtrs[i]->length();
+  l += scalars.size();
+
+  return l;
+}
+
+void MultiVector::print() const
+{
+  for (int i=0; i<vectorPtrs.size(); i++)
+    vectorPtrs[i]->print();
+  for (int i=0; i<scalars.size(); i++)
+    cout << scalars[i] << " ";
+  cout << endl;
+
+}
+
+void MultiVector::setVector(int i, const NOX::Abstract::Vector& v)
+{
+  if (vectorPtrs[i] != NULL)
+    *vectorPtrs[i] = v;
+  else
+    vectorPtrs[i] = v.clone();
+}
+
+void MultiVector::setScalar(int i, double s)
+{
+  scalars[i] = s;
+}
+
+const NOX::Abstract::Vector& MultiVector::getVector(int i) const
+{
+  return *vectorPtrs[i];
+}
+
+NOX::Abstract::Vector& MultiVector::getVector(int i)
+{
+  return *vectorPtrs[i];
+}
+
+double MultiVector::getScalar(int i) const
+{
+  return scalars[i];
+}
+
+double& MultiVector::getScalar(int i)
+{
+  return scalars[i];
+}
