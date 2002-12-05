56376d6c2faab69024266da98656b809ccf4c030
Jonathan Hu <jhu@sandia.gov>
2002-12-05 00:11:50 +0000

    Prototype for new function.

--- packages/ml/src/Main/ml_struct.h
+++ packages/ml/src/Main/ml_struct.h
@@ -282,6 +282,7 @@ extern double ML_Cycle_MGFull(ML_1Level *curr, double *sol, double *rhs,
 extern int ML_Solve_AMGV( ML *ml , double *din, double *dout);
 extern double ML_Cycle_AMGV(ML_1Level *curr, double *sol, double *rhs,
                      int approx_all_zeros, ML_Comm *comm);
+extern int ML_Solve_ProjectedAMGV( ML *ml , double *din, double *dout);
 extern int ML_Gen_SmootherGSextra( ML *ml , int nl, int pre_or_post, 
 				   int ntimes, double omega, int Nextra, 
 				   int extra[]);
