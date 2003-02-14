1c38944a9f087ef0c644ffebcdd2a3707039f9e0
Robert Heaphy <rheaphy>
2003-02-14 19:38:21 +0000

    Now compute lattice on the intersection of dropped box and original bounding box (Karen's idea), compute lattice using non commensurate intervals (prime number interval divisors), fixed problem introduced when implementing procs != parts

--- packages/zoltan/src/hsfc/hsfc_box_assign.c
+++ packages/zoltan/src/hsfc/hsfc_box_assign.c
@@ -32,7 +32,7 @@ extern "C" {
    have volume that falls within the box but has no lattice point falling
    within.  Hence, it may be missed.  */
 
-static void add_to_list(ZZ *, int, int, int *, int *, int, int) ;
+
 
 /****************************************************************************/
 
@@ -42,155 +42,142 @@ int Zoltan_HSFC_Box_Assign (
          double xhi, double yhi, double zhi, 
          int *procs, int *proc_count, int *parts, int *part_count)
    {
-   double     x[3] ;
-   double     xdelta, ydelta, zdelta ;
-   int       *part_array = NULL ;
-   int       *proc_array = NULL ;
-   int        tmp_part ;
-   int        tmp_proc ;
-   HSFC_Data *d ;
-   int        n, i, loop ;
-   const int  NN       = 4 ;    /* determines lattice spacing */
-   int        oldpartcount = 0 ;
-   int        oldproccount = 0 ;
-   const int  MAXLOOP  = 5 ;
-   int        err ;
-   char *yo = "Zoltan_HSFC_Box_Assign" ;
-   int        include_procs = (procs != NULL);
-   int        include_parts = (parts != NULL);
-   int        size;
+   double     x[3], xintl[3], xinth[3];
+   double     xdelta, ydelta, zdelta;
+   int       *part_array = NULL;
+   int       *proc_array = NULL;
+   int        tmp_part;
+   HSFC_Data *d;
+   int        n, i, loop, offset;
+   int        oldpartcount = 0;
+   int        first_proc, last_proc;
+   const int  MAXLOOP  = 15;
+   int        err = ZOLTAN_OK;
+   int prime[] = {3, 7, 17, 37, 79, 163, 331, 673, 1361, 2729, 5471, 10949,
+    21911, 43853, 87719, 175447, 350899, 701819, 1403641, 2807303, 5614657,
+    11229331, 22458671, 44917381};
+   char *yo = "Zoltan_HSFC_Box_Assign";
 
-   ZOLTAN_TRACE_ENTER (zz, yo) ;
-   d = (HSFC_Data *) zz->LB.Data_Structure ;
+   ZOLTAN_TRACE_ENTER (zz, yo);
+   d = (HSFC_Data *) zz->LB.Data_Structure;
    if (d == NULL)
       ZOLTAN_HSFC_ERROR (ZOLTAN_FATAL,
           "No Decomposition Data available; use KEEP_CUTS parameter.");
 
    *part_count = *proc_count = 0;
-
-   size = (include_parts ? zz->LB.Num_Global_Parts : 0) + 
-          (include_procs ? zz->Num_Proc : 0) ;
-   if (size == 0)
-      goto free ;
-
-   part_array = (int *) ZOLTAN_MALLOC (size * sizeof (int)) ;
+   part_array = (int *) ZOLTAN_MALLOC ((zz->LB.Num_Global_Parts + zz->Num_Proc)
+    * sizeof (int));
    if (part_array == NULL)
-      ZOLTAN_HSFC_ERROR (ZOLTAN_MEMERR, "Memory error.") ;
+      ZOLTAN_HSFC_ERROR (ZOLTAN_MEMERR, "Memory error.");
+   proc_array = part_array + zz->LB.Num_Global_Parts;
+   memset (part_array, 0, (zz->LB.Num_Global_Parts + zz->Num_Proc)*sizeof (int));
 
-   proc_array = part_array + (include_parts ? zz->LB.Num_Global_Parts : 0) ;
-
-   /* Clear processor array */
-   memset (part_array, 0, size * sizeof (int)) ;  
-
-   xdelta = (xlo == xhi) ? 1.0 : xhi - xlo ;
-   ydelta = (ylo == yhi) ? 1.0 : yhi - ylo ;
-   for (loop = 0, n = NN ; loop < MAXLOOP ; loop++, n = 2*n)
-      {
-      /* create regular lattice in given box, then look up associated processors */
-      if (d->ndimension == 3)
-         {
-         zdelta = (zlo == zhi) ? 1.0L : zhi - zlo ;
-         for       (x[0] = xlo ; x[0] <= xhi ; x[0] += xdelta/n)
-            for    (x[1] = ylo ; x[1] <= yhi ; x[1] += ydelta/n)
-               for (x[2] = zlo ; x[2] <= zhi ; x[2] += zdelta/n)
-                  if (Zoltan_HSFC_Point_Assign (zz, x, &tmp_proc, &tmp_part) == 
-                      ZOLTAN_OK) 
-                     add_to_list(zz, include_procs, include_parts,
-                                 proc_array, part_array, tmp_proc, tmp_part);
-         }
-
-      else if (d->ndimension == 2) 
-         {
-         for    (x[0] = xlo ; x[0] <= xhi ; x[0] += xdelta/n)
-            for (x[1] = ylo ; x[1] <= yhi ; x[1] += ydelta/n)
-               if (Zoltan_HSFC_Point_Assign (zz, x, &tmp_proc, &tmp_part) ==
-                   ZOLTAN_OK)
-                  add_to_list(zz, include_procs, include_parts,
-                              proc_array, part_array, tmp_proc, tmp_part);
-         }
-
-      else if (d->ndimension == 1)
-         {
-         Zoltan_HSFC_Point_Assign (zz, &xlo, NULL, &n) ;
-         Zoltan_HSFC_Point_Assign (zz, &xhi, NULL, &loop) ;
-         for (i = n ; i <= loop ; i++) 
-            {
-            tmp_proc = Zoltan_LB_Part_To_Proc(zz, i, NULL);
-            add_to_list(zz, include_procs, include_parts,
-                        proc_array, part_array, tmp_proc, i);
-            }
-         }
-
-      /* move results to user supplied array & hope it is big enough */
-      if (include_parts)
-         {
-         *part_count = 0 ;
-         for (i = 0 ; i < zz->LB.Num_Global_Parts ; i++)
-            if (part_array[i] > 0)
-               parts[(*part_count)++] = i ;
-         }
-     
-      if (include_procs)
-         {
-         *proc_count = 0 ;
-         for (i = 0 ; i < zz->Num_Proc ; i++)
-            if (proc_array[i] > 0)
-               procs[(*proc_count)++] = i ;
-         }
-
-      if (d->ndimension == 1 ||
-          ((*part_count==oldpartcount || *part_count==zz->LB.Num_Global_Parts)
-        && (*proc_count==oldproccount || *proc_count==zz->Num_Proc)))
-         break ;
-
-      oldpartcount = *part_count ;
-      oldproccount = *proc_count ;
+   if (d->ndimension == 1) {
+      Zoltan_HSFC_Point_Assign (zz, &xlo, NULL, &n);
+      Zoltan_HSFC_Point_Assign (zz, &xhi, NULL, &loop);
+      for (i = n; i <= loop; i++)
+         part_array[i] = 1;
+      goto fini;
       }
-   err = ZOLTAN_OK ;
+   
+   /* set initial resolution at about equal work for each dimension */
+   offset = (d->ndimension == 2) ? 2 : 1;
+
+   /* determine intersection of bounding box and dropped box */
+   if      (xhi < d->bbox_lo[0])  xintl[0] = xinth[0] = d->bbox_lo[0];
+   else if (xlo > d->bbox_hi[0])  xintl[0] = xinth[0] = d->bbox_hi[0];
+   else {
+        xintl[0] = (d->bbox_lo[0] < xlo) ? xlo           : d->bbox_lo[0];
+        xinth[0] = (d->bbox_hi[0] < xhi) ? d->bbox_hi[0] : xhi;
+        }
+
+   if      (yhi < d->bbox_lo[1])  xintl[1] = xinth[1] = d->bbox_lo[1];
+   else if (ylo > d->bbox_hi[1])  xintl[1] = xinth[1] = d->bbox_hi[1];
+   else {
+        xintl[1] = (d->bbox_lo[1] < ylo) ? ylo           : d->bbox_lo[1];
+        xinth[1] = (d->bbox_hi[1] < yhi) ? d->bbox_hi[1] : yhi;
+        }
+
+   if      (zhi < d->bbox_lo[2])  xintl[2] = xinth[2] = d->bbox_lo[2];
+   else if (zlo > d->bbox_hi[2])  xintl[2] = xinth[2] = d->bbox_hi[2];
+   else {
+        xintl[2] = (d->bbox_lo[2] < zlo) ? zlo           : d->bbox_lo[2];
+        xinth[2] = (d->bbox_hi[2] < zhi) ? d->bbox_hi[2] : zhi;
+        }
+
+   for (loop = 0; loop < MAXLOOP; loop++) {
+      oldpartcount = *part_count;
+
+      xdelta = (xinth[0] - xintl[0])/ (double) prime[loop + offset];
+      ydelta = (xinth[1] - xintl[1])/ (double) prime[loop + offset];
+      zdelta = (xinth[2] - xintl[2])/ (double) prime[loop + offset];
+
+      if (xdelta < REFINEMENT_LIMIT)  xdelta = 1.0;
+      if (ydelta < REFINEMENT_LIMIT)  ydelta = 1.0;
+      if (zdelta < REFINEMENT_LIMIT)  zdelta = 1.0;
+
+      /* create regular lattice in given box, then look up associated processors */
+      if (d->ndimension == 3) {
+        for     (x[0] = xintl[0]; x[0] <= xinth[0]; x[0] += xdelta)
+          for   (x[1] = xintl[1]; x[1] <= xinth[1]; x[1] += ydelta)
+            for (x[2] = xintl[2]; x[2] <= xinth[2]; x[2] += zdelta)
+               if (Zoltan_HSFC_Point_Assign(zz, x, NULL, &tmp_part) == ZOLTAN_OK)
+                   part_array[tmp_part] = 1;
+        }
+
+      else if (d->ndimension == 2) {
+         for   (x[0] = xintl[0]; x[0] <= xinth[0]; x[0] += xdelta)
+           for (x[1] = xintl[1]; x[1] <= xinth[1]; x[1] += ydelta)
+              if (Zoltan_HSFC_Point_Assign (zz, x, NULL, &tmp_part) == ZOLTAN_OK)
+                  part_array[tmp_part] = 1;
+         }
+
+      /* test for early exit */
+      for (i = 0; i < zz->LB.Num_Global_Parts; i++)
+         if (part_array[i] > 0)
+            *part_count++;
+      if (*part_count == oldpartcount)
+         break;
+      }
+
+   /* move results to user supplied array(s) & hope it(they) is(are) big enough */
+fini:
+   *part_count = 0;
+   if (parts) {
+      *part_count = 0;
+      for (i = 0; i < zz->LB.Num_Global_Parts; i++)
+         if (part_array[i] > 0)
+            parts[(*part_count)++] = i;
+      }
+
+   if (procs) {
+      for (i = 0; i < zz->LB.Num_Global_Parts; i++)
+         if (part_array[i] > 0) {
+            first_proc = Zoltan_LB_Part_To_Proc(zz, i, NULL);
+            proc_array[first_proc] = 1;
+            if (!zz->LB.Single_Proc_Per_Part) {
+               /* Part may be spread across multiple procs. Include them all. */
+               int j;
+               if (first_proc < zz->LB.Num_Global_Parts - 1)
+                  last_proc = Zoltan_LB_Part_To_Proc(zz, first_proc+1, NULL);
+               else
+                  last_proc = zz->Num_Proc;
+
+               for (j = first_proc + 1; j < last_proc; j++)
+                  proc_array[j] = 1;
+               }
+            }
+
+      *proc_count = 0;
+      for (i = 0; i < zz->Num_Proc; i++)
+         if (proc_array[i] > 0)
+            procs[(*proc_count)++] = i;
+      }
 
 free:
-   ZOLTAN_FREE (&part_array) ;
-   ZOLTAN_TRACE_EXIT (zz,yo) ;
+   ZOLTAN_FREE (&part_array);
+   ZOLTAN_TRACE_EXIT (zz, yo);
    return err;
-   }
-
-/****************************************************************************/
-static void add_to_list(
-   ZZ *zz,
-   int include_procs,    /* Flag:  add to processor list */
-   int include_parts,    /* Flag:  add to partition list */
-   int *proc_array,      /* Values to be incremented for procs to be added */
-   int *part_array,      /* Values to be incremented for parts to be added */
-   int proc,             /* First processor owning partition part. */
-   int part              /* Partition to be added. */
-)
-   {
-   /* Add a partition and ALL associated processors to the part_array and
-    * proc_array lists. 
-    * Note:  Special logic handles case where a partition is spread across
-    * many processors (!zz->LB.Single_Proc_Per_Part).  
-    * Want to include all processors in this case. 
-    */
-   int i;
-   int last_proc;    /* Starting processor of next partition */
-
-   if (include_parts) ++(part_array[part]) ;
-   if (include_procs) 
-      {
-      ++(proc_array[proc]) ;
-      if (!zz->LB.Single_Proc_Per_Part)
-         {
-         /* Partition may be spread across multiple procs.
-            Include them all. */
-         if (part < zz->LB.Num_Global_Parts - 1) 
-            last_proc = Zoltan_LB_Part_To_Proc(zz, part+1, NULL);
-         else
-            last_proc = zz->Num_Proc;
-                  
-         for (i = proc+1 ; i < last_proc ; i++)
-            ++(proc_array[i]) ;
-         }
-      }
    }
 
 #ifdef __cplusplus
