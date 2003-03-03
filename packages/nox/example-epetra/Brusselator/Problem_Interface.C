b5fb6b9078508906cdae22532d3426b160395fa2
Russell Hooper <rhoope@sandia.gov>
2003-03-03 22:10:35 +0000

    Interface files.
    The Brusselator problem is intended to provide a problem rich in physics that
    can be used by those interested in time integration, coupling via matrix free,
    and continuation strategies.

---
+++ packages/nox/example-epetra/Brusselator/Problem_Interface.C
@@ -0,0 +1,48 @@
+// ----------   Includes   ----------
+#include <iostream>
+#include "Problem_Interface.H"
+
+// ----------   User Defined Includes   ----------
+#include "Brusselator.H"
+
+//-----------------------------------------------------------------------------
+Problem_Interface::Problem_Interface(Brusselator& Problem) :
+  problem(Problem)
+{ }
+
+Problem_Interface::~Problem_Interface()
+{ }
+
+bool Problem_Interface::computeF(const Epetra_Vector& x, Epetra_Vector& F, FillType flag)
+{
+  return problem.evaluate(F_ONLY, &x, &F, NULL);
+}
+
+bool Problem_Interface::computeJacobian(const Epetra_Vector& x, Epetra_Operator& Jac)
+{
+  Epetra_RowMatrix* Jacobian = dynamic_cast<Epetra_RowMatrix*>(&Jac);
+  if (Jacobian == NULL) {
+    cout << "ERROR: Problem_Interface::computeJacobian() - The supplied"
+	 << "Epetra_Operator is NOT an Epetra_RowMatrix!" << endl;
+    throw;
+  }
+  return problem.evaluate(MATRIX_ONLY, &x, NULL, Jacobian);
+}
+
+bool Problem_Interface::computePrecMatrix(const Epetra_Vector& x, Epetra_RowMatrix& M)
+{
+  Epetra_RowMatrix* precMatrix = dynamic_cast<Epetra_RowMatrix*>(&M);
+  if (precMatrix == NULL) {
+    cout << "ERROR: Problem_Interface::computePreconditioner() - The supplied"
+	 << "Epetra_Operator is NOT an Epetra_RowMatrix!" << endl;
+    throw;
+  }
+  return problem.evaluate(MATRIX_ONLY, &x, NULL, precMatrix);
+}
+bool Problem_Interface::computePreconditioner(const Epetra_Vector& x, Epetra_Operator& M)
+{
+  cout << "ERROR: Problem_Interface::preconditionVector() - Use Explicit Jacobian only for this test problem!" << endl;
+  throw 1;
+}
+//-----------------------------------------------------------------------------
+
