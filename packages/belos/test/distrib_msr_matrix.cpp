5cbd2349410f8e4add45d4e93f20c8688620f25a
James M. Willenbring <jmwille@sandia.gov>
2003-03-24 22:24:09 +0000

    ----------------------------------------------------------------------
    Committing in .
    
    Added Files:
    	test/Util.h test/distrib_msr_matrix.cpp
    	test/read_hb_matrix.cpp test/BlockCG/bcsstk14.hb
    	test/BlockCG/bcsstk15.hb test/BlockCG/bcsstk18.hb
    	test/BlockCG/classicMakefile test/BlockCG/test.hb
    	test/BlockCG/test_bl_pcg_hb.cpp
     ----------------------------------------------------------------------
    This commit consists of the remaining files submitted by Teri.  These
    files relate to the test driver.  The classic make system has not been
    tested with the new directory structure.  Belos has not been setup to
    configure and build with Autotools yet either.

---
+++ packages/belos/test/distrib_msr_matrix.cpp
@@ -0,0 +1,136 @@
+//
+// file distrib_msr_matrix.cpp
+//
+// Modified version of Trilinos_Util_distrib_msr_matrix.cpp
+// This version only distributes the matrix from an HB
+// file. It ignores any right-hand side, initial guess,
+// and exact solution information that may be in the file.
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Trilinos_Util.h"
+#include "Epetra_Comm.h"
+#include "Epetra_Map.h"
+#include "Util.h"
+
+void distrib_msr_matrix(const Epetra_Comm & Comm, int *N_global,
+	      int *n_nonzeros, int *N_update, int **update,
+	      double **val, int **bindx)
+#undef DEBUG 
+
+{
+  int i, n_global_nonzeros;
+  int j, row, have_xexact = 0 ;
+  int *bindx1;
+  double *val1;
+
+  int MyPID = Comm.MyPID();
+  int NumProc = Comm.NumProc();
+
+  printf("Processor %d of %d entering distrib_matrix.\n",
+	 MyPID,NumProc) ;
+
+  /*************** Distribute global matrix to all processors ************/
+
+
+  if(NumProc  > 1)
+    { 
+
+      Comm.Broadcast( N_global, 1, 0);
+      Comm.Broadcast( n_nonzeros, 1, 0);
+
+      if(MyPID != 0)
+	{
+	  (*bindx) = (int   *) calloc(*n_nonzeros+1,sizeof(int)) ;
+	  (*val) = (double *) calloc(*n_nonzeros+1,sizeof(double)) ;
+	}
+
+      Comm.Broadcast( (*bindx), (*n_nonzeros+1),  0);
+      Comm.Broadcast( (*val),  (*n_nonzeros+1),   0);
+
+      printf("Processor %d of %d done with matrix broadcast.\n",
+	     MyPID,NumProc) ;
+ 
+    }
+
+  /********************** Generate update map  *************************/
+
+
+  Epetra_Map map(*N_global, 0, Comm);
+  *N_update = map.NumMyElements();
+  (*update) = (int *) calloc(*N_update,sizeof(int)) ;
+  map.MyGlobalElements(*update);
+  
+  printf("Processor %d of %d has %d rows of %d total rows.\n",
+	 MyPID,NumProc,*N_update,(*N_global)) ;
+
+  /*************** Construct local matrix from global matrix ************/
+
+  /* The local matrix is a copy of the rows assigned to this processor.  
+     It is stored in MSR format and still has global indices
+  */
+
+  if(NumProc  > 1)
+    { 
+      n_global_nonzeros = *n_nonzeros;
+
+      *n_nonzeros = *N_update;
+      
+      for (i=0; i<*N_update; i++)
+	*n_nonzeros += (*bindx)[(*update)[i]+1] - (*bindx)[(*update)[i]];
+
+      printf("Processor %d of %d has %d nonzeros of %d total nonzeros.\n",
+	     MyPID,NumProc,
+	     *n_nonzeros,n_global_nonzeros) ;
+
+      /* Allocate memory for local matrix */
+
+      bindx1 = (int   *) calloc(*n_nonzeros+1,sizeof(int)) ;
+      val1 = (double *) calloc(*n_nonzeros+1,sizeof(double)) ;
+     
+      bindx1[0] = *N_update+1;
+      
+      for (i=0; i<*N_update; i++)
+	{
+	  row = (*update)[i];
+	  val1[i] = (*val)[row];
+	  bindx1[i+1] = bindx1[i];
+
+#ifdef DEBUG	  
+	  printf("Proc %d of %d: Global row = %d: Local row = %d: bindx = %d: val = %12.4g \n",
+		 MyPID,NumProc, row, i, bindx1[i], val1[i]) ;
+#endif
+
+	  for (j = (*bindx)[row]; j < (*bindx)[row+1]; j++)
+	    {
+	      val1[  bindx1 [i+1] ] = (*val)[j];
+	      bindx1[bindx1 [i+1] ] = (*bindx)[j];
+	      bindx1[i+1] ++;
+	    }
+	}
+
+      printf("Processor %d of %d done with extracting local operators.\n",
+	     MyPID,NumProc) ;
+
+  
+      /* Release memory for global matrix, rhs and solution */
+      
+      free ((void *) (*val));
+      free ((void *) (*bindx));
+      //free ((void *) (*b));
+      //free ((void *) (*x));
+      //if (have_xexact) free((void *) *xexact);
+
+      /* Return local matrix through same pointers. */
+      
+      *val = val1;
+      *bindx = bindx1;
+
+    }
+     
+  
+  printf("Processor %d of %d leaving distrib_matrix.\n",
+	 MyPID,NumProc) ;
+  
+  /* end distrib_matrix */
+}
---
+++ packages/belos/test/read_hb_matrix.cpp
@@ -0,0 +1,147 @@
+//
+//  file read_hb_matrix.cpp
+//
+// Modified version of Trilinos_Util_read_hb.cpp
+// This version only reads in the matrix from an HB
+// data set. It ignores any right-hand side, initial guess,
+// and exact solution information that may be in the file.
+// 
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "Trilinos_Util.h"
+#include "iohb.h"
+#include "Util.h"
+
+void read_hb_matrix(char *data_file, int MyPID,
+	      int *N_global, int *n_nonzeros, 
+	      double **val, int **bindx)
+#undef DEBUG 
+     /*  read ASCII data file:
+	 line 1: N_global, number of entries (%d,%d)
+	 line 2-...: i,j,real (%d, %d, %f)
+     */
+
+{
+  FILE *in_file ;
+  char Title[73], Key[9], Rhstype[4];
+  char Type[4] = "XXX";
+  char Ptrfmt[17], Indfmt[17], Valfmt[21], Rhsfmt[21];
+  int Ptrcrd, Indcrd, Valcrd, Rhscrd;
+
+  int i, n_entries=0, N_columns=0, Nrhs=0;
+  int isym;
+  int *pntr, *indx1, *pntr1;
+  double *val1;
+
+  
+  if(MyPID == 0)  { 
+      in_file = fopen( data_file, "r");
+      if (in_file == NULL)
+	{
+	  printf("Error: Cannot open file: %s\n",data_file);
+	  exit(1);
+	}
+
+      /* Get information about the array stored in the file specified in the  */
+      /* argument list:                                                       */
+
+      printf("Reading matrix info from %s...\n",data_file);
+      
+      in_file = fopen( data_file, "r");
+      if (in_file == NULL)
+	{
+	  printf("Error: Cannot open file: %s\n",data_file);
+	  exit(1);
+	}
+
+      readHB_header(in_file, Title, Key, Type, N_global, &N_columns, 
+		    &n_entries, &Nrhs,
+		    Ptrfmt, Indfmt, Valfmt, Rhsfmt,
+		    &Ptrcrd, &Indcrd, &Valcrd, &Rhscrd, Rhstype);
+      fclose(in_file);
+
+      if (Nrhs < 0 ) Nrhs = 0;
+
+      printf("***************************************************************\n");
+      printf("Matrix in file %s is %d x %d, \n",data_file, *N_global, N_columns);
+      printf("with %d nonzeros with type %3s;\n", n_entries, Type);
+      printf("***************************************************************\n");
+      printf("Title: %72s\n",Title);
+      printf("***************************************************************\n");
+      /*Nrhs = 0; */
+      //printf("%d right-hand-side(s) available.\n",Nrhs);
+
+      if (Type[0] != 'R') perror("Can only handle real valued matrices");
+      isym = 0;
+      if (Type[1] == 'S') 
+	{
+	  printf("Converting symmetric matrix to nonsymmetric storage\n");
+	  n_entries = 2*n_entries - N_columns;
+	  isym = 1;
+	}
+      if (Type[2] != 'A') perror("Can only handle assembled matrices");
+      if (N_columns != *N_global) perror("Matrix dimensions must be the same");
+      *n_nonzeros = n_entries;
+      
+      /* Read the matrix information, generating the associated storage arrays  */
+      printf("Reading the matrix from %s...\n",data_file);
+
+      /* Allocate space.  Note that we add extra storage in case of zero
+	 diagonals.  This is necessary for conversion to MSR format. */
+
+      pntr   = (int    *) calloc(N_columns+1,sizeof(int)) ;
+      *bindx = (int    *) calloc(n_entries+N_columns+1,sizeof(int)) ;
+      *val   = (double *) calloc(n_entries+N_columns+1,sizeof(double)) ;
+      
+      readHB_mat_double(data_file, pntr, *bindx, *val);
+
+      for (i = 0; i <= *N_global; i++) pntr[i]--;
+      for (i = 0; i <= n_entries; i++) (*bindx)[i]--;
+   
+      /* Allocate temporary space */
+      
+      pntr1 = (int   *) calloc(N_columns+1,sizeof(int)) ;
+      indx1 = (int   *) calloc(n_entries+N_columns+1,sizeof(int)) ;
+      val1 = (double *) calloc(n_entries+N_columns+1,sizeof(double)) ;
+      
+      
+      /* Convert in the following way:
+	 - CSC to CSR 
+	 - CSR to MSR
+      */
+      Trilinos_Util_csrcsc(*N_global, *N_global, 0, 0, *val, *bindx, pntr, val1, indx1, pntr1);
+      
+      if (Type[1] == 'S') 
+	{
+	  int *indu, *iwk;
+	  int ierr;
+	  indu = new int[N_columns];
+	  iwk = new int[N_columns+1];
+	  ierr = Trilinos_Util_ssrcsr(3, 1, N_columns, val1, indx1, pntr1, n_entries,
+	  		  val1, indx1, pntr1, indu, iwk);
+	  delete [] indu;
+	  delete [] iwk;
+	  if (ierr !=0 ) 
+	    {
+	    printf(" Error in converting from symmetric form\n  IERR = %d\n",ierr);
+	    abort();
+	    }
+	}
+
+      Trilinos_Util_csrmsr(*N_global, val1, indx1, pntr1, *val, *bindx, *val, *bindx);
+      
+      /* Recompute number of nonzeros in case there were zero diagonals */
+      
+      *n_nonzeros = (*bindx)[*N_global] - 1; /* Still in Fortran mode so -2 */
+            
+      /* Release unneeded space */
+
+      free((void *) val1);
+      free((void *) indx1);
+      free((void *) pntr1);
+      free((void *) pntr);
+    }
+  
+  /* end read_hb */
+}
