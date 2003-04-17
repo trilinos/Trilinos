554d05cdaddfd4d37d2b6ee1e45e51a26a44a023
Eric T. Phipps <etphipp@sandia.gov>
2003-04-17 20:49:45 +0000

    Added AbstractGroup base classes.  These provide the interface originally provided by the LOCA::Abstract::Group.

---
+++ packages/nox/src-loca/src/LOCA_Bifurcation_HopfBord_AbstractGroup.C
@@ -0,0 +1,99 @@
+// $Id$
+// $Source$
+
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//   
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//   
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// 
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov).
+// 
+// ************************************************************************
+//@HEADER
+
+#include "NOX_Parameter_List.H"
+#include "LOCA_Bifurcation_HopfBord_AbstractGroup.H"
+
+NOX::Abstract::Group::ReturnType
+LOCA::Bifurcation::HopfBord::AbstractGroup::applyComplexInverseMulti(
+			     NOX::Parameter::List& params,
+			     const NOX::Abstract::Vector* const* inputs_real,
+			     const NOX::Abstract::Vector* const* inputs_imag,
+			     double frequency,
+			     NOX::Abstract::Vector** results_real,
+			     NOX::Abstract::Vector** results_imag,
+			     int nVecs) const
+{
+  NOX::Abstract::Group::ReturnType res;
+
+  for (int i=0; i<nVecs; i++) {
+    res = applyComplexInverse(params, *inputs_real[i], *inputs_imag[i],
+			      frequency, *results_real[i], *results_imag[i]);
+    if (res != NOX::Abstract::Group::Ok)
+      return res;
+  }
+
+  return res;
+}
+
+NOX::Abstract::Group::ReturnType
+LOCA::Bifurcation::HopfBord::AbstractGroup::applyComplex(
+		                      const NOX::Abstract::Vector& input_real,
+				      const NOX::Abstract::Vector& input_imag,
+				      double frequency,
+				      NOX::Abstract::Vector& result_real,
+				      NOX::Abstract::Vector& result_imag) const
+{
+  NOX::Abstract::Group::ReturnType res;
+
+  NOX::Abstract::Vector *tmp = input_real.clone(NOX::ShapeCopy);
+
+  // Compute J*y
+  res = applyJacobian(input_real, result_real);
+  if (res != NOX::Abstract::Group::Ok)
+    return res;
+
+  // Compute B*z
+  res = applyJacobian(input_imag, *tmp);
+  if (res != NOX::Abstract::Group::Ok)
+    return res;
+
+  // Compute J*y - w*B*z
+  result_real.update(-frequency, *tmp, 1.0);
+
+  // Compute J*z
+  res = applyJacobian(input_imag, result_imag);
+  if (res != NOX::Abstract::Group::Ok)
+    return res;
+
+  // Compute B*y
+  res = applyJacobian(input_real, *tmp);
+  if (res != NOX::Abstract::Group::Ok)
+    return res;
+
+  // Compute w*B*y + J*z
+  result_imag.update(frequency, *tmp, 1.0);
+
+  delete tmp;
+
+  return NOX::Abstract::Group::Ok;
+}
---
+++ packages/nox/src-loca/src/LOCA_Continuation_AbstractGroup.C
@@ -0,0 +1,79 @@
+// $Id$
+// $Source$
+
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This program is free software; you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation; either version 2, or (at your option)
+// any later version.
+//   
+// This program is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// General Public License for more details.
+//   
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+// 
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov).
+// 
+// ************************************************************************
+//@HEADER
+
+#include "NOX_Parameter_List.H"
+#include "LOCA_Continuation_AbstractGroup.H"
+
+NOX::Abstract::Group::ReturnType
+LOCA::Continuation::AbstractGroup::applyJacobianInverseMulti(
+			    NOX::Parameter::List& params,
+			    const NOX::Abstract::Vector* const* inputs,
+			    NOX::Abstract::Vector** outputs, int nVecs) const
+{
+  NOX::Abstract::Group::ReturnType res;
+
+  for (int i=0; i<nVecs; i++) {
+    res = applyJacobianInverse(params, *inputs[i], *outputs[i]);
+    if (res != NOX::Abstract::Group::Ok)
+      return res;
+  }
+
+  return res;
+}
+
+double
+LOCA::Continuation::AbstractGroup::computeScaledDotProduct(
+					 const NOX::Abstract::Vector& a,
+					 const NOX::Abstract::Vector& b) const
+{
+  return a.dot(b);
+}
+
+NOX::Abstract::Group::ReturnType
+LOCA::Continuation::AbstractGroup::computeEigenvalues(
+					        NOX::Parameter::List& params)
+{
+  cerr << "Error:  No eigensolver defined for group" << endl;
+  return NOX::Abstract::Group::Failed;
+}
+
+const LOCA::Continuation::AbstractGroup&
+LOCA::Continuation::AbstractGroup::getUnderlyingGroup() const
+{
+  return *this;
+}
+
+LOCA::Continuation::AbstractGroup&
+LOCA::Continuation::AbstractGroup::getUnderlyingGroup()
+{
+  return *this;
+}
