a4bcdfacfb3319a277c24b86c62f4d170ecb7669
Tamara G. Kolda <tgkolda@sandia.gov>
2003-04-29 17:09:02 +0000

    Revised the capability to use a user-defined line search. It's now nearly identical to the way user-defined directions are used.

--- packages/nox/src/NOX_LineSearch_Manager.C
+++ packages/nox/src/NOX_LineSearch_Manager.C
@@ -36,6 +36,7 @@
 #include "NOX_Abstract_Group.H"
 #include "NOX_Parameter_List.H"
 #include "NOX_Utils.H"
+#include "NOX_Parameter_LineSearchConstructor.H"
 
 // All the different line searches
 #include "NOX_LineSearch_FullStep.H"
@@ -49,88 +50,95 @@
 #include "NOX_LineSearch_MoreThuente2.H"
 #endif
 
-using namespace NOX;
-using namespace NOX::LineSearch;
-
-Manager::Manager(const NOX::Utils& u, NOX::Parameter::List& params) :
+NOX::LineSearch::Manager::Manager(const NOX::Utils& u, NOX::Parameter::List& params) :
   utils(u),
   method(""),
-  ptr(NULL),
-  isOwned(false)
+  ptr(NULL)
 {
   reset(params);
 }
 
-Manager::~Manager()
+NOX::LineSearch::Manager::~Manager()
 {
-  if (isOwned)
-    delete ptr;
+  delete ptr;
 }
 
-bool Manager::reset(Parameter::List& params)
+bool NOX::LineSearch::Manager::reset(Parameter::List& params)
 {
    string newmethod = params.getParameter("Method", "Full Step");
 
-  if (method != newmethod) {
-    
-    method = newmethod;
-    
-    if (isOwned) {
-      delete ptr;
-      ptr = NULL;
-    }
-    
-    if (method == "Full Step")
-      ptr = new FullStep(params);
-    else if (method == "Backtrack")
-      ptr = new Backtrack(utils, params);
-    else if (method == "Polynomial")
-      ptr = new Polynomial(utils, params);
-    else if (method == "More'-Thuente")
-      ptr = new MoreThuente(utils, params);
-    else if (method == "Secant")
-      ptr = new Secant(utils, params);
-#ifdef WITH_PRERELEASE
-    else if (method == "Quadratic")
-      ptr = new Quadratic(utils, params);
-    else if (method == "PolynomialWalker")
-      ptr = new PolynomialWalker(utils, params);
-    else if (method == "More'-Thuente2")
-      ptr = new MoreThuente2(utils, params);
-#endif
-    else if ((method == "User Defined") && 
-	     (params.isParameterArbitrary("User Defined Direction"))) {
-
-      const NOX::LineSearch::Generic* tmpPtr = 
-	dynamic_cast<const NOX::LineSearch::Generic*>
-	(&(params.getArbitraryParameter("User Defined Line Search")));
-      
-      ptr = NULL;
-      ptr = const_cast<NOX::LineSearch::Generic*>(tmpPtr);
-
-      if (ptr == NULL) {
-	if (utils.isPrintProcessAndType(NOX::Utils::Warning)) {
-	  cout << "NOX::LineSearch::Manager::reset() - Failed to dynamic_cast "
-	       << "arbitrary parameter in \"User Defined Line Search\" into "
-	       << "a NOX::LineSearch::Generic object!" << endl;
-	  throw "NOX Error";
-	}
-
-      }
-      isOwned = false;  
-    }
-    else {
-      ptr = NULL;
-      cout << "ERROR: NOX::LineSearch::Manager - invalid choice \"" 
-	   << method << "\" for line search method " << endl;
-      throw "NOX Error";
-    }
+  // If the method has not changeed, just call reset on the method.
+  if (method == newmethod) 
+  {
+    return ptr->reset(params);
   }
 
-  return ptr->reset(params);
+  method = newmethod;
+  delete ptr;
+  ptr = NULL;
+    
+  if (method == "Full Step")
+    ptr = new FullStep(params);
+  else if (method == "Backtrack")
+    ptr = new Backtrack(utils, params);
+  else if (method == "Polynomial")
+    ptr = new Polynomial(utils, params);
+  else if (method == "More'-Thuente")
+    ptr = new MoreThuente(utils, params);
+  else if (method == "Secant")
+    ptr = new Secant(utils, params);
+#ifdef WITH_PRERELEASE
+  else if (method == "Quadratic")
+    ptr = new Quadratic(utils, params);
+  else if (method == "PolynomialWalker")
+    ptr = new PolynomialWalker(utils, params);
+  else if (method == "More'-Thuente2")
+    ptr = new MoreThuente2(utils, params);
+#endif
+  else if (method == "User Defined")
+  {
+    // Check that the corresponding Direction parameter exists
+    if (!params.isParameterArbitrary("User Defined Constructor"))
+    {
+      printWarning("reset", "No \"User Defined Constructor\" specified");
+      return false;
+    }
+    
+    // Extract the Arbitrary Parameter
+    const NOX::Parameter::Arbitrary& ap = 
+      params.getArbitraryParameter("User Defined Constructor");
+    
+    // Dynamically cast the Arbitrary Parameter to a LineSearchConstructor Parameter
+    const NOX::Parameter::LineSearchConstructor* lscPtr = 
+      dynamic_cast<const NOX::Parameter::LineSearchConstructor*>(&ap);
+    
+    // Check that the cast was successful
+    if (lscPtr == NULL)
+    {
+      printWarning("reset", "Cannot do dynamic cast from Arbitrary to LineSearchConstructor");
+      return false;
+    }
+    
+    // Create a new direction from the LineSearchConstructor object
+    ptr = lscPtr->newLineSearch(utils, params);
+    
+    // Check that the creation was successful
+    if (ptr == NULL) 
+    {
+      printWarning("reset", "LineSearchConstructor object failed to create new direction");
+      return false;
+    }
+  }
+  else 
+  {
+    printWarning("reset", "invalid choice (" + method + ") for direction method");
+    return false;
+  }
+
+  return (ptr != NULL);
 }
 
-bool Manager::compute(Abstract::Group& newgrp, double& step, 
+bool NOX::LineSearch::Manager::compute(Abstract::Group& newgrp, double& step, 
 		      const Abstract::Vector& dir,
 		      const Solver::Generic& s) 
 {
@@ -138,3 +146,8 @@ bool Manager::compute(Abstract::Group& newgrp, double& step,
 }
 
 
+void NOX::LineSearch::Manager::printWarning(const string& name, const string& warning)
+{
+  if (utils.doPrint(NOX::Utils::Warning)) 
+    cout << "Calling NOX::LineSearch::Manager::" << name << " - " << warning << endl;
+}
