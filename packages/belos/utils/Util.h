57c9d8d6698e76345a6ff90d7220bc4f81b9575c
Heidi K. Thornquist <hkthorn@sandia.gov>
2003-05-07 21:34:42 +0000

    ----------------------------------------------------------------------
     Committing in .
    
     Modified Files:
     	test/Makefile.am test/Makefile.in test/BlockCG/Makefile.am
     	test/BlockCG/Makefile.in test/BlockCG/classicMakefile
     	test/BlockGmres/Makefile.am test/BlockGmres/Makefile.in
     	test/BlockGmres/classicMakefile
     Added Files:
     	utils/Makefile.am utils/Makefile.in utils/classicMakefile
     	utils/distrib_msr_matrix.cpp utils/read_hb_matrix.cpp
     	utils/Util.h
     ----------------------------------------------------------------------
    
      This commit creates a new belos/utils directory which will contain the
      Harwell-Boeing matrix input files for the block solvers.  Classic makefiles
      are created for this new library (libbelosutils) and those in the test
      directory are modified to find the new library.  The automake files Makefile.am
      for the utils and test directory will create and use the new library under
      the configure script build of belos.

---
+++ packages/belos/utils/Util.h
@@ -0,0 +1,20 @@
+//
+// file Util.h
+//
+// Additional function prototypes -- these 2 functions
+// are not included in Trilinos_Util.h
+//
+
+class Epetra_Comm;
+
+
+void read_hb_matrix(char *data_file, int MyPID,
+	      int *N_global, int *n_nonzeros, 
+	      double **val, int **bindx);
+
+
+
+void distrib_msr_matrix(const Epetra_Comm & Comm,
+	      int *N_global, int *n_nonzeros, 
+           int *N_update, int **update, 
+	      double **val, int **bindx);
