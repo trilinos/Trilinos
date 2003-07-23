10d84381b9267f9bd56fdd4d03a580bdf2668004
Jonathan Hu <jhu@sandia.gov>
2003-07-23 17:49:54 +0000

    This is a new header file that should be included in every other header file in the ML src tree.   If the macro ENABLE_HEADER_H exists, this file in turn includes a header file generated by autoconfig.  This allows autoconfig defines to toggle ML macro definitions as necessary.

---
+++ packages/ml/src/Include/ml_common.h
@@ -0,0 +1,20 @@
+/* ******************************************************************** */
+/* See the file COPYRIGHT for a complete copyright notice, contact      */
+/* person and disclaimer.                                               */        
+/* ******************************************************************** */
+
+/* ******************************************************************** */
+/* Include file that should be included in every header file.           */
+/* ******************************************************************** */
+/* Author        : Jonathan Hu                                          */
+/* Date          : July, 2003                                           */
+/* ******************************************************************** */
+
+#ifndef __MLCOMMON__
+#define __MLCOMMON__
+
+#ifdef HAVE_CONFIG_H 
+#include "ml_config.h"
+#endif
+
+#endif
