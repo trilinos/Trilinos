3032603a2d2405cec8293e9de390b2626fca6373
Jonathan Hu <jhu@sandia.gov>
2003-07-23 21:35:36 +0000

    Added some macro checks and definitions.

--- packages/ml/src/Include/ml_common.h
+++ packages/ml/src/Include/ml_common.h
@@ -15,6 +15,48 @@
 
 #ifdef HAVE_CONFIG_H 
 #include "ml_config.h"
+
+#ifdef HAVE_ML_AZTEC
+#define AZTEC
 #endif
+
+#ifdef HAVE_ML_SUPERLU
+#define SUPERLU
+#endif
+
+#ifdef HAVE_MPI
+#define ML_MPI
+#endif
+
+#if defined(HAVE_ML_EXTERNAL_MPI_FUNCTIONS) && defined(HAVE_MPI)
+#define ML_USING_MPI_FUNCTIONS
+#endif
+
+#ifdef HAVE_BLAS
+#define USE_VENDOR_BLAS
+#endif
+
+#ifdef HAVE_LAPACK
+#define USE_VENDOR_LAPACK
+#endif
+
+#ifdef HAVE_ML_ENRICH
+#define ML_ENRICH
+#endif
+
+#ifdef HAVE_ML_NEW_T_PE
+#define ML_NEW_T_PE
+#endif
+
+#ifdef HAVE_ML_COMPLEX_MAXWELL
+#define GREG
+#endif
+
+#ifdef HAVE_ML_TIMING
+#define ML_TIMING
+#endif
+
+
+#endif /*ifdef HAVE_CONFIG_H*/
 
 #endif
