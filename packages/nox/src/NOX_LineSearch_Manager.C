ae857c75b4639a74daad65edb86c42616638f2f1
Russell Hooper <rhoope@sandia.gov>
2003-08-13 15:24:30 +0000

    Removed header and instantiation of PolynomialWalker linesearch.

--- packages/nox/src/NOX_LineSearch_Manager.C
+++ packages/nox/src/NOX_LineSearch_Manager.C
@@ -46,7 +46,6 @@
 #include "NOX_LineSearch_MoreThuente2.H"
 #ifdef WITH_PRERELEASE
 #include "NOX_LineSearch_NonlinearCG.H"
-#include "NOX_LineSearch_PolynomialWalker.H"
 #endif
 
 NOX::LineSearch::Manager::Manager(const NOX::Utils& u, NOX::Parameter::List& params) :
@@ -89,8 +88,6 @@ bool NOX::LineSearch::Manager::reset(Parameter::List& params)
 #ifdef WITH_PRERELEASE
   else if (method == "NonlinearCG")
     ptr = new NonlinearCG(utils, params);
-  else if (method == "PolynomialWalker")
-    ptr = new PolynomialWalker(utils, params);
 #endif
   else if (method == "User Defined")
   {
