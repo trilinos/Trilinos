86acf8ae4b6787628047b41465c05da3196ad06d
Russell Hooper <rhoope@sandia.gov>
2003-08-27 22:03:35 +0000

    Expanded option choices.

--- packages/nox/src-petsc/NOX_Petsc_Options.C
+++ packages/nox/src-petsc/NOX_Petsc_Options.C
@@ -39,9 +39,9 @@ Options::Options()
 {
 }
 
-Options::Options(NOX::Parameter::List& params)
+Options::Options(NOX::Parameter::List& params, int rank_) :
+  rank(rank_)
 {
-  cout << "Inside Options......will now fill params !!" << endl << endl;
   setOptions(params);
 }
 
@@ -52,20 +52,22 @@ Options::~Options()
 
 bool Options::setOptions(NOX::Parameter::List& nlParams)
 {
-  cout << "Inside Options::setOptions......will now fill params !!" << endl << endl;
- 
+
   // First allow solution-type to be specified
-  ierr = PetscOptionsHasName(PETSC_NULL,"-nox_trustregion_based",&flg);CHKERRQ(ierr);
+  ierr = PetscOptionsHasName(PETSC_NULL,"-nox_trustregion_based",&flg);
+         CHKERRQ(ierr);
   if(flg)
     nlParams.setParameter("Nonlinear Solver", "Trust Region Based");
   else // default
+    // This is done to allow PetscOptions to register that this option was used
+    ierr = PetscOptionsHasName(PETSC_NULL,"-nox_linesearch_based",&flg);
+           CHKERRQ(ierr);
     nlParams.setParameter("Nonlinear Solver", "Line Search Based");
 
   // Now allow linesearch type to be specified
   NOX::Parameter::List& searchParams = nlParams.sublist("Line Search");
   ierr = PetscOptionsGetString(PETSC_NULL,"-nox_linesearch_type",
                optionString, maxStringLength, &flg);CHKERRQ(ierr);
-
   if(flg)
   {
     cout << "linesearch optionString --> " << optionString << endl;
@@ -77,8 +79,6 @@ bool Options::setOptions(NOX::Parameter::List& nlParams)
       searchParams.setParameter("Method", "Backtrack");
     if( !strcmp(optionString, "more_thuente") )
       searchParams.setParameter("Method", "More'-Thuente");
-    if( !strcmp(optionString, "more_thuente2") )
-      searchParams.setParameter("Method", "More'-Thuente2");
 #ifdef WITH_PRERELEASE
     if( !strcmp(optionString, "nonlinearcg") )
       searchParams.setParameter("Method", "NonlinearCG");
@@ -87,7 +87,66 @@ bool Options::setOptions(NOX::Parameter::List& nlParams)
   else // default
     searchParams.setParameter("Method", "Full Step");
 
-  nlParams.print(cout);
-  
+  // Now allow direction type to be specified
+  NOX::Parameter::List& dirParams = nlParams.sublist("Direction");
+  ierr = PetscOptionsGetString(PETSC_NULL,"-nox_direction_type",
+               optionString, maxStringLength, &flg);CHKERRQ(ierr);
+  if(flg)
+  {
+    if( !strcmp(optionString, "newton") )
+      dirParams.setParameter("Method", "Newton");
+    if( !strcmp(optionString, "steepest_descent") )
+    {
+      dirParams.setParameter("Method", "Steepest Descent");
+
+      // Check to see if any steepest_descent options are set
+      PetscTruth lflg;
+      ierr = PetscOptionsGetString(PETSC_NULL,"-nox_sd_scaling_type",
+                   optionString, maxStringLength, &lflg);CHKERRQ(ierr);
+      if(lflg)
+      {
+        NOX::Parameter::List& sdParams = dirParams.sublist("Steepest Descent");
+        if( !strcmp(optionString, "none") )
+          sdParams.setParameter("Scaling Type", "None");
+        else if( !strcmp(optionString, "2norm") )
+          sdParams.setParameter("Scaling Type", "2-Norm");
+        else if( !strcmp(optionString, "quadratic_model_min") )
+          sdParams.setParameter("Scaling Type", "Quadratic Model Min");
+        else 
+        {
+          if(rank == 0) cout << "WARNING: Unsupported Steepest Descent "
+                             << "Scaling Type --> " << optionString << endl;
+          sdParams.setParameter("Scaling Type", "None"); // default
+        }
+      }
+    } 
+#ifdef WITH_PRERELEASE
+    if( !strcmp(optionString, "nonlinearcg") )
+      dirParams.setParameter("Method", "Nonlinear CG");
+    // Need to make provision for the following
+      //NOX::Parameter::List& nlcgParams = dirParams.sublist("Nonlinear CG");
+        //nlcgParams.setParameter("Restart Frequency", 2000);
+        //nlcgParams.setParameter("Precondition", "On");
+        //nlcgParams.setParameter("Orthogonalize", "Polak-Ribiere");
+        //nlcgParams.setParameter("Orthogonalize", "Fletcher-Reeves");
+#endif
+  }
+  else // default
+    dirParams.setParameter("Method", "Newton");
+
+  // Now set output parameters via the "Printing" sublist 
+  // These are hard-coded for now
+  NOX::Parameter::List& printParams = nlParams.sublist("Printing");
+  printParams.setParameter("MyPID", rank);
+  printParams.setParameter("Output Precision", 3);
+  printParams.setParameter("Output Processor", 0);
+  printParams.setParameter("Output Information",
+                        NOX::Utils::OuterIteration +
+                        NOX::Utils::OuterIterationStatusTest +
+                        NOX::Utils::InnerIteration +
+                        NOX::Utils::Parameters +
+                        NOX::Utils::Details +
+                        NOX::Utils::Warning);
+
   return true;
 }
