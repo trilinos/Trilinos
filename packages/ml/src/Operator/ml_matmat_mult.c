5bcd74c06f11c4eb7c006b2b92f0dbceb7528941
Michael A. Heroux <maherou@sandia.gov>
2003-09-25 04:28:33 +0000

    ----------------------------------------------------------------------
     Modified Files:
     	ml_matmat_mult.c
     ----------------------------------------------------------------------
    
    Fixed a line break that split a starting " from the ending " in a C
    string.  The error caused GCC under Cygwin to fail.

--- packages/ml/src/Operator/ml_matmat_mult.c
+++ packages/ml/src/Operator/ml_matmat_mult.c
@@ -1040,8 +1040,7 @@ int ML_hash_it( int new_val, int hash_list[], int hash_length,int *hash_used) {
 #ifdef charles
     if (origindex == index)
        fprintf(stderr,"ML_hash_it: looped around"
-       " original index = %d, new_val = %d, hash_length = %d, hash_used =
-       %d\n");
+       " original index = %d, new_val = %d, hash_length = %d, hash_used = %d\n");
 #endif
   }
 
