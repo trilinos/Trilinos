7a3027f7e3dff3a81257f3e21bdcd12e9e8776f8
Marzio Sala <msala>
2003-11-03 22:24:52 +0000

    A new structure is defined here, ML_Aggregate_Viz_Stats_Struct. It is used to store information about nodal coordinates and the non-smoothed aggregation, in order to perform some statistics and visualizations.

---
+++ packages/ml/src/Main/ml_viz_stats.h
@@ -0,0 +1,29 @@
+/********************************************************************* */
+/* See the file COPYRIGHT for a complete copyright notice, contact      */
+/* person and disclaimer.                                               */   
+/* ******************************************************************** */
+
+#ifndef __MLVIZSTATS__
+#define __MLVIZSTATS__
+
+/*MS*/
+#define ML_AGGREGATE_VIZ_STATS_ID 24680
+
+typedef struct ML_Aggregate_Viz_Stats_Struct
+{
+  int id;
+  double *x;
+  double *y;
+  double *z;
+  int *graph_decomposition;
+  int Nlocal;
+  int Naggregates;
+  int local_or_global;
+  int is_filled;
+  void *Amatrix;  /* void * so that I do not have to include
+		     ml_operator.h */
+  
+} ML_Aggregate_Viz_Stats;
+/*ms*/
+
+#endif /* #ifndef __MLAGGMETIS__ */
