6f0c9ff3bedc8d7710ec3d6ee4e9ddf7b21e63c5
Kendall S. Stanley <ksstanl>
2003-11-19 15:05:32 +0000

    Cleaned up the example for inclusion in the web page

--- packages/new_package/example/cxx_main.cpp
+++ packages/new_package/example/cxx_main.cpp
@@ -1,31 +1,5 @@
-//@HEADER
-// ************************************************************************
-// 
-//          Trilinos: An Object-Oriented Solver Framework
-//              Copyright (2001) Sandia Corporation
-// 
-// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
-// license for use of this work by or on behalf of the U.S. Government.
-// 
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2, or (at your option)
-// any later version.
-//   
-// This program is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// General Public License for more details.
-//   
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-// 
-// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
-// 
-// ************************************************************************
-//@HEADER
-
+//  See "copypright.h" for copyright information
+#include "copyright.h"
 //
 // hello_test
 //
@@ -33,16 +7,11 @@
 //     hello_test
 //
 //  output:  
-//     stdout
+//     prints a summary line and one line "Hello" for each process to standard out
+//     If --enable-newp_swahili is set on the configure line:
+//        prints a summary line and one line "Jambo" for each process to standard out
 //
-//  exits with 0 if test completed (does not imply that the test passed)
-//  exits with -1 if command line options or file permissions are wrong 
-//
-
-//  #include "New_Package_config.h" - I suspect that Epatra_config.h has everything we need 
-//  and hence obviates the need for New_Package_config.h
-//  Or maybe not - we might still need HAVE_NEWP_SWAHILI 
-//  
+#endif
 #include "Newp_Hello.h"
 #ifdef HAVE_NEWP_SWAHILI
 #include "Newp_Jambo.h"
@@ -50,8 +19,10 @@
 
 int main(int argc, char **argv)
 {
-  int exit_value = 0 ; 
-
+  //
+  //  If --enable-mpi, an MPI communicator is used, otherwise a serial
+  //  stub communicator is used.  
+  //
 #ifdef EPETRA_MPI
   MPI_Init(&argc,&argv);
   Epetra_MpiComm Comm (MPI_COMM_WORLD);
@@ -59,8 +30,18 @@ int main(int argc, char **argv)
   Epetra_SerialComm Comm;
 #endif
 
+  //
+  //  Print out a summary line followed by a "Hello" line from each process
+  //
   Newp_Hello Hello( Comm ) ; 
   Hello.Print( cout );
+
+
+  //
+  //  If --enable-newp_swahili is set, HAVE_NEWP_SWAHILI is set in 
+  //    New_Package_config.h which is included by Newp_Hello.h and hence:
+  //      Print out a summary line followed by a "Jambo" line from each process
+  //
 #ifdef HAVE_NEWP_SWAHILI
   Newp_Jambo Jambo( Comm ) ; 
   Jambo.Print( cout );
@@ -69,7 +50,7 @@ int main(int argc, char **argv)
 #ifdef EPETRA_MPI
   MPI_Finalize();
 #endif
-  return( exit_value ) ; 
+  return 0;
 }
 
   
