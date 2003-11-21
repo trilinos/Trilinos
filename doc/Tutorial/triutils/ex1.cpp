7ffc670c7d1e095c2549b5f116ddfba6b7a9945b
Marzio Sala <msala>
2003-11-21 15:22:34 +0000

    minor modifications

--- doc/Tutorial/triutils/ex1.cpp
+++ doc/Tutorial/triutils/ex1.cpp
@@ -64,7 +64,7 @@ int main(int argc, char *argv[]) {
   int MyPID = Comm.MyPID();
 
   // matrix downloaded from MatrixMarket
-  char FileName[] = "bcsstk14.rsa";
+  char FileName[] = "../HBMatrices/bcsstk14.rsa";
 
   Epetra_Map * readMap; // Pointers because of Trilinos_Util_ReadHb2Epetra
   Epetra_CrsMatrix * readA; 
