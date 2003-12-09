b4ea5e2d3cd7a47310a83187f51e86b20f04400c
Alan B. Williams <william@sandia.gov>
2003-12-09 21:30:54 +0000

    Added doxygen comments for EpetraExt::MatrixMatrix::Product.

--- packages/epetraext/src/EpetraExt_MatrixMatrix.h
+++ packages/epetraext/src/EpetraExt_MatrixMatrix.h
@@ -40,6 +40,20 @@ namespace EpetraExt {
     virtual ~MatrixMatrix(){}
 
     /** Given Epetra_CrsMatrix objects A, B and C, form the product C = A*B.
+	In a parallel setting, A and B need not have matching distributions,
+	but C needs to have the same row-map as A.
+
+    @param A Input, must already have had 'FillComplete()' called.
+    @param B Input, must already have had 'FillComplete()' called.
+    @param C Result. On entry to this method, it doesn't matter whether
+             FillComplete() has already been called on C or not. If it has,
+	     then C's graph must already contain all nonzero locations that
+	     will be produced when forming the product A*B. On exit,
+	     C.FillComplete() will have been called.
+
+    @return error-code, 0 if successful. non-zero returns may result if A or
+             B are not already Filled, or if errors occur in putting values
+             into C, etc.
      */
     static int Product(const Epetra_CrsMatrix& A,
 		       const Epetra_CrsMatrix& B,
