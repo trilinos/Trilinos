e42dec4c133e6dd000f1591262667e7d11f4917e
Heidi K. Thornquist <hkthorn@sandia.gov>
2003-12-11 00:43:30 +0000

    ----------------------------------------------------------------------
     Committing in .
    
     Modified Files:
     	Teuchos_Time.cpp
     ----------------------------------------------------------------------
    
     Added some ICL/MINGW dependent preprocessing to use the correct timing calls
     for those compilers.

--- packages/teuchos/src/Teuchos_Time.cpp
+++ packages/teuchos/src/Teuchos_Time.cpp
@@ -2,8 +2,17 @@
 // 07.08.03 -- Move into Teuchos package/namespace
 
 #include "Teuchos_Time.hpp"
+
+#ifdef HAVE_MPI
+#include "mpi.h"
+#elif ICL
+#include <time.h>
+#else
 #include <sys/time.h>
+#ifndef MINGW
 #include <sys/resource.h>
+#endif
+#endif
 
 using namespace Teuchos;
 
@@ -34,6 +43,21 @@ double Time::stop()
 double Time::wallTime() 
 {
   /* KL: warning: this code is probably not portable! */
+  /* HT: have added some preprocessing to address problem compilers */
+#ifdef HAVE_MPI
+
+  return(MPI_Wtime());
+
+#elif ICL
+
+  clock_t start;
+
+  start = clock();
+  return( (double)( start ) / CLOCKS_PER_SEC );
+
+#else
+
+#ifndef MINGW
   struct timeval tp;
   static long start = 0, startu;
   if (!start)
@@ -45,5 +69,11 @@ double Time::wallTime()
   }
   gettimeofday(&tp, NULL);
   return( ((double) (tp.tv_sec - start)) + (tp.tv_usec-startu)/1000000.0 );
+#else
+  return( (double) clock() / CLOCKS_PER_SEC );
+#endif
+
+#endif
+
 }
 
