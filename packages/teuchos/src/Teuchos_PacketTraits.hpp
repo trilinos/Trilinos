5a62fe7cd2ecb7af32de556bf179f59d32811fa5
Heidi K. Thornquist <hkthorn@sandia.gov>
2004-01-16 21:13:07 +0000

    ----------------------------------------------------------------------
     Committing in .
    
     Modified Files:
     	Teuchos_PacketTraits.hpp
     ----------------------------------------------------------------------
    
     Modified misnamed OrdinalTraits structs to PacketTraits structs.
     Prepared PacketTraits file for move back to Tpetra, per discussion with
     Mike Heroux.

--- packages/teuchos/src/Teuchos_PacketTraits.hpp
+++ packages/teuchos/src/Teuchos_PacketTraits.hpp
@@ -1,13 +1,14 @@
-#ifndef _TEUCHOS_PACKETTRAITS_HPP_
-#define _TEUCHOS_PACKETTRAITS_HPP_
+#ifndef _TPETRA_PACKETTRAITS_HPP_
+#define _TPETRA_PACKETTRAITS_HPP_
 
 // Kris
 // 07.08.03 -- Move into Teuchos package/namespace
+// 01.16.04 -- Move back into Tpetra package/namespace per discussion with Mike Heroux.
 
-#include "Teuchos_ConfigDefs.hpp"
+#include "Tpetra_ConfigDefs.hpp"
 
-namespace Teuchos {
-  /** The Teuchos PacketTraits file.
+namespace Tpetra {
+  /** The Tpetra PacketTraits file.
 			
 	For the most general type 'class T', we define aborting functions, 
 	which should restrict implementations from using traits other than the
@@ -18,7 +19,7 @@ namespace Teuchos {
 	struct PacketTraits {
 		static inline int unsupportedType() {
 #ifndef TEUCHOS_NO_ERROR_REPORTS
-			cerr << endl << "Teuchos::PacketTraits: unsupported packet type." << endl;
+			cerr << endl << "Tpetra::PacketTraits: unsupported packet type." << endl;
 #endif
 			return(-1);
 		}
@@ -28,37 +29,37 @@ namespace Teuchos {
 	};
 	
 	template<>
-	struct OrdinalTraits<int> {
+	struct PacketTraits<int> {
 		static inline bool haveMachineParameters() {return(true);}; // Allows testing to see if packet traits machine parameters defined 
 		static inline int packetSize()             {return(sizeof(int));};
 		static inline const char* name()           {return("int");};
 	};
 
 	template<>
-	struct OrdinalTraits<float> {
+	struct PacketTraits<float> {
 		static inline bool haveMachineParameters() {return(true);}; // Allows testing to see if packet traits machine parameters defined 
 		static inline int packetSize()             {return(sizeof(float));};
 		static inline const char* name()           {return("float");};
 	};
 
 	template<>
-	struct OrdinalTraits<double> {
+	struct PacketTraits<double> {
 		static inline bool haveMachineParameters() {return(true);}; // Allows testing to see if packet traits machine parameters defined 
 		static inline int packetSize()             {return(sizeof(double));};
 		static inline const char* name()           {return("double");};
 	};
 
-#if (defined(HAVE_COMPLEX) || defined(HAVE_COMPLEX_H)) && defined(HAVE_TEUCHOS_COMPLEX)
+#if (defined(HAVE_COMPLEX) || defined(HAVE_COMPLEX_H))
 
 	template<>
-	struct OrdinalTraits<complex<float> > {
+	struct PacketTraits<complex<float> > {
 		static inline bool haveMachineParameters() {return(true);}; // Allows testing to see if packet traits machine parameters defined 
 		static inline int packetSize()             {return(sizeof(complex<float>));};
 		static inline const char* name()           {return("complex<float>");};
 	};
 
 	template<>
-	struct OrdinalTraits<complex<double> > {
+	struct PacketTraits<complex<double> > {
 		static inline bool haveMachineParameters() {return(true);}; // Allows testing to see if packet traits machine parameters defined 
 		static inline int packetSize()             {return(sizeof(complex<double>));};
 		static inline const char* name()           {return("complex<double>");};
@@ -66,6 +67,6 @@ namespace Teuchos {
 
 #endif // HAVE_COMPLEX || HAVE_COMPLEX_H
 
-} // namespace Teuchos
+} // namespace Tpetra
 
-#endif // _TEUCHOS_PACKETTRAITS_HPP_
+#endif // _TPETRA_PACKETTRAITS_HPP_
