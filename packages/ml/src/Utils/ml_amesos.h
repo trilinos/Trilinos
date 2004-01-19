a44a67e0fc48f4019ffc44643ec78a2516a75131
Marzio Sala <msala>
2004-01-19 16:03:42 +0000

    Erase ifpack functions.

--- packages/ml/src/Utils/ml_amesos.h
+++ packages/ml/src/Utils/ml_amesos.h
@@ -22,24 +22,4 @@ void ML_Smoother_Clean_Amesos(void *Amesos_Handle);
 
 int ML_Gen_Smoother_Amesos(ML *ml, int nl, int AmesosSolver, int);
 
-
-#define ML_IFPACK_ICT                     0
-#define ML_IFPACK_RICK                    1
-#define ML_IFPACK_RILUK                   2
-#define ML_IFPACK_JACOBI                 10
-/* options for ML_Gen_Smoother_Ifpack */
-#define ML_IFPACK_OPTIONS_SIZE           10
-#define ML_IFPACK_OVERLAP                 0
-#define ML_IFPACK_LEVEL_OF_FILL           1
-/* params for ML_Gen_Smoother_Ifpack */
-#define ML_IFPACK_PARAMS_SIZE            20
-#define ML_IFPACK_RELAX_VALUE             0
-
-extern int ML_Smoother_Ifpack(void *sm,int inlen,double x[],int outlen,
-			      double rhs[]);
-
-void ML_Smoother_Clean_Ifpack(void * Ifpack_Handle);
-
-int ML_Gen_Smoother_Ifpack(ML *ml, int nl, int choice, int *options, double *params);
-
 #endif /* #ifndef __ML_AMESOS_H__ */
--- packages/ml/src/Utils/ml_amesos_wrap.h
+++ packages/ml/src/Utils/ml_amesos_wrap.h
@@ -16,12 +16,6 @@ extern "C" {
 
   void ML_Amesos_Destroy(void *Amesos_Handle);
 
-  /* ~!@ */
-  int ML_Ifpack_Gen(ML *ml, int curr_level, int choice, int * options,
-		  double * params, void ** Ifpack_Handle);
-  int ML_Ifpack_Solve( void * Ifpack_Handle, double * x, double * rhs );
-  void ML_Ifpack_Destroy(void * Ifpack_Handle);
-  
 #ifndef ML_CPP
 #ifdef __cplusplus
 }
