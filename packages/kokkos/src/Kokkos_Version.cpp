9d714d9acd76cc3733e49c17318def3979477c81
James M. Willenbring <jmwille@sandia.gov>
2004-01-28 20:27:49 +0000

    ----------------------------------------------------------------------
    Committing in .
    
    Added Files:
    	Kokkos_Version.cpp
    ----------------------------------------------------------------------
    Contributes to Bug 178
    
    Kokkos_Version.cpp provides a function to return the version of Kokkos.
    
    Other than a search and replace, the file is the same as Paul Sexton's
    Tpetra_Version.cpp for Tpetra.
    
    Having this file will allow me to autotool Kokkos (it is cleaner if there
    is at least one .cpp file).

---
+++ packages/kokkos/src/Kokkos_Version.cpp
@@ -0,0 +1,10 @@
+//Eventually we will want to add Kokkos_ConfigDefs.hpp
+//#include "Kokkos_ConfigDefs.hpp"
+
+namespace Kokkos {
+
+	string Kokkos_Version() { 
+		return("Kokkos Version 1.0 - Not yet publicly released"); 
+	}
+
+} // namespace Kokkos 
