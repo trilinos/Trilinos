2440d4ed6e5b8cd3e2894797c447b8682e9cd5ba
Heidi K. Thornquist <hkthorn@sandia.gov>
2004-02-09 21:28:23 +0000

    ----------------------------------------------------------------------
     Committing in .
    
     Modified Files:
     	AnasaziBlockArnoldi.hpp AnasaziConfigDefs.hpp
     	AnasaziEigenproblem.hpp AnasaziMatrix.hpp AnasaziMultiVec.hpp
     	AnasaziOperator.hpp AnasaziPetraInterface.hpp
     	AnasaziReturnType.hpp AnasaziVersion.cpp
     ----------------------------------------------------------------------
    
     Added LGPL copyright header to files.

--- packages/anasazi/src/AnasaziConfigDefs.hpp
+++ packages/anasazi/src/AnasaziConfigDefs.hpp
@@ -1,7 +1,30 @@
-/*  This script was originated in Epetra (I believe) and is useful for defining a 
-common header file when the software package may not have been autoconfigured.
--- HKT  07/14/03
-*/
+// @HEADER
+// ***********************************************************************
+//
+//                 Anasazi: Block Eigensolvers Package
+//                 Copyright (2004) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ***********************************************************************
+// @HEADER
 
 #ifndef ANASAZI_CONFIGDEFS_HPP
 #define ANASAZI_CONFIGDEFS_HPP
