5b6958d693b7c80721738bb78fa47f73d92bf365
James M. Willenbring <jmwille@sandia.gov>
2004-02-10 23:28:41 +0000

    ----------------------------------------------------------------------
     Committing in .
    
     Modified Files:
     	src/Teuchos_RefCountPtr.cpp src/Teuchos_RefCountPtr.hpp
     	src/Teuchos_RefCountPtrDecl.hpp
     	src/Teuchos_TestForException.cpp
     	src/Teuchos_TestForException.hpp test/RefCountPtr/cxx_main.cpp
     ----------------------------------------------------------------------
    Contributes to bug 398
    
    Per a note from Ross B., I updated the copyrights in some older files.

--- packages/teuchos/src/Teuchos_RefCountPtrDecl.hpp
+++ packages/teuchos/src/Teuchos_RefCountPtrDecl.hpp
@@ -1,17 +1,30 @@
-// ///////////////////////////////////////////////////////////////////////
-// RefCountPtr_decl.hpp
-//
-// Copyright (C) 2001 Roscoe Ainsworth Bartlett
-//
-// This is free software; you can redistribute it and/or modify it
-// under the terms of the "Artistic License" (see the web site
-//   http://www.opensource.org/licenses/artistic-license.html).
-// This license is spelled out in the file COPYING.
-//
-// This software is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// above mentioned "Artistic License" for more details.
+// @HEADER
+// ***********************************************************************
+// 
+//                    Teuchos: Common Tools Package
+//                 Copyright (2004) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
+// 
+// ***********************************************************************
+// @HEADER
 
 #ifndef REFCOUNTPTR_DECL_H
 #define REFCOUNTPTR_DECL_H
