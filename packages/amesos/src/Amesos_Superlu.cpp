1d8f7d321fabddeff28ac86e30bbd0069a9b83cf
Kendall S. Stanley <ksstanl>
2004-03-04 01:23:58 +0000

    Adding Amesos_Superlu (still buggy)

---
+++ packages/amesos/src/Amesos_Superlu.cpp
@@ -0,0 +1,386 @@
+// @HEADER
+// ***********************************************************************
+// 
+//                Amesos: Direct Sparse Solver Package
+//                 Copyright (2004) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
+// 
+// ***********************************************************************
+// @HEADER
+
+#include "Amesos_Superlu.h"
+#include "Epetra_Map.h"
+#include "Epetra_Import.h"
+#include "Epetra_Export.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_Vector.h"
+#include "Epetra_Util.h"
+#include "CrsMatrixTranspose.h"
+extern "C" {
+  // #include "amd.h"
+#include "klu_btf.h"
+#if 0
+#include "klu_dump.h"
+#endif
+}
+
+class Amesos_Superlu_Pimpl {
+public:
+   klu_symbolic *Symbolic_ ;
+   klu_numeric *Numeric_ ;
+
+  Amesos_Superlu_Pimpl::Amesos_Superlu_Pimpl():
+    Symbolic_(0),
+    Numeric_(0)
+  {}
+  
+  Amesos_Superlu_Pimpl::~Amesos_Superlu_Pimpl(void){
+
+    if ( Symbolic_ ) klu_btf_free_symbolic (&Symbolic_) ;
+    if ( Numeric_ ) klu_btf_free_numeric (&Numeric_) ;
+  }
+
+} ;
+
+
+  //=============================================================================
+  Amesos_Superlu::Amesos_Superlu(const Epetra_LinearProblem &prob, 
+				 const AMESOS::Parameter::List &ParameterList ) :  
+    PrivateSuperluData_( new Amesos_Superlu_Pimpl() ),
+    SerialCrsMatrixA_(0), 
+    SerialMap_(0), 
+    SerialMatrix_(0), 
+    TransposeMatrix_(0),
+    UseTranspose_(false),
+    Matrix_(0) {
+
+  Problem_ = &prob ; 
+  ParameterList_ = &ParameterList ; 
+}
+
+//=============================================================================
+Amesos_Superlu::~Amesos_Superlu(void) {
+
+  if ( SerialMap_ ) delete SerialMap_ ; 
+  if ( SerialCrsMatrixA_ ) delete SerialCrsMatrixA_ ; 
+  if ( TransposeMatrix_ ) delete TransposeMatrix_ ; 
+  delete PrivateSuperluData_; 
+
+}
+//  See  pre and post conditions in Amesos_Superlu.h
+
+int Amesos_Superlu::ConvertToSerial() { 
+  
+  Epetra_RowMatrix *RowMatrixA = dynamic_cast<Epetra_RowMatrix *>(Problem_->GetOperator());
+  EPETRA_CHK_ERR( RowMatrixA == 0 ) ; 
+
+  Epetra_CrsMatrix *CastCrsMatrixA = dynamic_cast<Epetra_CrsMatrix*>(RowMatrixA) ; 
+  EPETRA_CHK_ERR( CastCrsMatrixA == 0 ) ; 
+
+  iam = Comm().MyPID() ;
+
+  const Epetra_Map &OriginalMap = CastCrsMatrixA->RowMap() ; 
+
+  NumGlobalElements_ = CastCrsMatrixA->NumGlobalRows();
+  numentries_ = CastCrsMatrixA->NumGlobalNonzeros();
+  assert( NumGlobalElements_ == CastCrsMatrixA->NumGlobalCols() );
+
+  //
+  //  Create a serial matrix 
+  //
+  assert( NumGlobalElements_ == OriginalMap.NumGlobalElements() ) ;
+  int NumMyElements_ = 0 ;
+  if (iam==0) NumMyElements_ = NumGlobalElements_;
+
+
+  IsLocal_ = ( OriginalMap.NumMyElements() == 
+	       OriginalMap.NumGlobalElements() )?1:0;
+  Comm().Broadcast( &IsLocal_, 1, 0 ) ; 
+
+  //
+  //  KEN:  Consider giving Epetra_RowMatrix_Transposer a shot
+  //  I am not confident that  Epetra_RowMatrix_Transposer works, 
+  //  but it is worth a try.  
+  //
+  //  Convert Original Matrix to Serial (if it is not already) 
+  //
+  if (SerialMap_) { delete SerialMap_ ; SerialMap_ = 0 ; } 
+  if ( SerialCrsMatrixA_ ) { delete SerialCrsMatrixA_ ; SerialCrsMatrixA_ = 0 ; } 
+  if ( IsLocal_==1 ) {
+     SerialMatrix_ = CastCrsMatrixA ;
+  } else {
+    if ( SerialMap_ ) delete SerialMap_ ; 
+    SerialMap_ = new Epetra_Map( NumGlobalElements_, NumMyElements_, 0, Comm() );
+
+    Epetra_Export export_to_serial( OriginalMap, *SerialMap_);
+
+    if ( SerialCrsMatrixA_ ) delete SerialCrsMatrixA_ ; 
+    SerialCrsMatrixA_ = new Epetra_CrsMatrix(Copy, *SerialMap_, 0);
+    SerialCrsMatrixA_->Export( *RowMatrixA, export_to_serial, Add ); 
+    
+    SerialCrsMatrixA_->TransformToLocal() ; 
+    SerialMatrix_ = SerialCrsMatrixA_ ;
+  }
+
+  return 0;
+} 
+
+//
+//  See also pre and post conditions in Amesos_Superlu.h
+//  Preconditions:  
+//    SerialMatrix_ points to the matrix to be factored and solved
+//    NumGlobalElements_ has been set to the dimension of the matrix
+//    numentries_ has been set to the number of non-zeros in the matrix
+//      (i.e. ConvertToSerial() has been callded) 
+//
+//  Postconditions:
+//    Ap, Ai, Aval contain the matrix as Superlu needs it (transposed 
+//     iff UseTranspose() is NOT true)
+//
+//  Issues:
+//    Is there a way to avoid deleting and recreating TransposeMatrix_?
+//    The straight forward attempt at a solution, i.e. re-using 
+//    TransposeMatrix_, fails in the call to CrsMatrixTranspose.
+//    However, since we own CrsMatrixTranspose, we can fix it.
+//
+//    Mike has mentioned a desire to have a Matrix Transpose operation
+//    in epetra (or epetraext).
+//
+int Amesos_Superlu::ConvertToSuperluCRS(bool firsttime){
+  
+  if ( UseTranspose() ) { 
+    Matrix_ = SerialMatrix_ ; 
+  } else { 
+#if 1
+    if ( TransposeMatrix_ ) delete TransposeMatrix_ ; 
+    TransposeMatrix_ =  new Epetra_CrsMatrix(Copy, (Epetra_Map &)SerialMatrix_->Map(), 0);
+#else
+    //
+    //  This fails in CrsMatrixTranspose
+    //
+    if ( firsttime && TransposeMatrix_ ) delete TransposeMatrix_ ; 
+    if ( firsttime ) TransposeMatrix_ =  new Epetra_CrsMatrix(Copy, (Epetra_Map &)SerialMatrix_->Map(), 0);
+#endif
+    EPETRA_CHK_ERR( CrsMatrixTranspose( SerialMatrix_, TransposeMatrix_ ) ) ; 
+    Matrix_ = TransposeMatrix_ ; 
+  }
+  //
+  //  Convert matrix to the form that Superlu expects (Ap, Ai, Aval) 
+  //
+
+  if ( iam==0 ) {
+    assert( NumGlobalElements_ == Matrix_->NumGlobalRows());
+    assert( NumGlobalElements_ == Matrix_->NumGlobalCols());
+    assert( numentries_ == Matrix_->NumGlobalNonzeros());
+    Ap.resize( NumGlobalElements_+1 );
+    Ai.resize( EPETRA_MAX( NumGlobalElements_, numentries_) ) ; 
+    Aval.resize( EPETRA_MAX( NumGlobalElements_, numentries_) ) ; 
+
+    int NumEntriesThisRow;
+    int Ai_index = 0 ; 
+    int MyRow;
+#ifdef USE_VIEW
+    double *RowValues;
+    int *ColIndices;
+#else
+    int MaxNumEntries_ = Matrix_->MaxNumEntries();
+    vector<int>ColIndices(MaxNumEntries_);
+    vector<double>RowValues(MaxNumEntries_);
+#endif
+    for ( MyRow = 0; MyRow <NumGlobalElements_; MyRow++ ) {
+#ifdef USE_VIEW
+      EPETRA_CHK_ERR( Matrix_->
+		      ExtractMyRowView( MyRow, NumEntriesThisRow, RowValues, 
+					ColIndices ) != 0 ) ;
+#else
+      EPETRA_CHK_ERR( Matrix_->
+		      ExtractMyRowCopy( MyRow, MaxNumEntries_, 
+					NumEntriesThisRow, &RowValues[0], 
+					&ColIndices[0] ) != 0 ) ;
+#endif
+      Ap[MyRow] = Ai_index ; 
+      for ( int j = 0; j < NumEntriesThisRow; j++ ) { 
+	Ai[Ai_index] = ColIndices[j] ; 
+	Aval[Ai_index] = RowValues[j] ; 
+	Ai_index++;
+      }
+    }
+    Ap[MyRow] = Ai_index ; 
+  }
+
+  
+  return 0;
+}   
+
+
+int Amesos_Superlu::ReadParameterList() {
+  if (ParameterList_->isParameterSublist("Superlu") ) {
+    AMESOS::Parameter::List SuperluParams = ParameterList_->sublist("Superlu") ;
+  }  
+  return 0;
+}
+
+
+int Amesos_Superlu::PerformSymbolicFactorization() {
+
+  if ( iam == 0 ) { 
+    PrivateSuperluData_->Symbolic_ = klu_btf_analyze (NumGlobalElements_, &Ap[0], &Ai[0] ) ;
+    if ( PrivateSuperluData_->Symbolic_ == 0 ) EPETRA_CHK_ERR( 1 ) ; 
+  }
+  return 0;
+}
+
+int Amesos_Superlu::PerformNumericFactorization( ) {
+
+  if ( iam == 0 ) {
+
+    const double tol = 0.001; //  At some point we need to expose this to the user 
+
+    if ( PrivateSuperluData_->Numeric_ ) klu_btf_free_numeric (&(PrivateSuperluData_->Numeric_)) ;
+    PrivateSuperluData_->Numeric_ = klu_btf_factor (&Ap[0], &Ai[0], &Aval[0], tol, PrivateSuperluData_->Symbolic_) ;
+    if ( PrivateSuperluData_->Numeric_ == 0 ) EPETRA_CHK_ERR( 2 ) ; 
+    
+  }
+  
+  return 0;
+}
+
+
+
+
+bool Amesos_Superlu::MatrixShapeOK() const { 
+  bool OK ;
+
+  if ( GetProblem()->GetOperator()->OperatorRangeMap().NumGlobalPoints() != 
+       GetProblem()->GetOperator()->OperatorDomainMap().NumGlobalPoints() ) OK = false;
+  return OK; 
+}
+
+
+int Amesos_Superlu::SymbolicFactorization() {
+
+  ConvertToSerial() ; 
+  
+  ConvertToSuperluCRS(true);
+  
+  PerformSymbolicFactorization();
+
+  return 0;
+}
+
+int Amesos_Superlu::NumericFactorization() {
+  
+  ConvertToSerial() ; 
+  ConvertToSuperluCRS(false);
+
+  PerformNumericFactorization( );
+  return 0;
+}
+
+
+int Amesos_Superlu::Solve() { 
+
+  Epetra_MultiVector   *vecX = Problem_->GetLHS() ; 
+  Epetra_MultiVector   *vecB = Problem_->GetRHS() ; 
+
+  //
+  //  Compute the number of right hands sides (and check that X and B have the same shape) 
+  //
+  int nrhs; 
+  if ( vecX == 0 ) { 
+    nrhs = 0 ;
+    EPETRA_CHK_ERR( vecB != 0 ) ; 
+  } else { 
+    nrhs = vecX->NumVectors() ; 
+    EPETRA_CHK_ERR( vecB->NumVectors() != nrhs ) ; 
+  }
+
+  Epetra_MultiVector *SerialB =0;
+  Epetra_MultiVector *SerialX =0;
+  //
+  //  Extract Serial versions of X and B 
+  //
+  double *SerialXvalues ;
+
+  Epetra_RowMatrix *RowMatrixA = dynamic_cast<Epetra_RowMatrix *>(Problem_->GetOperator());
+  Epetra_CrsMatrix *CastCrsMatrixA = dynamic_cast<Epetra_CrsMatrix*>(RowMatrixA) ; 
+  Epetra_MultiVector *SerialXextract = 0;
+  Epetra_MultiVector *SerialBextract = 0;
+    
+  //
+  //  Copy B to the serial version of B
+  //
+  if ( IsLocal_ ==1 ) { 
+    SerialB = vecB ; 
+    SerialX = vecX ; 
+  } else { 
+    assert( IsLocal_ == 0 ) ;
+    const Epetra_Map &OriginalMap = CastCrsMatrixA->RowMap();
+    Epetra_MultiVector *SerialXextract = new Epetra_MultiVector( *SerialMap_, nrhs ) ; 
+    Epetra_MultiVector *SerialBextract = new Epetra_MultiVector( *SerialMap_, nrhs ) ; 
+
+    Epetra_Import ImportToSerial( *SerialMap_, OriginalMap );
+    SerialBextract->Import( *vecB, ImportToSerial, Insert ) ;
+    SerialB = SerialBextract ; 
+    SerialX = SerialXextract ; 
+  } 
+
+  //
+  //  Call SUPERLU to perform the solve
+  //
+
+  
+  SerialX->Scale(1.0, *SerialB) ;
+
+  int SerialXlda ; 
+  if ( iam == 0 ) {
+    assert( SerialX->ExtractView( &SerialXvalues, &SerialXlda ) == 0 ) ; 
+
+    assert( SerialXlda == NumGlobalElements_ ) ; 
+    
+    for ( int j =0 ; j < nrhs; j++ ) { 
+      double *Control = (double *) NULL, *Info = (double *) NULL ;
+
+      int status = 0 ; 
+      vector<double> workspace( NumGlobalElements_ ) ; 
+      klu_btf_solve( PrivateSuperluData_->Symbolic_, PrivateSuperluData_->Numeric_,
+		     &SerialXvalues[j*SerialXlda], &workspace[0] );
+    }
+  }
+    
+
+   
+  //
+  //  Copy X back to the original vector
+  // 
+
+  if ( IsLocal_ == 0 ) { 
+    const Epetra_Map &OriginalMap = CastCrsMatrixA->RowMap() ; 
+    Epetra_Import ImportFromSerial( OriginalMap, *SerialMap_ );
+    vecX->Import( *SerialX, ImportFromSerial, Insert ) ;
+    delete SerialBextract ;
+    delete SerialXextract ;
+  } else {
+    assert( SerialBextract == 0 ) ;
+    assert( SerialXextract == 0 ) ;
+  }  
+  return(0) ; 
+}
---
+++ packages/amesos/src/Amesos_Superlu.h
@@ -0,0 +1,346 @@
+// @HEADER
+// ***********************************************************************
+// 
+//                Amesos: Direct Sparse Solver Package
+//                 Copyright (2004) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//  
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
+// 
+// ***********************************************************************
+// @HEADER
+
+#ifndef _AMESOS_SUPERLU_H_
+#define _AMESOS_SUPERLU_H_
+
+#include "Amesos_ConfigDefs.h"
+#include "Amesos_BaseSolver.h"
+#include "Epetra_LinearProblem.h"
+#ifdef EPETRA_MPI
+#include "Epetra_MpiComm.h"
+#else
+#include "Epetra_Comm.h"
+#endif
+#include "Epetra_CrsGraph.h"
+
+
+//! Amesos_Klu:  A serial, unblocked code ideal for getting started and for very sparse matrices, such as circuit matrces.  AmesosKlu computes <p class="code">A<sup>T</sup> X = B</p> more efficiently than <p class="code">A X = B</p>.
+/*!  Amesos_Klu, an object-oriented wrapper for Klu, will solve a linear systems of equations: <TT>A X = B</TT>
+   using Epetra objects and the Klu solver library, where
+  <TT>A</TT> is an Epetra_RowMatrix and <TT>X</TT> and <TT>B</TT> are 
+  Epetra_MultiVector objects.
+
+<br /><br /><p>AmesosKlu computes <p class="code">A<sup>T</sup> X = B</p> 
+more efficiently than <p class="code">A X = B</p>.  The
+latter requires a matrix transpose - which costs both time and space.
+
+<br /><br /><p>klu is Davis' implementation of Gilbert-Peierl's left-looking
+sparse partial pivoting algorithm, with Eisenstat & Liu's symmetric
+pruning.  Gilbert's version appears as [L,U,P]=lu(A) in MATLAB.
+It doesn't exploit dense matrix kernels, but it is the only sparse
+LU factorization algorithm known to be asymptotically optimal,
+in the sense that it takes time proportional to the number of
+floating-point operations.  It is the precursor to SuperLU,
+thus the name ("clark Kent LU").  For very sparse matrices that
+do not suffer much fill-in (such as most circuit matrices when
+permuted properly) dense matrix kernels do not help, and the
+asymptotic run-time is of practical importance.
+
+<br /><br /><p>The klu_btf code first permutes the matrix to upper block
+triangular form (using two algorithms by Duff and Reid,
+MC13 and MC21, in the ACM Collected Algorithms).  It then permutes
+each block via a symmetric minimum degree ordering (AMD, by Amestoy,
+Davis, and Duff).  This ordering phase can be done just once
+for a sequence of matrices.  Next, it factorizes each reordered
+block via the klu routine, which also attempts to preserve
+diagonal pivoting, but allows for partial pivoting if the diagonal
+is to small.    
+<!-- A fast-factorization version of klu is
+available, which does not do any partial pivoting at all.  -->
+
+
+<br /><br /><p>  Klu execution can be tuned through a variety of parameters.
+  Amesos_Klu.h allows control of these parameters through the
+  following named parameters, ignoring parameters with names that it
+  does not recognize.  Where possible, the parameters are common to
+  all direct solvers (although some may ignore them).  However, some
+  parameters, in particular tuning parameters, are unique to each
+  solver.
+    
+*/
+
+// Amesos_Superlu_Pimpl contains a pointer to two sructures defined in 
+// klu.h:  klu_symbolic and klu_numeric.  This prevents Amesos_Klu.h 
+// from having to include klu.h.
+//
+//  Doxygen does not handle forward class references well.
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+class Amesos_Superlu_Pimpl ; 
+#endif
+
+class Amesos_Superlu: public Amesos_BaseSolver { 
+
+public: 
+
+  //@{ \name Constructor methods
+  //! Amesos_Superlu Constructor.
+  /*! Creates an Amesos_Superlu instance, using an Epetra_LinearProblem,
+      passing in an already-defined Epetra_LinearProblem object. 
+
+      Note: The operator in LinearProblem must be an
+      Epetra_RowMatrix.
+
+  */
+  Amesos_Superlu(const Epetra_LinearProblem& LinearProblem, const AMESOS::Parameter::List &ParameterList );
+
+  //! Amesos_Superlu Destructor.
+  /*! Completely deletes an Amesos_Superlu object.  
+  */
+  ~Amesos_Superlu(void);
+  //@}
+
+  //@{ \name Mathematical functions.
+
+    //! Performs SymbolicFactorization on the matrix A.
+    /*! 
+      In addition to performing symbolic factorization on the matrix A, 
+      the call to SymbolicFactorization() implies that no change will
+      be made to the non-zero structure of the underlying matrix without 
+      a subsequent call to SymbolicFactorization().
+      
+      preconditions:<ul>
+      <li>GetProblem().GetOperator() != 0 (return -1)
+      <li>MatrixShapeOk(GetProblem().GetOperator()) == true (return -6)
+      </ul>
+
+      postconditions:<ul>
+      <li>Symbolic Factorization will be performed (or marked to be performed) 
+      allowing NumericFactorization() and Solve() to be called.
+      </ul>
+
+    \return Integer error code, set to 0 if successful.
+  */
+    int SymbolicFactorization() ;
+
+    //! Performs NumericFactorization on the matrix A.
+    /*!  In addition to performing numeric factorization (and symbolic
+      factorization if necessary) on the matrix A, the call to
+      NumericFactorization() implies that no change will be made to
+      the underlying matrix without a subsequent call to
+      NumericFactorization().  
+
+      preconditions:<ul>
+      <li>GetProblem().GetOperator() != 0 (return -1)
+      <li>MatrixShapeOk(GetProblem().GetOperator()) == true (return -6)
+      <li>The non-zero structure of the matrix should not have changed
+          since the last call to SymbolicFactorization().  
+      <li>The distribution of the matrix should not have changed 
+          since the last call to SymbolicFactorization()
+      </ul>
+
+      postconditions:<ul>
+      <li>Numeric Factorization will be performed (or marked to be performed) 
+      allowing Solve() to be performed correctly despite a potential change in 
+      in the matrix values (though not in the non-zero structure).
+      </ul>
+
+     \return Integer error code, set to 0 if successful.
+  */
+    int NumericFactorization() ;
+
+    //! Solves A X = B (or A<SUP>T</SUP> X = B) 
+    /*! 
+
+      preconditions:<ul>
+      <li>GetProblem().GetOperator() != 0 (return -1)
+      <li>MatrixShapeOk(GetProblem().GetOperator()) == true (return -6)
+      <li>GetProblem()->CheckInput (see Epetra_LinearProblem::CheckInput() for return values)
+      <li>The non-zero structure of the matrix should not have changed
+          since the last call to SymbolicFactorization().
+      <li>The distribution of the matrix should not have changed 
+          since the last call to SymbolicFactorization()
+      <li>The matrix should not have changed
+          since the last call to NumericFactorization().
+      </ul>
+
+      postconditions:<ul> 
+      <li>X will be set such that A X = B (or
+      A<SUP>T</SUP> X = B), within the limits of the accuracy of the
+      underlying solver.  
+      </ul>
+
+     \return Integer error code, set to 0 if successful.
+  */
+    int Solve();
+
+  //@}
+  
+  //@{ \name Additional methods required to support the Epetra_Operator interface.
+
+#if 0
+  //! Returns a character string describing the operator
+  char * Label() const {return(Epetra_Object::Label());};
+#endif
+    
+  //! Get a pointer to the Problem.
+  const Epetra_LinearProblem *GetProblem() const { return(Problem_); };
+
+  //! Get a pointer to the ParameterList.
+  const AMESOS::Parameter::List *GetParameterList() const { return(ParameterList_); };
+
+  //! Returns true if SUPERLU can handle this matrix shape 
+  /*! Returns true if the matrix shape is one that SUPERLU can
+    handle. SUPERLU only works with square matrices.  
+  */
+  bool MatrixShapeOK() const ;
+
+  //! SetUseTranpose(true) is more efficient in Amesos_Superlu
+  /*! 
+<ul>
+  <li>If SetUseTranspose() is set to true, 
+    <ul>
+       <li><p class="code">A<sup>T</sup> X = B</p> is computed</li>
+       <li>(This is the more efficient operation)</li>
+    </ul></li>
+  <li>else
+    <ul>
+       <li><p class="code">A X = B</p> is computed</li>
+       <li>(This requires a matrix transpose)</li>
+    </ul></li>
+</ul>
+  */  
+  int SetUseTranspose(bool UseTranspose) {UseTranspose_ = UseTranspose; return(0);};
+
+  //! Returns the current UseTranspose setting.
+  bool UseTranspose() const {return(UseTranspose_);};
+
+  //! Returns a pointer to the Epetra_Comm communicator associated with this matrix.
+  const Epetra_Comm & Comm() const {return(GetProblem()->GetOperator()->Comm());};
+
+  //! Reads the parameter list and updates internal variables. 
+  /*!
+    ReadParameterList is called by SymbolicFactorization.  Hence, few codes 
+    will need to make an explicit call to ReadParameterList.
+   */
+  int ReadParameterList() ;
+  //@}
+
+ private:  
+  /*
+  ConvertToSerial - Convert matrix to a serial Epetra_CrsMatrix
+    Preconditions:
+      Problem_ must be set 
+      SerialMap and SerialCrsMatrix must either be 0 or be pointers to 
+        appropriatly allocate objects.  If they are non-zero, those objects
+	will be deleted (and possibly recreated).  
+	
+    Postconditions:
+      IsLocal is set to 1 if the input matrix is entirely stored on process 0
+      SerialMap points to a serial map if IsLocal==1
+      SerialCrsMatrix contains a serial version of the matrix A if IsLocal==1
+      SerialMatrix points to a serial copy of the matrix
+      NumGlobalElements_   is set to the number of rows in the matrix
+      numentries_ is set to the number of non-zeroes in the matrix 
+   */
+  int ConvertToSerial();
+
+  /*
+    ConvertToSuperluCRS - Convert matirx to form expected by Superlu: Ai, Ap, Aval
+    Preconditions:
+      numentries_, NumGloalElements_ and SerialMatrix_ must be set.
+    Postconditions:
+      Ai, Ap, and Aval are resized and populated with a compresses row storage 
+      version of the input matrix A.
+  */
+  int ConvertToSuperluCRS(bool firsttime);     
+
+  /*
+    PerformSymbolicFactorization - Call Superlu to perform symbolic factorization
+    Preconditions:
+      IsLocal must be set to 1 if the input matrix is entirely stored on process 0
+      Ap, Ai and Aval are a compressed row storage version of the input matrix A.
+    Postconditions:
+      Symbolic points to an SUPERLU internal opaque object containing the
+        symbolic factorization and accompanying information.  
+      SymbolicFactorizationOK_ = true; 
+    Note:  All action is performed on process 0
+  */
+      
+  int PerformSymbolicFactorization(); 
+
+  /*
+    PerformNumericFactorization - Call Superlu to perform numeric factorization
+    Preconditions:
+      IsLocal must be set 
+      Ap, Ai and Aval are a compressed row storage version of the input matrix A.
+      Symbolic must be set
+    Postconditions:
+      Numeric points to an SUPERLU internal opaque object containing the
+        numeric factorization and accompanying information.  
+      NumericFactorizationOK_ = true; 
+    Note:  All action is performed on process 0
+  */
+  int PerformNumericFactorization(); 
+
+ protected:
+
+    int *Lp, *Li, *Up, *Ui, *P ;	
+    double *Lx, *Ux ;
+    Amesos_Superlu_Pimpl *PrivateSuperluData_; 
+    
+
+  //
+  //  Ap, Ai, Aval form the compressed row storage used by Superlu
+  //
+  vector <int> Ap;
+  vector <int> Ai;
+  vector <double> Aval;
+
+  int iam;                 //  Process number (i.e. Comm().MyPID() 
+  
+  int IsLocal_;            //  1 if Problem_->GetOperator() is stored entirely on process 0
+                           //  Note:  Local Problems do not require redistribution of
+                           //  the matrix A or vectors X and B.
+  int numentries_;         //  Number of non-zero entries in Problem_->GetOperator()
+  int NumGlobalElements_;  //  Number of rows and columns in the Problem_->GetOperator()
+
+  Epetra_Map *SerialMap_ ;               //  Points to a Serial Map (unused if IsLocal == 1 ) 
+  Epetra_CrsMatrix *SerialCrsMatrixA_ ;  //  Points to a Serial Copy of A (unused if IsLocal==1)
+  Epetra_CrsMatrix *SerialMatrix_ ;      //  Points to a Serial Copy of A 
+                                         //  IsLocal==1 - Points to the original matix 
+                                         //  IsLocal==0 - Points to SerialCrsMatrixA
+  Epetra_CrsMatrix *TransposeMatrix_ ;   //  Points to a Serial Transposed Copy of A 
+  //
+  //  This USE_VIEW define shows that we can could easily support the 
+  //  Epetra_RowMatrix interface.  
+  //
+#define USE_VIEW
+#ifdef USE_VIEW
+  Epetra_CrsMatrix *Matrix_ ;            //  Points to the matrix that is used to compute
+#else
+  Epetra_RowMatrix *Matrix_ ;            //  Points to the matrix that is used to compute
+#endif                                         //  the values passed to Superlu
+                                     
+
+  bool UseTranspose_;      //  Set by 
+  const Epetra_LinearProblem * Problem_;
+  const AMESOS::Parameter::List * ParameterList_ ; 
+
+};  // End of  class Amesos_Superlu  
+#endif /* _AMESOS_SUPERLU_H_ */
