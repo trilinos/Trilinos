95d046a7c621bdfb48348195d8b87136b4934692
Kris Kampshoff <kwkamps>
2004-04-09 21:16:38 +0000

    : Modified Files: : 	cxx_main.cpp
    Added templated utility functions for constructing Hilbert matrices, performing Cholesky factorization, and printing matrices and vectors.

--- packages/teuchos/example/hilbert/cxx_main.cpp
+++ packages/teuchos/example/hilbert/cxx_main.cpp
@@ -37,14 +37,178 @@ using namespace std;
 using namespace Teuchos;
 
 #ifdef HAVE_TEUCHOS_ARPREC
-#define SType1	   mp_real
+#define SType1	   double
 #else
-#define SType1     float
+#define SType1     double
 #endif
 #define SType2     double
 #define OType	   int
 
+template<typename TYPE>
+void ConstructHilbertMatrix(TYPE*, int);
+
+template<typename TYPE>
+void ConstructHilbertSumVector(TYPE*, int);
+
+template<typename TYPE>
+bool Cholesky(TYPE*, int);
+
+template<typename TYPE>
+void PrintArrayAsVector(TYPE*, int);
+
+template<>
+void PrintArrayAsVector(mp_real*, int);
+
+template<typename TYPE>
+void PrintArrayAsMatrix(TYPE*, int, int);
+
+template<>
+void PrintArrayAsMatrix(mp_real*, int, int);
+
 int main(int argc, char *argv[]) {
-  cout << "Hello, world!" << endl;
+
+#ifdef HAVE_TEUCHOS_ARPREC
+  mp::mp_init(40);
+#endif
+
+  int n = 3;
+  SType1* A = new SType1[n*n];
+  A[0] = 1;
+  A[1] = 2;
+  A[2] = 3;
+  A[3] = 2;
+  A[4] = 5;
+  A[5] = 8;
+  A[6] = 3;
+  A[7] = 8;
+  A[8] = 14;
+
+  PrintArrayAsMatrix(A, n, n);
+
+  Cholesky(A, n);
+
+  PrintArrayAsMatrix(A, n, n);
+
+  SType1* H5 = new SType1[5*5];
+  ConstructHilbertMatrix(H5, 5);
+  PrintArrayAsMatrix(H5, 5, 5);
+
+  SType1* b = new SType1[5];
+  ConstructHilbertSumVector(b, 5);
+  PrintArrayAsVector(b, 5);
+
+  delete[] A;
+  delete[] H5;
+  delete[] b;
+
+#ifdef HAVE_TEUCHOS_ARPREC
+  mp::mp_finalize();
+#endif
+
   return 0;
+}
+
+template<typename TYPE>
+void ConstructHilbertMatrix(TYPE* A, int n) {
+  TYPE sOne = ScalarTraits<TYPE>::one();
+  for(int i = 0; i < n; i++) {
+    for(int j = 0; j < n; j++) {
+      A[i + (j * n)] = (sOne / (i + j + sOne));
+    }
+  }
+}
+
+template<typename TYPE>
+void ConstructHilbertSumVector(TYPE* x, int n) {
+  TYPE sZero = ScalarTraits<TYPE>::zero();
+  TYPE sOne = ScalarTraits<TYPE>::one();
+  for(int i = 0; i < n; i++) {
+    x[i] = sZero;
+    for(int j = 0; j < n; j++) {
+      x[i] += (sOne / (i + j + sOne));
+    }
+  }  
+}
+
+template<typename TYPE>
+bool Cholesky(TYPE* A, int n) {
+  TYPE sZero = ScalarTraits<TYPE>::zero();
+  for(int k = 0; k < n; k++) {
+    for(int j = k + 1; j < n; j++) {
+      TYPE alpha = A[k + (j * n)] / A[k + (k * n)];
+      for(int i = j; i < n; i++) {
+	A[j + (i * n)] -= (alpha * A[k + (i * n)]);
+      }
+    }
+    if(A[k + (k * n)] <= sZero) {
+      return 0;
+    }
+    TYPE beta = ScalarTraits<TYPE>::squareroot(A[k + (k * n)]);
+    for(int i = k; i < n; i++) {
+      A[k + (i * n)] /= beta;
+    }
+  }
+  return 1;
+}
+
+template<typename TYPE>
+void PrintArrayAsVector(TYPE* x, int n) {
+  cout << "[";
+  for(int i = 0; i < n; i++) {
+    cout << " " << x[i];
+  }
+  cout << " ]" << endl;
+}
+
+template<>
+void PrintArrayAsVector(mp_real* x, int n) {
+  cout << "[ ";
+  for(int i = 0; i < n; i++) {
+    if(i != 0) {
+      cout << "  ";
+    }
+    cout << x[i];
+  }
+  cout << "]" << endl;
+}
+
+template<typename TYPE>
+void PrintArrayAsMatrix(TYPE* a, int m, int n) {
+  cout << "[";
+  for(int i = 0; i < m; i++) {
+    if(i != 0) {
+      cout << " ";
+    }
+    cout << "[";
+    for(int j = 0; j < n; j++) {
+      cout << " " << a[i + (j * m)];
+    }
+    cout << " ]";
+    if(i != (m - 1)) {
+      cout << endl;
+    }
+  }
+  cout << "]" << endl;
+}
+
+template<>
+void PrintArrayAsMatrix(mp_real* a, int m, int n) {
+  cout << "[";
+  for(int i = 0; i < m; i++) {
+    if(i != 0) {
+      cout << " ";
+    }
+    cout << "[";
+    for(int j = 0; j < n; j++) {
+      if(j != 0) {
+	cout << "  ";
+      }
+      cout << " " << a[i + (j * m)];
+    }
+    cout << " ]";
+    if(i != (m - 1)) {
+      cout << endl;
+    }
+  }
+  cout << "]" << endl; 
 }
