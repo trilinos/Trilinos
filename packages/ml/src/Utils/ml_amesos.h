51f86e7b12f5eb88ad7f2b887a5b76544bfb03b0
Marzio Sala <msala>
2004-04-14 23:34:01 +0000

    Fix a couple of errors, mainly C comments instead of C++ comments

--- packages/ml/src/Utils/ml_amesos.h
+++ packages/ml/src/Utils/ml_amesos.h
@@ -19,16 +19,35 @@
 
 #ifndef ML_CPP
 #ifdef __cplusplus
-   extern "C" {
+extern "C" {
 #endif
 #endif
 
-extern int ML_Smoother_Amesos(void *sm,int inlen,double x[],int outlen,
-			      double rhs[]);
+  /** Applies the Amesos direct solver to the given vector. */
+  extern int ML_Smoother_Amesos(void *sm,int inlen,double x[],int outlen,
+				double rhs[]);
 
-void ML_Smoother_Clean_Amesos(void *Amesos_Handle);
+  /** Clean memory associated to Amesos_Handle. */
+  void ML_Smoother_Clean_Amesos(void *Amesos_Handle);
+  
+  /** Generates the coarse solver using Amesos. */
+  /*! Generates the coarse solver using one of the Amesos supported
+    direct solvers.
 
-int ML_Gen_Smoother_Amesos(ML *ml, int nl, int AmesosSolver, int);
+    \param \inout ml: ML_Structure;
+
+    \param \in nl: level for which we have to define the coarse solver;
+
+    \param \in AmesosSolver: integer variable, that can be ML_AMESOS_KLU,
+    ML_AMESOS_UMFPACK, ML_AMESOS_SUPERLUDIST, ML_AMESOS_MUMPS,
+    ML_AMESOS_SCALAPACK;
+
+    \param \in MaxProcs: integer defining the maximum number of
+    processes to use in the coarse solution (only for some of the
+    supported Amesos solvers).
+  */
+  
+int ML_Gen_Smoother_Amesos(ML *ml, int nl, int AmesosSolver, int MaxProcs);
 
 
 #ifndef ML_CPP
--- packages/ml/src/Utils/ml_amesos_wrap.h
+++ packages/ml/src/Utils/ml_amesos_wrap.h
@@ -11,11 +11,35 @@ extern "C" {
 #endif
 #endif
 
+  /** Generates the direct solver. */
+  /*! This function performs several operations:
+    - wrap the ML_Operator for the given level to an Epetra_CrsMatrix
+    - creates the Amesos object;
+    - compute the symbolic and numeric factorzation.
+
+    \param \inout ml: ML_Structure
+
+    \param \in curr_level: specifies the level for which we have to
+    define the direct solver;
+
+    \param \in choice: integer variable, that can be ML_AMESOS_KLU,
+    ML_AMESOS_UMFPACK, ML_AMESOS_SUPERLUDIST, ML_AMESOS_MUMPS,
+    ML_AMESOS_SCALAPACK;
+
+    \param \in MaxProcs: integer defining the maximum number of
+    processes to use in the coarse solution (only for some of the
+    supported Amesos solvers);
+
+    \param \out: it will contain a pointer to the Amesos object (casted
+    to void *).
+  */
   int ML_Amesos_Gen(ML *ml, int curr_level, int choice,
 		    int MaxProcs, void **Amesos_Handle);
-  
+
+  /** Solves using Amesos, and the factorization computed by ML_Amesos_Gen. */
   int ML_Amesos_Solve( void *Amesos_Handle, double x[], double rhs[] ) ;
 
+  /** Destroy the Amesos object. Prints out some timing. */
   void ML_Amesos_Destroy(void *Amesos_Handle);
 
 #ifndef ML_CPP
