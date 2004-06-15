3ec2f57a87c2f1f778d28f37f9b2c8212d383c1f
Marzio Sala <msala>
2004-06-15 17:37:04 +0000

    new name for ml_adaptive.cpp

---
+++ packages/ml/examples/ml_filtering.cpp
@@ -0,0 +1,131 @@
+#ifndef HAVE_CONFIG_H
+#define HAVE_CONFIG_H
+#endif
+#include "ml_config.h"
+
+#if defined(HAVE_ML_EPETRA) && defined(HAVE_ML_TEUCHOS) && defined(HAVE_ML_TRIUTILS)
+
+#include "ml_include.h"
+
+#ifdef HAVE_MPI
+#include "mpi.h"
+#include "Epetra_MpiComm.h"
+#else
+#include "Epetra_SerialComm.h"
+#endif
+#include "Epetra_Map.h"
+#include "Epetra_IntVector.h"
+#include "Epetra_SerialDenseVector.h"
+#include "Epetra_Vector.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_VbrMatrix.h"
+#include "Epetra_LinearProblem.h"
+#include "Epetra_Time.h"
+#include "AztecOO.h"
+
+// includes required by ML
+#include "ml_epetra_preconditioner.h"
+
+#include "Trilinos_Util_CrsMatrixGallery.h"
+
+using namespace Teuchos;
+using namespace Trilinos_Util;
+
+int main(int argc, char *argv[])
+{
+  
+#ifdef EPETRA_MPI
+  MPI_Init(&argc,&argv);
+  Epetra_MpiComm Comm(MPI_COMM_WORLD);
+#else
+  Epetra_SerialComm Comm;
+#endif
+
+  Epetra_Time Time(Comm);
+
+  // Create the linear problem using the class `Trilinos_Util::CrsMatrixGallery.'
+  // Various matrix examples are supported; please refer to the
+  // Trilinos tutorial for more details.
+  
+  CrsMatrixGallery Gallery("recirc_2d", Comm);
+  Gallery.Set("problem_size", 10000);
+  
+  // retrive pointers for linear system matrix and linear problem
+  Epetra_RowMatrix * A = Gallery.GetMatrix();
+  Epetra_LinearProblem * Problem = Gallery.GetLinearProblem();
+
+  Epetra_MultiVector * RHS = Gallery.GetRHS();
+
+  // Construct a solver object for this problem
+  AztecOO solver(*Problem);
+
+  // =========================== begin of ML part ===========================
+  
+  // create a parameter list for ML options
+  ParameterList MLList;
+
+  // set defaults for classic smoothed aggregation
+  ML_Epetra::SetDefaults("SA",MLList);
+
+  // enable adaptation (also known as GGB)
+  MLList.set("filtering: enable", true);
+  MLList.set("filtering: use symmetric cycle", false);
+  MLList.set("filtering: eigensolver", "Anasazi");
+  MLList.set("filtering: eigenvalues to compute", 10);
+  MLList.set("filtering: type", "enhanced");
+  MLList.set("filtering: local aggregates", 1);
+
+  // require to tune eigen-solution (here with Anasazi)
+  MLList.set("eigen-analysis: tolerance", 1e-2);
+  MLList.set("eigen-analysis: block-size", 1);
+  MLList.set("eigen-analysis: length", 20);
+  
+  // create the preconditioner object and compute hierarchy
+  ML_Epetra::MultiLevelPreconditioner * MLPrec = new ML_Epetra::MultiLevelPreconditioner(*A, MLList, true);
+
+  // tell AztecOO to use this preconditioner, then solve
+  solver.SetPrecOperator(MLPrec);
+
+  // =========================== end of ML part =============================
+  
+  solver.SetAztecOption(AZ_solver, AZ_gmres);
+  solver.SetAztecOption(AZ_output, 32);
+
+  // solve with 500 iterations and 1e-12 tolerance  
+  solver.Iterate(1550, 1e-5);
+
+  delete MLPrec;
+  
+  // compute the real residual
+
+  double residual, diff;
+  Gallery.ComputeResidual(residual);
+  Gallery.ComputeDiffBetweenStartingAndExactSolutions(diff);
+  
+  if( Comm.MyPID()==0 ) {
+    cout << "||b-Ax||_2 = " << residual << endl;
+    cout << "||x_exact - x||_2 = " << diff << endl;
+    cout << "Total Time = " << Time.ElapsedTime() << endl;
+  }
+
+#ifdef EPETRA_MPI
+  MPI_Finalize() ;
+#endif
+
+  return 0 ;
+  
+}
+
+#else
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  puts("Please configure ML with --enable-epetra --enable-teuchos --enable-triutils");
+  
+  return 0;
+}
+
+#endif /* #if defined(ML_WITH_EPETRA) && defined(HAVE_ML_TEUCHOS) && defined(HAVE_ML_TRIUTILS) */
