2663fc92649c258c9635c60df5b9b7d55fe53c44
Raymond Tuminaro <rstumin@sandia.gov>
2004-07-07 14:25:19 +0000

    fixed memory leak in single precision conversion

--- packages/ml/src/Operator/ml_op_utils.c
+++ packages/ml/src/Operator/ml_op_utils.c
@@ -1093,6 +1093,8 @@ int ML_Operator_ChangeToSinglePrecision(ML_Operator *matrix)
 
    ML_Operator_Set_Getrow(matrix,ML_EXTERNAL,matrix->getrow->Nrows,sCSR_getrows);
    matrix->data_destroy   = ML_CSR_MSRdata_Destroy;
+   if (values  != NULL) ML_free(values);
+   if (columns != NULL) ML_free(columns);
 
    return 0;
 }
@@ -1164,6 +1166,8 @@ int ML_Operator_ChangeToChar(ML_Operator *matrix)
 
    ML_Operator_Set_Getrow(matrix,ML_EXTERNAL,matrix->getrow->Nrows,cCSR_getrows);
    matrix->data_destroy   = ML_CSR_MSRdata_Destroy;
+   if (values  != NULL) ML_free(values);
+   if (columns != NULL) ML_free(columns);
 
    return 0;
 }
