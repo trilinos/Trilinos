1987dbc991d45ee98d8c0d36546c70baab346c88
Marzio Sala <msala>
2004-09-03 19:53:49 +0000

    Amesos_Container is a simple class, that is meant to host all data for local linear systems. This class allocates internally the matrix (LAPACK or EpetraCrs), solution and rhs. The user can specify the ApplyInverse() method.
    The idea of this class is the following:
    1.- first, a local graph is decomposed using Amesos_Partitioner
    2.- submatrices corresponding to each subgraph are extracted, and stored
        in an Amesos_Container (one for each subgraph)
    3.- If using LAPACK, the class factorizes the matrix
    4.- If using EpetraCrs, the user can get a pointer to the matrix,
        build what he/she belives is a good "inverse" for this matrix,
        then pass the pointer back to the class.
    5.- block Jacobi preconditioner can take advantage of this class: they
        need to set the rhs, call the ApplyInverse() of the Amesos_Container,
        then get the compute solution.
    
    To use Amesos to compute the action of the inverse in ApplyInverse(), class
    Amesos_Preconditioner is provided.

---
+++ packages/amesos/src/Amesos_Container.h
@@ -0,0 +1,49 @@
+#ifndef AMESOS_CONTAINER_H
+#define AMESOS_CONTAINER_H
+
+class Epetra_BlockMap;
+
+class Amesos_Container {
+
+public:
+
+  virtual ~Amesos_Container() {};
+
+  virtual int Shape(const int NumRows, const int NumVectors = 1) = 0;
+
+  virtual int Shape(const Epetra_BlockMap& Map, const int NumVectors = 1) = 0;
+
+  virtual int Reshape(const int NumRows, const int NumVectors = 1) = 0;
+
+  virtual int Reshape(const Epetra_BlockMap& Map, const int NumVectors = 1) = 0;
+
+  virtual int NumRows() const = 0;
+
+  virtual int NumVectors() const = 0;
+
+  virtual double& LHS(const int i, const int Vector = 0) = 0;
+  
+  virtual double& RHS(const int i, const int Vector = 0) = 0;
+
+  virtual int SetMatrixElement(const int row, const int col,
+			       const double value) = 0;
+
+  virtual int GetMatrixPointer(void** Matrix) = 0;
+
+  virtual int SetInversePointer(void* InvMatrix) = 0;
+
+  virtual int Compute() = 0;
+
+  virtual bool IsProblemShaped() const = 0;
+
+  virtual bool IsProblemComputed() const = 0;
+
+  virtual int Apply() = 0;
+
+  virtual int ApplyInverse() = 0;
+
+  virtual int Destroy() = 0;
+
+};
+
+#endif // AMESOS_CONTAINER_H
