c43e1e99767e67dec74565d0dce82174bc22d5ef
William F. Spotz <wfspotz@sandia.gov>
2004-10-05 19:45:55 +0000

    Upgraded testSerialDense to use unittest; rolled into testEpetra

--- packages/PyTrilinos/test/testEpetra.py
+++ packages/PyTrilinos/test/testEpetra.py
@@ -416,14 +416,144 @@ class EpetraVector3DTestCase(unittest.TestCase):
                 for k in range(nk):
                     self.assertEqual(epetraVector[i,j,k], self.numPyArray[i,k,j])
 
+class EpetraSerialDenseTestCase(unittest.TestCase):
+    "TestCase class for Epetra SerialDense Vectors, Matrices and Solvers"
+
+    def setUp(self):
+        self.size = 4
+        self.rows = 2
+        self.cols = 4
+
+    def testDefaultVectorConstructor(self):
+        "Test the SerialDenseVector default constructor"
+        sdv = Epetra.SerialDenseVector()
+        self.assertEqual(sdv.CV(), Epetra.Copy)
+        self.assertEqual(sdv.Length(), 0)
+
+    def testVectorSizeResize(self):
+        "Test the SerialDenseVector Size and Resize methods"
+        sdv = Epetra.SerialDenseVector()
+        sdv.Size(3*self.size)
+        self.assertEqual(sdv.Length(), 3*self.size)
+        sdv.Resize(self.size)
+        self.assertEqual(sdv.Length(), self.size)
+
+    def testVectorSizedConstructor(self):
+        "Test the SerialDenseVector sized constructor"
+        sdv = Epetra.SerialDenseVector(self.size)
+        self.assertEqual(sdv.CV(), Epetra.Copy)
+        self.assertEqual(sdv.Length(), self.size)
+
+    def testVectorCopyConstructor(self):
+        "Test the SerialDenseVector copy constructor"
+        sdv1 = Epetra.SerialDenseVector(self.size)
+        sdv2 = Epetra.SerialDenseVector(sdv1)
+        self.assertEqual(sdv1.Length(), sdv2.Length())
+
+    def testVectorIndexErrors(self):
+        "Test the SerialDenseVector index errors "
+        sdv = Epetra.SerialDenseVector(self.size)
+        self.assertRaises(TypeError, sdv.__getitem__, 0,1)
+        self.assertRaises(TypeError, sdv.__setitem__, 0,1,3.14)
+
+    def testMatrixDefaultConstructor(self):
+        "Test the SerialDenseMatrix default constructor"
+        sdm = Epetra.SerialDenseMatrix()
+        self.assertEqual(sdm.CV(), Epetra.Copy)
+        self.assertEqual(sdm.M(), 0)
+        self.assertEqual(sdm.N(), 0)
+
+    def testMatrixShapeReshape(self):
+        "Test the SerialDenseMatrix Shape and Reshape methods"
+        sdm = Epetra.SerialDenseMatrix()
+        sdm.Shape(self.rows,self.cols)
+        self.assertEqual(sdm.M(), self.rows)
+        self.assertEqual(sdm.N(), self.cols)
+        sdm.Reshape(2*self.rows,2*self.cols)
+        self.assertEqual(sdm.M(), 2*self.rows)
+        self.assertEqual(sdm.N(), 2*self.cols)
+
+    def testMatrixSizedConstructor(self):
+        "Test the SerialDenseMatrix sized constructor"
+        sdm = Epetra.SerialDenseMatrix(self.rows,self.cols)
+        self.assertEqual(sdm.M(), self.rows)
+        self.assertEqual(sdm.N(), self.cols)
+
+    def testMatrixCopyConstructor(self):
+        "Test the SerialDenseMatrix copy constructor"
+        sdm1 = Epetra.SerialDenseMatrix(self.rows,self.cols)
+        sdm2 = Epetra.SerialDenseMatrix(sdm1)
+        self.assertEqual(sdm1.M(), sdm2.M())
+        self.assertEqual(sdm1.N(), sdm2.N())
+
+    def testMatrixNormOneNormInf(self):
+        "Test the SerialDenseMatrix NormOne and NormInf methods"
+        scalar = 2.0
+        size   = self.size
+        sdm    = Epetra.SerialDenseMatrix(size,size)
+        for i in range(size):
+            for j in range(size):
+                sdm[i,j] = scalar
+        self.assertEqual(sdm.NormOne(), scalar*size)
+        self.assertEqual(sdm.NormInf(), scalar*size)
+
+    def testMatrixIndexErrors(self):
+        "Test the SerialDenseMatrix index errors "
+        sdm = Epetra.SerialDenseMatrix(self.size,self.size)
+        self.assertRaises(TypeError, sdm.__getitem__, 0,1,2)
+        self.assertRaises(TypeError, sdm.__setitem__, 0,1,2,3.14)
+
+    def testSolver(self):
+        "Test the SerialDenseSolver"
+        size = self.size
+        sdm  = Epetra.SerialDenseMatrix(size,size)
+        for i in range(size):
+            if (i>0): sdm[i,i-1] = 1
+            sdm[i,i] = -2
+            if (i<size-1): sdm[i,i+1] = 1
+        inv  = Epetra.SerialDenseMatrix(sdm)
+        sys  = Epetra.SerialDenseSolver()
+        sys.SetMatrix(inv)
+        sys.Invert()
+        idty = Epetra.SerialDenseMatrix(size,size)
+        idty.Multiply("N","N",1,sdm,inv,0)
+        for i in range(size):
+            for j in range(size):
+                if i==j: self.assertAlmostEqual(idty[i,j],1.0,10)
+                else:    self.assertAlmostEqual(idty[i,j],0.0,10)
+
+##     def test(self):
+##         "Test the SerialDenseMatrix "
+
+##     def test(self):
+##         "Test the SerialDenseMatrix "
+
+##     def test(self):
+##         "Test the SerialDenseMatrix "
+
+##     def test(self):
+##         "Test the SerialDenseMatrix "
+
+##     def test(self):
+##         "Test the SerialDenseMatrix "
+
+##     def test(self):
+##         "Test the SerialDenseMatrix "
 
 if __name__ == "__main__":
+
+    # Create the test suite object
     suite = unittest.TestSuite()
-    suite.addTest(unittest.makeSuite(EpetraObjectTestCase    ))
-    suite.addTest(unittest.makeSuite(EpetraSerialCommTestCase))
-    suite.addTest(unittest.makeSuite(EpetraBlockMapTestCase  ))
-    suite.addTest(unittest.makeSuite(EpetraMapTestCase       ))
-    suite.addTest(unittest.makeSuite(EpetraVector1DTestCase  ))
-    suite.addTest(unittest.makeSuite(EpetraVector2DTestCase  ))
-    suite.addTest(unittest.makeSuite(EpetraVector3DTestCase  ))
+
+    # Add the test cases to the test suite
+    suite.addTest(unittest.makeSuite(EpetraObjectTestCase     ))
+    suite.addTest(unittest.makeSuite(EpetraSerialCommTestCase ))
+    suite.addTest(unittest.makeSuite(EpetraBlockMapTestCase   ))
+    suite.addTest(unittest.makeSuite(EpetraMapTestCase        ))
+    suite.addTest(unittest.makeSuite(EpetraVector1DTestCase   ))
+    suite.addTest(unittest.makeSuite(EpetraVector2DTestCase   ))
+    suite.addTest(unittest.makeSuite(EpetraVector3DTestCase   ))
+    suite.addTest(unittest.makeSuite(EpetraSerialDenseTestCase))
+
+    # Run the test suite
     unittest.TextTestRunner(verbosity=2).run(suite)
