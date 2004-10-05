f90c62c9c7c2694442f62d87f189f719f8578160
Erik G. Boman <egboman@sandia.gov>
2004-10-05 17:00:51 +0000

    Moved hash function to separate file, zz_hash.c.
    I have included an updated version of the Zoltan hash function that
    fixes some minor flaws Bob pointed out months ago, but these changes
    have been #ifedef'ed out for now because otherwise many of our answers
    would change and test_zoltan would fail. If we have to change many
    answers anyways, we should consider switching to a more sophisticated
    hash function, like MD5.
    
    This checkin saves the revised hash function for the future but does
    not change anything right now.

--- packages/zoltan/src/zz/zz_util.c
+++ packages/zoltan/src/zz/zz_util.c
@@ -33,63 +33,6 @@ extern "C" {
 /*****************************************************************************/
 /*****************************************************************************/
 
-/* Zoltan_Hash is a hash function for Zoltan ids (local or global). 
- *
- * Input:
- *   key: a key to hash of type ZOLTAN_ID_PTR
- *   num_id_entries: the number of (ZOLTAN_ID_TYPE-sized) entries of the key to use
- *   n: the range of the hash function is 0..n-1
- *
- * Return value:
- *   the hash value, an unsigned integer between 0 and n-1
- *
- * Algorithm: 
- *   This hash function is based on Don Knuth's golden ratio
- *   multiplicative method. Bitwise xor is used for keys
- *   longer than an int. The method works well for keys
- *   of size one or two ints, which is typically the case.
- *
- *   This hash function should be replaced with a stronger method
- *   if good hashing of a large number of keys is important.
- *
- * Author: 
- *   Erik Boman, eboman@cs.sandia.gov (SNL 9226)
- */
-
-
-unsigned int Zoltan_Hash(ZOLTAN_ID_PTR key, int num_id_entries, unsigned int n)
-{
-  unsigned int h, rest, *p, bytes, num_bytes;
-  char *byteptr;
-
-  num_bytes = (unsigned int) num_id_entries * sizeof(ZOLTAN_ID_TYPE);
-
-  /* First hash the int-sized portions of the key */
-  h = 0;
-  for (p = (unsigned int *)key, bytes=num_bytes;
-       bytes >= (unsigned int) sizeof(int); 
-       bytes-=sizeof(int), p++){
-    h = (h*2654435761U) ^ (*p);
-  }
-
-  /* Then take care of the remaining bytes, if any */
-  rest = 0;
-  for (byteptr = (char *)p; bytes > 0; bytes--, byteptr++){
-    rest = (rest<<8) | (*byteptr);
-  }
-
-  /* Merge the two parts */
-  if (rest)
-    h = (h*2654435761U) ^ rest;
-
-  /* Return h mod n */
-  return (h%n);
-}
-
-/*****************************************************************************/
-/*****************************************************************************/
-/*****************************************************************************/
-
 /* Remove leading & trailing white space and convert to upper case. */
 
 int Zoltan_Clean_String(
