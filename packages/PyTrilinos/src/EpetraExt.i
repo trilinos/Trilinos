809c010e2eb92d87a4cf48e600bf688aab9c5a61
William F. Spotz <wfspotz@sandia.gov>
2004-10-07 16:44:33 +0000

    Added testEpetraExt unit test

--- packages/PyTrilinos/src/EpetraExt.i
+++ packages/PyTrilinos/src/EpetraExt.i
@@ -29,11 +29,10 @@
 %ignore Epetra_MapColoring::operator[](int);
 %ignore Epetra_MapColoring::operator[](int) const;
 
-// SWIG library includes
+// C++ STL support
 %include "std_vector.i"
 
 // Epetra interface import
-//%import "RawEpetra.i"
 %import "Epetra_Object.h"
 %import "Epetra_SrcDistObject.h"
 %import "Epetra_DistObject.h"
@@ -41,14 +40,14 @@
 %import "Epetra_IntVector.h"
 %import "Epetra_MapColoring.h"
 
-// Epetra interface includes
+// EpetraExt interface includes
 %include "EpetraExt_Transform.h"
-%template (Transform_CrsGraph_MapColoring) EpetraExt::Transform<
-  Epetra_CrsGraph, Epetra_MapColoring>;
-%template (Transform_CrsGraph_MapColoringIndex) EpetraExt::Transform<
-  Epetra_CrsGraph, std::vector<Epetra_IntVector> >;
-%template (StructuralTransform_CrsGraph_MapColoring) EpetraExt::StructuralTransform<
-  Epetra_CrsGraph, Epetra_MapColoring>;
+%template (Transform_CrsGraph_MapColoring)
+  EpetraExt::Transform<Epetra_CrsGraph, Epetra_MapColoring>;
+%template (Transform_CrsGraph_MapColoringIndex)
+  EpetraExt::Transform<Epetra_CrsGraph, std::vector<Epetra_IntVector> >;
+%template (StructuralTransform_CrsGraph_MapColoring)
+  EpetraExt::StructuralTransform<Epetra_CrsGraph, Epetra_MapColoring>;
 %template (StructuralTransform_CrsGraph_MapColoringIndex)
   EpetraExt::StructuralTransform<Epetra_CrsGraph, std::vector<Epetra_IntVector> >;
 
--- packages/PyTrilinos/test/testEpetraExt.py
+++ packages/PyTrilinos/test/testEpetraExt.py
@@ -1,5 +1,60 @@
 #! /usr/bin/env python
 
-import setpath
-from PyTrilinos import EpetraExt
+# System imports
+from   Numeric  import *
+import unittest
 
+# Local imports
+import setpath
+from   PyTrilinos import Epetra
+from   PyTrilinos import EpetraExt
+
+
+class EpetraExtTestCase(unittest.TestCase):
+    "TestCase class for EpetraExt objects"
+
+    def setUp(self):
+        self.size = 11
+        self.comm = Epetra.SerialComm()
+        self.map  = Epetra.Map(self.size,0,self.comm)
+        self.crsg = Epetra.CrsGraph(Epetra.Copy, self.map, 3)
+        self.crsg.InsertGlobalIndices(0,2,array([0,1]))
+        for i in range(1,self.size-1):
+            self.crsg.InsertGlobalIndices(i,3,array([i-1,i,i+1]))
+        self.crsg.InsertGlobalIndices(self.size-1,2,array([self.size-2,self.size-1]))
+        self.crsg.FillComplete()
+        colors = array([3,2,1])
+        self.colors = resize(colors,(self.size,))
+        self.numEl  = [0,0,0,0]
+        for i in self.colors:
+            self.numEl[i] += 1
+
+    def testMapColoring(self):
+        "Test the EpetraExt CrsGraph-to-MapColoring transform"
+        mapColoring  = EpetraExt.CrsGraph_MapColoring(False)
+        colorMap     = mapColoring(self.crsg)
+        numColors    = colorMap.NumColors()
+        defaultColor = colorMap.DefaultColor()
+        self.assertEqual(numColors   , max(self.colors))
+        self.assertEqual(defaultColor, 0               )
+        for c in range(numColors):
+            self.assertEqual(colorMap.NumElementsWithColor(c+1), self.numEl[c+1])
+
+    def testColorMapIndex(self):
+        "Test the EpetraExt MapColoring-to-ColorMapIndex transform"
+        mapColoring   = EpetraExt.CrsGraph_MapColoring(False)
+        colorMap      = mapColoring(self.crsg)
+        colorMapIndex = EpetraExt.CrsGraph_MapColoringIndex(colorMap)
+        columns       = colorMapIndex(self.crsg)
+        # No assert test here; just executing has to be enough
+
+if __name__ == "__main__":
+
+    # Create the test suite object
+    suite = unittest.TestSuite()
+
+    # Add the tets cases to the test suite
+    suite.addTest(unittest.makeSuite(EpetraExtTestCase))
+
+    # Run the test suite
+    unittest.TextTestRunner(verbosity=2).run(suite)
