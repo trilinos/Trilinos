1ee2eaf811046a4b6168cff7e3efebc7616f69ab
Erik G. Boman <egboman@sandia.gov>
2004-11-11 16:24:34 +0000

    Added one more edge scaling option in phg: the clique model used by Robert in RHM (which may not work as well as Umit's absorption scaling, but I like to have both available). Current options are:  0 - no scaling  1 - scale by 1/(size[i]-1),  "absorption"  2 - scale by 2/(size[i]*(size[i]-1)), "clique model"

--- packages/zoltan/src/phg/phg_scale.c
+++ packages/zoltan/src/phg/phg_scale.c
@@ -35,10 +35,12 @@ extern "C" {
 int Zoltan_PHG_Scale_Weights (ZZ *zz, HGraph *hg, float *new_ewgt, 
                              PHGPartParams *hgp)
 {
-int    i;
+int    i, err;
 int    *lsize = NULL;  /* local edge sizes */
 int    *size = NULL;   /* edge sizes */
 static char *yo = "Zoltan_PHG_Scale_Weights";
+
+  err = ZOLTAN_OK; 
 
   /* allocate size arrays */
   if (!(size  = (int *) ZOLTAN_MALLOC (sizeof(int) * hg->nEdge)) ||
@@ -56,7 +58,11 @@ static char *yo = "Zoltan_PHG_Scale_Weights";
     break;
 
   case 1:
-    /* absorption or clique scaling; scale by #pins -1 */
+    /* absorption scaling; scale by 1/(size -1) */
+    /* intentionally fall through into next case! */
+  case 2:
+    /* clique scaling; scale by 2/(size*(size-1)) */
+
     /* first compute size of all hyperedges */
     for (i = 0; i < hg->nEdge; i++) {
       lsize[i] = hg->hindex[i+1] - hg->hindex[i];
@@ -69,20 +75,30 @@ static char *yo = "Zoltan_PHG_Scale_Weights";
 
     /* scale edge weights */
     for (i = 0; i < hg->nEdge; i++) {
-      /* EBEB printf("[%1d] Debug: Hyperedge %d has size %d\n", 
+      /* printf("[%1d] Debug: Hyperedge %d has size %d\n", 
          zz->Proc, i, size[i]); */
-      if (size[i]>1)
-        new_ewgt[i] = (hg->ewgt ? hg->ewgt[i] : 1.0) / (size[i]-1.0);
-      else
+      if (size[i]>1) {
+        if (hgp->ews==1)
+          new_ewgt[i] = (hg->ewgt ? hg->ewgt[i] : 1.0) / (size[i]-1.0);
+        else if (hgp->ews==2)
+          new_ewgt[i] = (hg->ewgt ? hg->ewgt[i] : 1.0) * 2.0 / 
+                        (size[i]*(size[i]-1.0));
+      }
+      else /* size[i] == 1 */
         new_ewgt[i] = 0.0;
     }
+    break;
+
+  default:
+    /* invalid scaling option */
+    err = ZOLTAN_FATAL;
     break;
   }
 
   ZOLTAN_FREE(&size);
   ZOLTAN_FREE(&lsize);
 
-  return ZOLTAN_OK;
+  return err;
 }
 
 #ifdef __cplusplus
