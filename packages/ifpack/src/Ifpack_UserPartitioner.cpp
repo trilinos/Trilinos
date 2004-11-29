1eccdaabc6ff13ae7e813b8cbda0969f814ae6d5
Marzio Sala <msala>
2004-11-29 15:28:26 +0000

    Added a user-defined partition to the list of available partitioning options. This feature is used in ML w/ IPFACK smoothers, to create block relaxation methods whose blocks coincide with the aggregates.

---
+++ packages/ifpack/src/Ifpack_UserPartitioner.cpp
@@ -0,0 +1,54 @@
+#include "Ifpack_ConfigDefs.h"
+#ifdef HAVE_IFPACK_TEUCHOS
+#include "Ifpack_Partitioner.h"
+#include "Ifpack_OverlappingPartitioner.h"
+#include "Ifpack_UserPartitioner.h"
+#include <vector>
+
+//==============================================================================
+int Ifpack_UserPartitioner::ComputePartitions()
+{
+  
+  if (Map_ == 0)
+    IFPACK_CHK_ERR(-1);
+
+  // simply copy user's vector
+  for (int i = 0 ; i < NumMyRows() ; ++i) {
+    Partition_[i] = Map_[i];
+  }
+
+  // put together all partitions composed by 1 one vertex
+  // (if any)
+  vector<int> singletons(NumLocalParts());
+  for (unsigned int i = 0 ; i < singletons.size() ; ++i) {
+    singletons[i] = 0;
+  }
+
+#if 0
+  // may want to uncomment the following to ensure that no
+  // partitions are in fact singletons
+  for (int i = 0 ; i < NumMyRows() ; ++i) {
+    ++singletons[Partition_[i]];
+  }
+  
+  int count = 0;
+  for (unsigned int i = 0 ; i < singletons.size() ; ++i) {
+    if (singletons[i] == 1)
+      ++count;
+  }
+
+  int index = -1;
+  for (int i = 0 ; i < NumMyRows() ; ++i) {
+    int j = Partition_[i];
+    if (singletons[j] == 1) {
+      if (index == -1)
+        index = j;
+      else
+        Partition_[i] = index;
+    }
+  }
+#endif
+
+  return(0);
+}
+#endif // HAVE_IFPACK_TEUCHOS
---
+++ packages/ifpack/src/Ifpack_UserPartitioner.h
@@ -0,0 +1,48 @@
+#ifndef IFPACK_USERPARTITIONER_H
+#define IFPACK_USERPARTITIONER_H
+
+#include "Ifpack_ConfigDefs.h"
+#ifdef HAVE_IFPACK_TEUCHOS
+#include "Ifpack_Partitioner.h"
+#include "Ifpack_OverlappingPartitioner.h"
+#include "Teuchos_ParameterList.hpp"
+class Epetra_Comm;
+class Ifpack_Graph;
+class Epetra_Map;
+class Epetra_BlockMap;
+class Epetra_Import;
+
+//! Ifpack_UserPartitioner: A class to define linear partitions.
+
+class Ifpack_UserPartitioner : public Ifpack_OverlappingPartitioner {
+
+public:
+
+  //! Constructor.
+  Ifpack_UserPartitioner(const Ifpack_Graph* Graph) :
+    Ifpack_OverlappingPartitioner(Graph),
+    Map_(0)
+  {}
+
+  //! Destructor.
+  virtual ~Ifpack_UserPartitioner() {};
+
+  //! Sets all the parameters for the partitioner (none for linear partioning).
+  int SetPartitionParameters(Teuchos::ParameterList& List)
+  {
+    Map_ = List.get("partitioner: map",Map_);
+    if (Map_ == 0)
+      IFPACK_CHK_ERR(-1);
+
+    return(0);
+  }
+
+  //! Computes the partitions. Returns 0 if successful.
+  int ComputePartitions();
+
+private:
+  int* Map_;
+};
+
+#endif // HAVE_IFPACK_TEUCHOS
+#endif // IFPACK_USERPARTITIONER_H
