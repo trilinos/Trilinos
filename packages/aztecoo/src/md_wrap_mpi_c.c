724a115cba5249e18406331ff39aefc7a99e7a3e
Andrew G. Salinger <agsalin@sandia.gov>
2004-12-21 23:44:24 +0000

    Made changes to md_wrap_mpi_c.c  so that the routines (e.g. md_send() ) are not hardwired to use MPI_COMM_WORLD. While I am reticent to change such low level code, I don't imagine these changes will break the code for anyone else. Here's what I did:
    (1) I created a new variable, global to the file:
    MPI_Comm Comm = MPI_COMM_WORLD;
    
    (2)In paralell_info(), which is called by AZ_set_proc_config with
    last argument   "MPI_Comm comm",  I added a line:
      Comm = comm;
    
    (3) I replaced MPI_COMM_WORLD with Comm everywhere in the file.
    
    Now the communication routines use the same communicator as
    set in proc_config. These changes allowed me to run 3 replicas
    of MPSalsa, each running on 2 procs, under a single
    "mpirun -np 6 .." call (not my final goal).
    
    The only use case that I think I broke, is if someone calls
    parallel_info() (via AZ_set_proc_config) using a communicator other
    then MPI_COMM_WORLD, but then still expects the md_read() routines
    to still communicate with MPI_COMM_WORLD.

--- packages/aztecoo/src/md_wrap_mpi_c.c
+++ packages/aztecoo/src/md_wrap_mpi_c.c
@@ -57,6 +57,8 @@ int gl_sbuf = 3;
 /******************************************************************************/
 /******************************************************************************/
 int the_proc_name = -1;
+MPI_Comm Comm = MPI_COMM_WORLD;
+
 
 void get_parallel_info(int *proc, int *nprocs, int *dim)
 
@@ -64,8 +66,8 @@ void get_parallel_info(int *proc, int *nprocs, int *dim)
 
   /* local variables */
 
-  MPI_Comm_size(MPI_COMM_WORLD, nprocs);
-  MPI_Comm_rank(MPI_COMM_WORLD, proc);
+  MPI_Comm_size(Comm, nprocs);
+  MPI_Comm_rank(Comm, proc);
   *dim = 0;
 the_proc_name = *proc;
 
@@ -90,11 +92,11 @@ int md_read(char *buf, int bytes, int *source, int *type, int *flag)
   if (*source == -1) *source = MPI_ANY_SOURCE;
 
   if (bytes == 0) {
-    err = MPI_Recv(&gl_rbuf, 1, MPI_BYTE, *source, *type, MPI_COMM_WORLD,
+    err = MPI_Recv(&gl_rbuf, 1, MPI_BYTE, *source, *type, Comm,
                    &status);
   }
   else {
-    err = MPI_Recv(buf, bytes, MPI_BYTE, *source, *type, MPI_COMM_WORLD,
+    err = MPI_Recv(buf, bytes, MPI_BYTE, *source, *type, Comm,
                    &status);
   }
 
@@ -120,10 +122,10 @@ int md_write(char *buf, int bytes, int dest, int type, int *flag)
   int err;
 
   if (bytes == 0) {
-    err = MPI_Send(&gl_sbuf, 1, MPI_BYTE, dest, type, MPI_COMM_WORLD);
+    err = MPI_Send(&gl_sbuf, 1, MPI_BYTE, dest, type, Comm);
   }
   else {
-    err = MPI_Send(buf, bytes, MPI_BYTE, dest, type, MPI_COMM_WORLD);
+    err = MPI_Send(buf, bytes, MPI_BYTE, dest, type, Comm);
   }
 
   if (err != 0) (void) fprintf(stderr, "MPI_Send error = %d\n", err);
@@ -173,11 +175,11 @@ int md_wrap_iread(void *buf, int bytes, int *source, int *type,
   if (*source == -1) *source = MPI_ANY_SOURCE;
 
   if (bytes == 0) {
-    err = MPI_Irecv(&gl_rbuf, 1, MPI_BYTE, *source, *type, MPI_COMM_WORLD,
+    err = MPI_Irecv(&gl_rbuf, 1, MPI_BYTE, *source, *type, Comm,
                     request);
   }
   else {
-    err = MPI_Irecv(buf, bytes, MPI_BYTE, *source, *type, MPI_COMM_WORLD,
+    err = MPI_Irecv(buf, bytes, MPI_BYTE, *source, *type, Comm,
                     request);
   }
 
@@ -222,10 +224,10 @@ int md_wrap_write(void *buf, int bytes, int dest, int type, int *flag)
   int err = 0;
 
   if (bytes == 0) {
-    err = MPI_Send(&gl_sbuf, 1, MPI_BYTE, dest, type, MPI_COMM_WORLD);
+    err = MPI_Send(&gl_sbuf, 1, MPI_BYTE, dest, type, Comm);
   }
   else {
-    err = MPI_Send(buf, bytes, MPI_BYTE, dest, type, MPI_COMM_WORLD);
+    err = MPI_Send(buf, bytes, MPI_BYTE, dest, type, Comm);
   }
 
   return err;
@@ -323,11 +325,11 @@ int md_wrap_iwrite(void *buf, int bytes, int dest, int type, int *flag,
   int err = 0;
 
   if (bytes == 0) {
-    err = MPI_Isend(&gl_sbuf, 1, MPI_BYTE, dest, type, MPI_COMM_WORLD,
+    err = MPI_Isend(&gl_sbuf, 1, MPI_BYTE, dest, type, Comm,
                   request);
   }
   else {
-    err = MPI_Isend(buf, bytes, MPI_BYTE, dest, type, MPI_COMM_WORLD,
+    err = MPI_Isend(buf, bytes, MPI_BYTE, dest, type, Comm,
                   request);
   }
 
@@ -349,6 +351,8 @@ void parallel_info(int *proc,int *nprocs,int *dim, MPI_Comm comm)
   MPI_Comm_rank(comm, proc);
   *dim = 0;
 the_proc_name = *proc;
+
+  Comm = comm;
 
 } /* get_parallel_info */
 /******************************************************************************/
