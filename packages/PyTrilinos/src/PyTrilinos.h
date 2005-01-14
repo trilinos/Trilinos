4ee58b8eadbf8767e2d8f73c715821924a527aa1
Eric T. Phipps <etphipp@sandia.gov>
2005-01-14 00:24:24 +0000

    Added what is essentially a dummy header file (for now at least).  Its only purpose right now is to allow the NOX configure script to locate the PyTrilinos source directory using its TRILINOS_HEADER_CHECK, and allows me to remove the hard-coded relative path between NOX and PyTrilinos in the NOX/LOCA Python Makefile's.

---
+++ packages/PyTrilinos/src/PyTrilinos.h
@@ -0,0 +1,8 @@
+#ifndef PYTRILINOS_H
+#define PYTRILINOS_H
+
+// This is the top-level PyTrilinos header file.  Currently it only exists
+// so the NOX configure script can determine where the PyTrilinos source
+// directory is
+
+#endif
