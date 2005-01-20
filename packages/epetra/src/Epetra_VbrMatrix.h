41b658cf584d23457c413b8e81f3b7cf8c90cb2d
Michael N. Phenow <mnphenow>
2005-01-20 22:13:09 +0000

    fixed detailed description comments so they get included correctly in Doxygen documentation

--- packages/epetra/src/Epetra_VbrMatrix.h
+++ packages/epetra/src/Epetra_VbrMatrix.h
@@ -838,13 +838,13 @@ class Epetra_VbrMatrix : public Epetra_DistObject,
   //@{ \name Atribute access functions
 
     //! Returns the infinity norm of the global matrix.
-    /* Returns the quantity \f$ \| A \|_\infty\f$ such that
+    /*! Returns the quantity \f$ \| A \|_\infty\f$ such that
        \f[\| A \|_\infty = \max_{1\lei\lem} \sum_{j=1}^n |a_{ij}| \f].
     */ 
     double NormInf() const;
 
     //! Returns the one norm of the global matrix.
-    /* Returns the quantity \f$ \| A \|_1\f$ such that
+    /*! Returns the quantity \f$ \| A \|_1\f$ such that
        \f[\| A \|_1 = \max_{1\lej\len} \sum_{i=1}^m |a_{ij}| \f].
     */ 
     double NormOne() const;
