71ddd8214ab53ae2ac8d119119bb7c6102291fdd
Marzio Sala <msala>
2005-01-24 22:07:03 +0000

    Added another filter that modify the diagonal of a matrix, using absolute and relative threshold. This is only for completeness, as incomplete factorizations already know how to deal with thresholds.

---
+++ packages/ifpack/src/Ifpack_DiagonalFilter.cpp
@@ -0,0 +1,68 @@
+#include "Ifpack_ConfigDefs.h"
+#include "Ifpack_DiagonalFilter.h"
+#include "Epetra_ConfigDefs.h"
+#include "Epetra_RowMatrix.h"
+#include "Epetra_Comm.h"
+#include "Epetra_Map.h"
+#include "Epetra_MultiVector.h"
+#include "Epetra_Vector.h"
+
+//==============================================================================
+int Ifpack_DiagonalFilter::
+ExtractMyRowCopy(int MyRow, int Length, int& NumEntries, 
+		 double* Values, int* Indices) const
+{
+
+  IFPACK_CHK_ERR(A_.ExtractMyRowCopy(MyRow, Length, NumEntries,
+				     Values,Indices));
+
+  // modify the diagonal value
+  for (int i = 0 ; i < NumEntries ; ++i) {
+    if (Indices[i] == MyRow) {
+      Values[i] = Values[i] * RelativeThreshold_ +
+        AbsoluteThreshold_ * EPETRA_SGN(Values[i]);
+    }
+  }
+
+  return(0);
+}
+
+//==============================================================================
+int Ifpack_DiagonalFilter::
+Multiply(bool TransA, const Epetra_MultiVector& X, 
+	 Epetra_MultiVector& Y) const
+{
+
+  int NumVectors = X.NumVectors();
+  assert (NumVectors = Y.NumVectors());
+
+  Y.PutScalar(0.0);
+
+  vector<int> Indices(MaxNumEntries());
+  vector<double> Values(MaxNumEntries());
+
+  for (int i = 0 ; i < NumMyRows() ; ++i) {
+
+    int Nnz;
+    ExtractMyRowCopy(i,MaxNumEntries(),Nnz,
+		     &Values[0], &Indices[0]);
+    if (!TransA) {
+      // no transpose first
+      for (int j = 0 ; j < NumVectors ; ++j) {
+	for (int k = 0 ; k < Nnz ; ++k) {
+	  Y[j][i] += Values[k] * X[j][Indices[k]];
+	}
+      }
+    }
+    else {
+      // transpose here
+      for (int j = 0 ; j < NumVectors ; ++j) {
+	for (int k = 0 ; k < Nnz ; ++k) {
+	  Y[j][Indices[k]] += Values[k] * X[j][i];
+	}
+      }
+    }
+  }
+
+  return(0);
+}
---
+++ packages/ifpack/src/Ifpack_DiagonalFilter.h
@@ -0,0 +1,244 @@
+#ifndef IFPACK_DIAGONALFILTER_H
+#define IFPACK_DIAGONALFILTER_H
+
+#include "Ifpack_ConfigDefs.h"
+#include "Epetra_RowMatrix.h"
+class Epetra_Comm;
+class Epetra_Map;
+class Epetra_MultiVector;
+class Epetra_Import;
+class Epetra_BlockMap;
+
+//! Ifpack_DiagonalFilter: Filter to modify the diagonal entries of a given Epetra_RowMatrix.
+/*!
+
+Ifpack_DiagonalFilter enables the dropping of all elements whose
+absolute value is below a specified threshold.
+
+A typical use is as follows:
+\code
+Epetra_RowMatrix* A;
+// drop all elements below this value
+double AbsoluteThreshold = 1e-3;
+double RelativeThreshold = 1.01;
+
+Ifpack_DiagonalFilter B(A, AbsoluteThreshold, RelativeThreshold);
+\endcode
+
+\author Marzio Sala, SNL 9214.
+
+\data Last modified on 24-Jan-05.
+
+*/
+class Ifpack_DiagonalFilter : public virtual Epetra_RowMatrix {
+
+public:
+  //! Constructor.
+  Ifpack_DiagonalFilter(Epetra_RowMatrix* Matrix,
+                        double AbsoluteThreshold,
+                        double RelativeThreshold) :
+    A_(*Matrix),
+    AbsoluteThreshold_(AbsoluteThreshold),
+    RelativeThreshold_(RelativeThreshold)
+  {}
+
+  //! Destructor.
+  virtual ~Ifpack_DiagonalFilter() {};
+
+  //! Returns the number of entries in MyRow.
+  virtual int NumMyRowEntries(int MyRow, int& NumEntries) const
+  {
+    return(A_.NumMyRowEntries(MyRow, NumEntries));
+  }
+
+  //! Returns the maximum number of entries.
+  virtual int MaxNumEntries() const
+  {
+    return(A_.MaxNumEntries());
+  }
+
+  virtual int ExtractMyRowCopy(int MyRow, int Length, int& NumEntries, 
+                               double* Values, int* Indices) const;
+
+  virtual int ExtractDiagonalCopy(Epetra_Vector & Diagonal) const
+  {
+    IFPACK_RETURN(A_.ExtractDiagonalCopy(Diagonal));
+  }
+
+  virtual int Multiply(bool TransA, const Epetra_MultiVector& X, 
+		       Epetra_MultiVector& Y) const;
+
+  virtual int Solve(bool Upper, bool Trans, bool UnitDiagonal, 
+		    const Epetra_MultiVector& X,
+		    Epetra_MultiVector& Y) const
+  {
+    IFPACK_CHK_ERR(-1);
+  }
+
+  virtual int Apply(const Epetra_MultiVector& X,
+		    Epetra_MultiVector& Y) const
+  {
+    IFPACK_RETURN(Multiply(UseTranspose(),X,Y));
+  }
+
+  virtual int ApplyInverse(const Epetra_MultiVector& X,
+			   Epetra_MultiVector& Y) const
+  {
+    IFPACK_CHK_ERR(-1);
+  }
+
+  virtual int InvRowSums(Epetra_Vector& x) const
+  {
+    IFPACK_CHK_ERR(-1);
+  }
+
+  virtual int LeftScale(const Epetra_Vector& x)
+  {
+    return(A_.LeftScale(x));
+  }
+
+  virtual int InvColSums(Epetra_Vector& x) const
+  {
+    IFPACK_CHK_ERR(-1);;
+  }
+
+  virtual int RightScale(const Epetra_Vector& x) 
+  {
+    return(A_.RightScale(x));
+  }
+
+  virtual bool Filled() const
+  {
+    return(A_.Filled());
+  }
+
+  //! Not implemented for efficiency reasons.
+  virtual double NormInf() const
+  {
+    return(-1.0);
+  }
+
+  //! Not implemented for efficiency reasons.
+  virtual double NormOne() const
+  {
+    return(-1.0);
+  }
+
+  virtual int NumGlobalNonzeros() const
+  {
+    return(A_.NumGlobalNonzeros());
+  }
+
+  virtual int NumGlobalRows() const
+  {
+    return(A_.NumGlobalRows());
+  }
+
+  virtual int NumGlobalCols() const
+  {
+    return(A_.NumGlobalCols());
+  }
+
+  virtual int NumGlobalDiagonals() const
+  {
+    return(A_.NumGlobalDiagonals());
+  }
+
+  virtual int NumMyNonzeros() const
+  {
+    return(A_.NumMyNonzeros());
+  }
+
+  virtual int NumMyRows() const
+  {
+    return(A_.NumMyRows());
+  }
+
+  virtual int NumMyCols() const
+  {
+    return(A_.NumMyCols());
+  }
+
+  virtual int NumMyDiagonals() const
+  {
+    return(A_.NumMyDiagonals());
+  }
+
+  virtual bool LowerTriangular() const
+  {
+    return(A_.LowerTriangular());
+  }
+
+  virtual bool UpperTriangular() const
+  {
+    return(A_.UpperTriangular());
+  }
+
+  virtual const Epetra_Map& RowMatrixRowMap() const
+  {
+    return(A_.RowMatrixRowMap());
+  }
+
+  virtual const Epetra_Map& RowMatrixColMap() const
+  {
+    return(A_.RowMatrixColMap());
+  }
+
+  virtual const Epetra_Import* RowMatrixImporter() const
+  {
+    return(A_.RowMatrixImporter());
+  }
+
+  int SetUseTranspose(bool UseTranspose)
+  {
+    return(A_.SetUseTranspose(UseTranspose));
+  }
+
+  bool UseTranspose() const 
+  {
+    return(A_.UseTranspose());
+  }
+
+  //! Not implemented for efficiency reasons.
+  bool HasNormInf() const
+  {
+    return(false);
+  }
+
+  const Epetra_Comm& Comm() const
+  {
+    return(A_.Comm());
+  }
+
+  const Epetra_Map& OperatorDomainMap() const 
+  {
+    return(A_.OperatorDomainMap());
+  }
+
+  const Epetra_Map& OperatorRangeMap() const 
+  {
+    return(A_.OperatorRangeMap());
+  }
+
+  const Epetra_BlockMap& Map() const 
+  {
+    return(A_.Map());
+  }
+
+  const char* Label() const{
+    return(A_.Label());
+  }
+
+private:
+
+  //! Pointer to the matrix to be filtered
+  Epetra_RowMatrix& A_;
+  //! This value (times the sgn(A(i,i)) is added to the diagonal elements
+  double AbsoluteThreshold_;
+  //! Multiplies A(i,i) by this value.
+  double RelativeThreshold_;
+
+};
+
+
+#endif /* IFPACK_DIAGONALFILTER_H */
