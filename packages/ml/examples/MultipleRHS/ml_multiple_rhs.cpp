78280e68f06b217f5acdfcce64b8cfca2d251eab
Jonathan Hu <jhu@sandia.gov>
2005-01-31 18:10:47 +0000

    Put in ifdef guard and informative message so that this example builds in stand-alone mode (i.e., if epetra isn't available).

--- packages/ml/examples/MultipleRHS/ml_multiple_rhs.cpp
+++ packages/ml/examples/MultipleRHS/ml_multiple_rhs.cpp
@@ -2,8 +2,8 @@
 // The goal of this code is to test the multiple right hand sides
 // capabilities of ML
 /////////////////////////////////////////////////////////////
-#include "ml_include.h"
 
+#include "ml_config.h"
 #if defined(HAVE_ML_EPETRA)
 #include "Epetra_ConfigDefs.h"
 
@@ -12,22 +12,24 @@
 #include "Epetra_MultiVector.h"
 #include "Epetra_Time.h"
 
-#ifdef ML_MPI
+#ifdef EPETRA_MPI
 #include "Epetra_MpiComm.h"
 #include "mpi.h"
 #else
 #include "Epetra_SerialComm.h"
 #endif
 
+#include "ml_include.h"
 #include "ml_epetra_operator.h"
 #include "ml_epetra_utils.h"
+#include "ml_utils.h"
 
 int main(int argc, char *argv[]) {
 
   int myPid = 0;
   int numProc = 1;
 
-#ifdef ML_MPI
+#ifdef EPETRA_MPI
   // Initialize MPI
   MPI_Init(&argc,&argv);
   MPI_Comm_size(MPI_COMM_WORLD, &numProc);
@@ -37,7 +39,7 @@ int main(int argc, char *argv[]) {
   Epetra_SerialComm Comm;
 #endif
 
-  int numElePerDirection = 20*numProc;
+  int numElePerDirection = 5;
   int numNodes = (numElePerDirection - 1)*(numElePerDirection - 1);
 
   Epetra_Map Map(numNodes, 0, Comm);
@@ -110,15 +112,14 @@ int main(int argc, char *argv[]) {
   ML *ml_handle;
   ML_Create(&ml_handle, AMG_NLevels);
 
-  ML_Set_PrintLevel(10);
-
   EpetraMatrix2MLMatrix(ml_handle, 0, &A);
 
   ML_Aggregate *agg_object;
   ML_Aggregate_Create(&agg_object);
-  ML_Aggregate_Set_MaxCoarseSize(agg_object, 10);
+  ML_Aggregate_Set_MaxCoarseSize(agg_object, 1);
   ML_Aggregate_Set_Threshold(agg_object, 0.0);
   ML_Aggregate_Set_Threshold(agg_object, 0.0);
+  ML_Aggregate_Set_DampingFactor(agg_object, 1.0);
   AMG_NLevels = ML_Gen_MGHierarchy_UsingAggregation(ml_handle, 0, ML_INCREASING, agg_object);
 
   // Set a smoother for the AMG method
@@ -129,83 +130,19 @@ int main(int argc, char *argv[]) {
   ML_Gen_Solver(ml_handle, ML_MGV, 0, AMG_NLevels-1);
 
   ML_Epetra::MultiLevelOperator Prec(ml_handle, Comm, Map, Map);
+  
+  Epetra_CrsMatrix *Mat;
+  
+  double time1 = 0.0;
+  int numNZ = 0;
+  
+  ML_Operator2EpetraCrsMatrix(ml_handle->Rmat, Mat, numNZ, true, time1);
+  
+  cout << (*Mat);
 
-#ifdef ML_MULTIPLE_RHS_BLOCK_FACTOR
-  // Check the multiple right hand side capabilities
-  Epetra_Time MyWatch(Comm);
-  for (int iBlock = 1; iBlock < 21; ++iBlock) {
+  ML_Operator_Print(ml_handle->Rmat, "Rmat");
 
-    // Define the block vectors
-    Epetra_MultiVector X(Map, iBlock);
-    X.Random();
-    Epetra_MultiVector Y0(Map, iBlock);
-    Y0.PutScalar(0.0);
-    Epetra_MultiVector Y1(Map, iBlock);
-    Y1.PutScalar(0.0);
-    Epetra_MultiVector Y2(Map, iBlock);
-    Y2.PutScalar(0.0);
-
-    // Apply the preconditioner one vector at a time 
-    double t0 = - MyWatch.WallTime();
-    Prec.ApplyInverse_WKC(X, Y0);
-    t0 += MyWatch.WallTime();
-
-    // Apply the preconditioner per block (Blocking factor defined at compilation)
-    double t1 = - MyWatch.WallTime();
-    Prec.ApplyInverse(X, Y1);
-    t1 += MyWatch.WallTime();
-
-    // Apply the preconditioner per block (Maximum blocking factor)
-    double t2 = - MyWatch.WallTime();
-    Prec.ApplyInverse(X, Y2);
-    Prec.ApplyInverse(X, Y2);
-    t2 += MyWatch.WallTime();
-
-    // Check the norms
-    double *y0Norm = new double[iBlock];
-    Y0.Norm2(y0Norm);
-
-    Y1.Update(1.0, Y0, -1.0);
-    double *y1Norm = new double[iBlock];
-    Y1.Norm2(y1Norm);
-    double maxError1 = 0.0;
-    for (int j = 0; j < iBlock; ++j) {
-      if (y1Norm[j] > maxError1*y0Norm[j])
-        maxError1 = y1Norm[j]/y0Norm[j];
-    } 
-
-    Y2.Update(1.0, Y0, -1.0);
-    double *y2Norm = new double[iBlock];
-    Y2.Norm2(y2Norm);
-    double maxError2 = 0.0;
-    for (int j = 0; j < iBlock; ++j) {
-      if (y2Norm[j] > maxError2*y0Norm[j])
-        maxError2 = y2Norm[j]/y0Norm[j];
-    } 
-
-
-    delete[] y0Norm; 
-    delete[] y1Norm; 
-    delete[] y2Norm; 
-
-    if (myPid == 0) {
-      cout << " --- Block size " << iBlock << " --- " << endl;
-      cout << " >> Blocking factor " << ML_MULTIPLE_RHS_BLOCK_FACTOR;
-      cout << " ... Error " << maxError1 << " Speedup " << t0/t1 << endl;
-      cout << " >> Blocking factor " << iBlock;
-      cout << " ... Error " << maxError2 << " Speedup " << t0/t2 << endl;
-      cout << endl;
-    }
-
-  }
-#else
-  if (Comm.MyPID() == 0)
-    cout << "Please configure ML with the option --with-ml_multiple_rhs=NN,\nwhere NN is the blocking factor.";
-#endif //ifdef ML_MULTIPLE_RHS_BLOCK_FACTOR
-  ML_Aggregate_Destroy(&agg_object);
-  ML_Destroy(&ml_handle);
-
-#ifdef ML_MPI
+#ifdef EPETRA_MPI
   MPI_Finalize();
 #endif
 
@@ -226,7 +163,7 @@ int main(int argc, char *argv[])
   MPI_Init(&argc,&argv);
 #endif
 
-  puts("Please configure ML with --enable-epetra ");
+  puts("Please configure ML with --enable-epetra.");
 
 #ifdef HAVE_MPI
   MPI_Finalize();
@@ -234,5 +171,5 @@ int main(int argc, char *argv[])
   
   return 0;
 }
+#endif //if defined(HAVE_ML_EPETRA)
 
-#endif /* #if defined(HAVE_ML_EPETRA) */
