9408e9ceff57cebc2890ea1b93c6964b685f03c4
Eric T. Phipps <etphipp@sandia.gov>
2005-02-03 01:19:01 +0000

    Modified the Chan python test to check some statistics of the solution using the unittest python module.

--- packages/nox/test/lapack/LOCA_python/testLOCA.py
+++ packages/nox/test/lapack/LOCA_python/testLOCA.py
@@ -1,16 +1,23 @@
 #! /usr/bin/env python
 
-print "Starting lapack/LOCA_python/testLOCA.exe"
-
 import os
 import sys
 
 # set paths
 import setpath
 
+# Get vebosity from the command line
+if (len(sys.argv) > 2):
+    print "usage:", sys.argv[0], "[-v]"
+    sys.exit(1)
+if (len(sys.argv) == 2 and sys.argv[1] == "-v"):
+    verbose = True
+else:
+   verbose = False
+   
 # run test
 import testLOCA_Chan
-result = testLOCA_Chan.main()
+result = testLOCA_Chan.runTests(verbose)
 
 if (result == 0):
     print "Test was successful!"
--- packages/nox/test/lapack/LOCA_python/testLOCA_Chan.py
+++ packages/nox/test/lapack/LOCA_python/testLOCA_Chan.py
@@ -1,11 +1,43 @@
 #! /usr/bin/env python
 
 import setpath
+import unittest
 
 import NOX
 import LOCA
 
-def main():
+def runTests(verbose):
+
+    # Our TestCase for the Chan Problem
+    # It is declared here so it can access the symbols below
+    class ChanTestCase(unittest.TestCase):
+        "TestCase for the Chan Problem"
+        
+        def testContinuation(self):
+            "Test the continuation run finishes correctly"
+            
+            self.assertEqual(status, LOCA.Iterator.Finished)
+
+        def testNumSteps(self):
+            "Test the number of continuation steps"
+            numSteps = stepper.getStepNumber()
+            self.assertEqual(numSteps, 32)
+
+        def testNumFailedSteps(self):
+            "Test the number of failed continuation steps"
+            numFailedSteps = stepper.getNumFailedSteps()
+            self.assertEqual(numFailedSteps, 0)
+
+        def testFinalValue(self):
+            "Test the final value of the continuation parameter"
+            p = grp.getParams()
+            alpha_final = p.getValue("alpha")
+            self.assertAlmostEqual(alpha_final, 5.0, 10)
+
+        def testNorm(self):
+            "Test the norm of the final solution"
+            norm_x = finalSolution.norm()
+            self.assertAlmostEqual(norm_x, 203.1991024, 6)
 
     # Set up constants
     n = 100
@@ -18,20 +50,20 @@ def main():
 
     # Create problem interface
     chan = LOCA.Chan.ProblemInterface(n, alpha, beta, scale)
-
+    
     # Set up parameter vector
     p = LOCA.ParameterVector()
     p.addParameter("alpha",alpha)
     p.addParameter("beta",beta)
     p.addParameter("scale",scale)
-
+    
     # Create group
     grp = LOCA.LAPACK.Group(chan)
     grp.setParams(p)
-
+    
     # Set up parameter lists
     paramList = NOX.Parameter.List()
-
+    
     locaParamsList = paramList.sublist("LOCA")
     
     # Create the stepper sublist and set the stepper parameters
@@ -52,7 +84,7 @@ def main():
     stepperList.setParameter("Min Tangent Factor", -1.0)
     stepperList.setParameter("Tangent Factor Exponent",1.0)
     stepperList.setParameter("Compute Eigenvalues",False)
-
+    
     # Create bifurcation sublist
     bifurcationList = locaParamsList.sublist("Bifurcation")
     bifurcationList.setParameter("Method", "None")
@@ -62,7 +94,7 @@ def main():
     predictorList.setParameter("Method", "Tangent")
     firstStepPredictorList = predictorList.sublist("First Step Predictor");
     firstStepPredictorList.setParameter("Method", "Constant")
-
+    
     # Create step size sublist
     stepSizeList = locaParamsList.sublist("Step Size")
     stepSizeList.setParameter("Method", "Adaptive")
@@ -72,37 +104,73 @@ def main():
     stepSizeList.setParameter("Aggressiveness", 0.5)
     stepSizeList.setParameter("Failed Step Reduction Factor", 0.5)
     stepSizeList.setParameter("Successful Step Increase Factor", 1.26)
-
+    
     # Set the LOCA Utilities
     locaUtilsList = locaParamsList.sublist("Utilities")
-    locaUtilsList.setParameter("Output Information",0)
-
+    if (verbose):
+        locaUtilsList.setParameter("Output Information",
+                                   LOCA.Utils.Error + 
+                                   LOCA.Utils.Warning +
+                                   LOCA.Utils.StepperIteration +
+                                   LOCA.Utils.StepperDetails +
+                                   LOCA.Utils.Solver +
+                                   LOCA.Utils.Parameters +
+                                   LOCA.Utils.SolverDetails)
+    else:
+        locaUtilsList.setParameter("Output Information",LOCA.Utils.Error)
+    
     # Create the "Solver" parameters sublist to be used with NOX Solvers
     nlParams = paramList.sublist("NOX");
     nlParams.setParameter("Nonlinear Solver", "Line Search Based")
-
+    
     nlPrintParams = nlParams.sublist("Printing");
-    nlPrintParams.setParameter("Output Information",0)
-
+    if (verbose):
+        nlPrintParams.setParameter("Output Information",
+                                   NOX.Utils.Error +
+                                   NOX.Utils.Details +
+                                   NOX.Utils.OuterIteration + 
+                                   NOX.Utils.InnerIteration + 
+                                   NOX.Utils.Warning +
+                                   NOX.Utils.TestDetails)
+    else:
+         nlPrintParams.setParameter("Output Information", NOX.Utils.Error)
+    
     # Set up the status tests
     normF = NOX.StatusTest.NormF(1.0e-8)
     maxIters = NOX.StatusTest.MaxIters(maxNewtonIters)
     comboOR = NOX.StatusTest.Combo(NOX.StatusTest.Combo.OR, 
                                    normF, 
                                    maxIters)
-
+    
     # Create the stepper
     stepper = LOCA.Stepper(grp, comboOR, paramList)
-    
-    # Perform continuation run
     status = stepper.run()
-    
-    if (status != LOCA.Iterator.Finished):
-        return 1
+    grp = stepper.getSolutionGroup()
+    finalSolution = grp.getX()
+
+    # Create the test suite object
+    suite = unittest.TestSuite()
+
+    # Add the test cases to the test suite
+    suite.addTest(unittest.makeSuite(ChanTestCase))
+
+    # Run the test suite
+    if (verbose):
+        print "\n***** Running Tests *****\n"
+        result = unittest.TextTestRunner(verbosity=2).run(suite)
     else:
+        result = unittest.TextTestRunner(verbosity=0).run(suite)
+        
+    if result.wasSuccessful():
         return 0
+    else:
+        return 1
 
 if (__name__ == "__main__"):
-    main()
+
+    # Run the tests
+    runTests()
+    
+    
     
 
