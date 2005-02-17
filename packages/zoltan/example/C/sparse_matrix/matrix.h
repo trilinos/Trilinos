9efa140b1334bd6e3a23387bc13a87abfba15439
Erik G. Boman <egboman@sandia.gov>
2005-02-17 23:34:35 +0000

    Added sparse matrix example designed for Marzio (ML) to the examples directory. Right now it's in a separate subdirectory, perhaps it's better to merge it with Lee Ann's examples.

---
+++ packages/zoltan/example/C/sparse_matrix/matrix.h
@@ -0,0 +1,15 @@
+/* Simple sparse matrix data structure. */
+
+typedef struct {
+    int i;     /* row index */
+    int j;     /* column index */
+    double val;  /* value */
+} Matrix_entry;
+
+typedef struct {
+   int num_globalrows;  /* global # rows */
+   int num_globalcols;  /* global # cols */
+   int num_mynz;        /* local #nonzeros in matrix */
+   Matrix_entry * entries; /* user must allocate space for the entries */
+} Matrix;
+
