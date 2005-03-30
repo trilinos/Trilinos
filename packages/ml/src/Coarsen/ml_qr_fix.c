f9297d70df015f507dd4422b695e51cb62a81dfc
mbrezi <mbrezi>
2005-03-30 22:24:10 +0000

    Functions to manage the information about any dead dofs associated with coarse nodes. This info is set during the aggregation and needs to be retained until we fix the coarse-level matrix.

--- packages/ml/src/Coarsen/ml_qr_fix.c
+++ packages/ml/src/Coarsen/ml_qr_fix.c
@@ -1,36 +1,87 @@
 #include "ml_include.h"
 #include "ml_qr_fix.h"
+#include <stdio.h>
 
-int ML_qr_fix_Create(ML_qr_fix** ptr)
+/* -mb: make this global var for now - decide where to put in ML */
+static ML_qr_fix *xCDeadNodDof = NULL;
+
+int ML_qr_fix_Create(const int nCoarseNod)
 {
-  *ptr = (ML_qr_fix*) ML_allocate(sizeof(ML_qr_fix));
-  (*ptr)->level = 0;
+   int    i, nBytes;
+   xCDeadNodDof = (ML_qr_fix*) ML_allocate(sizeof(ML_qr_fix));
+   xCDeadNodDof->level         = 0;
+   xCDeadNodDof->numDeadNodDof = 0;
+   xCDeadNodDof->nDeadNodDof   = nCoarseNod;
+   nBytes = (nCoarseNod+1)*sizeof(ML_QR_FIX_TYPE *);
+   xCDeadNodDof->xDeadNodDof   = (ML_QR_FIX_TYPE *)ML_allocate(nBytes);
+   
+   for (i=0; i < nCoarseNod; i++) (xCDeadNodDof->xDeadNodDof)[i] = 0;
 
-  return(0);
+   return(0);
 }
 
-int ML_qr_fix_Destroy(ML_qr_fix** ptr)
+int ML_qr_fix_Destroy(void)
 {
-  ML_qr_fix* fix = *ptr;
-  if (fix == NULL)
+  if (xCDeadNodDof == NULL)
     return(0);
 
   /* loop over all fields and release them */
+  if (xCDeadNodDof->xDeadNodDof) ML_free(xCDeadNodDof->xDeadNodDof);
 
   /* free the structure itself */
 
-  ML_free(*ptr);
-  *ptr = NULL;
+  ML_free(xCDeadNodDof);
+  xCDeadNodDof = NULL;
 
   return(0);
 }
 
 int ML_qr_fix_Print(ML_qr_fix* ptr)
 {
-  if (ptr == NULL)
-    return(-1);
+    int i, cnt;
 
-  printf("level = %d\n", ptr->level);
+    if (ptr == NULL)
+        return(-1);
 
-  return(0);
+    printf("level = %d nodes containing dead dofs:\n", ptr->level);
+    cnt = 0;
+    for (i=0; i < ptr->nDeadNodDof; i++) {
+        if ((ptr->xDeadNodDof)[i]) {
+            printf("No. %8d node index %8d\n", ++cnt, i);
+        }    
+    }
+
+    return(0);
+}
+
+int ML_qr_fix_NumDeadNodDof(void) { 
+    if (xCDeadNodDof == NULL) return 0;
+    return xCDeadNodDof->numDeadNodDof; 
+}
+
+ML_QR_FIX_TYPE ML_qr_fix_getDeadNod(const int nodInx)
+{
+     if (xCDeadNodDof == NULL) return 0;
+     return (xCDeadNodDof->xDeadNodDof)[nodInx];
+}
+
+void ML_qr_fix_setNumDeadNod(int num)
+{
+     if (xCDeadNodDof == NULL) return;
+     xCDeadNodDof->numDeadNodDof = num;
+}
+
+void ML_qr_fix_setDeadNod( 
+         const int      nodInx, 
+         ML_QR_FIX_TYPE val
+     )
+{
+     if (xCDeadNodDof == NULL) return;
+     (xCDeadNodDof->xDeadNodDof)[nodInx] = val;
+}
+
+int  ML_qr_fix_Bitsize(void)
+{
+     if (xCDeadNodDof == NULL) return 0;
+     return (int)(8*sizeof((xCDeadNodDof->xDeadNodDof)[0]));
 }
