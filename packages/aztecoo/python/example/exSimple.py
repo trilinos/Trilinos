38497c747095b51a7ea39d7c4d614905f90e05e9
Marzio Sala <msala>
2005-04-27 15:24:31 +0000

    Added a simple example that shows how to use the python interface.

---
+++ packages/aztecoo/python/example/exSimple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+from PyTrilinos import Triutils, AztecOO, Epetra
+nx = 100;
+ny = 100;
+Comm = Epetra.SerialComm();
+Gallery = Triutils.CrsMatrixGallery("laplace_2d", Comm)
+Gallery.Set("nx", nx);
+Gallery.Set("ny", ny);
+Matrix = Gallery.GetMatrix();
+LHS = Gallery.GetStartingSolution();
+RHS = Gallery.GetRHS();
+
+# Solve the linear problem
+if 0:
+  Problem = Epetra.LinearProblem(Matrix, LHS, RHS);
+  Solver = AztecOO.AztecOO(Problem);
+else:
+  Solver = AztecOO.AztecOO(Matrix, LHS, RHS);
+
+Solver.SetAztecOption(AztecOO.AZ_solver, AztecOO.AZ_cg);
+Solver.SetAztecOption(AztecOO.AZ_precond, AztecOO.AZ_dom_decomp);
+Solver.SetAztecOption(AztecOO.AZ_subdomain_solve, AztecOO.AZ_icc);
+Solver.SetAztecOption(AztecOO.AZ_output, 16);
+Solver.Iterate(1550, 1e-5)
