0ae6736ed01c811f2cb4d60df691918ede1db93a
Kendall S. Stanley <ksstanl>
2005-05-01 16:36:14 +0000

    Increasing MPI_COMM_MAX and MPI_GROUP_MAX to allow the tests to run on Atlantis

--- packages/amesos/test/scripts/daily/mpi/TestAmesos.sh
+++ packages/amesos/test/scripts/daily/mpi/TestAmesos.sh
@@ -110,8 +110,8 @@ echo `uname -a` >>& $file
 #
 # FIXME: Test_MultipleSolves is not passed on all machines!
 #  Set MPI_GROUP_MAX to allow this test to pass desipte bug #1210
-setenv MPI_COMM_MAX 1024
-setenv MPI_GROUP_MAX 1024
+setenv MPI_COMM_MAX 4096
+setenv MPI_GROUP_MAX 4096
 foreach f ( Test_Epetra_RowMatrix Test_Epetra_CrsMatrix Test_Detailed Test_UMFPACK Test_LAPACK Test_KLU Test_SuperLU Test_SuperLU_DIST Test_MUMPS Test_DSCPACK TestOptions )
   cd $f
   set exefiles = (*.exe)
