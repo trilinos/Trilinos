ff202e87660c26fb5f0cc6c41c203abe21f68cb8
Eric T. Phipps <etphipp@sandia.gov>
2005-05-05 21:15:31 +0000

    Added a norm constraint to the test problems library.

---
+++ packages/nox/test/lapack/LOCA_TestProblems/NormConstraint.C
@@ -0,0 +1,204 @@
+// $Id$ 
+// $Source$ 
+
+//@HEADER
+// ************************************************************************
+//
+//                  LOCA Continuation Algorithm Package
+//                 Copyright (2005) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Andy Salinger (agsalin@sandia.gov) or Eric Phipps
+// (etphipp@sandia.gov), Sandia National Laboratories.
+//
+// ************************************************************************
+//@HEADER
+
+#include "NormConstraint.H"
+#include "LOCA_Parameter_Vector.H"
+#include "LOCA_Utils.H"
+
+NormConstraint::NormConstraint(int N, const LOCA::ParameterVector& pVec) :
+  n(N),
+  constraints(1,1),
+  isValidConstraints(false),
+  dgdp(1,1),
+  p(pVec),
+  x()
+{
+  constraints.putScalar(0.0);
+  dgdp.putScalar(0.0);
+  dgdp(0,0) = -1.0; // derivative w.r.t. gamma
+  NOX::LAPACK::Vector xx(n);
+  x = Teuchos::rcp(xx.createMultiVector(1));
+}
+
+NormConstraint::NormConstraint(const NormConstraint& source, 
+			       NOX::CopyType type) :
+  n(source.n),
+  constraints(source.constraints),
+  isValidConstraints(false),
+  dgdp(source.dgdp),
+  p(source.p),
+  x(Teuchos::rcp(source.x->clone(type)))
+{
+  if (source.isValidConstraints && type == NOX::DeepCopy)
+    isValidConstraints = true;
+}
+
+NormConstraint::~NormConstraint()
+{
+}
+
+NormConstraint&
+NormConstraint::operator=(const NormConstraint& source)
+{
+  if (this != &source) {
+    n = source.n;
+    constraints = source.constraints;
+    isValidConstraints = source.isValidConstraints;
+    dgdp = source.dgdp;
+    p = source.p;
+    *x = *source.x;
+  }
+
+  return *this;
+}
+
+LOCA::MultiContinuation::ConstraintInterface& 
+NormConstraint::operator=(
+		   const LOCA::MultiContinuation::ConstraintInterface& source)
+{
+  return operator=(dynamic_cast<const NormConstraint&>(source));
+}
+
+Teuchos::RefCountPtr<LOCA::MultiContinuation::ConstraintInterface>
+NormConstraint::clone(NOX::CopyType type) const
+{
+  return Teuchos::rcp(new NormConstraint(*this, type));
+}
+
+int
+NormConstraint::numConstraints() const
+{
+  return 1;
+}
+
+void
+NormConstraint::setX(const NOX::Abstract::Vector& y)
+{
+  (*x)[0] = y;
+  x->scale(1.0/n);
+  isValidConstraints = false;
+}
+
+void
+NormConstraint::setParam(int paramID, double val)
+{
+  p[paramID] = val;
+  isValidConstraints = false;
+}
+
+void
+NormConstraint::setParams(
+			 const vector<int>& paramIDs, 
+			 const NOX::Abstract::MultiVector::DenseMatrix& vals)
+{
+  for (unsigned int i=0; i<paramIDs.size(); i++)
+    p[paramIDs[i]] = vals(i,0);
+  isValidConstraints = false;
+}
+
+NOX::Abstract::Group::ReturnType
+NormConstraint::computeConstraints()
+{
+  constraints(0,0) = 0.5 * n * (*x)[0].dot((*x)[0]) - p.getValue("gamma");
+  isValidConstraints = true;
+
+  return NOX::Abstract::Group::Ok;
+}
+
+NOX::Abstract::Group::ReturnType
+NormConstraint::computeConstraintDerivatives()
+{
+  return NOX::Abstract::Group::Ok;
+}
+
+bool
+NormConstraint::isConstraints() const
+{
+  return isValidConstraints;
+}
+
+bool
+NormConstraint::isConstraintDerivatives() const
+{
+  return true;
+}
+
+const NOX::Abstract::MultiVector::DenseMatrix&
+NormConstraint::getConstraints() const
+{
+  return constraints;
+}
+
+const NOX::Abstract::MultiVector::DenseMatrix*
+NormConstraint::getConstraintDerivativesP() const
+{
+  return &dgdp;
+}
+
+const NOX::Abstract::MultiVector*
+NormConstraint::getConstraintDerivativesX() const
+{
+  return x.get();
+}
+
+bool
+NormConstraint::isConstraintDerivativesXZero() const
+{
+  return false;
+}
+
+bool
+NormConstraint::isConstraintDerivativesPZero() const
+{
+  return false;
+}
+
+NOX::Abstract::Group::ReturnType
+NormConstraint::computeDgDp(const vector<int>& paramIDs, 
+			      NOX::Abstract::MultiVector::DenseMatrix& dgdp, 
+			      bool isValidG)
+{
+  if (!isValidG) {
+    dgdp(0,0) = constraints(0,0);
+  }
+
+  for (unsigned int i=0; i<paramIDs.size(); i++) {
+    if (p.getLabel(paramIDs[i]) == "gamma") {
+      dgdp(0,i+1) = -1.0;
+    }
+    else {
+      dgdp(0,i+1) = 0.0;
+    }
+  }
+
+  return NOX::Abstract::Group::Ok;
+}
