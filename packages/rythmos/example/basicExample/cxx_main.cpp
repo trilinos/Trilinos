2dcdbfcde2ff2a53e4496d333844cc74bed61c43
Todd S. Coffey <tscoffe@sandia.gov>
2005-05-06 15:43:34 +0000

    Added Makefile.export.rythmos support to configure.ac and Makefile.am.  This allows me to link against Thyra and Teuchos without having to specify 8 or 10 different include paths in each Makefile.am.  This required adding a few files to config/ and modifying bootstrap.
      Modified basicExample to not use Thyra vectors at this time.
    
      Note, this doesn't entirely work yet, it seems there's a few extra ../.. in
    the absolute paths for thyra's Makefile.export.thyra file and I think that's
    causing link failures.

--- packages/rythmos/example/basicExample/cxx_main.cpp
+++ packages/rythmos/example/basicExample/cxx_main.cpp
@@ -35,9 +35,14 @@
 
 #include "Epetra_Map.h"
 #include "Epetra_Vector.h"
+
 #include "Rythmos_ConfigDefs.h"
 
 #include "ExampleApplication.hpp"
+
+// Includes for Thyra:
+//#include "Thyra_VectorSpaceBase.hpp"
+//#include "Thyra_VectorBase.hpp"
 
 int main(int argc, char *argv[])
 {
@@ -59,41 +64,38 @@ int main(int argc, char *argv[])
   // Construct a Map with NumElements and index base of 0
   Epetra_Map Map(NumElements, 0, Comm);
 
-  // Create x and b vectors
+  // Construct a Thyra vector space
+//  Thyra::Thyra_VectorSpace vectorspace(NumElements, 0, Comm);
+  // Create two vectors in this vector space
+//  Thyra::Thyra_Vector x = vectorspace.createMember();
+//  Thyra::Thyra_Vector xn = vectorspace.createMember();
+
+  // Create x and xn vectors
   Epetra_Vector x(Map);
-  Epetra_Vector b(Map);
-
-  b.Random();
-  x.Update(2.0, b, 0.0); // x = 2*b
-
-  double bnorm, xnorm;
-  x.Norm2(&xnorm);
-  b.Norm2(&bnorm);
-
-  if (MyPID == 0) 
-    cout << "2 norm of x = " << xnorm << endl
-         << "2 norm of b = " << bnorm << endl;
+  Epetra_Vector xn(Map);
 
   cout << "Integrating \\dot{x}=\\lambda x from t=0 to t=1" << endl
        << "with initial x_0 = 10, and \\Delta t=0.1" << endl
        << "using forward Euler." << endl;
   double lambda = -0.9;
   ExampleApplication problem(lambda);
-  Epetra_Vector xn(Map);
   double t0 = 0;
   double t1 = 1;
   double dt = 0.1;
   double N = (t1-t0)/dt;
   double x_initial = 10; // initial condition
   xn.PutScalar(x_initial); 
+//  Thyra::assign(xn,x_initial);
   cout << "x(0.0) = " << xn[0] << endl;
   double t = t0;
   for (int i=1 ; i<N+1 ; ++i)
   {
     t = t0+i*dt;
     x.Scale(1.0,xn); // x = xn;
+//    Thyra::assign(x,xn);
     problem.evalResidual(x,t);
     xn.Update(dt,x,1.0); // xn = xn + dt*x
+//    Thyra::Vp_StV(xn,dt,x);
     cout << "x(" << t << ") = " << xn[0] << endl;
   }
   cout << "       " << x_initial*exp(problem.getCoeff()*t) << " = Exact solution" << endl;
