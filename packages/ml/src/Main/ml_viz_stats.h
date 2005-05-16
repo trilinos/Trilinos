46f2444a3e47271d18d5c92b037a9d1ef5f5f1b0
Jonathan Hu <jhu@sandia.gov>
2005-05-16 18:34:15 +0000

    Added two new fields, Ndim and MaxNodesPerAgg, to the definition of ML_Aggregate_Viz_Stats.

--- packages/ml/src/Main/ml_viz_stats.h
+++ packages/ml/src/Main/ml_viz_stats.h
@@ -15,11 +15,13 @@ typedef struct ML_Aggregate_Viz_Stats_Struct
   double *x;
   double *y;
   double *z;
+  int Ndim;
   int *graph_decomposition;
   int Nlocal;
   int Naggregates;
   int local_or_global;
   int is_filled;
+  int MaxNodesPerAgg;
   void *Amatrix;  /* void * so that I do not have to include
 		     ml_operator.h */
   
