005d5d89c2d8648c97b521939b68e92040cd944c
Marzio Sala <msala>
2005-05-25 19:06:44 +0000

    Added a set of utility classes: - NoCopiable prevents the use of copy constructor and operator= - Time contains basic tools to track CPU time usage - Status is a simple container of several variables, used by many   solvers (like verbose_, ComputeTrueResidual_, etc.) - Utils contains some methods used by many solvers (like a simple   procedure to compute the true residual and vector norms).
    Now all concrete Amesos classes are derived from Amesos_BaseSolver and
    all the these classes. This makes the code must shorter and more readable.

---
+++ packages/amesos/src/Amesos_Status.h
@@ -0,0 +1,62 @@
+#ifndef AMESOS_STATUS_H
+#define AMESOS_STATUS_H
+
+/*!
+ \class Amesos_Status
+ 
+ \brief Amesos_Status: Container for some status variables.
+
+ \author Marzio Sala, SNL 9214
+
+ \date Last updated on 24-May-05 (Champions' League Final day)
+*/
+
+class Amesos_Status
+{
+public:
+  //! Default constructor.
+  Amesos_Status()
+  {
+    IsSymbolicFactorizationOK_ = false;
+    IsNumericFactorizationOK_ = false;
+    PrintTiming_ = false;
+    PrintStatus_ = false;
+    AddToDiag_ = 0.0;
+    ComputeVectorNorms_ = false;
+    ComputeTrueResidual_ = false;
+    verbose_ = 0;
+    NumSymbolicFact_ = 0;
+    NumNumericFact_ = 0;
+    NumSolve_ = 0;  
+  }
+
+  //! Default destructor.
+  ~Amesos_Status() {};
+
+  //! If \c true, SymbolicFactorization() has been successfully called.
+  bool IsSymbolicFactorizationOK_;
+  //! If \c true, NumericFactorization() has been successfully called.
+  bool IsNumericFactorizationOK_;
+  //! If \c true, prints timing information in the destructor.
+  bool PrintTiming_;
+  //! If \c true, print additional information in the destructor.
+  bool PrintStatus_;
+  //! Add \c this value to the diagonal.
+  double AddToDiag_;
+  //! If \c true, prints the norms of X and B in Solve().
+  bool ComputeVectorNorms_;
+  //! If \c true, computes the true residual in Solve().
+  bool ComputeTrueResidual_;
+  
+  //! Toggles the output level.
+  int verbose_;
+
+  //! Number of symbolic factorization phases.
+  int NumSymbolicFact_;
+  //! Number of numeric factorization phases.
+  int NumNumericFact_;
+  //! Number of solves.
+  int NumSolve_;  
+};
+
+#endif
