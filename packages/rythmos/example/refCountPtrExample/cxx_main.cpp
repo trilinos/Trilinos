7f483c1ff5599b7486fb96d45757133b4ca0d0f2
Todd S. Coffey <tscoffe@sandia.gov>
2005-06-06 20:06:44 +0000

    Got it working.

--- packages/rythmos/example/refCountPtrExample/cxx_main.cpp
+++ packages/rythmos/example/refCountPtrExample/cxx_main.cpp
@@ -6,60 +6,77 @@
 class Foo
 {
   public:
-    Foo();
-    virtual ~Foo();
+    Foo() {};
+    virtual ~Foo() {};
     virtual double getx() = 0;
 };
 
 class Bar : public virtual Foo
 {
   public:
-    Bar();
-    ~Bar();
+    Bar() {};
+    ~Bar() {};
     double getx() { return(x_); };
     void setx(double x) { x_ = x; };
   protected:
     double x_;
 };
 
+class PrintFoo
+{
+  public:
+    PrintFoo() {};
+    PrintFoo(Teuchos::RefCountPtr<Foo> &F) { F_ = F; };
+    ~PrintFoo() {};
+    void print()
+      { std::cout << "x = " << F_->getx() << "!" << std::endl; };
+  protected:
+    Teuchos::RefCountPtr<Foo> F_;
+};
+
 
 // Print foo's x value when passed by RefCountPtr
-void printFoo(Teuchos::RefCountPtr<Foo> F)
+void printFooDirect(Teuchos::RefCountPtr<Foo> F)
 {
-  cout << "x = " << F->getx() << "!" << endl;
+std::cout << "x = " << F->getx() << "!" << std::endl;
 };
 // Print foo's x value when passed by reference
-void printFoo(Foo &F)
+void printFooDirect(Foo &F)
 {
   std::cout << "x = " << F.getx() << "!" << std::endl;
 };
 
 int main(int argc, char *argv[])
 {
-  std::cout << "Output should be 5.0: " << endl;
+  std::cout << "Output should be 5: " << std::endl;
 
+  /*
   Bar B;
   B.setx(5.0);
-  printFoo(B);
+  printFooDirect(B);
+  */
 
+  
   /*
   Teuchos::RefCountPtr<Bar> B = Teuchos::rcp(new Bar);
   B->setx(5.0);
-  printFoo(B); // fails because printFoo takes RefCountPtr<Foo> not RefCountPtr<Bar>
+  PrintFoo PF(B); // fails because PrintFoo takes RefCountPtr<Foo> not RefCountPtr<Bar>
+  PF.print();
   */
- 
-
-  /*
-  Teuchos::RefCountPtr<Foo> B = Teuchos::rcp(new Bar);
-  B->setx(5.0); // fails because B is RefCountPtr<Foo> not RefCountPtr<Bar>
-  printFoo(B);
-  */
+  
   
   /*
   Teuchos::RefCountPtr<Foo> B = Teuchos::rcp(new Bar);
-  (Teuchos::rcp_dynamic_cast<Bar>(B))->setx(5.0);
-  printFoo(B);
+  B->setx(5.0); // fails because B is RefCountPtr<Foo> not RefCountPtr<Bar>
+  PrintFoo PF(B);
+  PF.print();
   */
+  
+  
+  Teuchos::RefCountPtr<Foo> B = Teuchos::rcp(new Bar);
+  (Teuchos::rcp_dynamic_cast<Bar>(B))->setx(5.0);
+  PrintFoo PF(B);
+  PF.print();
 
   return(0);
 };
