bf7c7dd2e9ec9373706e22b9b1c82e423537175f
Todd S. Coffey <tscoffe@sandia.gov>
2005-06-21 20:50:01 +0000

    Okay, now I've got trouble.  I moved the ForwardEuler class out to its own set of files and I can't get it to compile now.  The error is:
    cxx_main.cpp:56: undefined reference to `Rythmos::ForwardEuler<double>::ForwardEuler(Teuchos::RefCountPtr<Rythmos::ModelEvaluator<double> const> const&)'
    
    Oof.

--- packages/rythmos/example/classExample/cxx_main.cpp
+++ packages/rythmos/example/classExample/cxx_main.cpp
@@ -2,11 +2,11 @@
 // This example is to debug why my code isn't compiling.
 // 1.  Set up basic class structure here all in one file
 //     virtual class Stepper with concrete derived class ForwardEuler
-//     virtual class NonlinearModel with concrete derived class ExampleApplicationRythmosInterface
-//     in main:  create object of type ExampleApplicationRythmosInterface and pass to constructer of ForwardEuler
+//     virtual class NonlinearModel with concrete derived class LinearProblem
+//     in main:  create object of type LinearProblem and pass to constructer of ForwardEuler
 //     Done.  My mistake was not including default constructors & destructors for virtual base classes.
 // 2.  Add in RefCountPtr
-//     Done.  If I pass by const reference, with const object, then the object
+//     Done.  Note:  if I pass by const reference, with const object, then the object
 //     needs to have const versions of its member functions in order to call
 //     them.
 // 3.  Separate into namespaces
@@ -14,79 +14,12 @@
 // 4.  Add in templating
 //     Done.  No errors.
 // 5.  Separate into files
+//     Problems!  
 
-namespace Rythmos {
-template<class Scalar>
-class Stepper
-{
-  public:
-    Stepper() {};
-    virtual ~Stepper() {};
-    virtual Scalar TakeStep(Scalar dt) = 0;
-    virtual Scalar get_solution() = 0;
-};
-} // namespace Rythmos
-
-namespace Rythmos {
-template<class Scalar>
-class ModelEvaluator
-{
-  public:
-    ModelEvaluator() {};
-    virtual ~ModelEvaluator() {};
-    virtual Scalar evalModel(Scalar x, Scalar t) const = 0;
-    virtual Scalar get_vector() const = 0;
-};
-} // namespace Rythmos
+#include "Stepper_ForwardEuler.hpp"
 
 #include "Teuchos_RefCountPtr.hpp"
 #include "Teuchos_ScalarTraits.hpp"
-
-namespace Rythmos {
-template<class Scalar>
-class ForwardEuler : public Stepper<Scalar>
-{
-  public:
-    ForwardEuler();
-    ForwardEuler(const Teuchos::RefCountPtr<const ModelEvaluator<Scalar> > &model);
-    ~ForwardEuler();
-    Scalar TakeStep(Scalar dt);
-    Scalar get_solution();
-  protected:
-    Scalar t_;
-    Scalar x_;
-    Scalar f_;
-    Teuchos::RefCountPtr<const ModelEvaluator<Scalar> > model_;
-};
-template<class Scalar>
-ForwardEuler<Scalar>::ForwardEuler() 
-{
-}
-template<class Scalar>
-ForwardEuler<Scalar>::ForwardEuler(const Teuchos::RefCountPtr<const ModelEvaluator<Scalar> > &model)
-{
-  model_ = model;
-  t_ = Teuchos::ScalarTraits<Scalar>::zero();
-  x_ = model_->get_vector();
-}
-template<class Scalar>
-ForwardEuler<Scalar>::~ForwardEuler() 
-{
-}
-template<class Scalar>
-Scalar ForwardEuler<Scalar>::TakeStep(Scalar dt)
-{
-  f_ = model_->evalModel(x_,t_);
-  x_ = x_ + dt*f_;
-  t_ = t_ + dt;
-  return(dt);
-}
-template<class Scalar>
-Scalar ForwardEuler<Scalar>::get_solution()
-{
-  return(x_);
-}
-} // namespace Rythmos
 
 class LinearProblem : public Rythmos::ModelEvaluator<double>
 {
