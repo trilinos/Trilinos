35d1fc5e712db354d3a3c28b1c99a9e45290cd71
Todd S. Coffey <tscoffe@sandia.gov>
2005-06-21 21:19:51 +0000

    Okay, I _KNEW_ it was something stupid I just didn't know.
      Heidi informs me that when you're playing with templated classes, all the
    member function definitions need to be in the header file and can't be
    located inside a cpp file.

--- packages/rythmos/example/classExample/Stepper_ForwardEuler.hpp
+++ packages/rythmos/example/classExample/Stepper_ForwardEuler.hpp
@@ -7,6 +7,7 @@
 #include "Teuchos_ScalarTraits.hpp"
 
 namespace Rythmos {
+
 template<class Scalar>
 class ForwardEuler : public Stepper<Scalar>
 {
@@ -22,6 +23,38 @@ class ForwardEuler : public Stepper<Scalar>
     Scalar f_;
     ModelEvaluator<Scalar> *model_;
 };
+
+
+template<class Scalar>
+ForwardEuler<Scalar>::ForwardEuler() 
+{
+}
+template<class Scalar>
+ForwardEuler<Scalar>::ForwardEuler(ModelEvaluator<Scalar> *model)
+{
+  model_ = model;
+  t_ = Teuchos::ScalarTraits<Scalar>::zero();
+  x_ = model_->get_vector();
+}
+template<class Scalar>
+ForwardEuler<Scalar>::~ForwardEuler() 
+{
+}
+template<class Scalar>
+Scalar ForwardEuler<Scalar>::TakeStep(Scalar dt)
+{
+  f_ = model_->evalModel(x_,t_);
+  x_ = x_ + dt*f_;
+  t_ = t_ + dt;
+  return(dt);
+}
+template<class Scalar>
+Scalar ForwardEuler<Scalar>::get_solution()
+{
+  return(x_);
+}
+
 } // namespace Rythmos
+
 
 #endif // RYTHMOS_STEPPER_FORWARDEULER
