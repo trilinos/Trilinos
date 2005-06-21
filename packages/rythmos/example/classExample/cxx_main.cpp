58bfb21e5521caacc67c262f39465f94bb82db9e
Todd S. Coffey <tscoffe@sandia.gov>
2005-06-21 20:53:42 +0000

    Okay, I put them back in the file and it now works.  I think this is something to do with the Makefile.

--- packages/rythmos/example/classExample/cxx_main.cpp
+++ packages/rythmos/example/classExample/cxx_main.cpp
@@ -16,7 +16,77 @@
 // 5.  Separate into files
 //     Problems!  
 
-#include "Stepper_ForwardEuler.hpp"
+//#include "Stepper_ForwardEuler.hpp"
+
+//-------------------------------------------------------------------------
+// Stepper_ForwardEuler.hpp
+//-------------------------------------------------------------------------
+//#ifndef RYTHMOS_STEPPER_FORWARDEULER
+//#define RYTHMOS_STEPPER_FORWARDEULER
+
+#include "Stepper.hpp"
+#include "ModelEvaluator.hpp"
+
+#include "Teuchos_RefCountPtr.hpp"
+#include "Teuchos_ScalarTraits.hpp"
+
+namespace Rythmos {
+template<class Scalar>
+class ForwardEuler : public Stepper<Scalar>
+{
+  public:
+    ForwardEuler();
+    ForwardEuler(const Teuchos::RefCountPtr<const ModelEvaluator<Scalar> > &model);
+    ~ForwardEuler();
+    Scalar TakeStep(Scalar dt);
+    Scalar get_solution();
+  protected:
+    Scalar t_;
+    Scalar x_;
+    Scalar f_;
+    Teuchos::RefCountPtr<const ModelEvaluator<Scalar> > model_;
+};
+} // namespace Rythmos
+
+//#endif // RYTHMOS_STEPPER_FORWARDEULER
+//-------------------------------------------------------------------------
+
+
+//-------------------------------------------------------------------------
+// Stepper_ForwardEuler.cpp
+//-------------------------------------------------------------------------
+//#include "Stepper_ForwardEuler.hpp"
+namespace Rythmos {
+template<class Scalar>
+ForwardEuler<Scalar>::ForwardEuler() 
+{
+}
+template<class Scalar>
+ForwardEuler<Scalar>::ForwardEuler(const Teuchos::RefCountPtr<const ModelEvaluator<Scalar> > &model)
+{
+  model_ = model;
+  t_ = Teuchos::ScalarTraits<Scalar>::zero();
+  x_ = model_->get_vector();
+}
+template<class Scalar>
+ForwardEuler<Scalar>::~ForwardEuler() 
+{
+}
+template<class Scalar>
+Scalar ForwardEuler<Scalar>::TakeStep(Scalar dt)
+{
+  f_ = model_->evalModel(x_,t_);
+  x_ = x_ + dt*f_;
+  t_ = t_ + dt;
+  return(dt);
+}
+template<class Scalar>
+Scalar ForwardEuler<Scalar>::get_solution()
+{
+  return(x_);
+}
+} // namespace Rythmos
+//-------------------------------------------------------------------------
 
 #include "Teuchos_RefCountPtr.hpp"
 #include "Teuchos_ScalarTraits.hpp"
