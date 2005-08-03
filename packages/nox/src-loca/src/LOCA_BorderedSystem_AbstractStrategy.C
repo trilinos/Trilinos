4ab761352191cec69b9dca82f73e14679a640aa7
Eric T. Phipps <etphipp@sandia.gov>
2005-08-03 22:59:10 +0000

    Added a setMatrixBlocks method to the Bordered System abstract interface that takes a multivec for the constraint instead of a constraint interface, and added a default implementation that builds a constraint interface from the multivec (LOCA::MultiContinuation::MultiVecConstraint).  This makes it easier to use bordered solvers when the constraint is coming from a multivector.

---
+++ packages/nox/src-loca/src/LOCA_BorderedSystem_AbstractStrategy.C
@@ -0,0 +1,47 @@
+// $Id$
+// $Source$
+
+//@HEADER
+// ************************************************************************
+//
+//                  LOCA Continuation Algorithm Package
+//                 Copyright (2005) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Andy Salinger (agsalin@sandia.gov) or Eric Phipps
+// (etphipp@sandia.gov), Sandia National Laboratories.
+//
+// ************************************************************************
+//@HEADER
+
+#include "LOCA_BorderedSystem_AbstractStrategy.H"
+#include "LOCA_MultiContinuation_MultiVecConstraint.H"
+
+void
+LOCA::BorderedSystem::AbstractStrategy::setMatrixBlocksMultiVecConstraint(
+	  const Teuchos::RefCountPtr<const NOX::Abstract::Group>& group,
+	  const Teuchos::RefCountPtr<const NOX::Abstract::MultiVector>& blockA,
+	  const Teuchos::RefCountPtr<const NOX::Abstract::MultiVector>& blockB,
+	  const Teuchos::RefCountPtr<const NOX::Abstract::MultiVector::DenseMatrix>& blockC)
+{
+  Teuchos::RefCountPtr<LOCA::MultiContinuation::MultiVecConstraint> con = 
+    Teuchos::rcp(new LOCA::MultiContinuation::MultiVecConstraint(blockB));
+
+  setMatrixBlocks(group, blockA, con, blockC);
+}
