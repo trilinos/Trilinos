b65683d5fc554544f6dac6cfe4004c2153ecb51b
William F. Spotz <wfspotz@sandia.gov>
2005-08-31 03:09:24 +0000

    Upgraded python build system to use export Makefiles

--- packages/new_package/python/src/setup.py
+++ packages/new_package/python/src/setup.py
@@ -31,106 +31,92 @@
 # System imports
 from   distutils.core import *
 from   distutils      import sysconfig
-import commands
 import os
-import re
-import string
 import sys
 
-# Any information that needs to be transferred from the autotooled Makefile is
-# written to file setup.txt using python syntax to define a dictionary.  The
-# keys of this 'makeInfo' dictionary are variable names and the corresponding
-# values represent the data that will be needed by this setup.py script.
+# Trilinos import
+TRILINOS_HOME_DIR = os.path.normpath(open("TRILINOS_HOME_DIR").read()[:-1])
+sys.path.insert(0,os.path.join(TRILINOS_HOME_DIR,"commonTools","buildTools"))
+from MakefileVariables import *
+
+# Build the makeVars dictionary by processing relevant Makefiles
+makeVars = { }
+makeVars.update(processFile(os.path.join("..","..","..","epetra"    ,
+                                         "Makefile.export.epetra"    )))
+makeVars.update(processFile(os.path.join("..","..","..","PyTrilinos",
+                                         "Makefile.export.pytrilinos")))
+makeVars.update(processFile(os.path.join("Makefile")))
+
+# Import the variable names and values into the global namespace.  This is
+# crucual: every variable name/value pair obtained by processing the specified
+# Makefiles above will become actual python variables in the global namespace.
+globals().update(makeVars)
+
+# Obtain the package version number string
 try:
-    f = open("setup.txt")
-    makeInfo = f.readlines()
-    f.close()
-    makeInfo = eval(string.join(makeInfo))
-except IOError:
-    makeInfo = { }
+    version = makeVars["PACKAGE_VERSION"]
+except KeyError:
+    version = makeVars.get("VERSION","??")
 
-# Certain directory paths are needed by setup.py.  pakDir is the path for the
-# new_package directory, and srcDir is the path for the python source directory
-pakDir = makeInfo.get("top_srcdir","")
-srcDir = makeInfo.get("srcdir"    ,"")
-CXX    = makeInfo.get("CXX"       ,"")
+# Initialize arguments that will be needed by the Extension class
+include_dirs       = [srcdir]
+library_dirs       = [      ]
+libraries          = [      ]
+extra_compile_args = [      ]
+extra_link_args    = [      ]
 
-# Obtain the version from the package version function, using regular
-# expressions.  This assumes that the function returns a string constant of the
-# form "PackageName Version xxx - mm/dd/yyyy" and extracts the xxx (which does
-# not have to be three characters long).
-versionRE     = re.compile(r"return.*Version\s+(.*)\s+-\s+\d")
-versionHeader = os.path.join(pakDir,"src","New_Package_Version.h")
-try:
-    header = open(versionHeader)
-    lines  = header.readlines()
-    header.close()
-    for line in lines:
-        match = versionRE.search(line)
-        if match:
-            version = match.group(1)
-except IOError:
-    version = "??"
+# New_Package does not support the export Makefile system, so we need to build
+# the NEW_PACKAGE_INCLUDES and NEW_PACKAGE_LIBS variables from scratch
+NEW_PACKAGE_INCLUDES = "-I%s -I%s" % (os.path.normpath(os.path.join(top_srcdir,"src")),
+                                      os.path.normpath(os.path.join(top_builddir,"src")))
+NEW_PACKAGE_LIBS     = "-L%s -lnew_package" % os.path.normpath(os.path.join(top_builddir,"src"))
 
-# Define the epetra include path, library directory and library name
-epetraInc    = os.path.join(pakDir, "..", "epetra", "src")
-epetraLibDir = os.path.join("..", "..", "..", "epetra", "src")
-epetraLib    = "epetra"
+# Get the relevant Makefile export variable values, split them into lists of
+# strings, add them together to obtain a big list of option strings, and then
+# remove any duplicate entries
+options = NEW_PACKAGE_INCLUDES.split() + \
+          NEW_PACKAGE_LIBS.split()     + \
+          EPETRA_INCLUDES.split()      + \
+          EPETRA_LIBS.split()          + \
+          PYTRILINOS_INCLUDES.split()  + \
+          PYTRILINOS_LIBS.split()
+uniquifyList(options)
 
-# Define the new_package include path, library directory and library name
-newPackageInc    = [os.path.join(pakDir, "src"),
-                    os.path.join("..","..","src")]
-newPackageLibDir = os.path.join("..", "..", "src")
-newPackageLib    = "new_package"
-
-# Standard libraries.  This is currently a bit of a hack.  The library "stdc++"
-# is added to the standard library list for a case where we know it sometimes
-# needs it.
-stdLibs = [ ]
-sysName = os.uname()[0]
-if sysName == "Linux":
-    stdLibs.append("stdc++")
-
-# Create the extra compile and link argument lists and complete the standard
-# libraries list.  This is accomplished by looping over the arguments in
-# LDFLAGS, BLAS_LIBS, LAPACK_LIBS, FLIBS and LIBS and adding them to the
-# appropriate list.
-extraCompileArgs = [ ]
-if makeInfo.get("NEWP_SWAHILI",""):
-    extraCompileArgs.append("-DNEWP_SWAHILI")
-extraLinkArgs = [ ]
-libs = makeInfo.get("LDFLAGS"    ,"").split() + \
-       makeInfo.get("BLAS_LIBS"  ,"").split() + \
-       makeInfo.get("LAPACK_LIBS","").split() + \
-       makeInfo.get("FLIBS"      ,"").split() + \
-       makeInfo.get("LIBS"       ,"").split()
-for lib in libs:
-    if lib[:2] == "-l":
-        stdLibs.append(lib[2:])
+# Distribute the individual options to the appropriate Extension class arguments
+for option in options:
+    if option[:2] == "-I":
+        include_dirs.append(option[2:])
+    elif option[:2] == "-L":
+        library_dirs.append(option[2:])
+    elif option[:2] == "-l":
+        libraries.append(option[2:])
     else:
-        extraLinkArgs.append(lib)
+        extra_link_args.append(option)
 
-# Define the strings that refer to the required source files.
+# NEWP_SWAHILI definition
+if NEWP_SWAHILI:
+    extra_compile_args.append("-DNEWP_SWAHILI")
+
+# Define the strings that refer to the required local source files
 newPackageWrap = "New_Package_wrap.cpp"
 
 # Compiler and linker
 sysconfig.get_config_vars()
-config_vars = sysconfig._config_vars;
-config_vars['CC' ] = CXX
-config_vars['CXX'] = CXX
+sysconfig._config_vars["CC" ] = CXX
+sysconfig._config_vars["CXX"] = CXX
 
 # _New_Package extension module
 _New_Package = Extension("PyTrilinos._New_Package",
                          [newPackageWrap],
-                         include_dirs       = newPackageInc + [epetraInc, srcDir],
-                         library_dirs       = [newPackageLibDir, epetraLibDir],
-                         libraries          = [newPackageLib, epetraLib] + stdLibs,
-                         extra_compile_args = extraCompileArgs,
-                         extra_link_args    = extraLinkArgs
+                         define_macros      = [("HAVE_CONFIG_H", "1")],
+                         include_dirs       = include_dirs,
+                         library_dirs       = library_dirs,
+                         libraries          = libraries,
+                         extra_compile_args = extra_compile_args,
+                         extra_link_args    = extra_link_args
                          )
 
-# PyTrilinos.New_Package setup.  This is what tells the distutils module how to
-# create the package.
+# PyTrilinos.New_Package setup
 setup(name         = "PyTrilinos.New_Package",
       version      = version,
       description  = "Python Interface to Trilinos Package New_Package",
