a1d43a1f525a23d7643ea58341afea3f8313f87d
William F. Spotz <wfspotz@sandia.gov>
2005-09-01 16:04:23 +0000

    Now works with --with-gnumake configure option

--- packages/epetra/python/src/setup.py
+++ packages/epetra/python/src/setup.py
@@ -41,10 +41,10 @@ from MakefileVariables import *
 
 # Build the makeVars dictionary by processing relevant Makefiles
 makeVars = { }
-makeVars.update(processFile(os.path.join("..","..","Makefile.export.epetra")))
-makeVars.update(processFile(os.path.join("..","..","..","PyTrilinos",
-                                         "Makefile.export.pytrilinos")))
-makeVars.update(processFile(os.path.join("Makefile")))
+makeVars.update(processMakefile(os.path.join("..","..","Makefile.export.epetra")))
+makeVars.update(processMakefile(os.path.join("..","..","..","PyTrilinos",
+                                             "Makefile.export.pytrilinos")))
+makeVars.update(processMakefile(os.path.join("Makefile")))
 
 # Import the variable names and values into the global namespace.  This is
 # crucual: every variable name/value pair obtained by processing the specified
