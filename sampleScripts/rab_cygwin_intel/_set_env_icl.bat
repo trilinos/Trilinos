441f67d6179c9ab13717183525edff146ff4badc
Roscoe A. Bartlett <rabartl@sandia.gov>
2005-09-29 21:32:42 +0000

    Here I am commiting the scripts that I used to get Trilinos working with the Intel C++ and Fortran compilers for Windows uder cygwin on my Windows XP laptop.  See the README file being checked in here for more details.  For the most part, this should not be too hard to get working on another Windows XP machine once cygwin and the Intel compilers are installed.

---
+++ sampleScripts/rab_cygwin_intel/_set_env_icl.bat
@@ -0,0 +1,6 @@
+REM
+REM This DOS script is to be called from cygwin using 'source set_env_icl'
+REM
+@call "C:\Program Files\Intel\Compiler\C++\9.0\IA32\Bin\ICLVars.bat"
+@call "C:\Program Files\Intel\Compiler\Fortran\9.0\IA32\Bin\IFortVars.bat"
+@call "c:\cygwin\cygwin.bat"
