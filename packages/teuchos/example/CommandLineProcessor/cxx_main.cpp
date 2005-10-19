6513a527cf16f416a72813c198e3a0eaac6dfdde
Roscoe A. Bartlett <rabartl@sandia.gov>
2005-10-19 22:38:20 +0000

    Cleaned up using GlobalMPISession and the catch macros.

--- packages/teuchos/example/CommandLineProcessor/cxx_main.cpp
+++ packages/teuchos/example/CommandLineProcessor/cxx_main.cpp
@@ -1,5 +1,7 @@
 #include "Teuchos_CommandLineProcessor.hpp"
 #include "Teuchos_GlobalMPISession.hpp"
+#include "Teuchos_oblackholestream.hpp"
+#include "Teuchos_StandardCatchMacros.hpp"
 #include "Teuchos_Version.hpp"
 
 // Enum for the speed option
@@ -10,85 +12,97 @@ int main(int argc, char* argv[])
   Teuchos::GlobalMPISession mpiSession(&argc,&argv);
   const int procRank = Teuchos::GlobalMPISession::getRank();
 
-  if ( procRank == 0 )
-    cout << Teuchos::Teuchos_Version() << endl << endl;
+  Teuchos::oblackholestream blackhole;
+  std::ostream &out = ( procRank == 0 ? std::cout : blackhole );
 
-  // Creating an empty command line processor looks like:
-  Teuchos::CommandLineProcessor My_CLP;
-
-  /* To set and option, it must be given a name and default value.  Additionally,
-     each option can be given a help string.  Although it is not necessary, a help
-     string aids a users comprehension of the acceptable command line arguments.
-     Some examples of setting command line options are:
-  */
-  // Set an integer command line option.
-  int NumIters = 1550;
-  My_CLP.setOption("iterations", &NumIters, "Number of iterations");
-  // Set a double-precision command line option.
-  double Tolerance = 1e-10;
-  My_CLP.setOption("tolerance", &Tolerance, "Tolerance");
-  // Set a string command line option.
-  string Solver = "GMRES";
-  My_CLP.setOption("solver", &Solver, "Linear solver");
-  // Set a boolean command line option.    
-  bool Precondition;
-  My_CLP.setOption("precondition","no-precondition",
-		   &Precondition,"Preconditioning flag");
-	// Set an enumeration command line option
-	const int    num_speed_values  = 3;
-	const ESpeed speed_opt_values[] = { SPEED_SLOW, SPEED_MEDIUM, SPEED_FAST };
-	const char*  speed_opt_names[]  = { "slow",     "medium",     "fast"     };
-	ESpeed       Speed = SPEED_MEDIUM;
-	My_CLP.setOption(
-		"speed", &Speed,
-		num_speed_values, speed_opt_values, speed_opt_names,
-		"Speed of our solver"
-		);
-
-  /* There are also two methods that control the behavior of the
-		 command line processor.  First, for the command line processor to
-		 allow an unrecognized a command line option to be ignored (and
-		 only have a warning printed), use:
-  */
-  My_CLP.recogniseAllOptions(true);
+  bool success = true;
   
-  /* Second, by default, if the parser finds a command line option it
-		 doesn't recognize or finds the --help option, it will throw an
-		 exception.  If you want prevent a command line processor from
-		 throwing an exception (which is important in this program since
-		 we don't have an try/catch around this) when it encounters a
-		 unrecognized option or help is printed, use:
-  */
-  My_CLP.throwExceptions(false);
+  try {
+    
+    out << Teuchos::Teuchos_Version() << std::endl << std::endl;
+    
+    // Creating an empty command line processor looks like:
+    Teuchos::CommandLineProcessor My_CLP;
+    
+    /* To set and option, it must be given a name and default value.  Additionally,
+       each option can be given a help string.  Although it is not necessary, a help
+       string aids a users comprehension of the acceptable command line arguments.
+       Some examples of setting command line options are:
+    */
+    // Set an integer command line option.
+    int NumIters = 1550;
+    My_CLP.setOption("iterations", &NumIters, "Number of iterations");
+    // Set a double-precision command line option.
+    double Tolerance = 1e-10;
+    My_CLP.setOption("tolerance", &Tolerance, "Tolerance");
+    // Set a string command line option.
+    string Solver = "GMRES";
+    My_CLP.setOption("solver", &Solver, "Linear solver");
+    // Set a boolean command line option.    
+    bool Precondition;
+    My_CLP.setOption("precondition","no-precondition",
+                     &Precondition,"Preconditioning flag");
+    // Set an enumeration command line option
+    const int    num_speed_values  = 3;
+    const ESpeed speed_opt_values[] = { SPEED_SLOW, SPEED_MEDIUM, SPEED_FAST };
+    const char*  speed_opt_names[]  = { "slow",     "medium",     "fast"     };
+    ESpeed       Speed = SPEED_MEDIUM;
+    My_CLP.setOption(
+      "speed", &Speed,
+      num_speed_values, speed_opt_values, speed_opt_names,
+      "Speed of our solver"
+      );
 
-	/* We now parse the command line where argc and argv are passed to
-		 the parse method.  Note that since we have turned off exception
-		 throwing above we had better grab the return argument so that
-		 we can see what happened and act accordingly.
-	*/
-  Teuchos::CommandLineProcessor::EParseCommandLineReturn
-		parseReturn= My_CLP.parse( argc, argv );
-	if( parseReturn == Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED ) {
-		return 0;
-	}
-	if( parseReturn != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL   ) {
-		return 1; // Error!
-	}
-	// Here is where you would use these command line arguments but for this example program
-	// we will just print the help message with the new values of the command-line arguments.
-	if (procRank == 0)
-    std::cout << "\nPrinting help message with new values of command-line arguments ...\n\n";
-	My_CLP.printHelpMessage(argv[0],std::cout);
+    /* There are also two methods that control the behavior of the
+       command line processor.  First, for the command line processor to
+       allow an unrecognized a command line option to be ignored (and
+       only have a warning printed), use:
+    */
+    My_CLP.recogniseAllOptions(true);
+  
+    /* Second, by default, if the parser finds a command line option it
+       doesn't recognize or finds the --help option, it will throw an
+       exception.  If you want prevent a command line processor from
+       throwing an exception (which is important in this program since
+       we don't have an try/catch around this) when it encounters a
+       unrecognized option or help is printed, use:
+    */
+    My_CLP.throwExceptions(false);
 
-	// Now we will print the option values
-	if (procRank == 0) {
-    std::cout << "\nPrinting user options after parsing ...\n\n";
-    std::cout << "NumIters     = " << NumIters << std::endl;
-    std::cout << "Tolerance    = " << Tolerance << std::endl;
-    std::cout << "Solver       = \"" << Solver << "\"\n";
-    std::cout << "Precondition = " << Precondition << std::endl;
-    std::cout << "Speed        = " << Speed << std::endl;
-	}
+    /* We now parse the command line where argc and argv are passed to
+       the parse method.  Note that since we have turned off exception
+       throwing above we had better grab the return argument so that
+       we can see what happened and act accordingly.
+    */
+    Teuchos::CommandLineProcessor::EParseCommandLineReturn
+      parseReturn= My_CLP.parse( argc, argv );
+    if( parseReturn == Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED ) {
+      return 0;
+    }
+    if( parseReturn != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL   ) {
+      return 1; // Error!
+    }
+    // Here is where you would use these command line arguments but for this example program
+    // we will just print the help message with the new values of the command-line arguments.
+    if (procRank == 0)
+      out << "\nPrinting help message with new values of command-line arguments ...\n\n";
+    My_CLP.printHelpMessage(argv[0],out);
 
-  return 0;
+    // Now we will print the option values
+    if (procRank == 0) {
+      out << "\nPrinting user options after parsing ...\n\n";
+      out << "NumIters     = " << NumIters << std::endl;
+      out << "Tolerance    = " << Tolerance << std::endl;
+      out << "Solver       = \"" << Solver << "\"\n";
+      out << "Precondition = " << Precondition << std::endl;
+      out << "Speed        = " << Speed << std::endl;
+    }
+
+  } // try
+  TEUCHOS_STANDARD_CATCH_STATEMENTS(true,std::cerr,success);
+  
+  if(success)
+    out << "\nEnd Result: TEST PASSED" << std::endl;	
+
+  return ( success ? 0 : 1 );
 }
