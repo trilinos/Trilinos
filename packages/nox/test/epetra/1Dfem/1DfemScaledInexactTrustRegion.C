40cf54ad5393e607573b8a5ab008e7df88f61418
Roger P. Pawlowski <rppawlo@sandia.gov>
2005-10-24 23:16:18 +0000

    Added tests for the standard and inexact trust region algorithms.

---
+++ packages/nox/test/epetra/1Dfem/1DfemScaledInexactTrustRegion.C
@@ -0,0 +1,345 @@
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//                                                                                 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA                                                                                
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov), Sandia National Laboratories.
+// 
+// ************************************************************************
+//@HEADER
+                                                                    
+// 1D Finite Element Test Problem
+/* Solves the nonlinear equation:
+ *
+ * d2u 
+ * --- - k * u**2 = 0
+ * dx2
+ *
+ * subject to @ x=0, u=1
+ */
+
+// NOX Objects
+#include "NOX.H"
+#include "NOX_Epetra.H"
+
+// Trilinos Objects
+#ifdef HAVE_MPI
+#include "Epetra_MpiComm.h"
+#else
+#include "Epetra_SerialComm.h"
+#endif
+#include "Epetra_Map.h"
+#include "Epetra_Vector.h"
+#include "Epetra_RowMatrix.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_Map.h"
+#include "Epetra_LinearProblem.h"
+#include "AztecOO.h"
+
+// User's application specific files 
+#include "1DfemInterface.H" 
+#include "1DfemPrePostOperator.H"
+
+#include "Teuchos_ParameterList.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+ 
+  // Initialize MPI
+#ifdef HAVE_MPI
+  MPI_Init(&argc,&argv);
+#endif
+
+  // Create a communicator for Epetra objects
+#ifdef HAVE_MPI
+  Epetra_MpiComm Comm( MPI_COMM_WORLD );
+#else
+  Epetra_SerialComm Comm;
+#endif
+
+  // Get the process ID and the total number of processors
+  int MyPID = Comm.MyPID();
+  int NumProc = Comm.NumProc();
+
+  // Check verbosity level
+  bool verbose = false;
+  if (argc > 1)
+    if (argv[1][0]=='-' && argv[1][1]=='v')
+      verbose = true;
+
+  // Get the number of elements from the command line
+  int NumGlobalElements = 0;
+  if ((argc > 2) && (verbose))
+    NumGlobalElements = atoi(argv[2]) + 1;
+  else if ((argc > 1) && (!verbose))
+    NumGlobalElements = atoi(argv[1]) + 1;
+  else 
+    NumGlobalElements = 101;
+
+  // The number of unknowns must be at least equal to the 
+  // number of processors.
+  if (NumGlobalElements < NumProc) {
+    cout << "numGlobalBlocks = " << NumGlobalElements 
+	 << " cannot be < number of processors = " << NumProc << endl;
+    cout << "Test failed!" << endl;
+    throw "NOX Error";
+  }
+
+  // Create the interface between NOX and the application
+  // This object is derived from NOX::Epetra::Interface
+  Teuchos::RefCountPtr<Interface> interface = 
+    Teuchos::rcp(new Interface(NumGlobalElements, Comm));
+
+  // Set the PDE factor (for nonlinear forcing term).  This could be specified
+  // via user input.
+  interface->setPDEfactor(100000.0);
+
+  // Use a scaled vector space.  The scaling must also be registered
+  // with the linear solver so the linear system is consistent!
+  Teuchos::RefCountPtr<Epetra_Vector> scaleVec = 
+    Teuchos::rcp(new Epetra_Vector( *(interface->getSolution())));
+  scaleVec->PutScalar(2.0);
+  Teuchos::RefCountPtr<NOX::Epetra::Scaling> scaling = 
+    Teuchos::rcp(new NOX::Epetra::Scaling);
+  scaling->addUserScaling(NOX::Epetra::Scaling::Left, scaleVec);
+
+  // Use a weighted vector space for scaling all norms
+  Teuchos::RefCountPtr<NOX::Epetra::VectorSpace> weightedVectorSpace = 
+    Teuchos::rcp(new NOX::Epetra::VectorSpaceScaledL2(scaling));
+
+  // Get the vector from the Problem
+  Teuchos::RefCountPtr<Epetra_Vector> soln = interface->getSolution();
+  Teuchos::RefCountPtr<NOX::Epetra::Vector> noxSoln = 
+    Teuchos::rcp(new NOX::Epetra::Vector(soln, 
+					 NOX::Epetra::Vector::CreateCopy,
+					 NOX::DeepCopy,
+					 weightedVectorSpace));
+
+  // Initial Guess 
+  noxSoln->init(2.0);
+
+  // Begin Nonlinear Solver ************************************
+
+  // Create the top level parameter list
+  Teuchos::RefCountPtr<NOX::Parameter::List> nlParamsPtr =
+    Teuchos::rcp(new NOX::Parameter::List);
+  NOX::Parameter::List& nlParams = *(nlParamsPtr.get());
+
+  // Set the nonlinear solver method
+  nlParams.setParameter("Nonlinear Solver", "Inexact Trust Region Based");
+  nlParams.sublist("Trust Region").
+    setParameter("Inner Iteration Method", "Inexact Trust Region");
+
+  // Set the printing parameters in the "Printing" sublist
+  NOX::Parameter::List& printParams = nlParams.sublist("Printing");
+  printParams.setParameter("MyPID", MyPID); 
+  printParams.setParameter("Output Precision", 3);
+  printParams.setParameter("Output Processor", 0);
+  if (verbose)
+    printParams.setParameter("Output Information", 
+			     NOX::Utils::OuterIteration + 
+			     NOX::Utils::OuterIterationStatusTest + 
+			     NOX::Utils::InnerIteration +
+			     NOX::Utils::LinearSolverDetails +
+			     NOX::Utils::Parameters + 
+			     NOX::Utils::Details + 
+			     NOX::Utils::Warning +
+                             NOX::Utils::Debug +
+			     NOX::Utils::TestDetails +
+			     NOX::Utils::Error);
+  else
+    printParams.setParameter("Output Information", NOX::Utils::Error +
+			     NOX::Utils::TestDetails);
+
+  // Create a print class for controlling output below
+  NOX::Utils printing(printParams);
+
+  // Sublist for direction
+  NOX::Parameter::List& dirParams = nlParams.sublist("Direction");
+  dirParams.setParameter("Method", "Newton");
+  NOX::Parameter::List& newtonParams = dirParams.sublist("Newton");
+    newtonParams.setParameter("Forcing Term Method", "Type 1");
+
+  // Sublist for linear solver for the Newton method
+  NOX::Parameter::List& lsParams = newtonParams.sublist("Linear Solver");
+  lsParams.setParameter("Aztec Solver", "GMRES");  
+  lsParams.setParameter("Max Iterations", 800);  
+  lsParams.setParameter("Tolerance", 1e-4);
+
+  // Various Preconditioner options
+  //lsParams.setParameter("Preconditioner", "AztecOO");
+  lsParams.setParameter("Preconditioner", "Ifpack");
+  lsParams.setParameter("Max Age Of Prec", 1);
+
+  // Sublist for Cauchy direction
+  NOX::Parameter::List& cauchyDirParams = nlParams.sublist("Cauchy Direction");
+  cauchyDirParams.setParameter("Method", "Steepest Descent");
+  NOX::Parameter::List& sdParams = cauchyDirParams.sublist("Steepest Descent");
+    sdParams.setParameter("Scaling Type", "Quadratic Model Min");
+
+  // Add a user defined pre/post operator object
+  Teuchos::RefCountPtr<UserPrePostOperator> ppo =
+    Teuchos::rcp(new UserPrePostOperator(printing));
+  nlParams.sublist("Solver Options").setParameter
+    <NOX::Abstract::PrePostOperator>("User Defined Pre/Post Operator", ppo);
+
+  // Let's force all status tests to do a full check
+  nlParams.sublist("Solver Options").
+    setParameter("Status Test Check Type", NOX::StatusTest::Complete);
+
+  // User supplied (Epetra_RowMatrix)
+  Teuchos::RefCountPtr<Epetra_RowMatrix> Analytic = interface->getJacobian();
+
+  // Create the linear system
+  Teuchos::RefCountPtr<NOX::Epetra::Interface::Required> iReq = interface;
+  Teuchos::RefCountPtr<NOX::Epetra::Interface::Jacobian> iJac = interface;
+  Teuchos::RefCountPtr<NOX::Epetra::LinearSystemAztecOO> linSys = 
+    Teuchos::rcp(new NOX::Epetra::LinearSystemAztecOO(printParams, lsParams,
+						      iReq,
+						      iJac, Analytic, 
+						      *noxSoln,
+						      scaling));
+  
+  // Create the Group
+  Teuchos::RefCountPtr<NOX::Epetra::Group> grpPtr = 
+    Teuchos::rcp(new NOX::Epetra::Group(printParams, 
+					iReq, 
+					*noxSoln, 
+					linSys));  
+  NOX::Epetra::Group& grp = *grpPtr;
+
+  // uncomment the following for loca supergroups
+  //MF->setGroupForComputeF(*grpPtr);
+  //FD->setGroupForComputeF(*grpPtr);
+
+  // Create the convergence tests
+  Teuchos::RefCountPtr<NOX::StatusTest::NormF> absresid = 
+    Teuchos::rcp(new NOX::StatusTest::NormF(1.0e-8));
+  Teuchos::RefCountPtr<NOX::StatusTest::NormF> relresid = 
+    Teuchos::rcp(new NOX::StatusTest::NormF(grp, 1.0e-2));
+  Teuchos::RefCountPtr<NOX::StatusTest::NormUpdate> update =
+    Teuchos::rcp(new NOX::StatusTest::NormUpdate(1.0e-5));
+  Teuchos::RefCountPtr<NOX::StatusTest::NormWRMS> wrms =
+    Teuchos::rcp(new NOX::StatusTest::NormWRMS(1.0e-2, 1.0e-8));
+  Teuchos::RefCountPtr<NOX::StatusTest::Combo> converged =
+    Teuchos::rcp(new NOX::StatusTest::Combo(NOX::StatusTest::Combo::AND));
+  converged->addStatusTest(absresid);
+  converged->addStatusTest(relresid);
+  converged->addStatusTest(wrms);
+  converged->addStatusTest(update);
+  Teuchos::RefCountPtr<NOX::StatusTest::MaxIters> maxiters = 
+    Teuchos::rcp(new NOX::StatusTest::MaxIters(200));
+  Teuchos::RefCountPtr<NOX::StatusTest::FiniteValue> fv =
+    Teuchos::rcp(new NOX::StatusTest::FiniteValue);
+  Teuchos::RefCountPtr<NOX::StatusTest::Combo> combo = 
+    Teuchos::rcp(new NOX::StatusTest::Combo(NOX::StatusTest::Combo::OR));
+  combo->addStatusTest(fv);
+  combo->addStatusTest(converged);
+  combo->addStatusTest(maxiters);
+
+  // Create the solver
+  NOX::Solver::Manager solver(grpPtr, combo, nlParamsPtr);
+  NOX::StatusTest::StatusType solvStatus = solver.solve();
+
+  // End Nonlinear Solver **************************************
+
+  // Get the Epetra_Vector with the final solution from the solver
+  const NOX::Epetra::Group& finalGroup = 
+    dynamic_cast<const NOX::Epetra::Group&>(solver.getSolutionGroup());
+  const Epetra_Vector& finalSolution = 
+    (dynamic_cast<const NOX::Epetra::Vector&>(finalGroup.getX())).
+    getEpetraVector();
+
+  // Output the parameter list
+  if (verbose) {
+    if (printing.isPrintType(NOX::Utils::Parameters)) {
+      printing.out() << endl << "Final Parameters" << endl
+	   << "****************" << endl;
+      solver.getParameterList().print(printing.out());
+      printing.out() << endl;
+    }
+  }
+
+  // Print solution
+  char file_name[25];
+  FILE *ifp;
+  int NumMyElements = soln->Map().NumMyElements();
+  (void) sprintf(file_name, "output.%d",MyPID);
+  ifp = fopen(file_name, "w");
+  for (int i=0; i<NumMyElements; i++)
+    fprintf(ifp, "%d  %E\n", soln->Map().MinMyGID()+i, finalSolution[i]);
+  fclose(ifp);
+
+
+  // Tests
+  int status = 0; // Converged
+  
+  // 1. Convergence
+  if (solvStatus != NOX::StatusTest::Converged) {
+      status = 1;
+      if (printing.isPrintType(NOX::Utils::Error))
+	printing.out() << "Nonlinear solver failed to converge!" << endl;
+  }
+#ifndef HAVE_MPI 
+  // 2. Linear solve iterations (14) - SERIAL TEST ONLY!
+  //    The number of linear iterations changes with # of procs.
+  if (solver.getParameterList().sublist("Direction").sublist("Newton").sublist("Linear Solver").sublist("Output").getParameter("Total Number of Linear Iterations",0) != 14) {
+    status = 2;
+  }
+#endif
+  // 3. Nonlinear solve iterations (17)
+  if (solver.getParameterList().sublist("Output").getParameter("Nonlinear Iterations", 0) != 17)
+    status = 3;
+  // 4. Test the pre/post iterate options
+  {
+  UserPrePostOperator* ppoPtr = dynamic_cast<UserPrePostOperator*>(ppo.get());
+  if (ppoPtr->getNumRunPreIterate() != 17)
+    status = 4;
+  if (ppoPtr->getNumRunPostIterate() != 17)
+    status = 4;
+  if (ppoPtr->getNumRunPreSolve() != 1)
+    status = 4;
+  if (ppoPtr->getNumRunPostSolve() != 1)
+    status = 4;
+  }
+  // 5. Number of Cauchy steps (3)
+  if (solver.getParameterList().sublist("Trust Region").sublist("Output").getParameter("Number of Cauchy Steps", 0) != 3)
+    status = 5;
+  // 6. Number of Newton steps (14)
+  if (solver.getParameterList().sublist("Trust Region").sublist("Output").getParameter("Number of Newton Steps", 0) != 14)
+    status = 6;
+
+  // Summarize test results 
+  if (status == 0)
+    printing.out() << "Test passed!" << endl;
+  else 
+    printing.out() << "Test failed!" << endl;
+  
+#ifdef HAVE_MPI
+  MPI_Finalize();
+#endif
+
+  // Final return value (0 = successfull, non-zero = failure)
+  return status;
+}
---
+++ packages/nox/test/epetra/DS6.5.1/Example_TrustRegion.C
@@ -0,0 +1,274 @@
+//@HEADER
+// ************************************************************************
+// 
+//            NOX: An Object-Oriented Nonlinear Solver Package
+//                 Copyright (2002) Sandia Corporation
+// 
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+// 
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//  
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//                                                                                 
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA                                                                                
+// Questions? Contact Tammy Kolda (tgkolda@sandia.gov) or Roger Pawlowski
+// (rppawlo@sandia.gov), Sandia National Laboratories.
+// 
+// ************************************************************************
+//@HEADER
+                                                                                
+//  Simple 2 equation test for quadratic and cubic line searches 
+//  from Dennis & Schnabel's book, chp 6.  The test problem is from
+//  Example 6.5.1
+/*  
+ *    U0**2 + U1**2 - 2 = 0
+ *    exp(U0-1) + U1**3 -2 = 0
+ */
+
+// NOX Library
+#include "NOX.H"
+#include "NOX_Epetra.H"
+
+// Trilinos Objects
+#ifdef HAVE_MPI
+#include "Epetra_MpiComm.h"
+#else
+#include "Epetra_SerialComm.h"
+#endif
+#include "Epetra_Map.h"
+#include "Epetra_Vector.h"
+#include "Epetra_RowMatrix.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_Map.h"
+#include "Epetra_LinearProblem.h"
+#include "AztecOO.h"
+
+// User's application specific files 
+#include "Problem_Interface.H" // Interface file to NOX
+#include "DennisSchnabel.H"              
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int i;
+
+  // Initialize MPI
+#ifdef HAVE_MPI
+  MPI_Init(&argc,&argv);
+#endif
+
+  // Create a communicator for Epetra objects
+#ifdef HAVE_MPI
+  Epetra_MpiComm Comm( MPI_COMM_WORLD );
+#else
+  Epetra_SerialComm Comm;
+#endif
+
+  bool verbose = false;
+  if (argc > 1)
+    if (argv[1][0]=='-' && argv[1][1]=='v')
+      verbose = true;
+
+  // Get the process ID and the total number of processors
+  int MyPID = Comm.MyPID();
+  int NumProc = Comm.NumProc();
+
+  int NumGlobalElements = 2;  // Hardcoded for D&S Example problem
+
+  // A maximum of 2 procesors is allowed since there are only 2 equations
+  if (NumProc >= 3) {
+    cout << "ERROR: Maximum number of processors is 2!" << endl;
+    exit(1);
+  }
+
+  // Create the Problem class.  This creates all required
+  // Epetra objects for the problem and allows calls to the 
+  // function (RHS) and Jacobian evaluation routines.
+  DennisSchnabel Problem(NumGlobalElements, Comm);
+
+  // Get the vector from the Problem
+  Teuchos::RefCountPtr<Epetra_Vector> soln = Problem.getSolution();
+  NOX::Epetra::Vector noxSoln(soln, NOX::Epetra::Vector::CreateView);
+
+  // Initialize Solution
+  if (MyPID==0) {
+    (*soln)[0]=2.0;
+    if (NumProc==1) 
+      (*soln)[1]=0.5;
+  } 
+  else 
+    (*soln)[0]=0.5;
+
+  // Begin Nonlinear Solver ************************************
+
+  // Create the top level parameter list
+  Teuchos::RefCountPtr<NOX::Parameter::List> nlParamsPtr =
+    Teuchos::rcp(new NOX::Parameter::List);
+  NOX::Parameter::List& nlParams = *(nlParamsPtr.get());
+
+  // Set the nonlinear solver method
+  nlParams.setParameter("Nonlinear Solver", "Inexact Trust Region Based");
+  nlParams.sublist("Trust Region").
+    setParameter("Inner Iteration Method", "Standard Trust Region");
+
+  // Set the printing parameters in the "Printing" sublist
+  NOX::Parameter::List& printParams = nlParams.sublist("Printing");
+  printParams.setParameter("MyPID", MyPID); 
+  printParams.setParameter("Output Precision", 5);
+  printParams.setParameter("Output Processor", 0);
+  if ( verbose )
+    printParams.setParameter("Output Information",
+                     NOX::Utils::OuterIteration +
+                     NOX::Utils::OuterIterationStatusTest +
+                     NOX::Utils::InnerIteration +
+                     NOX::Utils::Parameters +
+                     NOX::Utils::Details +
+                     NOX::Utils::Warning +
+                     NOX::Utils::TestDetails);
+  else
+    printParams.setParameter("Output Information", NOX::Utils::Error +
+                     NOX::Utils::TestDetails);
+
+  NOX::Utils printing(printParams);
+
+  // Identify the test problem
+  if (printing.isPrintType(NOX::Utils::TestDetails))
+    cout << "Starting epetra/DS6.5.1/DS_6_5_1.exe" << endl;
+
+  // Identify processor information
+#ifdef HAVE_MPI
+  if (printing.isPrintType(NOX::Utils::TestDetails)) {
+    cout << "Parallel Run" << endl;
+    cout << "Number of processors = " << NumProc << endl;
+    cout << "Print Process = " << MyPID << endl;
+  }
+  Comm.Barrier();
+  if (printing.isPrintType(NOX::Utils::TestDetails))
+    cout << "Process " << MyPID << " is alive!" << endl;
+  Comm.Barrier();
+#else
+  if (printing.isPrintType(NOX::Utils::TestDetails))
+    cout << "Serial Run" << endl;
+#endif
+
+  // Sublist for Newton direction
+  NOX::Parameter::List& dirParams = nlParams.sublist("Direction");
+  dirParams.setParameter("Method", "Newton");
+  NOX::Parameter::List& newtonParams = dirParams.sublist("Newton");
+    newtonParams.setParameter("Forcing Term Method", "Constant");
+
+  // Sublist for linear solver for the Newton method
+  NOX::Parameter::List& lsParams = newtonParams.sublist("Linear Solver");
+  lsParams.setParameter("Aztec Solver", "GMRES");  
+  lsParams.setParameter("Max Iterations", 20);  
+  lsParams.setParameter("Tolerance", 1e-4);
+  lsParams.setParameter("Preconditioner", "None");   
+  if( verbose )
+    lsParams.setParameter("Output Frequency", 1);    
+
+  // Sublist for Cauchy direction
+  NOX::Parameter::List& cauchyDirParams = nlParams.sublist("Cauchy Direction");
+  cauchyDirParams.setParameter("Method", "Steepest Descent");
+  NOX::Parameter::List& sdParams = cauchyDirParams.sublist("Steepest Descent");
+    sdParams.setParameter("Scaling Type", "Quadratic Model Min");
+
+  // Create the interface between the test problem and the nonlinear solver
+  Teuchos::RefCountPtr<Problem_Interface> interface = 
+    Teuchos::rcp(new Problem_Interface(Problem));
+  
+  // Create the Epetra_RowMatrix.  Uncomment one or more of the following:
+  // 1. User supplied (Epetra_RowMatrix)
+  Teuchos::RefCountPtr<Epetra_RowMatrix> A = Problem.getJacobian();
+
+  // Create the callback interfaces for filling the residual and Jacbian
+  Teuchos::RefCountPtr<NOX::Epetra::Interface::Required> iReq = interface;
+  Teuchos::RefCountPtr<NOX::Epetra::Interface::Jacobian> iJac = interface;
+
+  // Create the Linear System
+  Teuchos::RefCountPtr<NOX::Epetra::LinearSystemAztecOO> linSys = 
+    Teuchos::rcp(new NOX::Epetra::LinearSystemAztecOO(printParams, lsParams,
+						      iReq, iJac, A, noxSoln));
+
+  // Create the Group
+  Teuchos::RefCountPtr<NOX::Epetra::Group> grpPtr = 
+    Teuchos::rcp(new NOX::Epetra::Group(printParams, 
+					iReq, 
+					noxSoln, 
+					linSys)); 
+
+  // Create the convergence tests
+  Teuchos::RefCountPtr<NOX::StatusTest::NormF> testNormF = 
+    Teuchos::rcp(new NOX::StatusTest::NormF(1.0e-6));
+  Teuchos::RefCountPtr<NOX::StatusTest::MaxIters> testMaxIters = 
+    Teuchos::rcp(new NOX::StatusTest::MaxIters(25));
+  Teuchos::RefCountPtr<NOX::StatusTest::Combo> combo = 
+    Teuchos::rcp(new NOX::StatusTest::Combo(NOX::StatusTest::Combo::OR, 
+					    testNormF, testMaxIters));
+
+  // Create the method
+  NOX::Solver::Manager solver(grpPtr, combo, nlParamsPtr);
+  NOX::StatusTest::StatusType status = solver.solve();
+
+  // Get the Epetra_Vector with the final solution from the solver
+  const NOX::Epetra::Group& finalGroup = 
+      dynamic_cast<const NOX::Epetra::Group&>(solver.getSolutionGroup());
+  const Epetra_Vector& finalSolution = 
+      (dynamic_cast<const NOX::Epetra::Vector&>(finalGroup.getX())).getEpetraVector();
+
+  // End Nonlinear Solver **************************************
+
+  // Output the parameter list
+  if (printing.isPrintType(NOX::Utils::Parameters)) {
+    cout << endl << "Final Parameters" << endl
+	 << "****************" << endl;
+    solver.getParameterList().print(cout);
+    cout << endl;
+  }
+
+  // Print solution
+  char file_name[25];
+  FILE *ifp;
+  int NumMyElements = soln->Map().NumMyElements();
+  (void) sprintf(file_name, "output.%d",MyPID);
+  ifp = fopen(file_name, "w");
+  for (i=0; i<NumMyElements; i++)
+    fprintf(ifp, "%d  %E\n", soln->Map().MinMyGID()+i, finalSolution[i]);
+  fclose(ifp);
+
+  // Report results
+
+  int testStatus = 0; // Converged
+
+  // 1. Convergence
+  if (status != NOX::StatusTest::Converged) {
+    if (MyPID==0) cout << "Nonlinear solver failed to converge!" << endl;
+    testStatus = 1;
+  }
+  // 2. Nonlinear Iterations (5)
+  if (solver.getParameterList().sublist("Output").getParameter("Nonlinear Iterations",0) != 5) {
+    testStatus = 2;
+  }
+
+  if (testStatus == 0)
+    cout << "Test passed!" << endl;
+  else
+    cout << "Test failed!" << endl;
+
+#ifdef HAVE_MPI
+  MPI_Finalize() ;
+#endif
+ 
+  return testStatus;
+
+} // end main
