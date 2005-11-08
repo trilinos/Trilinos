4219ae77f469c16185857fcbdb279fd6643ab479
Roscoe A. Bartlett <rabartl@sandia.gov>
2005-11-08 23:30:43 +0000

    I have updated this script to include counts of semicolons for C and C++ source files.  I also added the directory name to the output.  Here is the current statistics for Trilinos and MOOCHO (currently not in Trilinos):
    Statistics for source files for Trilinos ...
    
    Number of *.hpp files = 928
    Number of *.cpp files = 1080
    Number of *.H files   = 344
    Number of *.C files   = 329
    Number of *.h files   = 887
    Number of *.c files   = 640
    Number of *.f files   = 78
    Number of *.F files   = 1
    
    Total number of files = 4287
    
    Number of lines in *.hpp files = 198230
    Number of lines in *.cpp files = 310510
    Number of lines in *.H files   = 58832
    Number of lines in *.C files   = 108957
    Number of lines in *.h files   = 128156
    Number of lines in *.c files   = 336037
    Number of lines in *.f files   = 39872
    Number of lines in *.F files   = 123
    
    Total lines of code            = 1180717
    
    Number of semicolons in *.hpp files = 39646
    Number of semicolons in *.cpp files = 120816
    Number of semicolons in *.H files   = 9302
    Number of semicolons in *.C files   = 35370
    Number of semicolons in *.h files   = 27673
    Number of semicolons in *.c files   = 127830
    
    Total number of semicolons          = 360689
    
    Statistics for source files for Moocho ...
    
    Number of *.hpp files = 501
    Number of *.cpp files = 381
    Number of *.h files   = 63
    Number of *.c files   = 61
    Number of *.f files   = 4
    
    Total number of files = 1010
    
    Number of lines in *.hpp files = 77773
    Number of lines in *.cpp files = 90906
    Number of lines in *.h files   = 6573
    Number of lines in *.c files   = 10474
    Number of lines in *.f files   = 394
    
    Total lines of code            = 186120
    
    Number of semicolons in *.hpp files = 10481
    Number of semicolons in *.cpp files = 25052
    Number of semicolons in *.h files   = 600
    Number of semicolons in *.c files   = 3081
    
    Total number of semicolons          = 39214
    
    Here we see that MOOCHO is only about 1/10th the size Trilinos so Trilinos will only increase by about 10 percent when MOOCHO is added as a package.

--- commonTools/refactoring/source_tree_stats.pl
+++ commonTools/refactoring/source_tree_stats.pl
@@ -7,19 +7,22 @@ use Cwd;
 
 my $basedir = Cwd::cwd();
 
-my ($num_hpp_files,  $num_hpp_lines) = get_files_lines_counts("hpp");
-my ($num_cpp_files, $num_cpp_lines)  = get_files_lines_counts("cpp");
-my ($num_H_files,   $num_H_lines)    = get_files_lines_counts("H");
-my ($num_C_files,   $num_C_lines)    = get_files_lines_counts("C");
-my ($num_h_files,   $num_h_lines)    = get_files_lines_counts("h");
-my ($num_c_files,   $num_c_lines)    = get_files_lines_counts("c");
-my ($num_f_files,   $num_f_lines)    = get_files_lines_counts("f");
-my ($num_F_files,   $num_F_lines)    = get_files_lines_counts("F");
+my ($num_hpp_files,  $num_hpp_lines, $num_hpp_semicolons) = get_files_lines_semicolons_counts("hpp");
+my ($num_cpp_files, $num_cpp_lines, $num_cpp_semicolons)  = get_files_lines_semicolons_counts("cpp");
+my ($num_H_files,   $num_H_lines, $num_H_semicolons)    = get_files_lines_semicolons_counts("H");
+my ($num_C_files,   $num_C_lines, $num_C_semicolons)    = get_files_lines_semicolons_counts("C");
+my ($num_h_files,   $num_h_lines, $num_h_semicolons)    = get_files_lines_semicolons_counts("h");
+my ($num_c_files,   $num_c_lines, $num_c_semicolons)    = get_files_lines_semicolons_counts("c");
+my ($num_f_files,   $num_f_lines, $num_f_semicolons)    = get_files_lines_semicolons_counts("f");
+my ($num_F_files,   $num_F_lines, $num_F_semicolons)    = get_files_lines_semicolons_counts("F");
 
 my $num_total_files = $num_hpp_files + $num_cpp_files + $num_H_files + $num_C_files + $num_h_files + $num_c_files + $num_f_files + $num_F_files;
 my $num_total_lines = $num_hpp_lines + $num_cpp_lines + $num_H_lines + $num_C_lines + $num_h_lines + $num_c_lines + $num_f_lines + $num_F_lines;
+my $num_total_semicolons = $num_hpp_semicolons + $num_cpp_semicolons + $num_H_semicolons + $num_C_semicolons + $num_h_semicolons
+  + $num_c_semicolons + $num_f_semicolons + $num_F_semicolons;
 
-print "\nStatistics for source files ...\n\n";
+my @pwd_list = split("/",$ENV{PWD});
+print "\nStatistics for source files for ", $pwd_list[-1], " ...\n\n";
 
 if($num_hpp_files) { print "Number of *.hpp files = $num_hpp_files\n"; }
 if($num_cpp_files) { print "Number of *.cpp files = $num_cpp_files\n"; }
@@ -47,20 +50,38 @@ print "Total lines of code            = $num_total_lines\n";
 
 print "\n";
 
-sub get_files_lines_counts {
+if($num_hpp_files) { print "Number of semicolons in *.hpp files = $num_hpp_semicolons\n"; }
+if($num_cpp_files) { print "Number of semicolons in *.cpp files = $num_cpp_semicolons\n"; }
+if($num_H_files)   { print "Number of semicolons in *.H files   = $num_H_semicolons\n"; }
+if($num_C_files)   { print "Number of semicolons in *.C files   = $num_C_semicolons\n"; }
+if($num_h_files)   { print "Number of semicolons in *.h files   = $num_h_semicolons\n"; }
+if($num_c_files)   { print "Number of semicolons in *.c files   = $num_c_semicolons\n"; }
+#if($num_f_files)   { print "Number of semicolons in *.f files   = $num_f_semicolons\n"; }
+#if($num_F_files)   { print "Number of semicolons in *.F files   = $num_F_semicolons\n"; }
+print "\n";
+print "Total number of semicolons          = $num_total_semicolons\n";
+
+print "\n";
+
+sub get_files_lines_semicolons_counts {
   my $file_ext = shift;
   my $output = `find $basedir -name *.$file_ext -exec wc -l '{}' ';'`;
-  return count_lines_files_from_output(split('\n',$output));
-}
-
-sub count_lines_files_from_output {
-  my @output_lines = @_;
+  my @output_lines = split('\n',$output);
   my $num_files = 0;
   my $num_lines = 0;
   foreach(@output_lines) {
-	++$num_files;
-	$num_lines += (split(" ",$_))[0];
-#	print "num_lines = $num_lines\n";
+    ++$num_files;
+    $num_lines += (split(" ",$_))[0];
+#	  print "num_lines = $num_lines\n";
   }
-  return ( $num_files, $num_lines );
+  my $num_semicolons = 0;
+  #$output = `find $basedir -name *.$file_ext -exec grep -Hc '\;' '{}' ';'`;
+  $output = `find $basedir -name *.$file_ext -exec grep -Hc '\\;' '{}' ';'`;
+  #print "output =\n$output\n";
+  @output_lines = split('\n',$output);
+  foreach(@output_lines) {
+    $num_semicolons += (split(":",$_))[1];
+#	  print "num_semicolons = $num_semicolons\n";
+  }
+  return ( $num_files, $num_lines, $num_semicolons );
 }
