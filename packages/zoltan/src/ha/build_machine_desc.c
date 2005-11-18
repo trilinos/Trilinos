75155efc4a1984d36785c05000a46709ca4f506b
Lee Ann Riesen <lafisk@sandia.gov>
2005-11-18 23:25:12 +0000

    use structure assignment to copy MachineType instead of field by field copies

--- packages/zoltan/src/ha/build_machine_desc.c
+++ packages/zoltan/src/ha/build_machine_desc.c
@@ -123,12 +123,10 @@ int Zoltan_Free_Machine_Desc(MachineType **desc)
 /*******************************************************************/
 /*******************************************************************/
 
-#define COPY_FIELD(f) (*to)->f = from->f;
-#define COPY_FIELD3(f) (*to)->f[0]=from->f[0];(*to)->f[1]=from->f[1];(*to)->f[2]=from->f[2];
-#define FUTURE_COPY(f)
-
 int Zoltan_Copy_Machine_Desc(MachineType **to, MachineType *from)
 {
+  MachineType *md = NULL;
+
   if (*to != NULL) {
     Zoltan_Free_Machine_Desc(to);
   }
@@ -137,30 +135,34 @@ int Zoltan_Copy_Machine_Desc(MachineType **to, MachineType *from)
     return ZOLTAN_OK;
   }
 
-  *to = (MachineType *)ZOLTAN_MALLOC(sizeof(MachineType));
+  md = *to = (MachineType *)ZOLTAN_MALLOC(sizeof(MachineType));
 
-  if (!(*to)){
+  if (!md){
     return ZOLTAN_MEMERR;
   }
-  memset(*to, 0, sizeof(MachineType));
 
-  COPY_FIELD(nnodes);
-  COPY_FIELD(ntypes);
-  COPY_FIELD(top_id);
-  COPY_FIELD(power);
-  COPY_FIELD(memory);
-  COPY_FIELD(bandwidth);
-  COPY_FIELD(latency);
-  COPY_FIELD(ndims);
-  COPY_FIELD3(cart_dim)
-  COPY_FIELD3(wrap_around);
+  *md = *from;
+
+  if (from->ntypes == 1){
+    md->type = (int *)malloc(sizeof(int));
+    md->type[0] = from->type[0];
+  }
+  else if (from->ntypes > 1){
+    md->type = (int *)malloc(sizeof(int) * from->nnodes);
+    memcpy(md->type, from->type, sizeof(int) * from->nnodes);
+  }
+  else{
+    md->type = NULL;
+  }
+
   /*
-  ** The following fields are not being used, so we don't know how to copy them
-  */
+  ** The following fields are not being used, but should
+  ** be copied when they are implemented.
   FUTURE_COPY(xadj);
   FUTURE_COPY(adjncy);
   FUTURE_COPY(adj_band);
   FUTURE_COPY(adj_lat);
+  */
 
   return ZOLTAN_OK;
 }
