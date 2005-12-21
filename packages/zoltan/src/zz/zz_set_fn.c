631d6091710b27a16940dd46777d46a7319e955b
Lee Ann Riesen <lafisk@sandia.gov>
2005-12-21 20:49:48 +0000

    forgot to check for new hg edge weight functions in Zoltan_Set_Fn.

--- packages/zoltan/src/zz/zz_set_fn.c
+++ packages/zoltan/src/zz/zz_set_fn.c
@@ -221,6 +221,14 @@ int ierr;
     ierr = Zoltan_Set_HG_CS_Fn(zz, 
                   (ZOLTAN_HG_CS_FN *) fn, data);
     break;
+  case ZOLTAN_HG_SIZE_EDGE_WEIGHTS_FN_TYPE:
+    ierr = Zoltan_Set_HG_Size_Edge_Weights_Fn(zz, 
+                  (ZOLTAN_HG_SIZE_EDGE_WEIGHTS_FN *) fn, data);
+    break;
+  case ZOLTAN_HG_EDGE_WEIGHTS_FN_TYPE:
+    ierr = Zoltan_Set_HG_Edge_Weights_Fn(zz, 
+                  (ZOLTAN_HG_EDGE_WEIGHTS_FN *) fn, data);
+    break;
   default:
     sprintf(msg, "ZOLTAN_FN_TYPE %d is invalid.\n"
             "Value must be in range 0 to %d.", fn_type, ZOLTAN_MAX_FN_TYPES);
