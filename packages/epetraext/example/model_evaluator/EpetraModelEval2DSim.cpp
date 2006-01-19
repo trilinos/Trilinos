7c5b465a478964e6bac4d6180043994f0e235e6a
Roscoe A. Bartlett <rabartl@sandia.gov>
2006-01-19 19:51:59 +0000

    Adding a simple 2D set of nonlinear equations to show how the ModelEvaluator software works.  I have not committed the configure and makefiles that will build this yet since I want to build from scratch to make sure that this works.

---
+++ packages/epetraext/example/model_evaluator/EpetraModelEval2DSim.cpp
@@ -0,0 +1,140 @@
+#include "EpetraModelEval2DSim.hpp"
+#include "Teuchos_ScalarTraits.hpp"
+#include "Epetra_SerialComm.h"
+#include "Epetra_CrsMatrix.h"
+
+EpetraModelEval2DSim::EpetraModelEval2DSim(
+  const double         d
+  ,const double        p0
+  ,const double        p1
+  ,const double        x00
+  ,const double        x01
+  )
+  :d_(d)
+{
+  using Teuchos::rcp;
+
+#ifdef HAVE_MPI
+	epetra_comm_ = rcp(new Epetra_MpiComm(MPI_COMM_WORLD));
+#else
+	epetra_comm_ = rcp(new Epetra_SerialComm());
+#endif
+
+  const int nx = 2;
+
+  map_x_ = rcp(new Epetra_Map(nx,0,*epetra_comm_));
+  x0_ = rcp(new Epetra_Vector(*map_x_));  (*x0_)[0] = x00; (*x0_)[1] = x01;
+  p_  = rcp(new Epetra_Vector(*map_x_));  (*p_)[0] = p0; (*p_)[1] = p1;
+
+  // Initialize the graph for W CrsMatrix object
+  W_graph_ = rcp(new Epetra_CrsGraph(::Copy,*map_x_,nx));
+  {
+    int indices[nx] = { 0, 1 };
+    for( int i = 0; i < nx; ++i )
+      W_graph_->InsertGlobalIndices(i,nx,indices);
+  }
+  W_graph_->FillComplete();
+
+  isInitialized_ = true;
+
+}
+
+// Overridden from EpetraExt::ModelEvaluator
+
+Teuchos::RefCountPtr<const Epetra_Map>
+EpetraModelEval2DSim::get_x_map() const
+{
+  return map_x_;
+}
+
+Teuchos::RefCountPtr<const Epetra_Map>
+EpetraModelEval2DSim::get_f_map() const
+{
+  return map_x_;
+}
+
+Teuchos::RefCountPtr<const Epetra_Vector>
+EpetraModelEval2DSim::get_x_init() const
+{
+  return x0_;
+}
+
+Teuchos::RefCountPtr<Epetra_Operator>
+EpetraModelEval2DSim::create_W() const
+{
+  return Teuchos::rcp(new Epetra_CrsMatrix(::Copy,*W_graph_));
+}
+
+EpetraExt::ModelEvaluator::InArgs
+EpetraModelEval2DSim::createInArgs() const
+{
+  InArgsSetup inArgs;
+  inArgs.setModelEvalDescription(this->description());
+  inArgs.setSupports(IN_ARG_x,true);
+  inArgs.setSupports(IN_ARG_beta,true);
+  return inArgs;
+}
+
+EpetraExt::ModelEvaluator::OutArgs
+EpetraModelEval2DSim::createOutArgs() const
+{
+  OutArgsSetup outArgs;
+  outArgs.setModelEvalDescription(this->description());
+  outArgs.setSupports(OUT_ARG_f,true);
+  outArgs.setSupports(OUT_ARG_W,true);
+  outArgs.set_W_properties(
+    DerivativeProperties(
+      DERIV_LINEARITY_NONCONST
+      ,DERIV_RANK_FULL
+      ,true // supportsAdjoint
+      )
+    );
+  return outArgs;
+}
+
+void EpetraModelEval2DSim::evalModel( const InArgs& inArgs, const OutArgs& outArgs ) const
+{
+  using Teuchos::dyn_cast;
+  using Teuchos::rcp_dynamic_cast;
+  //
+  // Get the input arguments
+  //
+  const Epetra_Vector &x = *inArgs.get_x();
+  //
+  // Get the output arguments
+  //
+  Teuchos::RefCountPtr<Epetra_Vector>       f_inout = outArgs.get_f();
+  //Teuchos::RefCountPtr<Epetra_Vector>       g_inout = outArgs.get_g(1);
+  Teuchos::RefCountPtr<Epetra_Operator>     W_inout = outArgs.get_W();
+  //
+  // Compute the functions
+  //
+  const Epetra_Vector &p = *p_;
+  if(f_inout.get()) {
+    Epetra_Vector &f = *f_inout;
+    // zero-based indexing for Epetra!
+    f[0] =        x[0]      + x[1]*x[1] - p[0];
+    f[1] = d_ * ( x[0]*x[0] - x[1]      - p[1] );
+  }
+  if(W_inout.get()) {
+    const double beta = inArgs.get_beta();
+    Epetra_CrsMatrix &DfDx = dyn_cast<Epetra_CrsMatrix>(*W_inout);
+    DfDx.PutScalar(0.0);
+    //
+    // Fill W = beta*DfDx
+    //
+    // W = beta*DfDx = beta * [      1.0,  2*x[1] ]
+    //                        [ 2*d*x[0],     -d  ]
+    //
+    double values[2];
+    int indexes[2];
+    // Row [0]
+    values[0] = beta;               indexes[0] = 0;
+    values[1] = 2.0*beta*x[1];      indexes[1] = 1;
+    DfDx.SumIntoGlobalValues( 0, 2, values, indexes );
+    // Row [1]
+    values[0] = 2.0*beta*d_*x[0];   indexes[0] = 0;
+    values[1] = -beta*d_;           indexes[1] = 1;
+    DfDx.SumIntoGlobalValues( 1, 2, values, indexes );
+  }
+}
---
+++ packages/epetraext/thyra/example/model_evaluator/ForwardSolveEpetraModelEval2DSimMain.cpp
@@ -0,0 +1,100 @@
+#include "EpetraModelEval2DSim.hpp"
+#include "Thyra_EpetraModelEvaluator.hpp"
+#include "Thyra_AmesosLinearOpWithSolveFactory.hpp"
+#include "Thyra_DampenedNewtonNonlinearSolver.hpp"
+#include "Teuchos_GlobalMPISession.hpp"
+#include "Teuchos_VerboseObject.hpp"
+#include "Teuchos_CommandLineProcessor.hpp"
+#include "Teuchos_StandardCatchMacros.hpp"
+
+int main( int argc, char* argv[] )
+{
+
+	using Teuchos::CommandLineProcessor;
+  typedef Teuchos::RefCountPtr<Thyra::VectorBase<double> > VectorPtr;
+
+  Teuchos::GlobalMPISession  mpiSession(&argc,&argv);
+
+  bool success = true;
+
+	try {
+	
+		//
+		// Get options from the command line
+		//
+		
+		double       d           = 10.0;
+    double       p0          = 2.0;
+    double       p1          = 0.0;
+    double       x00         = 0.0;
+    double       x01         = 1.0;
+
+    const int numVerbLevels = 6;
+    const Teuchos::EVerbosityLevel verbLevelValues[numVerbLevels]
+      = { Teuchos::VERB_DEFAULT, Teuchos::VERB_NONE, Teuchos::VERB_LOW
+          ,Teuchos::VERB_MEDIUM, Teuchos::VERB_HIGH, Teuchos::VERB_EXTREME };
+    const char* verbLevelNames[numVerbLevels]
+      = {"default","none","low","medium","high","extreme"};
+    Teuchos::EVerbosityLevel verbLevel = Teuchos::VERB_DEFAULT;
+
+    double       tol         = 1e-10;
+    int          maxIters    = 100;
+
+		CommandLineProcessor  clp(false); // Don't throw exceptions
+
+		clp.setOption( "d", &d );
+		clp.setOption( "p0", &p0 );
+		clp.setOption( "p1", &p1 );
+		clp.setOption( "x00", &x00 );
+		clp.setOption( "x01", &x01 );
+    clp.setOption( "verb-level", &verbLevel, numVerbLevels, verbLevelValues, verbLevelNames );
+    clp.setOption( "tol", &tol, "Nonlinear solve tolerance" );
+    clp.setOption( "max-iters", &maxIters, "Maximum number of nonlinear iterations" );
+	
+		CommandLineProcessor::EParseCommandLineReturn
+			parse_return = clp.parse(argc,argv,&std::cerr);
+
+		if( parse_return != CommandLineProcessor::PARSE_SUCCESSFUL )
+			return parse_return;
+
+    Teuchos::RefCountPtr<Teuchos::FancyOStream>
+      out = Teuchos::VerboseObjectBase::getDefaultOStream();
+
+    *out << "\nCreating the nonlinear equations object ...\n";
+		
+    EpetraModelEval2DSim epetraModel(d,p0,p1,x00,x01);
+
+    Thyra::EpetraModelEvaluator thyraModel; // Sets default options!
+    thyraModel.initialize(
+      Teuchos::rcp(&epetraModel,false)
+      ,Teuchos::rcp(new Thyra::AmesosLinearOpWithSolveFactory())
+      );
+    
+    *out << "\nCreating the nonlinear solver and solving the equations ...\n\n";
+
+    Thyra::DampenedNewtonNonlinearSolver<double> newtonSolver; // Set defaults
+    newtonSolver.setVerbLevel(verbLevel);
+    
+    VectorPtr x = createMember(thyraModel.get_x_space());
+    V_V( &*x, *thyraModel.get_x_init() );
+
+    Thyra::SolveCriteria<double> solveCriteria; // Sets defaults
+    solveCriteria.solveTolType = Thyra::SOLVE_TOL_REL_RESIDUAL_NORM;
+    solveCriteria.requestedTol = tol;
+    solveCriteria.maxIterations = maxIters;
+
+    Thyra::SolveStatus<double>
+      solveStatus = newtonSolver.solve(thyraModel,&*x,&solveCriteria);
+
+    *out << "\nNonlinear solver return status:\n";
+    if(1) {
+      Teuchos::OSTab tab(out);
+      *out << solveStatus;
+    }
+    *out << "\nFinal solution for x=\n" << *x;
+
+	}
+  TEUCHOS_STANDARD_CATCH_STATEMENTS(true,std::cerr,success)
+    
+  return  success ? 0 : 1;
+}
