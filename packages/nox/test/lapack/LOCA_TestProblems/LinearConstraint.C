79f4a8d89e648ee99f0b469d630e0364d21b92b5
Eric T. Phipps <etphipp@sandia.gov>
2006-02-16 22:30:28 +0000

    Added a linear constraint for bordered solver tests and changed the norm constraint to reflect changes in the constraint interface.

--- packages/nox/test/lapack/LOCA_TestProblems/LinearConstraint.C
+++ packages/nox/test/lapack/LOCA_TestProblems/LinearConstraint.C
@@ -30,164 +30,188 @@
 // ************************************************************************
 //@HEADER
 
-#include "NormConstraint.H"
+#include "LinearConstraint.H"
 #include "LOCA_Parameter_Vector.H"
 
-NormConstraint::NormConstraint(int N, const LOCA::ParameterVector& pVec) :
-  n(N),
-  constraints(1,1),
+LinearConstraint::LinearConstraint(int NumConstraints, 
+				   const LOCA::ParameterVector& pVec,
+				   NOX::Abstract::Vector& cloneVec) :
+  m(NumConstraints),
+  constraints(m,1),
   isValidConstraints(false),
-  p(pVec),
   x(),
-  isZeroDgDx(false)
+  dgdx(),
+  isZeroDgDx(false),
+  p(pVec),
+  pvec(Teuchos::View, p.getDoubleArrayPointer(), p.length(), p.length(), 1),
+  dgdp(NumConstraints, pVec.length())
 {
   constraints.putScalar(0.0);
-  NOX::LAPACK::Vector xx(n);
-  x = xx.createMultiVector(1);
+  x = cloneVec.createMultiVector(1);
+  dgdx = cloneVec.createMultiVector(m);
+  dgdp.putScalar(0.0);
 }
 
-NormConstraint::NormConstraint(const NormConstraint& source, 
-			       NOX::CopyType type) :
-  n(source.n),
+LinearConstraint::LinearConstraint(const LinearConstraint& source, 
+				   NOX::CopyType type) :
+  m(source.m),
   constraints(source.constraints),
   isValidConstraints(false),
-  p(source.p),
   x(source.x->clone(type)),
-  isZeroDgDx(source.isZeroDgDx)
+  dgdx(source.dgdx->clone(type)),
+  isZeroDgDx(source.isZeroDgDx),
+  p(source.p),
+  pvec(source.pvec),
+  dgdp(source.dgdp)
 {
   if (source.isValidConstraints && type == NOX::DeepCopy)
     isValidConstraints = true;
 }
 
-NormConstraint::~NormConstraint()
+LinearConstraint::~LinearConstraint()
 {
 }
 
 void
-NormConstraint::copy(const LOCA::MultiContinuation::ConstraintInterface& src)
+LinearConstraint::copy(const LOCA::MultiContinuation::ConstraintInterface& src)
 {
-  const NormConstraint& source = dynamic_cast<const NormConstraint&>(src);
+  const LinearConstraint& source = dynamic_cast<const LinearConstraint&>(src);
 
   if (this != &source) {
-    n = source.n;
+    m = source.m;
     constraints = source.constraints;
     isValidConstraints = source.isValidConstraints;
-    p = source.p;
     *x = *source.x;
+    *dgdx = *source.dgdx;
     isZeroDgDx = source.isZeroDgDx;
+    p = source.p;
+    pvec.assign(source.pvec);
+    dgdp = source.dgdp;
   }
 }
 
 Teuchos::RefCountPtr<LOCA::MultiContinuation::ConstraintInterface>
-NormConstraint::clone(NOX::CopyType type) const
+LinearConstraint::clone(NOX::CopyType type) const
 {
-  return Teuchos::rcp(new NormConstraint(*this, type));
+  return Teuchos::rcp(new LinearConstraint(*this, type));
 }
 
 int
-NormConstraint::numConstraints() const
+LinearConstraint::numConstraints() const
 {
-  return 1;
+  return m;
 }
 
 void
-NormConstraint::setX(const NOX::Abstract::Vector& y)
+LinearConstraint::setX(const NOX::Abstract::Vector& y)
 {
   (*x)[0] = y;
-  x->scale(1.0/n);
   isValidConstraints = false;
 }
 
 void
-NormConstraint::setParam(int paramID, double val)
+LinearConstraint::setParam(int paramID, double val)
 {
   p[paramID] = val;
-  isValidConstraints = false;
 }
 
 void
-NormConstraint::setParams(
+LinearConstraint::setParams(
 			 const vector<int>& paramIDs, 
 			 const NOX::Abstract::MultiVector::DenseMatrix& vals)
 {
   for (unsigned int i=0; i<paramIDs.size(); i++)
     p[paramIDs[i]] = vals(i,0);
-  isValidConstraints = false;
 }
 
 NOX::Abstract::Group::ReturnType
-NormConstraint::computeConstraints()
+LinearConstraint::computeConstraints()
 {
-  constraints(0,0) = 0.5 * n * (*x)[0].innerProduct((*x)[0]) - p.getValue("alpha");
+  // compute dg/dx*x + dg/dp*p
+  x->multiply(1.0, *dgdx, constraints);
+  if (pvec.numRows() > 0)
+    constraints.multiply(Teuchos::NO_TRANS, Teuchos::NO_TRANS, 1.0, dgdp, 
+			 pvec, 1.0);
   isValidConstraints = true;
 
   return NOX::Abstract::Group::Ok;
 }
 
 NOX::Abstract::Group::ReturnType
-NormConstraint::computeDX()
+LinearConstraint::computeDX()
 {
   return NOX::Abstract::Group::Ok;
 }
 
 NOX::Abstract::Group::ReturnType
-NormConstraint::computeDP(const vector<int>& paramIDs, 
-			  NOX::Abstract::MultiVector::DenseMatrix& dgdp, 
+LinearConstraint::computeDP(const vector<int>& paramIDs, 
+			  NOX::Abstract::MultiVector::DenseMatrix& dp, 
 			  bool isValidG)
 {
   if (!isValidG) {
-    dgdp(0,0) = constraints(0,0);
+    for (int i=0; i<m; i++)
+      dp(i,0) = constraints(i,0);
   }
 
   for (unsigned int i=0; i<paramIDs.size(); i++) {
-    if (p.getLabel(paramIDs[i]) == "alpha") {
-      dgdp(0,i+1) = -1.0;
-    }
-    else {
-      dgdp(0,i+1) = 0.0;
-    }
+    for (int j=0; j<m; j++)
+      dp(j,i+1) = dgdp(j,paramIDs[i]);
   }
 
   return NOX::Abstract::Group::Ok;
 }
 
 bool
-NormConstraint::isConstraints() const
+LinearConstraint::isConstraints() const
 {
   return isValidConstraints;
 }
 
 bool
-NormConstraint::isDX() const
+LinearConstraint::isDX() const
 {
   return true;
 }
 
 const NOX::Abstract::MultiVector::DenseMatrix&
-NormConstraint::getConstraints() const
+LinearConstraint::getConstraints() const
 {
   return constraints;
 }
 
 const NOX::Abstract::MultiVector*
-NormConstraint::getDX() const
+LinearConstraint::getDX() const
 {
-  return x.get();
+  return dgdx.get();
 }
 
 bool
-NormConstraint::isDXZero() const
+LinearConstraint::isDXZero() const
 {
   return isZeroDgDx;
 }
 
 void
-NormConstraint::setIsZeroDX(bool flag)
+LinearConstraint::notifyCompletedStep()
 {
-  isZeroDgDx = flag;
 }
 
 void
-NormConstraint::notifyCompletedStep()
+LinearConstraint::setDgDx(const NOX::Abstract::MultiVector& A)
 {
+  *dgdx = A;
+  isValidConstraints = false;
+}
+
+void
+LinearConstraint::setDgDp(const NOX::Abstract::MultiVector::DenseMatrix& A)
+{
+  dgdp.assign(A);
+  isValidConstraints = false;
+}
+
+void
+LinearConstraint::setIsZeroDX(bool flag)
+{
+  isZeroDgDx = flag;
 }
