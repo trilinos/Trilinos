2df4292533b304c3ba27238b72c2d5bcf925ac61
William F. Spotz <wfspotz@sandia.gov>
2006-03-01 16:41:20 +0000

    Upgraded testCrsMatrices.py to use unittest framework

--- packages/galeri/python/test/testCrsMatrices.py
+++ packages/galeri/python/test/testCrsMatrices.py
@@ -1,137 +1,230 @@
 #! /usr/bin/env python
 
+# @HEADER
+# ************************************************************************
+#
+#              PyTrilinos.Galeri: Python Interface to Galeri
+#                   Copyright (2005) Sandia Corporation
+#
+# Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+# license for use of this work by or on behalf of the U.S. Government.
+#
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of the
+# License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+# Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+#
+# ************************************************************************
+# @HEADER
+
+# Imports.  Users importing an installed version of PyTrilinos should use the
+# "from PyTrilinos import ..." syntax.  Here, the setpath module adds the build
+# directory, including "PyTrilinos", to the front of the search path.  We thus
+# use "import ..." for Trilinos modules.  This prevents us from accidentally
+# picking up a system-installed version and ensures that we are testing the
+# build module.
+import sys
+
 try:
-  import setpath
-  import Epetra, Galeri
+    import setpath
+    import Epetra
+    import Galeri
 except ImportError:
-  from PyTrilinos import Epetra, Galeri
-  print "Using system-installed Epetra, Galeri"
+    from PyTrilinos import Epetra, Galeri
+    print >>sys.stderr, "Using system-installed Epetra, Galeri"
 
-Comm = Epetra.PyComm()
+from   Numeric  import *
+import unittest
 
-nx = 16 * Comm.NumProc()
-ny = 16 
-nz = 16
-mx = Comm.NumProc()
-my = 1
-mz = 1
+##########################################################################
 
-# ============================================================================ 
+class CreateCrsMatrixTestCase(unittest.TestCase):
+    "TestCase for Galeri CreateCrsMatrix function"
 
-List = {"n": nx }
+    def setUp(self):
+        self.comm   = Epetra.PyComm()
+        nx = 4 * self.comm.NumProc()
+        ny = nz = 16
+        mx = self.comm.NumProc()
+        my = mz = 1
+        self.param1 = {"n"  : nx }
+        self.param2 = {"n"  : nx*ny,
+                       "nx" : nx,
+                       "ny" : ny,
+                       "mx" : mx,
+                       "my" : my    }
+        self.param3 = {"n"  : nx*ny*nz,
+                       "nx" : nx,
+                       "ny" : ny,
+                       "nz" : nz,
+                       "mx" : mx,
+                       "my" : my,
+                       "mz" : mz }
+        self.map1   = Galeri.CreateMap("Interlaced", self.comm, self.param1)
+        self.map2   = Galeri.CreateMap("Random"    , self.comm, self.param2)
+        self.map3   = Galeri.CreateMap("Random"    , self.comm, self.param3)
+        self.comm.Barrier()
 
-Map = Galeri.CreateMap("Interlaced", Comm, List)
+    def tearDown(self):
+        self.comm.Barrier()
 
-if Comm.MyPID() == 0:
-  print "Creating `Diag'..."
-Matrix = Galeri.CreateCrsMatrix("Diag", Map, List)
+    def testDiag(self):
+        "Test Galeri CreateCrsMatrix for problem 'Diag'"
+        matrix = Galeri.CreateCrsMatrix("Diag", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Tridiag'..."
-Matrix = Galeri.CreateCrsMatrix("Tridiag", Map, List)
+    def testTridiag(self):
+        "Test Galeri CreateCrsMatrix for problem 'Tridiag'"
+        matrix = Galeri.CreateCrsMatrix("Tridiag", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Laplace1D'..."
-Matrix = Galeri.CreateCrsMatrix("Laplace1D", Map, List)
+    def testLaplace1D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Laplace1D'"
+        matrix = Galeri.CreateCrsMatrix("Laplace1D", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Laplace1DNeumann'..."
-Matrix = Galeri.CreateCrsMatrix("Laplace1DNeumann", Map, List)
+    def testLaplace1DNeumann(self):
+        "Test Galeri CreateCrsMatrix for problem 'Laplace1DNeumann'"
+        matrix = Galeri.CreateCrsMatrix("Laplace1DNeumann", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Cauchy'..."
-Matrix = Galeri.CreateCrsMatrix("Cauchy", Map, List)
+    def testCauchy(self):
+        "Test Galeri CreateCrsMatrix for problem 'Cauchy'"
+        matrix = Galeri.CreateCrsMatrix("Cauchy", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Fielder'..."
-Matrix = Galeri.CreateCrsMatrix("Fielder", Map, List)
+    def testFielder(self):
+        "Test Galeri CreateCrsMatrix for problem 'Fielder'"
+        matrix = Galeri.CreateCrsMatrix("Fielder", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Hanowa'..."
-Matrix = Galeri.CreateCrsMatrix("Hanowa", Map, List)
+    def testHanowa(self):
+        "Test Galeri CreateCrsMatrix for problem 'Hanowa'"
+        matrix = Galeri.CreateCrsMatrix("Hanowa", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Hilbert'..."
-Matrix = Galeri.CreateCrsMatrix("Hilbert", Map, List)
+    def testHilbert(self):
+        "Test Galeri CreateCrsMatrix for problem 'Hilbert'"
+        matrix = Galeri.CreateCrsMatrix("Hilbert", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `JordanBlock'..."
-Matrix = Galeri.CreateCrsMatrix("JordanBlock", Map, List)
+    def testJordanBlock(self):
+        "Test Galeri CreateCrsMatrix for problem 'JordanBlock'"
+        matrix = Galeri.CreateCrsMatrix("JordanBlock", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `KMS'..."
-Matrix = Galeri.CreateCrsMatrix("KMS", Map, List)
+    def testKMS(self):
+        "Test Galeri CreateCrsMatrix for problem 'KMS'"
+        matrix = Galeri.CreateCrsMatrix("KMS", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Lehmer'..."
-Matrix = Galeri.CreateCrsMatrix("Lehmer", Map, List)
+    def testLehmer(self):
+        "Test Galeri CreateCrsMatrix for problem 'Lehmer'"
+        matrix = Galeri.CreateCrsMatrix("Lehmer", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Ones'..."
-Matrix = Galeri.CreateCrsMatrix("Ones", Map, List)
+    def testOnes(self):
+        "Test Galeri CreateCrsMatrix for problem 'Ones'"
+        matrix = Galeri.CreateCrsMatrix("Ones", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Pei'..."
-Matrix = Galeri.CreateCrsMatrix("Pei", Map, List)
+    def testPei(self):
+        "Test Galeri CreateCrsMatrix for problem 'Pei'"
+        matrix = Galeri.CreateCrsMatrix("Pei", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Ris'..."
-Matrix = Galeri.CreateCrsMatrix("Ris", Map, List)
+    def testRis(self):
+        "Test Galeri CreateCrsMatrix for problem 'Ris'"
+        matrix = Galeri.CreateCrsMatrix("Ris", self.map1, self.param1)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-# ============================================================================ 
+    def testCross2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Cross2D'"
+        matrix = Galeri.CreateCrsMatrix("Cross2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-List = {"n": nx * ny,
-        "nx": nx, "ny": ny,
-        "mx": mx, "my": my }
-Map = Galeri.CreateMap("Random", Comm, List)
+    def testBigCross2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'BigCross2D'"
+        matrix = Galeri.CreateCrsMatrix("BigCross2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Cross2D'..."
-Matrix = Galeri.CreateCrsMatrix("Cross2D", Map, List)
+    def testStar2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Star2D'"
+        matrix = Galeri.CreateCrsMatrix("Star2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `BigCross2D'..."
-Matrix = Galeri.CreateCrsMatrix("BigCross2D", Map, List)
+    def testBigStar2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'BigStar2D'"
+        matrix = Galeri.CreateCrsMatrix("BigStar2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Star2D'..."
-Matrix = Galeri.CreateCrsMatrix("Star2D", Map, List)
+    def testLaplace2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Laplace2D'"
+        matrix = Galeri.CreateCrsMatrix("Laplace2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `BigStar2D'..."
-Matrix = Galeri.CreateCrsMatrix("BigStar2D", Map, List)
+    def testStretched2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Stretched2D'"
+        matrix = Galeri.CreateCrsMatrix("Stretched2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Laplace2D'..."
-Matrix = Galeri.CreateCrsMatrix("Laplace2D", Map, List)
+    def testUniFlow2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'UniFlow2D'"
+        matrix = Galeri.CreateCrsMatrix("UniFlow2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Stretched2D'..."
-Matrix = Galeri.CreateCrsMatrix("Stretched2D", Map, List)
+    def testRecirc2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Recirc2D'"
+        matrix = Galeri.CreateCrsMatrix("Recirc2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `UniFlow2D'..."
-Matrix = Galeri.CreateCrsMatrix("UniFlow2D", Map, List)
+    def testBiharmonic2D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Biharmonic2D'"
+        matrix = Galeri.CreateCrsMatrix("Biharmonic2D", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Recirc2D'..."
-Matrix = Galeri.CreateCrsMatrix("Recirc2D", Map, List)
+    def testLaplace2DFourthOrder(self):
+        "Test Galeri CreateCrsMatrix for problem 'Laplace2DFourthOrder'"
+        matrix = Galeri.CreateCrsMatrix("Laplace2DFourthOrder", self.map2, self.param2)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Biharmonic2D'..."
-Matrix = Galeri.CreateCrsMatrix("Biharmonic2D", Map, List)
+    def testLaplace3D(self):
+        "Test Galeri CreateCrsMatrix for problem 'Laplace3D'"
+        matrix = Galeri.CreateCrsMatrix("Laplace3D", self.map3, self.param3)
+        self.assertEqual(isinstance(matrix, Epetra.CrsMatrix), True)
 
-if Comm.MyPID() == 0:
-  print "Creating `Laplace2DFourthOrder'..."
-Matrix = Galeri.CreateCrsMatrix("Laplace2DFourthOrder", Map, List)
+##########################################################################
 
-# ============================================================================ 
+if __name__ == "__main__":
 
-List = {"n": nx * ny * nz,
-        "nx": nx, "ny": ny, "nz": nz,
-        "mx": mx, "my": my, "mz": mz }
-Map = Galeri.CreateMap("Random", Comm, List)
+    # Create the test suite object
+    suite = unittest.TestSuite()
 
-if Comm.MyPID() == 0:
-  print "Creating `Laplace3D'..."
-Matrix = Galeri.CreateCrsMatrix("Laplace3D", Map, List)
+    # Add the test cases to the test suite
+    suite.addTest(unittest.makeSuite(CreateCrsMatrixTestCase))
+
+    # Create a communicator
+    comm    = Epetra.PyComm()
+    iAmRoot = comm.MyPID() == 0
+
+    # Run the test suite
+    if iAmRoot: print >>sys.stderr, "\n******************************" \
+       "\nTesting Galeri.CreateCrsMatrix\n******************************\n"
+    verbosity = 2 * int(iAmRoot)
+    result = unittest.TextTestRunner(verbosity=verbosity).run(suite)
+
+    # Exit with a code that indicates the total number of errors and failures
+    errsPlusFails = comm.SumAll(len(result.errors) + len(result.failures))[0]
+    if errsPlusFails == 0 and iAmRoot: print "End Result: TEST PASSED"
+    sys.exit(errsPlusFails)
