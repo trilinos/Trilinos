de42d3eaa509fb6cc191eb4793152761f8e5175e
William F. Spotz <wfspotz@sandia.gov>
2006-03-12 22:14:51 +0000

    Fixed typo

--- packages/epetraext/python/example/exInputOutput.py
+++ packages/epetraext/python/example/exInputOutput.py
@@ -120,9 +120,9 @@ def main():
 
 if __name__ == "__main__":
   if numProc == 1:
-    failures = main() # not all I/O works in parallel
-  else
-    failure = 0
+    failures = main()
+  else:
+    failure = 0    # Not all I/O works in parallel
   failures = comm.SumAll(failures)[0]
   if failures == 0 and iAmRoot: print "End Result: TEST PASSED"
   sys.exit(failures)
