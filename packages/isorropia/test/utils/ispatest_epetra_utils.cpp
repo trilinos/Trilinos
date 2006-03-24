bc508ce73459c601bc40ebf5ef3df1258a151d92
Alan B. Williams <william@sandia.gov>
2006-03-24 15:44:22 +0000

    Two initial Isorropia rebalancing functions are implemented, and declared in src/Isorropia_Rebalance.hpp:
    namespace Isorropia {
    
      Epetra_CrsMatrix* create_balanced_copy(const Epetra_CrsMatrix& input_matrix);
    
      Epetra_CrsMatrix* create_balanced_copy(const Epetra_CrsMatrix& input_matrix,
                                             const Epetra_Vector& row_weights);
    }
    
    The Isorropia_Rebalance.hpp header contains doxygen documentation for these
    functions.
    
    The first function simply creates a weights vector consisting of nnz for
    each row, then calls the second function.
    
    The implementation is exercised by the program in test/rebalance_1d_default,
    which can be run as follows:
      mpirun -np 2 rebalance_1d_default.exe --v
    
    It has been tested on linux and sun, with 2,3,4,5,6,7,32 processors.
    
    There is also a unit-test in test/utest,
    which can be run as follows:
      mpirun -np 1 utest.exe --v
    
    The --v flag in both cases is not necessary, it simply turns on verbose output.
    
    More testing and documentation is coming.
    
    This initial rebalancing capability is self-contained, and does not
    utilize Zoltan. The next major step will be to replicate the EpetraExt
    functionality that uses Zoltan's graph partitioning interface.

---
+++ packages/isorropia/test/utils/ispatest_epetra_utils.cpp
@@ -0,0 +1,90 @@
+//@HEADER
+/*
+************************************************************************
+
+              Isorropia: Partitioning and Load Balancing Package
+                Copyright (2006) Sandia Corporation
+
+Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+license for use of this work by or on behalf of the U.S. Government.
+
+This library is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation; either version 2.1 of the
+License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+USA
+Questions? Contact Alan Williams (william@sandia.gov)
+                or Erik Boman    (egboman@sandia.gov)
+
+************************************************************************
+*/
+//@HEADER
+
+#include <ispatest_epetra_utils.hpp>
+#include <Isorropia_Exception.hpp>
+
+#ifdef HAVE_EPETRA
+
+#include <Epetra_Map.h>
+#include <Epetra_CrsMatrix.h>
+
+/** ispatest is the namespace that contains isorropia's test-utilities
+*/
+namespace ispatest {
+
+int fill_matrix(Epetra_CrsMatrix& matrix,
+                int numNonzerosPerRow,
+                bool verbose)
+{
+  int err = 0;
+  const Epetra_Map& rowmap = matrix.RowMap();
+  int num_my_rows = rowmap.NumMyElements();
+  int num_global_rows = rowmap.NumGlobalElements();
+
+  std::vector<int> indices(numNonzerosPerRow);
+  std::vector<double> coefs(numNonzerosPerRow);
+
+  for(int i=0; i<num_my_rows; ++i) {
+    int global_row = rowmap.GID(i);
+    int first_col = global_row - numNonzerosPerRow/2;
+
+    if (first_col < 0) {
+      first_col = 0;
+    }
+    else if (first_col > (num_global_rows - numNonzerosPerRow)) {
+      first_col = num_global_rows - numNonzerosPerRow;
+    }
+
+    for(int j=0; j<numNonzerosPerRow; ++j) {
+      indices[j] = first_col + j;
+      coefs[j] = 1.0;
+    }
+
+    err = matrix.InsertGlobalValues(global_row, numNonzerosPerRow,
+                                    &coefs[0], &indices[0]);
+    if (err < 0) {
+      err = matrix.ReplaceGlobalValues(global_row, numNonzerosPerRow,
+                                      &coefs[0], &indices[0]);
+      if (err < 0) {
+        return(err);
+      }
+    }
+  }
+
+  err = matrix.FillComplete();
+  return(err);
+}
+
+}//namespace ispatest
+
+#endif //HAVE_EPETRA
+
