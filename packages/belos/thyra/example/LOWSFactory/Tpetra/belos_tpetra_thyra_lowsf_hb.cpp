3007b3533cda46cb81380365c62dc4b871259390
Heidi K. Thornquist <hkthorn@sandia.gov>
2006-05-24 21:36:50 +0000

    Added implementation of a Tpetra::Operator that uses the compressed column data format from a Harwell-Boeing file to apply the operator during the apply() function.  This operator (MyOperator) is then used in the Harwell-Boeing example of the Tpetra-Thyra interface to compute the solution to a linear system using Belos.

--- packages/belos/thyra/example/LOWSFactory/Tpetra/belos_tpetra_thyra_lowsf_hb.cpp
+++ packages/belos/thyra/example/LOWSFactory/Tpetra/belos_tpetra_thyra_lowsf_hb.cpp
@@ -61,6 +61,9 @@
 #  include "Tpetra_SerialPlatform.hpp"
 #endif
 
+// My Tpetra::Operator include
+#include "MyOperator.hpp"
+
 int main(int argc, char* argv[])
 {
   //
@@ -69,34 +72,38 @@ int main(int argc, char* argv[])
   Teuchos::RefCountPtr<Teuchos::FancyOStream>
     out = Teuchos::VerboseObjectBase::getDefaultOStream();
   
+  Teuchos::GlobalMPISession mpiSession(&argc,&argv);
+
 #ifdef HAVE_COMPLEX
-  typedef std::complex<double> ST;
+  typedef std::complex<double> ST;  // Scalar-type typedef
 #elif HAVE_COMPLEX_H
-  typedef ::complex<double> ST;
+  typedef ::complex<double> ST;     // Scalar-type typedef
 #else
-  *out << "Not compiled with complex support." << endl;
-  return -1;
-  }
+  typedef double ST;                // Scalar-type typedef
 #endif
-
-  typedef Teuchos::ScalarTraits<ST>::magnitudeType MT;
-  int info = 0;
-  int MyPID = 0;
-  bool norm_failure = false;
-
+  
+  typedef Teuchos::ScalarTraits<ST>::magnitudeType MT;  // Magnitude-type typedef
+  typedef int OT;                   // Ordinal-type typedef
+  ST one = Teuchos::ScalarTraits<ST>::one(); 
+  ST zero = Teuchos::ScalarTraits<ST>::zero(); 
+  
 #ifdef HAVE_MPI
-  // Initialize MPI
-  MPI_Init(&argc,&argv);
-  MPI_Comm_rank(MPI_COMM_WORLD, &MyPID);
+  MPI_Comm mpiComm = MPI_COMM_WORLD;
+  const Tpetra::MpiPlatform<OT,OT>  ordinalPlatform(mpiComm);
+  const Tpetra::MpiPlatform<OT,ST>   scalarPlatform(mpiComm);
+#else
+  const Tpetra::SerialPlatform<OT,OT>  ordinalPlatform;
+  const Tpetra::SerialPlatform<OT,ST>   scalarPlatform;
 #endif
-
+  
   //
   // Get the data from the HB file
   //
-
+  
   // Name of input matrix file
   std::string matrixFile = "mhd1280b.cua";
-
+  
+  int info=0;
   int dim,dim2,nnz;
   MT *dvals;
   int *colptr,*rowind;
@@ -104,30 +111,52 @@ int main(int argc, char* argv[])
   nnz = -1;
   info = readHB_newmat_double(matrixFile.c_str(),&dim,&dim2,&nnz,
                               &colptr,&rowind,&dvals);
+
   if (info == 0 || nnz < 0) {
-    if (MyPID==0) {
-      *out << "Error reading '" << matrixFile << "'" << endl;
-    }
+    *out << "Error reading '" << matrixFile << "'" << endl;
+  }
 #ifdef HAVE_MPI
-    MPI_Finalize();
+  MPI_Finalize();
 #endif
+
+  // Convert interleaved doubles to complex values
+  cvals = new ST[nnz];
+  for (int ii=0; ii<nnz; ii++) {
+    cvals[ii] = ST(dvals[ii*2],dvals[ii*2+1]);
+  }
+  
+  // Declare global dimension of the linear operator
+  OT globalDim = dim;
+  
+  // Create the element space and vector space
+  const Tpetra::ElementSpace<OT> elementSpace(globalDim,0,ordinalPlatform);
+  const Tpetra::VectorSpace<OT,ST> vectorSpace(elementSpace,scalarPlatform);
+  
+  // Create my implementation of a Tpetra::Operator
+  RefCountPtr<Tpetra::Operator<OT,ST> >
+    tpetra_A = rcp( new MyOperator<OT,ST>(vectorSpace,dim,colptr,nnz,rowind,cvals) );
+
+  // Create a Thyra linear operator (A) using the Tpetra::CisMatrix (tpetra_A).
+  RefCountPtr<Thyra::LinearOpBase<ST> >
+    A = Teuchos::rcp( new Thyra::TpetraLinearOp<OT,ST>(tpetra_A) );
+
   //
   // Set the parameters for the Belos LOWS Factory and create a parameter list.
   //
-  int             blockSize              = 2;
-  int             maxIterations          = 400;
-  int             maxRestarts            = 25;
-  int             gmresKrylovLength      = 25;
-  int             outputFrequency        = 10;
+  int             blockSize              = 1;
+  int             maxIterations          = globalDim;
+  int             maxRestarts            = 15;
+  int             gmresKrylovLength      = 50;
+  int             outputFrequency        = 100;
   bool            outputMaxResOnly       = true;
-  double          maxResid               = 1e-6;
-  
+  MT              maxResid               = 1e-5;
+
   Teuchos::RefCountPtr<Teuchos::ParameterList>
     belosLOWSFPL = Teuchos::rcp( new Teuchos::ParameterList() );
   
   belosLOWSFPL->set("Solver Type","GMRES");
   belosLOWSFPL->set("Max Iters",int(maxIterations));
-  belosLOWSFPL->set("Default Rel Res Norm",double(maxResid));
+  belosLOWSFPL->set("Default Rel Res Norm",MT(maxResid));
   belosLOWSFPL->set("Max Restarts",int(maxRestarts));
   belosLOWSFPL->set("Block Size",int(blockSize));
   belosLOWSFPL->sublist("GMRES").set("Max Number of Krylov Vectors",int(gmresKrylovLength*blockSize));
@@ -141,54 +170,55 @@ int main(int argc, char* argv[])
   bool success = true;
 
   // Number of random right-hand sides we will be solving for.
-  int numRhs = 5;
-
-  /*
-  // Create a Thyra linear operator (A) using the Epetra_CrsMatrix (epetra_A).
-  Teuchos::RefCountPtr<Thyra::LinearOpBase<double> > 
-    A = Teuchos::rcp(new Thyra::EpetraLinearOp(epetra_A));
+  int numRhs = 1;
 
   // Get the domain space for the Thyra linear operator 
-  Teuchos::RefCountPtr<const Thyra::VectorSpaceBase<double> > domain = A->domain();
+  Teuchos::RefCountPtr<const Thyra::VectorSpaceBase<ST> > domain = A->domain();
 
   // Create the Belos LOWS factory.
-  Teuchos::RefCountPtr<Thyra::LinearOpWithSolveFactoryBase<double> >
-    belosLOWSFactory = Teuchos::rcp(new Thyra::BelosLinearOpWithSolveFactory<double>());
+  Teuchos::RefCountPtr<Thyra::LinearOpWithSolveFactoryBase<ST> >
+    belosLOWSFactory = Teuchos::rcp(new Thyra::BelosLinearOpWithSolveFactory<ST>());
 
   // Set the parameter list to specify the behavior of the factory.
   belosLOWSFactory->setParameterList( belosLOWSFPL );
 
+  // Set the output stream and the verbosity level (prints to std::cout by defualt)
+  // NOTE:  Set to VERB_NONE for no output from the solver.
+  belosLOWSFactory->setVerbLevel(Teuchos::VERB_LOW);
+
   // Create a BelosLinearOpWithSolve object from the Belos LOWS factory.
-  Teuchos::RefCountPtr<Thyra::LinearOpWithSolveBase<double> >
+  Teuchos::RefCountPtr<Thyra::LinearOpWithSolveBase<ST> >
     nsA = belosLOWSFactory->createOp();
 
   // Initialize the BelosLinearOpWithSolve object with the Thyra linear operator.
   belosLOWSFactory->initializeOp( A, &*nsA );
 
-  // Create a right-hand side with numRhs vectors in it and randomize it.
-  Teuchos::RefCountPtr< Thyra::MultiVectorBase<double> > 
+  // Create a right-hand side with numRhs vectors in it.
+  Teuchos::RefCountPtr< Thyra::MultiVectorBase<ST> > 
     b = Thyra::createMembers(domain, numRhs);
-  Thyra::seed_randomize<double>(0);
-  Thyra::randomize(-1.0, 1.0, &*b);
 
-  // Create an initial vector with numRhs vectors in it and initialize it to zero.
-  Teuchos::RefCountPtr< Thyra::MultiVectorBase<double> >
+  // Create an initial vector with numRhs vectors in it and initialize it to one.
+  Teuchos::RefCountPtr< Thyra::MultiVectorBase<ST> >
     x = Thyra::createMembers(domain, numRhs);
-  Thyra::assign(&*x, 0.0);
+  Thyra::assign(&*x, one);
+
+  // Initialize the right-hand side so that the solution is a vector of ones.
+  A->apply( Thyra::NONCONJ_ELE, *x, &*b );
+  Thyra::assign(&*x, zero);
 
   // Perform solve using the linear operator to get the approximate solution of Ax=b,
   // where b is the right-hand side and x is the left-hand side.
-  Thyra::SolveStatus<double> solveStatus;
+  Thyra::SolveStatus<ST> solveStatus;
   solveStatus = Thyra::solve( *nsA, Thyra::NONCONJ_ELE, *b, &*x );
 
   // Print out status of solve.
   *out << "\nBelos LOWS Status: "<< solveStatus << endl;
 
   //
-  // Compute residual and double check convergence.
+  // Compute residual and ST check convergence.
   //
-  std::vector<double> norm_b(numRhs), norm_res(numRhs);
-  Teuchos::RefCountPtr< Thyra::MultiVectorBase<double> >
+  std::vector<MT> norm_b(numRhs), norm_res(numRhs);
+  Teuchos::RefCountPtr< Thyra::MultiVectorBase<ST> >
     y = Thyra::createMembers(domain, numRhs);
 
   // Compute the column norms of the right-hand side b.
@@ -198,13 +228,13 @@ int main(int argc, char* argv[])
   A->apply( Thyra::NONCONJ_ELE, *x, &*y );
   
   // Compute A*x-b = y-b
-  Thyra::update( -1.0, *b, &*y );
+  Thyra::update( -one, *b, &*y );
 
   // Compute the column norms of A*x-b.
   Thyra::norms_2( *y, &norm_res[0] );
 
   // Print out the final relative residual norms.
-  double rel_res = 0.0;
+  MT rel_res = 0.0;
   *out << "Final relative residual norms" << endl;  
   for (int i=0; i<numRhs; ++i) {
     rel_res = norm_res[i]/norm_b[i];
@@ -215,6 +245,4 @@ int main(int argc, char* argv[])
   }
 
   return ( success ? 0 : 1 );
-  */
-  }
 }
