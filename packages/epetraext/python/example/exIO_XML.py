41ef8ca20a6aa49d7f5befcadb5a906ad1efbbb4
William F. Spotz <wfspotz@sandia.gov>
2006-07-07 18:26:14 +0000

    Added --testharness and --verbosity= command-line options to example scripts

--- packages/epetraext/python/example/exIO_XML.py
+++ packages/epetraext/python/example/exIO_XML.py
@@ -28,85 +28,106 @@
 # ************************************************************************
 # @header
 
+from   optparse import *
 import sys
 
-try:
-  import setpath
-  import Epetra, EpetraExt
-except ImportError:
-  from PyTrilinos import Epetra, EpetraExt
-  print >>sys.stderr, "Using system-installed Epetra, EptraExt"
+parser = OptionParser()
+parser.add_option("-t", "--testharness", action="store_true",
+                  dest="testharness", default=False,
+                  help="test local build modules; prevent loading system-installed modules")
+parser.add_option("-v", "--verbosity", type="int", dest="verbosity", default=2,
+                  help="set the verbosity level [default 2]")
+options,args = parser.parse_args()
+if options.testharness:
+    import setpath
+    import Epetra
+    import EpetraExt
+else:
+    try:
+        import setpath
+        import Epetra, EpetraExt
+    except ImportError:
+        from PyTrilinos import Epetra, EpetraExt
+        print >>sys.stderr, "Using system-installed Epetra, EptraExt"
 
 # Build a global communicator
 comm    = Epetra.PyComm()
 numProc = comm.NumProc()
-iAmRoot = comm.MyPID() == 0
+myPID   = comm.MyPID()
+iAmRoot = myPID == 0
 
 def main():
 
-  failures = 0
-  tolerance = 1e-5
+    tolerance = 1e-5
 
-  # Construct a vector x and populate with random values.
-  # Then constructs a diagonal Epetra_CrsMatrix.
-  n       = 10 * numProc
-  map     = Epetra.Map(n, 0, comm)
-  x       = Epetra.Vector(map)
-  x.Random()
+    # Construct a vector x and populate with random values.
+    # Then construct a diagonal Epetra.CrsMatrix
+    n   = 10 * numProc
+    map = Epetra.Map(n, 0, comm)
+    x   = Epetra.Vector(map)
+    x.Random()
+    print "x =", x
 
-  matrix = Epetra.CrsMatrix(Epetra.Copy, map, 0);
+    matrix = Epetra.CrsMatrix(Epetra.Copy, map, 0)
 
-  for lrid in range(matrix.NumMyRows()):
-      grid = matrix.GRID(lrid)
-      matrix[grid,grid] = grid
+    for lrid in range(matrix.NumMyRows()):
+        grid = matrix.GRID(lrid)
+        matrix[grid,grid] = grid
 
-  matrix.FillComplete()
+    matrix.FillComplete()
+    print "matrix =\n", matrix
 
-  # -------------------------------- #
-  # Part I: Write elements to a file #
-  # -------------------------------- #
+    # -------------------------------- #
+    # Part I: Write elements to a file #
+    # -------------------------------- #
 
-  XMLWriter = EpetraExt.XMLWriter(comm, "data.xml");
+    XMLWriter = EpetraExt.XMLWriter(comm, "data.xml")
 
-  XMLWriter.Create("test xml file")
-  XMLWriter.Write("map",    map)
-  XMLWriter.Write("x",      x)
-  XMLWriter.Write("matrix", matrix)
-  XMLWriter.Close()
+    XMLWriter.Create("test xml file")
+    XMLWriter.Write("map"   , map   )
+    XMLWriter.Write("x"     , x     )
+    XMLWriter.Write("matrix", matrix)
+    XMLWriter.Close()
 
-  # --------------------------------- #
-  # Part II:Read elements from a file #
-  # --------------------------------- #
+    # --------------------------------- #
+    # Part II:Read elements from a file #
+    # --------------------------------- #
 
-  XMLReader = EpetraExt.XMLReader(comm, "data.xml");
+    XMLReader = EpetraExt.XMLReader(comm, "data.xml")
 
-  map2    = XMLReader.ReadMap("map")
-  if map2 == None:
-    failures += 1
+    map2 = XMLReader.ReadMap("map")
+    if map2 == None:
+        print "FAIL:  map2 == None on processor", myPID
+        return 1
 
-  x2      = XMLReader.ReadMultiVector("x")
-  if x2 == None:
-    failures += 1
+    x2 = XMLReader.ReadMultiVector("x")
+    if x2 == None:
+        print "FAIL:  x2 == None on processor", myPID
+        return 1
 
-  matrix2 = XMLReader.ReadCrsMatrix("matrix")
-  if matrix2 == None:
-    failures += 1
+    matrix2 = XMLReader.ReadCrsMatrix("matrix")
+    if matrix2 == None:
+        print "FAIL: matrix2 == None on processor", myPID
+        return 1
 
-  x2.Update(1.0, x, -1.0)
-  norm = x2.Norm2()
+    x2.Update(1.0, x, -1.0)
+    norm = x2.Norm2()
 
-  if abs(norm) < tolerance:
-      if iAmRoot: print "ok"
-  else:
-      if iAmRoot: print "FAILED"
-      failures += 1
+    if abs(norm) < tolerance:
+        if iAmRoot: print "ok"
+    else:
+        if iAmRoot:
+            print "FAILED:"
+            print "  abs(norm) =", abs(norm)
+            print "  tolerance =", tolerance
+        return 1
 
-  return failures
+    return 0
 
 ################################################################
 
 if __name__ == "__main__":
-  failures = main()
-  failures = comm.SumAll(failures)
-  if failures == 0 and iAmRoot: print "End Result: TEST PASSED"
-  sys.exit(failures)
+    failures = main()
+    failures = comm.SumAll(failures)
+    if failures == 0 and iAmRoot: print "End Result: TEST PASSED"
+    sys.exit(failures)
--- packages/epetraext/python/example/exMatrixMatrix.py
+++ packages/epetraext/python/example/exMatrixMatrix.py
@@ -28,14 +28,27 @@
 # ************************************************************************
 # @header
 
+from   optparse import *
 import sys
 
-try:
+parser = OptionParser()
+parser.add_option("-t", "--testharness", action="store_true",
+                  dest="testharness", default=False,
+                  help="test local build modules; prevent loading system-installed modules")
+parser.add_option("-v", "--verbosity", type="int", dest="verbosity", default=2,
+                  help="set the verbosity level [default 2]")
+options,args = parser.parse_args()
+if options.testharness:
     import setpath
-    import Epetra, EpetraExt
-except ImportError:
-    from PyTrilinos import Epetra, EpetraExt
-    print >>sys.stderr, "Using system-installed Epetra, EpetraExt"
+    import Epetra
+    import EpetraExt
+else:
+    try:
+        import setpath
+        import Epetra, EpetraExt
+    except ImportError:
+        from PyTrilinos import Epetra, EpetraExt
+        print >>sys.stderr, "Using system-installed Epetra, EpetraExt"
 
 # Create a global communicator
 comm    = Epetra.PyComm()
