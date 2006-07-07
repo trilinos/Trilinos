cb9da47f7dc1bc454d6102deab53700568cc0f53
William F. Spotz <wfspotz@sandia.gov>
2006-07-07 19:06:08 +0000

    Added --testharness and --verbosity= command-line options to example script

--- packages/aztecoo/python/example/exSimple.py
+++ packages/aztecoo/python/example/exSimple.py
@@ -1,22 +1,38 @@
 #! /usr/bin/env python
-try:
+
+from optparse import *
+
+parser = OptionParser()
+parser.add_option("-t", "--testharness", action="store_true",
+                  dest="testharness", default=False,
+                  help="test local build modules; prevent loading system-installed modules")
+parser.add_option("-v", "--verbosity", type="int", dest="verbosity", default=2,
+                  help="set the verbosity level [default 2]")
+options,args = parser.parse_args()
+if options.testharness:
    import setpath
-except ImportError:
-   from PyTrilinos import Epetra, Galeri, AztecOO
-   print "Using system-installed Epetra, Galeri, AztecOO"
-else:
    import Epetra
    import Galeri
    import AztecOO
+else:
+   try:
+      import setpath
+      import Epetra
+      import Galeri
+      import AztecOO
+   except ImportError:
+      from PyTrilinos import Epetra
+      from PyTrilinos import Galeri
+      from PyTrilinos import AztecOO
+      print "Using system-installed Epetra, Galeri, AztecOO"
 
 comm = Epetra.PyComm()
 
 nx = 30; ny = 30
-GaleriList = {
-  "n": nx * ny,  # for Linear map
-  "nx": nx,      # for Laplace2D, which requires nx
-  "ny": ny       # and ny
-}
+GaleriList = {"n": nx * ny,  # for Linear map
+              "nx": nx,      # for Laplace2D, which requires nx
+              "ny": ny       # and ny
+              }
 Map = Galeri.CreateMap("Linear", comm, GaleriList)
 Matrix = Galeri.CreateCrsMatrix("Laplace2D", Map, GaleriList)
 Exact = Epetra.Vector(Map) 
@@ -28,11 +44,11 @@ Matrix.Multiply(False, Exact, RHS) # fix rhs corresponding to Exact
 
 # Solve the linear problem
 if 0:
-  # this does not work on most installations
-  Problem = Epetra.LinearProblem(Matrix, LHS, RHS)
-  Solver = AztecOO.AztecOO(Problem)
+   # this does not work on most installations
+   Problem = Epetra.LinearProblem(Matrix, LHS, RHS)
+   Solver = AztecOO.AztecOO(Problem)
 else:
-  Solver = AztecOO.AztecOO(Matrix, LHS, RHS)
+   Solver = AztecOO.AztecOO(Matrix, LHS, RHS)
 
 Solver.SetAztecOption(AztecOO.AZ_solver, AztecOO.AZ_cg)
 Solver.SetAztecOption(AztecOO.AZ_precond, AztecOO.AZ_dom_decomp)
