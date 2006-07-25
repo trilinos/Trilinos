90b12ac8e1bd3e44b51220a0dad6313cd4b3464e
Eric T. Phipps <etphipp@sandia.gov>
2006-07-25 23:47:16 +0000

    Re-enabled the Chan homotopy test.

--- packages/nox/test/lapack/LOCA_ChanProblem/ChanHomotopy.C
+++ packages/nox/test/lapack/LOCA_ChanProblem/ChanHomotopy.C
@@ -54,8 +54,44 @@ int main(int argc, char *argv[])
       if (argv[1][0]=='-' && argv[1][1]=='v') 
 	verbose = true;
 
+    // Create parameter list
+    Teuchos::RefCountPtr<Teuchos::ParameterList> paramList = 
+      Teuchos::rcp(new Teuchos::ParameterList);
+
+    // Create LOCA sublist
+    Teuchos::ParameterList& locaParamsList = paramList->sublist("LOCA");
+
+    // Create the stepper sublist and set the stepper parameters
+    Teuchos::ParameterList& stepperList = locaParamsList.sublist("Stepper");
+
+    // Create the "Solver" parameters sublist to be used with NOX Solvers
+    Teuchos::ParameterList& nlParams = paramList->sublist("NOX");
+    nlParams.set("Nonlinear Solver", "Line Search Based");
+
+    Teuchos::ParameterList& nlPrintParams = nlParams.sublist("Printing");
+    if (verbose)
+       nlPrintParams.set("Output Information", 
+			 NOX::Utils::Error +
+			 NOX::Utils::Details +
+			 NOX::Utils::OuterIteration + 
+			 NOX::Utils::InnerIteration + 
+			 NOX::Utils::Warning +
+			 NOX::Utils::TestDetails + 
+			 NOX::Utils::StepperIteration +
+			 NOX::Utils::StepperDetails);
+     else
+       nlPrintParams.set("Output Information", NOX::Utils::Error);
+
+    // Create LAPACK Factory
+    Teuchos::RefCountPtr<LOCA::LAPACK::Factory> lapackFactory = 
+      Teuchos::rcp(new LOCA::LAPACK::Factory);
+
+    // Create global data object
+    Teuchos::RefCountPtr<LOCA::GlobalData> globalData =
+      LOCA::createGlobalData(paramList, lapackFactory);
+
     // Set up the problem interface
-    ChanProblemInterface chan(n, alpha, beta, scale);
+    ChanProblemInterface chan(globalData, n, alpha, beta, scale);
     LOCA::ParameterVector p;
     p.addParameter("alpha",alpha);
     p.addParameter("beta",beta);
@@ -64,118 +100,52 @@ int main(int argc, char *argv[])
     // Create a group which uses that problem interface. The group will
     // be initialized to contain the default initial guess for the
     // specified problem.
-    LOCA::LAPACK::Group grp(chan);
-    grp.setParams(p);
-
-    // Create parameter list
-    Teuchos::ParameterList paramList;
-
-    // Create LOCA sublist
-    Teuchos::ParameterList& locaParamsList = paramList.sublist("LOCA");
-
-    // Create the stepper sublist and set the stepper parameters
-    Teuchos::ParameterList& stepperList = locaParamsList.sublist("Stepper");
-    //stepperList.set("Continuation Method", "Natural");
-    stepperList.set("Continuation Method", "Arc Length");
-    stepperList.set("Continuation Parameter", "alpha");
-    stepperList.set("Initial Value", alpha);
-    stepperList.set("Max Value", 5.0/scale);
-    stepperList.set("Min Value", -1.0/scale);
-    stepperList.set("Max Steps", 100);
-    stepperList.set("Max Nonlinear Iterations", maxNewtonIters);
-    stepperList.set("Enable Arc Length Scaling", true);
-    stepperList.set("Goal Arc Length Parameter Contribution", 0.5);
-    stepperList.set("Max Arc Length Parameter Contribution", 0.7);
-    stepperList.set("Initial Scale Factor", 1.0);
-    stepperList.set("Min Scale Factor", 1.0e-8);
-    stepperList.set("Enable Tangent Factor Step Size Scaling",false);
-    stepperList.set("Min Tangent Factor", -1.0);
-    stepperList.set("Tangent Factor Exponent",1.0);
-    stepperList.set("Compute Eigenvalues",false);
-
-    // Create predictor sublist
-    Teuchos::ParameterList& predictorList = locaParamsList.sublist("Predictor");
-    predictorList.set("Method", "Constant");
-    //predictorList.set("Method", "Tangent");
-    //predictorList.set("Method", "Secant");
-
-    // Create step size sublist
-    Teuchos::ParameterList& stepSizeList = locaParamsList.sublist("Step Size");
-    stepSizeList.set("Method", "Constant");
-    //stepSizeList.set("Method", "Adaptive");
-    stepSizeList.set("Initial Step Size", 0.1);
-    //stepSizeList.set("Min Step Size", 1.0e-3/scale);
-    //stepSizeList.set("Max Step Size", 10.0/scale);
-    //stepSizeList.set("Max Step Size", 1.0);
-    stepSizeList.set("Aggressiveness", 0.5);
-    stepSizeList.set("Failed Step Reduction Factor", 0.5);
-    stepSizeList.set("Successful Step Increase Factor", 1.26); // for constant
-
-    // Set the LOCA Utilities
-    Teuchos::ParameterList& locaUtilsList = locaParamsList.sublist("Utilities");
-    if (verbose) {
-      locaUtilsList.set("Output Information", 
-				 LOCA::Utils::Error + 
-				 LOCA::Utils::Warning +
-				 LOCA::Utils::StepperIteration +
-				 LOCA::Utils::StepperDetails +
-				 LOCA::Utils::Solver +
-				 LOCA::Utils::Parameters +
-				 LOCA::Utils::SolverDetails);
-    }
-     else
-       locaUtilsList.set("Output Information", LOCA::Utils::Error);
-
-    // Create the "Solver" parameters sublist to be used with NOX Solvers
-    Teuchos::ParameterList& nlParams = paramList.sublist("NOX");
-    nlParams.set("Nonlinear Solver", "Line Search Based");
-
-    Teuchos::ParameterList& nlPrintParams = nlParams.sublist("Printing");
-    if (verbose)
-      nlPrintParams.set("Output Information", 
-				 NOX::Utils::Details +
-				 NOX::Utils::OuterIteration + 
-				 NOX::Utils::InnerIteration + 
-				 NOX::Utils::Warning +
-				 NOX::Utils::Error +
-				 NOX::Utils::TestDetails);
-    else
-       nlPrintParams.set("Output Information", NOX::Utils::Error);
+    Teuchos::RefCountPtr<LOCA::LAPACK::Group> grp = 
+      Teuchos::rcp(new LOCA::LAPACK::Group(globalData, chan));
+    grp->setParams(p);
 
     // Set up the status tests
-    Teuchos::RefCountPtr<NOX::StatusTest::NormF> statusTestA =
-      Teuchos::rcp(new NOX::StatusTest::NormF(grp, 1.0e-8));
+    Teuchos::RefCountPtr<NOX::StatusTest::NormF> statusTestA = 
+      Teuchos::rcp(new NOX::StatusTest::NormF(*grp, 1.0e-8));
     Teuchos::RefCountPtr<NOX::StatusTest::MaxIters> statusTestB = 
       Teuchos::rcp(new NOX::StatusTest::MaxIters(maxNewtonIters));
-    NOX::StatusTest::Combo combo(NOX::StatusTest::Combo::OR, statusTestA, statusTestB);
+    Teuchos::RefCountPtr<NOX::StatusTest::Combo> combo = 
+      Teuchos::rcp(new NOX::StatusTest::Combo(NOX::StatusTest::Combo::OR, 
+					       statusTestA, statusTestB));
 
     // Create the homotopy group
-    LOCA::Homotopy::Group hGrp(locaParamsList, grp);
+    Teuchos::RefCountPtr<LOCA::Homotopy::Group> hGrp = 
+      Teuchos::rcp(new LOCA::Homotopy::Group(locaParamsList, globalData, grp));
+
+    // Override default parameters
+    stepperList.set("Max Nonlinear Iterations", maxNewtonIters);
 
     // Create the stepper  
-    LOCA::Stepper stepper(hGrp, combo, paramList);
+    LOCA::NewStepper stepper(globalData, hGrp, combo, paramList);
 
     // Solve the nonlinear system
     LOCA::Abstract::Iterator::IteratorStatus status = stepper.run();
 
     if (status != LOCA::Abstract::Iterator::Finished) {
       ierr = 1;
-      if (LOCA::Utils::doPrint(LOCA::Utils::Error))
-	cout << "Stepper failed to converge!" << endl;
+      if (globalData->locaUtils->isPrintType(NOX::Utils::Error))
+	globalData->locaUtils->out() << "Stepper failed to converge!" 
+				     << std::endl;
     }
 
-    // Get the final solution from the solver
-    const LOCA::Homotopy::Group& finalGroup = 
-      dynamic_cast<const LOCA::Homotopy::Group&>(stepper.getBifurcationGroup());
+    // Get the final solution from the stepper
+    Teuchos::RefCountPtr<const LOCA::LAPACK::Group> finalGroup = 
+      Teuchos::rcp_dynamic_cast<const LOCA::LAPACK::Group>(stepper.getSolutionGroup());
     const NOX::LAPACK::Vector& finalSolution = 
-      dynamic_cast<const NOX::LAPACK::Vector&>(finalGroup.getX());
+      dynamic_cast<const NOX::LAPACK::Vector&>(finalGroup->getX());
 
     // Output the parameter list
-    if (LOCA::Utils::doPrint(LOCA::Utils::Parameters)) {
-      cout << endl << "Final Parameters" << endl
-	   << "****************" << endl;
-      stepper.getList().print(cout);
-      cout << endl;
+    if (globalData->locaUtils->isPrintType(NOX::Utils::Parameters)) {
+      globalData->locaUtils->out() 
+	<< std::endl << "Final Parameters" << std::endl
+	<< "****************" << std::endl;
+      stepper.getList()->print(globalData->locaUtils->out());
+      globalData->locaUtils->out() << std::endl;
     }
 
     // Check some statistics on the solution
@@ -201,7 +171,7 @@ int main(int argc, char *argv[])
 
     // Check final value of continuation parameter
     double alpha_final = 
-      finalGroup.getParam("Homotopy Continuation Parameter");
+      finalGroup->getParam("Homotopy Continuation Parameter");
     double alpha_expected = 1.0;
     ierr += testCompare.testValue(alpha_final, alpha_expected, 1.0e-14,
 				  "final value of continuation parameter", 
@@ -215,19 +185,23 @@ int main(int argc, char *argv[])
 				  NOX::TestCompare::Relative);
   }
 
+  catch (std::exception& e) {
+    std::cout << e.what() << std::endl;
+    ierr = 1;
+  }
   catch (const char *s) {
-    cout << s << endl;
+    std::cout << s << std::endl;
     ierr = 1;
   }
   catch (...) {
-    cout << "Caught unknown exception!" << endl;
+    std::cout << "Caught unknown exception!" << std::endl;
     ierr = 1;
   }
 
   if (ierr == 0)
-    cout << "All tests passed!" << endl;
+    std::cout << "All tests passed!" << std::endl;
   else
-    cout << ierr << " test(s) failed!" << endl;
+    std::cout << ierr << " test(s) failed!" << std::endl;
 
   return 0;
 }
