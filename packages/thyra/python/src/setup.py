44960287d5126363844481539f747bffc4bd9d15
William F. Spotz <wfspotz@sandia.gov>
2006-08-01 18:07:19 +0000

    Removed dependency on package PyTrilinos per bug 2403

--- packages/thyra/python/src/setup.py
+++ packages/thyra/python/src/setup.py
@@ -67,9 +67,7 @@ uniquifyList(extra_compile_args)
 # strings, add them together to obtain a big list of option strings, and then
 # remove any duplicate entries
 options = THYRA_INCLUDES.split() + \
-          THYRA_LIBS.split()     + \
-          PYTRILINOS_INCLUDES.split()  + \
-          PYTRILINOS_LIBS.split()
+          THYRA_LIBS.split()
 uniquifyList(options)
 
 # Distribute the individual options to the appropriate Extension class arguments
