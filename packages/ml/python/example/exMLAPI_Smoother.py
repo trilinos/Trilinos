e59820aa559e118b4019e260ee15ee604934d90f
William F. Spotz <wfspotz@sandia.gov>
2006-08-03 15:17:02 +0000

    Updated copyright headers

--- packages/ml/python/example/exMLAPI_Smoother.py
+++ packages/ml/python/example/exMLAPI_Smoother.py
@@ -1,5 +1,33 @@
 #! /usr/bin/env python
 
+# @HEADER
+# ************************************************************************
+# 
+#                  PyTrilinos.ML: Python Interface to ML
+#                   Copyright (2005) Sandia Corporation
+# 
+# Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+# license for use of this work by or on behalf of the U.S. Government.
+# 
+# This library is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 2.1 of the
+# License, or (at your option) any later version.
+#  
+# This library is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#  
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# USA
+# Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
+# 
+# ************************************************************************
+# @HEADER
+
 # -------------------------------------------------------------------------- #
 # This example shows how to derive class MLAPI_BaseOperator to define 
 # smoothers in the ML module of Python. Class `MyJacobiSmoother' is a 
