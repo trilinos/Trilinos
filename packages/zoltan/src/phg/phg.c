097cf6baf7aa257702989360d783d8aafe25828f
Vitus J. Leung <vjleung@sandia.gov>
2006-08-14 19:45:05 +0000

    added repartition migration output.

--- packages/zoltan/src/phg/phg.c
+++ packages/zoltan/src/phg/phg.c
@@ -374,16 +374,17 @@ End:
    * KDDKDD code.  */
   if ((err == ZOLTAN_OK) && hgp.final_output) {
     static int nRuns=0;
-    static double balsum = 0.0, cutlsum = 0.0, cutnsum = 0.0;
-    static double balmax = 0.0, cutlmax = 0.0, cutnmax = 0.0;
-    static double balmin = 1e100, cutlmin = 1e100, cutnmin = 1e100;
+    static double balsum = 0.0, cutlsum = 0.0, cutnsum = 0.0, movesum = 0.0;
+    static double balmax = 0.0, cutlmax = 0.0, cutnmax = 0.0, movemax = 0.0;
+    static double balmin = 1e100, cutlmin = 1e100, cutnmin=1e100,movemin=1e100;
     double bal = 0.; 
     double cutl = 0.; /* Connnectivity cuts:  sum_over_edges((npart-1)*ewgt) */
     double cutn = 0.; /* Net cuts:  sum_over_edges((nparts>1)*ewgt) */
+    double move = 0.; /* Migrations:  sum_over_vertices((nparts>1)) */
 
-    double rlocal[2];  /* local cut stats for removed edges */
-    double rglobal[2]; /* global cut stats for removed edges */
-    int gnremove;
+    double rlocal[3];  /* local cut stats for removed edges and move stat */
+    double rglobal[3]; /* global cut stats for removed edges and move stat */
+    int gnremove, i;
 
     if (do_timing) {
       /* Do not include final output time in partitioning time */
@@ -399,6 +400,10 @@ End:
                                       zz->LB.Num_Global_Parts, &err);
       cutn = Zoltan_PHG_Compute_NetCut(hg->comm, hg, parts,
                                        zz->LB.Num_Global_Parts);
+
+      for (i = 0; i < zoltan_hg->nObj; ++i)
+	if (zoltan_hg->Input_Parts[i] != zoltan_hg->Output_Parts[i])
+	  ++move;
     }
 
     if (!err) {
@@ -408,12 +413,17 @@ End:
                     zz->Communicator);
       if (gnremove) {
         err = Zoltan_PHG_Removed_Cuts(zz, zoltan_hg, rlocal);
-        MPI_Allreduce(rlocal, rglobal, 2, MPI_DOUBLE,MPI_SUM,zz->Communicator);
+	rlocal[2] = move;
+        MPI_Allreduce(rlocal, rglobal, 3, MPI_DOUBLE,MPI_SUM,zz->Communicator);
         
         cutl += rglobal[0];
         cutn += rglobal[1];
+	move += rglobal[2];
       }
   
+      movesum += move;
+      if (move > movemax) movemax = move;
+      if (move < movemin) movemin = move;
       cutlsum += cutl;
       if (cutl > cutlmax) cutlmax = cutl;
       if (cutl < cutlmin) cutlmin = cutl;
@@ -435,6 +445,9 @@ End:
         uprintf(hg->comm, 
                 "STATS Runs %d  cutn CURRENT %f  MAX %f  MIN %f  AVG %f\n", 
                 nRuns, cutn, cutnmax, cutnmin, cutnsum/nRuns);
+	uprintf(hg->comm,
+		"STATS Runs %d  move CURRENT %f  MAX %f  MIN %f  AVG %f\n",
+		nRuns, move, movemax, movemin, movesum/nRuns);
       }
     }
 
