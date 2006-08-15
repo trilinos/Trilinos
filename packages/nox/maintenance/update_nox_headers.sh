5d8411665dfde0b873e94f4e94050c2f36212ff2
Roger P. Pawlowski <rppawlo@sandia.gov>
2006-08-15 23:50:26 +0000

    Added file to update script.

--- packages/nox/maintenance/update_nox_headers.sh
+++ packages/nox/maintenance/update_nox_headers.sh
@@ -6,3 +6,4 @@ find . -type f -name *.C -exec maintenance/autoheader {} maintenance/header.txt 
 find . -type f -name \*.am -exec maintenance/autoheader {} maintenance/header.txt \;
 find . -type f -name \*.ac -exec maintenance/autoheader {} maintenance/header.txt \;
 maintenance/autoheader Makefile.export.nox.in maintenance/header.txt
+maintenance/autoheader README maintenance/header.txt
