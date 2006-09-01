db23d7b9a3ba7a44d20acf61f86f7a777f1f25ee
Christopher G. Baker <bakercg@ornl.gov>
2006-09-01 16:23:20 +0000

    merge from devel branch
    mistake in indexing causing test failures; fixed now.

--- packages/anasazi/test/BlockKrylovSchur/cxx_main_nh.cpp
+++ packages/anasazi/test/BlockKrylovSchur/cxx_main_nh.cpp
@@ -108,7 +108,7 @@ int main(int argc, char *argv[]) {
   std::vector<int> NumNz(NumMyElements);
 
   /* We are building a matrix of block structure:
-  
+
       | T -I          |
       |-I  T -I       |
       |   -I  T       |
@@ -141,7 +141,7 @@ int main(int argc, char *argv[]) {
   // When rho*h/2 <= 1, the discrete convection-diffusion operator has real eigenvalues.
   // When rho*h/2 > 1, the operator has complex eigenvalues.
   double rho = 2*(nx+1);
-  
+
   // Compute coefficients for discrete convection-diffution operator
   const double one = 1.0;
   std::vector<double> Values(4);
@@ -299,13 +299,13 @@ int main(int argc, char *argv[]) {
   // Create the eigenproblem.
   Teuchos::RefCountPtr<Anasazi::BasicEigenproblem<double, MV, OP> > MyProblem =
     Teuchos::rcp( new Anasazi::BasicEigenproblem<double, MV, OP>(A, ivec) );
-  
+
   // Inform the eigenproblem that the operator A is non-Hermitian
   MyProblem->setHermitian(false); 
-  
+
   // Set the number of eigenvalues requested
   MyProblem->setNEV( nev );
-  
+
   // Inform the eigenproblem that you are finishing passing it information
   boolret = MyProblem->setProblem();
   if (boolret != true) {
@@ -317,10 +317,10 @@ int main(int argc, char *argv[]) {
 #endif
     return -1;
   }
-  
+
   // Initialize the Block Arnoldi solver
   Anasazi::BlockKrylovSchurSolMgr<double, MV, OP> MySolverMgr(MyProblem, MyPL);
-  
+
   // Solve the problem to the specified tolerances or length
   Anasazi::ReturnType returnCode = MySolverMgr.solve();
   testFailed = false;
@@ -330,20 +330,20 @@ int main(int argc, char *argv[]) {
 
   // Get the Ritz values from the eigensolver
   std::vector<Anasazi::Value<double> > ritzValues = MySolverMgr.getRitzValues();
-  
+
   // Output computed eigenvalues and their direct residuals
   if (verbose && MyPID==0) {
     int numritz = (int)ritzValues.size();
-    cout.setf(ios_base::right, ios_base::adjustfield);	
+    cout.setf(ios_base::right, ios_base::adjustfield);        
     cout<<endl<< "Computed Ritz Values"<< endl;
     cout<< std::setw(16) << "Real Part"
-	<< std::setw(16) << "Imag Part"
-	<< endl;
+        << std::setw(16) << "Imag Part"
+        << endl;
     cout<<"-----------------------------------------------------------"<<endl;
     for (int i=0; i<numritz; i++) {
       cout<< std::setw(16) << ritzValues[i].realpart 
-	  << std::setw(16) << ritzValues[i].imagpart 
-	  << endl;
+          << std::setw(16) << ritzValues[i].imagpart 
+          << endl;
     }  
     cout<<"-----------------------------------------------------------"<<endl;
   }
@@ -354,105 +354,105 @@ int main(int argc, char *argv[]) {
   Teuchos::RefCountPtr<MV> evecs = sol.Evecs;
   std::vector<int> index = sol.index;
   int numev = sol.numVecs;
-  
+
   if (numev > 0) {
     // Compute residuals.
     Teuchos::LAPACK<int,double> lapack;
     std::vector<double> normA(numev);
-    
+
     // The problem is non-Hermitian.
     int i=0;
     std::vector<int> curind(1);
     std::vector<double> resnorm(1), tempnrm(1);
     Teuchos::RefCountPtr<MV> evecr, eveci, tempAevec;
     Epetra_MultiVector Aevec(Map,numev);
-    
+
     // Compute A*evecs
     OPT::Apply( *A, *evecs, Aevec );
-    
+
     Teuchos::SerialDenseMatrix<int,double> Breal(1,1), Bimag(1,1);
     while (i<numev) {
       if (index[i]==0) {
-	// Get a view of the current eigenvector (evecr)
-	curind[0] = i;
-	evecr = MVT::CloneView( *evecs, curind );
-	
-	// Get a copy of A*evecr
-	tempAevec = MVT::CloneCopy( Aevec, curind );
-	
-	// Compute A*evecr - lambda*evecr
-	Breal(0,0) = evals[i].realpart;
-	MVT::MvTimesMatAddMv( -1.0, *evecr, Breal, 1.0, *tempAevec );
-	
-	// Compute the norm of the residual and increment counter
-	MVT::MvNorm( *tempAevec, &resnorm );
-	normA[i] = resnorm[0] / Teuchos::ScalarTraits<MagnitudeType>::magnitude( evals[i].realpart );
-	i++;
+        // Get a view of the current eigenvector (evecr)
+        curind[0] = i;
+        evecr = MVT::CloneView( *evecs, curind );
+
+        // Get a copy of A*evecr
+        tempAevec = MVT::CloneCopy( Aevec, curind );
+
+        // Compute A*evecr - lambda*evecr
+        Breal(0,0) = evals[i].realpart;
+        MVT::MvTimesMatAddMv( -1.0, *evecr, Breal, 1.0, *tempAevec );
+
+        // Compute the norm of the residual and increment counter
+        MVT::MvNorm( *tempAevec, &resnorm );
+        normA[i] = resnorm[0] / Teuchos::ScalarTraits<MagnitudeType>::magnitude( evals[i].realpart );
+        i++;
       } else {
-	// Get a view of the real part of the eigenvector (evecr)
-	curind[0] = i;
-	evecr = MVT::CloneView( *evecs, curind );
-	
-	// Get a copy of A*evecr
-	tempAevec = MVT::CloneCopy( Aevec, curind );
-	
-	// Get a view of the imaginary part of the eigenvector (eveci)
-	curind[0] = i+1;
-	eveci = MVT::CloneView( *evecs, curind );
-	
-	// Set the eigenvalue into Breal and Bimag
-	Breal(0,0) = evals[i].realpart;
-	Bimag(0,0) = evals[i].imagpart;
-	
-	// Compute A*evecr - evecr*lambdar + eveci*lambdai
-	MVT::MvTimesMatAddMv( -1.0, *evecr, Breal, 1.0, *tempAevec );
-	MVT::MvTimesMatAddMv( 1.0, *eveci, Bimag, 1.0, *tempAevec );
-	MVT::MvNorm( *tempAevec, &tempnrm );
-	
-	// Get a copy of A*eveci
-	tempAevec = MVT::CloneCopy( Aevec, curind );
-	
-	// Compute A*eveci - eveci*lambdar - evecr*lambdai
-	MVT::MvTimesMatAddMv( -1.0, *evecr, Bimag, 1.0, *tempAevec );
-	MVT::MvTimesMatAddMv( -1.0, *eveci, Breal, 1.0, *tempAevec );
-	MVT::MvNorm( *tempAevec, &resnorm );
-	
-	// Compute the norms and scale by magnitude of eigenvalue
-	normA[i] = lapack.LAPY2( tempnrm[i], resnorm[i] ) /
-	  lapack.LAPY2( evals[i].realpart, evals[i].imagpart );
-	normA[i+1] = normA[i];
-	
-	i=i+2;
+        // Get a view of the real part of the eigenvector (evecr)
+        curind[0] = i;
+        evecr = MVT::CloneView( *evecs, curind );
+
+        // Get a copy of A*evecr
+        tempAevec = MVT::CloneCopy( Aevec, curind );
+
+        // Get a view of the imaginary part of the eigenvector (eveci)
+        curind[0] = i+1;
+        eveci = MVT::CloneView( *evecs, curind );
+
+        // Set the eigenvalue into Breal and Bimag
+        Breal(0,0) = evals[i].realpart;
+        Bimag(0,0) = evals[i].imagpart;
+
+        // Compute A*evecr - evecr*lambdar + eveci*lambdai
+        MVT::MvTimesMatAddMv( -1.0, *evecr, Breal, 1.0, *tempAevec );
+        MVT::MvTimesMatAddMv( 1.0, *eveci, Bimag, 1.0, *tempAevec );
+        MVT::MvNorm( *tempAevec, &tempnrm );
+
+        // Get a copy of A*eveci
+        tempAevec = MVT::CloneCopy( Aevec, curind );
+
+        // Compute A*eveci - eveci*lambdar - evecr*lambdai
+        MVT::MvTimesMatAddMv( -1.0, *evecr, Bimag, 1.0, *tempAevec );
+        MVT::MvTimesMatAddMv( -1.0, *eveci, Breal, 1.0, *tempAevec );
+        MVT::MvNorm( *tempAevec, &resnorm );
+
+        // Compute the norms and scale by magnitude of eigenvalue
+        normA[i] = lapack.LAPY2( tempnrm[0], resnorm[0] ) /
+          lapack.LAPY2( evals[i].realpart, evals[i].imagpart );
+        normA[i+1] = normA[i];
+
+        i=i+2;
       }
     }
-    
+
     // Output computed eigenvalues and their direct residuals
     if (verbose && MyPID==0) {
-      cout.setf(ios_base::right, ios_base::adjustfield);	
+      cout.setf(ios_base::right, ios_base::adjustfield);        
       cout<<endl<< "Actual Residuals"<<endl;
       cout<< std::setw(16) << "Real Part"
-	  << std::setw(16) << "Imag Part"
-	  << std::setw(20) << "Direct Residual"<< endl;
+          << std::setw(16) << "Imag Part"
+          << std::setw(20) << "Direct Residual"<< endl;
       cout<<"-----------------------------------------------------------"<<endl;
       for (int i=0; i<numev; i++) {
-	cout<< std::setw(16) << evals[i].realpart 
-	    << std::setw(16) << evals[i].imagpart 
-	    << std::setw(20) << normA[i] << endl;
-	if ( normA[i] > tol ) {
-	  testFailed = true;
-	}
+        cout<< std::setw(16) << evals[i].realpart 
+            << std::setw(16) << evals[i].imagpart 
+            << std::setw(20) << normA[i] << endl;
+        if ( normA[i] > tol ) {
+          testFailed = true;
+        }
       }  
       cout<<"-----------------------------------------------------------"<<endl;
     }
   }
-  
+
 #ifdef EPETRA_MPI
   MPI_Finalize();
 #endif
-  
+
   if (testFailed) {
     if (verbose && MyPID==0) {
-      cout << "End Result: TEST FAILED" << endl;	
+      cout << "End Result: TEST FAILED" << endl;        
     }
     return -1;
   }
@@ -462,6 +462,6 @@ int main(int argc, char *argv[]) {
   if (verbose && MyPID==0) {
     cout << "End Result: TEST PASSED" << endl;
   }
-  
+
   return 0;
 }
