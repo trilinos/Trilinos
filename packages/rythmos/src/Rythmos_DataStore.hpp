baa77dee519699955624dc9289dc28eaa087aebf
Todd S. Coffey <tscoffe@sandia.gov>
2006-10-09 20:15:44 +0000

    Bug 2521
    I reworked the DataStore class a bit.  I initially implemented this class as a
    list in LinearInterpolationBuffer, but then I realized a vector would be
    better.  Now you can use whichever you want because I provided two helper
    functions for converting between sequences of DataStore and t, x, xdot, and
    accuracy vectors.

--- packages/rythmos/src/Rythmos_DataStore.hpp
+++ packages/rythmos/src/Rythmos_DataStore.hpp
@@ -57,6 +57,9 @@ class DataStore : virtual public Teuchos::Describable
     /// Solution dot value of data at above time:
     Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > xdot;
 
+    /// Accuracy of x data.  This is the accuracy of interpolations
+    ScalarMag accuracy;
+
     /// Less than comparison for sorting:
     bool operator< (const DataStore<Scalar>& d1, const DataStore<Scalar>& d2) const
     { return( d1.time < d2.time ); }
@@ -73,30 +76,33 @@ class DataStore : virtual public Teuchos::Describable
 
 }
 
-// This is a helper function to convert vectors of t,x,xdot to a list of DataStore objects
-void VectorToDataStoreList(
-  std::list<Teuchos::RefCountPtr<DataStore<Scalar> > > *list_ds
-  ,const std::vector<ScalarMag> &time_vec
-  ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &x_vec
-  ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &xdot_vec) const;
+// This is a helper function to convert a sequence of DataStore objects to vectors of t,x,xdot,accuracy
+void DataStoreSeqToVector(
+      const std::sequence<Teuchos::RefCountPtr<DataStore<Scalar> > > &ds
+      ,std::vector<ScalarMag> *time_vec
+      ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *x_vec
+      ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *xdot_vec
+      ,std::vector<ScalarMag> *accuracy_vec) const
 
-// This is a helper function to convert a list of DataStore objects to vectors of t,x,xdot
-void DataStoreListToVector(
-  std::vector<ScalarMag> *time_vec
-  ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *x_vec
-  ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *xdot_vec
-  ,const std::list<Teuchos::RefCountPtr<DataStore<Scalar> > > &list_ds) const;
-
+// This is a helper function to convert vectors of t,x,xdot,accuracy to a sequence of DataStore objects
+void VectorToDataStoreSeq(
+      const std::vector<ScalarMag> &time_vec
+      ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &x_vec
+      ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &xdot_vec
+      ,const std::vector<ScalarMag> &accuracy_vec
+      ,std::sequence<Teuchos::RefCountPtr<DataStore<Scalar> > > *ds) const
 
 // DataStore definitions:
 template<class Scalar>
 DataStore<Scalar>::DataStore(ScalarMag &time_
   ,Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > &x_
-  ,Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > &xdot_)
+  ,Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > &xdot_
+  ,ScalarMag &accuracy_)
 {
   time = time_;
   x = x_;
   xdot = xdot_;
+  accuracy = accuracy_;
 }
 
 template<class Scalar>
@@ -118,47 +124,46 @@ void DataStore<Scalar>::describe(
 
 // DataStore Helper Function definitions:
 template<class Scalar>
-void VectorToDataStoreList(
-      std::list<Teuchos::RefCountPtr<DataStore<Scalar> > > *list_ds
-      ,const std::vector<ScalarMag> &time_vec
-      ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &x_vec
-      ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &xdot_vec) const
+void DataStoreSeqToVector(
+      const std::sequence<Teuchos::RefCountPtr<DataStore<Scalar> > > &ds
+      ,std::vector<ScalarMag> *time_vec
+      ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *x_vec
+      ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *xdot_vec
+      ,std::vector<ScalarMag> *accuracy_vec) const
 {
-  int N = time_vec.size();
-  int Nx = x_vec.size();
-  int Nxdot = xdot_vec.size();
-  if ((N != Nx) || (N != Nxdot))
+  std::sequence<DataStore<Scalar> >::iterator ds_it = ds.front();
+  for (; ds_it != ds.end() ; ds_it++)
   {
-    list_ds = NULL;
-    return;
-  }
-  list_ds->clear();
-  for (int i=0; i<N ; ++i)
-  {
-    list_ds->push_back(Teuchos::rcp(new DataStore(time_list[i],x_list[i],xdot_list[i])));
+    time_vec->push_back(ds_it->time);
+    x_vec->push_back(ds_it->x);
+    xdot_vec->push_back(ds_it->xdot);
+    accuracy_vec->push_back(ds_it->accuracy);
   }
 }
 
 template<class Scalar>
-void DataStoreListToVector(
-      std::vector<ScalarMag> *time_vec
-      ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *x_vec
-      ,std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > *xdot_vec
-      ,const std::list<Teuchos::RefCountPtr<DataStore<Scalar> > > &list_ds) const
+void VectorToDataStoreSeq(
+      const std::vector<ScalarMag> &time_vec
+      ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &x_vec
+      ,const std::vector<Teuchos::RefCountPtr<Thyra::VectorBase<Scalar> > > &xdot_vec
+      ,const std::vector<ScalarMag> &accuracy_vec
+      ,std::sequence<Teuchos::RefCountPtr<DataStore<Scalar> > > *ds) const
 {
-  int N = list_ds.size();
-  time_list->reserve(N); time_list->clear();
-  x_list->reserve(N);    x_list->clear();
-  xdot_list->reserve(N); xdot_list->clear();
-  std::list<DataStore<Scalar> >::iterator list_it = list_ds.begin();
-  for (; list_it != list_ds.end() ; list_it++)
+  int N = time_vec.size();
+  int Nx = x_vec.size();
+  int Nxdot = xdot_vec.size();
+  int Nacc = accuracy_vec.size();
+  if ( (N != Nx) || (N != Nxdot) || (N != Nacc) )
   {
-    time_list->push_back(list_it->time);
-    x_list->push_back(list_it->x);
-    x_dot_list->push_back(list_it->xdot);
+    ds = NULL;
+    return;
+  }
+  ds->clear();
+  for (int i=0; i<N ; ++i)
+  {
+    ds->insert(ds.end(),Teuchos::rcp(new DataStore(time_vec[i],x_vec[i],xdot_vec[i],accuracy_vec[i])));
   }
 }
-
 
 } // namespace Rythmos
 
