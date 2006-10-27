e658235dff3ff9aa9085b6bf92b694451bb41187
Stefan Chakerian <schake>
2006-10-27 00:48:15 +0000

    Updated to use 'exact' framework Updated to use checksums of output filenames

--- packages/zoltan/test/bin/exp.zoltan.pl
+++ packages/zoltan/test/bin/exp.zoltan.pl
@@ -1,51 +1,89 @@
 #!/usr/bin/perl
 #
 
-##
-## BEGIN COPY HERE
-##
+# environment variables supported:
+# EXACT_DRIVER - mpirun command.  For solaris, it defaults to
+#   "/Net/local/mpi/build/solaris/ch_p4/bin/mpirun -np"
+# ZOLTAN_ARCH - architechture of the machine.  Choices are generally:
+#   generic, solaris, sun, linux, qed.  Defaults to "solaris"
+# ZOLTAN_ZDRIVE - location of zdrive, defaults to
+#   "../../Obj_$ZOLTAN_ARCH/zdrive" 
+
 use Sys::Hostname;
 my $host = hostname();
-use Cwd;
 
+use Data::Dumper;
 
-#
-# See if the first argument is '-I' which specifies an include path
-# for the TestLib module
-#
-$ctr=0;
-if ($ARGV[0] eq "-I") {
-   unshift @INC, $ARGV[1];
-   $ENV{'PATH'} = $ENV{'PATH'} . ":.:" . $ARGV[1];
-   $ctr = 2;
-   }
-#
-# See if the TESTLIBDIR environmental variable is set, which provides
-# an include path for the TestLib module
-#
-$pwd = `pwd`;
-chomp($pwd);
-if ($ENV{TESTLIBDIR} ne "") {
-   $tmp = $ENV{TESTLIBDIR};
-   @words = split /[ ]+/, $tmp, $#tmp;
-   foreach $word (@words) {
-     unshift @INC, $word;
-     }
-   }
-require TestLib;
-##
-## END COPY HERE
-##
+# defaults, can be overridden in experiment.xml
+my $zarch = "sun";
+my $zdrive = "../../Obj_$zarch/zdrive";
+my $zinp_def = "zdrive.inp";	# static, used below for checks
+my $zinp = $zinp_def;		# zdrive.inp used
+my $mpi= "/Net/local/mpi/build/solaris/ch_p4/bin/mpirun -np";
 
+# values to track and print by parse() routine
 my (%bal, %cutl, %cutn);
-my $xml;
 my $iters;
+my $ckok = '';	# output string for pass/fail of checksums
 
-#
-# Statistics collected by the analyzeData call-back routine
-#
+# use specified driver if environment var is set.  Set up defaults here
+if ($ENV{EXACT_DRIVER}) {
+  $mpi = $ENV{EXACT_DRIVER};
+} elsif ($host eq "qed.sandia.gov") {
+  $mpi = "mpiexec -n";
+} else {
+  $mpi = "/Net/local/mpi/build/solaris/ch_p4/bin/mpirun -np";
+}
 
-sub analyzeData {
+# contents of $infile, after parseinfile()
+my %args;
+
+# checksums of all the answers files
+my %cksum;
+
+# list of output filenames, to be checksummed
+my @checkfiles=();
+
+# mesh/model name
+my $name;
+
+my $delimiter='"""';
+my $cmdline;	# constructed later
+
+my $outfile = $ARGV[1];
+my $logfile = $ARGV[2];
+
+
+sub parseinfile {
+  # Process infile (variables, levels, etc)
+  # argument: opened file descriptor
+
+  my $infile = @_[0];
+
+  while (<$infile>) {
+    my ($key,$a) = split /\t/, $_, 2;
+    chomp $a;
+    if ($a eq $delimiter) {
+      $a = '';
+      while (<$infile>) {
+	if (m/$delimiter/) {		# single line with delimiter
+	  chomp $a;
+	  break;
+	} else {
+	  $a .= $_;
+	}
+      }
+    }
+    $args{$key} = $a;
+  }
+}
+
+sub parseoutfile {
+  # parse zoltan output file
+  # arguments: INFD LOGFD
+  # INFD is a pipe to running command
+  # LOGFD is the logfile, to dump output into
+
   $iters = 0;
 
   ($bal{MAX},$bal{MIN},$bal{AVG}) = ("ERROR","ERROR","ERROR");
@@ -53,10 +91,11 @@ sub analyzeData {
   ($cutn{MAX},$cutn{MIN},$cutn{AVG}) = ("ERROR","ERROR","ERROR");
   $timeavg = -1;
 
-  my $filename = @_[0];
-  open ZOUT, "$filename" or die "can't open zdrive output $filename";
+  my $infd = @_[0];
+  my $logfd = @_[1];
 
-  while (<ZOUT>) {
+  while (<$infd>) {
+    print $logfd $_;
     if (/.*STATS Runs ([0-9]+)/) {
       my $vname="";
       $iters = "$1";
@@ -71,7 +110,6 @@ sub analyzeData {
       $timeavg = $1 + 0;
     }
   }
-  close ZOUT;
 
 #  print "iters: $iters\n";
 #  print "bal: $bal{MAX} $bal{MIN} $bal{AVG}\n";
@@ -79,168 +117,143 @@ sub analyzeData {
 #  print "cutn: $cutn{MAX} $cutn{MIN} $cutn{AVG}\n";
 #  print "time: $timeavg\n";
 
-  if ($timeavg == -1) {
-     @_[1]  = "Fail";
-  }
 }
 
-#
-# Default description value
-#
-$description="  <Description>ERROR</Description>\n";
 
-#
-# A callback that is used to print measurement information
-#
+sub printvars {
+  my $filename = @_[0];
+  open FD, ">$filename" or die "printout open failed on $filename";
 
-sub printMeasurements {
-  print "  <Trial id=\"0\" seed=\"0\" data=\"?\">\n";
-  print "     <Value name=\"BalMax\" type=\"numeric/double\">$bal{MAX}</Value>\n";
-  print "     <Value name=\"BalMin\" type=\"numeric/double\">$bal{MIN}</Value>\n";
-  print "     <Value name=\"BalAvg\" type=\"numeric/double\">$bal{AVG}</Value>\n";
-  print "     <Value name=\"CutlMax\" type=\"numeric/double\">$cutl{MAX}</Value>\n";
-  print "     <Value name=\"CutlMin\" type=\"numeric/double\">$cutl{MIN}</Value>\n";
-  print "     <Value name=\"CutlAvg\" type=\"numeric/double\">$cutl{AVG}</Value>\n";
-  print "     <Value name=\"CutnMax\" type=\"numeric/double\">$cutn{MAX}</Value>\n";
-  print "     <Value name=\"CutnMin\" type=\"numeric/double\">$cutn{MIN}</Value>\n";
-  print "     <Value name=\"CutnAvg\" type=\"numeric/double\">$cutn{AVG}</Value>\n";
-  print "     <Value name=\"SolverTime\" type=\"numeric/double\">$timeavg</Value>\n";
-  print "     <Value name=\"Iterations\" type=\"numeric/int\">$iters</Value>\n";
-
-  print "  </Trial>\n";
-
+  print FD "BalMax	numeric/double	$bal{MAX}\n";
+  print FD "BalMin	numeric/double	$bal{MIN}\n";
+  print FD "BalAvg	numeric/double	$bal{AVG}\n";
+  print FD "CutlMax	numeric/double	$cutl{MAX}\n";
+  print FD "CutlMin	numeric/double	$cutl{MIN}\n";
+  print FD "CutlAvg	numeric/double	$cutl{AVG}\n";
+  print FD "CutnMax	numeric/double	$cutn{MAX}\n";
+  print FD "CutnMin	numeric/double	$cutn{MIN}\n";
+  print FD "CutnAvg	numeric/double	$cutn{AVG}\n";
+  print FD "SolverTime	numeric/double	$timeavg\n";
+  print FD "Iterations	numeric/int	$iters\n";
+  print FD $ckok;
+  close FD;
 }
 
 ##
 ## MAIN ROUTINE
 ##
 if (! @ARGV) {
+   print "$0 - Zoltan experiment driver\n";
    print "\n";
-   print "$0 - Zoltan big experiment driver\n";
-   print "\n";
-   print "usage:\n";
-   print "\n";
-   print "   $0 [options] <expname> <testname> <factor-id> <factor-value> [...]\n";
-   print "\n";
-   print "options:\n";
-   print "\n";
-   print "   --debug\t\tPrint debugging information.\n";
-   print "\n";
-   print "notes:\n";
-   print "\n";
-   print "   This script is launched by 'runexp' to execute and process a\n";
-   print "   single test from an experiment file.  The output of this test\n";
-   print "   is stored in the file <expname>-<testname>.out, which can be\n";
-   print "   reviewed after the test is performed.  This script generates an\n";
-   print "   XML summary that is included in the <expname>.results.xml file\n";
-   print "   by 'runexp'.\n";
-   print "\n";
-   print "   This script is not intended for interactive use.  Consequently,\n";
-   print "   the processing of the command-line options is somewhat fragile.\n";
-   print "   For example, it depends on the order in which the options are\n";
-   print "   processed in this script!\n";
-   print "\n";
-   print "   This script assumes that the first factor is the test information,\n";
-   print "   and the remaining factors specify command-line options.\n";
+   print "   This script is launched by 'exact' to execute and process\n";
+   print "   tests from an experiment file.\n";
    exit;
 }
-#
-# Process command line arguments
-#
-$debug = 0;
-$testoptions = "";
-if ($ARGV[$ctr] eq "--debug") {
-   $ctr += 1;
-   $debug = 1;
+
+open LOGFILE, ">$logfile" or die "can't open $logfile";
+open STDERR, ">&LOGFILE" or die "can't dup stderr to logfile";
+
+select STDERR;	# all output is now going to logfile, as STDERR
+# $| = 1;		# unbuffered writes
+
+open INFILE, "<$ARGV[0]" or die "can't open $infile";
+parseinfile(INFILE);		# populate %args
+close INFILE;
+
+if ($args{_exact_debug}) {
+  print "DEBUG INFO ON\n";
+  print "Input file: ", Dumper(\%args);
 }
-
-if ($debug == 1) {
-   print "<debuginfo>\n"
-}
-
-$description = TestLib::generate_description($ctr,@ARGV);
-$expname = $ARGV[$ctr];
-$ctr +=1;
-$testname = $ARGV[$ctr];
-$ctr +=1;
-$ntrials = $ARGV[$ctr];
-$ctr+=3;
-while ($ctr <= $#ARGV) {
-  $testoptions .= $ARGV[$ctr] . " ";
-  $ctr+=3;
-}
-
-
-#
-# Process experiment tag/value pairs
-#
-# This is a convention that is not explicitly supported by the 
-# 'runexp' utility, but this is a convenient way for
-# structuring experiment testing.
-#
-$solnvalue = "unknown";
-$tolerance = 1e-12;
-$files = "";
-%testparams = ();
-$auxoptions = TestLib::process_testoptions($testoptions, \%testparams);
-
-if (defined($testparams{"_optimum"})) {
-   $solnvalue = $testparams{"_optimum"};
-} elsif (defined($testparams{"_value"})) {
-   $solnvalue = $testparams{"_value"};
-}
-
-$iters = 10; # default
-foreach $word (TestLib::get_tokens($testparams{"_iterations"})) {
-  $iters = $word;
-}
-
-foreach $word (TestLib::get_tokens($testparams{"_inp"})) {
-  $files = $files . $word . " ";
-}
-
-my $solver;
 
 # can configure a short script to create a custom zdrive here, if desired
 # but it uses the commandline to specify the name
-if ($host eq "qed.sandia.gov") {
-  $solver = "mpiexec -n $testparams{_np} ../../Obj_qed/zdrive";
-} else {
-  $solver = "/Net/local/mpi/build/solaris/ch_p4/bin/mpirun -np $testparams{_np} ../../Obj_solaris/zdrive";
-}
 
-# $solver = "/bin/cat test.out"; #debug
-
-if ($debug == 1) {
-   print "options: $testoptions\n";
-   print "exp:       $expname\n";
-   print "value:     $solnvalue\n";
-   print "input:     $files\n";
-   print "numproc:   $testparams{_np}\n";
-   print "iters:     $testparams{_iterations}\n";
-}
-   #
-   # Setup command line
-   #
-   $cmdline = "$solver ";
-   @options = split(/ /,$auxoptions,$#auxoptions);
-   while (@options) {
-     local $tmp = shift @options;
-     if ($tmp ne "") {
-        $cmdline .= " --" . $tmp;
-     }
-   }
-   $cmdline .= " " . $files;
-   print "CMDLINE: $cmdline\n" if ($debug == 1);
-
-# open Z
-# open ZINP ">zdrive.inp"
-
-if ($debug == 1) {
-   print "</debuginfo>\n"
-}
 
 #
-# Launch the test with the TestLib driver.
+# Setup command line
 #
-TestLib::run_experiments(\*STDOUT, $expname, $testname, $cmdline, $ntrials, \&analyzeData, \&printMeasurements, $auxoptions, \%testparams);
+
+if (! defined $args{np}) {
+  die "number of processors undefined";
+}
+if (defined $args{zdrive}) {
+  $zdrive = $args{zdrive};
+}
+if (defined $args{zinp}) {
+  $zinp = $args{zinp};
+}
+
+$cmdline = "$mpi $args{np} $zdrive $zinp";
+print "CMDLINE: $cmdline\n" if ($args{_exact_debug});
+
+open ZIN, $zinp or die "couldn't open $zinp";
+my $temprs = $/;
+undef $/;		# turn off input record separator
+my $ztext = <ZIN>;	# slurp whole zdrive.inp
+close ZIN;
+$/ = $temprs;
+
+unless ($ztext =~ /text output *= *0+\b/) {
+  if ($zinp eq $zinp_def) {
+    print "Running with default $zinp ... skipping diff check\n";
+    break;
+  }
+
+  # construct list of output files
+  my $l = length (sprintf "%d", $args{np});	# length of nproc
+  my $ll = sprintf "%0${l}d", 0;	# 5 becomes "05" when 9 < np < 100
+  if ($ztext =~ /File Name\s*=\s*(\w+)/) {
+    $name = $1;
+  } else {
+    print "Couldn't determine file name ... skipping diff check\n";
+    break;
+  }
+  my $fullname = "$name.out.$args{np}"; # needs the node number at end
+  for (my $i = 0; $i < $args{np}; $i++) {
+    push @checkfiles,"$fullname.$ll";
+    $ll++;				# string increment
+  }
+
+  # also need to read in checksum file
+  if (!defined (open ZIN, "checksums") ) {
+    print "Couldn't open checksums ... skipping diff check\n";
+    @checkfiles = ();
+  } else {
+    while (<ZIN>) {
+      /(\d+)\s+(\d+)\s+(\S+)/;
+      $cksum{"$3"} = [$1, $2];
+    }
+    close ZIN;
+  }
+}
+
+# Launch the test, finally, dumping output to LOGFILE
+
+open ZOLTAN, "$cmdline |" or die "Can't run $cmdline";
+parseoutfile(ZOLTAN, LOGFILE);
+close ZOLTAN;
+
+# check output file checksums, if @checkfiles non-empty
+my $pass = 0;
+my $fail = 0;
+foreach $f (@checkfiles) {
+  my $sum = `cksum $f`;
+  $sum =~ /(\d+)\s+(\d+)\s+(\S+)/;
+  my $sum0 = $1;
+  my $sum1 = $2;
+  my $partname = $zinp;
+  my $fullname = $f;
+  $partname =~ s/^${zinp_def}.//;
+  $fullname =~ s/^($name)\.out\./$1.$partname./;
+  print "CRC: $fullname ?= $f\n" if ($args{_exact_debug});
+  if ($cksum{$fullname}[0] == $sum0 && $cksum{$fullname}[1] == $sum1 ) {
+    $pass++;
+  } else {
+    $fail++;
+  }
+}
+$ckok = "OutputPass\tnumeric/int\t$pass\nOutputFail\tnumeric/int\t$fail\n";
+
+printvars($outfile);
+
+
