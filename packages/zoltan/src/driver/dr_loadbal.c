e0a285a8b898ac75925ea1fe3f8a2f45b5c931a4
Erik G. Boman <egboman@sandia.gov>
2007-01-16 23:55:08 +0000

    Changed meaning of test cases 7-9 for fixed vertices; I'll need test case 9 for constrained fine-grain experiments. No answer files are affected since we have no answers yet for fixed vertex tests.

--- packages/zoltan/src/driver/dr_loadbal.c
+++ packages/zoltan/src/driver/dr_loadbal.c
@@ -2204,26 +2204,26 @@ int proc, nprocs;
       }
       break;
   case 7:
-      /* Fix one object on each proc < Num_Global_Parts. */
-      if (proc < Num_Global_Parts)
-        for (i = 0; i < MIN(1, mesh->num_elems); i++) {
-          mesh->elements[i].fixed_part = proc;
-        }
+      /* Fix one object on each proc. */
+      for (i = 0; i < MIN(1, mesh->num_elems); i++) {
+        mesh->elements[i].fixed_part = (proc%Num_Global_Parts);
+      }
       break;
   case 8:
-      /* Fix two objects on half the procs < Num_Global_Parts. */
-      if (proc%2 && proc < Num_Global_Parts){
-        for (i = 0; i < MIN(2, mesh->num_elems); i++) {
-          mesh->elements[i].fixed_part = proc;
+      /* Fix half the objects on half the procs, 25% overall */
+      if (proc&1){
+        for (i = 0; i < mesh->num_elems/2; i++) {
+          mesh->elements[i].fixed_part = (proc%Num_Global_Parts);
         }
       }
       break;
   case 9:
-      /* Fix myproc objects on each proc < Num_Global_Parts. */
-      if (proc < Num_Global_Parts)
-        for (i = 0; i < MIN(proc, mesh->num_elems); i++) {
-          mesh->elements[i].fixed_part = proc;
+      /* Fix first two objects on proc 0 only. */
+      if (proc == 0){
+        for (i = 0; i < MIN(2, mesh->num_elems); i++) {
+          mesh->elements[i].fixed_part = i;
         }
+      }
       break;
   case 10:
       /* Fix 0% of objects */
