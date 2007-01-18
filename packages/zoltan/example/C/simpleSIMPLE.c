a496ff968efb408aa94812d801265e322bf0f9cc
Lee Ann Riesen <lafisk@sandia.gov>
2007-01-18 20:26:01 +0000

    make this example more similar to the one in the tutorial

--- packages/zoltan/example/C/simpleSIMPLE.c
+++ packages/zoltan/example/C/simpleSIMPLE.c
@@ -1,26 +1,54 @@
-/***************************************************************
-** The most basic example of using Zoltan. 
-** The objects have only a global ID and a weight. 
-** The partitioning method is SIMPLE.
-***************************************************************/
-
 #include <mpi.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include "zoltan.h"
-#include "simpleGraph.h"
-#include "simpleQueries.h"
+
+static int numObjects = 36;
+static int numProcs, myRank, myNumObj;
+static int myGlobalIDs[36];
+
+static float objWeight(int globalID)
+{
+float w;
+  if (globalID % numProcs == 0){
+    w = 3;  /* simulate an initial imbalance */
+  else
+    w = (globalID % 3 + 1);
+
+  return w;
+}
+static int get_number_of_objects(void *data, int *ierr)
+{
+  *ierr = ZOLTAN_OK;
+  return myNumObj;
+}
+static void get_object_list(void *data, int sizeGID, int sizeLID,
+            ZOLTAN_ID_PTR globalID, ZOLTAN_ID_PTR localID,
+                  int wgt_dim, float *obj_wgts, int *ierr)
+{
+int i;
+  *ierr = ZOLTAN_OK;
+  for (i=0; i<myNumObj; i++){
+    globalID[i] = myGlobalIDs[i];
+    if (obj_wgts){
+      obj_wgts[i] = objWeight(myGlobalIDs[i]);
+    }
+  }
+}
 
 int main(int argc, char *argv[])
 {
-  int rc, i, ngids, nextIdx;
+  int rc, i;
   float ver;
   struct Zoltan_Struct *zz;
   int changes, numGidEntries, numLidEntries, numImport, numExport;
-  ZOLTAN_ID_PTR importGlobalGids, importLocalGids, exportGlobalGids, exportLocalGids; 
+  ZOLTAN_ID_PTR importGlobalGids, importLocalGids;
+  ZOLTAN_ID_PTR exportGlobalGids, exportLocalGids; 
   int *importProcs, *importToPart, *exportProcs, *exportToPart;
-  float *wgt_list;
-  int *gid_flags, *gid_list, *lid_list;
+  int ngids;
+  int *gid_flags, *gid_list;
+  float wgt;
+  int j, nextIdx;
 
   /******************************************************************
   ** Initialize MPI and Zoltan
@@ -33,39 +61,42 @@ int main(int argc, char *argv[])
   rc = Zoltan_Initialize(argc, argv, &ver);
 
   if (rc != ZOLTAN_OK){
-    printf("sorry...\n");
+    printf("Error initializing Zoltan\n");
     MPI_Finalize();
     exit(0);
   }
 
   /******************************************************************
+  ** Create a simple initial partitioning for this example
+  ******************************************************************/
+
+  for (i=0, myNumObj=0; i<numObjects; i++){
+    if (i % numProcs == myRank){
+      myGlobalIDs[myNumObj++] = i+1;
+    }
+  }
+
+  /******************************************************************
   ** Create a Zoltan library structure for this instance of load
-  ** balancing.  Set the parameters and query functions that will
-  ** govern the library's calculation.  See the Zoltan User's
-  ** Guide for the definition of these and many other parameters.
+  ** balancing.  Set the parameters and query functions.
   ******************************************************************/
 
   zz = Zoltan_Create(MPI_COMM_WORLD);
 
   /* General parameters */
 
-  Zoltan_Set_Param(zz, "LB_METHOD", "SIMPLE");
-  Zoltan_Set_Param(zz, "NUM_GID_ENTRIES", "1"); 
-  Zoltan_Set_Param(zz, "NUM_LID_ENTRIES", "1");
-  Zoltan_Set_Param(zz, "OBJ_WEIGHT_DIM", "1");
-  Zoltan_Set_Param(zz, "RETURN_LISTS", "ALL");
+  Zoltan_Set_Param(zz, "LB_METHOD", "SIMPLE");  /* Zoltan method: "SIMPLE" */
+  Zoltan_Set_Param(zz, "NUM_GID_ENTRIES", "1"); /* global ID is 1 integer */
+  Zoltan_Set_Param(zz, "NUM_LID_ENTRIES", "0"); /* no local IDs */
+  Zoltan_Set_Param(zz, "OBJ_WEIGHT_DIM", "1"); /* weights are 1 float */
 
-  /* Query functions - defined in simpleQueries.h, to return
-   * information about objects defined in simpleGraph.h      */
+  /* Query functions */
 
   Zoltan_Set_Num_Obj_Fn(zz, get_number_of_objects, NULL);
   Zoltan_Set_Obj_List_Fn(zz, get_object_list, NULL);
 
   /******************************************************************
-  ** Zoltan can now partition the vertices in the simple mesh.
-  ** In this simple example, we assume the number of partitions is
-  ** equal to the number of processes.  Process rank 0 will own
-  ** partition 0, process rank 1 will own partition 1, and so on.
+  ** Call Zoltan to partition the objects.
   ******************************************************************/
 
   rc = Zoltan_LB_Partition(zz, /* input (all remaining fields are output) */
@@ -84,16 +115,11 @@ int main(int argc, char *argv[])
         &exportToPart);  /* Partition to which each vertex will belong */
 
   if (rc != ZOLTAN_OK){
-    printf("sorry...\n");
+    printf("Error in Zoltan library\n");
     MPI_Finalize();
     Zoltan_Destroy(&zz);
     exit(0);
   }
-
-  /******************************************************************
-  ** In a real application, you would rebalance the problem now by
-  ** sending the objects to their new partitions.
-  ******************************************************************/
 
   /******************************************************************
   ** Visualize the new partitioning.
@@ -102,39 +128,61 @@ int main(int argc, char *argv[])
   ******************************************************************/
 
   ngids = get_number_of_objects(NULL, &rc);
-  gid_flags = (int *)calloc(sizeof(int) , simpleNumVertices);
+  gid_flags = (int *)calloc(sizeof(int) , numObjects);
   gid_list = (int *)malloc(sizeof(int) * ngids);
-  lid_list = (int *)malloc(sizeof(int) * ngids);
-  wgt_list = (float *)malloc(sizeof(float) * simpleNumVertices);
   get_object_list(NULL, 1, 1,
-                  (ZOLTAN_ID_PTR)gid_list, (ZOLTAN_ID_PTR)lid_list,
-                  1, wgt_list, &rc);
+                  (ZOLTAN_ID_PTR)gid_list, NULL, 1, NULL, &rc);
 
-  draw_partitions("initial distribution", ngids, gid_list, 1, wgt_list, 0);
-
+  for (i=0; i <numProcs; i++){
+    if (i == myRank){
+      if (!i) printf("\nInitial Partitioning:\n========================\n");
+      wgt = 0.0;
+      printf("%d: ",i);
+      for (j=0; j<ngids; j++){
+        if (j && (j % 20 == 0)) printf("\n   ");
+        printf("%d (%2.0f) ",gid_list[j],objWeight(gid_list[j]));
+        wgt += objWeight(gid_list[j]);
+      }
+      printf("   weight: %f\n",wgt);
+      fflush(stdout);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+  
   for (i=0; i<ngids; i++){
     gid_flags[gid_list[i]-1] = 1;    /* my original vertices */
   }
-  for (i=0; i<numImport; i++){
+  for (i=0; i<numImport; i++){ 
     gid_flags[importGlobalGids[i] - 1] = 1;  /* my imports */
   }
   for (i=0; i<numExport; i++){
     gid_flags[exportGlobalGids[i] - 1] = 0;  /* my exports */
   }
   nextIdx = 0;
-  for (i=0; i<simpleNumVertices; i++){
-    if (gid_flags[i]){
-      gid_flags[nextIdx] = i+1; /* my new GID list */
-      wgt_list[nextIdx] = simpleNumEdges[i];
+  for (i=0; i<numObjects; i++){
+    if (gid_flags[i]){   
+      gid_flags[nextIdx] = i+1; /* my new GID list */ 
       nextIdx++;
     }
   }
-  draw_partitions("new partitioning", nextIdx, gid_flags, 1, wgt_list, 0);
-
+  for (i=0; i <numProcs; i++){
+    if (i == myRank){
+      if (!i) printf("\nNew Partitioning:\n=========================\n");
+      printf("%d: ",i);
+      wgt=0.0;
+      for (j=0; j<nextIdx; j++){
+        if (j && (j % 20 == 0)) printf("\n   ");
+        printf("%d (%2.0f) ",gid_flags[j],objWeight(gid_flags[j]));
+        wgt += objWeight(gid_flags[j]);
+      }
+      printf("   weight: %f\n",wgt);
+      fflush(stdout);
+    }
+    MPI_Barrier(MPI_COMM_WORLD);
+  }
+  
   if (gid_flags) free(gid_flags);
   if (gid_list) free(gid_list);
-  if (lid_list) free(lid_list);
-  if (wgt_list) free(wgt_list);
 
   /******************************************************************
   ** Free the arrays allocated by Zoltan_LB_Partition, and free
@@ -147,10 +195,6 @@ int main(int argc, char *argv[])
                       &exportProcs, &exportToPart);
 
   Zoltan_Destroy(&zz);
-
-  /**********************
-  ** all done ***********
-  **********************/
 
   MPI_Finalize();
 
