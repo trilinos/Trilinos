da7e8cf1243fe9c8b090b48197dfb323fe22867d
Roscoe A. Bartlett <rabartl@sandia.gov>
2007-02-13 21:05:51 +0000

    Added a comment on adding an include for the Teuchos_TestForException.hpp file and did a spell check.

--- packages/teuchos/refactoring/replace-assert-with-TEST_FOR_EXCEPT.sh
+++ packages/teuchos/refactoring/replace-assert-with-TEST_FOR_EXCEPT.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# This script will replace all occurances of:
+# This script will replace all occurrences of:
 #
 #    assert(anything)
 #
@@ -8,7 +8,7 @@
 #
 #    TEST_FOR_EXCEPT( !( anything ) ) 
 #
-# in the input file $1 in a faily safe way.
+# in the input file $1 in a fairly safe way.
 #
 # This script is used as:
 #
@@ -20,6 +20,33 @@
 # something like:
 #
 #    find . -name "*pp" -exec $ABS_DIR/replace-assert-with-TEST_FOR_EXCEPT.sh {} \;
+#
+#
+# WARNING! This script does not work correctly for multi-line statements like:
+#
+#    assert(something); assert(anotherThing);
+#
+# as it will make this:
+#
+#    TEST_FOR_EXCEPT( !( something); assert(anotherThing ) );
+#
+# which is wrong wrong wrong.  Always, look over you code to make sure the right
+# thing happened (or don't put multiple statements on the same line).
+#
+# WARNING! This script will not replace multi-line assert statements like:
+#
+#    assert(
+#      something
+#      );
+#
+# you must replace these manually!
+#
+# Also, don't forget to put in an:
+#
+#    #include "Teuchos_TestForException.hpp"
+#
+# line in some header file that all of your code will include at least
+# indirectly.
 #
 
 sed -i "s/\([^a-zA-Z0-9_]\)assert(\(.\+\))/\1TEST_FOR_EXCEPT( \!( \2 ) )/g" $1
