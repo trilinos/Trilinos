ff05a3f260523ce003fdae7cbd7c0e70a2acbf67
David M. Gay <dmgay@sandia.gov>
2007-05-16 22:41:09 +0000

    Tweak to default RAD_Const_Warn so it will print a line to stderr the first 10 times it is called.

--- packages/sacado/src/RAD_Const_Warn.c
+++ packages/sacado/src/RAD_Const_Warn.c
@@ -1,10 +1,23 @@
 #include <stdio.h>
 
-int RAD_Const_Warn_verbose = 0;
+int RAD_Const_Warn_warned = 0, RAD_Const_Warn_warnlim = 10;
+
+ typedef struct
+ADvari_head {
+	void *var;	/* address of the offending variable */
+	struct ADvari_head *next;
+	int gcgen;
+	int opno;
+	/* Double Val; */
+	/* mutable Double aval; */
+	} ADvari_head;
 
  int
 RAD_Const_Warn(void *v)
 {
-	if (RAD_Const_Warn_verbose)
-		printf("RAD_Const_Warn(%lx)\n", (unsigned long)v);
+	ADvari_head *V = (ADvari_head*)v;
+
+	if (++RAD_Const_Warn_warned <= RAD_Const_Warn_warnlim)
+		fprintf(stderr, "RAD_Const_Warn var #%lx had gcgen = %d, opno = %d\n",
+			(unsigned long)V->var, V->gcgen, -1 - V->opno);
 	}
