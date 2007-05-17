2f88a6c423535b839757dca2e43da9aea99d11d9
David M. Gay <dmgay@sandia.gov>
2007-05-17 15:36:24 +0000

    Inserted copyright comments.

--- packages/sacado/src/uninit.c
+++ packages/sacado/src/uninit.c
@@ -1,8 +1,32 @@
-/* uninit.c is part of the libf77 bundle in f2c available at 
+/* uninit.c is part of the libf2c source (libf2c.zip) in f2c available at
  * http://www.netlib.org/f2c/.  It is used by Rad to fill memory locations
  * with NaN values so that uninitialized accesses of those memory locations
- * will throw IEEE exceptions.  
+ * will throw IEEE exceptions.  The libf2c source includes a Notice file
+ * giving copyright and permission to use.  The contents of this file
+ * appear immediately below.
  */
+/****************************************************************
+Copyright 1990 - 1997 by AT&T, Lucent Technologies and Bellcore.
+
+Permission to use, copy, modify, and distribute this software
+and its documentation for any purpose and without fee is hereby
+granted, provided that the above copyright notice appear in all
+copies and that both that the copyright notice and this
+permission notice and warranty disclaimer appear in supporting
+documentation, and that the names of AT&T, Bell Laboratories,
+Lucent or Bellcore or any of their entities not be used in
+advertising or publicity pertaining to distribution of the
+software without specific, written prior permission.
+
+AT&T, Lucent and Bellcore disclaim all warranties with regard to
+this software, including all implied warranties of
+merchantability and fitness.  In no event shall AT&T, Lucent or
+Bellcore be liable for any special, indirect or consequential
+damages or any damages whatsoever resulting from loss of use,
+data or profits, whether in an action of contract, negligence or
+other tortious action, arising out of or in connection with the
+use or performance of this software.
+****************************************************************/
 
 #include <stdio.h>
 #include <string.h>
