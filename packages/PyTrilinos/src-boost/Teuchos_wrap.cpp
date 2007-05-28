a614631ab907a691d5b6129061fd49fae58f0747
William F. Spotz <wfspotz@sandia.gov>
2007-05-28 17:28:11 +0000

    Added first cut at Teuchos::Time class

--- packages/PyTrilinos/src-boost/Teuchos_wrap.cpp
+++ packages/PyTrilinos/src-boost/Teuchos_wrap.cpp
@@ -32,10 +32,24 @@ using namespace boost::python;
 
 // Teuchos initialization
 #include "Teuchos_Version.hpp"
+#include "Teuchos_Time.hpp"
 
 // Define the Teuchos python module
 BOOST_PYTHON_MODULE(_Teuchos)
 {
   // Teuchos version support
   def("Teuchos_Version", Teuchos::Teuchos_Version);
+
+  // Teuchos Time support
+  class_<Teuchos::Time>("Time", init<std::string, optional<bool> >())
+    .def("start", &Teuchos::Time::start)
+    .def("stop", &Teuchos::Time::stop)
+    .def("totalElapsedTime", &Teuchos::Time::totalElapsedTime)
+    .def("reset", &Teuchos::Time::reset)
+    .def("isRunning", &Teuchos::Time::isRunning)
+    //.def("name", &Teuchos::Time::name, return_value_policy<manage_new_object>())
+    .def("incrementNumCalls", &Teuchos::Time::incrementNumCalls)
+    .def("numCalls", &Teuchos::Time::numCalls)
+    .def("wallTime", &Teuchos::Time::wallTime)
+    ;
 }
