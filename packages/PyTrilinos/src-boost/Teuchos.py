8d4a79201232e20ef4d81054b0618f16b82109e5
Michael N. Phenow <mnpheno>
2007-09-12 01:30:14 +0000

    Merging back changes from the release 8.0 branch.  Not all of the merges have been done yet for the software.sandia.gov/trilinos --> trilinos.sandia.gov changes, but this should take care of the test harness changes.

--- packages/PyTrilinos/src-boost/Teuchos.py
+++ packages/PyTrilinos/src-boost/Teuchos.py
@@ -31,7 +31,7 @@ import sys
 PyTrilinos.Teuchos is the python interface to Trilinos package
 Teuchos:
 
-    http://software.sandia.gov/trilinos/packages/teuchos
+    http://trilinos.sandia.gov/packages/teuchos
 
 The purpose of Teuchos is to provide a number of utilities often
 needed by numerical applications, but that are not necessarily
