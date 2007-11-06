36839c346c9c5c066aa9dc56b753af94e9824eb3
James M. Willenbring <jmwille@sandia.gov>
2007-11-06 21:05:53 +0000

    Updated some obsolete parts of new_package.  Also added some #np# comments for the parts of new_package related to Makefile.export.

--- packages/new_package/ConvertToYourpackagename.sh
+++ packages/new_package/ConvertToYourpackagename.sh
@@ -17,10 +17,6 @@ mv src/Newp_Hello.cpp src/Yourpackagename_Hello.cpp
 mv src/Newp_Hello.h src/Yourpackagename_Hello.h 
 mv src/Newp_Jambo.cpp src/Yourpackagename_Jambo.cpp
 mv src/Newp_Jambo.h src/Yourpackagename_Jambo.h
-mv test/scripts/daily/mpi/new_packageTestAllMpi test/scripts/daily/mpi/YourpackagenameTestAllMpi
-mv test/scripts/daily/serial/new_packageTestAllSerial test/scripts/daily/serial/YourpackagenameTestAllSerial
-mv python/src/New_Package.i python/src/Yourpackagename.i
-mv python/test/testNewPackage.py python/test/testYourpackagename.py
 #
 #
 #
