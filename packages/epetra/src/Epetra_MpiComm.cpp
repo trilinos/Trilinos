1d2a8ed1c48a708217e97ca040f6f7bfbe39e123
William F. Spotz <wfspotz@sandia.gov>
2007-12-04 23:20:14 +0000

    SUMMARY: Changed incorrect MPI_INT arguments to correct MPI_LONG          arguments.
    BUGS: 3694
    
    CHECKLIST:
    
    1) Did you add any source files? no
    1.c) Did you do a new bootstrap? no
    
    2) Did you update or change the build system? no
    
    3) Did you rerun the test suite on at least the packages that you
    changed? no - bug manifest itself in PyTrilinos, and those
    tests were run. I am not aware that Epetra tests the long* versions
    of the MpiComm reduction operators.
    
    COMMENTS:
    
    Two methods, MpiComm::MinAll(long*,long*,int) and
    MpiComm::SumAll(long*,long*,int) had bugs: they called MPI_Allreduce
    with MPI_INT rather than MPI_LONG as arguments. This change fixes
    that. Interestingly, this never posed a problem until MPI
    PyTrilinos tests were run on a 64-bit architecture.

--- packages/epetra/src/Epetra_MpiComm.cpp
+++ packages/epetra/src/Epetra_MpiComm.cpp
@@ -105,7 +105,7 @@ int Epetra_MpiComm::SumAll(int * PartialSums, int * GlobalSums, int Count) const
 int Epetra_MpiComm::SumAll(long * PartialSums, long * GlobalSums, int Count) const {
   EPETRA_CHK_ERR(CheckInput(PartialSums,Count));
   EPETRA_CHK_ERR(CheckInput(GlobalSums,Count));
-  EPETRA_CHK_ERR(MPI_Allreduce(PartialSums, GlobalSums, Count, MPI_INT, MPI_SUM, MpiCommData_->Comm_));
+  EPETRA_CHK_ERR(MPI_Allreduce(PartialSums, GlobalSums, Count, MPI_LONG, MPI_SUM, MpiCommData_->Comm_));
   return(0);
 }
 //=============================================================================
@@ -147,7 +147,7 @@ int Epetra_MpiComm::MinAll(int * PartialMins, int * GlobalMins, int Count) const
 int Epetra_MpiComm::MinAll(long * PartialMins, long * GlobalMins, int Count) const {
   EPETRA_CHK_ERR(CheckInput(PartialMins,Count));
   EPETRA_CHK_ERR(CheckInput(GlobalMins,Count));
-  EPETRA_CHK_ERR(MPI_Allreduce(PartialMins, GlobalMins, Count, MPI_INT, MPI_MIN, MpiCommData_->Comm_));
+  EPETRA_CHK_ERR(MPI_Allreduce(PartialMins, GlobalMins, Count, MPI_LONG, MPI_MIN, MpiCommData_->Comm_));
   return(0);
 }
 //=============================================================================
