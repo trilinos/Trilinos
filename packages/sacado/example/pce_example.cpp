b2f6da6678b0d6494a484e9ae2356603bc4f8d89
Eric T. Phipps <etphipp@sandia.gov>
2008-01-22 18:01:28 +0000

    Cleaned up this example somewhat.

--- packages/sacado/example/pce_example.cpp
+++ packages/sacado/example/pce_example.cpp
@@ -56,95 +56,33 @@ ScalarT func(const ScalarT& a) {
 int main(int argc, char **argv)
 {
   try {
-    const unsigned int d = 30;
-    
-//     Sacado::PCE::HermiteEBasis<double> basis(d);
-//     std::cout << basis << std::endl;
-//     std::vector<double> a(d+1);
-//     for (unsigned int i=0; i<=d; i++) {
-//       const Sacado::PCE::StandardPoly<double>& p = basis.getBasisPoly(i);
-//       basis.project(p, a);
-//       std::cout << i << ":  ";
-//       for (unsigned int j=0; j<=d; j++)
-// 	std::cout << a[j] << " ";
-//       std::cout << std::endl;
-//     }
-
+    const unsigned int d = 7;
     Sacado::PCE::Hermite<double>::initWorkspace(d);
     Sacado::PCE::Hermite<double> u(d);
-    Sacado::PCE::HermiteBasis<double> basis(d);
-    std::vector<double> norm_squared = basis.norm_squared();
+    u.fastAccessCoeff(0) = 1.0;
+    u.fastAccessCoeff(1) = 0.4;
+    u.fastAccessCoeff(2) = 0.06;
+    u.fastAccessCoeff(3) = 0.002;
 
-//     for (unsigned int i=0; i<=5; i++)
-//       u.fastAccessCoeff(i) = 1.0*pow(10.0,-double(i+1));
+    Sacado::PCE::Hermite<double> w = std::log(u);
+    Sacado::PCE::Hermite<double> v = 1.0/(w*w + 1.0);
 
-    u.fastAccessCoeff(1) = 1.0;
+    std::cout << "u (hermite basis) = " << u << std::endl;
+    std::cout << "u (standard basis) = " << u.toStandardBasis() << std::endl;
+    std::cout << "v (hermite basis) = " << v << std::endl;
+    std::cout << "v (standard basis) = " << v.toStandardBasis() << std::endl;
 
-//     u.fastAccessCoeff(0) = 1.0;
-//     u.fastAccessCoeff(1) = 1.0;
-//     u.fastAccessCoeff(2) = 0.1;
-//     u.fastAccessCoeff(3) = 0.2;
-//     u.fastAccessCoeff(4) = -0.3;
+    Sacado::PCE::StandardPoly<double> us = u.toStandardBasis();
+    Sacado::Tay::Taylor<double> ut(d);
+    for (unsigned int i=0; i<=d; i++)
+      ut.fastAccessCoeff(i) = us.coeff(i);
 
-//     for (unsigned int i=0; i<=d; i++)
-//       u.fastAccessCoeff(i) /= std::sqrt(norm_squared[i]);
+    Sacado::Tay::Taylor<double> wt = std::log(ut);
+    Sacado::Tay::Taylor<double> vt = 1.0/(wt*wt + 1.0);
 
-//     Sacado::PCE::Hermite<double> v = std::exp(u);
-//     Sacado::PCE::Hermite<double> w = std::log(v);
-
-//     for (unsigned int i=0; i<=d; i++) {
-//       v.fastAccessCoeff(i) *= std::sqrt(norm_squared[i]);
-//       w.fastAccessCoeff(i) *= std::sqrt(norm_squared[i]);
-//     }
-//     std::cout << "u = " << u << std::endl;
-//     std::cout << "v = " << v << std::endl;
-//     std::cout << "v*v = " << v*v << std::endl;
-//     std::cout << "w = " << w << std::endl;
-    
- //    Sacado::PCE::Hermite<double> s = std::sin(u);
-//     Sacado::PCE::Hermite<double> c = std::cos(u);
-//     Sacado::PCE::Hermite<double> v = (std::exp(u) - std::exp(-u)) / 2.0;
-//     Sacado::PCE::Hermite<double> w = (std::exp(u) + std::exp(-u)) / 2.0;
-    
-//     for (unsigned int i=0; i<=d; i++) {
-//       s.fastAccessCoeff(i) *= std::sqrt(norm_squared[i]);
-//       c.fastAccessCoeff(i) *= std::sqrt(norm_squared[i]);
-//       v.fastAccessCoeff(i) *= std::sqrt(norm_squared[i]);
-//       w.fastAccessCoeff(i) *= std::sqrt(norm_squared[i]);
-//     }
-//     std::cout << "u = " << u << std::endl;
-//     std::cout << "s = " << s << std::endl;
-//     std::cout << "v-s = " << v-s << std::endl;
-//     std::cout << "c = " << c << std::endl;
-//     std::cout << "w-c = " << w-c << std::endl;
-//     std::cout << "c*c = " << c*c << std::endl;
-//     std::cout << "s*s = " << s*s << std::endl;
-//     std::cout << "c*c + s*s = " << c*c + s*s << std::endl;
-
-    Sacado::PCE::Hermite<double> c = std::sinh(u);
-    Sacado::PCE::Hermite<double> ac = std::asinh(c);
-    std::cout << "c = " << c << std::endl;
-    std::cout << "u = " << u << std::endl;
-    std::cout << "ac = " << ac << std::endl;
-    std::cout << "ac - u = " << ac - u << std::endl;
-
-//     Sacado::PCE::Hermite<double> cc = c*c;
-//     std::cout << "c*c = " << cc << std::endl;
-//     Sacado::PCE::Hermite<double> ss = s*s;
-//     std::cout << "s*s = " << ss << std::endl;
-
-//     Sacado::PCE::Hermite<double> c = std::cos(u);
-//     Sacado::PCE::Hermite<double> cc = std::cos(c);
-//     Sacado::PCE::Hermite<double> sc = std::sin(c);
-//     std::cout << c << std::endl;
-//     std::cout << cc << std::endl;
-//     std::cout << sc << std::endl;
-//     std::cout << cc*cc + sc*sc << std::endl;
-//     Sacado::PCE::Hermite<double> t = std::sin(c)/std::cos(c);
-//     Sacado::PCE::Hermite<double> s = 1.0/std::cos(c);
-//     std::cout << "t*t - s*s = " << t*t - s*s << std::endl;
- //    std::cout << "t*t - s*s = " << 
-//       (std::sin(c)*std::sin(c)-1.0) - (std::cos(c)*std::cos(c)) << std::endl;
+    std::cout.precision(12);
+    std::cout << "u (taylor basis) = " << ut << std::endl;
+    std::cout << "v (taylor basis) = " << vt << std::endl;
   }
   catch (std::exception& e) {
     std::cout << e.what() << std::endl;
