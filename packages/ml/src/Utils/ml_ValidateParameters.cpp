5eb5deffd1ee7a5f56664083e413144b85a30fb1
Jonathan Hu <jhu@sandia.gov>
2008-01-22 21:11:22 +0000

    SUMMARY:   Adding "do-nothing" to the list of valid ML smoothers.
         BUGS:      n/a
    
         CHECKLIST:
    
         1)   Did you add any source files?  no
    
         2)   Did you update or change the build system?  no
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  yes
    
         COMMENTS:
    
         none
    
    (checklist version 2007-06-20)

--- packages/ml/src/Utils/ml_ValidateParameters.cpp
+++ packages/ml/src/Utils/ml_ValidateParameters.cpp
@@ -60,8 +60,12 @@ Teuchos::ParameterList * ML_Epetra::GetValidMLPParameters(){
   strParam.allowString(true); 
 
   /* Allocate List for Smoothing Options */
-  const int num_smoothers=19;
-  const char* smoother_strings[num_smoothers]={"Aztec","IFPACK","Jacobi","ML symmetric Gauss-Seidel","symmetric Gauss-Seidel","ML Gauss-Seidel","Gauss-Seidel","Chebyshev","MLS","Hiptmair","Amesos-KLU","Amesos-Superlu","Amesos-UMFPACK","Amesos-Superludist","Amesos-MUMPS","user-defined","SuperLU","IFPACK-Chebyshev","self"};
+  const int num_smoothers=20;
+  const char* smoother_strings[num_smoothers]={"Aztec","IFPACK","Jacobi",
+   "ML symmetric Gauss-Seidel","symmetric Gauss-Seidel","ML Gauss-Seidel",
+   "Gauss-Seidel","Chebyshev","MLS","Hiptmair","Amesos-KLU","Amesos-Superlu",
+   "Amesos-UMFPACK","Amesos-Superludist","Amesos-MUMPS","user-defined",
+   "SuperLU","IFPACK-Chebyshev","self","do-nothing"};
   Array<std::string> smoothers(num_smoothers);
   for(int i=0;i<num_smoothers;i++) smoothers[i] = smoother_strings[i];
 
