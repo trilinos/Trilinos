ec0cfbccfe4333ef269ca36465e1640682a6a222
Lee Ann Riesen <lafisk@sandia.gov>
2008-06-19 23:28:07 +0000

    SUMMARY: removed some debugging code
         1)   Did you add any source files?  no
    
         2)   Did you update or change the build system?  no
    
         3) Did you rerun the test suite on at least the packages that you
            changed? no

--- packages/isorropia/src/epetra/zoltanLib/Isorropia_Zoltan_Repartition.cpp
+++ packages/isorropia/src/epetra/zoltanLib/Isorropia_Zoltan_Repartition.cpp
@@ -52,9 +52,6 @@ Questions? Contact Alan Williams (william@sandia.gov)
 
 #include <QueryObject.hpp>
 
-#define DEBUG_YES "yes"
-#define DEBUG_PROC   1
-
 namespace Isorropia {
 
 namespace Epetra {
@@ -219,13 +216,6 @@ load_balance(MPI_Comm &comm,
     zz->Set_Edge_List_Multi_Fn(QueryObject::Edge_List_Multi, (void *)&queryObject);
   }
 
-#ifdef DEBUG_YES
-  if (me == DEBUG_PROC)
-  {
-    std::cout << paramlist << std::endl;
-  }
-#endif
-
   //Generate Load Balance
   int changes, num_gid_entries, num_lid_entries, num_import, num_export;
   ZOLTAN_ID_PTR import_global_ids, import_local_ids;
@@ -241,14 +231,6 @@ load_balance(MPI_Comm &comm,
     throw Isorropia::Exception("Error computing partitioning with Zoltan");
     return -1;
   }
-
-#ifdef DEBUG_YES
-  if (me == DEBUG_PROC)
-  {
-    std::cout << "num imports " << num_import << std::endl;
-    std::cout << "num exports " << num_export << std::endl;
-  }
-#endif
 
   //Generate New Element List
   int numMyElements = queryObject.RowMap().NumMyElements();
