d7825394474ca78e29ae1da033a3b344d41e806a
Lee Ann Riesen <lafisk@sandia.gov>
2008-07-15 21:22:35 +0000

    SUMMARY: display an error message and return if output can not be                  opened for writing
         CHECKLIST:
    
         1)   Did you add any source files?  no
    
         2)   Did you update or change the build system? no
    
         3) Did you rerun the test suite on at least the packages that you
            changed? no
    
         COMMENTS:  zdrive crashed if it couldn't open the output files,
          no it prints a message and exits

--- packages/zoltan/src/driver/dr_output.c
+++ packages/zoltan/src/driver/dr_output.c
@@ -183,6 +183,7 @@ int output_results(const char *cmd_file,
   /* Local declarations. */
   const char  *yo = "output_results";
   char   par_out_fname[FILENAME_MAX+1], ctemp[FILENAME_MAX+1];
+  char cmsg[256];
 
   int   *global_ids = NULL;
   int   *parts = NULL;
@@ -228,6 +229,13 @@ int output_results(const char *cmd_file,
   gen_par_filename(ctemp, par_out_fname, pio_info, Proc, Num_Proc);
 
   fp = fopen(par_out_fname, "w");
+
+  if (fp == NULL){
+    sprintf(cmsg, "Error in %s; %s can not be opened for writing.", yo, par_out_fname);
+    Gen_Error(0, cmsg);
+    return 0;
+  }
+
   if (Proc == 0) 
     echo_cmd_file(fp, cmd_file);
 
