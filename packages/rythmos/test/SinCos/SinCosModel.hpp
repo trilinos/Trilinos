9868e4a08706459cbb0f81978def6371b209e9ca
Todd S. Coffey <tscoffe@sandia.gov>
2008-07-17 22:43:03 +0000

    SUMMARY:   Initial cut at a pure Thyra based model for Unit testing in     Rythmos.  This uses the new class Thyra::LinearOpWithSolveFactoryBase that     Ross wrote for me.
         BUGS:      2524
    
         CHECKLIST:
    
         1)   Did you add any source files?  YES
         1.a) Did you do a 'cvs add'?  YES
         1.b) Did you add them to a Makefile.am file in some way?  NO
         1.c) Did you do a new bootstrap?  NO
    
         2)   Did you update or change the build system?  NO
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  NO
    
         COMMENTS:
    
         This is just a checkpoint commit.  I've been playing around with what the
         class will look like and how it might function, the actual calls are not
         set up correctly yet.  I'm going to write unit tests for this class next
         and then implement the functionality.  It is not included in the automake
         system at all at this point.
    
    (checklist version 2007-06-20)

---
+++ packages/rythmos/test/SinCos/SinCosModel.hpp
@@ -0,0 +1,106 @@
+//@HEADER
+
+// ***********************************************************************
+//
+//                     Rythmos Package
+//                 Copyright (2006) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Todd S. Coffey (tscoffe@sandia.gov)
+//
+// ***********************************************************************
+//@HEADER
+
+#ifndef SIN_COS_MODEL_HPP
+#define SIN_COS_MODEL_HPP
+
+#include "Rythmos_ConfigDefs.h"
+#include "Rythmos_Types.hpp"
+
+#include "Thyra_ModelEvaluator.hpp" // Interface
+#include "Thyra_StateFuncModelEvaluatorBase.hpp" // Implementation
+
+#include "Teuchos_ParameterList.hpp"
+
+namespace Rythmos {
+
+class SinCosModel : public Thyra::StateFunctionModelEvaluatorBase<double> {
+public:
+
+  // Constructor
+  SinCosModel();
+
+  // Exact solution
+  RCP<const VectorBase<double> > getExactSolution(double t) const;
+
+  // Set explicit/implicit flag
+  void setImplicitFlag(bool implicit);
+
+  /** \name Public functions overridden from ModelEvaulator. */
+  //@{
+
+  /** \brief . */
+  RCP<const VectorSpaceBase<double> > get_x_space() const;
+  /** \brief . */
+  RCP<const VectorSpaceBase<double> > get_f_space() const;
+  /** \brief . */
+  ModelEvaluatorBase::InArgs<double> getNominalValues() const;
+  /** \brief . */
+  RCP<Thyra::LinearOpWithSolveBase<double> > create_W() const;
+  /** \brief . */
+  RCP<Thyra::LinearOpBase<double> > create_W_op() const;
+  /** \brief . */
+  ModelEvaluatorBase::InArgs<double> createInArgs() const;
+
+  //@}
+
+private:
+
+  /** \brief. */
+  void initialize_();
+
+  /** \name Private functions overridden from ModelEvaulatorDefaultBase. */
+  //@{
+
+  /** \brief . */
+  Thyra::ModelEvaluatorBase::OutArgs<double> createOutArgsImpl() const;
+  /** \brief . */
+  void evalModelImpl(
+    const Thyra::ModelEvaluatorBase::InArgs<double> &inArgs_bar,
+    const Thyra::ModelEvaluatorBase::OutArgs<double> &outArgs_bar
+    ) const;
+
+  //@}
+
+private:
+  bool isImplicit_; // false => \dot{x} = f(x,t)    W_op = beta*df/dx
+                    // true =>  F(\dot{x},x,t) = 0  W_op = alpha*dF/dxdot + beta*dF/dx
+  bool isInitialized_;
+  ModelEvaluatorBase::InArgs<double> inArgs_;
+  ModelEvaluatorBase::OutArgs<double> outArgs_;
+  ModelEvaluatorBase::InArgs<double> nominalValues_;
+  RCP<const VectorSpaceBase<double> > x_space_;
+  RCP<const VectorSpaceBase<double> > f_space_;
+  RCP<Thyra::LinearOpWithSolveBase<double> > W_;
+
+};
+
+} // namespace Rythmos 
+
+#endif // SIN_COS_MODEL_HPP
