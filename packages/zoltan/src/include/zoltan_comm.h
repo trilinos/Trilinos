5bf3519a2c44a09cc0e4032cbf0570d76b8ee239
Lee Ann Riesen <lafisk@sandia.gov>
2008-08-04 16:45:19 +0000

    SUMMARY:   replacing Zoltan_Comm_Do_Reverse_Post and Zoltan_Comm_Do_Reverse_Wait
         CHECKLIST:
    
         1)   Did you add any source files? no
    
         2)   Did you update or change the build system? no
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  part of it - doing this checkin so I can run the rest

--- packages/zoltan/src/include/zoltan_comm.h
+++ packages/zoltan/src/include/zoltan_comm.h
@@ -55,7 +55,8 @@ int Zoltan_Comm_Do_Wait(ZOLTAN_COMM_OBJ*, int, char*, int, char*);
 int Zoltan_Comm_Do_AlltoAll(ZOLTAN_COMM_OBJ*, char*, int, char*);
 
 int Zoltan_Comm_Do_Reverse     (ZOLTAN_COMM_OBJ*, int, char*, int, int*, char*);
-int Zoltan_Comm_Reverse_Plan(ZOLTAN_COMM_OBJ*, int, int*);
+int Zoltan_Comm_Do_Reverse_Post(ZOLTAN_COMM_OBJ*, int, char*, int, int*, char*);
+int Zoltan_Comm_Do_Reverse_Wait(ZOLTAN_COMM_OBJ*, int, char*, int, int*, char*);
 
 int Zoltan_Comm_Info(ZOLTAN_COMM_OBJ*, int*, int*, int*, int*, int*, int*, int*,
  int*, int*, int*, int*, int*, int*);
