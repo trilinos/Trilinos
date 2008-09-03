f914ea3049622f0f7644e3499988301a6d2b6373
Jonathan Hu <jhu@sandia.gov>
2008-09-03 16:58:19 +0000

    SUMMARY:   Adding doxygen comments to fix a dead link.
         BUGS:      n/a
    
         CHECKLIST:
    
         1)   Did you add any source files?  no
    
         2)   Did you update or change the build system?  no
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  no
    
         COMMENTS:
    
         Documentation change only.  This fixes a dead link from the ML doxygen
         main page.
    
    (checklist version 2007-06-20)

--- packages/ml/src/Utils/ml_ifpack_wrap.h
+++ packages/ml/src/Utils/ml_ifpack_wrap.h
@@ -1,3 +1,13 @@
+/*!
+ * \file ml_ifpack_wrap.h
+ *
+ * \brief Interface to the Trilinos package Ifpack.
+ *
+ * The ML/Amesos interface allows ML users to apply Ifpack iterative methods
+ * as smoothers.
+ *
+ *
+*/
 /* ******************************************************************** */
 /* See the file COPYRIGHT for a complete copyright notice, contact      */
 /* person and disclaimer.                                               */        
@@ -15,13 +25,14 @@ extern "C" {
 #endif
 #endif
 
-
+/** Apply the Ifpack smoother. */
 int ML_Smoother_Ifpack(ML_Smoother *sm,int inlen,double x[],int outlen,
 			      double rhs[]);
 
+/** Destroy the Ifpack smoother. */
 void ML_Smoother_Clean_Ifpack(void * Ifpack_Handle);
 
-/** Generates the ifpack smoother */
+/** Generate the Ifpack smoother */
 int ML_Gen_Smoother_Ifpack(ML *ml, const char* Type, int Overlap,
                            int nl, int pre_or_post, 
                            void *List,
