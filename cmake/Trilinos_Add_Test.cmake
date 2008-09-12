e1b63e0cbcc2a33f7c99c610ec8dd06671f7c1d8
Roscoe A. Bartlett <rabartl@sandia.gov>
2008-09-12 04:49:51 +0000

    Summary: More enhancements to TRILINOS_ADD_TEST(...) and more
    Here is what I did:
    
    (*) Add a global user option VERBOSE_CONFIGURE that is OFF by default.  When
    turned on, it results in several MESSAGE(...) statements being executed
    showing you what examples and tests are being added.  This is good for
    debugging.
    
    (*) Appended the number of processes being used to the name of the MPI test
    
    (*) Avoid appending a counter to the test name if there is only one argument
    
    (*) Added quotes to the lists of command-line arguments so that they got used
    together.  I was creating a new test for each command-line argument which is
    not what I want.  The BadUnitTest tests show an example of this.
    
    (*) Updated linux-gabriel-gcc-3.3.4-mpi-cmake to allow extra cmake options to
    be passed in on the command-line.
    
    I also manually tested that the PASS_REGULAR_EXPRESSION option works correctly
    and it seems to.  I am a little worried that we don't have unit tests in place
    for TRILINOS_ADD_TEST(...).
    
    I tested both serial and mpi builds and all of the tests pass.

--- cmake/Trilinos_Add_Test.cmake
+++ cmake/Trilinos_Add_Test.cmake
@@ -23,6 +23,11 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
      "NUM_MPI_PROCS;DIRECTORY;KEYWORDS;COMM;ARGS;NAME;PASS_REGULAR_EXPRESSION;HOST;XHOST;FAIL_REGULAR_EXPRESSION"  #lists
      "DESEND_INTO_DIR"   #options
      ${ARGN} )
+
+  IF(PARSE_ARGS)
+    LIST(LENGTH PARSE_ARGS NUM_PARSE_ARGS)
+    #MESSAGE(STATUS "NUM_PARSE_ARGS = ${NUM_PARSE_ARGS}")
+  ENDIF()
   
   #
   # B) Do not add any tests if host or xhost says not to
@@ -56,7 +61,6 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
   ELSE()
     SET(TEST_NAME "${PROJECT_NAME}_${EXE_NAME}")  
   ENDIF()
-  #MESSAGE("TRILINOS_ADD_TEST: ${EXE_NAME}: TEST_NAME = ${TEST_NAME}")
 
   IF(PARSE_DIRECTORY)
     SET(EXECUTABLE_PATH "./${PARSE_DIRECTORY}/${EXE_BINARY_NAME}")
@@ -66,7 +70,7 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
   #MESSAGE("TRILINOS_ADD_TEST: ${EXE_NAME}: EXECUTABLE_PATH = ${EXECUTABLE_PATH}")
 
   #
-  # D) Append keywords the the name of the test
+  # D) Append keywords to the name of the test
   #
   
   IF(PARSE_KEYWORDS)
@@ -79,31 +83,34 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
     
   IF(TRILINOS_ENABLE_MPI)
     SET(NP)
+    SET(NUM_PROCS_USED 1)
     IF(PARSE_NUM_MPI_PROCS)
       IF(${PARSE_NUM_MPI_PROCS} MATCHES [0-9]+-[0-9]+)
-        
-        STRING(REGEX REPLACE  "([0-9]+)-([0-9]+)" "\\1" MIN_NP ${PARSE_NUM_MPI_PROCS} )
-        STRING(REGEX REPLACE  "([0-9]+)-([0-9]+)" "\\2" MAX_NP ${PARSE_NUM_MPI_PROCS} )
+        STRING(REGEX REPLACE "([0-9]+)-([0-9]+)" "\\1" MIN_NP ${PARSE_NUM_MPI_PROCS} )
+        STRING(REGEX REPLACE "([0-9]+)-([0-9]+)" "\\2" MAX_NP ${PARSE_NUM_MPI_PROCS} )
         IF(${MIN_NP} LESS ${MPIEXEC_MAX_NUMPROCS} AND  ${MAX_NP} GREATER ${MPIEXEC_MAX_NUMPROCS} )
-            SET(NP ${MPI_NUMPROCS_FLAG} ${MPIEXEC_MAX_NUMPROCS})
+          SET(NUM_PROCS_USED ${MPIEXEC_MAX_NUMPROCS})
         ELSEIF(${MIN_NP} EQUAL ${MPIEXEC_MAX_NUMPROCS})
-            SET(NP ${MPI_NUMPROCS_FLAG} ${MIN_NP})
+          SET(NUM_PROCS_USED ${MIN_NP})
         ELSEIF(${MAX_NP} EQUAL ${MPIEXEC_MAX_NUMPROCS})
-            SET(NP ${MPI_NUMPROCS_FLAG} ${MAX_NP})
+          SET(NUM_PROCS_USED ${MAX_NP})
         ELSEIF(${MAX_NP} LESS ${MPIEXEC_MAX_NUMPROCS})
-            SET(NP ${MPI_NUMPROCS_FLAG} ${MAX_NP})
+          SET(NUM_PROCS_USED ${MAX_NP})
         ELSE()
-           RETURN() #The number of available processor is outside the given range
-                    #so the test should not be run
+          # The number of available processor is outside the given range
+          # so the test should not be run.
+          RETURN()
         ENDIF()
       ELSE()
         IF(${PARSE_NUM_MPI_PROCS} LESS ${MPIEXEC_MAX_NUMPROCS})
-          SET(NP ${MPI_NUMPROCS_FLAG} ${PARSE_NUM_MPI_PROCS})
+          SET(NUM_PROCS_USED ${PARSE_NUM_MPI_PROCS})
         ELSE()
-           SET(NP ${MPI_NUMPROCS_FLAG} ${MPIEXEC_MAX_NUMPROCS})
+          SET(NUM_PROCS_USED ${MPIEXEC_MAX_NUMPROCS})
         ENDIF()
       ENDIF()
     ENDIF()
+
+    SET(NP ${MPI_NUMPROCS_FLAG} ${NUM_PROCS_USED})
     
     #
     # E) Add the tests
@@ -116,29 +123,41 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
       # Else, if COMM is defined we have to find 'mpi'
       LIST (FIND PARSE_COMM "mpi" DO_MPI_INDEX)
     ENDIF()
+
     IF(NOT ${DO_MPI_INDEX} EQUAL -1)
-      SET(TEST_NAME "${TEST_NAME}_MPI")
+
+      SET(TEST_NAME "${TEST_NAME}_MPI_${NUM_PROCS_USED}")
       
       IF(PARSE_ARGS)
 
         SET(COUNTER 0)
+
         FOREACH(PARSE_ARG ${PARSE_ARGS})
+
+          IF(${NUM_PARSE_ARGS} EQUAL 1)
+            SET(TEST_NAME_COUNTER "${TEST_NAME}")
+          ELSE()
+            SET(TEST_NAME_COUNTER "${TEST_NAME}_${COUNTER}")
+          ENDIF()
+          IF("${VERBOSE_CONFIGURE}" STREQUAL  "ON")
+            MESSAGE("TRILINOS_ADD_TEST: ${EXE_NAME}: TEST_NAME_COUNTER = ${TEST_NAME_COUNTER}")
+          ENDIF()
           
           #This is a little bit of a hack
           #If the argument string has multiple arguments then the white space will need 
           #to replaced by a semicolin.  If this is not done the add_test command will
           #add a slash to each white space in the argument string.
           STRING(REPLACE " " ";" MYARG ${PARSE_ARG}) 
-          ADD_TEST(${TEST_NAME}_${COUNTER} ${MPI_EXECUTABLE} ${NP} ${EXECUTABLE_PATH} ${MYARG})
+          ADD_TEST(${TEST_NAME_COUNTER} ${MPI_EXECUTABLE} ${NP} ${EXECUTABLE_PATH} ${MYARG})
           SET(ADDED_THE_TEST ON)    
           
           IF (PARSE_PASS_REGULAR_EXPRESSION)
-            SET_TESTS_PROPERTIES(${TEST_NAME}_${COUNTER} PROPERTIES PASS_REGULAR_EXPRESSION
+            SET_TESTS_PROPERTIES(${TEST_NAME_COUNTER} PROPERTIES PASS_REGULAR_EXPRESSION
               ${PARSE_PASS_REGULAR_EXPRESSION})
           ENDIF()
   
           IF (PARSE_FAIL_REGULAR_EXPRESSION)
-            SET_TESTS_PROPERTIES(${TEST_NAME}_${COUNTER} PROPERTIES FAIL_REGULAR_EXPRESSION
+            SET_TESTS_PROPERTIES(${TEST_NAME_COUNTER} PROPERTIES FAIL_REGULAR_EXPRESSION
             ${PARSE_FAIL_REGULAR_EXPRESSION})
           ENDIF()
 
@@ -148,8 +167,10 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
 
       ELSE()
 
-        #ADD_TEST(${TEST_NAME} ${MPI_EXECUTABLE} ${MPI_EXECUTABLE_FLAGS} ${EXECUTABLE_PATH} ${PARSE_ARGS})
-        #MESSAGE("TRILINOS_ADD_TEST: ${EXE_NAME}: ADD_TEST: ${TEST_NAME}, ${MPI_EXECUTABLE}")
+        IF("${VERBOSE_CONFIGURE}" STREQUAL  "ON")
+          MESSAGE("TRILINOS_ADD_TEST: ${EXE_NAME}: TEST_NAME = ${TEST_NAME}")
+        ENDIF()
+
         ADD_TEST(${TEST_NAME} ${MPI_EXECUTABLE} ${NP} ${EXECUTABLE_PATH} )
         SET(ADDED_THE_TEST ON)    
         
@@ -172,23 +193,30 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
       IF(PARSE_ARGS)
 
         SET(COUNTER 0)
+
         FOREACH(PARSE_ARG ${PARSE_ARGS})
+
+          IF(${NUM_PARSE_ARGS} EQUAL 1)
+            SET(TEST_NAME_COUNTER "${TEST_NAME}")
+          ELSE()
+            SET(TEST_NAME_COUNTER "${TEST_NAME}_${COUNTER}")
+          ENDIF()
+          IF("${VERBOSE_CONFIGURE}" STREQUAL  "ON")
+            MESSAGE("TRILINOS_ADD_TEST: ${EXE_NAME}: TEST_NAME_COUNTER = ${TEST_NAME_COUNTER}")
+          ENDIF()
        
-          #This is a little bit of a hack
-          #If the argument string has multiple arguments then the white space will need 
-          #to replaced by a semicolin.  If this is not done the add_test command will
-          #add a slash to each white space in the argument string.
+          # See above about this hack
           STRING(REPLACE " " ";" MYARG ${PARSE_ARG})
-          ADD_TEST(${TEST_NAME}_${COUNTER} ${EXECUTABLE_PATH} ${MYARG})
+          ADD_TEST(${TEST_NAME_COUNTER} ${EXECUTABLE_PATH} ${MYARG})
           SET(ADDED_THE_TEST ON)    
           
           IF (PARSE_PASS_REGULAR_EXPRESSION)
-            SET_TESTS_PROPERTIES( ${TEST_NAME}_${COUNTER}
+            SET_TESTS_PROPERTIES( ${TEST_NAME_COUNTER}
               PROPERTIES PASS_REGULAR_EXPRESSION ${PARSE_PASS_REGULAR_EXPRESSION})
           ENDIF()
   
           IF (PARSE_FAIL_REGULAR_EXPRESSION)
-            SET_TESTS_PROPERTIES( ${TEST_NAME}_${COUNTER}
+            SET_TESTS_PROPERTIES( ${TEST_NAME_COUNTER}
               PROPERTIES FAIL_REGULAR_EXPRESSION ${PARSE_FAIL_REGULAR_EXPRESSION})
           ENDIF()
 
@@ -197,6 +225,10 @@ FUNCTION (TRILINOS_ADD_TEST EXE_NAME)
         ENDFOREACH()
 
       ELSE()
+
+        IF("${VERBOSE_CONFIGURE}" STREQUAL  "ON")
+           MESSAGE("TRILINOS_ADD_TEST: ${EXE_NAME}: TEST_NAME = ${TEST_NAME}")
+        ENDIF()
 
         ADD_TEST(${TEST_NAME} ${EXECUTABLE_PATH} )
         SET(ADDED_THE_TEST ON)    
