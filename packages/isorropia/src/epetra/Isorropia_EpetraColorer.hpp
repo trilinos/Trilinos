868db161774dafc3027bb62e483d68deac140201
Lee Ann Riesen <lafisk@sandia.gov>
2008-09-17 20:17:06 +0000

    SUMMARY:  adding doxygen info
         BUGS:  3866
    
         CHECKLIST:
    
         1)   Did you add any source files?  no
    
         2)   Did you update or change the build system? no
    
         3) Did you rerun the test suite on at least the packages that you
            changed? re ran doxygen

--- packages/isorropia/src/epetra/Isorropia_EpetraColorer.hpp
+++ packages/isorropia/src/epetra/Isorropia_EpetraColorer.hpp
@@ -65,9 +65,23 @@ namespace Epetra {
 class Colorer : virtual public Isorropia::Colorer, public Isorropia::Epetra::Operator {
 public:
 
+    /** Constructor
+
+    \param[in] input_graph the graph which is to have colors assigned to its rows
+    \param[in] paramlist this parameter list is currently ignored, but may be used in a future implementation
+    \param[in] compute_now  if @c true, the coloring is computed in the constructor, otherwise call Isorropia::Epetra::Colorer::color when you want to compute the coloring, defaults to @c false
+    */
+
   Colorer(Teuchos::RefCountPtr<const Epetra_CrsGraph> input_graph,
 	  const Teuchos::ParameterList& paramlist,
 	  bool compute_now=true);
+
+    /** Constructor
+
+    \param[in] input_matrix the matrix which is to have colors assigned to its rows
+    \param[in] paramlist this parameter list is currently ignored, but may be used in a future implementation
+    \param[in] compute_now  if @c true, the coloring is computed in the constructor, otherwise call Isorropia::Epetra::Colorer::color when you want to compute the coloring, defaults to @c true
+  */
 
   Colorer(Teuchos::RefCountPtr<const Epetra_RowMatrix> input_matrix,
 	  const Teuchos::ParameterList& paramlist,
@@ -76,8 +90,19 @@ public:
   /** Destructor */
   ~Colorer() {} ;
 
+  /** Compute the coloring if it has not already been computed, same effect as
+       Isorropia::Epetra::Colorer::compute
+
+    \param[in] force_coloring if @c true recompute the coloring even if it has already been computed, defaults to @c false
+    */
 
   void color(bool force_coloring=false);
+
+  /** Compute the coloring if it has not already been computed, same effect as
+       Isorropia::Epetra::Colorer::color
+
+    \param[in] force_compute if @c true recompute the coloring even if it has already been computed, defaults to @c false
+    */
 
   void compute(bool force_compute=false) {
     color(force_compute);
