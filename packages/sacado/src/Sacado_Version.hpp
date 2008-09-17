0a4beda0fdbcbc87d7a00ff84eadf6ba66e6607e
Eric T. Phipps <etphipp@sandia.gov>
2008-09-17 19:34:47 +0000

    Merging changes from release 9.0 branch.  Also incremented version number to 9.1-dev.

--- packages/sacado/src/Sacado_Version.hpp
+++ packages/sacado/src/Sacado_Version.hpp
@@ -37,7 +37,7 @@
 #include "Sacado_ConfigDefs.h"
 
 inline std::string Sacado_Version() { 
-  return("Sacado Version 9.0-dev"); 
+  return("Sacado Version 9.1-dev"); 
 }
 
 #endif /* SACADO_VERSION_HPP */
