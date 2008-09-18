9b7a592eb0694b1a1cb5eb60f427f90409f0d28f
Robert C. Kirby <robert.c.kirby@ttu.edu>
2008-09-18 21:47:24 +0000

    SUMMARY:   [reordered dof tags]
         BUGS:      [???]
    
         CHECKLIST:
    
         1)   Did you add any source files?  [no]
    
         2)   Did you update or change the build system?  [no]
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  [no]
    
         COMMENTS:
    
         [???]
    
    (checklist version 2007-06-20)

--- packages/intrepid/src/local/basis/Intrepid_F0_HEX_DDDef.hpp
+++ packages/intrepid/src/local/basis/Intrepid_F0_HEX_DDDef.hpp
@@ -50,17 +50,18 @@ void Basis_F0_HEX_DD<Scalar>::initialize() {
   int posScId  = 1;         // poisition in the tag, counting from 0, of the subcell id
   int posBfId  = 2;         // position in the tag, counting from 0, of DoF Id relative to the subcell
 
+
+  int degm1sq = (degree_-1)*(degree_-1);
+
   // An array with local DoF tags assigned to basis functions, in the order of their local enumeration 
   // I need to fill this!
   int *tags = new int[tagSize*numDof_];
 
   int *tagsCur = tags;
 
-  // indices: k runs along points on an edge
+  // indices: i runs along points on an edge
   //          j runs over edges in a face
-  //          i runs over faces in the cube
-
-  // start bottom face k == 0
+  //          k runs over faces in the cube
 
   // start at (-1,-1,-1), which is dimension 0, facet 0, dof 0, total dof on vertex, 1
   // it is also the case of i == j == k == 0;
@@ -71,234 +72,472 @@ void Basis_F0_HEX_DD<Scalar>::initialize() {
   tagsCur[3] = 1;
   tagsCur += tagSize;
 
-  // run along bottom edge from (-1,-1,-1) to (1,-1,-1), which is dimension 1, facet 0, degree_-1 dof on edge
-  
+  // now vary k (going up in the z direction).
+  // I stay on the edge 4, running from vertex 0 to vertex 4
   for (int k=1;k<degree_;k++) {
     tagsCur[0] = 1;
-    tagsCur[1] = 0;
+    tagsCur[1] = 4;
     tagsCur[2] = k-1;
     tagsCur[3] = degree_-1;
     tagsCur += tagSize;
   }
 
-  // now get vertex (1,-1,-1), which is dimension 0, facet 1, dof 0, dof on cell: 1
-  tagsCur[0] = 0;
-  tagsCur[1] = 1;
-  tagsCur[2] = 0;
-  tagsCur[3] = 1;
-  tagsCur += tagSize;
-
-  // now get middle rows on bottom face
-  for (int j=1;j<degree_;j++) {
-    // first node is on the edge from vertex 3 to vertex 0 (edge 3)
-    tagsCur[0] = 1;
-    tagsCur[1] = 3;
-    tagsCur[2] = j - 1;
-    tagsCur[3] = (degree_-1);
-    tagsCur += tagSize;
-
-    // now do the interior vertices on the i:th row of the bottom face, which is dim 2, facet 4
-    //
-    for (int k=1;k<degree_;k++) {
-      tagsCur[0] = 2;
-      tagsCur[1] = 4;
-      tagsCur[2] = (j-1)*(degree_-1) + (k-1);
-      tagsCur[3] = (degree_-1)*(degree_-1);
-      tagsCur += tagSize;
-    }
-
-    // the last node is on edge (1,2) (facet 1 of dimension 0), traversed forward
-    tagsCur[0] = 1;
-    tagsCur[1] = 1;
-    tagsCur[2] = j - 1;
-    tagsCur[3] = (degree_-1);
-    tagsCur += tagSize;
-  }
-
-  // now get vertex 3 (beginning of last row on bottom face)
-  tagsCur[0] = 0;  // dim 0
-  tagsCur[1] = 3;  // id: 3
-  tagsCur[2] = 0;  // first dof
-  tagsCur[3] = 1;  // one dof on the vertex
-  tagsCur += tagSize;
-
-  // get last edge on bottom face  This is edge 2, running from vertex 2 to vertex 3.
-
-  for (int k=1;k<degree_;k++) {
-    tagsCur[0] = 1; // dim 1
-    tagsCur[1] = 2; // id: 2
-    tagsCur[2] = k - 1; // dof on edge
-    tagsCur[3] = degree_ - 1; // total number of nodes on edge
-    tagsCur += tagSize;
-  }
-
-  // now get vertex 2 (end of last row on bottom face)
-  tagsCur[0] = 0; // dim 0
-  tagsCur[1] = 2; // id: 2
-  tagsCur[2] = 0; // first dof on vertex
-  tagsCur[3] = 1; // one dof on the vertex
-  tagsCur += tagSize;
-
-  // end bottom face
-
-  // begin interior faces
-  for (int i=1;i<degree_;i++) {
-    // j == 0 lives on face y==-1, which is face 0
-
-    // k == 0, node lives on edge 4
-    tagsCur[0] = 1;
-    tagsCur[1] = 4;
-    tagsCur[2] = i-1;
-    tagsCur[3] = (degree_-1);
-    tagsCur += tagSize;
-
-    // interior nodes
-    for (int k=1;k<degree_;k++) {
-      tagsCur[0] = 2;
-      tagsCur[1] = 0;
-      tagsCur[2] = (i-1)*(degree_-1) + (k-1);
-      tagsCur[3] = (degree_-1)*(degree_-1);
-      tagsCur += tagSize;
-    }    
-
-    // k == degree, node lives on edge 5
-    tagsCur[0] = 1;
-    tagsCur[1] = 5;
-    tagsCur[2] = i-1;
-    tagsCur[3] = (degree_-1);
-    tagsCur += tagSize;
-
-    // interior edges in i:th face
-    for (int j=1;j<degree_;j++) { 
-      // k == 0, node lives on face 3
-      tagsCur[0] = 2;
-      tagsCur[1] = 3;
-      tagsCur[2] = (i-1)*(degree_-1) + (j-1);
-      tagsCur[3] = (degree_-1)*(degree_-1);
-      tagsCur += tagSize;
-
-      // interior nodes on j:th edge
-      for (int k=1;k<degree_;k++) {
-	tagsCur[0] = 3;
-	tagsCur[1] = 0;
-	tagsCur[2] = (i-1)*(degree_-1)*(degree_-1) + (j-1)*(degree_-1) + (k-1);
-	tagsCur[3] = (degree_-1)*(degree_-1)*(degree_-1);
-	tagsCur += tagSize;
-      }
-
-      // k == degree, node lives on face 1
-      tagsCur[0] = 2;
-      tagsCur[1] = 1;
-      tagsCur[2] = (i-1)*(degree_-1)+(j-1);
-      tagsCur[3] = (degree_-1)*(degree_-1);
-      tagsCur += tagSize;
-      
-    }
-    // end interior j
-
-    // j == degree lives on face y==1, which is number 2
-    
-    // first node (k==0), lives on edge 7
-    tagsCur[0] = 1;
-    tagsCur[1] = 7;
-    tagsCur[2] = i-1;
-    tagsCur[3] = (degree_-1);
-    tagsCur += tagSize;
-
-    // interior nodes live on face 2
-    for (int k=1;k<degree_;k++) {
-      tagsCur[0] = 2;
-      tagsCur[1] = 2;
-      tagsCur[2] = (i-1)*(degree_-1) + (k-1);
-      tagsCur[3] = (degree_-1)*(degree_-1);
-      tagsCur += tagSize;
-    }
-
-    // last node (k==degree) lives on edge 6
-    tagsCur[0] = 1;
-    tagsCur[1] = 6;
-    tagsCur[2] = i-1;
-    tagsCur[3] = (degree_-1);
-    tagsCur += tagSize;
-
-    // end j
-  }  
-  // end interior faces
-
-  // begin top face
-  // start with vertex 4
+  // now get vertex (-1,-1,1), which is dimension 0, facet 4, dof 0, dof on cell: 1
   tagsCur[0] = 0;
   tagsCur[1] = 4;
   tagsCur[2] = 0;
   tagsCur[3] = 1;
   tagsCur += tagSize;
 
-  // now do edge 8
-  for (int k=1;k<degree_;k++) {
+
+  // now keep x=-1, increment y 
+  for (int j=1;j<degree_;j++) {
+    // first node is down on bottom edge running from (-1,-1,-1) (v0) to (-1,1,-1) (v3)
+    // this is edge 3
     tagsCur[0] = 1;
-    tagsCur[1] = 8;
-    tagsCur[2] = (k-1);
-    tagsCur[3] = (degree_-1);
+    tagsCur[1] = 3;
+    tagsCur[2] = j;
+    tagsCur[3] = degree_-1;
+    tagsCur += tagSize;
+    
+    // internal nodes to this line will lie on face 3.
+    for (int k=1;k<degree_;k++) {
+      tagsCur[0] = 2;
+      tagsCur[1] = 3;
+      tagsCur[2] = (j-1)*(degree_-1)+k-1;
+      tagsCur[3] = degm1sq;
+      tagsCur += tagSize;
+    }
+
+    // last node is on top edge running from (-1,-1,1) (v4) to (-1,1,1) (v7)
+    // this is edge 11
+    tagsCur[0] = 1;
+    tagsCur[1] = 11;
+    tagsCur[2] = j-1;
+    tagsCur[3] = degree_-1;
     tagsCur += tagSize;
   }
-  
-  // now do vertex 5
+
+  // now go up line (-1,1,z)
+  // first vertex is (-1,1,-1), vertex 3
   tagsCur[0] = 0;
-  tagsCur[1] = 5;
+  tagsCur[1] = 3;
   tagsCur[2] = 0;
   tagsCur[3] = 1;
   tagsCur += tagSize;
 
-  // now do interior edges of top face
-  for (int j=1;j<degree_;j++) {
-    // k==0, lives on edge 11
+  // middle of line, which is edge 7
+  for (int k=1;k<degree_;k++) {
     tagsCur[0] = 1;
-    tagsCur[1] = 11;
-    tagsCur[2] = j-1;
-    tagsCur[3] = (degree_-1);
-    tagsCur += tagSize;
-
-    // interior nodes of edges
-    for (int k=1;k<degree_;k++) {
-      tagsCur[0] = 2;
-      tagsCur[1] = 5;
-      tagsCur[2] = (j-1)*(degree_-1) + (k-1);
-      tagsCur[3] = (degree_-1)*(degree_-1);
-      tagsCur += tagSize;
-    }
-    
-    // k == degree, node lives on edge 9
-    tagsCur[0] = 1;
-    tagsCur[1] = 9;
-    tagsCur[2] = j-1;
-    tagsCur[3] = (degree_-1);
+    tagsCur[1] = 7;
+    tagsCur[2] = k-1;
+    tagsCur[3] = degree_-1;
     tagsCur += tagSize;
   }
-  // done with interior edges of top face.
-  // do last edge of top face
-  // vertex 7
+
+  // top of line is vertex 7
   tagsCur[0] = 0;
   tagsCur[1] = 7;
   tagsCur[2] = 0;
   tagsCur[3] = 1;
   tagsCur += tagSize;
 
-  // interior nodes on edge 10
-  for (int k=1;k<degree_;k++) {
+  // now do middle planes for each fixed i (x value)
+  for (int i=1;i<degree_;i++) {
+    // j == 0 \\ y == -1 and 
+
+    // k == 0 // z == -1, live on e0
+    tagsCur[0] = 1;
+    tagsCur[1] = 0;
+    tagsCur[2] = i-1;
+    tagsCur[3] = degree_-1;
+    tagsCur += tagSize;
+
+    // now go up the z axis (increase k along face 0)
+    for (int k=1;k<degree_;k++) {
+      tagsCur[0] = 2;
+      tagsCur[1] = 0;
+      tagsCur[2] = (i-1)*(degree_-1)+(k-1);
+      tagsCur[3] = degm1sq;
+      tagsCur += tagSize;
+    }
+
+    // now z == 1, edge 8
+    tagsCur[0] = 1;
+    tagsCur[1] = 8;
+    tagsCur[2] = i-1;
+    tagsCur[3] = degree_-1;
+    tagsCur += tagSize;
+
+    for (int j=1;j<degree_;j++) {
+      // k == 0, z == -1, live on face 4
+      tagsCur[0] = 2;
+      tagsCur[1] = 4;
+      tagsCur[2] = (i-1)*(degree_-1)+(j-1);
+      tagsCur[3] = degm1sq;
+      tagsCur += tagSize;
+
+      // loop over interior k
+      for (int k=1;k<degree_;k++) {
+	tagsCur[0] = 3;
+	tagsCur[1] = 0;
+	tagsCur[2] = (i-1)*(degm1sq) + (j-1)*(degree_-1) + (k-1);
+	tagsCur[3] = degm1sq * (degree_-1);
+	tagsCur += tagSize;
+      }
+
+      // k == degree_-1, z == 1, live on face 5
+      tagsCur[0] = 2;
+      tagsCur[1] = 5;
+      tagsCur[2] = (i-1)*(degree_-1)+(j-1);
+      tagsCur[3] = degm1sq;
+      tagsCur += tagSize;
+    }
+
+    // now have to handle j == degree_-1, y == 1.
+    // k == 0 // z == -1, live on e2
+    tagsCur[0] = 1;
+    tagsCur[1] = 2;
+    tagsCur[2] = i-1;
+    tagsCur[3] = degree_-1;
+    tagsCur += tagSize;
+
+    // now go up the z axis (increase k along face 2)
+    for (int k=1;k<degree_;k++) {
+      tagsCur[0] = 2;
+      tagsCur[1] = 2;
+      tagsCur[2] = (i-1)*(degree_-1)+(k-1);
+      tagsCur[3] = degm1sq;
+      tagsCur += tagSize;
+    }
+
+    // now z == 1, edge 10
     tagsCur[0] = 1;
     tagsCur[1] = 10;
+    tagsCur[2] = i-1;
+    tagsCur[3] = degree_-1;
+    tagsCur += tagSize;    
+
+  }
+
+  // now do final i (x=1 plane)
+  // first fix y==-1 (j=0)
+  // this is vertex 1
+  tagsCur[0] = 0;
+  tagsCur[1] = 1;
+  tagsCur[2] = 0;
+  tagsCur[3] = 1;
+  tagsCur += tagSize;
+
+  // edge dof along edge 5
+  for (int k=1;k<degree_;k++) {
+    tagsCur[0] = 1;
+    tagsCur[1] = 5;
     tagsCur[2] = k-1;
-    tagsCur[3] = (degree_-1);
+    tagsCur[3] = degree_-1;
     tagsCur += tagSize;
   }
 
-  // finally do verex 6
+  // vertex 5
+  tagsCur[0] = 0;
+  tagsCur[1] = 5;
+  tagsCur[2] = 0;
+  tagsCur[3] = 1;
+  tagsCur += tagSize;
+
+  // interior lines of x=1 plane
+  for (int j=1;j<degree_;j++) {
+    // first vertex is on bottom (z=-1), edge 1
+    tagsCur[0] = 1;
+    tagsCur[1] = 1;
+    tagsCur[2] = j-1;
+    tagsCur[3] = degree_-1;
+    tagsCur += tagSize;
+
+    // go up the line in interior of face 1
+    for (int k=1;k<degree_;k++) {
+      tagsCur[0] = 2;
+      tagsCur[1] = 1;
+      tagsCur[2] = (j-1)*(degree_-1)+(k-1);
+      tagsCur[3] = degm1sq;
+      tagsCur += tagSize;
+    }
+
+    // last vertex is on top (z=1), edge 9
+    tagsCur[0] = 1;
+    tagsCur[1] = 9;
+    tagsCur[2] = j-1;
+    tagsCur[3] = degree_-1;
+    tagsCur += tagSize;
+  }
+
+  // edge 6 runs from v2 to v6 (1,1,-1) to (1,1,1)
+  tagsCur[0] = 0;
+  tagsCur[1] = 2;
+  tagsCur[2] = 0;
+  tagsCur[3] = 1;
+  tagsCur += tagSize;
+
+  // go up edge 6
+  for (int k=1;k<degree_;k++) {
+    tagsCur[0] = 1;
+    tagsCur[1] = 6;
+    tagsCur[2] = k-1;
+    tagsCur[3] = degree_-1;
+    tagsCur += 1;
+  }
+
+  // vertex 6
   tagsCur[0] = 0;
   tagsCur[1] = 6;
   tagsCur[2] = 0;
   tagsCur[3] = 1;
+  tagsCur += tagSize;
+
+
+//   // indices: k runs along points on an edge
+//   //          j runs over edges in a face
+//   //          i runs over faces in the cube
+
+//   // start bottom face k == 0
+
+//   // start at (-1,-1,-1), which is dimension 0, facet 0, dof 0, total dof on vertex, 1
+//   // it is also the case of i == j == k == 0;
+
+//   tagsCur[0] = 0;
+//   tagsCur[1] = 0;
+//   tagsCur[2] = 0;
+//   tagsCur[3] = 1;
+//   tagsCur += tagSize;
+
+//   // run along bottom edge from (-1,-1,-1) to (1,-1,-1), which is dimension 1, facet 0, degree_-1 dof on edge
   
-  // end top face (finally!)
+//   for (int k=1;k<degree_;k++) {
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 0;
+//     tagsCur[2] = k-1;
+//     tagsCur[3] = degree_-1;
+//     tagsCur += tagSize;
+//   }
+
+//   // now get vertex (1,-1,-1), which is dimension 0, facet 1, dof 0, dof on cell: 1
+//   tagsCur[0] = 0;
+//   tagsCur[1] = 1;
+//   tagsCur[2] = 0;
+//   tagsCur[3] = 1;
+//   tagsCur += tagSize;
+
+//   // now get middle rows on bottom face
+//   for (int j=1;j<degree_;j++) {
+//     // first node is on the edge from vertex 3 to vertex 0 (edge 3)
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 3;
+//     tagsCur[2] = j - 1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+
+//     // now do the interior vertices on the i:th row of the bottom face, which is dim 2, facet 4
+//     //
+//     for (int k=1;k<degree_;k++) {
+//       tagsCur[0] = 2;
+//       tagsCur[1] = 4;
+//       tagsCur[2] = (j-1)*(degree_-1) + (k-1);
+//       tagsCur[3] = (degree_-1)*(degree_-1);
+//       tagsCur += tagSize;
+//     }
+
+//     // the last node is on edge (1,2) (facet 1 of dimension 0), traversed forward
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 1;
+//     tagsCur[2] = j - 1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+//   }
+
+//   // now get vertex 3 (beginning of last row on bottom face)
+//   tagsCur[0] = 0;  // dim 0
+//   tagsCur[1] = 3;  // id: 3
+//   tagsCur[2] = 0;  // first dof
+//   tagsCur[3] = 1;  // one dof on the vertex
+//   tagsCur += tagSize;
+
+//   // get last edge on bottom face  This is edge 2, running from vertex 2 to vertex 3.
+
+//   for (int k=1;k<degree_;k++) {
+//     tagsCur[0] = 1; // dim 1
+//     tagsCur[1] = 2; // id: 2
+//     tagsCur[2] = k - 1; // dof on edge
+//     tagsCur[3] = degree_ - 1; // total number of nodes on edge
+//     tagsCur += tagSize;
+//   }
+
+//   // now get vertex 2 (end of last row on bottom face)
+//   tagsCur[0] = 0; // dim 0
+//   tagsCur[1] = 2; // id: 2
+//   tagsCur[2] = 0; // first dof on vertex
+//   tagsCur[3] = 1; // one dof on the vertex
+//   tagsCur += tagSize;
+
+//   // end bottom face
+
+//   // begin interior faces
+//   for (int i=1;i<degree_;i++) {
+//     // j == 0 lives on face y==-1, which is face 0
+
+//     // k == 0, node lives on edge 4
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 4;
+//     tagsCur[2] = i-1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+
+//     // interior nodes
+//     for (int k=1;k<degree_;k++) {
+//       tagsCur[0] = 2;
+//       tagsCur[1] = 0;
+//       tagsCur[2] = (i-1)*(degree_-1) + (k-1);
+//       tagsCur[3] = (degree_-1)*(degree_-1);
+//       tagsCur += tagSize;
+//     }    
+
+//     // k == degree, node lives on edge 5
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 5;
+//     tagsCur[2] = i-1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+
+//     // interior edges in i:th face
+//     for (int j=1;j<degree_;j++) { 
+//       // k == 0, node lives on face 3
+//       tagsCur[0] = 2;
+//       tagsCur[1] = 3;
+//       tagsCur[2] = (i-1)*(degree_-1) + (j-1);
+//       tagsCur[3] = (degree_-1)*(degree_-1);
+//       tagsCur += tagSize;
+
+//       // interior nodes on j:th edge
+//       for (int k=1;k<degree_;k++) {
+// 	tagsCur[0] = 3;
+// 	tagsCur[1] = 0;
+// 	tagsCur[2] = (i-1)*(degree_-1)*(degree_-1) + (j-1)*(degree_-1) + (k-1);
+// 	tagsCur[3] = (degree_-1)*(degree_-1)*(degree_-1);
+// 	tagsCur += tagSize;
+//       }
+
+//       // k == degree, node lives on face 1
+//       tagsCur[0] = 2;
+//       tagsCur[1] = 1;
+//       tagsCur[2] = (i-1)*(degree_-1)+(j-1);
+//       tagsCur[3] = (degree_-1)*(degree_-1);
+//       tagsCur += tagSize;
+      
+//     }
+//     // end interior j
+
+//     // j == degree lives on face y==1, which is number 2
+    
+//     // first node (k==0), lives on edge 7
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 7;
+//     tagsCur[2] = i-1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+
+//     // interior nodes live on face 2
+//     for (int k=1;k<degree_;k++) {
+//       tagsCur[0] = 2;
+//       tagsCur[1] = 2;
+//       tagsCur[2] = (i-1)*(degree_-1) + (k-1);
+//       tagsCur[3] = (degree_-1)*(degree_-1);
+//       tagsCur += tagSize;
+//     }
+
+//     // last node (k==degree) lives on edge 6
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 6;
+//     tagsCur[2] = i-1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+
+//     // end j
+//   }  
+//   // end interior faces
+
+//   // begin top face
+//   // start with vertex 4
+//   tagsCur[0] = 0;
+//   tagsCur[1] = 4;
+//   tagsCur[2] = 0;
+//   tagsCur[3] = 1;
+//   tagsCur += tagSize;
+
+//   // now do edge 8
+//   for (int k=1;k<degree_;k++) {
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 8;
+//     tagsCur[2] = (k-1);
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+//   }
+  
+//   // now do vertex 5
+//   tagsCur[0] = 0;
+//   tagsCur[1] = 5;
+//   tagsCur[2] = 0;
+//   tagsCur[3] = 1;
+//   tagsCur += tagSize;
+
+//   // now do interior edges of top face
+//   for (int j=1;j<degree_;j++) {
+//     // k==0, lives on edge 11
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 11;
+//     tagsCur[2] = j-1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+
+//     // interior nodes of edges
+//     for (int k=1;k<degree_;k++) {
+//       tagsCur[0] = 2;
+//       tagsCur[1] = 5;
+//       tagsCur[2] = (j-1)*(degree_-1) + (k-1);
+//       tagsCur[3] = (degree_-1)*(degree_-1);
+//       tagsCur += tagSize;
+//     }
+    
+//     // k == degree, node lives on edge 9
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 9;
+//     tagsCur[2] = j-1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+//   }
+//   // done with interior edges of top face.
+//   // do last edge of top face
+//   // vertex 7
+//   tagsCur[0] = 0;
+//   tagsCur[1] = 7;
+//   tagsCur[2] = 0;
+//   tagsCur[3] = 1;
+//   tagsCur += tagSize;
+
+//   // interior nodes on edge 10
+//   for (int k=1;k<degree_;k++) {
+//     tagsCur[0] = 1;
+//     tagsCur[1] = 10;
+//     tagsCur[2] = k-1;
+//     tagsCur[3] = (degree_-1);
+//     tagsCur += tagSize;
+//   }
+
+//   // finally do verex 6
+//   tagsCur[0] = 0;
+//   tagsCur[1] = 6;
+//   tagsCur[2] = 0;
+//   tagsCur[3] = 1;
+  
+//   // end top face (finally!)
 
 
   // Basis-independent function sets tag and enum data in the static arrays:
