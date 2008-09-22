915c780a010ef9cf36f02d7579333b6553d898bf
Roger P. Pawlowski <rppawlo@sandia.gov>
2008-09-22 23:29:14 +0000

    SUMMARY:   Fixed a problem with the overload of operator<< for the FieldTag.  Also fixed the copyright in some headers that were not updated correctly.
    (checklist version 2007-06-20)

--- packages/phalanx/src/Phalanx_FieldTag.cpp
+++ packages/phalanx/src/Phalanx_FieldTag.cpp
@@ -29,46 +29,13 @@
 // ************************************************************************
 // @HEADER
 
-#ifndef PHX_FIELDTAG_HPP
-#define PHX_FIELDTAG_HPP
+#include "Phalanx_FieldTag.hpp"
 
-#include "Phalanx_ConfigDefs.hpp"
-#include "Teuchos_RCP.hpp"
+//**********************************************************************
+std::ostream& PHX::operator<<(std::ostream& os, const PHX::FieldTag& t)
+{
+  t.print(os);
+  return os;
+}
 
-namespace PHX {
-
-  class DataLayout;
-
-  class FieldTag {
-
-  public:
-
-    FieldTag() {}
-    
-    virtual ~FieldTag() {}
-
-    virtual Teuchos::RCP<FieldTag> clone() const = 0;
-
-    virtual bool operator==(const FieldTag& t) const = 0;
-    
-    virtual bool operator!=(const FieldTag& t) const
-    { return !(*this == t); };
-    
-    virtual const std::string& name() const = 0;
-
-    virtual const PHX::DataLayout& dataLayout() const = 0;
-
-    virtual const std::type_info& dataTypeInfo() const = 0;
-    
-    //! Unique name identifier that can be used for strict weak ordering in stl std::map keys.
-    virtual const std::string identifier() const = 0;
-
-    virtual void print(std::ostream& os) const = 0;
-
-  };
-
-  std::ostream& operator<<(std::ostream& os, const PHX::FieldTag& t);
-  
-} 
-
-#endif 
+//**********************************************************************
