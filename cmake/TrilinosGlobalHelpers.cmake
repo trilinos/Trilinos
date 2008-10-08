18c96fc18997fddbb3836bd0b566503abf77d83d
Roscoe A. Bartlett <rabartl@sandia.gov>
2008-10-08 05:33:23 +0000

    Summary: Adding unit tests for dependency handling logic
    I have added some unit tests for the dependency handling capabilities.  The
    current set of packages that I have to work with is pretty small but I think
    there is enough here to keep the capability safe.  These unit tests are
    enabled by default on non-windows systems.  I did this because I am using a
    unix shell script and the chmod command.  I don't think it is important to run
    these unit tests on MS Windows.
    
    A few other things to note:
    
    (*) Generate_Script_File_Target.cmake: Defines a macro
    GENERATE_SCRIPT_FILE_TARGET(...) that makes in really easy to define a script
    target file that will get gnerated just given a script file name and a string
    containing the script.
    
    (*) Multiline_Set.cmake: Created a nice little helper script for creating a
    single SET(...) call using an array of string arguments.  This makes it much
    easier to format a multi-line string and still use indentation correctly."
    The only problem is that you should avoid ';' in the string text or it will
    have a fit because it thinks it is an array.  CMake is kind of messed up this
    way.
    
    I tested this with both serial debug and mpi optimized with cmake.

--- cmake/TrilinosGlobalHelpers.cmake
+++ cmake/TrilinosGlobalHelpers.cmake
@@ -9,9 +9,10 @@ INCLUDE(TrilinosHelpers)
 # Trilinos_ENABLE_${PACKAGE_NAME}, ${PACKAGE_NAME}_ENABLE_TESTS, and
 # ${PACKAGE_NAME}_ENABLE_EXAMPLES by just setting them to regular variables
 # instead of putting them back on the cache.  That means that they are used as
-# global varibles but we don't want to distrube the cache since that would
+# global varibles but we don't want to disturb the cache since that would
 # change the behavior for future invocations of cmake.  Because of this, these
-# macros must all be called from the top-level Trilinos CMakeLists.txt file.
+# macros must all be called from the top-level Trilinos CMakeLists.txt file
+# and macros must call macros as not to change the variable scope.
 #
 # I had to do it this way in order to be able to get the right behavior which
 # is:
@@ -22,6 +23,98 @@ INCLUDE(TrilinosHelpers)
 # would make it hard to change package enables/disable later without blowing
 # away the cache
 # 
+
+#
+# Macro that gets the current list of enables packages
+#
+# Accesses the global varaibles:
+#
+#   Trilinos_PACKAGES
+#   Trilinos_ENABLE_${PACKAGE}
+#
+# where ${PACKAGE} is every package in TRILINOS_PACKAGES_IN
+#
+
+MACRO(TRILINOS_GET_ENABLED_PACKAGES_LIST TRILINOS_ENABLED_PACKAGES_OUT NUM_ENABLED_OUT)
+  SET(${TRILINOS_ENABLED_PACKAGES_OUT} "")
+  SET(${NUM_ENABLED_OUT} 0)
+  FOREACH(PACKAGE ${Trilinos_PACKAGES})
+    ASSERT_DEFINED(Trilinos_ENABLE_${PACKAGE})
+    IF (Trilinos_ENABLE_${PACKAGE})
+      SET(${TRILINOS_ENABLED_PACKAGES_OUT} "${${TRILINOS_ENABLED_PACKAGES_OUT}} ${PACKAGE}")
+      MATH(EXPR ${NUM_ENABLED_OUT} "${${NUM_ENABLED_OUT}}+1")
+    ENDIF()
+  ENDFOREACH()
+ENDMACRO()
+
+
+#
+# Function that prints the current set of packages
+#
+FUNCTION(TRILINOS_PRINT_ENABLED_PACKAGE_LIST DOCSTRING)
+  TRILINOS_GET_ENABLED_PACKAGES_LIST(Trilinos_ENABLED_PACKAGES NUM_ENABLED)
+  MESSAGE("${DOCSTRING}: ${Trilinos_ENABLED_PACKAGES} ${NUM_ENABLED}")
+ENDFUNCTION()
+
+
+#
+# Macro that reads in package dependencies for a package and sets forward
+# dependencies for packages already read in.
+#
+# Modifies the global varibles:
+#
+#   ${PACKAGE_NAME}_LIB_REQUIRED_DEP_PACKAGES
+#   ${PACKAGE_NAME}_LIB_OPTIONAL_DEP_PACKAGES
+#   ${PACKAGE_NAME}_TEST_REQUIRED_DEP_PACKAGES
+#   ${PACKAGE_NAME}_TEST_OPTIONAL_DEP_PACKAGES
+#   ${PACKAGE_NAME}_FORWARD_LIB_REQUIRED_DEP_PACKAGES
+#   ${PACKAGE_NAME}_FORWARD_LIB_OPTIONAL_DEP_PACKAGES
+#   ${PACKAGE_NAME}_FORWARD_TEST_REQUIRED_DEP_PACKAGES
+#   ${PACKAGE_NAME}_FORWARD_TEST_OPTIONAL_DEP_PACKAGES
+#
+
+MACRO(TRILINOS_READ_PACKAGE_DEPENDENCIES PACKAGE_NAME PACKAGE_DIR)
+
+  SET(${PACKAGE_NAME}_FORWARD_LIB_REQUIRED_DEP_PACKAGES "")
+  SET(${PACKAGE_NAME}_FORWARD_LIB_OPTIONAL_DEP_PACKAGES "")
+  SET(${PACKAGE_NAME}_FORWARD_TEST_REQUIRED_DEP_PACKAGES "")
+  SET(${PACKAGE_NAME}_FORWARD_TEST_OPTIONAL_DEP_PACKAGES "")
+
+  INCLUDE(packages/${PACKAGE_DIR}/cmake/Dependencies.cmake)
+
+  SET(${PACKAGE_NAME}_LIB_REQUIRED_DEP_PACKAGES ${LIB_REQUIRED_DEP_PACKAGES})
+  SET(${PACKAGE_NAME}_LIB_OPTIONAL_DEP_PACKAGES ${LIB_OPTIONAL_DEP_PACKAGES})
+  SET(${PACKAGE_NAME}_TEST_REQUIRED_DEP_PACKAGES ${TEST_REQUIRED_DEP_PACKAGES})
+  SET(${PACKAGE_NAME}_TEST_OPTIONAL_DEP_PACKAGES ${TEST_OPTIONAL_DEP_PACKAGES})
+
+  TRILINOS_APPEND_FORWARD_DEP_PACKAGES(${PACKAGE_NAME} LIB_REQUIRED_DEP_PACKAGES)
+  TRILINOS_APPEND_FORWARD_DEP_PACKAGES(${PACKAGE_NAME} LIB_OPTIONAL_DEP_PACKAGES)
+  TRILINOS_APPEND_FORWARD_DEP_PACKAGES(${PACKAGE_NAME} TEST_REQUIRED_DEP_PACKAGES)
+  TRILINOS_APPEND_FORWARD_DEP_PACKAGES(${PACKAGE_NAME} TEST_OPTIONAL_DEP_PACKAGES)
+
+ENDMACRO()
+
+
+#
+# Macro that prints out dependencies for a package
+#
+# Does not modify the global state.
+#
+
+MACRO(TRILINOS_PRINT_PACKAGE_DEPENDENCIES PACKAGE_NAME)
+
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_LIB_REQUIRED_DEP_PACKAGES)
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_LIB_OPTIONAL_DEP_PACKAGES)
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_TEST_REQUIRED_DEP_PACKAGES)
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_TEST_OPTIONAL_DEP_PACKAGES)
+
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_FORWARD_LIB_REQUIRED_DEP_PACKAGES)
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_FORWARD_LIB_OPTIONAL_DEP_PACKAGES)
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_FORWARD_TEST_REQUIRED_DEP_PACKAGES)
+  PRINT_NONEMPTY_VAR(${PACKAGE_NAME}_FORWARD_TEST_OPTIONAL_DEP_PACKAGES)
+
+ENDMACRO()
+
 
 #
 # Macro that sets up standard user options for each package
@@ -299,12 +392,9 @@ MACRO(TRILINOS_APPLY_TEST_EXAMPLE_ENABLES PACKAGE_NAME)
 ENDMACRO()
 
 
-
-
 #
 # Private helper macro
 #
-
 
 MACRO(TRILINOS_PRIVATE_ENABLE_FORWARD_PACKAGE FORWARD_DEP_PACKAGE_NAME PACKAGE_NAME)
   # Enable the forward package if it is not already set to ON or OFF
@@ -345,7 +435,20 @@ ENDMACRO()
 
 
 #
-# Public macro
+# Private helper macro
+#
+
+MACRO(TRILINOS_PRIVATE_ENABLE_DEP_PACKAGE PACKAGE_NAME DEP_PACKAGE_NAME)
+  ASSERT_DEFINED(Trilinos_ENABLE_${DEP_PACKAGE_NAME})
+  IF(Trilinos_ENABLE_${DEP_PACKAGE_NAME} STREQUAL "")
+    MESSAGE(STATUS "Setting Trilinos_ENABLE_${DEP_PACKAGE_NAME}=ON because Trilinos_ENABLE_${PACKAGE_NAME}=ON")
+    ASSERT_DEFINED(Trilinos_ENABLE_${DEP_PACKAGE_NAME})
+    SET(Trilinos_ENABLE_${DEP_PACKAGE_NAME} ON)
+  ENDIF()
+ENDMACRO()
+
+#
+# Macro that sets the optional packages for given package
 #
 
 MACRO(TRILINOS_ENABLE_OPTIONAL_PACKAGES PACKAGE_NAME)
@@ -358,14 +461,11 @@ MACRO(TRILINOS_ENABLE_OPTIONAL_PACKAGES PACKAGE_NAME)
   IF (Trilinos_ENABLE_${PACKAGE_NAME})
 
     FOREACH(DEP_PKG ${${PACKAGE_NAME}_LIB_OPTIONAL_DEP_PACKAGES})
+      TRILINOS_PRIVATE_ENABLE_DEP_PACKAGE(${PACKAGE_NAME} ${DEP_PKG})
+    ENDFOREACH()
 
-      ASSERT_DEFINED(Trilinos_ENABLE_${DEP_PKG})
-      IF(Trilinos_ENABLE_${DEP_PKG} STREQUAL "")
-        MESSAGE(STATUS "Setting Trilinos_ENABLE_${DEP_PKG}=ON because Trilinos_ENABLE_${PACKAGE_NAME}=ON")
-        ASSERT_DEFINED(Trilinos_ENABLE_${DEP_PKG})
-        SET(Trilinos_ENABLE_${DEP_PKG} ON)
-      ENDIF()
-
+    FOREACH(DEP_PKG ${${PACKAGE_NAME}_TEST_OPTIONAL_DEP_PACKAGES})
+      TRILINOS_PRIVATE_ENABLE_DEP_PACKAGE(${PACKAGE_NAME} ${DEP_PKG})
     ENDFOREACH()
 
   ENDIF()
@@ -374,7 +474,7 @@ ENDMACRO()
 
 
 #
-# Public macro
+# Macro that sets the required packages for given package
 #
 
 MACRO(TRILINOS_ENABLE_REQUIRED_PACKAGES PACKAGE_NAME)
@@ -387,16 +487,88 @@ MACRO(TRILINOS_ENABLE_REQUIRED_PACKAGES PACKAGE_NAME)
   IF (Trilinos_ENABLE_${PACKAGE_NAME})
 
     FOREACH(DEP_PKG ${${PACKAGE_NAME}_LIB_REQUIRED_DEP_PACKAGES})
+      TRILINOS_PRIVATE_ENABLE_DEP_PACKAGE(${PACKAGE_NAME} ${DEP_PKG})
+    ENDFOREACH()
 
-      ASSERT_DEFINED(Trilinos_ENABLE_${DEP_PKG})
-      IF(Trilinos_ENABLE_${DEP_PKG} STREQUAL "")
-        MESSAGE(STATUS "Setting Trilinos_ENABLE_${DEP_PKG}=ON because Trilinos_ENABLE_${PACKAGE_NAME}=ON")
-        ASSERT_DEFINED(Trilinos_ENABLE_${DEP_PKG})
-        SET(Trilinos_ENABLE_${DEP_PKG} ON)
-      ENDIF()
-
+    FOREACH(DEP_PKG ${${PACKAGE_NAME}_TEST_REQUIRED_DEP_PACKAGES})
+      TRILINOS_PRIVATE_ENABLE_DEP_PACKAGE(${PACKAGE_NAME} ${DEP_PKG})
     ENDFOREACH()
 
   ENDIF()
+
+ENDMACRO()
+
+
+#
+# Macro that adjusts all of the package enables from what the user input
+# to the final set that will be used to enable packages
+#
+
+MACRO(TRILNOS_ADJUST_PACKAGE_ENABLES)
+
+  MESSAGE("")
+  MESSAGE("Disabling forward pacakges that have a required dependancy on explicitly disabled packages ...")
+  MESSAGE("")
+  FOREACH(PACKAGE ${Trilinos_PACKAGES})
+    TRILINOS_DISABLE_FORWARD_REQUIRED_DEP_PACKAGES(${PACKAGE})
+  ENDFOREACH()
+  
+  IF (Trilinos_ENABLE_ALL_PACKAGES)
+    MESSAGE("")
+    MESSAGE("Enabling all packages that are not currently disabled ...")
+    MESSAGE("")
+    FOREACH(PACKAGE ${Trilinos_PACKAGES})
+      TRILINOS_APPLY_ALL_PACKAGE_ENABLES(${PACKAGE})
+    ENDFOREACH()
+  ENDIF()
+  
+  IF (Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES)
+    MESSAGE("")
+    MESSAGE("Enabling all forward dependent packages ...")
+    MESSAGE("")
+    FOREACH(PACKAGE ${Trilinos_PACKAGES})
+      TRILINOS_ENABLE_FORWARD_PACKAGE_ENABLES(${PACKAGE})
+    ENDFOREACH()
+    SET(Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES ON)
+  ENDIF()
+  
+  IF (Trilinos_ENABLE_TESTS OR Trilinos_ENABLE_EXAMPLES)
+    MESSAGE("")
+    MESSAGE("Enabling all tests and examples that can be enabled ...")
+    MESSAGE("")
+    FOREACH(PACKAGE ${Trilinos_PACKAGES})
+      TRILINOS_APPLY_TEST_EXAMPLE_ENABLES(${PACKAGE})
+    ENDFOREACH()
+  ENDIF()
+  # NOTE: Above, we enable tests and examples here, before the remaining required
+  # packages so that we don't enable tests that don't need to be enabled based
+  # on the use of the option Trilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES.
+  
+  IF (Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES)
+    MESSAGE("")
+    MESSAGE("Enabling all optional packages for current set of enabled packages ...")
+    MESSAGE("")
+    FOREACH(PACKAGE ${Trilinos_REVERSE_PACKAGES})
+      TRILINOS_ENABLE_OPTIONAL_PACKAGES(${PACKAGE})
+    ENDFOREACH()
+  ENDIF()
+  # NOTE: Above, we have to loop through the packages backward to enable all the
+  # pacakges that feed into these packages.
+  # NOTE Above, we don't have to enable the required packages because that will
+  # come next
+  
+  MESSAGE("")
+  MESSAGE("Enabling all remaining required packages for the current set of enabled packages ...")
+  MESSAGE("")
+  FOREACH(PACKAGE ${Trilinos_REVERSE_PACKAGES})
+    TRILINOS_ENABLE_REQUIRED_PACKAGES(${PACKAGE})
+  ENDFOREACH()
+  
+  MESSAGE("")
+  MESSAGE("Enabling all optional package intrapackage enables that can be if both sets of packages are enabled ...")
+  MESSAGE("")
+  FOREACH(PACKAGE ${Trilinos_PACKAGES})
+    TRILINOS_POSTPROCESS_OPTIONAL_PACKAGE_ENABLES(${PACKAGE})
+  ENDFOREACH()
 
 ENDMACRO()
