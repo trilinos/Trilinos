7eb5d1504cfdfdfc59040831c2279eaff844dde2
Lee Ann Riesen <lafisk@sandia.gov>
2008-10-29 21:35:16 +0000

    SUMMARY:   adding more tests to test_geometric.cpp, and ensuring that we start out with a bad balance before repartitioning
         CHECKLIST:
    
         1)   Did you add any source files?  no
    
         2)   Did you update or change the build system? no
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  yes

--- packages/isorropia/test/geometric/test_geometric.cpp
+++ packages/isorropia/test/geometric/test_geometric.cpp
@@ -29,9 +29,11 @@
 // Read in the file "simple.coords" and perform geometric partitioning
 // using different coordinate weights and different Zoltan methods.
 //
-// Use --f={filename} to process a different file.  File should be a
-// text file containing 1, 2 or 3-dimensional coordinates, one per line,
-// with white space separating coordinate values.
+// --f={filename} will read a different coordinate file
+// --v will print out the partitioning (small coordinate files only)
+//
+// The input file should be a text file containing 1, 2 or 3-dimensional 
+// coordinates, one per line, with white space separating coordinate values.
 //
 
 #include <Isorropia_ConfigDefs.hpp>
@@ -39,14 +41,17 @@
 #include <Isorropia_EpetraPartitioner.hpp>
 #include <Isorropia_EpetraRedistributor.hpp>
 
+#define PRINTLIMIT 100
+
 #ifdef HAVE_EPETRA
-#include <Epetra_Import.h>
 #ifdef HAVE_MPI
 #include <Epetra_MpiComm.h>
 #else
 #include <Epetra_SerialComm.h>
 #endif
 #include <Epetra_MultiVector.h>
+#include <Epetra_Import.h>
+#include <Epetra_BlockMap.h>
 
 #include <Teuchos_CommandLineProcessor.hpp>
 #include <Teuchos_RCP.hpp>
@@ -60,6 +65,8 @@ static int localProc = 0;
 static int numProcs = 1;
 static int run_test(Teuchos::RCP<const Epetra_MultiVector> &coords,
                     Teuchos::RCP<const Epetra_MultiVector> &weights,
+                    const Teuchos::ParameterList &params);
+static int run_test(Teuchos::RCP<const Epetra_MultiVector> &coords,
                     const Teuchos::ParameterList &params);
 
 int main(int argc, char** argv) {
@@ -76,7 +83,7 @@ int main(int argc, char** argv) {
 #endif
 
   if (getenv("DEBUGME")){
-    std::cerr << localProc << " gdb test_rcb.exe " << getpid() << std::endl;
+    std::cerr << localProc << " gdb test_geometric.exe " << getpid() << std::endl;
     sleep(15);
   }
 
@@ -87,9 +94,12 @@ int main(int argc, char** argv) {
   Teuchos::CommandLineProcessor clp(false,true);
 
   std::string *inputFile = new std::string("simple.coords");
+  bool verbose = false;
 
-  clp.setOption( "f", inputFile, 
-                "Name of input matrix market file");
+  clp.setOption( "f", inputFile, "Name of coordinate input file");
+
+  clp.setOption( "v", "q", &verbose,
+                "Display coordinates and weights before and after partitioning.");
 
   Teuchos::CommandLineProcessor::EParseCommandLineReturn parse_return =
     clp.parse(argc,argv);
@@ -109,6 +119,7 @@ int main(int argc, char** argv) {
 
   // =============================================================
   // Open file of coordinates and distribute them across processes
+  // so they are unbalanced.
   // =============================================================
   
   Epetra_MultiVector *mv = ispatest::file2multivector(Comm, *inputFile);
@@ -118,100 +129,206 @@ int main(int argc, char** argv) {
       std::cerr << "Invalid input file " << *inputFile << std::endl;
     exit(1);
   }
-
+  
   if (localProc == 0){
-    std::cout << "Input file " << *inputFile << ", " ;
-    std::cout << dim << " dimensional coordinates" << std::endl;
+    std::cerr << "Found input file " << *inputFile << ", " ;
+    std::cerr << dim << " dimensional coordinates" << std::endl;
   }
 
-  Teuchos::RCP<const Epetra_MultiVector> coords = Teuchos::rcp(mv);
+  delete inputFile;
 
-  // =============================================================
-  //  Create a parameter list for Zoltan
-  // =============================================================
-
-  Teuchos::ParameterList params;
-  Teuchos::ParameterList &sublist = params.sublist("ZOLTAN");
-
-  //sublist.set("DEBUG_LEVEL", "1"); // Zoltan will print out parameters
-  //sublist.set("DEBUG_LEVEL", "5");   // proc 0 will trace Zoltan calls
-  //sublist.set("DEBUG_MEMORY", "2");  // Zoltan will trace alloc & free
+  int base = mv->Map().IndexBase();
+  int globalSize = mv->GlobalLength();
+  int myShare = 0;
+  int n = numProcs - 1;
   
-  sublist.set("OBJ_WEIGHT_DIM", "1");
+  if (n){
+    if (localProc < n){
+      int oneShare = globalSize / n;
+      int leftOver = globalSize - (n * oneShare);
+      myShare = oneShare + ((localProc < leftOver) ? 1 : 0);
+    }
+  }
+  else{
+    myShare = globalSize;
+  }
+
+  Epetra_BlockMap unbalancedMap(globalSize, myShare, 1, base, mv->Map().Comm());
+  Epetra_Import importer(unbalancedMap, mv->Map());
+  Epetra_MultiVector umv(unbalancedMap, dim);
+  umv.Import(*mv, importer, Insert);
+
+  delete mv;
+
+  Teuchos::RCP<const Epetra_MultiVector> coords = 
+    Teuchos::rcp(new const Epetra_MultiVector(umv));
 
   // =============================================================
   // Create some different coordinate weight vectors
   // =============================================================
 
-  Epetra_MultiVector *unitWgts = ispatest::makeWeights(mv->Map(), &ispatest::unitWeights);
-  Epetra_MultiVector *veeWgts = ispatest::makeWeights(mv->Map(), &ispatest::veeWeights);
-  Epetra_MultiVector *altWgts = ispatest::makeWeights(mv->Map(), &ispatest::alternateWeights);
+  Epetra_MultiVector *unitWgts = ispatest::makeWeights(coords->Map(), &ispatest::unitWeights);
+  Epetra_MultiVector *veeWgts = ispatest::makeWeights(coords->Map(), &ispatest::veeWeights);
+  Epetra_MultiVector *altWgts = ispatest::makeWeights(coords->Map(), &ispatest::alternateWeights);
 
   Teuchos::RCP<const Epetra_MultiVector> unit_weights_rcp = Teuchos::rcp(unitWgts);
   Teuchos::RCP<const Epetra_MultiVector> vee_weights_rcp = Teuchos::rcp(veeWgts);
   Teuchos::RCP<const Epetra_MultiVector> alt_weights_rcp = Teuchos::rcp(altWgts);
 
-  // =============================================================
-  // Run some tests, performing Zoltan geometric partitioning
-  // =============================================================
-
   if (localProc == 0){
-    std::cout << "RIB test with weights that alternate with even/odd process ranks" << std::endl;
-  }
-  sublist.set("LB_METHOD", "RIB");
-
-  fail = run_test(coords, alt_weights_rcp, params);
-
-  if (fail) goto failure; 
-
-  if (localProc == 0){
-    std::cout << "PASS" << std::endl;
+    std::cerr << "Unit weights: Each object has weight 1.0" << std::endl;
+    std::cerr << "V weights: Low and high GIDs have high weights, center GIDs have low weights" << std::endl;
+    std::cerr << "Alternate weights: Objects on even rank processes have one weight, on odd another weight" << std::endl;
+    std::cerr << std::endl;
   }
 
-  // *************************************************************
+  // ======================================================================
+  //  Create a parameter list for Zoltan, and one for internal partitioning
+  // ======================================================================
 
+  Teuchos::ParameterList internalParams;
+
+  internalParams.set("PARTITIONING_METHOD", "SIMPLE_LINEAR");
+  
+
+  Teuchos::ParameterList zoltanParams;
+
+  Teuchos::ParameterList sublist = zoltanParams.sublist("ZOLTAN");
+
+  //sublist.set("DEBUG_LEVEL", "1"); // Zoltan will print out parameters
+  //sublist.set("DEBUG_LEVEL", "5");   // proc 0 will trace Zoltan calls
+  //sublist.set("DEBUG_MEMORY", "2");  // Zoltan will trace alloc & free
+
+  // =============================================================
+  // Run some tests
+  // =============================================================
   sublist.set("LB_METHOD", "RCB");
 
   if (localProc == 0){
-    std::cout << "RCB test with unit weights" << std::endl;
+    std::cerr << "RCB - unit weights" << std::endl;
   }
 
-  fail = run_test(coords, unit_weights_rcp, params);
+  fail = run_test(coords, unit_weights_rcp, zoltanParams);
 
   if (fail) goto failure; 
 
   if (localProc == 0){
-    std::cout << "PASS" << std::endl;
+    std::cerr << "PASS" << std::endl << std::endl;
   }
-
   // *************************************************************
 
   if (localProc == 0){
-    std::cout << "HSFC test with higher weights for low and high process ranks " << std::endl;
+    std::cerr << "HSFC - V weights" << std::endl;
   }
 
   sublist.set("LB_METHOD", "HSFC");
 
-  fail = run_test(coords, vee_weights_rcp, params);
+  fail = run_test(coords, vee_weights_rcp, zoltanParams);
 
   if (fail) goto failure; 
 
   if (localProc == 0){
-    std::cout << "PASS" << std::endl;
+    std::cerr << "PASS" << std::endl << std::endl;
+  }
+
+  // *************************************************************
+
+  if (localProc == 0){
+    std::cerr << "RIB - alternate weights" << std::endl;
+  }
+  sublist.set("LB_METHOD", "RIB");
+
+  fail = run_test(coords, alt_weights_rcp, zoltanParams);
+
+  if (fail) goto failure; 
+
+  if (localProc == 0){
+    std::cerr << "PASS" << std::endl << std::endl;
+  }
+
+  // *************************************************************
+
+  if (localProc == 0){
+    std::cerr << "RIB - no weights supplied" << std::endl;
+  }
+  sublist.set("LB_METHOD", "RIB");
+
+  fail = run_test(coords, zoltanParams);
+
+  if (fail) goto failure; 
+
+  if (localProc == 0){
+    std::cerr << "PASS" << std::endl << std::endl;
+  }
+
+  // *************************************************************
+
+  if (localProc == 0){
+    std::cerr << "No Zoltan - unit weights" << std::endl;
+  }
+
+  fail = run_test(coords, unit_weights_rcp, internalParams);
+
+  if (fail) goto failure; 
+
+  if (localProc == 0){
+    std::cerr << "PASS" << std::endl << std::endl;
+  }
+
+  // *************************************************************
+
+  if (localProc == 0){
+    std::cerr << "No Zoltan - V weights" << std::endl;
+  }
+
+  fail = run_test(coords, vee_weights_rcp, internalParams);
+
+  if (fail) goto failure; 
+
+  if (localProc == 0){
+    std::cerr << "PASS" << std::endl << std::endl;
+  }
+  // *************************************************************
+
+  if (localProc == 0){
+    std::cerr << "No Zoltan - alternate weights" << std::endl;
+  }
+
+  fail = run_test(coords, alt_weights_rcp, internalParams);
+
+  if (fail) goto failure; 
+
+  if (localProc == 0){
+    std::cerr << "PASS" << std::endl << std::endl;
+  }
+
+  // *************************************************************
+
+  if (localProc == 0){
+    std::cerr << "No Zoltan - no weights supplied" << std::endl;
+  }
+
+  fail = run_test(coords, internalParams);
+
+  if (fail) goto failure; 
+
+  if (localProc == 0){
+    std::cerr << "PASS" << std::endl << std::endl;
   }
 
   // *************************************************************
 
   goto done;
 
+
 failure:
 
   if (localProc == 0){
-    std::cout << "FAIL: test failed" << std::endl;
+    std::cerr << "FAIL: test failed" << std::endl;
   }
 
-
 done:
+
 #ifdef HAVE_MPI
   MPI_Finalize();
 #endif
@@ -219,17 +336,10 @@ done:
   return fail;
 }
 
-static int run_test(Teuchos::RCP<const Epetra_MultiVector> &coords,
+static int check_test(Teuchos::RCP<const Epetra_MultiVector> &coords,
                     Teuchos::RCP<const Epetra_MultiVector> &weights,
-                    const Teuchos::ParameterList &params)
+                    Teuchos::RCP<Isorropia::Epetra::Partitioner> &part)
 {
-  // =============================================================
-  // Create a partitioner, by default this will perform the partitioning as well
-  // =============================================================
-
-  Teuchos::RCP<Isorropia::Epetra::Partitioner> part =
-    Teuchos::rcp(new Isorropia::Epetra::Partitioner(coords, weights, params));
-
   // Create a Redistributor based on the partitioning
 
   Isorropia::Epetra::Redistributor rd(part);
@@ -255,18 +365,46 @@ static int run_test(Teuchos::RCP<const Epetra_MultiVector> &coords,
   ispatest::compute_balance(*before, goal, min1, max1, avg1);
 
   if (localProc == 0){
-    std::cout << "Balance before partitioning: min " ;
-    std::cout << min1 << " max " << max1 << " average " << avg1 << std::endl;
+    std::cerr << "Balance before partitioning: min " ;
+    std::cerr << min1 << " max " << max1 << " average " << avg1 << std::endl;
   }
 
   ispatest::compute_balance(*after, goal, min2, max2, avg2);
 
   if (localProc == 0){
-    std::cout << "Balance after partitioning:  min ";
-    std::cout << min2 << " max " << max2 << " average " << avg2 << std::endl;
+    std::cerr << "Balance after partitioning:  min ";
+    std::cerr << min2 << " max " << max2 << " average " << avg2 << std::endl;
   }
 
   return (avg2 > avg1);
+}
+static int run_test(Teuchos::RCP<const Epetra_MultiVector> &coords,
+                    Teuchos::RCP<const Epetra_MultiVector> &weights,
+                    const Teuchos::ParameterList &params)
+{
+
+  Teuchos::RCP<Isorropia::Epetra::Partitioner> part =
+    Teuchos::rcp(new Isorropia::Epetra::Partitioner(coords, weights, params));
+
+  return check_test(coords, weights, part);
+}
+
+static int run_test(Teuchos::RCP<const Epetra_MultiVector> &coords,
+                    const Teuchos::ParameterList &params)
+{
+  Teuchos::RCP<Isorropia::Epetra::Partitioner> part =
+    Teuchos::rcp(new Isorropia::Epetra::Partitioner(coords, params));
+
+  // both Zoltan and Isorropia partitioners will use unit weights,
+  // so we create unit weights to compute balances
+
+  Epetra_MultiVector tmp(coords->Map(), 1);
+  tmp.PutScalar(1.0);
+
+  Teuchos::RCP<const Epetra_MultiVector> unitweights = 
+    Teuchos::rcp(new const Epetra_MultiVector(tmp));
+
+  return check_test(coords, unitweights, part);
 }
 
 #endif   // HAVE_EPETRA
