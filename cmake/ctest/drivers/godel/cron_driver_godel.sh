a7cd8b84e7dfaefa623daf0b13ef4a907da910fa
Roscoe A. Bartlett <rabartl@sandia.gov>
2008-10-30 04:28:14 +0000

    Making URL to dashbaord more specific.

--- cmake/ctest/drivers/godel/cron_driver_godel.sh
+++ cmake/ctest/drivers/godel/cron_driver_godel.sh
@@ -9,4 +9,4 @@ cvs -q update -dP
 
 ctest -S /home/rabartl/PROJECTS/dashboards/Trilinos/SERIAL_DEBUG/Trilinos/cmake/ctest/ctest_linux_nightly_serial_debug_godel.cmake
 
-$HOME/mailmsg.py "Trilinos serial debug on thumper: http://trilinos.sandia.gov/cdash"
+$HOME/mailmsg.py "Trilinos serial debug on godel: http://trilinos.sandia.gov/cdash/index.php?project=Trilinos"
