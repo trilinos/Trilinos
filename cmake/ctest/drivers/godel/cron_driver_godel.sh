97f5ac4f38636e3f7b134906beba59c09b1ac8c4
Roscoe A. Bartlett <rabartl@sandia.gov>
2008-10-31 16:41:55 +0000

    Added package dependency testing.

--- cmake/ctest/drivers/godel/cron_driver_godel.sh
+++ cmake/ctest/drivers/godel/cron_driver_godel.sh
@@ -5,13 +5,21 @@ cd $HOME
 source .bash_profile
 
 echo
-echo "Checking out just the drivers"
+echo "A) Checking out just the drivers"
 echo
 
 BASEDIR=/home/rabartl/PROJECTS/dashboards/Trilinos
 
 cd $BASEDIR
 cvs -q -d :ext:@software.sandia.gov:/space/CVS co -d scripts Trilinos/cmake/ctest
+
+echo
+echo "Doing dependency checking-only build"
+echo
+
+time /usr/local/bin/ctest -S $BASEDIR/scripts/ctest_linux_nightly_package_deps_godel.cmake -VV
+
+/home/rabartl/mailmsg.py "Trilinos dependency checking on godel: http://trilinos.sandia.gov/cdash/index.php?project=Trilinos"
 
 echo
 echo "Doing serial debug build"
