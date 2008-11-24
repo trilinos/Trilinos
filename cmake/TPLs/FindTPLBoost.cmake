3e9be3f6881c14ea24a86e838b98b0ff2d097a32
Roscoe A. Bartlett <rabartl@sandia.gov>
2008-11-24 21:49:45 +0000

    Beginnings of offical CMake external library TPL support.

---
+++ cmake/TPLs/FindTPLBoost.cmake
@@ -0,0 +1,45 @@
+INCLUDE(Assert_Defined)
+
+
+IF (TPL_Boost_INCLUDE_DIRS)
+
+  # The Boost library has already been found or the user has specified
+  # these manually.  In this case, just make sure that everything has been
+  # specified correctly
+
+  # Make sure the other variables have been defined
+  ASSERT_DEFINED(TPL_Boost_LIBRARY_DIRS)
+  ASSERT_DEFINED(TPL_Boost_LIBRARIES)
+
+  # Verify that indeed we have found Boost!
+  # ToDo: Implement
+
+ELSE()
+
+  # Otherwise, we need to look for the Boost headers and libraries
+
+  FIND_PACKAGE(Boost) # This is the CMake built-in FindBoost module
+
+  IF (Boost_FOUND)
+
+     ASSERT_DEFINED(Boost_INCLUDE_DIRS)
+     SET(TPL_Boost_INCLUDE_DIRS ${Boost_INCLUDE_DIRS} CACHE STRING
+       "Boost include directories" )
+
+     ASSERT_DEFINED(Boost_LIBRARY_DIRS)
+     SET(TPL_Boost_LIBRARY_DIRS ${Boost_LIBRARY_DIRS} CACHE STRING
+       "Boost library directories" )
+
+     SET(TPL_Boost_LIBRARIES ${Boost_LIBRARIES} CACHE STRING
+       "Boost library directories" )
+     # ToDo: Add whatever boost libraries you want above!  Or, the user
+     # can add them!
+    
+  ELSE()
+
+    MESSAGE(FATAL_ERROR "Error, could not find the boost Library!")
+
+  ENDIF()
+  
+
+ENDIF()
