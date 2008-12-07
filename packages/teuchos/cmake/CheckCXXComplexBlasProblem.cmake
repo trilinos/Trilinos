20f85340d7779ebc78db86489c749ce1fe5dae17
Roscoe A. Bartlett <rabartl@sandia.gov>
2008-12-07 01:17:41 +0000

    Summary: Adding configure-time testing for complex BLAS problem
    Bugs: 4238
    
    The main thing I did here was to add CMake configure-time tests for
    the complex blas function problem that Chris Baker had put into the
    autotools build system.  This was developed and tested on
    godel.sandia.gov where the complex blas function return works fine.  I
    will now take this to gabriel.sandia.gov where it does not.
    
    I also removed the need to confugre a FortranMangle.h file.  It is
    just as easy to define the F77_BLAS_MANGLE macro right in source code
    string.  Now these BLAS tests are contined in just one file.
    
    Specific changes:
    
    (*) teuchos/CMakeLists.txt,
    teuchos/cmake/CheckCXXComplexBlasProblem.cmake,
    teuchos/cmake/CheckCXXComplexBlasProblemCanBeFixed.cmake: Added
    Complex BLAS test and fix check.
    
    (*) teuchos/cmake/CheckBlasFloat.cmake: Removed need for
    FortranMangle.h
    
    (*) teuchos/cmake/FortranMangle.h.in: No longer needed
    
    (*) packages/teuchos/cmake/TeuchosConfig.cmake.in: Has not been needed
    for a long time.
    
    (*) zoltan/CMakeLists.txt: Removed unnecessary and ignored enables for
    ParMETIS and Sctoch.  Added test-only enable for Zoltan drivers.
    
    (*) zoltan/src/CMakeLists.txt: Removed build of drivers.  These are
    now only built as part of tests.
    
    (*) packages/zoltan/test/ch_simple/CMakeLists.txt: Changed include to
    just Trilinos_Add_Test.
    
    (*) cmake/HOWTO.CMAKEIFY_PACKAGE: Added note about how optional
    intra-package enable cache variables are automatically added at
    framework level.
    
    (*) cmake/TODO: More items
    
    Tested with:
    
    - CMake serial debug only reconfigure
    
    - CMake mpi opt rebuild (all 165 tests ran passed)

---
+++ packages/teuchos/cmake/CheckCXXComplexBlasProblem.cmake
@@ -0,0 +1,43 @@
+INCLUDE(CheckCXXSourceRuns)
+
+FUNCTION(CHECK_CXX_COMPLEX_BLAS_PROBLEM VARNAME)
+
+  SET(CMAKE_REQUIRED_LIBRARIES ${TPL_BLAS_LIBRARIES})
+
+  SET(SOURCE
+  "
+#include <complex>
+
+#define F77_BLAS_MANGLE${F77_BLAS_MANGLE}
+
+extern \"C\" {
+  std::complex<float> F77_BLAS_MANGLE(cdotc,CDOTC)(
+    const int* n, const  std::complex<float> x[], const int* incx,
+    const std::complex<float> y[], const int* incy); 
+}
+
+int main() {
+  const int NUM=2;
+  const int INC=1;
+  std::complex<float> f[NUM];
+  const std::complex<float>
+    ONE = std::complex<float>(1.0,0.0),
+    TWO = std::complex<float>(2.0,0.0);
+  f[0] =  ONE;
+  f[1] =  ONE;
+  const std::complex<float> ret =
+    F77_BLAS_MANGLE(cdotc,CDOTC)(&NUM, f, &INC, f, &INC);
+  return (ret == TWO ? 0 : 1);
+}
+  "
+  )
+  
+  CHECK_CXX_SOURCE_RUNS("${SOURCE}" CXX_COMPLEX_BLAS_WORKS)
+
+  IF (CXX_COMPLEX_BLAS_WORKS)
+    SET(${VARNAME} FALSE PARENT_SCOPE)
+  ELSE()
+    SET(${VARNAME} TRUE PARENT_SCOPE)
+  ENDIF()
+  
+ENDFUNCTION()
