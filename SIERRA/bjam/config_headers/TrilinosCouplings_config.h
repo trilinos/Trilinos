77ca5ad3eed5822b485d2c45eda5f9c2627e2364
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-02-11 19:25:20 +0000

    Removed some worthless autotools macros that case the SIERRA + Trilinos Dev build to break with the BJAM build system.  The build now works.  Since this only affects the BJAM system there is not need to test Trilinos with the native build system.

--- SIERRA/bjam/config_headers/TrilinosCouplings_config.h
+++ SIERRA/bjam/config_headers/TrilinosCouplings_config.h
@@ -12,18 +12,3 @@
 
 /* Define if want to build nox */
 #define HAVE_NOX 
-
-/* Define to the address where bug reports for this package should be sent. */
-#define PACKAGE_BUGREPORT "jhu@sandia.gov"
-
-/* Define to the full name of this package. */
-#define PACKAGE_NAME "trilinoscouplings"
-
-/* Define to the full name and version of this package. */
-#define PACKAGE_STRING "trilinoscouplings 1.1d"
-
-/* Define to the one symbol short name of this package. */
-#define PACKAGE_TARNAME "trilinoscouplings"
-
-/* Define to the version of this package. */
-#define PACKAGE_VERSION "1.1d"
