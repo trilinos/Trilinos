dfd2f8e10ae19f76f4dd195cc71aade2972f9ece
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-02-14 23:39:14 +0000

    Fixing header name.  Now this seems to work with Sundance.

--- cmake/TPLs/FindTPLExodusII.cmake
+++ cmake/TPLs/FindTPLExodusII.cmake
@@ -1,6 +1,7 @@
 INCLUDE(TPLDeclareLibraries)
 
 TPL_DECLARE_LIBRARIES( ExodusII
-  REQUIRED_HEADERS exodus.h
+  #REQUIRED_HEADERS exodus.h
+  REQUIRED_HEADERS exodusII.h
   REQUIRED_LIBS_NAMES exoIIv2c netcdf
   )
