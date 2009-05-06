9896f5edcc43ba64f1eb5ef8c4c98e69a149943f
Eric C. Cyr <eccyr@sandia.gov>
2009-05-06 13:15:01 +0000

    Usage example for a Epetra_Strided operator

---
+++ packages/teko/examples/StridedSolve/strided_solve.cpp
@@ -0,0 +1,138 @@
+#include "Teuchos_ConfigDefs.hpp"
+#include "Teuchos_MPISession.hpp"
+#include "Teuchos_GlobalMPISession.hpp"
+#include "Teuchos_FancyOStream.hpp"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_XMLParameterListHelpers.hpp"
+
+// Thyra includes
+#include "Thyra_LinearOpBase.hpp"
+#include "Thyra_LinearOpWithSolveFactoryHelpers.hpp"
+#include "Thyra_PreconditionerFactoryHelpers.hpp"
+#include "Thyra_DefaultMultipliedLinearOp.hpp"
+#include "Thyra_DefaultScaledAdjointLinearOp.hpp"
+#include "Thyra_DefaultBlockedLinearOp.hpp"
+#include "Thyra_DefaultPreconditioner.hpp"
+#include "Thyra_DefaultProductVector.hpp"
+#include "Thyra_DefaultBlockedLinearOp.hpp"
+
+// include basic Epetra information
+#ifdef HAVE_MPI
+   #include "Epetra_MpiComm.h"
+   #include "mpi.h"
+#else
+   #include "Epetra_SerialComm.h"
+#endif
+#include "Epetra_Map.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_Vector.h"
+#include "Epetra_LinearProblem.h"
+#include "Epetra_Export.h"
+
+// EpetraExt 
+#include "EpetraExt_CrsMatrixIn.h"
+#include "EpetraExt_VectorIn.h"
+#include "EpetraExt_VectorOut.h"
+#include "EpetraExt_MatrixMatrix.h"
+#include "EpetraExt_RowMatrixOut.h"
+
+// Thyra-Epetra adapter includes
+#include "Thyra_EpetraLinearOp.hpp"
+#include "Thyra_EpetraThyraWrappers.hpp"
+#include "Thyra_get_Epetra_Operator.hpp"
+#include "Thyra_DefaultBlockedLinearOp.hpp"
+
+// PB-Package includes
+#include "PB_Utilities.hpp"
+#include "PB_InverseFactory.hpp"
+#include "NS/PB_SIMPLEPreconditionerFactory.hpp"
+#include "NS/PB_LSCPreconditionerFactory.hpp"
+#include "Epetra/PB_StridedEpetraOperator.hpp"
+#include "Epetra/PB_EpetraBlockPreconditioner.hpp"
+
+// Aztec includes
+#include "AztecOO.h"
+#include "AztecOO_Operator.h"
+
+#include <EcUtils++/directory.h>
+
+#include <iostream>
+#include <fstream>
+#include <cmath>
+
+using Teuchos::RCP;
+using Teuchos::rcp;
+using Teuchos::rcp_dynamic_cast;
+using Teuchos::null;
+using Teuchos::ParameterList;
+
+int main(int argc,char * argv[])
+{
+   // calls MPI_Init and MPI_Finalize
+   Teuchos::GlobalMPISession mpiSession(&argc,&argv);
+
+   // Handles some I/O to the output screen
+   RCP<Teuchos::FancyOStream> out = Teuchos::VerboseObjectBase::getDefaultOStream();
+
+   // build global (or serial communicator
+   #ifdef HAVE_MPI
+      Epetra_MpiComm Comm(MPI_COMM_WORLD);
+   #else
+      Epetra_SerialComm Comm;
+   #endif
+ 
+   // get process information
+   int numProc = Comm.NumProc();
+   int myPID   = Comm.MyPID();
+
+   // output garbage
+   *out << "Approaching Barrier: proc = " << numProc << ", pid = " << myPID << std::endl;
+   Comm.Barrier();
+
+   RCP<Teuchos::ParameterList> paramList = Teuchos::getParametersFromXmlFile("solverparams.xml");
+
+   Epetra_Map map(15444,0,Comm);
+   Epetra_CrsMatrix * ptrA = 0;
+   Epetra_Vector * ptrf = 0;
+   Epetra_Vector * ptrx = 0;
+
+   std::cout << "Reading matrix market file" << std::endl;
+   EpetraExt::MatrixMarketFileToCrsMatrix("./jac_test.mm",map,map,map,ptrA);
+   EpetraExt::MatrixMarketFileToVector("./rhs_test.mm",map,ptrf);
+   EpetraExt::MatrixMarketFileToVector("./lhs_test.mm",map,ptrx);
+
+   RCP<Epetra_CrsMatrix> A = rcp(ptrA);
+   RCP<Epetra_Vector> b = rcp(ptrf);
+   RCP<Epetra_Vector> x = rcp(ptrx);
+
+   std::cout << "Building strided operator" << std::endl;
+   std::vector<int> vec(2); vec[0] = 2; vec[1] = 1;
+   PB::Epetra::StridedEpetraOperator sA(vec,A);
+
+   double alpha = 0.9;
+   RCP<const PB::InverseFactory> inverse = PB::invFactoryFromParamList(*paramList,"Amesos");
+#if 0
+   RCP<PB::BlockPreconditionerFactory> precFact 
+         = rcp(new PB::NS::SIMPLEPreconditionerFactory(inverse,alpha));
+#else
+   RCP<PB::NS::LSCStrategy> precStrat = rcp(new PB::NS::InvLSCStrategy(inverse));
+   RCP<PB::BlockPreconditionerFactory> precFact = rcp(new PB::NS::LSCPreconditionerFactory(precStrat));
+#endif
+
+   PB::Epetra::EpetraBlockPreconditioner prec(precFact);
+   prec.buildPreconditioner(sA);
+
+   Epetra_LinearProblem problem(&*A,&*x,&*b);
+
+   // build solver
+   AztecOO::AztecOO solver(problem);
+   solver.SetAztecOption(AZ_solver,AZ_gmres);
+   solver.SetAztecOption(AZ_precond,AZ_none);
+   solver.SetAztecOption(AZ_kspace,50);
+   solver.SetAztecOption(AZ_output,10);
+   solver.SetPrecOperator(&prec);
+
+   solver.Iterate(1000,1e-5);
+
+   return 0;
+}
