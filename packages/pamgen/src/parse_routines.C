e31d14f8a44a3e12d5f9ed29ca3447d915c84794
David M. Hensinger <dmhensi@sandia.gov>
2009-06-15 15:08:18 +0000

    fixing long long constants to have proper format

--- packages/pamgen/src/parse_routines.C
+++ packages/pamgen/src/parse_routines.C
@@ -79,7 +79,7 @@ std::string EnglishNumber(long long the_number) {
   // set up our core numbers from which other numbers can be constructed
   const long long numbers[] = {0,1,2,3,4,5,6,7,8,9,
 			 10,11,12,13,14,15,16,17,18,19,20,30,40,50,60,70,80,90,
-			 100,1000,1000000,1000000000,1000000000000};
+			 100,1000,1000000LL,1000000000LL,1000000000000LL};
   
   const std::string english[] = {"zero", "one", "two", "three", "four", "five", "six",
 				 "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen",
