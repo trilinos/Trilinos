5d17301568b3d2d034155b9a61ade8a046c416a9
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-06-18 18:06:44 +0000

    Disabling Zoltan tests on gabriel and checking in other changes I forgot to commit for testing on gabriel.

--- cmake/ctest/drivers/gabriel/cron_driver_gabriel.sh
+++ cmake/ctest/drivers/gabriel/cron_driver_gabriel.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-# Source the 
+# Source the env
 cd $HOME
 source .bash_profile
 
+# Set what ctest to use
 CTEST_EXE=/usr/local/bin/ctest
 
 
@@ -11,12 +12,15 @@ echo
 echo "Starting nightly Trilinos testing on gabriel: `date`"
 echo
 
+BASEDIR=`echo $0 | sed "s/\(.*\)\/Trilinos\/cmake\/ctest\/.*\.sh/\1/g"`
+echo "BASEDIR = $BASEDIR"
+DRIVER_SCRIPT_DIR=$BASEDIR/Trilinos/cmake/ctest/drivers/gabriel
+echo "DRIVER_SCRIPT_DIR = $DRIVER_SCRIPT_DIR"
+
 
 echo
 echo "Checking out just the skeleton cmake/ctest code: `date`"
 echo
-
-BASEDIR=/home/rabartl/PROJECTS/dashboards/Trilinos.base
 
 cd $BASEDIR
 cvs -q -d :ext:software:/space/CVS co Trilinos/cmake Trilinos/CTestConfig.cmake
@@ -26,7 +30,7 @@ echo
 echo "Doing mpi optimized build: `date`"
 echo
 
-time ${CTEST_EXE} -S $BASEDIR/Trilinos/cmake/ctest/drivers/gabriel/ctest_linux_nightly_mpi_optimized_gabriel.cmake -VV \
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_mpi_optimized_gabriel.cmake -VV \
   &> $BASEDIR/ctest_linux_nightly_mpi_optimized_gabriel.out
 
 
@@ -41,7 +45,7 @@ echo
 echo "Doing serial debug build: `date`"
 echo
 
-time ${CTEST_EXE} -S $BASEDIR/Trilinos/cmake/ctest/drivers/gabriel/ctest_linux_nightly_serial_debug_gabriel.cmake -VV \
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_serial_debug_gabriel.cmake -VV \
   &> $BASEDIR/ctest_linux_nightly_serial_debug_gabriel.out
 
 
@@ -50,4 +54,4 @@ echo "Ending nightly Trilinos testing on gabriel: `date`"
 echo
 
 
-/home/rabartl/mailmsg.py "Finished nightly Trilinos tests gabriel: http://trilinos-dev.sandia.gov/cdash/index.php?project=Trilinos"
+/home/rabartl/mailmsg.py "Finished nightly Trilinos CMake tests gabriel: http://trilinos-dev.sandia.gov/cdash/index.php?project=Trilinos"
