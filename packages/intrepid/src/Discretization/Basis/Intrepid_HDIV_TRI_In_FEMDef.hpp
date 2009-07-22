e26dba5c4a2f429ce834e28a7d5886b68b89d515
Robert C. Kirby <robert.c.kirby@ttu.edu>
2009-07-22 21:23:41 +0000

    SUMMARY:   [added H(div) basis, first kind on triangles]
         BUGS:      [???]
    
         CHECKLIST:
    
         1)   Did you add any source files?  [yes]
         1.a) Did you do a 'cvs add'?  [yes]
         1.b) Did you add them to a Makefile.am file in some way?  [cmake]
    
         2)   Did you update or change the build system?  [yes, cmake]
              executables and then rebuild?  [yes]
    
         3) Did you rerun the test suite on at least the packages that you
            changed?  [yes]
    
         COMMENTS:
    
         [???]
    
    (checklist version 2007-06-20)

---
+++ packages/intrepid/src/Discretization/Basis/Intrepid_HDIV_TRI_In_FEMDef.hpp
@@ -0,0 +1,351 @@
+// @HEADER
+// ************************************************************************
+//
+//                           Intrepid Package
+//                 Copyright (2007) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Pavel Bochev (pbboche@sandia.gov) or
+//                    Denis Ridzal (dridzal@sandia.gov).
+//
+// ************************************************************************
+// @HEADER
+
+/** \file   Intrepid_HDIV_TRI_In_FEM_Def.hpp
+    \brief  Definition file for FEM basis functions (Raviart-Thomas)
+    of degree n for H(div) functions on TRI.  
+    \author Created by R. Kirby.
+*/
+
+namespace Intrepid {
+
+  template<class Scalar, class ArrayScalar>
+  Basis_HDIV_TRI_In_FEM<Scalar,ArrayScalar>::Basis_HDIV_TRI_In_FEM( const int n ,
+								    const EPointType pointType ):
+    Phis( n ),
+    coeffs( (n+1)*(n+2) , n*(n+2) )
+  {
+    const int N = n*(n+2);
+    this -> basisCardinality_  = N;
+    this -> basisDegree_       = n;
+    this -> basisCellTopology_ = shards::CellTopology(shards::getCellTopologyData<shards::Triangle<3> >() );
+    this -> basisType_         = BASIS_FEM_FIAT;
+    this -> basisCoordinates_  = COORDINATES_CARTESIAN;
+    this -> basisTagsAreSet_   = false;
+
+
+    const int littleN = n*(n+1);   // dim of (P_{n-1})^2 -- smaller space
+    const int bigN = (n+1)*(n+2);  // dim of (P_{n})^2 -- larger space
+    const int scalarSmallestN = (n-1)*n / 2;
+    const int scalarLittleN = littleN/2;
+    const int scalarBigN = bigN/2;
+
+    // first, need to project the basis for RT space onto the
+    // orthogonal basis of degree n
+    // get coefficients of PkHx
+
+    Teuchos::SerialDenseMatrix<int,Scalar> V1(bigN, N);
+
+    // basis for the space is 
+    // { (phi_i,0) }_{i=0}^{scalarLittleN-1} ,
+    // { (0,phi_i) }_{i=0}^{scalarLittleN-1} ,
+    // { (x,y) . phi_i}_{i=scalarLittleN}^{scalarBigN-1}
+    // columns of V1 are expansion of this basis in terms of the basis
+    // for P_{n}^2
+
+    // these two loops get the first two sets of basis functions
+    for (int i=0;i<scalarLittleN;i++) {
+      V1(i,i) = 1.0;
+      V1(scalarBigN+i,scalarLittleN+i) = 1.0;
+    }
+
+    // now I need to integrate { (x,y) phi } against the big basis
+    // first, get a cubature rule.
+    CubatureDirectTriDefault<Scalar,FieldContainer<Scalar> > myCub( 2 * n );
+    FieldContainer<Scalar> cubPoints( myCub.getNumPoints() , 2 );
+    FieldContainer<Scalar> cubWeights( myCub.getNumPoints() );
+    myCub.getCubature( cubPoints , cubWeights );
+
+    // tabulate the scalar orthonormal basis at cubature points
+    FieldContainer<Scalar> phisAtCubPoints( scalarBigN , myCub.getNumPoints() );
+    Phis.getValues( phisAtCubPoints , cubPoints , OPERATOR_VALUE );
+
+    // now do the integration
+    for (int i=0;i<n;i++) {
+      for (int j=0;j<scalarBigN;j++) { 	// int (x,y) phi_i \cdot (phi_j,0)
+	V1(j,littleN+i) = 0.0;
+	for (int k=0;k<myCub.getNumPoints();k++) {
+	  V1(j,littleN+i) += 
+	    cubWeights(k) * cubPoints(k,0) 
+	    * phisAtCubPoints(scalarSmallestN+i,k) 
+	    * phisAtCubPoints(j,k);
+	}
+      }
+      for (int j=0;j<scalarBigN;j++) {  // int (x,y) phi_i \cdot (0,phi_j)
+	V1(j+scalarBigN,littleN+i) = 0.0;
+	for (int k=0;k<myCub.getNumPoints();k++) {
+	  V1(j+scalarBigN,littleN+i) += 
+	    cubWeights(k) * cubPoints(k,1) 
+	    * phisAtCubPoints(scalarSmallestN+i,k) 
+	    * phisAtCubPoints(j,k);
+	}
+      }
+    }
+
+    //std::cout << V1 << "\n";
+
+    
+    // next, apply the RT nodes (rows) to the basis for (P_n)^2 (columns)
+    Teuchos::SerialDenseMatrix<int,Scalar> V2(N , bigN);
+
+    // first 3 * degree nodes are normals at each edge
+    // get the points on the line
+    FieldContainer<Scalar> linePts( n , 1 );
+    if (pointType == POINTTYPE_WARPBLEND) {
+      CubatureDirectLineGauss<Scalar> edgeRule( n );
+      FieldContainer<Scalar> edgeCubWts( n );
+      edgeRule.getCubature( linePts , edgeCubWts );
+    }
+    else if (pointType == POINTTYPE_EQUISPACED ) {
+      shards::CellTopology linetop(shards::getCellTopologyData<shards::Line<2> >() );
+
+      PointTools::getLattice<Scalar,FieldContainer<Scalar> >( linePts , 
+							      linetop ,
+							      n+1 , 1 ,
+							      POINTTYPE_EQUISPACED );
+    }
+    // holds the image of the line points 
+    FieldContainer<Scalar> edgePts( n , 2 );
+    FieldContainer<Scalar> phisAtEdgePoints( scalarBigN , n );
+
+    // these are scaled by the appropriate edge lengths.
+    const Scalar nx[] = {0.0,1.0,-1.0};
+    const Scalar ny[] = {-1.0,1.0,0.0};
+    
+    for (int i=0;i<3;i++) {  // loop over edges
+      CellTools<Scalar>::mapToReferenceSubcell( edgePts ,
+						linePts ,
+						1 ,
+						i ,
+						this->basisCellTopology_ );
+
+      Phis.getValues( phisAtEdgePoints , edgePts , OPERATOR_VALUE );
+
+      // loop over points (rows of V2)
+      for (int j=0;j<n;j++) {
+	// loop over orthonormal basis functions (columns of V2)
+	for (int k=0;k<scalarBigN;k++) {
+	  V2(n*i+j,k) = nx[i] * phisAtEdgePoints(k,j);
+	  V2(n*i+j,k+scalarBigN) = ny[i] * phisAtEdgePoints(k,j);
+	}
+      }
+    }
+
+    // next map the points to each edge
+
+
+    // remaining nodes are divided into two pieces:  point value of x
+    // components and point values of y components.  These are
+    // evaluated at the interior of a lattice of degree + 1, For then
+    // the degree == 1 space corresponds classicaly to RT0 and so gets
+    // no internal nodes, and degree == 2 corresponds to RT1 and needs
+    // one internal node per vector component.
+    const int numInternalPoints = PointTools::getLatticeSize( this->getBaseCellTopology() ,
+							      n + 1 ,
+							      1 );
+
+    FieldContainer<Scalar> internalPoints( numInternalPoints , 2 );
+    PointTools::getLattice<Scalar,FieldContainer<Scalar> >( internalPoints ,
+							    this->getBaseCellTopology() , 
+							    n + 1 ,
+							    1 ,
+							    pointType );
+    
+    FieldContainer<Scalar> phisAtInternalPoints( scalarBigN , numInternalPoints );
+    Phis.getValues( phisAtInternalPoints , internalPoints , OPERATOR_VALUE );
+
+    // copy values into right positions of V2
+    for (int i=0;i<numInternalPoints;i++) {
+      for (int j=0;j<scalarBigN;j++) {
+	// x component
+	V2(3*n+i,j) = phisAtInternalPoints(j,i);
+	// y component
+	V2(3*n+numInternalPoints+i,scalarBigN+j) = phisAtInternalPoints(j,i);
+      }
+    }
+
+//     std::cout << "Nodes on big basis\n";
+//     std::cout << V2 << "\n";
+//     std::cout << "End nodes\n";
+
+    Teuchos::SerialDenseMatrix<int,Scalar> Vsdm( N , N );
+
+    // multiply V2 * V1 --> V
+    Vsdm.multiply( Teuchos::NO_TRANS , Teuchos::NO_TRANS , 1.0 , V2 , V1 , 0.0 );
+
+//     std::cout << "Vandermonde:\n";
+//     std::cout << Vsdm << "\n";
+//     std::cout << "End Vandermonde\n";
+    
+    Teuchos::SerialDenseSolver<int,Scalar> solver;
+    solver.setMatrix( rcp( &Vsdm , false ) );
+    solver.invert( );
+
+    Teuchos::SerialDenseMatrix<int,Scalar> Csdm( bigN , N );
+    Csdm.multiply( Teuchos::NO_TRANS , Teuchos::NO_TRANS , 1.0 , V1 , Vsdm , 0.0 );
+
+    //    std::cout << Csdm << "\n";
+
+    for (int i=0;i<bigN;i++) {
+      for (int j=0;j<N;j++) {
+	coeffs(i,j) = Csdm(i,j);
+      }
+    }
+  }  
+    
+  template<class Scalar, class ArrayScalar>
+  void Basis_HDIV_TRI_In_FEM<Scalar, ArrayScalar>::initializeTags() {
+  
+    // Basis-dependent initializations
+    int tagSize  = 4;        // size of DoF tag, i.e., number of fields in the tag
+    int posScDim = 0;        // poisition in the tag, counting from 0, of the subcell dim 
+    int posScOrd = 1;        // poisition in the tag, counting from 0, of the subcell ordinal
+    int posDfOrd = 2;        // position in the tag, counting from 0, of DoF ordinal relative to the subcell
+  
+    // An array with local DoF tags assigned to the basis functions, in the order of their local enumeration 
+
+    int *tags = new int[ tagSize * this->getCardinality() ];
+    int *tag_cur = tags;
+    const int degree = this->getDegree();
+
+    // there are degree internal dofs on each edge -- normals.  Let's do them
+    for (int ed=0;ed<3;ed++) {
+      for (int i=0;i<degree;i++) {
+	tag_cur[0] = 1;  tag_cur[1] = ed;  tag_cur[2] = i;  tag_cur[3] = degree;
+	tag_cur += tagSize;
+      }
+    }
+			   
+    // end edge dofs
+
+    // the rest of the dofs are internal
+    const int numFaceDof = (degree-1)*degree;
+    for (int i=3*degree;i<degree*(degree+1);i++) {
+      tag_cur[0] = 2;  tag_cur[1] = 0;  tag_cur[2] = 1;  tag_cur[3] = numFaceDof;
+      tag_cur += tagSize;
+    }
+    
+    
+    Intrepid::setOrdinalTagData(this -> tagToOrdinal_,
+				this -> ordinalToTag_,
+				tags,
+				this -> basisCardinality_,
+				tagSize,
+				posScDim,
+				posScOrd,
+				posDfOrd);
+
+    delete []tags;
+  
+  }  
+
+
+
+  template<class Scalar, class ArrayScalar> 
+  void Basis_HDIV_TRI_In_FEM<Scalar, ArrayScalar>::getValues(ArrayScalar &        outputValues,
+							      const ArrayScalar &  inputPoints,
+							      const EOperator      operatorType) const {
+  
+    // Verify arguments
+#ifdef HAVE_INTREPID_DEBUG
+    Intrepid::getValues_HDIV_Args<Scalar, ArrayScalar>(outputValues,
+							inputPoints,
+							operatorType,
+							this -> getBaseCellTopology(),
+							this -> getCardinality() );
+#endif
+    const int numPts = inputPoints.dimension(0);
+    const int numBf = this->getCardinality();
+    const int deg = this -> getDegree();
+    const int scalarBigN = (deg+1)*(deg+2)/2;
+
+    try {
+      switch (operatorType) {
+      case OPERATOR_VALUE:
+	{
+	  FieldContainer<Scalar> phisCur( scalarBigN , numPts );
+	  Phis.getValues( phisCur , inputPoints , OPERATOR_VALUE );
+
+	  for (int i=0;i<outputValues.dimension(0);i++) { // RT bf
+	    for (int j=0;j<outputValues.dimension(1);j++) {  // point
+	      outputValues(i,j,0) = 0.0;
+	      outputValues(i,j,1) = 0.0;
+	      for (int k=0;k<scalarBigN;k++) { // Dubiner bf
+		outputValues(i,j,0) += coeffs(k,i) * phisCur(k,j);
+		outputValues(i,j,1) += coeffs(k+scalarBigN,i) * phisCur(k,j);
+	      }
+	    }
+	  }
+	}
+	break;
+      case OPERATOR_DIV:
+	{
+	  FieldContainer<Scalar> phisCur( numBf , numPts , 2 );
+	  Phis.getValues( phisCur , inputPoints , OPERATOR_GRAD );
+	  for (int i=0;i<outputValues.dimension(0);i++) { // bf loop
+	    for (int j=0;j<outputValues.dimension(1);j++) { // point loop
+	      // dx of x component
+	      outputValues(i,j) = 0.0;
+	      for (int k=0;k<scalarBigN;k++) {
+		outputValues(i,j) += coeffs(k,i) * phisCur(k,j,0);
+	      }
+	      // dy of y component
+	      for (int k=0;k<scalarBigN;k++) {
+		outputValues(i,j) += coeffs(k+scalarBigN,i) * phisCur(k,j,1);
+	      }
+	    }
+	  }
+	}
+	break;
+      default:
+	TEST_FOR_EXCEPTION( true , std::invalid_argument,
+			    ">>> ERROR (Basis_HDIV_TRI_In_FEM): Operator type not implemented");    	
+	break;
+      }
+    }
+    catch (std::invalid_argument &exception){
+      TEST_FOR_EXCEPTION( true , std::invalid_argument,
+			  ">>> ERROR (Basis_HDIV_TRI_In_FEM): Operator type not implemented");    
+    }
+
+  }
+  
+
+  
+  template<class Scalar, class ArrayScalar>
+  void Basis_HDIV_TRI_In_FEM<Scalar, ArrayScalar>::getValues(ArrayScalar&           outputValues,
+							      const ArrayScalar &    inputPoints,
+							      const ArrayScalar &    cellVertices,
+							      const EOperator        operatorType) const {
+    TEST_FOR_EXCEPTION( (true), std::logic_error,
+			">>> ERROR (Basis_HDIV_TRI_In_FEM): FEM Basis calling an FVD member function");
+  }
+
+
+}// namespace Intrepid
---
+++ packages/intrepid/test/Discretization/Basis/HDIV_TRI_In_FEM/test_01.cpp
@@ -0,0 +1,277 @@
+// @HEADER
+// ************************************************************************
+//
+//                           Intrepid Package
+//                 Copyright (2007) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Pavel Bochev (pbboche@sandia.gov) or
+//                    Denis Ridzal (dridzal@sandia.gov) or
+//                    Robert Kirby (robert.c.kirby@ttu.edu)
+//
+// ************************************************************************
+// @HEADER
+
+
+/** \file
+\brief  Unit test of Raviart-Thomas class for triangles.
+\author Created by R. Kirby
+*/
+
+#include "Intrepid_FieldContainer.hpp"
+#include "Teuchos_oblackholestream.hpp"
+#include "Teuchos_RCP.hpp"
+#include "Teuchos_GlobalMPISession.hpp"
+#include "Intrepid_PointTools.hpp"
+#include "Intrepid_HDIV_TRI_In_FEM.hpp"
+#include "Shards_CellTopology.hpp"
+
+#include <iostream>
+using namespace Intrepid;
+
+int main(int argc, char *argv[]) {
+
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv);
+  
+  // This little trick lets us print to std::cout only if a (dummy) command-line argument is provided.
+  int iprint     = argc - 1;
+  
+  Teuchos::RCP<std::ostream> outStream;
+  Teuchos::oblackholestream bhs; // outputs nothing
+  
+  if (iprint > 0)
+    outStream = Teuchos::rcp(&std::cout, false);
+  else
+    outStream = Teuchos::rcp(&bhs, false);
+  
+  // Save the format state of the original std::cout.
+  Teuchos::oblackholestream oldFormatState;
+  oldFormatState.copyfmt(std::cout);
+  
+  *outStream \
+    << "===============================================================================\n" \
+    << "|                                                                             |\n" \
+    << "|                           Unit Test HDIV_TRI_In_FEM                         |\n" \
+    << "|                                                                             |\n" \
+    << "|     1) Tests triangular Raviart-Thomas basis                                |\n" \
+    << "|                                                                             |\n" \
+    << "|  Questions? Contact  Pavel Bochev (pbboche@sandia.gov) or                   |\n" \
+    << "|                      Denis Ridzal (dridzal@sandia.gov) or                   |\n" \
+    << "|                      Robert Kirby (robert.c.kirby@ttu.edu)                  |\n" \
+    << "|                                                                             |\n" \
+    << "|  Intrepid's website: http://trilinos.sandia.gov/packages/intrepid           |\n" \
+    << "|  Trilinos website:   http://trilinos.sandia.gov                             |\n" \
+    << "|                                                                             |\n" \
+    << "===============================================================================\n";
+  
+  int errorFlag  = 0;
+
+  // test for basis values, compare against fiat
+  try {
+    const int deg = 2;
+    Basis_HDIV_TRI_In_FEM<double,FieldContainer<double> >  myBasis( deg , POINTTYPE_EQUISPACED );
+
+    // Get a lattice
+    const int np_lattice = PointTools::getLatticeSize( myBasis.getBaseCellTopology() , deg , 0 );
+    FieldContainer<double> lattice( np_lattice , 2 );
+    FieldContainer<double> myBasisValues( myBasis.getCardinality() , np_lattice , 2 );
+    PointTools::getLattice<double,FieldContainer<double> >( lattice , 
+							    myBasis.getBaseCellTopology() , 
+							    deg , 
+							    0 , 
+							    POINTTYPE_EQUISPACED );    
+
+    myBasis.getValues( myBasisValues , lattice , OPERATOR_VALUE );
+
+    const double fiat_vals[] = {
+      5.194805194805292e-02, -2.000000000000000e+00,
+      2.272727272727657e-02, -5.000000000000001e-01,
+      -1.233766233766231e+00, 1.000000000000000e+00,
+      -6.493506493506596e-03, -1.493506493506507e-01,
+      -2.987012987013012e-01, 3.279220779220801e-01,
+      5.194805194805287e-02, -2.857142857142830e-01,
+      2.337662337662333e-01, 9.999999999999997e-01,
+      4.772727272727243e-01, -5.000000000000000e-01,
+      1.948051948051946e+00, -2.000000000000000e+00,
+      -2.922077922077916e-02, 3.279220779220787e-01,
+      1.558441558441582e-01, -1.493506493506513e-01,
+      2.337662337662332e-01, -2.857142857142884e-01,
+      2.337662337662333e-01, -1.110223024625157e-16,
+      4.772727272727246e-01, 2.775557561562891e-17,
+      1.948051948051946e+00, -5.551115123125783e-17,
+      -2.922077922077895e-02, -6.720779220779212e-01,
+      1.558441558441579e-01, 3.506493506493489e-01,
+      2.337662337662326e-01, -1.285714285714288e+00,
+      -2.857142857142860e-01, -5.551115123125783e-17,
+      -5.000000000000008e-01, -5.551115123125783e-17,
+      -7.142857142857150e-01, -5.551115123125783e-17,
+      3.571428571428574e-02, 3.214285714285722e-01,
+      1.428571428571434e-01, 3.214285714285712e-01,
+      -2.857142857142860e-01, 2.571428571428572e+00,
+      7.142857142857146e-01, -1.665334536937735e-16,
+      4.999999999999997e-01, -1.110223024625157e-16,
+      2.857142857142857e-01, -5.551115123125783e-17,
+      -4.642857142857141e-01, 3.214285714285715e-01,
+      -3.571428571428567e-01, 3.214285714285710e-01,
+      -2.285714285714285e+00, 2.571428571428571e+00,
+      -1.948051948051947e+00, 0.000000000000000e+00,
+      -4.772727272727238e-01, -1.110223024625157e-16,
+      -2.337662337662305e-01, -1.110223024625157e-16,
+      -5.064935064935067e-01, 3.506493506493494e-01,
+      7.012987012986992e-01, -6.720779220779199e-01,
+      1.051948051948052e+00, -1.285714285714282e+00,
+      -1.818181818181798e-01, 2.220446049250313e-16,
+      1.045454545454552e+00, 5.648254401576814e-17,
+      -1.818181818181759e-01, 2.220446049250313e-16,
+      2.272727272727237e-02, 1.022727272727270e+00,
+      1.045454545454541e+00, -1.022727272727269e+00,
+      -1.818181818181795e-01, 2.953271850160422e-15,
+      3.376623376623393e-01, 2.220446049250313e-16,
+      5.227272727272771e-01, 1.110223024625157e-16,
+      -5.194805194805161e-01, 2.220446049250313e-16,
+      -4.220779220779241e-02, 1.529220779220777e+00,
+      -4.415584415584443e-01, 5.064935064935090e-01,
+      3.376623376623393e-01, -8.571428571428555e-01
+    };
+
+    int cur=0;
+    for (int i=0;i<myBasisValues.dimension(0);i++) {
+      for (int j=0;j<myBasisValues.dimension(1);j++) {
+	for (int k=0;k<myBasisValues.dimension(2);k++) {
+	  if (std::abs( myBasisValues(i,j,k) - fiat_vals[cur] ) > INTREPID_TOL ) {
+	    errorFlag++;
+	    *outStream << std::setw(70) << "^^^^----FAILURE!" << "\n";
+	    
+	    // Output the multi-index of the value where the error is:
+	    *outStream << " At multi-index { ";
+	    *outStream << i << " " << j << " " << k;
+	    *outStream << "}  computed value: " << myBasisValues(i,j,k)
+		       << " but correct value: " << fiat_vals[cur] << "\n";
+          *outStream << "Difference: " << std::abs( myBasisValues(i,j,k) - fiat_vals[cur] ) << "\n";
+	  }
+	  cur++;
+	}
+      }
+    }
+  }
+  catch (std::exception err) {
+    *outStream << err.what() << "\n\n";
+    errorFlag = -1000;
+  }
+  try {
+    const int deg = 2;
+    Basis_HDIV_TRI_In_FEM<double,FieldContainer<double> >  myBasis( deg , POINTTYPE_EQUISPACED );
+
+    // Get a lattice
+    const int np_lattice = PointTools::getLatticeSize( myBasis.getBaseCellTopology() , deg , 0 );
+    FieldContainer<double> lattice( np_lattice , 2 );
+    FieldContainer<double> myBasisDivs( myBasis.getCardinality() , np_lattice );
+    PointTools::getLattice<double,FieldContainer<double> >( lattice , 
+							    myBasis.getBaseCellTopology() , 
+							    deg , 
+							    0 , 
+							    POINTTYPE_EQUISPACED );    
+
+    myBasis.getValues( myBasisDivs , lattice , OPERATOR_DIV );
+
+    const double fiat_divs[] = {
+      6.857142857142859e+00,
+      2.357142857142859e+00,
+      -2.142857142857144e+00,
+      2.357142857142858e+00,
+      -2.142857142857142e+00,
+      -2.142857142857140e+00,
+      -2.142857142857147e+00,
+      2.357142857142856e+00,
+      6.857142857142859e+00,
+      -2.142857142857144e+00,
+      2.357142857142856e+00,
+      -2.142857142857143e+00,
+      -2.142857142857145e+00,
+      2.357142857142855e+00,
+      6.857142857142858e+00,
+      -2.142857142857144e+00,
+      2.357142857142855e+00,
+      -2.142857142857142e+00,
+      -1.714285714285716e+00,
+      -1.714285714285716e+00,
+      -1.714285714285718e+00,
+      2.785714285714288e+00,
+      2.785714285714286e+00,
+      7.285714285714281e+00,
+      -1.714285714285718e+00,
+      -1.714285714285716e+00,
+      -1.714285714285716e+00,
+      2.785714285714286e+00,
+      2.785714285714286e+00,
+      7.285714285714278e+00,
+      6.857142857142857e+00,
+      2.357142857142859e+00,
+      -2.142857142857141e+00,
+      2.357142857142858e+00,
+      -2.142857142857140e+00,
+      -2.142857142857136e+00,
+      9.000000000000004e+00,
+      3.996802888650562e-15,
+      -9.000000000000000e+00,
+      4.500000000000000e+00,
+      -4.499999999999996e+00,
+      8.881784197001252e-16,
+      8.571428571428573e+00,
+      4.071428571428576e+00,
+      -4.285714285714253e-01,
+      -4.285714285714295e-01,
+      -4.928571428571429e+00,
+      -9.428571428571420e+00
+    };
+
+    int cur=0;
+    for (int i=0;i<myBasisDivs.dimension(0);i++) {
+      for (int j=0;j<myBasisDivs.dimension(1);j++) {
+	if (std::abs( myBasisDivs(i,j) - fiat_divs[cur] ) > INTREPID_TOL ) {
+	  errorFlag++;
+	  *outStream << std::setw(70) << "^^^^----FAILURE!" << "\n";
+	  
+	  // Output the multi-index of the value where the error is:
+	  *outStream << " At multi-index { ";
+	  *outStream << i << " " << j;
+	  *outStream << "}  computed value: " << myBasisDivs(i,j)
+		     << " but correct value: " << fiat_divs[cur] << "\n";
+          *outStream << "Difference: " << std::abs( myBasisDivs(i,j) - fiat_divs[cur] ) << "\n";
+	}
+	cur++;
+      }
+    }
+  }
+  catch (std::exception err) {
+    *outStream << err.what() << "\n\n";
+    errorFlag = -1000;
+  }
+
+
+  if (errorFlag != 0)
+    std::cout << "End Result: TEST FAILED\n";
+  else
+    std::cout << "End Result: TEST PASSED\n";
+  
+  // reset format state of std::cout
+  std::cout.copyfmt(oldFormatState);
+  
+  return errorFlag;
+}
