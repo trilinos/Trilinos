655cec5b6149397cd6c86f9c7dd81b52933290ad
David M. Hensinger <dmhensi@sandia.gov>
2009-07-28 00:05:25 +0000

    adding extra functions directory to pamgen, these functions are helpers for calculating global ids

---
+++ packages/pamgen/extra_functions/pamgen_extras.cpp
@@ -0,0 +1,330 @@
+#include "../mesh_spec_lt/im_exodusII_l.h"
+#include "../mesh_spec_lt/im_ne_nemesisI_l.h"
+#include "pamgen_extras.h"
+#include <vector>
+#include <set>
+#include <algorithm>
+
+#ifdef HAVE_MPI
+#include <mpi.h>
+#endif
+
+/*****************************************************************************/
+void  Conform_Boundary_IDS(long long ** comm_entities,
+			   long long * entity_counts,
+			   long long * proc_ids, 
+			   long long * data_array,
+			   long long num_comm_pairs,
+			   long long  rank)
+/*****************************************************************************/
+{
+#ifdef HAVE_MPI
+  //relies on data array having a -1 for unassigned values
+  unsigned nncm = num_comm_pairs;
+
+  //Will load an ownership flag along with the data to allow the conform
+  MPI_Request * req = new MPI_Request[nncm];
+  
+  long long ** send_buffer = new long long * [nncm];
+  long long ** receive_buffer = new long long * [nncm];
+  for(unsigned i = 0; i < nncm; i ++)send_buffer[i]    = new long long [entity_counts[i]];
+  for(unsigned i = 0; i < nncm; i ++)receive_buffer[i] = new long long [entity_counts[i]];
+
+  // load up send buffer
+  for(unsigned i = 0; i < nncm; i ++){
+    for(unsigned j = 0; j < entity_counts[i];j++){
+      send_buffer[i][j] = data_array[comm_entities[i][j]-1];
+    }
+  }
+
+  //communicate
+
+  for(unsigned i = 0; i < nncm ;i ++){
+    int size = entity_counts[i];
+    int proc = proc_ids[i];
+    MPI_Irecv(receive_buffer[i],size, MPI_LONG_LONG_INT, proc, 1, MPI_COMM_WORLD, req + i);
+  }
+
+  for(unsigned i = 0; i < nncm ;i ++){
+    int size = entity_counts[i];
+    int proc = proc_ids[i];
+    MPI_Send(send_buffer[i], size, MPI_LONG_LONG_INT, proc, 1,MPI_COMM_WORLD);
+  }
+
+  for(unsigned i = 0; i < nncm ;i ++){
+    MPI_Status stat;
+    MPI_Wait(req + i, &stat);
+  }
+
+  for(unsigned i = 0; i < nncm; i ++){
+    for(unsigned j = 0; j < entity_counts[i];j++){
+      if(receive_buffer[i][j] >= 0)data_array[comm_entities[i][j]-1] = receive_buffer[i][j];
+    }
+  }
+  
+  for(unsigned i = 0; i < nncm; i ++){
+    if(send_buffer[i])   delete [] send_buffer[i];
+    if(receive_buffer[i])delete [] receive_buffer[i];
+  }
+  delete [] send_buffer;
+  delete [] receive_buffer;
+#endif
+}
+
+
+
+/*****************************************************************************/
+void  Conform_Boundary_IDS_topo_entity(std::vector < std:: vector < topo_entity * > > & topo_entities,
+				       long long * proc_ids, 
+				       long long  rank)
+/*****************************************************************************/
+{
+#ifdef HAVE_MPI
+  //relies on data array having a -1 for unassigned values
+  unsigned nncm = topo_entities.size();
+
+  //Will load an ownership flag along with the data to allow the conform
+  MPI_Request * req = new MPI_Request[nncm];
+  
+  long long ** send_buffer = new long long * [nncm];
+  long long ** receive_buffer = new long long * [nncm];
+  for(unsigned i = 0; i < nncm; i ++){
+    if(topo_entities[i].size() > 0){
+      send_buffer[i]    = new long long [topo_entities[i].size()];
+      receive_buffer[i] = new long long [topo_entities[i].size()];
+    }
+    else{
+      send_buffer[i] = NULL;
+      receive_buffer[i] = NULL;
+    }
+  }
+
+  // load up send buffer
+  for(unsigned i = 0; i < nncm; i ++){
+    for(unsigned j = 0; j < topo_entities[i].size();j++){
+      send_buffer[i][j] = topo_entities[i][j]->global_id;
+    }
+  }
+
+  //communicate
+
+  for(unsigned i = 0; i < nncm ;i ++){
+    int size = topo_entities[i].size();
+    if(size > 0){
+      int proc = proc_ids[i];
+      MPI_Irecv(receive_buffer[i],size, MPI_LONG_LONG_INT, proc, 1, MPI_COMM_WORLD, req + i);
+    }
+  }
+
+  for(unsigned i = 0; i < nncm ;i ++){
+    int size = topo_entities[i].size();
+    if(size > 0){
+      int proc = proc_ids[i];
+      MPI_Send(send_buffer[i], size, MPI_LONG_LONG_INT, proc, 1,MPI_COMM_WORLD);
+    }
+  }
+
+  for(unsigned i = 0; i < nncm ;i ++){
+    MPI_Status stat;
+    int size = topo_entities[i].size();
+    if(size > 0){
+      MPI_Wait(req + i, &stat);
+    }
+  }
+
+  for(unsigned i = 0; i < nncm; i ++){
+    for(unsigned j = 0; j < topo_entities[i].size();j++){
+      if(receive_buffer[i][j] >= 0)topo_entities[i][j]->global_id = receive_buffer[i][j];
+    }
+  }
+
+  for(unsigned i = 0; i < nncm; i ++){
+    if(send_buffer[i])   delete [] send_buffer[i];
+    if(receive_buffer[i])delete [] receive_buffer[i];
+  }
+  delete [] send_buffer;
+  delete [] receive_buffer;
+#endif
+}
+
+/*******************************************************************************/
+void calc_global_node_ids(long long * globalNodeIds,
+			  bool * nodeIsOwned,
+			  long long numNodes,
+			  long long num_node_comm_maps,
+			  long long * node_cmap_node_cnts,
+			  long long * node_comm_proc_ids,
+			  long long * * comm_node_ids,
+			  int rank)
+/*******************************************************************************/
+{
+  for(long long i = 0; i < numNodes; i ++){
+    globalNodeIds[i] = 1l;
+    nodeIsOwned[i] = true;
+  }
+  for(long long j = 0; j < num_node_comm_maps; j++) {
+    for(long long k = 0; k < node_cmap_node_cnts[j] ; k ++){
+      if(node_comm_proc_ids[j] < rank){
+	globalNodeIds[comm_node_ids[j][k]-1] = -1;	
+	nodeIsOwned[comm_node_ids[j][k]-1] = false;
+      }
+    }
+  }
+  long long num_unique_nodes = 0;
+  for(long long  i = 0 ; i < numNodes; i ++)if(globalNodeIds[i] == 1l)num_unique_nodes ++;
+  long long start_id = 0;
+
+#ifdef HAVE_MPI
+  MPI_Scan(&num_unique_nodes,&start_id,1,
+	   MPI_LONG_LONG_INT,
+	   MPI_SUM,
+	   MPI_COMM_WORLD);
+#endif
+
+  start_id -= num_unique_nodes;
+
+  int num_assigned = 0;
+  for(long long  i = 0 ; i < numNodes; i ++)if(globalNodeIds[i] == 1l){
+    globalNodeIds[i] = num_assigned + start_id;
+    num_assigned ++;
+  }
+
+  //Conforms global nodal ids
+  Conform_Boundary_IDS(comm_node_ids,
+		       node_cmap_node_cnts,
+		       node_comm_proc_ids, 
+		       globalNodeIds,
+		       num_node_comm_maps,
+		       rank);
+
+}
+
+
+/*******************************************************************************/
+void calc_global_ids(std::vector < topo_entity * > eof_vec,
+		long long **comm_node_ids,
+		long long * node_comm_proc_ids,
+		long long * node_cmap_node_cnts,
+		int num_node_comm_maps,
+		int rank,
+		std::string fname_string)
+/*******************************************************************************/
+{
+  std::vector < std:: vector < topo_entity *> > topo_entities;
+  int nncm = num_node_comm_maps;
+  // make a vector of sets of comm nodes
+  std::vector < std::set < long long> > comm_vector_set;
+  for(int i = 0; i < nncm; i ++){
+    std::vector < topo_entity * >v;
+    topo_entities.push_back(v);
+    std::set < long long > as;
+    for(int j = 0; j < node_cmap_node_cnts[i];j ++){
+      as.insert(comm_node_ids[i][j]);
+    }
+    comm_vector_set.push_back(as);
+  }
+/*run over all edges, faces*/
+
+  for(unsigned tec = 0;tec != eof_vec.size();tec ++){
+    topo_entity * teof = eof_vec[tec];
+
+    for(int i = 0; i < nncm; i ++){
+      bool found = true;
+      std::list <long long > :: iterator lit;
+      for( lit = teof->local_node_ids.begin();
+	   lit != teof->local_node_ids.end() && found == true;
+	   lit ++){
+	if(comm_vector_set[i].find(*lit) == comm_vector_set[i].end())found = false;
+      }
+      //if all component nodes found then add face,edge to comm lists
+      if(found){
+	topo_entities[i].push_back(teof);
+	if(node_comm_proc_ids[i] < rank)teof->owned = false;//not owned if found on lower processor
+      }
+      else{
+      }
+    }
+  }
+
+  //need to sort the edges_or_face vectors by their sorted global node ids
+  for(unsigned i = 0; i < topo_entities.size(); i ++){
+    if(!topo_entities[i].empty()){
+      std::sort(topo_entities[i].begin(),topo_entities[i].end(),compare_sorted_global_node_ids);
+    }
+  }
+#ifdef DEBUG_PRINTING
+ std::stringstream aname;
+  aname << fname_string;
+  aname << rank;
+  aname << ".txt";
+  ofstream fout(aname.str().c_str());
+#endif
+
+  //need to sort the edges_or_face vectors by their sorted global node ids
+  for(unsigned i = 0; i < topo_entities.size(); i ++){
+    if(!topo_entities[i].empty()){
+      std::sort(topo_entities[i].begin(),topo_entities[i].end(),compare_sorted_global_node_ids);
+    }
+#ifdef DEBUG_PRINTING
+    fout << " from proc rank " << rank 
+	 << " to proc rank " << node_comm_proc_ids[i] 
+	 << " has " << topo_entities[i].size() 
+	 << " entries " << std::endl;
+
+    if(!topo_entities[i].empty()){
+      for(unsigned j = 0; j < topo_entities[i].size();j ++){
+	topo_entity * eof = topo_entities[i][j];
+	for(std::list < long long > :: iterator klit = eof->sorted_global_node_ids.begin();
+	    klit != eof->sorted_global_node_ids.end(); klit ++){
+	  fout << (*klit) << " " ;
+	}
+	fout << endl;
+      }
+    }
+#endif
+  }
+
+  //count the number of entities owned;
+  long long owned_entities = 0;
+  for(unsigned ict = 0; ict < eof_vec.size(); ict ++){
+    if(eof_vec[ict]->owned)owned_entities ++;
+  }
+#ifdef DEBUG_PRINTING
+  fout << " proc " << rank << " owns " << owned_entities << " edges " << std::endl;
+#endif
+
+  long long start_id = 0;
+
+#ifdef HAVE_MPI
+  MPI_Scan(&owned_entities,&start_id,1,
+	   MPI_LONG_LONG_INT,
+	   MPI_SUM,
+	   MPI_COMM_WORLD);
+#endif
+
+  start_id -= owned_entities;
+#ifdef DEBUG_PRINTING
+  fout << " proc " << rank << " start_id " << start_id << std::endl;
+#endif
+  //DMH
+  long long num_assigned = 0;
+  for(unsigned ict = 0; ict < eof_vec.size(); ict ++){
+    if(eof_vec[ict]->owned){
+      eof_vec[ict]->global_id = num_assigned + start_id;
+      start_id ++;
+    }
+  }
+
+  Conform_Boundary_IDS_topo_entity(topo_entities,
+				    node_comm_proc_ids, 
+				    rank);
+
+#ifdef DEBUG_PRINTING
+  for(unsigned ict = 0; ict < eof_vec.size(); ict ++){
+    fout << "on proc " << rank << " entity " << ict << " has id " << eof_vec[ict]->global_id << std::endl;
+  }
+
+  fout.close();
+#endif
+
+}
