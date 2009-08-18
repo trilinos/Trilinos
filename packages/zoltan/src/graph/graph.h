bdbb5a0bf127a5dc8418889a1569ac7d93e0bc9a
Karen D. Devine <kddevin@sandia.gov>
2009-08-18 23:08:35 +0000

    SUMMARY:  Removed cvs conflict from header
         BUGZILLA BUG #(s):  NA
    
         CHECKLIST
         ---------
    
         1) Did you 'cvs add' all new files and directories [Y:N:N/A]?  NA
    
         2) Did you add new source files to a CMakeLists.txt file [Y:N:N/A]?  NA
    
         3)  Did you modify or add any source or build system files?  no
    
         DESCRIPTION OF CHANGES:
    
         [???]
    
    (checklist version 2009-07-14)

--- packages/zoltan/src/graph/graph.h
+++ packages/zoltan/src/graph/graph.h
@@ -8,11 +8,6 @@
  *    $RCSfile$
  *    $Author$
  *    $Date$
-=======
- *    $RCSfile$
- *    $Author$
- *    $Date$
->>>>>>> d38681a1388a71b0ee4b652621f3b2f81a79e97f:packages/zoltan/src/graph/graph.h
  *    $Revision$
  ****************************************************************************/
 
