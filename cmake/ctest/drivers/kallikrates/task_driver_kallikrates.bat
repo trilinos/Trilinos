4e9bd0276c3a3f337f6e04f647090c6f46e479d0
David Cole <david.cole@kitware.com>
2009-09-21 21:12:41 +0000

    Update scripts to work on kallikrates with release branch.

--- cmake/ctest/drivers/kallikrates/task_driver_kallikrates.bat
+++ cmake/ctest/drivers/kallikrates/task_driver_kallikrates.bat
@@ -1,30 +1,33 @@
+rem Driver script for dashboards on kallikrates
+
 rem Call this script and pass in the base directory for the dashboard
 rem save state before changing anything
 setlocal
 
-rem cygwin CVS needs to know to use ssh
-set CVS_RSH=C:\Users\bmpersc\helperscripts\plink_wrap.bat
-rem Driver script for dashboards on kallikrates
+rem CVS needs to know how to use ssh
+set CVS_RSH=C:\T10\davcole\ssh.cmd
+
 rem Set location of CTEST_EXE, and CVS_EXE
-set CVS_EXE="C:\Program Files (x86)\CVSNT\cvs.exe
-set CTEST_EXE="C:\Program Files (x86)\CMake 2.7\bin\ctest.exe
+set CVS_EXE=C:\Program Files (x86)\CVSNT\cvs.exe
+set CTEST_EXE=C:\T10\cmake-2.7.20090918-win32-x86\bin\ctest.exe
 
 rem Set the base directory which is one above where Trilinos will be 
 rem checked out.
 
-set BASEDIR=%1%
+set BASEDIR=%~1
 
 rem setup the environment for command line cl to run
 call "C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\Tools\vsvars32.bat"
 
 rem change into the basedir
-cd %BASEDIR%
+cd "%BASEDIR%"
 
 rem checkout the basics from Trilinos needed to run the dashboard including
 rem this script.
-call %CVS_EXE% -q -d :ext:software.sandia.gov:/space/CVS co Trilinos/cmake Trilinos/CTestConfig.cmake
+call "%CVS_EXE%" -q -d :ext:software.sandia.gov:/space/CVS co -r trilinos-release-10-0-branch Trilinos/cmake Trilinos/CTestConfig.cmake
 
 rem Now run ctest on each of the ctest build scripts for this machine
 
-call %CTEST_EXE% -S %BASEDIR%\Trilinos\cmake\ctest\drivers\kallikrates\ctest_windows_nightly_serial_release.cmake -VV > %BASEDIR%\ctest_msvc_nightly_serial_release_kallikrates.out
+call "%CTEST_EXE%" -S "%BASEDIR%\Trilinos\cmake\ctest\drivers\kallikrates\ctest_windows_nightly_serial_release.cmake" -VV >"%BASEDIR%\ctest_msvc_nightly_serial_release_kallikrates.out" 2>&1
+
 endlocal
