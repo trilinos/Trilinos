5d0f457622d2980ac7d6b17a5ac92266ae132090
Brent M. Perschbacher <bmpersc@sandia.gov>
2009-10-29 22:49:12 +0000

    Committing testing files to migrate godel tests to trilinos-test
    created files for testing 4 builds that are being done on godel that will need
    to be moved to trilinos-test. The builds are
    MPI opt on development branch
    MPI debug on development branch
    MPI opt shared on development branch
    serial debug on development branch
    
    these haven't been tested yet. checking them in so that they can be run through
    the actual full process of testing which includes these scripts being checked
    out on a nightly basis.
    
    the Godel testing has not been disabled yet and won't be until these are running
    correctly.

---
+++ cmake/ctest/drivers/trilinos-test/cron_driver_nightly_development_trilinos-test.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+CTEST_EXE=/home/trilinos/cmake/bin/ctest
+BASEDIR=/home/bmpersc/nightly/Trilinos.base/development
+BASEDATADIR=/home/bmpersc/nightly
+DRIVER_SCRIPT_DIR=$BASEDIR/Trilinos/cmake/ctest/drivers/trilinos-test
+
+export CMAKE_LIBRARY_PATH="/home/trilinos/tpl/gcc4.1.2/exodusII_4.84/lib:/home/trilinos/tpl/gcc4.1.2/netcdf_4.0/lib"
+export CMAKE_INCLUDE_PATH="/home/trilinos/tpl/gcc4.1.2/exodusII_4.84/include:/home/trilinos/tpl/gcc4.1.2/netcdf_4.0/include"
+export TRILINOSDATADIRECTORY=$BASEDATADIR/TrilinosData
+
+#get the date for use in log files
+DATE=`date "+%m-%d-%Y"`
+
+echo
+echo "Starting nightly Trilinos development testing on trilinos-test: `date`"
+echo
+
+echo
+echo "Checking out just the drivers: `date`"
+echo
+
+
+cd $BASEDATADIR
+#checkout the trilinos data directory. Needed for a few tests to run
+cvs -q -d :ext:software:/space/CVS co TrilinosData
+
+cd $BASEDIR
+#checkout the bits of trilinos needed for running the nightly test scripts
+cvs -q -d :ext:software:/space/CVS co Trilinos/cmake Trilinos/CTestConfig.cmake
+
+echo
+echo "Doing mpi optimized Development build: `date`"
+echo
+
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_mpi_development_opt_trilinos-test.cmake -VV &> "MPI_OPT_DEV_$DATE.log"
+
+echo
+echo "Doing mpi debug Development build: `date`"
+echo
+
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_mpi_development_debug_trilinos-test.cmake -VV &> "MPI_DEBUG_DEV_$DATE.log"
+
+echo
+echo "Doing mpi optimized development shared library build: `date`"
+echo 
+
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_mpi_development_opt_shared_trilinos-test.cmake -VV &> "MPI_OPT_DEV_SHARED_$DATE.log"
+
+echo
+echo "Doing serial debug build: `date`"
+echo
+
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_serial_development_debug_trilinos-test.cmake -VV &> "SERIAL_DEBUG_DEV_$DATE.log"
+
+echo
+echo "Ending nightly Trilinos testing on trilinos-test: `date`"
+echo
---
+++ cmake/ctest/drivers/trilinos-test/ctest_linux_nightly_mpi_development_opt_trilinos-test.cmake
@@ -0,0 +1,37 @@
+
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.trilinos-test.gcc.cmake")
+
+#
+# Set the options specific to this build case
+#
+
+SET(COMM_TYPE MPI)
+SET(BUILD_TYPE RELEASE)
+SET(BUILD_DIR_NAME MPI_OPT_DEV)
+
+SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DDART_TESTING_TIMEOUT:STRING=120"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON"
+  "-DTrilinos_DATA_DIR:STRING=$ENV{TRILINOSDATADIRECTORY}"
+  "-DMPI_BASE_DIR:PATH=/usr/lib64/openmpi/1.2.7-gcc"
+  "-DTPL_ENABLE_Pthread:BOOL=ON"
+  "-DTPL_ENABLE_Boost:BOOL=ON"
+  "-DBoost_INCLUDE_DIRS:FILEPATH=/home/trilinos/tpl/gcc4.1.2/boost_1_38_0"
+  "-DTPL_ENABLE_ParMETIS:BOOL=ON"
+  "-DParMETIS_LIBRARY_DIRS:PATH=/home/trilinos/tpl/gcc4.1.2/ParMETIS3_1"
+  "-DTPL_ENABLE_Scotch:BOOL=ON"
+  "-DScotch_INCLUDE_DIRS:PATH=/home/trilinos/tpl/gcc4.1.2/scotch_5.1/include"
+  "-DScotch_LIBRARY_DIRS:PATH=/home/trilinos/tpl/gcc4.1.2/scotch_5.1/lib "
+  "-DTPL_ENABLE_ExodusII:BOOL=ON"
+  "-DTPL_ENABLE_Nemesis:BOOL=ON"
+  "-DNemesis_INCLUDE_DIRS:FILEPATH=/home/trilinos/tpl/gcc4.1.2/nemesis_3.09/include"
+  "-DNemesis_LIBRARY_DIRS:FILEPATH=/home/trilinos/tpl/gcc4.1.2/nemesis_3.09/lib"
+  )
+
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
+
+TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
