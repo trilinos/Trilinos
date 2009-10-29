3bbb1c6c9107e9d5ad894cac0b133e32e045182b
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-10-29 14:02:35 +0000

    Adding missing ML and Belos adapter libraries in Stratimikos
    By some miracle, Stratimikos was building with ML and Belos enabled even
    through the adapter libraries for ML and Belos where missing as dependencies
    of the main 'stratimikos' library.  This affects dynamical libraries and the
    link order.  Amazingly, this only caused a problem on very new Mac machines.
    Every Linux machine this built fine otherwise (with and without shared
    libraries).
    
    Other changes:
    --------------
    
    (*) packages/stratimikos/cmake/Dependencies.cmake,
    cmake/python/data/[CDashSubprojectDependencies.xml,
    TrilinosPackageDependencies.xml, TrilinosPackageDependenciesTable.html]:
    Adding Epetra as a required dependency of Stratimikos (see the comment in the
    Dependencies.cmake file).
    
    
    
    
    =============================
    Automated status information
    =============================
    
    
    Thu Oct 29 08:02:29 MDT 2009
    
    
    
    -------------------
    Summary of Results:
    -------------------
    
    
    MPI_DEBUG Results:
    ------------------------
    
      passed : Trilinos/MPI_DEBUG: passed=347,notpassed=0
    
      Thu Oct 29 07:56:45 MDT 2009
    
      Enabled Packages: Stratimikos
      Hostname: gabriel.sandia.gov
      Source Dir: /home/rabartl/PROJECTS/Trilinos.base/Trilinos
      Build Dir: /home/rabartl/PROJECTS/Trilinos.base/BUILDS/CMAKE/CHECKIN/MPI_DEBUG
    
      CMake Cache Varibles: -DTPL_ENABLE_MPI:BOOL=ON -DTrilinos_ENABLE_TESTS:BOOL=ON -DDART_TESTING_TIMEOUT:STRING=180.0 -DCMAKE_BUILD_TYPE:STRING=RELEASE -DTrilinos_ENABLE_DEBUG:BOOL=ON -DTrilinos_ENABLE_CHECKED_STL:BOOL=ON -DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON -DBUILD_SHARED_LIBS:BOOL=ON -DTrilinos_ENABLE_Stratimikos:BOOL=ON -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON -DTrilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES:BOOL=ON
    
      Make Options: -j4
    
      CTest Options: -j4
    
      Update: Passed (0.125854249795 min)
      Configure: Passed (1.23868743181 min)
      Build: Passed (2.78983628352 min)
      Test: Passed (5.80118743579 min)
    
      Test summary:
      -------------
    
      100% tests passed, 0 tests failed out of 347
    
      Label Time Summary:
      MOOCHO                   = 303.92 sec
      Meros                    =   0.76 sec
      NOX                      = 237.62 sec
      Rythmos                  = 120.86 sec
      Stratimikos              =  38.35 sec
      Sundance                 = 504.09 sec
      TrilinosFramework_Dep    =  17.41 sec
    
      Total Test time (real) = 347.06 sec
    
    
      Final:
      ------
    
      Total time for MPI_DEBUG = 9.83134724696 minutes
    
    
    SERIAL_RELEASE Results:
    ------------------------
    
      passed : Trilinos/SERIAL_RELEASE: passed=284,notpassed=0
    
      Thu Oct 29 08:00:34 MDT 2009
    
      Enabled Packages: Stratimikos
      Hostname: gabriel.sandia.gov
      Source Dir: /home/rabartl/PROJECTS/Trilinos.base/Trilinos
      Build Dir: /home/rabartl/PROJECTS/Trilinos.base/BUILDS/CMAKE/CHECKIN/SERIAL_RELEASE
    
      CMake Cache Varibles: -DTrilinos_ENABLE_TESTS:BOOL=ON -DDART_TESTING_TIMEOUT:STRING=180.0 -DCMAKE_BUILD_TYPE:STRING=RELEASE -DTrilinos_ENABLE_DEBUG:BOOL=OFF -DTrilinos_ENABLE_CHECKED_STL:BOOL=OFF -DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=OFF -DBUILD_SHARED_LIBS:BOOL=ON -DTrilinos_ENABLE_Sundance:BOOL=OFF -DTrilinos_ENABLE_Stratimikos:BOOL=ON -DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON -DTrilinos_ENABLE_ALL_FORWARD_DEP_PACKAGES:BOOL=ON
    
      Make Options: -j4
    
      CTest Options: -j4
    
      Update: Passed (0.125854249795 min)
      Configure: Passed (0.818677266439 min)
      Build: Passed (1.7467473348 min)
      Test: Passed (1.24818601608 min)
    
      Test summary:
      -------------
    
      100% tests passed, 0 tests failed out of 284
    
      Label Time Summary:
      MOOCHO                   =  55.47 sec
      Meros                    =   0.41 sec
      NOX                      =  70.01 sec
      Rythmos                  =  79.40 sec
      Stratimikos              =  28.57 sec
      TrilinosFramework_Dep    =  17.46 sec
    
      Total Test time (real) =  74.03 sec
    
    
      Final:
      ------
    
      Total time for SERIAL_RELEASE = 3.81528931856 minutes

--- packages/stratimikos/cmake/Dependencies.cmake
+++ packages/stratimikos/cmake/Dependencies.cmake
@@ -1,4 +1,8 @@
-SET(LIB_REQUIRED_DEP_PACKAGES Thyra)
+# Here we list Epetra as a required dependence so that the Thyra/Epetra
+# adapters will get enabled by default.  Note that Belos and ML do *not* have
+# a required dependence on Epetra but the Stratimikos Belos and ML adpaters
+# need the Thyra/Epetra adapters.
+SET(LIB_REQUIRED_DEP_PACKAGES Thyra Epetra)
 SET(LIB_OPTIONAL_DEP_PACKAGES Amesos AztecOO Belos Ifpack ML)
 SET(TEST_REQUIRED_DEP_PACKAGES)
 SET(TEST_OPTIONAL_DEP_PACKAGES Triutils EpetraExt)
