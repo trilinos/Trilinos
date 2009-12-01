1f6d28e3914c4d68c83dc41d752ba8474da0d285
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-12-01 07:31:11 -0700

    Adding script to set up my git profile on any machine where I develop.

---
+++ sampleScripts/git-profiles/git-config-rabartl.sh
@@ -0,0 +1,10 @@
+# Set your username
+eg config --global user.name "Roscoe A. Bartlett"
+# Set your email address
+eg config --global user.email rabartl@sandia.gov
+# Use colorized output when it makes sense
+eg config --global color.ui true
+# Set up some shortcut commands
+eg config --global alias.log-short1 "log --pretty=format:'%Cgreen%H%Creset%nAuthor: %an <%ae>%nDate: %cd%n%n%s%n'"
+eg config --global alias.log-short2 "log --pretty=format:'%Cgreen%h%Creset \"%s\"%nAuthor: %an <%ae>%nDate: %ad%n'"
+eg config --global alias.log-online1 "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> %ad'"
