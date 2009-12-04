bd0015d9ae46acf4958190f978ef791e94d6d0b9
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-12-04 15:23:34 -0700

    Changing to just log-short (was log-short2) and log-online (was log-oneline1)
    I got rid of log-short1 because I hardly ever used it.

--- sampleScripts/git-profiles/git-config-rabartl.sh
+++ sampleScripts/git-profiles/git-config-rabartl.sh
@@ -5,6 +5,5 @@ eg config --global user.email rabartl@sandia.gov
 # Use colorized output when it makes sense
 eg config --global color.ui true
 # Set up some shortcut commands
-eg config --global alias.log-short1 "log --pretty=format:'%Cgreen%H%Creset%nAuthor: %an <%ae>%nDate:   %cd (%ar)%n%n%s%n'"
-eg config --global alias.log-short2 "log --pretty=format:'%Cgreen%h%Creset \"%s\"%nAuthor: %an <%ae>%nDate:   %ad (%ar)%n'"
-eg config --global alias.log-online1 "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> %ad'"
+eg config --global alias.log-short "log --pretty=format:'%Cgreen%h%Creset \"%s\"%nAuthor: %an <%ae>%nDate:   %ad (%ar)%n'"
+eg config --global alias.log-oneline "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad]'"
