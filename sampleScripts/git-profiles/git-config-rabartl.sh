02ba13b2a2623128b980790b90df6298975bed8a
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-12-06 15:24:31 -0700

    Adding relative date to log-oneline

--- sampleScripts/git-profiles/git-config-rabartl.sh
+++ sampleScripts/git-profiles/git-config-rabartl.sh
@@ -6,4 +6,4 @@ eg config --global user.email rabartl@sandia.gov
 eg config --global color.ui true
 # Set up some shortcut commands
 eg config --global alias.log-short "log --pretty=format:'%Cgreen%h%Creset \"%s\"%nAuthor: %an <%ae>%nDate:   %ad (%ar)%n'"
-eg config --global alias.log-oneline "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad]'"
+eg config --global alias.log-oneline "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)'"
