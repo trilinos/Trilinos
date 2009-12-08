4578c886928b295f24bdce59ce9cdefe64469c0a
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-12-08 14:29:11 -0700

    Added --oneline to log-oneline; added alias log-local diff-local

--- sampleScripts/git-profiles/git-config-rabartl.sh
+++ sampleScripts/git-profiles/git-config-rabartl.sh
@@ -6,4 +6,6 @@ eg config --global user.email rabartl@sandia.gov
 eg config --global color.ui true
 # Set up some shortcut commands
 eg config --global alias.log-short "log --pretty=format:'%Cgreen%h%Creset \"%s\"%nAuthor: %an <%ae>%nDate:   %ad (%ar)%n'"
-eg config --global alias.log-oneline "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)'"
+eg config --global alias.log-oneline "log --oneline --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)'"
+eg config --global alias.log-local "log --oneline --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)' origin.."
+eg config --global alias.diff-local "diff --name-status origin.."
