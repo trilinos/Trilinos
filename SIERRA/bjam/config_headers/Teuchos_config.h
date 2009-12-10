8e2f7a34ec6488d0c35765b978c5e417d61692e5
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-12-10 15:56:57 -0700

    Adding macros needed to build Teuchos after the DLL hooks were added.

--- SIERRA/bjam/config_headers/Teuchos_config.h
+++ SIERRA/bjam/config_headers/Teuchos_config.h
@@ -1,12 +1,11 @@
 
 #include "TrilinosPlatform_config.h"
 
+#define TEUCHOS_ORDINAL_TYPE ptrdiff_t
+#define TEUCHOS_LIB_DLL_EXPORT
 #define HAVE_TEUCHOS_BLASFLOAT 
-
 #define HAVE_TEUCHOS_COMM_TIMERS 
-
 #define HAVE_TEUCHOS_COMPLEX 
-
 #define HAVE_TEUCHOS_EXTENDED 
 
 /* #define HAVE_TEUCHOS_EXPLICIT_INSTANTIATION */
