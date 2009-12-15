a6c11e134d6981ca6213ac7a15f3c20290840893
Roscoe A. Bartlett <rabartl@sandia.gov>
2009-12-15 06:39:22 -0700

    Upgraded checkin-test.py script driver for SCICO LAN.

--- sampleScripts/checkin-test-scicolan-rabartl.sh
+++ sampleScripts/checkin-test-scicolan-rabartl.sh
@@ -16,18 +16,27 @@ EXTRA_ARGS=$@
 #
 
 echo "
+-DTrilinos_ENABLE_Fortran:BOOL=OFF
+" > COMMON.config
+
+echo "
 -DMPI_BASE_DIR:PATH=/home/sntools/extras/mpi/mpich-1.2.7p1-gcc-4.2.4-64Bit
 -DMPI_EXEC_PRE_NUMPROCS_FLAGS:STRING=--all-local
 " > MPI_DEBUG.config
+
+echo "
+-DCMAKE_C_COMPILER:PATH=/usr/bin/gcc
+-DCMAKE_CXX_COMPILER:PATH=/usr/bin/g++
+" > SERIAL_RELEASE.config
 
 #
 # Run the standard checkin testing script with my specializations
 #
 
-/sierra/Dev/rabartl/PROJECTS/Trilinos.base/Trilinos/checkin-test.py \
---make-options="-j8" \
---ctest-options="-j8" \
+../Trilinos/checkin-test.py \
+--no-eg-git-version-check \
+--make-options=-j8 \
+--ctest-options=-j4 \
 --ctest-timeout=180 \
---without-serial-release \
 --commit-msg-header-file=checkin_message \
 $EXTRA_ARGS  
