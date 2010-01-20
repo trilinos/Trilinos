542d92c64ebd169a0b0fb8a0134214ec046899fc
Christopher G. Baker <bakercg@ornl.gov>
2010-01-20 13:54:58 -0500

    Multiply Hybrid node changes and corrections.
    Added new machine configuration files. Files now for testing hybrid, gpu only, mpi only and threaded.
    
    Discovered a bug in CrsMatrix::apply() semantics. RHS matrix must be initialized.
    Otherwise, NaNs will not be annihilated by multiplication by zero.
    Also, valgrind will complain about usage of uninitialized data.
    Modified Power Method examples and Hybrid platform test to address this (was causing failures on GPU nodes). They all run beautifully now.

--- packages/tpetra/test/BasicPerfTest/HybridPlatformTest.cpp
+++ packages/tpetra/test/BasicPerfTest/HybridPlatformTest.cpp
@@ -20,29 +20,27 @@ bool testPassed;
 template <class Node, class Scalar, class Ordinal>
 Scalar power_method(const Teuchos::RCP<const Tpetra::Operator<Scalar,Ordinal,Ordinal,Node> > &A, size_t niters, typename Teuchos::ScalarTraits<Scalar>::magnitudeType tolerance, bool verbose) {
   typedef typename Teuchos::ScalarTraits<Scalar>::magnitudeType Magnitude;
-  // create three vectors; do not bother initializing them to zero
-  Tpetra::Vector<Scalar,Ordinal,Ordinal,Node> q(A->getRangeMap(), false);
-  Tpetra::Vector<Scalar,Ordinal,Ordinal,Node> z(A->getRangeMap(), false);
-  Tpetra::Vector<Scalar,Ordinal,Ordinal,Node> resid(A->getRangeMap(), false);
-
+  const bool NO_INITIALIZE_TO_ZERO = false;
+  // create three vectors; do not bother initializing q to zero, as we will fill it with random below
+  Tpetra::Vector<Scalar,Ordinal,Ordinal,Node> z(A->getRangeMap(), NO_INITIALIZE_TO_ZERO),
+                                              q(A->getRangeMap()),
+                                              r(A->getRangeMap());
   // Fill z with random numbers
   z.randomize();
-
   // Variables needed for iteration
+  const Scalar ONE  = Teuchos::ScalarTraits<Scalar>::one();
+  const Scalar ZERO = Teuchos::ScalarTraits<Scalar>::zero();
   Scalar lambda = static_cast<Scalar>(0.0);
   Magnitude normz, residual = static_cast<Magnitude>(0.0);
-
-  const Scalar one  = Teuchos::ScalarTraits<Scalar>::one();
-  const Scalar zero = Teuchos::ScalarTraits<Scalar>::zero();
-
+  // power iteration
   for (size_t iter = 0; iter < niters; ++iter) {
     normz = z.norm2();                            // Compute 2-norm of z
-    q.scale(one/normz, z);                        // Set q = z / normz
+    q.scale(ONE/normz, z);                        // Set q = z / normz
     A->apply(q, z);                               // Compute z = A*q
     lambda = q.dot(z);                            // Approximate maximum eigenvalue: lamba = dot(q,z)
     if ( iter % 100 == 0 || iter + 1 == niters ) {
-      resid.update(one, z, -lambda, q, zero);     // Compute A*q - lambda*q
-      residual = Teuchos::ScalarTraits<Scalar>::magnitude(resid.norm2() / lambda);
+      r.update(ONE, z, -lambda, q, ZERO);     // Compute A*q - lambda*q
+      residual = Teuchos::ScalarTraits<Scalar>::magnitude(r.norm2() / lambda);
       if (verbose) {
         std::cout << "Iter = " << iter << "  Lambda = " << lambda 
                   << "  Residual of A*q - lambda*q = " 
@@ -101,7 +99,7 @@ int main(int argc, char **argv) {
   // Get test parameters from command-line processor
   //  
   Teuchos::CommandLineProcessor cmdp(false,true);
-  string fnMachine("machine.xml");
+  string fnMachine("mpionly.xml");
   cmdp.setOption("matrix-file",&fnMatrix,"Filename for Harwell-Boeing test matrix.");
   cmdp.setOption("machine-file",&fnMachine,"Filename for XML machine description file.");
   if (cmdp.parse(argc,argv) != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL) {
