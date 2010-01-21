0183bd0e216234d12b3770b4578ae70b034ec9d7
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-01-21 16:38:34 -0700

    Adding DLL skeleton for SIERRA.

--- SIERRA/bjam/config_headers/Epetra_config.h
+++ SIERRA/bjam/config_headers/Epetra_config.h
@@ -1,11 +1,7 @@
 
 #include "TrilinosPlatform_config.h"
 
-/* Define if want to build with epetra enabled */
 #define HAVE_EPETRA_TEUCHOS 1
-
-/* Define if want to build with fatal_messages enabled */
 #define HAVE_FATAL_MESSAGES 1
-
-/* Define if want to build with format_io enabled */
 #define HAVE_FORMAT_IO 1
+#define EPETRA_LIB_DLL_EXPORT
