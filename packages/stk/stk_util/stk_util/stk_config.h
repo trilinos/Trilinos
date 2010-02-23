6a93537ce65b87071da0d59e657ccd29b6b80e66
Russell Hooper <rhoope@sandia.gov>
2010-02-23 07:45:22 -0700

    A few more tweaks for serial stk.

--- packages/stk/stk_util/stk_util/stk_config.h
+++ packages/stk/stk_util/stk_util/stk_config.h
@@ -3,7 +3,10 @@
 
 #define STK_PACKAGE stk
 #define STK_VERSION 0.1a
-#define STK_HAS_MPI
 #define STK_HAS_SNL_EXODUSII
+
+#ifdef HAVE_MPI
+#define STK_HAS_MPI
+#endif
 
 #endif /* stk_util_config_h */
