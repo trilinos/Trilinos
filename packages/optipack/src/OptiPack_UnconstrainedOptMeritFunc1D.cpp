b54414e17659deb6cd9508d533855466ede8caf4
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-03-08 15:56:58 -0700

    Standarizing the copyright headers.

--- packages/optipack/src/OptiPack_UnconstrainedOptMeritFunc1D.cpp
+++ packages/optipack/src/OptiPack_UnconstrainedOptMeritFunc1D.cpp
@@ -2,7 +2,7 @@
 // @HEADER
 // ***********************************************************************
 // 
-//    GlobiPack: Collection of Scalar 1D globalizaton utilities
+//    OptiPack: Collection of simple Thyra-based Optimization ANAs
 //                 Copyright (2009) Sandia Corporation
 // 
 // Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
@@ -12,12 +12,12 @@
 // it under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation; either version 2.1 of the
 // License, or (at your option) any later version.
-//  
+// 
 // This library is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-//  
+// 
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
