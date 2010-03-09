d01012a0683251eb4f945029e1a6ca337d208031
Russell Hooper <rhoope@sandia.gov>
2010-03-09 09:52:34 -0700

    Making Trilinos stk consistent with changes to sierra stk
    =============================
    Build/Test Cases Summary
    =============================
    
    Tue Mar  9 10:01:12 MST 2010
    
    Enabled Packages: STK
    Enabled all Forward Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=3,notpassed=0 (0.0 min)
    1) SERIAL_RELEASE => passed: Trilinos/SERIAL_RELEASE: passed=3,notpassed=0 (0.0 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    a5f696a Making Trilinos stk consistent with changes to sierra stk

--- packages/stk/stk_util/stk_util/stk_config.h
+++ packages/stk/stk_util/stk_util/stk_config.h
@@ -9,20 +9,19 @@
 #ifndef stk_util_config_h
 #define stk_util_config_h
 
-#ifndef STK_BUILT_IN_SIERRA
+#ifdef STK_BUILT_IN_SIERRA
+#define STK_HAS_MPI
+#else
 // This file gets created by cmake during a Trilinos build
 // and will not be present in a sierra build using bjam or associated wrappers
 #include <STK_config.h>
 #ifdef HAVE_MPI
 #define STK_HAS_MPI
 #endif
-#else
-#define STK_HAS_MPI
 #endif
 
 #define STK_PACKAGE stk
 #define STK_VERSION 0.1a
 #define STK_HAS_SNL_EXODUSII
-
 
 #endif /* stk_util_config_h */
