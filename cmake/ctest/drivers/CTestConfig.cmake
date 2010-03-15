564216464af8ce27c258238b29ffd9f09dfa051d
David Cole <david.cole@kitware.com>
2010-03-15 13:48:59 -0400

    Add generic cron_driver.sh, usable from any Linux or Mac box.
    The bash shell script clones or updates the Trilinos repository
    to get the most up to date scripts. Then it downloads and installs
    the release of CMake. Then it uses that ctest to drive the script
    TrilinosDriverDashboard.cmake.
    
    That script configures/builds/tests the TrilinosDriver project at
    cmake/ctest/drivers/CMakeLists.txt. Each *test* on the driving
    dashboard is a full separate run of a Trilinos dashboard.
    
    At most, one of the subdirectories of cmake/ctest/drivers is used
    on a given machine.

--- cmake/ctest/drivers/CTestConfig.cmake
+++ cmake/ctest/drivers/CTestConfig.cmake
@@ -1,20 +1,7 @@
-#=============================================================================
-# CMake - Cross Platform Makefile Generator
-# Copyright 2000-2009 Kitware, Inc., Insight Software Consortium
-#
-# Distributed under the OSI-approved BSD License (the "License");
-# see accompanying file Copyright.txt for details.
-#
-# This software is distributed WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
-# See the License for more information.
-#=============================================================================
-set(CTEST_PROJECT_NAME "Trilinos_Driver")
-set(CTEST_NIGHTLY_START_TIME "21:00:00 EDT")
+set(CTEST_PROJECT_NAME "TrilinosDriver")
+set(CTEST_NIGHTLY_START_TIME "06:01:00 UTC")
 
 set(CTEST_DROP_METHOD "http")
 set(CTEST_DROP_SITE "trilinos.sandia.gov")
 set(CTEST_DROP_LOCATION "/cdash/submit.php?project=TrilinosDriver")
 set(CTEST_DROP_SITE_CDASH TRUE)
-set(CTEST_CDASH_VERSION "1.5")
-set(CTEST_CDASH_QUERY_VERSION TRUE)
