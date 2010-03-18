cb48bcf11efb9057d91fd690eac08042ad8b6925
Alan Williams <william@sandia.gov>
2010-03-18 15:00:23 -0600

    Minor fix to Teuchos::Hashtable.
    An int counter called 'nHits_' is incremented by each call to
    the 'accumulateAvgFill' method. The Aleph application uses Teuchos::Hashtable
    and over the course of a long transient run, this happened more than
    2 billion times which exhausted the capacity of int and caused errors.
    This fix changes nHits_ to be of type size_t. I realize this only
    puts off the problem, but it puts it off a long ways on most machines.
    
    A better solution is to use tr1::unordered_map. But that isn't supported
    by all compilers yet (PGI).
    Another solution would be to avoid the continual incrementing of nHits_
    in Teuchos::Hashtable. I'll try to look into this next.
    
    =============================
    Build/Test Cases Summary
    =============================
    
    Sat Mar 20 17:56:59 MDT 2010
    
    Enabled Packages: Teuchos
    Disabled Packages: Sundance,Rythmos,MOOCHO,Intrepid
    Enabled all Forward Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=812,notpassed=0 (9.19666775068 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect commit/push readiness! (0.0 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    a856c07 Minor fix to Teuchos::Hashtable.

---
+++ packages/teuchos/test/UnitTest/Hashtable_UnitTests.cpp
@@ -0,0 +1,38 @@
+#include "Teuchos_UnitTestHarness.hpp"
+#include "Teuchos_as.hpp"
+#include "Teuchos_Hashtable.hpp"
+
+
+namespace {
+
+
+TEUCHOS_UNIT_TEST_TEMPLATE_2_DECL( Hashtable, test0, Key, Value )
+{
+  using Teuchos::as;
+
+  Teuchos::Hashtable<Key,Value> hashtable;
+
+  hashtable.put(as<Key>(1), as<Value>(1));
+  hashtable.put(as<Key>(3), as<Value>(9));
+  hashtable.put(as<Key>(5), as<Value>(7));
+
+  TEST_EQUALITY( hashtable.size(), 3 );
+
+  TEST_EQUALITY( hashtable.containsKey(as<Key>(3)), true );
+  TEST_EQUALITY( hashtable.containsKey(as<Key>(4)), false );
+
+  TEST_EQUALITY( hashtable.get(as<Key>(5)), as<Value>(7) );
+}
+
+//
+// Instantiations
+//
+
+
+#define UNIT_TEST_GROUP( K, V ) \
+  TEUCHOS_UNIT_TEST_TEMPLATE_2_INSTANT( Hashtable, test0, K, V )
+
+UNIT_TEST_GROUP(int, int)
+UNIT_TEST_GROUP(int, float)
+
+} // namespace
