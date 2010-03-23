2567888f0453f0ea78e530b077e088a6ce759ce1
James M. Willenbring <jmwille@sandia.gov>
2010-03-23 08:34:51 -0600

    Making the OpenMP check optional based on having OpenMP enabled.
    Unconditionally checking for OpenMP caused problems when searching for the C++
    flags for the nightly Zoltan C-only build.
    
    =============================
    Build/Test Cases Summary
    =============================
    
    Tue Mar 23 12:48:11 MDT 2010
    
    Enabled Packages: TrilinosFramework
    Disabled Packages: Rythmos
    Enabled all Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=1112,notpassed=0 (18.3155057192 min)
    1) SERIAL_RELEASE => passed: Trilinos/SERIAL_RELEASE: passed=1156,notpassed=0 (5.63066145182 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    53967e8 Making the OpenMP check optional based on having OpenMP enabled.

--- cmake/TrilinosProbeEnv.cmake
+++ cmake/TrilinosProbeEnv.cmake
@@ -86,8 +86,8 @@ ENDIF()
 
 # OpenMP isn't really a TPL because support is built into the compiler.
 
-INCLUDE(FindOpenMP)
 IF(Trilinos_ENABLE_OpenMP)
+  INCLUDE(FindOpenMP)
   IF(OPENMP_FOUND)
     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${OpenMP_CXX_FLAGS}")
     SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${OpenMP_C_FLAGS}")
