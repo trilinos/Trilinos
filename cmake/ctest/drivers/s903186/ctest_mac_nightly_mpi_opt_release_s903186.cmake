e9c71fa740909397a6766e0d83ae5b6c62c09ac0
James M. Willenbring <jmwille@sandia.gov>
2010-03-25 13:44:26 -0600

    Adding release testing scripts for s903186.
    Eventually we want to modify these scripts so that modification is
    not necessary for each release (other than changing the version
    number in one place), but that was not implemented for 10.2.

---
+++ cmake/ctest/drivers/s903186/ctest_mac_nightly_mpi_opt_release_s903186.cmake
@@ -0,0 +1,28 @@
+
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.s903186.gcc.cmake")
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../../TrilinosVersion.cmake")
+
+#
+# Set the options specific to this build case
+#
+
+SET(COMM_TYPE MPI)
+SET(BUILD_TYPE RELEASE)
+SET(BUILD_DIR_NAME "MPI_OPT_${Trilinos_VERSION}")
+SET(Trilinos_TRACK ${Trilinos_TESTING_TRACK})
+SET(Trilinos_BRANCH ${Trilinos_REPOSITORY_BRANCH})
+
+SET(EXTRA_EXCLUDE_PACKAGES Zoltan PyTrilinos)
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DDART_TESTING_TIMEOUT:STRING=600"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON"
+  "-DTPL_ENABLE_Boost:BOOL=ON"
+  "-DNOX_ENABLE_ABSTRACT_IMPLEMENTATION_LAPACK=ON"
+  )
+
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
+
+TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
---
+++ cmake/ctest/drivers/s903186/ctest_mac_nightly_serial_debug_release_s903186.cmake
@@ -0,0 +1,30 @@
+
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.s903186.gcc.cmake")
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../../TrilinosVersion.cmake")
+
+#
+# Set the options specific to this build case
+#
+
+SET(COMM_TYPE SERIAL)
+SET(BUILD_TYPE DEBUG)
+SET(BUILD_DIR_NAME "SERIAL_DEBUG_${Trilinos_VERSION}")
+SET(Trilinos_TRACK ${Trilinos_TESTING_TRACK})
+SET(Trilinos_BRANCH ${Trilinos_REPOSITORY_BRANCH})
+
+SET(CTEST_PARALLEL_LEVEL 8)
+
+# Exclude Sundance because of strange segfault (see bug 4382)
+SET(EXTRA_EXCLUDE_PACKAGES Sundance PyTrilinos)
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DDART_TESTING_TIMEOUT:STRING=600"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON"
+  "-DTPL_ENABLE_Boost:BOOL=ON"
+  )
+
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
+
+TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
