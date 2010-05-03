27a4d4dab97c1861a4d0f624d0033720bbfd8cf2
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-05-03 13:44:11 -0600

    Upgraded Trilinos CTest/CDash driver on gabriel.sandia.gov to use TrilinosDriver system
    Here I have updated the scripts to use the new TrilinosDriver system.  Now the
    MPI_DEBUG_PS and SERIAL_RELEASE_PS builds run in parallel of each other where
    each uses four processes.  This ensures that no more than eight processes are
    used at any one time so the machine will not be overloaded.  Hopefully the
    throughput willl be better now.
    
    The new crontab script entry has:
    
     10 00  *  *  *  cd /mnt/disk2/rabartl/CDash/Trilinos.base/Trilinos/cmake/ctest/drivers/gabriel && ./cron_driver_gabriel.sh &> /mnt/disk2/rabartl/CDash/Trilinos.base/cron_driver_gabriel.log
    
    =============================
    Build/Test Cases Summary
    =============================
    
    Mon May  3 14:00:19 MDT 2010
    
    Enabled Packages: TrilinosFramework
    Enabled all Forward Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=52,notpassed=0 (1.4935929815 min)
    1) SERIAL_RELEASE => passed: Trilinos/SERIAL_RELEASE: passed=52,notpassed=0 (1.0470189333 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    657ea9a Upgraded Trilinos CTest/CDash driver on gabriel.sandia.gov to use TrilinosDriver system
    19b7ba5 Major upgrade to TrilinosDriver scripts
    5af50a8 Adding echo of command-line and added warning about html-proxy not set.
    1ee8785 Moving SET_DEFAULT(..) and SET_DEFAULT_AND_FROM_ENV(...) to modules for reuse, removed defined assert from PRINT_VAR(...)

--- cmake/ctest/drivers/gabriel/cron_driver_gabriel.sh
+++ cmake/ctest/drivers/gabriel/cron_driver_gabriel.sh
@@ -1,73 +1,15 @@
 #!/bin/bash
 
-# Set what ctest to use
-CTEST_EXE=/usr/local/bin/ctest
-
-
 echo
 echo "Starting nightly Trilinos testing on gabriel: `date`"
 echo
 
-#BASEDIR=`echo $0 | sed "s/\(.*\)\/Trilinos\/cmake\/ctest\/.*\.sh/\1/g"`
-BASEDIR=/mnt/disk2/rabartl/CDash/Trilinos.base
-echo "BASEDIR = $BASEDIR"
-DRIVER_SCRIPT_DIR=$BASEDIR/Trilinos/cmake/ctest/drivers/gabriel
-echo "DRIVER_SCRIPT_DIR = $DRIVER_SCRIPT_DIR"
-
-
-echo
-echo "Setting up thet environment ..."
-echo
-
-cd $DRIVER_SCRIPT_DIR
-source bash_profile
-echo $PATH
-which eg
-
-
-echo
-echo "Updating Trilinos driver code: `date`"
-echo
-
-cd $BASEDIR/Trilinos
-eg pull
-
-cd $BASEDIR
-  
-echo
-echo "Doing mpi debug build: `date`"
-echo
-
-time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_mpi_debug_gabriel.cmake -VV \
-  &> $BASEDIR/ctest_linux_nightly_mpi_debug_gabriel.out
-
-
-echo
-echo "Kill any remaining 'orted' processes: `date`"
-echo
-
-killall -s 9 orted
-
-
-echo
-echo "Doing serial release build: `date`"
-echo
-
-time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_serial_release_gabriel.cmake -VV \
-  &> $BASEDIR/ctest_linux_nightly_serial_release_gabriel.out
-
-
-echo
-echo "Doing serial debug boost tracing build: `date`"
-echo
-
-time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_linux_nightly_serial_debug_boost_tracing_gabriel.cmake -VV \
-  &> $BASEDIR/ctest_linux_nightly_serial_debug_boost_tracing_gabriel.out
-
+export TDD_PARALLEL_LEVEL=2
+export TDD_HTTP_PROXY="http://wwwproxy.sandia.gov:80/"
+time ../cron_driver.py
 
 echo
 echo "Ending nightly Trilinos testing on gabriel: `date`"
 echo
-
 
 /home/rabartl/mailmsg.py "Finished nightly Trilinos CMake tests gabriel: http://trilinos-dev.sandia.gov/cdash/index.php?project=Trilinos"
