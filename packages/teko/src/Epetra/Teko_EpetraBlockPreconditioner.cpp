3ca1716c6c393a1c7ebb85e279f049f94ac7b376
Eric C. Cyr <eccyr@sandia.gov>
2010-05-05 06:49:07 -0600

    Removed unneeded comments in epetra block preconditioner

--- packages/teko/src/Epetra/Teko_EpetraBlockPreconditioner.cpp
+++ packages/teko/src/Epetra/Teko_EpetraBlockPreconditioner.cpp
@@ -96,9 +96,9 @@ void EpetraBlockPreconditioner::initPreconditioner(bool clearOld)
   */
 void EpetraBlockPreconditioner::buildPreconditioner(const Epetra_Operator & A,bool clear)
 {
+   Teko_DEBUG_SCOPE("EBP::buildPreconditioner",10);
+
    // extract EpetraOperatorWrapper (throw on failure) and corresponding thyra operator
-   // const RCP<const EpetraOperatorWrapper> & eow = rcp_dynamic_cast<const EpetraOperatorWrapper>(rcpFromRef(A),true);
-   // RCP<const Thyra::LinearOpBase<double> > thyraA = eow->getThyraOp(); 
    RCP<const Thyra::LinearOpBase<double> > thyraA = extractLinearOp(A);
 
    // set the mapping strategy
@@ -140,20 +140,18 @@ void EpetraBlockPreconditioner::buildPreconditioner(const Epetra_Operator & A,bo
   */
 void EpetraBlockPreconditioner::buildPreconditioner(const Epetra_Operator & A,const Epetra_MultiVector & epetra_mv,bool clear)
 {
+   Teko_DEBUG_SCOPE("EBP::buildPreconditioner - with solution",10);
+
    // extract EpetraOperatorWrapper (throw on failure) and corresponding thyra operator
-   // const RCP<const EpetraOperatorWrapper> & eow = rcp_dynamic_cast<const EpetraOperatorWrapper>(rcpFromRef(A),true);
-   // RCP<const Thyra::LinearOpBase<double> > thyraA = eow->getThyraOp(); 
    RCP<const Thyra::LinearOpBase<double> > thyraA = extractLinearOp(A);
 
    // set the mapping strategy
-   // SetMapStrategy(rcp(new InverseMappingStrategy(eow->getMapStrategy())));
    SetMapStrategy(rcp(new InverseMappingStrategy(extractMappingStrategy(A))));
 
    TEUCHOS_ASSERT(getMapStrategy()!=Teuchos::null);
    
    // build the thyra version of the source multivector
    RCP<Thyra::MultiVectorBase<double> > thyra_mv = Thyra::createMembers(thyraA->range(),epetra_mv.NumVectors());
-   // getMapStrategy()->copyEpetraIntoThyra(epetra_mv,thyra_mv.ptr(),*eow);
    getMapStrategy()->copyEpetraIntoThyra(epetra_mv,thyra_mv.ptr(),*this);
 
    // build preconObj_ 
@@ -188,6 +186,8 @@ void EpetraBlockPreconditioner::buildPreconditioner(const Epetra_Operator & A,co
   */
 void EpetraBlockPreconditioner::rebuildPreconditioner(const Epetra_Operator & A)
 {
+   Teko_DEBUG_SCOPE("EBP::rebuildPreconditioner",10);
+
    // if the preconditioner hasn't been built yet, rebuild from scratch
    if(not firstBuildComplete_) {
       buildPreconditioner(A,false);
@@ -197,8 +197,6 @@ void EpetraBlockPreconditioner::rebuildPreconditioner(const Epetra_Operator & A)
 
    // extract EpetraOperatorWrapper (throw on failure) and corresponding thyra operator
    Teko_DEBUG_EXPR(timer.start(true));
-   // const RCP<const EpetraOperatorWrapper> & eow = rcp_dynamic_cast<const EpetraOperatorWrapper>(rcpFromRef(A),true);
-   // RCP<const Thyra::LinearOpBase<double> > thyraA = eow->getThyraOp(); 
    RCP<const Thyra::LinearOpBase<double> > thyraA = extractLinearOp(A);
    Teko_DEBUG_EXPR(timer.stop());
    Teko_DEBUG_MSG("EBP::rebuild get thyraop time =  " << timer.totalElapsedTime(),2);
@@ -235,6 +233,8 @@ void EpetraBlockPreconditioner::rebuildPreconditioner(const Epetra_Operator & A)
   */
 void EpetraBlockPreconditioner::rebuildPreconditioner(const Epetra_Operator & A,const Epetra_MultiVector & epetra_mv)
 {
+   Teko_DEBUG_SCOPE("EBP::rebuildPreconditioner - with solution",10);
+
    // if the preconditioner hasn't been built yet, rebuild from scratch
    if(not firstBuildComplete_) {
       buildPreconditioner(A,epetra_mv,false);
@@ -244,8 +244,6 @@ void EpetraBlockPreconditioner::rebuildPreconditioner(const Epetra_Operator & A,
 
    // extract EpetraOperatorWrapper (throw on failure) and corresponding thyra operator
    Teko_DEBUG_EXPR(timer.start(true));
-   // const RCP<const EpetraOperatorWrapper> & eow = rcp_dynamic_cast<const EpetraOperatorWrapper>(rcpFromRef(A),true);
-   // RCP<const Thyra::LinearOpBase<double> > thyraA = eow->getThyraOp(); 
    RCP<const Thyra::LinearOpBase<double> > thyraA = extractLinearOp(A);
    Teko_DEBUG_EXPR(timer.stop());
    Teko_DEBUG_MSG("EBP::rebuild get thyraop time =  " << timer.totalElapsedTime(),2);
