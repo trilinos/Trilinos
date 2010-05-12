b29a42fcc2e63408fb8cccea4e697381e1db0d8b
Roger Pawlowski <rppawlo@sandia.gov>
2010-05-12 13:16:39 -0600

    Updating the cmake demangling check to work on gnu 4.3+ compilers.
    This check was previously failing on 4.3+ compilers because in gnu 4.3, they cleaned up the header dependencies and we now need to include cstdlib explicitly.

--- packages/teuchos/cmake/CheckForAbiDemangle.cmake
+++ packages/teuchos/cmake/CheckForAbiDemangle.cmake
@@ -8,6 +8,7 @@ FUNCTION(CHECK_FOR_ABI_DEMANGLE VARNAME)
   "
   #include <cxxabi.h>
   #include <string>
+  #include <cstdlib>
   namespace MyNamespace {
     class MyClass {};
   }
@@ -20,7 +21,7 @@ FUNCTION(CHECK_FOR_ABI_DEMANGLE VARNAME)
     char
       *_demangledName = abi::__cxa_demangle(mangledName.c_str(),0,0,&status);
     const std::string demangledName(_demangledName);
-    free(_demangledName);
+    std::free(_demangledName);
     return ( demangledName == \"MyNamespace::MyClass\" ? 0 : 1 );
   }
   "
