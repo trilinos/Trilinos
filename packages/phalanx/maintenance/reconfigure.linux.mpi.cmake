8c901abcf36644ae1c4c702912df380a5c8ba1a0
Roger Pawlowski <rppawlo@sandia.gov>
2010-05-13 09:16:40 -0600

    Minor changes to sample build script

--- packages/phalanx/maintenance/reconfigure.linux.mpi.cmake
+++ packages/phalanx/maintenance/reconfigure.linux.mpi.cmake
@@ -1,6 +1,7 @@
 #!/usr/bin/tcsh
 cmake \
       -D CMAKE_INSTALL_PREFIX="/home/rppawlo/JUNK15" \
+      -D Trilinos_ENABLE_DEBUG=ON \
       -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
       -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON \
       -D Trilinos_ENABLE_Phalanx:BOOL=ON \
@@ -19,15 +20,15 @@ cmake \
       -D CMAKE_CXX_COMPILER:FILEPATH="/home/rppawlo/local/bin/mpiCC" \
       -D CMAKE_C_COMPILER:FILEPATH="/home/rppawlo/local/bin/mpicc" \
       -D CMAKE_Fortran_COMPILER:FILEPATH="/home/rppawlo/local/bin/mpif77" \
-      -D CMAKE_CXX_FLAGS:STRING="-DNDEBUG -O3 -ansi -pedantic -ftrapv -Wall -Wno-long-long -Wno-strict-aliasing -DBOOST_NO_HASH" \
+      -D CMAKE_CXX_FLAGS:STRING="-g -O0 -ansi -pedantic -ftrapv -Wall -Wno-long-long -Wno-strict-aliasing -DBOOST_NO_HASH" \
       -D Trilinos_EXTRA_LINK_FLAGS:STRING="-L/usr/lib -lgfortran" \
-      -D HAVE_STRING_H:BOOL=ON \
       -D CMAKE_VERBOSE_MAKEFILE:BOOL=OFF \
       -D Trilinos_VERBOSE_CONFIGURE:BOOL=OFF \
       -D CMAKE_SKIP_RULE_DEPENDENCY=ON \
       -D Trilinos_ENABLE_STRONG_CXX_COMPILE_WARNINGS=OFF \
       -D Trilinos_ENABLE_STRONG_C_COMPILE_WARNINGS=OFF \
       -D Trilinos_ENABLE_SHADOW_WARNINGS=OFF \
+      -D CMAKE_BUILD_TYPE:STRING=NONE \
        ../Trilinos
 
 ##      -D CMAKE_EXE_LINKER_FLAGS:STRING="-L/usr/lib -lgfortran" \
@@ -38,3 +39,5 @@ cmake \
 ##      -D TPL_ENABLE_ADOLC:BOOL=ON \
 ##      -D ADOLC_INCLUDE_DIRS:FILEPATH="/home/rppawlo/junk/include" \
 ##      -D ADOLC_LIBRARY_DIRS:FILEPATH="/home/rppawlo/junk/lib" \
+
+##      -D CMAKE_CXX_FLAGS:STRING="-DNDEBUG -O3 -ansi -pedantic -ftrapv -Wall -Wno-long-long -Wno-strict-aliasing -DBOOST_NO_HASH" \
