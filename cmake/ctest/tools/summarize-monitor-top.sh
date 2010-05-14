0b6f4889b928c92068fa8e83f5de241ed4cd54f2
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-05-14 11:58:04 -0600

    Updated documentation for top monitor scripts.

--- cmake/ctest/tools/summarize-monitor-top.sh
+++ cmake/ctest/tools/summarize-monitor-top.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #
-# Grab just the top lines of each top output to see what processes are
-# taking the most.  This script should be run on the output from the
-# monitor-top.sh command.  Therefore, if monitor-top.sh was run as:
+# Grab just the top lines of each top output to see what processes are taking
+# the most.  This script should be run on the output from the monitor-top.sh
+# command.  Therefore, if monitor-top.sh was run as:
 #
-#   $ monitor-top.sh SOME_BASE_DIR/top.log
+#   $ monitor-top.sh &> SOME_BASE_DIR/top.log
 #
 # then you would run this script as:
 #
-#   $ summarize-monitor-top.sh SOME_BASE_DIR [NUM_LINES_TO_TAKE]
+#   $ summarize-monitor-top.sh SOME_BASE_DIR/top.log [NUM_LINES_TO_TAKE]
 #
 
-# Number of times to take.  Note, this will include the 'top' header
-# so you will get several less of the actual process lines.
-
+# The top log file
 TOP_LOG_INPUT_FILE=$1
 
+# Number of line to take.  Note, this will include the 'top' header so you
+# will get several less of the actual process lines.
 NUMBER_OF_LINES_TO_TAKE=$2
 
 if [ "$NUMBER_OF_LINES_TO_TAKE" == "" ] ; then
