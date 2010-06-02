9ba81104ccf0da3643c580a538e12d9f206e5f18
klnusba <klnusbaum@gmail.com>
2010-06-02 17:05:16 -0600

    Officially added Qt a TPL so that it's libraries and include dirs are included when using the FindTrilinos module.
    -Kurtis
    
    =============================
    Build/Test Cases Summary
    =============================
    
    Thu Jun  3 09:31:32 MDT 2010
    
    Enabled Packages: TrilinosFramework, Optika
    Enabled all Forward Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=54,notpassed=0 (2.24647931655 min)
    1) SERIAL_RELEASE => passed: Trilinos/SERIAL_RELEASE: passed=54,notpassed=0 (0.769670569897 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    7036240 Officially added Qt a TPL so that it's libraries and include dirs are included when using the FindTrilinos module.

---
+++ cmake/TPLs/FindTPLQt.cmake
@@ -0,0 +1,6 @@
+INCLUDE(TPLDeclareLibraries)
+
+TPL_DECLARE_LIBRARIES( Qt
+  REQUIRED_HEADERS QMainWindow QDialog QAbstratItemModel QTreeView QItemDelegate QPushButton QGridLayout QSpinBox QComboBox QLineEdit QLabel QScrollArea QDir QXmlStreamWriter QXmlStreamReader QStringList
+  REQUIRED_LIBS_NAMES QtCore QtGui
+  )
