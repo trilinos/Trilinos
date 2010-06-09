ca016b178419580735b2f1e19fbeb659cc6faf1f
Alan Williams <william@sandia.gov>
2010-06-09 16:09:37 -0600

    Update VbrMatrix unit-test to exercise multiple-vector matvec.
    =============================
    Build/Test Cases Summary
    =============================
    
    Wed Jun  9 16:22:11 MDT 2010
    
    Enabled Packages: Kokkos, Tpetra
    Disabled Packages: Sundance,Rythmos,MOOCHO,Intrepid
    Enabled all Forward Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=376,notpassed=0 (9.3363171339 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect commit/push readiness! (0.0 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    108d239 Update VbrMatrix unit-test to exercise multiple-vector matvec.
    2e05a85 Kokkos BlockSparseMultiply now handles MultiVectors with multiple columns.

--- packages/tpetra/test/VbrMatrix/VbrMatrix_UnitTests.cpp
+++ packages/tpetra/test/VbrMatrix/VbrMatrix_UnitTests.cpp
@@ -357,7 +357,7 @@ namespace {
   }
 
   ////
-  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL( VbrMatrix, apply1, LO, GO, Scalar, Node )
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL( VbrMatrix, applySimple, LO, GO, Scalar, Node )
   {
     //This test builds a block-diagonal matrix and tests the apply method.
     //Since the matrix is block-diagonal, the apply does not require any
@@ -479,7 +479,7 @@ namespace {
   }
 
   ////
-  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL( VbrMatrix, apply2, LO, GO, Scalar, Node )
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL( VbrMatrix, applyParallel, LO, GO, Scalar, Node )
   {
     //This test fills a (block-tri-diagonal) matrix such that in parallel the
     //column-map should have an overlapping set of entries (i.e., different than
@@ -565,6 +565,101 @@ namespace {
     TEST_COMPARE_FLOATING_ARRAYS( v2, v3, 2*Teuchos::ScalarTraits<Scalar>::eps());
   }
 
+  ////
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL( VbrMatrix, applyParallelMV, LO, GO, Scalar, Node )
+  {
+    //This test fills a (block-tri-diagonal) matrix such that in parallel the
+    //column-map should have an overlapping set of entries (i.e., different than
+    //the row-map), and verify that apply works correctly. If the column-map
+    //has an overlapping set of entries, then apply must do an import of the x
+    //vector in order to get a correct result.
+    //This test uses MultiVectors that have more than 1 column.
+
+    RCP<Node> node = getNode<Node>();
+    typedef ScalarTraits<Scalar> ST;
+    typedef BlockMultiVector<Scalar,LO,GO,Node> BMV;
+    typedef VbrMatrix<Scalar,LO,GO,Node> MAT;
+    typedef typename ST::magnitudeType Mag;
+    typedef ScalarTraits<Mag> MT;
+    const global_size_t INVALID = OrdinalTraits<global_size_t>::invalid();
+    // get a comm
+    RCP<const Comm<int> > comm = getDefaultComm();
+    GO indexBase = 0;
+    // create a Map
+    const size_t numLocalBlocks = 2;
+    GO maxGlobalBlock = numLocalBlocks*comm->getSize();
+    const LO blockSize = 2;
+    const size_t maxEntriesPerRow = 3;
+    RCP<BlockMap<LO,GO,Node> > rowmap = rcp( new BlockMap<LO,GO,Node>(INVALID,numLocalBlocks,blockSize,indexBase,comm,node) );
+
+    //now set up the list of block-column-ids that we expect the
+    //column-map to contain after fillComplete:
+    size_t numLocalColBlocks = numLocalBlocks;
+    if (comm->getRank() != 0) ++numLocalColBlocks;
+    if (comm->getRank() != comm->getSize()-1) ++numLocalColBlocks;
+    Array<GO> blockColIDs(numLocalColBlocks);
+    typedef typename Array<GO>::size_type Tsize_t;
+    Teuchos::ArrayView<const GO> blk_rows = rowmap->getBlockIDs();
+    GO first_row = blk_rows[0];
+    Tsize_t offset = 0;
+    if (comm->getRank() != 0) {
+      blockColIDs[offset++] = first_row - 1;
+    }
+    GO last_row = 0;
+    LO row_offset = 0;
+    for(LO i=0; i<blk_rows.size(); ++i) {
+      blockColIDs[offset++] = blk_rows[i];
+      last_row = blk_rows[i];
+    }
+    if (offset < blockColIDs.size()) blockColIDs[offset++] = last_row + 1;
+
+    size_t numVecs = 3;
+    size_t numPointRows = rowmap->getPointMap()->getNodeNumElements();
+    RCP<BMV> bmv1 = rcp(new BMV(rowmap, numVecs));
+    RCP<BMV> bmv2 = rcp(new BMV(rowmap, numVecs));
+    RCP<BMV> bmv3 = rcp(new BMV(rowmap, numVecs));
+    bmv1->putScalar(1.0);
+    ArrayRCP<Scalar> v3 = bmv3->get1dViewNonConst();
+
+    // create the matrix
+    RCP<MAT> vbr = rcp( new MAT(rowmap,maxEntriesPerRow,DynamicProfile) );
+    for(int i=0; i<blk_rows.size(); ++i) {
+      Scalar val = 0;
+      GO row = blk_rows[i];
+      {
+        GO col = row;
+        Teuchos::Array<Scalar> blkEntry(blockSize * blockSize, row+col+1);
+        vbr->setGlobalBlockEntry(row, col, blockSize, blockSize, blockSize, blkEntry());
+        val += (row+col+1)*blockSize;
+      }
+      if (row > indexBase) {
+        GO col = row - 1;
+        Teuchos::Array<Scalar> blkEntry(blockSize * blockSize, row+col+1);
+        vbr->setGlobalBlockEntry(row, col, blockSize, blockSize, blockSize, blkEntry());
+        val += (row+col+1)*blockSize;
+      }
+      if (row < maxGlobalBlock-1) {
+        GO col = row + 1;
+        Teuchos::Array<Scalar> blkEntry(blockSize * blockSize, row+col+1);
+        vbr->setGlobalBlockEntry(row, col, blockSize, blockSize, blockSize, blkEntry());
+        val += (row+col+1)*blockSize;
+      }
+      for(int k=0; k<blockSize; ++k) {
+        for(size_t v=0; v<numVecs; ++v) {
+          v3[v*numPointRows+row_offset] = val;
+        }
+        ++row_offset;
+      }
+    }
+
+    vbr->fillComplete();
+
+    vbr->apply(*bmv1, *bmv2);
+
+    ArrayRCP<Scalar> v2 = bmv2->get1dViewNonConst();
+    TEST_COMPARE_FLOATING_ARRAYS( v2, v3, 2*Teuchos::ScalarTraits<Scalar>::eps());
+  }
+
 // 
 // INSTANTIATIONS
 //
@@ -581,9 +676,10 @@ typedef std::complex<double> ComplexDouble;
       TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, Basic, LO, GO, SCALAR, NODE ) \
       TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, SetAndGetBlockEntry1, LO, GO, SCALAR, NODE ) \
       TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, SetAndGetBlockEntry2, LO, GO, SCALAR, NODE ) \
-      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, apply1, LO, GO, SCALAR, NODE ) \
+      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, applySimple, LO, GO, SCALAR, NODE ) \
       TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, ColMap1, LO, GO, SCALAR, NODE ) \
-      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, apply2, LO, GO, SCALAR, NODE )
+      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, applyParallel, LO, GO, SCALAR, NODE ) \
+      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( VbrMatrix, applyParallelMV, LO, GO, SCALAR, NODE )
 
 
 #define UNIT_TEST_SERIALNODE(LO, GO, SCALAR) \
