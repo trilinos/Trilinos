c97dff31cad64da8e4b85b3c5e4e871df336ae60
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-06-10 10:41:41 -0600

    Breaking off AlgorithmA in a test-only library
    Here, I have broken off a test-only library for AlgorithmA used in the
    FancyOutputting test.  I did this mostly to debug an issue with an
    Intel compiler on a Mac for SIERRA.  However, this does serve as a
    nice example of how to use test-only libraries in the CMake system.
    
    =============================
    Build/Test Cases Summary
    =============================
    
    Thu Jun 10 11:12:23 MDT 2010
    
    Enabled Packages: Teuchos
    
    Build test results:
    -------------------
    0) MPI_DEBUG => passed: Trilinos/MPI_DEBUG: passed=57,notpassed=0 (3.34830645323 min)
    1) SERIAL_RELEASE => passed: Trilinos/SERIAL_RELEASE: passed=54,notpassed=0 (3.14920651913 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    a85fac5 Breaking off AlgorithmA in a test-only library

---
+++ packages/teuchos/test/FancyOutputting/AlgorithmA.hpp
@@ -0,0 +1,60 @@
+#include "Teuchos_ParameterListAcceptor.hpp"
+#include "Teuchos_VerboseObject.hpp"
+
+#ifndef TEUCHOS_ALGORITHM_A_HPP
+#define TEUCHOS_ALGORITHM_A_HPP
+
+
+void someDumbFunction( std::ostream &out, const std::string &indentSpacer );
+
+
+void someLessDumbFunction( std::ostream &out_arg );
+
+
+// This is a typical numerical class that derives from VerboseObject and does
+// outputting.  Note that the use of the OSTab class requires initialization
+// using VerboseObject::getOSTab(...) which takes care of the hassles and is
+// easy to use.
+//
+// This class also derives from ParameterListAcceptor and uses helper
+// functio  ns to read options for VerboseObject from a parameter sublist.
+class AlgorithmA
+  : public Teuchos::VerboseObject<AlgorithmA>,
+    public Teuchos::ParameterListAcceptor
+{
+public:
+
+  // Constructor(s)
+
+  AlgorithmA();
+
+  // Overridden from ParameterListAccpetor
+
+  void setParameterList(Teuchos::RCP<Teuchos::ParameterList> const& paramList);
+
+  Teuchos::RCP<Teuchos::ParameterList> getNonconstParameterList();
+
+  Teuchos::RCP<Teuchos::ParameterList> unsetParameterList();
+
+  Teuchos::RCP<const Teuchos::ParameterList> getParameterList() const;
+
+  Teuchos::RCP<const Teuchos::ParameterList> getValidParameters() const;
+
+  // Other functions
+
+  void doAlgorithm();
+
+private:
+
+  enum EAlgoType { ALGO_BOB, ALGO_JOHN, ALGO_HARRY };
+
+  static const std::string toString( AlgorithmA::EAlgoType algoType );
+
+  Teuchos::RCP<Teuchos::ParameterList> paramList_;
+  EAlgoType algoType_;
+  double algoTol_;
+  
+};
+
+
+#endif // TEUCHOS_ALGORITHM_A_HPP
