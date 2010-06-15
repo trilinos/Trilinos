2b92bb40ed8334b659931db38dba5f665cb846c0
Brent Perschbacher <bmpersc@sandia.gov>
2010-06-15 10:58:48 -0600

    Replace existing Makefile export system with one based off of find Trilinos
    The old Makefile export system while functional had several issues with it.
    Since a lot of the required functionality was shared with the find Trilinos
    capability it the Makefile export system was re-done to take advantage of that
    work. Right now both the Makefile export and find Trilinos share all the logic
    for determining which packages/libraries/includes/TPLs/etc were enabled and the
    proper ordering for correct linking. This way all the functionality for one can
    be guaranteed for both. The only difference is that the compiler decorations for
    adding include/library paths and libraries are added to the appropriate
    variables so that they can be directly used by a Makefile. For example what
    would appear in a TrilinosConfig.cmake file as "Epetra;Epetraext" would appear
    in the Makefile.export.Trilinos as "-lEpetra -lEpetraext".
    
    There are some importants changes from the old Makefile Export system. The most
    important is that the variable names have been changed. Since many new variables
    were added compared to the old Makefile export since there were a lot more
    variables in the find Trilinos set that could be useful in Makefiles as well.
    Because of that all the variables in the Makefile export files are named exactly
    the same as they were in the find Trilinos capability, which means that package
    names are not all uppercase. Also compared to the old autotools system
    specifically many variables that were previously one variable have been split
    out into several variables, for instance a package's dependent libraries and the
    TPL libraries that it uses are now two different variables.  This will likely
    mean all projects which currently use the curent Makefile export or the old
    autotools version will have to be updated to use the new variables.
    
    documentation and a simple example for this capability and the find Trilinos
    capability has been moved to commontools/importing.
    
    =============================
    Build/Test Cases Summary
    =============================
    
    Wed Jun 16 14:24:33 MDT 2010
    
    Enabled Packages: TrilinosFramework
    Enabled all Packages
    
    Build test results:
    -------------------
    0) MPI_DEBUG => FAILED: Trilinos/MPI_DEBUG: passed=1204,notpassed=1 => Not ready for final commit/push! (0.0 min)
    1) SERIAL_RELEASE => passed: Trilinos/SERIAL_RELEASE: passed=1253,notpassed=0 (0.0 min)
    
    Local commits for this build/test group:
    ----------------------------------------
    8986a57 Replace existing Makefile export system with one based off of find Trilinos
    
    ***
    *** WARNING: The acceptance criteria for doing a commit/push has *not*
    *** been met, but a commit/push is being forced anyway by --force-commit-push!
    ***

--- cmake/package_arch/PackageWritePackageConfig.cmake
+++ cmake/package_arch/PackageWritePackageConfig.cmake
@@ -1,5 +1,41 @@
 INCLUDE(PackageGeneralMacros)
 
+#
+#  This function will take a list and turn it into a space separated string
+#  adding the prefix to the front of every entry. 
+#
+FUNCTION(LIST_TO_STRING LIST PREFIX OUTPUT_STRING)
+  SET(LIST_STRING "")
+  
+  FOREACH(ITEM ${LIST})
+    SET(LIST_STRING "${LIST_STRING} ${PREFIX}${ITEM}")
+  ENDFOREACH()
+
+  SET(${OUTPUT_STRING} ${LIST_STRING} PARENT_SCOPE)
+ENDFUNCTION()
+
+#
+#  This function will take a list of libraries and turn it into a space
+#  separated string. In this case though the prefix is not always added
+#  to the front of each entry as libraries can be specified either as a
+#  name of a library to find or the absolute path to the library file
+#  with any decorations the system uses. When an absolute path is given
+#  the entry is used verbatim.
+#
+FUNCTION(LIBRARY_LIST_TO_STRING LIST PREFIX OUTPUT_STRING)
+  SET(LIST_STRING "")
+  
+  FOREACH(ITEM ${LIST})
+    IF(EXISTS ${ITEM})
+      SET(LIST_STRING "${LIST_STRING} ${ITEM}")
+    ELSE()
+      SET(LIST_STRING "${LIST_STRING} ${PREFIX}${ITEM}")
+    ENDIF()
+  ENDFOREACH()
+
+  SET(${OUTPUT_STRING} ${LIST_STRING} PARENT_SCOPE)
+ENDFUNCTION()
+
 FUNCTION(PACKAGE_WRITE_PACKAGE_CONFIG_FILE PACKAGE_NAME)
 
   IF(${PROJECT_NAME}_VERBOSE_CONFIGURE)
@@ -99,10 +135,13 @@ FUNCTION(PACKAGE_WRITE_PACKAGE_CONFIG_FILE PACKAGE_NAME)
   # Generate a note discouraging editing of the <package>Config.cmake file
   SET(DISCOURAGE_EDITING "Do not edit: This file was generated automatically by CMake.")
   
+  ######
   # Create a configure file for the build tree. Creating this file in the base dir
   # of the package since it is possible that the cmake returned path where the file
   # was found would be useful for a package, and having to dig through the hiding that
   # is done wouldn't be nice.
+  ######
+
   SET(LIBRARY_DIRS ${${PACKAGE_NAME}_LIBRARY_DIRS})
   SET(INCLUDE_DIRS ${${PACKAGE_NAME}_INCLUDE_DIRS})
 
@@ -111,10 +150,39 @@ FUNCTION(PACKAGE_WRITE_PACKAGE_CONFIG_FILE PACKAGE_NAME)
     ${CMAKE_CURRENT_BINARY_DIR}/${PACKAGE_NAME}Config.cmake
     )
 
+  IF(${PROJECT_NAME}_ENABLE_EXPORT_MAKEFILES)
+    ######
+    # Create a Makefile.export.<package_name> for the build tree. This is the equivalent
+    # of the cmake version only slightly changed so that it can be directly imported into
+    # a Makefile.
+    ######
+
+    LIST_TO_STRING("${FULL_LIBRARY_SET}" ${CMAKE_LINK_LIBRARY_FLAG} MAKEFILE_FULL_LIBRARY_SET)
+    LIST_TO_STRING("${LIBRARY_DIRS}" ${CMAKE_LIBRARY_PATH_FLAG} MAKEFILE_LIBRARY_DIRS)
+    LIST_TO_STRING("${INCLUDE_DIRS}" "-I" MAKEFILE_INCLUDE_DIRS)
+    LIST_TO_STRING("${${PACKAGE_NAME}_TPL_INCLUDE_DIRS}" "-I" MAKEFILE_${PACKAGE_NAME}_TPL_INCLUDE_DIRS)
+    LIST_TO_STRING("${${PACKAGE_NAME}_TPL_LIBRARY_DIRS}" "-I" MAKEFILE_${PACKAGE_NAME}_TPL_LIBRARY_DIRS)
+    #the TPL library names have to be treated differently
+    LIBRARY_LIST_TO_STRING("${${PACKAGE_NAME}_TPL_LIBRARIES}" ${CMAKE_LINK_LIBRARY_FLAG} MAKEFILE_${PACKAGE_NAME}_TPL_LIBRARIES)
+    LIST_TO_STRING("${FULL_PACKAGE_SET}" "" MAKEFILE_FULL_PACKAGE_SET)
+    LIST_TO_STRING("${ORDERED_FULL_TPL_SET}" "" MAKEFILE_ORDERED_FULL_TPL_SET)
+
+    #we have to handle the libraries a little differently since they can be specified either as a name with
+    #of a library to find or the absolute path to the library file with any decoration.
+
+    CONFIGURE_FILE(
+      ${PROJECT_SOURCE_DIR}/cmake/PackageConfig.export.in 
+      ${CMAKE_CURRENT_BINARY_DIR}/Makefile.export.${PACKAGE_NAME}
+      )
+  ENDIF()
+
+  ######
   # Create a configure file for the install tree and set the install target for it. This
   # file isn't generally useful inside the build tree so it is being "hidden" in the 
   # CMakeFiles directory. It will be placed in the base install directory for Trilinos
   # when installed.
+  ######
+
   SET(LIBRARY_DIRS ${CMAKE_INSTALL_PREFIX}/${${PROJECT_NAME}_INSTALL_LIB_DIR})
   SET(INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/${${PROJECT_NAME}_INSTALL_INCLUDE_DIR})
 
@@ -129,6 +197,27 @@ FUNCTION(PACKAGE_WRITE_PACKAGE_CONFIG_FILE PACKAGE_NAME)
     RENAME ${PACKAGE_NAME}Config.cmake
   )
 
+  IF(${PROJECT_NAME}_ENABLE_EXPORT_MAKEFILES)
+    ######
+    # Create a Makefile.export.<package_name> for the install tree. This is the equivalent
+    # of the cmake version only slightly changed so that it can be directly imported into
+    # a Makefile.
+    ######
+
+    LIST_TO_STRING("${LIBRARY_DIRS}" ${CMAKE_LIBRARY_PATH_FLAG} MAKEFILE_LIBRARY_DIRS)
+    LIST_TO_STRING("${INCLUDE_DIRS}" "-I" MAKEFILE_INCLUDE_DIRS)
+
+    CONFIGURE_FILE(
+      ${PROJECT_SOURCE_DIR}/cmake/PackageConfig.export.in 
+      ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/Makefile.export.${PACKAGE_NAME}_install
+      )
+
+    INSTALL(
+      FILES ${CMAKE_CURRENT_BINARY_DIR}/CMakeFiles/Makefile.export.${PACKAGE_NAME}_install
+      DESTINATION "."
+      RENAME Makefile.export.${PACKAGE_NAME}
+    )
+  ENDIF()
 ENDFUNCTION()
 
 FUNCTION(PACKAGE_WRITE_TRILINOS_CONFIG_FILE)
@@ -210,6 +299,32 @@ FUNCTION(PACKAGE_WRITE_TRILINOS_CONFIG_FILE)
   FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/TrilinosConfig.cmake ${GLOB_LINE})
   FILE(APPEND ${CMAKE_CURRENT_BINARY_DIR}/TrilinosConfig.cmake ${FOREACH_BLOCK})
 
+  IF(${PROJECT_NAME}_ENABLE_EXPORT_MAKEFILES)
+    ######
+    # Create a Makefile.export.<project_name> for the build tree. This is the equivalent
+    # of the cmake version only slightly changed so that it can be directly imported into
+    # a Makefile.
+    ######
+
+    LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_LIBRARIES}" ${CMAKE_LINK_LIBRARY_FLAG} MAKEFILE_${PROJECT_NAME}_CONFIG_LIBRARIES)
+    LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_LIBRARY_DIRS}" ${CMAKE_LIBRARY_PATH_FLAG} MAKEFILE_${PROJECT_NAME}_CONFIG_LIBRARY_DIRS)
+    LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_INCLUDE_DIRS}" "-I" MAKEFILE_${PROJECT_NAME}_CONFIG_INCLUDE_DIRS)
+    LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_TPL_INCLUDE_DIRS}" "-I" MAKEFILE_${PROJECT_NAME}_CONFIG_TPL_INCLUDE_DIRS)
+    LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_TPL_LIBRARY_DIRS}" "-I" MAKEFILE_${PROJECT_NAME}_CONFIG_TPL_LIBRARY_DIRS)
+    #the TPL library names have to be treated differently
+    LIBRARY_LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_TPL_LIBRARIES}" ${CMAKE_LINK_LIBRARY_FLAG} MAKEFILE_${PROJECT_NAME}_CONFIG_TPL_LIBRARIES)
+
+    LIST_TO_STRING("${${TPL_MPI_LIBRARIES}}" "-l" "MAKEFILE_TPL_MPI_LIBRARIES")
+    LIST_TO_STRING("${${TPL_MPI_LIBRARY_DIRS}}" "-L" "MAKEFILE_TPL_MPI_LIBRARY_DIRS")
+    LIST_TO_STRING("${${TPL_MPI_INCLUDE_DIRS}}" "-I" "MAKEFILE_TPL_MPI_INCLUDE_DIRS")
+    
+    LIST_TO_STRING("${FULL_PACKAGE_SET}" "" MAKEFILE_FULL_PACKAGE_SET)
+    LIST_TO_STRING("${FULL_TPL_SET}" "" MAKEFILE_FULL_TPL_SET)
+
+    CONFIGURE_FILE( ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TrilinosConfig.export.in
+      ${CMAKE_CURRENT_BINARY_DIR}/Makefile.export.${PROJECT_NAME})
+  ENDIF()  
+
   #Config file for setting variables and finding include/library paths from the Install directory
   SET(${PROJECT_NAME}_CONFIG_INCLUDE_DIRS ${CMAKE_INSTALL_PREFIX}/${${PROJECT_NAME}_INSTALL_INCLUDE_DIR})
   SET(${PROJECT_NAME}_CONFIG_LIBRARY_DIRS ${CMAKE_INSTALL_PREFIX}/${${PROJECT_NAME}_INSTALL_LIB_DIR})
@@ -228,6 +343,25 @@ FUNCTION(PACKAGE_WRITE_TRILINOS_CONFIG_FILE)
     RENAME TrilinosConfig.cmake
   )
   
+  IF(${PROJECT_NAME}_ENABLE_EXPORT_MAKEFILES)
+    ######
+    # Create a Makefile.export.<project_name> for the install tree. This is the equivalent
+    # of the cmake version only slightly changed so that it can be directly imported into
+    # a Makefile.
+    ######
+
+    LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_LIBRARY_DIRS}" ${CMAKE_LIBRARY_PATH_FLAG} MAKEFILE_${PROJECT_NAME}_CONFIG_LIBRARY_DIRS)
+    LIST_TO_STRING("${${PROJECT_NAME}_CONFIG_INCLUDE_DIRS}" "-I" MAKEFILE_${PROJECT_NAME}_CONFIG_INCLUDE_DIRS)
+
+    CONFIGURE_FILE( ${CMAKE_CURRENT_SOURCE_DIR}/cmake/TrilinosConfig.export.in
+      ${CMAKE_CURRENT_BINARY_DIR}/Makefile.export.${PROJECT_NAME}_install )
+
+    INSTALL(
+      FILES ${CMAKE_CURRENT_BINARY_DIR}/Makefile.export.${PROJECT_NAME}_install
+      DESTINATION "."
+      RENAME Makefile.export.${PROJECT_NAME}
+    )
+  ENDIF()
   
   #
   # Configure the version file for Trilinos
