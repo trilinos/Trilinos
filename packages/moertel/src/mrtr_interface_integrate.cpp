615006037f7235a26f1de3f5e4e0a6afae593b3f
Glen Hansen <Glen.Hansen@inl.gov>
2010-06-17 14:53:33 -0600

    Changed contact email
    Changed contact person's email from Michael Gee to Glen Hansen

--- packages/moertel/src/mrtr_interface_integrate.cpp
+++ packages/moertel/src/mrtr_interface_integrate.cpp
@@ -22,7 +22,7 @@
 # License along with this library; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
-# Questions? Contact Michael Gee (mwgee@sandia.gov)
+# Questions? Contact Glen Hansen (Glen.Hansen@inl.gov)
 #
 # ************************************************************************
 #@HEADER
