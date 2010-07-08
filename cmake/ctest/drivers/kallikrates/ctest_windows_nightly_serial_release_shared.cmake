9fbbe1f6cfef116c6dfacb2be22fb1d5cbd37967
Brent Perschbacher <bmpersc@sandia.gov>
2010-07-08 15:59:09 -0600

    updates for Kallikrates testing
    Updated the kalli release testing to reference 10.4.
    
    There is a known issue with the shared build and how dlls are found which will
    need to be addressed. Basically the way we tell windows to find the dlls isn't
    general enough to handle bug fix version updates. I have a couple thoughts on
    how to fix this, but none of them tickle my fancy. In the mean time though what
    is in the driver batch file will work for now so it isn't something that is
    broken. It will need to be addressed soon though.

---
+++ cmake/ctest/drivers/kallikrates/ctest_windows_nightly_serial_release_shared.cmake
@@ -0,0 +1,28 @@
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.kallikrates.msvc.cmake")
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../../TrilinosVersion.cmake")
+
+#
+# Set the options specific to this build case
+#
+#SET(CTEST_DO_UPDATES FALSE)
+SET(COMM_TYPE SERIAL)
+SET(BUILD_TYPE RELEASE)
+SET(BUILD_DIR_NAME SERIAL_OPT_RELEASE_${Trilinos_VERSION}_SHARED)
+SET(Trilinos_TRACK ${Trilinos_TESTING_TRACK})
+SET(CTEST_TEST_TYPE EXPERIMENTAL)
+
+SET(Trilinos_BRANCH ${Trilinos_REPOSITORY_BRANCH})
+
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
+
+#The dll export macros have only been added for a few  packages so we can really only test those packages
+SET(Trilinos_PACKAGES "Teuchos;Epetra;Anasazi")
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DBUILD_SHARED_LIBS:BOOL=ON"
+  "-DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF"
+)
+
+TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
