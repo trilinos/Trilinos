fe1a9619ff3f18df9f47bc388bcd8daeded357f6
Eric T Bavier <etbavier@sandia.gov>
2010-07-19 12:33:39 -0500

    Remove test of solve with random system.  Instead, check solution of known system.

--- packages/amesos2/test/solvers/Superlu_UnitTests.cpp
+++ packages/amesos2/test/solvers/Superlu_UnitTests.cpp
@@ -222,7 +222,6 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL( Superlu, Solve, SCALAR, LO, GO )
     
   RCP<const Comm<int> > comm = getDefaultComm();
   //const size_t numprocs = comm->getSize();
-  const size_t rank     = comm->getRank();
 
   const size_t numVectors = 1;
 
@@ -284,198 +283,42 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL( Superlu, Solve, SCALAR, LO, GO )
 
   // ETB: Should not use randomness in tests.  It would be better to find a
   // few good sample matrices to test with
-  // 
-  // /* Now create a random system.  The matrix A and MultiVector X are
-  //  * randomized and multiplied to create MultiVector B.  We then solve the
-  //  * system and check that the answer is identical to X.
-  //  */
-  // const size_t numVectors_r = 5;
-
-  // // Create random X
-  // RCP<MV> X_r = rcp(new MV(A->getColMap(), numVectors));
-  // X_r->randomize();
-
-  // // Create B from A and X
-  // RCP<MV> B_r = rcp(new MV(A->getRowMap(),numVectors));
-  // A->multiply(*X_r, *B_r, Teuchos::NO_TRANS, as<SCALAR>(1), as<SCALAR>(0)); // B_r = A * X_r
-
-  // RCP<MV> xCheck_r = rcp(new MV(A->getColMap(), numVectors));
-
-  // // Construct Solver from Factory
-  // RCP<Amesos::SolverBase> solver_r
-  //   = Amesos::Factory<MAT,MV>::create("Superlu", A, xCheck_r, B_r);
-
-  // solver_r->symbolicFactorization().numericFactorization().solve();
-
-  // // Test equaility of original X and solution
-  // X_r->update(as<SCALAR>(-1),*xCheck_r,as<SCALAR>(1));
-  // X_r->norm2(norms);
-  // TEST_COMPARE_FLOATING_ARRAYS(norms,zeros,0.5);
+  /*
+   *  const size_t numVectors_r = 5;
+   *
+   *  // Create random X
+   *  RCP<MV> X_r = rcp(new MV(A->getColMap(), numVectors));
+   *  X_r->randomize();
+   *
+   *  // Create B from A and X
+   *  RCP<MV> B_r = rcp(new MV(A->getRowMap(),numVectors));
+   *  A->multiply(*X_r, *B_r, Teuchos::NO_TRANS, as<SCALAR>(1), as<SCALAR>(0)); // B_r = A * X_r
+   *
+   *  RCP<MV> xCheck_r = rcp(new MV(A->getColMap(), numVectors));
+   *
+   *  // Construct Solver from Factory
+   *  RCP<Amesos::SolverBase> solver_r
+   *    = Amesos::Factory<MAT,MV>::create("Superlu", A, xCheck_r, B_r);
+   *
+   *  solver_r->symbolicFactorization().numericFactorization().solve();
+   *
+   *  // Test equaility of original X and solution
+   *  X_r->update(as<SCALAR>(-1),*xCheck_r,as<SCALAR>(1));
+   *  X_r->norm2(norms);
+   *  TEST_COMPARE_FLOATING_ARRAYS(norms,zeros,0.5);
+   */
 }
 
-
-TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL( Superlu, Dimensions, SCALAR, LO, GO )
-{
-  // Test that the dimensions reported by the adapter match those as reported
-  // by the Tpetra::CrsMatrix
-  // Check equality of mapped method calls
-  typedef ScalarTraits<SCALAR> ST;
-  typedef CrsMatrix<SCALAR,LO,GO,Node> MAT;
-  const global_size_t INVALID = OrdinalTraits<global_size_t>::invalid();
-  RCP<const Comm<int> > comm = getDefaultComm();
-  //const size_t numprocs = comm->getSize();
-  const size_t rank     = comm->getRank();
-  // create a Map
-  const size_t numLocal = 10;
-  RCP<Map<LO,GO,Node> > map = rcp( new Map<LO,GO,Node>(INVALID,numLocal,0,comm) );
-  RCP<MAT> eye = rcp( new MAT(map,1) );
-  GO base = numLocal*rank;
-  for( size_t i = 0; i < numLocal; ++i ){
-    eye->insertGlobalValues(base+i,tuple<GO>(base+i),tuple<SCALAR>(ST::one()));
-  }
-  eye->fillComplete();
-
-  // Constructor from RCP
-  RCP<MatrixAdapter<MAT> > adapter  = rcp(new MatrixAdapter<MAT>(eye));
-  TEST_EQUALITY(eye->getGlobalNumEntries(), adapter->getGlobalNNZ());
-  TEST_EQUALITY(eye->getNodeNumEntries(), adapter->getLocalNNZ());
-  TEST_EQUALITY(eye->getGlobalNumRows(), adapter->getGlobalNumRows());
-  TEST_EQUALITY(eye->getNodeNumRows(), adapter->getLocalNumRows());
-  TEST_EQUALITY(eye->getNodeNumCols(), adapter->getLocalNumCols());
-  TEST_EQUALITY(eye->getGlobalNumCols(), adapter->getGlobalNumCols());
-  TEST_EQUALITY(eye->getGlobalMaxNumRowEntries(), adapter->getMaxNNZ());
-}
-
-TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL( Superlu, CRS, SCALAR, LO, GO )
-{
-  /* Test the getCrs() method of MatrixAdapter.  We check against a simple
-   * test matrix that we construct on the fly.
-   */
-  typedef ScalarTraits<SCALAR> ST;
-  typedef CrsMatrix<SCALAR,LO,GO,Node> MAT;
-  typedef MatrixAdapter<MAT> ADAPT;
-
-  RCP<const Comm<int> > comm = getDefaultComm();
-  //const size_t numprocs = comm->getSize();
-  //const size_t rank     = comm->getRank();
-  // create a Map for our matrix
-  global_size_t nrows = 6;
-  RCP<Map<LO,GO,Node> > map = rcp( new Map<LO,GO,Node>(nrows,0,comm) );
-  RCP<MAT> mat = rcp( new MAT(map,3) ); // max of three entries in a row
-
-  /* We will be using the following matrix for this test:
-   * 
-   * [ [ 7,  0,  -3, 0,  -1, 0 ]
-   *   [ 2,  8,  0,  0,  0,  0 ]
-   *   [ 0,  0,  1,  0,  0,  0 ]
-   *   [ -3, 0,  0,  5,  0,  0 ]
-   *   [ 0,  -1, 0,  0,  4,  0 ]
-   *   [ 0,  0,  0,  -2, 0,  6 ] ]
-   */
-  // Construct matrix
-  mat->insertGlobalValues(0,tuple<GO>(0,2,4),tuple<SCALAR>(7,-3,-1));
-  mat->insertGlobalValues(1,tuple<GO>(0,1),tuple<SCALAR>(2,8));
-  mat->insertGlobalValues(2,tuple<GO>(2),tuple<SCALAR>(1));
-  mat->insertGlobalValues(3,tuple<GO>(0,3),tuple<SCALAR>(-3,5));
-  mat->insertGlobalValues(4,tuple<GO>(1,4),tuple<SCALAR>(-1,4));
-  mat->insertGlobalValues(5,tuple<GO>(3,5),tuple<SCALAR>(-2,6));
-  mat->fillComplete();
-
-  // Print for sanity sake
-  // RCP<FancyOStream> os = getDefaultOStream();
-  // mat->describe(*os,Teuchos::VERB_EXTREME);
-
-  RCP<ADAPT> adapter = rcp(new ADAPT(mat));
-
-  Array<SCALAR> nzvals_test(tuple<SCALAR>(7,-3,-1,2,8,1,-3,5,-1,4,-2,6));
-  Array<GO> colind_test(tuple<GO>(0,2,4,0,1,2,0,3,1,4,3,5));
-  Array<global_size_t> rowptr_test(tuple<global_size_t>(0,3,5,6,8,10,12));
-
-  Array<SCALAR> nzvals(adapter->getGlobalNNZ());
-  Array<GO> colind(adapter->getGlobalNNZ());
-  Array<global_size_t> rowptr(adapter->getGlobalNumRows() + 1);
-  size_t nnz;
-
-  adapter->getCrs(nzvals,colind,rowptr,nnz);
-
-  TEST_COMPARE_ARRAYS(nzvals,nzvals_test);
-  TEST_COMPARE_ARRAYS(colind,colind_test);
-  TEST_COMPARE_ARRAYS(rowptr,rowptr_test);
-  TEST_EQUALITY(nnz,12);
-}
-
-TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL( Superlu, CCS, SCALAR, LO, GO )
-{
-  /* Test the getCcs() method of MatrixAdapter.  Again, check against a known
-   * matrix
-   */
-  typedef ScalarTraits<SCALAR> ST;
-  typedef CrsMatrix<SCALAR,LO,GO,Node> MAT;
-  typedef MatrixAdapter<MAT> ADAPT;
-
-  RCP<const Comm<int> > comm = getDefaultComm();
-  //const size_t numprocs = comm->getSize();
-  //const size_t rank     = comm->getRank();
-  // create a Map for our matrix
-  global_size_t nrows = 6;
-  RCP<Map<LO,GO,Node> > map = rcp( new Map<LO,GO,Node>(nrows,0,comm) );
-  RCP<MAT> mat = rcp( new MAT(map,3) ); // max of three entries in a row
-
-  /* We will be using the following matrix for this test:
-   * 
-   * [ [ 7,  0,  -3, 0,  -1, 0 ]
-   *   [ 2,  8,  0,  0,  0,  0 ]
-   *   [ 0,  0,  1,  0,  0,  0 ]
-   *   [ -3, 0,  0,  5,  0,  0 ]
-   *   [ 0,  -1, 0,  0,  4,  0 ]
-   *   [ 0,  0,  0,  -2, 0,  6 ] ]
-   */
-  // Construct matrix
-  mat->insertGlobalValues(0,tuple<GO>(0,2,4),tuple<SCALAR>(7,-3,-1));
-  mat->insertGlobalValues(1,tuple<GO>(0,1),tuple<SCALAR>(2,8));
-  mat->insertGlobalValues(2,tuple<GO>(2),tuple<SCALAR>(1));
-  mat->insertGlobalValues(3,tuple<GO>(0,3),tuple<SCALAR>(-3,5));
-  mat->insertGlobalValues(4,tuple<GO>(1,4),tuple<SCALAR>(-1,4));
-  mat->insertGlobalValues(5,tuple<GO>(3,5),tuple<SCALAR>(-2,6));
-  mat->fillComplete();
-
-  // Print for sanity sake
-  // RCP<FancyOStream> os = getDefaultOStream();
-  // mat->describe(*os,Teuchos::VERB_EXTREME);
-
-  RCP<ADAPT> adapter = rcp(new ADAPT(mat));
-
-  Array<SCALAR> nzvals_test(tuple<SCALAR>(7,2,-3,8,-1,-3,1,5,-2,-1,4,6));
-  Array<GO> rowind_test(tuple<GO>(0,1,3,1,4,0,2,3,5,0,4,5));
-  Array<global_size_t> colptr_test(tuple<global_size_t>(0,3,5,7,9,11,12));
-
-  Array<SCALAR> nzvals(adapter->getGlobalNNZ());
-  Array<GO> rowind(adapter->getGlobalNNZ());
-  Array<global_size_t> colptr(adapter->getGlobalNumRows() + 1);
-  size_t nnz;
-
-  adapter->getCcs(nzvals,rowind,colptr,nnz);
-
-  TEST_COMPARE_ARRAYS(nzvals,nzvals_test);
-  TEST_COMPARE_ARRAYS(rowind,rowind_test);
-  TEST_COMPARE_ARRAYS(colptr,colptr_test);
-  TEST_EQUALITY(nnz,12);
-}
-
-/* Also need to test the updateValues[Crs,Ccs]() methods, to make sure that
- * those changes are persistent in the underlying matrices (once implemented).
- */
 
 /*
  * Instantiations
  */
-
 #ifdef HAVE_TEUCHOS_COMPLEX
 #  define UNIT_TEST_GROUP_ORDINAL_COMPLEX_FLOAT(LO, GO)\
-  typedef std::complex<float> ComplexFloat; \
+  typedef std::complex<float>  ComplexFloat;\
   UNIT_TEST_GROUP_ORDINAL_SCALAR(LO, GO, ComplexFloat)
 #  define UNIT_TEST_GROUP_ORDINAL_COMPLEX_DOUBLE(LO, GO)\
-  typedef std::complex<double> ComplexDouble; \
+  typedef std::complex<double> ComplexDouble;\
   UNIT_TEST_GROUP_ORDINAL_SCALAR(LO, GO, ComplexDouble) 
 #else
 #  define UNIT_TEST_GROUP_ORDINAL_COMPLEX_FLOAT(LO, GO)
