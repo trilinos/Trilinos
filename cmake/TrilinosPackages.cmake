6a36f9f448bd9a941de4722866f600c027e384ea
Russell Hooper <rhoope@sandia.gov>
2010-07-20 10:19:29 -0600

    Tweak

--- cmake/TrilinosPackages.cmake
+++ cmake/TrilinosPackages.cmake
@@ -52,7 +52,7 @@ INCLUDE(PackageListHelpers)
 SET( Trilinos_PACKAGES_AND_DIRS_AND_CLASSIFICATIONS
   TrilinosFramework     ../cmake                       PS # Only tests, no libraries/capabilities!
   Teuchos               teuchos                        PS
-  Trios                 Trios                          EX # Depends on netcdf, optionally hdf5, xdmf
+  Trios                 Trios                          SS # Depends on netcdf, optionally hdf5, xdmf
   ThreadPool            ThreadPool                     PS # Depends on ptheads system library
   Sacado                sacado                         PS
   RTOp                  rtop                           PS
