5a14136900efecb83805eaac9b58bd61e226fac7
Mark Hoemmen <mhoemme@sandia.gov>
2010-08-31 17:01:09 -0600

    Anasazi: TSQR: Added documentation to TSQR::StatTimeMonitor and TSQR::TimeStats

--- packages/anasazi/src/TSQR/Tsqr_StatTimeMonitor.hpp
+++ packages/anasazi/src/TSQR/Tsqr_StatTimeMonitor.hpp
@@ -9,9 +9,29 @@
 
 namespace TSQR {
 
+  /// \class StatTimeMonitor
+  /// \brief Like Teuchos::TimeMonitor, but collects running stats
+  ///
+  /// Like Teuchos::TimeMonitor, this class uses the RAII idiom to
+  /// time a scope.  However, it also maintains running statistics,
+  /// via a reference to a TimeStats object.
+  ///
+  /// \note Implementers: You may safely add new statistics to
+  ///   TimeStats without needing to change this class.
+  ///
   class StatTimeMonitor {
   public:
+    /// \brief Constructor
+    ///
+    /// \param timer [in/out] Reference to the raw timer object
+    ///
+    /// \param stats [in/out] Running statistics, to be updated when
+    ///   this StatTimeMonitor falls out of scope
     StatTimeMonitor (Teuchos::Time& timer, TimeStats& stats);
+
+    /// \brief Destructor
+    ///
+    /// Updates running statistics via the TimeStats object.
     ~StatTimeMonitor ();
 
   private:
@@ -20,7 +40,7 @@ namespace TSQR {
     StatTimeMonitor& operator= (const StatTimeMonitor&);
 
     Teuchos::Time& timer_;
-    TimeStats stats_;
+    TimeStats& stats_;
   };
 
 } // namespace TSQR
--- packages/anasazi/src/TSQR/Tsqr_TimeStats.hpp
+++ packages/anasazi/src/TSQR/Tsqr_TimeStats.hpp
@@ -9,19 +9,42 @@
 namespace TSQR {
 
   /// \class TimeStats
-  /// \brief Collect running statistics on a particular timing
+  /// \brief Collect running statistics
+  ///
+  /// TimeStats collects running statistics on a particular timing,
+  /// which is collected count() times.  When you get a new timing,
+  /// call update().
   class TimeStats {
   public:
     TimeStats();
     ~TimeStats() {}
 
+    /// Reset the statistics
+    ///
     void init ();
+
+    /// Add a new data point and update the running statistics
+    ///
     void update (const double curTime);
 
+    /// Minimum value seen thus far (+Inf if no data has been
+    /// collected)
     double min() const { return min_; }
+
+    /// Maximum value seen thus far (-Inf if no data has been
+    /// collected)
     double max() const { return max_; }
+
+    /// Arithmetic mean thus far (0 if no data has been collected)
+    ///
     double mean() const { return mean_; }
+
+    /// Total thus far (0 if no data has been collected)
+    ///
     double total() const { return total_; }
+
+    /// Count of data points collected thus far
+    ///
     size_t count() const { return count_; }
 
   private:
