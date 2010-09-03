eb1465f461a28c287565ab4911e9053dbe51114e
James M. Willenbring <jmwille@sandia.gov>
2010-09-03 13:42:58 -0600

    Updated copyright headers to 2010 instead of 2009.

--- packages/amesos2/src/SuiteSparse/KLU2/Source/klu2_free_numeric.hpp
+++ packages/amesos2/src/SuiteSparse/KLU2/Source/klu2_free_numeric.hpp
@@ -4,8 +4,8 @@
 // @HEADER
 // ***********************************************************************
 //
-//                   KLU2: A Direct Linear Solver package.
-//                   Copyright (2009) Sandia Corporation
+//                   KLU2: A Direct Linear Solver package
+//                    Copyright 2010 Sandia Corporation
 //
 // Under terms of Contract DE-AC04-94AL85000, with Sandia Corporation, the 
 // U.S. Government retains certain rights in this software.
