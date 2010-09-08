aad4d7df76a394f931e3d617b7c2f604457c43ed
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-09-08 14:39:43 -0600

    Standardizing the copyright headers for Teuchos.
    I did this mostly as an example of how to replace all of the
    copyright headers when moving to BSD.
    
    I had to fix a couple of headers manually since their headers were not in the
    standard form (Teuchos_BLAS_wrappers.hpp and test/Handle/cxx_main.cpp).
    Hopefully this will not be a problem for other files in other packages.
    
    Build/Test Cases Summary
    Enabled Packages: Teuchos
    0) MPI_DEBUG => passed: passed=57,notpassed=0 (1.70 min)
    1) SERIAL_RELEASE => passed: passed=54,notpassed=0 (1.15 min)
    Other local commits for this build/test group: 14ea21e

--- packages/teuchos/test/ParameterList/ParameterList_UnitTest_Parallel.cpp
+++ packages/teuchos/test/ParameterList/ParameterList_UnitTest_Parallel.cpp
@@ -1,8 +1,8 @@
-//@HEADER
+// @HEADER
 // ***********************************************************************
 //
-//                           Teuchos Package
-//                 Copyright (2006) Sandia Corporation
+//                    Teuchos: Common Tools Package
+//                 Copyright (2004) Sandia Corporation
 //
 // Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
 // license for use of this work by or on behalf of the U.S. Government.
@@ -21,10 +21,10 @@
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 // USA
-// Questions? Contact Todd S. Coffey (tscoffe@sandia.gov)
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
 //
 // ***********************************************************************
-//@HEADER
+// @HEADER
 
 #include "Teuchos_ParameterList.hpp"
 #include "Teuchos_XMLParameterListHelpers.hpp"
