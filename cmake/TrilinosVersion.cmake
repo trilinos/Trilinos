f5ff376d0092ff054460983ab3394bfcaec01b46
Brent Perschbacher <bmpersc@sandia.gov>
2010-09-20 12:25:46 -0600

    Initial commit for 10.6
    Updated version numbers and set to development mode.

--- cmake/TrilinosVersion.cmake
+++ cmake/TrilinosVersion.cmake
@@ -1,11 +1,11 @@
-SET(Trilinos_VERSION "10.5")
+SET(Trilinos_VERSION "10.6")
 SET(Trilinos_MAJOR_VERSION "10")
-SET(Trilinos_MAJOR_MINOR_VERSION "100500")
-SET(Trilinos_VERSION_STRING "10.5 (Dev)")
+SET(Trilinos_MAJOR_MINOR_VERSION "100600")
+SET(Trilinos_VERSION_STRING "10.6")
 
 #this is for our testing scripts and should not be used elsewhere
-SET(Trilinos_REPOSITORY_BRANCH "master" CACHE INTERNAL "")
-SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
+SET(Trilinos_REPOSITORY_BRANCH "trilinos-release-10-6-branch" CACHE INTERNAL "")
+SET(Trilinos_TESTING_TRACK "Nightly Release 10.6" CACHE INTERNAL "")
 
 
 SET(TRILINOS_MAJOR_VERSION TRUE)
