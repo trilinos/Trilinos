668f480cc6a5d5ff84c3d28064a45c37f61cf344
Eric C. Cyr <eccyr@sandia.gov>
2010-09-28 16:31:38 -0600

    Modified LSCStrategy and added new LSCSIMPLEC strategy
    LSCStrategy now differentiates between external and internal
    stabilization for the BQFQBt operator.
    
    Also added a preconditioner with Schur complement
       -(BHBt-C)^{-1} (BHFHBt-C)(BHBt-C)^{-1}
    where H=AbsRowSum(F)^{-1}.

--- packages/teko/src/NS/Teko_LSCSIMPLECStrategy.hpp
+++ packages/teko/src/NS/Teko_LSCSIMPLECStrategy.hpp
@@ -44,8 +44,8 @@
 
 */
 
-#ifndef __Teko_PresLaplaceLSCStrategy_hpp__
-#define __Teko_PresLaplaceLSCStrategy_hpp__
+#ifndef __Teko_LSCSIMPLECStrategy_hpp__
+#define __Teko_LSCSIMPLECStrategy_hpp__
 
 #include "Teko_LSCStrategy.hpp"
 
@@ -63,17 +63,14 @@ class LSCPrecondState; // forward declration
   * in, if it is the diagonal is extracted and that is used to
   * form the inverse approximation.
   */
-class PresLaplaceLSCStrategy : public LSCStrategy {
+class LSCSIMPLECStrategy : public LSCStrategy {
 public:
    //! \name Constructors
    //@{
-   PresLaplaceLSCStrategy();
-   PresLaplaceLSCStrategy(const Teuchos::RCP<InverseFactory> & factory);
-   PresLaplaceLSCStrategy(const Teuchos::RCP<InverseFactory> & invFactF,
-                  const Teuchos::RCP<InverseFactory> & invFactS);
+   LSCSIMPLECStrategy();
    //@}
 
-   virtual ~PresLaplaceLSCStrategy() {}
+   virtual ~LSCSIMPLECStrategy() {}
 
    //! Functions inherited from LSCStrategy
    //@{
@@ -118,6 +115,7 @@ public:
    virtual LinearOp getInvF(const BlockedLinearOp & A,BlockPreconditionerState & state) const;
 
    /** Get the inverse for stabilizing the whole schur complement approximation.
+     * Returns \f$\alpha D^{-1}\f$.
      *
      * \param[in] A The linear operator to be preconditioned by LSC.
      * \param[in] state State object for storying reusable information about
@@ -125,11 +123,10 @@ public:
      *
      * \returns The operator to stabilize the whole Schur complement.
      */
-   // virtual LinearOp getInvAlphaD(const BlockedLinearOp & A,BlockPreconditionerState & state) const;
-   virtual LinearOp getOuterStabilization(const BlockedLinearOp & A,BlockPreconditionerState & state) const;
-
-   virtual LinearOp getInnerStabilization(const BlockedLinearOp & A,BlockPreconditionerState & state) const
+   virtual LinearOp getOuterStabilization(const BlockedLinearOp & A,BlockPreconditionerState & state) const
    { return Teuchos::null; }
+
+   virtual LinearOp getInnerStabilization(const BlockedLinearOp & A,BlockPreconditionerState & state) const;
 
    /** Get the inverse mass matrix.
      *
@@ -159,24 +156,9 @@ public:
      */
    virtual bool useFullLDU() const { return useFullLDU_; }
 
-   /** Tell strategy that this operator is supposed to be symmetric.
-     * Behavior of LSC is slightly different for non-symmetric case.
-     *
-     * \param[in] isSymmetric Is this operator symmetric?
-     */
-   virtual void setSymmetric(bool isSymmetric) 
-   { isSymmetric_ = isSymmetric; }
-
    //! Initialize from a parameter list
    virtual void initializeFromParameterList(const Teuchos::ParameterList & pl,
                                             const InverseLibrary & invLib); 
-
-   //! For assiting in construction of the preconditioner
-   virtual Teuchos::RCP<Teuchos::ParameterList> getRequestedParameters() const;
-
-   //! For assiting in construction of the preconditioner
-   virtual bool updateRequestedParameters(const Teuchos::ParameterList & pl);
-   //@}
 
    //! Initialize the state object using this blocked linear operator
    virtual void initializeState(const BlockedLinearOp & A,LSCPrecondState * state) const;
@@ -188,37 +170,22 @@ public:
      */
    void computeInverses(const BlockedLinearOp & A,LSCPrecondState * state) const;
 
-   //! Set the number of power series iterations to use when finding the spectral radius
-   virtual void setEigSolveParam(int sz) { eigSolveParam_ = sz; }
-
-   //! Return the number of power series iterations to use when finding the spectral radius
-   virtual int getEigSolveParam() { return eigSolveParam_; }
-
    //! Set to true to use the Full LDU decomposition, false otherwise
    virtual void setUseFullLDU(bool val) { useFullLDU_ = val; }
 
+   virtual void setSymmetric(bool isSymmetric) { }
+
 protected:
    // how to invert the matrices
-   Teuchos::RCP<InverseFactory> invFactoryV_;
-   Teuchos::RCP<InverseFactory> invFactoryP_;
+   Teuchos::RCP<InverseFactory> invFactoryF_;
+   Teuchos::RCP<InverseFactory> invFactoryS_;
 
    // flags for handling various options
    bool useFullLDU_;
-   bool isSymmetric_;
-   int eigSolveParam_;
-
-   // scaling operator parameters
-   bool useMass_;
    DiagonalType scaleType_; 
 
 private:
-   PresLaplaceLSCStrategy(const PresLaplaceLSCStrategy &);
-
-public:
-   // some static functions for determining strings
-
-   static std::string getPressureLaplaceString() { return "Pressure Laplace Operator"; }
-   static std::string getVelocityMassString() { return "Velocity Mass Operator"; }
+   LSCSIMPLECStrategy(const LSCSIMPLECStrategy &);
 };
 
 } // end namespace NS
