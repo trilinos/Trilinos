7b953668ecc839d0951b3cc0feb8da3471daa475
Jeremie Gaidamour <jngaida@sandia.gov>
2010-10-19 12:17:16 -0600

    Cthulhu: new convenient headers
    - to define default types for user code or examples/
    - to get ride of template parameters when we can (in examples/ or src/)
    - to switch easily between tpetra and epetra at compile time (for user code or examples/)

---
+++ packages/muelu/cthulhu/src/Def/Cthulhu.hpp
@@ -0,0 +1,58 @@
+#ifndef CTHULHU_MYTYPES_HPP
+#define CTHULHU_MYTYPES_HPP
+
+// The purpose of this file is to allow a user to write program without worrying about templates parameters.
+// For example, you can replace:
+//
+// typedef int LocalOrdinal;
+// typedef int GlobalOrdinal;
+//
+// GlobalOrdinal nx = 4;
+// GlobalOrdinal ny = 4;
+// RCP<const Map<LocalOrdinal,GlobalOrdinal> > map = rcp( new TpetraMap<LocalOrdinal,GlobalOrdinal>(nx*ny, 0, comm) );
+//
+// by:
+//
+// #include <Cthulhu.hpp>
+//
+// GO nx = 4;
+// GO ny = 4;
+// RCP<const Map> map = rcp( new MyMap(nx*ny, 0, comm) ); // MyMap = TpetraMap or EpetraMap according to the macro USE_EPETRA and USE_TPETRA.
+//
+// The definition of template types can still be modified if needed.
+
+//
+
+// Cthulhu_DefaultTypes.hpp defines the types: ScalarType, LocalOrdinal, GlobalOrdinal and Node.
+// You can use your own definition of these types by defining them before including Cthulhu.hpp. You just have to define also the macro CTHULHU_DEFAULTTYPES_HPP.
+#include "Cthulhu_DefaultTypes.hpp"
+
+// Cthulhu_UseShortNames.hpp get ride of template type.
+// For example, instead of using the type 'Map<LocalOrdinal,GlobalOrdinal,Node>', you can simply use the short type name 'Map'.
+#include "Cthulhu_UseShortNames.hpp"
+
+// You can optionally define which implementation of the linar algebra layer you want to use (ie: Epetra or Tpetra) by defining the macro USE_EPETRA or USE_TPETRA.
+// Instead of using directly TpetraMap or EpetraMap when allocating a Cthulhu::Map, you can then use the type 'MyMap'. This type is define according to USE_EPETRA and USE_TPETRA.
+// This allows you to switch from one implementation to another at compile time. Another way is to use a Cthulhu::MapFactory (selection at runtime).
+
+#if defined (USE_TPETRA) && !defined(USE_EPETRA)
+
+#include <Cthulhu_TpetraMap.hpp>
+#include <Cthulhu_TpetraCrsMatrix.hpp>
+
+typedef Cthulhu::TpetraMap<LocalOrdinal, GlobalOrdinal, Node> MyMap;
+typedef Cthulhu::TpetraCrsMatrix<ScalarType, LocalOrdinal, GlobalOrdinal, Node> MyCrsMatrix;
+typedef Cthulhu::TpetraMultiVector<ScalarType, LocalOrdinal, GlobalOrdinal, Node> MyMultiVector;
+
+#elif defined(USE_EPETRA) && !defined(USE_TPETRA)
+
+#include <Cthulhu_EpetraMap.hpp>
+#include <Cthulhu_EpetraCrsMatrix.hpp>
+
+typedef Cthulhu::EpetraMap<LocalOrdinal, GlobalOrdinal, Node> MyMap;
+typedef Cthulhu::EpetraCrsMatrix<ScalarType, LocalOrdinal, GlobalOrdinal, Node> MyCrsMatrix;
+typedef Cthulhu::EpetraMultiVector<ScalarType, LocalOrdinal, GlobalOrdinal, Node> MyMultiVector;
+
+#endif // USE_E/TPETRA
+
+#endif // CTHULHU_MYTYPES_HPP
---
+++ packages/muelu/cthulhu/src/Def/Cthulhu_DefaultTypes.hpp
@@ -0,0 +1,20 @@
+#ifndef CTHULHU_DEFAULTTYPES_HPP
+#define CTHULHU_DEFAULTTYPES_HPP
+
+#include <Kokkos_DefaultNode.hpp> // Note: we should not need this header for Epetra
+#include <Kokkos_DefaultKernels.hpp>
+
+// Define default data types
+typedef double ScalarType;
+typedef int    LocalOrdinal;
+typedef int    GlobalOrdinal;
+typedef Kokkos::DefaultNode::DefaultNodeType Node;
+typedef Kokkos::DefaultKernels<ScalarType,LocalOrdinal,Node>::SparseOps LocalMatOps;
+
+// Define convenient shortcut for data types
+typedef ScalarType    SC;
+typedef LocalOrdinal  LO;
+typedef GlobalOrdinal GO;
+typedef Node          NO;
+
+#endif
