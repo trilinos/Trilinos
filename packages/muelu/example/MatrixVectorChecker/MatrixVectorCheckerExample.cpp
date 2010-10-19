b0be7adfcac933d69a811fd7b429323ca665d97d
Jeremie Gaidamour <jngaida@sandia.gov>
2010-10-19 10:09:51 -0600

    example MatrixVectorChecker
    - I translated for Cthulhu matrices (but is working for Cthulhu::TpetraCrsMatrix matrices only for the moment)
    - I temporarily removed tests using matrix-matrix multiply (not available for Tpetra matrices)
    - The driver now usew muelu gallery instead of Galeri
    - The driver is using 'typename' to be able to switch easily from Cthulhu::EpetraCrsMatrix to Cthulhu::TpetraCrsMatrix matrices.
    - I have to create a factory for MatrixVectorChecker.hpp to be able to create vectors of the same type as the checked CrsMatrix.

--- packages/muelu/example/MatrixVectorChecker/MatrixVectorCheckerExample.cpp
+++ packages/muelu/example/MatrixVectorChecker/MatrixVectorCheckerExample.cpp
@@ -1,49 +1,75 @@
-#ifdef HAVE_MPI
-#include <mpi.h>
-#include <Epetra_MpiComm.h>
-#else
-#include <Epetra_SerialComm.h>
-#endif
-
-#include <Epetra_Map.h>
-#include <Epetra_CrsMatrix.h>
-
-#include <Teuchos_RCP.hpp>
 #include <Teuchos_ParameterList.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_GlobalMPISession.hpp>
+#include <Teuchos_DefaultComm.hpp>
 
-#include <Galeri_Maps.h>
-#include <Galeri_CrsMatrices.h>
+// Using Galeri:
+// #include <Galeri_Maps.h>
+// #include <Galeri_CrsMatrices.h>
+
+// Using MueLu gallery:
+#include <Cthulhu_Map.hpp>
+#include <Cthulhu_CrsMatrix.hpp>
+#include "MueLu_MatrixFactory.hpp"
+#include "MueLu_MatrixTypes.hpp"
 
 #include "MatrixVectorChecker.hpp"
 
-using namespace Teuchos;
+// Define template types
+typedef double ScalarType;
+typedef int    LocalOrdinal;
+typedef int    GlobalOrdinal;
+typedef Kokkos::DefaultNode::DefaultNodeType Node;
+
+// Convenient shortcut for template types
+typedef ScalarType    SC;
+typedef LocalOrdinal  LO;
+typedef GlobalOrdinal GO;
+typedef Node          NO;
+
+// Get ride of template parameters for some classes
+typedef Cthulhu::Map<LocalOrdinal, GlobalOrdinal, Node> Map;
+typedef Cthulhu::CrsMatrix<ScalarType, LocalOrdinal, GlobalOrdinal, Node> CrsMatrix;
+typedef Cthulhu::MultiVector<ScalarType, LocalOrdinal, GlobalOrdinal, Node> MultiVector;
+
+// Use Tpetra
+#include <Cthulhu_TpetraMap.hpp>
+#include <Cthulhu_TpetraCrsMatrix.hpp>
+typedef Cthulhu::TpetraMap<LocalOrdinal, GlobalOrdinal, Node> MyMap;
+typedef Cthulhu::TpetraCrsMatrix<ScalarType, LocalOrdinal, GlobalOrdinal, Node> MyCrsMatrix;
+typedef Cthulhu::TpetraMultiVector<ScalarType, LocalOrdinal, GlobalOrdinal, Node> MyMultiVector;
 
 int main(int argc, char *argv[])
 {
   
-#ifdef HAVE_MPI
-  MPI_Init(&argc,&argv);
-  Epetra_MpiComm Comm(MPI_COMM_WORLD);
-#else
-  Epetra_SerialComm Comm;
-#endif
+  Teuchos::GlobalMPISession mpiSession(&argc,&argv);
+  Teuchos::RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
 
-  Teuchos::ParameterList galeriList;
-  galeriList.set("nx", 10 * Comm.NumProc());
-  galeriList.set("ny", 10);
+  Teuchos::ParameterList paramList;
+  paramList.set("nx", 10 * comm->getSize());
+  paramList.set("ny", 10);
+  
+  paramList.set("mx", comm->getSize());
+  paramList.set("my", 1);
 
-  galeriList.set("mx", Comm.NumProc());
-  galeriList.set("my", 1);
+  // Using Galeri:
+  //
+  // RCP<Tpetra_Map> map = rcp( Galeri::CreateMap("Cartesian2D", comm, paramList) );
+  // RCP<Tpetra_CrsMatrix> matrix = rcp( Galeri::CreateCrsMatrix("Laplace2D", map.get(), paramList) );
 
-  RCP<Epetra_Map> map = rcp( Galeri::CreateMap("Cartesian2D", Comm, galeriList) );
-  RCP<Epetra_CrsMatrix> matrix = rcp( Galeri::CreateCrsMatrix("Laplace2D", map.get(), galeriList) );
+  // Using MueLu gallery:
+  GO nx = paramList.get<GO>("nx");
+  GO ny = paramList.get<GO>("ny");
 
-  MatrixVectorChecker(matrix);
+  RCP<const Map> map = rcp( new MyMap(nx*ny, 0, comm) );
+  RCP<const CrsMatrix> matrix = CreateCrsMatrix<SC,LO,GO,NO>("Laplace2D", map, paramList);
 
-#ifdef HAVE_MPI
-  MPI_Finalize() ;
-#endif
+  MatrixVectorChecker<SC,LO,GO,NO>(matrix);
 
   return(EXIT_SUCCESS);
 
 }
+
+// JG TODO:
+// - add a method CreateMap for cthulhu/gallery (as Galeri)
+// - wrap galeri matrix for the new MatrixVectorChecker
