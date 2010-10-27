9e042d3bcba8288b351b6ca5a12c69f6b5ddbc07
Jeremie Gaidamour <jngaida@sandia.gov>
2010-10-27 14:39:36 -0600

    Doc: added a doxygen documentation for Cthulhu.

---
+++ packages/muelu/cthulhu/doc/Cthulhu_DoxygenDocumentation.hpp
@@ -0,0 +1,119 @@
+// @HEADER
+// @HEADER
+
+#ifndef CTHULHU_DOXYGEN_DOCUMENTATION_HPP
+#define CTHULHU_DOXYGEN_DOCUMENTATION_HPP
+
+/*!
+
+\mainpage
+
+\section main_index Index
+
+- \ref overview
+- \ref news
+- \ref todo
+- \ref user_guide
+- \ref faq
+- \ref bugs
+- \ref history
+- \ref authors
+- \ref copyright
+- \ref questions
+
+\section overview Overview
+
+Cthulhu
+
+\section news News
+
+<ul>
+<li> 2010.09.08: We are now building as a Trilinos package!
+</ul>
+
+\section bugs Reporting Bugs and Making Enhancement Requests
+
+  To reports bugs or make enhancement requests, visit the <A HREF="http://software.sandia.gov/bugzilla/">Trilinos Bugzilla (Bug Tracking) Database</A>, and use the following instructions.
+      <UL>
+      <LI>Click on "Enter a new bug report"
+      <LI>Choose "Cthulhu"
+      <LI>Either login or create a new account
+      <LI>Submit your bug report
+      </UL>
+
+\section history History
+
+
+
+\section authors Authors and Contributors
+
+  - Jeremie Gaidamour, SNL 01416
+  - Axel Gerstenberger, SNL 01416
+  - Jonathan Hu, SNL 01416
+  - Chris Siefert, SNL 01431
+  - Ray Tuminaro, SNL 01416
+
+\section copyright Copyright
+
+\verbinclude copyright.txt
+
+\section questions For All Questions and Comments...
+  
+   Please contact Jonathan Hu (jhu@sandia.gov).
+
+*/
+
+/* ************************************************************************ */
+/* ************************************************************************ */
+
+/*! \page user_guide Users Guide
+
+\section user_guide_index Index
+
+- \ref user_guide_getting_started
+- \ref questions
+
+\section user_guide_getting_started Getting Started
+
+*/
+
+/* ************************************************************************ */
+/* ************************************************************************ */
+
+/*! \page todo ToDo List
+
+Short Term:
+<ul>
+<li> ???
+</ul>
+
+Long Term:
+<ul>
+<li> ???
+<li>
+</ul>
+
+*/
+
+/* ************************************************************************ */
+/* ************************************************************************ */
+
+/*! \page faq Frequently Asked Questions
+
+\section Questions
+\ref faq1
+
+\ref faq2
+
+\section faq1 1. Why name the code Cthulhu?  
+Why not?
+
+\section faq2 2. Put your question here?
+Answer your question here.
+
+*/
+
+/* ************************************************************************ */
+/* ************************************************************************ */
+
+#endif //ifndef CTHULHU_DOXYGEN_DOCUMENTATION_HPP
