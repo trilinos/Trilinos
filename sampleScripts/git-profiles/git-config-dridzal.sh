c5e5ffece01e7fcc78d5a0b29b13df940e4584ab
Denis Ridzal <dridzal@sandia.gov>
2010-11-17 14:14:37 -0700

    Config for dridzal

---
+++ sampleScripts/git-profiles/git-config-dridzal.sh
@@ -0,0 +1,9 @@
+# Set your username
+eg config --global user.name "Denis Ridzal"
+# Set your email address
+eg config --global user.email dridzal@sandia.gov
+# Use colorized output when it makes sense
+eg config --global color.ui true
+# Set up some shortcut commands
+_SCRIPT_DIR=`echo $0 | sed "s/\(.*\)\/.*\.sh/\1/g"`
+$_SCRIPT_DIR/../../commonTools/git/git-config-alias.sh
