2f1157d903f0e0478a70c38a81349b23a8570f6c
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-11-24 10:56:10 -0700

    Changing pretty log alias from using (%ar) to (%cr).
    The old format was showing the dates as:
    
       [%ad] ($ar)
    
    which was showing the same author date in two ways as [absolute author date]
    (relative author date).  However, I decided that it would be useful to have
    the relative date instead show the committer date which will be different if
    the commit message is amended in anyway (e.g. through rebasing).
    
    The new format for dates are:
    
       [%ad] ($cr)
    
    I think that showing the reative committer date instead of the relative author
    date gives more information in the short log.
    
    I think this change.

--- commonTools/git/git-config-alias.sh
+++ commonTools/git/git-config-alias.sh
@@ -1,16 +1,26 @@
 # Set up some shortcut commands
 
+# Note that the dates are shown in the format:
+#
+#   [absolute author date] (relative committer date)
+#
+# The author date is when the commit was first created by the author.  The
+# committer date is the date of the last time this commit was changed in
+# anyway (e.g. in a rebase or an amend).  These dates are often the same but
+# not if a rebase occurs.  Anyway, I think showing both of these dates is
+# helpful.
+
 # Shorter versions of log
-eg config --global alias.log-short "log --pretty=format:'%Cgreen%h%Creset \"%s\"%nAuthor: %an <%ae>%nDate:   %ad (%ar)%n'"
-eg config --global alias.log-oneline "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)'"
-eg config --global alias.log-oneline-nc "log --pretty=format:'%h \"%s\" <%ae> [%ad] (%ar)'"
-eg config --global alias.log-local "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)' origin.."
+eg config --global alias.log-short "log --pretty=format:'%Cgreen%h%Creset \"%s\"%nAuthor: %an <%ae>%nDate:   %ad (%cr)%n'"
+eg config --global alias.log-oneline "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%cr)'"
+eg config --global alias.log-oneline-nc "log --pretty=format:'%h \"%s\" <%ae> [%ad] (%cr)'"
+eg config --global alias.log-local "log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%cr)' origin.."
 
 # Summarizing changes locally and remotely
-eg config --global alias.local-stat "!git status ; echo ; echo 'Commits in local repo not yet pushed to origin/master:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)' --name-status HEAD ^origin/master"
-eg config --global alias.remote-stat "!git status ; echo ; echo 'Commits in origin/master not in local repo:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)' --name-status ^HEAD origin/master"
-eg config --global alias.local-stat-short "!git status ; echo ; echo 'Commits in local repo not yet pushed to origin/master:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)' --shortstat --dirstat=0 HEAD ^origin/master"
-eg config --global alias.remote-stat-short "!git status ; echo ; echo 'Commits in origin/master not in local repo:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%ar)' --shortstat --dirstat=0 ^HEAD origin/master"
+eg config --global alias.local-stat "!git status ; echo ; echo 'Commits in local repo not yet pushed to origin/master:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%cr)' --name-status HEAD ^origin/master"
+eg config --global alias.remote-stat "!git status ; echo ; echo 'Commits in origin/master not in local repo:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%cr)' --name-status ^HEAD origin/master"
+eg config --global alias.local-stat-short "!git status ; echo ; echo 'Commits in local repo not yet pushed to origin/master:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%cr)' --shortstat --dirstat=0 HEAD ^origin/master"
+eg config --global alias.remote-stat-short "!git status ; echo ; echo 'Commits in origin/master not in local repo:' ; echo ; git log --pretty=format:'%Cgreen%h%Creset \"%s\" <%ae> [%ad] (%cr)' --shortstat --dirstat=0 ^HEAD origin/master"
 
 # Diffing text files like latex (undocumented option --color-words)
 eg config --global alias.wlog "log --color-words"
