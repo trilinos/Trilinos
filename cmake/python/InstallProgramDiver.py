fbc8ce4a89a22dfef22eaafe03759a0db20b8416
Roscoe A. Bartlett <rabartl@sandia.gov>
2010-11-24 21:05:15 -0700

    Some more work (but not working yet).

--- cmake/python/InstallProgramDiver.py
+++ cmake/python/InstallProgramDiver.py
@@ -1,12 +1,15 @@
 
+
 from GeneralScriptSupport import *
 from optparse import OptionParser
 
 
 class InstallProgramDriver:
 
+
   def __init__(self, installObj):
     self.installObj = installObj
+
 
   def runDriver(self):
 
@@ -14,17 +17,23 @@ class InstallProgramDriver:
     baseDirName = self.installObj.getBaseDirName()
 
     scriptName = getScriptName();
-      
-    usageHelp = scriptName+" [OPTIONS] [--install-dir=<SOMEDIR> ...]
 
-Tool that checks out, configures, builds, and installs "+productName+" in one shot!
+
+    #
+    # 1) Set up the help text
+    #
+      
+    usageHelp = scriptName+\
+""" [OPTIONS] [--install-dir=<SOMEDIR> ...]
+
+Tool that checks out, untars, configures, builds, and installs """+productName+""" in one shot!
 
 By default, if you just type:
 
-   $ SOME_DIR/"+scriptName+" --do-all
+   $ SOME_DIR/"""+scriptName+""" --do-all
 
-then the directory "+baseDirName+" will get created in the local working directory
-and it will contain a tarball for "+productName+" and the build files. NOTE: This
+then the directory """+baseDirName+""" will get created in the local working directory
+and it will contain a tarball for """+productName+""" and the build files. NOTE: This
 requires that you run this as root (or with an account that has root
 privileges).  For not running as root, you need to specify --install-dir.
 
@@ -34,29 +43,33 @@ The one option that you may need to change if you do not have root privileges
 is the --install-dir option which is set to /usr/local/bin by default.  For
 example, you might just type:
 
-  $ SOME_DIR/"+scriptName+" --install-dir=$HOME --do-all
+  $ SOME_DIR/"""+scriptName+""" --install-dir=$HOME --do-all
 
 and then it would install "++" and the other executables in $HOME/bin.
 NOTE: You will have to update your PATH variable to include whatever directory
-you choose to install "+productName+" in.
+you choose to install """+productName+""" in.
 
 NOTE: If you need to use sudo to install in /usr/local/bin or some other place
 that needs root privileges, do:
 
-  $ SOME_DIR/"+scriptName+" --install-dir=$HOME --checkout --untar --configure --build
-  $ sudo SOME_DIR/"+scriptName+" --install-dir=$HOME --install
+  $ SOME_DIR/"""+scriptName+""" --install-dir=$HOME --checkout --untar --configure --build
+  $ sudo SOME_DIR/"""+scriptName+""" --install-dir=$HOME --install
 
 This appears to work on some systems.
 
 After you have done a successful install, you might want to do:
 
-  $ rm -r "+baseDirName+"
+  $ rm -r """+baseDirName+"""
 
 in order to remove the source and build files.
 
-Good luck with "+productName+"!
+Good luck with """+productName+"""!
 
-"
+"""
+
+    #
+    # 2) Parse the command-line
+    #
 
     clp = OptionParser(usage=usageHelp)
     
@@ -65,9 +78,9 @@ Good luck with "+productName+"!
       help="Do the checkout of the tarball" )
     
     clp.add_option(
-      "--cvs-command", dest="cvsCommand", type="string",
-      git default=self.installObj.getDefaultCvsCommand()
-      help="Command used to check out "+productName+" tarball." )
+      "--checkout-command", dest="checkoutCommand", type="string",
+      default=self.installObj.getDefaultCheckoutCommand(),
+      help="Command used to check out "+productName+" tarball(s)." )
     
     clp.add_option(
       "--untar", dest="untar", action="store_true", default=False,
@@ -96,6 +109,10 @@ Good luck with "+productName+"!
       help="Install "+productName )
     
     clp.add_option(
+      "--show-final", dest="showFinal", action="store_true", default=False,
+      help="Show final instructions for using "+productName )
+    
+    clp.add_option(
       "--do-all", dest="doAll", action="store_true", default=False,
       help="Same as: --checkout --untar --configure --build --install" )
     
@@ -107,9 +124,17 @@ Good luck with "+productName+"!
       options.configure = True
       options.build = True
       options.install = True
+      options.showFinal = True
+      
+
+    #
+    # 3) Echo the command-line options
+    #
+
+
     
     #
-    # Execute the commands
+    # 4) Execute the commands
     #
     
     baseDir = os.getcwd()
@@ -119,7 +144,7 @@ Good luck with "+productName+"!
     self.installObj.setup(options)
 
     print ""
-    print "A) Checkout the tarball for "+productName+" ..."
+    print "A) Checkout the tarball(s) for "+productName+" ..."
     print ""
     
     if options.checkout:
@@ -128,7 +153,7 @@ Good luck with "+productName+"!
       print "Skipping on request ..."
     
     print ""
-    print "B) Untar the tarball ..."
+    print "B) Untar the tarball(s) and set up ready to configure ..."
     print ""
     
     if options.untar:
@@ -168,10 +193,3 @@ Good luck with "+productName+"!
       print "Skipping on request ..."
     
     print "[End]"
-
-
-
-#
-# Unit Testing
-#
-
