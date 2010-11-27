4a0675ee0315182ffb3c2a519cfd56d696266beb
Roger Pawlowski <rppawlo@sandia.gov>
2010-11-27 10:38:10 -0700

    Added shards utilities used in region data object
    This should eventually be moved into shards proper, but has not yet happened.

---
+++ packages/panzer/src/Panzer_Shards_Utilities.hpp
@@ -0,0 +1,168 @@
+#ifndef PANZER_SHARDS_UTILITIES
+#define PANZER_SHARDS_UTILITIES
+
+#include <iostream>
+#include <vector>
+#include <list>
+#include "Teuchos_TestForException.hpp"
+#include "Shards_CellTopology.hpp"
+
+namespace panzer {
+  
+  template<typename ArrayCellGIDs, typename ArraySideGIDs>
+    unsigned 
+    getLocalSideIndexFromGlobalNodeList(const ArrayCellGIDs& cellGIDs, 
+					const ArraySideGIDs& sideGIDs,
+					const shards::CellTopology& cell)
+  {
+    unsigned cell_dim = cell.getDimension();
+    //TEST_FOR_EXCEPTION(!cell.getSubcellHomogeneity(cell_dim - 1),
+    //	       std::runtime_error, "Sides are not homogeneous!");
+    
+    unsigned local_side;
+    bool found_local_side = false;
+    unsigned side = 0;  
+    while ( (side < cell.getSideCount()) && (!found_local_side) ) {
+      
+      const shards::CellTopology 
+	side_topo(cell.getTopology(cell.getDimension()-1, side));
+      
+      unsigned num_side_nodes = 
+	cell.getTopology()->side[side].topology->node_count;
+ 
+
+      std::list<unsigned> tmp_side_gid_list;
+      for (unsigned node = 0; node < num_side_nodes; ++node)
+	tmp_side_gid_list.push_back(cellGIDs[cell.getNodeMap(cell_dim - 1, 
+							     side, node)]);
+     
+      bool side_matches = true;
+      unsigned node = 0;
+      while ( side_matches && (node < num_side_nodes) ) {
+
+	std::list<unsigned>::iterator search = 
+	  std::find(tmp_side_gid_list.begin(), tmp_side_gid_list.end(),
+		    sideGIDs[node]);
+	
+	if (search == tmp_side_gid_list.end())
+	  side_matches = false;
+	  
+	++node;
+      }
+      
+      if (side_matches) {
+	found_local_side = true;
+	local_side = side;
+      }
+      
+      ++side;
+    }
+    
+    TEST_FOR_EXCEPTION(!found_local_side, std::runtime_error,
+		       "Failed to find side!");
+    
+    return local_side;
+  }
+
+  /** This function takes a list of of GIDs defined on
+    * a cell and returns the local subcell Shards index for
+    * a user specified subcell dimension.
+    *
+    * \param[in] cellGIDs Node GIDs for the cell
+    * \param[in] subcellGIDs Node GIDs for the subcell to be found
+    * \param[in] cell Shards cell topology used to find local index
+    * \param[in] subcell_dim Dimension of subcell to be found
+    *
+    * \returns Local Shards index for the subcell described by subcellGIDs
+    */
+  template<typename ArrayCellGIDs, typename ArraySideGIDs>
+    unsigned
+    getLocalSubcellIndexFromGlobalNodeList(const ArrayCellGIDs& cellGIDs,
+                                           const ArraySideGIDs& subcellGIDs,
+                                           const shards::CellTopology& cell,unsigned subcell_dim)
+  {
+    unsigned cell_dim = cell.getDimension();
+  
+    unsigned local_subcell;
+    bool found_local_subcell = false;
+    unsigned subcell = 0;
+    while ( (subcell < cell.getSubcellCount(subcell_dim)) && (!found_local_subcell) ) {
+  
+      unsigned num_subcell_nodes =
+  	cell.getTopology()->subcell[subcell_dim][subcell].topology->node_count;
+  
+      std::list<unsigned> tmp_subcell_gid_list;
+      for (unsigned node = 0; node < num_subcell_nodes; ++node)
+        tmp_subcell_gid_list.push_back(cellGIDs[cell.getNodeMap(subcell_dim,
+                                                             subcell, node)]);
+  
+      bool subcell_matches = true;
+      unsigned node = 0;
+      while ( subcell_matches && (node < num_subcell_nodes) ) {
+  
+        std::list<unsigned>::iterator search =
+          std::find(tmp_subcell_gid_list.begin(), tmp_subcell_gid_list.end(),
+                    subcellGIDs[node]);
+  
+        if (search == tmp_subcell_gid_list.end())
+          subcell_matches = false;
+  
+        ++node;
+      }
+  
+      if (subcell_matches) {
+        found_local_subcell = true;
+        local_subcell = subcell;
+      }
+  
+      ++subcell;
+    }
+  
+    TEST_FOR_EXCEPTION(!found_local_subcell, std::runtime_error,
+                       "Failed to find subcell!");
+  
+    return local_subcell;
+  }
+  
+  /** This function takes a vector of lists of GIDs defined on
+    * a cell and returns a the local Shards subcell numbering to 
+    * indexes into the vector. (Essentially will return a 
+    * function defining (Shards #ing -> Local Elmt #ing).
+    *
+    * \param[in] cellGIDs Node GIDs for the cell
+    * \param[in] subcellGIDs Node GIDs for the subcell to be found
+    *                        for each subcell of a particular dimension
+    * \param[in] cell Shards cell topology used to find local index
+    * \param[in] subcell_dim Dimension of subcell to be found
+    * \param[in,out] subcellMap Vector mapping the shards local
+    *                           numbering to the user specified
+    *                           local numbering.
+    *
+    * \notes To use the mapping "function" to find the GIDs of the
+    *        subcell associated with a Shards indexed subcell
+    *        simply do: <code>subcellGIDs[subcellMap[shardsIndex]]</code>
+    */
+  template<typename ArrayCellGIDs, typename ArraySubcellGIDs>
+    void getLocalSubcellMapFromGlobalNodeLists(const ArrayCellGIDs& cellGIDs,
+                                               const std::vector<ArraySubcellGIDs> & subcellGIDs,
+                                               const shards::CellTopology& cell,unsigned subcell_dim,
+                                               std::vector<unsigned> & subcellMap)
+  {
+     subcellMap.resize(subcellGIDs.size());
+  
+     // loop over subcell node indices searching for local subcell index
+     unsigned index = 0;
+     typename std::vector<ArraySubcellGIDs>::const_iterator subcellIter;
+     for(subcellIter=subcellGIDs.begin();subcellIter!=subcellGIDs.end();++subcellIter) {
+        unsigned localSubcell = getLocalSubcellIndexFromGlobalNodeList(cellGIDs,*subcellIter,cell,subcell_dim);
+  
+        // build vector mapping current index to local subcell index
+        subcellMap[localSubcell] = index;
+  
+        index++;
+     }
+  }
+  
+}
+
+#endif
---
+++ packages/panzer/test/core_tests/shards_utilities.cpp
@@ -0,0 +1,201 @@
+#include <Teuchos_ConfigDefs.hpp>
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_TimeMonitor.hpp>
+
+#include "Panzer_Shards_Utilities.hpp"
+
+using std::cout;
+using std::endl;
+using panzer::getLocalSideIndexFromGlobalNodeList;
+
+namespace panzer {
+  TEUCHOS_UNIT_TEST(shards_utilities, quad_test)
+  {    
+    const shards::CellTopology 
+      quad(shards::getCellTopologyData<shards::Quadrilateral<4> >());
+    const CellTopologyData* quad_data = quad.getTopology();
+    
+    //cout << quad << endl;
+    
+    unsigned local_face = 0;
+    std::vector<unsigned> element_gids(quad.getNodeCount()); 
+    const unsigned num_nodes_per_side = 
+      quad_data->side[0].topology->node_count;
+    std::vector<unsigned> edge_gids(num_nodes_per_side);
+    
+    /* GIDs
+       
+          2 *----* 8
+            |    |  
+            |    |
+         10 *----* 30
+	 
+    */
+    element_gids[0] = 10;
+    element_gids[1] = 30;
+    element_gids[2] = 8;
+    element_gids[3] = 2;
+    
+    edge_gids[0] = 8;
+    edge_gids[1] = 2;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     quad);
+    TEST_EQUALITY(local_face, 2);
+    
+    edge_gids[0] = 10;
+    edge_gids[1] = 30;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     quad);
+    TEST_EQUALITY(local_face, 0);
+    
+    edge_gids[0] = 2;
+    edge_gids[1] = 10;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     quad);
+    TEST_EQUALITY(local_face, 3);
+    
+    edge_gids[0] = 30;
+    edge_gids[1] = 8;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     quad);
+    TEST_EQUALITY(local_face, 1);
+    
+    // Edge with order reversed 
+    edge_gids[0] = 8;
+    edge_gids[1] = 30;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     quad);
+    TEST_EQUALITY(local_face, 1);
+    
+    // Give it an incorrect edge and make sure it throws
+    bool caught_throw = false;
+    try {
+      
+      edge_gids[0] = 8;
+      edge_gids[1] = 10;
+      local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						       quad);
+      
+    }
+    catch (const std::runtime_error& e) { 
+      caught_throw = true;
+      //cout << e.what() << std::endl;
+    }
+    
+    TEST_ASSERT(caught_throw);
+    
+  }
+
+  TEUCHOS_UNIT_TEST(shards_utilities, hex_test)
+  {
+    const shards::CellTopology 
+      hex(shards::getCellTopologyData<shards::Hexahedron<8> >());
+    const CellTopologyData* hex_data = hex.getTopology();
+    
+    //cout << hex << endl;
+    
+    unsigned local_face = 0;
+    std::vector<unsigned> element_gids(hex.getNodeCount()); 
+    const unsigned num_nodes_per_side = 
+      hex_data->side[0].topology->node_count;
+    std::vector<unsigned> edge_gids(num_nodes_per_side);
+    
+    // local to global mapping for element
+    // 0 --> 10
+    // 1 --> 18 
+    // 2 --> 5
+    // 3 --> 7
+    // 4 --> 1
+    // 5 --> 30
+    // 6 --> 20
+    // 7 --> 17
+
+    element_gids[0] = 10;
+    element_gids[1] = 18;
+    element_gids[2] = 5;
+    element_gids[3] = 7;
+    element_gids[4] = 1;
+    element_gids[5] = 30;
+    element_gids[6] = 20;
+    element_gids[7] = 17;
+    
+    edge_gids[0] = 10;
+    edge_gids[1] = 18;
+    edge_gids[2] = 30;
+    edge_gids[3] = 1;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     hex);
+    TEST_EQUALITY(local_face, 0);
+    
+    edge_gids[0] = 18;
+    edge_gids[1] = 5;
+    edge_gids[2] = 20;
+    edge_gids[3] = 30;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     hex);
+    TEST_EQUALITY(local_face, 1);
+  
+    edge_gids[0] = 5;
+    edge_gids[1] = 7;
+    edge_gids[2] = 17;
+    edge_gids[3] = 20;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     hex);
+    TEST_EQUALITY(local_face, 2);
+  
+    edge_gids[0] = 10;
+    edge_gids[1] = 1;
+    edge_gids[2] = 17;
+    edge_gids[3] = 7;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     hex);
+    TEST_EQUALITY(local_face, 3);
+  
+    edge_gids[0] = 10;
+    edge_gids[1] = 7;
+    edge_gids[2] = 5;
+    edge_gids[3] = 18;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     hex);
+    TEST_EQUALITY(local_face, 4);
+  
+    edge_gids[0] = 1;
+    edge_gids[1] = 30;
+    edge_gids[2] = 20;
+    edge_gids[3] = 17;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     hex);
+    TEST_EQUALITY(local_face, 5);
+  
+    // Valid edge with bad ordering 
+    edge_gids[0] = 30;
+    edge_gids[1] = 20;
+    edge_gids[2] = 1;
+    edge_gids[3] = 17;
+    local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						     hex);
+    TEST_EQUALITY(local_face, 5);
+  
+    // Give it an incorrect edge and make sure it throws
+    bool caught_throw = false;
+    try {
+      
+      edge_gids[0] = 30;
+      edge_gids[1] = 7;
+      edge_gids[2] = 18;
+      edge_gids[3] = 17;
+      local_face = getLocalSideIndexFromGlobalNodeList(element_gids, edge_gids,
+						       hex);
+      
+    }
+    catch (const std::runtime_error& e) { 
+      caught_throw = true;
+      //cout << e.what() << std::endl;
+    }
+    
+    TEST_ASSERT(caught_throw);
+    
+  }
+  
+}
