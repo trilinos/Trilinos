4e95198fea762ac73335c0b9aee5a0f0a1d72d2f
Lee Ann Riesen <lriesen@sandia.gov>
2010-12-01 14:33:21 -0700

    Nightly test of Zoltan and Isorropia
    Runs on s861036.srn.sandia.gov and tests with 64 bit global IDs.

---
+++ cmake/ctest/drivers/s861036/cron_driver.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+echo
+echo "Starting nightly Trilinos development testing on trilinos-test: `date`"
+echo
+
+#
+# Is mpd running
+#
+
+mpdhost=$(mpdtrace)
+
+if [ "$mpdhost" != "s861036" ] ; then
+  echo "Starting mpd"
+  mpd &
+fi
+
+exit
+
+# TrilinosDriver settings:
+#
+export TDD_GIT_EXE=/usr/local/bin/git
+export TDD_PARALLEL_LEVEL=2
+export TDD_HTTP_PROXY="http://wwwproxy.sandia.gov:80/"
+
+# Trilinos settings:
+#
+export TDD_CTEST_TEST_TYPE=Nightly
+
+# Machine specific environment:
+#
+export PATH=.:/usr/local/bin:/usr/kerberos/bin:/usr/bin:/bin:/usr/X11R6/bin:/sbin:/home/lriesen/bin
+
+# BASEDIR is the parent directory of this script's Trilinos source tree...
+BASEDIR=`cd "\`dirname \"$0\"\`/../../../../..";pwd`
+echo BASEDIR=$BASEDIR
+BASEDATADIR=$BASEDIR
+
+#export CMAKE_LIBRARY_PATH="/home/trilinos/tpl/gcc4.1.2/exodusII_4.84/lib:/home/trilinos/tpl/gcc4.1.2/netcdf_4.0/lib"
+#export CMAKE_INCLUDE_PATH="/home/trilinos/tpl/gcc4.1.2/exodusII_4.84/include:/home/trilinos/tpl/gcc4.1.2/netcdf_4.0/include"
+export CVS_RSH=ssh
+export LD_LIBRARY_PATH="$BASEDIR/MPI_OPT_DEV_SHARED/BUILD/packages/PyTrilinos/src"
+export PYTHONPATH="/usr/local/lib/python2.7:/usr/local/lib/python2.7/site-packages:/usr/local/lib/python2.7/lib-dynload"
+
+export TRILINOSDATADIRECTORY="$BASEDATADIR/TrilinosData"
+
+pushd "$BASEDATADIR"
+cvs -q -d :ext:software.sandia.gov:/space/CVS co TrilinosData
+popd
+
+# Machine independent cron_driver:
+#
+SCRIPT_DIR=`cd "\`dirname \"$0\"\`";pwd`
+$SCRIPT_DIR/../cron_driver.py
+
+echo
+echo "Ending nightly Trilinos development testing on trilinos-test: `date`"
+echo
+
---
+++ cmake/ctest/drivers/s861036/ctest_zoltan_ids64.cmake
@@ -0,0 +1,42 @@
+
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.trilinos-test.gcc.cmake")
+
+#
+# Set the options specific to this build case
+#   Build Zoltan, Isorropia, Epetra and EpetraExt
+#   Specify 64-bit global IDs for Zoltan and Isorropia
+#
+
+SET(COMM_TYPE MPI)
+SET(BUILD_TYPE RELEASE)
+SET(BUILD_DIR_NAME MPI_OPT_DEV)
+
+SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE OFF)
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DDART_TESTING_TIMEOUT:STRING=120"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON"
+  "-DTrilinos_DATA_DIR:STRING=$ENV{TRILINOSDATADIRECTORY}"
+  "-D CMAKE_CPP_FLAGS:STRING=-DZOLTAN_ID_TYPE_LONG"
+  "-D CMAKE_C_FLAGS:STRING=-std=c99"
+  "-D MPI_EXEC_MAX_NUMPROCS:STRING=11"
+  "-D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF"
+  "-D Trilinos_ENABLE_EXAMPLES:BOOL=ON"
+  "-D Trilinos_ENABLE_TESTS:BOOL=ON"
+  "-D Trilinos_ENABLE_Zoltan:BOOL=ON"
+  "-D Trilinos_ENABLE_Isorropia:BOOL=ON"
+  "-D Trilinos_ENABLE_Epetra:BOOL=ON"
+  "-D Trilinos_ENABLE_EpetraExt:BOOL=ON"
+  "-D Zoltan_ENABLE_ParMETIS:BOOL=ON"
+  "-D Zoltan_ENABLE_Scotch:BOOL=ON"
+  "-D ParMETIS_INCLUDE_DIRS:FILEPATH=/home/lafisk/system/parmetis/ParMetis-3.1"
+  "-D ParMETIS_LIBRARY_DIRS:FILEPATH=/home/lafisk/system/parmetis/ParMetis-3.1"
+  "-D Scotch_INCLUDE_DIRS:FILEPATH=/home/lriesen/system/scotch_5.1.10a-32/include"
+  "-D Scotch_LIBRARY_DIRS:FILEPATH=/home/lriesen/system/scotch_5.1.10a-32/lib"
+  )
+
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
+
+TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
