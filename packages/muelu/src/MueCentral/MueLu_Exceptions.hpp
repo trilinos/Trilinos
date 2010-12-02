541479a0289470afb58e34afe0b77d53c6a499e6
Jeremie Gaidamour <jngaida@sandia.gov>
2010-12-02 08:42:31 -0700

    MueLu Exceptions

---
+++ packages/muelu/src/MueCentral/MueLu_Exceptions.hpp
@@ -0,0 +1,37 @@
+#ifndef MUELU_EXCEPTIONS_HPP
+#define MUELU_EXCEPTIONS_HPP
+
+#include <exception>
+#include <Teuchos_Exceptions.hpp>
+
+#include "MueLu_ConfigDefs.hpp"
+
+namespace MueLu {
+  namespace Exceptions {
+    
+    //! Exception indicating invalid cast attempted
+    class BadCast : public Teuchos::ExceptionBase
+    {
+    public:
+      BadCast(const std::string& what_arg) : Teuchos::ExceptionBase(what_arg) {}
+    };
+
+    //! Exception throws when you call an unimplemented method of MueLu
+    /** Mainly use for development in progress. **/
+    class NotImplemented : public Teuchos::ExceptionBase
+    {
+    public:
+      NotImplemented(const std::string& what_arg) : Teuchos::ExceptionBase(what_arg) {}
+    };
+      
+    //! Exception throws to report errors in the internal logical of the program.
+    class RuntimeError : public Teuchos::ExceptionBase
+    {
+    public:
+      RuntimeError(const std::string& what_arg) : Teuchos::ExceptionBase(what_arg) {}
+    };
+
+  }
+}
+
+#endif
