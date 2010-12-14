8598a84bfa49012b1b71f1a96ffe41b39285a28b
Jeremie Gaidamour <jngaida@sandia.gov>
2010-12-13 18:48:44 -0700

    Cthulhu: VectorFactory can now create EpetraIntVector

--- packages/muelu/cthulhu/src/Vector/Cthulhu_VectorFactory.hpp
+++ packages/muelu/cthulhu/src/Vector/Cthulhu_VectorFactory.hpp
@@ -14,6 +14,7 @@
 #ifdef HAVE_CTHULHU_EPETRA
 #include "Cthulhu_EpetraMap.hpp"
 #include "Cthulhu_EpetraVector.hpp"
+#include "Cthulhu_EpetraIntVector.hpp"
 #endif
 
 #include "Cthulhu_Debug.hpp"
@@ -50,6 +51,38 @@ namespace Cthulhu {
       if (tMap != null)
         return rcp( new TpetraVector(map, zeroOut) );
 #endif
+      TEST_FOR_EXCEPTION(1,Cthulhu::Exceptions::BadCast,"Cannot dynamically cast Cthulhu::Map to an EpetraMap or a TpetraMap. The exact type of the Map 'map' is unknown");
+    }
+
+#ifdef CTHULHU_NOT_IMPLEMENTED
+    // Other constructors here
+#endif
+    
+  };
+
+  template <>
+  class VectorFactory<double, int, int, Kokkos::DefaultNode::DefaultNodeType> {
+    
+    typedef Map<int, int, Kokkos::DefaultNode::DefaultNodeType> Map;
+    typedef Vector<double, int, int, Kokkos::DefaultNode::DefaultNodeType> Vector;
+#ifdef HAVE_CTHULHU_TPETRA
+    typedef TpetraMap<int, int, Kokkos::DefaultNode::DefaultNodeType> TpetraMap;
+    typedef TpetraVector<double, int, int, Kokkos::DefaultNode::DefaultNodeType> TpetraVector;
+#endif
+
+  private:
+    //! Private constructor. This is a static class. 
+    VectorFactory() {}
+    
+  public:
+    
+    //! Constructor specifying the number of non-zeros for all rows.
+    static RCP<Vector> Build(const Teuchos::RCP<const Map> &map, bool zeroOut=true) {
+#ifdef HAVE_CTHULHU_TPETRA
+      const RCP<const TpetraMap> &tMap = Teuchos::rcp_dynamic_cast<const TpetraMap>(map);
+      if (tMap != null)
+        return rcp( new TpetraVector(map, zeroOut) );
+#endif
 #ifdef HAVE_CTHULHU_EPETRA
       const RCP<const EpetraMap> &eMap = Teuchos::rcp_dynamic_cast<const EpetraMap>(map);
       if (eMap != null)
@@ -64,7 +97,46 @@ namespace Cthulhu {
     
   };
 
+  template <>
+  class VectorFactory<int, int, int, Kokkos::DefaultNode::DefaultNodeType> {
+    
+    typedef Map<int, int, Kokkos::DefaultNode::DefaultNodeType> Map;
+    typedef Vector<int, int, int, Kokkos::DefaultNode::DefaultNodeType> Vector;
+#ifdef HAVE_CTHULHU_TPETRA
+    typedef TpetraMap<int, int, Kokkos::DefaultNode::DefaultNodeType> TpetraMap;
+    typedef TpetraVector<int, int, int, Kokkos::DefaultNode::DefaultNodeType> TpetraVector;
+#endif
+
+  private:
+    //! Private constructor. This is a static class. 
+    VectorFactory() {}
+    
+  public:
+    
+    //! Constructor specifying the number of non-zeros for all rows.
+    static RCP<Vector> Build(const Teuchos::RCP<const Map> &map, bool zeroOut=true) {
+#ifdef HAVE_CTHULHU_TPETRA
+      const RCP<const TpetraMap> &tMap = Teuchos::rcp_dynamic_cast<const TpetraMap>(map);
+      if (tMap != null)
+        return rcp( new TpetraVector(map, zeroOut) );
+#endif
+#ifdef HAVE_CTHULHU_EPETRA
+      const RCP<const EpetraMap> &eMap = Teuchos::rcp_dynamic_cast<const EpetraMap>(map);
+      if (eMap != null)
+        return rcp( new EpetraIntVector(map, zeroOut) );
+#endif
+      TEST_FOR_EXCEPTION(1,Cthulhu::Exceptions::BadCast,"Cannot dynamically cast Cthulhu::Map to an EpetraMap or a TpetraMap. The exact type of the Map 'map' is unknown");
+    }
+
+#ifdef CTHULHU_NOT_IMPLEMENTED
+    // Other constructors here
+#endif
+    
+  };
+
 }
 
 // TODO: Only one factory for Vector and MultiVector ?? -> Yes
+
+#define CTHULHU_VECTORFACTORY_SHORT
 #endif
