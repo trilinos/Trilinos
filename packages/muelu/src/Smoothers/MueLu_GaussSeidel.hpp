7f04189bfd63fa49e83e562ac5c760060e4f6ba3
Jonathan Hu <jhu@sandia.gov>
2011-01-05 10:55:32 -0800

    Temporary Gauss Seidel smoother.

---
+++ packages/muelu/src/Smoothers/MueLu_GaussSeidel.hpp
@@ -0,0 +1,113 @@
+#ifndef MUELU_GAUSSSEIDEL_HPP
+#define MUELU_GAUSSSEIDEL_HPP
+
+namespace MueLu {
+
+template
+<class ScalarType, class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+class GaussSeidel : public SmootherPrototype<ScalarType,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> {
+
+#include "MueLu_UseShortNames.hpp"
+
+  private:
+
+    //! sweeps
+    LO nIts_;
+    //! relaxation parameter
+    SC omega_;
+    Teuchos::RCP<Operator> A_;
+
+  public:
+
+  //! @name Constructors/Destructors
+  //@{
+  GaussSeidel(LO sweeps=1, SC omega=1.0) : nIts_(sweeps), omega_(omega) {}
+
+  virtual ~GaussSeidel() {}
+  //@}
+
+  //! @name Setup and apply methods.
+  //@{
+
+  //! Set up the smoother. (Right now, just grab A from the Level.)
+  void Setup(Teuchos::RCP<Level> const level)
+  {
+    A_ = level.GetA();
+  }
+
+  /*! Solve A*x=b approximately with the smoother.
+
+      FIXME right now, InitialGuessIsZero is ignored.
+
+      @param x  unknown vector
+      @param b  right-hand side
+      @param InitialGuessIsZero if true, indicates that x is zero, and that some flops might be avoided
+  */
+  void Apply(Teuchos::RCP<MultiVector> x, Teuchos::RCP<MultiVector> const b, bool InitialGuessIsZero=false)
+  {
+     if (InitialGuessIsZero)
+       throw(Exceptions::NotImplemented("No logic for handling zero initial guesses"));
+
+     // get matrix diagonal
+     RCP<Vector> diag = VectorFactory::Build(A_->getRangeMap());
+     A_->getLocalDiagCopy(*diag);
+
+     Teuchos::ArrayRCP<const SC> bdata = b->getData(0);
+     Teuchos::ArrayRCP<SC>       xdata = x->getDataNonConst(0);
+     Teuchos::ArrayRCP<const SC> diagdata = diag->getData(0);
+
+     // loop through rows
+     SC sum;
+     Teuchos::ArrayRCP<const LO> indices;
+     Teuchos::ArrayRCP<const SC>  values;
+     for (int i=0; i<A_->getNodeNumRows(); i++) {
+       A_->getLocalRowView(i,indices,values);
+       sum = bdata[i];
+       for (int j=0; j< indices.size(); j++)
+         sum -= values[j]*xdata[indices[j]];
+       xdata[i] += (omega_/diagdata[i]) * sum;
+     }
+  } //Apply()
+
+  //@}
+
+  //! @name Set/Get methods.
+  //@{
+
+  void SetNIts(LO Nits) {
+    nIts_ = Nits;
+  }
+
+  LO GetNIts() {
+    return nIts_;
+  }
+
+  //@}
+
+  //! @name Utilities.
+  //@{
+
+  void Print(std::string prefix) {
+    throw(Exceptions::NotImplemented("Printing not implemented yet"));
+  }
+
+  RCP<SmootherPrototype> Copy()
+  {
+    return rcp(new GaussSeidel(*this) );
+  }
+
+  void CopyParameters(RCP<SmootherPrototype> source)
+  {
+    nIts_ = source.nIts_;
+    omega_ = source.omega_;
+    A_ = source.A_;
+  }
+
+  //@}
+
+}; //class GaussSeidel
+
+} //namespace MueLu
+
+#define MUELU_GAUSSSEIDEL_SHORT
+#endif //ifndef MUELU_GAUSSSEIDEL_HPP
