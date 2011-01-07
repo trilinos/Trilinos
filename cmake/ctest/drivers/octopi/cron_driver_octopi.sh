026671f177fc7a2a3142e72069bae2875336b23b
K. Devine <kddevin@sandia.gov>
2011-01-07 12:39:21 -0700

    Moving purify tests from experimental to nightly so we get emails about them.

--- cmake/ctest/drivers/octopi/cron_driver_octopi.sh
+++ cmake/ctest/drivers/octopi/cron_driver_octopi.sh
@@ -5,4 +5,4 @@ export TDD_CTEST_TEST_TYPE=Nightly
 export HTTP_PROXY="http://sonproxy.sandia.gov:80/"
 export PATH=/opt/lam714-gcc346-pure/bin:/usr/local/bin:$PATH
 /opt/lam714-gcc346-pure/bin/lamboot
-env Trilinos_PACKAGES=Zoltan CTEST_TEST_TYPE=Experimental ../cron_driver.py
+env Trilinos_PACKAGES=Zoltan CTEST_TEST_TYPE=Nightly ../cron_driver.py
