10af13812178425f3dc4b01bf5c2c87a8d935f06
Brent Perschbacher <bmpersc@sandia.gov>
2011-01-12 15:20:48 -0700

    re-enable the previously disabled gtest tpl
    The gtest tpl was disabled earlier today in response to errors that were caused
    by a the commit that introduced it. Gtest was not the cause of the errors, and
    changes have been made to STK to no longer require gtest for all builds so it is
    safe to re-enable this as a valid tpl. Tests were run to ensure that STK will
    configure/build without gtest available.j

--- cmake/TPLs/FindTPLgtest.cmake
+++ cmake/TPLs/FindTPLgtest.cmake
@@ -1,8 +1,6 @@
-# All this broke trilinos configuration 1/11/11
-#
 INCLUDE(TPLDeclareLibraries)
-#
+
 TPL_DECLARE_LIBRARIES( gtest
-#  REQUIRED_HEADERS gtest/gtest.h 
-#  REQUIRED_LIBS_NAMES gtest
+  REQUIRED_HEADERS gtest/gtest.h 
+  REQUIRED_LIBS_NAMES gtest
   )
