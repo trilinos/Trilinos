df519e761587ab75dbe5c19f242d79ca4c56e8c6
Baker Christopher G <ogb@billmp1.ornl.gov>
2011-01-19 09:01:50 -0500

    Forgot DROP_SITE and misspelled CDash in email URL

--- cmake/ctest/drivers/billmp1/cron_driver_billmp1.sh
+++ cmake/ctest/drivers/billmp1/cron_driver_billmp1.sh
@@ -18,10 +18,13 @@ cd -
 export TDD_PARALLEL_LEVEL=1
 export TDD_CTEST_TEST_TYPE=Experimental
 
+export CTEST_DROP_SITE=casl-dev.ornl.gov
+export CTEST_DROP_LOCATION="/CDash/submit.php?project=Trilinos"
+
 time /usr/bin/python ../cron_driver.py
 
 echo
 echo "Ending nightly Trilinos testing on billmp1: `date`"
 echo
 
-echo "Finished nightly Trilinos CMake tests billmp1: http://casl-dev.ornl.gov/cdash/index.php?project=Trilinos" | mailx -s "Nightly CTest: billmp1" bakercg@ornl.gov
+echo "Finished nightly Trilinos CMake tests billmp1: http://casl-dev.ornl.gov/CDash/index.php?project=Trilinos" | mailx -s "Nightly CTest: billmp1" bakercg@ornl.gov
