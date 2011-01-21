bd1a43e24efe14686d1cb7e6597290cb580c854f
James M. Willenbring <jmwille@sandia.gov>
2011-01-21 07:39:02 -0700

    Removing the BOOST TPL library search again.

--- cmake/TPLs/FindTPLBoost.cmake
+++ cmake/TPLs/FindTPLBoost.cmake
@@ -2,7 +2,6 @@ INCLUDE(TPLDeclareLibraries)
 
 TPL_DECLARE_LIBRARIES( Boost
   REQUIRED_HEADERS boost/version.hpp boost/mpl/at.hpp
-  REQUIRED_LIBS_NAMES "program_options"
   )
 
 # This broke trilinos configuration:
