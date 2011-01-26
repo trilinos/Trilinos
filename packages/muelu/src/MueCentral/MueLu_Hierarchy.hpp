e15f8a91365b05762ad4e39373e3ef29065cf392
Jonathan Hu <jhu@sandia.gov>
2011-01-26 11:08:11 -0800

    Intermediate changes to MueLu::Hierarchy.
    Starting to synchronize with design of MueMat.
    Basically, I've duplicated what Axel has done with splitting out SetCoarsest from Set Smoothers.
    There's no notion yet of PRFactory or GenericPRFactory, i.e., PFact and RFact are still separate
    arguments to FillHierarchy.  In particular, this means no coarse A gets built if RFact isn't supplied
    explicitly.

--- packages/muelu/src/MueCentral/MueLu_Hierarchy.hpp
+++ packages/muelu/src/MueCentral/MueLu_Hierarchy.hpp
@@ -10,6 +10,7 @@
 #include "MueLu_RAPFactory.hpp"
 #include "MueLu_SmootherFactory.hpp"
 #include "MueLu_Types.hpp"
+#include "MueLu_IfpackSmoother.hpp"
 
 namespace MueLu {
 /*!
@@ -69,6 +70,11 @@ class Hierarchy : public Teuchos::VerboseObject<Hierarchy<Scalar,LocalOrdinal,Gl
      Teuchos::RCP<Level>& GetLevel(int const levelID) {
        return Levels_[levelID];
      }
+
+     LO GetNumberOfLevels() {
+       return Levels_.size();
+     }
+
    //@}
 
    //! @name Populate Methods.
@@ -76,12 +82,14 @@ class Hierarchy : public Teuchos::VerboseObject<Hierarchy<Scalar,LocalOrdinal,Gl
 
      /*!
        @brief Constructs components of the hierarchy.
-       FIXME should return status
-       FIXME also calculate complexity here
+       FIXME should return status data structure
 
-       The only required factory is for the prolongator.  Factories to build the smoothers,
-       restriction matrices, and coarse level matrices are optional.  Default behavior is to
-       ignore any empty factories.  FillHierarchy invokes this method.
+       Invoke a set of factories to populate (construct prolongation,
+       restriction, coarse level discretizations, and smoothers in this
+       order) a multigrid Hierarchy starting with information on 'startLevel'
+       and continuing for at most 'numDesiredLevels'.
+
+       Note: Empty factories are simply skipped.
      */
      void FullPopulate(Teuchos::RCP<OperatorFactory> PFact,
                        Teuchos::RCP<OperatorFactory> RFact=Teuchos::null,
@@ -91,112 +99,234 @@ class Hierarchy : public Teuchos::VerboseObject<Hierarchy<Scalar,LocalOrdinal,Gl
      {
        Teuchos::OSTab tab(out_);
        MueLu_cout(Teuchos::VERB_HIGH) << "Hierarchy::FullPopulate()" << std::endl;
+
+       if (RFact == Teuchos::null)  RFact = rcp( new TransPFactory());
+       if (AcFact == Teuchos::null) AcFact = rcp( new RAPFactory());
+       if (SmooFact == Teuchos::null) {
+//FIXME #ifdef we're using tpetra
+//FIXME    throw(Exceptions::NotImplemented("No default smoother is defined"));
+//FIXME #else we're using epetra
+         Teuchos::ParameterList  ifpackList;
+         ifpackList.set("relaxation: type", "Gauss-Seidel");
+         ifpackList.set("relaxation: sweeps", (int) 1);
+         ifpackList.set("relaxation: damping factor", (double) 1.0);
+         ifpackList.set("relaxation: zero starting solution", false);
+         RCP<IfpackSmoother>  smoother = rcp( new IfpackSmoother("point relaxation stand-alone",ifpackList) );
+         RCP<SmootherFactory>    SmooFact = rcp( new SmootherFactory(smoother) );
+//FIXME #endif
+       }
+
+       FillHierarchy(PFact,RFact,AcFact,startLevel,numDesiredLevels /*,status*/);
+       SetSmoothers(SmooFact,startLevel,numDesiredLevels);
+
+     } //FullPopulate()
+
+     /*! @brief Populate hierarchy with A's, R's, and P's.
+         TODO should return status structure containing complexity, start level, end level
+         TODO replace PFact and RFact with PRFact
+
+         Prolongator factory defaults to SaPFactory.
+     */
+     void FillHierarchy() {
+       RCP<OperatorFactory> PFact = rcp(new SaPFactory());
+       FillHierarchy(PFact);
+     } //FillHierarchy()
+     
+     /*! @brief Populate hierarchy with A's, R's, and P's.
+         TODO should return status structure containing complexity, start level, end level
+         TODO replace PFact and RFact with PRFact
+
+         Invoke a set of factories to populate (construct prolongation,
+         restriction, and coarse level discretizations in this
+         order) a multigrid Hierarchy starting with information on 'startLevel' 
+         and continuing for at most 'numDesiredLevels'. 
+     */
+     void FillHierarchy(Teuchos::RCP<OperatorFactory> PFact,
+                       Teuchos::RCP<OperatorFactory> RFact=Teuchos::null,
+                       Teuchos::RCP<OperatorFactory> AcFact=Teuchos::null,
+                       int startLevel=0, int numDesiredLevels=10 )
+     {
+       Teuchos::OSTab tab(out_);
+       MueLu_cout(Teuchos::VERB_HIGH) << "Hierarchy::FillHierachy()" << std::endl;
+       if (PFact == Teuchos::null) PFact = rcp(new SaPFactory());
        bool goodBuild=true;
        int i = startLevel;
        while (i < startLevel + numDesiredLevels - 1)
        {
-         //TODO MueMat has a check for an empty level at position i+1
-         //TODO I'm not sure how this can happen, but if it does, we must insert code to handle
-         //TODO it similarly to MueMat.
-         if (PFact != Teuchos::null) {
-           if ( (i+1) >= (int) Levels_.size() || Levels_[i+1] == Teuchos::null ) {
-             Teuchos::OSTab tab(out_);
-             Levels_.push_back( Levels_[i]->Build(*out_) );
-           }
-           Levels_[i+1]->SetLevelID(i+1);
-           goodBuild = PFact->Build(*(Levels_[i]),*(Levels_[i+1]) /*,MySpecs*/);
-         } //if (PFact != Teuchos::null)
+         if ( (i+1) >= (int) Levels_.size() || Levels_[i+1] == Teuchos::null ) {
+           Teuchos::OSTab tab(out_);
+           Levels_.push_back( Levels_[i]->Build(*out_) );
+         }
+         Levels_[i+1]->SetLevelID(i+1);
+         goodBuild = PFact->Build(*(Levels_[i]),*(Levels_[i+1]) /*,MySpecs*/);
          if ((int)Levels_.size() <= i) goodBuild=false; //TODO is this the right way to cast?
-         if (!goodBuild) /*TODO make Levels_ be length i*/;
+         if (!goodBuild) {
+           Levels_.resize(i+1); //keep only entries 0..i
+           break;
+         }
          if (RFact != Teuchos::null)
-           if ( !RFact->Build(*(Levels_[i]),*(Levels_[i+1]) /*,MySpecs*/) ) {
-             //make Levels_ be length i
+           if ( !RFact->Build(*(Levels_[i]),*(Levels_[i+1])) ) {
+             Levels_.resize(i+1); //keep only entries 0..i
              break;
            }
          if (AcFact != Teuchos::null)
-           if ( !AcFact->Build(*(Levels_[i]),*(Levels_[i+1]) /*,MySpecs*/) ) {
-             //make Levels_ be length i
-           break;
+           if ( !AcFact->Build(*(Levels_[i]),*(Levels_[i+1])) ) {
+             Levels_.resize(i+1); //keep only entries 0..i
+             break;
            }
-         if (SmooFact != Teuchos::null) {
-           Teuchos::RCP<SmootherPrototype> preSm, postSm;
-           SmooFact->Build(Levels_[i], preSm, postSm);
-           if (preSm != Teuchos::null) Levels_[i]->SetPreSmoother(preSm);
-           if (postSm != Teuchos::null) Levels_[i]->SetPostSmoother(postSm);
-         }
          ++i;
        } //while
-     } //FullPopulate()
+     }
 
-     /*! @brief Construct smoothers on all levels.
+     /*! @brief Construct smoothers on all levels but the coarsest.
        TODO should return status
+       TODO need to check whether using Tpetra or Epetra
+
+        Invoke a set of factories to construct smoothers within 
+        a multigrid Hierarchy starting with information on 'startLevel' 
+        and continuing for at most 'numDesiredLevels'. 
+
+        Note: last level smoother will not be set here. Use SetCoarsestSolver()
+        to define a smoother for the last level. Otherwise, a direct solve is
+        assumed
      */
-     void SetSmoothers(RCP<SmootherFactory> smooFact=Teuchos::null, LO startLevel=0, LO numDesiredLevels=-1) {
+     void SetSmoothers(RCP<SmootherFactory> smooFact=Teuchos::null, LO startLevel=0, LO numDesiredLevels=-1)
+     {
        Teuchos::OSTab tab(out_);
        MueLu_cout(Teuchos::VERB_HIGH) << "Hierarchy::SetSmoothers()" << std::endl;
        if (smooFact == Teuchos::null) {
-         throw(Exceptions::NotImplemented("No default smoother is defined"));
+//FIXME #ifdef we're using tpetra
+//FIXME    throw(Exceptions::NotImplemented("No default smoother is defined"));
+//FIXME #else we're using epetra
+         Teuchos::ParameterList  ifpackList;
+         ifpackList.set("relaxation: type", "Gauss-Seidel");
+         ifpackList.set("relaxation: sweeps", (int) 1);
+         ifpackList.set("relaxation: damping factor", (double) 1.0);
+         ifpackList.set("relaxation: zero starting solution", false);
+         RCP<IfpackSmoother>  smoother = rcp( new IfpackSmoother("point relaxation stand-alone",ifpackList) );
+         RCP<SmootherFactory>    smooFact = rcp( new SmootherFactory(smoother) );
+//FIXME #endif
        }
        if (numDesiredLevels == -1)
-         numDesiredLevels = Levels_.size()-startLevel+1;
-       //TODO FullPopulate should return boolean status
-       FullPopulate(Teuchos::null,Teuchos::null,Teuchos::null,smooFact,startLevel,numDesiredLevels);
+         numDesiredLevels = GetNumberOfLevels()-startLevel;
+       LO lastLevel = startLevel + numDesiredLevels - 1;
+
+       //checks
+       if (startLevel >= GetNumberOfLevels())
+         throw(Exceptions::RuntimeError("startLevel >= actual number of levels"));
+
+       if (lastLevel >= GetNumberOfLevels()) {
+         lastLevel = GetNumberOfLevels() - 1;
+         MueLu_cout(Teuchos::VERB_HIGH)
+           << "Warning: Coarsest Level will have a direct solve!" << std::endl;
+       }
+
+       for (int i=startLevel; i<=lastLevel; i++) {
+         Teuchos::RCP<SmootherPrototype> preSm, postSm;
+         smooFact->Build(Levels_[i], preSm, postSm);
+         if (preSm != Teuchos::null) Levels_[i]->SetPreSmoother(preSm);
+         if (postSm != Teuchos::null) Levels_[i]->SetPostSmoother(postSm);
+       }
+
      } //SetSmoothers()
-
-     /*!
-       @brief Constructs components of the hierarchy.
-       TODO should return status
-
-       The only required factory is for the prolongator.  Factories to build the restriction and
-       coarse level matrices are optional.  Default behavior is to ignore any empty factories.
-       Internally, this calls FullPopulate.
-
-     */
-     void FillHierarchy(Teuchos::RCP<OperatorFactory> PFact,
-                        Teuchos::RCP<OperatorFactory> RFact=Teuchos::null,
-                        Teuchos::RCP<OperatorFactory> AcFact=Teuchos::null,
-                        int startLevel=0, int numDesiredLevels=10 /*,Needs*/)
-     {
-       if (PFact == Teuchos::null) {throw(std::logic_error("FillHierarchy: must supply at least a Prolongator factory"));}
-       if (RFact == Teuchos::null) RFact = Teuchos::rcp(new TransPFactory());
-       if (AcFact == Teuchos::null) AcFact = Teuchos::rcp(new RAPFactory());
-
-       Teuchos::RCP<SmootherFactory> SmooFact=Teuchos::null;
-       FullPopulate(PFact,RFact,AcFact,SmooFact,startLevel,numDesiredLevels);
-     }
 
      /*!
        @brief Apply the multigrid preconditioner.
      */
 
-     void Iterate(MultiVector const &rhs, LO nIts, MultiVector const &solution,
-                  bool InitialGuessIsZero=false, CycleType const Cycle=VCYCLE, LO const startLevel=1)
-     {
-       Teuchos::OSTab tab(out_);
-       MueLu_cout(Teuchos::VERB_HIGH) << "Hierarchy::Iterate()" << std::endl;
+         typedef typename Teuchos::ScalarTraits<SC>::magnitudeType Magnitude;
 
-     /*
+#define GimmeNorm(someVec,someLabel) {(someVec)->norm2(norms); \
+         *out_ << someLabel << " = " << norms<< std::endl;}
+
+     void Iterate(RCP<MultiVector> const &rhs, LO nIts, RCP<MultiVector> &X,
+                  bool const &InitialGuessIsZero=false, CycleType const &Cycle=VCYCLE, LO const &startLevel=0)
+     {
+       Teuchos::Array<Magnitude> norms(1);
+       Teuchos::OSTab tab(*out_);
+       *out_ << "Hierarchy::Iterate()" << std::endl;
+
+           *out_ << "Levels_ size = " << Levels_.size() << std::endl;
+           *out_ << "startLevel = " << startLevel << std::endl;
+
        for (LO i=0; i<nIts; i++) {
+
          RCP<Level> Fine = Levels_[startLevel];
-         RCP<Smoother> preSmoo = Fine->GetPreSmoother();}
-         RCP<Smoother> postSmoo = Fine->GetPostSmoother();
+         RCP<SmootherPrototype> preSmoo = Fine->GetPreSmoother();
+         RCP<SmootherPrototype> postSmoo = Fine->GetPostSmoother();
+
+         X->norm2(norms);
+         *out_ << " level " << startLevel << ": ||X_init|| = " << norms<< std::endl;
+         GimmeNorm(rhs,"rhs coming into Iterate");
 
          //If on the coarse level, do either smoothing (if defined) or a direct solve.
-         if (// check if on coarse level//) {
-
-         //intermediate level
+         if (startLevel == Levels_.size()-1) //FIXME is this right?
+         {
+           //FIXME no coarse direct solver right now, just smooth
+           //if (preSmoo != Teuchos::null) preSmoo->Apply(X, rhs, InitialGuessIsZero);
+           GimmeNorm(X,"||X|| before ''direct solve''");
+           GimmeNorm(rhs,"||rhs|| before ''direct solve''");
+           bool emptySolve=true;
+           if (preSmoo != Teuchos::null) {preSmoo->Apply(X, rhs, false); emptySolve=false;}
+           if (postSmoo != Teuchos::null) {postSmoo->Apply(X, rhs, false); emptySolve=false;}
+           if (emptySolve==true)
+             *out_ << "WARNING:  no coarse grid solve!!" << std::endl;
+           GimmeNorm(X,"||X|| after ''direct solve''");
          } else {
-           Coarse = Levels_[startLevel+];
+           //on an intermediate level
+           *out_ << "on intermediate level" << std::endl;
+           RCP<Level> Coarse = Levels_[startLevel+1];
            if (preSmoo != Teuchos::null)
-             preSmoo->Apply(solution, rhs, InitialGuessIsZero);
+             preSmoo->Apply(X, rhs, false);
+             //preSmoo->Apply(X, rhs, InitialGuessIsZero);
 
-           RCP<MultiVector> residual;
-           RCP<MultiVector> workVector;
-           //TODO FINISH ME 
+           RCP<MultiVector> residual = Utils::Residual(*(Fine->GetA()),*X,*rhs);
+           *out_ << "   intermediate ||r||" << " = " << Utils::ResidualNorm(*(Fine->GetA()),*X,*rhs) << std::endl;
+           RCP<Operator> R = Coarse->GetR();
+           RCP<const Epetra_CrsMatrix> epR = Utils::Op2EpetraCrs(R);
+           //*out_ << "epR\n========\n" << *epR << std::endl;
+           //*out_ << *residual << std::endl;
+           //RCP<const Epetra_MultiVector> epResidual = Utils::MV2EpetraMV(residual);
+           //*out_ << "epResidual\n========\n" << *epResidual << std::endl;
+           RCP<MultiVector> coarseRhs = MultiVectorFactory::Build(R->getRowMap(),X->getNumVectors());
+           R->multiply(*residual,*coarseRhs,Teuchos::NO_TRANS,1.0,0.0);
+           //RCP<Epetra_MultiVector> epCoarseRhs = Utils::MV2NonConstEpetraMV(coarseRhs);
+           //*out_ << "epCoarseRhs before multiply\n=====================\n" << *epCoarseRhs << std::endl;
+           //TODO try multiplying the Epetra R time the residual ..
+           //epR->Multiply(false,*epResidual,*epCoarseRhs);
+           //*out_ << "epCoarseRhs\n========\n" << *epCoarseRhs << std::endl;
+           RCP<MultiVector> coarseX = MultiVectorFactory::Build(R->getRowMap(),X->getNumVectors());
+           coarseX->putScalar(0.);
+           *out_ << "   ||coarseRhs||" << " = " << Utils::ResidualNorm(*(Coarse->GetA()),*coarseX,*coarseRhs) << std::endl;
+           Iterate(coarseRhs,1,coarseX,true,Cycle,startLevel+1);
+           //                           ^^ zero initial guess
+           if (Cycle>1)
+             Iterate(coarseRhs,1,coarseX,false,Cycle,startLevel+1);
+             //                           ^^ nonzero initial guess
+     
+           RCP<Operator> P = Coarse->GetP();
+           RCP<MultiVector> correction = MultiVectorFactory::Build(P->getRowMap(),X->getNumVectors());
+           P->multiply(*coarseX,*correction,Teuchos::NO_TRANS,1.0,0.0);
+           correction->norm2(norms);
+           *out_ << "||correction||" << " = " << norms << std::endl;
 
+           X->update(1.0,*correction,1.0);
+
+           if (postSmoo != Teuchos::null) {
+             X->norm2(norms);
+             *out_ << " level " << startLevel << ": before postsmoother, ||X_init|| = " << norms<< std::endl;
+             postSmoo->Apply(X, rhs, false);
+             *out_ << "here" << std::endl;
+           }
          }
 
-       */
+             *out_ << "here 2" << std::endl;
+         if (Fine->GetLevelID() == 1) {
+           *out_ << "||r||_" << i << " = " << Utils::ResidualNorm(*(Fine->GetA()),*X,*rhs) << std::endl;
+         }
+
+       } //for (LO i=0; i<nIts; i++)
+             *out_ << "here 3" << std::endl;
 
      } //Iterate()
 
--- packages/muelu/test/unit_tests/Hierarchy.cpp
+++ packages/muelu/test/unit_tests/Hierarchy.cpp
@@ -11,12 +11,10 @@
 
 namespace {
 
+#ifdef SKIP_THIS_FOR_NOW
 //this macro declares the unit-test-class:
 TEUCHOS_UNIT_TEST(Hierarchy,Test0)
 {
-//we are now in a class method declared by the above macro, and
-//that method has these input arguments:
-//Teuchos::FancyOStream& out, bool& success
 
   using namespace Teuchos;
 
@@ -44,9 +42,104 @@ TEUCHOS_UNIT_TEST(Hierarchy,Test0)
   */
 
 } //TEST0
+#endif
+TEUCHOS_UNIT_TEST(Hierarchy,Constructor)
+{
+
+  using namespace Teuchos;
+
+  out << "version: " << MueLu::Version() << std::endl;
+
+  RCP<Hierarchy> H = rcp(new Hierarchy);
+
+  TEUCHOS_TEST_INEQUALITY(H, Teuchos::null, out, success);
+
+} //Constructor
+
+TEUCHOS_UNIT_TEST(Hierarchy,SetAndGetLevel)
+{
+
+  using namespace Teuchos;
+
+  out << "version: " << MueLu::Version() << std::endl;
+
+  Hierarchy H;
+  RCP<Level> level = rcp(new Level() );
+  H.SetLevel(level);
+  RCP<Level> dupLevel = H.GetLevel(0);
+  TEUCHOS_TEST_EQUALITY(level, dupLevel, out, success);
+
+}//SetAndGetLevel
+
+TEUCHOS_UNIT_TEST(Hierarchy,NumberOfLevels)
+{
+
+  using namespace Teuchos;
+
+  out << "version: " << MueLu::Version() << std::endl;
+
+  Hierarchy H;
+  RCP<Level> levelOne = rcp(new Level() );
+  RCP<Level> levelTwo = rcp(new Level() );
+  RCP<Level> levelThree = rcp(new Level() );
+  H.SetLevel(levelOne);
+  H.SetLevel(levelTwo);
+  H.SetLevel(levelThree);
+  TEUCHOS_TEST_EQUALITY(H.GetNumberOfLevels(), 3, out, success);
+
+}//NumberOfLevels
+
+TEUCHOS_UNIT_TEST(Hierarchy,FillHierarchy_PFact_Only)
+{
+  using Teuchos::RCP;
+  using Teuchos::rcp;
+
+  out << "version: " << MueLu::Version() << std::endl;
+
+  RCP<Level> levelOne = rcp(new Level() );
+  levelOne->SetLevelID(1);
+  RCP<CrsOperator> A = MueLu::UnitTest::create_1d_poisson_matrix<SC,LO,GO>(99);
+  levelOne->SetA(A);
+
+  Hierarchy H;
+  H.SetLevel(levelOne);
+
+  RCP<SaPFactory>  PFact = rcp(new SaPFactory());
+
+  out << "Providing just prolongator factory to FillHierarchy." << std::endl;
+  //FIXME until Hierarchy.FillHierarchy takes a generic (?) RP factory, this
+  //FIXME won't work with with more than 2 levels
+  H.FillHierarchy(PFact,Teuchos::null,Teuchos::null,0,1);
+}
+
+TEUCHOS_UNIT_TEST(Hierarchy,FillHierarchy_NoFactoriesGiven)
+{
+  using Teuchos::RCP;
+  using Teuchos::rcp;
+
+  out << "version: " << MueLu::Version() << std::endl;
+
+  RCP<Level> levelOne = rcp(new Level() );
+  levelOne->SetLevelID(1);
+  RCP<CrsOperator> A = MueLu::UnitTest::create_1d_poisson_matrix<SC,LO,GO>(99);
+  levelOne->SetA(A);
+
+  Hierarchy H;
+  H.SetLevel(levelOne);
+
+  out << "Providing no factories to FillHierarchy." << std::endl;
+  //FIXME until Hierarchy.FillHierarchy takes a generic (?) RP factory, this
+  //FIXME test will ALWAYS fail because it is trying to produce more than two levels.
+  //FIXME Without an R factory, Acoarse can't be made, and the next time through,
+  //FIXME GetA fails.
+  H.FillHierarchy();
+}
 
 TEUCHOS_UNIT_TEST(Hierarchy,FillHierarchy1)
 {
+//we are now in a class method declared by the above macro, and
+//that method has these input arguments:
+//Teuchos::FancyOStream& out, bool& success
 
   using Teuchos::RCP;
   using Teuchos::rcp;
