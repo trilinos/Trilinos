93aa9f7f3ffbf6b873b5099e3af21f878b14c95a
Jonathan Hu <jhu@sandia.gov>
2011-01-26 08:51:57 -0800

    Adding helper function to generate matrices via gallery.

--- packages/muelu/test/unit_tests/test_helpers.hpp
+++ packages/muelu/test/unit_tests/test_helpers.hpp
@@ -12,12 +12,21 @@
 #ifdef HAVE_CTHULHU_TPETRA
 #include "Cthulhu_TpetraMap.hpp"
 #endif
+#include "Cthulhu_CrsMatrix.hpp"
+#include "Cthulhu_CrsOperator.hpp"
 
 #include "MueLu_MatrixFactory.hpp"
 #include "MueLu_MatrixTypes.hpp"
 #include <iostream>
 
-namespace MueLu_UnitTest {
+#include "MueLu_Exceptions.hpp"
+
+#include "MueLu_UseDefaultTypes.hpp"
+#include "MueLu_UseShortNames.hpp"
+
+namespace MueLu {
+namespace UnitTest {
+
 
   using Cthulhu::global_size_t;
   using Teuchos::RCP;
@@ -49,6 +58,51 @@ namespace MueLu_UnitTest {
   } // create_map()
 #endif
 
-} // namespace MueLu_UnitTest
+  //create a matrix as specified by parameter list options
+  template<class SC, class LO,class GO>
+  RCP<CrsOperator> create_test_matrix(Teuchos::ParameterList &matrixList)
+  {
+    RCP<const Teuchos::Comm<int> > comm = getDefaultComm();
+
+    GO nx,ny,nz;
+    nx = ny = nz = 5;
+    nx = matrixList.get("nx",nx);
+    ny = matrixList.get("ny",ny);
+    nz = matrixList.get("nz",nz);
+
+    std::string matrixType = matrixList.get("matrixType","Laplace1D");
+    GO numGlobalElements;
+    if (matrixType == "Laplace1D")
+      numGlobalElements = nx;
+    else if (matrixType == "Laplace2D")
+      numGlobalElements = nx*ny;
+    else if (matrixType == "Laplace3D")
+      numGlobalElements = nx*ny*nz;
+    else {
+      std::string msg = matrixType + " is unsupported (in unit testing)";
+      throw(MueLu::Exceptions::RuntimeError(msg));
+    }
+    LO indexBase = 0;
+    RCP<const Map > map;
+    map = rcp( new Cthulhu::EpetraMap(numGlobalElements, indexBase, comm) );
+
+    RCP<CrsOperator> Op = MueLu::Gallery::CreateCrsMatrix<SC,LO,GO, Map,
+                                                    CrsOperator>(matrixType,map,matrixList);
+    return Op;
+  } //create_test_matrix
+
+  //create a 1D Poisson matrix with the specified number of rows
+  template<class SC, class LO, class GO>
+  RCP<CrsOperator> create_1d_poisson_matrix(GO numRows)
+  {
+    Teuchos::ParameterList matrixList;
+    matrixList.set("nx",numRows);
+    matrixList.set("matrixType","Laplace1D");
+    RCP<CrsOperator> A = create_test_matrix<SC,LO,GO>(matrixList);
+    return A;
+  }
+
+} // namespace UnitTest
+} // namespace MueLu
 
 #endif // ifndef MUELU_UNITTEST_HELPERS_H
