b4e18b8962bf628d19eb9e67dde0495e957586d1
James M. Willenbring <jmwille@sandia.gov>
2011-02-02 00:02:52 -0700

    Added a few files for PGI testing.
    These builds are not run nightly yet.  One build uses PGI 10.1,
    the other uses PGI 11.1.

---
+++ cmake/ctest/drivers/trilinos-test/ctest_linux_nightly_serial_development_opt_pgi_11.1_trilinos-test.cmake
@@ -0,0 +1,23 @@
+
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.trilinos-test.pgi11.cmake")
+
+#
+# Set the options specific to this build case
+#
+
+SET(COMM_TYPE SERIAL)
+SET(BUILD_TYPE RELEASE)
+SET(BUILD_DIR_NAME SERIAL_OPT_DEV2)
+#SET(CTEST_TEST_TIMEOUT 900)
+
+SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DTrilinos_DATA_DIR:STRING=$ENV{TRILINOSDATADIRECTORY}"
+  )
+
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
+
+TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
