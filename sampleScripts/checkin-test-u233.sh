56228718238e65c49f3fb222a6dd1fac2139692c
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-02-16 17:56:41 -0500

    Adding checkin-test.py driver script for u233 machine.

---
+++ sampleScripts/checkin-test-u233.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Used to test Trilinos on u233.ornl.gov
+
+EXTRA_ARGS=$@
+
+echo "
+-DBUILD_SHARED_LIBS:BOOL=ON
+-DTPL_BLAS_LIBRARIES=/usr/lib64/libblas.so.3
+-DTPL_LAPACK_LIBRARIES=/usr/lib64/liblapack.so.3
+" > COMMON.config
+
+../../Trilinos/checkin-test.py \
+-j12 \
+--ctest-timeout=180 \
+$EXTRA_ARGS  
