b522fd3cfe9eb94b472df1f16f69076b6fc2f743
Roger Pawlowski <rppawlo@sandia.gov>
2011-02-18 06:13:12 -0700

    Added better testing for equation set construction
    updated kanban for a few tasks

--- packages/panzer/test/equation_set/equation_set.cpp
+++ packages/panzer/test/equation_set/equation_set.cpp
@@ -9,7 +9,7 @@
 
 namespace panzer {
 
-  TEUCHOS_UNIT_TEST(equation_set, basic)
+  TEUCHOS_UNIT_TEST(equation_set, steady_state)
   {
     panzer::InputEquationSet ies;
     ies.name = "Energy";
@@ -27,9 +27,26 @@ namespace panzer {
   
     user_app::MyFactory my_factory;
     eq_set = my_factory.buildEquationSet(ies, cell_data, false);
+  }
 
+  TEUCHOS_UNIT_TEST(equation_set, transient)
+  {
+    panzer::InputEquationSet ies;
+    ies.name = "Energy";
+    ies.basis = "Q2";
+    ies.integration_order = 1;
+    ies.model_id = 6;
+    ies.model_factory = "rf";
+    ies.prefix = "ION_";
     
+    int num_cells = 20;
+    int cell_dim = 2;
+    panzer::CellData cell_data(num_cells, cell_dim);
 
+    Teuchos::RCP<panzer::EquationSet_TemplateManager<panzer::Traits> > eq_set;
+  
+    user_app::MyFactory my_factory;
+    eq_set = my_factory.buildEquationSet(ies, cell_data, true);
   }
 
 }
