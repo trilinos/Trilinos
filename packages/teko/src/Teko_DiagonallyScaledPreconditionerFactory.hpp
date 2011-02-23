6c702929e56fe8faef607d56116710ccef24b8ee
Eric C. Cyr <eccyr@sandia.gov>
2011-02-23 11:22:25 -0700

    Small updates to the diagonally scaled preconditioner fixing default conditions

--- packages/teko/src/Teko_DiagonallyScaledPreconditionerFactory.hpp
+++ packages/teko/src/Teko_DiagonallyScaledPreconditionerFactory.hpp
@@ -59,8 +59,8 @@ namespace Teko {
   *        the explicity scaled matrix. The inverse of the
   *        scaling operation is automatically applied
   *
-  * The current behavior for this operator on an operator
-  * \f$A\f$.  Is to compute the absolute row sum
+  * The current behavior for this factory on an operator
+  * \f$A\f$ is to compute the absolute row sum
   * of \f$A\f$ and define the scaling matrix \f$D\f$. Then
   * explicitly right muliply by \f$D^{-1}\f$ (\f$A\f$ is not
   * modified, and invert the linear system \f$ A D^{-1}\f$
@@ -76,6 +76,8 @@ namespace Teko {
   \code
      <Parameter name="Type" type="string" value="Diagonal Scaling"/>
      <Parameter name="Inverse Factory" type="string" value="<Some Inverse Factory>"/>
+     <Parameter name="Scaling Type" type="string" value="<Row,Column>"/> <!-- Row is default -->
+     <Parameter name="Diagonal Type" type="string" value="<AbsRowSum,Diagonal,Lumped>"/> <!-- AbsRowSum is default -->
   \endcode
   */
 class DiagonallyScaledPreconditionerFactory 
@@ -90,7 +92,7 @@ public:
      *
      * \param[in] invFactory Factory to perform the inverse
      */
-   DiagonallyScaledPreconditionerFactory(const Teuchos::RCP<Teko::InverseFactory> & invFactory,ScalingType columnScaling=COLUMN_SCALING);
+   DiagonallyScaledPreconditionerFactory(const Teuchos::RCP<Teko::InverseFactory> & invFactory,ScalingType columnScaling=COLUMN_SCALING,DiagonalType diagonalType=AbsRowSum);
 
    //! default destructor: prints out diagnostic string
    virtual ~DiagonallyScaledPreconditionerFactory();
@@ -124,39 +126,13 @@ public:
      * \param[in] settings Parmaeter list to use as the internal settings
      */
    virtual void initializeFromParameterList(const Teuchos::ParameterList & settings);
-
-   /** \brief Request the additional parameters this preconditioner factory
-     *        needs. 
-     *
-     * Request the additonal parameters needed by this preconditioner factory.
-     * The parameter list will have a set of fields that can be filled with 
-     * the requested values. These fields include all requirements, even those
-     * of the sub-solvers if there are any.  Once correctly filled the object
-     * can be updated by calling the updateRequestedParameters with the filled
-     * parameter list.
-     *
-     * \returns A parameter list with the requested parameters.
-     */
-   virtual Teuchos::RCP<Teuchos::ParameterList> getRequestedParameters() const;
-   
-   /** \brief Update this object with the fields from a parameter list.
-     *
-     * Update the requested fields using a parameter list. This method is
-     * expected to pair with the getRequestedParameters method (i.e. the fields
-     * requested are going to be update using this method).
-     *
-     * \param[in] pl Parameter list containing the requested parameters.
-     *
-     * \returns If the method succeeded (found all its required parameters) this
-     *          method returns true, otherwise it returns false.
-     */
-   virtual bool updateRequestedParameters(const Teuchos::ParameterList & pl);
    
    //@}
 
 private:
    Teuchos::RCP<Teko::InverseFactory> invFactory_;
    ScalingType scalingType_;
+   DiagonalType diagonalType_;
 };
 
 } // end namespace Teko
