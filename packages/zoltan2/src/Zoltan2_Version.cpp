a02b2e72430455edb099c0d564b0ddd13dd0c73a
Lee Ann Riesen <lriesen@sandia.gov>
2011-03-09 15:21:32 -0700

    version file
    I needed a .cpp file in the library in order to compile.
    There's probably a way to build a test with a library that
    is all in headers, but I didn't figure out how.

---
+++ packages/zoltan2/src/Zoltan2_Version.cpp
@@ -0,0 +1,19 @@
+//@HEADER
+// ************************************************************************
+//         copyright
+// ************************************************************************
+//@HEADER
+
+#include <string>
+
+/*! \file Zoltan2_Version.cpp
+    \brief Simple function for returning the current version number
+*/
+
+namespace Zoltan2 {
+
+	std::string Zoltan2_Version() { 
+		return("Zoltan2 Version 0.5 - 9/31/2011"); 
+	}
+
+} // namespace Zoltan2 
