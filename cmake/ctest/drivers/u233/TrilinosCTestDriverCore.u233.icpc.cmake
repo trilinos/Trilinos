38211ca36c9afca0c376eca2250a3d8f5a20fc75
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-03-18 14:51:25 -0400

    Setting up to run Trilinos builds with Intel to submit to main Trilinos CDash server.

--- cmake/ctest/drivers/u233/TrilinosCTestDriverCore.u233.icpc.cmake
+++ cmake/ctest/drivers/u233/TrilinosCTestDriverCore.u233.icpc.cmake
@@ -7,10 +7,6 @@ INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../TrilinosCTestDriverCore.cmake")
 
 MACRO(TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER)
 
-  # Point to a different CDash dashboard
-  SET(CTEST_DROP_SITE "casl-dev.ornl.gov")
-  SET(CTEST_DROP_LOCATION "/CDash/submit.php?project=Trilinos")
-
   # Base of Trilinos/cmake/ctest then BUILD_DIR_NAME
   SET( CTEST_DASHBOARD_ROOT "${TRILINOS_CMAKE_DIR}/../../${BUILD_DIR_NAME}" )
 
@@ -20,8 +16,6 @@ MACRO(TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER)
 
   #SET( CTEST_MEMORYCHECK_COMMAND /usr/bin/valgrind )
   #SET( CTEST_MEMORYCHECK_COMMAND_OPTIONS )
-
-  SET_DEFAULT( Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
 
   # Turn off SS packages we don't have TPLs for
   SET_DEFAULT( Trilinos_EXCLUDE_PACKAGES PyTrilinos TriKota Optika)
