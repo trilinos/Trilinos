f6e3e780b9c6e0545981dc998a7231fe5ccc3168
James M. Willenbring <jmwille@sandia.gov>
2011-03-23 13:58:03 -0600

    Adding a nightly script for Windows MPI

---
+++ cmake/ctest/drivers/kallikrates/ctest_windows_nightly_mpi_development.cmake
@@ -0,0 +1,23 @@
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.kallikrates.msvc.cmake")
+
+#
+# Set the options specific to this build case
+#
+#SET(CTEST_DO_UPDATES FALSE)
+SET(COMM_TYPE MPI)
+SET(BUILD_TYPE RELEASE)
+SET(BUILD_DIR_NAME MPI_OPT_DEV)
+SET(CTEST_TEST_TYPE EXPERIMENTAL)
+#SET(CTEST_TEST_TIMEOUT 900)
+
+SET( EXTRA_EXCLUDE_PACKAGES TrilinosFramework Amesos Ifpack ML Moertel Stokhos Piro Optika Mesquite FEApp)
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DTrilinos_ENABLE_KNOWN_EXTERNAL_REPOS_TYPE:STRING=Experimental"
+  )
+
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
+
+TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
