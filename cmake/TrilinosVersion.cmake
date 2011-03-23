99b3a0847d3036cb34261ec601826117010f6f32
Brent Perschbacher <bmpersc@sandia.gov>
2011-03-23 10:00:37 -0600

    Changes for 10.6.4 release update

--- cmake/TrilinosVersion.cmake
+++ cmake/TrilinosVersion.cmake
@@ -1,7 +1,7 @@
-SET(Trilinos_VERSION "10.6.3")
+SET(Trilinos_VERSION "10.6.4")
 SET(Trilinos_MAJOR_VERSION "10")
-SET(Trilinos_MAJOR_MINOR_VERSION "100603")
-SET(Trilinos_VERSION_STRING "10.6.3")
+SET(Trilinos_MAJOR_MINOR_VERSION "100604")
+SET(Trilinos_VERSION_STRING "10.6.4")
 
 #this is for our testing scripts and should not be used elsewhere
 SET(Trilinos_REPOSITORY_BRANCH "trilinos-release-10-6-branch" CACHE INTERNAL "")
