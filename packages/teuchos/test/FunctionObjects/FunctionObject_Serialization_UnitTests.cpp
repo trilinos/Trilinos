9e47284fb569d1cbbea2365581534e3dab6f6fa1
Kurtis Nusbaum <klnusbaum@gmail.com>
2011-04-03 17:27:12 -0500

    Added all basic testing for function object serialization.
    -Kurtis

--- packages/teuchos/test/FunctionObjects/FunctionObject_Serialization_UnitTests.cpp
+++ packages/teuchos/test/FunctionObjects/FunctionObject_Serialization_UnitTests.cpp
@@ -62,6 +62,81 @@ TEUCHOS_UNIT_TEST(Teuchos_Dependencies, SubtractionTests){
     intTester->getModifiyingOperand());
 }
 
+TEUCHOS_UNIT_TEST(Teuchos_Dependencies, AdditionTests){
+  RCP<AdditionFunction<int> > intTester = rcp(
+    new AdditionFunction<int>(10));
+
+  XMLObject addFuncXML = FunctionObjectXMLConverterDB::convertFunctionObject(
+    intTester);
+
+  std::string type = addFuncXML.getRequired(
+    FunctionObjectXMLConverter::getTypeAttributeName());
+  TEST_ASSERT(type == intTester->getTypeAttributeValue() );
+  int operand = addFuncXML.getRequired<int>(
+    SimpleFunctionXMLConverter<int>::getOperandAttributeName());
+  TEST_ASSERT(operand == intTester->getModifiyingOperand());
+
+  RCP<FunctionObject> readIn = 
+    FunctionObjectXMLConverterDB::convertXML(addFuncXML);
+  RCP<AdditionFunction<int> > readInCasted = 
+    rcp_dynamic_cast<AdditionFunction<int> >(readIn);
+  TEST_ASSERT(readInCasted.get() != NULL);
+  TEST_ASSERT(
+    readInCasted->getModifiyingOperand()
+    ==
+    intTester->getModifiyingOperand());
+}
+
+TEUCHOS_UNIT_TEST(Teuchos_Dependencies, MultiplicationTests){
+  RCP<MultiplicationFunction<int> > intTester = rcp(
+    new MultiplicationFunction<int>(10));
+
+  XMLObject multiFuncXML = FunctionObjectXMLConverterDB::convertFunctionObject(
+    intTester);
+
+  std::string type = multiFuncXML.getRequired(
+    FunctionObjectXMLConverter::getTypeAttributeName());
+  TEST_ASSERT(type == intTester->getTypeAttributeValue() );
+  int operand = multiFuncXML.getRequired<int>(
+    SimpleFunctionXMLConverter<int>::getOperandAttributeName());
+  TEST_ASSERT(operand == intTester->getModifiyingOperand());
+
+  RCP<FunctionObject> readIn = 
+    FunctionObjectXMLConverterDB::convertXML(multiFuncXML);
+  RCP<MultiplicationFunction<int> > readInCasted = 
+    rcp_dynamic_cast<MultiplicationFunction<int> >(readIn);
+  TEST_ASSERT(readInCasted.get() != NULL);
+  TEST_ASSERT(
+    readInCasted->getModifiyingOperand()
+    ==
+    intTester->getModifiyingOperand());
+}
+
+TEUCHOS_UNIT_TEST(Teuchos_Dependencies, DivisionTests){
+  RCP<DivisionFunction<int> > intTester = rcp(
+    new DivisionFunction<int>(10));
+
+  XMLObject divisFuncXML = FunctionObjectXMLConverterDB::convertFunctionObject(
+    intTester);
+
+  std::string type = divisFuncXML.getRequired(
+    FunctionObjectXMLConverter::getTypeAttributeName());
+  TEST_ASSERT(type == intTester->getTypeAttributeValue() );
+  int operand = divisFuncXML.getRequired<int>(
+    SimpleFunctionXMLConverter<int>::getOperandAttributeName());
+  TEST_ASSERT(operand == intTester->getModifiyingOperand());
+
+  RCP<FunctionObject> readIn = 
+    FunctionObjectXMLConverterDB::convertXML(divisFuncXML);
+  RCP<DivisionFunction<int> > readInCasted = 
+    rcp_dynamic_cast<DivisionFunction<int> >(readIn);
+  TEST_ASSERT(readInCasted.get() != NULL);
+  TEST_ASSERT(
+    readInCasted->getModifiyingOperand()
+    ==
+    intTester->getModifiyingOperand());
+}
+
 
 
 } //namespace Teuchos
