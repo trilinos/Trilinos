83119291647adf3ed3485780f9ad6d4756d5c488
Christopher G. Baker <bakercg@ornl.gov>
2011-04-06 14:45:28 -0400

    Better cuda support, from native support in cmake Added new package_add_cuda_library macro, simply calls package_add_library macro, with option to use CUDA_ADD_LIBRARY instead of ADD_LIBRARY Modified kokkos, tpetra, stokhos, sacade and phalanx

--- cmake/TPLs/FindTPLCusp.cmake
+++ cmake/TPLs/FindTPLCusp.cmake
@@ -1,5 +1,9 @@
 INCLUDE(TPLDeclareLibraries)
 
-TPL_DECLARE_LIBRARIES( Cusp
-  REQUIRED_HEADERS cusp/version.h
-  )
+IF (NOT TPL_ENABLE_Thrust)
+  MESSAGE(FATAL_ERROR "\nCusp TPL requires that Thrust support is enabled. Please set \n  TPL_ENABLE_Thrust=ON\n\n")
+ELSE()
+  TPL_DECLARE_LIBRARIES( Cusp
+    REQUIRED_HEADERS cusp/version.h
+    )
+ENDIF()
