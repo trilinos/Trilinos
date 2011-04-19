8a01d237fcdffca25e05598e7c249a2af7ac4888
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-04-19 17:01:01 -0600

    Requiring Python 2.4 or newer
    Here I have updated the local Trilinos FindPythonInterp.cmake module to allow
    searching for a given version but allowing no version be found also.  I have
    hard-coded Trilinos to require at least Python 2.4 (which is needed for the
    Trilinos python xml dependency testing).  However, I changed the behavior of
    the script that if you set the minimum version that it will not be enforced
    unless some python is found.
    
    This represents slightly new default behavior for Trilinos.  Before, if any
    Python was found it would just be used with no check of the version and then
    we would see various failures.  Now, if a python is found but is not at least
    the minimum version, python support will be disabled.  I think this is the
    best overall behavior that is appropriately backward compatible.
    
    I did this to get around a problem with the AIX build that finds Python 2.3
    and just used it but a bunch of Trilinos framework tests failed (as would
    generation of *.xml and *.html files who's python scripts require python 2.4).
    
    I just tested this manually.  We should unit test this but we will need to
    mock out some built-in CMake functions (like FIND_EXECUTABLE).  We really need
    a general system for writting CMake mocks to use in unit tests but this will
    take some work.  Note that by not writting proper unit tests for this module I
    have just propogated some technical debt in Trilinos (but not by much since
    the code was not unit tested before anyway).
    
    Build/Test Cases Summary
    Enabled Packages: TrilinosFramework
    Enabled all Forward Packages
    0) MPI_DEBUG => passed: passed=85,notpassed=0 (1.19 min)
    1) SERIAL_RELEASE => passed: passed=85,notpassed=0 (1.14 min)

--- cmake/FindPythonInterp.cmake
+++ cmake/FindPythonInterp.cmake
@@ -8,6 +8,15 @@
 #  PYTHONINTERP_FOUND - Was the Python executable found
 #  PYTHON_EXECUTABLE  - path to the Python interpreter
 #
+# This module also responds to the following CMake cache varibles:
+#
+#  PythonInterp_FIND_VERSION - Requires that a given version be found
+#    if some Python executable is found (e.g. '2.4').  Note that this
+#    results in the most recent Python version that that is supported
+#    on the system, not the minimum version!
+#  PythonInterp_MUST_BE_FOUND - If set to TRUE, then the python executable
+#    must be found or the CMake configure will stop immediately.
+#
 
 FIND_PROGRAM(PYTHON_EXECUTABLE
   NAMES python2.7 python2.6 python2.5 python2.4 python2.3 python2.2 python2.1 python2.0 python1.6 python1.5 python
@@ -35,18 +44,29 @@ MARK_AS_ADVANCED(PYTHON_EXECUTABLE)
 # Version checking: If a version check is requested, set
 # PythonInterp_VERSION, convert it to a list, extract the last element
 # of the list, and compare it to the requested version
-IF(PythonInterp_FIND_VERSION)
-  EXECUTE_PROCESS(COMMAND
-    ${PYTHON_EXECUTABLE} "-V"
-    ERROR_VARIABLE PythonInterp_VERSION
-    ERROR_STRIP_TRAILING_WHITESPACE
-    )
-  SEPARATE_ARGUMENTS(PythonInterp_VERSION)
-  LIST(GET PythonInterp_VERSION -1 PythonInterp_VERSION)
-  IF(${PythonInterp_VERSION} VERSION_LESS ${PythonInterp_FIND_VERSION})
-    MESSAGE(FATAL_ERROR
-      "Python version " ${PythonInterp_VERSION}
-      " is less than required version " ${PythonInterp_FIND_VERSION}
+#MESSAGE("PYTHON_EXECUTABLE = ${PYTHON_EXECUTABLE}")
+#MESSAGE("PythonInterp_MUST_BE_FOUND = ${PythonInterp_MUST_BE_FOUND}")
+IF (PYTHON_EXECUTABLE OR PythonInterp_MUST_BE_FOUND)
+  IF (NOT PYTHON_EXECUTABLE)
+    MESSAGE(FATAL_ERROR "Error, Python must be found!")
+  ENDIF()
+  IF (PythonInterp_FIND_VERSION)
+    EXECUTE_PROCESS(COMMAND
+      ${PYTHON_EXECUTABLE} "-V"
+      ERROR_VARIABLE PythonInterp_VERSION
+      ERROR_STRIP_TRAILING_WHITESPACE
       )
-  ENDIF(${PythonInterp_VERSION} VERSION_LESS ${PythonInterp_FIND_VERSION})
-ENDIF(PythonInterp_FIND_VERSION)
+    SEPARATE_ARGUMENTS(PythonInterp_VERSION)
+    LIST(GET PythonInterp_VERSION -1 PythonInterp_VERSION)
+    IF(${PythonInterp_VERSION} VERSION_LESS ${PythonInterp_FIND_VERSION})
+      MESSAGE(WARNING
+        "Python version ${PythonInterp_VERSION}"
+        " is less than required version ${PythonInterp_FIND_VERSION}!"
+        "  Disabling Python!"
+        )
+      SET(PYTHONINTERP_FOUND FALSE)
+      UNSET(PYTHON_EXECUTABLE)
+      UNSET(PYTHON_EXECUTABLE CACHE)
+    ENDIF()
+  ENDIF()
+ENDIF()
