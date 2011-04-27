216b9651f5097132ab94b5d821aa17b063065d51
Jeremie Gaidamour <jngaida@sandia.gov>
2011-04-27 14:57:31 -0600

    MueLu UnitTest Amesos: skiped for Tpetra +clean up

--- packages/muelu/test/unit_tests/Amesos.cpp
+++ packages/muelu/test/unit_tests/Amesos.cpp
@@ -1,11 +1,6 @@
 #include "Teuchos_UnitTestHarness.hpp"
-#include "Cthulhu_DefaultPlatform.hpp"
-#include "MueLu_Version.hpp"
 #include "MueLu_TestHelpers.hpp"
-
-#include <Cthulhu_Map.hpp>
-#include <Cthulhu_CrsOperator.hpp>
-#include <Cthulhu_MultiVectorFactory.hpp>
+#include "MueLu_Version.hpp"
 
 #include "MueLu_Level.hpp"
 #include "MueLu_SmootherFactory.hpp"
@@ -17,90 +12,72 @@
 
 namespace {
 
-bool testMpi = true;
-
-Teuchos::RCP<const Teuchos::Comm<int> > getDefaultComm()
-{
-  Teuchos::RCP<const Teuchos::Comm<int> > ret;
-  if (testMpi) {
-    ret = Cthulhu::DefaultPlatform::getDefaultPlatform().getComm();
-  }
-  else {
-    ret = Teuchos::rcp(new Teuchos::SerialComm<int>());
-  }
-  return ret;
-}
-
-//this macro declares the unit-test-class:
-TEUCHOS_UNIT_TEST(Amesos, NotSetup)
-{
-//we are now in a class method declared by the above macro, and
-//that method has these input arguments:
-//Teuchos::FancyOStream& out, bool& success
-
   using Teuchos::RCP;
   using Teuchos::rcp;
+  using namespace MueLu::TestHelpers;
 
-  out << "version: " << MueLu::Version() << std::endl;
+  //this macro declares the unit-test-class:
+  TEUCHOS_UNIT_TEST(Amesos, NotSetup)
+  {
+    //we are now in a class method declared by the above macro, and
+    //that method has these input arguments:
+    //Teuchos::FancyOStream& out, bool& success
 
-  RCP<const Teuchos::Comm<int> > comm = getDefaultComm();
+    MUELU_TEST_ONLY_FOR(Cthulhu::UseEpetra)
+      {
+        out << "version: " << MueLu::Version() << std::endl;
 
-  GO nx,ny,nz;
-  nx = ny = nz = 5;
-  GO numGlobalElements = nx*ny*nz;
-  LO indexBase = 0;
-  RCP<const Map > map;
-  map = rcp( new Cthulhu::EpetraMap(numGlobalElements, indexBase, comm) );
+        GO nx,ny,nz;
+        nx = ny = nz = 5;
+        GO numGlobalElements = nx*ny*nz;
+        RCP<const Map> map = MapFactory::Build(Parameters::getLib(), numGlobalElements, 0, Parameters::getDefaultComm());
 
-  Teuchos::ParameterList  amesosList;
-  RCP<AmesosSmoother>  smoother = rcp( new AmesosSmoother("Amesos_Klu",amesosList) );
+        Teuchos::ParameterList amesosList;
+        RCP<AmesosSmoother>    smoother = rcp( new AmesosSmoother("Amesos_Klu",amesosList) );
 
-  RCP<MultiVector> X = MultiVectorFactory::Build(map,1);
-  RCP<MultiVector> RHS = MultiVectorFactory::Build(map,1);
+        RCP<MultiVector> X   = MultiVectorFactory::Build(map,1);
+        RCP<MultiVector> RHS = MultiVectorFactory::Build(map,1);
 
-  //try applying without setting up
-  TEST_THROW( smoother->Apply(*X,*RHS) , MueLu::Exceptions::RuntimeError );
-  TEST_THROW( smoother->SetNIts(5), MueLu::Exceptions::RuntimeError );
+        //try applying without setting up
+        TEST_THROW( smoother->Apply(*X,*RHS), MueLu::Exceptions::RuntimeError );
+        TEST_THROW( smoother->SetNIts(5),     MueLu::Exceptions::RuntimeError ); //JG: Why ?
+      }
+  }
 
-}
+  TEUCHOS_UNIT_TEST(Amesos, KLUSolve)
+  {
+    MUELU_TEST_ONLY_FOR(Cthulhu::UseEpetra)
+      {
+        out << "version: " << MueLu::Version() << std::endl;
 
-TEUCHOS_UNIT_TEST(Amesos, KLUSolve)
-{
-  using Teuchos::RCP;
-  using Teuchos::rcp;
+        Teuchos::ParameterList amesosList;
+        amesosList.set("PrintTiming",false);
+        amesosList.set("OutputLevel",0);
+        RCP<AmesosSmoother> smoother = rcp( new AmesosSmoother("Amesos_Klu",amesosList) );
 
-  out << "version: " << MueLu::Version() << std::endl;
+        RCP<Operator> Op = MueLu::TestHelpers::Factory<SC,LO,GO,NO,LMO>::Build1DPoisson(125);
+        Level aLevel;
+        aLevel.SetA(Op);
 
-  RCP<const Teuchos::Comm<int> > comm = getDefaultComm();
+        smoother->Setup(aLevel);
 
-  RCP<Operator> Op = MueLu::TestHelpers::Factory<SC,LO,GO,NO,LMO>::Build1DPoisson(125);
+        RCP<MultiVector> X   = MultiVectorFactory::Build(Op->getDomainMap(),1);
+        RCP<MultiVector> RHS = MultiVectorFactory::Build(Op->getDomainMap(),1);
 
-  Teuchos::ParameterList amesosList;
-  amesosList.set("PrintTiming",false);
-  amesosList.set("OutputLevel",0);
-  RCP<AmesosSmoother>  smoother = rcp( new AmesosSmoother("Amesos_Klu",amesosList) );
-  Level aLevel;
-  aLevel.SetA(Op);
+        X->setSeed(846930886);
+        X->randomize();
+        Op->multiply(*X,*RHS,Teuchos::NO_TRANS,(SC)1.0,(SC)0.0);
 
-  RCP<MultiVector> X = MultiVectorFactory::Build(Op->getDomainMap(),1);
-  RCP<MultiVector> RHS = MultiVectorFactory::Build(Op->getDomainMap(),1);
+        X->putScalar((SC) 0.0);
+        Teuchos::Array<ST::magnitudeType> res;
+        res = Utils::ResidualNorm(*Op,*X,*RHS);
+        out << "||initial residual|| = " << res[0] << std::endl;
 
-  smoother->Setup(aLevel);
-
-  X->setSeed(846930886);
-  X->randomize();
-  Op->multiply(*X,*RHS,Teuchos::NO_TRANS,(SC)1.0,(SC)0.0);
-
-  X->putScalar((SC) 0.0);
-  Teuchos::Array<ST::magnitudeType> res;
-  res = Utils::ResidualNorm(*Op,*X,*RHS);
-  out << "||initial residual|| = " << res[0] << std::endl;
-
-  smoother->Apply(*X,*RHS);
-  res = Utils::ResidualNorm(*Op,*X,*RHS);
-  out << "||final residual|| = " << res[0] << std::endl;
-  TEUCHOS_TEST_EQUALITY(res[0] < 1e-12,true,out,success)
-}
-
-}//namespace <anonymous>
-
+        smoother->Apply(*X,*RHS);
+        res = Utils::ResidualNorm(*Op,*X,*RHS);
+        out << "||final residual|| = " << res[0] << std::endl;
+        TEUCHOS_TEST_EQUALITY(res[0] < 1e-12,true,out,success);
+      }
+  }
+  
+} // namespace <anonymous>
