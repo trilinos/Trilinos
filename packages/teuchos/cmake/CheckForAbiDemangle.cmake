0a6ead166d911845f6cbf2888358806d334cd1f4
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-05-05 17:59:40 -0600

    Minor format change.

--- packages/teuchos/cmake/CheckForAbiDemangle.cmake
+++ packages/teuchos/cmake/CheckForAbiDemangle.cmake
@@ -6,27 +6,27 @@ FUNCTION(CHECK_FOR_ABI_DEMANGLE VARNAME)
 
   SET(SOURCE
   "
-  #include <cxxabi.h>
-  #include <string>
-  #include <cstdlib>
-  namespace MyNamespace {
-    class MyClass {};
-  }
-  
-  int main()
-  {
-    const std::string
-      mangledName = typeid(MyNamespace::MyClass).name();
-    int status;
-    char
-      *_demangledName = abi::__cxa_demangle(mangledName.c_str(),0,0,&status);
-    const std::string demangledName(_demangledName);
-    std::free(_demangledName);
-    return ( demangledName == \"MyNamespace::MyClass\" ? 0 : 1 );
-  }
-  "
+#include <cxxabi.h>
+#include <string>
+#include <cstdlib>
+namespace MyNamespace {
+  class MyClass {};
+}
+
+int main()
+{
+  const std::string
+    mangledName = typeid(MyNamespace::MyClass).name();
+  int status;
+  char *_demangledName = abi::__cxa_demangle(mangledName.c_str(), 0, 0, &status);
+  const std::string demangledName(_demangledName);
+  std::free(_demangledName);
+  return ( demangledName == \"MyNamespace::MyClass\" ? 0 : 1 );
+}
+"
   )
   
+  #SET(CMAKE_REQUIRED_LIBRARIES ${${PROJECT_NAME}_EXTRA_LINK_FLAGS})
   CHECK_CXX_SOURCE_RUNS("${SOURCE}" ${VARNAME})
   
 ENDFUNCTION()
