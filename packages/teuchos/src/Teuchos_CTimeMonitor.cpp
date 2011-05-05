1e944512b8e6120d1a5c8ca4ca9c39f3fdcd553b
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-05-05 14:50:58 -0600

    Adding catches to remove warnings from MS Visual C++ compiler

--- packages/teuchos/src/Teuchos_CTimeMonitor.cpp
+++ packages/teuchos/src/Teuchos_CTimeMonitor.cpp
@@ -29,11 +29,16 @@
 #include "Teuchos_CTimeMonitor.h"
 #include "Teuchos_TimeMonitor.hpp"
 #include "Teuchos_implicit_cast.hpp"
+#include "Teuchos_StandardCatchMacros.hpp"
+#include "Teuchos_VerboseObject.hpp"
+
 
 namespace {
 
+
 typedef Teuchos::Array< Teuchos::RCP<Teuchos::Time> >  TimerArray_t;
 TimerArray_t timerArray;
+
 
 } // namespace
 
@@ -41,26 +46,34 @@ TimerArray_t timerArray;
 int Teuchos_startTimer( char timerName[], int timerID )
 {
   using Teuchos::implicit_cast;
-  if( timerID < 0 ) {
-    // The timer does not exist so create it!
-    timerArray.push_back(Teuchos::TimeMonitor::getNewTimer(timerName));
-    timerArray.back()->start();
-    return timerArray.size()-1;
+  bool success = true;
+  try {
+    if( timerID < 0 ) {
+      // The timer does not exist so create it!
+      timerArray.push_back(Teuchos::TimeMonitor::getNewTimer(timerName));
+      timerArray.back()->start();
+      return timerArray.size()-1;
+    }
+    // Else, the timer already exists so return it
+    TEST_FOR_EXCEPTION(
+      timerID >=  implicit_cast<int>(timerArray.size()), std::logic_error,
+      "Teuchos_startTimer(...): Error, timerID="<<timerID
+      <<" is >= timerArray.size()="<<timerArray.size()
+      <<" for timerName=\""<<timerName<<"\"!"
+      );
+    Teuchos::RCP<Teuchos::Time> timer = timerArray[timerID];
+    TEST_FOR_EXCEPTION(
+      timer->isRunning(), std::logic_error,
+      "Teuchos_startTimer(...): Error, timerID="<<timerID
+      <<", timerName=\""<<timerName<<"\" is already running!"
+      );
+    timer->start();
   }
-  // Else, the timer already exists so return it
-  TEST_FOR_EXCEPTION(
-    timerID >=  implicit_cast<int>(timerArray.size()), std::logic_error,
-    "Teuchos_startTimer(...): Error, timerID="<<timerID
-    <<" is >= timerArray.size()="<<timerArray.size()
-    <<" for timerName=\""<<timerName<<"\"!"
-    );
-  Teuchos::RCP<Teuchos::Time> timer = timerArray[timerID];
-  TEST_FOR_EXCEPTION(
-    timer->isRunning(), std::logic_error,
-    "Teuchos_startTimer(...): Error, timerID="<<timerID
-    <<", timerName=\""<<timerName<<"\" is already running!"
-    );
-  timer->start();
+  TEUCHOS_STANDARD_CATCH_STATEMENTS(true,
+    *Teuchos::VerboseObjectBase::getDefaultOStream(), success);
+  if (!success) {
+    return -1;
+  }
   return timerID;
 }
 
@@ -68,13 +81,18 @@ int Teuchos_startTimer( char timerName[], int timerID )
 void Teuchos_stopTimer( int timerID )
 {
   using Teuchos::implicit_cast;
-  TEST_FOR_EXCEPTION(
-    timerID < 0 || timerID >= implicit_cast<int>(timerArray.size()),
-    std::logic_error,
-    "Teuchos_stopTimer(...): Error, timerID="<<timerID<<" is invalid!"
-    );
-  Teuchos::RCP<Teuchos::Time> timer = timerArray[timerID];
-  timer->stop();
-  // Increment the number of times the timer has been used (start to stop).
-  timer->incrementNumCalls();
+  bool success = true;
+  try {
+    TEST_FOR_EXCEPTION(
+      timerID < 0 || timerID >= implicit_cast<int>(timerArray.size()),
+      std::logic_error,
+      "Teuchos_stopTimer(...): Error, timerID="<<timerID<<" is invalid!"
+      );
+    Teuchos::RCP<Teuchos::Time> timer = timerArray[timerID];
+    timer->stop();
+    // Increment the number of times the timer has been used (start to stop).
+    timer->incrementNumCalls();
+  }
+  TEUCHOS_STANDARD_CATCH_STATEMENTS(true,
+    *Teuchos::VerboseObjectBase::getDefaultOStream(), success);
 }
