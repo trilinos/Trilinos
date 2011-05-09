9c181b5f9ad87b6df6e8da32a4eff7c0ca157315
Brent Perschbacher <bmpersc@sandia.gov>
2011-05-09 16:03:35 -0600

    Revert "Fix issues in Teuchos with gnu 4.6.0 compiler"
    This reverts commit d6ac3b38b365952891e1be9dc6e9fd318376b218.
    This change was committed prematurely. I am backing it out because
    of the errors that have cropped up because of it.

--- packages/teuchos/src/Teuchos_CommHelpers.hpp
+++ packages/teuchos/src/Teuchos_CommHelpers.hpp
@@ -1057,14 +1057,14 @@ void Teuchos::reduceAllAndScatter(
 #ifdef TEUCHOS_DEBUG
   Ordinal sumRecvCounts = 0;
   for( Ordinal i = 0; i < size; ++i )
-    sumRecvCounts += recvCounts[static_cast<std::ptrdiff_t>(i)];
+    sumRecvCounts += recvCounts[static_cast<ptrdiff_t>(i)];
   TEST_FOR_EXCEPT(!(sumRecvCounts==sendCount));
 #endif
 
   ConstValueTypeSerializationBuffer<Ordinal,Packet>
     charSendBuffer(sendCount,sendBuffer);
   ValueTypeSerializationBuffer<Ordinal,Packet>
-    charMyGlobalReducts(recvCounts[static_cast<std::ptrdiff_t>(rank)], myGlobalReducts);
+    charMyGlobalReducts(recvCounts[static_cast<ptrdiff_t>(rank)], myGlobalReducts);
   CharToValueTypeReductionOp<Ordinal,Packet>
     charReductOp(rcp(&reductOp,false));
 
@@ -1074,7 +1074,7 @@ void Teuchos::reduceAllAndScatter(
   WorkspaceStore* wss = get_default_workspace_store().get();
   Workspace<Ordinal> charRecvCounts(wss, size);
   for (Ordinal k = 0; k < size; ++k) {
-    charRecvCounts[k] = as<Ordinal>(recvCounts[static_cast<std::ptrdiff_t>(k)] * packetSize);
+    charRecvCounts[k] = as<Ordinal>(recvCounts[static_cast<ptrdiff_t>(k)] * packetSize);
   }
  
   comm.reduceAllAndScatter(
