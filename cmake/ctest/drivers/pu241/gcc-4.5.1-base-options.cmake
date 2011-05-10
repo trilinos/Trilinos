520aa71935049499a87ba1e4522fbc3b1d932586
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-05-10 13:54:41 -0400

    Adding GCC 4.5.1 builds
    These are not fully tested but I need to push these changes to make testing easier.
    
    Build/Test Cases Summary
    Enabled Packages: TrilinosFramework, Teuchos
    0) MPI_DEBUG => passed: passed=158,notpassed=0 (1.20 min)
    1) SERIAL_RELEASE => passed: passed=155,notpassed=0 (1.03 min)
    Other local commits for this build/test group: 39ac176, b746c8f, 1c4002c, 5bf521a, 2a8618b, 845ddf7

---
+++ cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
@@ -0,0 +1,5 @@
+SET(TRILINOS_TOOLSET_BASE /opt/gcc-4.5.1/trilinos-toolset)
+
+SET(Trilinos_EXTRA_LINK_FLAGS "-Wl,-rpath,${TRILINOS_TOOLSET_BASE}/lib64" CACHE STRING "")
+SET(TPL_BLAS_LIBRARIES "/usr/lib64/libblas.so.3" CACHE FILEPATH "")
+SET(TPL_LAPACK_LIBRARIES "/usr/lib64/liblapack.so.3" CACHE FILEPATH "")
