a8f7e891e4083f36da17af1da57de891443d4c09
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-05-10 19:42:06 -0400

    Setting up to run builds on 8 proces each 4 sets in parallel
    This should make better use of the 32 core CASL fissile 4 machines.

--- cmake/ctest/drivers/pu241/cron_driver_pu241.sh
+++ cmake/ctest/drivers/pu241/cron_driver_pu241.sh
@@ -13,9 +13,10 @@ BASEDIR=/home/casl-vri-admin/Dashboards
 DRIVER_SCRIPT_DIR=$BASEDIR/Trilinos/cmake/ctest/drivers/pu241
 TRILINOS_REPOSITORY_LOCATION="cgbaker@software.sandia.gov:/space/git/Trilinos"
 
-export TDD_PARALLEL_LEVEL=2
+export TDD_PARALLEL_LEVEL=4
 export TDD_CTEST_TEST_TYPE=Experimental
 
+# Submit the outer TDD tests to casl-dev always since these are CASL machines
 export TDD_CTEST_DROP_SITE=casl-dev.ornl.gov
 export TDD_CTEST_DROP_LOCATION="/CDash/submit.php?project=TrilinosDriver"
 
