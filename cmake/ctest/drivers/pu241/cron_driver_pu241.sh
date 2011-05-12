f2d125a28bbb2d3755c6d7fb82942aaf0b2d6b81
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-05-11 23:04:19 -0400

    Setting umask for casl-vri-admin.

--- cmake/ctest/drivers/pu241/cron_driver_pu241.sh
+++ cmake/ctest/drivers/pu241/cron_driver_pu241.sh
@@ -8,6 +8,7 @@ export PATH="${PATH}:/opt/casldev/env"
 eval `vera_dev_env.py load casl`
 # eval `vera_dev_env.py load vera` # sets cdash dropsite to casl-dev.ornl.gov
 eval `vera_dev_env.py load intel/12`
+umask u=rwx,g=rwx,o=
 
 BASEDIR=/home/casl-vri-admin/Dashboards
 DRIVER_SCRIPT_DIR=$BASEDIR/Trilinos/cmake/ctest/drivers/pu241
