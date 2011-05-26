34e50e19033e78cc310cc08293132cf531d5a9af
Christopher G. Baker <bakercg@ornl.gov>
2011-05-25 21:46:02 -0400

    Added CASLRAVE package

--- cmake/ctest/drivers/pu241/ExtraExternalRepositories.casl_vri.cmake
+++ cmake/ctest/drivers/pu241/ExtraExternalRepositories.casl_vri.cmake
@@ -9,7 +9,7 @@ SET( Trilinos_EXTRAREPOS_DIR_GITREPOURL_CATEGORY
   StarCCMExt  casl-dev.ornl.gov:/git-root/StarCCMClient  Continuous
   DeCARTExt  nstdsrv.ornl.gov:/gitroot/casldecart  Continuous
   CASLBOA  nstdsrv.ornl.gov:/gitroot/casl_boa  Continuous
-  # CASLRAVE  nstdsrv.ornl.gov:/gitroot/caslvipre  Continuous
+  CASLRAVE  nstdsrv.ornl.gov:/gitroot/caslvipre  Continuous
   LIMEExt  software.sandia.gov:/space/git/LIMEExt  Continuous
   PSSDriversExt  nstdsrv.ornl.gov:/gitroot/casl_pssdrivers  Continuous
   )
