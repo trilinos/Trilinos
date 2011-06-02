e974fa193dd9322d5b0b3945d25f27f9513412b6
Eric T Bavier <etbavie@sandia.gov>
2011-06-02 10:31:34 -0600

    Modified some of the documentation.  Also reworked some of the functions to optimize for scalar types that map directly to SuperLU types.  Changed the leading dimension to reflect the global vector length in the dense matrix creator, rather than reflecting the local stride.

--- packages/amesos2/src/Amesos2_Superlu_MatrixHelper.hpp
+++ packages/amesos2/src/Amesos2_Superlu_MatrixHelper.hpp
@@ -64,37 +64,37 @@ template <class Matrix, class Vector> class Superlu;
 namespace Amesos {
 
 
-template <>
-struct MatrixHelper<Superlu>
-{
+  template <>
+  struct MatrixHelper<Superlu>
+  {
 
-  /** \brief Creates a Superlu compressed-row Matrix from the given Matrix
-   *
-   * \tparam Matrix A matrix type conforming to the interface, in particular
-   *         an Amesos::MatrixAdapter<>.
-   *
-   * \param [in]     mat    The matrix which will be converted to SuperLU format
-   * \param [in,out] nzval  A user-provided persisting store for the nonzero
-   *                        values of the matrix.  The SuperLU matrix will expect
-   *                        the array to persist throughout its existence.
-   * \param [in,out] colind A user-provided persisting store for the column
-   *                        indices of the matrix.
-   * \param [in,out] rowptr User-provided persisting store for row pointers
-   * \param [out]    A      Pointer to the SuperLU SuperMatrix which is to be constructed
-   * \param [out]    mtxRedistTime Will have additional time added to it for the
-   *                        time to redistribute the \c mat matrix.
-   *
-   * \callgraph
-   */
-  template <class Matrix>
-  static void createCRSMatrix(
-    const Teuchos::Ptr<Matrix>& mat,
-    const Teuchos::ArrayView<typename TypeMap<Superlu,typename Matrix::scalar_type>::type>& nzval,
-    const Teuchos::ArrayView<int>& colind,
-    const Teuchos::ArrayView<int>& rowptr,
-    const Teuchos::Ptr<SLU::SuperMatrix>& A,
-    Teuchos::Time& mtxRedistTime
-    )
+    /** \brief Creates a Superlu compressed-row Matrix from the given Matrix
+     *
+     * \tparam Matrix A matrix type conforming to the interface, in particular
+     *         an Amesos::MatrixAdapter<>.
+     *
+     * \param [in]     mat    The matrix which will be converted to SuperLU format
+     * \param [in,out] nzval  A user-provided persisting store for the nonzero
+     *                        values of the matrix.  The SuperLU matrix will expect
+     *                        the array to persist throughout its existence.
+     * \param [in,out] colind A user-provided persisting store for the column
+     *                        indices of the matrix.
+     * \param [in,out] rowptr User-provided persisting store for row pointers
+     * \param [out]    A      Pointer to the SuperLU SuperMatrix which is to be constructed
+     * \param [out]    mtxRedistTime Will have additional time added to it for the
+     *                        time to redistribute the \c mat matrix.
+     *
+     * \callgraph
+     */
+    template <class Matrix>
+    static void createCRSMatrix(
+                                const Teuchos::Ptr<Matrix>& mat,
+                                const Teuchos::ArrayView<typename TypeMap<Superlu,typename Matrix::scalar_type>::type>& nzval,
+                                const Teuchos::ArrayView<int>& colind,
+                                const Teuchos::ArrayView<int>& rowptr,
+                                const Teuchos::Ptr<SLU::SuperMatrix>& A,
+                                Teuchos::Time& mtxRedistTime
+                                )
     {
       typedef typename Matrix::scalar_type                     scalar_type;
       typedef typename Matrix::global_ordinal_type                 go_type;
@@ -113,14 +113,14 @@ struct MatrixHelper<Superlu>
       cols = Teuchos::as<int>(mat->getGlobalNumCols());
 
       TEST_FOR_EXCEPTION( Teuchos::as<int>(nzval.size()) < nnz,
-        std::runtime_error,
-        "nzval array not large enough to hold data");
+                          std::runtime_error,
+                          "nzval array not large enough to hold data");
       TEST_FOR_EXCEPTION( Teuchos::as<int>(colind.size()) < nnz,
-        std::runtime_error,
-        "colind array not large enough to hold data");
+                          std::runtime_error,
+                          "colind array not large enough to hold data");
       TEST_FOR_EXCEPTION( Teuchos::as<int>(rowptr.size()) < rows + 1,
-        std::runtime_error,
-        "rowptr array not large enough to hold data");
+                          std::runtime_error,
+                          "rowptr array not large enough to hold data");
 
       Array<scalar_type> nzval_tmp(nzval.size());
       Array<go_type>   colind_tmp(colind.size());
@@ -133,20 +133,21 @@ struct MatrixHelper<Superlu>
         Teuchos::TimeMonitor mtxRedistTimer( mtxRedistTime );
 
         mat->getCrs((ArrayView<scalar_type>)nzval_tmp,
-          (ArrayView<go_type>)colind_tmp, (ArrayView<gs_type>)rowptr_tmp, nnz_ret);
+                    (ArrayView<go_type>)colind_tmp, (ArrayView<gs_type>)rowptr_tmp, nnz_ret);
       }
 
       TEST_FOR_EXCEPTION( nnz_ret != Teuchos::as<size_t>(nnz),
-        std::runtime_error,
-        "Number of nonzeros returned by getCrs() different from getGlobalNNZ()");
+                          std::runtime_error,
+                          "Number of nonzeros returned by getCrs() different from getGlobalNNZ()");
 
       /* Convert types
        *
-       * Note: We cannot simply convert when necessary.  That is, we cannot
-       * check whether the matrix types are double, int, and int, and then do
-       * a simple ArrayView::assign if they are the same, since the compiler
-       * still has to do the static linking and cannot find an appropriate
-       * function call in the case that the types are not double, int, and int
+       * Note: We cannot simply convert when necessary.  That is, we
+       * cannot check whether the matrix types are double, int, and
+       * int, and then do a simple ArrayView::assign if they are the
+       * same, since the compiler still has to do the static linking
+       * and cannot find an appropriate function call in the case that
+       * the types are not double, int, and int
        */
       {
         typename Array<scalar_type>::size_type i, size;
@@ -172,34 +173,76 @@ struct MatrixHelper<Superlu>
       // end conversions
 
       FunctionMap<Superlu,scalar_type>::create_CompRow_Matrix(
-        A.getRawPtr(), rows, cols, nnz, nzval.getRawPtr(),
-        colind.getRawPtr(), rowptr.getRawPtr(), SLU::SLU_NR,
-        dtype, SLU::SLU_GE);
+                                                              A.getRawPtr(), rows, cols, nnz, nzval.getRawPtr(),
+                                                              colind.getRawPtr(), rowptr.getRawPtr(), SLU::SLU_NR,
+                                                              dtype, SLU::SLU_GE);
     }
 
 
-  /** \brief Creates a Superlu Dense Matrix from the given MultiVector
-   *
-   * \tparam MV A multi-vector type conforming to the interface, in particular
-   *         an Amesos::MultiVecAdapter<>.
-   *
-   * \param [in]     mv   The MultiVector which will be converted to SuperLU format
-   * \param [in,out] vals A user-provided persisting store for the values of
-   *                      the multi-vector.  The SuperLU matrix will expect the array
-   *                      to persist throughout its existence.
-   * \param [out]    ldx  Leading dimension of \c vals
-   * \param [out]    X    Pointer to the SuperLU Dense SuperMatrix which is to be
-   *                      constructed
-   * \param [out]    vecRedistTime Will have time added for redistribution of \c \mv
-   */
-  template <class MV>
-  static void createMVDenseMatrix(
-    const Teuchos::Ptr<MV>& mv,
-    const Teuchos::ArrayView<typename TypeMap<Superlu,typename MV::scalar_type>::type>& vals,
-    int& ldx,
-    const Teuchos::Ptr<SLU::SuperMatrix>& X,
-    Teuchos::Time& vecRedistTime
-    )
+    /*
+     * If the scalar and SuperLU types are the same, then we can do
+     * a simple straight copy.
+     */
+    template <typename MV>
+    struct same_type_get_copy {
+      static void apply(const Teuchos::Ptr<MV>& mv,
+			const Teuchos::ArrayView<typename MV::scalar_type>& v,
+			const size_t& ldx)
+      {
+	bool get_root_global_copy = true;
+	mv->get1dCopy(v, ldx, get_root_global_copy);
+      }
+    };
+
+    /*
+     * In the case where the scalar type of the multi-vector and the
+     * corresponding SuperLU type are different, then we need to
+     * first get a copy of the scalar values, then convert each one
+     * into the SuperLU type before inserting into the vals array.
+     */
+    template <typename MV>
+    struct diff_type_get_copy {
+      static void apply(const Teuchos::Ptr<MV>& mv,
+			const Teuchos::ArrayView<typename TypeMap<Superlu,typename MV::scalar_type>::type>& v,
+			const size_t& ldx)
+      {
+	typedef typename MV::scalar_type scalar_type;
+	typedef typename TypeMap<Superlu,scalar_type>::type slu_type;
+	  
+	bool get_root_global_copy = true;
+	int vals_length = v.size();
+	Teuchos::Array<scalar_type> vals_tmp(vals_length);
+	  
+	mv->get1dCopy(vals_tmp(), ldx, get_root_global_copy);
+
+	for ( int i = 0; i < vals_length; ++i ){
+	  v[i] = Teuchos::as<slu_type>(vals_tmp[i]);
+	}
+      }
+    };
+
+
+    /** \brief Creates a Superlu Dense Matrix from the given MultiVector
+     *
+     * \tparam MV A multi-vector type conforming to the interface, in particular
+     *         an Amesos::MultiVecAdapter<>.
+     *
+     * \param [in]     mv   The MultiVector which will be converted to SuperLU format
+     * \param [in,out] vals A user-provided persisting store for the values of
+     *                      the multi-vector.  The SuperLU matrix will expect the array
+     *                      to persist throughout its existence.
+     * \param [out]    ldx  Leading dimension of \c vals
+     * \param [out]    X    Pointer to the SuperLU Dense SuperMatrix which is to be
+     *                      constructed
+     * \param [out]    vecRedistTime Will have time added for redistribution of \c \mv
+     */
+    template <class MV>
+    static void createMVDenseMatrix(
+        const Teuchos::Ptr<MV>& mv,
+        const Teuchos::ArrayView<typename TypeMap<Superlu,typename MV::scalar_type>::type>& vals,
+        size_t& ldx,
+        const Teuchos::Ptr<SLU::SuperMatrix>& X,
+        Teuchos::Time& vecRedistTime)
     {
       typedef typename MV::scalar_type scalar_type;
       typedef typename TypeMap<Superlu,scalar_type>::type slu_type;
@@ -208,51 +251,53 @@ struct MatrixHelper<Superlu>
       int rows, cols;
       rows = Teuchos::as<int>(mv->getGlobalLength());
       cols = Teuchos::as<int>(mv->getGlobalNumVectors());
-      ldx  = Teuchos::as<int>(mv->getStride());
+      ldx  = Teuchos::as<size_t>(rows);
 
-      if ( Util::is_same<scalar_type,slu_type>::value ){
+      {
         Teuchos::TimeMonitor redistTimer( vecRedistTime );
-        mv->get1dCopy(vals,ldx);
-      } else {
-        int vals_length = rows * cols;
-        const Teuchos::Array<scalar_type> vals_tmp(vals_length);
-        {
-          Teuchos::TimeMonitor redistTimer( vecRedistTime );
-          mv->get1dCopy(vals_tmp.view(0, vals_length));
-        }
-        for ( int i = 0; i < vals_length; ++i ){
-          vals[i] = Teuchos::as<slu_type>(vals_tmp[i]);
-        }
+
+	// Dispatch to the copy function appropriate for the type
+        Util::if_then_else<Util::is_same<scalar_type,slu_type>::value,
+          same_type_get_copy<MV>,
+          diff_type_get_copy<MV> >::type::apply(mv, vals, ldx);
       }
 
       FunctionMap<Superlu,scalar_type>::create_Dense_Matrix(
-        X.getRawPtr(), rows, cols, vals.getRawPtr(), ldx,
-        SLU::SLU_DN, dtype, SLU::SLU_GE);
+        X.getRawPtr(), rows, cols, vals.getRawPtr(), Teuchos::as<int>(ldx),
+	SLU::SLU_DN, dtype, SLU::SLU_GE);
     }
 
 
-  /** \brief Creates a Superlu Dense Matrix from the given MultiVector
-   *
-   * \tparam MV A multi-vector type conforming to the interface, in particular
-   *         an Amesos::MultiVecAdapter<>.
-   *
-   * \param [in]     mv   The MultiVector which will be converted to SuperLU format
-   * \param [out]    X    Pointer to the SuperLU Dense SuperMatrix which is to be
-   *                      constructed
-   * \param [out]    vecRedistTime Will have time added for redistribution of \c \mv
-   *
-   * \return A Teuchos::ArrayRCP pointing to the beginning of a contiguous
-   * store of the values in \c X , which is <b>not</b> necessarily the beginning of
-   * the contiguous store of values in \c mv .
-   */
-  template <class MV>
-  static
-  Teuchos::ArrayRCP<typename TypeMap<Superlu,typename MV::scalar_type>::type>
-  createMVDenseMatrix(
-    const Teuchos::Ptr<MV>& mv,
-    const Teuchos::Ptr<SLU::SuperMatrix>& X,
-    Teuchos::Time& vecRedistTime
-    )
+    /** \brief Creates a Superlu Dense Matrix from the given MultiVector
+     *
+     * \tparam MV A multi-vector type conforming to the interface, in
+     *         particular an Amesos::MultiVecAdapter<>.
+     *
+     * \param [in]     mv   The MultiVector which will be converted to SuperLU format
+     * \param [out]    X    Pointer to the SuperLU Dense SuperMatrix which is to be
+     *                      constructed
+     * \param [out]    vecRedistTime Will have time added for redistribution of \c \mv
+     *
+     * \return A Teuchos::ArrayRCP pointing to the beginning of a
+     *         contiguous store of the values in \c X , which is
+     *         <b>not</b> necessarily the beginning of the contiguous
+     *         store of values in \c mv .
+     *
+     * \deprecated The other overloaded version of this function which
+     * asks for a persisting store for the matrix values should be
+     * favoured over this version.  This function uses the not-safe
+     * `get1dViewNonConst'.  In any case, we eventually have to convert
+     * the values into SuperLU's types, which makes a copy of the data
+     * anyhow.
+     */
+    template <class MV>
+    static
+    Teuchos::ArrayRCP<typename TypeMap<Superlu,typename MV::scalar_type>::type>
+    createMVDenseMatrix(
+			const Teuchos::Ptr<MV>& mv,
+			const Teuchos::Ptr<SLU::SuperMatrix>& X,
+			Teuchos::Time& vecRedistTime
+			)
     {
       typedef typename MV::scalar_type scalar_type;
       typedef typename TypeMap<Superlu,scalar_type>::type slu_type;
@@ -282,8 +327,8 @@ struct MatrixHelper<Superlu>
       }
 
       FunctionMap<Superlu,scalar_type>::create_Dense_Matrix(
-        X.getRawPtr(), rows, cols, slu_vals.getRawPtr(), ldx,
-        SLU::SLU_DN, dtype, SLU::SLU_GE);
+							    X.getRawPtr(), rows, cols, slu_vals.getRawPtr(), ldx,
+							    SLU::SLU_DN, dtype, SLU::SLU_GE);
 
       return slu_vals;
     }
