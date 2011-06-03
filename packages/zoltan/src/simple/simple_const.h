2a6063153664b965720dd7471aaa0d4da404aa1b
Lee Ann Riesen <lriesen@sandia.gov>
2011-06-03 13:31:50 -0600

    Add the parameter checking function to Random
    so that set_param doesn't complain that parameter is invalid.

---
+++ packages/zoltan/src/simple/simple_const.h
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Zoltan Library for Parallel Applications                                  *
+ * Copyright (c) 2000,2001,2002, Sandia National Laboratories.               *
+ * For more info, see the README file in the top-level Zoltan directory.     *  
+ *****************************************************************************/
+/*****************************************************************************
+ * CVS File Information :
+ *    $RCSfile$
+ *    $Author$
+ *    $Date$
+ *    $Revision$
+ ****************************************************************************/
+
+
+#ifndef __SIMPLE_CONST_H
+#define __SIMPLE_CONST_H
+
+#ifdef __cplusplus
+/* if C++, define the rest of this header file as extern C */
+extern "C" {
+#endif
+
+
+extern int Zoltan_Random_Set_Param(char *, char *);
+
+#ifdef __cplusplus
+} /* closing bracket for extern "C" */
+#endif
+
+#endif
