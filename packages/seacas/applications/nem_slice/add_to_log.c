7aecc650e7cb36d80172d53c4ceb95e53847b0ef
Greg Sjaardema <gdsjaar@sandia.gov>
2011-06-09 13:52:20 -0600

    SEACAS: Sync usage logging files with seacas

--- packages/seacas/applications/nem_slice/add_to_log.c
+++ packages/seacas/applications/nem_slice/add_to_log.c
@@ -2,23 +2,23 @@
  * Copyright (C) 2009 Sandia Corporation.  Under the terms of Contract
  * DE-AC04-94AL85000 with Sandia Corporation, the U.S. Government retains
  * certain rights in this software
- *
+ * 
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
  * met:
- *
+ * 
  *     * Redistributions of source code must retain the above copyright
  *       notice, this list of conditions and the following disclaimer.
- *
+ * 
  *     * Redistributions in binary form must reproduce the above
  *       copyright notice, this list of conditions and the following
  *       disclaimer in the documentation and/or other materials provided
  *       with the distribution.
- *
+ * 
  *     * Neither the name of Sandia Corporation nor the names of its
  *       contributors may be used to endorse or promote products derived
  *       from this software without specific prior written permission.
- *
+ * 
  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
  * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -30,7 +30,7 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * 
  */
 
 #include <stdio.h>
@@ -46,7 +46,7 @@
 
 void add_to_log(const char *my_name)
 {
-#define LEN 128
+#define LEN 512
   char time_string[LEN];
   char log_string[LEN];
 
@@ -62,50 +62,49 @@ void add_to_log(const char *my_name)
     return;
   }
   
-  username = getlogin();
-  if (username == NULL) {
-    username = getenv("LOGNAME");
-  }
-
-  codename = strrchr (my_name, '/');
-  if (codename == NULL)
-    codename = my_name;
-  else
-    codename++;
-
-  {
-    time_t calendar_time = time(NULL);
-    struct tm *local_time = localtime(&calendar_time);
-    strftime(time_string, LEN, "%a %b %d %H:%M:%S %Z %Y", local_time);
-  }
-
-  {
-    int ticks_per_second;
-    struct tms time_buf;
-    times(&time_buf);
-    ticks_per_second = sysconf(_SC_CLK_TCK);
-    u_time = (double)(time_buf.tms_utime + time_buf.tms_cutime) / ticks_per_second;
-    s_time = (double)(time_buf.tms_stime + time_buf.tms_cstime) / ticks_per_second;
-  }
-  
-  uname(&sys_info);
-
-  sprintf(log_string, "%s %s %s %.3fu %.3fs 0:00.00 0.0%% 0+0k 0+0io 0pf+0w %s\n",
-	  codename, username, time_string, u_time, s_time, sys_info.nodename);
-
   /* Now try to find the $ACCESS/etc/audit.log file */
   /* Don't need to try too hard since information is not critical; just useful */
-  {
-    char *access_dir = getenv("ACCESS");
-    if (access_dir != NULL) {
-      char filename[LEN];
-      sprintf(filename, "%s/etc/audit.log", access_dir);
-      if (0 == access(filename, W_OK)) {
-	FILE *audit = fopen(filename, "a");
-	if (audit != NULL) {
-	  fprintf(audit, "%s", log_string);
-	  fclose(audit);
+  char *access_dir = getenv("ACCESS");
+  if (access_dir != NULL) {
+    char filename[LEN];
+    sprintf(filename, "%s/etc/audit.log", access_dir);
+    if (0 == access(filename, W_OK)) {
+      FILE *audit = fopen(filename, "a");
+      if (audit != NULL) {
+
+	username = getlogin();
+	if (username == NULL) {
+	  username = getenv("LOGNAME");
 	}
+
+	codename = strrchr (my_name, '/');
+	if (codename == NULL)
+	  codename = my_name;
+	else
+	  codename++;
+
+	{
+	  time_t calendar_time = time(NULL);
+	  struct tm *local_time = localtime(&calendar_time);
+	  strftime(time_string, LEN, "%a %b %d %H:%M:%S %Z %Y", local_time);
+	}
+
+	{
+	  int ticks_per_second;
+	  struct tms time_buf;
+	  times(&time_buf);
+	  ticks_per_second = sysconf(_SC_CLK_TCK);
+	  u_time = (double)(time_buf.tms_utime + time_buf.tms_cutime) / ticks_per_second;
+	  s_time = (double)(time_buf.tms_stime + time_buf.tms_cstime) / ticks_per_second;
+	}
+  
+	uname(&sys_info);
+
+	sprintf(log_string, "%s %s %s %.3fu %.3fs 0:00.00 0.0%% 0+0k 0+0io 0pf+0w %s\n",
+		codename, username, time_string, u_time, s_time, sys_info.nodename);
+
+	fprintf(audit, "%s", log_string);
+	fclose(audit);
       }
     }
   }
