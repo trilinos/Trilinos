b199c92d8749ebff35414500bbade0dbcd5364b2
Christopher G. Baker <bakercg@ornl.gov>
2011-06-09 11:31:08 -0400

    Updated gcc base options fragment to point to new mkl-12 fragment

--- cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
+++ cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
@@ -1,4 +1,4 @@
-INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/mkl-11.064-options.cmake)
+INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/mkl-12.0.4-options.cmake)
 
 SET(TRILINOS_TOOLSET_BASE /opt/gcc-4.5.1/trilinos-toolset)
 SET(Trilinos_EXTRA_LINK_FLAGS "-Wl,-rpath,${TRILINOS_TOOLSET_BASE}/lib64" CACHE STRING "")
