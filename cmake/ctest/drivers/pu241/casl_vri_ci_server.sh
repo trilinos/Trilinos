9e0e793606bdccebd798a7db476d32efba8aaecf
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-06-27 13:49:26 -0400

    Enabling the CI server for casl-dev packages.
    This should be a fully functioning CI server for CASL VRI software.
    Every feature you would want to have is not implemented but it should
    be send emails when major things break and not spam with email when
    things are working.
    
    Build/Test Cases Summary
    Enabled Packages: TrilinosFramework
    Disabled Packages: PyTrilinos,TriKota,Optika,Sundance,Stokhos
    Enabled all Forward Packages
    0) MPI_DEBUG => passed: passed=86,notpassed=0 (1.20 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)
    Other local commits for this build/test group: bed6527, 67f0618

--- cmake/ctest/drivers/pu241/casl_vri_ci_server.sh
+++ cmake/ctest/drivers/pu241/casl_vri_ci_server.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-
 # This script runs a CI server that builds the CASL VRI packages in a
 # continuous integration loop and sends results to the CASL VRI CDash
 # server.
@@ -9,14 +8,11 @@
 # Set up
 #
 
-# Override the pointer to Trilinos if you need to.
-if ["$TRILINOS_BASE" == ""] ; then
-  TRILINOS_BASE=/home/casl-vri-admin/Dashbaords/Trilinos
-fi
+TRILINOS_BASE=/home/casl-vri-admin/CIDashboards/Trilinos
 echo "TRILINOS_BASE = '$TRILINOS_BASE'"
 
 # Must always start from this dir!
-DASHBOARD_DRIVER_DIR=/home/casl-vri-admin/Dashbaords/Trilinos/cmake/ctest/drivers/pu241
+DASHBOARD_DRIVER_DIR=/home/casl-vri-admin/CIDashboards/Trilinos/cmake/ctest/drivers/pu241
 cd $DASHBOARD_DRIVER_DIR
 
 BASE_COMMAND=$TRILINOS_BASE/cmake/ctest/drivers/pu241/cron_driver_single_ci_iter_pu241.sh
@@ -39,27 +35,24 @@ export PATH=/opt/trilinos-toolset/bin:$PATH
 
 $TRILINOS_BASE/cmake/python/mailmsg.py "Starting CASL VRI CI testing server on pu241"
 
-# ToDo: Set TDD_CONTINUOUS_INTEGRATION_MODE on the env and have it
-# picked up in the CMakeLists.txt file and have it run the CI test script(s)!
-
 echo
 echo "***"
 echo "*** Starting CI server that tests changes that can affect CASL"
 echo "***"
 echo
 
-# Run the first build from scratch and build everything
-env \
-CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=ON \
-CTEST_ENABLE_MODIFIED_PACKAGES_ONLY=OFF \
-$BASE_COMMAND
+# A) Run the first build all SS packages from scratch
 
-# Run a CI loop that will terminate on time
+env CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=TRUE CTEST_ENABLE_MODIFIED_PACKAGES_ONLY=OFF $BASE_COMMAND
 
-CI_COMMAND=env \
-CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=OFF \
-CTEST_ENABLE_MODIFIED_PACKAGES_ONLY=ON \
-$BASE_COMMAND
+# B) Run a CI loop that will terminate on time
+
+# B.1) Define the inner loop command
+
+CI_COMMAND="env TDD_FORCE_CMAKE_INSTALL=0 TDD_DO_SUBMIT=OFF CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=OFF CTEST_ENABLE_MODIFIED_PACKAGES_ONLY=ON $BASE_COMMAND"
+echo "CI_COMMAND = $CI_COMMAND"
+
+# B.2) Run the loop
 
 $TRILINOS_BASE/cmake/python/generic-looping-demon.py \
 --command="$CI_COMMAND" \
@@ -67,3 +60,21 @@ $TRILINOS_BASE/cmake/python/generic-looping-demon.py \
 --loop-interval=$LOOP_INTERVAL
 
 $TRILINOS_BASE/cmake/python/mailmsg.py "Finished CASL VRI CI testing server on pu241"
+
+# NOTES:
+#
+# On the first CI iteration, we let it update CMake/CTest and do the
+# outer submit to the TDD dashboard.  This provides some detail on how
+# the outter configure is being done and should provide some useful
+# info.  However, for now, we turn off CDash submits to the outer TDD
+# dashboard because currently it would submit worthless no-op results
+# every LOOP_INTERVAL (e.g. 1 minute).  To be able to do the submit of
+# the outer results we would have to make some tricky modifications to
+# the CTest driver script so that the outter TDD driver could detect
+# when at least one build case was done and if it was then do the
+# submit in one shot at the very end.  I think you can do this but it
+# would require some work.
+#
+# Above, we only install the offical version of CMake/CTest once on
+# the first CI iteration.  We do not reinstall CMake over and over
+# again to save time and there is little value in doing so;5B.
--- cmake/ctest/drivers/pu241/cron_driver_single_ci_iter_pu241.sh
+++ cmake/ctest/drivers/pu241/cron_driver_single_ci_iter_pu241.sh
@@ -9,7 +9,7 @@ eval `vera_dev_env.py load casl`
 eval `vera_dev_env.py load intel/12`
 umask u=rwx,g=rwx,o=
 
-BASEDIR=/home/casl-vri-admin/Dashboards
+BASEDIR=/home/casl-vri-admin/CIDashboards
 DRIVER_SCRIPT_DIR=$BASEDIR/Trilinos/cmake/ctest/drivers/pu241
 TRILINOS_REPOSITORY_LOCATION="cgbaker@software.sandia.gov:/space/git/Trilinos"
 
@@ -19,6 +19,8 @@ export TDD_CTEST_TEST_TYPE=Continuous
 # Submit the outer TDD tests to casl-dev always since these are CASL machines
 export TDD_CTEST_DROP_SITE=casl-dev.ornl.gov
 export TDD_CTEST_DROP_LOCATION="/CDash/submit.php?project=TrilinosDriver"
+
+export RUN_CI_SERVER=ON
 
 time env python ../cron_driver.py
 
