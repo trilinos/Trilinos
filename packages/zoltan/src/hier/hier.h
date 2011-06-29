f542a244eb2b6b2e0abcfec839791b79dc37051e
Lee Ann Riesen <lriesen@sandia.gov>
2011-06-28 18:25:47 -0600

    Rewrite Zoltan_Hier to make it faster.
    By using Zoltan_Map (which uses Zoltan_Hash) we can reduce the
    size of the queries to the Zoltan_DD global data directories.  Also because
    of Zoltan_Map we don't need to keep the global number system sequential
    and increasing across processes.
    
    The hier test answers are all the same except for one of the ch_hammond
    tests.  The metrics are almost identical but the answer is not exactly
    the same.
    
    Running hierarchical partitioning (2, 2, 6) with 24 processes each
    on one core of a dual socket, dual die, 6-core die node, with a
    graph with 1 million vertices, using scotch for partitioning at
    each level, the old Zoltan_Hier took 38 seconds and the new
    Zoltan_Hier took 14 seconds.

--- packages/zoltan/src/hier/hier.h
+++ packages/zoltan/src/hier/hier.h
@@ -26,64 +26,39 @@ extern "C" {
 
 /* Parameters to hierarchical balancing */
 struct HierPartParamsStruct {
-  int output_level;                  /* flag indicating amount of debugging
-					output from hierarchical balancing.
-					See levels in HIER_DEBUG_* below */
-  int num_levels;                    /* number of levels of hierarchy to be
-					dealt with at local processor */
-  int global_num_levels;             /* max num_levels across all procs */
+  int output_level;                  /* amount of debugging info */
+  int checks;                        /* should we do sanity checks? */
+  int gen_files;                      /* call Zoltan_Generate_Files */
+
+  int num_levels;                    /* number of levels I do */
+  int global_num_levels;             /* max number of levels */
   int level;                         /* level currently being processed */
   MPI_Comm hier_comm;                /* MPI communicator for each level */
-  int *hier_ranks_of_orig;           /* orig ranks of the procs in hier_comm */
  
   ZZ *origzz;                        /* Zoltan struct passed into top level */
   ZZ *hierzz;                        /* internal zoltan struct for balancing 
 					within the hierarchy */
+
   int part_to_compute;               /* partition to compute at each level */
   int num_parts;                     /* number of partitions to compute */
+
   int use_geom, use_graph;           /* flags for whether methods to be
 					used will require geometric
 					and/or graph information */
-  int checks;                        /* should we do sanity checks? */
-  int init_num_obj;                  /* number of local objects at start */
-  int hier_num_obj;                  /* number of local objects during
-					hierarchical balancing */
-  ZOLTAN_ID_PTR local_ids;           /* / lists of local ids and global ids */
-  ZOLTAN_ID_PTR global_ids;          /* \ for initial partitioning */
-  char *migrated;                    /* array of flags indicating whether
-					each gid in global_ids has been
-					migrated somewhere else */
-  struct Zoltan_DD_Struct *dd;       /* distributed data to track migrated 
-					gnos during hierarchical balancing */
-  int allocsize_gnos_of_interest;    /* size of gnos_of_interest array */
-  int num_gnos_of_interest;          /* num gnos in gnos_of_interest */
-  ZOLTAN_GNO_TYPE *gnos_of_interest;    /* list of gnos of interest, used
-					when looking up remote proc locations
-					for graph edge callbacks */
-  int *gnos_of_interest_procs;       /* list of procs where gnos of interest
-					are located */
+  int num_obj;                       /* number of local objects at start */
   int obj_wgt_dim, edge_wgt_dim;     /* object and edge weight dimensions */
+  ZOLTAN_GNO_TYPE invalid_gno;  /* a value guaranteed not to be one of the gnos */
+  ZOLTAN_GNO_TYPE *gno;              /* global vertex ids */
   float *vwgt;                       /* vector of vertex weights */
-  int *input_parts;                  /* Initial partitions for objects. */
-  ZOLTAN_GNO_TYPE *vtxdist;          /* intermediate graph structure storage */
   int *xadj;                         /* intermediate graph structure storage */
   ZOLTAN_GNO_TYPE *adjncy;              /*    see Zoltan_Build_Graph */
   float *ewgts;                      /* edge weights for intermediate struct */
-  int *adjproc;                      /* adjacent proc for each edge */
-  int ndims;                         /* number of dimensions for geom data */
-  int num_edges;                     /* number of edges in graph rep */
-  double *geom_vec;                  /* geometry of objects in intermediate */
-  int num_migrated_in_gnos;          /* number of gnos migrated to this proc */
-  int alloc_migrated_in_gnos;        /* size of allocated array of migrated
-					in gnos */
-  ZOLTAN_GNO_TYPE *migrated_in_gnos;    /* ordered array of gnos migrated in */
-  void **migrated_in_data;           /* data migrated in, parallel array to
-					migrated_in_gnos */
+  int *adjproc;                      /* adjacent proc (in current MPI group) */
 
-                                         /* If non-null, Zoltan is computing the */
+  int ndims;                         /* number of dimensions for geom data */
+  double *geom_vec;                  /* geometry of objects in intermediate */
+
   zoltan_platform_specification *spec;   /* levels based on network topology */
-  int gen_files;                      /* call Zoltan_Generate_Files */
-                                         
 };
 typedef struct HierPartParamsStruct HierPartParams;
 
