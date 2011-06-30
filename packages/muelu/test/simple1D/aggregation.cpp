320d53a2889891078e98d2a90c7a789d739e6bb4
Jonathan Hu <jhu@sandia.gov>
2011-06-30 09:21:05 -0700

    Adding my aggregation profiling test.

---
+++ packages/muelu/test/simple1D/aggregation.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+
+// Teuchos
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_ParameterList.hpp>
+#include <Teuchos_CommandLineProcessor.hpp>
+#include <Teuchos_GlobalMPISession.hpp>
+#include <Teuchos_DefaultComm.hpp>
+
+#include "MueLu_Hierarchy.hpp"
+#include "MueLu_SaLevel.hpp"
+#include "MueLu_SaPFactory.hpp"
+#include "MueLu_Utilities.hpp"
+#include "MueLu_AggregationOptions.hpp"
+
+/**********************************************************************************/
+/* CREATE INITAL MATRIX                                                           */
+/**********************************************************************************/
+#include <Cthulhu_Map.hpp>
+#include <Cthulhu_CrsOperator.hpp>
+#include <Cthulhu_Vector.hpp>
+#include <Cthulhu_VectorFactory.hpp>
+#include <Cthulhu_MultiVectorFactory.hpp>
+#include <Cthulhu_Parameters.hpp>
+
+// Gallery
+#define CTHULHU_ENABLED // == Gallery have to be build with the support of Cthulhu matrices.
+#include <MueLu_GalleryParameters.hpp>
+#include <MueLu_MatrixFactory.hpp>
+
+#include "MueLu_UseDefaultTypes.hpp"
+#include "MueLu_UseShortNames.hpp"
+#include <unistd.h>
+/**********************************************************************************/
+
+int main(int argc, char *argv[]) {
+
+  using Teuchos::RCP;
+  using Teuchos::rcp;
+ 
+  Teuchos::oblackholestream blackhole;
+  Teuchos::GlobalMPISession mpiSession(&argc,&argv,&blackhole);
+  RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
+
+  /**********************************************************************************/
+  /* SET TEST PARAMETERS                                                            */
+  /**********************************************************************************/
+  // Note: use --help to list available options.
+  Teuchos::CommandLineProcessor clp(false);
+  
+  // Default is Laplace1D with nx = 8748.
+  // It's a nice size for 1D and perfect aggregation. (6561=3^8)
+  //Nice size for 1D and perfect aggregation on small numbers of processors. (8748=4*3^7)
+  MueLu::Gallery::Parameters<GO> matrixParameters(clp, 8748); // manage parameters of the test case
+  Cthulhu::Parameters cthulhuParameters(clp);             // manage parameters of cthulhu
+
+  // custom parameters
+  LO maxLevels = 3;
+  LO its=10;
+  clp.setOption("maxLevels",&maxLevels,"maximum number of levels allowed");
+  clp.setOption("its",&its,"number of multigrid cycles");
+  
+  switch (clp.parse(argc,argv)) {
+  case Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED:        return EXIT_SUCCESS; break;
+  case Teuchos::CommandLineProcessor::PARSE_UNRECOGNIZED_OPTION: return EXIT_FAILURE; break;
+  case Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL:                               break;
+  }
+  
+  matrixParameters.check();
+  cthulhuParameters.check();
+  // TODO: check custom parameters
+
+  if (comm->getRank() == 0) {
+    matrixParameters.print();
+    cthulhuParameters.print();
+    // TODO: print custom parameters
+  }
+
+  /**********************************************************************************/
+  /* CREATE INITIAL MATRIX                                                          */
+  /**********************************************************************************/
+  const RCP<const Map> map = MapFactory::Build(cthulhuParameters.GetLib(), matrixParameters.GetNumGlobalElements(), 0, comm);
+  RCP<CrsOperator> Op = MueLu::Gallery::CreateCrsMatrix<SC, LO, GO, Map, CrsOperator>(matrixParameters.GetMatrixType(), map, matrixParameters.GetParameterList()); //TODO: Operator vs. CrsOperator
+  /**********************************************************************************/
+  /*                                                                                */
+  /**********************************************************************************/
+
+  MueLu::AggregationOptions aggOptions;
+  aggOptions.SetPrintFlag(6);
+  aggOptions.SetMinNodesPerAggregate(2);
+  aggOptions.SetMaxNeighAlreadySelected(0);
+  aggOptions.SetOrdering(MueLu::AggOptions::NATURAL);
+  aggOptions.SetPhase3AggCreation(0.5);
+  UCAggregationFactory aggFact(aggOptions);
+  RCP<Graph> graph = rcp(new Graph(Op->getCrsGraph(), "someGraphLabel"));
+  double t0 = MPI_Wtime();
+  RCP<Aggregates> aggregates = aggFact.Build(*graph);
+  double t1 = MPI_Wtime() - t0;
+  printf("Aggregation time only: %g\n",t1);
+
+  return EXIT_SUCCESS;
+
+}
