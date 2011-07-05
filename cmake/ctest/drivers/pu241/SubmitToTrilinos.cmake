1a62e4031d78dc032078b71a5dac52fe9c4e5480
Roscoe A. Bartlett <rabartl@sandia.gov>
2011-07-05 09:54:39 -0600

    Changing to submit to testing.sandia.gov.

--- cmake/ctest/drivers/pu241/SubmitToTrilinos.cmake
+++ cmake/ctest/drivers/pu241/SubmitToTrilinos.cmake
@@ -1,3 +1,3 @@
 # Sumbit to the main Trilinos dashbaord
-SET(CTEST_DROP_SITE "trilinos.sandia.gov")
+SET(CTEST_DROP_SITE "testing.sandia.gov")
 SET(CTEST_DROP_LOCATION "/cdash/submit.php?project=Trilinos")
