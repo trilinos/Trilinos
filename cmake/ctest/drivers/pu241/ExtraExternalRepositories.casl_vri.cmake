1d4f539fcafc12714bff6e1a47bb03760c2e33df
Christopher G. Baker <bakercg@ornl.gov>
2011-07-13 17:02:28 -0400

    Changed location of CASL repos w.r.t. recent repo migration

--- cmake/ctest/drivers/pu241/ExtraExternalRepositories.casl_vri.cmake
+++ cmake/ctest/drivers/pu241/ExtraExternalRepositories.casl_vri.cmake
@@ -9,9 +9,9 @@ SET( Trilinos_EXTRAREPOS_DIR_REPOTYPE_REPOURL_PACKSTAT_CATEGORY
   #Dakota  packages/TriKota/Dakota SVN
   #   https://software.sandia.gov/svn/public/dakota/public/trunk  NOPACKAGES  Nightly  
   StarCCMExt  ""  GIT  casl-dev.ornl.gov:/git-root/StarCCMClient  ""  Continuous
-  DeCARTExt  ""  GIT  nstdsrv.ornl.gov:/gitroot/casldecart  ""  Continuous
-  CASLBOA  ""  GIT  nstdsrv.ornl.gov:/gitroot/casl_boa  ""  Continuous
-  CASLRAVE  ""  GIT  nstdsrv.ornl.gov:/gitroot/caslvipre  ""  Continuous
-  LIMEExt  ""  GIT  software.sandia.gov:/space/git/LIMEExt  ""  Continuous
-  PSSDriversExt  ""  GIT  nstdsrv.ornl.gov:/gitroot/casl_pssdrivers  ""  Continuous
+  DeCARTExt  ""  GIT   casl-dev.ornl.gov:/git-root/casl_decart  ""  Continuous
+  CASLBOA  ""  GIT     casl-dev.ornl.gov:/git-root/casl_boa  ""  Continuous
+  CASLRAVE  ""  GIT    casl-dev.ornl.gov:/git-root/casl_rave  ""  Continuous
+  LIMEExt  ""  GIT     software.sandia.gov:/space/git/LIMEExt  ""  Continuous
+  PSSDriversExt  ""  GIT  casl-dev.ornl.gov:/git-root/casl_vripss  ""  Continuous
   )
