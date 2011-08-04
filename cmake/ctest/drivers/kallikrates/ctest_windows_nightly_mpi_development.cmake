a36f8d3626ff850efdcb5edda22eee33da30ee07
Brent Perschbacher <bmpersc@sandia.gov>
2011-08-04 16:55:37 -0600

    Disable Intrepid on windows
    Intrepid is relying on a couple symbols from math.h that are not available on
    windows. There is a plan for adding in versions of these symbols for windows
    only, however, we cannot push them just yet. I didn't want to leave Intrepid
    failing while we work this out so I am disabling it for now. This won't be the
    permanent solution and I expect we will be able to re-enable it early next week.

--- cmake/ctest/drivers/kallikrates/ctest_windows_nightly_mpi_development.cmake
+++ cmake/ctest/drivers/kallikrates/ctest_windows_nightly_mpi_development.cmake
@@ -10,7 +10,7 @@ SET(BUILD_DIR_NAME MPI_OPT_DEV)
 #SET(CTEST_TEST_TYPE EXPERIMENTAL)
 #SET(CTEST_TEST_TIMEOUT 900)
 
-SET( EXTRA_EXCLUDE_PACKAGES TrilinosFramework Amesos Ifpack ML Moertel Stokhos Piro Optika Mesquite FEApp)
+SET( EXTRA_EXCLUDE_PACKAGES TrilinosFramework Amesos Ifpack ML Moertel Stokhos Piro Optika Mesquite FEApp Intrepid)
 
 SET( EXTRA_CONFIGURE_OPTIONS
   "-DTrilinos_ENABLE_KNOWN_EXTERNAL_REPOS_TYPE:STRING=Experimental"
