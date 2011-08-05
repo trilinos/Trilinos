2bd49a581b447b5bcf5090ebfb8539ecd048e3c1
Christopher G. Baker <bakercg@ornl.gov>
2011-08-05 17:01:39 -0400

    Added a gcc build of pvm, set builds to use the appropriate one

--- cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
+++ cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
@@ -1,5 +1,8 @@
 INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/mkl-12.0.4-options.cmake)
 INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/casl-vri-tpls.cmake)
 
+SET(PVM_LIBRARY_DIRS /opt/gcc-4.5.1/tpls/pvm3/lib/LINUX64                    CACHE FILEPATH "")
+SET(PVM_INCLUDE_DIRS /opt/gcc-4.5.1/tpls/pvm3/include                        CACHE FILEPATH "")
+
 SET(TRILINOS_TOOLSET_BASE /opt/gcc-4.5.1/trilinos-toolset)
 SET(Trilinos_EXTRA_LINK_FLAGS "-Wl,-rpath,${TRILINOS_TOOLSET_BASE}/lib64" CACHE STRING "")
