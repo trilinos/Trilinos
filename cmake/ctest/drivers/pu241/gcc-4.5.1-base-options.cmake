4882705c4e2a34eabe9896debd4d76c238095279
Christopher G. Baker <bakercg@ornl.gov>
2011-08-08 05:36:15 -0400

    Pointed to proper HDF5 and LIBXML2 TPLs for gcc and intel builds

--- cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
+++ cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
@@ -3,6 +3,9 @@ INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/casl-vri-tpls.cmake)
 
 SET(PVM_LIBRARY_DIRS /opt/gcc-4.5.1/tpls/pvm3/lib/LINUX64                    CACHE FILEPATH "")
 SET(PVM_INCLUDE_DIRS /opt/gcc-4.5.1/tpls/pvm3/include                        CACHE FILEPATH "")
+SET(HDF5_LIBRARY_NAMES "hdf5_hl;hdf5"                                        CACHE STRING   "")
+SET(HDF5_LIBRARY_DIRS   /opt/gcc-4.5.1/tpls/hdf5/lib                         CACHE FILEPATH "")
+SET(HDF5_INCLUDE_DIRS   /opt/gcc-4.5.1/tpls/hdf5/include                     CACHE FILEPATH "")
 
 SET(TRILINOS_TOOLSET_BASE /opt/gcc-4.5.1/trilinos-toolset)
 SET(Trilinos_EXTRA_LINK_FLAGS "-Wl,-rpath,${TRILINOS_TOOLSET_BASE}/lib64" CACHE STRING "")
