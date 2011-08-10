778f28fc6035e45d7bbb324d1f01ecf678ab9bbc
Christopher G. Baker <bakercg@ornl.gov>
2011-08-09 21:20:07 -0400

    Modified casl-vri-tpl list to instruct EpetraExt not to use HDF5 (it isn't a parallel HDF5, which EpetraExt requires) I'll watch the CI server to see whether this works

--- cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
+++ cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
@@ -6,6 +6,7 @@ SET(PVM_INCLUDE_DIRS /opt/gcc-4.5.1/tpls/pvm3/include                        CAC
 SET(HDF5_LIBRARY_NAMES "hdf5_hl;hdf5"                                        CACHE STRING   "")
 SET(HDF5_LIBRARY_DIRS /opt/gcc-4.5.1/tpls/hdf5-1.8.5-patch1/lib              CACHE FILEPATH "")
 SET(HDF5_INCLUDE_DIRS /opt/gcc-4.5.1/tpls/hdf5-1.8.5-patch1/include          CACHE FILEPATH "")
+SET(EpetraExt_ENABLE_HDF5 OFF CACHE BOOL "")
 
 SET(TRILINOS_TOOLSET_BASE /opt/gcc-4.5.1/trilinos-toolset)
 SET(Trilinos_EXTRA_LINK_FLAGS "-Wl,-rpath,${TRILINOS_TOOLSET_BASE}/lib64" CACHE STRING "")
