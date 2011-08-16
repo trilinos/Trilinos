4e75fd645d3c2d630ea8d763630483bf04b7d715
Jeremie Gaidamour <jngaida@sandia.gov>
2011-08-16 13:37:55 -0600

    MueLu: regenerated Cthulhu::EpetraImport using scripts

--- packages/muelu/cthulhu/src/Import/Cthulhu_EpetraImport.cpp
+++ packages/muelu/cthulhu/src/Import/Cthulhu_EpetraImport.cpp
@@ -1,24 +1,28 @@
-#include <Cthulhu_EpetraImport.hpp>
+#include "Cthulhu_EpetraImport.hpp"
 
 namespace Cthulhu {
 
+  EpetraImport::EpetraImport(const Teuchos::RCP<const Map<int,int> > & source, const Teuchos::RCP<const Map<int,int> > & target)
+    : import_(rcp(new Epetra_Import(toEpetra(target), toEpetra(source)))) { } // Warning: Epetra(Target, Source) vs. Tpetra(Source, Target)
+  
+  // //! copy constructor. 
+  // EpetraImport::EpetraImport(const Import<int,int> & import) { // TODO: refactoring
+  //   CTHULHU_DYNAMIC_CAST(const EpetraImport, import, tImport, "Cthulhu::EpetraImport copy constructors only accept Cthulhu::EpetraImport as input arguments.");
+  //   import_ = rcp(new Epetra_Import(*tImport.getEpetra_Import()));
+  // }
+  
   // TODO: move that elsewhere
-//   template <class LocalOrdinal, class GlobalOrdinal, class Node>
-//   const Tpetra::Import<LocalOrdinal,GlobalOrdinal,Node> & toTpetra(const Import<LocalOrdinal,GlobalOrdinal,Node> &import) {
-//     // TODO: throw exception
-//     const TpetraImport<LocalOrdinal,GlobalOrdinal,Node> & tpetraImport = dynamic_cast<const TpetraImport<LocalOrdinal,GlobalOrdinal,Node> &>(import);
-//     return *tpetraImport.getTpetra_Import();
-//   }
+  //   const Epetra_Import & toEpetra(const Import<int, int> &import) {
+  //     // TODO: throw exception
+  //     const EpetraImport & tpetraImport = dynamic_cast<const EpetraImport &>(import);
+  //     return *tpetraImport.getEpetra_Import();
+  //   }
 
-// RCP< const Import<int, int > > toCthulhu(const Epetra_Import &import) {
-//   RCP<const Epetra_Import> imp = rcp(new Epetra_Import(import)); //NOTE: non consitent: return pointer, take ref
-//   return rcp ( new Cthulhu::EpetraImport(imp) );
-// }
-
-RCP< const Import<int, int > > toCthulhu(const Epetra_Import *import) {
-  RCP<const Epetra_Import> imp = rcp(new Epetra_Import(*import)); //NOTE: non consitent: return pointer, take ref
-  return rcp ( new Cthulhu::EpetraImport(imp) );
-}
-//
+  RCP< const Import<int, int > > toCthulhu(const Epetra_Import *import) {
+    RCP<const Epetra_Import> imp = rcp(new Epetra_Import(*import)); //NOTE: non consitent: return pointer, take ref
+    return rcp ( new Cthulhu::EpetraImport(imp) );
+  }
+  //
 
 } // Cthulhu namespace
+
