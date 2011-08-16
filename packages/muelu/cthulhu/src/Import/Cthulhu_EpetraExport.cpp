71ded69a4740b1631d7d92ce1c3f83338753a98d
Jeremie Gaidamour <jngaida@sandia.gov>
2011-08-16 13:48:59 -0600

    MueLu: regenerated Cthulhu::EpetraExport using scripts

---
+++ packages/muelu/cthulhu/src/Import/Cthulhu_EpetraExport.cpp
@@ -0,0 +1,15 @@
+#include "Cthulhu_EpetraExport.hpp"
+
+namespace Cthulhu {
+
+  EpetraExport::EpetraExport(const Teuchos::RCP<const Map<int,int> > & source, const Teuchos::RCP<const Map<int,int> > & target)
+    : export_(rcp(new Epetra_Export(toEpetra(target), toEpetra(source)))) { } // Warning: Epetra(Target, Source) vs. Tpetra(Source, Target)
+
+  RCP< const Export<int, int > > toCthulhu(const Epetra_Export *import) {
+    RCP<const Epetra_Export> imp = rcp(new Epetra_Export(*import)); //NOTE: non consitent: return pointer, take ref
+    return rcp ( new Cthulhu::EpetraExport(imp) );
+  }
+  //
+
+} // Cthulhu namespace
+
