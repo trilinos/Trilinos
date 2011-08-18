2f9cdc5e88853dc590d38ce67eb279b241e1dee8
Christopher G. Baker <bakercg@ornl.gov>
2011-08-18 16:19:49 -0400

    added INCLUDE_DIRS to BLAS and LAPACK TPL; we will hijack these to get access to the fortran modules
    Build/Test Cases Summary
    Enabled Packages: TrilinosFramework, CASLRAVE, CASLRAVECoreStore, CASLRAVEWENV, CASLRAVEPIP, CASLRAVEFLIB, CASLRAVECODEIF, CASLRAVENexus, CASLRAVENFDLIB, CASLRAVEVIPREW
    Disabled Packages: PyTrilinos,TriKota,Optika,Sundance,Stokhos
    Enabled all Packages
    0) MPI_DEBUG => passed: passed=1282,notpassed=0 (27.50 min)
    1) SERIAL_RELEASE => passed: passed=1296,notpassed=0 (6.72 min)

--- cmake/ctest/drivers/pu241/mkl-12.0.4-options.cmake
+++ cmake/ctest/drivers/pu241/mkl-12.0.4-options.cmake
@@ -2,7 +2,11 @@ SET(INTEL_12_0_4_ROOT /opt/intel/Compiler/composerxe-2011.4.191)
 
 SET(MKLROOT ${INTEL_12_0_4_ROOT}/mkl)
 
-SET(BLAS_LIBRARY_NAMES "mkl_intel_lp64;mkl_blas95_lp64;mkl_core;mkl_sequential" CACHE STRING "")
-SET(BLAS_LIBRARY_DIRS "${MKLROOT}/lib/intel64" CACHE STRING "")
-SET(LAPACK_LIBRARY_NAMES "mkl_lapack95_lp64" CACHE STRING "")
-SET(LAPACK_LIBRARY_DIRS "${MKLROOT}/lib/intel64" CACHE FILEPATH "")
+SET(BLAS_LIBRARY_NAMES   "mkl_intel_lp64;mkl_blas95_lp64;mkl_core;mkl_sequential"   CACHE STRING   "")
+SET(BLAS_LIBRARY_DIRS    "${MKLROOT}/lib/intel64"                                   CACHE FILEPATH "")
+SET(LAPACK_LIBRARY_NAMES "mkl_lapack95_lp64"                                        CACHE STRING   "")
+SET(LAPACK_LIBRARY_DIRS  "${MKLROOT}/lib/intel64"                                   CACHE FILEPATH "")
+
+# hijack these to get modules paths into the include list; needed by CASLRAVE/ANC
+SET(TPL_BLAS_INCLUDE_DIRS   ${MKLROOT}/include/intel64/lp64                         CACHE PATH     "")
+SET(TPL_LAPACK_INCLUDE_DIRS ${TPL_BLAS_INCLUDE_DIRS}                                CACHE PATH     "")
