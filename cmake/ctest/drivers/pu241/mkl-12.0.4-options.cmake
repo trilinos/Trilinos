5e482b679f66a758322246350d8bd96ce92c7eac
Christopher G. Baker <bakercg@ornl.gov>
2011-08-19 10:28:11 -0400

    modified CASL TPLs so that compiler-specific MKL modules are picked up

--- cmake/ctest/drivers/pu241/mkl-12.0.4-options.cmake
+++ cmake/ctest/drivers/pu241/mkl-12.0.4-options.cmake
@@ -2,11 +2,7 @@ SET(INTEL_12_0_4_ROOT /opt/intel/Compiler/composerxe-2011.4.191)
 
 SET(MKLROOT ${INTEL_12_0_4_ROOT}/mkl)
 
-SET(BLAS_LIBRARY_NAMES   "mkl_intel_lp64;mkl_blas95_lp64;mkl_core;mkl_sequential"   CACHE STRING   "")
+SET(BLAS_LIBRARY_NAMES   "mkl_intel_lp64;mkl_blas95_lp64;mkl_core;mkl_sequential"   CACHE STRING   "Sequential 64-bit BLAS with 32-bit integers")
 SET(BLAS_LIBRARY_DIRS    "${MKLROOT}/lib/intel64"                                   CACHE FILEPATH "")
-SET(LAPACK_LIBRARY_NAMES "mkl_lapack95_lp64"                                        CACHE STRING   "")
+SET(LAPACK_LIBRARY_NAMES "mkl_lapack95_lp64"                                        CACHE STRING   "64-bit LAPACK with 32-bit integers")
 SET(LAPACK_LIBRARY_DIRS  "${MKLROOT}/lib/intel64"                                   CACHE FILEPATH "")
-
-# hijack these to get modules paths into the include list; needed by CASLRAVE/ANC
-SET(BLAS_INCLUDE_DIRS   ${MKLROOT}/include/intel64/lp64                     CACHE PATH     "")
-SET(LAPACK_INCLUDE_DIRS ${BLAS_INCLUDE_DIRS}                                CACHE PATH     "")
