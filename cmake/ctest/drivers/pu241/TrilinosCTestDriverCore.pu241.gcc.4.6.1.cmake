efe3d2c43d887431b42bdf7f1519e5902715e7c4
Christopher G. Baker <bakercg@ornl.gov>
2011-08-22 14:12:10 -0400

    add 4.6.1 to the nightly disabled kokkos under 4.6.1 disabled CASLRAVEANC and CASLRAVEANCKVIPRE under GNU builds

---
+++ cmake/ctest/drivers/pu241/TrilinosCTestDriverCore.pu241.gcc.4.6.1.cmake
@@ -0,0 +1,59 @@
+  
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../TrilinosCTestDriverCore.cmake")
+
+#
+# Platform/compiler specific options for godel using gcc
+#
+
+MACRO(TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER)
+
+  # Base of Trilinos/cmake/ctest then BUILD_DIR_NAME
+  SET( CTEST_DASHBOARD_ROOT "${TRILINOS_CMAKE_DIR}/../../${BUILD_DIR_NAME}" )
+
+  SET( CTEST_NOTES_FILES "${CTEST_SCRIPT_DIRECTORY}/${CTEST_SCRIPT_NAME}" )
+  
+  SET( CTEST_BUILD_FLAGS "-j8 -i" )
+  SET( CTEST_PARALLEL_LEVEL 8 )
+
+  #SET( CTEST_MEMORYCHECK_COMMAND /usr/bin/valgrind )
+  #SET( CTEST_MEMORYCHECK_COMMAND_OPTIONS )
+
+  # We don't have TPLs for PyTrilinos, Optika, and TriKota
+  # 
+  SET_DEFAULT( Trilinos_EXCLUDE_PACKAGES
+     PyTrilinos TriKota Optika  # We don't have TPLs for these
+     Sundance Stokhos # Currently have failures and nor currently needed by CASL
+     TrilinosFramework # Has 11 failing tests for some reason so disabling for now
+     )
+  SET(EXTRA_CONFIGURE_OPTIONS
+    -DTrilinos_ENABLE_TriKota:BOOL=OFF
+    # Allow user to override these by putting theirs at the bottom
+    ${EXTRA_CONFIGURE_OPTIONS}
+    )
+  
+  SET_DEFAULT(COMPILER_VERSION "GCC-4.6.1")
+
+  SET_DEFAULT(Trilinos_ENABLE_KNOWN_EXTERNAL_REPOS_TYPE Nightly)
+  
+  IF (COMM_TYPE STREQUAL MPI)
+  
+    SET( EXTRA_SYSTEM_CONFIGURE_OPTIONS
+      ${EXTRA_CONFIGURE_OPTIONS}
+      "-DTrilinos_CONFIGURE_OPTIONS_FILE:FILEPATH=${CTEST_SCRIPT_DIRECTORY}/gcc-4.6.1-mpi-ss-options.cmake"
+      "-DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE}"
+      "-DTPL_ENABLE_MPI:BOOL=ON"
+    )
+  
+  ELSE()
+  
+    SET( EXTRA_SYSTEM_CONFIGURE_OPTIONS
+      ${EXTRA_CONFIGURE_OPTIONS}
+      "-DTrilinos_CONFIGURE_OPTIONS_FILE:FILEPATH=${CTEST_SCRIPT_DIRECTORY}/gcc-4.6.1-serial-ss-options.cmake"
+      "-DCMAKE_BUILD_TYPE:STRING=${BUILD_TYPE}"
+    )
+
+  ENDIF()
+
+  TRILINOS_CTEST_DRIVER()
+
+ENDMACRO()
--- cmake/ctest/drivers/pu241/ctest_linux_nightly_mpi_debug_gcc_4.6.1_casl_dev.cmake
+++ cmake/ctest/drivers/pu241/ctest_linux_nightly_mpi_debug_gcc_4.6.1_casl_dev.cmake
@@ -1,8 +1,8 @@
 #
-# Build all Secondary Stable CASL VRI add-on Trilnos packages with GCC 4.5.1 compiler
+# Build all Secondary Stable CASL VRI add-on Trilnos packages with GCC 4.6.1 compiler
 #
 
-INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.pu241.gcc.4.5.1.cmake")
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.pu241.gcc.4.6.1.cmake")
 INCLUDE("${CTEST_SCRIPT_DIRECTORY}/SubmitToCaslDev.cmake")
 INCLUDE("${CTEST_SCRIPT_DIRECTORY}/casl-vri-packages-coupled.cmake")
 
@@ -14,8 +14,7 @@ SET(BUILD_DIR_NAME MPI_DEBUG_GCC_CASLDEV)
 SET(Trilinos_PACKAGES STARCCM DeCART CASLBOA CASLRAVE LIME VRIPSS)
 SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
 SET(EXTRA_CONFIGURE_OPTIONS
-  -DTrilinos_ENABLE_CASLRAVEANC:BOOL=OFF
-  -DTrilinos_ENABLE_CASLRAVEANCKVIPRE:BOOL=OFF
+  -DTrilinos_ENABLE_Kokkos:BOOL=OFF
   ${EXTRA_CONFIGURE_OPTIONS}
   -DTPL_ENABLE_Netcdf=ON
   )
