5df2ae575ef57b36a723e1e6cef7eb0b8cacc55a
Will Dicharry <wdichar@sandia.gov>
2011-08-22 14:51:21 -0600

    Add initial scripts for nightly testing on brain.
    The implementation currently includes only an optimized MPI
    build.

---
+++ cmake/ctest/drivers/brain/cron_driver.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo
+echo "Starting nightly Trilinos development testing on brain: `date`"
+echo
+
+# TrilinosDriver settings:
+#
+
+export TDD_GIT_EXE=/usr/local/git/bin/git
+export TDD_PARALLEL_LEVEL=8
+export TDD_CTEST_TEST_TYPE=Nightly
+export TDD_IN_TESTING_MODE=OFF
+
+# CTest Settings
+#
+export CTEST_DO_SUBMIT=FALSE
+export CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=TRUE
+
+# Trilinos settings:
+#
+# export Trilinos_PACKAGES=Teuchos
+
+# Machine specific environment:
+#
+
+# Machine independent cron_driver:
+#
+
+SCRIPT_DIR=`cd "\`dirname \"$0\"\`";pwd`
+$SCRIPT_DIR/../cron_driver.py
+
+echo
+echo "Ending nightly Trilinos development testing on brain: `date`"
+echo
