824cc77f21ca332c7cf57b8ca009bcfd913d1097
Will Dicharry <wdichar@sandia.gov>
2011-08-23 08:23:23 -0600

    Changed brain track to experimental and enabled submissions.

--- cmake/ctest/drivers/brain/cron_driver.sh
+++ cmake/ctest/drivers/brain/cron_driver.sh
@@ -9,12 +9,12 @@ echo
 
 export TDD_GIT_EXE=/usr/local/git/bin/git
 export TDD_PARALLEL_LEVEL=8
-export TDD_CTEST_TEST_TYPE=Nightly
+export TDD_CTEST_TEST_TYPE=Experimental
 export TDD_IN_TESTING_MODE=ON
 
 # CTest Settings
 #
-export CTEST_DO_SUBMIT=FALSE
+export CTEST_DO_SUBMIT=TRUE
 export CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=TRUE
 
 # Trilinos settings:
