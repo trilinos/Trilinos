8c84b3299ab54ba2ccfd3f1f71aea678999d5cde
Roscoe A. Bartlett <bartlettra@ornl.gov>
2011-08-26 08:25:12 -0400

    Aggregating all CASL add-on package enables into one file, enabling Panzer and Drekar
    Here I have aggregated all of the CASL add-on packages into a single
    file to avoid duplication.
    
    I have also enabled Panzer and Drekar for MPI but not SERIAL.
    
    I added hard disabled of MPI in all SERIAL builds to make sure that no
    MPI-dependent packages get enabled.

--- cmake/ctest/drivers/pu241/ExtraExternalRepositories.casl_vri.cmake
+++ cmake/ctest/drivers/pu241/ExtraExternalRepositories.casl_vri.cmake
@@ -10,6 +10,7 @@ SET( Trilinos_EXTRAREPOS_DIR_REPOTYPE_REPOURL_PACKSTAT_CATEGORY
   #   https://software.sandia.gov/svn/public/dakota/public/trunk  NOPACKAGES  Nightly  
   StarCCMExt      ""                                  GIT  casl-dev.ornl.gov:/git-root/StarCCMClient  ""  Continuous
   DeCARTExt       ""                                  GIT  casl-dev.ornl.gov:/git-root/casl_decart    ""  Continuous
+  Panzer          ""                                  GIT  software.sandia.gov:/space/git/Panzer      ""  Continuous
   NeutronicsExt   "NeutronicsExt"                     GIT  casl-dev.ornl.gov:/git-root/denovoExt      ""  Continuous
   Denovo          "NeutronicsExt/denovo"              GIT  angmar.ornl.gov:/data/git/denovo.git       NOPACKAGES  Continuous
   Nemesis         "NeutronicsExt/denovo/src/nemesis"  GIT  angmar.ornl.gov:/data/git/nemesis.git      NOPACKAGES  Continuous
