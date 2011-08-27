6472e06e90d1b8268b9791a51e88ecae88f41bbf
Roscoe A. Bartlett <bartlettra@ornl.gov>
2011-08-27 01:24:15 -0400

    Added a little more documentation and a warning about usage.

--- commonTools/git/sparse_checkout.sh
+++ commonTools/git/sparse_checkout.sh
@@ -4,10 +4,15 @@
 # checkout of directories.  For example:
 #
 #  $ cd Trilinos
-#  $ ./commonTools/git/sparse_checkout.sh commonTools
+#  $ ./commonTools/git/sparse_checkout.sh commonTools packages
 #  $ ls
 #
-# should return just 'commonTools'
+# should return just 'commonTools' and 'packages'
+#
+# Warning: Make sure you run this script before you change any files
+# or the 'git read-tree ...' command will blow them away (a lesson I
+# learned the hard way).  In general, commit your work *before* your
+# run this script.
 
 DIRS_FILES_LIST=$@
 
@@ -17,9 +22,9 @@ git config core.sparsecheckout true
 # Set up the list of dirs/files for sparse checkout
 SC_FILE=.git/info/sparse-checkout
 echo > $SC_FILE
-for dir_file in $DIRS_FILES_LIST ; do
-  echo "File/directory to keep: $dir_file";
-  echo $dir_file >> $SC_FILE
+for dir_or_file in $DIRS_FILES_LIST ; do
+  echo "File/directory to keep: $dir_or_file";
+  echo $dir_or_file >> $SC_FILE
 done
 
 # Tell git to process the list of dirs/files
