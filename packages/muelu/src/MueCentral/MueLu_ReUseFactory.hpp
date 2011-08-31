6088cbe7aa6f362627f1bc3411737d776d7bd448
Jeremie Gaidamour <jngaida@sandia.gov>
2011-08-31 15:05:15 -0600

    MueLu: first sketch for a ReUseFactory

---
+++ packages/muelu/src/MueCentral/MueLu_ReUseFactory.hpp
@@ -0,0 +1,58 @@
+#ifndef MUELU_REUSEFACTORY_HPP
+#define MUELU_REUSEFACTORY_HPP
+
+#include "MueLu_ConfigDefs.hpp"
+#include "MueLu_Exceptions.hpp"
+#include "MueLu_FactoryBase.hpp"
+
+namespace MueLu {
+
+  class ReUseFactory : public FactoryBase {
+
+  public:
+    //! @name Constructors/Destructors.
+    //@{
+
+    //! Constructor.
+    ReUseFactory() //TODO Add argument (std::string, factory) to be able to check if data available
+    {
+
+    }
+
+    //! Destructor.
+    virtual ~ReUseFactory() {}
+    //@}
+
+    //! @name Set/Get methods.
+    //@{
+
+    //TODO Add option to do the build if not available or to throw exception
+
+    //@}
+
+    //! Input
+    //@{
+
+    void DeclareInput(Level &currentLevel) {
+      // TODO
+    }
+
+    //@}
+
+    //! @name Build methods.
+    //@{
+
+    /*! @brief Build aggregates. */
+    virtual bool NewBuild(Level & requestedLevel) const {
+      // TODO: add some tests here, ie throw exception if reuse not possible and option ReBuild=false
+      // or, better, call currentLevel->Get(string_,factory_); to transfer data to "this"
+
+      return true; // ??
+    }
+
+  }; // class ReUseFactory
+
+} //namespace MueLu
+
+#define MUELU_REUSEFACTORY_SHORT
+#endif //ifndef MUELU_REUSEFACTORY_HPP
