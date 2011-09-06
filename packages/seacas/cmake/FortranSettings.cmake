fd7466224f6f2267530221c12d735fd3e039bf68
Greg Sjaardema <gdsjaar@sandia.gov>
2011-09-06 14:59:22 -0600

    SEACAS: Specify fortran/c name mangling using cmake variables

--- packages/seacas/cmake/FortranSettings.cmake
+++ packages/seacas/cmake/FortranSettings.cmake
@@ -1,4 +1,8 @@
-ADD_DEFINITIONS(-DBuild64 -DADDC_)
+ADD_DEFINITIONS(-DBuild64)
+
+IF (${FC_FN_UNDERSCORE} MATCHES "UNDER")
+  ADD_DEFINITIONS(-DADDC_)
+ENDIF()
 
 IF (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
   SET(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fcray-pointer -fdefault-real-8 -fdefault-integer-8")
