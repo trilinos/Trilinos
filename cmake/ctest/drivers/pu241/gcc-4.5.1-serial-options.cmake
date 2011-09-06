0d8fd41beb721774a40ad0330049edf4f2455645
Christopher G. Baker <bakercg@ornl.gov>
2011-09-06 10:39:43 -0400

    Added fortran flags needed to build BOA/RAVE components

--- cmake/ctest/drivers/pu241/gcc-4.5.1-serial-options.cmake
+++ cmake/ctest/drivers/pu241/gcc-4.5.1-serial-options.cmake
@@ -60,8 +60,9 @@ INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cm
 # SET(LAPACK_INCLUDE_DIRS ${BLAS_INCLUDE_DIRS}                             CACHE PATH     "Path to MKL LAPACK Fortran modules compatible with GCC 4.5.1")
 
 SET(CMAKE_Fortran_COMPILER "${INTEL_COMPILER_BASE}/bin/intel64/ifort" CACHE FILEPATH "")
-SET(BLAS_INCLUDE_DIRS   ${MKLROOT}/include/intel64/lp64                     CACHE PATH  "Path to MKL BLAS Fortran modules compatible with Intel fortran")
-SET(LAPACK_INCLUDE_DIRS ${BLAS_INCLUDE_DIRS}                                CACHE PATH  "Path to MKL LAPACK Fortran modules compatible with Intel fortran")
+SET(BLAS_INCLUDE_DIRS   ${MKLROOT}/include/intel64/lp64               CACHE PATH     "Path to MKL BLAS Fortran modules compatible with Intel fortran")
+SET(LAPACK_INCLUDE_DIRS ${BLAS_INCLUDE_DIRS}                          CACHE PATH     "Path to MKL LAPACK Fortran modules compatible with Intel fortran")
+SET(CMAKE_Fortran_FLAGS "-save -zero -nocheck -threads -fpp"          CACHE STRING   "")
 
 SET(CMAKE_CXX_COMPILER "${TRILINOS_TOOLSET_BASE}/bin/g++" CACHE FILEPATH "")
 SET(CMAKE_C_COMPILER "${TRILINOS_TOOLSET_BASE}/bin/gcc" CACHE FILEPATH "")
