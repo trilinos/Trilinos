895735ae7a0fc65299de69e5e84d15b98b201ac1
Jeremie Gaidamour <jngaida@sandia.gov>
2011-09-09 09:50:32 -0600

    MueLu: unit_test: simplified AmesosSmoother tests

--- packages/muelu/test/unit_tests/Smoothers/AmesosSmoother.cpp
+++ packages/muelu/test/unit_tests/Smoothers/AmesosSmoother.cpp
@@ -1,75 +1,42 @@
 #include "Teuchos_UnitTestHarness.hpp"
 #include "MueLu_TestHelpers.hpp"
-#include "MueLu_Version.hpp"
+#include "MueLu_TestHelpersSmoothers.hpp"
 
-#include "MueLu_Level.hpp"
-#include "MueLu_SmootherFactory.hpp"
 #include "MueLu_AmesosSmoother.hpp"
-#include "MueLu_Utilities.hpp"
 
 #include "MueLu_UseDefaultTypes.hpp"
 #include "MueLu_UseShortNames.hpp"
 
 namespace MueLuTests {
 
+  using namespace Smoother;
+
   TEUCHOS_UNIT_TEST(AmesosSmoother, NotSetup)
   {
     MUELU_TEST_ONLY_FOR(Xpetra::UseEpetra)
       {
-        out << "version: " << MueLu::Version() << std::endl;
 
-        GO nx,ny,nz;
-        nx = ny = nz = 5;
-        GO numGlobalElements = nx*ny*nz;
-        RCP<const Map> map = MapFactory::Build(Parameters::getLib(), numGlobalElements, 0, Parameters::getDefaultComm());
+        AmesosSmoother smoother("Amesos_Klu", Teuchos::ParameterList());
+        testApplyNoSetup(smoother, out, success);
 
-        Teuchos::ParameterList amesosList;
-        RCP<AmesosSmoother>    smoother = rcp( new AmesosSmoother("Amesos_Klu",amesosList) );
-
-        RCP<MultiVector> X   = MultiVectorFactory::Build(map,1);
-        RCP<MultiVector> RHS = MultiVectorFactory::Build(map,1);
-
-        //try applying without setting up
-        TEST_THROW( smoother->Apply(*X,*RHS), MueLu::Exceptions::RuntimeError );
       }
   }
 
-  TEUCHOS_UNIT_TEST(AmesosSmoother, KLUSolve)
+  TEUCHOS_UNIT_TEST(AmesosSmoother, Apply_Correctness_KLU)
   {
     MUELU_TEST_ONLY_FOR(Xpetra::UseEpetra)
       {
-        out << "version: " << MueLu::Version() << std::endl;
 
-        Teuchos::ParameterList amesosList;
-        amesosList.set("PrintTiming",false);
-        amesosList.set("OutputLevel",0);
-        RCP<AmesosSmoother> smoother = rcp( new AmesosSmoother("Amesos_Klu",amesosList) );
+        AmesosSmoother smoother("Amesos_Klu", Teuchos::ParameterList());
+        ST::magnitudeType residualNorms = testApply_A125_X0_RandomRHS(smoother, out, success);
+        TEST_EQUALITY(residualNorms < 1e-12, true);
 
-        Level level;
-        MueLu::TestHelpers::Factory<SC,LO,GO,NO,LMO>::createSingleLevelHierarchy(level);
-
-        RCP<Operator> Op = MueLu::TestHelpers::Factory<SC,LO,GO,NO,LMO>::Build1DPoisson(125);
-        level.Set("A",Op);
-
-        smoother->Setup(level);
-
-        RCP<MultiVector> X   = MultiVectorFactory::Build(Op->getDomainMap(),1);
-        RCP<MultiVector> RHS = MultiVectorFactory::Build(Op->getDomainMap(),1);
-
-        X->setSeed(846930886);
-        X->randomize();
-        Op->apply(*X,*RHS,Teuchos::NO_TRANS,(SC)1.0,(SC)0.0);
-
-        X->putScalar((SC) 0.0);
-        Teuchos::Array<ST::magnitudeType> res;
-        res = Utils::ResidualNorm(*Op,*X,*RHS);
-        out << "||initial residual|| = " << res[0] << std::endl;
-
-        smoother->Apply(*X,*RHS);
-        res = Utils::ResidualNorm(*Op,*X,*RHS);
-        out << "||final residual|| = " << res[0] << std::endl;
-        TEST_EQUALITY(res[0] < 1e-12, true);
       }
   }
   
 } // namespace MueLuTests
+
+//TODO
+// Test with SuperLU
+// Test with invalid std::string
+// Test if paramList takes into account
