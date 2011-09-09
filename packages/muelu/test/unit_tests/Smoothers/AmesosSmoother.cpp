5b7ed642c2b2cec6beb722c1e1bb8e48eff928da
Jeremie Gaidamour <jngaida@sandia.gov>
2011-09-09 10:46:33 -0600

    MueLu: unit_test: updated Amesos tests - added #ifdef safeguards - change keys "Amesos_Klu" to "Klu", because "Klu" is valid for both Amesos and Amesos2 - added a test with SuperLU

--- packages/muelu/test/unit_tests/Smoothers/AmesosSmoother.cpp
+++ packages/muelu/test/unit_tests/Smoothers/AmesosSmoother.cpp
@@ -1,4 +1,5 @@
-#include "Teuchos_UnitTestHarness.hpp"
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Amesos_config.h>
 #include "MueLu_TestHelpers.hpp"
 #include "MueLu_TestHelpersSmoothers.hpp"
 
@@ -15,28 +16,34 @@ namespace MueLuTests {
   {
     MUELU_TEST_ONLY_FOR(Xpetra::UseEpetra)
       {
-
-        AmesosSmoother smoother("Amesos_Klu", Teuchos::ParameterList());
+#ifdef HAVE_AMESOS_KLU
+        AmesosSmoother smoother("Klu", Teuchos::ParameterList());
         testApplyNoSetup(smoother, out, success);
-
+#endif
       }
   }
 
-  TEUCHOS_UNIT_TEST(AmesosSmoother, Apply_Correctness_KLU)
+  TEUCHOS_UNIT_TEST(AmesosSmoother, Apply_Correctness)
   {
     MUELU_TEST_ONLY_FOR(Xpetra::UseEpetra)
       {
-
-        AmesosSmoother smoother("Amesos_Klu", Teuchos::ParameterList());
+#ifdef HAVE_AMESOS_KLU
+        AmesosSmoother smoother("Klu", Teuchos::ParameterList());
         ST::magnitudeType residualNorms = testApply_A125_X0_RandomRHS(smoother, out, success);
         TEST_EQUALITY(residualNorms < 1e-12, true);
+#endif
 
+#ifdef HAVE_AMESOS_SUPERLU
+        AmesosSmoother smoother("Superlu", Teuchos::ParameterList());
+        ST::magnitudeType residualNorms = testApply_A125_X0_RandomRHS(smoother, out, success);
+        TEST_EQUALITY(residualNorms < 1e-12, true);
+#endif
       }
   }
   
 } // namespace MueLuTests
 
 //TODO
-// Test with SuperLU
 // Test with invalid std::string
 // Test if paramList takes into account
+// Check if all the defaults that are used by MueLu are tested
