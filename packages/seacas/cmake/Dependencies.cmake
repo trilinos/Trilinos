eb1fba4629ceba518ab7ec6663e38dbf7f823081
Brent Perschbacher <bmpersc@sandia.gov>
2011-09-09 09:21:52 -0600

    Changed which subpackages are required for SEACAS
    This updates all subpackages for SEACAS should be required to make it easier to
    enable everything inside of SEACAS. Packages that have dependencies on on parts
    of SEACAS such as Ioss should still state their dependence at the subpackage
    level to avoid enabling more than they actually need.
    
    Build/Test Cases Summary
    Enabled Packages: TrilinosFramework, SEACAS
    Enabled all Forward Packages
    0) MPI_DEBUG => passed: passed=88,notpassed=0 (5.52 min)
    1) SERIAL_RELEASE => passed: passed=88,notpassed=0 (5.27 min)
    2) MPI_DEBUG_SS => passed: passed=99,notpassed=0 (38.24 min)
    3) SERIAL_RELEASE_SS => passed: passed=99,notpassed=0 (38.19 min)
    Other local commits for this build/test group: ada88c5

--- packages/seacas/cmake/Dependencies.cmake
+++ packages/seacas/cmake/Dependencies.cmake
@@ -1,29 +1,29 @@
 SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
   Exodus      libraries/exodus        SS  REQUIRED
-  Exodus_for  libraries/exodus_for    SS  OPTIONAL
-  Nemesis     libraries/nemesis       SS  OPTIONAL
-  Ioss        libraries/ioss          SS  OPTIONAL
-  Chaco       libraries/chaco         SS  OPTIONAL
-  Aprepro_lib libraries/aprepro_lib   SS  OPTIONAL
-  Supes       libraries/supes         SS  OPTIONAL
-  Suplib      libraries/suplib        SS  OPTIONAL
-  Algebra     applications/algebra    SS  OPTIONAL
-  Aprepro     applications/aprepro    SS  OPTIONAL
-  Conjoin     applications/conjoin    SS  OPTIONAL
-  Ejoin       applications/ejoin      SS  OPTIONAL
-  Epu         applications/epu        SS  OPTIONAL
+  Exodus_for  libraries/exodus_for    SS  REQUIRED
+  Nemesis     libraries/nemesis       SS  REQUIRED
+  Ioss        libraries/ioss          SS  REQUIRED
+  Chaco       libraries/chaco         SS  REQUIRED
+  Aprepro_lib libraries/aprepro_lib   SS  REQUIRED
+  Supes       libraries/supes         SS  REQUIRED
+  Suplib      libraries/suplib        SS  REQUIRED
+  Algebra     applications/algebra    SS  REQUIRED
+  Aprepro     applications/aprepro    SS  REQUIRED
+  Conjoin     applications/conjoin    SS  REQUIRED
+  Ejoin       applications/ejoin      SS  REQUIRED
+  Epu         applications/epu        SS  REQUIRED
   Exo2mat     applications/exo2mat    SS  OPTIONAL
-  Exodiff     applications/exodiff    SS  OPTIONAL
-  Exomatlab   applications/exomatlab  SS  OPTIONAL
-  Exotxt      applications/exotxt     SS  OPTIONAL
-  Gjoin       applications/gjoin      SS  OPTIONAL
-  Grope       applications/grope      SS  OPTIONAL
-  Mapvar      applications/mapvar     SS  OPTIONAL
-  Mapvar-kd   applications/mapvar-kd  SS  OPTIONAL
+  Exodiff     applications/exodiff    SS  REQUIRED
+  Exomatlab   applications/exomatlab  SS  REQUIRED
+  Exotxt      applications/exotxt     SS  REQUIRED
+  Gjoin       applications/gjoin      SS  REQUIRED
+  Grope       applications/grope      SS  REQUIRED
+  Mapvar      applications/mapvar     SS  REQUIRED
+  Mapvar-kd   applications/mapvar-kd  SS  REQUIRED
   Mat2exo     applications/mat2exo    SS  OPTIONAL
-  Nemslice    applications/nem_slice  SS  OPTIONAL
-  Nemspread   applications/nem_spread SS  OPTIONAL
-  Txtexo      applications/txtexo     SS  OPTIONAL
+  Nemslice    applications/nem_slice  SS  REQUIRED
+  Nemspread   applications/nem_spread SS  REQUIRED
+  Txtexo      applications/txtexo     SS  REQUIRED
   )
 
 SET(LIB_REQUIRED_DEP_PACKAGES)
