eb619cd39935d19618d6933d035733901a16d917
Jeremie Gaidamour <jngaida@sandia.gov>
2011-09-09 11:02:19 -0600

    MueLu: unit_test: new: Amesos2 tests

---
+++ packages/muelu/test/unit_tests/Smoothers/Amesos2Smoother.cpp
@@ -0,0 +1,42 @@
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Amesos2_config.h>
+#include "MueLu_TestHelpers.hpp"
+#include "MueLu_TestHelpersSmoothers.hpp"
+
+#include "MueLu_Amesos2Smoother.hpp"
+
+#include "MueLu_UseDefaultTypes.hpp"
+#include "MueLu_UseShortNames.hpp"
+
+namespace MueLuTests {
+
+  using namespace Smoother;
+
+  TEUCHOS_UNIT_TEST(Amesos2Smoother, NotSetup)
+  {
+    MUELU_TEST_ONLY_FOR(Xpetra::UseTpetra)
+      {
+#ifdef HAVE_AMESOS_KLU
+        Amesos2Smoother smoother("Klu", Teuchos::ParameterList());
+        testApplyNoSetup(smoother, out, success);
+#endif
+      }
+  }
+
+  TEUCHOS_UNIT_TEST(Amesos2Smoother, Apply_Correctness)
+  {
+    MUELU_TEST_ONLY_FOR(Xpetra::UseTpetra)
+      {
+#ifdef HAVE_AMESOS_KLU
+        Amesos2Smoother smoother("Klu", Teuchos::ParameterList());
+        testDirectSolver(smoother, out, success);
+#endif
+
+#ifdef HAVE_AMESOS_SUPERLU
+        Amesos2Smoother smoother("Superlu", Teuchos::ParameterList());
+        testDirectSolver(smoother, out, success);
+#endif
+      }
+  }
+  
+} // namespace MueLuTests
