f3523596eee80da9c40a4e7f50d61d937d379c9d
Christopher G. Baker <bakercg@ornl.gov>
2011-09-11 21:16:32 -0400

    moved ifort flags from serial-options to base-options (moved back? who changed this? MPI builds of RAVE still need these, i'm told) split PVM TPL into PVMHeaders and PVMLibraries TPLs

--- cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
+++ cmake/ctest/drivers/pu241/gcc-4.5.1-base-options.cmake
@@ -57,12 +57,14 @@ INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/mkl-12.0.4-options.cmake)
 
 INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/casl-vri-tpls.cmake)
 
-SET(PVM_LIBRARY_DIRS /opt/gcc-4.5.1/tpls/pvm3/lib/LINUX64                    CACHE FILEPATH "")
-SET(PVM_INCLUDE_DIRS /opt/gcc-4.5.1/tpls/pvm3/include                        CACHE FILEPATH "")
+SET(PVMLibraries_LIBRARY_DIRS /opt/gcc-4.5.1/tpls/pvm3/lib/LINUX64                    CACHE FILEPATH "")
+SET(PVMHeaders_INCLUDE_DIRS /opt/gcc-4.5.1/tpls/pvm3/include                        CACHE FILEPATH "")
 SET(HDF5_LIBRARY_NAMES "hdf5_hl;hdf5;hdf5_cpp"                               CACHE STRING   "")
 SET(HDF5_LIBRARY_DIRS /opt/gcc-4.5.1/tpls/hdf5-1.8.5-patch1/lib              CACHE FILEPATH "")
 SET(HDF5_INCLUDE_DIRS /opt/gcc-4.5.1/tpls/hdf5-1.8.5-patch1/include          CACHE FILEPATH "")
 SET(EpetraExt_ENABLE_HDF5 OFF                                                CACHE BOOL "")
+
+SET(CMAKE_Fortran_FLAGS "-save -zero -nocheck -threads"          CACHE STRING   "")
 
 # To avoid problem with EpetraExt_inout_test failure in optimized code
 SET(Epetra_ENABLE_Fortran OFF CACHE BOOL "")
--- cmake/ctest/drivers/pu241/intel-12.0.4-options.cmake
+++ cmake/ctest/drivers/pu241/intel-12.0.4-options.cmake
@@ -60,8 +60,8 @@ SET(LAPACK_INCLUDE_DIRS ${BLAS_INCLUDE_DIRS}                                CACH
 INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/boost-1.46.1-options.cmake)
 INCLUDE(${TRILINOS_HOME_DIR}/cmake/ctest/drivers/pu241/casl-vri-tpls.cmake)
 
-SET(PVM_LIBRARY_DIRS /opt/intel-11.1.064/tpls/pvm3/lib/LINUX64                    CACHE FILEPATH "")
-SET(PVM_INCLUDE_DIRS /opt/intel-11.1.064/tpls/pvm3/include                        CACHE FILEPATH "")
+SET(PVMLibraries_LIBRARY_DIRS /opt/intel-11.1.064/tpls/pvm3/lib/LINUX64                    CACHE FILEPATH "")
+SET(PVMHeaders_INCLUDE_DIRS /opt/intel-11.1.064/tpls/pvm3/include                        CACHE FILEPATH "")
 SET(HDF5_LIBRARY_NAMES "hdf5_hl;hdf5;hdf5_cpp"                                    CACHE STRING   "")
 SET(HDF5_LIBRARY_DIRS /opt/intel-11.1.064/tpls/hdf5-1.8.5-patch1/lib              CACHE FILEPATH "")
 SET(HDF5_INCLUDE_DIRS /opt/intel-11.1.064/tpls/hdf5-1.8.5-patch1/include          CACHE FILEPATH "")
