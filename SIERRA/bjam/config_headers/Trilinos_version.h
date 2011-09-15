7eef780e986fec32be6c73b99299f088c7656683
Brent Perschbacher <bmpersc@sandia.gov>
2011-09-15 11:00:53 -0600

    Update Trilinos version to 10.9
    Updating the development version to 10.9 now that the 10.8 branch has been made.

--- SIERRA/bjam/config_headers/Trilinos_version.h
+++ SIERRA/bjam/config_headers/Trilinos_version.h
@@ -118,7 +118,7 @@
 * version 10.2.4 is designated 100204.  This preserves the comparability of
 * these version numbers with simple comparison operators used in #ifdef tests.
 */
-#define TRILINOS_MAJOR_MINOR_VERSION 100700
+#define TRILINOS_MAJOR_MINOR_VERSION 100900
 
 /* NOTE: These macros are given long int values to allow comparisons in
  * preprocessor #if statements.  For example, you can do comparisons with ==,
@@ -133,6 +133,6 @@
  *
  * NOTE: This string is to be used for outputting, not for comparison logic.
  */
-#define TRILINOS_VERSION_STRING "10.7 (Dev)"
+#define TRILINOS_VERSION_STRING "10.9 (Dev)"
 
 #endif /* TRILINOS_VERSION_H */
