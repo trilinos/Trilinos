5d62a1cecfb5fa69ab34a716d2f513ed8737d90e
Will Dicharry <wdichar@sandia.gov>
2011-09-26 10:27:26 -0600

    Relocate GCC 4.6.1 testing on Zan to Brain.

--- cmake/ctest/drivers/brain/ctest_linux_nightly_serial_development_debug_gcc_4.6.1_brain.cmake
+++ cmake/ctest/drivers/brain/ctest_linux_nightly_serial_development_debug_gcc_4.6.1_brain.cmake
@@ -54,17 +54,16 @@
 # @HEADER
 
 
-INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.zan.gcc.cmake")
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/TrilinosCTestDriverCore.brain.gcc-4.6.1.cmake")
 
 #
 # Set the options specific to this build case
 #
 
-SET(COMM_TYPE MPI)
-SET(BUILD_TYPE RELEASE)
-SET(BUILD_DIR_NAME MPI_OPT_DEV2)
+SET(COMM_TYPE SERIAL)
+SET(BUILD_TYPE DEBUG)
+SET(BUILD_DIR_NAME SERIAL_DEBUG_DEV2)
 SET(COMPILER_VERSION "GCC-4.6.1")
-SET(ENV{LD_LIBRARY_PATH} "/home/jmwille/install/mpc-0.9/lib:/home/jmwille/install/mpfr-2.4.2/lib:/home/jmwille/install/gmp-4.3.2/lib:/home/jmwille/install/gcc-4.6.1/lib64:$ENV{LD_LIBRARY_PATH}")
 SET(CTEST_TEST_TYPE EXPERIMENTAL)
 #SET(CTEST_TEST_TIMEOUT 900)
 
@@ -73,15 +72,15 @@ SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
 #implicitly enabled.  Sundance depends on Stokhos.
 #SET(EXTRA_EXCLUDE_PACKAGES Phalanx Stokhos Sundance)
 
+# FIXME: add boost back to the build, path was:
+# "-DBoost_INCLUDE_DIRS=/home/trilinos/tpl/gcc4.4.4/boost_1_46_1"
 SET( EXTRA_CONFIGURE_OPTIONS
   "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON"
   "-DTrilinos_DATA_DIR:STRING=$ENV{TRILINOSDATADIRECTORY}"
   "-DTPL_ENABLE_Pthread:BOOL=ON"
   "-DNOX_ENABLE_ABSTRACT_IMPLEMENTATION_LAPACK=ON"
-  "-DMPI_BASE_DIR:PATH=/home/jmwille/install/gcc-4.6.1/openmpi-1.4.3"
-  "-DBoost_INCLUDE_DIRS=/home/trilinos/tpl/gcc4.4.4/boost_1_46_1"
-  "-DNetcdf_INCLUDE_DIRS=/home/trilinos/tpl/gcc4.4.4/netcdf-4.1.3/include"
-  "-DNetcdf_LIBRARY_DIRS=/home/trilinos/tpl/gcc4.4.4/netcdf-4.1.3/lib"
+  "-DNetcdf_INCLUDE_DIRS=$ENV{HOME}/tpls/gcc/4.6.1/netcdf/4.1.3/include"
+  "-DNetcdf_LIBRARY_DIRS=$ENV{HOME}/tpls/gcc/4.6.1/netcdf/4.1.3/lib"
   )
 
 #
