dc3ac24c704a3a32ea6fe0c96aeb8d4445ab74e5
Jonathan Hu <jhu@sandia.gov>
2011-09-26 16:26:55 -0700

    Fixing two bugs in MakeTentative that Chris Siefert reported.
    1) Adding check to ensure that the local process has received some parts of Ptent (i.e., some locally owned rows/dofs belong to a remote
    aggregate, so another process did the QR) before trying to insert into Ptent.
    
    2) Fixing a bug in the case that the local process owns no aggregates.  I was using an ArrayRCP of zero length :(.
    
    Also added some comments.

--- packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_TentativePFactory.hpp
+++ packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_TentativePFactory.hpp
@@ -129,7 +129,40 @@ namespace MueLu {
     
     /*! @brief Make tentative prolongator with QR.
 
-    - FIXME There is no attempt to detect if the aggregate is too small to support the NS.
+      We note that the implementation would have been *much* easier
+      if Ptent were allowed to have a row map based upon aggregates, i.e., a row
+      map such that all DoF's in an aggregate were local and consecutive.
+      In this case, we could still have used the matrix A's domain map to FillComplete Ptent.
+      However, the prolongator smoothing step Ptent - A*Ptent would then be impossible
+      because neither Epetra nor Tpetra support adding matrices with differing row maps.
+
+      The following is a high-level view of how this method is implemented.  The result is a tentative
+      prolongator such that Ptent has the same row map as A.
+
+      1) The nullspace NS is communicated in such a way that if proc A owns aggregate k, the part of NS
+         corresponding to k is replicated on A.  This will happen if aggregate k contains DoFs belonging
+         to proc A and other procs.
+
+      2) Each processor A does a local QR on the parts of the NS corresponding to aggregates that A owns.
+
+      3) The rows of Q that A owns (i.e., rows of Q corresponding to DoFs that A owns) are inserted immediately into Ptentative.
+
+      4) Any rows of Q that A doesn't own  are set  to the owning processor as follows:
+         (This step is necessary because Epetra does not allow insertion of rows owned by other processors.)
+
+         a) Q is stored as a CSR matrix.  We know each row has at most dim(NS) nonzeros.
+            For each row of Q, we must transmit both the values and columns.  We do this by communicating separate vectors,
+            each of length dim(NS), for the values and column indices.
+
+         b) We try to make this communication more efficient by first communicating just the global row numbers
+            that each processor should expect to receive. A "reduced" map is created from just the expected row numbers.
+
+         c) We then communicate the rows of Q themselves using this "reduced" map, i.e., the target of the Importer is the reduced map.
+            Otherwise, the only alternative was to base the Importer on A's rowmap, which is probably much larger than the reduced map.
+
+      5) Once received, the rows are inserted by the owning processes and Ptent is fillCompleted.
+
+      - FIXME There is no attempt to detect if the aggregate is too small to support the NS.
     */
      void MakeTentative(const Operator& fineA, const Aggregates& aggregates, const MultiVector & fineNullspace, //-> INPUT
                         RCP<MultiVector> & coarseNullspace, RCP<Operator> & Ptentative) const                   //-> OUTPUT 
@@ -207,9 +240,12 @@ namespace MueLu {
                                                     indexBase, fineA.getRowMap()->getComm()); //JG:Xpetra::global_size_t>?
 
       coarseNullspace = MultiVectorFactory::Build(coarseMap,NSDim);
+
+      int mypid = fineA.getRowMap()->getComm()->getRank();
+
       ArrayRCP< ArrayRCP<SC> > coarseNS(NSDim);
       for (size_t i=0; i<NSDim; ++i)
-        coarseNS[i] = coarseNullspace->getDataNonConst(i);
+        if (coarseMap->getNodeNumElements() > 0) coarseNS[i] = coarseNullspace->getDataNonConst(i);
 
       // Builds overlapped nullspace.
       const RCP<const Map> nonUniqueMap = aggregates.GetMap();
@@ -234,8 +270,8 @@ namespace MueLu {
       ArrayRCP<GO> colPtr(maxAggSize*NSDim,0);
       ArrayRCP<SC> valPtr(maxAggSize*NSDim,0.);
 
-      // Ptentative's row map is such that all DoF's in an aggregate (NO LONGER TRUE)
-      // are local and consecutive.  Use fineA's domain map to FillComplete. (STILL TRUE)
+      // Note! DoF's in an aggregate may not be local or consecutive.
+      // Use fineA's domain map to FillComplete.
 
       //This makes the rowmap of Ptent the same as that of fineA.
       //This requires moving some parts of some local Q's to other processors
@@ -477,12 +513,16 @@ namespace MueLu {
         }
       }
 
+      valPtr = ArrayRCP<SC>(NSDim,0.);
+      colPtr = ArrayRCP<GO>(NSDim,0);
       for (typename Array<GO>::iterator r=gidsToImport.begin(); r!=gidsToImport.end(); ++r) {
+        if (targetQvalues->getLocalLength() > 0) {  //TODO put this plus above bit all inside a big if...
         for (size_t j=0; j<NSDim; ++j) {
           valPtr[j] = targetQvals[j][reducedMap->getLocalElement(*r)];
           colPtr[j] = (GO) targetQcols[j][reducedMap->getLocalElement(*r)];
         }
         Ptentative->insertGlobalValues((GO)*r, colPtr.view(0,NSDim), valPtr.view(0,NSDim));
+        } //if (targetQvalues->getLocalLength() > 0)
       }
 
       Ptentative->fillComplete(coarseMap,fineA.getDomainMap()); //(domain,range) of Ptentative
