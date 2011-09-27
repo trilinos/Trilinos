ef48ecdc1faaf5f0f74ceb6a4b5d47084c04dd61
Mark Hoemmen <mhoemme@sandia.gov>
2011-09-27 10:57:56 -0600

    Teuchos: SerializationTraits desperately needed documentation.  I had a few spare minutes so I decided to add some.  I didn't write this class; I had to do "digital forensics" to figure out what it did.  Thus, if you have any complaints, you'll have to go to the original author or fix it yourself.
    Build/Test Cases Summary
    Enabled Packages: Teuchos
    0) MPI_DEBUG => passed: passed=77,notpassed=0 (5.18 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)

--- packages/teuchos/src/Teuchos_SerializationTraits.hpp
+++ packages/teuchos/src/Teuchos_SerializationTraits.hpp
@@ -39,11 +39,9 @@
 // ***********************************************************************
 // @HEADER
 
-
-// NOTE: Before adding specializations of ScalarTraits, make sure that they do not duplicate 
-// specializations already present in PyTrilinos (see packages/PyTrilinos/src/Teuchos_Traits.i)
-
-
+/// \file Teuchos_SerializationTraits.hpp
+/// \brief Teuchos::SerializationTraits and Teuchos::DirectSerializationTraits definitions.
+///
 #ifndef TEUCHOS_SERIALIZATION_TRAITS_HPP
 #define TEUCHOS_SERIALIZATION_TRAITS_HPP
 
@@ -56,7 +54,12 @@
 
 namespace Teuchos {
 
-
+/// \class UndefinedSerializationTraits
+/// \brief Report an error if a specialization of \c SerializationTraits is missing.
+/// 
+/// This class reports a compile-time error if you attempt to
+/// instantiate it.  We use this class to make it easy to detect a
+/// missing specialization of \c SerializationTraits.
 template<typename T>
 struct UndefinedSerializationTraits {
   //! This function should not compile if there is an attempt to instantiate!
@@ -64,20 +67,70 @@ struct UndefinedSerializationTraits {
 };
 
 
-/** \brief Serialization traits class for types that use value semantics.
+/** \class SerializationTraits 
+ * \brief Serialization traits class for types T that use value semantics.
  *
- * There are one of two modes associated with serialization.
+ * This traits class describes how to convert between arrays of T, and
+ * arrays of char.  We call the process of converting from T to char
+ * "serialization," and from char to T "deserialization."  Teuchos
+ * uses serialization and deserialization mainly for implementing
+ * distributed-memory message-passing communication in a generic way.
+ *
+ * \tparam Ordinal The same template parameter as that of \c Comm.
+ *   The integer type used to count the number of packets sent and
+ *   received.
+ *
+ * \tparam T The type of the objects that this class shows how to
+ *   serialize.  
+ *
+ * Teuchos defines specializations of this class for many commonly
+ * used types in distributed-memory communication, such as char, int
+ * (signed and unsigned), float, double, and std::pair<P1, P2> for
+ * certain types P1 and P2.  Depending on your Trilinos build options,
+ * other specializations may be defined as well, for example for long
+ * long int, double-double and quad-double real types (dd_real
+ * resp. qd_real), or certain std::complex<T> specializations.  If a
+ * specialization of this class does not exist for your type T, you
+ * may define your own specialization.
+ *
+ * \note Before defining specializations of this class, make sure that
+ *   they do not duplicate specializations already present in
+ *   PyTrilinos (see packages/PyTrilinos/src/Teuchos_Traits.i)
  * 
- * ToDo: Finish documenation!
+ * There are two different serialization modes: direct and indirect.
+ * "Direct" serialization means that you can convert directly between
+ * an object of type T and an array of char, of a specific length
+ * dependent only on the type T and not on the particular instance.
+ * Specifically, it means you can
+ *
+ * 1. reinterpret_cast a pointer to an instance of T into an array of
+ *    char (which array has length dependent only on the type T and
+ *    not on the specific T instance),
+ * 2. serialize the resulting array of char, and finally
+ * 3. deserialize by reading in the array of char and doing a
+ *    reinterpret_cast back into a T.
+ *
+ * "Indirect" serialization is defined as any serialization method
+ * more general than that.  The \c supportsDirectSerialization class
+ * Boolean tells you whether this specialization supports direct
+ * serialization.
+ *
+ * SerializationTraits is used by classes such as \c
+ * ValueTypeSerializationBuffer, \c ConstValueTypeSerializationBuffer,
+ * \c ValueTypeDeserializationBuffer, and \c
+ * ConstValueTypeDeserializationBuffer.
  */
 template <typename Ordinal, typename T>
 class SerializationTraits {
 public:
   
-  //! @name Seialization type selection 
+  //! @name Serialization type selection 
   //@{
 
-  /** \brief Determines if the type supports direct serialization. */
+  /// \brief Whether the type T supports direct serialization.
+  ///
+  /// See the class documentation for definitions of "direct" and
+  /// "indirect" serialization.
   static const bool supportsDirectSerialization = false;
 
   //@}
@@ -86,22 +139,46 @@ public:
   //@{
 
   /** \brief Return the number of bytes for <tt>count</tt> objects. */
-  static Ordinal fromCountToDirectBytes(const Ordinal count) { (void)count; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static Ordinal fromCountToDirectBytes(const Ordinal count) { 
+    (void)count; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   /** \brief Convert the pointer type to <tt>char*</tt>. */
-  static char* convertToCharPtr( T* ptr ) { (void)ptr; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static char* convertToCharPtr( T* ptr ) { 
+    (void)ptr; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   /** \brief Convert the pointer type to <tt>const char*</tt>. */
-  static const char* convertToCharPtr( const T* ptr ) { (void)ptr; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static const char* convertToCharPtr( const T* ptr ) { 
+    (void)ptr; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   /** \brief Return the number of objects for <tt>bytes</tt> of storage. */
-  static Ordinal fromDirectBytesToCount(const Ordinal bytes) { (void)bytes; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static Ordinal fromDirectBytesToCount(const Ordinal bytes) { 
+    (void)bytes; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   /** \brief Convert the pointer type from <tt>char*</tt>. */
-  static T* convertFromCharPtr( char* ptr ) { (void)ptr; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static T* convertFromCharPtr( char* ptr ) { 
+    (void)ptr; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   /** \brief Convert the pointer type from <tt>char*</tt>. */
-  static const T* convertFromCharPtr( const char* ptr ) { (void)ptr; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static const T* convertFromCharPtr( const char* ptr ) { 
+    (void)ptr; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   //@}
 
@@ -109,7 +186,11 @@ public:
   //@{
 
   /** \brief Return the number of bytes for <tt>count</tt> objects. */
-  static Ordinal fromCountToIndirectBytes(const Ordinal count) { (void)count; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static Ordinal fromCountToIndirectBytes(const Ordinal count) { 
+    (void)count; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   /** \brief Serialize to an indirect <tt>char[]</tt> buffer.
    *
@@ -126,13 +207,21 @@ public:
    * <li><tt>bytes==fromCountToIndirectBytes(count)</tt>
    * </ul>
    */
-  static void serialize(
-    const Ordinal count, const T buffer[], const Ordinal bytes, char charBuffer[]
-    )
-    { (void)count; (void)buffer; (void)bytes; (void)charBuffer; UndefinedSerializationTraits<T>::notDefined(); }
+  static void serialize (const Ordinal count, 
+			 const T buffer[], 
+			 const Ordinal bytes, 
+			 char charBuffer[])
+  { 
+    (void)count; (void)buffer; (void)bytes; (void)charBuffer; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+  }
 
   /** \brief Return the number of objects for <tt>bytes</tt> of storage. */
-  static Ordinal fromIndirectBytesToCount(const Ordinal bytes) { (void)bytes; UndefinedSerializationTraits<T>::notDefined(); return 0; }
+  static Ordinal fromIndirectBytesToCount(const Ordinal bytes) { 
+    (void)bytes; 
+    UndefinedSerializationTraits<T>::notDefined(); 
+    return 0; 
+  }
 
   /** \brief Deserialize from an indirect <tt>char[]</tt> buffer.
    *
@@ -149,17 +238,51 @@ public:
    * <li><tt>count==fromIndirectBytesToCount(bytes)</tt>
    * </ul>
    */
-  static void deserialize(
-    const Ordinal bytes, const char charBuffer[], const Ordinal count, T buffer[]
-    )
-    { (void)bytes; (void)charBuffer; (void)count; (void)buffer;
-      UndefinedSerializationTraits<T>::notDefined(); }
+  static void deserialize (const Ordinal bytes, 
+			   const char charBuffer[], 
+			   const Ordinal count, 
+			   T buffer[])
+  { 
+    (void)bytes; (void)charBuffer; (void)count; (void)buffer;
+    UndefinedSerializationTraits<T>::notDefined(); 
+  }
   
   //@}
 
 };
 
-
+/// \class DirectSerializationTraits
+/// \brief Serialization traits for objects that support direct serialization.
+///
+/// "Direct" serialization means that you can convert directly
+/// between an object of type T and an array of char, of a specific
+/// length dependent only on the type T and not on the particular
+/// instance.  Specifically, it means you can
+///
+/// 1. reinterpret_cast a pointer to an instance of T into an 
+///    array of char (which array has length dependent only on 
+///    the type T and not on the specific T instance),
+/// 2. serialize the resulting array of char, and finally
+/// 3. deserialize by reading in the array of char and doing a 
+///    reinterpret_cast back into a T.
+///
+/// "Indirect" serialization is defined as any serialization method
+/// more general than that.
+///
+/// We use partial specializations of DirectSerializationTraits
+/// (specialized on certain T types, not Ordinal) as public base
+/// classes for the corresponding SerializationTraits specialization.
+/// This provides high-performance default implementations of
+/// serialization for commonly used types T (including char, int, and
+/// double).
+///
+/// \tparam Ordinal The same template parameter as that of \c Comm.
+///   The integer type used to count the number of packets sent and
+///   received.
+///
+/// \tparam T The type of the objects that this class shows how to
+///   serialize.  
+///
 template <typename Ordinal, typename T>
 class DirectSerializationTraits {
 public:
