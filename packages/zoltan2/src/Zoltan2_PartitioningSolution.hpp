6472747e7b1093f31792a1d81d26cff1f2cd8efa
K Devine <kddevin@sandia.gov>
2011-10-05 10:52:34 -0600

    Placeholder for solution classes.

---
+++ packages/zoltan2/src/Zoltan2_PartitioningSolution.hpp
@@ -0,0 +1,34 @@
+// @HEADER
+// ***********************************************************************
+//
+//                Copyright message goes here.   TODO
+//
+// ***********************************************************************
+// @HEADER
+
+/*! \file Zoltan2_Model.hpp
+
+    \brief The abstract interface for a computational model.
+*/
+
+
+#ifndef _ZOLTAN2_PARTITIONINGSOLUTION_HPP_
+#define _ZOLTAN2_PARTITIONINGSOLUTION_HPP_
+
+#include <Zoltan2_Standards.hpp>
+#include <Zoltan2_Solution.hpp>
+
+namespace Zoltan2 {
+
+/*! Zoltan2::PartitioningSolution
+    Just a placeholder for now.
+*/
+
+template <Z2CLASS_TEMPLATE>
+  class PartitioningSolution : public Solution<Z2PARAM_TEMPLATE>
+{
+};
+
+}
+
+#endif
---
+++ packages/zoltan2/src/Zoltan2_Solution.hpp
@@ -0,0 +1,33 @@
+// @HEADER
+// ***********************************************************************
+//
+//                Copyright message goes here.   TODO
+//
+// ***********************************************************************
+// @HEADER
+
+/*! \file Zoltan2_Model.hpp
+
+    \brief The abstract interface for a computational model.
+*/
+
+
+#ifndef _ZOLTAN2_SOLUTION_HPP_
+#define _ZOLTAN2_SOLUTION_HPP_
+
+#include <Zoltan2_Standards.hpp>
+
+namespace Zoltan2 {
+
+/*! Zoltan2::Solution
+    Just a placeholder for now.
+*/
+
+template <Z2CLASS_TEMPLATE>
+  class Solution
+{
+};
+
+}
+
+#endif
