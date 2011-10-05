893e6f8cf51eed992f92ccc55ac1d943817255fe
Lee Ann Riesen <lriesen@sandia.gov>
2011-10-04 18:25:45 -0600

    A Model base class

---
+++ packages/zoltan2/src/Zoltan2_Model.hpp
@@ -0,0 +1,32 @@
+// @HEADER
+// ***********************************************************************
+//
+//                Copyright message goes here.   TODO
+//
+// ***********************************************************************
+// @HEADER
+
+/*! \file Zoltan2_Model.hpp
+
+    \brief The abstract interface for a computational model.
+*/
+
+
+#ifndef _ZOLTAN2_MODEL_HPP_
+#define _ZOLTAN2_MODEL_HPP_
+
+#include <Zoltan2_Standards.hpp>
+
+namespace Zoltan2 {
+
+/*! Zoltan2::Model
+    to be called asynchronously.
+*/
+template <Z2CLASS_TEMPLATE>
+  class Model
+{
+};
+
+}
+
+#endif
