7e7733d216573301f91ed7cc764a05ec7f16f9d4
Greg Sjaardema <gdsjaar@sandia.gov>
2011-10-17 07:56:12 -0600

    IOSS: Fix connectivity transfer to skip raw field; fix vector3d sync
    (From Sierra:  f97b924c97810d73876630ed2df51e10698a5dd2)

--- packages/seacas/libraries/ioss/src/main/cth_pressure_map.C
+++ packages/seacas/libraries/ioss/src/main/cth_pressure_map.C
@@ -805,6 +805,7 @@ namespace {
 
     data.resize(isize);
 
+    if (field_name == "connectivity_raw") return;
     ige->get_field_data(field_name, &data[0], isize);
     oge->put_field_data(field_name, &data[0], isize);
   }
