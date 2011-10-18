73898a023e94a6c4c4d4b17d4e74c08a4ab0acde
Greg Sjaardema <gdsjaar@sandia.gov>
2011-10-18 10:50:11 -0600

    SEACAS: Remove unused variable

--- packages/seacas/libraries/ioss/src/Ioss_EdgeSet.C
+++ packages/seacas/libraries/ioss/src/Ioss_EdgeSet.C
@@ -38,10 +38,6 @@
 #include <Ioss_Utils.h>
 #include <string>
 
-namespace {
-  const std::string SCALAR() { return std::string("scalar");}
-}
-
 Ioss::EdgeSet::EdgeSet()
   : Ioss::EntitySet(NULL, "invalid", 0)
 {}
