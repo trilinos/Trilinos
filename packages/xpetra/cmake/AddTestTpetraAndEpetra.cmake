505d7a559ac71895526fc62ba0892bd2908698ed
Jeremie Gaidamour <jngaida@sandia.gov>
2011-10-24 17:20:29 -0600

    MueLu: Simple example can only be run in sequential right know, because they are using the minimalist GaussSeidelSmoother

--- packages/xpetra/cmake/AddTestTpetraAndEpetra.cmake
+++ packages/xpetra/cmake/AddTestTpetraAndEpetra.cmake
@@ -25,3 +25,32 @@ FUNCTION(PACKAGE_ADD_TEST_TPETRA_AND_EPETRA TEST_NAME)
   ENDIF()
   
 ENDFUNCTION()
+
+# Also run tests with mpi builds, but with only one proc
+FUNCTION(PACKAGE_ADD_TEST_SEQ_TPETRA_AND_EPETRA TEST_NAME)
+
+  IF (${PACKAGE_NAME}_ENABLE_Tpetra)
+    
+    PACKAGE_ADD_TEST(
+      ${TEST_NAME}
+      NAME ${TEST_NAME}-Tpetra
+      ARGS "--linAlgebra=1"
+      NUM_MPI_PROCS 1
+      COMM serial mpi
+      )
+    
+  ENDIF()
+  
+  IF (${PACKAGE_NAME}_ENABLE_Epetra)
+    
+    PACKAGE_ADD_TEST(
+      ${TEST_NAME}
+      NAME ${TEST_NAME}-Epetra
+      ARGS "--linAlgebra=0"
+      NUM_MPI_PROCS 1
+      COMM serial mpi
+      )
+    
+  ENDIF()
+  
+ENDFUNCTION()
