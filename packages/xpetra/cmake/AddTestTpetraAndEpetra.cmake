c5250c1061ac26f1bc6c97f2a4993bafeb6f37c5
Jeremie Gaidamour <jngaida@sandia.gov>
2011-10-24 15:23:42 -0600

    MueLu/Xpetra: new cmake macro to simplify CMakeList.txt

---
+++ packages/xpetra/cmake/AddTestTpetraAndEpetra.cmake
@@ -0,0 +1,27 @@
+FUNCTION(PACKAGE_ADD_TEST_TPETRA_AND_EPETRA TEST_NAME)
+
+  IF (${PACKAGE_NAME}_ENABLE_Tpetra)
+    
+    PACKAGE_ADD_TEST(
+      $TEST_NAME
+      NAME $TEST_NAME"Tpetra"
+      ARGS "--linAlgebra=1"
+      NUM_MPI_PROCS 4
+      COMM serial mpi
+      )
+    
+  ENDIF()
+  
+  IF (${PACKAGE_NAME}_ENABLE_Epetra)
+    
+    PACKAGE_ADD_TEST(
+      $TEST_NAME
+      NAME $TEST_NAME"Epetra"
+      ARGS "--linAlgebra=0"
+      NUM_MPI_PROCS 4
+      COMM serial mpi
+      )
+    
+  ENDIF()
+  
+ENDFUNCTION()
