fdf29bd2e8c965646a3f1a0b2f223d9966414b50
Jeremie Gaidamour <jngaida@sandia.gov>
2011-10-24 14:57:48 -0600

    MueLu: reimplemented Simple-LongInt

--- packages/muelu/example/Simple/Simple-LongInt.cpp
+++ packages/muelu/example/Simple/Simple-LongInt.cpp
@@ -1,22 +1,17 @@
-// Teuchos
-#include <Teuchos_RCP.hpp>
-#include <Teuchos_GlobalMPISession.hpp>
-#include <Teuchos_DefaultComm.hpp>
-#include <Teuchos_ArrayView.hpp>
+//
+// To compile and run this example, Trilinos must be configured with the following option:
+// -D Teuchos_ENABLE_LONG_LONG_INT:BOOL=ON
+//
 
-// Xpetra
-#include <Xpetra_MapFactory.hpp>
-#include <Xpetra_CrsOperatorFactory.hpp>
-#include <Xpetra_VectorFactory.hpp>
+#include <iostream>
 
-// MueLu
-#include "MueLu_Hierarchy.hpp"
-#include "MueLu_Utilities.hpp"
+// MueLu main header: include most common header files in one line
+#include "MueLu.hpp"
 
-// Define default template types
+// Define default types
 typedef double Scalar;
-typedef long   LocalOrdinal;
-typedef long   GlobalOrdinal;
+typedef int LocalOrdinal;
+typedef long long int GlobalOrdinal; // <<<<<<
 typedef Kokkos::DefaultNode::DefaultNodeType Node;
 typedef Kokkos::DefaultKernels<Scalar,LocalOrdinal,Node>::SparseOps LocalMatOps;
 
@@ -24,46 +19,57 @@ typedef Kokkos::DefaultKernels<Scalar,LocalOrdinal,Node>::SparseOps LocalMatOps;
 #include "MueLu_UseShortNames.hpp"
 
 int main(int argc, char *argv[]) {
-  using Teuchos::RCP;
+  using Teuchos::RCP; // Reference count pointers
 
   //
-  // MPI initialization
+  // MPI initialization using Teuchos
   //
 
-  Teuchos::oblackholestream blackhole;
-  Teuchos::GlobalMPISession mpiSession(&argc,&argv,&blackhole);
-  RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv, NULL);
+  RCP< const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
+
+  //
+  // Parameters
+  //
+
+  GlobalOrdinal numGlobalElements = 256;         // problem size
+
+#ifdef HAVE_MUELU_TPETRA
+  Xpetra::UnderlyingLib lib = Xpetra::UseTpetra; // linear algebra library
+#else
+  Xpetra::UnderlyingLib lib = Xpetra::UseEpetra;
+#endif
 
   //
   // Construct the problem
   //
 
-  // Construct a Map that puts approximately the same number of equations on each processor.
-  RCP<const Map> map = Xpetra::createUniformContigMap<Ordinal,Ordinal>(numGlobalElements, comm);
+  // Construct a Map that puts approximately the same number of equations on each processor
+  RCP<const Map> map = MapFactory::createUniformContigMap(lib, numGlobalElements, comm);
 
   // Get update list and number of local equations from newly created map.
   const size_t numMyElements = map->getNodeNumElements();
-  Teuchos::ArrayView<const Ordinal> myGlobalElements = map->getNodeElementList();
+  Teuchos::ArrayView<const GlobalOrdinal> myGlobalElements = map->getNodeElementList();
 
   // Create a CrsMatrix using the map, with a dynamic allocation of 3 entries per row
-  RCP<CrsMatrix> A = Xpetra::CrsOperatorFactory(map,3);
+  RCP<Operator> A = rcp(new CrsOperator(map, 3));
 
   // Add rows one-at-a-time
   for (size_t i = 0; i < numMyElements; i++) {
     if (myGlobalElements[i] == 0) {
-      A->insertGlobalValues( myGlobalElements[i],
-                             tuple<Ordinal>( myGlobalElements[i], myGlobalElements[i]+1 ),
-                             tuple<Scalar> ( 2.0, -1.0 ) );
+      A->insertGlobalValues(myGlobalElements[i], 
+                            Teuchos::tuple<GlobalOrdinal>(myGlobalElements[i], myGlobalElements[i] +1), 
+                            Teuchos::tuple<Scalar> (2.0, -1.0));
     }
-    else if (myGlobalElements[i] == numGlobalElements-1) {
-      A->insertGlobalValues( myGlobalElements[i],
-                             tuple<Ordinal>( myGlobalElements[i]-1, myGlobalElements[i] ),
-                             tuple<Scalar> ( -1.0, 2.0 ) );
+    else if (myGlobalElements[i] == numGlobalElements - 1) {
+      A->insertGlobalValues(myGlobalElements[i], 
+                            Teuchos::tuple<GlobalOrdinal>(myGlobalElements[i] -1, myGlobalElements[i]), 
+                            Teuchos::tuple<Scalar> (-1.0, 2.0));
     }
     else {
-      A->insertGlobalValues( myGlobalElements[i],
-                             tuple<Ordinal>( myGlobalElements[i]-1, myGlobalElements[i], myGlobalElements[i]+1 ),
-                             tuple<Scalar> ( -1.0, 2.0, -1.0 ) );
+      A->insertGlobalValues(myGlobalElements[i], 
+                            Teuchos::tuple<GlobalOrdinal>(myGlobalElements[i] -1, myGlobalElements[i], myGlobalElements[i] +1), 
+                            Teuchos::tuple<Scalar> (-1.0, 2.0, -1.0));
     }
   }
 
@@ -71,34 +77,38 @@ int main(int argc, char *argv[]) {
   A->fillComplete();
 
   //
-  // Construct AMG preconditioner
+  // Construct a multigrid preconditioner
   //
 
-  // AMG Hierarchy
+  // Multigrid Hierarchy
   Hierarchy H(A);
-  H.setDefaultVerbLevel(Teuchos::VERB_HIGH);
+  H.setVerbLevel(Teuchos::VERB_HIGH);
 
-  // AMG Setup phase
-  H->Setup();
+  // Multigrid setup phase (using default parameters)
+  FactoryManager M;                         // -
+  M.SetFactory("A", rcp(new RAPFactory())); // TODO: to be remove, but will require some work
+  H.Setup(M);                               // -
+  // Should be instead: H.Setup();
 
   //
   // Solve Ax = b
   //
 
-  RCP<Vector> X = VectorFactory::Build(map,1);
-  RCP<Vector> B = VectorFactory::Build(map,1);
+  RCP<Vector> X = VectorFactory::Build(map, 1);
+  RCP<Vector> B = VectorFactory::Build(map, 1);
   
-  X->putScalar((SC) 0.0);
+  X->putScalar((Scalar) 0.0);
   B->setSeed(846930886); B->randomize();
 
-  // Use AMG directly as an iterative method
+  // Use AMG directly as an iterative solver (not as a preconditionner)
   int nIts = 9;
 
-  H.Iterate(*B, its, *X);
+  H.Iterate(*B, nIts, *X);
 
   // Print relative residual norm
-  ST::magnitudeType residualNorms = MueLu::Utils::ResidualNorm(A, X, B);
-  std::out << "||Residual|| = " << std::setiosflags(ios::fixed) << std::setprecision(20) << residualNorms << std::endl;
+  ST::magnitudeType residualNorms = Utils::ResidualNorm(*A, *X, *B)[0];
+  if (comm->getRank() == 0)
+    std::cout << "||Residual|| = " << residualNorms << std::endl;
 
   return EXIT_SUCCESS;
 }
