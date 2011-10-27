ecc5e67f427617839ee1229ab70c15be485d4484
Axel gerstenberger <agerste@sandia.gov>
2011-10-27 16:03:51 -0600

    adapted to latest output features of Graph and Operators

--- packages/muelu/test/unit_tests/CoalesceDropFactory.cpp
+++ packages/muelu/test/unit_tests/CoalesceDropFactory.cpp
@@ -3,6 +3,7 @@
 #include "MueLu_Version.hpp"
 
 #include "MueLu_CoalesceDropFactory.hpp"
+#include "MueLu_PreDropFunctionConstVal.hpp"
 
 #include "MueLu_UseDefaultTypes.hpp"
 #include "MueLu_UseShortNames.hpp"
@@ -32,6 +33,37 @@ namespace MueLuTests {
     coalesceDropFact.Build(fineLevel);
     //FIXME how do we verify that this is correct?
   } //Build
+
+  TEUCHOS_UNIT_TEST(CoalesceDropFactory, PreDrop)
+  {
+    out << "version: " << MueLu::Version() << std::endl;
+//    RCP<Teuchos::FancyOStream> out = Teuchos::fancyOStream(Teuchos::rcpFromRef(std::cout));
+
+    Level fineLevel;
+    TestHelpers::Factory<SC,LO,GO,NO,LMO>::createSingleLevelHierarchy(fineLevel);
+
+    RCP<Operator> A = TestHelpers::Factory<SC,LO,GO,NO,LMO>::Build1DPoisson(3);
+    fineLevel.Set("A", A);
+    A->describe(out,Teuchos::VERB_EXTREME);
+//    std::cout << *A << std::endl;
+
+    CoalesceDropFactory dropFact = CoalesceDropFactory();
+    dropFact.SetVerbLevel(MueLu::Extreme);
+    dropFact.SetPreDropFunction(rcp(new PreDropFunctionConstVal(0.00001)));
+
+    fineLevel.Request("Graph", &dropFact);
+
+    dropFact.Build(fineLevel);
+
+    fineLevel.print(out);
+    RCP<Graph> graph = fineLevel.Get<RCP<Graph> >("Graph", &dropFact);
+
+    std::cout << graph->GetDomainMap()->getGlobalNumElements() << std::endl;
+    graph->describe(out, Teuchos::VERB_EXTREME);
+
+//    TEST_EQUALITY(1 == 0, true);
+
+  } //PreDrop
 
 } // namespace MueLuTests
 
