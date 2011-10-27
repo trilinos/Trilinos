7c240f134cc92dd7928bcac8878301641f7ee138
Jeremie Gaidamour <jngaida@sandia.gov>
2011-10-27 08:53:42 -0600

    MueLu: fixed a nightly build error. Wrong headers: if the example is Epetra only, it must always be compiled with double,int,int

--- packages/muelu/example/Structure/Structure2D_epetra.cpp
+++ packages/muelu/example/Structure/Structure2D_epetra.cpp
@@ -49,19 +49,8 @@
 #include "MueLu_Exceptions.hpp"
 #include "MueLu_Aggregates.hpp"
 
-//
-typedef double Scalar;
-typedef int LocalOrdinal;
-#ifdef HAVE_TEUCHOS_LONG_LONG_INT
-typedef long long int GlobalOrdinal;
-#else
-typedef int GlobalOrdinal;
-#endif
-//
-typedef Kokkos::DefaultNode::DefaultNodeType Node;
-typedef Kokkos::DefaultKernels<Scalar,LocalOrdinal,Node>::SparseOps
-    LocalMatOps;
-//
+// 
+#include "MueLu_UseDefaultTypes.hpp"
 #include "MueLu_UseShortNames.hpp"
 
 /*!
