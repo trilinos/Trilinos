d29a38a520ade7f5b6290fbad6b0ae9bb5428c7f
Jeremie Gaidamour <jngaida@sandia.gov>
2011-10-28 14:51:10 -0600

    MueLu: explicit instantiation tests on LocalAggregationAlgorithm

---
+++ packages/muelu/src/Graph/MueLu_LocalAggregationAlgorithm_decl.hpp
@@ -0,0 +1,100 @@
+#ifndef MUELU_LOCALAGGREGATIONFACTORY_HPP_DECL
+#define MUELU_LOCALAGGREGATIONFACTORY_HPP_DECL
+
+#ifdef HAVE_MUELU_EXPLICIT_INSTANTIATION // Otherwise, class will be declared twice because _decl.hpp file also have the class definition (FIXME)
+
+#include <assert.h>
+#include <stdio.h>
+#include <math.h>
+#include <vector>
+
+#include <iostream>
+
+#include "MueLu_ConfigDefs.hpp"
+#include "MueLu_FactoryBase.hpp"
+#include "MueLu_Aggregates.hpp"
+#include "MueLu_Exceptions.hpp"
+
+#include "MueLu_LinkedList.hpp"
+
+#include "MueLu_Monitor.hpp"
+
+// MPI helper
+#define sumAll(rcpComm, in, out)                                        \
+  Teuchos::reduceAll(*rcpComm, Teuchos::REDUCE_SUM, in, Teuchos::outArg(out));
+#define minAll(rcpComm, in, out)                                        \
+  Teuchos::reduceAll(*rcpComm, Teuchos::REDUCE_MIN, in, Teuchos::outArg(out));
+#define maxAll(rcpComm, in, out)                                        \
+  Teuchos::reduceAll(*rcpComm, Teuchos::REDUCE_MAX, in, Teuchos::outArg(out));
+
+namespace MueLu {
+
+  namespace AggOptions {
+
+    enum Ordering {
+      NATURAL = 0, 
+      RANDOM  = 1, 
+      GRAPH   = 2  
+    };
+  }
+
+  using namespace AggOptions;
+
+  typedef struct MueLu_SuperNode_Struct
+  {
+    int    length;
+    int    maxLength;
+    int    index;
+    Teuchos::ArrayRCP<int> list;
+    struct MueLu_SuperNode_Struct *next;
+  } MueLu_SuperNode;
+
+
+  enum NodeState {
+    READY   = -11,
+    NOTSEL  = -12,
+    SELECTED = -13
+  };
+
+  template <class LocalOrdinal = int, class GlobalOrdinal = LocalOrdinal, class Node = Kokkos::DefaultNode::DefaultNodeType, class LocalMatOps = typename Kokkos::DefaultKernels<void,LocalOrdinal,Node>::SparseOps> //TODO: or BlockSparseOp ?
+  class LocalAggregationAlgorithm : public BaseClass {
+#include "MueLu_UseShortNamesOrdinal.hpp"
+
+    typedef GO global_size_t; //TODO
+    typedef LO my_size_t; //TODO
+
+
+  public:
+  
+    LocalAggregationAlgorithm(RCP<FactoryBase> const &graphFact=Teuchos::null);
+
+    virtual ~LocalAggregationAlgorithm();
+
+    void SetOrdering(Ordering ordering);                          
+    void SetMinNodesPerAggregate(int minNodesPerAggregate)  ;     
+    void SetMaxNeighAlreadySelected(int maxNeighAlreadySelected) ;
+    
+    Ordering GetOrdering()                const;
+    int      GetMinNodesPerAggregate()    const;
+    int      GetMaxNeighAlreadySelected() const;
+
+    void CoarsenUncoupled(Graph const & graph, Aggregates & aggregates) const;
+
+  private:
+    Ordering ordering_;                /**<  natural, random, graph           */
+    int      minNodesPerAggregate_;    /**<  aggregate size control           */
+    int      maxNeighAlreadySelected_; /**<  complexity control               */
+
+    void RandomReorder(Teuchos::ArrayRCP<LO> list) const;
+
+    int RandomOrdinal(int min, int max) const;
+  
+  };
+
+}
+
+#define MUELU_LOCALAGGREGATIONALGORITHM_SHORT
+
+#endif //HAVE_MUELU_EXPLICIT_INSTANTIATION
+
+#endif //ifndef MUELU_LOCALAGGREGATIONFACTORY_HPP_DECL
