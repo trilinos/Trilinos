eb06d1ae4c71cf69aa32669ec91aebcc92d83a6e
Todd Kordenbrock <thkorde@sandia.gov>
2011-10-28 15:51:07 -0500

    rewrote this script to load modules based on the TARGET_COMPILER.

--- packages/trios/sampleScripts/config/cielo/loadenv.sh
+++ packages/trios/sampleScripts/config/cielo/loadenv.sh
@@ -1,13 +1,66 @@
 #!/bin/bash
 
+# This script gets sourced by do-configure.  But it can also be soruced in your shell like this:
+# bash> TARGET_COMPILER=pgi source loadenv.sh
+#
+# The steps taken below can be summarized as:
+# - check that TARGET_COMPILER is set.
+# - check that TARGET_COMPILER is set to a known value.
+# - cleanup the module list by unloading all known PrgEnv modules.
+# - load the correct PrgEnv for the TARGET_COMPILER
+# - load compiler neutral modules
+# - perform any last minute compiler specific module swaps
+
+
 # Load the appropriate bash environment
 . /opt/modules/default/init/bash
 
-# Unload GNU modules for catamount
-module load PrgEnv-pgi
+
+if [ -z $TARGET_COMPILER ]; then
+  echo "=============================="
+  echo "=============================="
+  echo "TARGET_COMPILER is not defined in your environment.  Please use one of the known values - pgi, gnu, cray or intel.  Aborting."
+  echo "=============================="
+  echo "=============================="
+  return
+fi
+
+case ${TARGET_COMPILER} in
+  pgi|gnu|cray|intel)
+    # TARGET_COMPILER is recognized.  Carry on.
+    ;;
+  *)
+    echo "=============================="
+    echo "=============================="
+    echo "'$TARGET_COMPILER' is not a recognized value for TARGET_COMPILER.  Please use one of the known values - pgi, gnu, cray or intel.  Aborting."
+    echo "=============================="
+    echo "=============================="
+    return
+    ;;
+esac
+
+# Unload any existing PrgEnv modules
+module unload PrgEnv-pgi
+module unload PrgEnv-gnu
+module unload PrgEnv-cray
+module unload PrgEnv-intel
+
+module load PrgEnv-${TARGET_COMPILER}
+
 module load xt-papi
 
-# These two specific modules are required (defaults don't work)
+# These specific module versions are required (defaults don't work)
 module load fftw/3.2.2.1
-module swap pgi/10.9.0
 
+# If you need to something special for your compiler.  Do it here.
+case ${TARGET_COMPILER} in
+  pgi)
+    module swap pgi/10.9.0
+    ;;
+  gnu)
+    ;;
+  cray)
+    ;;
+  intel)
+    ;;
+esac
