448194019dc301d2f9f26d8d51ee39cca360897a
Todd Kordenbrock <thkorde@sandia.gov>
2011-11-09 09:56:10 -0600

    add a check for pthread_yield_np(). add a check for pthread_yield(). add a check for sched_yield().

--- packages/trios/cmake/TriosProbeThreads.cmake
+++ packages/trios/cmake/TriosProbeThreads.cmake
@@ -13,3 +13,16 @@ check_c_source_compiles(
     HAVE_TRIOS_PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP
 )
 
+# Test for a way to yield besides usleep(0)
+check_c_source_compiles(
+    "#include <pthread.h>\nint main(){pthread_yield_np();return 0;}"
+    HAVE_TRIOS_PTHREAD_YIELD_NP
+)
+check_c_source_compiles(
+    "#include <pthread.h>\nint main(){pthread_yield();return 0;}"
+    HAVE_TRIOS_PTHREAD_YIELD
+)
+check_c_source_compiles(
+    "#include <pthread.h>\nint main(){sched_yield();return 0;}"
+    HAVE_TRIOS_SCHED_YIELD
+)
