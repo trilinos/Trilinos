e51aacb7c17b7e76ef2800d34399ecef1eb76a6d
Todd Kordenbrock <thkorde@sandia.gov>
2011-11-10 10:05:52 -0600

    tests for thread yield methods got duplicated during merge.  dedup.
    Build/Test Cases Summary
    Enabled Packages: Trios
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG => passed: passed=11,notpassed=0 (1.12 min)

--- packages/trios/cmake/TriosProbeThreads.cmake
+++ packages/trios/cmake/TriosProbeThreads.cmake
@@ -12,6 +12,7 @@ check_c_source_compiles(
     "#include <pthread.h>\nint main(){pthread_mutex_t mutex=PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP;return 0;}"
     HAVE_TRIOS_PTHREAD_RECURSIVE_MUTEX_INITIALIZER_NP
 )
+# Test for a way to yield besides usleep(0)
 check_c_source_compiles(
     "#include <pthread.h>\nint main(){pthread_yield();return 0;}"
     HAVE_TRIOS_PTHREAD_YIELD
@@ -22,20 +23,5 @@ check_c_source_compiles(
 )
 check_c_source_compiles(
     "#include <sched.h>\nint main(){sched_yield();return 0;}"
-    HAVE_TRIOS_SCHED_YIELD
-)
-
-
-# Test for a way to yield besides usleep(0)
-check_c_source_compiles(
-    "#include <pthread.h>\nint main(){pthread_yield_np();return 0;}"
-    HAVE_TRIOS_PTHREAD_YIELD_NP
-)
-check_c_source_compiles(
-    "#include <pthread.h>\nint main(){pthread_yield();return 0;}"
-    HAVE_TRIOS_PTHREAD_YIELD
-)
-check_c_source_compiles(
-    "#include <pthread.h>\nint main(){sched_yield();return 0;}"
     HAVE_TRIOS_SCHED_YIELD
 )
