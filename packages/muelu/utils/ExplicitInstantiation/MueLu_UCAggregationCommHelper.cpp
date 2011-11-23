50d0884d0b558acc815bbaabefadb8f16444cfbc
Jeremie Gaidamour <jngaida@sandia.gov>
2011-11-23 10:05:01 -0700

    MueLu: explicit instantiation: removed debug #ifdef

--- packages/muelu/utils/ExplicitInstantiation/MueLu_UCAggregationCommHelper.cpp
+++ packages/muelu/utils/ExplicitInstantiation/MueLu_UCAggregationCommHelper.cpp
@@ -4,14 +4,12 @@
 
 #ifdef HAVE_MUELU_INST_DOUBLE_INT_INT
 template class MueLu::UCAggregationCommHelper<int, int, Kokkos::DefaultNode::DefaultNodeType, Kokkos::DefaultKernels<void, int, Kokkos::DefaultNode::DefaultNodeType>::SparseOps>;
-#else
-#error
 #endif
 
 #ifdef HAVE_MUELU_INST_DOUBLE_INT_LONGLONGINT
 # ifdef HAVE_TEUCHOS_LONG_LONG_INT
 template class MueLu::UCAggregationCommHelper<int, long long int, Kokkos::DefaultNode::DefaultNodeType, Kokkos::DefaultKernels<void, int, Kokkos::DefaultNode::DefaultNodeType>::SparseOps>;
-#else
+# else
 # warning To compile MueLu with 'long long int' support, please turn on HAVE_TEUCHOS_LONG_LONG_INT
 # endif
 #endif
