603af2a461222ca4999a8dd55ad5c231ba0aae77
Lee Ann Riesen <lriesen@sandia.gov>
2011-11-29 16:06:38 -0700

    more work toward having block algorithm exampele

---
+++ packages/zoltan2/src/Zoltan2_BasicIdentifierInput.hpp
@@ -0,0 +1,87 @@
+// @HEADER
+// ***********************************************************************
+//
+//                Copyright message goes here.   TODO
+//
+// ***********************************************************************
+// @HEADER
+
+/*! \file Zoltan2_BasicIdentifierInput.hpp
+
+    \brief An input adapter for a simple array of identifiers and weights.
+*/
+
+
+#ifndef _ZOLTAN2_BASICIDENTIFIERINPUT_HPP_
+#define _ZOLTAN2_BASICIDENTIFIERINPUT_HPP_
+
+#include <Zoltan2_IdentifierInput.hpp>
+
+namespace Zoltan2 {
+
+/*! \brief This class represents a collection of global Identifiers
+ *           and their associated weights, if any.
+ *
+ *  A pointer to the global identifiers is supplied in the constructor.
+ *  Local identifiers are implied to be consecutive beginning at zero.
+ */
+
+template <typename User>
+class BasicIdentifierInput: IdentifierInput<User> {
+
+public:
+
+  typedef typename InputTraits<User>::scalar_t scalar_t;
+  typedef typename InputTraits<User>::lno_t    lno_t;
+  typedef typename InputTraits<User>::gno_t    gno_t;
+  typedef typename InputTraits<User>::lid_t    lid_t;
+  typedef typename InputTraits<User>::gid_t    gid_t;
+  typedef typename InputTraits<User>::node_t   node_t;
+
+  BasicIdentifierInput(lno_t numIds, int numWeights, const gid_t *ids, 
+    const scalar_t *weights): numIds_(numIds), numWeights_(numWeights), 
+      idList_(ids), idWeights_(weights) { }
+
+  ////////////////////////////////////////////////////////////////
+  // The InputAdapter interface.
+  ////////////////////////////////////////////////////////////////
+
+  std::string inputAdapterName() const {return std::string("BasicIdentifier");}
+
+  bool haveLocalIds() const {return true;}
+
+  bool haveConsecutiveLocalIds(size_t &base) const 
+  {
+    base = 0;
+    return true;
+  }
+
+  ////////////////////////////////////////////////////////////////
+  // The IdentifierInput interface.
+  ////////////////////////////////////////////////////////////////
+
+  size_t getLocalNumIds() const { return numIds_;}
+
+  size_t getIdList(const gid_t *&gids, const lid_t *&lids,
+    const scalar_t *&weights) const
+  {
+    gids = idList_;
+    lids = NULL;   // it's implied to be 0 to n-1
+    weights = idWeights_;
+    return numIds_;
+  }
+   
+  int getNumWeights() const { return numWeights_; }
+
+private:
+
+  lno_t numIds_;
+  int numWeights_;
+  const gid_t *idList_;
+  const scalar_t *idWeights_;
+};
+  
+  
+}  //namespace Zoltan2
+  
+#endif
---
+++ packages/zoltan2/test/unit/BasicIdentifierAdapter.cpp
@@ -0,0 +1,115 @@
+// @HEADER
+// ***********************************************************************
+//
+//         Zoltan2: Sandia Partitioning Ordering & Coloring Library
+//
+//                Copyright message goes here.   TODO
+//
+// ***********************************************************************
+//
+// Basic testing of Zoltan2::BasicIdentifierInput 
+
+#include <ErrorHandlingForTests.hpp>
+#include <Zoltan2_BasicIdentifierInput.hpp>
+#include <Teuchos_GlobalMPISession.hpp>
+#include <Teuchos_DefaultComm.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_CommHelpers.hpp>
+
+using Teuchos::RCP;
+using Teuchos::Comm;
+using Teuchos::DefaultComm;
+
+int main(int argc, char *argv[])
+{
+  Teuchos::GlobalMPISession session(&argc, &argv);
+  RCP<const Comm<int> > comm = DefaultComm<int>::getComm();
+  int rank = comm->getRank();
+  int nprocs = comm->getSize();
+  int fail = 0, gfail=0;
+
+  typedef double scalar_t;
+  typedef int    lid_t;
+  typedef long   gid_t;
+  typedef int    lno_t;
+  typedef long   gno_t;
+
+  // Create global identifiers with weights
+
+  int numLocalIds = 10;
+  int weightDim = 2;
+
+  gid_t *myIds = new gid_t [numLocalIds];
+  scalar_t *weights = new scalar_t [numLocalIds*weightDim];
+  gid_t base = rank * numLocalIds * numLocalIds;
+
+  for (lno_t i=0; i < numLocalIds; i++){
+    myIds[i] = base+i;
+    weights[i*weightDim] = 1.0;
+    weights[i*weightDim + 1] = (nprocs-rank) / (i+1);
+  }
+
+  // Create a Zoltan2::BasicIdentifierInput object
+  // and verify that it is correct
+
+  typedef Zoltan2::BasicUserTypes<
+    scalar_t, lid_t, gid_t, lno_t, gno_t> userTypes_t;
+
+  Zoltan2::BasicIdentifierInput<userTypes_t> ia(
+    numLocalIds, weightDim, myIds, weights);
+
+  if (!fail && ia.haveLocalIds() != true)
+    fail = 1;
+
+  size_t localIdBase;
+  if (!fail && !ia.haveConsecutiveLocalIds(localIdBase))
+    fail = 2;
+
+  if (!fail && localIdBase != 0)
+    fail = 3;
+
+  if (!fail && ia.getLocalNumIds() != numLocalIds){
+    fail = 4;
+  }
+
+  if (!fail && ia.getNumWeights() != weightDim)
+    fail = 5;
+
+  const gid_t *globalIdsIn;
+  const lid_t *localIdsIn;
+  const scalar_t *weightsIn;
+
+  if (!fail && ia.getIdList(globalIdsIn, localIdsIn, weightsIn) != numLocalIds)
+    fail = 6;
+
+  if (!fail && localIdsIn != NULL)
+    fail = 7;
+
+  if (!fail){
+    for (lno_t i=0; i < numLocalIds; i++){
+      if (globalIdsIn[i] != base+i){
+        fail = 8;
+        break;    
+      }
+      if (weightsIn[i*weightDim] != 1.0){
+        fail = 9;
+        break;    
+      }
+      if (weightsIn[i*weightDim + 1] != weights[i*weightDim+1]){
+        fail = 10;
+        break;    
+      }
+    }
+  }
+
+  delete [] myIds;
+  delete [] weights;
+
+  gfail = globalFail(comm, fail);
+  if (gfail)
+    printFailureCode(comm, fail);   // will exit(1)
+
+  if (rank == 0)
+    std::cout << "PASS" << std::endl;
+}
+
