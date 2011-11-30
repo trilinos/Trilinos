9ccc0e2cb401e3aba5e5e8be82bf44de8509f547
Will Dicharry <wdichar@sandia.gov>
2011-11-30 08:42:48 -0700

    Explicitly set CMAKE_CURRENT_LIST_DIR for CMake 2.8.0 compatibility.

--- cmake/ctest/TrilinosCTestDriverCore.cmake
+++ cmake/ctest/TrilinosCTestDriverCore.cmake
@@ -2,6 +2,7 @@
 # existing driver CTest scripts.
 
 # Existing scripts need a value for TRILINOS_CMAKE_DIR.
+get_filename_component(CMAKE_CURRENT_LIST_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 set(TRILINOS_CMAKE_DIR "${CMAKE_CURRENT_LIST_DIR}/../")
 
 #
