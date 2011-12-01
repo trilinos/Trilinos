b6dd72b9b3e286ef0eba0a78b7613420619b4013
Brent Perschbacher <bmpersc@sandia.gov>
2011-12-01 12:27:16 -0700

    Revert "Giving Tribits its own copyright header and applying to all files"
    This reverts commit 7f649308b2b59591e4242de4229deaeefb6f0040.
    
    Reverting because of a few issues with the copyright headers. They will be
    updated at a later date.
    
    Conflicts:
    
    	cmake/tribits/checkin-test.py
    
    The conflict invovled removing changes to the copyright of a file that had been
    removed by a later commit. I resolved it by just removing the file.
    
    Build/Test Cases Summary
    Enabled Packages: TrilinosFramework
    Enabled all Packages
    0) MPI_DEBUG => passed: passed=1303,notpassed=0 (165.37 min)
    1) SERIAL_RELEASE => passed: passed=1315,notpassed=0 (99.53 min)

--- cmake/tribits/python/CMakeVersions.py
+++ cmake/tribits/python/CMakeVersions.py
@@ -1,11 +1,11 @@
 # @HEADER
 # ************************************************************************
 #
-#            TriBITS: Tribial Build, Test, and Integrate System
-#                 Copyright (2011) Sandia Corporation
+#            Trilinos: An Object-Oriented Solver Framework
+#                 Copyright (2001) Sandia Corporation
 #
 #
-# Copyright (2011) Sandia Corporation. Under the terms of Contract
+# Copyright (2001) Sandia Corporation. Under the terms of Contract
 # DE-AC04-94AL85000, there is a non-exclusive license for use of this
 # work by or on behalf of the U.S. Government.  Export of this program
 # may require a license from the United States Government.
