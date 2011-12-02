0bf8170b00ed297f909642bb7ae592c671539f36
Will Dicharry <wdichar@sandia.gov>
2011-12-01 17:29:39 -0700

    Fix timestamp number to be 16 hours.

--- cmake/ctest/drivers/trilinos-test2/ctest_linux_continuous_mpi_opt_shared_trilinos-test2_jenkins.cmake
+++ cmake/ctest/drivers/trilinos-test2/ctest_linux_continuous_mpi_opt_shared_trilinos-test2_jenkins.cmake
@@ -69,33 +69,25 @@ SET(BUILD_DIR_NAME CONTINUOUS_${COMM_TYPE}_OPT_DEV_SHARED)
 SET( CTEST_BUILD_FLAGS "-j12 -i" )
 SET( CTEST_PARALLEL_LEVEL "12" )
 
-# SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
-# 
-# #disabling Mesquite because of a build error when shared libs is turned on.
-# SET(EXTRA_EXCLUDE_PACKAGES Mesquite STK Claps)
-# 
-# SET( EXTRA_CONFIGURE_OPTIONS
-#   "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON"
-#   "-DTrilinos_ENABLE_DEBUG:BOOL=ON"
-#   "-DBUILD_SHARED_LIBS:BOOL=ON"
-#   "-DMPI_BASE_DIR:PATH=/home/trilinos"
-#   "-DTPL_ENABLE_Pthread:BOOL=ON"
-#   "-DTPL_ENABLE_Boost:BOOL=ON"
-#   "-DBoost_INCLUDE_DIRS:FILEPATH=/home/trilinos/include"
-#   "-DTPL_ENABLE_Expat:BOOL=ON"
-#   "-DTPL_ENABLE_LAMMPS:BOOL=ON"
-#   "-DTPL_ENABLE_couple:BOOL=ON"
-#   "-DTPL_ENABLE_SPPARKS:BOOL=ON"
-#   "-DTPL_ENABLE_SuperLU:BOOL=ON"
-#   )
+SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
 
-# 2009/11/26: rabartl: Do we really want to be pointing to Trilinos_DATA_DIR?
-# Unless the CI sever is going to be updatting this every iteration this is
-# likely to cause an inconsistency and a test failure.  For example, if
-# someone adds a new test and then adds new test data, the CI server will only
-# get the new test but not the new test data.  Therefore, I am removing this.
-# # "-DTrilinos_DATA_DIR:STRING=$ENV{TRILINOSDATADIRECTORY}"
+#disabling Mesquite because of a build error when shared libs is turned on.
+SET(EXTRA_EXCLUDE_PACKAGES Mesquite STK Claps)
 
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=ON"
+  "-DTrilinos_ENABLE_DEBUG:BOOL=ON"
+  "-DBUILD_SHARED_LIBS:BOOL=ON"
+  "-DMPI_BASE_DIR:PATH=/home/trilinos"
+  "-DTPL_ENABLE_Pthread:BOOL=ON"
+  "-DTPL_ENABLE_Boost:BOOL=ON"
+  "-DBoost_INCLUDE_DIRS:FILEPATH=/home/trilinos/include"
+  "-DTPL_ENABLE_Expat:BOOL=ON"
+  "-DTPL_ENABLE_LAMMPS:BOOL=ON"
+  "-DTPL_ENABLE_couple:BOOL=ON"
+  "-DTPL_ENABLE_SPPARKS:BOOL=ON"
+  "-DTPL_ENABLE_SuperLU:BOOL=ON"
+  )
 
 #
 # Set the rest of the system-specific options and run the dashboard build/test
@@ -119,7 +111,7 @@ if(NOT EXISTS ${_timestamp_file})
 else()
   file(READ ${_timestamp_file} _last_time)
   math(EXPR _difference "${_current_time} - ${_last_time}") 
-  if(${_difference} GREATER 60) # 16 hours
+  if(${_difference} GREATER 57600) # 16 hours
     message("Timestamp is more than 16 hours old, performing a clean build.")
     SET(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY ON)
     SET(CTEST_ENABLE_MODIFIED_PACKAGES_ONLY OFF)
