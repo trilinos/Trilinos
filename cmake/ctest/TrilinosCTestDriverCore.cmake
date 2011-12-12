445ea3a34a5a1eaf5c7fa2ac8d8e25ed9d595969
Will Dicharry <wdichar@sandia.gov>
2011-12-12 16:44:04 -0700

    Reintoduce TRILINOS_CMAKE_DIR to Trilinos tests who use it.

--- cmake/ctest/TrilinosCTestDriverCore.cmake
+++ cmake/ctest/TrilinosCTestDriverCore.cmake
@@ -8,6 +8,10 @@ get_filename_component(CMAKE_CURRENT_LIST_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
 SET(TRIBITS_PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}/../..")
 #MESSAGE("TRIBITS_PROJECT_ROOT = '${TRIBITS_PROJECT_ROOT}'")
 
+# Many of the existing scripts use the variable TRILINOS_CMAKE_DIR, so we set
+# it here.
+SET(TRILINOS_CMAKE_DIR ${CMAKE_CURRENT_LIST_DIR}/../)
+
 INCLUDE("${TRIBITS_PROJECT_ROOT}/cmake/tribits/ctest/TribitsCTestDriverCore.cmake")
 
 macro(TRILINOS_CTEST_DRIVER)
