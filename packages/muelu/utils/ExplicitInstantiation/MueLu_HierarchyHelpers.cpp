6a1c37dd98e79af39d0e19f288fabc5152505f9f
Jeremie Gaidamour <jngaida@sandia.gov>
2011-12-14 13:18:43 -0700

    MueLu: more explicit instantiation for complex

--- packages/muelu/utils/ExplicitInstantiation/MueLu_HierarchyHelpers.cpp
+++ packages/muelu/utils/ExplicitInstantiation/MueLu_HierarchyHelpers.cpp
@@ -17,3 +17,14 @@ template class MueLu::TopSmootherFactory<double, int, long long int, Kokkos::Def
 # warning To compile MueLu with 'long long int' support, please turn on HAVE_TEUCHOS_LONG_LONG_INT
 # endif
 #endif
+
+#ifdef HAVE_MUELU_INST_COMPLEX_INT_INT
+# ifdef HAVE_TEUCHOS_COMPLEX
+#include <complex>
+template class MueLu::TopRAPFactory<std::complex<double>, int, int, Kokkos::DefaultNode::DefaultNodeType, Kokkos::DefaultKernels<void, int, Kokkos::DefaultNode::DefaultNodeType>::SparseOps>;
+template class MueLu::TopSmootherFactory<std::complex<double>, int, int, Kokkos::DefaultNode::DefaultNodeType, Kokkos::DefaultKernels<void, int, Kokkos::DefaultNode::DefaultNodeType>::SparseOps>;
+# else
+# warning To compile MueLu with 'complex' support, please turn on Teuchos_ENABLE_COMPLEX
+# endif
+#endif
+
