aa90408f8c1f48a98dbec38b677ae24df8ab4273
K Devine <kddevin@sandia.gov>
2011-12-14 16:00:59 -0700

    Changes to more clearly return OVIS parameters.

--- packages/zoltan/src/ha/ha_ovis.h
+++ packages/zoltan/src/ha/ha_ovis.h
@@ -14,7 +14,17 @@ extern "C" {
 
 #ifdef ZOLTAN_OVIS
 
-extern int Zoltan_OVIS_Setup(ZZ *);
+#include "ovis.h"
+
+/* Structure for storing OVIS parameters */
+
+struct OVIS_parameters {
+  int outputLevel;
+  char hello[MAX_PARAM_STRING_LEN];
+  double minVersion;
+};
+
+extern int Zoltan_OVIS_Setup(ZZ *, struct OVIS_parameters *);
 extern int Zoltan_OVIS_Set_Param(char *, char *);
 
 #endif /* ZOLTAN_OVIS */
