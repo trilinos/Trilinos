00c1f4529ade205f7d65cf02fdc5f88b2cf80caa
James M. Willenbring <jmwille@sandia.gov>
2012-01-06 11:17:36 -0700

    Updating new nightly tests.
    Moving OpenMP test to nightly track.  Excluding some packages from
    the no explicit instantiation build.

--- cmake/ctest/drivers/brain/ctest_linux_nightly_serial_development_debug_gcc_4.6.1_noei_brain.cmake
+++ cmake/ctest/drivers/brain/ctest_linux_nightly_serial_development_debug_gcc_4.6.1_noei_brain.cmake
@@ -68,9 +68,7 @@ SET(CTEST_TEST_TYPE EXPERIMENTAL)
 #SET(CTEST_TEST_TIMEOUT 900)
 
 SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
-#Stokhos is explicitly disabled below to prevent the package from being
-#implicitly enabled.  Sundance depends on Stokhos.
-#SET(EXTRA_EXCLUDE_PACKAGES Phalanx Stokhos Sundance)
+SET(EXTRA_EXCLUDE_PACKAGES STK MOOCHO Piro Sundance)
 SET_DEFAULT(Trilinos_ENABLE_KNOWN_EXTERNAL_REPOS_TYPE Nightly)
 
 SET( EXTRA_CONFIGURE_OPTIONS
