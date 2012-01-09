6f3fcf74a7d60f053330508467759f8e3b2e3309
Stephen Kennon <srkenno@sandia.gov>
2012-01-09 14:09:11 -0700

    stk_percept,adapt: fix intel compile errors (thanks for help to Dan Sunderland).
    (From Sierra)

--- packages/stk/stk_percept/stk_percept/PerceptBoostArray.hpp
+++ packages/stk/stk_percept/stk_percept/PerceptBoostArray.hpp
@@ -1,19 +1,7 @@
 #if defined(__INTEL_COMPILER) && (__INTEL_COMPILER == 1210)
-
-#  define PERCEPT_BOOST_DISABLE_ASSERTS 0
-#  if defined(BOOST_DISABLE_ASSERTS)
-#    define PERCEPT_BOOST_DISABLE_ASSERTS 1
-#  endif
-
-#  define BOOST_DISABLE_ASSERTS
-
+#pragma warning disable 2196 2536 279
 #endif
 
 #include <boost/multi_array.hpp>
 #include <boost/array.hpp>
 
-#if PERCEPT_BOOST_DISABLE_ASSERTS
-#define BOOST_DISABLE_ASSERTS
-#else
-#undef BOOST_DISABLE_ASSERTS
-#endif
