bc8651784618c7220581c21056690bfaa5b9e222
Stephen Kennon <srkenno@sandia.gov>
2012-01-10 14:22:46 -0700

    stk_percept: new mesquite unit test
    (From Sierra)

--- packages/stk/stk_percept/stk_percept/mesh/mod/mesquite-interface/PMMShapeImprover.cpp
+++ packages/stk/stk_percept/stk_percept/mesh/mod/mesquite-interface/PMMShapeImprover.cpp
@@ -53,7 +53,12 @@ namespace stk {
       term_inner.add_relative_successive_improvement( successiveEps );
       term_inner.add_iteration_limit( 50 );
       term_inner.write_iterations("shape.gpt", err);
+
       term_outer.add_iteration_limit( pmesh ? parallelIterations : 1 );
+      //term_outer.add_absolute_quality_improvement( 1.e-6 );
+      term_outer.add_absolute_gradient_L2_norm( gradNorm );
+      term_outer.add_relative_successive_improvement( successiveEps );
+
       shape_solver.set_inner_termination_criterion( &term_inner );
       shape_solver.set_outer_termination_criterion( &term_outer );
 
