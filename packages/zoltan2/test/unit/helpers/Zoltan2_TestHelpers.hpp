0279a9bffb75f7cefbaa9ce59064823c83e38d20
Lee Ann Riesen <lriesen@sandia.gov>
2012-01-17 14:58:33 -0700

    use only ZOLTAN2 macros in conditionals

--- packages/zoltan2/test/unit/helpers/Zoltan2_TestHelpers.hpp
+++ packages/zoltan2/test/unit/helpers/Zoltan2_TestHelpers.hpp
@@ -4,37 +4,46 @@
 typedef Kokkos::DefaultNode::DefaultNodeType node_t;
 
 //
+// If Tpetra is compiled with explicit instantiation,
+// then we will use these types in our tests.
+//
 // Epetra uses int, int, double data types.  If we
 // are using these data types, then we can test 
 // cases of Epetra user input.
 //
 
-#define HAVE_EPETRA_DATA_TYPES
 
-#ifdef HAVE_TPETRA_EXPLICIT_INSTANTIATION
+#ifdef HAVE_ZOLTAN2_INST_FLOAT_INT_LONG
 
-#ifdef HAVE_TPETRA_INST_INT_LONG
 typedef int lno_t;
 typedef long gno_t;
-#undef HAVE_EPETRA_DATA_TYPES
-#else
-typedef int lno_t;
-typedef int gno_t;
-#endif
-
-#ifdef HAVE_TPETRA_INST_FLOAT
 typedef float scalar_t;
-#undef HAVE_EPETRA_DATA_TYPES
-#else
-typedef double scalar_t;
-#endif
 
-#else   // no tpetra explicit instantiation
+#elif HAVE_ZOLTAN2_INST_DOUBLE_INT_LONG
+
+typedef int lno_t;
+typedef long gno_t;
+typedef double scalar_t;
+
+#elif HAVE_ZOLTAN2_INST_FLOAT_INT_INT
+
+typedef int lno_t;
+typedef int gno_t;
+typedef float scalar_t;
+
+#elif HAVE_ZOLTAN2_INST_DOUBLE_INT_INT
 
 typedef int lno_t;
 typedef int gno_t;
 typedef double scalar_t;
+#define HAVE_EPETRA_DATA_TYPES
 
+#else
+
+typedef int lno_t;
+typedef int gno_t;
+typedef double scalar_t;
+#define HAVE_EPETRA_DATA_TYPES
 
 #endif
 
