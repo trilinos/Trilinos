02b62e64a93c19c33eeb85c0308ad0dc40c1e8a3
Roscoe A. Bartlett <bartlettra@ornl.gov>
2012-01-18 20:38:47 -0500

    Adding TeuchosWrappersExt and Coupler to CI and Nightly testing

--- cmake/ctest/drivers/pu241/enable_casl_packages.gcc.mpi.cmake
+++ cmake/ctest/drivers/pu241/enable_casl_packages.gcc.mpi.cmake
@@ -1,1 +1,16 @@
-SET(${PROJECT_NAME}_PACKAGES STARCCM DeCART Panzer Drekar nemesis denovo CASLBOA CASLRAVE RELAP5 LIME VRIPSS)
+SET(${PROJECT_NAME}_PACKAGES
+  CTeuchos
+  ForTeuchos
+  Coupler
+  STARCCM
+  DeCART
+  Panzer
+  Drekar
+  nemesis
+  denovo
+  CASLBOA
+  CASLRAVE
+  RELAP5
+  LIME
+  VRIPSS
+  )
