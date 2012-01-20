16a7362a91848139016e7f941684e33c473bb01c
Tobias Wiesner <wiesner@poincare.lnm.mw.tum.de>
2012-01-20 14:13:24 +0100

    MueLu: submit forward declaration and explicit instantiation code for new Repartition class (generated with Jeremie's bash scripts)

---
+++ packages/muelu/utils/ForwardDeclaration/MueLu_Repartition_fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef MUELU_REPARTITION_FWD_HPP
+#define MUELU_REPARTITION_FWD_HPP
+
+namespace MueLu {       
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+  class Repartition;
+}
+
+#ifndef MUELU_REPARTITION_SHORT
+#define MUELU_REPARTITION_SHORT
+#endif
+
+#endif // MUELU_REPARTITION_FWD_HPP
