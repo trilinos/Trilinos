d4004e46ba1cbd1ab17127e74135ca4558c9c43d
Lee Ann Riesen <lriesen@sandia.gov>
2012-01-20 12:53:08 -0700

    conform to apparent Trilinos conventions for Version information

---
+++ packages/zoltan2/src/util/Zoltan2_Version.hpp
@@ -0,0 +1,20 @@
+//@HEADER
+// ************************************************************************
+//         copyright
+// ************************************************************************
+//@HEADER
+
+#include <string>
+
+#ifndef ZOLTAN2_VERSION_HPP
+#define ZOLTAN2_VERSION_HPP
+
+/*! \file Zoltan2_Version.hpp
+    \brief Declaration of version string.
+*/
+
+namespace Zoltan2 {
+  std::string Zoltan2_Version();
+} // namespace Zoltan2 
+
+#endif
