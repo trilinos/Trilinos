cf1e21ad20fea152d7f16f60eef1e6b4148dfc02
Jonathan Hu <jhu@sandia.gov>
2012-01-23 15:10:28 -0800

    Cleaning up some comments.

--- packages/muelu/src/Graph/MueLu_UCAggregationCommHelper_decl.hpp
+++ packages/muelu/src/Graph/MueLu_UCAggregationCommHelper_decl.hpp
@@ -13,6 +13,13 @@
 
 namespace MueLu {
 
+  /*!
+    @class UCAggregationCommHelper
+    @brief Helper class for providing arbitrated communication across processors
+
+    This class is mainly used by the aggregation phase to uniquely place unknowns in aggregates in parallel.
+  */
+
   template <class LocalOrdinal  = int, class GlobalOrdinal = LocalOrdinal, class Node = Kokkos::DefaultNode::DefaultNodeType, class LocalMatOps = typename Kokkos::DefaultKernels<void,LocalOrdinal,Node>::SparseOps>
   class UCAggregationCommHelper : public BaseClass {
 
@@ -51,97 +58,114 @@ namespace MueLu {
     //
   public:
 
+    //! @name Constructors/Destructors.
+    //@{
+
+    //! Constructor.
     UCAggregationCommHelper(const RCP<const Map> & uniqueMap, const RCP<const Map> & nonUniqueMap);
 
+    //! Destructor.
     ~UCAggregationCommHelper() { }
 
+    //@}
+
+    /*!
+     @brief This method assigns unknowns to aggregates.
+
+            Tie-breaking is possible is using random weights.
+
+            @param[in] weights vector of weights that help determine ownership.
+
+            @param[in,out] aggregates aggregate data structure
+
+            @param[in] perturb flag indicating whether weights should be randomly perturbed for tie-breaking purposes.
+    */
     void ArbitrateAndCommunicate(Vector &weights, Aggregates &aggregates, const bool perturb) const {
       ArbitrateAndCommunicate(weights, *aggregates.GetProcWinner(), &*aggregates.GetVertex2AggId(), perturb);
     }
 
-    //
-    // For each GlobalId associated with weight.getMap():
-    //
-    //      1) find the maximum absolute value of weight[] distributed across all
-    //         processors and assign this to all local elements of weight[] (across 
-    //         processors) associated with the GlobalId.
-    //      2) set procWinner[] to the MyPid() that had the largest element.
-    //         procWinner[] is still set if only one processor owns a GlobalId. 
-    //
-    //         The ONLY CASE when procWinner[i] is NOT set corresponds to when
-    //         all local weights associated with a GlobalId are zero. This allows
-    //         one to effectively skip the maximum/winner calculation for a subset
-    //         of GlobalId's.  This might occur when a processor has already
-    //         claimed ownership for a GlobalId and so all local copies have
-    //         the same value. We want to skip the maximum calculation with 
-    //         tiebreaking to avoid another processor claiming ownership.
-    //
-    //      3) optionally, set companion[] (across all relevant processors) to the
-    //         local companion value associated with the procWinner[] processor.
-    //
-    //  Input:
-    //     weight                   Vector of weights. ASSUMED TO BE nonnegative.
-    //
-    //     procWinner               Allocated but contents ignored.
-    //
-    //     companion                Either NULL or allocated but contents ignored.
-    //                              If NULL, step 3 above is skipped.
-    //
+    /*!
+     @brief Given a vector of nonnegative weights, this method determines which processor has the maximum weight.
+            Tie-breaking is possible, see the detailed comments.
 
-    //     perturb                  Optional arguments that is either true or 
-    //                              false (default: true). weight is perturbed
-    //                              and the perturbed values are used in step 1)
-    //                              above. Returned values reflect the perturbed
-    //                              data. This option avoids having lots of
-    //                              tiebreaks where the large MyPid() always wins.
-    //
-    //  Output:
-    //     weight                   weight[k] <-- Max(weight[k1],...,weight[kn])
-    //                              where weight[kj] live on different processors
-    //                              but have the same GlobalId as weight[k] on
-    //                              this processor.
-    //
-    //     procWinner               procWinner[k] <-- MyPid associated with the
-    //                              kj yielding the max in 
-    //                                    Max(weight[k1],...,weight[kn]) .
-    //                              See weight Output comments.
-    //                              NOTE: If all input weight[kj]'s are zero,
-    //                                    then procWinner[k] is left untouched.
-    //
-    //     companion                If not null, 
-    //                                 companion[k] <-- companion[kj] where
-    //                              companion[kj] lives on processor procWinner[k].
-    //                              and corresponds to the same GlobalId as k.
-    //                              NOTE: If for a particlar GlobalId, no processor
-    //                                    has a value of procWinner that matches
-    //                                    its MyPid, the corresponding companion
-    //                                    is not altered.
-    //
+     For each GlobalId associated with weight.getMap():
+    
+          1) find the maximum absolute value of weight[] distributed across all
+             processors and assign this to all local elements of weight[] (across 
+             processors) associated with the GlobalId.
+          2) set procWinner[] to the MyPid() that had the largest element.
+             procWinner[] is still set if only one processor owns a GlobalId. 
+    
+             The ONLY CASE when procWinner[i] is NOT set corresponds to when
+             all local weights associated with a GlobalId are zero. This allows
+             one to effectively skip the maximum/winner calculation for a subset
+             of GlobalId's.  This might occur when a processor has already
+             claimed ownership for a GlobalId and so all local copies have
+             the same value. We want to skip the maximum calculation with 
+             tiebreaking to avoid another processor claiming ownership.
+    
+          3) optionally, set companion[] (across all relevant processors) to the
+             local companion value associated with the procWinner[] processor.
+    
+      Input:
+         @param weight                   Vector of weights. ASSUMED TO BE nonnegative.
+    
+         @param procWinner               Allocated but contents ignored.
+    
+         @param companion                Either NULL or allocated but contents ignored.
+                                  If NULL, step 3 above is skipped.
+    
+
+         @param perturb                  Optional arguments that is either true or 
+                                  false (default: true). weight is perturbed
+                                  and the perturbed values are used in step 1)
+                                  above. Returned values reflect the perturbed
+                                  data. This option avoids having lots of
+                                  tiebreaks where the large MyPid() always wins.
+    
+      Output:
+         @param weight            \f$ weight[k]  \Leftarrow  \max(weight[k_1],\dots,weight[k_n]) \f$
+                                  where \f$ weight[k_j] \f$ live on different processors
+                                  but have the same GlobalId as weight[k] on this processor.
+    
+         @param procWinner               procWinner[k] <-- MyPid associated with the
+                                  kj yielding the max in 
+                                        Max(weight[k1],...,weight[kn]) .
+                                  See weight Output comments.
+                                  NOTE: If all input weight[kj]'s are zero,
+                                        then procWinner[k] is left untouched.
+    
+         @param companion                If not null, 
+                                     companion[k] <-- companion[kj] where
+                                  companion[kj] lives on processor procWinner[k].
+                                  and corresponds to the same GlobalId as k.
+                                  NOTE: If for a particlar GlobalId, no processor
+                                        has a value of procWinner that matches
+                                        its MyPid, the corresponding companion
+                                        is not altered.
+    */
     void ArbitrateAndCommunicate(Vector &weight_, LOVector &procWinner_, LOVector *companion, const bool perturb) const; //ArbitrateAndCommunicate(Vector&, LOVector &, LOVector *, const bool) const
 
-    // Redistribute data in source to dest where both source and dest might have 
-    // multiple copies of the same global id across many processors. The source
-    // may not have the same value for all of these multiple copies, but on 
-    // termination dest will have a unique value for each global id.  When multiple
-    // copies exist in source, 'what' determines how they are combined to make a 
-    // unique value in dest (see CombineMode).
-    //
-    //  Input:
-    //     source                   Vector where multiple copies of some GlobalIds
-    //                              might exist and might have different values.
-    //
-    //     dest                     Allocated but contents ignored.
-    //
-    //     what                     Determines how multiple copies of the same
-    //                              GlobalId are combined (see CombineMode).
-    //
-    //  Output:
-    //
-    //     dest                     dest has redistributed data from source where
-    //                              'what' determines how multiple copies of source
-    //                              values associated with the same GlobalId are
-    //                              combined into a unique value on all processors.
-    //
+    /*!  @brief Redistribute data in source to dest where both source and dest might have multiple copies of the same global id across many processors.
+       
+       The source may not have the same value for all of these multiple copies, but on 
+       termination dest will have a unique value for each global id.  When multiple
+       copies exist in source, 'what' determines how they are combined to make a 
+       unique value in dest (see CombineMode).
+      
+        Input:
+           @param[in] source        Vector where multiple copies of some GlobalIds
+                                    might exist and might have different values.
+      
+           @param[in,out] dest      On input, allocated but contents ignored.
+                                    On output, contains redistributed data from source where
+                                    'what' determines how multiple copies of source
+                                    values associated with the same GlobalId are
+                                    combined into a unique value on all processors.
+      
+           @param[in] what          Determines how multiple copies of the same
+                                    GlobalId are combined (see CombineMode).
+    */
     void NonUnique2NonUnique(const Vector &source, Vector &dest, const Xpetra::CombineMode what) const;
    
   };
