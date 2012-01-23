adb9382b658ce8cb2c18696fd387fd62547ec3fc
Jeremie Gaidamour <jngaida@sandia.gov>
2012-01-23 11:14:42 -0700

    Xpetra: added fwd declaration header for BlockedCrsOperator

---
+++ packages/xpetra/ForwardDeclaration/Xpetra_BlockedCrsOperator_fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef XPETRA_BLOCKEDCRSOPERATOR_FWD_HPP
+#define XPETRA_BLOCKEDCRSOPERATOR_FWD_HPP
+
+namespace Xpetra {       
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+  class BlockedCrsOperator;
+}
+
+#ifndef XPETRA_BLOCKEDCRSOPERATOR_SHORT
+#define XPETRA_BLOCKEDCRSOPERATOR_SHORT
+#endif
+
+#endif // XPETRA_BLOCKEDCRSOPERATOR_FWD_HPP
