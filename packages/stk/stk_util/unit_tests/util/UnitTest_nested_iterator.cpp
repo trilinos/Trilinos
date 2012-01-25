0dbd2c21982964fc8d1dc88ade613ef04de15462
Alan Williams <william@sandia.gov>
2012-01-25 14:26:11 -0700

    remove subroster-meshobj traversal, add bucket-iterator unit-test
    MeshObjLinkage was inherited by MeshObj, and was used to create
    linked-lists of MeshObjs for subroster. Removing this makes the
    MeshObj class smaller by 2 pointers.
    
    (From Sierra)

--- packages/stk/stk_util/unit_tests/util/UnitTest_nested_iterator.cpp
+++ packages/stk/stk_util/unit_tests/util/UnitTest_nested_iterator.cpp
@@ -1,6 +1,7 @@
 #include <stk_util/unit_test_support/stk_utest_macros.hpp>
 
 #include <stk_util/util/nested_iterator.hpp>
+#include <stk_util/util/nested_range.hpp>
 
 #include <vector>
 #include <list>
@@ -10,19 +11,15 @@ namespace vector_vector_int {
 
   typedef std::vector< std::vector<int> > nested_type;
 
-  struct to_inner_range {
-    typedef std::vector<int> & value_type;
-    typedef boost::iterator_range< std::vector<int>::iterator> result_type;
-
-    result_type operator()(value_type value) const {
-      return result_type(boost::begin(value),boost::end(value));
-    }
-  };
-
-  typedef stk::util::nested_iterator< std::vector<std::vector<int> >::iterator,
-                                      std::vector<int>::iterator,
-                                      to_inner_range
+  typedef stk::util::nested_iterator< std::vector<std::vector<int> >,
+                                      std::vector<int>,
+                                      stk::util::details::identity<std::vector<int> >
                                     > nested_iterator;
+
+  typedef stk::util::nested_iterator< const std::vector<std::vector<int> >,
+                                      std::vector<int>,
+                                      stk::util::details::identity<std::vector<int> >
+                                    > const_nested_iterator;
 }
 
 STKUNIT_UNIT_TEST ( nested_iterator, vector_vector_int)
@@ -44,7 +41,7 @@ STKUNIT_UNIT_TEST ( nested_iterator, vector_vector_int)
     }
   }
 
-  nested_iterator itr(a,to_inner_range());
+  nested_iterator itr(a);
   const nested_iterator end;
 
   STKUNIT_EXPECT_EQUAL( OUTER*INNER, std::distance(itr,end));
@@ -59,22 +56,49 @@ STKUNIT_UNIT_TEST ( nested_iterator, vector_vector_int)
 
 }
 
+STKUNIT_UNIT_TEST ( nested_iterator, vector_vector_int_nonconst_to_const)
+{
+  using namespace vector_vector_int;
+
+  const int OUTER = 10;
+  const int INNER = 10;
+
+  nested_type a(OUTER);
+
+  {
+    int count = 0;
+    for (int i=0; i<OUTER; ++i) {
+      a[i].resize(INNER);
+      for (int j=0; j<INNER; ++j) {
+        a[i][j] = ++count;
+      }
+    }
+  }
+
+  nested_iterator itr(a);
+
+  const_nested_iterator const_itr = itr;
+
+  const_nested_iterator end;
+
+  STKUNIT_EXPECT_EQUAL( OUTER*INNER, std::distance(const_itr,end));
+
+  {
+    int count = 0;
+    for(; const_itr != end; ++const_itr) {
+      STKUNIT_EXPECT_EQUAL(++count,*const_itr);
+    }
+  }
+
+}
+
 namespace list_vector_int {
 
   typedef std::list< std::vector<int> > nested_type;
 
-  struct to_inner_range {
-    typedef std::vector<int> & value_type;
-    typedef boost::iterator_range< std::vector<int>::iterator> result_type;
-
-    result_type operator()(value_type value) const {
-      return result_type(boost::begin(value),boost::end(value));
-    }
-  };
-
-  typedef stk::util::nested_iterator< std::list<std::vector<int> >::iterator,
-                                      std::vector<int>::iterator,
-                                      to_inner_range
+  typedef stk::util::nested_iterator< std::list<std::vector<int> >,
+                                      std::vector<int>,
+                                      stk::util::details::identity<std::vector<int> >
                                     > nested_iterator;
 }
 
@@ -98,7 +122,7 @@ STKUNIT_UNIT_TEST ( nested_iterator, list_vector_int)
     }
   }
 
-  nested_iterator itr(a,to_inner_range());
+  nested_iterator itr(a);
   const nested_iterator end;
 
   STKUNIT_EXPECT_EQUAL( OUTER*INNER, std::distance(itr,end));
@@ -112,3 +136,4 @@ STKUNIT_UNIT_TEST ( nested_iterator, list_vector_int)
   }
 
 }
+
---
+++ packages/stk/stk_util/unit_tests/util/UnitTest_nested_range.cpp
@@ -0,0 +1,105 @@
+#include <stk_util/unit_test_support/stk_utest_macros.hpp>
+
+#include <stk_util/util/nested_range.hpp>
+#include <boost/foreach.hpp>
+
+#include <vector>
+#include <list>
+#include <iostream>
+
+namespace vector_vector_int {
+
+typedef std::vector< std::vector<int> > nested_type;
+typedef std::vector< std::pair<int*,int*> > nested_pair_type;
+
+typedef stk::util::nested_range< nested_type > range;
+typedef stk::util::nested_range< nested_pair_type > pair_range;
+
+
+}
+
+STKUNIT_UNIT_TEST ( nested_range, basic)
+{
+  using namespace vector_vector_int;
+
+  const int OUTER = 10;
+  const int INNER = 10;
+
+  nested_type a(OUTER);
+  nested_pair_type ap(OUTER);
+
+  {
+    int count = 0;
+    for (int i=0; i<OUTER; ++i) {
+      a[i].resize(INNER);
+      int* int_ptr = NULL;
+      ap[i] = std::make_pair(int_ptr,int_ptr);
+      for (int j=0; j<INNER; ++j) {
+        a[i][j] = ++count;
+      }
+    }
+  }
+
+  range rng(a);
+rng.begin();
+//  BOOST_FOREACH(int i, rng) {
+//   std::cout<<i<<std::endl;
+//  }
+
+  pair_range prng(ap);
+prng.begin();
+//  BOOST_FOREACH(int i, prng) {
+//   std::cout<<i<<std::endl;
+//  }
+
+}
+
+//namespace vector_vector_int {
+//
+//struct to_inner_range {
+//  typedef std::vector<int> result_type;
+//  typedef std::vector<int>* value_type;
+//
+//  result_type& operator()(value_type& r) const { return *r; }
+//  const result_type& operator()(const value_type& r) const { return *r; }
+//};
+//
+//typedef std::vector< std::vector<int>* > nested_ptr_type;
+//
+//typedef stk::util::nested_range< nested_ptr_type, std::vector<int>, to_inner_range > ptr_range;
+//
+//}
+//
+//STKUNIT_UNIT_TEST ( nested_range, nested_ptr)
+//{
+//  using namespace vector_vector_int;
+//
+//  const int OUTER = 10;
+//  const int INNER = 10;
+//
+//  nested_ptr_type a(OUTER);
+//
+//  {
+//    int count = 0;
+//    for (int i=0; i<OUTER; ++i) {
+//      a[i] = new std::vector<int>(INNER);
+//      for (int j=0; j<INNER; ++j) {
+//        (*a[i])[j] = ++count;
+//      }
+//    }
+//  }
+//
+//  BOOST_FOREACH(std::vector<int>* vecptr,a) {
+//    std::vector<int>& vec = *vecptr;
+//    BOOST_FOREACH(int i,vec) {
+//      std::cout<<i<<std::endl;
+//    }
+//  }
+//
+//  ptr_range rng(a,to_inner_range());
+//  BOOST_FOREACH(int i, rng) {
+//   std::cout<<i<<std::endl;
+//  }
+//
+//}
+
