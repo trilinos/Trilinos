4251dd424532d6e4b144626a82ce0af08ba200c5
Jeremie Gaidamour <jngaida@sandia.gov>
2012-01-30 14:43:18 -0700

    Stratimikos: added files for MueLu adapters Right now, MueLu adapters are just a copy of ML adapters...

---
+++ packages/muelu/adapters/stratimikos/src/Thyra_MueLuPreconditionerFactory.hpp
@@ -0,0 +1,102 @@
+//TODO: Copyright Header
+
+#ifndef THYRA_MUELU_PRECONDITIONER_FACTORY_DECL_HPP
+#define THYRA_MUELU_PRECONDITIONER_FACTORY_DECL_HPP
+
+
+#include "Thyra_PreconditionerFactoryBase.hpp"
+#include "Thyra_EpetraOperatorViewExtractorBase.hpp"
+#include "Teuchos_StandardCompositionMacros.hpp"
+
+namespace Thyra {
+
+
+/** \brief Concrete preconditioner factory subclass based on MueLu.
+ *
+ * ToDo: Finish documentation!
+ */
+class MueLuPreconditionerFactory : public PreconditionerFactoryBase<double> {
+public:
+
+  /** @name Constructors/initializers/accessors */
+  //@{
+
+  /** \brief . */
+  MueLuPreconditionerFactory();
+    
+  /** \brief Set the strategy object used to extract an
+   * <tt>Epetra_Operator</tt> view of an input forward operator.
+   *
+   * This view will then be dynamically casted to <tt>Epetra_RowMatrix</tt>
+   * before it is used.
+   *
+   * The default implementation used is <tt>EpetraOperatorViewExtractorBase</tt>.
+   */
+  STANDARD_COMPOSITION_MEMBERS(
+    EpetraOperatorViewExtractorBase, epetraFwdOpViewExtractor );
+
+  //@}
+
+  /** @name Overridden from PreconditionerFactoryBase */
+  //@{
+
+  /** \brief . */
+  bool isCompatible( const LinearOpSourceBase<double> &fwdOp ) const;
+  /** \brief . */
+  bool applySupportsConj(EConj conj) const;
+  /** \brief . */
+  bool applyTransposeSupportsConj(EConj conj) const;
+  /** \brief . */
+  Teuchos::RCP<PreconditionerBase<double> > createPrec() const;
+  /** \brief . */
+  void initializePrec(
+    const Teuchos::RCP<const LinearOpSourceBase<double> > &fwdOp,
+    PreconditionerBase<double> *prec,
+    const ESupportSolveUse supportSolveUse
+    ) const;
+  /** \brief . */
+  void uninitializePrec(
+    PreconditionerBase<double> *prec
+    ,Teuchos::RCP<const LinearOpSourceBase<double> > *fwdOp
+    ,ESupportSolveUse *supportSolveUse
+    ) const;
+
+  //@}
+
+  /** @name Overridden from Teuchos::ParameterListAcceptor */
+  //@{
+
+  /** \brief . */
+  void setParameterList(
+    Teuchos::RCP<Teuchos::ParameterList> const& paramList);
+  /** \brief . */
+  Teuchos::RCP<Teuchos::ParameterList> getNonconstParameterList();
+  /** \brief . */
+  Teuchos::RCP<Teuchos::ParameterList> unsetParameterList();
+  /** \brief . */
+  Teuchos::RCP<const Teuchos::ParameterList> getParameterList() const;
+  /** \brief . */
+  Teuchos::RCP<const Teuchos::ParameterList> getValidParameters() const;
+  //@}
+
+  /** \name Public functions overridden from Describable. */
+  //@{
+
+  /** \brief . */
+  std::string description() const;
+
+  // ToDo: Add an override of describe(...) to give more detail!
+
+  //@}
+
+private:
+
+  Teuchos::RCP<Teuchos::ParameterList> paramList_;
+
+};
+
+
+} // namespace Thyra
+
+
+#endif // THYRA_MUELU_PRECONDITIONER_FACTORY_DECL_HPP
