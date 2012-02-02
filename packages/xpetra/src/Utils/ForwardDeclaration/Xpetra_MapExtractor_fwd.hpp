3ef85dd843cc4253cb4c2c618af153633319ff10
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2012-02-02 13:35:05 +0100

    Xpetra: add MapExtractor to list for generating forward declaration files

---
+++ packages/xpetra/src/Utils/ForwardDeclaration/Xpetra_MapExtractor_fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef XPETRA_MAPEXTRACTOR_FWD_HPP
+#define XPETRA_MAPEXTRACTOR_FWD_HPP
+
+namespace Xpetra {       
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+  class MapExtractor;
+}
+
+#ifndef XPETRA_MAPEXTRACTOR_SHORT
+#define XPETRA_MAPEXTRACTOR_SHORT
+#endif
+
+#endif // XPETRA_MAPEXTRACTOR_FWD_HPP
