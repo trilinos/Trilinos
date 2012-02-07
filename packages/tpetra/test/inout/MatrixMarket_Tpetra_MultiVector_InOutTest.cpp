e8a00bc724a0fb38b2cfb01eb47f2d055ecc2146
Mark Hoemmen <mhoemme@sandia.gov>
2012-02-07 12:35:37 -0700

    Tpetra: Added test for Matrix Market input of multivectors with a nonnull contiguous Map on input to readDenseFile().

--- packages/tpetra/test/inout/MatrixMarket_Tpetra_MultiVector_InOutTest.cpp
+++ packages/tpetra/test/inout/MatrixMarket_Tpetra_MultiVector_InOutTest.cpp
@@ -131,7 +131,7 @@ namespace {
     }
   }
 
-  // Test Tpetra::MatrixMarket::Reader::readDenseFile()
+  // Test Tpetra::MatrixMarket::Reader::readDenseFile(), using no input Map.
   //
   // \tparam MV Tpetra::MultiVector specialization
   //
@@ -210,20 +210,128 @@ namespace {
       typedef Tpetra::MatrixMarket::Writer<sparse_matrix_type> writer_type;
       writer_type::writeDenseFile (outputFilename, X);
       // For the sanity test, we also check that reading in the
-      // multivector produces a Map which is the same as X's Map.
+      // multivector produces a Map which is compatible with X's Map.
+      // It does NOT have to be the same as X's Map, and it won't be
+      // if X's Map is noncontiguous, has an indexBase other than
+      // zero, etc.
+      //
       // We're not testing here the functionality of readDenseFile()
       // to reuse an existing Map.
       RCP<const map_type> testMap;
       RCP<MV> Y = reader_type::readDenseFile (inputFilename, comm, node, 
 					      testMap, tolerant, debug);
-      TEUCHOS_TEST_FOR_EXCEPTION(! map->isSameAs (*testMap), std::logic_error,
-        "Writing the read-in Tpetra::MultiVector X to a file and reading it "
-        "back in resulted in a multivector Y with a different Map.  Please "
-        "report this bug to the Tpetra developers.");
+      TEUCHOS_TEST_FOR_EXCEPTION(! map->isCompatible (*testMap), std::logic_error,
+        "Writing the read-in Tpetra::MultiVector X (with no input Map provided) "
+        "to a file and reading it back in resulted in a multivector Y with an "
+	"incompatible Map.  Please report this bug to the Tpetra developers.");
       assertMultiVectorsEqual<MV> (X, Y);
     }
     return X;
   }
+
+  // Test Tpetra::MatrixMarket::Reader::readDenseFile(), using a custom input Map.
+  //
+  // \tparam MV Tpetra::MultiVector specialization
+  //
+  // \param inputFilename [in] Name of the Matrix Market format dense
+  //   matrix file to read (on Proc 0 of map's communicator only).
+  // \param map [in] Map over which to distribute the returned
+  //   Tpetra::MultiVector.  Must be nonnull.  Use testReadDenseFile()
+  //   to test the null input map case.
+  // \param tolerant [in] Whether or not to parse the file tolerantly.
+  // \param verbose [in] Whether to print verbose output.
+  // \param debug [in] Whether to print debugging output.
+  template<class MV>
+  Teuchos::RCP<MV>
+  testReadDenseFileWithInputMap (const std::string& inputFilename, 
+				 const std::string& outputFilename,
+				 const Teuchos::RCP<const Tpetra::Map<typename MV::local_ordinal_type, typename MV::global_ordinal_type, typename MV::node_type> >& map,
+				 const bool tolerant, 
+				 const bool verbose,
+				 const bool debug)
+  {
+    using Teuchos::RCP;
+    using std::cerr;
+    using std::cout;
+    using std::endl;
+
+    typedef typename MV::scalar_type scalar_type;
+    typedef typename MV::local_ordinal_type local_ordinal_type;
+    typedef typename MV::global_ordinal_type global_ordinal_type;
+    typedef typename MV::node_type node_type;
+
+    typedef Teuchos::ScalarTraits<scalar_type> STS;
+    typedef typename STS::magnitudeType magnitude_type;
+    typedef Tpetra::Map<local_ordinal_type, global_ordinal_type, node_type> 
+      map_type;
+
+    // The reader and writer classes are templated on the
+    // Tpetra::CrsMatrix specialization, from which the
+    // Tpetra::MultiVector specialization is derived.
+    typedef Tpetra::CrsMatrix<scalar_type, local_ordinal_type, 
+      global_ordinal_type, node_type> sparse_matrix_type;
+
+    TEUCHOS_TEST_FOR_EXCEPTION(map.is_null(), std::invalid_argument, 
+      "testReadDenseFileWithInputMap requires a nonnull input map.");
+
+    RCP<const Teuchos::Comm<int> > comm = map->getComm();
+    RCP<node_type> node = map->getNode();
+    const int myRank = comm->getRank ();
+    if (verbose && myRank == 0) {
+      cout << "testReadDenseFileWithInputMap:" << endl;
+    }
+    if (verbose && myRank == 0) {
+      cout << "Reading Matrix Market file \"" << inputFilename << "\":" << endl;
+    }
+
+    // readDenseFile()'s interface reserves the right to modify the
+    // input map pointer, as it takes a nonconst reference to an RCP
+    // of a const Tpetra::Map.  Tpetra::Map doesn't have a copy or
+    // clone routine (as of 07 Feb 2012), so we just copy the pointer
+    // and hope for the best.  Ideally, we should copy the map, pass
+    // readDenseFile() the copy, and ensure that the resulting output
+    // map isSameAs() the input map.
+    RCP<const map_type> theMap = map;
+
+    TEUCHOS_TEST_FOR_EXCEPTION(theMap.is_null(), std::logic_error, 
+      "testReadDenseFileWithInputMap: theMap (a shallow copy of map) should not "
+      "be null, but it is.  Please report this bug to the Tpetra developers.");
+
+    // Read the dense matrix from the given Matrix Market file.
+    // This routine acts like an MPI barrier.
+    typedef Tpetra::MatrixMarket::Reader<sparse_matrix_type> reader_type;
+    RCP<MV> X = reader_type::readDenseFile (inputFilename, comm, node,
+					    theMap, tolerant, debug);
+    TEUCHOS_TEST_FOR_EXCEPTION(X.is_null(), std::runtime_error,
+      "The Tpetra::MultiVector returned from readDenseFile() is null.");
+    TEUCHOS_TEST_FOR_EXCEPTION(theMap.is_null(), std::runtime_error,
+      "The Tpetra::Map returned from readDenseFile() is null.");
+    if (! X.is_null() && verbose && myRank == 0) {
+      cout << "Finished reading file." << endl;
+    }
+
+    // Sanity check: write the multivector X to a file, read it back
+    // in as a different multivector Y, then make sure it has the same
+    // column norms as X.  This assumes that writing a multivector
+    // works.  Only do this if the caller supplied a nonempty output
+    // file name.
+    if (outputFilename != "") {
+      typedef Tpetra::MatrixMarket::Writer<sparse_matrix_type> writer_type;
+      writer_type::writeDenseFile (outputFilename, X);
+
+      // The input Map need not necessarily be compatible with Y's
+      // Map.  This is because we are supplying a null input Map to
+      // readDenseFile(), so readDenseFile() will construct a
+      // contiguous, uniformly distributed Map.  The input Map need
+      // not necessarily be contiguous or uniformly distributed.
+      RCP<const map_type> testMap;
+      RCP<MV> Y = reader_type::readDenseFile (inputFilename, comm, node, 
+					      testMap, tolerant, debug);
+      assertMultiVectorsEqual<MV> (X, Y);
+    }
+    return X;
+  }
+
 
   // Test Tpetra::MatrixMarket::Reader::writeDenseFile()
   //
@@ -310,6 +418,8 @@ namespace {
 int 
 main (int argc, char *argv[]) 
 {
+  using Teuchos::Array;
+  using Teuchos::as;
   using Teuchos::Comm;
   using Teuchos::CommandLineProcessor;
   using Teuchos::ParameterList;
@@ -327,9 +437,15 @@ main (int argc, char *argv[])
   RCP<const Comm<int> > comm = 
     Tpetra::DefaultPlatform::getDefaultPlatform().getComm();
   const int myRank = comm->getRank();
+  const int numProcs = comm->getSize();
 
   std::string inputFilename;  // Matrix Market file to read
   std::string outputFilename; // Matrix Market file to write (if applicable)
+  bool testContiguousInputMap = true;
+  // FIXME (mfh 07 Feb 2012) This test FAILS; reading in the
+  // multivector appears to be correct, but writing it results in a
+  // multivector of all zeros.
+  bool testNoncontiguousInputMap = false; 
   bool testWrite = true; // Test Matrix Market output?
   bool tolerant = false; // Parse the file tolerantly?
   bool echo = false;     // Echo the read-in matrix back?
@@ -344,6 +460,14 @@ main (int argc, char *argv[])
 		  "this file in Matrix Market format on (MPI) Proc 0.  "
 		  "Otherwise, this argument is ignored.  Note that the output "
 		  "file may not be identical to the input file.");
+  cmdp.setOption ("testContiguousInputMap", "dontTestContiguousInputMap",
+		  &testContiguousInputMap,
+		  "Whether to test input and output for a nonnull contiguous "
+		  "input Map.");
+  cmdp.setOption ("testNoncontiguousInputMap", "dontTestNoncontiguousInputMap",
+		  &testNoncontiguousInputMap,
+		  "Whether to test input and output for a nonnull noncontiguous "
+		  "input Map.");
   cmdp.setOption ("testWrite", "noTestWrite", &testWrite,
 		  "Whether to test Matrix Market file output.  Ignored if no "
 		  "--outputFilename value is given.");
@@ -376,9 +500,8 @@ main (int argc, char *argv[])
   // Get a Kokkos Node instance for the particular Node type.
   RCP<node_type> node = getNode<node_type>();
 
-  // Test reading in the sparse matrix.  If no filename or an empty
-  // filename is specified, we don't invoke the test and report a
-  // "TEST PASSED" message.
+  // Run all the tests.  If no input filename was specified, we don't
+  // invoke the test and we report a "TEST PASSED" message.
   if (inputFilename != "") {
     // Convenient abbreviations
     typedef scalar_type ST;
@@ -386,15 +509,88 @@ main (int argc, char *argv[])
     typedef global_ordinal_type GO;
     typedef node_type NT;
     typedef Tpetra::MultiVector<ST, LO, GO, NT> MV;
+    typedef Tpetra::Map<LO, GO, NT> MT;
 
     // If not testing writes, don't do the sanity check that tests
     // input by comparing against output.
     std::string outFilename = testWrite ? outputFilename : "";
+
+    // Test 1: null input Map.
     RCP<MV> X = testReadDenseFile<MV> (inputFilename, outFilename, comm,
 				       node, tolerant, verbose, debug);
     if (outFilename != "") {
       testWriteDenseFile<MV> (outFilename, X, echo, verbose, debug);
     }
+
+    // Test 2: nonnull contiguous input Map.  In order to recycle the
+    // same input filename, we create an input map with the same
+    // number of global rows as X, but with a different indexBase.
+    if (testContiguousInputMap) {
+      const Tpetra::global_size_t globalNumRows = X->getMap()->getGlobalNumElements();
+      // Choose the Map's index base so that the global ordinal sets
+      // of X->getMap() and map don't overlap.  This will ensure that
+      // we test something nontrivial.
+      const GO indexBase = X->getMap()->getMaxGlobalIndex() + 1;
+      // Create the Map.
+      RCP<const MT> map = 
+	rcp (new Tpetra::Map<LO, GO, NT> (globalNumRows, indexBase, comm, 
+					  Tpetra::GloballyDistributed, node));
+      RCP<MV> X2 = 
+	testReadDenseFileWithInputMap<MV> (inputFilename, outFilename,
+					   map, tolerant, verbose, debug);
+      if (outFilename != "") {
+	testWriteDenseFile<MV> (outFilename, X2, echo, verbose, debug);
+      }
+    }
+
+    if (testNoncontiguousInputMap) {
+      // Test 2: nonnull input Map.  In order to recycle the same input
+      // filename, we create an input map with the same number of global
+      // rows as X, but with a different distribution.
+      RCP<const MT> map;
+      {
+	// Make sure that the global ordinal sets of X->getMap() and map
+	// don't overlap.
+	GO indexBase = X->getMap()->getMaxGlobalIndex() + 1;
+	const Tpetra::global_size_t globalNumRows = X->getMap()->getGlobalNumElements();
+
+	// Compute number of GIDs owned by each process.  We're
+	// replicating Tpetra functionality here because we want to
+	// trick Tpetra into thinking we have a noncontiguous
+	// distribution.  This is the most general case and the most
+	// likely to uncover bugs.
+	const size_t quotient = globalNumRows / numProcs;
+	const size_t remainder = globalNumRows - quotient * numProcs;
+	const size_t localNumRows = (as<size_t> (myRank) < remainder) ? 
+	  (quotient + 1) : quotient;
+
+	// Build the list of GIDs owned by this process.
+	Array<GO> elementList (localNumRows);
+	GO myStartGID;
+	if (as<size_t> (myRank) < remainder) {
+	  myStartGID = indexBase + as<GO> (myRank) * as<GO> (quotient + 1);
+	} 
+	else {
+	  // This branch does _not_ assume that GO is a signed type.
+	  myStartGID = indexBase + as<GO> (remainder) * as<GO> (quotient + 1) +
+	    (as<GO> (remainder) - as<GO> (myRank)) * as<GO> (quotient);
+	}
+	for (GO i = 0; i < as<GO> (localNumRows); ++i) {
+	  elementList[i] = myStartGID + i;
+	}
+
+	// Create the Map.
+	using Tpetra::createNonContigMapWithNode;
+	map = createNonContigMapWithNode<LO, GO, NT> (elementList(), comm, node);
+      }
+      {
+	RCP<MV> X3 = testReadDenseFileWithInputMap<MV> (inputFilename, outFilename,
+							map, tolerant, verbose, debug);
+	if (outFilename != "") {
+	  testWriteDenseFile<MV> (outFilename, X3, echo, verbose, debug);
+	}
+      }
+    } // if test noncontiguous input Map
   }
 
   // Only Rank 0 gets to write to cout.
