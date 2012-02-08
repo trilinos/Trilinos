e0276ac979680e862dc6d0a8d9af193402c4b865
Kendall Hugh Pierson <khpiers@sandia.gov>
2012-02-08 14:15:24 -0700

    stk_expreval: fix unit test.
    (From Sierra)

--- packages/stk/stk_expreval/unit_tests/UnitTestEvaluator.cpp
+++ packages/stk/stk_expreval/unit_tests/UnitTestEvaluator.cpp
@@ -207,7 +207,7 @@ EXPREVAL_DEFINE_TEST(f28, recttopola(x,1.0),atan2(1.0, x));
 EXPREVAL_DEFINE_TEST(f29, poltorectx(x,PI/4.0),x*cos(stk::expreval::s_pi/4.0));
 EXPREVAL_DEFINE_TEST(f30, poltorecty(x,PI/4.0),x*sin(stk::expreval::s_pi/4.0));
 EXPREVAL_DEFINE_TEST1(f31, 0.4209+4.5e-4*x);
-EXPREVAL_DEFINE_TEST1(f32, cosine_ramp(x, 1.0), (1.0-cos(x*stk::expreval::s_pi/1.0)/2);
+//EXPREVAL_DEFINE_TEST(f32, cosine_ramp(x, 1.0), (1.0-cos(x*stk::expreval::s_pi/1.0)/2));
 
 // EXPREVAL_DEFINE_TEST2(f1, fpart(x),modf(x,&y));
 // EXPREVAL_DEFINE_TEST2(f1, rand(x),rand());
@@ -252,6 +252,7 @@ UnitTestEvaluator::testEvaluator()
   STKUNIT_EXPECT_TRUE(fail_syntax("(x)y"));
   STKUNIT_EXPECT_TRUE(fail_syntax("()"));
   STKUNIT_EXPECT_TRUE(syntax("rand()"));
+  STKUNIT_EXPECT_TRUE(syntax("cosine_ramp(x,y)"));
 
 #define EXPREVAL_TEST(name) test(name##_expr, name)
 
@@ -307,6 +308,7 @@ UnitTestEvaluator::testEvaluator()
   STKUNIT_EXPECT_TRUE(EXPREVAL_TEST(f29));
   STKUNIT_EXPECT_TRUE(EXPREVAL_TEST(f30));
   STKUNIT_EXPECT_TRUE(EXPREVAL_TEST(f31));
+  //STKUNIT_EXPECT_TRUE(EXPREVAL_TEST(f32));
 
   STKUNIT_EXPECT_TRUE(EXPREVAL_TEST(b1));
   STKUNIT_EXPECT_TRUE(EXPREVAL_TEST(b2));
