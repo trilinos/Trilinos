0ef74bee5a357c7f823e13dd4a4d41a8d82d9981
Jeremie Gaidamour <jngaida@sandia.gov>
2012-02-13 16:40:42 -0700

    MueLu: updated CMakeList to add more nightly tests

--- packages/xpetra/cmake/XpetraAddTest.cmake
+++ packages/xpetra/cmake/XpetraAddTest.cmake
@@ -4,15 +4,15 @@
 # Run test only if Tpetra available
 #
 
-FUNCTION(XPETRA_ADD_TEST_TPETRA TEST_NAME COMM NUM_MPI_PROCS)
-  IF (${XPETRA_NAME}_ENABLE_Tpetra)
+FUNCTION(XPETRA_ADD_TEST_TPETRA TEST_NAME NUM_MPI_PROCS)
+  IF (${PACKAGE_NAME}_ENABLE_Tpetra)
     
     TRIBITS_ADD_TEST(
       ${TEST_NAME}
       NAME ${TEST_NAME}-Tpetra
       ARGS "--linAlgebra=1"
       NUM_MPI_PROCS ${NUM_MPI_PROCS}
-      COMM ${COMM}
+      COMM mpi serial
       )
     
   ENDIF()
@@ -23,15 +23,15 @@ ENDFUNCTION()
 # Run test only if Epetra available
 #
 
-FUNCTION(XPETRA_ADD_TEST_EPETRA TEST_NAME COMM NUM_MPI_PROCS)
-  IF (${XPETRA_NAME}_ENABLE_Epetra)
+FUNCTION(XPETRA_ADD_TEST_EPETRA TEST_NAME NUM_MPI_PROCS)
+  IF (${PACKAGE_NAME}_ENABLE_Epetra)
     
     TRIBITS_ADD_TEST(
       ${TEST_NAME}
       NAME ${TEST_NAME}-Epetra
       ARGS "--linAlgebra=0"
       NUM_MPI_PROCS ${NUM_MPI_PROCS}
-      COMM ${COMM}
+      COMM mpi serial
       )
     
   ENDIF()
@@ -42,31 +42,9 @@ ENDFUNCTION()
 # Run test for both Epetra and Tpetra (if available)
 #
 
-FUNCTION(XPETRA_ADD_TEST_TPETRA_AND_EPETRA TEST_NAME COMM NUM_MPI_PROCS)
+FUNCTION(XPETRA_ADD_TEST NUM_MPI_PROCS)
 
-  XPETRA_ADD_TEST_TPETRA(${TEST_NAME} ${COMM} ${NUM_MPI_PROCS})
-  XPETRA_ADD_TEST_EPETRA(${TEST_NAME} ${COMM} ${NUM_MPI_PROCS})
+  XPETRA_ADD_TEST_TPETRA(${TEST_NAME} ${NUM_MPI_PROCS})
+  XPETRA_ADD_TEST_EPETRA(${TEST_NAME} ${NUM_MPI_PROCS})
  
-ENDFUNCTION()
-
-#
-# Run parallel test for [Epetra,Tpetra] and [1,4] processors
-#
-
-FUNCTION(XPETRA_ADD_TEST_PARA_TPETRA_AND_EPETRA TEST_NAME)
-
-  XPETRA_ADD_TEST_TPETRA_AND_EPETRA(${TEST_NAME} "mpi"        4)
-  XPETRA_ADD_TEST_TPETRA_AND_EPETRA(${TEST_NAME} "serial mpi" 1)
-
-ENDFUNCTION()
-
-#
-# Run sequential test for [Epetra,Tpetra] and 1 processors
-#
-
-FUNCTION(XPETRA_ADD_TEST_SEQ_TPETRA_AND_EPETRA TEST_NAME)
-
-  # Also run seq tests with mpi builds, but with only one proc.
-  XPETRA_ADD_TEST_TPETRA_AND_EPETRA(${TEST_NAME} "serial mpi" 1)
-
 ENDFUNCTION()
