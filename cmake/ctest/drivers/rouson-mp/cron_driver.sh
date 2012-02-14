a0522db58a378b73bb992e331f7ed42fdfef7bde
Karla <knmorri@sandia.gov>
2012-02-14 09:23:20 -0800

    Adding flags to cron_driver

--- cmake/ctest/drivers/rouson-mp/cron_driver.sh
+++ cmake/ctest/drivers/rouson-mp/cron_driver.sh
@@ -14,14 +14,23 @@ export TDD_HTTP_PROXY="http://wwwproxy.sandia.gov:80/"
 # Trilinos settings:
 #
 
-export TDD_CTEST_TEST_TYPE=Nightly
+#export TDD_CTEST_TEST_TYPE=Nightly
+#export CTEST_DO_SUBMIT=ON
+#export TDD_DO_SUBMIT=ON
+#export CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=TRUE
 
-export CTEST_DO_SUBMIT=ON
-export TDD_DO_SUBMIT=ON
-
-export CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=FALSE
-
-export Trilinos_PACKAGES=ForTrilinos
+export Trilinos_PACKAGES=ForTrilinos 
+export CTEST_DO_UPDATES=TRUE 
+export CTEST_DO_SUBMIT=FALSE 
+export TDD_DO_SUBMIT=FALSE 
+export TDD_IN_TESTING_MODE=ON 
+export CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=TRUE 
+export TDD_CTEST_TEST_TYPE=Experimental 
+export CTEST_TEST_TYPE=Experimental 
+export TDD_FORCE_CMAKE_INSTALL=0 
+export TDD_FORCE_INNER_CMAKE_INSTALL=0 
+export CTEST_EXPLICITLY_ENABLE_IMPLICITLY_ENABLED_PACKAGES=TRUE 
+#${TRILINOS_SRC}/cmake/ctest/drivers/rouson-mp/cron_driver_release_rouson-mp.sh
 
 # Machine specific environment:
 #
