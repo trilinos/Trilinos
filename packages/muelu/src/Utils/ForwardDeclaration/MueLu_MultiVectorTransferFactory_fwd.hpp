db3185f3d1fb64729158813442649cb3d4c64bfb
Jonathan Hu <jhu@sandia.gov>
2012-02-22 12:21:37 -0800

    Explicit and forward files.

---
+++ packages/muelu/src/Utils/ForwardDeclaration/MueLu_MultiVectorTransferFactory_fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef MUELU_MULTIVECTORTRANSFERFACTORY_FWD_HPP
+#define MUELU_MULTIVECTORTRANSFERFACTORY_FWD_HPP
+
+namespace MueLu {       
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+  class MultiVectorTransferFactory;
+}
+
+#ifndef MUELU_MULTIVECTORTRANSFERFACTORY_SHORT
+#define MUELU_MULTIVECTORTRANSFERFACTORY_SHORT
+#endif
+
+#endif // MUELU_MULTIVECTORTRANSFERFACTORY_FWD_HPP
---
+++ packages/muelu/src/Utils/ForwardDeclaration/MueLu_RepartitionFactory_fwd.hpp
@@ -0,0 +1,13 @@
+#ifndef MUELU_REPARTITIONFACTORY_FWD_HPP
+#define MUELU_REPARTITIONFACTORY_FWD_HPP
+
+namespace MueLu {       
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+  class RepartitionFactory;
+}
+
+#ifndef MUELU_REPARTITIONFACTORY_SHORT
+#define MUELU_REPARTITIONFACTORY_SHORT
+#endif
+
+#endif // MUELU_REPARTITIONFACTORY_FWD_HPP
