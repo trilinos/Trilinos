57ee5786588a87d2fdd117c24ce6a974ae0c090c
Karla <knmorri@sandia.gov>
2012-02-23 09:41:57 -0800

    Edit script to remove old environment variables

--- cmake/ctest/drivers/rouson-mp/cron_driver.sh
+++ cmake/ctest/drivers/rouson-mp/cron_driver.sh
@@ -9,25 +9,19 @@ echo
 
 export TDD_GIT_EXE=/usr/local/git/bin/git
 export TDD_PARALLEL_LEVEL=2
-#export TDD_HTTP_PROXY="http://config.ca.sandia.gov/autoproxy.pac"
 export TDD_HTTP_PROXY="http://wwwproxy.ca.sandia.gov:80/"
 
 # Trilinos settings:
 #
 
 export TDD_CTEST_TEST_TYPE=Nightly
+export CTEST_TEST_TYPE=Nightly
 export CTEST_DO_SUBMIT=ON
 export TDD_DO_SUBMIT=ON
-#export CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=TRUE
-export TRILINOS_SRC=/Users/knmorri/NightlyTestingTrilinos/Trilinos
 export Trilinos_PACKAGES=ForTrilinos 
 export CTEST_DO_UPDATES=TRUE
-#export CTEST_DO_SUBMIT=TRUE
-#export TDD_DO_SUBMIT=TRUE
 export TDD_IN_TESTING_MODE=ON 
 export CTEST_START_WITH_EMPTY_BINARY_DIRECTORY=TRUE
-#export TDD_CTEST_TEST_TYPE=Experimental 
-export CTEST_TEST_TYPE=Experimental 
 #export TDD_FORCE_CMAKE_INSTALL=0 
 #export TDD_FORCE_INNER_CMAKE_INSTALL=0 
 export CTEST_EXPLICITLY_ENABLE_IMPLICITLY_ENABLED_PACKAGES=TRUE 
@@ -42,8 +36,6 @@ export PYTHONPATH=/usr/bin/python
 
 SCRIPT_DIR=`cd "\`dirname \"$0\"\`";pwd`
 $SCRIPT_DIR/../cron_driver.py
-#${TRILINOS_SRC}/cmake/ctest/drivers/rouson-mp/cron_driver_release_rouson-mp.sh
-#${TRILINOS_SRC}/cmake/ctest/drivers/rouson-mp/cron_driver_rouson-mp.sh
 
 echo
 echo "Ending nightly Trilinos development testing on rouson-mp: `date`"
