4506f4d4b729c427bfdff802be9866b5e093f117
Karla <knmorri@sandia.gov>
2012-02-22 22:01:47 -0800

    Updating scripts for nighty testing on rouson-mp for the release branch

--- cmake/ctest/drivers/rouson-mp/cron_driver_release_rouson-mp.sh
+++ cmake/ctest/drivers/rouson-mp/cron_driver_release_rouson-mp.sh
@@ -5,15 +5,15 @@ DATE=`date "+%m-%d-%Y"`
 
 CTEST_EXE=/usr/bin/ctest
 GIT_EXE=/usr/local/git/bin/git
-BASEDIR=/Users/Shared/NightlyTestingTrilinos/Trilinos/
+BASEDIR=/Users/knmorri/NightlyTestingTrilinos/Trilinos
 DRIVER_SCRIPT_DIR=$BASEDIR/cmake/ctest/drivers/rouson-mp
-BRANCH="trilinos-release-10-8-branch"
+BRANCH="trilinos-release-10-10-branch"
 #BRANCH="master"
 TRILINOS_REPOSITORY_LOCATION="software.sandia.gov:/space/git/Trilinos"
 export PYTHONPATH=/usr/bin/python
 
 echo
-echo "Starting nightly Trilinos Release 10.8 testing on rouson-mp: `date`"
+echo "Starting nightly Trilinos Release 10.10 testing on rouson-mp: `date`"
 echo
 
 echo
@@ -33,18 +33,18 @@ fi
 
 
 echo
-echo "Doing mpi optimized Release 10.8 build: `date`"
+echo "Doing mpi optimized Release 10.10 build: `date`"
 echo
 
-time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_mac_nightly_mpi_opt_release_rouson-mp.cmake -VV &> "MPI_OPT_RELEASE_10.8_$DATE.log"
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_mac_nightly_mpi_opt_release_rouson-mp.cmake -VV &> "MPI_OPT_RELEASE_10.10_$DATE.log"
 
 echo
-echo "Doing serial debug Release 10.8 build: `date`"
+echo "Doing serial debug Release 10.10 build: `date`"
 echo
 
-time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_mac_nightly_serial_debug_release_rouson-mp.cmake -VV &> "SERIAL_DEBUG_RELEASE_10.8_$DATE.log"
+time ${CTEST_EXE} -S $DRIVER_SCRIPT_DIR/ctest_mac_nightly_serial_debug_release_rouson-mp.cmake -VV &> "SERIAL_DEBUG_RELEASE_10.10_$DATE.log"
 
 echo
-echo "Ending nightly Trilinos Release 10.8 testing on rouson-mp: `date`"
+echo "Ending nightly Trilinos Release 10.10 testing on rouson-mp: `date`"
 echo
 
