b25bd2c5912c4bf50a96428fa86760b0b40fb635
Brent Perschbacher <bmpersc@sandia.gov>
2012-03-08 14:22:51 -0700

    Update Kalli driver scripts
    Kalli has been behaving pretty well recently so updating the scripts in
    preparation of switching all nightly testing on kalli to these TDD scripts.
    
    Added a section for release testing, though the release branch has not been
    tested yet. This is more to get a starting point for the release testing ready
    in preparation of testing it. It won't be enabled just yet.

--- cmake/ctest/drivers/kallikrates/ctest_windows_nightly_serial_release_shared.cmake
+++ cmake/ctest/drivers/kallikrates/ctest_windows_nightly_serial_release_shared.cmake
@@ -62,16 +62,22 @@ INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../../../Version.cmake")
 #SET(CTEST_DO_UPDATES FALSE)
 SET(COMM_TYPE SERIAL)
 SET(BUILD_TYPE RELEASE)
-SET(BUILD_DIR_NAME "SERIAL_RELEASE_${Trilinos_VERSION}")
+SET(BUILD_DIR_NAME SERIAL_OPT_RELEASE_${Trilinos_VERSION}_SHARED)
 SET(Trilinos_TRACK ${Trilinos_TESTING_TRACK})
-#SET(CTEST_TEST_TIMEOUT 900)
 
 SET(Trilinos_BRANCH ${Trilinos_REPOSITORY_BRANCH})
-
-SET( EXTRA_EXCLUDE_PACKAGES TrilinosFramework Stokhos Piro FEApp)
 
 #
 # Set the rest of the system-specific options and run the dashboard build/test
 #
+
+#The dll export macros have only been added for a few  packages so we can really only test those packages
+SET(Trilinos_PACKAGES "Teuchos;Epetra;Anasazi")
+SET(CTEST_EXPLICITLY_ENABLE_IMPLICITLY_ENABLED_PACKAGES FALSE)
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DBUILD_SHARED_LIBS:BOOL=ON"
+  "-DTrilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=OFF"
+)
 
 TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
