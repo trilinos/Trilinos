fb3b4ab89f1d647cec5c9b566a889f930b544ff3
Lee Ann Riesen <lriesen@sandia.gov>
2012-03-09 20:11:43 -0700

    more work on rcb

--- packages/zoltan2/src/algorithms/partition/Zoltan2_AlgRCB.hpp
+++ packages/zoltan2/src/algorithms/partition/Zoltan2_AlgRCB.hpp
@@ -8,8 +8,8 @@
     \brief Contains the recursive coordinate bisection algorthm.
 */
 
-#ifndef _ZOLTAN2_ALGRCP_HPP_
-#define _ZOLTAN2_ALGRCP_HPP_
+#ifndef _ZOLTAN2_ALGRCB_HPP_
+#define _ZOLTAN2_ALGRCB_HPP_
 
 #include <Zoltan2_CoordinateModel.hpp>
 #include <Zoltan2_PartitioningSolution.hpp>
@@ -21,6 +21,27 @@
 #include <string>
 
 namespace Zoltan2{
+
+// The boolean parameters
+enum rcbParams{
+  doStatus,
+  doTiming,
+  doMemstats,
+  fastSolution,
+  goodSolution,
+  balancedSolution,
+  lowMemory,
+  lowRunTime,
+  balanceMemoryRunTime,
+  balanceCount,
+  balanceWeight,
+  minTotalWeight,
+  minMaximumWeight,
+  balanceTotalMaximum,
+  averageCuts,
+  rectilinearBlocks,
+  NUM_RCB_PARAMS
+};
 
 /*! \brief Recursive coordinate bisection partitioning.
  *
@@ -39,11 +60,11 @@ namespace Zoltan2{
  */
 
 template <typename Adapter>
-void AlgRCP(
-  const RCP<const Environment> &env,
-  const RCP<const Comm<int> > &problemComm,
-  const RCP<const CoordinateModel<Adapter> > &coords, 
-  RCP<PartitioningSolution<typename Adapter::user_t> > &solution
+void AlgRCB(
+  const RCB<const Environment> &env,
+  const RCB<const Comm<int> > &problemComm,
+  const RCB<const CoordinateModel<Adapter> > &coords, 
+  RCB<PartitioningSolution<typename Adapter::user_t> > &solution
 ) 
 {
   using std::string;
@@ -55,6 +76,8 @@ void AlgRCP(
   int rank = env->myRank_;
   int nprocs = env->numProcs_;
 
+  std::bitset<NUM_RCB_PARAMS> params;
+
   ////////////////////////////////////////////////////////
   // Library parameters of interest:
   //
@@ -64,11 +87,11 @@ void AlgRCP(
   //    speed_versus_quality
   //    memory_versus_speed
 
-  bool debug = env->doStatus();
-  bool timing = env->doTiming();
-  bool memstats = env->doMemoryProfiling();
+  params.set(doStatus, env->doStatus());
+  params.set(doTiming, env->doTiming());
+  params.set(doMemstats, env->doMemoryProfiling());
 
-  if (debug)
+  if (params.test(doStatus))
     env->debug(DETAILED_STATUS, string("Entering AlgRCB"));
 
   const Teuchos::ParameterList &pl = env->getParameters();
@@ -82,13 +105,13 @@ void AlgRCP(
   if (!svq)
     svq = &defaultVal;
 
-  bool fastSolution = (*svq==string("speed"));
-  bool goodSolution = (*svq==string("quality"));
-  bool balancedSolution = (*svq==string("balance"));
+  params.set(fastSolution, (*svq==string("speed")));
+  params.set(goodSolution, (*svq==string("quality")));
+  params.set(balancedSolution, (*svq==string("balance")));
  
-  bool lowMemory = (*mvr==string("memory"));
-  bool lowRunTime = (*mvr==string("speed"));
-  bool balanceMemoryRunTime = (*mvr==string("balance"));
+  params.set(lowMemory, (*mvr==string("memory")));
+  params.set(lowRunTime, (*mvr==string("speed")));
+  params.set(balancedMemoryRunTime, (*mvr==string("balance")));
 
   ////////////////////////////////////////////////////////
   // Partitioning problem parameters of interest:
@@ -108,14 +131,14 @@ void AlgRCP(
   double imbalanceTolerance = (tol ? *tol : 1.1);
   string objective = (obj ? *obj : string("balance_object_weight"));
 
-  bool balanceCount = (objective == string("balance_object_count"));
-  bool balanceWeight = (objective == string("balance_object_weight"));
-  bool minTotalWeight = 
-    (objective == string("multicriteria_minimize_total_weight"));
-  bool minMaximumWeight = 
-    (objective == string("multicriteria_minimize_maximum_weight"));
-  bool balanceTotalMaximum = 
-    (objective == string("multicriteria_balance_total_maximum"));
+  params.set(balanceCount, (objective == string("balance_object_count")));
+  params.set(balanceWeight, (objective == string("balance_object_weight")));
+  params.set(minTotalWeight, 
+    (objective == string("multicriteria_minimize_total_weight")));
+  params.set(minMaximumWeight, 
+    (objective == string("multicriteria_minimize_maximum_weight")));
+  params.set(balanceTotalMaximum, 
+    (objective == string("multicriteria_balance_total_maximum")));
 
   ////////////////////////////////////////////////////////
   // Geometric partitioning problem parameters of interest:
@@ -123,8 +146,8 @@ void AlgRCP(
   //    rectilinear_blocks
   //    bisection_num_test_cuts (experimental)
 
-  bool averageCuts=false;
-  bool rectilinearBlocks=false;
+  params.set(averageCuts,false);
+  params.set(rectilinearBlocks,false);
   int  numTestCuts=3; 
 
   if (env->hasPartitioningParameters()){
@@ -133,10 +156,10 @@ void AlgRCP(
       const Teuchos::ParameterList &geom = plPart.sublist("geometry");
 
       const int *zeroOne = geom.getPtr<int>("average_cuts");
-      if (zeroOne && (*zeroOne==1)) averageCuts = true;
+      if (zeroOne && (*zeroOne==1)) params.set(averageCuts, true);
 
       const int *zeroOne = geom.getPtr<int>("rectilinear_blocks");
-      if (zeroOne && (*zeroOne==1)) rectilinearBlocks = true;
+      if (zeroOne && (*zeroOne==1)) params.set(rectilinearBlocks, true);
 
       const int *numCuts = geom.getPtr<int>("bisection_num_test_cuts");
       if (numCuts) numTestCuts = *numCuts;
@@ -167,11 +190,9 @@ void AlgRCP(
   //
 
   size_t numGlobalParts = solution->getGlobalNumberOfParts();
-  size_t numLocalParts = solution->getLocalNumberOfParts();
-
-  const int *partDist = solution->getPartDistribution();
-  const size_t *procDist = solution->getProcsParts();
-  double *partSizes0 = getCriteriaPartSizes(0);
+  Array<bool> uniformParts(weightDim);
+  for (int wdim = 0; wdim < weightDim; wdim++)
+    uniformParts[wdim] = solution->criteriaHasUniformPartSizes(wdim);
   
   ////////////////////////////////////////////////////////
   // The algorithm
@@ -181,6 +202,69 @@ void AlgRCP(
   //    an imbalance for each weight 
   //    a tree representing the cuts
 
-}   // namespace Zoltan2
+}
 
+template <typename scalar_t, typename lno_t, typename gno_t, typename node_t>
+  static int findCut(
+    const RCP<const Teuchos::Comm<int> &comm,
+    Tpetra::Vector<scalar_t, lno_t, gno_t, node_t> &coords,
+    Array<Tpetra::Vector<scalar_t> > &weights, 
+    scalar_t coordGlobalMin,
+    scalar_t coordGlobalMax,
+    int numTestCuts,
+    float imbalanceTolerance,
+    std::bitset<NUM_RCB_PARAMS> &params)
+{
+  int rank = comm->getRank();
+  int nprocs = comm->getSize();
+
+  // For each weight dimension, are weights uniform or not?
+
+  int weightDim = weights.size();
+  Array<int> uniform(weightDim, 0);
+  for (int i=0; i < weightDim; i++)
+    if (weights[i].getGlobalLength() == 0)
+      uniform[i] = 1;
+
+  // Where do we make the first test cuts?
+
+  int numRegions = numTestCuts + 1;
+  Array<scalar_t> testCuts(numTestCuts);
+  scalar_t diff = (coordGlobalMax - coordGlobalMin) / numRegions;
+  for (int i=0; i < numTestCuts; i++){
+    testCuts[i] = coordGlobalMin + diff;
+    diff += diff;
+  }
+
+  // Sum weights or counts for each weight dimension
+
+  Array<Array<gno_t> > counts[weightDim];
+  Array<Array<scalar_t> > weightSums[weightDim];
+
+  if (params.test(balanceCount)){
+    // We ignore the weights and balance the number of objects
+  }
+  else if params.test(balanceWeight)){
+    // If weightDim is one, balance the weights.  If not, it's minTotalWeight.
+  }
+  else if params.test(minTotalWeight)){
+    // Balance the total of the weightDim weights
+    // Zoltan RCB_MULTICRITERIA_NORM 1
+  }
+  else if params.test(minMaximumWeight)){
+    // Check all weightDim weights, and minimize the maximum imbalance.
+    // Zoltan RCB_MULTICRITERIA_NORM 3
+  }
+  else if params.test(balanceTotalMaximum)){
+    // Zoltan RCB_MULTICRITERIA_NORM 2
+  }
+  else{
+    // A logic error this should never happen
+  }
+  
+
+
+}
+
+}   // namespace Zoltan2
 #endif
