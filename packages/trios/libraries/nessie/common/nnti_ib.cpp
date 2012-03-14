f49dded0e324f19d78bf124b02f17ccf28895c98
Todd Kordenbrock <thkorde@sandia.gov>
2012-03-14 15:27:58 -0500

    use RDMA target ACKs by default.  this is for NSSI compatibility.

--- packages/trios/libraries/nessie/common/nnti_ib.cpp
+++ packages/trios/libraries/nessie/common/nnti_ib.cpp
@@ -46,7 +46,7 @@
  * semantics for RDMA ops.   in this mode, when the wait returns the
  * the RDMA op is complete and status indicates what data was addressed.
  */
-//#define USE_RDMA_TARGET_ACK
+#define USE_RDMA_TARGET_ACK
 
 
 
