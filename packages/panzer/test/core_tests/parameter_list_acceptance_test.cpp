b384f355270f76ddb0728ede5a0055e5abcba800
Roger Pawlowski <rppawlo@sandia.gov>
2012-03-15 08:12:57 -0600

    Adding acceptance test for new parameter list functionality
    This verifies the changes to acceptance test are working as advertised.

---
+++ packages/panzer/test/core_tests/parameter_list_acceptance_test.cpp
@@ -0,0 +1,35 @@
+#include <Teuchos_ConfigDefs.hpp>
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_RCP.hpp>
+#include <Teuchos_TimeMonitor.hpp>
+
+#include "Teuchos_ParameterList.hpp"
+#include "Teuchos_XMLParameterListHelpers.hpp"
+#include <iostream>
+
+namespace panzer {
+
+  TEUCHOS_UNIT_TEST(parameter_list_acceptance_test,order_preserving)
+  {
+    using namespace Teuchos;
+
+    ParameterList p;
+    updateParametersFromXmlFile("parameter_list_acceptance_test.xml", ptrFromRef(p));
+
+    TEST_ASSERT(p.isSublist("Boundary Conditions"));
+
+    ParameterList bcs = p.sublist("Boundary Conditions");
+
+    typedef ParameterList::ConstIterator pl_it;
+
+    int index = 0;
+    for (pl_it bc=bcs.begin(); bc != bcs.end(); ++bc,++index) {
+      ParameterList* sublist=0;
+      TEST_EQUALITY(bc->second.getValue(sublist).get<int>("order"),index);
+
+    }
+
+    out << p << std::endl;
+  }
+
+}
