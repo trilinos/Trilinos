1253ecc2ff7cb99d562bfc3475e6a171ea6f615a
Eric Phipps <etphipp@sandia.gov>
2012-03-19 10:38:35 -0600

    Added original matrix-free SG approach plus some other minor changes.
    To better compare the product tensor approach I added the original matrix-free
    SG multiply algorithm, which currently requires stokhos.  To handle this I
    changed the build script to have a "stokhos" option, which requires passing
    in the path to top-level of a Trilinos build enabling stokhos.  I also changed
    the ranges of polynomial order and stochastic dimension, and disabled some
    of the approaches in the testing output (it currently matches the data I used
    for the Copper Mountain presentation).  To support this, I had to make a
    couple of changes to the internals of Kokkos:
    
    * Add support for a multivector update method, i.e., a saxpy
    * Add support for crs matrix times multivector for host and cuda.
    * Add support for writing out a crs matrix to a matrix market file (host only)
    
    Hopefully this doesn't screw up any of Carter's testing/development.  If so,
    we can always go back to the original TestStochastic.hpp file.  However I would
    like to keep this one around so we can regenerate the data for the presentation
    if necessary.

--- packages/kokkos/array/src/impl/Kokkos_MultiVector_macros.hpp
+++ packages/kokkos/array/src/impl/Kokkos_MultiVector_macros.hpp
@@ -231,6 +231,42 @@ private:
   template < class , class > friend class Impl::Factory ;
 };
 
+namespace Impl{
+template < typename ValueType >
+class Update< MultiVector< ValueType, KOKKOS_MACRO_DEVICE > > {
+public:
+
+  typedef MultiVector< ValueType, KOKKOS_MACRO_DEVICE > multivec_type;
+  typedef typename multivec_type::value_type value_type;
+  typedef typename multivec_type::device_type device_type;
+  typedef typename multivec_type::size_type size_type;
+
+  Update( const value_type & arg_alpha , const multivec_type & arg_x , 
+          const value_type & arg_beta  , const multivec_type & arg_y )
+    : x( arg_x ), y( arg_y ), alpha( arg_alpha ), beta( arg_beta ) {}
+
+  inline
+  KOKKOS_MACRO_DEVICE_FUNCTION
+  void operator()( const size_type row ) const 
+  {
+    for ( size_type col = 0 ; col < x.count() ; ++col )
+      x(row,col) = alpha * x(row,col) + beta * y(row,col);
+  }
+
+  static void run( const value_type & arg_alpha , const multivec_type & arg_x , 
+                   const value_type & arg_beta  , const multivec_type & arg_y )
+  {
+    parallel_for( arg_x.length(), Update(arg_alpha, arg_x, arg_beta, arg_y) );
+  }
+private:
+
+  multivec_type x ;
+  multivec_type y ;
+  value_type alpha ;
+  value_type beta ;  
+};
+}
+
 } // namespace Kokkos
 
 //----------------------------------------------------------------------------
