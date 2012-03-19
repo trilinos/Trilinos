7d9010cd11ac5a5a46e1f721728549c9121b60b0
Jonathan Hu <jhu@sandia.gov>
2012-03-19 14:21:51 -0700

    Updating xml scaling test.
    Fixed point or CG via Belos.
    Minor spelling fix in xml deck.

--- packages/muelu/test/scaling/ScalingTestParamList.cpp
+++ packages/muelu/test/scaling/ScalingTestParamList.cpp
@@ -16,6 +16,12 @@
 #include <MueLu_UseDefaultTypes.hpp>
 #include <MueLu_UseShortNames.hpp>  
 
+#include <BelosConfigDefs.hpp>
+#include <BelosLinearProblem.hpp>
+#include <BelosBlockCGSolMgr.hpp>
+#include <BelosXpetraAdapter.hpp>     // => This header defines Belos::XpetraOp
+#include <BelosMueLuAdapter.hpp>      // => This header defines Belos::MueLuOp
+
 int main(int argc, char *argv[]) {
   using Teuchos::RCP; // reference count pointers
 
@@ -35,7 +41,9 @@ int main(int argc, char *argv[]) {
   MueLu::Gallery::Parameters<GO> matrixParameters(clp, 8748); // manage parameters of the test case
   Xpetra::Parameters             xpetraParameters(clp);      // manage parameters of xpetra
 
-  std::string xmlFileName = "scalingTest.xml"; clp.setOption("xml",   &xmlFileName, "read parameters from a file. Otherwise, this example uses by default 'muelu_ParameterList.xml'");
+  std::string xmlFileName = "scalingTest.xml"; clp.setOption("xml",   &xmlFileName, "read parameters from a file. Otherwise, this example uses by default 'scalingTest.xml'");
+  int amgAsPrecond=1; clp.setOption("precond",&amgAsPrecond,"apply multigrid as preconditioner");
+  int amgAsSolver=0; clp.setOption("fixPoint",&amgAsSolver,"apply multigrid as solver");
 
   switch (clp.parse(argc,argv)) {
   case Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED:        return EXIT_SUCCESS; break;
@@ -106,12 +114,66 @@ int main(int argc, char *argv[]) {
     X->putScalar( (SC) 0.0);
   }
 
-  // Use AMG directly as an iterative solver (not as a preconditionner)
-  int nIts = 10;
+  if (amgAsSolver) {
+    H->IsPreconditioner(true);
+    H->Iterate(*B,25,*X);
+    H->IsPreconditioner(false);
+  }
 
-  H->IsPreconditioner(true);
-  H->Iterate(*B,nIts,*X);
-  H->IsPreconditioner(false);
-  
-  return EXIT_SUCCESS;
-}
+  if (amgAsPrecond) {
+    // Operator and Multivector type that will be used with Belos
+    typedef MultiVector          MV;
+    typedef Belos::OperatorT<MV> OP;
+
+    // Define Operator and Preconditioner
+    RCP<OP> belosOp   = rcp(new Belos::XpetraOp<SC, LO, GO, NO, LMO>(A)); // Turns a Xpetra::Operator object into a Belos operator
+    RCP<OP> belosPrec = rcp(new Belos::MueLuOp<SC, LO, GO, NO, LMO>(H));  // Turns a MueLu::Hierarchy object into a Belos operator
+
+    // Construct a Belos LinearProblem object
+    RCP< Belos::LinearProblem<SC, MV, OP> > belosProblem = rcp(new Belos::LinearProblem<SC, MV, OP>(belosOp, X, B));
+    belosProblem->setLeftPrec(belosPrec);
+
+    bool set = belosProblem->setProblem();
+    if (set == false) {
+      if (comm->getRank() == 0)
+        std::cout << std::endl << "ERROR:  Belos::LinearProblem failed to set up correctly!" << std::endl;
+      return EXIT_FAILURE;
+    }
+
+    // Belos parameter list
+    int maxIts = 100;
+    double tol = 1e-8;
+    Teuchos::ParameterList belosList;
+    belosList.set("Maximum Iterations",    maxIts); // Maximum number of iterations allowed
+    belosList.set("Convergence Tolerance", tol);    // Relative convergence tolerance requested
+    belosList.set("Verbosity", Belos::Errors + Belos::Warnings + Belos::TimingDetails + Belos::StatusTestDetails);
+    belosList.set("Output Frequency",1);
+    belosList.set("Output Style",Belos::Brief);
+
+    // Create an iterative solver manager
+    RCP< Belos::SolverManager<SC, MV, OP> > solver = rcp(new Belos::BlockCGSolMgr<SC, MV, OP>(belosProblem, rcp(&belosList, false)));
+
+    // Perform solve
+    Belos::ReturnType ret=Belos::Unconverged;
+    try {
+      ret = solver->solve();
+
+      // Get the number of iterations for this solve.
+      if (comm->getRank() == 0)
+        std::cout << "Number of iterations performed for this solve: " << solver->getNumIters() << std::endl;
+
+    } //try
+
+    catch(...) {
+      std::cout << std::endl << "ERROR:  Belos threw an error! " << std::endl;
+    }
+
+    // Check convergence
+    if (ret != Belos::Converged) {
+      if (comm->getRank() == 0) std::cout << std::endl << "ERROR:  Belos did not converge! " << std::endl;
+    } else {
+      if (comm->getRank() == 0) std::cout << std::endl << "SUCCESS:  Belos converged!" << std::endl;
+    }
+  } //if (amgAsPrecond)
+
+} //main
