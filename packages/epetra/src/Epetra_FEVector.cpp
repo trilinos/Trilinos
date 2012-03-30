21a2f2e67832e3dbc86686a303dcf3d70611d83f
Alan Williams <william@sandia.gov>
2012-03-30 15:06:01 -0600

    Epetra_FEVector: fix a memory leak

--- packages/epetra/src/Epetra_FEVector.cpp
+++ packages/epetra/src/Epetra_FEVector.cpp
@@ -380,39 +380,24 @@ int Epetra_FEVector::GlobalAssemble(Epetra_CombineMode mode,
 //----------------------------------------------------------------------------
 void Epetra_FEVector::createNonlocalMapAndExporter()
 {
-  if (nonlocalMap_ != 0) {
-    delete nonlocalMap_;
-  }
+  delete nonlocalMap_;
   int* nlIDptr = nonlocalIDs_.size()>0 ? &nonlocalIDs_[0] : NULL;
   nonlocalMap_ = new Epetra_BlockMap (-1, nonlocalIDs_.size(), nlIDptr, 
 				      &nonlocalElementSize_[0], Map().IndexBase(), 
 				      Map().Comm());
-  if (exporter_ != 0) {
-    delete exporter_;
-  }
+  delete exporter_;
   exporter_ = new Epetra_Export (*nonlocalMap_, Map());
 
-  if (nonlocalVector_ == 0) {
-    delete nonlocalVector_;
-  }
+  delete nonlocalVector_;
   nonlocalVector_ = new Epetra_MultiVector (*nonlocalMap_, NumVectors());
 }
 
 //----------------------------------------------------------------------------
 void Epetra_FEVector::destroyNonlocalMapAndExporter()
 {
-  if (nonlocalMap_ != 0) {
-    delete nonlocalMap_;
-    nonlocalMap_ = 0;
-  }
-  if (exporter_ != 0) {
-    delete exporter_;
-    exporter_ = 0;
-  }
-  if (nonlocalVector_ == 0) {
-    delete nonlocalVector_;
-    nonlocalVector_ = 0;
-  }
+  delete nonlocalMap_;    nonlocalMap_ = 0;
+  delete exporter_;       exporter_ = 0;
+  delete nonlocalVector_; nonlocalVector_ = 0;
 }
 
 //----------------------------------------------------------------------------
