dd26b0fe309264e1dee51e7376b1da9c726c675e
Roscoe A. Bartlett <bartlettra@ornl.gov>
2012-04-24 14:27:29 -0400

    Changing so that the config file gets changes first; make more verbose
    Now this script seems to set up permissions correctly.

--- cmake/tribits/common_tools/git/hooks/setup-shared-repo.sh
+++ cmake/tribits/common_tools/git/hooks/setup-shared-repo.sh
@@ -5,10 +5,17 @@
 GROUP_NAME=$1
 #echo $GROUP_NAME
 OWNER_NAME=$2
+#echo $OWNER_NAME
 
-chmod -R g+ws $PWD
-chgrp -R $GROUP_NAME $PWD
-if [ "$OWNER_NAME" != "" ] ; then
-  chown -R $OWNER_NAME $PWD
-fi
+echo "Making this a shared repo in git."
 git repo-config core.sharedRepository true
+echo "Making repo group read/writable and sticky" 
+chmod -R g+rws .
+echo "Changing group to $GROUP_NAME"
+chgrp -R $GROUP_NAME .
+if [ "$OWNER_NAME" != "" ] ; then
+  echo "Changing default owner on . to $OWNER_NAME"
+  chown -R $OWNER_NAME .
+  echo "Changing default owner on config to $OWNER_NAME"
+  chown $OWNER_NAME config
+fi
