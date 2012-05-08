0b1c76d6e8c4da71fa3c3d5c94eacc5818797a06
Greg Sjaardema <gdsjaar@sandia.gov>
2012-05-08 15:30:15 -0600

    SEACAS: Argument processing kluge for exodiff steps option
    (From Sierra)

--- packages/seacas/libraries/suplib/GetLongOpt.C
+++ packages/seacas/libraries/suplib/GetLongOpt.C
@@ -267,7 +267,8 @@ GetLongOption::setcell(Cell *c, char *valtoken, char *nexttoken, const char *nam
 	 return 0;
       }
       else {
-	 if ( nexttoken != 0 && nexttoken[0] != optmarker ) {
+	// KLUGE for exodiff "-steps -1" parsing.  need to do a better way...
+	if ( nexttoken != 0 && (nexttoken[0] != optmarker || (nexttoken[0] == optmarker && nexttoken[1] == '1') )) {
 	    c->value = nexttoken;
 	    return 1;
 	 }
