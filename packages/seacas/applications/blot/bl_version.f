dbd27ab1a279cba156604d13c6b7de7f5e921afb
Greg Sjaardema <gdsjaar@sandia.gov>
2012-06-06 08:06:05 -0600

    SEACAS: plt - init font data in c routine to avoid too many continuation lines in fortran
    (From Sierra)

--- packages/seacas/applications/blot/bl_version.f
+++ packages/seacas/applications/blot/bl_version.f
@@ -37,8 +37,8 @@ C OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
       CHARACTER*(MXQARC) QAINFO(6)
 
       QAINFO(1) = 'blot                            '
-      QAINFO(2) = '2012/04/10                      '
-      QAINFO(3) = ' 3.01                           '
+      QAINFO(2) = '2012/06/06                      '
+      QAINFO(3) = ' 3.02                           '
       QAINFO(4) = '                                '
 
       RETURN
