9d3a3f70d17382ac72801d67e97f51452574a6fc
Jeremie Gaidamour <jngaida@sandia.gov>
2012-06-08 14:51:54 -0600

    Xpetra: Unit test of bug fix See also: 360c22b4ad

---
+++ packages/xpetra/test/Vector/Vector_UnitTests2.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Teuchos_Comm.hpp>
+#include <Teuchos_DefaultComm.hpp>
+
+#include <Xpetra_ConfigDefs.hpp>
+#include <Xpetra_UnitTestHelpers.hpp>
+
+#include <Xpetra_MapFactory.hpp>
+#include <Xpetra_VectorFactory.hpp>
+#include <Tpetra_Vector.hpp>
+
+namespace {
+
+  bool testMpi = true;
+
+  TEUCHOS_STATIC_SETUP()
+  {
+    Teuchos::CommandLineProcessor &clp = Teuchos::UnitTestRepository::getCLP();
+    clp.addOutputSetupOptions(true);
+    clp.setOption(
+        "test-mpi", "test-serial", &testMpi,
+        "Test MPI (if available) or force test of serial.  In a serial build,"
+        " this option is ignored and a serial comm is always used." );
+  }
+
+  //
+  // UNIT TESTS
+  // 
+
+  // Test if the reference returned by toTpetra is valid.
+  // cf. bug #5603
+  TEUCHOS_UNIT_TEST(Vector, toXpetra)
+  {
+    Teuchos::RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
+
+    Teuchos::RCP<Xpetra::Map<int> >            map = Xpetra::MapFactory<int>::Build(Xpetra::UseTpetra, 1000, 0, comm);
+    Teuchos::RCP<Xpetra::Vector<double, int> > vec = Xpetra::VectorFactory<double, int>::Build(map);
+
+    Tpetra::Vector<double, int> *tVec = &Xpetra::toTpetra<double, int>(*vec);
+    tVec->putScalar(Teuchos::ScalarTraits<double>::one());
+  
+    std::cout << tVec->isDistributed() << std::endl;
+    TEST_EQUALITY_CONST(tVec->meanValue(), 1);
+  }
+
+} // namespace
