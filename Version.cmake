968c30eb6fbb550df80dab9203797a7ede54f6ff
Brent Perschbacher <bmpersc@sandia.gov>
2012-07-12 15:40:14 -0600

    Initial commit for 10.12
    Updated version numbers and set development mode to off by default.

--- Version.cmake
+++ Version.cmake
@@ -59,12 +59,12 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 10.11)
+SET(Trilinos_VERSION 10.12)
 SET(Trilinos_MAJOR_VERSION 10)
-SET(Trilinos_MAJOR_MINOR_VERSION 101100)
-SET(Trilinos_VERSION_STRING "10.11 (Dev)")
-SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
+SET(Trilinos_MAJOR_MINOR_VERSION 101200)
+SET(Trilinos_VERSION_STRING "10.12")
+SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
-SET(Trilinos_REPOSITORY_BRANCH "master" CACHE INTERNAL "")
-SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
+SET(Trilinos_REPOSITORY_BRANCH "trilinos-release-10-12-branch" CACHE INTERNAL "")
+SET(Trilinos_TESTING_TRACK "Nightly Release 10.12" CACHE INTERNAL "")
