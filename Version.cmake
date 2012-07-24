7cf287d885f58f88de261ab0e5a5f440d74ef957
Brent Perschbacher <bmpersc@sandia.gov>
2012-07-24 09:37:08 -0600

    Update version to 10.12.1

--- Version.cmake
+++ Version.cmake
@@ -59,10 +59,10 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 10.12)
+SET(Trilinos_VERSION 10.12.1)
 SET(Trilinos_MAJOR_VERSION 10)
-SET(Trilinos_MAJOR_MINOR_VERSION 101200)
-SET(Trilinos_VERSION_STRING "10.12")
+SET(Trilinos_MAJOR_MINOR_VERSION 101201)
+SET(Trilinos_VERSION_STRING "10.12.1")
 SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
