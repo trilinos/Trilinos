6ced38e05b783e7ca6e14fb6e2e80ce497429343
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2012-07-29 20:29:04 +0200

    Xpetra: add a new constructor for OperatorFactory creates a diagonal operator using a given vector as diagonal

--- packages/xpetra/sup/Operator/Xpetra_OperatorFactory.hpp
+++ packages/xpetra/sup/Operator/Xpetra_OperatorFactory.hpp
@@ -5,6 +5,7 @@
 #include "Xpetra_Operator.hpp"
 #include "Xpetra_CrsOperator.hpp"
 #include "Xpetra_Map.hpp"
+#include "Xpetra_Vector.hpp"
 #include "Xpetra_Exceptions.hpp"
 
 namespace Xpetra {
@@ -39,6 +40,22 @@ namespace Xpetra {
     static RCP<Operator> Build(const RCP<const Map> &rowMap, const ArrayRCP<const LocalOrdinal> &NumEntriesPerRowToAlloc, ProfileType pftype = Xpetra::DynamicProfile) {
       return rcp( new CrsOperator(rowMap, NumEntriesPerRowToAlloc, pftype) );
     }
+
+    //! Constructor for creating a diagonal Xpetra::Operator using the entries of a given vector for the diagonal
+    static RCP<Operator> Build(const RCP<const Vector> & diagonal) {
+      Teuchos::ArrayRCP<const Scalar> vals = diagonal->getData(0);
+      Teuchos::RCP<CrsOperator> mtx = Teuchos::rcp(new CrsOperator(diagonal->getMap(), 1, Xpetra::StaticProfile));
+      LocalOrdinal NumMyElements = diagonal->getMap()->getNodeNumElements();
+      Teuchos::ArrayView<const GlobalOrdinal> MyGlobalElements = diagonal->getMap()->getNodeElementList();
+      for (LocalOrdinal i = 0; i < NumMyElements; ++i) {
+          mtx->insertGlobalValues(MyGlobalElements[i],
+                                  Teuchos::tuple<GlobalOrdinal>(MyGlobalElements[i]),
+                                  Teuchos::tuple<Scalar>(vals[i]) );
+      }
+      mtx->fillComplete();
+      return mtx;
+    }
+
   };
 
 }
