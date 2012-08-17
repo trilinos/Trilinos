aa2269912b0321d8cbb9f84a73a1735922a90c2a
Tobias Wiesner <wiesner@poincare.lnm.mw.tum.de>
2012-08-17 10:30:52 +0200

    MueLu: clean up SchurComplementFactory and BraessSarazinSmoother

--- packages/muelu/src/Smoothers/BlockedSmoothers/MueLu_BraessSarazinSmoother_def.hpp
+++ packages/muelu/src/Smoothers/BlockedSmoothers/MueLu_BraessSarazinSmoother_def.hpp
@@ -108,10 +108,7 @@ namespace MueLu {
 
   template <class Scalar,class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
   void BraessSarazinSmoother<Scalar, LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::DeclareInput(Level &currentLevel) const {
-    //Global A operator for getting the blocks.
     currentLevel.DeclareInput("A", AFact_.get());
-    //Smoother for the Schur complement.
-    // TODO check if FactManger_ is not Teuchos::null
     TEUCHOS_TEST_FOR_EXCEPTION(FactManager_ == Teuchos::null, Exceptions::RuntimeError, "MueLu::BraessSarazinSmoother::DeclareInput: FactManager_ must not be Teuchos::null! error.");
     currentLevel.DeclareInput("PreSmoother",FactManager_->GetFactory("PreSmoother").get());
   }
@@ -122,7 +119,7 @@ namespace MueLu {
     // Setup routine can be summarized in 4 steps:
     // - Set the map extractors
     // - Set the blocks
-    // - Create and set the inverse of the diagonal of F (note that this means if a different approximation of F^-1 is desired, it must be coded again)
+    // - Create and set the inverse of the diagonal of F
     // - Set the smoother for the Schur Complement
 
     FactoryMonitor m(*this, "Setup blocked Braess-Sarazin Smoother", currentLevel);
@@ -140,9 +137,7 @@ namespace MueLu {
     rangeMapExtractor_ = bA->getRangeMapExtractor();
     domainMapExtractor_ = bA->getDomainMapExtractor();
 
-    //***************************************************************************
     // Store the blocks in local member variables
-    //
     Teuchos::RCP<CrsMatrix> A00 = bA->getMatrix(0, 0);
     Teuchos::RCP<CrsMatrix> A01 = bA->getMatrix(0, 1);
     Teuchos::RCP<CrsMatrix> A10 = bA->getMatrix(1, 0);
@@ -158,16 +153,12 @@ namespace MueLu {
     D_ = Teuchos::rcp_dynamic_cast<Operator>(Op10);
     Z_ = Teuchos::rcp_dynamic_cast<Operator>(Op11);
 
-    //******************************************************************
     // Create the inverse of the diagonal of F
-
-    // it is better if diagFinv_ is a vector, so as to be used in apply, eq 8.13
     RCP<Vector> diagFVector = VectorFactory::Build(F_->getRowMap());
     F_->getLocalDiagCopy(*diagFVector);       // extract diagonal of F
     diagFVector->reciprocal(*diagFVector);    // build reciprocal
     diagFinv_ = diagFVector;
 
-    //********************************************************************
     // Set the Smoother
     smoo_ = currentLevel.Get<RCP<SmootherBase> > ("PreSmoother", FactManager_->GetFactory("PreSmoother").get());
 
@@ -179,85 +170,48 @@ namespace MueLu {
   {
     TEUCHOS_TEST_FOR_EXCEPTION(SmootherPrototype::IsSetup() == false, Exceptions::RuntimeError, "MueLu::BraessSarazinSmoother::Apply(): Setup() has not been called");
 
-    // TODO DO NOT use v and p, because it will be used in generic blocked matrices!
-    //Range map is ok because in vtemp the result from 1/omega * Fhatinv * rvel is stored.
+    // TODO change notation for v and p
     RCP<MultiVector> vtemp = MultiVectorFactory::Build(F_->getRowMap(),1);
     RCP<MultiVector> ptemp = MultiVectorFactory::Build(Z_->getRowMap(),1);
 
     RCP<Xpetra::MultiVector<Scalar, LocalOrdinal, GlobalOrdinal, Node> > residual = MultiVectorFactory::Build(B.getMap(), B.getNumVectors());
     RCP<MultiVector> rcpX = Teuchos::rcpFromRef(X);
 
-#define PRINT_RESIDUAL 0
-#if PRINT_RESIDUAL
-     //This is necessary to print out the residual
-     Teuchos::Array<typename Teuchos::ScalarTraits<Scalar>::magnitudeType> normsvel(1);
-     Teuchos::Array<typename Teuchos::ScalarTraits<Scalar>::magnitudeType> normspre(1);
-
-
-#endif
-
-    //run for each sweep
     for (LocalOrdinal run = 0; run < nSweeps_; ++run) {
-      //************************
       // Calculate residual
       // note: A_ is the full blocked operator
       // r = B - A*X
       residual->update(1.0,B,0.0); // residual = B
       A_->apply(X, *residual, Teuchos::NO_TRANS, -1.0, 1.0); // residual = B-A*X
 
-      //************************
       // split the data
       // extract corresponding subvectors from X and residual
-
       Teuchos::RCP<MultiVector> rvel = rangeMapExtractor_->ExtractVector(residual, 0);
       Teuchos::RCP<MultiVector> rpre = rangeMapExtractor_->ExtractVector(residual, 1);
-
-
-#if PRINT_RESIDUAL
-      //print the norm
-      rvel->norm2(normsvel);
-      rpre->norm2(normspre);
-      // This format is in such a way it can be used for plotting.
-      this->GetOStream(Runtime0, 0) << run << "\t" << normsvel[0] << "\t" << normspre[0] << std::endl;
-#endif
 
       Teuchos::RCP<MultiVector> tXvel = domainMapExtractor_->ExtractVector(rcpX, 0);
       Teuchos::RCP<MultiVector> tXpre = domainMapExtractor_->ExtractVector(rcpX, 1);
 
-      //**********************
       // Calculate qrhs = rpre - D * vtemp (equation 8.14)
       RCP<MultiVector> qrhs = MultiVectorFactory::Build(Z_->getRowMap(),1);
       vtemp->putScalar(0.0);
       vtemp->elementWiseMultiply(1.0/omega_,*diagFinv_,*rvel,0.0); // vtemp = 1/omega*Fhatinv * rvel (equation 8.13)
 
-      //This is used for printing intermediate results (uncomment to use)
-      //Teuchos::Array<typename Teuchos::ScalarTraits<Scalar>::magnitudeType> normsall(1);
-      //vtemp->norm2(normsall);
-      //this->GetOStream(Runtime0, 0) << "BSS::Setup, ||vtemp|| = " << normsall[0];
-
       D_->apply(*vtemp,*qrhs); // qrhs = D*vtemp (intermediate step)
       qrhs->update(1.0,*rpre,-1.0); // qrhs = rpre - D*vtemp
 
-      //***********************
       //Pressure correction, using the preconditioner
-      //RCP<MultiVector> q = MultiVectorFactory::Build(Z_->getDomainMap(),1); // TODO think about this.
       RCP<MultiVector> q = MultiVectorFactory::Build(Z_->getRowMap(),1); // TODO think about this.
       smoo_->Apply(*q,*qrhs);
 
-
-      //*****************************
       //Update
-
       vtemp->putScalar(0.0);
       G_->apply(*q,*vtemp);
       vtemp->update(1.0,*rvel,-1.0); //velres - G*q
       RCP<MultiVector> vx = MultiVectorFactory::Build(F_->getRowMap(),1);
-      vx->elementWiseMultiply(1.0/omega_,*diagFinv_,*vtemp,1.0); //vx = 1/(omega)*DFhatinv*(velres-G*q)
+      vx->elementWiseMultiply(1.0/omega_,*diagFinv_,*vtemp,1.0);
 
-
-      //**********************
       // update with values
-
       tXvel->update(1.0,*vx,1.0);
       tXpre->update(1.0,*q,1.0);
 
