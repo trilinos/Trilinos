002f76e1b6cee592e4d53f3299b35f17a2e7c9c3
Todd Kordenbrock <thkorde@sandia.gov>
2012-08-20 13:51:49 -0500

    define the dependencies for the Trios package and subpackages

--- packages/trios/cmake/Dependencies.cmake
+++ packages/trios/cmake/Dependencies.cmake
@@ -1,8 +1,18 @@
-SET(LIB_REQUIRED_DEP_PACKAGES Teuchos)
-SET(LIB_OPTIONAL_DEP_PACKAGES SEACASExodus)
-SET(TEST_REQUIRED_DEP_PACKAGES ThreadPool)
+SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
+        support         libraries/support              EX  OPTIONAL
+        nnti            libraries/nessie/nnti          EX  OPTIONAL
+        nssi            libraries/nessie/common        EX  OPTIONAL
+        programs        programs                       EX  OPTIONAL
+        examples        examples                       EX  OPTIONAL
+        tests           tests                          EX  OPTIONAL
+        netcdf-service  services/netcdf                EX  OPTIONAL
+)
+
+SET(LIB_REQUIRED_DEP_PACKAGES )
+SET(LIB_OPTIONAL_DEP_PACKAGES Teuchos)
+SET(TEST_REQUIRED_DEP_PACKAGES)
 SET(TEST_OPTIONAL_DEP_PACKAGES)
 SET(LIB_REQUIRED_DEP_TPLS)
-SET(LIB_OPTIONAL_DEP_TPLS MPI Netcdf Pnetcdf Pthread CrayPortals Portals Gemini InfiniBand Pablo) 
+SET(LIB_OPTIONAL_DEP_TPLS MPI Netcdf Pnetcdf Pthread CrayPortals Portals Gemini InfiniBand Pablo)
 SET(TEST_REQUIRED_DEP_TPLS)
 SET(TEST_OPTIONAL_DEP_TPLS)
