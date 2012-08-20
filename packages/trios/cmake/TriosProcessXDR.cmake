19c14f5567ca184f01abba056fef4a97a8136451
Todd Kordenbrock <thkorde@sandia.gov>
2012-08-20 13:41:19 -0500

    used to replaced rpc/rpc.h with Trios_nssi_xdr.h in all rpcgen created headers.  change to ${file}.h so that the .c file includes its own header instead of the possibly unrelated Trios_nssi_xdr.h.

--- packages/trios/cmake/TriosProcessXDR.cmake
+++ packages/trios/cmake/TriosProcessXDR.cmake
@@ -7,23 +7,23 @@ function (TriosProcessXDR path)
    add_custom_command(
      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.c
      COMMAND rpcgen -Cc ${path}
-     	| sed -e "\"s#include.*${file}.*#include <${file}.h>#\""
+        | sed -e "\"s#include.*${file}.*#include <${file}.h>#\""
         > ${CMAKE_CURRENT_BINARY_DIR}/${file}.c
-     DEPENDS ${path} ${file}.h)    
+     DEPENDS ${path} ${file}.h)
 
-     
+
    add_custom_command(
      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.h
      COMMAND rpcgen -Ch ${path}
-        | sed -e "s#rpc/rpc.h#Trios_nssi_xdr.h#"
+        | sed -e "s#rpc/rpc.h#${file}.h#"
         > ${CMAKE_CURRENT_BINARY_DIR}/${file}.h
      DEPENDS ${path})
 
    # Need target to force construction of nssi_types_xdr.{c,h} and nnti_xdr.{c,h}
    add_custom_target(${file} ALL DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${file}.c ${CMAKE_CURRENT_BINARY_DIR}/${file}.h)
-   
+
    INCLUDE_DIRECTORIES(BEFORE SYSTEM ${CMAKE_CURRENT_BINARY_DIR})
-   
+
    # These Flags convert the "extra comma" error into a warning when using the -pedantic flag
    IF (CMAKE_COMPILER_IS_GNUCXX)
      #SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fpermissive")
