4d620673ee11c9e817a7f622943cbc044d142b13
raoldfi <raoldfi@sandia.gov>
2012-08-21 01:30:38 -0600

    Added some checks for xdr functions.

--- packages/trios/cmake/TriosProbeNetwork.cmake
+++ packages/trios/cmake/TriosProbeNetwork.cmake
@@ -25,11 +25,6 @@ check_include_files("sys/types.h;sys/socket.h;ifaddrs.h" HAVE_TRIOS_IFADDRS_H)
 
 ########## Probe for various network configurations ##############
 
-# XDR_SIZEOF
-check_c_source_compiles(
-    "#include <rpc/xdr.h>\nint main(){xdr_sizeof(NULL,NULL);return 0;}"
-    HAVE_TRIOS_XDR_SIZEOF
-)
 
 check_function_exists(getifaddrs TRIOS_HAVE_GETIFADDRS)
 
@@ -43,10 +38,10 @@ set(CMAKE_EXTRA_INCLUDE_FILES)
 IF (${PACKAGE_NAME}_ENABLE_Portals OR ${PACKAGE_NAME}_ENABLE_CrayPortals)
 
     message(STATUS "Checking Portals configuration: PORTALS_IFACE_DEFAULT=${PORTALS_IFACE_DEFAULT}")
-  
+
     # Need this macro defined to get the right integer types for Portals
     ADD_DEFINITIONS(-D__STDC_CONSTANT_MACROS)
-  
+
     # Also need values for PTL_IFACE_SERVER and PTL_IFACE_CLIENT
     if (NOT PORTALS_IFACE_SERVER)
         set(PORTALS_IFACE_SERVER "PTL_IFACE_DEFAULT" CACHE STRING "Default NAL for the Portals server")
@@ -56,21 +51,21 @@ IF (${PACKAGE_NAME}_ENABLE_Portals OR ${PACKAGE_NAME}_ENABLE_CrayPortals)
     endif (NOT PORTALS_IFACE_CLIENT)
 
     ADD_DEFINITIONS(-DPTL_IFACE_CLIENT=${PORTALS_IFACE_CLIENT})
-    ADD_DEFINITIONS(-DPTL_IFACE_SERVER=${PORTALS_IFACE_SERVER}) 
-    
+    ADD_DEFINITIONS(-DPTL_IFACE_SERVER=${PORTALS_IFACE_SERVER})
+
     # Figure out the name of the Portals include file
     IF (${PACKAGE_NAME}_ENABLE_Portals)
       SET (PORTALS_INCLUDE_FILE "portals3.h")
     ENDIF (${PACKAGE_NAME}_ENABLE_Portals)
-    
+
     IF (${PACKAGE_NAME}_ENABLE_CrayPortals)
       SET (PORTALS_INCLUDE_FILE "portals/portals3.h")
     ENDIF (${PACKAGE_NAME}_ENABLE_CrayPortals)
-    
+
     SET(CMAKE_REQUIRED_INCLUDES ${Portals_INCLUDE_DIRS})
     SET(CMAKE_EXTRA_INCLUDE_FILES ${PORTALS_INCLUDE_FILE})
     set(CMAKE_REQUIRED_LIBRARIES ${TPL_Portals_LIBRARIES})
-    
+
     check_include_files(p3nal_utcp.h HAVE_TRIOS_P3NAL_UTCP_H)
     check_include_files("p3nal_utcp.h" HAVE_TRIOS_P3NAL_UTCP_H)
     check_include_files("p3rt/p3rt.h" HAVE_TRIOS_P3RT_P3RT_H)
@@ -96,11 +91,11 @@ IF (${PACKAGE_NAME}_ENABLE_Portals OR ${PACKAGE_NAME}_ENABLE_CrayPortals)
     check_function_exists(PtlEventKindStr HAVE_TRIOS_PTLEVENTKINDSTR)
     check_function_exists(PtlGetJid HAVE_TRIOS_PTLGITJID)
     check_function_exists(PtlACEntry HAVE_TRIOS_PTLACENTRY)
-    
+
     SET(CMAKE_REQUIRED_INCLUDES)
     SET(CMAKE_EXTRA_INCLUDE_FILES)
     set(CMAKE_REQUIRED_LIBRARIES)
-  
+
 ENDIF (${PACKAGE_NAME}_ENABLE_Portals OR ${PACKAGE_NAME}_ENABLE_CrayPortals)
 
 
