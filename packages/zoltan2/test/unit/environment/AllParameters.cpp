a61e8613ec468324fd2bdb61676043a58844c438
Lee Ann Riesen <lriesen@sandia.gov>
2012-08-29 16:57:04 -0600

    prepare to remove this test

--- packages/zoltan2/test/unit/environment/AllParameters.cpp
+++ packages/zoltan2/test/unit/environment/AllParameters.cpp
@@ -49,28 +49,51 @@
 #include <Zoltan2_Environment.hpp>
 #include <Teuchos_ParameterList.hpp>
 #include <Teuchos_DefaultComm.hpp>
+#include <string>
 
+using namespace std;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+
+//
 // For all parameters:
 //   parameter name, a valid value, an invalid value
 
-#define NUMPARAMS 36
+// FileNameValidator - a number is invalid
 
-static char *(paramList[NUMPARAMS][3])={
+#define NUMFN 3
+static string fnParams[NUMFN][3]={
+  {"debug_output_file", "temp.txt", "5"},
+  {"timer_output_file", "timerInfo.txt", "10.3"},
+  {"memory_output_file", "memory.txt", "3.33"}
+};
+
+// Value value is any integer - a string is invalid
+
+#define NUMANYINT 2 
+static string anyIntParams[NUMANYINT][3]={
+  {"bisection_num_test_cuts", "3", "dont_know"},
+  {"parallel_part_calculation_count", "2", "dont_know"}
+};
+
+// Value value is a particular string
+
+#define NUMSTR 31
+static string strParams[NUMSTR][3]={
   {"error_check_level", "basic_assertions", "invalid_assertion_request"},
   {"debug_level", "basic_status", "bogus_status"},
   {"timer_type", "no_timers", "invalid_timers"},
   {"debug_output_stream", "cout", "invalid_stream"},
   {"timer_output_stream", "/dev/null", "invalid_stream"},
   {"memory_output_stream", "cerr", "invalid_stream"},
-  {"debug_output_file", "temp.txt", "----"},
-  {"timer_output_file", "temp.txt", "----"},
-  {"memory_output_file", "temp.txt", "----"},
   {"debug_procs", "all", "not_a_valid_list_of_any_type"},
   {"pqParts", "2,3,4", "not_a_valid_list_of_any_type"},
   {"memory_procs", "2-10", "not_a_valid_list_of_any_type"},
   {"speed_versus_quality", "balance", "amazing_performance"},
   {"memory_versus_speed", "memory", "impossible_performance"},
-  {"random_seed", "9.999", "nan"},
+  {"random_seed", "9.999", "xxx"},
   {"order_method", "rcm", "rcmNew"},
   {"order_package", "amd", "amdNew"},
   {"compute_metrics", "yes", "maybe"},
@@ -81,20 +104,46 @@ static char *(paramList[NUMPARAMS][3])={
   {"num_global_parts", "12", "xxx"},
   {"num_local_parts", "1", "no_idea"},
   {"partitioning_approach", "repartition", "cut_it_up"},
-  {"objects_to_partition", "vertices", "nothing"},
+  {"objects_to_partition", "graph_vertices", "nothing"},
   {"model", "graph", "manifold"},
   {"algorithm", "rcb", "xyz"},
   {"rectilinear_blocks", "yes", "dont_know"},
   {"average_cuts", "false", "dont_know"},
-  {"bisection_num_test_cuts", "3", "dont_know"},
-  {"symmetrize_input", "true", "dont_know"},
+  {"symmetrize_input", "transpose", "dont_know"},
   {"subset_graph", "1", "dont_know"},
   {"force_binary_search", "true", "dont_know"},
-  {"force_linear_search", "yes", "dont_know"},
-  {"parallel_part_calculation_count", "2", "dont_know"}
+  {"force_linear_search", "yes", "dont_know"} 
 };
 
-typedef Teuchos::Array<int> rangeList_t;
+
+template <typename T>
+int testInvalidValue( Teuchos::ParameterList &pl, 
+  string paramName, T badValue)
+{
+  Teuchos::ParameterList validParameters;
+  pl.set(paramName, badValue);
+  cout << endl;
+  cout << paramName << " = " << badValue << endl;
+
+  bool failed = false;
+  try{
+    Zoltan2::createValidatorList(pl, validParameters);
+    pl.validateParametersAndSetDefaults(validParameters);
+  }
+  catch(std::exception &e){
+    cout << "Correctly generated an error:" << endl;
+    cout << e.what() << endl;
+    failed = true;
+  }
+
+  if (!failed){
+    cerr << "Bad parameter was not detected in parameter list." << endl;
+    return 1;
+  }
+  return 0;
+}
+
+  // Print out all the documentation
 
 int main(int argc, char *argv[])
 {
@@ -112,8 +161,20 @@ int main(int argc, char *argv[])
   Teuchos::ParameterList validParameters;
   Teuchos::ParameterList myParams("testParameterList");
 
-  for (int i=0; i < NUMPARAMS; i++)
-    myParams.set(paramList[i][0], paramList[i][1]);
+  for (int i=0; i < NUMSTR; i++){
+    myParams.set(strParams[i][0], strParams[i][1]);
+  }
+
+  for (int i=0; i < NUMANYINT; i++){
+    istringstream iss(anyIntParams[i][1]);
+    int paramValue;
+    iss >> paramValue;
+    myParams.set(anyIntParams[i][0], paramValue);
+  }
+
+  for (int i=0; i < NUMFN; i++){
+    myParams.set(fnParams[i][0], fnParams[i][1]);
+  }
 
   Teuchos::ParameterList origParams(myParams);
 
@@ -138,29 +199,39 @@ int main(int argc, char *argv[])
 
   // Try invalid parameter values
 
-  for (int i=0; i < NUMPARAMS; i++){
-    validParameters = Teuchos::ParameterList();
+  for (int i=0; i < NUMSTR; i++){
     Teuchos::ParameterList badParams(origParams);
-    badParams.set(paramList[i][0], paramList[i][2]);
-    cout << endl;
-    cout << paramList[i][0] << " = " << paramList[i][2] << endl;
-
-    bool failed = false;
-    try{
-      Zoltan2::createValidatorList(badParams, validParameters);
-      badParams.validateParametersAndSetDefaults(validParameters);
-    }
-    catch(std::exception &e){
-      cout << "Correctly generated an error:" << endl;
-      cout << e.what() << endl;
-      failed = true;
-    }
-
-    if (!failed){
-      cerr << "Bad parameter was not detected in parameter list." << endl;
+    int fail = 
+      testInvalidValue<string>(badParams, strParams[i][0], strParams[i][2]);
+    if (fail){
+      cout << "FAIL" << endl;
       return 1;
     }
   }
+
+  for (int i=0; i < NUMANYINT; i++){
+    Teuchos::ParameterList badParams(origParams);
+    int fail = 
+       testInvalidValue<string>(badParams, anyIntParams[i][0], anyIntParams[i][2]);
+    if (fail){
+      cout << "FAIL" << endl;
+      return 1;
+    }
+  }
+
+  for (int i=0; i < NUMFN; i++){
+    Teuchos::ParameterList badParams(origParams);
+    istringstream iss(fnParams[i][2]);
+    double badVal;
+    iss >> badVal;
+    int fail = 
+       testInvalidValue<double>(badParams, fnParams[i][0], badVal);
+    if (fail){
+      cout << "FAIL" << endl;
+      return 1;
+    }
+  }
+
 
   // Print out all the documentation
 
