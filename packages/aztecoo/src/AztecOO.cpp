af888896838b5f25e6c868105c49185f34e6b24a
Chetan Jhurani <jhurani@txcorp.com>
2012-08-30 13:46:36 -0600

    Use NumGlobalNonzeros64 or NumGlobalNonzeros depending on global index type.  In future, only the 64 suffix will be needed.
    Build/Test Cases Summary
    Enabled Packages: FEApp, Amesos, AztecOO, Epetra, EpetraExt, Ifpack, Isorropia, ML, NOX, Phalanx, ThyraEpetraAdapters, Tpetra, TrilinosCouplings, Triutils, Xpetra
    Enabled all Forward Packages
    0) MPI_DEBUG => passed: passed=808,notpassed=0 (15.09 min)
    1) SERIAL_RELEASE => passed: passed=774,notpassed=0 (12.54 min)
    Other local commits for this build/test group: a2a714a, f568942, 2c53340, ead7f5b, fdfb3f3, 51e976c, f3a162f, 4511b34, 3920a2c, 42023bb, cbc9a24, 9857436, 7e09e7b, 237b163, 23d8b64, 4f9f7b5, ba448e9, 48c2103, f91fd3f, 5721806, 3d8dde1, 372781d, 29ef1d4, dbfc097, e6ec9c4, 1d5e7b3, 424b78c, 0ce9c68, f0073b4, 2ce533c, 542a37c, c61a0f7, 615a117, 8282980, c06da28, 863d133, 7822c84, a2b7aea, 1eabc62, dc81a86, e3d0c1b, 8c40b80, 5a8d48e, 1170b41, 7fb2a0a, 2c84526, ce97fea, cf64926, 2c3ac99, 62c996e, 8961c6c, 562e95b, 0196e22

--- packages/aztecoo/src/AztecOO.cpp
+++ packages/aztecoo/src/AztecOO.cpp
@@ -908,7 +908,9 @@ int AztecOO::Iterate(long long MaxIters, double Tolerance)
 
   if (UserMatrixData_!=0)
     if (GetUserMatrix()!=0) {
-      int nnz = GetUserMatrix()->NumGlobalNonzeros();
+      long long nnz = GetUserMatrix()->RowMatrixRowMap().GlobalIndicesInt() ?
+         GetUserMatrix()->NumGlobalNonzeros() :
+         GetUserMatrix()->NumGlobalNonzeros64();
       if (nnz==0) {
 	EPETRA_CHK_ERR(-12); // Matrix has no entries.
       }
