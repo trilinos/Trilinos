6e07298e5b43bc124b4e8582d6d46a43365fe2e3
Siva Rajamanickam <srajama@sandia.gov>
2012-09-05 11:21:18 -0600

    Setting up a experimental nightly test for ShyLU.

---
+++ cmake/ctest/drivers/s921620/cron_driver_s921620.sh
@@ -0,0 +1,7 @@
+
+export MAILTO="shylu-regression@software.sandia.gov"
+export TDD_HTTP_PROXY="http://sonproxy.sandia.gov:80/"
+export TDD_CTEST_TEST_TYPE=Nightly
+export HTTP_PROXY="http://sonproxy.sandia.gov:80/"
+/usr/local/bin/git pull
+env Trilinos_PACKAGES=ShyLU CTEST_TEST_TYPE=Nightly ../cron_driver.py
