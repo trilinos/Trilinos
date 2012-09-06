77bd2c57024c658d08cc315fd7941fe6a0969862
Siva Rajamanickam <srajama@sandia.gov>
2012-09-06 13:17:05 -0600

    Adding tests for Amesos2

--- cmake/ctest/drivers/s921620/cron_driver_s921620.sh
+++ cmake/ctest/drivers/s921620/cron_driver_s921620.sh
@@ -1,8 +1,6 @@
 
 export MAILTO="shylu-regression@software.sandia.gov"
 export TDD_HTTP_PROXY="http://sonproxy.sandia.gov:80/"
-#export TDD_CTEST_TEST_TYPE=Nightly
 export HTTP_PROXY="http://sonproxy.sandia.gov:80/"
 /usr/local/bin/git pull
-#env Trilinos_PACKAGES=ShyLU CTEST_TEST_TYPE=Nightly ../cron_driver.py
-env Trilinos_PACKAGES=ShyLU ../cron_driver.py
+env ../cron_driver.py
