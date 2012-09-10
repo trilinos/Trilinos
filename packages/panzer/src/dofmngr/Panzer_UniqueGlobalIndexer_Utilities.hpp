a21368075aa3d78219bb342c96b4f5ae46c1a4fb
Eric C. Cyr <eccyr@sandia.gov>
2012-09-10 14:51:23 -0600

    Panzer: Adding field number based accessor to UGI utilities
    This is more convenient to work with then the field strings

--- packages/panzer/src/dofmngr/Panzer_UniqueGlobalIndexer_Utilities.hpp
+++ packages/panzer/src/dofmngr/Panzer_UniqueGlobalIndexer_Utilities.hpp
@@ -233,6 +233,13 @@ public:
    Teuchos::RCP<const Tpetra::Map<int,GlobalOrdinalT,Node> >
    getFieldMap(const std::string & fieldName) const;
 
+   /** Build a map that contains only global IDs associated with a particular field.
+     * This serves to go from a unique vector of all fields, to a vector
+     * containing the uniquely owned global ids for a single field.
+     */
+   Teuchos::RCP<const Tpetra::Map<int,GlobalOrdinalT,Node> >
+   getFieldMap(int fieldNum) const;
+
 protected:
    typedef Tpetra::Vector<int,int,GlobalOrdinalT,Node> IntVector;
    typedef Tpetra::Map<int,GlobalOrdinalT,Node> Map;
