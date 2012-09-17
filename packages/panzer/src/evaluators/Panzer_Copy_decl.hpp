18183b4830593da1940066c93140ac6c532b5615
Eric C. Cyr <eccyr@sandia.gov>
2012-09-17 11:25:55 -0600

    Panzer: Adding copy evaluator
    This basically exists to get the same data a different field name.
    Also adding a new "more flexible" BCStrategy define.

--- packages/panzer/src/evaluators/Panzer_Copy_decl.hpp
+++ packages/panzer/src/evaluators/Panzer_Copy_decl.hpp
@@ -40,47 +40,32 @@
 // ***********************************************************************
 // @HEADER
 
-#include <iostream>
-#include "Teuchos_RCP.hpp"
-#include "Panzer_BC.hpp"
-#include "Panzer_BCStrategy_TemplateManager.hpp"
+#ifndef PANZER_EVALUATOR_COPY_HPP
+#define PANZER_EVALUATOR_COPY_HPP
 
-#undef PANZER_DECLARE_BCSTRATEGY_TEMPLATE_BUILDER
-#define PANZER_DECLARE_BCSTRATEGY_TEMPLATE_BUILDER(key, fClass, fType)				        \
-									\
-  struct fType ## _TemplateBuilder {					\
-    const panzer::BC& m_bc;						\
-    const Teuchos::RCP<panzer::GlobalData> m_global_data;               \
-    fType ## _TemplateBuilder(const panzer::BC& bc, const Teuchos::RCP<panzer::GlobalData>& global_data) : m_bc(bc), m_global_data(global_data) {} \
-									\
-    template<typename EvalT>						\
-    Teuchos::RCP<panzer::BCStrategyBase> build() const {           	\
-      fClass <EvalT>* ptr = new fClass <EvalT>(m_bc,m_global_data);   	\
-      return Teuchos::rcp(ptr);						\
-    }									\
-    									\
-  };
+#include "Phalanx_Evaluator_Macros.hpp"
+#include "Phalanx_MDField.hpp"
 
-#define PANZER_DECLARE_BCSTRATEGY_TEMPLATE_BUILDER_EXTRA(key, fClass, fType, extraSteps)				        \
-									\
-  struct fType ## _TemplateBuilder {					\
-    const panzer::BC& m_bc;						\
-    const Teuchos::RCP<panzer::GlobalData> m_global_data;               \
-    fType ## _TemplateBuilder(const panzer::BC& bc, const Teuchos::RCP<panzer::GlobalData>& global_data) : m_bc(bc), m_global_data(global_data) {} \
-									\
-    template<typename EvalT>						\
-    Teuchos::RCP<panzer::BCStrategyBase> build() const {           	\
-      fClass <EvalT>* ptr = new fClass <EvalT>(m_bc,m_global_data);   	\
-      { extraSteps }                                                    \
-      return Teuchos::rcp(ptr);						\
-    }									\
-    									\
-  };
+namespace panzer {
+    
+/** Copies the contents of one field to anouther with a different
+  * name. This basically allows easy renaming of fields. The constructor
+  * takes a parameter list of the form
+    \verbatim
+    <ParameterList>
+      <Parameter name="Source Name" type="string" value="<Input Field Name>"/>
+      <Parameter name="Destination Name" type="string" value="<Output Field Name>"/>
+      <Parameter name="Data Layout" type="RCP<PHX::DataLayout>" value="<Pointer to data layout describing input and output fields>"/>
+    <ParameterList/>
+    \endverbatim
+  */
+PHX_EVALUATOR_CLASS(Copy)
+  
+  PHX::MDField<ScalarT> input;
+  PHX::MDField<ScalarT> output;
 
-#undef PANZER_BUILD_BCSTRATEGY_OBJECTS
-#define PANZER_BUILD_BCSTRATEGY_OBJECTS(key, fClass, fType)	        \
-  if (bc.strategy() == key) {						\
-    fType ## _TemplateBuilder builder(bc, global_data);			\
-      bcs_tm->buildObjects(builder);				        \
-      found = true;                                                     \
-    }
+PHX_EVALUATOR_CLASS_END
+
+}
+
+#endif
