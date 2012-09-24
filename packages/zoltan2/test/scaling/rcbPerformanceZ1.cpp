5301961e23b800f2367954d9091bd24e1600e541
K Devine <kddevin@sandia.gov>
2012-09-24 14:30:14 -0600

    Disable Zoltan REMAP by default; we know it doesn't scale. ;) Command-line argument --remap re-enables it.

--- packages/zoltan2/test/scaling/rcbPerformanceZ1.cpp
+++ packages/zoltan2/test/scaling/rcbPerformanceZ1.cpp
@@ -338,6 +338,7 @@ int main(int argc, char *argv[])
   bool doMemory=false;
   int numGlobalParts = nprocs;
   int dummyTimer=0;
+  int remap=0;
 
   CommandLineProcessor commandLine(false, true);
   commandLine.setOption("size", &numGlobalCoords, 
@@ -347,6 +348,8 @@ int main(int argc, char *argv[])
   commandLine.setOption("weightDim", &weightDim, 
     "Number of weights per coordinate, zero implies uniform weights.");
   commandLine.setOption("debug", &debugLevel, "Zoltan1 debug level");
+  commandLine.setOption("remap", &remap,
+    "Zoltan1 REMAP parameter; disabled by default for scalability testing");
   commandLine.setOption("timers", &dummyTimer, "ignored");
   commandLine.setOption(memoryOn.c_str(), memoryOff.c_str(), &doMemory,
     "do memory profiling");
@@ -461,6 +464,11 @@ int main(int argc, char *argv[])
   oss.str("");
   oss << debugLevel;
   Zoltan_Set_Param(zz, "DEBUG_LEVEL", oss.str().c_str());
+
+  if (remap)
+    Zoltan_Set_Param(zz, "REMAP", "1");
+  else
+    Zoltan_Set_Param(zz, "REMAP", "0");
 
   if (objective != balanceCount){
     oss.str("");
