e9334854aa277464fba9045585b9fb23843e0300
Jeremie Gaidamour <jngaida@sandia.gov>
2012-09-26 16:37:37 -0600

    MueLu: updated package dependencies
    SERIAL_RELEASE: 100% tests passed, 0 tests failed out of 662
    MPI_DEBUG:      100% tests passed, 0 tests failed out of 703

--- packages/muelu/cmake/Dependencies.cmake
+++ packages/muelu/cmake/Dependencies.cmake
@@ -1,11 +1,10 @@
 SET(LIB_REQUIRED_DEP_PACKAGES Teuchos Xpetra)
-SET(LIB_OPTIONAL_DEP_PACKAGES Amesos Amesos2 Belos Epetra EpetraExt KokkosClassic
+SET(LIB_OPTIONAL_DEP_PACKAGES Amesos Amesos2 Epetra EpetraExt KokkosClassic
                               Ifpack Ifpack2 ML Tpetra Zoltan Stratimikos)
 SET(TEST_REQUIRED_DEP_PACKAGES )
-SET(TEST_OPTIONAL_DEP_PACKAGES Epetra EpetraExt Galeri Tpetra) # TODO: clean up this line
+SET(TEST_OPTIONAL_DEP_PACKAGES Epetra EpetraExt Galeri Tpetra Belos AztecOO) # TODO: clean up this line
 SET(LIB_REQUIRED_DEP_TPLS BLAS LAPACK)
 SET(LIB_OPTIONAL_DEP_TPLS)
 SET(TEST_REQUIRED_DEP_TPLS)
 SET(TEST_OPTIONAL_DEP_TPLS)
 
-#TODO: why there is no EXAMPLE_*_DEP_* ?
