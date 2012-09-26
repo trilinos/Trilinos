dbfd68b4930ab5a4112f4e9edff743021286ccad
Dan Sunderland <dsunder@sandia.gov>
2012-09-25 18:43:10 -0600

    KokkosArray: added macros which add device keywords depending on the compiler

--- packages/kokkos/array/perf_test/PerfTestHost.cpp
+++ packages/kokkos/array/perf_test/PerfTestHost.cpp
@@ -1,13 +1,13 @@
 /*
 //@HEADER
 // ************************************************************************
-// 
+//
 //   KokkosArray: Manycore Performance-Portable Multidimensional Arrays
 //              Copyright (2012) Sandia Corporation
-// 
+//
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -35,8 +35,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact  H. Carter Edwards (hcedwar@sandia.gov) 
-// 
+// Questions? Contact  H. Carter Edwards (hcedwar@sandia.gov)
+//
 // ************************************************************************
 //@HEADER
 */
@@ -49,12 +49,10 @@
 
 #include <KokkosArray_Host.hpp>
 
-#include <KokkosArray_Host_macros.hpp>
 #include <PerfTestHexGrad.hpp>
 #include <PerfTestBlasKernels.hpp>
 #include <PerfTestGramSchmidt.hpp>
 #include <PerfTestDriver.hpp>
-#include <KokkosArray_Clear_macros.hpp>
 
 //------------------------------------------------------------------------
 
@@ -66,9 +64,9 @@ protected:
   {
     KokkosArray::Host::initialize( 1 , 8 );
   }
-  
+
   static void TearDownTestCase()
-  { 
+  {
     KokkosArray::Host::finalize();
   }
 };
--- packages/kokkos/array/usecases/explicit_dynamics/explicit_test_host.cpp
+++ packages/kokkos/array/usecases/explicit_dynamics/explicit_test_host.cpp
@@ -1,13 +1,13 @@
 /*
 //@HEADER
 // ************************************************************************
-// 
+//
 //   KokkosArray: Manycore Performance-Portable Multidimensional Arrays
 //              Copyright (2012) Sandia Corporation
-// 
+//
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -35,8 +35,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact  H. Carter Edwards (hcedwar@sandia.gov) 
-// 
+// Questions? Contact  H. Carter Edwards (hcedwar@sandia.gov)
+//
 // ************************************************************************
 //@HEADER
 */
@@ -51,9 +51,9 @@
 
 #include <KokkosArray_Host.hpp>
 
-#include <KokkosArray_Host_macros.hpp>
+#include <impl/KokkosArray_Host_macros.hpp>
 #include <explicit_dynamics_app.hpp>
-#include <KokkosArray_Clear_macros.hpp>
+#include <impl/KokkosArray_Clear_macros.hpp>
 
 namespace Test{
 
