d22e1a931329d669cdb56cfb983ec0f9eb7fa56f
Jeremie Gaidamour <jngaida@sandia.gov>
2012-09-28 16:23:56 -0600

    MueLu: code refactorization of MLParameterListInterpreter
    - There was a lot of source code duplication. Make it more readable.
    - Fixed some bugs.
    - Changed parameter defaults to match ML defaults. This likely have side effect on existing examples.
    
    FIXME: Default parameters that are not read from the parameter list are still using MueLu's default.
           Ex: smoother sweeps == 1 if smoothers are not defined. ML default value is 2.

--- packages/muelu/src/Facade/MueLu_MLParameterListInterpreter_decl.hpp
+++ packages/muelu/src/Facade/MueLu_MLParameterListInterpreter_decl.hpp
@@ -43,12 +43,6 @@
 // ***********************************************************************
 //
 // @HEADER
-/*
- * MueLu_Interpreter_decl.hpp
- *
- *  Created on: Dec 7, 2011
- *      Author: wiesner
- */
 
 #ifndef MUELU_MLPARAMETERLISTINTERPRETER_DECL_HPP
 #define MUELU_MLPARAMETERLISTINTERPRETER_DECL_HPP
@@ -87,6 +81,7 @@ namespace MueLu {
   /*!
     @class MLParameterListInterpreter class.
     @brief Class that accepts ML-style parameters and builds a MueLu preconditioner.
+    This interpreter uses the same default values as ML. This allows to compare ML/MueLu results
   */
 
   template <class Scalar = double, class LocalOrdinal = int, class GlobalOrdinal = LocalOrdinal, class Node = Kokkos::DefaultNode::DefaultNodeType, class LocalMatOps = typename Kokkos::DefaultKernels<void,LocalOrdinal,Node>::SparseOps>
@@ -143,11 +138,11 @@ namespace MueLu {
     //! @brief static helper functions that also can be used from outside for translating ML parameters into MueLu objects
     //@{
 
-    //! Read coarse solver options and build the corresponding smoother factory
-    static RCP<SmootherFactory> GetCoarsestSolverFactory(const Teuchos::ParameterList & params, const RCP<FactoryBase> & AFact = Teuchos::null);
-
     //! Read smoother options and build the corresponding smoother factory
-    static RCP<SmootherFactory> GetSmootherFactory(const Teuchos::ParameterList & params, int level, const RCP<FactoryBase> & AFact = Teuchos::null);
+    // @param smootherType: "smoother" or "coarse"
+    // @param level: level number (must be specified if smootherType == "smootherType")
+    // @param AFact: Factory used by smoother to find 'A'
+    static RCP<SmootherFactory> GetSmootherFactory(const Teuchos::ParameterList & topLevelParamList, const std::string & smootherType, const int levelID = -1, const RCP<FactoryBase> & AFact = Teuchos::null);
 
     //@}
 
@@ -160,7 +155,7 @@ namespace MueLu {
     to add some factories that write out some debug information etc. which are not handled by the ML
     Parameter List itself. See information about the RAPFactory::AddTransferFactory method, too!
     */
-    void AddTransferFactory(const RCP<FactoryBase>& factory);
+    void AddTransferFactory(const RCP<FactoryBase> & factory);
 
     //! Returns number of transfer factories.
     size_t NumTransferFactories() const;
