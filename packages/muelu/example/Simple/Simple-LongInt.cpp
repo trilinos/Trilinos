ebffdf862f0a5c35a3aa17ac274da6a7b5e87fad
Jeremie Gaidamour <jngaida@sandia.gov>
2012-10-01 11:21:49 -0600

    MueLu: fixed Simple-LongInt example

--- packages/muelu/example/Simple/Simple-LongInt.cpp
+++ packages/muelu/example/Simple/Simple-LongInt.cpp
@@ -81,12 +81,8 @@ int main(int argc, char *argv[]) {
   GlobalOrdinal numGlobalElements = 256;
 
   // linear algebra library
-  // this example require Tpetra+Ifpack2+Amesos2 or Epetra+Ifpack+Amesos
-#if   defined(HAVE_MUELU_TPETRA) && defined(HAVE_MUELU_IFPACK2) && defined(HAVE_MUELU_AMESOS2)
+  // this example require Tpetra+Ifpack2+Amesos2
   Xpetra::UnderlyingLib lib = Xpetra::UseTpetra; 
-#elif defined(HAVE_MUELU_EPETRA) && defined(HAVE_MUELU_IFPACK)  && defined(HAVE_MUELU_AMESOS)
-  Xpetra::UnderlyingLib lib = Xpetra::UseEpetra;
-#endif
 
   //
   // Construct the problem
