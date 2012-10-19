770393b9b44e4139aec164ef1e670bc58d127f44
Jonathan Hu <jhu@sandia.gov>
2012-10-19 11:28:48 -0700

    Fixing umr reported by valgrind.
    Build/Test Cases Summary
    Enabled Packages: MueLu, Xpetra
    Enabled all Forward Packages
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    2) MPI_SS_DEBUG => passed: passed=173,notpassed=0 (2.47 min)
    Other local commits for this build/test group: 7504ff8

--- packages/xpetra/scripts/lib/XpetraLibConfig.py
+++ packages/xpetra/scripts/lib/XpetraLibConfig.py
@@ -1,2 +1,2 @@
-#trilinosRoot_dir = "/home/jngaida/dev/MueLu/src/"
-trilinosRoot_dir = "/home/jhu/Research/MLoo/clean/Trilinos"
+#trilinosRoot_dir = "/home/jhu/Research/MLoo/clean/Trilinos"
+trilinosRoot_dir = "../../.."
