4346d35e458d42b07da0fcec0147df0f5fc35cac
Eric Phipps <etphipp@sandia.gov>
2012-10-26 10:44:20 -0600

    Fixed Stokhos usage of KokkosArray macros.

--- packages/stokhos/src/sacado/kokkos/Stokhos_StaticFixedStorage.hpp
+++ packages/stokhos/src/sacado/kokkos/Stokhos_StaticFixedStorage.hpp
@@ -62,37 +62,37 @@ namespace Stokhos {
     };
 
     //! Constructor
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     StaticFixedStorage(const ordinal_type& sz,
 		       const value_type& x = value_type(0.0)) { 
       ss::fill(coeff_, Num, x); 
     }
 
     //! Copy constructor
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     StaticFixedStorage(const StaticFixedStorage& s) {
       ss::copy(s.coeff_, coeff_, Num);
     }
 
     //! Destructor
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     ~StaticFixedStorage() {}
 
     //! Assignment operator
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     StaticFixedStorage& operator=(const StaticFixedStorage& s) {
       ss::copy(s.coeff_, coeff_, Num);
       return *this;
     }
 
     //! Initialize values to a constant value
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     void init(const_reference v) { 
       ss::fill(coeff_, Num, v); 
     }
 
     //! Initialize values to an array of values
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     void init(const_pointer v, const ordinal_type& sz = 0) {
       if (sz == 0)
       	ss::copy(v, coeff_, Num);
@@ -101,48 +101,48 @@ namespace Stokhos {
     }
 
     //! Load values to an array of values
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     void load(pointer v) { 
       ss::copy(coeff_, v, Num); 
     }
 
     //! Resize to new size (values are preserved)
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     void resize(const ordinal_type& sz) {}
 
     //! Reset storage to given array, size, and stride
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     void shallowReset(pointer v, const ordinal_type& sz, 
 		      const ordinal_type& stride, bool owned) {}
 
     //! Return size
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     static ordinal_type size() { return Num; }
 
     //! Coefficient access (avoid if possible)
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     const_reference operator[] (const ordinal_type& i) const { 
       return coeff_[i];
     }
 
     //! Coefficient access (avoid if possible)
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     reference operator[] (const ordinal_type& i) { return coeff_[i]; }
 
     template <int i>
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     reference getCoeff() { return coeff_[i]; }
 
     template <int i>
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     const_reference getCoeff() const { return coeff_[i]; }
 
     //! Get coefficients
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     const_pointer coeff() const { return coeff_; }
 
     //! Get coefficients
-    KOKKOSARRAY_INLINE_DEVICE_FUNCTION
+    KOKKOSARRAY_INLINE_FUNCTION
     pointer coeff() { return coeff_; }
 
   private:
