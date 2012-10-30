10651494805448239f1503b4f87cc742033abf43
Julien Cortial <jcortia@sandia.gov>
2012-10-30 13:20:00 -0700

    Piro: Added basic RythmosSolver unit testing

--- packages/piro/test/Piro_RythmosSolver_UnitTests.cpp
+++ packages/piro/test/Piro_RythmosSolver_UnitTests.cpp
@@ -47,17 +47,30 @@
 #ifdef Piro_ENABLE_Rythmos
 #include "Piro_RythmosSolver.hpp"
 
+#include "Piro_Test_ThyraSupport.hpp"
+
 #include "MockModelEval_A.hpp"
 
 #include "Thyra_EpetraModelEvaluator.hpp"
 #include "Thyra_ModelEvaluatorHelpers.hpp"
 
+#include "Thyra_DefaultNominalBoundsOverrideModelEvaluator.hpp"
+
 #include "Thyra_AmesosLinearOpWithSolveFactory.hpp"
 
 #include "Teuchos_UnitTestHarness.hpp"
 
+#include "Teuchos_Ptr.hpp"
+#include "Teuchos_Array.hpp"
+#include "Teuchos_Tuple.hpp"
+
 using namespace Teuchos;
 using namespace Piro;
+using namespace Piro::Test;
+
+namespace Thyra {
+  typedef ModelEvaluatorBase MEB;
+} // namespace Thyra
 
 // Setup support
 
@@ -77,7 +90,9 @@ const RCP<Thyra::ModelEvaluatorDefaultBase<double> > thyraModelNew(const RCP<Epe
   return epetraModelEvaluator(epetraModel, lowsFactory);
 }
 
-const RCP<RythmosSolver<double> > solverNew(const RCP<Thyra::ModelEvaluatorDefaultBase<double> > &thyraModel)
+const RCP<RythmosSolver<double> > solverNew(
+    const RCP<Thyra::ModelEvaluatorDefaultBase<double> > &thyraModel,
+    double finalTime)
 {
   const RCP<Rythmos::DefaultIntegrator<double> > integrator =
     Rythmos::defaultIntegrator<double>();
@@ -86,19 +101,87 @@ const RCP<RythmosSolver<double> > solverNew(const RCP<Thyra::ModelEvaluatorDefau
   const RCP<Rythmos::SolverAcceptingStepperBase<double> > stepper =
     Rythmos::backwardEulerStepper<double>(thyraModel, stepSolver);
 
-  const double finalTime = 0.0;
-
   return rcp(new RythmosSolver<double>(integrator, stepper, stepSolver, thyraModel, finalTime));
 }
 
-const RCP<RythmosSolver<double> > solverNew(const RCP<EpetraExt::ModelEvaluator> &epetraModel)
+const RCP<RythmosSolver<double> > solverNew(
+    const RCP<EpetraExt::ModelEvaluator> &epetraModel,
+    double finalTime)
 {
-  return solverNew(thyraModelNew(epetraModel));
+  return solverNew(thyraModelNew(epetraModel), finalTime);
 }
 
-TEUCHOS_UNIT_TEST(Piro_RythmosSolver, Setup)
+void vectorAssign(Thyra::VectorBase<double> &target, const ArrayView<const double> &values) {
+  for (int i = 0; i < values.size(); ++i) {
+    Thyra::set_ele(i, values[i], ptrFromRef(target));
+  }
+}
+
+// Steady state model is such that f(x_init) = 0
+RCP<Thyra::ModelEvaluatorDefaultBase<double> > steadyStateModelNew() {
+  const RCP<Thyra::ModelEvaluatorDefaultBase<double> > model = thyraModelNew(epetraModelNew());
+
+  const RCP<Thyra::MEB::InArgs<double> > steadyStateNominal(
+      new Thyra::MEB::InArgs<double>(model->getNominalValues()));
+  {
+    const RCP<Thyra::VectorBase<double> > steadyStateSolution =
+      Thyra::createMember(model->get_x_space());
+    vectorAssign(*steadyStateSolution, tuple(1.0, 2.0, 3.0, 4.0));
+    steadyStateNominal->set_x(steadyStateSolution);
+  }
+
+  return rcp(new Thyra::DefaultNominalBoundsOverrideModelEvaluator<double>(model, steadyStateNominal));
+}
+
+// Floating point tolerance
+const double tol = 1.0e-8;
+
+TEUCHOS_UNIT_TEST(Piro_RythmosSolver, SteadyState_Solution)
 {
-  const RCP<RythmosSolver<double> > solver = solverNew(epetraModelNew());
+  const RCP<Thyra::ModelEvaluatorDefaultBase<double> > model = steadyStateModelNew();
+  const double finalTime = 0.0;
+
+  const RCP<RythmosSolver<double> > solver = solverNew(model, finalTime);
+
+  const Thyra::MEB::InArgs<double> inArgs = solver->getNominalValues();
+
+  Thyra::MEB::OutArgs<double> outArgs = solver->createOutArgs();
+  const int solutionResponseIndex = solver->Ng() - 1;
+  const RCP<Thyra::VectorBase<double> > solution =
+    Thyra::createMember(solver->get_g_space(solutionResponseIndex));
+  outArgs.set_g(solutionResponseIndex, solution);
+
+  solver->evalModel(inArgs, outArgs);
+
+  const RCP<const Thyra::VectorBase<double> > initialCondition =
+    model->getNominalValues().get_x();
+
+  TEST_COMPARE_FLOATING_ARRAYS(
+      arrayFromVector(*solution),
+      arrayFromVector(*initialCondition),
+      tol);
+}
+
+TEUCHOS_UNIT_TEST(Piro_RythmosSolver, SteadyState_Response)
+{
+  const RCP<Thyra::ModelEvaluatorDefaultBase<double> > model = steadyStateModelNew();
+  const double finalTime = 0.0;
+
+  const RCP<RythmosSolver<double> > solver = solverNew(model, finalTime);
+
+  const Thyra::MEB::InArgs<double> inArgs = solver->getNominalValues();
+
+  Thyra::MEB::OutArgs<double> outArgs = solver->createOutArgs();
+  const int responseIndex = 0;
+  const RCP<Thyra::VectorBase<double> > response =
+    Thyra::createMember(solver->get_g_space(responseIndex));
+  outArgs.set_g(responseIndex, response);
+
+  solver->evalModel(inArgs, outArgs);
+
+  const Array<double> actual = arrayFromVector(*outArgs.get_g(responseIndex));
+  const Array<double> expected = tuple(8.0);
+  TEST_COMPARE_FLOATING_ARRAYS(actual, expected, tol);
 }
 
 #endif /* Piro_ENABLE_Rythmos */
