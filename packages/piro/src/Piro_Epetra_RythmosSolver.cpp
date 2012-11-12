ac00a46f35bde27eae679f7e0b06cffae10d6ea9
Julien Cortial <jcortia@sandia.gov>
2012-11-12 14:36:15 -0800

    Piro: Implemented Epetra RythmosSolver using Thyra version

--- packages/piro/src/Piro_Epetra_RythmosSolver.cpp
+++ packages/piro/src/Piro_Epetra_RythmosSolver.cpp
@@ -61,18 +61,16 @@
 #include <string>
 #include <iostream>
 
-Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> piroParams_,
+Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> piroParams,
                           Teuchos::RCP<EpetraExt::ModelEvaluator> model_,
-                          Teuchos::RCP<Rythmos::IntegrationObserverBase<Scalar> > observer ) :
-  piroParams(piroParams_),
-  model(model_)
+                          Teuchos::RCP<Rythmos::IntegrationObserverBase<double> > observer ) :
+  model(model_),
+  num_p(model->createInArgs().Np()),
+  num_g(model->createOutArgs().Ng()),
+  out(Teuchos::VerboseObjectBase::getDefaultOStream())
 {
-  //piroParams->validateParameters(*Piro::getValidPiroParameters(),0);
-
   using Teuchos::RCP;
   using Teuchos::rcp;
-
-      out = Teuchos::VerboseObjectBase::getDefaultOStream();
 
   // Allow for Matrix-Free implementation
   string jacobianSource = piroParams->get("Jacobian Operator", "Have Jacobian");
@@ -84,9 +82,6 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
     else model = Teuchos::rcp(new Piro::Epetra::MatrixFreeDecorator(model));
   }
 
-
-  num_p = model->createInArgs().Np();
-  num_g = model->createOutArgs().Ng();
 
   TEUCHOS_TEST_FOR_EXCEPTION(num_p > 1, Teuchos::Exceptions::InvalidParameter,
                      std::endl << "Error in Piro::Epetra::RythmosSolver " <<
@@ -106,10 +101,8 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
 //  directly passed to Rythmols builders, and a Stratimikos sublist. THe
 //  old way will be deprecated at some point.
 
-/** Old parameter list format **/
   if (piroParams->isSublist("Rythmos")) {
-
-      oldListStyle=true;
+    /** Old parameter list format **/
       RCP<Teuchos::ParameterList> rythmosPL = sublist(piroParams, "Rythmos", true);
       rythmosPL->validateParameters(*getValidRythmosParameters(),0);
 
@@ -124,11 +117,11 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
       }
 
       const int numTimeSteps = rythmosPL->get("Num Time Steps", 10);
-      const Scalar t_init = 0.0;
+      const double t_init = 0.0;
       t_final = rythmosPL->get("Final Time", 0.1);
 
-      const Rythmos::TimeRange<Scalar> fwdTimeRange(t_init, t_final);
-      const Scalar delta_t = t_final / (double) numTimeSteps;
+      const Rythmos::TimeRange<double> fwdTimeRange(t_init, t_final);
+      const double delta_t = t_final / (double) numTimeSteps;
       *out << "\ndelta_t = " << delta_t;
 
       const string stepperType = rythmosPL->get("Stepper Type", "Backward Euler");
@@ -142,7 +135,7 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
 
       Stratimikos::DefaultLinearSolverBuilder linearSolverBuilder;
       linearSolverBuilder.setParameterList(sublist(rythmosPL, "Stratimikos", true));
-      RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar> >
+      RCP<Thyra::LinearOpWithSolveFactoryBase<double> >
 	W_factory = createLinearSolveStrategy(linearSolverBuilder);
 
       //
@@ -165,7 +158,7 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
 
       fwdStateModel = epetraModelEvaluator(model, W_factory);
 
-      const RCP<const Thyra::VectorSpaceBase<Scalar> >
+      const RCP<const Thyra::VectorSpaceBase<double> >
 	x_space = fwdStateModel->get_x_space();
 
       //
@@ -198,18 +191,16 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
         bool var = integrationControlPL->get( "Take Variable Steps", false );
         integrationControlPL->set( "Fixed dt", Teuchos::as<double>(delta_t) );
 
-        RCP<Rythmos::DefaultIntegrator<Scalar> >
-          defaultIntegrator = Rythmos::controlledDefaultIntegrator<Scalar>(
-            Rythmos::simpleIntegrationControlStrategy<Scalar>(integrationControlPL)
+        RCP<Rythmos::DefaultIntegrator<double> >
+          defaultIntegrator = Rythmos::controlledDefaultIntegrator<double>(
+            Rythmos::simpleIntegrationControlStrategy<double>(integrationControlPL)
             );
         fwdStateIntegrator = defaultIntegrator;
       }
       fwdStateIntegrator->setParameterList(sublist(rythmosPL, "Rythmos Integrator", true));
     }
-/** New parameter list format **/
   else if (piroParams->isSublist("Rythmos Solver")) {
-      oldListStyle=false;
-
+/** New parameter list format **/
       RCP<Teuchos::ParameterList> rythmosSolverPL = sublist(piroParams, "Rythmos Solver", true);
       RCP<Teuchos::ParameterList> rythmosPL = sublist(rythmosSolverPL, "Rythmos", true);
       rythmosSolverPL->validateParameters(*getValidRythmosSolverParameters(),0);
@@ -238,7 +229,7 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
 
       Stratimikos::DefaultLinearSolverBuilder linearSolverBuilder;
       linearSolverBuilder.setParameterList(sublist(rythmosSolverPL, "Stratimikos", true));
-      RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar> >
+      RCP<Thyra::LinearOpWithSolveFactoryBase<double> >
 	W_factory = createLinearSolveStrategy(linearSolverBuilder);
 
       //
@@ -262,7 +253,7 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
 
       fwdStateModel = epetraModelEvaluator(model, W_factory);
 
-      const RCP<const Thyra::VectorSpaceBase<Scalar> >
+      const RCP<const Thyra::VectorSpaceBase<double> >
 	x_space = fwdStateModel->get_x_space();
 
       //
@@ -286,20 +277,36 @@ Piro::Epetra::RythmosSolver::RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> 
     Thyra::ModelEvaluatorBase::InArgs<double> ic = fwdStateModel->getNominalValues();
     RCP<Rythmos::IntegratorBase<double> > integrator = ib->create(fwdStateModel,ic,fwdTimeStepSolver);
     fwdStateIntegrator = Teuchos::rcp_dynamic_cast<Rythmos::DefaultIntegrator<double> >(integrator,true);
-  }
-  else
+
+    fwdStateStepper = fwdStateIntegrator->getNonconstStepper();
+  } else {
       TEUCHOS_TEST_FOR_EXCEPTION(piroParams->isSublist("Rythmos")||piroParams->isSublist("Rythmos Solver"),
         Teuchos::Exceptions::InvalidParameter, std::endl <<
        "Error! Piro::Epetra::RythmosSolver: must have either Rythmos or Rythmos Solver sublist ");
+  }
 
 
   if (observer != Teuchos::null)
     fwdStateIntegrator->setIntegrationObserver(observer);
 
+  thyraImplementation_ = rcp(new ThyraRythmosSolver(
+   fwdStateIntegrator,
+   fwdStateStepper,
+   fwdTimeStepSolver,
+   fwdStateModel,
+   t_final,
+   Teuchos::null, // No initial condition model
+   solnVerbLevel));
 }
 
-Piro::Epetra::RythmosSolver::~RythmosSolver()
+int Piro::Epetra::RythmosSolver::Np() const
 {
+  return num_p;
+}
+
+int Piro::Epetra::RythmosSolver::Ng() const
+{
+  return num_g + 1;
 }
 
 Teuchos::RCP<const Epetra_Map> Piro::Epetra::RythmosSolver::get_x_map() const
@@ -354,11 +361,9 @@ Teuchos::RCP<const Epetra_Vector> Piro::Epetra::RythmosSolver::get_p_init(int l)
 
 EpetraExt::ModelEvaluator::InArgs Piro::Epetra::RythmosSolver::createInArgs() const
 {
-  //return underlyingME->createInArgs();
   EpetraExt::ModelEvaluator::InArgsSetup inArgs;
   inArgs.setModelEvalDescription(this->description());
-  inArgs.set_Np(num_p);
-//  inArgs.setSupports(IN_ARG_x,true);
+  inArgs.set_Np(thyraImplementation_->Np());
   return inArgs;
 }
 
@@ -367,221 +372,80 @@ EpetraExt::ModelEvaluator::OutArgs Piro::Epetra::RythmosSolver::createOutArgs() 
   EpetraExt::ModelEvaluator::OutArgsSetup outArgs;
   outArgs.setModelEvalDescription(this->description());
 
-  // Ng is 1 bigger then model-Ng so that the solution vector can be an outarg
-  outArgs.set_Np_Ng(num_p, num_g+1);
+  {
+    const int thyraNp = thyraImplementation_->Np();
+    const int thyraNg = thyraImplementation_->Ng();
 
-  EpetraExt::ModelEvaluator::OutArgs model_outargs = model->createOutArgs();
-  for (int i=0; i<num_g; i++)
-    for (int j=0; j<num_p; j++)
-      if (!model_outargs.supports(OUT_ARG_DgDp, i, j).none())
-        outArgs.setSupports(OUT_ARG_DgDp, i, j,
-                            DerivativeSupport(DERIV_MV_BY_COL));
+    outArgs.set_Np_Ng(thyraNp, thyraNg);
+
+    const Thyra::ModelEvaluatorBase::OutArgs<double> thyraOutArgs = thyraImplementation_->createOutArgs();
+    for (int j = 0; j < thyraNg; ++j) {
+      for (int l = 0; l < thyraNp; ++l) {
+        EpetraExt::ModelEvaluator::DerivativeSupport dgdp_support;
+        {
+          const Thyra::ModelEvaluatorBase::DerivativeSupport thyra_dgdp_support =
+            thyraOutArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_DgDp, j, l);
+
+          if (thyra_dgdp_support.supports(Thyra::ModelEvaluatorBase::DERIV_MV_JACOBIAN_FORM)) {
+            dgdp_support.plus(EpetraExt::ModelEvaluator::DERIV_MV_BY_COL);
+          }
+          if (thyra_dgdp_support.supports(Thyra::ModelEvaluatorBase::DERIV_MV_GRADIENT_FORM)) {
+            dgdp_support.plus(EpetraExt::ModelEvaluator::DERIV_TRANS_MV_BY_ROW);
+          }
+        }
+        outArgs.setSupports(EpetraExt::ModelEvaluator::OUT_ARG_DgDp, j, l, dgdp_support);
+      }
+    }
+  }
 
   return outArgs;
 }
 
-void Piro::Epetra::RythmosSolver::evalModel( const InArgs& inArgs,
-                                     const OutArgs& outArgs ) const
+void Piro::Epetra::RythmosSolver::evalModel(const InArgs& inArgs, const OutArgs& outArgs) const
 {
-  using Teuchos::RCP;
-  using Teuchos::rcp;
+  Thyra::ModelEvaluatorBase::InArgs<double> thyraInArgs = thyraImplementation_->createInArgs();
+  for (int l = 0; l < thyraInArgs.Np(); ++l) {
+    const Teuchos::RCP<const Epetra_Vector> p_in = inArgs.get_p(l);
+    if (Teuchos::nonnull(p_in)) {
+      thyraInArgs.set_p(l, Thyra::create_Vector(p_in, thyraImplementation_->get_p_space(l)));
+    }
+  }
 
-  // Parse InArgs
-
-  RCP<const Epetra_Vector> p_in;
-  if (num_p > 0) p_in = inArgs.get_p(0);
-
-  // Parse OutArgs: always 1 extra
-  RCP<Epetra_Vector> g_out;
-  if (num_g > 0) g_out = outArgs.get_g(0);
-  RCP<Epetra_Vector> gx_out = outArgs.get_g(num_g);
-
-  // Parse out-args for sensitivity calculation
-  RCP<Epetra_MultiVector> dgdp_out;
-  if (num_p>0 && num_g>0)
-    dgdp_out = outArgs.get_DgDp(0,0).getMultiVector();
-
-  RCP<const Epetra_Vector> finalSolution;
-
-  Thyra::ModelEvaluatorBase::InArgs<Scalar>
-     state_ic = fwdStateModel->getNominalValues();
-
-  // Set paramters p_in as part of initial conditions
-  if (num_p > 0)
-    state_ic.set_p(0,Thyra::create_Vector(p_in, fwdStateModel->get_p_space(0)));
-
-  //*out << "\nstate_ic:\n" << Teuchos::describe(state_ic,Teuchos::VERB_NONE);
-  *out << "\nstate_ic:\n" << Teuchos::describe(state_ic,solnVerbLevel);
-
-  if (dgdp_out == Teuchos::null) {
-      //
-      *out << "\nE) Solve the forward problem ...\n";
-      //
-
-      if (oldListStyle) {
-        fwdStateStepper->setInitialCondition(state_ic);
-        fwdStateIntegrator->setStepper(fwdStateStepper, t_final, true);
+  Thyra::ModelEvaluatorBase::OutArgs<double> thyraOutArgs = thyraImplementation_-> createOutArgs();
+  {
+    // Translate outArgs -> thyraOutArgs
+    // Responses
+    for (int j = 0; j < thyraOutArgs.Ng(); ++j) {
+      const Teuchos::RCP<Epetra_Vector> g_out = outArgs.get_g(j);
+      if (Teuchos::nonnull(g_out)) {
+        thyraOutArgs.set_g(j, Thyra::create_Vector(g_out, thyraImplementation_->get_g_space(j)));
       }
-      else
-        fwdStateIntegrator->getNonconstStepper()->setInitialCondition(state_ic);
+    }
+    // Derivatives
+    for (int j = 0; j < thyraOutArgs.Ng(); ++j) {
+      for (int l = 0; l < thyraOutArgs.Np(); ++l) {
+        const Thyra::ModelEvaluatorBase::DerivativeSupport dgdp_support =
+          thyraOutArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_DgDp, j, l);
+        if (!dgdp_support.none()) {
+          const EpetraExt::ModelEvaluator::Derivative dgdp_out = outArgs.get_DgDp(j, l);
+          if (!dgdp_out.isEmpty()) {
+            TEUCHOS_ASSERT(Teuchos::is_null(dgdp_out.getLinearOp()));
+            const Thyra::ModelEvaluatorBase::EDerivativeMultiVectorOrientation mv_orient =
+              Thyra::convert(dgdp_out.getMultiVectorOrientation());
+            const Teuchos::RCP<const Thyra::VectorSpaceBase<double> > mv_range =
+              (mv_orient == Thyra::ModelEvaluatorBase::DERIV_MV_JACOBIAN_FORM) ?
+              thyraImplementation_->get_g_space(j) :
+              thyraImplementation_->get_p_space(l);
+            const Teuchos::RCP<Thyra::MultiVectorBase<double> > mv_thyra =
+              Thyra::create_MultiVector(dgdp_out.getMultiVector(), mv_range);
+            thyraOutArgs.set_DgDp(j, l, Thyra::ModelEvaluatorBase::Derivative<double>(mv_thyra, mv_orient));
+          }
+        }
+      }
+    }
+  }
 
-      Teuchos::Array<RCP<const Thyra::VectorBase<Scalar> > > x_final_array;
-      fwdStateIntegrator->getFwdPoints(
-        Teuchos::tuple<Scalar>(t_final), &x_final_array, NULL, NULL
-        );
-      const RCP<const Thyra::VectorBase<Scalar> > x_final = x_final_array[0];
-
-      finalSolution = Thyra::get_Epetra_Vector(*model->get_x_map(), x_final);
-
-      if (Teuchos::VERB_MEDIUM <= solnVerbLevel)
-         cout << "Final Solution\n" << *finalSolution << std::endl;
-
-     // As post-processing step, calc responses at final solution
-     EpetraExt::ModelEvaluator::InArgs model_inargs = model->createInArgs();
-     EpetraExt::ModelEvaluator::OutArgs model_outargs = model->createOutArgs();
-     model_inargs.set_x(finalSolution);
-     if (num_p > 0)  model_inargs.set_p(0, p_in);
-     if (g_out != Teuchos::null) {
-       g_out->PutScalar(0.0);
-       model_outargs.set_g(0, g_out);
-     }
-
-     model->evalModel(model_inargs, model_outargs);
-
-   }
-   else {//Doing sensitivities
-      //
-      *out << "\nE) Solve the forward problem with Sensitivities...\n";
-      //
-
-      RCP<Rythmos::ForwardSensitivityStepper<Scalar> > stateAndSensStepper =
-        Rythmos::forwardSensitivityStepper<Scalar>();
-      stateAndSensStepper->initializeSyncedSteppers(
-          fwdStateModel, 0, fwdStateModel->getNominalValues(),
-          fwdStateStepper, fwdTimeStepSolver);
-
-      //
-      // Set the initial condition for the state and forward sensitivities
-      //
-
-      RCP<Thyra::VectorBase<Scalar> > s_bar_init
-        = createMember(stateAndSensStepper->getFwdSensModel()->get_x_space());
-      assign( s_bar_init.ptr(), 0.0 );
-      RCP<Thyra::VectorBase<Scalar> > s_bar_dot_init
-        = createMember(stateAndSensStepper->getFwdSensModel()->get_x_space());
-      assign( s_bar_dot_init.ptr(), 0.0 );
-      // Above, I believe that these are the correct initial conditions for
-      // s_bar and s_bar_dot given how the EpetraExt::DiagonalTransientModel
-      // is currently implemented!
-
-      RCP<const Rythmos::StateAndForwardSensitivityModelEvaluator<Scalar> >
-        stateAndSensModel = stateAndSensStepper->getStateAndFwdSensModel();
-
-      Thyra::ModelEvaluatorBase::InArgs<Scalar>
-        state_and_sens_ic = stateAndSensStepper->getModel()->createInArgs();
-
-      // Copy time, parameters etc.
-      state_and_sens_ic.setArgs(state_ic);
-      // Set initial condition for x_bar = [ x; s_bar ]
-      state_and_sens_ic.set_x(
-        stateAndSensModel->create_x_bar_vec(state_ic.get_x(),s_bar_init)
-        );
-      // Set initial condition for x_bar_dot = [ x_dot; s_bar_dot ]
-      state_and_sens_ic.set_x_dot(
-        stateAndSensModel->create_x_bar_vec(state_ic.get_x_dot(),s_bar_dot_init)
-        );
-
-//      *out << "\nstate_and_sens_ic:\n" << Teuchos::describe(state_and_sens_ic,Teuchos::VERB_DEFAULT);
-
-      stateAndSensStepper->setInitialCondition(state_and_sens_ic);
-
-      //
-      // Use a StepperAsModelEvaluator to integrate the state+sens
-      //
-
-      RCP<Rythmos::StepperAsModelEvaluator<Scalar> >
-        stateAndSensIntegratorAsModel = Rythmos::stepperAsModelEvaluator(
-          Teuchos::rcp_implicit_cast<Rythmos::StepperBase<Scalar> >(stateAndSensStepper),
-          Teuchos::rcp_implicit_cast<Rythmos::IntegratorBase<Scalar> >(fwdStateIntegrator),
-          state_and_sens_ic
-          );
-
-      *out << "\nUse the StepperAsModelEvaluator to integrate state + sens x_bar(p,t_final) ... \n";
-
-      RCP<Thyra::VectorBase<Scalar> > x_bar_final;
-
-      Teuchos::OSTab tab(out);
-
-      x_bar_final = createMember(stateAndSensIntegratorAsModel->get_g_space(0));
-
-      eval_g(
-        *stateAndSensIntegratorAsModel,
-        0, *state_ic.get_p(0),
-        t_final,
-        0, &*x_bar_final
-        );
-
-      *out
-        << "\nx_bar_final = x_bar(p,t_final) evaluated using "
-        << "stateAndSensIntegratorAsModel:\n"
-        << Teuchos::describe(*x_bar_final,solnVerbLevel);
-
-     // As post-processing step, calc responses and gradient at final solution
-     const RCP<const Thyra::VectorBase<Scalar> > x_final =
-           Thyra::productVectorBase<Scalar>(x_bar_final)->getVectorBlock(0);
-     finalSolution = Thyra::get_Epetra_Vector(*model->get_x_map(), x_final);
-
-      *out << "\nF) Check the solution to the forward problem ...\n";
-
-     // Extract sensitivity vectors into Epetra_MultiVector
-     Teuchos::RCP<const Epetra_MultiVector> dxdp =
-       Thyra::get_Epetra_MultiVector(*model->get_x_map(),
-         Teuchos::rcp_dynamic_cast<const Thyra::DefaultMultiVectorProductVector<Scalar> >
-           (Thyra::productVectorBase<Scalar>(x_bar_final)->getVectorBlock(1))
-             ->getMultiVector() );;
-
-     dgdp_out->PutScalar(0.0);
-
-     Teuchos::RCP<Epetra_MultiVector> dgdx
-          = Teuchos::rcp(new Epetra_MultiVector(finalSolution->Map(),
-                                                   dgdp_out->GlobalLength()));
-     Teuchos::Array<int> p_indexes =
-       outArgs.get_DgDp(0,0).getDerivativeMultiVector().getParamIndexes();
-
-     EpetraExt::ModelEvaluator::DerivativeMultiVector dmv_dgdp(dgdp_out,
-                                                               DERIV_MV_BY_COL,
-                                                               p_indexes);
-
-     EpetraExt::ModelEvaluator::InArgs model_inargs = model->createInArgs();
-     EpetraExt::ModelEvaluator::OutArgs model_outargs = model->createOutArgs();
-     model_inargs.set_x(finalSolution);
-     model_inargs.set_p(0, p_in);
-
-     if (g_out != Teuchos::null) {
-       g_out->PutScalar(0.0);
-       model_outargs.set_g(0, g_out);
-     }
-     model_outargs.set_DgDp(0,0,dmv_dgdp);
-     model_outargs.set_DgDx(0,dgdx);
-
-     model->evalModel(model_inargs, model_outargs);
-
-
-     // (3) Calculate dg/dp = dg/dx*dx/dp + dg/dp
-     // This may be the transpose of what we want since we specified
-     // we want dg/dp by column in createOutArgs().
-     // In this case just interchange the order of dgdx and dxdp
-     // We should really probably check what the underlying ME does
-
-     if (Teuchos::VERB_MEDIUM <= solnVerbLevel) cout << " dgdx \n" << *dgdx << endl;
-     if (Teuchos::VERB_MEDIUM <= solnVerbLevel) cout << " dxdp \n" << *dxdp << endl;
-
-     dgdp_out->Multiply('T', 'N', 1.0, *dgdx, *dxdp, 1.0);
-
-   }
-
-   // return the final solution as an additional g-vector, if requested
-   if (gx_out != Teuchos::null)  *gx_out = *finalSolution;
+  thyraImplementation_->evalModel(thyraInArgs, thyraOutArgs);
 }
 
 Teuchos::RCP<const Teuchos::ParameterList>
--- packages/piro/src/Piro_Epetra_RythmosSolver.hpp
+++ packages/piro/src/Piro_Epetra_RythmosSolver.hpp
@@ -50,16 +50,11 @@
 #include "Rythmos_TimeStepNonlinearSolver.hpp"
 #include "Rythmos_StepperBase.hpp"
 
+#include "Piro_RythmosSolver.hpp"
+
 #include "Teuchos_ParameterList.hpp"
 
-/** \brief Epetra-based Model Evaluator subclass for Charon!
- *
- * This class will support a wide number of different types of abstract
- * problem types that will allow NOX, LOCA, Rythmos, Aristos, and MOOCHO to
- * solve different types of problems with Charon.
- *
- * ToDo: Finish documentation!
- */
+/** \brief . */
 
 namespace Piro {
 namespace Epetra {
@@ -67,77 +62,78 @@ namespace Epetra {
 class RythmosSolver
     : public EpetraExt::ModelEvaluator
 {
-
   public:
 
   typedef double Scalar;
-
   /** \name Constructors/initializers */
   //@{
 
   /** \brief Takes the number of elements in the discretization . */
   RythmosSolver(Teuchos::RCP<Teuchos::ParameterList> piroParams,
                 Teuchos::RCP<EpetraExt::ModelEvaluator> model,
-                Teuchos::RCP<Rythmos::IntegrationObserverBase<Scalar> > observer = Teuchos::null
+                Teuchos::RCP<Rythmos::IntegrationObserverBase<double> > observer = Teuchos::null
                 );
 
   //@}
 
-  ~RythmosSolver();
-
-
   /** \name Overridden from EpetraExt::ModelEvaluator . */
   //@{
-
-  Teuchos::RCP<const Epetra_Map> get_g_map(int j) const;
-  /** \brief . */
-  Teuchos::RCP<const Epetra_Vector> get_p_init(int l) const;
-  /** \brief . */
-//  Teuchos::RCP<Epetra_Operator> create_W() const;
-  /** \brief . */
-  EpetraExt::ModelEvaluator::InArgs createInArgs() const;
-  /** \brief . */
-  EpetraExt::ModelEvaluator::OutArgs createOutArgs() const;
-  /** \brief . */
-  void evalModel( const InArgs& inArgs, const OutArgs& outArgs ) const;
-
-  private:
   /** \brief . */
   Teuchos::RCP<const Epetra_Map> get_x_map() const;
   /** \brief . */
   Teuchos::RCP<const Epetra_Map> get_f_map() const;
   /** \brief . */
+  Teuchos::RCP<const Epetra_Map> get_p_map(int l) const;
+  /** \brief . */
+  Teuchos::RCP<const Epetra_Map> get_g_map(int j) const;
+
+  /** \brief . */
   Teuchos::RCP<const Epetra_Vector> get_x_init() const;
   /** \brief . */
-  Teuchos::RCP<const Epetra_Map> get_p_map(int l) const;
+  Teuchos::RCP<const Epetra_Vector> get_p_init(int l) const;
+  /** \brief . */
+  EpetraExt::ModelEvaluator::InArgs createInArgs() const;
+  /** \brief . */
+  EpetraExt::ModelEvaluator::OutArgs createOutArgs() const;
 
-  //! Valid list for old "Rythmos" parameter list style
-  Teuchos::RCP<const Teuchos::ParameterList> getValidRythmosParameters() const;
+  /** \brief . */
+  void evalModel(const InArgs& inArgs, const OutArgs& outArgs) const;
+  //@}
 
-  //! Valid list for new "Rythmos Solver" parameter list style
-  Teuchos::RCP<const Teuchos::ParameterList> getValidRythmosSolverParameters() const;
-
+  /** \name Basic information . */
+  //@{
+  /** \brief Return the number of sets of auxiliary parameters. */
+  int Np() const;
+  /** \brief Return the number of sets of auxiliary response functions. */
+  int Ng() const;
   //@}
 
   private:
+  /** \name Parameter list validation . */
+  //@{
+  //! Valid list for old "Rythmos" parameter list style
+  Teuchos::RCP<const Teuchos::ParameterList> getValidRythmosParameters() const;
+  //! Valid list for new "Rythmos Solver" parameter list style
+  Teuchos::RCP<const Teuchos::ParameterList> getValidRythmosSolverParameters() const;
+  //@}
 
-   //These are set in the constructor and used in evalModel
-   mutable Teuchos::RCP<Teuchos::ParameterList> piroParams;
    Teuchos::RCP<EpetraExt::ModelEvaluator> model;
 
    int num_p;
    int num_g;
 
-   Teuchos::RCP<Teuchos::FancyOStream> out;
-   Teuchos::EVerbosityLevel solnVerbLevel;
-   Teuchos::RCP<Rythmos::DefaultIntegrator<Scalar> > fwdStateIntegrator;
-   Teuchos::RCP<Thyra::ModelEvaluator<double> > fwdStateModel;
+   typedef ::Piro::RythmosSolver<double> ThyraRythmosSolver;
+   Teuchos::RCP<ThyraRythmosSolver> thyraImplementation_;
+
+   // To pass to implementation
+   Teuchos::RCP<Rythmos::DefaultIntegrator<double> > fwdStateIntegrator;
+   Teuchos::RCP<Rythmos::StepperBase<double> > fwdStateStepper;
    Teuchos::RCP<Rythmos::TimeStepNonlinearSolver<double> > fwdTimeStepSolver;
-   Scalar t_final;
+   double t_final;
+   Teuchos::RCP<Thyra::ModelEvaluatorDefaultBase<double> > fwdStateModel;
+   Teuchos::EVerbosityLevel solnVerbLevel;
 
-   bool oldListStyle;
-   Teuchos::RCP<Rythmos::StepperBase<Scalar> > fwdStateStepper;
-
+   Teuchos::RCP<Teuchos::FancyOStream> out;
 };
 
 }
