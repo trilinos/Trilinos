867b305e9d981384c69db4d26027188f5ced29e0
Jonathan Hu <jhu@sandia.gov>
2012-11-13 16:42:22 -0800

    Adding some documentation.
    This is mostly to refresh my own memory.

--- packages/muelu/src/Utils/MueLu_Monitor.hpp
+++ packages/muelu/src/Utils/MueLu_Monitor.hpp
@@ -58,11 +58,14 @@
 
 namespace MueLu {
 
-  //! Manage indentation of output using OSTab and verbosity level
+  /*! @class PrintMonitor
+    Manages indentation of output using Teuchos::OSTab and verbosity level
+  */
   class PrintMonitor : public BaseClass {
 
   public:
 
+    //! Constructor
     PrintMonitor(const BaseClass& object, const std::string& msg, MsgType msgLevel = Runtime0) {
 
       // Inherit verbosity from 'object'
@@ -86,8 +89,18 @@ namespace MueLu {
     RCP<Teuchos::OSTab> tab_;
   };
 
-  // Main Monitor
-  // A timer is created only if 'timerLevel' (Timings0 by default) is enabled
+  /*! @class Monitor
+     @brief Main MueLu time monitor.
+
+     A timer is created only if 'timerLevel' (Timings0 by default) is enabled.
+     This class manages verbosity, as well as times this object and all its children over all levels.
+
+     The timer yields output such as
+      \verbatim
+       MueLu: SaPFactory: Prolongator smoothing (total)             6.508 (6)        6.551 (6)        6.557 (6)        1.092 (6)
+      \endverbatim
+     Note that the keyword \a total denotes timing of the object and its children.
+  */
   class Monitor: public BaseClass {
   public:
     Monitor(const BaseClass& object, const std::string & msg, MsgType msgLevel = Runtime0, MsgType timerLevel = Timings0)
@@ -96,12 +109,27 @@ namespace MueLu {
     { }
 
   private:
+    //! Manages printing.
     PrintMonitor printMonitor_;
+    //! Records total time spent  in this object and all its children, over all levels.
     TimeMonitor timerMonitor_;
   };
 
-  // Another version of the Monitor, that does not repeat the object description
-  // A timer is created only if 'timerLevel' (Timings1 by default) is enabled
+  //---------------------------------------------------------------------------------------------------
+
+  /*! @class SubMonitor
+    @brief Another version of Monitor that doesn't repeat the object description.
+
+    Times object and all its children over all levels.
+    A timer is created only if 'timerLevel' (Timings1 by default) is enabled.
+
+    The timer yields output such as
+     \verbatim
+     MueLu: SaPFactory: Eigenvalue estimate (sub, total)        0.149 (6)        0.1615 (6)       0.1643 (6)       0.02692 (6)
+     \endverbatim
+     Note that the keyword \a sub denotes that this is output from a SubFactoryMonitor.
+     Note that the keyword \a total denotes timing of the object and its children.
+  */
   class SubMonitor: public BaseClass {
   public:
     SubMonitor(const BaseClass& object, const std::string & msg, MsgType msgLevel = Runtime1, MsgType timerLevel = Timings1)
@@ -114,42 +142,80 @@ namespace MueLu {
     TimeMonitor timerMonitor_;
   };
 
-  // Factory monitor
-  // Similar to Monitor but add a timer level by level
+  //---------------------------------------------------------------------------------------------------
+
+  /*! @class FactoryMonitor
+      @brief Similar to Monitor but with additional timers.
+
+     This class provides the following three timers for an object:
+     - Timer for this object over all levels, excluding calls to children.
+     - Timer for this object per level, excluding calls to children.
+     - Timer for this object over all levels, including calls to children.
+
+     The three timers above yield output like the following:
+     \verbatim
+       MueLu: SaPFactory: Prolongator smoothing                     2.599 (6)        2.602 (6)        2.607 (6)        0.4336 (6)
+       MueLu: SaPFactory: Prolongator smoothing (level=5)           0.4731 (1)       0.4735 (1)       0.4736 (1)       0.4735 (1)
+       MueLu: SaPFactory: Prolongator smoothing (total)             6.508 (6)        6.551 (6)        6.557 (6)        1.092 (6)
+     \endverbatim
+
+     Note that the keyword \a total denotes timing of the object and its children.
+  */
   class FactoryMonitor: public Monitor {
   public:
+
+    //! Constructor
     FactoryMonitor(const BaseClass& object, const std::string & msg, int levelID, MsgType msgLevel = Runtime0, MsgType timerLevel = Timings0)
       : Monitor(object, msg, msgLevel, timerLevel),
         timerMonitorExclusive_(object, object.ShortClassName() + ": " + msg, timerLevel)
     {
       if (IsPrint(TimingsByLevel)) {
         levelTimeMonitor_ = rcp(new TimeMonitor(object, object.ShortClassName() + ": " + msg + " (total, level=" + Teuchos::Utils::toString(levelID) + ")", timerLevel));
-	levelTimeMonitorExclusive_ = rcp(new MutuallyExclusiveTimeMonitor<Level>(object, object.ShortClassName() + ": " + msg + " (level=" + Teuchos::Utils::toString(levelID) + ")", timerLevel));
+        levelTimeMonitorExclusive_ = rcp(new MutuallyExclusiveTimeMonitor<Level>(object, object.ShortClassName() + ": " + msg + " (level=" + Teuchos::Utils::toString(levelID) + ")", timerLevel));
       }
     }
 
-    //TODO: code factorization
+    /*! Constructor
+
+      TODO: code factorization
+    */
     FactoryMonitor(const BaseClass& object, const std::string & msg, const Level & level, MsgType msgLevel = Runtime0, MsgType timerLevel = Timings0)
       : Monitor(object, msg, msgLevel, timerLevel),
         timerMonitorExclusive_(object, object.ShortClassName() + ": " + msg, timerLevel)
     {
       if (IsPrint(TimingsByLevel)) {
         levelTimeMonitor_ = rcp(new TimeMonitor(object, object.ShortClassName() + ": " +  msg + " (total, level=" + Teuchos::Utils::toString(level.GetLevelID()) + ")", timerLevel));
-	levelTimeMonitorExclusive_ = rcp(new MutuallyExclusiveTimeMonitor<Level>(object, object.ShortClassName() + ": " + msg + " (level=" + Teuchos::Utils::toString(level.GetLevelID()) + ")", timerLevel));
+        levelTimeMonitorExclusive_ = rcp(new MutuallyExclusiveTimeMonitor<Level>(object, object.ShortClassName() + ": " + msg + " (level=" + Teuchos::Utils::toString(level.GetLevelID()) + ")", timerLevel));
       }
     }
 
   private:
-    RCP<TimeMonitor> levelTimeMonitor_;
-
+    //! Total time spent on this level in this object and all its children.
+    RCP<TimeMonitor>                           levelTimeMonitor_;
+    //! Total time spent on all levels in this object only, excluding all children.
     MutuallyExclusiveTimeMonitor<FactoryBase>  timerMonitorExclusive_;
+    //! Total time spent on this level in this object only, excluding all children.
     RCP<MutuallyExclusiveTimeMonitor<Level> >  levelTimeMonitorExclusive_;
   };
 
-  // Factory monitor
-  // Similar to SubMonitor but add a timer level by level
+  //---------------------------------------------------------------------------------------------------
+
+  /*! @class SubFactoryMonitor
+    @brief Similar to SubMonitor but adds a timer level by level.
+
+    Times an object and all its children on a level-by-level basis.
+
+    The timer yields output such as
+     \verbatim
+       MueLu: SaPFactory: Eigenvalue estimate (sub, total, level=4)    0.01999 (1)      0.02103 (1)      0.02121 (1)      0.02103 (1)
+     \endverbatim
+     Note that the keyword \a sub denotes that this is output from a SubFactoryMonitor.
+     Note that the keyword \a total denotes timing of the object and its children.
+  */
   class SubFactoryMonitor: public SubMonitor {
   public:
+
+    //!Constructor
     SubFactoryMonitor(const BaseClass& object, const std::string & msg, int levelID, MsgType msgLevel = Runtime1, MsgType timerLevel = Timings1)
       : SubMonitor(object, msg, msgLevel, timerLevel)
     {
@@ -158,6 +224,7 @@ namespace MueLu {
       }
     }
 
+    //!Constructor
     SubFactoryMonitor(const BaseClass& object, const std::string & msg, const Level & level, MsgType msgLevel = Runtime1, MsgType timerLevel = Timings1)
       : SubMonitor(object, msg, msgLevel, timerLevel)
     {
@@ -166,6 +233,7 @@ namespace MueLu {
       }
     }
   private:
+    //! Total time spent on this level in this object and all children.
     RCP<TimeMonitor> levelTimeMonitor_;
   };
 
