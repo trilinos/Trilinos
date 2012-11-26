f640552b138e35918a1b1cf5643be5de9f5ed82a
Mark Hoemmen <mhoemme@sandia.gov>
2012-11-25 18:47:25 -0700

    Teuchos: Fixed spelling error in documentation of getConst(), and improved documentation by adding an example.
    Build/Test Cases Summary
    Enabled Packages: TeuchosCore
    0) MPI_DEBUG => passed: passed=44,notpassed=0 (1.70 min)
    1) SERIAL_RELEASE => passed: passed=42,notpassed=0 (1.49 min)

--- packages/teuchos/core/src/Teuchos_getConst.hpp
+++ packages/teuchos/core/src/Teuchos_getConst.hpp
@@ -47,19 +47,40 @@
 namespace Teuchos {
 
 /** \brief Return a constant reference to an object given a non-const reference.
+ * \tparam T The type (without const qualifier) to make const.
  *
- * This function just provides a shorthand notation for
+ * This template function provides a shorthand for
  * \verbatim const_cast<const T&>(t) \endverbatim
  * as
- * \verbatim getCost(t) \endverbatim
- * so that one does not have to type in the name of the class
- * which can be quite long in some cases.
+ * \verbatim getConst(t) \endverbatim
+ * This saves you the trouble of typing the name of the class,
+ * and also saves readers of your code from looking at the
+ * (possibly long) class name.
+ *
+ * Here is an example showing why you might want to use this function.
+ * \code
+ * // A class with a very long name.
+ * class MyVeryLongClassNameWhichIsIndeedQuiteLongIsItNot { ... };
+ *
+ * // A function which takes a const reference to an instance of that class.
+ * void f1 (const MyVeryLongClassNameWhichIsIndeedQuiteLongIsItNot& x)
+ *
+ * // A function which takes a nonconst reference to an instance of that class,
+ * // but needs to call f1, which takes a const reference.
+ * void f2 (MyVeryLongClassNameWhichIsIndeedQuiteLongIsItNot& x) {
+ *   // ... do some stuff with x ...
+ *   // Call f1.  Notice how few characters this takes,
+ *   // compared with a const_cast.
+ *   f1 (getConst (x));
+ *   // ... do other stuff ...
+ * }
+ * \endcode
  *
  * \ingroup teuchos_language_support_grp
  */
 template<class T>
-inline const T& getConst( T& t ) {	return t; }
+inline const T& getConst( T& t ) {      return t; }
 
-}	// end namespace Teuchos
+}       // end namespace Teuchos
 
 #endif // TEUCHOS_GET_CONST_HPP
