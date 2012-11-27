6504c6a1de21823b7f42fcf025d304565d491142
Tobias Wiesner <wiesner@poincare.lnm.mw.tum.de>
2012-11-27 09:38:06 +0100

    MueLu: UncoupledAggregation - remove "coarseAggStat" variable build aggStat information within Build routine. This simplifies code a lot. Unifying the Coupled and UncoupledAggregation code now should be much easier.

--- packages/muelu/src/Graph/UncoupledAggregation/MueLu_UncoupledAggregationFactory_def.hpp
+++ packages/muelu/src/Graph/UncoupledAggregation/MueLu_UncoupledAggregationFactory_def.hpp
@@ -54,6 +54,7 @@
 #define MUELU_UNCOUPLEDAGGREGATIONFACTORY_DEF_HPP_
 
 #include <Xpetra_Map.hpp>
+#include <Xpetra_MapFactory.hpp>
 #include <Xpetra_Vector.hpp>
 #include <Xpetra_VectorFactory.hpp>
 
@@ -72,86 +73,91 @@
 
 namespace MueLu {
 
-  template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
-  UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::UncoupledAggregationFactory(RCP<const FactoryBase> graphFact, bool bMaxLinkAggregation, bool bEmergencyAggregation)
-    : bDefinitionPhase_(true)
+template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::UncoupledAggregationFactory(RCP<const FactoryBase> graphFact, bool bMaxLinkAggregation, bool bEmergencyAggregation)
+: bDefinitionPhase_(true), mapName_(""), mapFact_(Teuchos::null)
   {
-    SetFactory("Graph", graphFact); // for compatibility with old code
-    algos_.push_back(Teuchos::rcp(new MueLu::OnePtAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
-    algos_.push_back(Teuchos::rcp(new MueLu::UncoupledAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
-    if (bMaxLinkAggregation)   algos_.push_back(Teuchos::rcp(new MueLu::MaxLinkAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
-    if (bEmergencyAggregation) algos_.push_back(Teuchos::rcp(new MueLu::EmergencyAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
-  }
+  SetFactory("Graph", graphFact);       // for compatibility with old code
+  SetFactory("DofsPerNode", graphFact); // for compatibility with old code
+  algos_.push_back(Teuchos::rcp(new MueLu::OnePtAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
+  algos_.push_back(Teuchos::rcp(new MueLu::UncoupledAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
+  if (bMaxLinkAggregation)   algos_.push_back(Teuchos::rcp(new MueLu::MaxLinkAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
+  if (bEmergencyAggregation) algos_.push_back(Teuchos::rcp(new MueLu::EmergencyAggregationAlgorithm<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>(graphFact)));
+}
 
-  template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
-  void UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::DeclareInput(Level &currentLevel) const {
-    Input(currentLevel, "Graph");
+template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+void UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::DeclareInput(Level &currentLevel) const {
+  Input(currentLevel, "Graph");
+  Input(currentLevel, "DofsPerNode");
 
-    if (currentLevel.GetLevelID() == 0) currentLevel.DeclareInput("coarseAggStat", MueLu::NoFactory::get(), this);
-    else                                currentLevel.DeclareInput("coarseAggStat", this, this);
+  if(mapName_.length() > 0)
+    currentLevel.DeclareInput(mapName_,mapFact_.get());
+}
 
-  }
+template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+void UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::Append(const RCP<MueLu::AggregationAlgorithmBase<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps> > & alg) {
+  TEUCHOS_TEST_FOR_EXCEPTION(bDefinitionPhase_==false,Exceptions::RuntimeError,"MueLu::UncoupledAggregationFactory::Build: cannot call Append after Build. Error.");
+  algos_.push_back(alg);
+}
 
-  template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
-  void UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::Append(const RCP<MueLu::AggregationAlgorithmBase<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps> > & alg) {
-    TEUCHOS_TEST_FOR_EXCEPTION(bDefinitionPhase_==false,Exceptions::RuntimeError,"MueLu::UncoupledAggregationFactory::Build: cannot call Append after Build. Error.");
-    algos_.push_back(alg);
-  }
+template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+void UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::Build(Level &currentLevel) const
+{
+  FactoryMonitor m(*this, "Aggregation (Uncoupled)", currentLevel);
 
-  template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
-  void UncoupledAggregationFactory<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::Build(Level &currentLevel) const
+  bDefinitionPhase_ = false;  // definition phase is finished, now all aggregation algorithm information is fixed
+
+  RCP<Aggregates> aggregates;
   {
-    FactoryMonitor m(*this, "Aggregation (Uncoupled)", currentLevel);
+    // Level Get
+    RCP<const Graph> graph   = Get< RCP<Graph> >(currentLevel, "Graph");
+    LocalOrdinal nDofsPerNode = Get<LocalOrdinal>(currentLevel, "DofsPerNode");
 
-    bDefinitionPhase_ = false;  // definition phase is finished, now all aggregation algorithm information is fixed
-
-    RCP<Aggregates> aggregates;
-    {
-      // Level Get
-      RCP<const Graph> graph = Get< RCP<Graph> >(currentLevel, "Graph");
-
-      // Build
-      aggregates = rcp(new Aggregates(*graph));
-      aggregates->setObjectLabel("UC");
-
-
-      const LocalOrdinal nRows = graph->GetNodeNumVertices();
-
-      Teuchos::ArrayRCP<unsigned int> aggStat;
-      if(currentLevel.GetLevelID() == 0 && currentLevel.IsAvailable("coarseAggStat",MueLu::NoFactory::get())) {
-        aggStat = currentLevel.Get<Teuchos::ArrayRCP<unsigned int> >("coarseAggStat",MueLu::NoFactory::get());
-      } else if (currentLevel.IsAvailable("coarseAggStat", this)) {
-        aggStat = currentLevel.Get<Teuchos::ArrayRCP<unsigned int> >("coarseAggStat",this);
-      } else {
-        if(nRows > 0) aggStat = Teuchos::arcp<unsigned int>(nRows);
-        for(LocalOrdinal i=0; i<nRows; ++i) {
-          aggStat[i] = NodeStats::READY;
-        }
-      }
-
-      Teuchos::ArrayRCP<unsigned int> coarse_aggStat = Teuchos::arcp<unsigned int>(nRows);
-      for(LocalOrdinal i=0; i<nRows; ++i) {
-        coarse_aggStat[i] = NodeStats::READY;
-      }
-
-      // TODO: check return values of functions
-      LocalOrdinal nonAggregatedNodes = -1;
-      for(size_t a = 0; a < algos_.size(); a++) {
-        nonAggregatedNodes = algos_[a]->BuildAggregates(*graph,*aggregates,aggStat,coarse_aggStat);
-      }
-      TEUCHOS_TEST_FOR_EXCEPTION(nonAggregatedNodes > 0,Exceptions::RuntimeError,"MueLu::UncoupledAggregationFactory::Build: Leftover nodes found! Error!");
-
-      LocalOrdinal numAggs = aggregates->GetNumAggregates();
-      coarse_aggStat.resize(Teuchos::as<int>(numAggs));
-      currentLevel.Set("coarseAggStat", coarse_aggStat, this);
+    // TODO create a map of Xpetra::Maps for different
+    // aggregation information (OnePtAggregegates...)
+    Teuchos::RCP<const Map> OnePtMap = Teuchos::null;
+    if(mapName_.length() > 0) {
+      OnePtMap = currentLevel.Get<Teuchos::RCP<const Map> >(mapName_,mapFact_.get());
     }
 
-    // Level Set
-    Set(currentLevel, "Aggregates", aggregates);
+    // Build
+    aggregates = rcp(new Aggregates(*graph));
+    aggregates->setObjectLabel("UC");
 
-    aggregates->describe(GetOStream(Statistics0, 0), getVerbLevel());
 
+    const LocalOrdinal nRows = graph->GetNodeNumVertices();
+
+    // construct aggStat information
+    Teuchos::ArrayRCP<unsigned int> aggStat;
+    if(nRows > 0) aggStat = Teuchos::arcp<unsigned int>(nRows);
+    for(LocalOrdinal i=0; i<nRows; ++i) {
+      aggStat[i] = NodeStats::READY;
+      if(OnePtMap != Teuchos::null) {
+        // reconstruct global row id (FIXME only works for contingoous maps)
+        GlobalOrdinal grid = graph->GetDomainMap()->getGlobalElement(i) * nDofsPerNode;
+        if(OnePtMap->isNodeGlobalElement(grid)) {
+          aggStat[i] = MueLu::NodeStats::ONEPT;
+        }
+      }
+    }
+
+    // TODO remove coarseAggStat
+    Teuchos::ArrayRCP<unsigned int> coarse_aggStat = Teuchos::arcp<unsigned int>(nRows);
+
+    // TODO: check return values of functions
+    LocalOrdinal nonAggregatedNodes = -1;
+    for(size_t a = 0; a < algos_.size(); a++) {
+      nonAggregatedNodes = algos_[a]->BuildAggregates(*graph,*aggregates,aggStat,coarse_aggStat);
+    }
+    TEUCHOS_TEST_FOR_EXCEPTION(nonAggregatedNodes > 0,Exceptions::RuntimeError,"MueLu::UncoupledAggregationFactory::Build: Leftover nodes found! Error!");
   }
+
+  // Level Set
+  Set(currentLevel, "Aggregates", aggregates);
+
+  aggregates->describe(GetOStream(Statistics0, 0), getVerbLevel());
+
+}
 
 } //namespace MueLu
 
