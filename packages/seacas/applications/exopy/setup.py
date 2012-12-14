58e7875957dfc0ec6571384ec35bcbd31420b7c9
Timothy R. Shelton <trshelt@sandia.gov>
2012-12-14 14:21:23 -0700

    Have setup.py use relative path to location in code repository and detect if on Mac.
    (From Sierra)
    
    Build/Test Cases Summary
    Enabled Packages: SEACAS
    Disabled Packages: Panzer
    Enabled all Forward Packages
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_SS => passed: passed=3,notpassed=0 (2.28 min)
    3) SERIAL_RELEASE_SS => passed: passed=5,notpassed=0 (1.96 min)
    Other local commits for this build/test group: cf912ef, c103b0a

--- packages/seacas/applications/exopy/setup.py
+++ packages/seacas/applications/exopy/setup.py
@@ -1,3 +1,4 @@
+import os
 from distutils.core import setup, Extension
 
 exopy_PreinstallSeacas = \
@@ -8,25 +9,34 @@ exopy_PreinstallSeacas = \
             library_dirs = ['/projects/seacas/linux_rhel5/current/lib'],
            )
 
-exopy_CodeSeacas = \
+
+relativeLocLen = -len('/TPLs_src/Trilinos/packages/seacas/applications/exopy')
+codePath = os.getcwd()[:relativeLocLen]
+
+exopy_Mac = \
   Extension('exopy',
             ['exopy.c'],
-            libraries = ['exodus', 'netcdf', 'hl', 'hdf5', 'mpich', 'mpl', 'tmpe', 'pmpich'], # 'mpih'],
+            libraries = ['exodus', 'netcdf', 'hl', 'hdf5', 'mpich', 'mpl', 'tmpe', 'pmpich'],
             include_dirs = [
-              '/Users/trshelt/sierra/codeMac/seacas/libraries/exodus/cbind/include',
-              '/Users/trshelt/sierra/codeMac/TPLs_src/netcdf/src/include',
-              '/Users/trshelt/sierra/codeMac/TPLs_src/hdf5/hl/src',
-              '/Users/trshelt/sierra/codeMac/TPLs_src/hdf5/hdf5-1.8.7/src',
+              ( codePath + '/seacas/libraries/exodus/cbind/include' ),
+              ( codePath + '/TPLs_src/netcdf/src/include' ),
+              ( codePath + '/TPLs_src/hdf5/hl/src' ),
+              ( codePath + '/TPLs_src/hdf5/hdf5-1.8.7/src' ),
               '/opt/local/include/mpich2'
             ],
             library_dirs = [
-              '/Users/trshelt/sierra/codeMac/objs/apps/seacas/votd/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared',
-              '/Users/trshelt/sierra/codeMac/objs/tpls/netcdf/4.1.3/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared',
-              '/Users/trshelt/sierra/codeMac/objs/tpls/hdf5/1.8.7/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared',
-              '/Users/trshelt/sierra/codeMac/objs/apps/MPIH/votd/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared',
+              ( codePath + '/objs/apps/seacas/votd/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared' ),
+              ( codePath + '/objs/tpls/netcdf/4.1.3/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared' ),
+              ( codePath + '/objs/tpls/hdf5/1.8.7/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared' ),
+              ( codePath + '/objs/apps/MPIH/votd/darwin-4.4.macports/release/address-model-64/mpi-mpich/runtime-link-shared' ),
               '/opt/local/lib'
             ]
            )
+
+sierra_machine = os.getenv('SIERRA_MACHINE')
+exopy_module = exopy_PreinstallSeacas
+if sierra_machine == 'darwin':
+  exopy_module = exopy_Mac
 
 setup (
        name = 'ExodusPython',
@@ -34,7 +44,6 @@ setup (
        description = 'This a python interface to the ExodusII library.',
        author='Mike Veilleux, Timothy Shelton, and Kendall Pierson',
        author_email='sierra-help@sandia.gov',
-       ext_modules = [ exopy_PreinstallSeacas ]
-       #ext_modules = [ exopy_CodeSeacas ]
+       ext_modules = [ exopy_module ]
       )
 
