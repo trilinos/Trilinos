eccb311230fa4724d908d87b32f1db3691205502
Carter Edwards <hcedwar@sandia.gov>
2012-12-19 09:30:49 -0700

    Add missing file.

--- packages/kokkos/array/research/uq_on_gpgpu/src/Cuda/KokkosArray_Cuda_CrsProductTensorLegendre.hpp
+++ packages/kokkos/array/research/uq_on_gpgpu/src/Cuda/KokkosArray_Cuda_CrsProductTensorLegendre.hpp
@@ -55,15 +55,14 @@
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
 
-KokkosArray::Impl::CudaTexture<double> 
-  multiply_crs_product_tensor_A ,
-  multiply_crs_product_tensor_X ;
 
-//----------------------------------------------------------------------------
-//----------------------------------------------------------------------------
+#if 1
 
 namespace KokkosArray {
 namespace Impl {
+
+/** \brief
+ */
 
 template< typename MatrixScalar ,
           typename VectorScalar >
@@ -268,11 +267,235 @@ public:
   }
 };
 
+} // namespace Impl
+} // namespace KokkosArray
+
 //----------------------------------------------------------------------------
 //----------------------------------------------------------------------------
 
+#else
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
+
+KokkosArray::Impl::CudaTexture<double> 
+  multiply_crs_product_tensor_legendre_A ,
+  multiply_crs_product_tensor_legendre_X ;
+
+namespace KokkosArray {
+namespace Impl {
+
+/** \brief
+ *
+ *  grid.x = inner dimension
+ *  grid.y = outer dimension
+ *
+ */
+
+template<>
+class Multiply<
+  BlockCrsMatrix< CrsProductTensorLegendre< double , Cuda > , double , Cuda > ,
+  View< double** , LayoutLeft , Cuda > ,
+  View< double** , LayoutLeft , Cuda > >
+{
+public:
+
+  typedef Cuda                    device_type ;
+  typedef device_type::size_type  size_type ;
+
+  typedef CrsProductTensorLegendre< double , device_type >    tensor_type ;
+  typedef BlockCrsMatrix< tensor_type, double, device_type >  matrix_type ;
+  typedef View< double** , LayoutLeft , Cuda >                vector_type ;
+
+private:
+
+  const matrix_type m_A ;
+  const vector_type m_x ;
+  const vector_type m_y ;
+  const size_type m_strideA ;
+  const size_type m_strideX ;
+
+public:
+
+  __device__
+  void operator()(void) const
+  {
+    // Number of bases in the stochastic system and padding to warp size.
+    const size_type tensor_dim = m_A.block.dimension();
+    const size_type tensor_dim_align = tensor_dim ;
+
+    // Shared memory:
+    //   sh_work[ CudaTraits::WarpSize * blockDim.y ]
+    //   sh_y[ tensor_dim_align ]
+    //   sh_offset[ 2 * tensor_dim_align + 1 ]
+
+    volatile VectorScalar * const sh_work = kokkos_impl_cuda_shared_memory<VectorScalar>();
+    VectorScalar * const sh_y = (VectorScalar *)( sh_work + blockDim.x * blockDim.y );
+    unsigned     * const sh_offset = (unsigned *)( sh_y + tensor_dim_align );
+
+    // Block size and thread id within the entire block:
+
+    const size_type nid = CudaTraits::WarpSize * blockDim.y ;
+    const size_type tid = threadIdx.x + CudaTraits::WarpSize * threadIdx.y ;
+
+    // Load the tensor row into shared memory
+
+    if ( 3 < tid ) {
+      sh_offset[tid] = m_A.block.entry_offset( blockIdx.x + tid );
+    }
+
+    __syncthreads();
+
+    {
+      const size_type iBeg = sh_offset[0];
+      const size_type iEnd = sh_offset[2];
+
+      for ( ; i < iEnd ; ++i
+
+    // blockIdx.x == row in the stochastic system
+    // blockIdx.y == row in the deterministic (finite element) system
+
+    // Loop over columns in the discrete (finite element) system.
+
+    const size_type iBlockEntryEnd = m_A.graph.row_map[ blockIdx.y + 1 ];
+          size_type iBlockEntry    = m_A.graph.row_map[ blockIdx.y ];
+
+    for ( ; iBlockEntry < iBlockEntryEnd ; ++iBlockEntry ) {
+
+      // Offset into A and x 
+      const size_type ix = m_strideX * m_A.graph.entries( iBlockEntry );
+      const size_type iA = m_strideA * iBlockEntry ;
+
+      // Each warp computes a row of the stochastic block 
+      // for coalesced reads and no need for explicit synchronization.
+
+      for ( size_type iyInner = threadIdx.y ; iyInner < tensor_dim ; iyInner += blockDim.y ) {
+
+        VectorScalar y = 0 ;
+
+        // Product tensor entries which this warp will iterate:
+        //
+        const size_type iBeg        = sh_offset[ 2 * iyInner ];
+        const size_type iBegOffDiag = sh_offset[ 2 * iyInner + 1 ];
+        const size_type iEnd        = sh_offset[ 2 * iyInner + 2 ];
+
+        size_type i = iBeg + threadIdx.x ;
+
+        // Loop through sparse tensor diagonal contributions:
+
+        for ( ; i < iBegOffDiag ; i += blockDim.x ) {
+          const unsigned j = m_A.block.m_coordinate(i);
+          const double aj = CudaTexture<double>::fetch( multiply_crs_product_tensor_legendre_A , iA + j );
+          const double xj = CudaTexture<double>::fetch( multiply_crs_product_tensor_legendre_X , ix + j );
+          y += m_A.block.m_value(i) * aj * xj ;
+        }
+
+        // Loop through sparse tensor off-diagonal contributions:
+
+        for ( ; i < iEnd ; i += blockDim.x ) {
+          const unsigned kj = m_A.block.m_coordinate(i);
+          const unsigned j  = kj & 0x0ffff ;
+          const unsigned k  = kj >> 16 ;
+          const double aj = CudaTexture<double>::fetch( multiply_crs_product_tensor_legendre_A , iA + j );
+          const double ak = CudaTexture<double>::fetch( multiply_crs_product_tensor_legendre_A , iA + k );
+          const double xj = CudaTexture<double>::fetch( multiply_crs_product_tensor_legendre_X , ix + j );
+          const double xk = CudaTexture<double>::fetch( multiply_crs_product_tensor_legendre_X , ix + k );
+          y += m_A.block.m_value(i) * ( aj * xk + ak * xj );
+        }
+
+        // Reduction of 'y' within the warp
+
+        sh_work[ tid ] = y ;
+
+        if ( threadIdx.x + 16 < CudaTraits::WarpSize ) sh_work[tid] += sh_work[tid+16];
+        if ( threadIdx.x +  8 < CudaTraits::WarpSize ) sh_work[tid] += sh_work[tid+ 8];
+        if ( threadIdx.x +  4 < CudaTraits::WarpSize ) sh_work[tid] += sh_work[tid+ 4];
+        if ( threadIdx.x +  2 < CudaTraits::WarpSize ) sh_work[tid] += sh_work[tid+ 2];
+        if ( threadIdx.x +  1 < CudaTraits::WarpSize ) sh_work[tid] += sh_work[tid+ 1];
+
+        if ( 0 == threadIdx.x ) { // One thread in the warp saves to shared memory
+          sh_y[ iyInner ] = sh_work[ tid ];
+        }
+      }
+
+      // Coalesced write by the whold block to global memory
+      for ( size_type i = tid ; i < tensor_dim ; i += nid ) {
+        m_y( i , blockIdx.x ) = sh_y[i];
+      }
+    }
+  }
+
+  //------------------------------------
+
+  Multiply( const matrix_type & A ,
+            const vector_type & x ,
+            const vector_type & y )
+  : m_A( A ), m_x( x ), m_y( y )
+  {
+    const size_type row_count        = A.graph.row_map.dimension(0) - 1 ;
+    const size_type tensor_dimension = A.block.dimension();
+    const size_type tensor_dim_align = tensor_dimension ;
+
+    // Shared memory:
+    //   sh_work[ CudaTraits::WarpSize * blockDim.y ]
+    //   sh_y[ tensor_dim_align ]
+    //   sh_offset[ 2 * tensor_dim_align + 1 ]
+
+    const int nsh_warp = sizeof(VectorScalar) * CudaTraits::WarpSize ; // sh_work
+
+    const int nsh_base = sizeof(VectorScalar) * tensor_dim_align             // sh_y
+                       + sizeof(VectorScalar) * tensor_dim_align             // sh_x
+                       + sizeof(MatrixScalar) * tensor_dim_align             // sh_A
+                       + sizeof(unsigned int) * ( 2 * tensor_dim_align + 1 ) // sh_offset
+                       ;
+
+    enum { ShCapacity = KokkosArray::Impl::CudaTraits::SharedMemoryCapacity };
+
+    int nWarp = ( ShCapacity - nsh_base ) / nsh_warp ;
+
+    if ( nWarp < 1 ) {
+      std::ostringstream msg ;
+      msg << "Multiply< BlockCrsMatrix< CrsProductTensor ... > >::apply "
+          << " FAILED to meet shared memory need: "
+          << nsh_base << " + " << nsh_warp
+          << " * nWarp <= " << ShCapacity ;
+      throw std::runtime_error( msg.str() );
+    }
+
+    nWarp = std::min( nWarp , (int) cuda_internal_maximum_warp_count() );
+    nWarp = std::min( nWarp , (int) tensor_dimension );
+
+    const size_type shmem = nsh_base + nsh_warp * nWarp ;
+
+    const dim3 dBlock( CudaTraits::WarpSize , nWarp , 1 );
+
+    const dim3 dGrid( row_count , 1 , 1 );
+
+#if 0
+
+    std::cout << "Multiply< BlockCrsMatrix< CrsProductTensorLegendre ... > >"
+              << std::endl 
+              << "  grid(" << dGrid.x << "," << dGrid.y << ")" << std::endl
+              << "  block(" << dBlock.x << "," << dBlock.y << ")" << std::endl
+              << "  shmem(" << shmem << ")" << std::endl
+              << "  row_count(" << row_count << ")" << std::endl
+              << "  tensor_dimension(" << tensor_dimension << ")" << std::endl
+              << "  tensor_dim_align(" << tensor_dim_align << ")" << std::endl
+              ;
+#endif
+
+    cuda_parallel_launch_local_memory< Multiply ><<< dGrid , dBlock , shmem >>>( *this );
+    // Impl::CudaParallelLaunch< Multiply >( *this, dGrid , dBlock , shmem );
+  }
+};
+
 } // namespace Impl
 } // namespace KokkosArray
+
+#endif
+
+//----------------------------------------------------------------------------
+//----------------------------------------------------------------------------
 
 #endif /* #ifndef KOKKOSARRAY_CUDA_CRSPRODUCTTENSORLEGENDRE_HPP */
 
---
+++ packages/kokkos/array/research/uq_on_gpgpu/unit_test/TestProductTensorLegendre.hpp
@@ -0,0 +1,121 @@
+
+#include <utility>
+#include <cmath>
+#include <iostream>
+
+#include <impl/KokkosArray_Timer.hpp>
+
+namespace unit_test {
+
+//----------------------------------------------------------------------------
+
+template< typename VectorScalar , typename MatrixScalar , typename TensorScalar , class Device >
+std::pair<size_t,double>
+test_product_tensor_legendre(
+  const std::vector<int> & arg_var_degree ,
+  const int nGrid ,
+  const int iterCount ,
+  const bool print_flag = false )
+{
+  typedef KokkosArray::View< VectorScalar** ,
+                             KokkosArray::LayoutLeft ,
+                             Device > vector_type ;
+
+  typedef KokkosArray::CrsProductTensorLegendre< TensorScalar , Device >  tensor_type ;
+
+  typedef KokkosArray::BlockCrsMatrix< tensor_type , MatrixScalar , Device > matrix_type ;
+
+  typedef typename matrix_type::graph_type graph_type ;
+
+  //------------------------------
+  // Generate graph for "FEM" box structure:
+
+  std::vector< std::vector<size_t> > graph ;
+
+  const size_t outer_length = nGrid * nGrid * nGrid ;
+  const size_t graph_length = unit_test::generate_fem_graph( nGrid , graph );
+
+  //------------------------------
+  // Generate CRS block-tensor matrix:
+
+  std::vector<unsigned> var_degree( arg_var_degree.size() , 0u );
+
+  unsigned maximum_degree = 2 ; // Minimum degree to capture pair-wise covariance
+  for ( unsigned i = 0 ; i < arg_var_degree.size() ; ++i ) {
+    var_degree[i] = arg_var_degree[i];
+    maximum_degree = std::max( maximum_degree , var_degree[i] );
+  }
+
+  matrix_type matrix ;
+
+  matrix.block = tensor_type( var_degree , maximum_degree );
+
+  matrix.graph = KokkosArray::create_crsarray<graph_type>( std::string("test crs graph") , graph );
+
+  const size_t inner_length      = matrix.block.dimension();
+  const size_t inner_matrix_size = matrix.block.dimension();
+
+  matrix.values = vector_type( "matrix" , inner_matrix_size , graph_length );
+
+  vector_type x = vector_type( "x" , inner_length , outer_length );
+  vector_type y = vector_type( "y" , inner_length , outer_length );
+
+  typename vector_type::HostMirror hM = KokkosArray::create_mirror( matrix.values );
+  
+  for ( size_t i = 0 ; i < graph_length ; ++i ) {
+    for ( size_t j = 0 ; j < inner_length ; ++j ) {
+      hM(j,i) = 1 + i ;
+    }
+  }
+  
+  KokkosArray::deep_copy( matrix.values , hM );
+
+  //------------------------------
+  // Generate input multivector:
+  
+  typename vector_type::HostMirror hx = KokkosArray::create_mirror( x );
+
+  for ( size_t i = 0 ; i < outer_length ; ++i ) {
+    for ( size_t j = 0 ; j < inner_length ; ++j ) {
+      hx(j,i) = 1 + j + 10 * i ;
+    }
+  }
+
+  KokkosArray::deep_copy( x , hx );
+
+  //------------------------------
+
+  KokkosArray::Impl::Timer clock ;
+  for ( int iter = 0 ; iter < iterCount ; ++iter ) {
+    KokkosArray::Impl::Multiply< matrix_type , vector_type , vector_type >( matrix , x , y );
+  }
+  Device::fence();
+
+  const double seconds_per_iter = clock.seconds() / ((double) iterCount );
+
+  //------------------------------
+
+  if ( print_flag ) {
+    typename vector_type::HostMirror hy = KokkosArray::create_mirror( y );
+
+    KokkosArray::deep_copy( hy , y );
+
+    std::cout << std::endl << "test_product_tensor_matrix" << std::endl ;
+    for ( size_t i = 0 ; i < outer_length ; ++i ) {
+      std::cout << "hy(:," << i << ") =" ;
+      for ( size_t j = 0 ; j < inner_length ; ++j ) {
+        std::cout << " " << hy(j,i);
+      }
+      std::cout << std::endl ;
+    }
+  }
+
+  return std::pair<size_t,double>( outer_length * inner_length , seconds_per_iter );
+}
+
+//----------------------------------------------------------------------------
+
+} // namespace unit_test
+
+
+
