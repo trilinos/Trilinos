a3071079292dffb9f09403e9945792b2d2696119
Jeremie Gaidamour <jngaida@sandia.gov>
2012-12-20 14:07:45 -0700

    MueLu: added an optional dependency to Zoltan2

--- packages/muelu/cmake/Dependencies.cmake
+++ packages/muelu/cmake/Dependencies.cmake
@@ -1,6 +1,6 @@
 SET(LIB_REQUIRED_DEP_PACKAGES Teuchos Xpetra)
 SET(LIB_OPTIONAL_DEP_PACKAGES Amesos Amesos2 Epetra EpetraExt KokkosClassic
-                              Ifpack Ifpack2 ML Tpetra Zoltan Stratimikos)
+                              Ifpack Ifpack2 ML Tpetra Zoltan Zoltan2 Stratimikos)
 SET(TEST_REQUIRED_DEP_PACKAGES )
 SET(TEST_OPTIONAL_DEP_PACKAGES AztecOO Epetra EpetraExt Galeri Tpetra Belos) # TODO: clean up this line
 SET(LIB_REQUIRED_DEP_TPLS BLAS LAPACK)
