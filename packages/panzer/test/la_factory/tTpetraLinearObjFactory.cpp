b20da5573ac4514ad75357a3ec43cefbd37c62f2
Roger Pawlowski <rppawlo@sandia.gov>
2013-01-04 08:56:25 -0500

    Panzer: fixed signed/unsigned comparison warnings

--- packages/panzer/test/la_factory/tTpetraLinearObjFactory.cpp
+++ packages/panzer/test/la_factory/tTpetraLinearObjFactory.cpp
@@ -532,28 +532,28 @@ TEUCHOS_UNIT_TEST(tTpetraLinearObjFactory, initializeContainer)
       TEST_EQUALITY(tContainer->get_dxdt(), Teuchos::null)
       TEST_EQUALITY(tContainer->get_f(),    Teuchos::null)
       TEST_EQUALITY(tContainer->get_A(),    Teuchos::null)
-      TEST_EQUALITY(tContainer->get_x()->getLocalLength(),(int) ownedIndices.size());
+	TEST_EQUALITY(tContainer->get_x()->getLocalLength(),(std::size_t) ownedIndices.size());
    
       la_factory->initializeContainer(LOC::DxDt,*container);
       TEST_EQUALITY(tContainer->get_x(),    Teuchos::null)
       TEST_ASSERT(tContainer->get_dxdt()!=Teuchos::null);
       TEST_EQUALITY(tContainer->get_f(),    Teuchos::null)
       TEST_EQUALITY(tContainer->get_A(),    Teuchos::null)
-      TEST_EQUALITY(tContainer->get_dxdt()->getLocalLength(),(int) ownedIndices.size());
+	TEST_EQUALITY(tContainer->get_dxdt()->getLocalLength(),(std::size_t) ownedIndices.size());
    
       la_factory->initializeContainer(LOC::F,*container);
       TEST_EQUALITY(tContainer->get_x(),    Teuchos::null)
       TEST_EQUALITY(tContainer->get_dxdt(), Teuchos::null)
       TEST_ASSERT(tContainer->get_f()!=Teuchos::null);
       TEST_EQUALITY(tContainer->get_A(),    Teuchos::null)
-      TEST_EQUALITY(tContainer->get_f()->getLocalLength(),(int) ownedIndices.size());
+	TEST_EQUALITY(tContainer->get_f()->getLocalLength(),(std::size_t) ownedIndices.size());
    
       la_factory->initializeContainer(LOC::Mat,*container);
       TEST_EQUALITY(tContainer->get_x(),    Teuchos::null)
       TEST_EQUALITY(tContainer->get_dxdt(), Teuchos::null)
       TEST_EQUALITY(tContainer->get_f(),    Teuchos::null)
       TEST_ASSERT(tContainer->get_A()!=Teuchos::null);
-      TEST_EQUALITY(tContainer->get_A()->getNodeNumRows(),(int) ownedIndices.size());
+      TEST_EQUALITY(tContainer->get_A()->getNodeNumRows(),(std::size_t) ownedIndices.size());
    
       // jacobian and residual vector output
       la_factory->initializeContainer(LOC::F | LOC::Mat,*container);
@@ -637,28 +637,28 @@ TEUCHOS_UNIT_TEST(tTpetraLinearObjFactory, initializeContainer)
       TEST_EQUALITY(tGhostedContainer->get_dxdt(), Teuchos::null)
       TEST_EQUALITY(tGhostedContainer->get_f(),    Teuchos::null)
       TEST_EQUALITY(tGhostedContainer->get_A(),    Teuchos::null)
-      TEST_EQUALITY(tGhostedContainer->get_x()->getLocalLength(),(int) ownedAndSharedIndices.size());
+	TEST_EQUALITY(tGhostedContainer->get_x()->getLocalLength(),(std::size_t) ownedAndSharedIndices.size());
    
       la_factory->initializeGhostedContainer(LOC::DxDt,*ghostedContainer);
       TEST_EQUALITY(tGhostedContainer->get_x(),    Teuchos::null)
       TEST_ASSERT(tGhostedContainer->get_dxdt()!=Teuchos::null);
       TEST_EQUALITY(tGhostedContainer->get_f(),    Teuchos::null)
       TEST_EQUALITY(tGhostedContainer->get_A(),    Teuchos::null)
-      TEST_EQUALITY(tGhostedContainer->get_dxdt()->getLocalLength(),(int) ownedAndSharedIndices.size());
+	TEST_EQUALITY(tGhostedContainer->get_dxdt()->getLocalLength(),(std::size_t) ownedAndSharedIndices.size());
    
       la_factory->initializeGhostedContainer(LOC::F,*ghostedContainer);
       TEST_EQUALITY(tGhostedContainer->get_x(),    Teuchos::null)
       TEST_EQUALITY(tGhostedContainer->get_dxdt(), Teuchos::null)
       TEST_ASSERT(tGhostedContainer->get_f()!=Teuchos::null);
       TEST_EQUALITY(tGhostedContainer->get_A(),    Teuchos::null)
-      TEST_EQUALITY(tGhostedContainer->get_f()->getLocalLength(),(int) ownedAndSharedIndices.size());
+	TEST_EQUALITY(tGhostedContainer->get_f()->getLocalLength(),(std::size_t) ownedAndSharedIndices.size());
    
       la_factory->initializeGhostedContainer(LOC::Mat,*ghostedContainer);
       TEST_EQUALITY(tGhostedContainer->get_x(),    Teuchos::null)
       TEST_EQUALITY(tGhostedContainer->get_dxdt(), Teuchos::null)
       TEST_EQUALITY(tGhostedContainer->get_f(),    Teuchos::null)
       TEST_ASSERT(tGhostedContainer->get_A()!=Teuchos::null);
-      TEST_EQUALITY(tGhostedContainer->get_A()->getNodeNumRows(),(int) ownedAndSharedIndices.size());
+      TEST_EQUALITY(tGhostedContainer->get_A()->getNodeNumRows(),(std::size_t) ownedAndSharedIndices.size());
    
       // jacobian and residual vector output
       la_factory->initializeGhostedContainer(LOC::F | LOC::Mat,*ghostedContainer);
