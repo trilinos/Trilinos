998ccd25c3277f0db55ba9a430429b217a4567e9
Mark Hoemmen <mhoemme@sandia.gov>
2013-01-09 16:18:38 -0700

    Tpetra: CrsMatrixMultiplyOp: Improved public documentation.
    The documentation now explains what the class is for, where it's
    used in Tpetra, and that most users don't need to worry about this
    class.  It also clarifies the cached multivectors, which will help
    me and others remember to which Map each of them belongs.

--- packages/tpetra/src/Tpetra_CrsMatrixMultiplyOp_decl.hpp
+++ packages/tpetra/src/Tpetra_CrsMatrixMultiplyOp_decl.hpp
@@ -62,142 +62,270 @@ namespace Tpetra {
   class CrsMatrix;
 #endif
 
-  //! \brief A class for wrapping a CrsMatrix multiply in a Operator.
-  template <class Scalar, class MatScalar = Scalar, class LocalOrdinal = int, class GlobalOrdinal = LocalOrdinal, class Node = Kokkos::DefaultNode::DefaultNodeType, class LocalMatOps = typename Kokkos::DefaultKernels<MatScalar,LocalOrdinal,Node>::SparseOps >
-  class CrsMatrixMultiplyOp : public Operator<Scalar,LocalOrdinal,GlobalOrdinal,Node> {
-    public:
-      //! @name Constructor/Destructor Methods
-      //@{
+  /// \brief A class for wrapping a CrsMatrix multiply in a Operator.
+  ///
+  /// \note Most Tpetra users do not need to use this class.  It will
+  ///   be useful to Tpetra users who want to do mixed-precision
+  ///   sparse matrix-vector multiply, where the sparse matrix's
+  ///   entries have a different precision than that of the input and
+  ///   output vectors.  If your sparse matrix and vectors have the
+  ///   same type of entries, then you don't need to use this class.
+  ///
+  /// This class makes a <tt>CrsMatrix<MatScalar, ...></tt> "look
+  /// like" an <tt>Operator<Scalar, ...></tt>, where
+  /// <tt>MatScalar</tt> and <tt>Scalar</tt> may be different types.
+  /// It does so by working around a limitation of C++, namely that
+  /// template methods of a class can't be virtual.
+  ///
+  /// Here is a detailed description of how the language issue relates
+  /// to CrsMatrix.  If you call the <tt>apply</tt> method of
+  /// CrsMatrix, you will always get the version that takes a
+  /// <tt>MultiVector<Scalar, ...></tt> input and produces a
+  /// <tt>MultiVector<Scalar, ...></tt> output.  CrsMatrix actually
+  /// implements a a templated sparse matrix-vector multiply operation
+  /// (its <tt>localMultiply</tt> method).  It is templated on the
+  /// scalar types of its input and output multivectors
+  /// (<tt>DomainScalar</tt> resp. <tt>RangeScalar</tt>).  However,
+  /// Operator can't access this templated mat-vec method.  This is
+  /// because Operator::apply is virtual, and therefore cannot have a
+  /// template parameter for the <tt>Scalar</tt> type of the
+  /// MultiVector input and output.
+  ///
+  /// Users who want to access the templated sparse mat-vec in
+  /// CrsMatrix through the Operator interface may wrap the CrsMatrix
+  /// in an instance of this class.  This class implements an Operator
+  /// that takes <tt>MultiVector<Scalar, ...></tt> input and output,
+  /// but the CrsMatrix may contain any desired type
+  /// <tt>MatScalar</tt>.  The type <tt>MatScalar</tt> may differ from
+  /// the <tt>Scalar</tt> type of the MultiVector input and output.
+  /// That works around the "no virtual template methods" issue for
+  /// input and output multivectors of the same type.
+  ///
+  /// Interestingly enough, CrsMatrix implements its <tt>apply</tt>
+  /// method using an instance of this class with <tt>Scalar ==
+  /// MatScalar</tt>.  CrsMatrix does not actually contain an
+  /// implementation of "nonlocal" (distributed over multiple MPI
+  /// processes) mat-vec; its <tt>apply</tt> defers the nonlocal part
+  /// to this class' apply() method.  The same is true for the
+  /// gaussSeidel() method.
+  ///
+  /// \tparam Scalar The type of the entries of the input and output
+  ///   MultiVector of the apply() method.  Same as the first template
+  ///   parameter of Operator.
+  ///
+  /// \tparam MatScalar The type of the entries of the CrsMatrix; the
+  ///   first template parameter of CrsMatrix.
+  ///
+  /// \tparam LocalOrdinal The second template parameter of CrsMatrix
+  ///   and Operator.
+  ///
+  /// \tparam GlobalOrdinal The third template parameter of CrsMatrix
+  ///   and Operator.
+  ///
+  /// \tparam Node The fourth template parameter of CrsMatrix and
+  ///   Operator.
+  ///
+  /// \tparam LocalMatOps The fifth template parameter of CrsMatrix.
+  ///   (Operator only takes four template parameters.)
+  template <class Scalar,
+            class MatScalar = Scalar,
+            class LocalOrdinal = int,
+            class GlobalOrdinal = LocalOrdinal,
+            class Node = Kokkos::DefaultNode::DefaultNodeType,
+            class LocalMatOps = typename Kokkos::DefaultKernels<MatScalar,LocalOrdinal,Node>::SparseOps >
+  class CrsMatrixMultiplyOp :
+    public Operator<Scalar,LocalOrdinal,GlobalOrdinal,Node>
+  {
+  public:
+    //! @name Constructor and destructor
+    //@{
 
-      //! Constructor
-      CrsMatrixMultiplyOp(const Teuchos::RCP<const CrsMatrix<MatScalar,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> > &A);
+    /// \brief Constructor
+    ///
+    /// \param A [in] The CrsMatrix to wrap as an
+    ///   <tt>Operator<Scalar, ...></tt>.
+    CrsMatrixMultiplyOp(const Teuchos::RCP<const CrsMatrix<MatScalar,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> > &A);
 
-      //! Destructor
-      virtual ~CrsMatrixMultiplyOp();
+    //! Destructor
+    virtual ~CrsMatrixMultiplyOp();
 
-      //@}
+    //@}
+    //! @name Methods implementing Operator
+    //@{
 
-      //! @name Methods implementing Operator
-      //@{
+    /// \brief Compute <tt>Y = beta*Y + alpha*Op(A)*X</tt>, where
+    ///   <tt>Op(A)</tt> is either A, \f$A^T\f$, or \f$A^H\f$.
+    ///
+    /// This method calls the underlying CrsMatrix object's
+    /// localMultiply<Scalar,Scalar>() method.
+    void
+    apply (const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node>& X,
+           MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node>& Y,
+           Teuchos::ETransp mode = Teuchos::NO_TRANS,
+           Scalar alpha = Teuchos::ScalarTraits<Scalar>::one(),
+           Scalar beta = Teuchos::ScalarTraits<Scalar>::zero()) const;
 
-      //! Computes this matrix-vector multilication Y = A X.
-      //! This calls multiply<Scalar,Scalar>() on the underlying CrsMatrix object.
-      void apply(const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> & X, MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &Y,
-                 Teuchos::ETransp mode = Teuchos::NO_TRANS, Scalar alpha = Teuchos::ScalarTraits<Scalar>::one(), Scalar beta = Teuchos::ScalarTraits<Scalar>::zero()) const;
+    /// \brief "Hybrid" Jacobi + (Gauss-Seidel or SOR) on \f$B = A X\f$.
+    ///
+    /// "Hybrid" means Jacobi for interprocess communication, but
+    /// Successive Over-Relaxation (SOR) or Gauss-Seidel for
+    /// intraprocess computation.  Gauss-Seidel is a special case of
+    /// SOR, where the damping factor is one.
+    ///
+    /// The Forward or Backward sweep directions have their usual SOR
+    /// meaning within the process.  Interprocess communication occurs
+    /// once before the sweep, as it would in Jacobi.
+    ///
+    /// The Symmetric sweep direction means first Forward, then
+    /// Backward.  Before each sweep is an interprocess communication,
+    /// as in Jacobi.  Thus, Symmetric results in two interprocess
+    /// communication steps.
+    ///
+    /// \param B [in] Right-hand side(s), in the range Map of the
+    ///   matrix.
+    /// \param X [in/out] On input: initial guess(es).  On output:
+    ///   result multivector(s).  This must be a domain Map view of
+    ///   a column Map multivector.
+    /// \param D [in] Inverse of diagonal entries of the matrix A,
+    ///   in the row Map of the matrix.
+    /// \param dampingFactor [in] SOR damping factor.  A damping
+    ///   factor of one results in Gauss-Seidel.
+    /// \param direction [in] Sweep direction: Forward, Backward, or
+    ///   Symmetric.
+    /// \param numSweeps [in] Number of sweeps.  We count each
+    ///   Symmetric sweep (including both its Forward and its Backward
+    ///   sweep) as one.
+    ///
+    /// \pre Domain, range, and row Maps of the sparse matrix are all
+    ///   the same.  (The domain and range Maps must be the same
+    ///   because this kernel overwrites its input.  The row Map must
+    ///   be the same because the kernel uses the same local indices
+    ///   for the rows of the sparse matrix, and for the rows of the
+    ///   input / output multivector.)
+    ///
+    /// \pre No other argument aliases X.
+    void
+    gaussSeidel (const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &B,
+                 MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &X,
+                 const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &D,
+                 const Scalar& dampingFactor,
+                 const ESweepDirection direction,
+                 const int numSweeps) const;
 
-      /// \brief "Hybrid" Jacobi + (Gauss-Seidel or SOR) on \f$B = A X\f$.
-      ///
-      /// "Hybrid" means Jacobi for interprocess communication, but
-      /// Successive Over-Relaxation (SOR) or Gauss-Seidel for
-      /// intraprocess computation.  Gauss-Seidel is a special case of
-      /// SOR, where the damping factor is one.
-      ///
-      /// The Forward or Backward sweep directions have their usual
-      /// SOR meaning within the process.  Interprocess communication
-      /// occurs once before the sweep, as it would in Jacobi.
-      ///
-      /// The Symmetric sweep direction means first Forward, then
-      /// Backward.  Before each sweep is an interprocess
-      /// communication, as in Jacobi.  Thus, Symmetric results in two
-      /// interprocess communication steps.
-      ///
-      /// \param B [in] Right-hand side(s), in the range Map of the
-      ///   matrix.
-      /// \param X [in/out] On input: initial guess(es).  On output:
-      ///   result multivector(s).  This must be a domain Map view of
-      ///   a column Map multivector.
-      /// \param D [in] Inverse of diagonal entries of the matrix A,
-      ///   in the row Map of the matrix.
-      /// \param dampingFactor [in] SOR damping factor.  A damping
-      ///   factor of one results in Gauss-Seidel.
-      /// \param direction [in] Sweep direction: Forward, Backward, or
-      ///   Symmetric.
-      /// \param numSweeps [in] Number of sweeps.  We count each
-      ///   Symmetric sweep (including both its Forward and its
-      ///   Backward sweep) as one.
-      ///
-      /// \pre Domain, range, and row Maps of the sparse matrix are all the same.
-      /// \pre No other argument aliases X.
-      void
-      gaussSeidel (const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &B,
-                   MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &X,
-                   const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &D,
-                   const Scalar& dampingFactor,
-                   const ESweepDirection direction,
-                   const int numSweeps) const;
+    /// \brief Version of gaussSeidel(), with fewer requirements on X.
+    ///
+    /// This method is just like gaussSeidel(), except that X need
+    /// only be in the domain Map.  This method does not require that
+    /// X be a domain Map view of a column Map multivector.  As a
+    /// result, this method must copy X into a domain Map multivector
+    /// before operating on it.
+    ///
+    /// \param X [in/out] On input: initial guess(es).  On output:
+    ///   result multivector(s).
+    /// \param B [in] Right-hand side(s), in the range Map.
+    /// \param D [in] Inverse of diagonal entries of the matrix,
+    ///   in the row Map.
+    /// \param dampingFactor [in] SOR damping factor.  A damping
+    ///   factor of one results in Gauss-Seidel.
+    /// \param direction [in] Sweep direction: Forward, Backward, or
+    ///   Symmetric.
+    /// \param numSweeps [in] Number of sweeps.  We count each
+    ///   Symmetric sweep (including both its Forward and its
+    ///   Backward sweep) as one.
+    ///
+    /// \pre Domain, range, and row Maps of the sparse matrix are
+    ///   all the same.
+    /// \pre No other argument aliases X.
+    void
+    gaussSeidelCopy (MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &X,
+                     const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &B,
+                     const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &D,
+                     const Scalar& dampingFactor,
+                     const ESweepDirection direction,
+                     const int numSweeps) const;
 
-      /// \brief Version of gaussSeidel(), with fewer requirements on X.
-      ///
-      /// This method is just like gaussSeidel(), except that X need
-      /// only be in the domain Map.  This method does not require that
-      /// X be a domain Map view of a column Map multivector.  As a
-      /// result, this method must copy X into a domain Map multivector
-      /// before operating on it.
-      ///
-      /// \param X [in/out] On input: initial guess(es).  On output:
-      ///   result multivector(s).
-      /// \param B [in] Right-hand side(s), in the range Map.
-      /// \param D [in] Inverse of diagonal entries of the matrix,
-      ///   in the row Map.
-      /// \param dampingFactor [in] SOR damping factor.  A damping
-      ///   factor of one results in Gauss-Seidel.
-      /// \param direction [in] Sweep direction: Forward, Backward, or
-      ///   Symmetric.
-      /// \param numSweeps [in] Number of sweeps.  We count each
-      ///   Symmetric sweep (including both its Forward and its
-      ///   Backward sweep) as one.
-      ///
-      /// \pre Domain, range, and row Maps of the sparse matrix are
-      ///   all the same.
-      /// \pre No other argument aliases X.
-      void
-      gaussSeidelCopy (MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &X,
-                       const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &B,
-                       const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &D,
-                       const Scalar& dampingFactor,
-                       const ESweepDirection direction,
-                       const int numSweeps) const;
+    /// \brief Whether this Operator's apply() method can apply the
+    ///   transpose or conjugate transpose.
+    ///
+    /// This is always true, since it is true for the CrsMatrix that
+    /// this object wraps.
+    bool hasTransposeApply() const;
 
-      //! Indicates whether this operator supports inverting the adjoint operator.
-      //! This is true.
-      bool hasTransposeApply() const;
+    //! The domain Map of this Operator.
+    const Teuchos::RCP<const Map<LocalOrdinal,GlobalOrdinal,Node> > & getDomainMap() const;
 
-      //! \brief Returns the Map associated with the domain of this operator.
-      //! This is the range map of the underlying CrsMatrix.
-      const Teuchos::RCP<const Map<LocalOrdinal,GlobalOrdinal,Node> > & getDomainMap() const;
+    //! The range Map of this Operator.
+    const Teuchos::RCP<const Map<LocalOrdinal,GlobalOrdinal,Node> > & getRangeMap() const;
 
-      //! Returns the Map associated with the domain of this operator.
-      //! This is the domain map of the underlying CrsMatrix.
-      const Teuchos::RCP<const Map<LocalOrdinal,GlobalOrdinal,Node> > & getRangeMap() const;
+    //@}
 
-      //@}
+  protected:
+    typedef MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> MV;
 
-    protected:
-      typedef MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> MV;
+    //! The underlying CrsMatrix object.
+    const Teuchos::RCP<const CrsMatrix<MatScalar,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> > matrix_;
 
-      // underlying CrsMatrix
-      const Teuchos::RCP<const CrsMatrix<MatScalar,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> > matrix_;
+    /// \brief Column Map MultiVector used in apply().
+    ///
+    /// This is a column Map MultiVector.  It is used as the target of
+    /// the forward mode Import operation (if necessary) in
+    /// applyNonTranspose(), and the source of the reverse mode Export
+    /// operation (if necessary) in applyTranspose().  Both of these
+    /// methods create this MultiVector on demand if needed, and reuse
+    /// it (if possible) for subsequent calls.
+    ///
+    /// This is declared <tt>mutable</tt> because the apply() method
+    /// is const, yet the method needs to cache the MultiVector for
+    /// later use.
+    mutable Teuchos::RCP<MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> > importMV_;
 
-      // multivectors used for import/export dest/source in apply()
-      mutable Teuchos::RCP<MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> > importMV_, exportMV_;
+    /// \brief Row Map MultiVector used in apply().
+    ///
+    /// This is a row Map MultiVector.  It is uses as the source of
+    /// the forward mode Export operation (if necessary) in
+    /// applyNonTranspose(), and the target of the reverse mode Import
+    /// operation (if necessary) in applyTranspose().  Both of these
+    /// methods create this MultiVector on demand if needed, and reuse
+    /// it (if possible) for subsequent calls.
+    ///
+    /// This is declared <tt>mutable</tt> because the apply() method
+    /// is const, yet the method needs to cache the MultiVector for
+    /// later use.
+    mutable Teuchos::RCP<MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> > exportMV_;
 
 #ifdef HAVE_KOKKOSCLASSIC_CUDA_NODE_MEMORY_PROFILING
     Teuchos::RCP<Teuchos::Time> importTimer_, exportTimer_;
 #endif
 
-      // private methods for transpose or non-transpose
-      void applyTranspose(const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> & X, MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &Y,
-                          Scalar alpha, Scalar beta) const;
+    /// \brief Apply the transpose or conjugate transpose of the
+    ///   matrix to X, producing Y.
+    void
+    applyTranspose (const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node>& X,
+                    MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &Y,
+                    Scalar alpha,
+                    Scalar beta) const;
 
-      void applyNonTranspose(const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> & X, MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &Y,
-                             Scalar alpha, Scalar beta) const;
+    //! Apply the matrix (not its transpose) to X, producing Y.
+    void
+    applyNonTranspose (const MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node>& X,
+                       MultiVector<Scalar,LocalOrdinal,GlobalOrdinal,Node> &Y,
+                       Scalar alpha,
+                       Scalar beta) const;
   };
 
-  /*! \brief Non-member function to create CrsMatrixMultiplyOp
-
-      \relatesalso CrsMatrixMultiplyOp
-   */
-  template <class Scalar, class MatScalar, class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
+  /// \brief Non-member function to create a CrsMatrixMultiplyOp.
+  /// \relatesalso CrsMatrixMultiplyOp
+  ///
+  /// The function has the same template parameters of CrsMatrixMultiplyOp.
+  ///
+  /// \param A [in] The CrsMatrix instance to wrap in an CrsMatrixMultiplyOp.
+  /// \return The CrsMatrixMultiplyOp wrapper for the given CrsMatrix.
+  template <class Scalar,
+            class MatScalar,
+            class LocalOrdinal,
+            class GlobalOrdinal,
+            class Node,
+            class LocalMatOps>
   Teuchos::RCP< CrsMatrixMultiplyOp<Scalar,MatScalar,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> >
   createCrsMatrixMultiplyOp(const Teuchos::RCP<const CrsMatrix<MatScalar,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> > &A);
 
