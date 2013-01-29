827749e67e2db71520a1204cac31eb5f6a258df7
Greg Sjaardema <gdsjaar@sandia.gov>
2013-01-29 06:54:49 -0700

    SEACAS: exodiff - fix bug where missing variable did not detect/report a diff
    (From Sierra)

--- packages/seacas/applications/exodiff/ED_Version.h
+++ packages/seacas/applications/exodiff/ED_Version.h
@@ -32,7 +32,7 @@
 #ifndef SEACAS_Version_h
 #define SEACAS_Version_h
 
-static std::string version("2.66");
-static std::string verdate("2013-01-18");
+static std::string version("2.67");
+static std::string verdate("2013-01-29");
 
 #endif // SEACAS_Version_h
