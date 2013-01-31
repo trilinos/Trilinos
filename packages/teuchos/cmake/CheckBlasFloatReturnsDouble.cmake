309f4f212e42de21c6f0183b64e71efc153a9870
Christopher G. Baker <bakercg@ornl.gov>
2013-01-31 16:34:23 -0500

    modified blas float test to check all blas routines that should return a float added new test for whether they actually return a double (i.e., Mac OS X vecLib) added this variable to the cache and the teuchos config.h modified teuchos top-level cmake to continue blas float processing if double-return is detected

--- packages/teuchos/cmake/CheckBlasFloatReturnsDouble.cmake
+++ packages/teuchos/cmake/CheckBlasFloatReturnsDouble.cmake
@@ -1,6 +1,6 @@
 INCLUDE(CheckCXXSourceRuns)
 
-FUNCTION(CHECK_BLAS_FLOAT VARNAME)
+FUNCTION(CHECK_BLAS_FLOAT_DOUBLE_RETURN VARNAME)
 
   SET(CMAKE_REQUIRED_LIBRARIES ${TPL_BLAS_LIBRARIES})
 
@@ -17,11 +17,11 @@ FUNCTION(CHECK_BLAS_FLOAT VARNAME)
 #define SCNRM2_F77  F77_BLAS_MANGLE(scnrm2, SCNRM2)
 
 extern \"C\" {
-  float SDOT_F77(const int* n, const float x[], const int* incx, const float y[], const int* incy);
-  float SASUM_F77(const int* n, const float x[], const int* incx);
-  float SCASUM_F77(const int* n, const std::complex<float> x[], const int* incx);
-  float SNRM2_F77(const int* n, const float x[], const int* incx);
-  float SCNRM2_F77(const int* n, const std::complex<float> x[], const int* incx);
+  double SDOT_F77(const int* n, const float x[], const int* incx, const float y[], const int* incy);
+  double SASUM_F77(const int* n, const float x[], const int* incx);
+  double SCASUM_F77(const int* n, const std::complex<float> x[], const int* incx);
+  double SNRM2_F77(const int* n, const float x[], const int* incx);
+  double SCNRM2_F77(const int* n, const std::complex<float> x[], const int* incx);
 }
 
 int main()
@@ -32,7 +32,7 @@ int main()
                                       std::complex<float>(0.0,0.0),
                                       std::complex<float>(0.0,0.0) };
   const int incx = 1;
-  float val;
+  double val;
 
   val = SDOT_F77(&n, x, &incx, x, &incx);
   if (val != 9.0) return 1;
@@ -53,7 +53,7 @@ int main()
 }
   "
   )
-  
+
   CHECK_CXX_SOURCE_RUNS("${SOURCE}" ${VARNAME})
-  
+
 ENDFUNCTION()
