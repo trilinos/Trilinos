c6444e934a599e07b3c96438c2117da93ab40de4
Todd Kordenbrock <thkorde@sandia.gov>
2013-02-25 12:24:24 -0600

    remove attempts to work around conflicts between "gcc -ansi -pedantic" and rpcgen output. instead, add grep/perl to the rpcgen pipeline to filter out the conflicts.

--- packages/trios/cmake/TriosProcessXDR.cmake
+++ packages/trios/cmake/TriosProcessXDR.cmake
@@ -8,6 +8,7 @@ function (TriosProcessXDR path)
      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.c
      COMMAND rpcgen -Cc ${path}
         | sed -e "\"s#include.*${file}.*#include <${file}.h>#\""
+        | grep -v \"register int32_t \\*buf\;\"
         > ${CMAKE_CURRENT_BINARY_DIR}/${file}.c
      DEPENDS ${path} ${file}.h)
 
@@ -15,22 +16,12 @@ function (TriosProcessXDR path)
    add_custom_command(
      OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${file}.h
      COMMAND rpcgen -Ch ${path}
-        | sed -e "s#rpc/rpc.h#${file}.h#"
+        | sed -e "\"s#rpc/rpc.h#${file}.h#\""
+        | perl -pe \"BEGIN{undef $$/\;} s/\(enum\\s\\w+\\s\\{\\n\(\\s*.*?,\\n\)*?\\s*.*?\),\(\\n\\s*\\}\;\)/\\1\\3/smg\"
         > ${CMAKE_CURRENT_BINARY_DIR}/${file}.h
      DEPENDS ${path})
 
    # Need target to force construction of nssi_types_xdr.{c,h} and nnti_xdr.{c,h}
    add_custom_target(${file} ALL DEPENDS ${CMAKE_CURRENT_BINARY_DIR}/${file}.c ${CMAKE_CURRENT_BINARY_DIR}/${file}.h)
 
-   INCLUDE_DIRECTORIES(BEFORE SYSTEM ${CMAKE_CURRENT_BINARY_DIR})
-
-   # These Flags convert the "extra comma" error into a warning when using the -pedantic flag
-   IF (CMAKE_COMPILER_IS_GNUCXX)
-     #SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fpermissive")
-     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -isystem ${CMAKE_CURRENT_BINARY_DIR}")
-     SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -fpermissive")
-     SET(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -isystem ${CMAKE_CURRENT_BINARY_DIR}")
-   ENDIF (CMAKE_COMPILER_IS_GNUCXX)
-
 endfunction (TriosProcessXDR)
-
