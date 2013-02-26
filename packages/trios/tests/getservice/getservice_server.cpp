874835b994190596031f642ea6aed0bb34daeffe
Todd Kordenbrock <thkorde@sandia.gov>
2013-02-26 15:38:52 -0600

    remove unused variables.  working toward a clean compile.

--- packages/trios/tests/getservice/getservice_server.cpp
+++ packages/trios/tests/getservice/getservice_server.cpp
@@ -92,16 +92,13 @@ int getservice_server_main(struct getservice_args &args, MPI_Comm server_comm)
     int rc = NSSI_OK;
 
     nssi_service getservice_svc;
-    log_level debug_level;
     int server_rank;
 
     MPI_Comm_rank(server_comm, &server_rank);
 
     /* options that can be overriden by the command-line */
-    int verbose = 3;  /* default debug_level */
     std::string server_url(NSSI_URL_LEN, '\0');          /* NNTI-style url of the server */
     std::string logfile("");
-    const char *log_str=NULL;
 
 
     memset(&getservice_svc, 0, sizeof(nssi_service));
