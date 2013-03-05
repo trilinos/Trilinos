8605c9667d59f85c3ff3b5b1b3a32cc8ee0eecf8
Eric Phipps <etphipp@sandia.gov>
2013-03-05 15:27:52 -0700

    TrilinosCouplings:  Added AztecOO version of IntrepidPoissonPamgen example

---
+++ packages/trilinoscouplings/examples/scaling/TrilinosCouplings_EpetraIntrepidPoissonExample_SolveWithAztecOO.cpp
@@ -0,0 +1,77 @@
+// @HEADER
+// ************************************************************************
+//
+//                           Intrepid Package
+//                 Copyright (2007) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Pavel Bochev  (pbboche@sandia.gov),
+//                    Denis Ridzal  (dridzal@sandia.gov),
+//                    Kara Peterson (kjpeter@sandia.gov).
+//
+// ************************************************************************
+// @HEADER
+
+#include "TrilinosCouplings_EpetraIntrepidPoissonExample_SolveWithAztecOO.hpp"
+
+// AztecOO solver
+#include "AztecOO.h"
+
+namespace TrilinosCouplings {
+namespace EpetraIntrepidPoissonExample {
+
+void
+solveWithAztecOO (bool& converged,
+		  int& numItersPerformed,
+		  const double tol,
+		  const int maxNumIters,
+		  const Teuchos::RCP<Epetra_Vector>& X,
+		  const Teuchos::RCP<Epetra_Operator>& A,
+		  const Teuchos::RCP<Epetra_Vector>& B,
+		  const Teuchos::RCP<Epetra_Operator>& M_right)
+{
+  // Set these in advance, so that if the AztecOO solver throws an
+  // exception for some reason, these will have sensible values.
+  converged = false;
+  numItersPerformed = 0;
+
+  TEUCHOS_TEST_FOR_EXCEPTION(A.is_null () || X.is_null () || B.is_null (),
+    std::invalid_argument, "solveWithBelos: The A, X, and B arguments must all "
+    "be nonnull.");
+
+  AztecOO solver;
+  solver.SetAztecOption(AZ_solver, AZ_gmres);
+  solver.SetAztecOption(AZ_precond, AZ_none);
+  solver.SetAztecOption(AZ_kspace, 200);
+  solver.SetAztecOption(AZ_conv, AZ_r0);
+  solver.SetAztecOption(AZ_output, 10);
+  solver.SetUserOperator(A.get());
+  if (M_right != Teuchos::null)
+    solver.SetPrecOperator(M_right.get());
+  solver.SetLHS(X.get());
+  solver.SetRHS(B.get());
+
+  int result = solver.Iterate(maxNumIters, tol);
+
+  converged = (result == 0);
+  numItersPerformed = solver.NumIters ();
+}
+
+} // namespace EpetraIntrepidPoissonExample
+} // namespace TrilinosCouplings
---
+++ packages/trilinoscouplings/examples/scaling/TrilinosCouplings_EpetraIntrepidPoissonExample_SolveWithAztecOO.hpp
@@ -0,0 +1,87 @@
+// @HEADER
+// ************************************************************************
+//
+//                           Intrepid Package
+//                 Copyright (2007) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// This library is free software; you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as
+// published by the Free Software Foundation; either version 2.1 of the
+// License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+// USA
+// Questions? Contact Pavel Bochev  (pbboche@sandia.gov),
+//                    Denis Ridzal  (dridzal@sandia.gov),
+//                    Kara Peterson (kjpeter@sandia.gov).
+//
+// ************************************************************************
+// @HEADER
+
+#ifndef __TrilinosCouplings_IntrepidPoissonExample_SolveWithAztecOO_hpp
+#define __TrilinosCouplings_IntrepidPoissonExample_SolveWithAztecOO_hpp
+
+/// \file TrilinosCouplings_IntrepidPoissonExample_SolveWithAztecOO.hpp
+/// \brief AztecOO solver for the Intrepid Poisson test problem example.
+
+#include "Teuchos_RCP.hpp"
+#include "Epetra_Vector.h"
+#include "Epetra_Operator.h"
+
+namespace TrilinosCouplings {
+namespace EpetraIntrepidPoissonExample {
+
+/// \brief Solve the linear system(s) AX=B with AztecOO.
+///
+/// This interface will change in the future to accept the name of the
+/// Aztec solver to use.  For now, the solver is hard-coded to
+/// CG.
+///
+/// \param converged [out] Whether Aztec reported that the iterative
+///   method converged (solved the linear system to the desired
+///   tolerance).
+///
+/// \param numItersPerformed [out] Number of iterations that the Aztec
+///   solver performed.
+///
+/// \param tol [in] Convergence tolerance for the iterative method.
+///   The meaning of this depends on the particular iterative method.
+///
+/// \param maxNumIters [in] Maximum number of iterations that the
+///   iterative method should perform, regardless of whether it
+///   converged.
+///
+/// \param X [in/out] On input: the initial guess(es) for the iterative
+///   method.  On output: the computed approximate solution.
+///
+/// \param A [in] The matrix in the linear system(s) AX=B to solve.
+///
+/// \param B [in] The right-hand side in the linear system AX=B to solve.
+///
+/// \param M_right [in] If nonnull, a right preconditioner that the
+///   iterative method may use.  If null, the iterative method will
+///   not use a right preconditioner.
+void
+solveWithAztecOO (bool& converged,
+		  int& numItersPerformed,
+		  const double tol,
+		  const int maxNumIters,
+		  const Teuchos::RCP<Epetra_Vector>& X,
+		  const Teuchos::RCP<Epetra_Operator>& A,
+		  const Teuchos::RCP<Epetra_Vector>& B,
+		  const Teuchos::RCP<Epetra_Operator>& M_right);
+
+} // namespace EpetraIntrepidPoissonExample
+} // namespace TrilinosCouplings
+
+#endif // __TrilinosCouplings_IntrepidPoissonExample_SolveWithAztecOO_hpp
