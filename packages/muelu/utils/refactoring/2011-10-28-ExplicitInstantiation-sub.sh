520f794a5b33a0f804f8fb5c61e095b1ffa9baad
Andrey Prokopenko <aprokop@sandia.gov>
2013-03-12 13:19:28 -0600

    MueLu: removed old refactoring scripts

--- packages/muelu/utils/refactoring/2011-10-28-ExplicitInstantiation-sub.sh
+++
@@ -1,2 +0,0 @@
-python /home/jngaida/dev/MueLu/src/preCopyrightTrilinos/muelu/utils/refactoring/2011-10-28-ExplicitInstantiation.py -i $1 -o `dirname $1`/`basename $1 .hpp`_decl.hpp -l $2
-#tkdiff $1 `dirname $1`/`basename $1 .hpp`_decl.hpp
