368f2a828e27b4d54c7f2ea099d006950d8b8859
Bill Spotz <wfspotz@sandia.gov>
2013-04-09 16:46:46 -0600

    Updated numpy_include.h
    Modified numpy_include.h after testing with older versions of NumPy.
    It turns out that PyArray_NDIM(), PyArray_DIMS() and PyArray_DATA() do
    not need to be redefined.  Also got rid of #include "noprefix.h" and
    verified that PyTrilinos still compiles without it.
    
    This should complete the upgrade to compatibility with deprecated
    NumPy code.

--- packages/PyTrilinos/src/numpy_include.h
+++ packages/PyTrilinos/src/numpy_include.h
@@ -29,13 +29,17 @@
 #ifndef NUMPY_INCLUDE_H
 #define NUMPY_INCLUDE_H
 
-// This include file takes care of four of the five things necessary
+// This include file takes care of five of the six things necessary
 // when including the numpy header file arrayobject.h.  First, the
 // Python.h header file is included.  Second, the
-// PY_ARRAY_UNIQUE_SYMBOL is defined.  Third, the numpy/arrayobject.h
-// header file is included.  Finally, the numpy/noprefix.h header file
-// is included if the NUMPY_NOPREFIX macro is defined, which the user
-// should make sure is the case for numpy version 1.0 or greater.
+// PY_ARRAY_UNIQUE_SYMBOL is defined, which will allow PyTrilinos to
+// work with other extension modules that are compiled against NumPy.
+// Third, the NPY_NO_DEPRECATED macro is set to NPY_1_7_API_VERSION to
+// ensure that no deprecated NumPy code is used.  Fourth, the
+// numpy/arrayobject.h header file is included.  Fifth and finally,
+// the NPY_API_VERSION macro from arrayobject.h is checked, and if it
+// is old enough, macros are defined so that PyTrilinos will compile
+// with older versions of NumPy.
 
 // The user is responsible for defining the macro NO_IMPORT_ARRAY in
 // those source files that do not call the numpy routine
@@ -46,19 +50,11 @@
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #include <numpy/arrayobject.h>
 
-#ifdef NUMPY_NOPREFIX
-#include <numpy/noprefix.h>
-#define PyArray_SBYTE PyArray_BYTE
-#endif
-
 #if NPY_API_VERSION < 0x00000007
 #define NPY_ANYORDER      PyArray_ANYORDER
 #define NPY_DOUBLE        PyArray_DOUBLE
 #define NPY_INT           PyArray_INT
 #define NPY_ARRAY_FARRAY  NPY_FARRAY
-#define PyArray_NDIM(arr) (arr->nd)
-#define PyArray_DIMS(arr) (arr->dimensions)
-#define PyArray_DATA(arr) (arr->data)
 #endif
 
 #endif // NUMPY_INCLUDE_H
