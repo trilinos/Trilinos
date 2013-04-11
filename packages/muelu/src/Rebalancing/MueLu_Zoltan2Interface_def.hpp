a4832aa30b8a052ae412eb554fd141dc30273f26
Andrey Prokopenko <aprokop@sandia.gov>
2013-04-11 17:36:16 -0600

    MueLu: Zoltan2Interface: added parameter "pqparts" and "2k" algorithm
    2k algorithm works very similar to 2^k 3^m, but only uses 2s. This is
    the closest algorithm to RCB one can use.

--- packages/muelu/src/Rebalancing/MueLu_Zoltan2Interface_def.hpp
+++ packages/muelu/src/Rebalancing/MueLu_Zoltan2Interface_def.hpp
@@ -75,6 +75,7 @@ namespace MueLu {
     validParamList->set< RCP<const FactoryBase> >("number of partitions",   Teuchos::null, "(advanced) Factory computing the number of partitions");
     validParamList->set< int >                   ("rowWeight",                          0, "Default weight to rows (total weight = nnz + rowWeight");
     validParamList->set< std::string >           ("algorithm",              "multijagged", "Zoltan2 partitioning algorithm (multijagged,rcb)");
+    validParamList->set< std::string >           ("pqparts",                       "2k3m", "Algorithm for deciding the pqparts split");
 
     return validParamList;
   }
@@ -151,7 +152,8 @@ namespace MueLu {
     TEUCHOS_TEST_FOR_EXCEPTION(algo != "multijagged" && algo != "rcb", Exceptions::RuntimeError, "Unknown partitioning algorithm: \"" << algo << "\"");
     if (algo == "multijagged") {
       params.set("algorithm",             "multijagged");
-      params.set("pqParts",               getPQParts(numParts, dim));
+      std::string pqalgo = pL.get<std::string>("pqparts");
+      params.set("pqParts",               getPQParts(pqalgo, numParts, dim));
 
     } else if (algo == "rcb") {
       params.set("algorithm",             "rcb");
@@ -192,8 +194,10 @@ namespace MueLu {
 
   // Algorithm is kind of complex, but the details are not important
   template <class LocalOrdinal, class GlobalOrdinal, class Node, class LocalMatOps>
-  std::string Zoltan2Interface<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::getPQParts(GO numParts, size_t dim) const {
+  std::string Zoltan2Interface<LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::getPQParts(const std::string& algo, GO numParts, size_t dim) const {
     std::ostringstream pqParts;
+
+    TEUCHOS_TEST_FOR_EXCEPTION(algo != "2k3m" && algo != "2k", Exceptions::RuntimeError, "Unknown pqparts determination algorithm: \"" << algo << "\"");
 
     if (dim == 1) {
       pqParts << numParts;
@@ -202,83 +206,108 @@ namespace MueLu {
 
     // We would like to find a good recursive representation of the numProcs. It is not
     // possible for all values, so we try to find a close value which has one
-    // Here is one of the possible algorithms: try to find a closest number
-    // of form 2^k*3^m which is smaller than numParts. Generally, this makes the number
-    // of processors off by less than 15%
-    int i2 = -1, m2 = Teuchos::as<int>(floor(log(numParts)/log(2)));
-    int i3 = -1, m3 = Teuchos::as<int>(floor(log(numParts)/log(3)));
-    int d = Teuchos::as<int>(1e+9);
+    if (algo == "2k3m") {
+      // Here is one of the possible algorithms: try to find a closest number
+      // of form 2^k*3^m which is smaller than numParts. Generally, this makes the number
+      // of processors off by less than 15%
+      int i2 = -1, m2 = Teuchos::as<int>(floor(log(numParts)/log(2)));
+      int i3 = -1, m3 = Teuchos::as<int>(floor(log(numParts)/log(3)));
+      int d = Teuchos::as<int>(1e+9);
 
-    for (int i = 0; i <= m2; i++)
-      for (int j = 0; j <= m3; j++) {
-        int k = Teuchos::as<int>(std::pow(2.,i) * std::pow(3.,j));
-        if (k <= numParts && (numParts - k < d)) {
-          d = numParts - k;
-          i2 = i;
-          i3 = j;
+      for (int i = 0; i <= m2; i++)
+        for (int j = 0; j <= m3; j++) {
+          int k = Teuchos::as<int>(std::pow(2.,i) * std::pow(3.,j));
+          if (k <= numParts && (numParts - k < d)) {
+            d = numParts - k;
+            i2 = i;
+            i3 = j;
+          }
         }
+      if (d) {
+        numParts -= d;
+        GetOStream(Runtime0,0) << "Changing number of partitions to " << numParts << " in order to improve Zoltan2 pqJagged algorithm" << std::endl;
       }
-    if (d) {
-      numParts -= d;
-      GetOStream(Runtime0,0) << "Changing number of partitions to " << numParts << " in order to improve Zoltan2 pqJagged algorithm" << std::endl;
-    }
 
-    std::multiset<int> vals;
-    std::multiset<int>::const_reverse_iterator rit, rit1;
+      std::multiset<int> vals;
+      std::multiset<int>::const_reverse_iterator rit, rit1;
 
-    // Step 1: initialization with 1,2,3 factors
-    for (int i = 1; i <= i2; i++)           vals.insert(2);
-    for (int i = 1; i <= i3; i++)           vals.insert(3);
-    while (vals.size() < dim)               vals.insert(1);
+      // Step 1: initialization with 1,2,3 factors
+      for (int i = 1; i <= i2; i++)           vals.insert(2);
+      for (int i = 1; i <= i3; i++)           vals.insert(3);
+      while (vals.size() < dim)               vals.insert(1);
 
-    // Step 2: create factor products in order to limit ourselves to 2*dim factors
-    while (vals.size() > 2*dim) {
-      int v1 = *vals.begin(); vals.erase(vals.begin());
-      int v2 = *vals.begin(); vals.erase(vals.begin());
-      vals.insert(v1*v2);
-    }
-    // Save the current state for the possible fallback
-    std::multiset<int> vals_copy = vals;
-
-    // Step 3: try to do some further aggregation
-    const int threshold_factor = 4;
-    for (size_t k = 0; dim+k < 5 && vals.size() > dim; k++) {
-      int v1 = *vals.begin(), v2 = *(++vals.begin());
-      rit = vals.rbegin()++;
-      if (k+1 != dim)
-        rit++;
-      if (v1*v2 <= threshold_factor * (*rit)) {
-        vals.erase(vals.begin());
-        vals.erase(vals.begin());
+      // Step 2: create factor products in order to limit ourselves to 2*dim factors
+      while (vals.size() > 2*dim) {
+        int v1 = *vals.begin(); vals.erase(vals.begin());
+        int v2 = *vals.begin(); vals.erase(vals.begin());
         vals.insert(v1*v2);
       }
-    }
+      // Save the current state for the possible fallback
+      std::multiset<int> vals_copy = vals;
 
-    // Step 4: check if the aggregation is good enough
-    if (vals.size() > dim && vals.size() < 2*dim) {
-      rit = vals.rbegin();
-      for (size_t k = 0; k < dim; k++, rit++);
-      for (; rit != vals.rend() && (*rit <= 6); rit++);
-      if (rit != vals.rend()) {
-        // We don't like this factorization, fallback
-        vals = vals_copy;
+      // Step 3: try to do some further aggregation
+      const int threshold_factor = 4;
+      for (size_t k = 0; dim+k < 5 && vals.size() > dim; k++) {
+        int v1 = *vals.begin(), v2 = *(++vals.begin());
+        rit = vals.rbegin()++;
+        if (k+1 != dim)
+          rit++;
+        if (v1*v2 <= threshold_factor * (*rit)) {
+          vals.erase(vals.begin());
+          vals.erase(vals.begin());
+          vals.insert(v1*v2);
+        }
       }
-    }
 
-    // Step 5: save factors in a string
-    // Tricky: first <dim> factors are printed in reverse order
-    rit1 = vals.rbegin();
-    for (size_t k = 0; k < dim; k++) {
-      rit = vals.rbegin();
-      for (size_t j = 1; j+k < dim; j++)
-        rit++;
-      if (k)
-        pqParts << ",";
-      pqParts << *rit;
-      rit1++;
+      // Step 4: check if the aggregation is good enough
+      if (vals.size() > dim && vals.size() < 2*dim) {
+        rit = vals.rbegin();
+        for (size_t k = 0; k < dim; k++, rit++);
+        for (; rit != vals.rend() && (*rit <= 6); rit++);
+        if (rit != vals.rend()) {
+          // We don't like this factorization, fallback
+          vals = vals_copy;
+        }
+      }
+
+      // Step 5: save factors in a string
+      // Tricky: first <dim> factors are printed in reverse order
+      rit1 = vals.rbegin();
+      for (size_t k = 0; k < dim; k++) {
+        rit = vals.rbegin();
+        for (size_t j = 1; j+k < dim; j++)
+          rit++;
+        if (k)
+          pqParts << ",";
+        pqParts << *rit;
+        rit1++;
+      }
+      for (; rit1 != vals.rend(); rit1++)
+        pqParts << "," << *rit1;
+
+    } else if (algo == "2k") {
+      // Here is one of the possible algorithms: try to find a closest number
+      // of form 2^k which is smaller than numParts. This makes the number
+      // of processors off by less than 50%
+      int i2 = Teuchos::as<int>(floor(log(numParts)/log(2)));
+
+      int d = numParts - Teuchos::as<int>(std::pow(2.,i2));
+      if (d) {
+        numParts -= d;
+        GetOStream(Runtime0,0) << "Changing number of partitions to " << numParts << " in order to improve Zoltan2 pqJagged algorithm" << std::endl;
+      }
+
+      std::multiset<int> vals;
+
+      // Step 1: initialization with 1,2 factors
+      for (int i = 1; i <= i2; i++)           vals.insert(2);
+      while (vals.size() < dim)               vals.insert(1);
+
+      // Step 2: save factors in a string
+      pqParts << *(vals.begin());
+      for (std::multiset<int>::const_iterator it = (++vals.begin()); it != vals.end(); it++)
+        pqParts << "," << *it;
     }
-    for (; rit1 != vals.rend(); rit1++)
-      pqParts << "," << *rit1;
 
     return pqParts.str();
   }
