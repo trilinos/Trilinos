7ffba74160e14acbe758ad14dd25481a5d61f26f
Todd Kordenbrock <thkorde@sandia.gov>
2013-04-16 13:19:27 -0500

    add a case statement to select modules to load.  selection is based on $TARGET_COMPILER.  defaults to the intel compilers.

--- packages/trios/sampleScripts/config/chama/loadenv.sh
+++ packages/trios/sampleScripts/config/chama/loadenv.sh
@@ -2,7 +2,19 @@
 
 module purge
 
-module load gnu
-module load openmpi-gnu
+case $TARGET_COMPILER in
+    gnu)
+        module load gnu/4.7.0
+        module load openmpi-gnu/1.4
+        ;;
+    pgi)
+        module load pgi/12.5
+        module load openmpi-pgi/1.4
+        ;;
+    intel | *)
+        module load intel/12.1
+        module load openmpi-intel/1.4
+        ;;
+esac
 
 module list
