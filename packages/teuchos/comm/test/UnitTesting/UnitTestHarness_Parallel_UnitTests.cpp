bde175eba9d1ce92ad95beb96f7cdf75c1b7599e
Roscoe A. Bartlett <bartlettra@ornl.gov>
2013-04-18 11:29:47 -0400

    Adding all basic unit tests for failure cases that shoudl be reduced correctly (Trilinos #5909)

--- packages/teuchos/comm/test/UnitTesting/UnitTestHarness_Parallel_UnitTests.cpp
+++ packages/teuchos/comm/test/UnitTesting/UnitTestHarness_Parallel_UnitTests.cpp
@@ -50,11 +50,33 @@
 namespace Teuchos {
 
 
-TEUCHOS_UNIT_TEST( UnitTestHarness, globalReduce ) {
+TEUCHOS_UNIT_TEST( UnitTestHarness, nonRankFails ) {
   out << "Pass on proc 0 but fail on other procs!\n";
   const RCP<const Comm<int> > comm = DefaultComm<int>::getComm();
   const int procRank = comm->getRank();
   TEST_EQUALITY_CONST(procRank, 0);
+}
+
+
+TEUCHOS_UNIT_TEST( UnitTestHarness, nonRankThrowsTeuchosExcept ) {
+  out << "Pass on proc 0 but throws Teuchos exception on other processes!\n";
+  const RCP<const Comm<int> > comm = DefaultComm<int>::getComm();
+  const int procRank = comm->getRank();
+  TEUCHOS_ASSERT_EQUALITY(procRank, 0); // Throws on non-root processes
+  int myval = 1;
+  TEST_EQUALITY_CONST(myval, 1);
+}
+
+
+TEUCHOS_UNIT_TEST( UnitTestHarness, nonRankThrowsIntExcept ) {
+  out << "Pass on proc 0 but throws int exception on other processes!\n";
+  const RCP<const Comm<int> > comm = DefaultComm<int>::getComm();
+  const int procRank = comm->getRank();
+  if (procRank != 0) {
+    throw procRank;
+  }
+  int myval = 1;
+  TEST_EQUALITY_CONST(myval, 1);
 }
 
 
