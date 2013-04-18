54914ccceeee57de4c51f35fe17014431211ec93
Jonathan Hu <jhu@sandia.gov>
2013-04-18 11:16:18 -0700

    Trilinos: Fixing scripts for test machine eleanor.
    Adding Intel bash scripts that set the right environmental variables to detect MKL.
    Also adding Blas/Lapack TPL options to the cmake bits of the scripts.
    Removing the valgrind test from the list of nightly tests. We probably want this in long term,
    but right now I just want something working.

--- cmake/ctest/drivers/eleanor/cron_driver.sh
+++ cmake/ctest/drivers/eleanor/cron_driver.sh
@@ -25,6 +25,9 @@ export TDD_HTTP_PROXY="http://wwwproxy.sandia.gov:80"
 export http_proxy="http://wwwproxy.sandia.gov:80"
 export TDD_FORCE_CMAKE_INSTALL=0
 
+source /usr/local/intel/Compiler/11.1/064/bin/iccvars.sh intel64
+source /usr/local/intel/Compiler/11.1/064/mkl/tools/environment/mklvarsem64t.sh
+
 # Machine independent cron_driver:
 #
 
