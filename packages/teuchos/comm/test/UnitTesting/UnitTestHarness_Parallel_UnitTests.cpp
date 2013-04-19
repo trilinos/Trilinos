ba99b3e78588ad12f0c4afdf415f817a2b38c716
Roscoe A. Bartlett <bartlettra@ornl.gov>
2013-04-19 16:37:56 -0400

    Making tests fail on even/odd basis (Trilinos #5909)
    This will allow for better testing of testing of global reduction of
    pass/fail.  Next, I will add feature to actually print the ranks of the
    failing processes.

--- packages/teuchos/comm/test/UnitTesting/UnitTestHarness_Parallel_UnitTests.cpp
+++ packages/teuchos/comm/test/UnitTesting/UnitTestHarness_Parallel_UnitTests.cpp
@@ -51,28 +51,28 @@ namespace Teuchos {
 
 
 TEUCHOS_UNIT_TEST( UnitTestHarness, nonRootFails ) {
-  out << "Pass on proc 0 but fail on other procs!\n";
+  out << "Pass on even procs but fail on other procs!\n";
   const RCP<const Comm<int> > comm = DefaultComm<int>::getComm();
   const int procRank = comm->getRank();
-  TEST_EQUALITY_CONST(procRank, 0);
+  TEST_EQUALITY_CONST(procRank%2, 0);
 }
 
 
 TEUCHOS_UNIT_TEST( UnitTestHarness, nonRootThrowsTeuchosExcept ) {
-  out << "Pass on proc 0 but throws Teuchos exception on other processes!\n";
+  out << "Pass on even procs but throws Teuchos exception on other processes!\n";
   const RCP<const Comm<int> > comm = DefaultComm<int>::getComm();
   const int procRank = comm->getRank();
-  TEUCHOS_ASSERT_EQUALITY(procRank, 0); // Throws on non-root processes
+  TEUCHOS_ASSERT_EQUALITY(procRank%2, 0); // Throws on non-root processes
   int myval = 1;
   TEST_EQUALITY_CONST(myval, 1);
 }
 
 
 TEUCHOS_UNIT_TEST( UnitTestHarness, nonRootThrowsIntExcept ) {
-  out << "Pass on proc 0 but throws int exception on other processes!\n";
+  out << "Pass on even procs but throws int exception on other processes!\n";
   const RCP<const Comm<int> > comm = DefaultComm<int>::getComm();
   const int procRank = comm->getRank();
-  if (procRank != 0) {
+  if (procRank%2 != 0) {
     throw procRank;
   }
   int myval = 1;
