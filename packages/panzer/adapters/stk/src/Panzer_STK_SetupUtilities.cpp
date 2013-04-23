189f3d6c097a267c204a50deef07fa4ca9b9aeb7
Eric C. Cyr <eccyr@sandia.gov>
2013-04-23 16:39:20 -0600

    Panzer: Adding construction of element cascade in STK
    This builds the elements and local subcell ids and dimensions that
    touch a particular side. This will be useful for the high order surface
    flux calculations.

--- packages/panzer/adapters/stk/src/Panzer_STK_SetupUtilities.cpp
+++ packages/panzer/adapters/stk/src/Panzer_STK_SetupUtilities.cpp
@@ -51,8 +51,6 @@ buildWorksets(const panzer_stk::STK_Interface & mesh,
 {
   using namespace workset_utils;
 
-  const std::size_t workset_size = pb.cellData().numCells();
-
   std::vector<std::string> element_blocks;
 
   std::vector<std::size_t> local_cell_ids;
@@ -70,12 +68,11 @@ buildWorksets(const panzer_stk::STK_Interface & mesh,
 Teuchos::RCP<std::vector<panzer::Workset> >  
 buildWorksets(const panzer_stk::STK_Interface & mesh,
               const panzer::PhysicsBlock & pb,
-              const std::string & sideset)
+              const std::string & sideset,
+              bool useCascade)
 {
   using namespace workset_utils;
   using Teuchos::RCP;
-
-  const std::size_t workset_size = pb.cellData().numCells();
 
   std::vector<stk::mesh::Entity*> sideEntities; 
 
@@ -116,16 +113,35 @@ buildWorksets(const panzer_stk::STK_Interface & mesh,
   }
   
   std::vector<stk::mesh::Entity*> elements;
-  std::vector<std::size_t> local_side_ids;
-  getSideElements(mesh, pb.elementBlockID(),
-		      sideEntities,local_side_ids,elements);
+  std::map<std::pair<unsigned,unsigned>,std::vector<std::size_t> > local_cell_ids;
+  if(!useCascade) {
+    unsigned subcell_dim = pb.cellData().baseCellDimension()-1;
+    std::vector<std::size_t> local_side_ids;
+    getSideElements(mesh, pb.elementBlockID(),
+  		      sideEntities,local_side_ids,elements);
 
-  // build local cell_ids, mapped by local side id
-  std::map<unsigned,std::vector<std::size_t> > local_cell_ids;
-  for(std::size_t elm=0;elm<elements.size();++elm) {
-    stk::mesh::Entity * element = elements[elm];
+    // build local cell_ids, mapped by local side id
+    for(std::size_t elm=0;elm<elements.size();++elm) {
+      stk::mesh::Entity * element = elements[elm];
 	
-    local_cell_ids[local_side_ids[elm]].push_back(mesh.elementLocalId(element));
+      local_cell_ids[std::make_pair(subcell_dim,local_side_ids[elm])].push_back(mesh.elementLocalId(element));
+    }
+  }
+  else {
+    TEUCHOS_ASSERT(false);
+/*
+    std::vector<std::size_t> local_subcell_ids, subcell_dim;
+    getSideElementCascade(mesh, pb.elementBlockID(),
+  		          sideEntities,subcell_dim,local_subcell_dim,elements);
+
+    // build local cell_ids, mapped by local side id
+    std::map<unsigned,std::vector<std::size_t> > local_cell_ids;
+    for(std::size_t elm=0;elm<elements.size();++elm) {
+      stk::mesh::Entity * element = elements[elm];
+	
+      local_cell_ids[local_side_ids[elm]].push_back(mesh.elementLocalId(element));
+    }
+*/
   }
 
   // only build workset if there are elements to worry about
@@ -139,7 +155,7 @@ buildWorksets(const panzer_stk::STK_Interface & mesh,
     Teuchos::RCP<std::vector<panzer::Workset> > worksets = Teuchos::rcp(new std::vector<panzer::Workset>);
 
     // loop over each side
-    for(std::map<unsigned,std::vector<std::size_t> >::const_iterator itr=local_cell_ids.begin();
+    for(std::map<std::pair<unsigned,unsigned>,std::vector<std::size_t> >::const_iterator itr=local_cell_ids.begin();
         itr!=local_cell_ids.end();++itr) {
  
       if(itr->second.size()==0)
@@ -153,8 +169,8 @@ buildWorksets(const panzer_stk::STK_Interface & mesh,
 
       // correct worksets so the sides are correct
       for(std::size_t w=0;w<current->size();w++) {
-        (*current)[w].subcell_dim = pb.cellData().baseCellDimension()-1;
-        (*current)[w].subcell_index = itr->first;
+        (*current)[w].subcell_dim = itr->first.first;
+        (*current)[w].subcell_index = itr->first.second;
       }
 
       // append new worksets
@@ -280,6 +296,42 @@ void getSubcellElements(const panzer_stk::STK_Interface & mesh,
   }
 }
 
+void getSideElementCascade(const panzer_stk::STK_Interface & mesh,
+                           const std::string & blockId, 
+                           const std::vector<stk::mesh::Entity*> & sides,
+                           std::vector<std::size_t> & localSubcellDim, 
+                           std::vector<std::size_t> & localSubcellIds, 
+                           std::vector<stk::mesh::Entity*> & elements)
+{
+  // This is a sketch of the alogrithm, for computing the side element
+  // cascade. The requirements are that for a particular set of sides
+  // we compute all elements and subcells where they touch the side. Note
+  // that elements can be and will be repeated within this list.
+
+  std::vector<std::vector<stk::mesh::Entity*> > subcells;
+  getSubcellEntities(mesh,sides,subcells);
+  subcells.push_back(sides);
+
+  // subcells now contains a unique list of faces, edges and nodes that
+  // intersect with the sides
+
+  for(std::size_t d=0;d<subcells.size();d++) {
+    std::vector<std::size_t> subcellIds;
+    std::vector<stk::mesh::Entity*> subcellElements;
+
+    // find elements connected to the subcells and their local subcell information
+    getSubcellElements(mesh,blockId,subcells[d],subcellIds,subcellElements);
+
+    // sanity check
+    TEUCHOS_ASSERT(subcellIds.size()==subcellElements.size());
+
+    // concatenate with found elements
+    localSubcellDim.insert(localSubcellDim.end(),subcellElements.size(),d);
+    localSubcellIds.insert(localSubcellIds.end(),subcellIds.begin(),subcellIds.end());
+    elements.insert(elements.end(),subcellElements.begin(),subcellElements.end());
+  }
+}
+
 void getSideElements(const panzer_stk::STK_Interface & mesh,
                      const std::string & blockId, 
                      const std::vector<stk::mesh::Entity*> & sides,
@@ -296,6 +348,67 @@ void getNodeElements(const panzer_stk::STK_Interface & mesh,
                      std::vector<stk::mesh::Entity*> & elements)
 {
    getSubcellElements(mesh,blockId,nodes,localNodeIds,elements);
+}
+
+void getSubcellEntities(const panzer_stk::STK_Interface & mesh,
+		        const std::vector<stk::mesh::Entity*> & entities,
+	 	        std::vector<std::vector<stk::mesh::Entity*> > & subcells)
+{
+  int maxRankIndex = mesh.getDimension()-1;
+  stk::mesh::EntityRank master_rank = entities[0]->entity_rank();
+  std::vector<stk::mesh::EntityRank> ranks(mesh.getDimension()+1);
+
+  // build rank array, and compute maximum rank index (within rank array)
+  // for these entities with "master_rank"
+  switch(mesh.getDimension()) {
+  case 3:
+    ranks[2] = mesh.getFaceRank();
+    maxRankIndex = (master_rank==mesh.getFaceRank() ? 1 : maxRankIndex);
+  case 2:
+    ranks[1] = mesh.getEdgeRank();
+    maxRankIndex = (master_rank==mesh.getEdgeRank() ? 0 : maxRankIndex);
+  case 1:
+    ranks[0] = mesh.getNodeRank();
+    maxRankIndex = (master_rank==mesh.getNodeRank() ? -1 : maxRankIndex);
+    break;
+  default:
+    TEUCHOS_ASSERT(false);
+    break;
+  };
+  ranks[mesh.getDimension()] = mesh.getElementRank();
+
+  // make sure the rank index is ok
+  TEUCHOS_ASSERT(maxRankIndex>-1);
+
+  std::vector<std::set<stk::mesh::Entity*> > subcells_set(maxRankIndex+1);
+
+  // loop over each entitiy extracting elements and local entity ID that
+  // are containted in specified block.
+  std::vector<stk::mesh::Entity*>::const_iterator entityItr;
+  for(entityItr=entities.begin();entityItr!=entities.end();++entityItr) {
+    stk::mesh::Entity * entity = *entityItr;
+
+    // sanity check, enforcing that there is only one rank
+    TEUCHOS_ASSERT(entity->entity_rank()==master_rank); 
+    
+    for(int i=0;i<=maxRankIndex;i++) {
+      stk::mesh::PairIterRelation relations = entity->relations(ranks[i]);
+
+      // for each relation insert the appropriate entity (into the set
+      // which gurantees uniqueness
+      for(std::size_t e=0;e<relations.size();++e) {
+        stk::mesh::Entity * subcell = relations[e].entity();
+
+        subcells_set[i].insert(subcell);
+      }
+    }
+  }
+
+  // copy unique entities into vector
+  subcells.clear();
+  subcells.resize(subcells_set.size());
+  for(std::size_t i=0;i<subcells_set.size();i++)
+    subcells[i].assign(subcells_set[i].begin(),subcells_set[i].end());
 }
 
 }
