d95ab5ec0de1bcf6de466f756be2208c4c8c6aaa
Chris Siefert <csiefer@sandia.gov>
2013-04-26 15:31:03 -0600

    ML: Adding Maxwell1 support to mlmex.

--- packages/ml/matlab/ml.m
+++ packages/ml/matlab/ml.m
@@ -30,7 +30,20 @@ function varargout=ml(varargin)
 %                    Values: 'mlapi' or 'epetra'  [string]
 %                    Default: 'epetra'  
 %
-% (2) ml(h,A,b,['parameter',value,...])
+% (2) [h,oc]=ml('setup',Kedge,Knode,Grad,['parameter',value,...]) -
+% sets up ML for Maxwell's equations
+%  Input:
+%  Kedge           - Matrix to be solved with [sparse matrix]
+%  Knode           - Auxillary nodal Laplacian [sparse matrix]
+%  Grad            - Discrete gradient
+%  parameter       - Teuchos parameter [string]
+%  value           - Value for Teuchos parameter [???]
+%  Output:
+%  h               - The ML handle for the system
+%  [oc]            - Operator complexity of the preconditioner
+%
+%
+% (3) ml(h,A,b,['parameter',value,...])
 %  Input:
 %  h               - The ML handle for the system to solve [int]
 %  A               - Matrix to be solved with [sparse matrix]
@@ -46,19 +59,19 @@ function varargout=ml(varargin)
 %  Even if the ML_Epetra interface is used, these options are given
 %  in the MLAPI style.
 %
-% (3) ml('cleanup',[h]) - frees allocated memory
+% (4) ml('cleanup',[h]) - frees allocated memory
 %  Input:
 %  h               - The ML handle for the system to clean up.
 %                   Calling 'cleanup' with no handle cleans up all
 %                   the systems.
 %
-% (4) ml('status',[h]) - prints out status information
+% (5) ml('status',[h]) - prints out status information
 %  Input:
 %  h               - The ML handle for the system to print status for.
 %                   Calling 'status' with no handle prints status
 %                   for all the systems.
 %
-% (5) agg=ml('aggregate',A,['parameter',value,...]) - Does a single
+% (6) agg=ml('aggregate',A,['parameter',value,...]) - Does a single
 % level of aggregation on the matrix A.
 %  Input:
 %  A               - Matrix to be solved with [sparse matrix]
@@ -75,6 +88,7 @@ function varargout=ml(varargin)
 % by: Chris Siefert <csiefer@sandia.gov>
 
 % Version History
+% 04/26/2013 - Adding Maxwell support.
 % 10/04/2006 - Finally added the aggregation interface
 % 08/30/2006 - Added ML_epetra interface functionality.
 % 08/15/2006 - Added operator complexity handling.
@@ -88,7 +102,7 @@ if(nargin>=2 && strcmp(varargin{1},'setup'))
   % Setup mode
   [out,oc]=mlmex(0,varargin{2:nargin});
   varargout{1}=out;
-  if(nargout==2),varargout{2}=oc;end
+  if(nargout==2),varargout{2}=oc;end 
 elseif(nargin>=3 && isnumeric(varargin{1}) && issparse(varargin{2}))
   % Solve mode
   if(size(varargin{2},1)~=length(varargin{3})), fprintf('ML: Error size mismatch between A + B\n');out=0;
---
+++ packages/ml/matlab/mlmex.h
@@ -0,0 +1,207 @@
+/* ******************************************************************** */
+/* See the file COPYRIGHT for a complete copyright notice, contact      */
+/* person and disclaimer.                                               */        
+/* ******************************************************************** */
+#ifndef MLMEX_H
+#define MLMEX_H
+
+#include "ml_config.h"
+#include "ml_common.h"
+
+#ifdef HAVE_ML_MLAPI
+/* MLAPI Headers */
+#include "MLAPI_Space.h"
+#include "MLAPI_Operator.h"
+#include "MLAPI_MultiVector.h"
+#include "MLAPI_Expressions.h"
+#include "MLAPI_MultiLevelAdaptiveSA.h"
+#include "MLAPI_DistributedMatrix.h"
+#include "MLAPI_Krylov.h"
+#endif
+
+/* ML_Epetra Headers */
+#include "ml_include.h"
+#include "ml_MultiLevelPreconditioner.h"
+#include "Epetra_SerialComm.h"
+#include "Epetra_Map.h"
+#include "Epetra_CrsMatrix.h"
+#include "Epetra_LinearProblem.h"
+#include "Teuchos_ParameterList.hpp"
+#include "AztecOO.h"
+
+
+#ifdef HAVE_ML_MATLAB
+#include "mex.h"
+
+/* The big classes of data */
+class ml_data_pack;
+class ml_data_pack{
+public:
+  ml_data_pack();
+  virtual ~ml_data_pack();
+
+  /* setup - sets up an ml_data_pack object
+     Parameters:
+     N       - Number of unknowns [I]
+     rowind  - Row indices of matrix (CSC format) [I]
+     colptr  - Column indices of matrix (CSC format) [I]
+     vals    - Nonzero values of matrix (CSC format) [I]
+     Returns: IS_TRUE if setup was succesful, IS_FALSE otherwise
+  */
+  virtual int setup(int N,int* rowind,int* colptr, double* vals)=0;
+
+  /* status - reports (to stdout) the status of the object
+     Returns: IS_TRUE 
+  */
+  virtual int status()=0;
+
+  /* solve - Given two Teuchos lists, one in the ml_data_pack, and one of
+     solve-time options, this routine calls the relevant solver and returns the solution.
+     Parameters:
+     TPL     - Teuchos list of solve-time options [I]
+     N       - Number of unknowns [I]
+     b       - RHS vector [I]
+     x       - solution vector [O]
+     iters   - number of iterations taken [O]
+     returns: IS_TRUE if sucessful, IS_FALSE otherwise.
+  */
+  virtual int solve(Teuchos::ParameterList* TPL, int N, double*b, double*x, int &iters)=0;  
+public:
+  int id;
+  Teuchos::ParameterList *List;
+  double operator_complexity;  
+  ml_data_pack *next;
+};
+
+class mlapi_data_pack:public ml_data_pack{
+public:
+  mlapi_data_pack();
+  ~mlapi_data_pack();
+
+  /* setup - sets up an ml_data_pack object
+     Parameters:
+     N       - Number of unknowns [I]
+     rowind  - Row indices of matrix (CSC format) [I]
+     colptr  - Column indices of matrix (CSC format) [I]
+     vals    - Nonzero values of matrix (CSC format) [I]
+     Returns: IS_TRUE if setup was succesful, IS_FALSE otherwise
+  */
+  int setup(int N,int* rowind,int* colptr, double* vals);
+
+  /* status - reports (to stdout) the status of the object
+     Returns: IS_TRUE 
+  */
+  int status();
+
+
+  /* solve - Given two Teuchos lists, one in the ml_data_pack, and one of
+     solve-time options, this routine calls the relevant solver and returns the solution.
+     Parameters:
+     TPL     - Teuchos list of solve-time options [I]
+     N       - Number of unknowns [I]
+     b       - RHS vector [I]
+     x       - solution vector [O]
+     iters   - number of iterations taken [O] (NOT IMPLEMENTED)
+     returns: IS_TRUE if sucessful, IS_FALSE otherwise.
+  */
+  int solve(Teuchos::ParameterList* TPL, int N, double*b, double*x, int &iters);  
+
+private:
+  MLAPI::Space * FineSpace;
+  MLAPI::DistributedMatrix * A;
+  MLAPI::MultiLevelAdaptiveSA *Prec;
+};
+
+
+class ml_epetra_data_pack:public ml_data_pack{
+public:
+  ml_epetra_data_pack();
+  ~ml_epetra_data_pack();
+
+  /* setup - sets up an ml_data_pack object
+     Parameters:
+     N       - Number of unknowns [I]
+     rowind  - Row indices of matrix (CSC format) [I]
+     colptr  - Column indices of matrix (CSC format) [I]
+     vals    - Nonzero values of matrix (CSC format) [I]
+     Returns: IS_TRUE if setup was succesful, IS_FALSE otherwise
+  */
+  int setup(int N,int* rowind,int* colptr, double* vals);
+
+  /* status - reports (to stdout) the status of the object
+     Returns: IS_TRUE 
+  */
+  int status();
+
+  /* solve - Given two Teuchos lists, one in the ml_data_pack, and one of
+     solve-time options, this routine calls the relevant solver and returns the solution.
+     Parameters:
+     TPL     - Teuchos list of solve-time options [I]
+     N       - Number of unknowns [I]
+     b       - RHS vector [I]
+     x       - solution vector [O]
+     iters   - number of iterations taken [O]
+     returns: IS_TRUE if sucessful, IS_FALSE otherwise.
+  */
+  int solve(Teuchos::ParameterList* TPL, int N, double*b, double*x,int &iters);  
+
+  /* GetPreconditioner - returns a pointer to the preconditioner */     
+  ML_Epetra::MultiLevelPreconditioner* GetPreconditioner(){return Prec;}
+  
+  
+private:
+  Epetra_CrsMatrix * A;
+  ML_Epetra::MultiLevelPreconditioner *Prec;
+};
+
+
+class ml_maxwell1_data_pack:public ml_data_pack{
+public:
+  ml_maxwell1_data_pack();
+  ~ml_maxwell1_data_pack();
+  
+  /* setup - sets up an ml_data_pack object
+     Parameters:
+     name    - Name of matrix to create [I]
+     mxa     - mxArray containing data
+     rewrap_ints - if the ints don't line up correctly
+     Returns: IS_TRUE if setup was succesful, IS_FALSE otherwise
+  */
+  int setup_matrix(const char * name, const mxArray * mxa, bool rewrap_ints);
+  /*
+    Returns: IS_TRUE if setup was succesful, IS_FALSE otherwise
+  */
+  int setup_preconditioner();
+
+  /* status - reports (to stdout) the status of the object
+     Returns: IS_TRUE 
+  */
+  int status();
+
+  /* solve - Given two Teuchos lists, one in the ml_data_pack, and one of
+     solve-time options, this routine calls the relevant solver and returns the solution.
+     Parameters:
+     TPL     - Teuchos list of solve-time options [I]
+     N       - Number of unknowns [I]
+     b       - RHS vector [I]
+     x       - solution vector [O]
+     iters   - number of iterations taken [O]
+     returns: IS_TRUE if sucessful, IS_FALSE otherwise.
+  */
+  int solve(Teuchos::ParameterList* TPL, int N, double*b, double*x,int &iters);  
+
+  /* GetPreconditioner - returns a pointer to the preconditioner */     
+  ML_Epetra::MultiLevelPreconditioner* GetPreconditioner(){return Prec;}
+  
+  
+private:
+  // Disabled
+  int setup(int N,int* rowind,int* colptr, double* vals){return 0;}
+
+
+  Epetra_CrsMatrix *EdgeMatrix, *GradMatrix, *NodeMatrix;
+  ML_Epetra::MultiLevelPreconditioner *Prec;
+};
+
+#endif
+#endif
