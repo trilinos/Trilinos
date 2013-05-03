1ccf561982fd551692f536341d66fbf8ed5f8fc4
Todd Kordenbrock <thkorde@sandia.gov>
2013-05-03 18:42:48 -0500

    openmpi/1.4 (default module) doesn't support launching in MPMD/MIMD mode.  use openmpi/1.6 instead.

--- packages/trios/sampleScripts/config/chama/loadenv.sh
+++ packages/trios/sampleScripts/config/chama/loadenv.sh
@@ -5,15 +5,15 @@ module purge
 case $TARGET_COMPILER in
     gnu)
         module load gnu/4.7.0
-        module load openmpi-gnu/1.4
+        module load openmpi-gnu/1.6
         ;;
     pgi)
         module load pgi/12.5
-        module load openmpi-pgi/1.4
+        module load openmpi-pgi/1.6
         ;;
     intel | *)
         module load intel/12.1
-        module load openmpi-intel/1.4
+        module load openmpi-intel/1.6
         ;;
 esac
 
