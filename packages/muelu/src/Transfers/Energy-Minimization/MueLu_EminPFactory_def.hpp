14b91cf03540e70960a1714b2bc493777f2caaac
Andrey Prokopenko <aprokop@sandia.gov>
2013-05-06 16:02:43 -0600

    MueLu: Emin: cleanup

--- packages/muelu/src/Transfers/Energy-Minimization/MueLu_EminPFactory_def.hpp
+++ packages/muelu/src/Transfers/Energy-Minimization/MueLu_EminPFactory_def.hpp
@@ -25,16 +25,16 @@ namespace MueLu {
   RCP<const ParameterList> EminPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::GetValidParameterList(const ParameterList& paramList) const {
     RCP<ParameterList> validParamList = rcp(new ParameterList());
 
-    validParamList->set< RCP<const FactoryBase> >("A",           Teuchos::null, "Generating factory for the matrix A used during internal iterations");
-    validParamList->set< RCP<const FactoryBase> >("P",           Teuchos::null, "Generating factory for the initial guess");
-    validParamList->set< RCP<const FactoryBase> >("Nullspace",   Teuchos::null, "Generating factory for the nullspace");
-    validParamList->set< RCP<const FactoryBase> >("Constraint",  Teuchos::null, "Generating factory for constraints");
-    validParamList->set< int >                   ("Niterations",             3, "Number of iterations of the internal iterative method");
+    validParamList->set< RCP<const FactoryBase> >("A",                 Teuchos::null, "Generating factory for the matrix A used during internal iterations");
+    validParamList->set< RCP<const FactoryBase> >("P",                 Teuchos::null, "Generating factory for the initial guess");
+    validParamList->set< RCP<const FactoryBase> >("Nullspace",         Teuchos::null, "Generating factory for the nullspace");
+    validParamList->set< RCP<const FactoryBase> >("Constraint",        Teuchos::null, "Generating factory for constraints");
+    validParamList->set< int >                   ("Niterations",                   3, "Number of iterations of the internal iterative method");
     validParamList->set< int >                   ("Reuse Niterations",             1, "Number of iterations of the internal iterative method");
-    validParamList->set< RCP<Matrix           > >("P0",           Teuchos::null, "Initial guess at P");
-    validParamList->set< bool >                  ("Keep P0",      false, "Keep an initial P0 (for reuse)");
-    validParamList->set< RCP<Constraint       > >("Constraint0",   Teuchos::null, "Initial Constraint");
-    validParamList->set< bool >                  ("Keep Constraint0", false, "Keep an initial Constraint (for reuse)");
+    validParamList->set< RCP<Matrix> >           ("P0",                Teuchos::null, "Initial guess at P");
+    validParamList->set< bool >                  ("Keep P0",                   false, "Keep an initial P0 (for reuse)");
+    validParamList->set< RCP<Constraint> >       ("Constraint0",       Teuchos::null, "Initial Constraint");
+    validParamList->set< bool >                  ("Keep Constraint0",          false, "Keep an initial Constraint (for reuse)");
     return validParamList;
   }
 
@@ -42,6 +42,7 @@ namespace MueLu {
   void EminPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node, LocalMatOps>::DeclareInput(Level& fineLevel, Level& coarseLevel) const {
     Input(fineLevel,   "A");
     Input(fineLevel,   "Nullspace");
+    // FIXME: we should not request for P or Constraint if we reuse stuff
     Input(coarseLevel, "P");
     Input(coarseLevel, "Constraint");
   }
@@ -58,49 +59,48 @@ namespace MueLu {
     const ParameterList & pL = GetParameterList();
 
     // Set keep flags
-    if(pL.isParameter("Keep P0") && pL.get<bool>("Keep P0"))
-	coarseLevel.Keep("P0",this);
-    if(pL.isParameter("Keep Constraint0") && pL.get<bool>("Keep Constraint0"))
-	coarseLevel.Keep("Constraint0",this);
+    if (pL.isParameter("Keep P0") && pL.get<bool>("Keep P0"))
+      coarseLevel.Keep("P0",this);
+    if (pL.isParameter("Keep Constraint0") && pL.get<bool>("Keep Constraint0"))
+      coarseLevel.Keep("Constraint0",this);
 
     // Reuse
     int Niterations;
-    
+
     // Get A, B
-    RCP<Matrix>      A          = Get< RCP<Matrix> >     (fineLevel,   "A");
-    RCP<MultiVector> B          = Get< RCP<MultiVector> >(fineLevel,   "Nullspace");
+    RCP<Matrix>      A = Get< RCP<Matrix> >     (fineLevel,   "A");
+    RCP<MultiVector> B = Get< RCP<MultiVector> >(fineLevel,   "Nullspace");
 
     // Get P0 or make P
     RCP<Matrix>      P0;
-    if(coarseLevel.IsAvailable("P0",this)) { 
-      P0 = coarseLevel.Get<RCP<Matrix> >("P0",this);
+    if (coarseLevel.IsAvailable("P0", this)) {
+      P0          = coarseLevel.Get<RCP<Matrix> >("P0", this);
       Niterations = pL.get<int>("Reuse Niterations");
       GetOStream(Runtime0, 0) << "EminPFactory: Reusing P0"<<std::endl;
 
-    }
-    else {
-      P0 = Get< RCP<Matrix> >(coarseLevel, "P");
+    } else {
+      P0          = Get< RCP<Matrix> >(coarseLevel, "P");
       Niterations = pL.get<int>("Niterations");
     }
 
     // Get Constraint0 or make Constraint
-    RCP<Constraint>  X;
-    if(coarseLevel.IsAvailable("Constraint0",this)) {
-      X = coarseLevel.Get<RCP<Constraint> >("Constraint0",this);
+    RCP<Constraint> X;
+    if (coarseLevel.IsAvailable("Constraint0", this)) {
+      X = coarseLevel.Get<RCP<Constraint> >("Constraint0", this);
       GetOStream(Runtime0, 0) << "EminPFactory: Reusing Constraint0"<<std::endl;
-    }
-    else {
+
+    } else {
       X = Get< RCP<Constraint> > (coarseLevel, "Constraint");
     }
 
 
-    RCP<Matrix>      P;
+    RCP<Matrix> P;
     CGSolver EminSolver(Niterations);
     EminSolver.Iterate(*A, *X, *P0, *B, P);
 
     Set(coarseLevel, "Constraint0", X);
-    Set(coarseLevel, "P", P);
-    Set(coarseLevel, "P0",P);
+    Set(coarseLevel, "P",           P);
+    Set(coarseLevel, "P0",          P);
 
     RCP<ParameterList> params = rcp(new ParameterList());
     params->set("printLoadBalancingInfo", true);
