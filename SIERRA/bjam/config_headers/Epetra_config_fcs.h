d718de71d01df67b7b87f2f4ea707357289e5f49
Brent Perschbacher <bmpersc@sandia.gov>
2013-05-06 16:37:15 -0600

    Trilinos: Update flat headers
    Update the configured headers from Trilinos to match the updated Trilinos.
    
    (From Sierra)

--- SIERRA/bjam/config_headers/Epetra_config_fcs.h
+++ SIERRA/bjam/config_headers/Epetra_config_fcs.h
@@ -134,6 +134,8 @@
 
 /* #undef Epetra_ENABLE_MKL_SPARSE */
 
+/* #undef Epetra_DISABLE_MKL_SPARSE_MM */
+
 /* Define if you want to have long long (64 bit) global indices only. */
 /* #undef EPETRA_NO_32BIT_GLOBAL_INDICES */
 
