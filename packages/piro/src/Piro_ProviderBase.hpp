e3a8299bbe213168a00cd060eb7486a8075b8286
Julien Cortial <jcortia@sandia.gov>
2013-05-07 09:28:56 -0700

    Piro: Improved Provider documentation

--- packages/piro/src/Piro_ProviderBase.hpp
+++ packages/piro/src/Piro_ProviderBase.hpp
@@ -55,14 +55,21 @@ namespace Piro {
 template <typename T>
 class ProviderBase {
 public:
-  //! \brief Returns an owning pointer to an object of a subclass of T
-  //! \details The returned value may refer to a new instance,
-  //! to an already existing -- therefore possibly shared -- one,
-  //! or simply be the null pointer.
+  /*! \brief Returns an owning pointer to an object of a subclass of T
+   *
+   *  The returned value may refer to a new instance, to an already existing -- therefore possibly shared -- one,
+   *  or simply be the null pointer.
+   */
   virtual Teuchos::RCP<T> getInstance(const Teuchos::RCP<Teuchos::ParameterList> &params) = 0;
 
+  //! \name Constructor and destructor
+  //@{
+  //! \brief Empty default constructor
   ProviderBase() {}
+
+  //! \brief Virtual empty destructor
   virtual ~ProviderBase() {}
+  //@}
 
 private:
   // Disallow copy & assignment
