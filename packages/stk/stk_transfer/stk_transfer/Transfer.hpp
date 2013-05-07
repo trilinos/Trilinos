840447f06a49f86ece9033063fddbb46020114ed
James Overfelt <jroverf@sandia.gov>
2013-05-07 09:03:01 -0600

    Simple STK::transfer test.
    Filled in the outline created by Steve Kennon
    with a very simple transfer example that will
    be used to research algorithms.
    
    The use case is linear interpolation from a
    random set of point/values to another set
    of points.  This is very simple with the
    stk::search class that is already in place but
    demonstrates many of the attributes that
    are in complicated transfers:
    
    (1) Framework-like user defined tolerances
    used to define a search distance around
    the points. How to automatically define
    this rather obscure parameter?
    
    (2) What to do when tolerance from (1) is
    too small or too large? Leads to yet another
    user defined multiplication factor.
    
    (3) Incremental update of position. Is
    there a way to use stk::search to optimize
    the algorithm if locations are incrementally
    updated?
    
    (4) How to handle parallel communication?
    Currently is only written for serial and
    is independent of stk::Mesh.
    
    (5) Scaling studies would be nice.
    
    One thing that this example is not very
    useful for is studying conservative transfers.
    That will need to wait for a mesh based transfer
    example.
    
    It's been a while, hopefully this does not break
    anything.
    
    (From Sierra)

--- packages/stk/stk_transfer/stk_transfer/Transfer.hpp
+++ packages/stk/stk_transfer/stk_transfer/Transfer.hpp
@@ -1,8 +1,58 @@
+/*------------------------------------------------------------------------*/
+/*                 Copyright 2013 Sandia Corporation.                     */
+/*  Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive   */
+/*  license for use of this work by or on behalf of the U.S. Government.  */
+/*  Export of this program may require a license from the                 */
+/*  United States Government.                                             */
+/*------------------------------------------------------------------------*/
+
+
 #ifndef Transfer_hpp
 #define Transfer_hpp
 
+#include <Intrepid_FieldContainer.hpp>
+#include <stk_mesh/base/Comm.hpp>
+
+
+namespace stk {
+namespace transfer {
+
 class Transfer {
 public:
-  Transfer();
+  typedef Intrepid::FieldContainer<double> MDArray;
+
+  struct Hints {
+    Hints() : 
+      Tolerance(.1), 
+      ExpansionFactor(1.5){};
+    double Tolerance;
+    double ExpansionFactor;
+  };
+
+  // Constructor is trivial but could be expanded in the
+  // future to allocate and initialize input data.
+  Transfer(const Hints& hints=Hints());
+
+  // Given values defined at points in arrays FromValues and
+  // FromPoints which have to be of the same length, and given
+  // points to transfer in in ToPoints, find the N+1 nearest
+  // points to each ToPoints from the FromPoints and do linear
+  // interpolation of the FromValues to the ToValues.  N is the
+  // spatical dimension and the second dimension of the FromPoints
+  // and ToPoints matrix.
+
+  // Is Intrepid::FieldContainer the standard for large amounts
+  // of ordered data? Seems that STK might have it's own class
+  // for this. Should PointToPoint depend on Intrepid this way?
+  void PointToPoint(      MDArray &ToValues,
+                    const MDArray &ToPoints,
+                    const MDArray &FromValues,
+                    const MDArray &FromPoints,
+                    const stk::ParallelMachine  comm);
+private :
+  const double Tolerance;
+  const double ExpansionFactor;
 };
+
+}}
 #endif
--- packages/stk/stk_transfer/use_cases/UseCase_5.cpp
+++ packages/stk/stk_transfer/use_cases/UseCase_5.cpp
@@ -1,6 +1,83 @@
+/*------------------------------------------------------------------------*/
+/*                 Copyright 2013 Sandia Corporation.                     */      
+/*  Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive   */
+/*  license for use of this work by or on behalf of the U.S. Government.  */
+/*  Export of this program may require a license from the                 */      
+/*  United States Government.                                             */      
+/*------------------------------------------------------------------------*/
+
+#include <Intrepid_FieldContainer.hpp>
+
+
+#include <stk_mesh/base/Comm.hpp>
+
+#include <stk_util/use_cases/UseCaseEnvironment.hpp>
+#include <stk_util/diag/PrintTimer.hpp>
+
 #include <stk_transfer/Transfer.hpp>
 
-int main()
+
+int main(int argc, char **argv)
 {
-  Transfer a;
+  stk::diag::Timer timer("Transfer Use Case 5", 
+                          use_case::TIMER_TRANSFER, 
+                          use_case::timer());
+  stk::diag::Timer timer_point_to_point(" Point To Point", timer);
+  use_case::timerSet().setEnabledTimerMask(use_case::TIMER_ALL);
+
+
+  enum {           DIM = 3  };
+  enum { FROMNUMPOINTS = 100  };
+  enum {   TONUMPOINTS = 100  };
+  stk::ParallelMachine comm(stk::parallel_machine_init(&argc, &argv));
+ 
+  typedef stk::transfer::Transfer::MDArray MDArray;
+
+  stk::transfer::Transfer transfer;
+
+  MDArray FromPoints (FROMNUMPOINTS,DIM), 
+          ToPoints   (  TONUMPOINTS,DIM), 
+          FromValues (FROMNUMPOINTS,  1), 
+          ToValues   (  TONUMPOINTS,  1);
+  for (unsigned i=0 ; i<FROMNUMPOINTS; ++i) {
+    double v = 0; 
+    for (unsigned j=0 ; j<DIM; ++j) {
+      FromPoints(i,j) = static_cast<double>(rand())/RAND_MAX;
+      v += FromPoints(i,j);
+    }
+    FromValues(i,0) = v;
+  }
+  for (unsigned i=0 ; i<TONUMPOINTS; ++i) {
+    for (unsigned j=0 ; j<DIM; ++j) {
+      ToPoints(i,j) = static_cast<double>(rand())/RAND_MAX;
+    }
+  }
+
+  {
+    stk::diag::TimeBlock __timer_point_to_point(timer_point_to_point);
+    transfer.PointToPoint(ToValues, ToPoints, FromValues, FromPoints, comm);
+  }
+
+  bool success = true;
+  for (unsigned i=0 ; i<TONUMPOINTS; ++i) {
+    double check = 0;
+    for (unsigned j=0 ; j<DIM; ++j) {
+      check += ToPoints(i,j);
+    }
+    if (.00001 < abs(check-ToValues(i,0)) ) {
+      std::cout <<__FILE__<<":"<<__LINE__
+                <<" ToValues:"<<ToValues(i,0)
+                <<" check:"<<check
+                <<" error:"<<abs(check-ToValues(i,0))
+                <<std::endl;
+      success = false;
+    }
+  }
+  timer.stop();
+  stk::diag::printTimersTable(std::cout, timer, 
+        stk::diag::METRICS_CPU_TIME | stk::diag::METRICS_WALL_TIME, false, comm);
+
+  if (success) std::cout <<" Success " <<std::endl;
+  else         std::cout <<" Failure " <<std::endl;
+  return success ? 0 : 1;
 }
