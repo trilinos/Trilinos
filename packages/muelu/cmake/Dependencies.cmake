1e6f9ea83bf7bb0b2944c96e6fafb4043ec75ac8
Julien Cortial <jcortia@sandia.gov>
2013-05-14 18:07:14 -0700

    MueLu: Added optional dependence on ThyraTpetraAdapters
    This package is required to build the forthcoming Thyra wrapper code for
    the Tpetra adapters.

--- packages/muelu/cmake/Dependencies.cmake
+++ packages/muelu/cmake/Dependencies.cmake
@@ -1,6 +1,6 @@
 SET(LIB_REQUIRED_DEP_PACKAGES Teuchos Xpetra)
 SET(LIB_OPTIONAL_DEP_PACKAGES Amesos Amesos2 Epetra EpetraExt KokkosClassic
-                              Ifpack Ifpack2 ML Tpetra Zoltan Zoltan2 Stratimikos)
+                              Ifpack Ifpack2 ML Tpetra Zoltan Zoltan2 Stratimikos ThyraTpetraAdapters)
 SET(TEST_REQUIRED_DEP_PACKAGES )
 SET(TEST_OPTIONAL_DEP_PACKAGES AztecOO Epetra EpetraExt Galeri Tpetra Belos) # TODO: clean up this line
 SET(LIB_REQUIRED_DEP_TPLS BLAS LAPACK)
