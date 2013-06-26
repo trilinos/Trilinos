5d75bbb0692268c930f3c23f4d9119077e1ce15f
Mark Hoemmen <mhoemme@sandia.gov>
2013-06-26 14:58:54 -0600

    Kokkos Compat: Added more Teuchos / Kokkos Array conversion examples.
    Kokkos Compat's test now exercises all the use cases of conversion
    between Teuchos memory management objects and Kokkos Array Views that
    are useful for implementing Tpetra::MultiVector's current interface.
    The test's internal documentation lists for each example the
    corresponding Tpetra::MultiVector methods.
    
    Build/Test Cases Summary
    Enabled Packages: KokkosClassic, KokkosArray, KokkosCompat
    0) MPI_DEBUG => Test case MPI_DEBUG was not run! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_SS => passed: passed=26,notpassed=0 (2.99 min)
    3) SERIAL_RELEASE_SS => passed: passed=24,notpassed=0 (0.29 min)

--- packages/kokkos/compat/test/linkTest.cpp
+++ packages/kokkos/compat/test/linkTest.cpp
@@ -46,6 +46,7 @@
 #include <Teuchos_UnitTestHarness.hpp>
 #include <KokkosCompat_TMM.hpp>
 #include <Teuchos_Array.hpp>
+#include <Kokkos_MultiVector.hpp> // Kokkos Classic
 
 namespace {
 
@@ -70,6 +71,25 @@ namespace {
 
   private:
     KokkosArray::View<double*, Device> vec_;
+  };
+
+  // Custom deallocator for Teuchos::ArrayRCP.  It doesn't actually
+  // deallocate the ArrayRCP's data (which belongs to the View passed
+  // into Deallocator's constructor, not to the ArrayRCP).  Instead,
+  // it just keeps a reference to the View.  That way, the data won't
+  // go away until the View's reference count goes to zero.
+  template<class Scalar, class ViewType>
+  class Deallocator {
+  public:
+    // Constructor takes the View that owns the memory.
+    Deallocator (const ViewType& view) : view_ (view) {}
+
+    // "Deallocation function" doesn't actually deallocate its input
+    // pointer; the View is responsible for deallocation of its
+    // memory.
+    void free (Scalar*) {}
+  private:
+    ViewType view_; // View that owns the memory
   };
 
 } // namespace (anonymous)
@@ -98,10 +118,8 @@ TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, NoInteraction ) {
 }
 
 
-// First test of interaction between Teuchos and Kokkos objects:
-//
-// Get a Teuchos::ArrayView of a KokkosArray::View, 
-// and make sure that it points to the same data.
+// Get a Teuchos::ArrayView of a KokkosArray::View, and make sure that
+// it points to the same data.
 TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, ArrayViewOfView ) {
   typedef Teuchos::Array<double>::size_type size_type;
   typedef KokkosArray::View<double*, KokkosArray::LayoutLeft, KokkosArray::Host> ka_view_type;
@@ -125,13 +143,14 @@ TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, ArrayViewOfView ) {
 }
 
 
-// Second test of interaction between Teuchos and Kokkos objects:
-//
 // Get a KokkosArray::View of a Teuchos::ArrayView, and make sure that
 // it points to the same data.  Thanks to Christian Trott for
 // implementing the necessary functionality (View constructor for
 // certain View specializations, that takes a raw pointer and
 // dimensions) in Kokkos Array.
+//
+// This example will be useful for implementing the
+// Tpetra::MultiVector methods get1dCopy and get2dCopy.
 TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, ViewOfArrayView ) {
   typedef Teuchos::Array<double>::size_type size_type;
   typedef KokkosArray::View<double*, KokkosArray::LayoutLeft, KokkosArray::Host, KokkosArray::MemoryUnmanaged> ka_view_type;
@@ -164,4 +183,209 @@ TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, ViewOfArrayView ) {
   }
 }
 
+
+// Create a 2-D View (LayoutLeft, in Host memory), and then create an
+// ArrayRCP which owns that View (using a custom destructor).  This
+// will be useful for implementing Tpetra::MultiVector's getData,
+// getDataNonConst, get1dView, and get1dViewNonConst methods.
+TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, ArrayRCP1D_of_2DView ) {
+  typedef KokkosArray::View<double**, KokkosArray::LayoutLeft, KokkosArray::Host> ka_view_type;
+
+  const size_t numRows = 75;
+  const size_t numCols = 5;
+  const size_t stride = 100;
+  const size_t ZERO = static_cast<size_t> (0);
+
+  ka_view_type X ("X", stride, numCols);
+  ka_view_type X_view = KokkosArray::subview<ka_view_type> (X, std::make_pair (ZERO, numRows), std::make_pair (ZERO, numCols));
+  TEST_EQUALITY(X_view.dimension_0(), numRows);
+  TEST_EQUALITY(X_view.dimension_1(), numCols);
+
+  // Test that the strides of X_view are correct, for int.  Kokkos
+  // Array templates the stride() method on the integer type of the
+  // input.  I just want to make sure that this works for different
+  // integer types; I'll test int here and size_t (useful for
+  // Kokkos(Classic)::MultiVector) below.
+  {
+    int strides[2];
+    strides[0] = 0;
+    strides[1] = 1;
+    X_view.stride (strides);
+    TEST_EQUALITY_CONST(strides[0], 1); // stride between X_view(i,j) and X_view(i+1,j)
+    // The stride must be at least as given, but can be greater (due to possible padding for alignment).
+    TEST_ASSERT(static_cast<size_t>(strides[1]) >= stride); // stride between X_view(i,j) and X_view(i,j+1)
+  }
+
+  // Test that the strides of X_view are correct, for size_t.
+  {
+    size_t strides[2];
+    strides[0] = static_cast<size_t> (0);
+    strides[1] = static_cast<size_t> (1);
+    X_view.stride (strides);
+    TEST_EQUALITY_CONST(strides[0], static_cast<size_t>(1)); // stride between X_view(i,j) and X_view(i+1,j)
+    // The stride must be at least as given, but can be greater (due to possible padding for alignment).
+    TEST_ASSERT(strides[1] >= stride); // stride between X_view(i,j) and X_view(i,j+1)
+  }
+
+  // Create a nonowning "view" of X's data.  The ArrayRCP has a custom
+  // destructor which simply holds a reference to the View and doesn't
+  // actually deallocate memory.  That way, the ArrayRCP can use the
+  // View's raw pointer, but still defers to the View for memory
+  // management.
+  Teuchos::ArrayRCP<double> Y_values (X.ptr_on_device (), 0, stride*numCols, Deallocator<double, ka_view_type> (X), true);
+  TEST_EQUALITY(Y_values.getRawPtr(), X.ptr_on_device());
+  TEST_EQUALITY(Y_values.getRawPtr(), X_view.ptr_on_device());
+  TEST_EQUALITY(Y_values.size(), stride*numCols);
+}
+
+
+// Create a 2-D View (LayoutLeft, in Host memory), and then create an
+// ArrayRCP<ArrayRCP<double> >, each entry of which owns the
+// corresponding column of that View (using a custom destructor).
+// This will be useful for implementing Tpetra::MultiVector's
+// get2dView and get2dViewNonConst methods.
+TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, ArrayRCP2D_of_2DView ) {
+  // View<double*, LayoutLeft, ...> and View<double*, LayoutRight,
+  // ...> always have unit stride.  Furthermore, subview(X,j) returns
+  // a view of a column for LayoutLeft, and a view of a row for
+  // LayoutRight.  Thus, to support both layouts, each column of the
+  // multivector still needs to be a View<double**, ...>, and we have
+  // to use the subview() overload that takes two ranges of row and
+  // column indices.
+  typedef KokkosArray::View<double**, KokkosArray::LayoutLeft, KokkosArray::Host> ka_view_type;
+
+  const size_t numRows = 75;
+  const size_t numCols = 5;
+  const size_t stride = 100;
+  const size_t ZERO = static_cast<size_t> (0);
+
+  ka_view_type X ("X", stride, numCols);
+  ka_view_type X_view = KokkosArray::subview<ka_view_type> (X, std::make_pair (ZERO, numRows), std::make_pair (ZERO, numCols));
+  TEST_EQUALITY(X_view.dimension_0(), numRows);
+  TEST_EQUALITY(X_view.dimension_1(), numCols);
+
+  // Test that the strides of X_view are correct, for size_t.
+  {
+    size_t strides[2];
+    strides[0] = static_cast<size_t> (0);
+    strides[1] = static_cast<size_t> (0);
+    X_view.stride (strides);
+    TEST_EQUALITY_CONST(strides[0], static_cast<size_t>(1)); // stride between X_view(i,j) and X_view(i+1,j)
+    // The stride must be at least as given, but can be greater (due to possible padding for alignment).
+    TEST_ASSERT(strides[1] >= stride); // stride between X_view(i,j) and X_view(i,j+1)
+  }
+
+  // Make a 2-D "view" (array of arrays) of X_view.  This is how we
+  // will implement Tpetra::MultiVector methods like get2dView.
+  Teuchos::ArrayRCP<Teuchos::ArrayRCP<double> > Y_2D (X_view.dimension_1 ());
+  for (size_t j = 0; j < static_cast<size_t> (X_view.dimension_1 ()); ++j) {
+    ka_view_type X_j = KokkosArray::subview<ka_view_type> (X_view, std::make_pair (ZERO, numRows), std::make_pair (j, j+1));
+    TEST_EQUALITY(static_cast<size_t>(X_j.dimension_0()), numRows);
+    TEST_EQUALITY_CONST(X_j.dimension_1(), 1);
+
+    // Test that the strides of X_j are correct.
+    {
+      size_t strides[2];
+      strides[0] = static_cast<size_t> (0);
+      strides[1] = static_cast<size_t> (1);
+      X_j.stride (strides);
+      TEST_EQUALITY_CONST(strides[0], static_cast<size_t>(1)); // stride between X_j(i,j) and X_j(i+1,j)
+      // Stride between X_j(i,j) and X_j(i,j+1), even though X_j only
+      // has one column.  The stride must be at least as given, but
+      // can be greater (due to possible padding for alignment).
+      TEST_ASSERT(strides[1] >= stride); 
+    }
+
+    // Create a nonowning "view" of X_j's data.  The ArrayRCP has a
+    // custom destructor which simply holds a reference to the View
+    // X_j, and doesn't actually deallocate memory.  That way, the
+    // ArrayRCP can use the View's raw pointer, but still defers to
+    // the View for memory management.
+    Teuchos::ArrayRCP<double> Y_j (X_j.ptr_on_device (), 0, numRows, Deallocator<double, ka_view_type> (X_j), true);
+    Y_2D[j] = Y_j;
+  }
+}
+
+
+// Create a 2-D LayoutLeft Host-memory View, and create a
+// Kokkos::MultiVector (Kokkos Classic) which views its memory and
+// holds a reference to the View.  The latter will ensure that the
+// Kokkos::MultiVector can safely be used beyond the lifetime of the
+// Tpetra::MultiVector (which created the View).
+//
+// We will use this example to implement the Tpetra::MultiVector
+// methods getLocalMV and getLocalMVNonConst methods.
+//
+// Preserving Tpetra's current interface will not require a way to
+// return a View that is an owning (i.e., persisting) view of a
+// Kokkos(Classic)::MultiVector's data.  This is because the porting
+// process will start by changing the internal data storage from
+// Kokkos(Classic)::MultiVector to View.
+TEUCHOS_UNIT_TEST( LinkTeuchosAndKokkosArray, KMV_of_2DView ) {
+  typedef KokkosArray::View<double**, KokkosArray::LayoutLeft, KokkosArray::Host> ka_view_type;
+  typedef Kokkos::MultiVector<double, Kokkos::SerialNode> KMV;
+
+  const size_t numRows = 75;
+  const size_t numCols = 5;
+  const size_t stride = 100;
+  const size_t ZERO = static_cast<size_t> (0);
+
+  ka_view_type X ("X", stride, numCols);
+  ka_view_type X_view = KokkosArray::subview<ka_view_type> (X, std::make_pair (ZERO, numRows), std::make_pair (ZERO, numCols));
+  TEST_EQUALITY(X_view.dimension_0(), numRows);
+  TEST_EQUALITY(X_view.dimension_1(), numCols);
+
+  // Test that the strides of X_view are correct, for int.  Kokkos
+  // Array templates the stride() method on the integer type of the
+  // input.  I just want to make sure that this works for different
+  // integer types; I'll test int here and size_t (useful for
+  // Kokkos(Classic)::MultiVector) below.
+  {
+    int strides[2];
+    strides[0] = 0;
+    strides[1] = 1;
+    X_view.stride (strides);
+    TEST_EQUALITY_CONST(strides[0], 1); // stride between X_view(i,j) and X_view(i+1,j)
+    // The stride must be at least as given, but can be greater (due to possible padding for alignment).
+    TEST_ASSERT(static_cast<size_t>(strides[1]) >= stride); // stride between X_view(i,j) and X_view(i,j+1)
+  }
+
+  // Test that the strides of X_view are correct, for size_t.
+  {
+    size_t strides[2];
+    strides[0] = static_cast<size_t> (0);
+    strides[1] = static_cast<size_t> (1);
+    X_view.stride (strides);
+    TEST_EQUALITY_CONST(strides[0], static_cast<size_t>(1)); // stride between X_view(i,j) and X_view(i+1,j)
+    // The stride must be at least as given, but can be greater (due to possible padding for alignment).
+    TEST_ASSERT(strides[1] >= stride); // stride between X_view(i,j) and X_view(i,j+1)
+  }
+
+  // Create a nonowning "view" of X's data.  The ArrayRCP has a custom
+  // destructor which simply holds a reference to the View and doesn't
+  // actually deallocate memory.  That way, the ArrayRCP can use the
+  // View's raw pointer, but still defers to the View for memory
+  // management.
+  Teuchos::ArrayRCP<double> Y_values (X.ptr_on_device (), 0, stride*numCols, Deallocator<double, ka_view_type> (X), true);
+  TEST_EQUALITY(Y_values.getRawPtr(), X.ptr_on_device());
+  TEST_EQUALITY(Y_values.getRawPtr(), X_view.ptr_on_device());
+  TEST_EQUALITY(Y_values.size(), stride*numCols);
+
+  // Create a Kokkos (Classic) Node for the Kokkos::MultiVector.
+  Teuchos::RCP<Kokkos::SerialNode> node;
+  {
+    Teuchos::ParameterList pl;
+    node = Teuchos::rcp (new Kokkos::SerialNode (pl));
+  }
+  // Create the Kokkos(Classic)::MultiVector.  Initialization takes two steps.
+  KMV Y (node);
+  Y.initializeValues (numRows, numCols, Y_values, stride);
+  
+  // Test that the dimensions, stride, and pointers of Y are correct.
+  TEST_EQUALITY(Y.getNumRows(), numRows);
+  TEST_EQUALITY(Y.getNumCols(), numCols);
+  TEST_EQUALITY(Y.getStride(), stride);
+  TEST_EQUALITY(Y.getValues().getRawPtr(), X.ptr_on_device());
+  TEST_EQUALITY(Y.getValues().getRawPtr(), X_view.ptr_on_device());
+}
 
