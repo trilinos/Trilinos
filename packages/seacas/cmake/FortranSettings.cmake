4b310d9bfbbebc12bbd9055d1dec56499ae36dfc
Brent Perschbacher <bmpersc@sandia.gov>
2013-06-26 10:28:33 -0600

    SEACAS: Fix configure issue when Fortran is disabled
    A conditional checking the value of the Fortran mangling scheme caused a
    configure error when Fortran is disabled because the Fortran mangling scheme
    isn't set. To fix this the variables used are quoted so that when they are empty
    cmake will see an empty string to use for comparison instead of a missing
    parameter.

--- packages/seacas/cmake/FortranSettings.cmake
+++ packages/seacas/cmake/FortranSettings.cmake
@@ -1,10 +1,10 @@
 ADD_DEFINITIONS(-DBuild64)
 
-IF (${FC_FN_UNDERSCORE} STREQUAL "UNDER")
+IF ("${FC_FN_UNDERSCORE}" STREQUAL "UNDER")
   ADD_DEFINITIONS(-DADDC_)
 ENDIF()
 
-IF (${CMAKE_Fortran_COMPILER_ID} MATCHES "GNU")
+IF ("${CMAKE_Fortran_COMPILER_ID}" MATCHES "GNU")
   SET(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -fcray-pointer -fdefault-real-8 -fdefault-integer-8")
 ELSE()
   SET(CMAKE_Fortran_FLAGS "${CMAKE_Fortran_FLAGS} -r8 -i8")
