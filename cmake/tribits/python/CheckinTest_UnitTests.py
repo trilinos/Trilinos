8be4fad307ded4bb8d70393cc9c03fedfee96ad0
Roscoe A. Bartlett <bartlettra@ornl.gov>
2013-06-27 21:39:10 -0400

    Making warning of missing repos a little less verbose
    VERA takes advantage of smooth handling of missing repos so it would be nice
    if it was a little less verbose.
    
    Build/Test Cases Summary
    Enabled Packages: TrilinosFramework
    Disabled Packages: CTrilinos,ForTrilinos,PyTrilinos,Didasko,Mesquite,Phdmesh,Pliris,Claps
    Enabled all Forward Packages
    0) MPI_DEBUG => passed: passed=109,notpassed=0 (2.80 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)

--- cmake/tribits/python/CheckinTest_UnitTests.py
+++ cmake/tribits/python/CheckinTest_UnitTests.py
@@ -765,7 +765,7 @@ class test_TribitsGitRepos(unittest.TestCase):
       GitRepo('preCopyrightTrilinos', 'preCopyrightTrilinos', "GIT", True),
       ]
     consoleRegexMatches = \
-      "WARNING!  Ignoring missing extra repo .MissingRepo. as requested since\n"
+      "WARNING: Ignoring missing extra repo .MissingRepo. as requested since\n"
     consoleRegexNotMatches = \
       "Adding extra Continuous repository MissingRepo"
     test_TribitsGitRepos_run_case(self, testName, inOptions, expectedPass, \
@@ -2787,7 +2787,7 @@ class test_checkin_test(unittest.TestCase):
       \
       True,
       \
-      "WARNING!  Ignoring missing extra repo .MissingRepo. as requested since\n" \
+      "WARNING: Ignoring missing extra repo .MissingRepo. as requested since\n" \
       "Pulling in packages from extra repos: preCopyrightTrilinos ...\n" \
       ,
       mustHaveCheckinTestOut=False
