137919c6acd14cbf861f5ed9de7511b9a0b63140
Alan Williams <william@sandia.gov>
2013-07-01 10:23:40 -0600

    Comment out unit-test that uses KokkosArray.
    There is a build-error for intel-11.1 and since Dan is out of town
    and the solution isn't obvious to the rest of us, Dan suggests
    commenting this out til he gets back.
    
    (From Sierra)

--- packages/stk/stk_exp/unit_tests/util/unit_test_kokkosarray.cpp
+++ packages/stk/stk_exp/unit_tests/util/unit_test_kokkosarray.cpp
@@ -4,21 +4,21 @@
 
 TEST( KokkosArray, basic )
 {
-  using namespace KokkosArray;
-  View<int*[3],Host> a("test",10);
-
-  ASSERT_EQ( 2u, a.rank );
-  ASSERT_EQ( 10u, a.dimension(0) );
-  ASSERT_EQ( 3u, a.dimension(1) );
-
-  for (int i=0; i<10; ++i) {
-  for (int j=0; j<3; ++j) {
-    a(i,j) = i*10 + j;
-  }}
-
-  View<const int*[3],Host> b = a;
-  for (int i=0; i<10; ++i) {
-  for (int j=0; j<3; ++j) {
-    EXPECT_EQ( i*10+j, b(i,j));
-  }}
+//  using namespace KokkosArray;
+//  View<int*[3],Host> a("test",10);
+//
+//  ASSERT_EQ( 2u, a.rank );
+//  ASSERT_EQ( 10u, a.dimension(0) );
+//  ASSERT_EQ( 3u, a.dimension(1) );
+//
+//  for (int i=0; i<10; ++i) {
+//  for (int j=0; j<3; ++j) {
+//    a(i,j) = i*10 + j;
+//  }}
+//
+//  View<const int*[3],Host> b = a;
+//  for (int i=0; i<10; ++i) {
+//  for (int j=0; j<3; ++j) {
+//    EXPECT_EQ( i*10+j, b(i,j));
+//  }}
 }
