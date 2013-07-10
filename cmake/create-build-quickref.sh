626f2cff74ba87805e37704ffa17ab9442f55c52
Roscoe A. Bartlett <bartlettra@ornl.gov>
2013-07-10 11:22:01 -0400

    Direct pass-through of arugments and fixed documentation

--- cmake/create-build-quickref.sh
+++ cmake/create-build-quickref.sh
@@ -6,24 +6,16 @@
 #
 # This script gets run from the base Trilinos source directory as:
 #
-#   ./cmake/generate-build-quickref.sh
+#   ./cmake/create-build-quickref.sh
 #
-# If the programs for rst2html and rst2latex need to be specified differently
-# (such as rst2html.py and rst2latex.py) these can be set as:
+# if any options need to be overridden (for example to point to different
+# versions of rst2thml or rst2latex), they can be passed into this scirpt, for
+# example, as:
 #
-#   env RST2HTML=rst2html.py RST2LATEX=rst2latex.py \
-#        ./cmake/generate-build-quickref.sh
+#   ./cmake/create-build-quickref.sh --generate-html=rst2html \
+#     --generate-latex=rst2latex
 #  
 
-ARGS=""
-
-if [ "${RST2HTML}" != "" ] ; then
-  ARGS="$ARGS --generate-html=${RST2HTML}"
-fi
-
-if [ "${RST2LATEX}" != "" ] ; then
-  ARGS="$ARGS --generate-latex=${RST2LATEX}"
-fi
-#echo "ARGS = $ARGS"
+ARGS=$@
 
 ./cmake/tribits/doc/create-project-build-quickref.py $ARGS
