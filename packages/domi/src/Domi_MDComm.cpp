31cd2a7ae8ea663fa0f25a18fdc24ba473f8c104
Bill Spotz <wfspotz@sandia.gov>
2013-07-10 13:05:44 -0600

    Added Domi::MDComm class to build system
    The MDComm is a multidimensional communicator that requires an
    RCP<const Teuchos::Comm<int> > object for construction.

---
+++ packages/domi/src/Domi_MDComm.cpp
@@ -0,0 +1,206 @@
+// @HEADER
+// ***********************************************************************
+//
+//            Domi: Multidimensional Datastructures Package
+//                 Copyright (2013) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact William F. Spotz (wfspotz@sandia.gov)
+//
+// ***********************************************************************
+// @HEADER
+
+// Domi includes
+#include "Domi_Utils.hpp"
+#include "Domi_MDComm.hpp"
+
+namespace Domi
+{
+
+MDComm::MDComm(const TeuchosComm teuchosComm,
+               const Teuchos::ArrayView< int > & axisSizes) :
+  _teuchosComm(teuchosComm),
+  _axisSizes(regularizeAxisSizes(teuchosComm->getSize(),
+                                 axisSizes.size(),
+                                 axisSizes)),
+  _axisRanks(computeAxisRanks(teuchosComm->getRank(),
+                              _axisSizes)),
+  _axisStrides(computeStrides(_axisSizes,
+                              DEFAULT_ORDER))
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+
+MDComm::MDComm(const TeuchosComm teuchosComm,
+               int numDims) :
+  _teuchosComm(teuchosComm),
+  _axisSizes(regularizeAxisSizes(teuchosComm->getSize(),
+                                 numDims,
+                                 Teuchos::Array< int >())),
+  _axisRanks(computeAxisRanks(teuchosComm->getRank(),
+                              _axisSizes)),
+  _axisStrides(computeStrides(_axisSizes,
+                              DEFAULT_ORDER))
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+
+MDComm::MDComm(const TeuchosComm teuchosComm,
+               int numDims,
+               const Teuchos::ArrayView< int > & axisSizes) :
+  _teuchosComm(teuchosComm),
+  _axisSizes(regularizeAxisSizes(teuchosComm->getSize(),
+                                 numDims,
+                                 axisSizes)),
+  _axisRanks(computeAxisRanks(teuchosComm->getRank(),
+                              _axisSizes)),
+  _axisStrides(computeStrides(_axisSizes,
+                              DEFAULT_ORDER))
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+
+MDComm::MDComm(const Teuchos::RCP< MDComm > parent,
+               const Teuchos::ArrayView< Slice > & slices) :
+  _axisSizes(parent->getNumDims()),    // just allocate the size
+  _axisRanks(parent->_axisRanks),      // will be adjusted below...
+  _axisStrides(parent->_axisStrides)   // copy constructor
+{
+  // Sanity check
+  size_type numDims = parent->getNumDims();
+  if (slices.size() != numDims)
+    throw std::invalid_argument("Length of array of slices does not match "
+                                "number of dimension of parent MDComm");
+  // Make sure the array of Slices we work with is concrete, compute
+  // the _axisSizes and _axisRanks arrays, and compute the size of the
+  // ranks array
+  size_type rankSize = 1;
+  Teuchos::Array< Slice > bounds;
+  for (int axis = 0; axis < numDims; ++axis)
+  {
+    bounds.push_back(slices[axis].bounds(parent->getAxisSize(axis)));
+    _axisSizes[axis] = (bounds[axis].stop() - bounds[axis].start());
+    _axisRanks[axis] -= bounds[axis].start();
+    rankSize *= _axisSizes[axis];
+  }
+  // Compute the ranks of the subcommunicator
+  Teuchos::Array< int > ranks(rankSize);
+  Teuchos::Array< int > mdIndex(numDims, 0);
+  bool done = false;
+  for (int index = 0; not done; ++index)
+  {
+    // Compute ranks[index]
+    int val = 0;
+    for (int axis = 0; axis < numDims; ++axis)
+      val += (mdIndex[axis] + bounds[axis].start()) * _axisStrides[axis];
+    ranks[index] = val;
+    // Increment mdIndex
+    int axis = 0;
+    done = false;
+    while (not done)
+    {
+      ++mdIndex[axis];
+      done = (mdIndex[axis] < _axisSizes[axis]);
+      if (not done)
+      {
+        mdIndex[axis] = 0;
+        ++axis;
+        if (axis >= numDims)
+          done = true;
+      }
+    }
+  }
+  // Set the communicator
+  _teuchosComm =
+    parent->getTeuchosComm()->createSubcommunicator(ranks()).getConst();
+}
+
+////////////////////////////////////////////////////////////////////////
+
+MDComm::~MDComm()
+{
+}
+
+////////////////////////////////////////////////////////////////////////
+
+TeuchosComm
+MDComm::getTeuchosComm() const
+{
+  return _teuchosComm;
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int
+MDComm::getNumDims() const
+{
+  return _axisSizes.size();
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int
+MDComm::getAxisSize(int axis) const
+{
+  return _axisSizes[axis];
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int
+MDComm::getAxisRank(int axis) const
+{
+  return _axisRanks[axis];
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int
+MDComm::getLowerNeighbor(int axis) const
+{
+  if (_axisRanks[axis] == 0) return -1;
+  return _teuchosComm->getRank() - _axisStrides[axis];
+}
+
+////////////////////////////////////////////////////////////////////////
+
+int
+MDComm::getUpperNeighbor(int axis) const
+{
+  if (_axisRanks[axis] == _axisSizes[axis] - 1) return -1;
+  return _teuchosComm->getRank() + _axisStrides[axis];
+}
+
+}    // End namespace Domi
---
+++ packages/domi/src/Domi_MDComm.hpp
@@ -0,0 +1,98 @@
+// @HEADER
+// ***********************************************************************
+//
+//            Domi: Multidimensional Datastructures Package
+//                 Copyright (2013) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact William F. Spotz (wfspotz@sandia.gov)
+//
+// ***********************************************************************
+// @HEADER
+
+#ifndef DOMI_MDCOMM_HPP
+#define DOMI_MDCOMM_HPP
+
+// Teuchos includes
+#include "Teuchos_Comm.hpp"
+#include "Teuchos_Array.hpp"
+#include "Teuchos_ArrayView.hpp"
+
+// Domi includes
+#include "Domi_ConfigDefs.hpp"
+#include "Domi_Slice.hpp"
+
+namespace Domi
+{
+
+typedef Teuchos::RCP< const Teuchos::Comm<int> > TeuchosComm;
+
+class MDComm
+{
+public:
+
+  typedef Domi_Ordinal Ordinal;
+  typedef Teuchos::Array< int >::size_type size_type;
+
+  MDComm(const TeuchosComm teuchosComm,
+         const Teuchos::ArrayView< int > & axisSizes);
+  MDComm(const TeuchosComm teuchosComm,
+         int numDims);
+  MDComm(const TeuchosComm teuchosComm,
+         int numDims,
+         const Teuchos::ArrayView< int > & axisSizes);
+  MDComm(const Teuchos::RCP< MDComm > parent,
+         const Teuchos::ArrayView< Slice > & slices);
+  ~MDComm();
+  TeuchosComm getTeuchosComm() const;
+  int getNumDims() const;
+  int getAxisSize(int axis) const;
+  int getAxisRank(int axis) const;
+  int getLowerNeighbor(int axis) const;
+  int getUpperNeighbor(int axis) const;
+
+protected:
+
+  MDComm();
+
+private:
+
+  TeuchosComm           _teuchosComm;
+  Teuchos::Array< int > _axisSizes;
+  Teuchos::Array< int > _axisRanks;
+  Teuchos::Array< int > _axisStrides;
+
+};
+
+}  // namespace Domi
+
+#endif
