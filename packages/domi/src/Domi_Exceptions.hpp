574c8f0b4c3b16fd29fc49d17c3541286009562f
Bill Spotz <wfspotz@sandia.gov>
2013-07-14 15:36:18 -0600

    Added Domi exceptions

---
+++ packages/domi/src/Domi_Exceptions.hpp
@@ -0,0 +1,68 @@
+// @HEADER
+// ***********************************************************************
+//
+//            Domi: Multidimensional Datastructures Package
+//                 Copyright (2013) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact William F. Spotz (wfspotz@sandia.gov)
+//
+// ***********************************************************************
+// @HEADER
+
+#ifndef DOMI_EXCEPTIONS_HPP
+#define DOMI_EXCEPTIONS_HPP
+
+#include "Domi_ConfigDefs.hpp"
+
+namespace Domi
+{
+
+/** \brief Subcommunicator Error exception type
+ */
+class SubcommunicatorError : public std::domain_error
+{
+public:
+  /** \brief Constructor
+   *
+   * \param op [in] Sohould be the name of the operator or operation
+   *        that is throwing the SubcommunicatorError
+   */
+  SubcommunicatorError(std::string op) :
+    std::domain_error(op + " attempted on a processor that does "
+                      "not belong to the operative subcommunicator")
+  { }
+};
+
+}
+
+#endif
