32dc4c7e4f95116255c13ad75fa9e670d1a1c2a3
Bill Spotz <wfspotz@sandia.gov>
2013-07-22 10:54:13 -0600

    Added unit tests for MDMaps with halos or ghosts (but not yet both)

--- packages/domi/test/MDMap/MDMap_UnitTests.cpp
+++ packages/domi/test/MDMap/MDMap_UnitTests.cpp
@@ -112,8 +112,7 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( MDMap, dimensionsConstructor, T )
   for (int axis = 0; axis < numDims; ++axis)
   {
     int axisRank = mdMap.getAxisRank(axis);
-    if (axisSizes[axis] > 0)
-      TEST_EQUALITY(mdMap.getAxisCommSize(axis), axisSizes[axis]);
+    TEST_EQUALITY(mdMap.getAxisCommSize(axis), axisSizes[axis]);
     TEST_EQUALITY(mdMap.getGlobalDim(axis), dims[axis]);
     TEST_EQUALITY(mdMap.getLocalDim(axis) , localDim  );
     Slice globalBounds = mdMap.getGlobalAxisBounds(axis);
@@ -130,40 +129,155 @@ TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( MDMap, dimensionsConstructor, T )
   }
 }
 
-// TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( MDMap, dimensionsConstructor, T )
-// {
-//   TeuchosCommRCP comm = Teuchos::DefaultComm< int >::getComm();
-//   Domi::splitStringOfIntsWithCommas(axisSizesStr, axisSizes);
-//   MDCommRCP mdComm = Teuchos::rcp(new MDComm(comm, numDims, axisSizes));
+TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( MDMap, halosConstructor, T )
+{
+  TeuchosCommRCP comm = Teuchos::DefaultComm< int >::getComm();
+  // Note: axisSizes from command line should be fully specified
+  Domi::splitStringOfIntsWithCommas(axisSizesStr, axisSizes);
+  MDCommRCP mdComm = Teuchos::rcp(new MDComm(comm, numDims, axisSizes));
 
-//   // Construct dimensions
-//   Array< T > dims(numDims);
-//   for (int axis = 0; axis < numDims; ++axis)
-//     dims[axis] = 20;
+  // Check that the axisSizes are completely specified
+  TEST_EQUALITY(axisSizes.size(), numDims)
+  for (int axis = 0; axis < numDims; ++axis)
+  {
+    TEST_ASSERT(axisSizes[axis] > 0);
+  }
 
-//   // Construct an MDMap
-//   MDMap< T > mdMap(mdComm, dims());
+  // Construct dimensions
+  T localDim = 10;
+  Array< T > dims(numDims);
+  for (int axis = 0; axis < numDims; ++axis)
+    dims[axis] = localDim * mdComm->getAxisSize(axis);
 
-//   // Fill out axisSizes for testing purposes
-//   for (int axis = axisSizes.size(); axis < numDims; ++axis)
-//     axisSizes.push_back(-1);
+  // Construct halos
+  Array< int > halos(numDims);
+  for (int axis = 0; axis < numDims; ++axis)
+    halos[axis] = axis+1;
 
-//   // Perform unit tests
-//   TEST_ASSERT(mdMap.onSubcommunicator());
-//   TEST_EQUALITY(mdMap.getNumDims(), numDims);
-//   for (int axis = 0; axis < numDims; ++axis)
-//   {
-//     if (axisSizes[axis] > 0)
-//       TEST_EQUALITY(mdMap.getAxisCommSize(axis), axisSizes[axis]);
-//   }
-// }
+  // Construct an MDMap
+  MDMap< T > mdMap(mdComm, dims(), halos());
+
+  // Perform unit tests of MDMap as a whole
+  TEST_ASSERT(mdMap.onSubcommunicator());
+  TEST_EQUALITY(mdMap.getNumDims(), numDims);
+  TEST_EQUALITY(mdMap.hasHalos(), comm->getSize() > 1);
+  TEST_EQUALITY(mdMap.getStorageOrder(), Domi::DEFAULT_ORDER);
+
+  // Perform unit tests of MDMap axis quantities
+  for (int axis = 0; axis < numDims; ++axis)
+  {
+    int axisRank = mdMap.getAxisRank(axis);
+    int lowerHalo = 0;
+    if (axisRank > 0) lowerHalo = halos[axis];
+    int upperHalo = 0;
+    if (axisRank < axisSizes[axis]-1) upperHalo = halos[axis];
+    T myDim = localDim + lowerHalo + upperHalo;
+
+    TEST_EQUALITY(mdMap.getAxisCommSize(axis), axisSizes[axis]);
+    TEST_EQUALITY(mdMap.getGlobalDim(axis), dims[axis]);
+    TEST_EQUALITY(mdMap.getLocalDim(axis,true ), myDim   );
+    TEST_EQUALITY(mdMap.getLocalDim(axis,false), localDim);
+    Slice globalBounds = mdMap.getGlobalAxisBounds(axis);
+    TEST_EQUALITY(globalBounds.start(),  axisRank   *localDim);
+    TEST_EQUALITY(globalBounds.stop() , (axisRank+1)*localDim);
+    Slice localBounds  = mdMap.getLocalAxisBounds(axis,true);
+    TEST_EQUALITY_CONST(localBounds.start(), 0);
+    TEST_EQUALITY(localBounds.stop() , myDim);
+    localBounds        = mdMap.getLocalAxisBounds(axis,false);
+    TEST_EQUALITY_CONST(localBounds.start(), lowerHalo);
+    TEST_EQUALITY(localBounds.stop(), lowerHalo+localDim);
+    TEST_EQUALITY_CONST(mdMap.getLowerHalo(axis), lowerHalo);
+    TEST_EQUALITY_CONST(mdMap.getUpperHalo(axis), upperHalo);
+    TEST_EQUALITY_CONST(mdMap.getHaloSize(axis), halos[axis]);
+    TEST_EQUALITY_CONST(mdMap.getGhostSize(axis), 0);
+    TEST_ASSERT(not mdMap.getPeriodic(axis));
+  }
+}
+
+TEUCHOS_UNIT_TEST_TEMPLATE_1_DECL( MDMap, ghostsConstructor, T )
+{
+  TeuchosCommRCP comm = Teuchos::DefaultComm< int >::getComm();
+  // Note: axisSizes from command line should be fully specified
+  Domi::splitStringOfIntsWithCommas(axisSizesStr, axisSizes);
+  MDCommRCP mdComm = Teuchos::rcp(new MDComm(comm, numDims, axisSizes));
+
+  // Check that the axisSizes are completely specified
+  TEST_EQUALITY(axisSizes.size(), numDims)
+  for (int axis = 0; axis < numDims; ++axis)
+  {
+    TEST_ASSERT(axisSizes[axis] > 0);
+  }
+
+  // Construct dimensions
+  T localDim = 10;
+  Array< T > dims(numDims);
+  for (int axis = 0; axis < numDims; ++axis)
+    dims[axis] = localDim * mdComm->getAxisSize(axis);
+
+  // Construct halos
+  Array< int > halos;
+
+  // Construct ghosts
+  Array< int > ghosts(numDims);
+  for (int axis = 0; axis < numDims; ++axis)
+    ghosts[axis] = axis+1;
+
+  // Construct an MDMap
+  MDMap< T > mdMap(mdComm, dims(), halos(), ghosts());
+
+  // Perform unit tests of MDMap as a whole
+  TEST_ASSERT(mdMap.onSubcommunicator());
+  TEST_EQUALITY(mdMap.getNumDims(), numDims);
+  TEST_ASSERT(mdMap.hasHalos());
+  TEST_EQUALITY(mdMap.getStorageOrder(), Domi::DEFAULT_ORDER);
+
+  // Perform unit tests of MDMap axis quantities
+  for (int axis = 0; axis < numDims; ++axis)
+  {
+    int axisRank = mdMap.getAxisRank(axis);
+    int lowerGhost = 0;
+    if (axisRank == 0) lowerGhost = ghosts[axis];
+    int upperGhost = 0;
+    if (axisRank == axisSizes[axis]-1) upperGhost = ghosts[axis];
+    T myDim = localDim + lowerGhost + upperGhost;
+
+    TEST_EQUALITY(mdMap.getAxisCommSize(axis), axisSizes[axis]);
+    TEST_EQUALITY(mdMap.getGlobalDim(axis,true ), dims[axis]+2*ghosts[axis]);
+    TEST_EQUALITY(mdMap.getGlobalDim(axis,false), dims[axis]               );
+    TEST_EQUALITY(mdMap.getLocalDim(axis,true ), myDim   );
+    TEST_EQUALITY(mdMap.getLocalDim(axis,false), localDim);
+    Slice globalBounds = mdMap.getGlobalAxisBounds(axis,false);
+    T myStart = ghosts[axis] +  axisRank    * localDim;
+    T myStop  = ghosts[axis] + (axisRank+1) * localDim;
+    TEST_EQUALITY(globalBounds.start(), myStart);
+    TEST_EQUALITY(globalBounds.stop() , myStop );
+    globalBounds = mdMap.getGlobalAxisBounds(axis,true);
+    if (axisRank == 0                ) myStart -= ghosts[axis];
+    if (axisRank == axisSizes[axis]-1) myStop  += ghosts[axis];
+    TEST_EQUALITY(globalBounds.start(), myStart);
+    TEST_EQUALITY(globalBounds.stop( ), myStop );
+    Slice localBounds  = mdMap.getLocalAxisBounds(axis,true);
+    TEST_EQUALITY_CONST(localBounds.start(), 0);
+    TEST_EQUALITY(localBounds.stop(), myDim);
+    localBounds        = mdMap.getLocalAxisBounds(axis,false);
+    TEST_EQUALITY_CONST(localBounds.start(), lowerGhost);
+    TEST_EQUALITY(localBounds.stop(), lowerGhost+localDim);
+    TEST_EQUALITY_CONST(mdMap.getLowerHalo(axis), lowerGhost);
+    TEST_EQUALITY_CONST(mdMap.getUpperHalo(axis), upperGhost);
+    TEST_EQUALITY_CONST(mdMap.getHaloSize(axis), 0);
+    TEST_EQUALITY_CONST(mdMap.getGhostSize(axis), ghosts[axis]);
+    TEST_ASSERT(not mdMap.getPeriodic(axis));
+  }
+}
 
 //
 // Instantiations
 //
 
 #define UNIT_TEST_GROUP( T ) \
-  TEUCHOS_UNIT_TEST_TEMPLATE_1_INSTANT( MDMap, dimensionsConstructor, T )
+  TEUCHOS_UNIT_TEST_TEMPLATE_1_INSTANT( MDMap, dimensionsConstructor, T ) \
+  TEUCHOS_UNIT_TEST_TEMPLATE_1_INSTANT( MDMap, halosConstructor, T ) \
+  TEUCHOS_UNIT_TEST_TEMPLATE_1_INSTANT( MDMap, ghostsConstructor, T )
 
 UNIT_TEST_GROUP(int)
 #if 0
