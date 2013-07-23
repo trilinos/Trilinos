ca253b87407b92feef4fd4ecc72d3759255b29b6
Eric C. Cyr <eccyr@sandia.gov>
2013-07-23 15:47:36 -0600

    Teko: Adding an identity "Preconditioner" for testing and rapid development
    All this does is inject as the approximat inverse an identity matrix.

---
+++ packages/teko/src/Teko_IdentityPreconditionerFactory.hpp
@@ -0,0 +1,94 @@
+/*
+// @HEADER
+// 
+// ***********************************************************************
+// 
+//      Teko: A package for block and physics based preconditioning
+//                  Copyright 2010 Sandia Corporation 
+//  
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//  
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//  
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//  
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//  
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission. 
+//  
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//  
+// Questions? Contact Eric C. Cyr (eccyr@sandia.gov)
+// 
+// ***********************************************************************
+// 
+// @HEADER
+
+*/
+
+#ifndef __Teko_IdentityPreconditionerFactory_hpp__
+#define __Teko_IdentityPreconditionerFactory_hpp__
+
+#include "Teuchos_RCP.hpp"
+
+#include "Teko_BlockPreconditionerFactory.hpp"
+#include "Teko_BlockInvDiagonalStrategy.hpp"
+
+namespace Teko {
+
+/** Uses a scaled identity operator as the approximate inverse.
+  * This is a last resort!
+  *
+  \code
+       <Parameter name="Type" type="string" value="Identity"/>
+       <Parameter name="Scaling" type="double" value="<Some value>"/>
+  \endcode
+  */
+class IdentityPreconditionerFactory : public PreconditionerFactory {
+   public:
+      //! @name Constructors.
+      //@{
+
+      /** Build an empty Identity preconditioner factory.
+        */
+      IdentityPreconditionerFactory();
+
+      //@}
+
+      /** \brief Create the Identity preconditioner operator.
+        *
+        * This method breaks apart the BlockLinearOp and builds a block
+        * diagonal preconditioner. The inverse of the diagonals are specified
+        * by the BlockInvDiagonalStrategy object.
+        */
+      LinearOp buildPreconditionerOperator(LinearOp & lo,PreconditionerState & state) const;
+ 
+   protected: 
+      //! some members
+      double scaling_;
+
+      //! Initialize from a parameter list
+      virtual void initializeFromParameterList(const Teuchos::ParameterList & pl);
+};
+
+} // end namespace Teko
+
+#endif
