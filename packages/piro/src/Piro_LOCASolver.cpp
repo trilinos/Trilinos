563e49f0f94fee252c62b762ad32a79e4692511a
Julien Cortial <jcortia@sandia.gov>
2013-07-29 12:39:36 -0700

    Piro: Enabled param names from model in LOCASolver
    The implementation now correctly handles three cases described in the
    documentation of Thyra::ModelEvaluator::get_p_names, namely:
    1) No name available (default behavior)
    2) One unique name shared by all entries in the vector
    3) A name for each entry.
    
    When no name exists, each entry is labeled "Parameter k" where k denotes
    the corresponding index in the vector.

--- packages/piro/src/Piro_LOCASolver.cpp
+++ packages/piro/src/Piro_LOCASolver.cpp
@@ -55,3 +55,36 @@ template Teuchos::RCP<LOCASolver<double> > observedLocaSolver(
     const Teuchos::RCP<Piro::ObserverBase<double> > &observer);
 
 } // namespace Piro
+
+
+#include "Teuchos_OrdinalTraits.hpp"
+#include "Teuchos_toString.hpp"
+
+Piro::Detail::ModelEvaluatorParamName::ModelEvaluatorParamName(
+    const Teuchos::RCP<const Teuchos::Array<std::string> > &p_names) :
+  p_names_(p_names)
+{
+  if (Teuchos::is_null(p_names_)) {
+    type_ = Default;
+  } else if (p_names_->size() == Teuchos::OrdinalTraits<Teuchos_Ordinal>::one()) {
+    type_ = OneShared;
+  } else {
+    type_ = FullList;
+  }
+}
+
+std::string
+Piro::Detail::ModelEvaluatorParamName::operator()(Teuchos_Ordinal k) const
+{
+  switch (type_) {
+    case Default:
+      return "Parameter " + Teuchos::toString(k);
+    case OneShared:
+      return p_names_->front();
+    case FullList:
+      return (*p_names_)[k];
+  }
+
+  TEUCHOS_TEST_FOR_EXCEPT(true);
+  return std::string();
+}
