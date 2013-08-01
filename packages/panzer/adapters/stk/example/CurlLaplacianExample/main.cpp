651f91cad0dbc62a6045d36856f980335e270893
Roscoe A. Bartlett <bartlettra@ornl.gov>
2013-08-01 12:08:31 -0400

    Fixing lingering std:: problems from Epetra_ConfigDefs.h cleanup (Trilinos #5945)
    While trying to test downstream packages from ThyraCore for support for empty
    processes, I came across this.  It seems that Panzer is not being tested in
    Trilinos?

--- packages/panzer/adapters/stk/example/CurlLaplacianExample/main.cpp
+++ packages/panzer/adapters/stk/example/CurlLaplacianExample/main.cpp
@@ -144,6 +144,7 @@ void solveTpetraSystem(panzer::LinearObjContainer & container);
 // calls MPI_Init and MPI_Finalize
 int main(int argc,char * argv[])
 {
+   using std::endl; 
    using Teuchos::RCP;
    using Teuchos::rcp_dynamic_cast;
    using panzer::StrPureBasisPair;
