00dd0cc3764d6d6c2ca66947baf385fdca491e86
Chetan Jhurani <jhurani@txcorp.com>
2013-08-10 16:36:55 -0600

    Trilinoscouplings: Fix "using std::" backwards compatibility issue
    Bug 5945 required fixing Epetra_config header by removing
    "using std::ostream" and other such declarations.  This
    change is to fix examples and tests so that they do not
    rely on presence of such symbols imported into global namespace.

--- packages/trilinoscouplings/examples/ml/NonlinML/Problem_Interface.cpp
+++ packages/trilinoscouplings/examples/ml/NonlinML/Problem_Interface.cpp
@@ -65,8 +65,8 @@ bool Problem_Interface::computeJacobian(const Epetra_Vector& x, Epetra_Operator&
 {
   Epetra_RowMatrix* Jacobian = dynamic_cast<Epetra_RowMatrix*>(&Jac);
   if (Jacobian == NULL) {
-    cout << "ERROR: Problem_Interface::computeJacobian() - The supplied"
-	 << "Epetra_Operator is NOT an Epetra_RowMatrix!" << endl;
+    std::cout << "ERROR: Problem_Interface::computeJacobian() - The supplied"
+	 << "Epetra_Operator is NOT an Epetra_RowMatrix!" << std::endl;
     throw -1;
   }
   return problem.evaluate(MATRIX_ONLY, &x, NULL, Jacobian);
