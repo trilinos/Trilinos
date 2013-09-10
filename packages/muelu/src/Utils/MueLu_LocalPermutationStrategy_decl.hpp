d0498d71af898a969a5fd53caab87b45aa2aebc3
Tobias Wiesner <wiesner@descartes.lnm.mw.tum.de>
2013-09-10 15:34:02 +0200

    MueLu: remove some debug output

--- packages/muelu/src/Utils/MueLu_LocalPermutationStrategy_decl.hpp
+++ packages/muelu/src/Utils/MueLu_LocalPermutationStrategy_decl.hpp
@@ -19,6 +19,10 @@
 #include "MueLu_Level.hpp"
 #include "MueLu_BaseClass.hpp"
 
+// MPI helper
+#define sumAll(rcpComm, in, out)                                        \
+  Teuchos::reduceAll(*rcpComm, Teuchos::REDUCE_SUM, in, Teuchos::outArg(out));
+
 namespace MueLu {
 
   //! @brief Local permutation strategy
