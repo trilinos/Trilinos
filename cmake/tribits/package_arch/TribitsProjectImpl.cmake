afb55605830de014186fe883d6a46ca5aa43b69d
Roscoe A. Bartlett <bartlettra@ornl.gov>
2013-09-19 20:42:20 -0400

    Finished basic refactoring breaking out general TriBITS functionality (Trilinos #5970)
    This just maintains initial functionality.

--- cmake/tribits/package_arch/TribitsProjectImpl.cmake
+++ cmake/tribits/package_arch/TribitsProjectImpl.cmake
@@ -281,7 +281,8 @@ MACRO(TRIBITS_PROJECT_IMPL)
   # K) Setup for packaging and distribution
   #
 
-  # Loop through the Repositories and run their callback functions.
+  # K.1) Loop through the Repositories and run their callback functions.
+
   FOREACH(REPO ${${PROJECT_NAME}_ALL_REPOSITORIES})
     TRIBITS_GET_REPO_NAME_DIR(${REPO}  REPO_NAME  REPO_DIR)
     IF (${PROJECT_NAME}_VERBOSE_CONFIGURE)
@@ -289,7 +290,98 @@ MACRO(TRIBITS_PROJECT_IMPL)
     ENDIF()
     TRIBITS_REPOSITORY_DEFINE_PACKAGING_RUNNER(${REPO_NAME})
   ENDFOREACH()
-  
+   
+  # K.2) Removing any packages not enabled from the tarball
+
+  TRIBITS_GET_ENABLED_LIST_LIST(
+    ${PROJECT_NAME}_PACKAGES ${PROJECT_NAME}
+    OFF  # ENABLED_FLAG
+    TRUE  # INCLUDE_EMPTY 
+    NON_ENABLED_PACKAGES  NUM_NON_ENABLED)
+
+  SET(PACKAGE_IDX 0)
+  FOREACH(TRIBITS_PACKAGE ${NON_ENABLED_PACKAGES})
+
+    # Determine if this is a package to not ignore
+    FIND_LIST_ELEMENT(TRIBITS_CPACK_PACKAGES_TO_NOT_IGNORE
+       ${TRIBITS_PACKAGE}  TRIBITS_PACKAGE_DONT_IGNORE)
+
+    IF (NOT TRIBITS_PACKAGE_DONT_IGNORE)
+
+      LIST(GET ${PROJECT_NAME}_PACKAGE_DIRS ${PACKAGE_IDX} PACKAGE_DIR)
+      
+      # Checking if we have a relative path to the package's files. Since the
+      # exclude is a regular expression any "../" will be interpretted as <any
+      # char><any char>/ which would never match the package's actual
+      # directory. There isn't a direct way in cmake to convert a relative
+      # path into an absolute path with string operations so as a way of
+      # making sure that we get the correct path of the package we use a
+      # find_path for the CMakeLists.txt file for the package. Since the
+      # package has to have this file to work correctly it should be
+      # guaranteed to be there.
+      STRING(REGEX MATCH "[.][.]/" IS_RELATIVE_PATH ${PACKAGE_DIR})
+      IF("${IS_RELATIVE_PATH}" STREQUAL "")
+        SET(CPACK_SOURCE_IGNORE_FILES ${PROJECT_SOURCE_DIR}/${PACKAGE_DIR}
+          ${CPACK_SOURCE_IGNORE_FILES})
+      ELSE()
+        FIND_PATH(ABSOLUTE_PATH  CMakeLists.txt  PATHS 
+          ${PROJECT_SOURCE_DIR}/${PACKAGE_DIR} NO_DEFAULT_PATH)
+        IF("${ABSOLUTE_PATH}" STREQUAL "ABSOLUTE_PATH-NOTFOUND")
+          MESSAGE(AUTHOR_WARNING "Relative path found for disabled package"
+            " ${TRIBITS_PACKAGE} but package was missing a CMakeLists.txt file."
+            " This disabled package will likely not be excluded from a source release")
+        ENDIF()
+        SET(CPACK_SOURCE_IGNORE_FILES ${ABSOLUTE_PATH} ${CPACK_SOURCE_IGNORE_FILES})
+      ENDIF()
+    ENDIF()
+
+    MATH(EXPR PACKAGE_IDX "${PACKAGE_IDX}+1")
+
+  ENDFOREACH()
+
+  IF(${PROJECT_NAME}_VERBOSE_CONFIGURE OR
+    ${PROJECT_NAME}_DUMP_CPACK_SOURCE_IGNORE_FILES
+    )
+    MESSAGE("Exclude files when building source packages")
+    FOREACH(item ${CPACK_SOURCE_IGNORE_FILES})
+      MESSAGE(${item})
+    ENDFOREACH()
+  ENDIF()
+
+  # K.3) Set up install component dependencies
+
+  TRIBITS_GET_ENABLED_LIST_LIST(
+    ${PROJECT_NAME}_PACKAGES  ${PROJECT_NAME}
+    ON  # ENABLED_FLAG
+    FALSE  # INCLUDE_EMPTY 
+    ENABLED_PACKAGES NUM_ENABLED)
+  #message("ENABLED PACKAGES: ${ENABLED_PACKAGES} ${NUM_ENABLED}")
+
+  FOREACH(PKG ${ENABLED_PACKAGES})
+    IF(NOT "${${PKG}_LIB_REQUIRED_DEP_PACKAGES}" STREQUAL "")
+        string(TOUPPER ${PKG} UPPER_PKG)
+        #message("${UPPER_PKG} depends on : ${${PKG}_LIB_REQUIRED_DEP_PACKAGES}")
+        SET(CPACK_COMPONENT_${UPPER_PKG}_DEPENDS ${${PKG}_LIB_REQUIRED_DEP_PACKAGES})
+    ENDIF()
+    #message("${PKG} depends on : ${${PKG}_LIB_REQUIRED_DEP_PACKAGES}")
+  ENDFOREACH()
+
+  # K.4) Resetting the name to avoid overwriting registery keys when installing
+
+  IF(WIN32)
+    SET(CPACK_PACKAGE_NAME "${CPACK_PACKAGE_NAME}-${${PROJECT_NAME}_VERSION}")
+    IF (TPL_ENABLE_MPI)
+      SET(CPACK_PACKAGE_NAME "${CPACK_PACKAGE_NAME}-mpi")
+    ELSE ()
+      SET(CPACK_PACKAGE_NAME "${CPACK_PACKAGE_NAME}-serial")
+    ENDIF()
+    SET(CPACK_GENERATOR "NSIS")
+    SET(CPACK_NSIS_MODIFY_PATH OFF)
+  ENDIF()
+ 
+  # K.5) Finally process with CPack
+  INCLUDE(CPack)
+
   
   #
   # L) Install-related commands
