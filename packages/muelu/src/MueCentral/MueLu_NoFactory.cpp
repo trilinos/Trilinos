6e725cec4ffc2bac45a601f74b07b303fc37075b
Andrey Prokopenko <aprokop@sandia.gov>
2013-09-25 15:00:03 -0600

    MueLu: NoFactory: cleanup

--- packages/muelu/src/MueCentral/MueLu_NoFactory.cpp
+++ packages/muelu/src/MueCentral/MueLu_NoFactory.cpp
@@ -52,27 +52,7 @@
 
 #include "MueLu_NoFactory.hpp"
 
-#include "MueLu_Exceptions.hpp"
-
 namespace MueLu {
-
-  NoFactory::NoFactory() { }
-
-  NoFactory::~NoFactory() { }
-
-  void NoFactory::CallBuild(Level & requestedLevel) const {
-    TEUCHOS_TEST_FOR_EXCEPTION(true, Exceptions::RuntimeError, "MueLu::NoFactory::Build(): this method cannot be called.");
-  }
-
-  void NoFactory::CallDeclareInput(Level & requestedLevel) const {  }
-
-  const RCP<const NoFactory> NoFactory::getRCP() {
-    if(noFactory_ == Teuchos::null) {
-      noFactory_ = rcp(new NoFactory());
-    }
-
-    return noFactory_;
-  }
 
   const NoFactory* NoFactory::get() {
     return getRCP().get();
