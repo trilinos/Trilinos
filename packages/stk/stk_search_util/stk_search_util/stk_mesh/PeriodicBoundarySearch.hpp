1a3d46ffa5b1043db27ea66598309d49bc6c12c3
Jonathan Clausen <jclause@sandia.gov>
2013-10-01 16:44:02 -0600

    stk_search_util: add periodic boundary search
    (From Sierra)

---
+++ packages/stk/stk_search_util/stk_search_util/stk_mesh/PeriodicBoundarySearch.hpp
@@ -0,0 +1,261 @@
+#ifndef STK_SEARCH_UTIL_STK_MESH_PERIODIC_BOUNDARY_SEARCH_HPP
+#define STK_SEARCH_UTIL_STK_MESH_PERIODIC_BOUNDARY_SEARCH_HPP
+
+#include <stk_search/CoarseSearch.hpp>
+#include <stk_search/BoundingBox.hpp>
+#include <stk_search/IdentProc.hpp>
+#include <stk_mesh/base/GetBuckets.hpp>
+#include <stk_mesh/base/GetEntities.hpp>
+#include <stk_mesh/base/FieldParallel.hpp>
+#include <stk_util/parallel/ParallelReduce.hpp>
+
+namespace stk { namespace mesh {
+
+struct GetCoordiantes;
+
+template <typename CoordinateFunctor>
+class PeriodicBoundarySearch
+{
+public:
+  typedef double Scalar;
+  typedef std::vector<std::pair<stk::mesh::Selector, stk::mesh::Selector> > SelectorPairVector;
+  typedef stk::search::ident::IdentProc<stk::mesh::EntityKey,unsigned> SearchId;
+  typedef stk::search::box::SphereBoundingBox<SearchId,Scalar,3> AABB;
+  typedef std::vector<AABB> AABBVector;
+  typedef std::vector<std::pair<SearchId,SearchId> > SearchPairVector;
+
+  enum CoordinatesTransform {
+    LINEAR_TRANSLATION
+  };
+
+  PeriodicBoundarySearch( stk::mesh::BulkData & bulk_data,
+                          const CoordinateFunctor & getCoordinates )
+    : m_bulk_data(bulk_data),
+      m_get_coordinates(getCoordinates),
+      m_periodic_pairs(),
+      m_transform_coords(),
+      m_search_results(),
+      m_periodic_ghosts(NULL)
+  {}
+
+  const SearchPairVector & get_pairs() const { return m_search_results; }
+
+  void find_periodic_nodes(stk::ParallelMachine parallel)
+  {
+    AABBVector side_1_vector, side_2_vector;
+    stk::mesh::Selector & side1 = m_periodic_pairs[0].first;
+    stk::mesh::Selector & side2 = m_periodic_pairs[0].second;
+    const CoordinatesTransform transform_method = m_transform_coords[0];
+
+    m_search_results.clear();
+
+    double local_centroid[6] = {0};
+    size_t local_node_count[2];
+
+    local_node_count[0] =
+        populate_search_vector(  side1
+                                ,side_1_vector
+                                ,local_centroid );
+
+    local_node_count[1] =
+        populate_search_vector( side2
+                                ,side_2_vector
+                                ,local_centroid + 3 );
+
+
+    switch (transform_method)
+    {
+      case LINEAR_TRANSLATION:
+        translate_coordinates(parallel, local_node_count, local_centroid, side_1_vector, side_2_vector);
+        break;
+      default:
+        ThrowRequireMsg(false, "Periodic transform method doesn't exist");
+        break;
+    }
+
+    stk::search::FactoryOrder order;
+    order.m_communicator = parallel;
+    stk::search::coarse_search( m_search_results
+        ,side_2_vector
+        ,side_1_vector
+        ,order
+    );
+
+  }
+
+  size_t size() const { return m_search_results.size();}
+
+  std::pair<stk::mesh::Entity, stk::mesh::Entity> operator[](size_t i) const
+  {
+    return std::make_pair(m_bulk_data.get_entity(m_search_results[i].first.ident),
+        m_bulk_data.get_entity(m_search_results[i].second.ident));
+  }
+
+  void add_periodic_pair(const stk::mesh::Selector & domain,
+      const stk::mesh::Selector & range,
+      CoordinatesTransform transform = LINEAR_TRANSLATION)
+  {
+    m_periodic_pairs.push_back(std::make_pair(domain, range));
+    m_transform_coords.push_back(transform);
+  }
+
+  const stk::mesh::Ghosting & get_ghosting() const
+  {
+    ThrowRequire(m_periodic_ghosts);
+    return *m_periodic_ghosts;
+  }
+
+  void create_ghosting(const std::string & name)
+  {
+    ThrowRequire(m_bulk_data.synchronized_state() == m_bulk_data.MODIFIABLE);
+    const int parallel_rank = m_bulk_data.parallel_rank();
+    size_t num_constraints = 0;
+    std::vector<stk::mesh::EntityProc> send_nodes;
+    for (size_t i=0, size=m_search_results.size(); i<size; ++i) {
+        stk::mesh::Entity domain_node = m_bulk_data.get_entity(m_search_results[i].first.ident);
+        int domain_proc = m_search_results[i].first.proc;
+        stk::mesh::Entity range_node = m_bulk_data.get_entity(m_search_results[i].second.ident);
+        int range_proc = m_search_results[i].second.proc;
+
+        if (parallel_rank == domain_proc) ++num_constraints;
+
+        if ((parallel_rank != domain_proc) && (parallel_rank == range_proc)) {
+          send_nodes.push_back(stk::mesh::EntityProc(range_node, domain_proc));
+        }
+        else if ((parallel_rank == domain_proc) && (parallel_rank != range_proc)) {
+          send_nodes.push_back(stk::mesh::EntityProc(domain_node, range_proc));
+        }
+        ThrowAssert((parallel_rank == domain_proc) || (parallel_rank == range_proc));
+    }
+
+    m_periodic_ghosts = &m_bulk_data.create_ghosting(name);
+    m_bulk_data.change_ghosting(*m_periodic_ghosts, send_nodes);
+  }
+
+private:
+  stk::mesh::BulkData & m_bulk_data;
+  CoordinateFunctor m_get_coordinates;
+  SelectorPairVector m_periodic_pairs;
+  std::vector<CoordinatesTransform>  m_transform_coords;
+  SearchPairVector m_search_results;
+  stk::mesh::Ghosting * m_periodic_ghosts;
+
+  size_t populate_search_vector(stk::mesh::Selector side_selector
+                              , AABBVector & aabb_vector
+                              , double * centroid
+                             )
+  {
+    const int spatial_dimension = m_bulk_data.mesh_meta_data().spatial_dimension();
+    const double radius = 1e-10;
+    const unsigned parallel_rank = m_bulk_data.parallel_rank();
+
+    stk::mesh::BucketVector buckets;
+
+    stk::mesh::get_buckets( side_selector
+                            ,m_bulk_data.buckets(stk::topology::NODE_RANK)
+                            ,buckets
+                            );
+
+    size_t num_nodes = 0;
+
+    for (size_t bindex = 0, num_buckets = buckets.size(); bindex < num_buckets; ++bindex) {
+      stk::mesh::Bucket & b = *buckets[bindex];
+      double coords[3];
+      for (size_t ord =0, num_entities = b.size(); ord < num_entities; ++ord) {
+        ++num_nodes;
+        m_get_coordinates(b[ord], coords);
+        SearchId search_id( m_bulk_data.entity_key(b[ord]), parallel_rank);
+        aabb_vector.push_back(AABB( coords, radius, search_id));
+        for (int i=0; i<spatial_dimension; ++i) {
+          centroid[i] += coords[i];
+        }
+      }
+    }
+    return num_nodes;
+  }
+
+  void translate_coordinates(stk::ParallelMachine parallel,
+      size_t local_node_count[2],
+      double local_centroid[6],
+      AABBVector & side_1_vector,
+      AABBVector & side_2_vector) const
+  {
+    //do coordinate tranformation here
+    size_t global_node_count[2] =
+    { };
+    double global_centroid[6] =
+    { };
+    stk::all_reduce_sum(parallel, local_node_count, global_node_count, 2);
+    stk::all_reduce_sum(parallel, local_centroid, global_centroid, 6);
+    double translate[3];
+    for (int i = 0; i < 3; ++i)
+    {
+      translate[i] = (global_centroid[i + 3] / global_node_count[1]) - (global_centroid[i] / global_node_count[0]);
+    }
+    // translate domain to range, i.e. master to slave
+    for (size_t i = 0, size = side_1_vector.size(); i < size; ++i)
+    {
+      for (int j = 0; j < 3; ++j)
+      {
+        side_1_vector[i].center[j] += translate[j];
+      }
+    }
+  }
+
+};
+
+template <class CoordFieldType, typename Scalar = double, unsigned SpatialDimension = 3>
+struct GetCoordinates
+{
+  typedef void result_type;
+  GetCoordinates(stk::mesh::BulkData & bulk_data, CoordFieldType & coords_field)
+    : m_bulk_data(bulk_data),
+      m_coords_field(coords_field)
+  {}
+
+  void operator()(stk::mesh::Entity e, Scalar * coords) const
+  {
+    const double * const temp_coords = m_bulk_data.field_data(m_coords_field, e);
+    for (unsigned i = 0; i < SpatialDimension; ++i) {
+      coords[i] = temp_coords[i];
+    }
+  }
+
+  stk::mesh::BulkData & m_bulk_data;
+  CoordFieldType & m_coords_field;
+};
+
+template <class ModelCoordFieldType, class DispCoordFieldType, typename Scalar = double, unsigned SpatialDimension = 3>
+struct GetDisplacedCoordinates
+{
+  typedef void result_type;
+  GetDisplacedCoordinates(stk::mesh::BulkData & bulk_data, ModelCoordFieldType & model_coord_field, DispCoordFieldType & disp_coord_field)
+    : m_bulk_data(bulk_data),
+      m_model_coord_field(model_coord_field),
+      m_disp_coord_field(disp_coord_field)
+  {}
+
+  void operator()(stk::mesh::Entity e, Scalar * coords) const
+  {
+    const double * const temp_model_coords = m_bulk_data.field_data(m_model_coord_field, e);
+    const double * const temp_disp_coords = m_bulk_data.field_data(m_disp_coord_field, e);
+    for (unsigned i = 0; i < SpatialDimension; ++i) {
+      coords[i] = temp_model_coords[i] + temp_disp_coords[i];
+    }
+  }
+
+  stk::mesh::BulkData & m_bulk_data;
+  ModelCoordFieldType & m_model_coord_field;
+  DispCoordFieldType & m_disp_coord_field;
+};
+
+
+
+
+}}//namespace stk::mesh
+
+#endif /*STK_SEARCH_UTIL_STK_MESH_PERIODIC_BOUNDARY_SEARCH_HPP*/
+
+
+
+
--- packages/stk/stk_search_util/unit_tests/UnitTestSearchWithPeriodicBC.cpp
+++ packages/stk/stk_search_util/unit_tests/UnitTestSearchWithPeriodicBC.cpp
@@ -1,59 +1,15 @@
 #include <stk_util/unit_test_support/stk_utest_macros.hpp>
-#include <stk_search/CoarseSearch.hpp>
-#include <stk_search/BoundingBox.hpp>
-#include <stk_search/IdentProc.hpp>
-#include <stk_mesh/base/GetBuckets.hpp>
-#include <stk_mesh/base/GetEntities.hpp>
-#include <stk_mesh/base/FieldParallel.hpp>
-#include <stk_util/parallel/ParallelReduce.hpp>
 #include <stk_mesh/fixtures/HexFixture.hpp>
+#include <stk_search_util/stk_mesh/PeriodicBoundarySearch.hpp>
 #include <iostream>
 #include <iomanip>
 #include <sstream>
 
-typedef double Scalar;
-typedef stk::search::ident::IdentProc<stk::mesh::EntityId,unsigned> SearchId;
-typedef stk::search::box::SphereBoundingBox<SearchId,Scalar,3> AABB;
-typedef std::vector<AABB> AABBVector;
-typedef std::vector<std::pair<SearchId,SearchId> > SearchPairVector;
 typedef stk::mesh::fixtures::HexFixture::CoordFieldType CoordFieldType;
 
 namespace {
 
-size_t populate_search_vector( stk::mesh::BulkData & bulk_data
-                              ,CoordFieldType & coords_field
-                              ,stk::mesh::Selector side_selector
-                              ,AABBVector & aabb_vector
-                              ,double * centroid
-                             )
-{
-  const int spatial_dimension = bulk_data.mesh_meta_data().spatial_dimension();
-  const double radius = 1e-10;
-  const unsigned parallel_rank = bulk_data.parallel_rank();
-
-  stk::mesh::BucketVector buckets;
-  stk::mesh::get_buckets( side_selector
-                         ,bulk_data.buckets(stk::topology::NODE_RANK)
-                         ,buckets
-                        );
-
-  size_t num_nodes = 0;
-
-  for (size_t bindex = 0, num_buckets = buckets.size(); bindex < num_buckets; ++bindex) {
-    stk::mesh::Bucket & b = *buckets[bindex];
-    for (size_t ord =0, num_entities = b.size(); ord < num_entities; ++ord) {
-      ++num_nodes;
-      const double * const coords = bulk_data.field_data(coords_field, b, ord);
-      SearchId search_id( bulk_data.identifier(b[ord]), parallel_rank);
-      aabb_vector.push_back(AABB( coords, radius, search_id));
-      for (int i=0; i<spatial_dimension; ++i) {
-        centroid[i] += coords[i];
-      }
-    }
-  }
-  return num_nodes;
-}
-
+template<typename SearchPairVector>
 void check_gold( const SearchPairVector & search_results )
   // check search result
 {
@@ -80,71 +36,13 @@ void check_gold( const SearchPairVector & search_results )
   gold.push_back(std::make_pair(61,64));
 
   for (size_t i=0, size=search_results.size(); i<size; ++i) {
-    stk::mesh::EntityId domain_node = search_results[i].first.ident;
-    stk::mesh::EntityId range_node = search_results[i].second.ident;
+    stk::mesh::EntityId domain_node = search_results[i].first.ident.id();
+    stk::mesh::EntityId range_node = search_results[i].second.ident.id();
 
     EXPECT_TRUE((std::lower_bound(gold.begin(),gold.end(),std::make_pair(domain_node,range_node))) != gold.end());
   }
 }
 
-void find_periodic_nodes( stk::mesh::BulkData & bulk_data
-                         ,stk::ParallelMachine parallel
-                         ,CoordFieldType & coords_field
-                         ,stk::mesh::Selector side1
-                         ,stk::mesh::Selector side2
-                         ,SearchPairVector & search_results
-                        )
-{
-  AABBVector side_1_vector, side_2_vector;
-
-  double local_centroid[6] = {0};
-  size_t local_node_count[2];
-
-  local_node_count[0] =
-    populate_search_vector( bulk_data
-                           ,coords_field
-                           ,side1
-                           ,side_1_vector
-                           ,local_centroid
-                          );
-
-  local_node_count[1] =
-    populate_search_vector( bulk_data
-                           ,coords_field
-                           ,side2
-                           ,side_2_vector
-                           ,local_centroid + 3
-                           );
-
-  size_t global_node_count[2] = {0};
-  double global_centroid[6] = {0};
-
-  stk::all_reduce_sum( parallel, local_node_count, global_node_count, 2);
-  stk::all_reduce_sum( parallel, local_centroid, global_centroid, 6);
-
-  // TODO pass in translation method...
-  double translate[3];
-  for (int i=0; i<3; ++i) {
-    translate[i] =   (global_centroid[i+3]/global_node_count[1])
-                   - (global_centroid[i]/global_node_count[0]);
-  }
-
-  // translate domain to range, i.e. master to slave
-  for(size_t i=0, size=side_1_vector.size(); i<size; ++i) {
-    for (int j=0; j<3; ++j) {
-      side_1_vector[i].center[j] += translate[j];
-    }
-  }
-
-  stk::search::FactoryOrder order;
-  order.m_communicator = parallel;
-  stk::search::coarse_search( search_results
-                             ,side_2_vector
-                             ,side_1_vector
-                             ,order
-                            );
-
-}
 
 
 }// namespace
@@ -152,6 +50,7 @@ void find_periodic_nodes( stk::mesh::BulkData & bulk_data
 STKUNIT_UNIT_TEST(CoarseSearch, PeriodicBC)
 {
   const unsigned x = 3, y = 3, z = 3;
+
 
   stk::mesh::fixtures::HexFixture fixture(MPI_COMM_WORLD, x, y, z);
 
@@ -188,61 +87,30 @@ STKUNIT_UNIT_TEST(CoarseSearch, PeriodicBC)
   }}
   bulk_data.modification_end();
 
-  SearchPairVector search_results;
+  //do periodic search
+  typedef stk::mesh::GetCoordinates<CoordFieldType> CoordinateFunctor;
+  typedef stk::mesh::PeriodicBoundarySearch<CoordinateFunctor> PeriodicSearch;
+  PeriodicSearch pbc_search(bulk_data, CoordinateFunctor(bulk_data, coords_field));
 
-  find_periodic_nodes( bulk_data
-                      ,bulk_data.parallel()
-                      ,coords_field
-                      ,side_0 & meta_data.locally_owned_part()
-                      ,side_3 & meta_data.locally_owned_part()
-                      ,search_results
-                     );
+  pbc_search.add_periodic_pair(side_0 & meta_data.locally_owned_part(), side_3 & meta_data.locally_owned_part() );
+  pbc_search.find_periodic_nodes(bulk_data.parallel());
 
 
-  check_gold(search_results);
-
-
-  const int parallel_rank = bulk_data.parallel_rank();
-  size_t num_constraints = 0;
-  std::vector<stk::mesh::EntityProc> send_nodes;
-  for (size_t i=0, size=search_results.size(); i<size; ++i) {
-      stk::mesh::Entity domain_node = bulk_data.get_entity(stk::topology::NODE_RANK, search_results[i].first.ident);
-      int domain_proc = search_results[i].first.proc;
-      stk::mesh::Entity range_node = bulk_data.get_entity(stk::topology::NODE_RANK, search_results[i].second.ident);
-      int range_proc = search_results[i].second.proc;
-
-      if (parallel_rank == domain_proc) ++num_constraints;
-
-      if ((parallel_rank != domain_proc) && (parallel_rank == range_proc)) {
-        send_nodes.push_back(stk::mesh::EntityProc(range_node, domain_proc));
-      }
-      else if ((parallel_rank == domain_proc) && (parallel_rank != range_proc)) {
-        send_nodes.push_back(stk::mesh::EntityProc(domain_node, range_proc));
-      }
-      ThrowAssert((parallel_rank == domain_proc) || (parallel_rank == range_proc));
-  }
+  check_gold(pbc_search.get_pairs() );
 
   bulk_data.modification_begin();
-  stk::mesh::Ghosting & periodic_bc_ghosting = bulk_data.create_ghosting( "periodic_bc");
-  bulk_data.change_ghosting(periodic_bc_ghosting, send_nodes);
-
+  pbc_search.create_ghosting("periodic_ghosts");
   bulk_data.modification_end();
+
+  const stk::mesh::Ghosting & periodic_bc_ghosting = pbc_search.get_ghosting();
+
 
   std::vector< stk::mesh::FieldBase const * > ghosted_fields;
   ghosted_fields.push_back(&coords_field);
   stk::mesh::communicate_field_data( periodic_bc_ghosting, ghosted_fields);
 
-  SearchPairVector local_search_results;
+  pbc_search.find_periodic_nodes(MPI_COMM_SELF);
 
-  find_periodic_nodes( bulk_data
-                      ,MPI_COMM_SELF
-                      ,coords_field
-                      ,side_0
-                      ,side_3
-                      ,local_search_results
-                     );
-
-  check_gold(local_search_results);
-
+  check_gold(pbc_search.get_pairs());
 }
 
