cdaffd1fa60ef1a01d3208c13b3c42fb55809a9c
Eric Phipps <etphipp@sandia.gov>
2013-10-08 09:11:33 -0600

    Kokkos LinAlg:  Fix compiler error.

--- packages/kokkos/linalg/src/Kokkos_MV.hpp
+++ packages/kokkos/linalg/src/Kokkos_MV.hpp
@@ -751,8 +751,8 @@ RVector MV_Add( const RVector & r, const XVector & x, const YVector & y)
 
     V_Add(r_1d,x_1d,y_1d);
     return r;
-  } else
-	typename XVector::scalar_type a = 1.0;
+  }
+  typename XVector::scalar_type a = 1.0;
   return MV_Add(r,a,x,a,y,1,1);
 }
 
