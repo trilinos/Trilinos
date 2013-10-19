a2173e6408bc108ee6ebb953f12b2ab5c1b8307b
Dan Sunderland <dsunder@sandia.gov>
2013-10-19 08:26:55 -0600

    stk_util: Added a block_vector
    A block_vector is random access container like a  vector or deque.
    It provides the same API as a std::vector, but has much better
    memory behavior when the vector gets very large.
    
    A block_vector can be thought of as a vector of constant sized blocks.
    Block_vectors do not guarantee that items are stored in contiguous
    memory, since each block is allocated separately.  Block_vectors behave
    much better than vectors when inserting and removing from the end.
    In particular, a block_vector does not double the amount of memory
    allocated when growing.  Also, when growing or shrinking to fit, it
    does not move existing blocks around.  If fast insertion/deletion is
    needed from both the front and the end of the vector use a deque.
    
    The block size must be a power of 2, and the better performance has
    been observed when a block fits within 1 memory page.
    
    Comparison to vector<int>
    Size = 10^8
    Minimal Memory : 381.5 M
    
      vector<int>
            push_back : 1x
             iterator : 1x
             for loop : 1x
           TOTAL time : 5.6 seconds
               MEMORY : 768 M
    
      block_vector<int>
            push_back : 0.72x
             iterator : 1.9x
             for loop : 1.9x
           TOTAL time : 9.5 seconds
               MEMORY : 386.3 M
    
      deque<int>
            push_back : 0.73x
             iterator : 1.5x
             for loop : 3.3x
           TOTAL time : 12 seconds
               MEMORY : 395.3 M
    
    Comparison to vector<size_t>
    Size = 10^8
    Minimal Memory : 762.9 M
    
      vector<size_t>
            push_back : 1x
             iterator : 1x
             for loop : 1x
           TOTAL time : 7.2 seconds
               MEMORY : 1.5 G
    
      block_vector<size_t>
            push_back : 0.62x
             iterator : 1.5x
             for loop : 1.3x
           TOTAL time : 9.1 seconds
               MEMORY : 772.6 M
    
      deque<size_t>
            push_back : 0.67x
             iterator : 1.2x
             for loop : 2.6x
           TOTAL time : 12 seconds
               MEMORY : 790.6 M
    
    Comparison to vector<pair<double,size_t>>
    Size = 2^24
    Minimal Memory : 256 M
    
      vector<pair<double,size_t>>
            push_back : 1x
             iterator : 1x
             for loop : 1x
           TOTAL time : 18 seconds
               MEMORY : 768 M
    
      block_vector<pair<double,size_t>>
            push_back : 0.56x
             iterator : 1.1x
             for loop : 1.1x
           TOTAL time : 20 seconds
               MEMORY : 257.5 M
    
      deque<pair<double,size_t>>
            push_back : 0.63x
             iterator : 1x
             for loop : 1.8x
           TOTAL time : 25 seconds
               MEMORY : 266.3 M
    
    (From Sierra)

---
+++ packages/stk/stk_util/stk_util/util/CacheAlignedAllocator.hpp
@@ -0,0 +1,104 @@
+#ifndef STK_UTIL_STK_UTIL_UTIL_CACHE_ALIGNED_ALLOCATOR_HPP
+#define STK_UTIL_STK_UTIL_UTIL_CACHE_ALIGNED_ALLOCATOR_HPP
+
+#include <stk_util/util/AllocatorMemoryUsage.hpp>
+#include <cstdlib>
+#include <limits>
+#include <boost/type_traits/is_same.hpp>
+#include <boost/static_assert.hpp>
+
+namespace stk {
+
+template <typename T, typename Tag = void, size_t CacheSize = 64 >
+class cache_aligned_allocator
+{
+public:
+  BOOST_STATIC_ASSERT(( CacheSize != 0u && !( CacheSize & (CacheSize-1u)) ));
+
+  typedef Tag                         tag;
+  typedef stk::allocator_memory_usage<tag> memory_usage;
+
+  // type definitions
+  typedef T              value_type;
+  typedef T*             pointer;
+  typedef const T*       const_pointer;
+  typedef T&             reference;
+  typedef const T&       const_reference;
+  typedef std::size_t    size_type;
+  typedef std::ptrdiff_t difference_type;
+
+  // rebind allocator to type U
+  template <typename U>
+  struct rebind
+  {
+    typedef cache_aligned_allocator<U,tag> other;
+  };
+
+  // constructors
+  cache_aligned_allocator() {}
+
+  cache_aligned_allocator(const cache_aligned_allocator&) {}
+
+  template <typename U>
+  cache_aligned_allocator (const cache_aligned_allocator<U,tag>&) {}
+
+  // destructor
+  ~cache_aligned_allocator() {}
+
+  // return address of values
+  static pointer       address(      reference value) { return &value; }
+  static const_pointer address(const_reference value) { return &value; }
+
+
+  // return maximum number of elements that can be allocated
+  static size_type max_size()
+  {
+    return std::numeric_limits<std::size_t>::max() / sizeof(T);
+  }
+
+  // allocate but don't initialize num elements of type T
+  static pointer allocate(size_type num, const void* = 0)
+  {
+    size_t size = num * sizeof(T);
+
+    memory_usage::allocate(size);
+
+    pointer ptr = NULL;
+    posix_memalign( (void**)&ptr, CacheSize, size );
+
+    return ptr;
+  }
+
+  // deallocate storage p of deleted elements
+  static void deallocate(pointer p, size_type num)
+  {
+    memory_usage::deallocate(num * sizeof(T));
+    free(p);
+  }
+
+  // initialize elements of allocated storage p with value value
+  static void construct(pointer p, const T& value)
+  {
+    new((void*)p)T(value);
+  }
+
+  // destroy elements of initialized storage p
+  static void destroy(pointer p)
+  {
+    p->~T();
+  }
+};
+
+// return that all specializations of the cache_aligned_allocator with the same allocator and same tag are interchangeable
+template <typename T1, typename T2, typename Tag1, typename Tag2>
+inline bool operator==(const cache_aligned_allocator<T1,Tag1>&, const cache_aligned_allocator<T2,Tag2>&)
+{ return boost::is_same<Tag1,Tag2>::value; }
+
+template <typename T1, typename T2, typename Tag1, typename Tag2>
+inline bool operator!=(const cache_aligned_allocator<T1,Tag1>&, const cache_aligned_allocator<T2,Tag2>&)
+{ return !boost::is_same<Tag1,Tag2>::value; }
+
+} // namespace stk
+
+#endif // STK_UTIL_STK_UTIL_UTIL_CACHE_ALIGNED_ALLOCATOR_HPP
+
