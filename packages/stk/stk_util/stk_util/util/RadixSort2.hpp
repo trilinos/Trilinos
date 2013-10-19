8c3f0c6ae2145beac78a43c5d75dad063c96c5fc
Dan Sunderland <dsunder@sandia.gov>
2013-10-19 15:45:44 -0600

    stk_util:  Added a radix_sort that is suitable for all primitive types
    This radix sort will work with any integral, enum, or floating point
    type.  It is templated on the container type, and depends on a fast
    std::swap between containers.  It requires container.size() temporary
    storage.
    
    Performance comparison of std::sort to stk::radix_sort
    Size = 2^24
    
    vector<size_t>
            std::sort : 1.21 secs
      stk::radix_sort : 0.717 secs, 1.68x speedup
    
    block_vector<size_t>
            std::sort : 1.77 secs
      stk::radix_sort : 0.609 secs, 2.9x speedup
    
    deque<size_t>
            std::sort : 1.41 secs
      stk::radix_sort : 1.29 secs, 1.09x speedup
    
    (From Sierra)

---
+++ packages/stk/stk_util/stk_util/util/RadixSort2.hpp
@@ -0,0 +1,332 @@
+#ifndef STK_UTIL_RADIX_SORT_2_HPP
+#define STK_UTIL_RADIX_SORT_2_HPP
+
+#include <boost/static_assert.hpp>
+#include <boost/type_traits.hpp>
+#include <boost/utility/enable_if.hpp>
+#include <boost/array.hpp>
+
+#include <inttypes.h>
+
+namespace stk { namespace details {
+
+template <typename Key>
+inline
+typename boost::enable_if_c< boost::is_unsigned<Key>::value, bool >::type
+is_negative(const Key &)
+{ return false; }
+
+template <typename Key>
+inline
+typename boost::enable_if_c< !boost::is_unsigned<Key>::value, bool  >::type
+is_negative(const Key & k)
+{ return (k < (Key)0); }
+
+inline
+bool is_little_endian()
+{
+  const uint32_t i=0x01020304u;
+  unsigned char const * const c = reinterpret_cast<unsigned char const * const>(&i);
+  return *c == static_cast<unsigned char>(4u);
+}
+
+
+template <bool IsLittleEndian> struct GetRadix;
+
+template <> struct GetRadix<true>
+{
+  template <typename Key>
+  unsigned operator()( Key const& key, unsigned byte ) const
+  {
+    return reinterpret_cast<unsigned char const * const>(&key)[byte];
+  }
+
+};
+
+template <> struct GetRadix<false>
+{
+  template <typename Key>
+  unsigned operator()( Key const& key, unsigned byte ) const
+  {
+    return reinterpret_cast<unsigned char const * const>(&key)[sizeof(Key) - byte -1];
+  }
+
+};
+
+template < typename KeyArray , bool IsLittleEndian >
+struct RadixSort
+{
+  typedef KeyArray key_array;
+  typedef typename KeyArray::value_type key_type;
+
+  BOOST_STATIC_ASSERT(( boost::is_integral<key_type>::value || boost::is_enum<key_type>::value || boost::is_floating_point<key_type>::value ));
+
+
+  static const unsigned key_size = sizeof(key_type);
+  static const unsigned radix_bits = 8;
+  static const unsigned num_bytes = ((key_size*8) + (radix_bits - 1))/radix_bits;
+  static const unsigned radix_size = 1u << radix_bits;
+  static const unsigned radix_mask = radix_size - 1u;
+
+  typedef boost::array< boost::array<size_t, radix_size>, num_bytes> histogram_type;
+  typedef histogram_type offset_type;
+  typedef boost::array<bool,num_bytes> skip_type;
+
+  struct histogram_result
+  {
+    size_t unsorted;
+    size_t num_negatives;
+  };
+
+  // compute the histogram
+  // returns true if the array is already sorted
+  histogram_result compute_histogram( KeyArray const & keys
+                                     ,histogram_type & histogram
+                                    ) const
+  {
+    GetRadix<IsLittleEndian> get_radix;
+
+    histogram_result results = {0,0};
+
+    for (size_t i=0, ie=keys.size(); i<ie; ++i) {
+      results.unsorted += (i>0) ? (keys[i] <= keys[i-i]) : 0;
+      results.num_negatives += is_negative(keys[i]);
+      for (unsigned byte=0; byte<num_bytes; ++byte) {
+        unsigned radix = get_radix(keys[i],byte);
+        ++histogram[byte][radix];
+      }
+    }
+
+    return results;
+  }
+
+  void compute_offsets_unsigned( const histogram_type & histogram
+                                ,const size_t size
+                                ,offset_type & offsets
+                                ,skip_type & skip_byte
+                               ) const
+  {
+    for (unsigned byte=0; byte<num_bytes; ++byte) {
+      for (unsigned radix=1; radix<radix_size; ++radix) {
+        // if all the keys are in the same radix
+        // we can skip the byte
+        const size_t hist_count = histogram[byte][radix-1];
+        skip_byte[byte] |= (hist_count == size);
+
+        // starting offset for radix => prefix sum
+        offsets[byte][radix] = offsets[byte][radix-1] + hist_count;
+      }
+      skip_byte[byte] |= histogram[byte][radix_size-1] == size;
+    }
+  }
+
+  void compute_offsets_signed( const histogram_type & histogram
+                              ,const size_t size
+                              ,offset_type & offsets
+                              ,skip_type & skip_byte
+                             ) const
+  {
+    for (unsigned byte=0; byte<num_bytes-1; ++byte) {
+      for (unsigned radix=1; radix<radix_size; ++radix) {
+        // if all the keys are in the same radix
+        // we can skip the byte
+        const size_t hist_count = histogram[byte][radix-1];
+        skip_byte[byte] |= (hist_count == size);
+
+        // starting offset for radix => prefix sum
+        offsets[byte][radix] = offsets[byte][radix-1] + hist_count;
+      }
+      skip_byte[byte] |= histogram[byte][radix_size-1] == size;
+    }
+    {
+      const unsigned byte = num_bytes-1;
+      // move negative integers before positive integers
+      for (unsigned radix=1; radix<radix_size; ++radix) {
+        // if all the keys are in the same radix
+        // we can skip the byte
+        const int prev = ((radix_size>>1)+radix-1u) & radix_mask;
+        const int curr = ((radix_size>>1)+radix) & radix_mask;
+        const size_t hist_count = histogram[byte][prev];
+        skip_byte[byte] |= (hist_count == size);
+
+        // starting offset for radix => prefix sum
+        offsets[byte][curr] = offsets[byte][prev] + hist_count;
+      }
+      skip_byte[byte] |= histogram[byte][(radix_size>>1)] == size;
+    }
+  }
+
+  // this will only work with floats that conform to the IEEE 734 standard
+  void compute_offsets_signed_float( const histogram_type & histogram
+                                    ,const size_t size
+                                    ,const size_t num_negatives
+                                    ,offset_type & offsets
+                                    ,skip_type & skip_byte
+                                   ) const
+  {
+    for (unsigned byte=0; byte<num_bytes-1; ++byte) {
+      for (unsigned radix=1; radix<radix_size; ++radix) {
+        // if all the keys are in the same radix
+        // we can skip the byte
+        const size_t hist_count = histogram[byte][radix-1];
+        skip_byte[byte] |= (hist_count == size);
+
+        // starting offset for radix => prefix sum
+        offsets[byte][radix] = offsets[byte][radix-1] + hist_count;
+      }
+      skip_byte[byte] |= histogram[byte][radix_size-1] == size;
+    }
+    {
+      const unsigned byte = num_bytes-1;
+      // move negative integers before positive integers
+      offsets[byte][0] = num_negatives;
+      for (unsigned radix=1; radix<=(radix_size>>1); ++radix) {
+        // if all the keys are in the same radix
+        // we can skip the byte
+        const size_t hist_count = histogram[byte][radix-1];
+        skip_byte[byte] |= (hist_count == size);
+
+        // starting offset for radix => prefix sum
+        offsets[byte][radix] = offsets[byte][radix-1] + hist_count;
+      }
+      offsets[byte][radix_size-1] = 0;
+
+      for (unsigned inv_radix=radix_size-2; inv_radix>(radix_size>>1); --inv_radix) {
+        // if all the keys are in the same radix
+        // we can skip the byte
+        const size_t hist_count = histogram[byte][inv_radix+1];
+        skip_byte[byte] |= (hist_count == size);
+
+        // starting offset for radix => prefix sum
+        offsets[byte][inv_radix] = offsets[byte][inv_radix+1] + hist_count;
+      }
+      skip_byte[byte] |= histogram[byte][(radix_size>>1)] == size;
+      skip_byte[byte] |= histogram[byte][(radix_size-1)]  == size;
+    }
+  }
+
+  void sort_keys(key_array & keys, offset_type & offsets, const skip_type & skip_byte) const
+  {
+    GetRadix<IsLittleEndian> get_radix;
+
+    const size_t size = keys.size();
+    key_array scratch(size);
+
+    for (unsigned byte=0; byte<num_bytes; ++byte) {
+      if (!skip_byte[byte]) {
+        for (size_t i=0; i<size; ++i) {
+          unsigned radix = get_radix(keys[i],byte);
+          scratch[offsets[byte][radix]++] = keys[i];
+        }
+        // swap buffers
+        std::swap(keys,scratch);
+      }
+    }
+  }
+
+  void sort_keys_signed_floats(key_array & keys, offset_type & offsets, const skip_type & skip_byte) const
+  {
+    GetRadix<IsLittleEndian> get_radix;
+
+    const size_t size = keys.size();
+    key_array scratch(size);
+
+    for (unsigned byte=0; byte<num_bytes-1; ++byte) {
+      if (!skip_byte[byte]) {
+        for (size_t i=0; i<size; ++i) {
+          unsigned radix = get_radix(keys[i],byte);
+          scratch[offsets[byte][radix]++] = keys[i];
+        }
+        // swap buffers
+        std::swap(keys,scratch);
+      }
+    }
+    {
+      unsigned byte = num_bytes-1;
+      if (!skip_byte[byte]) {
+        for (size_t i=0; i<size; ++i) {
+          unsigned radix = get_radix(keys[i],byte);
+          size_t index = radix <= (radix_size>>1) ? offsets[byte][radix]++ : --offsets[byte][radix-1];
+          scratch[index] = keys[i];
+        }
+        // swap buffers
+        std::swap(keys,scratch);
+      }
+    }
+  }
+
+  void operator()(key_array & keys) const
+  {
+    const size_t size = keys.size();
+
+    offset_type offsets = {};
+    skip_type skip_byte = {};
+
+    bool has_negative_float = false;
+
+    // compute histogram and offsets
+    {
+      histogram_type histogram = {};
+      const histogram_result r = compute_histogram(keys,histogram);
+
+      if (r.unsorted == 0u) {
+        // already sorted
+        return;
+      }
+      else if (r.unsorted == size) {
+        // reversed sorted
+        for (size_t i = 0, j= size-1; i < j; ++i, --j) {
+          std::swap(keys[i],keys[j]);
+        }
+        return;
+      }
+
+      if (!r.num_negatives) {
+        // no negative values present
+        compute_offsets_unsigned(histogram, size, offsets, skip_byte);
+      }
+      else if(boost::is_integral<key_type>::value || boost::is_enum<key_type>::value) {
+        // negative integers
+        compute_offsets_signed(histogram, size, offsets, skip_byte);
+      }
+      else {
+        // negative floats
+        has_negative_float = true;
+        compute_offsets_signed_float(histogram, size, r.num_negatives, offsets, skip_byte);
+      }
+    }
+
+    if (!has_negative_float) {
+      sort_keys(keys, offsets, skip_byte);
+    }else {
+      sort_keys_signed_floats(keys, offsets, skip_byte);
+    }
+  }
+
+};
+
+}} //namespace stk::details
+
+namespace stk {
+
+template <typename KeyArray>
+typename boost::enable_if_c<(   boost::is_integral<typename KeyArray::value_type>::value
+                             || boost::is_enum<typename KeyArray::value_type>::value
+                             || boost::is_floating_point<typename KeyArray::value_type>::value
+                            ),void>::type
+radix_sort( KeyArray & keys)
+{
+  if (details::is_little_endian()) {
+    details::RadixSort<KeyArray, true> rsort;
+    rsort(keys);
+  }
+  else {
+    details::RadixSort<KeyArray, false> rsort;
+    rsort(keys);
+  }
+}
+
+} // namespace stk
+
+
+#endif // STK_UTIL_RADIX_SORT_2_HPP
---
+++ packages/stk/stk_util/unit_tests/UnitTestRadixSort.cpp
@@ -0,0 +1,214 @@
+#include <stk_util/unit_test_support/stk_utest_macros.hpp>
+#include <stk_util/util/TrackingAllocator.hpp>
+#include <stk_util/util/CacheAlignedAllocator.hpp>
+#include <stk_util/util/memory_util.hpp>
+
+#include <stk_util/util/BlockVector.hpp>
+#include <stk_util/util/RadixSort2.hpp>
+#include <stk_util/util/RadixSort.hpp>
+
+#include <boost/timer.hpp>
+
+#include <cstdlib>
+#include <iostream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <deque>
+
+STKUNIT_UNIT_TEST( radix_sort, sort_unsigned )
+{
+  size_t size = 1000;
+  std::vector<unsigned> a(size);
+
+  srand(0);
+  for (size_t i=0; i<size; ++i) a[i] = std::rand();
+
+  std::vector<unsigned> b(a);
+
+  std::sort(a.begin(), a.end());
+
+  stk::radix_sort(b);
+
+  ASSERT_EQ( a.size(), b.size());
+  ASSERT_TRUE( std::equal(a.begin(), a.end(), b.begin()));
+}
+
+STKUNIT_UNIT_TEST( radix_sort, sort_signed )
+{
+  size_t size = 1000;
+  std::vector<int> a(size);
+
+  srand(0);
+  for (size_t i=0; i<size; ++i) a[i] = std::rand() - (RAND_MAX >> 1);
+
+  std::vector<int> b(a);
+
+  std::sort(a.begin(), a.end());
+
+  stk::radix_sort(b);
+
+  ASSERT_EQ( a.size(), b.size());
+  ASSERT_TRUE( std::equal(a.begin(), a.end(), b.begin()));
+}
+
+STKUNIT_UNIT_TEST( radix_sort, sort_float )
+{
+  size_t size = 1000;
+  std::vector<float> a(size);
+
+  srand(0);
+  for (size_t i=0; i<size; ++i)
+    a[i] = std::rand() / float(RAND_MAX);
+
+  std::vector<float> b(a);
+
+  std::sort(a.begin(), a.end());
+
+  stk::radix_sort(b);
+
+  ASSERT_EQ( a.size(), b.size());
+  ASSERT_TRUE( std::equal(a.begin(), a.end(), b.begin()));
+}
+
+STKUNIT_UNIT_TEST( radix_sort, sort_signed_double )
+{
+  size_t size = 1000;
+  std::vector<double> a(size);
+
+  srand(0);
+  for (size_t i=0; i<size; ++i)
+    a[i] = std::rand() / double(RAND_MAX) -0.5;
+
+  std::vector<double> b(a);
+
+  std::sort(a.begin(), a.end());
+
+  stk::radix_sort(b);
+
+  ASSERT_EQ( a.size(), b.size());
+  ASSERT_TRUE( std::equal(a.begin(), a.end(), b.begin()));
+}
+
+
+namespace {
+
+struct RadixTag {};
+struct SortTag {};
+
+} //namespace
+
+
+
+STKUNIT_UNIT_TEST( radix_sort, performance_vs_radix_sort_unsigned_vector )
+{
+  typedef std::vector<size_t> Vector;
+
+  size_t size = 1ull << 24;
+  Vector a(size);
+  for (size_t i=0; i<size; ++i) a[i] = std::rand();
+
+  Vector b(a);
+
+  boost::timer timer;
+
+  timer.restart();
+  stk::util::radix_sort_unsigned( &*a.begin(), a.size());
+  double sort_time = timer.elapsed();
+
+  timer.restart();
+  stk::radix_sort(b);
+  double radix_time = timer.elapsed();
+
+  std::cout.precision(3);
+
+  std::cout << "Compare stk::util::radix_sort_unsigned to stk::radix_sort => vector<size_t> " << std::endl;
+  std::cout << "Size = 2^24" << std::endl;
+
+  std::cout << "  stk::util::radix_sort_unsigned : " << sort_time << " secs " << std::endl;
+  std::cout << "                 stk::radix_sort : " << radix_time << " secs, " << sort_time/radix_time << "x speedup" << std::endl;
+}
+
+STKUNIT_UNIT_TEST( radix_sort, performance_vs_std_sort_vector )
+{
+  typedef std::vector<size_t> Vector;
+
+  size_t size = 1ull << 24;
+  Vector a(size);
+  for (size_t i=0; i<size; ++i) a[i] = std::rand();
+
+  Vector b(a);
+
+  boost::timer timer;
+
+  timer.restart();
+  std::sort(a.begin(), a.end());
+  double sort_time = timer.elapsed();
+
+  timer.restart();
+  stk::radix_sort(b);
+  double radix_time = timer.elapsed();
+
+  std::cout << "Compare std::sort to stk::radix_sort => vector<size_t>" << std::endl;
+  std::cout << "Size = 2^24" << std::endl;
+
+  std::cout << "        std::sort : " << sort_time << " secs " << std::endl;
+  std::cout << "  stk::radix_sort : " << radix_time << " secs, " << sort_time/radix_time  << "x speedup" << std::endl;
+
+}
+
+STKUNIT_UNIT_TEST( radix_sort, performance_vs_std_sort_block_vector )
+{
+  typedef stk::block_vector<size_t> Vector;
+
+  size_t size = 1ull << 24;
+  Vector a(size);
+  for (size_t i=0; i<size; ++i) a[i] = std::rand();
+
+  Vector b(a);
+
+  boost::timer timer;
+
+  timer.restart();
+  std::sort(a.begin(), a.end());
+  double sort_time = timer.elapsed();
+
+  timer.restart();
+  stk::radix_sort(b);
+  double radix_time = timer.elapsed();
+
+  std::cout << "Compare std::sort to stk::radix_sort => block_vector<size_t>" << std::endl;
+  std::cout << "Size = 2^24" << std::endl;
+
+  std::cout << "        std::sort : " << sort_time << " secs " << std::endl;
+  std::cout << "  stk::radix_sort : " << radix_time << " secs, " << sort_time/radix_time << "x speedup"  << std::endl;
+
+}
+
+STKUNIT_UNIT_TEST( radix_sort, performance_vs_std_sort_deque )
+{
+  typedef std::deque<size_t> Vector;
+
+  size_t size = 1ull << 24;
+  Vector a(size);
+  for (size_t i=0; i<size; ++i) a[i] = std::rand();
+
+  Vector b(a);
+
+  boost::timer timer;
+
+  timer.restart();
+  std::sort(a.begin(), a.end());
+  double sort_time = timer.elapsed();
+
+  timer.restart();
+  stk::radix_sort(b);
+  double radix_time = timer.elapsed();
+
+  std::cout << "Compare std::sort to stk::radix_sort => deque<size_t>" << std::endl;
+  std::cout << "Size = 2^24" << std::endl;
+
+  std::cout << "        std::sort : " << sort_time << " secs " << std::endl;
+  std::cout << "  stk::radix_sort : " << radix_time << " secs, " << sort_time/radix_time << "x speedup"  << std::endl;
+}
+
