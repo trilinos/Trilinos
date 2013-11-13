f4e9522139470c30b6c696a66f32b05a5426a7e0
Andrey Prokopenko <aprokop@sandia.gov>
2013-11-13 16:00:13 -0700

    MueLu: easy interface test: multiple bug fixes
    * Initialize seed not for the whole test, but rather for every xml file.
      This allows proper comparison among different xml files
    * When running with multiple mpi ranks, do not overwrite the output file
    * Reset max eigenvalue of the matrix, which could have been cached by
      one of the previous runs

--- packages/muelu/test/interface/EasyParameterListInterpreter.cpp
+++ packages/muelu/test/interface/EasyParameterListInterpreter.cpp
@@ -64,9 +64,6 @@ int main(int argc, char *argv[]) {
   using Teuchos::rcp;
   using Teuchos::TimeMonitor;
 
-  // Set seed
-  std::srand(12345);
-
   // =========================================================================
   // MPI initialization using Teuchos
   // =========================================================================
@@ -110,16 +107,29 @@ int main(int argc, char *argv[]) {
 
   bool failed = false;
   for (int i = 0; i < fileList.size(); i++) {
-    std::string xmlFile = "EasyParameterListInterpreter/" + fileList[i];
-    std::string baseFile = xmlFile.substr(0, xmlFile.find_last_of('.')) + (lib == Xpetra::UseEpetra ? "_epetra" : "_tpetra");
+    // Set seed
+    std::srand(12345);
 
-    // Redirect output
+    // Reset (potentially) cached value of the estimate
+    A->SetMaxEigenvalueEstimate(-Teuchos::ScalarTraits<SC>::one());
+
+    std::string xmlFile = "EasyParameterListInterpreter/" + fileList[i];
+    std::string baseFile = xmlFile.substr(0, xmlFile.find_last_of('.'));
+    std::size_t found = baseFile.find("_np");
+    if (numProc == 1 && found != std::string::npos)
+      baseFile = baseFile.substr(0, found);
+    baseFile = baseFile + (lib == Xpetra::UseEpetra ? "_epetra" : "_tpetra");
+
     std::filebuf buffer;
-    buffer.open((baseFile + ".out").c_str(), std::ios::out);
-    std::streambuf* oldbuffer = std::cout.rdbuf(&buffer);
+    std::streambuf* oldbuffer;
+    if (myRank == 0) {
+      // Redirect output
+      buffer.open((baseFile + ".out").c_str(), std::ios::out);
+      oldbuffer = std::cout.rdbuf(&buffer);
+    }
 
     // NOTE: we cannot use EasyParameterListInterpreter(xmlFile, comm), because we want to update the ParameterList
-    // first to include verbosity
+    // first to include "test" verbosity
     Teuchos::ParameterList paramList;
     Teuchos::updateParametersFromXmlFileAndBroadcast(xmlFile, Teuchos::Ptr<Teuchos::ParameterList>(&paramList), *comm);
     paramList.set("verbosity", "test");
@@ -136,18 +146,18 @@ int main(int argc, char *argv[]) {
       mueluFactory.SetupHierarchy(*H);
 
     } catch (Teuchos::ExceptionBase& e) {
-      if (!myRank) {
+      if (myRank == 0) {
         std::string msg = e.what();
         msg = msg.substr(msg.find_last_of('\n')+1);
         std::cout << "Caught exception: " << msg << std::endl;
       }
     }
 
-    // Redirect output back
-    std::cout.rdbuf(oldbuffer);
-    buffer.close();
+    if (myRank == 0) {
+      // Redirect output back
+      std::cout.rdbuf(oldbuffer);
+      buffer.close();
 
-    {
       // Tpetra produces different eigenvalues in Chebyshev due to using
       // std::rand() for generating random vectors, which may be initialized
       // using different seed, and may have different algorithm from one
@@ -182,18 +192,18 @@ int main(int argc, char *argv[]) {
 #endif
       system((sed_cmd + baseFile + ".res").c_str());
       system((sed_cmd + baseFile + ".out").c_str());
-    }
 
-    // Run comparison
-    std::string cmd = "diff -w " + baseFile + ".res " + baseFile + ".out";
-    int ret = system(cmd.c_str());
-    if (ret)
-      failed = true;
-    if (!myRank)
+      // Run comparison
+      std::string cmd = "diff -w " + baseFile + ".res " + baseFile + ".out";
+      int ret = system(cmd.c_str());
+      if (ret)
+        failed = true;
+
       std::cout << xmlFile << ": " << (ret ? "failed" : "passed") << std::endl;
+    }
   }
 
-  if (comm->getRank() == 0)
+  if (myRank == 0)
     std::cout << std::endl << "End Result: TEST " << (failed ? "FAILED" : "PASSED") << std::endl;
 
   return 0;
