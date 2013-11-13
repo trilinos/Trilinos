8bf281065419395c95bbe2d0995fdc35ee25ccd4
Greg Sjaardema <gdsjaar@sandia.gov>
2013-11-13 08:21:13 -0700

    STK_IO: Rename meta/bulk_data_is_set() functions to is_????_data_non_null()
    (From Sierra)

--- packages/stk/stk_io/stk_io/StkMeshIoBroker.hpp
+++ packages/stk/stk_io/stk_io/StkMeshIoBroker.hpp
@@ -437,11 +437,11 @@ namespace stk {
         //   -- If reading data from the mesh also, then may need both.
         // * What state used for input/output -- pass as argument to process_restart?
 
-        bool meta_data_is_set() const
+        bool is_meta_data_non_null() const
         {
           return !Teuchos::is_null(m_meta_data);
         }
-        bool bulk_data_is_set() const
+        bool is_bulk_data_non_null() const
         {
           return !Teuchos::is_null(m_bulk_data);
         }
