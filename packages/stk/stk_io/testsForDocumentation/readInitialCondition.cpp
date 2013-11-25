5acdb5dd35184c13cce6c4ab127f0a52b16989bc
Greg Sjaardema <gdsjaar@sandia.gov>
2013-11-25 15:30:28 -0700

    STK_IO: Add read initial conditions documentation test
    (From Sierra)

---
+++ packages/stk/stk_io/testsForDocumentation/readInitialCondition.cpp
@@ -0,0 +1,101 @@
+#include <gtest/gtest.h>
+#include <string>
+#include <mpi.h>
+#include <stk_io/StkMeshIoBroker.hpp>
+#include <stk_mesh/base/MetaData.hpp>
+#include <stk_mesh/base/BulkData.hpp>
+#include <stk_mesh/base/Types.hpp>
+#include <stk_mesh/base/GetEntities.hpp>
+namespace {
+
+  TEST(StkMeshIoBrokerHowTo, readInitialCondition)
+  {
+    std::string ic_name = "input_field_example.e";
+    MPI_Comm communicator = MPI_COMM_WORLD;
+
+    {
+      // ============================================================
+      //+ INITIALIZATION:
+      //+ Create a mesh with the nodal field "temp" for 3 timesteps.
+      //+ The value of the field at each node is 0.0 at time 0.0,
+      //+ 1.0 at time 1.0, and 2.0 at time 2.0
+      stk::io::StkMeshIoBroker stkIo(communicator);
+
+      const std::string generatedFileName = "generated:8x8x8";
+      stkIo.open_mesh_database(generatedFileName, stk::io::READ_MESH);
+      stkIo.create_input_mesh();
+
+      stk::mesh::Field<double> &temperature =
+	stkIo.meta_data().declare_field<stk::mesh::Field<double> >("temperature", 1);
+      stk::mesh::put_field(temperature, stk::mesh::Entity::NODE,
+			   stkIo.meta_data().universal_part());
+      stkIo.populate_bulk_data();
+
+      size_t fh = stkIo.create_output_mesh(ic_name, stk::io::WRITE_RESULTS);
+
+      //+ The name of the field on the database will be "temp"
+      stkIo.add_field(fh, temperature, "temp");
+	
+      std::vector<stk::mesh::Entity> nodes;
+      stk::mesh::get_entities(stkIo.bulk_data(),
+			      stk::topology::NODE_RANK, nodes);
+    
+      // Add three steps to the database
+      // For each step, the value of the field is the value 'time'
+      for (size_t i=0; i < 3; i++) {
+	double time = i;
+
+	for(size_t i=0; i<nodes.size(); i++) {
+	  double *fieldDataForNode =
+	    reinterpret_cast<double*>(stkIo.bulk_data().field_data(temperature, nodes[i]));
+	  *fieldDataForNode = time;
+	}
+
+	stkIo.begin_output_step(fh, time);
+	stkIo.write_defined_output_fields(fh);
+	stkIo.end_output_step(fh);
+      }
+    }
+
+    {
+      //-BEGIN
+      // ============================================================
+      //+ EXAMPLE: 
+      //+ Read the value of the "temp" field at step 2 and populate
+      //+ the nodal field "temperature" for use as an initial condition
+      stk::io::StkMeshIoBroker stkIo(communicator);
+      stkIo.open_mesh_database(ic_name, stk::io::READ_MESH);
+      stkIo.create_input_mesh();
+
+      stk::mesh::Field<double> &temperature =
+	stkIo.meta_data().declare_field<stk::mesh::Field<double> >
+	                                            ("temperature", 1);
+      stk::mesh::put_field(temperature, stk::mesh::Entity::NODE,
+			   stkIo.meta_data().universal_part());
+      stkIo.populate_bulk_data();
+
+      //+ The name of the field on the database is "temp"
+      stkIo.add_input_field(temperature, "temp");
+
+      //+ Read the field values from the database at time 2.0
+      stkIo.read_defined_input_fields(2.0);
+
+      // ============================================================
+      //+ VERIFICATION
+      //+ The value of the field at all nodes should be 2.0
+      std::vector<stk::mesh::Entity> nodes;
+      stk::mesh::get_entities(stkIo.bulk_data(), stk::topology::NODE_RANK,
+			      nodes);
+      for(size_t i=0; i<nodes.size(); i++) {
+	double *fieldDataForNode =
+	  reinterpret_cast<double*>(stkIo.bulk_data().
+				    field_data(temperature, nodes[i]));
+	EXPECT_DOUBLE_EQ(2.0, *fieldDataForNode);
+      }
+      //-END      
+    }
+    // ============================================================
+    // Cleanup
+    unlink(ic_name.c_str());
+  }
+}
