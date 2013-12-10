e07558d9ca34d90ca028de9bd7534668053c5f6f
Bill Spotz <wfspotz@sandia.gov>
2013-12-10 15:36:06 -0700

    Made a variety of changes to Domi::getValidParameters()
    * Added a validator to the "layout" parameter
    
    * Changed parameter names to be more readable:
      axisCommSizes -> "axis comm sizes"
      commPad       -> "communication pad"
      bndryPad      -> "boundary pad"
    
    * Minor formatting changes

--- packages/domi/src/Domi_getValidParameters.cpp
+++ packages/domi/src/Domi_getValidParameters.cpp
@@ -42,10 +42,12 @@
 
 #include "Domi_getValidParameters.hpp"
 #include "Teuchos_Array.hpp"
+#include "Teuchos_StandardParameterEntryValidators.hpp"
 #include "Domi_Utils.hpp"
 
-namespace Domi
+using std::string;
 
+namespace Domi
 {
 
   Teuchos::RCP< const Teuchos::ParameterList > getValidParameters() 
@@ -53,67 +55,98 @@ namespace Domi
     static Teuchos::RCP< const Teuchos::ParameterList > result;
 
     if ( result.is_null() )
-      {
+    {
 
-	Teuchos::ParameterList* plist = new Teuchos::ParameterList;
+      Teuchos::ParameterList* plist = new Teuchos::ParameterList;
 
-	/* Parameter applies to MDComm */
-	Teuchos::Array< int > axisCommSizes(1);
-	axisCommSizes[0] = -1;
-	plist->set("axisCommSizes", axisCommSizes, 
-		   "An array of ints whose length is the number of dimensions "
-		   "of the problem and whose entries specify the size of the "
-		   "MDComm along each axis. A negative value tells Domi to "
-		   "fill in a logical value based on the total number of "
-		   "processors");
+      /* "axis comm sizes" parameter applies to MDComm */
+      Teuchos::Array< int > axisCommSizes(1);
+      axisCommSizes[0] = -1;
+      plist->set("axis comm sizes",
+                 axisCommSizes, 
+                 "An array of ints whose length is the number of dimensions "
+                 "of the problem and whose entries specify the size of the "
+                 "MDComm along each axis. A negative value tells Domi to "
+                 "fill in a logical value based on the total number of "
+                 "processors");
 
-	/* Parameter applies to MDComm */
-	Teuchos::Array< int > periodic(1);
-	periodic[0] = 0;
-	plist->set("periodic", periodic,
-		   "An array of int flags specifying whether each axis is a "
-		   "periodic axis. If the periodic array is shorter than the "
-		   "length of axisCommSizes array, then the unspecified "
-		   "entries are given a default value of zero (not "
-		   "periodic).");
+      /* "periodic" parameter applies to MDComm */
+      Teuchos::Array< int > periodic(1);
+      periodic[0] = 0;
+      plist->set("periodic",
+                 periodic,
+                 "An array of int flags specifying whether each axis is a "
+                 "periodic axis. If the periodic array is shorter than the "
+                 "length of axisCommSizes array, then the unspecified "
+                 "entries are given a default value of zero (not "
+                 "periodic).");
 
-	/* Parameter applies to MDMap */
-	Teuchos::Array< long long > dimensions(1);
-	dimensions[0] = 0;
-	plist->set("dimensions", dimensions,
-		   "An array of ordinals specifying the global dimensions of "
-		   "the MDMap. The length of this array should be the same as "
-		   "the length of the axisCommSizes array. Note that this is "
-		   "an array of long long and it will need to be copied to an "
-		   "array of type GlobalOrd.");
+      /* "dimensions" parameter applies to MDMap */
+      Teuchos::Array< long long > dimensions(1);
+      dimensions[0] = 0;
+      plist->set("dimensions",
+                 dimensions,
+                 "An array of ordinals specifying the global dimensions of "
+                 "the MDMap. The length of this array should be the same as "
+                 "the length of the axisCommSizes array. Note that this is "
+                 "an array of long long and it will need to be copied to an "
+                 "array of type GlobalOrd.");
 
-	Teuchos::Array< int > pad;
-	/* Parameter applies to MDMap */
-	plist->set("boundaryPad", pad,
-		  "An array of ints specifying the size of the boundary "
-		   "padding along each axis. All unspecified entries are "
-		   "assumed to be zero.");
+      Teuchos::Array< int > pad;
+      /* "boundary pad" parameter applies to MDMap */
+      plist->set("boundary pad",
+                 pad,
+                 "An array of ints specifying the size of the boundary "
+                 "padding along each axis. All unspecified entries are "
+                 "assumed to be zero.");
 
-	/* Parameter applies to MDMap */
-	plist->set("commPad", pad,
-		  "An array of ints specifying the size of the commmunication "
-		   "padding along each axis. All unspecified entries are "
-		   "assumed to be zero.");
+      /* "communication pad" parameter applies to MDMap */
+      plist->set("communication pad",
+                 pad,
+                 "An array of ints specifying the size of the commmunication "
+                 "padding along each axis. All unspecified entries are "
+                 "assumed to be zero.");
 
+      /* "layout" parameter applies to MDMap */
+      string layout = "Default";
+      Teuchos::ArrayView< const string > layoutOpts =
+        Teuchos::tuple(string("C Order"),
+                       string("Fortran Order"),
+                       string("Row Major"),
+                       string("Column Major"),
+                       string("Last Index Fastest"),
+                       string("First Index Fastest"),
+                       string("Default"));
+      Teuchos::ArrayView< const string > layoutDocs =
+        Teuchos::tuple(string("C storage order (last index varies fastest)"),
+                       string("Fortran storage order (first index varies "
+                              "fastest)"),
+                       string("Row major storage order (last index varies "
+                              "fastest)"),
+                       string("Column major storage order (first index "
+                              "varies fastest)"),
+                       string("Last index varies fastest"),
+                       string("First index varies fastest"),
+                       string("Fortran storage order"));
+      Teuchos::ArrayView< const int > layoutVals =
+        Teuchos::tuple(0, 1, 0, 1, 0, 1, 1);
+      Teuchos::RCP< const Teuchos::ParameterEntryValidator > layoutValidator =
+        Teuchos::rcp(new Teuchos::StringToIntegralParameterEntryValidator< int >
+                     (layoutOpts,
+                      layoutDocs,
+                      layoutVals,
+                      string("Default"),
+                      false));
+      plist->set("layout",
+                 layout,
+                 "A string indicating how the data is laid out in memory. "
+                 "Default is currently set to Fortran order.",
+                 layoutValidator);
 
-	/* Parameter applies to MDMap */
-	std::string layout = "Default";
-	plist->set("layout", layout,
-		   "A string indicating how the data is laid out in memory. "
-		   "Default is currently set to Fortran order.");
+      result.reset(plist);
+    }
 
-	result.reset(plist);
-
-      }
-    
-
-    return result;
- 
+    return result; 
   }
 
 }
