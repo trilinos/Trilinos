bd9207a8beea3f9f2209de8eaaaa47d27f4ba39f
Mark Hoemmen <mhoemme@sandia.gov>
2013-12-11 09:22:02 -0700

    KokkosClassic: TBBNode: Fixed minor syntax error.
    KokkosClassic::TBBNode was calling the parallel_reduce functor's (WDP's)
    identity() method as a class method, rather than as an instance method
    (as it should).  This commit fixes that.
    
    This is related to previous commits that make KokkosClassic more
    consistent in how it uses functors.  Those commits, in turn, are related
    to the new Kokkos wrapper Nodes that were recently added to facilitate
    porting Tpetra to use new Kokkos.  We hadn't fixed TBBNode to make it
    consistent with TPINode, SerialNode, OpenMPNode, and ThrustGPUNode,
    because TBBNode is disabled by default (unless one enables the TBB TPL).
    This commit remedies that omission.
    
    For some reason, when I enable this Node type, many (but not all)
    KokkosClassic and Tpetra tests fail with a malloc() issue.  I'm not sure
    why, but I know that the build error needs to be fixed, so I'm forcing
    the push.
    
    Build/Test Cases Summary
    Enabled Packages: TeuchosNumerics, KokkosClassic, Tpetra
    0) MPI_DEBUG => FAILED: passed=91,notpassed=15 => Not ready to push! (1.97 min)
    1) SERIAL_RELEASE => FAILED: passed=77,notpassed=12 => Not ready to push! (0.12 min)
    Other local commits for this build/test group: 66e477e
    WARNING: Forced the push!

--- packages/kokkos/classic/NodeAPI/Kokkos_TBBNode.hpp
+++ packages/kokkos/classic/NodeAPI/Kokkos_TBBNode.hpp
@@ -1,12 +1,12 @@
 //@HEADER
 // ************************************************************************
-// 
+//
 //          Kokkos: Node API and Parallel Node Kernels
 //              Copyright (2008) Sandia Corporation
-// 
+//
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -34,8 +34,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
-// 
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
 // ************************************************************************
 //@HEADER
 
@@ -62,32 +62,42 @@ namespace KokkosClassic {
   template <class WDPin>
   struct BlockedRangeWDP {
     mutable WDPin *wd;
-    inline BlockedRangeWDP(WDPin &in) : wd(&in) {}
-    inline void operator()(tbb::blocked_range<int> &rng) const { 
-      for (int i=rng.begin(); i != rng.end(); ++i) wd->execute(i);
+    inline BlockedRangeWDP (WDPin &in) : wd (&in) {}
+    inline void operator () (tbb::blocked_range<int>& rng) const {
+      for (int i = rng.begin (); i != rng.end (); ++i) {
+        wd->execute (i);
+      }
     }
   };
-  
+
   template <class WDPin>
   struct BlockedRangeWDPReducer {
     WDPin &wd;
     typename WDPin::ReductionType result;
-    BlockedRangeWDPReducer(WDPin &in) : wd(in), result(WDPin::identity()) {}
-    BlockedRangeWDPReducer(BlockedRangeWDPReducer &in, tbb::split) : wd(in.wd) {result = wd.identity();}
-    void operator()(tbb::blocked_range<int> &rng)
-    { 
+    BlockedRangeWDPReducer (WDPin &in) :
+      wd (in),
+      result (in.identity ())
+    {}
+
+    BlockedRangeWDPReducer (BlockedRangeWDPReducer &in, tbb::split) :
+      wd (in.wd)
+    {
+      result = wd.identity ();
+    }
+
+    void operator () (tbb::blocked_range<int> &rng) {
       typename WDPin::ReductionType tmpi;
-      int end = rng.end();
-      for (int i=rng.begin(); i != end; ++i) {
-        tmpi = wd.generate(i);
-        result = wd.reduce( result, tmpi );
+      const int end = rng.end ();
+      for (int i = rng.begin (); i != end; ++i) {
+        tmpi = wd.generate (i);
+        result = wd.reduce (result, tmpi);
       }
     }
-    inline void join( const BlockedRangeWDPReducer<WDPin> &other ) {
-      result = wd.reduce( result, other.result );
+    inline void join (const BlockedRangeWDPReducer<WDPin> &other) {
+      result = wd.reduce (result, other.result);
     }
   };
-  
+
   /** \brief %Kokkos node interface to the Intel Threading Building Blocks threading library.
       \ingroup kokkos_node_api
    */
@@ -95,17 +105,17 @@ namespace KokkosClassic {
   public:
     //! Constructor that sets default parameters.
     TBBNode ();
-  
+
     /*! \brief Constructor that takes a list of parameters.
-          
+
       This constructor accepts the following parameters:
       - "Num Threads" [int] Specifies the number of threads; calls
         TBBNode::init() if non-negative. Otherwise, late
         initialization. Default: -1.
     */
     TBBNode(ParameterList &pl);
-  
-    //! Default destructor, calls tbb::task_scheduler_init::terminate(). 
+
+    //! Default destructor, calls tbb::task_scheduler_init::terminate().
     ~TBBNode();
 
     /*! \brief Get default parameters for this node */
@@ -121,7 +131,7 @@ namespace KokkosClassic {
     template <class WDP>
     static void parallel_for(int begin, int end, WDP wd) {
       BlockedRangeWDP<WDP> tbb_wd(wd);
-      tbb::parallel_for(tbb::blocked_range<int>(begin,end), tbb_wd, tbb::auto_partitioner()); 
+      tbb::parallel_for(tbb::blocked_range<int>(begin,end), tbb_wd, tbb::auto_partitioner());
     }
 
     //! \begin parallel reduction skeleton, a wrapper around tbb::parallel_reduce. See \ref kokkos_node_api "Kokkos Node API"
@@ -135,14 +145,14 @@ namespace KokkosClassic {
 
     //! \begin No-op for TBBNode.
     inline void sync() const {};
-  
+
   private:
     bool alreadyInit_;
     tbb::task_scheduler_init tsi_;
   };
-  
-  template <> 
-  class ArrayOfViewsHelper<TBBNode> : 
+
+  template <>
+  class ArrayOfViewsHelper<TBBNode> :
     public ArrayOfViewsHelperTrivialImpl<TBBNode> {};
 
 } // end of Kokkos namespace
