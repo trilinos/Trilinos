8324080e9190ed1e2d63bc6f2c71382558698f77
Mark Hoemmen <mhoemme@sandia.gov>
2013-12-12 13:58:59 -0700

    Ifpack2: Added OneLevelFactory to help with nested prec creation.
    OneLevelFactory takes care of creation of a "non-nested"
    ("single-level") Preconditioner subclasses in Ifpack2.  "Non-nested"
    preconditioners are those which do not inherit from
    Details::NestedPreconditioner, and thus do not take an "inner
    preconditioner."  "Nested" preconditioners include AdditiveSchwarz,
    Krylov, and SupportGraph.
    
    OneLevelFactory will let nested preconditioners create a default inner
    preconditioner.  Nested preconditioners cannot use Factory, because that
    would cause a circular dependency (Factory must be able to create nested
    preconditioners).  Nested preconditioners will instead be able to use
    OneLevelFactory to create a default inner preconditioner.
    
    Nested preconditioners will only need to do this if they are created
    directly by the user (i.e., by calling their constructors directly).  If
    they are created by Factory (the preferred method), Factory can assign
    them any arbitrary inner preconditioner, including another nested
    preconditioner, since there would be no circular dependency in that case
    (Factory already depends on all of Ifpack2's preconditioners).
    
    I have not implemented anything in the second or third paragraphs above
    yet.  OneLevelFactory is an enabling technology for those changes,
    however.
    
    Build/Test Cases Summary
    Enabled Packages: Ifpack2
    0) MPI_DEBUG => passed: passed=23,notpassed=0 (4.13 min)
    1) SERIAL_RELEASE => passed: passed=18,notpassed=0 (6.62 min)
    Other local commits for this build/test group: 2d54421, f083942

---
+++ packages/ifpack2/src/Ifpack2_Details_OneLevelFactory_def.hpp
@@ -0,0 +1,130 @@
+/*@HEADER
+// ***********************************************************************
+//
+//       Ifpack2: Tempated Object-Oriented Algebraic Preconditioner Package
+//                 Copyright (2009) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ***********************************************************************
+//@HEADER
+*/
+
+#ifndef IFPACK2_DETAILS_ONELEVELFACTORY_DEF_HPP
+#define IFPACK2_DETAILS_ONELEVELFACTORY_DEF_HPP
+
+#include "Ifpack2_Chebyshev.hpp"
+#include "Ifpack2_Details_DenseSolver.hpp"
+#include "Ifpack2_Diagonal.hpp"
+#include "Ifpack2_IdentitySolver.hpp"
+#include "Ifpack2_ILUT.hpp"
+#include "Ifpack2_Relaxation.hpp"
+#include "Ifpack2_RILUK.hpp"
+
+#if defined(HAVE_IFPACK2_EXPERIMENTAL) && defined(HAVE_IFPACK2_AMESOS2)
+#  include "Ifpack2_Details_Amesos2Wrapper.hpp"
+#endif //  defined(HAVE_IFPACK2_EXPERIMENTAL) && defined(HAVE_IFPACK2_AMESOS2)
+
+namespace Ifpack2 {
+namespace Details {
+
+template<class MatrixType>
+Teuchos::RCP<Preconditioner<typename MatrixType::scalar_type,
+                            typename MatrixType::local_ordinal_type,
+                            typename MatrixType::global_ordinal_type,
+                            typename MatrixType::node_type> >
+OneLevelFactory::create (const std::string& precType,
+                         const Teuchos::RCP<const MatrixType>& matrix)
+{
+  using Teuchos::RCP;
+  using Teuchos::rcp;
+  typedef typename MatrixType::scalar_type scalar_type;
+  typedef typename MatrixType::local_ordinal_type local_ordinal_type;
+  typedef typename MatrixType::global_ordinal_type global_ordinal_type;
+  typedef typename MatrixType::node_type node_type;
+  typedef Preconditioner<scalar_type, local_ordinal_type,
+                         global_ordinal_type, node_type> prec_base_type;
+
+  RCP<prec_base_type> prec;
+
+  // precTypeUpper is the upper-case version of precType.
+  std::string precTypeUpper (precType);
+  if (precTypeUpper.size () > 0) {
+    std::locale locale;
+    for (size_t k = 0; k < precTypeUpper.size (); ++k) {
+      precTypeUpper[k] = std::toupper<char> (precTypeUpper[k], locale);
+    }
+  }
+
+  const bool one_mpi_rank = (matrix->getComm ()->getSize () == 1);
+  // Forestall "unused variable" warnings.
+  (void) one_mpi_rank;
+
+  if (precTypeUpper == "CHEBYSHEV") {
+    prec = rcp (new ::Ifpack2::Chebyshev<MatrixType> (matrix));
+  }
+  else if (precTypeUpper == "DENSE" || precTypeUpper == "LAPACK") {
+    prec = rcp (new Details::DenseSolver<MatrixType> (matrix));
+  }
+#if defined(HAVE_IFPACK2_EXPERIMENTAL) && defined(HAVE_IFPACK2_AMESOS2)
+  else if (precTypeUpper == "AMESOS2") {
+    prec = rcp (new Details::Amesos2Wrapper<MatrixType> (matrix));
+  }
+#endif
+  else if (precTypeUpper == "DIAGONAL") {
+    prec = rcp (new Diagonal<MatrixType> (matrix));
+  }
+  else if (precTypeUpper == "ILUT") {
+    prec = rcp (new ILUT<MatrixType> (matrix));
+  }
+  else if (precTypeUpper == "RELAXATION") {
+    prec = rcp (new Relaxation<MatrixType> (matrix));
+  }
+  else if (precTypeUpper == "RILUK") {
+    prec = rcp (new RILUK<MatrixType> (matrix));
+  }
+  else if (precTypeUpper == "IDENTITY" || precTypeUpper == "IDENTITY_SOLVER") {
+    prec = rcp (new IdentitySolver<MatrixType> (matrix));
+  }
+  else {
+    TEUCHOS_TEST_FOR_EXCEPTION(
+      true, std::invalid_argument, "Ifpack2::Details::OneLevelFactory::create: "
+      "Invalid preconditioner type \"" << precType << "\".");
+  }
+  return prec;
+}
+
+} // namespace Details
+} // namespace Ifpack2
+
+#endif // IFPACK2_DETAILS_ONELEVELFACTORY_DEF_HPP
