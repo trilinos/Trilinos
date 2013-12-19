781d3524a5abc96996c59592c8c5230dea9ee58f
K Devine <kddevin@sandia.gov>
2013-12-19 13:05:01 -0700

    The model had been always setting the number of weights per object to 1. That seems wasteful (in terms of time/space) when the user doesn't provide weights. This change allows the model to return zero when no user weights are provided.

--- packages/zoltan2/src/models/Zoltan2_Model.hpp
+++ packages/zoltan2/src/models/Zoltan2_Model.hpp
@@ -188,20 +188,18 @@ protected:
    * weights are uniform.  If lengths for a given weight dimension
    * are zero on all processes, then we know that uniform weights are implied.
    *
-   * This must be called by all processes.
+   * This method must be called by all processes.
    */
   void setWeightArrayLengths(const Array<lno_t> &len, 
     const Teuchos::Comm<int> &comm)
   {
     weightDim_ = len.size();
 
-    if (weightDim_ < 1)
-      weightDim_ = 1;          // uniform weights are implied
+    int lvalsize = (weightDim_ > 0 ? weightDim_ : 1);
+    int *lval = new int [lvalsize];
+    uniform_ = arcp(lval, 0, lvalsize);
 
-    int *lval = new int [weightDim_];
-    uniform_ = arcp(lval, 0, weightDim_);
-
-    if (len.size() < 1){
+    if (weightDim_ < 1){
       uniform_[0] = 1;
       return;
     }
