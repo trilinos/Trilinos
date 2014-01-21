8f102fb2959b6995e8cd9fd278e2856bee2a91fb
Brent Perschbacher <bmpersc@sandia.gov>
2014-01-21 16:47:36 -0700

    Trilinos: Update flat header directory.
    (From Sierra)
    
    Build/Test Cases Summary
    Enabled Packages: SEACAS
    Disabled Packages: Panzer,PyTrilinos,SEACASExo2mat,SEACASMat2exo
    Enabled all Forward Packages
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_SS => passed: passed=120,notpassed=0 (31.29 min)
    3) SERIAL_RELEASE_SS => passed: passed=122,notpassed=0 (29.61 min)
    Other local commits for this build/test group: 1af6374

--- SIERRA/bjam/config_headers/Thyra_Config_fcs.h
+++ SIERRA/bjam/config_headers/Thyra_Config_fcs.h
@@ -26,4 +26,11 @@
 #endif
 
 
-#define THYRA_DEPRECATED
+#ifndef THYRA_DEPRECATED
+#  if (__GNUC__ > 3 || (__GNUC__ == 3 && __GNUC_MINOR__ >= 1))
+#    define THYRA_DEPRECATED  __attribute__((__deprecated__))
+#  else
+#    define THYRA_DEPRECATED
+#  endif
+#endif
+
