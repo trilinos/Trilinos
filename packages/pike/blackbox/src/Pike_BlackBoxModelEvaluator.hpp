412032a1edb1ad7480d69e128691a99f29d45fd2
Roger Pawlowski <rppawlo@sandia.gov>
2014-01-24 11:52:43 -0500

    more documentation

--- packages/pike/blackbox/src/Pike_BlackBoxModelEvaluator.hpp
+++ packages/pike/blackbox/src/Pike_BlackBoxModelEvaluator.hpp
@@ -29,15 +29,27 @@ namespace pike {
      */
     virtual bool isLocallyConverged() const = 0;
 
-    /** \brief Optional function for assessing convergence of the
-     *	globally coupled problem.  Returns true if the metrics local
-     *	to this application are converged.  This allows for users to
-     *	track responses internally and determine convergence of the
-     *	global coupled system based on relative error of local
-     *	responses without having to expose the responses through a
-     *	pike::Response.  This is primarily for backwards
-     *	compatibility, new codes should use the StatusTest objects for
-     *	global convergence (and for checking the local convergence).
+    /** \brief Returns true if the metrics local to this application
+     *	consider the global coupled problem to be converged.
+
+     *  This method allows individual applications to track responses
+     *	internally and determine convergence of the global coupled
+     *	system.  For example, an application might monitor the
+     *	relative error of local responses between iterations without
+     *	having to expose the responses through the model evaluator
+     *	interface.  This is primarily for user convenience, codes
+     *	should really be using the StatusTest objects for checking
+     *	global convergence.
+
+     *  IMPORTANT NOTE: It is dangerous to use this function for
+     *	global convergence as opposed to pike::StatusTest objects
+     *	because it assume that each call to solve is for a new
+     *	iteration (i.e. assumes implicit knowledge of the solver
+     *	algorithm).  If a solve is called for some other purpose
+     *	(perturbing for sensitivities), then relative change of
+     *	internal responses is not a good indicator!  We strongly
+     *	recommend that you use pike::StatusTest objects for
+     *	determining global convergence!
      */
     virtual bool isGloballyConverged() const = 0;
 
