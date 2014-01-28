6d0e0465d0a595696c95af91e653ffd3c4fa4f94
Jonathan Hu <jhu@sandia.gov>
2014-01-28 11:23:15 -0800

    MueLu: python timeline files for MM analysis
    Use these w/ arcadia to pull out timers for the matrix matrix multiply.

---
+++ packages/muelu/test/scaling/hopper/python_scripts/EpetraIXtimelines.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+#epetra i&x details timelines
+LABELS       = ['ix-imp1', 'ix-imp2', 'ix-imp3', 'ix-imp4']
+TIMELINES    = ['MMM Ionly Import-1', 'MMM Ionly Import-2', 'MMM Ionly Import-3', 'MMM Ionly Import-4']
---
+++ packages/muelu/test/scaling/hopper/python_scripts/EpetraSmooPtimelines.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+#Epetra timelines for matrix matrix multiply in prolongator smoother
+LABELS    = ['i&x', 'cmap', 'lookups', 'serialCore', 'finalSort', 'fastIE', 'esfc']
+TIMELINES    = ['Jacobi All I&X', 'Jacobi Newmatrix CMap', 'Jacobi Newmatrix Lookups', 'Jacobi Newmatrix SerialCore', 'Jacobi Newmatrix Final Sort', 'Jacobi Newmatrix Fast IE', 'Jacobi Newmatrix ESFC']
