257a2a5178572186f04e27764232b012a0060163
Nathan Crane <nkcrane@sandia.gov>
2014-01-31 17:08:07 -0700

    Keep pulling apart Combo.h, getting close....
    (From Sierra)

--- packages/stk/stk_mesh/stk_mesh/base/Combo.hpp
+++ packages/stk/stk_mesh/stk_mesh/base/Combo.hpp
@@ -53,108 +53,10 @@ typedef boost::iterator_range<RelationIterator> RelationRange;
 // Relation/Entity/Bucket
 ///////////////////////////////////////////////////////////////////////////////
 
-namespace stk {
-namespace mesh {
-
-
-
-
-//
-// BucketConnectivity
-//
-
-template <EntityRank TargetRank>
-template <typename BulkData> // hack to get around dependency
-inline
-void impl::BucketConnectivity<TargetRank, FIXED_CONNECTIVITY>::end_modification(BulkData* mesh)
-{
-  //TODO: If bucket is blocked, no longer need to shrink to fit!
-
-  if (m_targets.size() < m_targets.capacity()) {
-
-    {
-      EntityVector temp(m_targets.begin(), m_targets.end());
-      m_targets.swap(temp);
-    }
-
-    {
-      PermutationVector temp(m_permutations.begin(), m_permutations.end());
-      m_permutations.swap(temp);
-    }
-  }
-
-  invariant_check_helper(mesh);
-}
-
-template <EntityRank TargetRank>
-template <typename BulkData>
-inline
-void impl::BucketConnectivity<TargetRank, DYNAMIC_CONNECTIVITY>::end_modification(BulkData* mesh)
-{
-  if (m_active && m_needs_shrink_to_fit) {
-    resize_and_order_by_index();
-
-    {
-      UInt32Vector temp(m_indices.begin(), m_indices.end());
-      m_indices.swap(temp);
-    }
-
-    {
-      UInt16Vector temp(m_num_connectivities.begin(), m_num_connectivities.end());
-      m_num_connectivities.swap(temp);
-    }
-
-    m_needs_shrink_to_fit = false;
-  }
-
-  invariant_check_helper(mesh);
-}
-
-template <typename BulkData>
-inline
-bool impl::LowerConnectivitityRankSensitiveCompare<BulkData>::operator()(Entity first_entity, ConnectivityOrdinal first_ordinal,
-                                                                         Entity second_entity, ConnectivityOrdinal second_ordinal) const
-{
-  const EntityRank first_rank = m_mesh.entity_rank(first_entity);
-  const EntityRank second_rank = m_mesh.entity_rank(second_entity);
-
-  return (first_rank < second_rank)
-         || ((first_rank == second_rank) && (first_ordinal < second_ordinal));
-}
-
-template <typename BulkData>
-inline
-bool impl::HigherConnectivityRankSensitiveCompare<BulkData>::operator()(Entity first_entity, ConnectivityOrdinal first_ordinal, Entity second_entity, ConnectivityOrdinal second_ordinal) const
-{
-  const EntityRank first_rank = m_mesh.entity_rank(first_entity);
-  const EntityRank second_rank = m_mesh.entity_rank(second_entity);
-
-  if (first_rank < second_rank) {
-    return true;
-  }
-  if (first_rank > second_rank) {
-    return false;
-  }
-  // Needs to match LessRelation in BulkData.hpp
-  return std::make_pair(first_ordinal,  first_entity.is_local_offset_valid() ?  first_entity.local_offset()  : Entity::MaxEntity) <
-         std::make_pair(second_ordinal, second_entity.is_local_offset_valid() ? second_entity.local_offset() : Entity::MaxEntity);
-}
-
-
-
-
-} // namespace mesh
-} // namespace stk
-
 
 #ifdef SIERRA_MIGRATION
 
-namespace stk {
-namespace mesh {
-struct Entity;
-class BulkData;
-}
-}
+namespace stk { namespace mesh { class BulkData;} }
 
 namespace sierra {
 namespace Fmwk {
