fd6e2b5838363fa9d2304542cbab7bfbe7cc3fe9
Greg Sjaardema <gdsjaar@sandia.gov>
2014-02-03 16:02:55 -0700

    SEACAS: aprepro - fix switch capability
    (From Sierra)
    
    Build/Test Cases Summary
    Enabled Packages: SEACASAprepro, SEACASAprepro_lib
    Disabled Packages: Panzer,PyTrilinos,SEACASExo2mat,SEACASMat2exo
    Enabled all Forward Packages
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_SS => passed: passed=3,notpassed=0 (1.95 min)
    3) SERIAL_RELEASE_SS => passed: passed=5,notpassed=0 (1.93 min)
    Other local commits for this build/test group: e78784d, 712f9d9

--- packages/seacas/libraries/aprepro_lib/apr_aprepro.cc
+++ packages/seacas/libraries/aprepro_lib/apr_aprepro.cc
@@ -15,7 +15,7 @@
 
 namespace {
   const unsigned int HASHSIZE = 5939;
-  const char* version_string = "4.03 (2014/02/03)";
+  const char* version_string = "4.04 (2014/02/03)";
   
   unsigned hash_symbol (const char *symbol)
   {
