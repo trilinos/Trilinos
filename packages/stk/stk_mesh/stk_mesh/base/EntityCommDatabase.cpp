22d75d18a82e69ceaa699409679384fe15ef2b82
Greg Sjaardema <gdsjaar@sandia.gov>
2014-02-28 08:27:43 -0700

    STK_MESH: Bug with inlining these on intel-13; move to non-inline since not needed to be inlined (found by Steve Kennon)
    (From Sierra)

--- packages/stk/stk_mesh/stk_mesh/base/EntityCommDatabase.cpp
+++ packages/stk/stk_mesh/stk_mesh/base/EntityCommDatabase.cpp
@@ -217,6 +217,206 @@ bool unpack_field_values(const BulkData& mesh,
 
 //----------------------------------------------------------------------
 
+bool EntityCommDatabase::cached_find(const EntityKey& key) const
+{
+  if (m_last_lookup != m_comm_map.end() && key == m_last_lookup->first) {
+    return true;
+  }
+
+  map_type::iterator find_it = const_cast<map_type&>(m_comm_map).find(key);
+  if (find_it == m_comm_map.end()) {
+    return false;
+  }
+  else {
+    m_last_lookup = find_it;
+    return true;
+  }
+}
+
+
+void EntityCommDatabase::insert(const EntityKey& key)
+{
+  if (!cached_find(key)) {
+    m_last_lookup = m_comm_map.insert(std::make_pair(key, EntityComm())).first;
+  }
+}
+
+
+int EntityCommDatabase::owner_rank( const EntityKey & key ) const
+{
+  if (!cached_find(key)) return InvalidProcessRank;
+
+  return m_last_lookup->second.owner_rank;
+}
+
+
+PairIterEntityComm EntityCommDatabase::sharing( const EntityKey & key ) const
+{
+  if (!cached_find(key)) return PairIterEntityComm();
+
+  const EntityCommInfoVector & comm_map = m_last_lookup->second.comm_map;
+
+  EntityCommInfoVector::const_iterator i = comm_map.begin();
+  EntityCommInfoVector::const_iterator e = comm_map.end();
+
+  e = std::lower_bound( i , e , EntityCommInfo(1,     // ghost id, 1->aura
+                                               0 ) ); // proc
+
+  // Contains everything up the first aura comm (IE, only contains shared comms)
+  return PairIterEntityComm( i , e );
+}
+
+
+PairIterEntityComm EntityCommDatabase::comm( const EntityKey & key ) const
+{
+  if (!cached_find(key)) return PairIterEntityComm();
+
+  const EntityCommInfoVector & comm_map = m_last_lookup->second.comm_map;
+  return PairIterEntityComm(comm_map);
+}
+
+
+PairIterEntityComm EntityCommDatabase::comm( const EntityKey & key, const Ghosting & sub ) const
+{
+  if (!cached_find(key)) return PairIterEntityComm();
+
+  const EntityCommInfoVector & comm_map = m_last_lookup->second.comm_map;
+
+  const EntityCommInfo s_begin( sub.ordinal() ,     0 );
+  const EntityCommInfo s_end(   sub.ordinal() + 1 , 0 );
+
+  EntityCommInfoVector::const_iterator i = comm_map.begin();
+  EntityCommInfoVector::const_iterator e = comm_map.end();
+
+  i = std::lower_bound( i , e , s_begin );
+  e = std::lower_bound( i , e , s_end );
+
+  return PairIterEntityComm( i , e );
+}
+
+
+bool EntityCommDatabase::insert( const EntityKey & key, const EntityCommInfo & val, int owner )
+{
+  TraceIfWatching("stk::mesh::EntityComm::insert", LOG_ENTITY, key);
+  DiagIfWatching(LOG_ENTITY, key, "owner " << owner);
+
+  insert(key);
+  EntityCommInfoVector & comm_map = m_last_lookup->second.comm_map;
+  m_last_lookup->second.owner_rank = owner;
+
+  EntityCommInfoVector::iterator i =
+    std::lower_bound( comm_map.begin() , comm_map.end() , val );
+
+  const bool result = ((i == comm_map.end()) || (val != *i));
+
+  if ( result ) {
+    comm_map.insert( i , val );
+  }
+
+  return result;
+}
+
+
+bool EntityCommDatabase::erase( const EntityKey & key, const EntityCommInfo & val )
+{
+  TraceIfWatching("stk::mesh::EntityComm::erase(comm)", LOG_ENTITY, key);
+
+  if (!cached_find(key)) return false;
+
+  EntityCommInfoVector & comm_map = m_last_lookup->second.comm_map;
+
+  EntityCommInfoVector::iterator i =
+    std::lower_bound( comm_map.begin() , comm_map.end() , val );
+
+  const bool result = ( (i != comm_map.end()) && (val == *i) ) ;
+
+  if ( result ) {
+    comm_map.erase( i );
+    if (comm_map.empty()) {
+      m_last_lookup = m_comm_map.erase(m_last_lookup);
+    }
+  }
+
+  return result ;
+}
+
+
+bool EntityCommDatabase::erase( const EntityKey & key, const Ghosting & ghost )
+{
+  TraceIfWatching("stk::mesh::EntityComm::erase(ghost)", LOG_ENTITY, key);
+
+  if (!cached_find(key)) return false;
+
+  EntityCommInfoVector & comm_map = m_last_lookup->second.comm_map;
+
+  const EntityCommInfo s_begin( ghost.ordinal() ,     0 );
+  const EntityCommInfo s_end(   ghost.ordinal() + 1 , 0 );
+
+  EntityCommInfoVector::iterator i = comm_map.begin();
+  EntityCommInfoVector::iterator e = comm_map.end();
+
+  i = std::lower_bound( i , e , s_begin );
+  e = std::lower_bound( i , e , s_end );
+
+  const bool result = i != e ;
+
+  if ( result ) {
+    comm_map.erase( i , e );
+    if (comm_map.empty()) {
+      m_last_lookup = m_comm_map.erase(m_last_lookup);
+    }
+  }
+
+  // if there is no more comm info, just remove it from the map?
+
+  return result ;
+}
+
+
+void EntityCommDatabase::comm_clear_ghosting(const EntityKey & key)
+{
+  TraceIfWatching("stk::mesh::EntityComm::comm_clear_ghosting", LOG_ENTITY, key);
+
+  if (!cached_find(key)) return;
+
+  EntityCommInfoVector & comm_map = m_last_lookup->second.comm_map;
+
+  EntityCommInfoVector::iterator j = comm_map.begin();
+  while ( j != comm_map.end() && j->ghost_id == 0 ) { ++j ; }
+  comm_map.erase( j , comm_map.end() );
+
+  if (comm_map.empty()) {
+    m_last_lookup = m_comm_map.erase(m_last_lookup);
+  }
+}
+
+
+void EntityCommDatabase::comm_clear(const EntityKey & key)
+{
+  TraceIfWatching("stk::mesh::EntityComm::comm_clear", LOG_ENTITY, key);
+
+  if (!cached_find(key)) return;
+
+  m_last_lookup = m_comm_map.erase(m_last_lookup);
+}
+
+
+bool EntityCommDatabase::change_owner_rank(const EntityKey& key, int owner)
+{
+  // Do not add key to map, only update rank if it was already in the map
+  if (cached_find(key)) {
+    TraceIfWatching("stk::mesh::EntityComm::change_owner_rank", LOG_ENTITY, key);
+    DiagIfWatching(LOG_ENTITY, key, "new owner " << owner);
+
+    const int orig_owner = m_last_lookup->second.owner_rank;
+    m_last_lookup->second.owner_rank = owner;
+    return orig_owner != owner;
+  }
+  return false;
+}
+
+
+
 } // namespace mesh
 }
 
