563c2f0935572d7f236fc1f9b99968f56cf7176f
Greg Sjaardema <gdsjaar@sandia.gov>
2014-03-04 10:33:25 -0700

    STK_MESH: A "fixture" which creates a mesh with 2 degenerate hexes for use in create faces unit tests
    (From Sierra)

---
+++ packages/stk/stk_mesh/stk_mesh/fixtures/degenerate_mesh.cpp
@@ -0,0 +1,147 @@
+/*------------------------------------------------------------------------*/
+/*                 Copyright 2014 Sandia Corporation.                     */
+/*  Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive   */
+/*  license for use of this work by or on behalf of the U.S. Government.  */
+/*  Export of this program may require a license from the                 */
+/*  United States Government.                                             */
+/*------------------------------------------------------------------------*/
+
+#include <vector>
+#include <stdexcept>
+#include <sstream>
+
+#include <stk_mesh/fixtures/degenerate_mesh.hpp>
+#include <Shards_BasicTopologies.hpp>
+
+#include <stk_util/parallel/Parallel.hpp>
+
+#include <stk_mesh/base/FindRestriction.hpp>
+#include <stk_mesh/base/MetaData.hpp>
+#include <stk_mesh/base/BulkData.hpp>
+#include <stk_mesh/base/Entity.hpp>
+#include <stk_mesh/base/Field.hpp>
+#include <stk_mesh/base/CoordinateSystems.hpp>
+#include <stk_mesh/base/Comm.hpp>
+#include <stk_mesh/base/Stencils.hpp>
+#include <stk_mesh/base/TopologyDimensions.hpp>
+#include <stk_mesh/base/FEMHelpers.hpp>
+
+namespace stk {
+  namespace mesh {
+    namespace fixtures {
+
+      enum { SpatialDim = 3 };
+
+      //----------------------------------------------------------------------
+
+      //--------------------------------------------------------------------
+      //
+
+      void degenerate_mesh_meta_data(
+				     stk::mesh::MetaData & meta_data ,
+				     VectorFieldType & node_coord )
+      {
+	stk::mesh::Part & universal        = meta_data.universal_part();
+	declare_part<shards::Hexahedron<8> >(meta_data, "hexes");
+  
+	const stk::mesh::FieldBase::Restriction & res =
+	  stk::mesh::find_restriction(node_coord, stk::topology::NODE_RANK , universal );
+
+	if ( res.num_scalars_per_entity() != 3 ) {
+	  std::ostringstream msg ;
+	  msg << "stk_mesh/unit_tests/degenerate_mesh_meta_data FAILED, coordinate dimension must be 3 != "
+	      << res.num_scalars_per_entity() ;
+	  throw std::runtime_error( msg.str() );
+	}
+      }
+
+      //--------------------------------------------------------------------
+      /*----------------------------------------------------------------------
+       * Degenerate hex mesh generation
+       *
+       * 12 hexes degenerated down to wedges.
+       *
+       *----------------------------------------------------------------------*/
+
+      namespace {
+
+	enum { node_count = 10 };
+	enum { number_hex = 2 };
+
+	/*  Z = 0 plane:
+	 *
+	 *    Y
+	 *    ^ 
+	 *    !
+	 *     
+	 *   4*       *5
+	 *    |\     /|
+	 *    | \   / |
+	 *    |  \ /  |
+	 *    *---*---* ----> X
+	 *    1   2   3
+	 *
+	 *  Z = -1 plane:
+	 *
+	 *    Y
+	 *    ^ 
+	 *    !
+	 *     
+	 *   9*       *10
+	 *    |\     /|
+	 *    | \   / |
+	 *    |  \ /  |
+	 *    *---*---* ----> X
+	 *    6   7   8
+	 *
+	 */
+
+	static const double node_coord_data[ node_count ][ SpatialDim ] = {
+	  { 0 , 0 , 0 } , { 1 , 0 , 0 } , { 2, 0, 0} , {0, 1, 0}, {2, 1, 0}, 
+	  { 0 , 0 ,-1 } , { 1 , 0 ,-1 } , { 2, 0,-1} , {0, 1,-1}, {2, 1,-1}};
+
+	static const stk::mesh::EntityId hex_node_ids[number_hex][ shards::Hexahedron<8> ::node_count ] = {
+	  { 1, 2, 7, 6, 4, 2,  7, 9}, 
+	  { 2, 3, 8, 7, 2, 5, 10, 7}};
+
+      }
+      //----------------------------------------------------------------------
+      //----------------------------------------------------------------------
+
+      void degenerate_mesh_bulk_data(
+				     stk::mesh::BulkData & bulk_data ,
+				     const VectorFieldType & node_coord )
+      {
+	static const char method[] =
+	  "stk_mesh::fixtures::heterogenous_mesh_bulk_data" ;
+
+	bulk_data.modification_begin();
+
+	const stk::mesh::MetaData & meta_data = stk::mesh::MetaData::get(bulk_data);
+
+	stk::mesh::Part & hex_block        = * meta_data.get_part("hexes",method);
+
+	unsigned elem_id = 1 ;
+
+	for ( unsigned i = 0 ; i < number_hex ; ++i , ++elem_id ) {
+	  stk::mesh::declare_element( bulk_data, hex_block, elem_id, hex_node_ids[i] );
+	}
+
+	for ( unsigned i = 0 ; i < node_count ; ++i ) {
+
+	  stk::mesh::Entity const node = bulk_data.get_entity( stk::topology::NODE_RANK , i + 1 );
+
+	  double * const coord = stk::mesh::field_data( node_coord , node );
+
+	  coord[0] = node_coord_data[i][0] ;
+	  coord[1] = node_coord_data[i][1] ;
+	  coord[2] = node_coord_data[i][2] ;
+	}
+
+	bulk_data.modification_end();
+      }
+
+    }}}
+
+//----------------------------------------------------------------------
+
