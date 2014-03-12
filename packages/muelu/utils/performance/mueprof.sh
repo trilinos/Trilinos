ce1c03cf1c4a7057c0d6b90c4bb08c59b0fc26b0
Jonathan Hu <jhu@sandia.gov>
2014-03-11 18:22:35 -0700

    MueLu:  new mueprof option for comparing
    -a allow comparison of two Epetra output files or two Tpetra output files
    
    example:  mueprof.sh -a -n 100 epetra1.out epetra2.out

--- packages/muelu/utils/performance/mueprof.sh
+++ packages/muelu/utils/performance/mueprof.sh
@@ -18,15 +18,20 @@ SCRIPTPATH=$(dirname $SCRIPT)
 USAGE="Usage: `basename $0` [-h] [ARGS] file1 file2"
 OPTDESCR="\n\t-h \thelp\n\t-n \twhich solver block to analyze [1]\n
           \t-s \twhich linear algebra lib to sort by: \"Epetra\",[\"Tpetra\"]\n
-          \t-d \thow to display delta in times: \"ratio\",[\"diff\"]\n"
+          \t-d \thow to display delta in times: \"ratio\",[\"diff\"]\n
+          \t-a \tallows comparison of two Epetra or two Tpetra files\n"
 
 numReqd=1;
 blockNumber=1;
 sortByLib="Tpetra"
 deltaDisplay="diff"
+agnostic=0
 # Parse command line options.
-while getopts hn:s:d: OPT; do
+while getopts hn:s:d:a OPT; do
     case "$OPT" in
+        a)
+            agnostic=1
+            ;;
         d)
             deltaDisplay=$OPTARG
             ;;
@@ -75,4 +80,4 @@ export AWKPATH
 #ttt=`awk --version`
 #echo "awk info: $ttt"
 
-awk -v "blockNumber=$blockNumber" -v "sortByLib=$sortByLib" -v "etDelta=$deltaDisplay" -f $SCRIPTPATH/mueprof.awk $file1 $file2
+awk -v "blockNumber=$blockNumber" -v "sortByLib=$sortByLib" -v "etDelta=$deltaDisplay" -v "agnostic=$agnostic" -f $SCRIPTPATH/mueprof.awk $file1 $file2
