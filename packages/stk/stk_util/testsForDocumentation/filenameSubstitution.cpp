07075d7e535f7c188778684b0f8d5feb44545cf3
cwilson <cwilson@sandia.gov>
2014-03-18 10:22:40 -0600

    Add a test that shows using filename_substitution with filename coming from command line args.
    (From Sierra)

--- packages/stk/stk_util/testsForDocumentation/filenameSubstitution.cpp
+++ packages/stk/stk_util/testsForDocumentation/filenameSubstitution.cpp
@@ -1,16 +1,41 @@
 #include <gtest/gtest.h>
 #include <string>
 #include <mpi.h>
+#include <stk_util/environment/ProgramOptions.hpp>
+#include <stk_util/diag/Env.hpp>
 #include <stk_util/diag/FileUtils.hpp>
 
 namespace
 {
-  TEST(StkUtilHowTo, filenameSubstitution)
+  TEST(StkUtilHowTo, useFilenameSubstitutionWithNoCommandLineOptions)
   {
-    std::string file_name_to_change_in_place = "%B-%P.txt";
-    std::string expected_result = "stdin-1.txt";
-    
-    stk::util::filename_substitution(file_name_to_change_in_place);
-    EXPECT_STREQ(file_name_to_change_in_place.c_str(), expected_result.c_str());
+    const std::string default_base_filename = "stdin";
+    const std::string numProcsString = "1";
+    const std::string expected_filename = default_base_filename + "-" + numProcsString + ".e";
+
+    std::string file_name = "%B-%P.e";
+    stk::util::filename_substitution(file_name);
+    EXPECT_EQ(expected_filename, file_name);
+  }
+
+  void setFilenameInCommandLineOptions(const std::string &filename)
+  {
+    boost::program_options::variables_map &command_line_options = stk::get_variables_map();
+    command_line_options.insert(std::make_pair("input-deck", boost::program_options::variable_value(filename, false)));
+    sierra::Env::setInputFileName(filename);
+  }
+  TEST(StkUtilHowTo, useFilenameSubstitutionWithFileComingFromCommandLineOptions)
+  {
+    const std::string base_filename = "myfile";
+    const std::string full_filename = "/path/to/" + base_filename + ".g";
+    setFilenameInCommandLineOptions(full_filename);
+
+    const std::string numProcsString = "1";
+    const std::string expected_filename = base_filename + "-" + numProcsString + ".e";
+
+    std::string file_name = "%B-%P.e";
+    stk::util::filename_substitution(file_name);
+
+    EXPECT_EQ(expected_filename, file_name);
   }
 }
