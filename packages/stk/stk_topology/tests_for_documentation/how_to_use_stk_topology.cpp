22d8c25620ffe5db2cd05f8306720a912af5dcc8
Manoj K Bhardwaj <mkbhard@sandia.gov>
2014-03-20 14:14:09 -0600

    Adding super element doc test.
    (From Sierra)

--- packages/stk/stk_topology/tests_for_documentation/how_to_use_stk_topology.cpp
+++ packages/stk/stk_topology/tests_for_documentation/how_to_use_stk_topology.cpp
@@ -553,6 +553,87 @@ TEST(stk_topology_understanding, lexicographical_smallest_permutation)
     }
 }
 
+TEST(stk_topology_understanding, sub_topology)
+{
+    stk::topology hex20 = stk::topology::HEX_20;
+    unsigned hex20Nodes[20] = {
+            0, 1, 2, 3,
+            4, 5, 6, 7,
+            8, 9, 10, 11,
+            12, 13, 14, 15,
+            16, 17, 18, 19
+    };
+
+    unsigned numFaces = hex20.num_sub_topology(stk::topology::FACE_RANK);
+    EXPECT_EQ(6u, numFaces);
+
+    unsigned faceIndex=2;
+    stk::topology top = hex20.sub_topology(stk::topology::FACE_RANK, faceIndex);
+    EXPECT_EQ(stk::topology::QUADRILATERAL_8, top);
+
+    unsigned nodeIdsFace[8];
+    hex20.sub_topology_nodes(hex20Nodes, stk::topology::FACE_RANK, faceIndex, nodeIdsFace);
+
+    unsigned goldIdsFace[8] = { 2, 3, 7, 6, 10, 15, 18, 14 };
+    for (unsigned i=0;i<hex20.face_topology(faceIndex).num_nodes();i++)
+    {
+        EXPECT_EQ(goldIdsFace[i], nodeIdsFace[i]);
+    }
+}
+
+TEST(stk_topology_understanding, sides)
+{
+    stk::topology hex20 = stk::topology::HEX_20;
+    EXPECT_EQ(6u, hex20.num_sides());
+
+    stk::topology quad8 = stk::topology::SHELL_QUADRILATERAL_8;
+    EXPECT_EQ(2u, quad8.num_sides());
+
+    stk::topology wedge = stk::topology::WEDGE_15;
+    EXPECT_EQ(5u, wedge.num_sides());
+    EXPECT_EQ(stk::topology::QUADRILATERAL_8, wedge.side_topology(0));
+    EXPECT_EQ(stk::topology::QUADRILATERAL_8, wedge.side_topology(1));
+    EXPECT_EQ(stk::topology::QUADRILATERAL_8, wedge.side_topology(2));
+    EXPECT_EQ(stk::topology::TRIANGLE_6, wedge.side_topology(3));
+    EXPECT_EQ(stk::topology::TRIANGLE_6, wedge.side_topology(4));
+
+}
+
+TEST(stk_topology_understanding, superelements)
+{
+    unsigned eightNodes=8;
+    stk::topology validSuperElement = stk::create_superelement_topology(eightNodes);
+    EXPECT_TRUE(validSuperElement.is_superelement());
+    EXPECT_TRUE(stk::topology::ELEMENT_RANK == validSuperElement.rank());
+    EXPECT_EQ(eightNodes, validSuperElement.num_nodes());
+    EXPECT_EQ(0u, validSuperElement.num_edges());
+    EXPECT_EQ(0u, validSuperElement.num_faces());
+    EXPECT_EQ(0u, validSuperElement.num_permutations());
+    EXPECT_EQ(0u, validSuperElement.num_sides());
+    EXPECT_EQ(0u, validSuperElement.dimension());
+    EXPECT_EQ(stk::topology::INVALID_TOPOLOGY, validSuperElement.face_topology());
+    EXPECT_EQ(stk::topology::INVALID_TOPOLOGY, validSuperElement.edge_topology());
+    EXPECT_EQ(stk::topology::INVALID_TOPOLOGY, validSuperElement.base());
+    EXPECT_FALSE(validSuperElement.has_homogeneous_faces());
+    EXPECT_FALSE(validSuperElement.is_shell());
+
+    unsigned zeroNodes=0;
+    stk::topology invalidSuperElement = stk::create_superelement_topology(zeroNodes);
+    EXPECT_FALSE(invalidSuperElement.is_superelement());
+    EXPECT_TRUE(stk::topology::INVALID_RANK == invalidSuperElement.rank());
+    EXPECT_EQ(zeroNodes, invalidSuperElement.num_nodes());
+    EXPECT_EQ(0u, invalidSuperElement.num_edges());
+    EXPECT_EQ(0u, invalidSuperElement.num_faces());
+    EXPECT_EQ(0u, invalidSuperElement.num_permutations());
+    EXPECT_EQ(0u, invalidSuperElement.num_sides());
+    EXPECT_EQ(0u, invalidSuperElement.dimension());
+    EXPECT_EQ(stk::topology::INVALID_TOPOLOGY, invalidSuperElement.face_topology());
+    EXPECT_EQ(stk::topology::INVALID_TOPOLOGY, invalidSuperElement.edge_topology());
+    EXPECT_EQ(stk::topology::INVALID_TOPOLOGY, invalidSuperElement.base());
+    EXPECT_FALSE(invalidSuperElement.has_homogeneous_faces());
+    EXPECT_FALSE(invalidSuperElement.is_shell());
+}
+
 }
 
 // stk_mesh should get away from generic entity, the entity keyword altogether, and use words
