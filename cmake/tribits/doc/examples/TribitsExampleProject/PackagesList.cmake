3c6e5e8566d55253649a6c69b02eb726a115f90b
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-03-20 17:09:13 -0400

    Making WrapExternal PT but putting in auto-disables for when it is known not to work

--- cmake/tribits/doc/examples/TribitsExampleProject/PackagesList.cmake
+++ cmake/tribits/doc/examples/TribitsExampleProject/PackagesList.cmake
@@ -2,5 +2,7 @@ TRIBITS_DEFINE_REPOSITORY_PACKAGES(
   SimpleCxx               packages/simple_cxx                 PT
   MixedLanguage           packages/mixed_language             PT
   PackageWithSubpackages  packages/package_with_subpackages   PT
-  WrapExternal            packages/wrap_external              EX
+  WrapExternal            packages/wrap_external              PT
   )
+
+TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(WrapExternal Windows)
