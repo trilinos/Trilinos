a6cfb1997e90fbdc2c491cf69ee2e6129b1ba296
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-03-25 11:09:42 -0400

    Small comment fixes

--- cmake/tribits/package_arch/TribitsGetExtraReposForCheckinTest.cmake
+++ cmake/tribits/package_arch/TribitsGetExtraReposForCheckinTest.cmake
@@ -42,7 +42,6 @@
 # info for extra repos read from a file
 #
 
-
 #
 # A) Get the input
 #
@@ -57,7 +56,7 @@ MESSAGE("EXTRA_REPOS_PYTHON_OUT_FILE = ${EXTRA_REPOS_PYTHON_OUT_FILE}")
 MESSAGE("ENABLE_KNOWN_EXTERNAL_REPOS_TYPE = ${ENABLE_KNOWN_EXTERNAL_REPOS_TYPE}")
 MESSAGE("IGNORE_MISSING_EXTRA_REPOSITORIES = ${IGNORE_MISSING_EXTRA_REPOSITORIES}")
 
-# Set up necessary varaibles
+# Set up necessary variables
 INCLUDE(${PROJECT_SOURCE_DIR}/ProjectName.cmake)
 MESSAGE("PROJECT_NAME = ${PROJECT_NAME}")
 SET(${PROJECT_NAME}_TRIBITS_DIR ${TRIBITS_BASE_DIR})
@@ -149,7 +148,7 @@ APPEND_STRING_VAR_EXT(EXTRA_REPOS_PYTHON_STRING
   "]\n")  
 
 #
-# G) Write the generated python list/dictionary string to the output file
+# E) Write the generated python list/dictionary string to the output file
 #
 # NOTE: This must be the only output from this script since it gets evaluated.
 #
