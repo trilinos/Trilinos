a432d7d08eae4ae4941c3fd4bc9481940dea7a91
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-03-28 09:28:46 -0400

    Removed copyright header to allow include in doc.

--- cmake/tribits/package_arch/UnitTests/MockTrilinos/PackagesList.cmake
+++ cmake/tribits/package_arch/UnitTests/MockTrilinos/PackagesList.cmake
@@ -1,46 +1,3 @@
-# @HEADER
-# ************************************************************************
-#
-#            TriBITS: Tribial Build, Integrate, and Test System
-#                    Copyright 2013 Sandia Corporation
-#
-# Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
-# the U.S. Government retains certain rights in this software.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are
-# met:
-#
-# 1. Redistributions of source code must retain the above copyright
-# notice, this list of conditions and the following disclaimer.
-#
-# 2. Redistributions in binary form must reproduce the above copyright
-# notice, this list of conditions and the following disclaimer in the
-# documentation and/or other materials provided with the distribution.
-#
-# 3. Neither the name of the Corporation nor the names of the
-# contributors may be used to endorse or promote products derived from
-# this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-# PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-# EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-# PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-# PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-# NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-#
-# ************************************************************************
-# @HEADER
-
-# This list is just used for unit testing the dependency handling
-# CMake code.  The reason that we have a separate list is so that we
-# can keep very stable unit tests.
-
 TRIBITS_DEFINE_REPOSITORY_PACKAGES(
   TrilinosFramework   cmake                           PT
   Teuchos             packages/teuchos                PT
@@ -68,8 +25,8 @@ TRIBITS_DEFINE_REPOSITORY_PACKAGES(
   Panzer              packages/panzer                 ST
   )
 
-# NOTE: Sacado is really PT but for testing purpose it is made ST
-# NOTE: Belos is really PT but for testing purpose it is made ST
+# NOTE: Sacado was really PT but for testing purpose it is made ST
+# NOTE: Belos was really PT but for testing purpose it is made ST
 
 TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(ML BadSystem1)
 TRIBITS_DISABLE_PACKAGE_ON_PLATFORMS(Ifpack BadSystem1 BadSystem2)
