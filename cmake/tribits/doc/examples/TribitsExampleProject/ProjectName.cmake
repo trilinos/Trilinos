ade9429024334a5dd46701894e99041097117ae5
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-03-28 07:14:17 -0400

    Fix and shorten comment.

--- cmake/tribits/doc/examples/TribitsExampleProject/ProjectName.cmake
+++ cmake/tribits/doc/examples/TribitsExampleProject/ProjectName.cmake
@@ -1,2 +1,2 @@
-# Must set the project name as a varaible at very beginning before including anything else
+# Must set the project name at very beginning before including anything else
 SET(PROJECT_NAME TribitsExProj)
