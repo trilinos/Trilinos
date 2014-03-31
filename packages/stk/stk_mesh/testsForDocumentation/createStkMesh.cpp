700c1180ef48b51ccb43394d9c6bc17e25e42a65
Alan Williams <william@sandia.gov>
2014-03-31 09:11:56 -0600

    Improve one of the documentation tests.
    (From Sierra)

--- packages/stk/stk_mesh/testsForDocumentation/createStkMesh.cpp
+++ packages/stk/stk_mesh/testsForDocumentation/createStkMesh.cpp
@@ -1,4 +1,3 @@
-//-BEGIN
 #include <gtest/gtest.h>                // for AssertHelper, EXPECT_EQ, etc
 #include <mpi.h>                        // for MPI_COMM_WORLD, MPI_Comm, etc
 #include <stk_io/StkMeshIoBroker.hpp>   // for StkMeshIoBroker
@@ -13,47 +12,33 @@
 
 namespace
 {
+//-BEGIN    
   TEST(StkMeshHowTo, UsingStkIO)
   {
     MPI_Comm communicator = MPI_COMM_WORLD;
-    const std::string exodusFileName = "example.exo";
-
-//-END
-    {
-      // ============================================================
-      //+ INITIALIZATION:
-      //+ Create a mesh 
-      stk::io::StkMeshIoBroker stkIo(communicator);
-
-      const std::string generatedFileName = "generated:8x8x8";
-      size_t index = stkIo.add_mesh_database(generatedFileName, stk::io::READ_MESH);
-      stkIo.set_active_mesh(index);
-      stkIo.create_input_mesh();
-      stkIo.populate_bulk_data();
-
-      size_t fh = stkIo.create_output_mesh(exodusFileName, stk::io::WRITE_RESULTS);
-      stkIo.write_output_mesh(fh);
-    }
-    //-BEGIN    
+    const std::string fileName = "generated:8x8x8";
+    //The 'generated:' syntax in fileName causes a hex mesh to be generated in memory.
+    //If an exodus file-name is used instead, then the mesh is read from file. The code
+    //below remains the same in either case.
 
     // Use STK IO to populate a STK Mesh
-    stk::io::StkMeshIoBroker exodusFileReader(communicator);
-    exodusFileReader.add_mesh_database(exodusFileName, stk::io::READ_MESH);
+    stk::io::StkMeshIoBroker meshReader(communicator);
+    meshReader.add_mesh_database(fileName, stk::io::READ_MESH);
     // Create MetaData in STK IO
-    exodusFileReader.create_input_mesh();
+    meshReader.create_input_mesh();
     // Create BulkData in STK IO
-    exodusFileReader.populate_bulk_data();
+    meshReader.populate_bulk_data();
 
     // Get the STK Mesh (BulkData and MetaData) from STK IO
-    stk::mesh::MetaData &stkMeshMetaData = exodusFileReader.meta_data();
-    stk::mesh::BulkData &stkMeshBulkData = exodusFileReader.bulk_data();
+    stk::mesh::MetaData &stkMeshMetaData = meshReader.meta_data();
+    stk::mesh::BulkData &stkMeshBulkData = meshReader.bulk_data();
 
     // Test if the STK Mesh has 512 elements. Other examples will discuss details below.
     stk::mesh::Selector allEntities = stkMeshMetaData.universal_part();
     std::vector<unsigned> entityCounts;
     stk::mesh::count_entities(allEntities, stkMeshBulkData, entityCounts);
     EXPECT_EQ(512u, entityCounts[stk::topology::ELEMENT_RANK]);
-    unlink(exodusFileName.c_str());
+    unlink(fileName.c_str());
   }
-}
 //-END
+}
