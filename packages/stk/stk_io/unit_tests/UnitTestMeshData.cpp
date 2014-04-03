be7ecb60594028b98100807fb60ed1b1767edb6e
Greg Sjaardema <gdsjaar@sandia.gov>
2014-04-03 07:02:42 -0600

    STK_IO: Missing mesh file will now cause failure and not core dump -- rest of tests will now run
    (From Sierra)

--- packages/stk/stk_io/unit_tests/UnitTestMeshData.cpp
+++ packages/stk/stk_io/unit_tests/UnitTestMeshData.cpp
@@ -55,26 +55,30 @@ STKUNIT_UNIT_TEST( StkMeshIoBroker, iofixture )
 
   std::string input_base_filename = "unit_test.g";
 
-  // Initialize meta data from exodus file
-  fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
-  fixture.create_input_mesh();
+  try {
+    // Initialize meta data from exodus file
+    fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
+    fixture.create_input_mesh();
 
-  stk::mesh::MetaData & meta_data = fixture.meta_data();
+    stk::mesh::MetaData & meta_data = fixture.meta_data();
 
-  // Commit meta_data
-  meta_data.commit();
+    // Commit meta_data
+    meta_data.commit();
 
-  // bulk_data initialize (from exodus file)
-  fixture.populate_bulk_data();
+    // bulk_data initialize (from exodus file)
+    fixture.populate_bulk_data();
 
-  // exodus file creation
-  std::string output_base_filename = "unit_test_output.e";
-  size_t output_index = fixture.create_output_mesh(output_base_filename, stk::io::WRITE_RESULTS);
+    // exodus file creation
+    std::string output_base_filename = "unit_test_output.e";
+    size_t output_index = fixture.create_output_mesh(output_base_filename, stk::io::WRITE_RESULTS);
 
-  // process output
-  const double time_step = 0;
-  fixture.process_output_request(output_index, time_step);
-
+    // process output
+    const double time_step = 0;
+    fixture.process_output_request(output_index, time_step);
+  }
+  catch(...) {
+    ASSERT_TRUE(1==0 && "Could not open unit test file");
+  }
   // Since correctness can only be established by running SEACAS tools, correctness
   // checking is left to the test XML.
 }
@@ -88,34 +92,39 @@ STKUNIT_UNIT_TEST( StkMeshIoBroker, active_only )
 
   std::string input_base_filename = "unit_test.g";
 
-  // Initialize meta data from exodus file
-  fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
+  try {
+    // Initialize meta data from exodus file
+    fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
 
-  fixture.create_input_mesh();
-  stk::mesh::MetaData & meta_data = fixture.meta_data();
+    fixture.create_input_mesh();
+    stk::mesh::MetaData & meta_data = fixture.meta_data();
 
-  // Add an "active" part...
-  stk::mesh::Part &active = meta_data.declare_part("active", stk::topology::ELEMENT_RANK);
-  meta_data.commit();
+    // Add an "active" part...
+    stk::mesh::Part &active = meta_data.declare_part("active", stk::topology::ELEMENT_RANK);
+    meta_data.commit();
 
-  // bulk_data initialize (from exodus file)
-  fixture.populate_bulk_data();
+    // bulk_data initialize (from exodus file)
+    fixture.populate_bulk_data();
 
-  // Put some entities into the "active" part...
-  // This will be used to test the I/O filtering via a selector...
-  activate_entities(fixture, active);
+    // Put some entities into the "active" part...
+    // This will be used to test the I/O filtering via a selector...
+    activate_entities(fixture, active);
 
-  // exodus file creation
-  std::string output_base_filename = "unit_test_output_filtered.e";
-  size_t index = fixture.create_output_mesh( output_base_filename, stk::io::WRITE_RESULTS );
+    // exodus file creation
+    std::string output_base_filename = "unit_test_output_filtered.e";
+    size_t index = fixture.create_output_mesh( output_base_filename, stk::io::WRITE_RESULTS );
 
-  // Set the output filter on the mesh_data...
-  stk::mesh::Selector active_selector(active);
-  fixture.set_subset_selector(index, active_selector);
+    // Set the output filter on the mesh_data...
+    stk::mesh::Selector active_selector(active);
+    fixture.set_subset_selector(index, active_selector);
 
-  // process output
-  const double time_step = 0;
-  fixture.process_output_request(index, time_step);
+    // process output
+    const double time_step = 0;
+    fixture.process_output_request(index, time_step);
+  }
+  catch(...) {
+    ASSERT_TRUE(1==0 && "Could not open unit test file");
+  }
 
 
   // Since correctness can only be established by running SEACAS tools, correctness
@@ -130,51 +139,56 @@ STKUNIT_UNIT_TEST( StkMeshIoBroker, active_and_all )
 
   std::string input_base_filename = "unit_test.g";
 
-  fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
+  try {
+    fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
   
-  fixture.create_input_mesh();
-  stk::mesh::MetaData & meta_data = fixture.meta_data();
+    fixture.create_input_mesh();
+    stk::mesh::MetaData & meta_data = fixture.meta_data();
 
-  // Add an "active" part...
-  stk::mesh::Part &active = meta_data.declare_part("active", stk::topology::ELEMENT_RANK);
-  meta_data.commit();
+    // Add an "active" part...
+    stk::mesh::Part &active = meta_data.declare_part("active", stk::topology::ELEMENT_RANK);
+    meta_data.commit();
 
-  // bulk_data initialize (from exodus file)
-  fixture.populate_bulk_data();
+    // bulk_data initialize (from exodus file)
+    fixture.populate_bulk_data();
 
-  // Put some entities into the "active" part...
-  // This will be used to test the I/O filtering via a selector...
-  activate_entities(fixture, active);
+    // Put some entities into the "active" part...
+    // This will be used to test the I/O filtering via a selector...
+    activate_entities(fixture, active);
 
-  // exodus file creation
-  std::string filtered_output_base_filename = "unit_test_output_first_of_two.e";
-  std::string unfiltered_output_base_filename = "unit_test_output_second_of_two.e";
-  size_t filtered_index =  fixture.create_output_mesh( filtered_output_base_filename, stk::io::WRITE_RESULTS );
-  size_t universal_index = fixture.create_output_mesh( unfiltered_output_base_filename, stk::io::WRITE_RESULTS );
+    // exodus file creation
+    std::string filtered_output_base_filename = "unit_test_output_first_of_two.e";
+    std::string unfiltered_output_base_filename = "unit_test_output_second_of_two.e";
+    size_t filtered_index =  fixture.create_output_mesh( filtered_output_base_filename, stk::io::WRITE_RESULTS );
+    size_t universal_index = fixture.create_output_mesh( unfiltered_output_base_filename, stk::io::WRITE_RESULTS );
 
-  // Set the output filter on the mesh_data...
-  // Only output the part declared above as "active"
-  stk::mesh::Selector active_selector(active);
-  fixture.set_subset_selector(filtered_index, active_selector);
+    // Set the output filter on the mesh_data...
+    // Only output the part declared above as "active"
+    stk::mesh::Selector active_selector(active);
+    fixture.set_subset_selector(filtered_index, active_selector);
 
-  // process output
-  double time_step = 0;
-  fixture.process_output_request(filtered_index,  time_step);
-  fixture.process_output_request(universal_index, time_step);
+    // process output
+    double time_step = 0;
+    fixture.process_output_request(filtered_index,  time_step);
+    fixture.process_output_request(universal_index, time_step);
 
-  ++time_step;
+    ++time_step;
 
-  fixture.process_output_request(filtered_index,  time_step);
-  fixture.process_output_request(universal_index, time_step);
+    fixture.process_output_request(filtered_index,  time_step);
+    fixture.process_output_request(universal_index, time_step);
 
-  ++time_step;
+    ++time_step;
 
-  fixture.process_output_request(filtered_index,  time_step);
-  fixture.process_output_request(universal_index, time_step);
+    fixture.process_output_request(filtered_index,  time_step);
+    fixture.process_output_request(universal_index, time_step);
+  }
+  catch(...) {
+    ASSERT_TRUE(1==0 && "Could not open unit test file");
+  }
 
   // Since correctness can only be established by running SEACAS tools, correctness
   // checking is left to the test XML.
- }
+}
 
 STKUNIT_UNIT_TEST( StkMeshIoBroker, large_mesh_test )
 {
@@ -184,32 +198,37 @@ STKUNIT_UNIT_TEST( StkMeshIoBroker, large_mesh_test )
 
   std::string input_base_filename = "1mCube_20x20x20.g";
 
-  // Initialize meta data from exodus file
-  fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
+  try {
+    // Initialize meta data from exodus file
+    fixture.add_mesh_database(input_base_filename, stk::io::READ_MESH);
 
-  fixture.create_input_mesh();
-  stk::mesh::MetaData & meta_data = fixture.meta_data();
+    fixture.create_input_mesh();
+    stk::mesh::MetaData & meta_data = fixture.meta_data();
 
-  // Commit
-  meta_data.commit();
+    // Commit
+    meta_data.commit();
 
-  // bulk_data initialize (from exodus file)
-  fixture.populate_bulk_data();
-  stk::mesh::BulkData &bulk_data = fixture.bulk_data();
+    // bulk_data initialize (from exodus file)
+    fixture.populate_bulk_data();
+    stk::mesh::BulkData &bulk_data = fixture.bulk_data();
 
-  const stk::mesh::BucketVector & element_buckets
-    = bulk_data.buckets( stk::topology::ELEMENT_RANK);
+    const stk::mesh::BucketVector & element_buckets
+      = bulk_data.buckets( stk::topology::ELEMENT_RANK);
 
-  // iterate elements and check num nodal relations
-  for ( stk::mesh::BucketVector::const_iterator ib = element_buckets.begin() ;
-        ib != element_buckets.end() ; ++ib ) {
-    stk::mesh::Bucket & b = **ib ;
-    const int length   = b.size();
-    for ( int k = 0 ; k < length ; ++k ) {
-      // get element
-      stk::mesh::Entity elem = b[k];
-      size_t num_elem_node_rels = bulk_data.count_valid_connectivity(elem, stk::topology::NODE_RANK);
-      STKUNIT_EXPECT_EQ( 8u, num_elem_node_rels);
+    // iterate elements and check num nodal relations
+    for ( stk::mesh::BucketVector::const_iterator ib = element_buckets.begin() ;
+	  ib != element_buckets.end() ; ++ib ) {
+      stk::mesh::Bucket & b = **ib ;
+      const int length   = b.size();
+      for ( int k = 0 ; k < length ; ++k ) {
+	// get element
+	stk::mesh::Entity elem = b[k];
+	size_t num_elem_node_rels = bulk_data.count_valid_connectivity(elem, stk::topology::NODE_RANK);
+	STKUNIT_EXPECT_EQ( 8u, num_elem_node_rels);
+      }
     }
+  }
+  catch(...) {
+    ASSERT_TRUE(1==0 && "Could not open unit test file");
   }
 }
