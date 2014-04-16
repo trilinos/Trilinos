d9cd72ffb7b7b0a643b0f0a46814fa87b012bd1d
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-04-16 16:22:26 -0400

    Disable MixedLanguage when Fortran is disabled and add NoFortran test
    I added and if statement for Fortran around the test
    TribitsExampleProject_ALL_PT and gutted the tarfile and file trace.  Enabling
    the MixedLanguage package does not make for better tarball testing or file
    stack tracing so we might as well not require Fortran for that.  Hopefully I
    can soon figure out how to get most of these tests running on Windows under
    NMake or MSVS.

--- cmake/tribits/doc/examples/TribitsExampleProject/cmake/CallbackSetupExtraOptions.cmake
+++ cmake/tribits/doc/examples/TribitsExampleProject/cmake/CallbackSetupExtraOptions.cmake
@@ -16,4 +16,14 @@ MACRO(TRIBITS_REPOSITORY_SETUP_EXTRA_OPTIONS)
     SET(${PROJECT_NAME}_ENABLE_WrapExternal OFF)
   ENDIF()
 
+  IF (NOT ${PROJECT_NAME}_ENABLE_Fortran)
+    MESSAGE(
+      "\n***"
+      "\n*** Warning: Setting ${PROJECT_NAME}_ENABLE_MixedLanguage=OFF"
+      " because ${PROJECT_NAME}_ENABLE_Fortran=OFF!"
+      "\n***\n"
+      )
+    SET(${PROJECT_NAME}_ENABLE_MixedLanguage OFF)
+  ENDIF()
+
 ENDMACRO()
