87863ee2e424b28a27c7c1543fa898ef96ee5535
Brent Perschbacher <bmpersc@sandia.gov>
2014-05-05 10:57:53 -0600

    STK: Added stk_search_util to Trilinos build
    Made the necessary files and modifications to build stk_search_util under the
    Trilinos build system.
    
    (From Sierra)

--- packages/stk/cmake/Dependencies.cmake
+++ packages/stk/cmake/Dependencies.cmake
@@ -1,9 +1,10 @@
 SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
-  Util      stk_util        SS  OPTIONAL
-  Topology  stk_topology    SS  OPTIONAL
-  Mesh      stk_mesh        SS  OPTIONAL
-  IO        stk_io          SS  OPTIONAL
-  Search    stk_search      SS  OPTIONAL
+  Util       stk_util        SS  OPTIONAL
+  Topology   stk_topology    SS  OPTIONAL
+  Mesh       stk_mesh        SS  OPTIONAL
+  IO         stk_io          SS  OPTIONAL
+  Search     stk_search      SS  OPTIONAL
+  SearchUtil stk_search_util SS  OPTIONAL
 )
 
 SET(LIB_REQUIRED_DEP_PACKAGES)
