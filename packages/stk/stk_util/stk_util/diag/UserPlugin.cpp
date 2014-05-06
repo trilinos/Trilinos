17168e023505167c19d42e4062a3faa9a5bc17f8
Paul Wolfenbarger <prwolfe@sandia.gov>
2014-05-06 15:47:48 -0600

    fortran underscore fixes for bgq - From Dan Sunderland...
    There were a lot of places where we were relying on platform or compiler
    defines to set the underscore decoration requirements for fortran. Since
    bgq uses gcc, but the underscaore requirements need to match vacpp this
    is non-functional.  This changes those header files to use the macro
    defined by bjam which correctly disambiguates the 2 versions of gcc.
    
    (From Sierra)

--- packages/stk/stk_util/stk_util/diag/UserPlugin.cpp
+++ packages/stk/stk_util/stk_util/diag/UserPlugin.cpp
@@ -158,17 +158,17 @@ Registry::registerDL(
   if (!dl){
     throw std::runtime_error(dlerror());
   }
-  
+
   if (function_name) {
     std::string s = std::strlen(function_name) ? function_name : "dl_register";
-    
+
     dl_register_t f = reinterpret_cast<dl_register_t>(dlsym(dl, s.c_str()));
     if (!f) {
       s = s + SIERRA_FORTRAN_SUFFIX;
-      
+
       f = reinterpret_cast<dl_register_t>(dlsym(dl, s.c_str()));
     }
-    
+
     if (f) {
       slibout.m(Slib::LOG_PLUGIN) << "Executing dynamic library " << so_path << " function " << s << "()" << stk::diag::dendl;
       (*f)();
@@ -181,7 +181,7 @@ Registry::registerDL(
       }
     }
   }
-  
+
 #else
   throw std::runtime_error("Dynamic linkage is not supported on this platform");
 #endif
