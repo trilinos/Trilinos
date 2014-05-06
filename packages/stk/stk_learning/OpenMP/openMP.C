84d721ec91feea1830a902ffa514c9779669711f
Nate Roehrig <nsroehr@sandia.gov>
2014-05-06 10:15:18 -0600

    Fixed issues with private variable that only showed up in debug.
    (From Sierra)

--- packages/stk/stk_learning/OpenMP/openMP.C
+++ packages/stk/stk_learning/OpenMP/openMP.C
@@ -7,10 +7,10 @@
 #include <stk_util/environment/WallTime.hpp>
 #include <stk_util/environment/CPUTime.hpp>
 
+#if defined(_OPENMP) && !defined(__INTEL_COMPILER)
 // there seems to be an issue with OpenMP combined with GoogleTest macros with Intel compilers
 // example of error:
 //    openMP.C(206): internal error: assertion failed at: "shared/cfe/edgcpfe/checkdir.c", line 5531
-#if defined(_OPENMP) && !defined(__INTEL_COMPILER)
 #include <omp.h>
 
 namespace
@@ -19,7 +19,7 @@ namespace
 //DocTest1
 TEST(OPENMP, HelloWorldDontSetNumThreadsInsideCode)
 {
-#pragma omp parallel
+    #pragma omp parallel
     {
         std::cout << "Hello World\n";
     }
@@ -30,7 +30,7 @@ TEST(OpenMp, HelloWorldSetNumThreadsstart_timeInsideCode)
 {
     int numThreads = 8;
     omp_set_num_threads(numThreads);
-#pragma omp parallel
+    #pragma omp parallel
     {
         std::cout << "Hello World\n";
     }
@@ -42,10 +42,12 @@ TEST(OPENMP, HelloWorldUsingPrivate)
     int numThreads = 8;
     omp_set_num_threads(numThreads);
     int threadId = -1;
-#pragma omp parallel private(threadId)
+    #pragma omp parallel private(threadId)
     {
         threadId = omp_get_thread_num();
-        std::cout << "Hello World from thread " << threadId << "/" << numThreads << std::endl;
+        std::stringstream avoidScrambledOutputByHavingSingleCoutCommand;
+        avoidScrambledOutputByHavingSingleCoutCommand << "Hello World from thread " << threadId << "/" << numThreads << std::endl;
+        std::cout << avoidScrambledOutputByHavingSingleCoutCommand.str();
     }
 }
 
@@ -53,94 +55,94 @@ TEST(OPENMP, HelloWorldUsingPrivate)
 // #define DO_OUTPUT
 TEST(OpenMp, MatrixVectorMultiplyUsingThreads)
 {
+    int numThreads = 8;
+    omp_set_num_threads(numThreads);
 //    size_t numRows = 200000;
 //    size_t numCols = 20000;
     size_t numRows = 2000;
     size_t numCols = 2000;
     std::vector<std::vector<double> > matrix(numRows);
-    std::vector<double> vec_in(numCols, 1);
-    std::vector<double> vec_out(numRows, 0);
+    std::vector<double> vecIn(numCols, 1);
+    std::vector<double> vecOut(numRows, 0);
     for(size_t i = 0; i < numRows; i++)
     {
         matrix[i].resize(numCols, i + 1);
     }
 
-    double start_time = stk::cpu_time();
-    double start_time_wall = stk::wall_time();
+    double startTime = stk::cpu_time();
+    double startWallTime = stk::wall_time();
 
-    size_t i = 0, j = 0;
-    int num_threads = 0;
-
-#pragma omp parallel
+    #pragma omp parallel
     {
-#if defined(DO_OUTPUT)
+        #if defined(DO_OUTPUT)
         int tid = omp_get_thread_num();
         std::ostringstream oss;
         oss << "Thread_" << tid;
         std::string filename = oss.str();
         std::ofstream output(filename.c_str());
-#endif
-        num_threads = omp_get_num_threads();
+        #endif
+        numThreads = omp_get_num_threads();
         #pragma omp for
-        for(i = 0; i < numRows; i++)
+        for(size_t i = 0; i < numRows; i++)
         {
-#if defined(DO_OUTPUT)
+            #if defined(DO_OUTPUT)
             output << "Thread " << tid << " working on row " << i << std::endl;
-#endif
-            for(j = 0; j < numCols; j++)
+            #endif
+            for(size_t j = 0; j < numCols; j++)
             {
-                vec_out[i] += matrix[i][j] * vec_in[j];
+                vecOut[i] += matrix[i][j] * vecIn[j];
             }
         }
-#if defined(DO_OUTPUT)
+        #if defined(DO_OUTPUT)
         output.close();
-#endif
+        #endif
     }
 
-    double end_time = stk::cpu_time();
-    double end_time_wall = stk::wall_time();
+    double endTime = stk::cpu_time();
+    double endWallTime = stk::wall_time();
 
-    std::cerr << "Num threads: " << num_threads << std::endl;
-    std::cerr << "Time: " << (end_time - start_time)/double(num_threads) << std::endl;
-    std::cerr << "Wall Time: " << (end_time_wall - start_time_wall) << std::endl;
+    std::cerr << "Num threads: " << numThreads << std::endl;
+    std::cerr << "Time: " << (endTime - startTime)/double(numThreads) << std::endl;
+    std::cerr << "Wall Time: " << (endWallTime - startWallTime) << std::endl;
 
     for(size_t i = 0; i < numRows; i++)
     {
         double goldAnswer = (i + 1) * numCols;
-        EXPECT_EQ(goldAnswer, vec_out[i]);
+        EXPECT_EQ(goldAnswer, vecOut[i]);
     }
 }
 
 //DocTest5
 TEST(OpenMp, SumOverVector)
 {
+    int numThreads = 8;
+    omp_set_num_threads(numThreads);
     //size_t sizeOfVector = 4000000000;
     size_t sizeOfVector = 1000000;
     double initVal = 1.0;
-    std::vector<double> vec(sizeOfVector,initVal);
-    int num_threads = 0;
+    std::vector<double> vec(sizeOfVector, initVal);
 
     double sum = 0;
 
-    double start_time = stk::cpu_time();
-    double start_time_wall = stk::wall_time();
+    double startTime = stk::cpu_time();
+    double startWallTime = stk::wall_time();
 
     #pragma omp parallel
     {
-        num_threads = omp_get_num_threads();
+        numThreads = omp_get_num_threads();
         #pragma omp for reduction(+:sum)
-        for (size_t i=0;i<sizeOfVector;i++)
+        for (size_t i = 0; i < sizeOfVector; i++)
         {
             sum += vec[i];
         }
     }
 
-    double end_time = stk::cpu_time();
-    double end_time_wall = stk::wall_time();
+    double endTime = stk::cpu_time();
+    double endWallTime = stk::wall_time();
 
-    std::cerr << "Num threads: " << num_threads << std::endl;
-    std::cerr << "Time: " << (end_time - start_time)/double(num_threads) << std::endl;
-    std::cerr << "Wall Time: " << (end_time_wall - start_time_wall) << std::endl;
+    std::cerr << "Num threads: " << numThreads << std::endl;
+    std::cerr << "Time: " << (endTime - startTime)/double(numThreads) << std::endl;
+    std::cerr << "Wall Time: " << (endWallTime - startWallTime) << std::endl;
 
     double goldAnswer = sizeOfVector*initVal;
     EXPECT_EQ(goldAnswer, sum);
@@ -155,24 +157,23 @@ TEST(OpenMp, SumUsingSections)
     size_t sizeOfVector = 100000;
     double initVal = 1.0;
     std::vector<double> vec(sizeOfVector,initVal);
-    int num_threads = 0;
 
     double sum1 = 0, sum2 = 0;
 
-    double start_time = stk::cpu_time();
-    double start_time_wall = stk::wall_time();
+    double startTime = stk::cpu_time();
+    double startWallTime = stk::wall_time();
 
     size_t halfLoopSize = sizeOfVector/2;
 
     #pragma omp parallel
     {
-        num_threads = omp_get_num_threads();
+        numThreads = omp_get_num_threads();
         #pragma omp sections
         {
             #pragma omp section
             {
                 std::cerr << "Loop 1: hello from thread: " << omp_get_thread_num() << std::endl;
-                for (size_t i=0;i<halfLoopSize;i++)
+                for (size_t i = 0; i < halfLoopSize; i++)
                 {
                     sum1 += vec[i];
                 }
@@ -180,7 +181,7 @@ TEST(OpenMp, SumUsingSections)
             #pragma omp section
             {
                 std::cerr << "Loop 2: hello from thread: " << omp_get_thread_num() << std::endl;
-                for (size_t i=halfLoopSize;i<sizeOfVector;i++)
+                for (size_t i = halfLoopSize; i < sizeOfVector; i++)
                 {
                     sum2 += vec[i];
                 }
@@ -188,14 +189,14 @@ TEST(OpenMp, SumUsingSections)
         }
     }
 
-    double sum = sum1+sum2;
+    double sum = sum1 + sum2;
 
-    double end_time = stk::cpu_time();
-    double end_time_wall = stk::wall_time();
+    double endTime = stk::cpu_time();
+    double endWallTime = stk::wall_time();
 
-    std::cerr << "Num threads: " << num_threads << std::endl;
-    std::cerr << "Time: " << (end_time - start_time)/double(num_threads) << std::endl;
-    std::cerr << "Wall Time: " << (end_time_wall - start_time_wall) << std::endl;
+    std::cerr << "Num threads: " << numThreads << std::endl;
+    std::cerr << "Time: " << (endTime - startTime)/double(numThreads) << std::endl;
+    std::cerr << "Wall Time: " << (endWallTime - startWallTime) << std::endl;
 
     double goldAnswer = sizeOfVector*initVal;
     EXPECT_EQ(goldAnswer, sum);
