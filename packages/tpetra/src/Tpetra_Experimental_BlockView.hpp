b69cf97c05f36080f1e776c7db854539e33272e4
Mark Hoemmen <mhoemme@sandia.gov>
2014-05-07 08:44:50 -0600

    Tpetra: Added public docs to Little{Block,Vector} & moved namespaces.
    The header file now has the Tpetra copyright notice, and the new
    Little{Block,Vector} classes now live in the Tpetra::Experimental
    namespace (where the new BlockMultiVector class also lives -- which
    differs from the existing Tpetra::BlockMultiVector!).

--- packages/tpetra/src/Tpetra_Experimental_BlockView.hpp
+++ packages/tpetra/src/Tpetra_Experimental_BlockView.hpp
@@ -1,25 +1,109 @@
+// @HEADER
+// ***********************************************************************
+//
+//          Tpetra: Templated Linear Algebra Services Package
+//                 Copyright (2008) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+
 #ifndef TPETRA_EXPERIMENTAL_BLOCKVIEW_HPP
 #define TPETRA_EXPERIMENTAL_BLOCKVIEW_HPP
 
+/// \file Tpetra_Experimental_BlockView.hpp
+/// \brief Declaration and definition of LittleBlock and LittleVector
+
 #include <Teuchos_ScalarTraits.hpp>
 
+namespace Tpetra {
+
+/// \brief Namespace for new Tpetra features that are not ready for
+///   public release, but are ready for evaluation by friendly expert
+///   users.
+///
+/// \warning Expect header files, classes, functions, and other
+///   interfaces to change or disappear.  Anything in this namespace
+///   is under active development and evaluation.  Documentation may
+///   be sparse or not exist yet.  Generally, unit tests will exist,
+///   but coverage may be lacking.  If you understand these caveats
+///   and accept them, please feel free to take a look inside and try
+///   things out.
+namespace Experimental {
+
+/// \class LittleBlock
+/// \brief Nonowning view of a square dense block in a block matrix.
+/// \tparam Scalar The type of entries in the block.
+/// \tparam LO The local ordinal type: a signed integer type.
+///
+/// "Little" means local (not distributed over multiple MPI processes;
+/// stored to maximize locality) and small (think 3x3, not 1000x1000).
+///
+/// The \c Scalar template parameter may be const or nonconst.  This
+/// is one reason why instance methods below that take a LittleBlock
+/// accept it as a template parameter: that lets you add a const
+/// LittleBlock (e.g., LittleBlock<const double, int>) to a nonconst
+/// LittleBlock (e.g., LittleBlock<double, int>).
 template<class Scalar, class LO>
 class LittleBlock {
 private:
   typedef Teuchos::ScalarTraits<Scalar> STS;
 
 public:
-  LittleBlock (Scalar* const A, const LO blockSize, const LO strideX, const LO strideY) :
-    A_ (A), blockSize_ (blockSize), strideX_ (strideX), strideY_ (strideY) {}
+  /// \brief Constructor
+  /// \param A [in] Pointer to the block's entries
+  /// \param blockSize [in] Dimension of the block (all blocks are square)
+  /// \param strideX [in] Stride between consecutive entries in a column
+  /// \param strideY [in] Stride between consecutive entries in a row
+  LittleBlock (Scalar* const A,
+               const LO blockSize,
+               const LO strideX,
+               const LO strideY) :
+    A_ (A), blockSize_ (blockSize), strideX_ (strideX), strideY_ (strideY)
+  {}
 
+  //! Pointer to the block's entries.
   Scalar* getRawPtr () const {
     return A_;
   }
 
+  //! Reference to entry (i,j) of the block.
   Scalar& operator() (const LO i, const LO j) const {
     return A_[i * strideX_ + j * strideY_];
   }
 
+  //! <tt>*this := *this + alpha * X</tt>.
   template<class LittleBlockType>
   void update (const Scalar& alpha, const LittleBlockType& X) const {
     for (LO j = 0; j < blockSize_; ++j) {
@@ -29,6 +113,7 @@ public:
     }
   }
 
+  //! <tt>*this := X</tt>.
   template<class LittleBlockType>
   void assign (const LittleBlockType& X) const {
     for (LO j = 0; j < blockSize_; ++j) {
@@ -38,6 +123,7 @@ public:
     }
   }
 
+  //! <tt>(*this)(i,j) := alpha * (*this)(i,j)</tt> for all (i,j).
   void scale (const Scalar& alpha) const {
     for (LO j = 0; j < blockSize_; ++j) {
       for (LO i = 0; i < blockSize_; ++i) {
@@ -46,6 +132,7 @@ public:
     }
   }
 
+  //! <tt>(*this)(i,j) := alpha</tt> for all (i,j).
   void fill (const Scalar& alpha) const {
     for (LO j = 0; j < blockSize_; ++j) {
       for (LO i = 0; i < blockSize_; ++i) {
@@ -54,6 +141,10 @@ public:
     }
   }
 
+  /// \brief <tt>(*this)(i,j) := max(abs((*this)(i,j)), abs(X(i,j)))</tt>
+  ///   for all (i,j).
+  ///
+  /// Tpetra uses this operation to implement the ABSMAX CombineMode.
   template<class LittleBlockType>
   void absmax (const LittleBlockType& X) const {
     for (LO j = 0; j < blockSize_; ++j) {
@@ -73,24 +164,46 @@ private:
 };
 
 
-
+/// \class LittleVector
+/// \brief Nonowning view of a set of degrees of freedom corresponding
+///   to a mesh point in a block vector or multivector.
+/// \tparam Scalar The type of entries.
+/// \tparam LO The local ordinal type: a signed integer type.
+///
+/// "Little" means local (not distributed over multiple MPI processes;
+/// stored to maximize locality) and small (think length 3, not length
+/// 1000).
+///
+/// The \c Scalar template parameter may be const or nonconst.  This
+/// is one reason why instance methods below that take a LittleVector
+/// accept it as a template parameter: that lets you add a const
+/// LittleVector (e.g., LittleVector<const double, int>) to a nonconst
+/// LittleVector (e.g., LittleVector<double, int>).
 template<class Scalar, class LO>
 class LittleVector {
 private:
   typedef Teuchos::ScalarTraits<Scalar> STS;
 
 public:
+  /// \brief Constructor
+  /// \param A [in] Pointer to the vector's entries
+  /// \param blockSize [in] Dimension of the vector
+  /// \param strideX [in] Stride between consecutive entries
   LittleVector (Scalar* const A, const LO blockSize, const LO strideX) :
-    A_ (A), blockSize_ (blockSize), strideX_ (strideX) {}
+    A_ (A), blockSize_ (blockSize), strideX_ (strideX)
+  {}
 
+  //! Pointer to the block's entries.
   Scalar* getRawPtr () const {
     return A_;
   }
 
+  //! Reference to entry (i) of the vector.
   Scalar& operator() (const LO i) const {
     return A_[i * strideX_];
   }
 
+  //! <tt>*this := *this + alpha * X</tt>.
   template<class LittleBlockType>
   void update (const Scalar& alpha, const LittleBlockType& X) const {
     for (LO i = 0; i < blockSize_; ++i) {
@@ -98,6 +211,7 @@ public:
     }
   }
 
+  //! <tt>*this := X</tt>.
   template<class LittleBlockType>
   void assign (const LittleBlockType& X) const {
     for (LO i = 0; i < blockSize_; ++i) {
@@ -105,18 +219,24 @@ public:
     }
   }
 
+  //! <tt>(*this)(i,j) := alpha * (*this)(i,j)</tt> for all (i,j).
   void scale (const Scalar& alpha) const {
     for (LO i = 0; i < blockSize_; ++i) {
       (*this)(i) *= alpha;
     }
   }
 
+  //! <tt>(*this)(i,j) := alpha</tt> for all (i,j).
   void fill (const Scalar& alpha) const {
     for (LO i = 0; i < blockSize_; ++i) {
       (*this)(i) = alpha;
     }
   }
 
+  /// \brief <tt>(*this)(i,j) := max(abs((*this)(i,j)), abs(X(i,j)))</tt>
+  ///   for all (i,j).
+  ///
+  /// Tpetra uses this operation to implement the ABSMAX CombineMode.
   template<class LittleBlockType>
   void absmax (const LittleBlockType& X) const {
     for (LO i = 0; i < blockSize_; ++i) {
@@ -131,5 +251,7 @@ private:
   const LO strideX_;
 };
 
+} // namespace Experimental
+} // namespace Tpetra
 
 #endif // TPETRA_EXPERIMENTAL_BLOCKVIEW_HPP
