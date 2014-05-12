615eaeb7f18eadda9cab7363cc2fc91f3bb0b925
Manoj K Bhardwaj <mkbhard@sandia.gov>
2014-05-12 14:31:00 -0600

    Adding OpenMP pragmas for testing fields' performance via unit test.
    (From Sierra)

--- packages/stk/stk_performance_tests/stk_mesh/NodalFieldPerf.cpp
+++ packages/stk/stk_performance_tests/stk_mesh/NodalFieldPerf.cpp
@@ -6,13 +6,18 @@
 #include <stk_topology/topology.hpp>
 #include <stk_mesh/base/CoordinateSystems.hpp>
 #include <stk_util/environment/CPUTime.hpp>
+#include <stk_util/environment/WallTime.hpp>
 
 #include <mpi.h>                        // for MPI_COMM_WORLD, MPI_Comm, etc
 #include <stk_io/StkMeshIoBroker.hpp>   // for StkMeshIoBroker
 #include <stk_mesh/base/GetEntities.hpp>  // for count_entities
-
 #include <vector>
 #include <string>
+
+#if defined(_OPENMP)
+#include <omp.h>
+#endif
+
 
 extern int* STKUNIT_ARGC;
 extern char** STKUNIT_ARGV;
@@ -37,19 +42,19 @@ void createNodalVectorFields(stk::mesh::MetaData& meshMetaData)
     meshMetaData.commit();
 }
 
-std::string getOption(const std::string& option, const std::string defaultString="no")
+std::string getOption(const std::string& option, const std::string defaultString = "no")
 {
     std::string returnValue = defaultString;
-    if ( STKUNIT_ARGV != 0 )
+    if(STKUNIT_ARGV != 0)
     {
-        for (int i=0;i<*STKUNIT_ARGC;i++)
+        for(int i = 0; i < *STKUNIT_ARGC; i++)
         {
             std::string input_argv(STKUNIT_ARGV[i]);
-            if ( option == input_argv )
+            if(option == input_argv)
             {
-                if ( (i+1) < *STKUNIT_ARGC )
+                if((i + 1) < *STKUNIT_ARGC)
                 {
-                    returnValue = std::string(STKUNIT_ARGV[i+1]);
+                    returnValue = std::string(STKUNIT_ARGV[i + 1]);
                 }
                 break;
             }
@@ -61,7 +66,7 @@ std::string getOption(const std::string& option, const std::string defaultString
 void createMetaAndBulkData(stk::io::StkMeshIoBroker &exodusFileReader, stk::mesh::FieldDataManager *fieldDataManager)
 {
     std::string exodusFileName = getOption("-i", "NO_FILE_SPECIFIED");
-    ASSERT_NE(exodusFileName,"NO_FILE_SPECIFIED");
+    ASSERT_NE(exodusFileName, "NO_FILE_SPECIFIED");
 
     exodusFileReader.add_mesh_database(exodusFileName, stk::io::READ_MESH);
 
@@ -95,53 +100,59 @@ void timeFieldOperations(stk::mesh::MetaData &stkMeshMetaData, stk::mesh::BulkDa
 {
     std::string numIterationsString = getOption("-numIter", "1");
     const int numIterations = std::atoi(numIterationsString.c_str());
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &disp_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("disp");
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &vel_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("vel");
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &acc_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("acc");
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &force_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("force");
+
+    const stk::mesh::BucketVector& allNodeBuckets = stkMeshBulkData.buckets(stk::topology::NODE_RANK);
+    const size_t numNodeBuckets = allNodeBuckets.size();
+
     std::cerr << "Starting timer for " << numIterations << " iterations of nodal addition (vec4 = vec1 + vec2 + vec3) test." << std::endl;
-    double startTime   = stk::cpu_time();
+    double startTime = stk::cpu_time();
+    double startWallTime = stk::wall_time();
 
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &disp_field  = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("disp");
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &vel_field   = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("vel");
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &acc_field   = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("acc");
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &force_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("force");
-
-        const stk::mesh::BucketVector& allNodeBuckets = stkMeshBulkData.buckets(stk::topology::NODE_RANK);
-        const size_t numNodeBuckets = allNodeBuckets.size();
-
-    for(int i=0; i<numIterations; i++)
+    for(int i = 0; i < numIterations; i++)
     {
-        for (size_t bucketIndex=0;bucketIndex<numNodeBuckets;++bucketIndex)
+#if defined(_OPENMP)
+#pragma omp parallel for schedule(dynamic, 5)
+#endif
+        for(size_t bucketIndex = 0; bucketIndex < numNodeBuckets; ++bucketIndex)
         {
             const stk::mesh::Bucket &bucket = *allNodeBuckets[bucketIndex];
             double *disp = stk::mesh::field_data(disp_field, bucket);
-            double *vel  = stk::mesh::field_data(vel_field, bucket);
-            double *acc  = stk::mesh::field_data(acc_field, bucket);
+            double *vel = stk::mesh::field_data(vel_field, bucket);
+            double *acc = stk::mesh::field_data(acc_field, bucket);
             double *force = stk::mesh::field_data(force_field, bucket);
             size_t bucketLoopEnd = 3 * bucket.size();
-            for (size_t nodeIndex=0;nodeIndex<bucketLoopEnd;nodeIndex+=3)
+            for(size_t nodeIndex = 0; nodeIndex < bucketLoopEnd; nodeIndex ++)
             {
-                force[nodeIndex+0] = alpha*disp[nodeIndex+0] + beta*vel[nodeIndex+0] + gamma*acc[nodeIndex+0];
-                force[nodeIndex+1] = alpha*disp[nodeIndex+1] + beta*vel[nodeIndex+1] + gamma*acc[nodeIndex+1];
-                force[nodeIndex+2] = alpha*disp[nodeIndex+2] + beta*vel[nodeIndex+2] + gamma*acc[nodeIndex+2];
+                force[nodeIndex] = alpha * disp[nodeIndex] + beta * vel[nodeIndex] + gamma * acc[nodeIndex];
             }
         }
     }
-    double elapsedTime = stk::cpu_time()- startTime;
-    std::cerr << "That took " << elapsedTime << " seconds." << std::endl;
+
+    double elapsedTime = stk::cpu_time() - startTime;
+    double elapsedWallTime = stk::wall_time() - startWallTime;
+
+    std::cerr << "That took CPU time of " << elapsedTime << " seconds." << std::endl;
+    std::cerr << "That took wall time of " << elapsedWallTime << " seconds." << std::endl;
 }
 
 void testGoldValues(stk::mesh::MetaData &stkMeshMetaData, stk::mesh::BulkData &stkMeshBulkData, double alpha, double beta, double gamma)
 {
-    double goldX=initial_value1[0]*(alpha+beta+gamma);
-    double goldY=initial_value1[1]*(alpha+beta+gamma);
-    double goldZ=initial_value1[2]*(alpha+beta+gamma);
+    double goldX = initial_value1[0] * (alpha + beta + gamma);
+    double goldY = initial_value1[1] * (alpha + beta + gamma);
+    double goldZ = initial_value1[2] * (alpha + beta + gamma);
 
-    stk::mesh::Field<double, stk::mesh::Cartesian3d> &force_field  = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("force");
-    double tol=1.0e-4;
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &force_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("force");
+    double tol = 1.0e-4;
     const stk::mesh::BucketVector& buckets = stkMeshBulkData.buckets(stk::topology::NODE_RANK);
-    for (size_t bucketIndex=0;bucketIndex<buckets.size();++bucketIndex)
+    for(size_t bucketIndex = 0; bucketIndex < buckets.size(); ++bucketIndex)
     {
         const stk::mesh::Bucket &bucket = *buckets[bucketIndex];
-        double *force  = stk::mesh::field_data(force_field, bucket);
-        for (size_t nodeIndex=0;nodeIndex<3*bucket.size();nodeIndex+=3)
+        double *force = stk::mesh::field_data(force_field, bucket);
+        for(size_t nodeIndex = 0; nodeIndex < 3 * bucket.size(); nodeIndex += 3)
         {
             EXPECT_NEAR(goldX, force[nodeIndex+0], tol);
             EXPECT_NEAR(goldY, force[nodeIndex+1], tol);
@@ -164,43 +175,61 @@ void test1ToNSumOfNodalFields(stk::mesh::ContiguousFieldDataManager *fieldDataMa
     std::cerr << "Number of node buckets: " << buckets.size() << std::endl;
 
     double alpha = -1.4;
-    double beta  = 0.3333333;
+    double beta = 0.3333333;
     double gamma = 3.14159;
 
     std::string numIterationsString = getOption("-numIter", "1");
     const int numIterations = std::atoi(numIterationsString.c_str());
+
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &disp_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("disp");
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &vel_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("vel");
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &acc_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("acc");
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> &force_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("force");
+
+    const stk::mesh::BucketVector& allNodeBuckets = stkMeshBulkData.buckets(stk::topology::NODE_RANK);
+
+    size_t numBytesForField = fieldDataManager->get_num_bytes_allocated_on_field(disp_field.mesh_meta_data_ordinal());
+    stk::mesh::FieldMetaDataVector& field_meta_data_vector = const_cast<stk::mesh::FieldMetaDataVector&>(disp_field.get_meta_data_for_field());
+            int numBytesPerEntity = field_meta_data_vector[0].m_bytes_per_entity;
+    size_t numNodes = numBytesForField / numBytesPerEntity;
+    size_t nodeLoopEnd = 3 * numNodes;
+
     std::cerr << "Starting timer for " << numIterations << " iterations of nodal addition (vec4 = vec1 + vec2 + vec3) test." << std::endl;
-    double startTime   = stk::cpu_time();
+    double startTime = stk::cpu_time();
+    double startWallTime = stk::wall_time();
 
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &disp_field  = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("disp");
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &vel_field   = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("vel");
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &acc_field   = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("acc");
-        stk::mesh::Field<double, stk::mesh::Cartesian3d> &force_field = *stkMeshMetaData.get_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >("force");
+    size_t numThreads = 1;
+//    const size_t chunkSize = 118720;
 
-        const stk::mesh::BucketVector& allNodeBuckets = stkMeshBulkData.buckets(stk::topology::NODE_RANK);
-        const stk::mesh::Bucket &bucket = *allNodeBuckets[0];
-        double *disp = stk::mesh::field_data(disp_field, bucket);
-        double *vel  = stk::mesh::field_data(vel_field, bucket);
-        double *acc  = stk::mesh::field_data(acc_field, bucket);
-        double *force = stk::mesh::field_data(force_field, bucket);
-
-        size_t numBytesForField = fieldDataManager->get_num_bytes_allocated_on_field(disp_field.mesh_meta_data_ordinal());
-        stk::mesh::FieldMetaDataVector& field_meta_data_vector = const_cast<stk::mesh::FieldMetaDataVector&>(disp_field.get_meta_data_for_field());
-        int numBytesPerEntity = field_meta_data_vector[0].m_bytes_per_entity;
-        size_t numNodes = numBytesForField / numBytesPerEntity;
-        size_t nodeLoopEnd = 3 * numNodes;
-
-    for(int i=0; i<numIterations; i++)
+    for(int i = 0; i < numIterations; i++)
     {
-        for (size_t nodeIndex=0;nodeIndex<nodeLoopEnd;nodeIndex+=3)
+#if defined(_OPENMP)
+#pragma omp parallel firstprivate(alpha, beta, gamma)
+#endif
         {
-            force[nodeIndex+0] = alpha*disp[nodeIndex+0] + beta*vel[nodeIndex+0] + gamma*acc[nodeIndex+0];
-            force[nodeIndex+1] = alpha*disp[nodeIndex+1] + beta*vel[nodeIndex+1] + gamma*acc[nodeIndex+1];
-            force[nodeIndex+2] = alpha*disp[nodeIndex+2] + beta*vel[nodeIndex+2] + gamma*acc[nodeIndex+2];
+            const stk::mesh::Bucket &bucket = *allNodeBuckets[0];
+            double *disp = stk::mesh::field_data(disp_field, bucket);
+            double *vel = stk::mesh::field_data(vel_field, bucket);
+            double *acc = stk::mesh::field_data(acc_field, bucket);
+            double *force = stk::mesh::field_data(force_field, bucket);
+#if defined(_OPENMP)
+#pragma omp for
+#endif
+            for(size_t nodeIndex = 0; nodeIndex < nodeLoopEnd; nodeIndex++)
+            {
+                force[nodeIndex] = alpha * disp[nodeIndex] + beta * vel[nodeIndex] + gamma * acc[nodeIndex];
+            }
+#if defined(_OPENMP)
+            numThreads = omp_get_num_threads();
+#endif
         }
     }
-    double elapsedTime = stk::cpu_time()- startTime;
-    std::cerr << "That took " << elapsedTime << " seconds." << std::endl;
+    double elapsedTime = stk::cpu_time() - startTime;
+    double elapsedWallTime = stk::wall_time() - startWallTime;
+
+    std::cerr << "That took CPU time of " << elapsedTime << " seconds." << std::endl;
+    std::cerr << "That took wall time of " << elapsedWallTime << " seconds." << std::endl;
+    std::cerr << "Using " << numThreads << " threads" << std::endl;
 
     testGoldValues(stkMeshMetaData, stkMeshBulkData, alpha, beta, gamma);
 }
@@ -219,7 +248,7 @@ void testSumOfNodalFields(stk::mesh::FieldDataManager *fieldDataManager)
     std::cerr << "Number of node buckets: " << buckets.size() << std::endl;
 
     double alpha = -1.4;
-    double beta  = 0.3333333;
+    double beta = 0.3333333;
     double gamma = 3.14159;
 
     timeFieldOperations(stkMeshMetaData, stkMeshBulkData, alpha, beta, gamma);
