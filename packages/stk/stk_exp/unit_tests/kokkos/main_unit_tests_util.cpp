bdd87a645396c7fed72c30e5eac163a7de0d86c1
Manoj K Bhardwaj <mkbhard@sandia.gov>
2014-05-13 10:53:38 -0600

    Adding "temporarily" the fields unit test here to experiment with Kokkos.
    Note: Currently the executable is not built for this test. To do so,
    edit the stk_exp's Jamfile and comment out the kokkos unit test items (2 areas).
    
    (From Sierra)

--- packages/stk/stk_exp/unit_tests/kokkos/main_unit_tests_util.cpp
+++ packages/stk/stk_exp/unit_tests/kokkos/main_unit_tests_util.cpp
@@ -1,8 +1,16 @@
 #include <gtest/gtest.h>
+#include <mpi.h>
+
+int* STKUNIT_ARGC;
+char** STKUNIT_ARGV;
 
 int main( int argc, char **argv )
 {
   ::testing::InitGoogleTest(&argc,argv);
-
-  return RUN_ALL_TESTS();
+  MPI_Init( & argc , & argv );
+  STKUNIT_ARGC = &argc;
+  STKUNIT_ARGV = argv;
+  int err = RUN_ALL_TESTS();
+  MPI_Finalize();
+  return err;
 }
