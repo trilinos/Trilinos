4b52df0d7d42d2c050171b2f912a9998ca0d382b
Dan Sunderland <dsunder@sandia.gov>
2014-05-15 12:23:14 -0600

    Kokkos: small refactor of CMakeLists.txt to improve integration of Kokkos into the Sierra build system

--- packages/kokkos/core/src/OpenMP/Kokkos_OpenMPexec.cpp
+++ packages/kokkos/core/src/OpenMP/Kokkos_OpenMPexec.cpp
@@ -1,13 +1,13 @@
 /*
 //@HEADER
 // ************************************************************************
-// 
+//
 //   Kokkos: Manycore Performance-Portable Multidimensional Arrays
 //              Copyright (2012) Sandia Corporation
-// 
+//
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -35,11 +35,13 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact  H. Carter Edwards (hcedwar@sandia.gov) 
-// 
+// Questions? Contact  H. Carter Edwards (hcedwar@sandia.gov)
+//
 // ************************************************************************
 //@HEADER
 */
+
+#ifdef KOKKOS_HAVE_OPENMP
 
 #include <stdio.h>
 #include <limits>
@@ -423,3 +425,4 @@ void OpenMP::finalize()
 
 } // namespace Kokkos
 
+#endif //KOKKOS_HAVE_OPENMP
