819308a892f2b84c1fdd9fd416b3698d6d418df7
Manoj K Bhardwaj <mkbhard@sandia.gov>
2014-05-21 13:57:11 -0600

    Adding unit tests to show how to communicate using STK's CommAll.
    (From Sierra)

--- packages/stk/stk_util/unit_tests/parallel/UnitTestParallelComm.cpp
+++ packages/stk/stk_util/unit_tests/parallel/UnitTestParallelComm.cpp
@@ -14,7 +14,8 @@
 #include "mpi.h"                        // for MPI_COMM_WORLD, etc
 
 
-TEST(ParallelComm, CommAllDestructor) {
+TEST(ParallelComm, CommAllDestructor)
+{
     stk::ParallelMachine comm = MPI_COMM_WORLD ;
     int mpi_size = stk::parallel_machine_size(comm);
     const unsigned zero = 0 ;
@@ -30,3 +31,103 @@ TEST(ParallelComm, CommAllDestructor) {
     }
     // This should not produce a memory leak.
 }
+
+//DocTest1
+TEST(ParallelComm, HowToCommunicateOneValue)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    stk::CommAll comm_all(comm);
+
+    int myProcId = comm_all.parallel_rank();
+    int numProcs = comm_all.parallel_size();
+
+    double sendSomeNumber = 100-myProcId;
+
+    for(int phase = 0; phase < 2; ++phase)
+    {
+        for (int proc=0;proc<numProcs;proc++)
+        {
+            if ( proc != myProcId )
+            {
+                stk::CommBuffer& proc_buff = comm_all.send_buffer(proc);
+                proc_buff.pack<double>(sendSomeNumber);
+            }
+        }
+        if(phase == 0)
+        {
+            // NOTE: The value passed into allocate_buffers determines whether a sparse or dense communication will occur
+            comm_all.allocate_buffers(numProcs / 4);
+        }
+        else
+        {
+            comm_all.communicate();
+        }
+    }
+
+
+    for (int proc=0;proc<numProcs;proc++)
+    {
+        if ( proc != myProcId )
+        {
+            stk::CommBuffer& dataReceived = comm_all.recv_buffer(proc);
+            double val = -1;
+            dataReceived.unpack(val);
+            EXPECT_EQ(100-proc, val);
+        }
+    }
+}
+//DocTest2
+TEST(ParallelComm, HowToCommunicateAnArbitraryNumberOfValues)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    stk::CommAll comm_all(comm);
+
+    int myProcId = comm_all.parallel_rank();
+    int numProcs = comm_all.parallel_size();
+
+    double sendSomeNumber = 100-myProcId;
+
+    for(int phase = 0; phase < 2; ++phase)
+    {
+        for (int proc=0;proc<numProcs;proc++)
+        {
+            if ( proc != myProcId )
+            {
+                stk::CommBuffer& proc_buff = comm_all.send_buffer(proc);
+                for (int i=0;i<myProcId;i++)
+                {
+                    proc_buff.pack<double>(sendSomeNumber+i);
+                }
+            }
+        }
+        if(phase == 0)
+        {
+            // NOTE: The value passed into allocate_buffers determines whether a sparse or dense communication will occur
+            comm_all.allocate_buffers(numProcs / 4);
+        }
+        else
+        {
+            comm_all.communicate();
+        }
+    }
+
+
+    for (int procFromWhichDataIsReceived=0;procFromWhichDataIsReceived<numProcs;procFromWhichDataIsReceived++)
+    {
+        if ( procFromWhichDataIsReceived != myProcId )
+        {
+            stk::CommBuffer& dataReceived = comm_all.recv_buffer(procFromWhichDataIsReceived);
+            int numItemsReceived = 0;
+            while ( dataReceived.remaining() )
+            {
+                double val = -1;
+                dataReceived.unpack(val);
+                EXPECT_EQ(100-procFromWhichDataIsReceived+numItemsReceived, val);
+                numItemsReceived++;
+            }
+            int goldNumItemsReceived = procFromWhichDataIsReceived;
+            EXPECT_EQ(goldNumItemsReceived, numItemsReceived);
+        }
+    }
+}
+//EndDocTest
