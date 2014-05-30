01ee450fc76fa877086ca83fa7554dc0b3589e98
Jordan P. Lefebvre <jap@ornl.gov>
2014-05-30 14:56:56 -0400

    Attempt to add auto-detection of C++11 flags when ${PROJECT_NAME}_ENABLE_CXX11=ON

--- cmake/tribits/package_arch/TribitsCXX11Support.cmake
+++ cmake/tribits/package_arch/TribitsCXX11Support.cmake
@@ -39,6 +39,90 @@
 
 INCLUDE(CheckCXXSourceRuns)
 
+FUNCTION(TRIBITS_ENABLE_CXX11)
+  ##
+  ## Only try introspection if we have not already
+  ## or if the user has not overridden then
+  ##
+  IF(NOT TRIBITS_CXX11_FLAGS)
+     MESSAGE(STATUS "Search for CXX11 compiler flag.")
+     INCLUDE(CheckCXXSourceCompiles)
+     ##
+     ## List of possible compiler flags to use
+     ##
+     SET(CXX11_FLAG_OPTIONS
+         "/Qstd=c++11"   # intel windows
+         "-std=c++11"    # intel/clang linux/mac
+         "-std=gnu++11"  # gcc
+     )
+     ##
+     ## Same CXX11 source
+     ##
+     SET(TSOURCE
+        "
+        #include <vector>
+        int main() {
+          // check >> closing brackets
+          std::vector<std::vector<float>> vecvecfloat(1);
+          vecvecfloat[0].resize(1);
+          vecvecfloat[0][0] = 0.0f;
+          std::vector<int> vec(10);
+          auto b        = vec.begin();
+          decltype(b) e = vec.end();
+          std::fill(b,e,1);
+          // two examples, taken from the wikipedia article on C++0x :)
+          std::vector<int> some_list;
+          int total = 0;
+          int value = 5;
+          std::for_each(some_list.begin(), some_list.end(), [&total](int x) {
+              total += x;
+              });
+          std::for_each(some_list.begin(), some_list.end(), [&, value](int x) {
+              total += x * value;
+              });
+          return 0;
+        }
+        "
+     )
+     ##
+     ## Try to compile with each flag
+     ##
+     FOREACH( TOPTION ${CXX11_FLAG_OPTIONS})
+        IF(${PROJECT_NAME}_VERBOSE_CONFIGURE)
+          MESSAGE(STATUS "Testing CXX11 flag: ${TOPTION}")
+        ENDIF()
+        SET(CMAKE_REQUIRED_FLAGS "${TOPTION}")
+        CHECK_CXX_SOURCE_COMPILES("${TSOURCE}" CXX11_FLAGS_COMPILE_RESULT
+           ## Some compilers do not fail with a bad flag
+           FAIL_REGEX "unrecognized .*option"                     # GNU
+           FAIL_REGEX "ignoring unknown option"                   # MSVC
+           FAIL_REGEX "warning D9002"                             # MSVC, any lang
+           FAIL_REGEX "[Uu]nknown option"                         # HP
+           FAIL_REGEX "[Ww]arning: [Oo]ption"                     # SunPro
+           FAIL_REGEX "command option .* is not recognized"       # XL
+        )
+        IF(CXX11_FLAGS_COMPILE_RESULT)
+          # CACHE the successful flags
+          #SET(CXX11_FLAGS_COMPILE_RESULT "${TOPTION}" CACHE INTERNAL "cxx11 support flag")
+          # Append compiler flag to CMAKE_CXX_FLAGS
+          SET(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${TOPTION}" CACHE INTERNAL "")
+          IF(${PROJECT_NAME}_VERBOSE_CONFIGURE})
+            MESSAGE(STATUS "Successful CXX11 flag: '${TOPTION}'")
+          ENDIF()
+          SET(TRIBITS_CXX11_FLAGS ${TOPTION} PARENT_SCOPE)
+          BREAK()
+        ELSE()
+          SET(CXX11_FLAGS_COMPILE_RESULT TRUE)
+        ENDIF()
+     ENDFOREACH()
+  ELSE(NOT TRIBITS_CXX11_FLAGS)
+     ##
+     ## We already detected or set the cxx11 flags
+     ##
+     MESSAGE(STATUS "CXX11 Flags already set: '${TRIBITS_CXX11_FLAGS}'")
+  ENDIF(NOT TRIBITS_CXX11_FLAGS)
+ENDFUNCTION(TRIBITS_ENABLE_CXX11)
+
 FUNCTION(TRIBITS_CHECK_CXX11_SUPPORT VARNAME)
 
   # support for >> in addition to > > when closing double templates
