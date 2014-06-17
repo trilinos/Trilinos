0709bd8fbe3d3d7b07c25d3fd5758f2ad394c574
Todd S. Coffey <tscoffe@sandia.gov>
2014-06-17 10:46:28 -0600

    Removing stk_samba.
    (From Sierra)

--- packages/stk/stk_samba/samba/set_expression.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef SAMBA_SAMBA_SET_EXPRESSION_HPP
-#define SAMBA_SAMBA_SET_EXPRESSION_HPP
-
-#include <samba/set_expression/set_expression_impl.hpp>
-#include <samba/set_expression/output.hpp>
-#include <samba/set_expression/operators.hpp>
-
-#endif //SAMBA_SAMBA_SET_EXPRESSION_HPP
