e18ca2d6f882d6bd8f901a4d1db7d9f33ace78f5
Mark Hoemmen <mhoemme@sandia.gov>
2014-06-25 11:49:34 -0600

    MueLu: Fixed grammar and formatting of tutorial driver comments.
    This commit only affects comments, not source code.

--- packages/muelu/example/Tutorial/tutorial.cpp
+++ packages/muelu/example/Tutorial/tutorial.cpp
@@ -47,6 +47,11 @@
 /*!
 \example tutorial.cpp
 \brief Driver examples for an application that wants to use MueLu as a preconditioner.
+
+This example shows how to create a problem, set up a MueLu hierarchy
+with the help of an XML file, and to solve it using MueLu as a
+preconditioner.  It is meant to be used with one of the .xml files in
+this directory.
 */
 
 //
@@ -88,9 +93,11 @@ typedef KokkosClassic::DefaultKernels<Scalar,LocalOrdinal,Node>::SparseOps  Loca
 
 int main(int argc, char *argv[]) {
   // Most MueLu and Xpetra classes are templated on some or all of the
-  // following template types: Scalar, LocalOrdinal, GlobalOrdinal, Node, and
-  // LocalMatOps. In order to make more concise, MueLu provides typedefs for
-  // its classes, called short names. Thus, instead of writing, for instance,
+  // following template types: Scalar, LocalOrdinal, GlobalOrdinal,
+  // Node, and LocalMatOps. In order to make types more concise, MueLu
+  // has an option to let users refer to its classes by typedefs,
+  // which we call "short names."  Thus, instead of writing, for
+  // instance,
   //
   //   MueLu::Hierarchy<Scalar,LocalOrdinal,GlobalOrdinal,Node,LocalMatOps> H;
   //
@@ -98,14 +105,19 @@ int main(int argc, char *argv[]) {
   //
   //   Hierarchy H;
   //
-  // provided the MueLu_UseShortNames.hpp header and the appropriate class are
-  // included.
+  // provided the MueLu_UseShortNames.hpp header and the appropriate
+  // class are included.  If one wishes to use short names, one must
+  // include this header file before referring to any of the short
+  // names.  One may include it anywhere in the program, but it is
+  // better to include it in the scope where one wants the typedefs
+  // defined.  It's better not to include it at global scope, in order
+  // to avoid polluting the global scope with typedefs.
 #include <MueLu_UseShortNames.hpp>
 
-  // These "using" declarations make the code more concise, in that you don't
-  // have to write the namespace along with the class or object name. This is
-  // especially helpful with commonly used things like std::endl or
-  // Teuchos::RCP.
+  // These "using" declarations make the code more concise, in that
+  // you don't have to write the namespace along with the class or
+  // object name. This is especially helpful with commonly used things
+  // like std::endl or Teuchos::RCP.
   using Teuchos::RCP;
   using Teuchos::rcp;
   using Teuchos::ParameterList;
@@ -125,36 +137,36 @@ int main(int argc, char *argv[]) {
   // Get a pointer to the communicator object representing
   // MPI_COMM_WORLD.
   //
-  // If we didn't build with MPI, we'll get a serial "communicator", i.e. a
-  // communicator with size 1, whose only process has rank 0.
+  // If we didn't build with MPI, we'll get a serial "communicator",
+  // i.e. a communicator with size 1, whose only process has rank 0.
   RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
 
   // Get my process' rank. Equivalent to MPI_Comm_rank.
   const int myRank = comm->getRank();
 
-  // Teuchos::ScalarTraits provides a traits interface to general scalar types.
-  // This allows the same code to be valid for different scalar types, like
-  // double, complex, or high precision (i.e., double-double)
+  // Teuchos::ScalarTraits provides a traits interface to general
+  // scalar types.  This allows the same code to be valid for
+  // different scalar types, like double, complex, or high precision
+  // (i.e., double-double).
   typedef Teuchos::ScalarTraits<SC> STS;
   SC zero = STS::zero(), one = STS::one();
 
   // Make an output stream (for verbose output) that only prints on
-  // Proc 0 of the communicator.
+  // Process 0 of the communicator.
   Teuchos::oblackholestream blackHole;
   std::ostream& out = (myRank == 0) ? std::cout : blackHole;
 
-  // Teuchos provides an interface to get arguments from command line. The first
-  // argument indicates that we don't want any exceptions to be thrown during
-  // command line parsing
+  // Teuchos provides an interface to get arguments from the command
+  // line. The first argument indicates that we don't want any
+  // exceptions to be thrown during command line parsing.
   Teuchos::CommandLineProcessor clp(false);
 
   // The list of valid parameters for the driver comes from three sources:
-  //  - Galeri parameters
-  //    These are responsible for constructing the problem data (i.e., matrix and coordinates).
-  //  - Xpetra parameters
-  //    Allow switching between Epetra and Tpetra
-  //  - Driver parameters
-  //    These are specified below
+  //
+  //  - Galeri parameters: These are responsible for constructing the
+  //    problem data (i.e., matrix and coordinates).
+  //  - Xpetra parameters: Allow switching between Epetra and Tpetra.
+  //  - Driver parameters: These are specified below.
   GO nx = 100, ny = 100, nz = 100;
   Galeri::Xpetra::Parameters<GO> galeriParameters(clp, nx, ny, nz, "Laplace1D"); // manage parameters of the test case
   Xpetra::Parameters             xpetraParameters(clp);                          // manage parameters of Xpetra
@@ -177,36 +189,41 @@ int main(int argc, char *argv[]) {
     case Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL:          break;
   }
 
-  // Retrieve matrix parameters (they may have been changed on the command line)
+  // Retrieve matrix parameters (they may have been changed on the
+  // command line).
   ParameterList galeriList = galeriParameters.GetParameterList();
 
-  // Construct the problem data
-  // Typically, we construct matrix, coordinates, and nullspace, though only matrix is mandatory
+  // Construct the problem data.  Typically, we construct the matrix,
+  // coordinates, and nullspace, though only the matrix is mandatory.
   RCP<Matrix>      A;
   RCP<const Map>   map;
   RCP<MultiVector> coordinates;
   if (matrixFile.empty()) {
     // Use Galeri to construct the data
-    // Galeri provides several pre-defined problem types, including some
-    // stencil based ones (like Laplace matrices in 2D and 3D), and some others
-    // (like elasticity in 2D and 3D)
+    //
+    // Galeri provides several pre-defined problem types, including
+    // some stencil based ones (like Laplace matrices in 2D and 3D),
+    // and some others (like elasticity in 2D and 3D)
     out << "========================================================\n" << xpetraParameters << galeriParameters;
 
     // Galeri will attempt to create a square-as-possible distribution of
     // subdomains di, e.g.,
+    //
     //                                 d1  d2  d3
     //                                 d4  d5  d6
     //                                 d7  d8  d9
     //                                 d10 d11 d12
-    // A perfect distribution is only possible when the #processors is a
-    // perfect square.  This *will* result in "strip" distribution if the
-    // #processors is a prime number or if the factors are very different in
-    // size. For example, np=14 will give a 7-by-2 distribution.  If you don't
-    // want Galeri to do this, specify mx or my on the galeriList.
+    //
+    // A perfect distribution is only possible when the number of
+    // processes is a perfect square.  This *will* result in "strip"
+    // distribution if the number of processes is a prime number or if
+    // the factors are very different in size. For example, np=14 will
+    // give a 7-by-2 distribution.  If you don't want Galeri to do
+    // this, specify mx or my on the galeriList.
     std::string matrixType = galeriParameters.GetMatrixType();
 
-    // Create map (Map) and coordinates (MultiVector)
-    // Xpetra's Map and MultiVector follow Tpetra's interface
+    // Create map (Map) and coordinates (MultiVector).  Xpetra's Map
+    // and MultiVector imitate Tpetra's interface.
     if (matrixType == "Laplace1D") {
       map = Galeri::Xpetra::CreateMap<LO, GO, Node>(xpetraParameters.GetLib(), "Cartesian1D", comm, galeriList);
       coordinates = Galeri::Xpetra::Utils::CreateCartesianCoordinates<SC,LO,GO,Map,MultiVector>("1D", map, galeriList);
@@ -225,17 +242,19 @@ int main(int argc, char *argv[]) {
         << "Processor subdomains in z direction: " << galeriList.get<int>("mz") << std::endl
         << "========================================================" << std::endl;
 
-    // Construct the matrix based on the problem name and provided parameters
+    // Construct the matrix based on the problem name and provided
+    // parameters.
     RCP<Galeri::Xpetra::Problem<Map,CrsMatrixWrap,MultiVector> > Pr =
         Galeri::Xpetra::BuildProblem<SC,LO,GO,Map,CrsMatrixWrap,MultiVector>(galeriParameters.GetMatrixType(), map, galeriList);
     A = Pr->BuildMatrix();
 
   } else {
     // Read in data
-    // This is a convenient way to use the driver, provided you have some
-    // custom data you want to run.  Typically, you need a map (though you may
-    // avoid that in a serial run), a matrix (in a MatrixMarket format), and
-    // a file with coordinates
+    //
+    // This is a convenient way to use the driver, provided you have
+    // some custom data you want to run.  Typically, you need a Map
+    // (though you may avoid that in a serial run), a matrix (in a
+    // MatrixMarket format), and a file with coordinates.
     RCP<const Map> map = (mapFile.empty() ? Teuchos::null : Utils2::ReadMap(mapFile, xpetraParameters.GetLib(), comm));
 
     A = Utils::Read(matrixFile, map);
@@ -243,60 +262,71 @@ int main(int argc, char *argv[]) {
     coordinates = Utils2::ReadMultiVector(coordFile, map);
   }
 
-  // For scalar equations, we assume that the constant vector is a good
-  // approximate nullspace Note, though, that we may skip this step as this is
-  // also a default nullspace in MueLu, if user has not provided it.
+  // For scalar equations, we assume that the constant vector is a
+  // good approximate nullspace.  Note, though, that we may skip this
+  // step as this is also a default nullspace in MueLu, if user has
+  // not provided it.
   RCP<MultiVector> nullspace = MultiVectorFactory::Build(map, 1);
   nullspace->putScalar(one);
 
   out << "Galeri complete.\n========================================================" << std::endl;
 
-  // Hierarchy is a key concept in MueLu. Once set up, it has all the data
-  // required to solve Bx=y, where B is the preconditioner. MueLu provides
-  // multiple interfaces to construct a Hierarchy. The first one is a C++
-  // interface, which is suitable for advanced users. The second one is a XML
-  // file interface, used in this driver. The documentation for the interface
-  // and a list of parameter names is located in
-  // trilinos/packages/muelu/doc/UsersGuide.
+  // Hierarchy is a key concept in MueLu. Once set up, it has all the
+  // data required to apply the preconditioner. MueLu provides
+  // multiple interfaces to construct a Hierarchy. The first one is a
+  // C++ interface, which is suitable for advanced users. The second
+  // one is a XML file interface, used in this driver. You may find
+  // the documentation for the interface and a list of parameter names
+  // in Trilinos/packages/muelu/doc/UsersGuide.
   //
-  // HierarchyManager creates a Hierarchy object based on a provided XML file
-  // parameters. We additionally provide the communicator object so that the
-  // parameter list data can be redistributed to all processors.
+  // HierarchyManager creates a Hierarchy object based on an XML file
+  // containing the parameters.  Only Process 0 reads from this file.
+  // We additionally provide a communicator, so that Process 0 can
+  // broadcast the parameters to all other processes.
   RCP<HierarchyManager> mueLuFactory = rcp(new ParameterListInterpreter(xmlFileName, *comm));
 
-  // The CreateHierarchy() call creates a Hierarchy object. This object is not
-  // set up, though, so it cannot be used yet.
+  // The CreateHierarchy() call creates a Hierarchy object. This
+  // object is not set up, though, so it cannot be used yet.
   RCP<Hierarchy> H = mueLuFactory->CreateHierarchy();
 
-  // All hierarchies have at least one level: the finest level, which must
-  // contain users matrix. We use incrementing numbers for coarser levels. This
-  // way, if we have L levels, level 0 corresponds to the finest level, and
+  // All hierarchies have at least one level: the finest level, which
+  // must contain the user's matrix.  The finest level is level 0, and
+  // subsequent coarser levels have increasing indices.  This way, if
+  // we have L levels, level 0 corresponds to the finest level, and
   // level L-1 is the coarsest level.
   RCP<Level>     L = H->GetLevel(0);
 
-  // Setting finest level matrix is mandatory in MueLu. However, setting
-  // nullspace and coordinates is optional. If nullspace is not provided, it is
-  // automatically generated to be a constant vector. Coordinates are needed
-  // only in two cases: if one uses distance Laplacian filtering, or one uses
-  // repartitioning (which is based on geometric partitioning).
+  // Setting the finest level matrix is mandatory in MueLu. However,
+  // setting the nullspace and coordinates is optional. If nullspace
+  // is not provided, it is automatically generated to be a constant
+  // vector. Coordinates are needed only in two cases: if one uses
+  // distance Laplacian filtering, or one uses repartitioning (which
+  // is based on geometric partitioning).
   L->Set("A",           A);
   L->Set("Nullspace",   nullspace);
   L->Set("Coordinates", coordinates);
 
   // Now that we have set the mandatory data, we can construct the
-  // preconditioner.  The result of this is a fully constructed hierarchy,
-  // which can be used to solve the system.
+  // preconditioner.  The result of this is a fully constructed
+  // hierarchy, which can be used to solve the linear system.
   mueLuFactory->SetupHierarchy(*H);
 
-  // Multigrid methods can be used in a standalon fashion, or they can be used to precondition a Krylov method.
-  // Typically, the second method is preferred, which is indicated by the argument of IsPreconditioner function.
+  // Multigrid methods can be used in a standalone fashion, or they
+  // can be used to precondition a Krylov method.  Typically, the
+  // second method is preferred, which is indicated by the argument of
+  // IsPreconditioner function.
   H->IsPreconditioner(true);
 
-  // To complete the problem description, we construct an initial guess X, and
-  // a right hand size B. Both are base on the same map as is used for row map in A
+  // To complete the problem description, we construct an initial
+  // guess X and a right-hand side B (of the linear system AX=B). Both
+  // have the same Map as A's row Map.
   RCP<Vector> X = VectorFactory::Build(map);
   RCP<Vector> B = VectorFactory::Build(map);
 
+  // For this test problem, we determine the exact solution first (as
+  // a random vector), and compute B so that AX=B exactly.  We then
+  // make X an initial guess vector by filling it with zeros.  You may
+  // use any initial guess you like.
   {
     // We set seed for reproducibility
     Utils::SetRandomSeed(*comm);
@@ -312,28 +342,32 @@ int main(int argc, char *argv[]) {
   typedef MultiVector          MV;
   typedef Belos::OperatorT<MV> OP;
 
-  // Internally, Belos uses its own types. We use special wrappers to wrap
-  // MueLu and Xpetra objects into Belos operators
+  // Internally, Belos uses its own types. We use special wrappers to
+  // wrap MueLu and Xpetra objects into Belos operators.  This is only
+  // one of many different ways one could choose to wrap MueLu and
+  // Xpetra objects in order to get them to work with Belos.
   RCP<OP> belosOp   = rcp(new Belos::XpetraOp<SC, LO, GO, NO, LMO>(A));
   RCP<OP> belosPrec = rcp(new Belos::MueLuOp <SC, LO, GO, NO, LMO>(H));
 
-  // Construct a Belos LinearProblem object. This is a complete problem
-  // formulation. All the data necessary to solve the system is now inside
-  // that problem.
-  RCP< Belos::LinearProblem<SC, MV, OP> > belosProblem = rcp(new Belos::LinearProblem<SC, MV, OP>(belosOp, X, B));
-  belosProblem->setRightPrec(belosPrec);
+  // Construct a Belos LinearProblem object. This is a complete
+  // problem formulation. All the data necessary to solve the system
+  // are now inside that problem.
+  RCP< Belos::LinearProblem<SC, MV, OP> > belosProblem =
+    rcp (new Belos::LinearProblem<SC, MV, OP> (belosOp, X, B));
+  belosProblem->setRightPrec (belosPrec);
 
-  // Prepare the linear problem to solve the linear system that was already passed in.
-  bool set = belosProblem->setProblem();
-  if (set == false) {
+  // Prepare the linear problem to solve the linear system that was
+  // already passed in.
+  bool set = belosProblem->setProblem ();
+  if (! set) {
     out << "\nERROR:  Belos::LinearProblem failed to set up correctly!" << std::endl;
     return EXIT_FAILURE;
   }
 
-  // As many packages in Trilinos, Belos provides a parameter list interface
-  // This allows for an easy tweaking of solver behaviour, such as changing the
-  // number of iterations, or verbosity. For a full list of Belos parameters,
-  // please consult the Belos package documentation.
+  // As many packages in Trilinos, Belos provides a parameter list
+  // interface.  This lets users easily change solver behavior, like
+  // the number of iterations or its verbosity. For a full list of
+  // Belos parameters, please consult the Belos package documentation.
   ParameterList belosList;
   belosList.set("Maximum Iterations",    maxIts); // Maximum number of iterations allowed
   belosList.set("Convergence Tolerance", tol);    // Relative convergence tolerance requested
@@ -341,19 +375,21 @@ int main(int argc, char *argv[]) {
   belosList.set("Output Frequency",      1);
   belosList.set("Output Style",          Belos::Brief);
 
-  // Create an iterative solver manager
-  // Belos provides multiple Krylov methods, with CG and GMRES being most
-  // popular. Based on a command line argument, we select the proper one.  Note
-  // that the C++ objects are templated on scalar, multivector and operator.
-  // This is due to the traits interface in Belos.
+  // Create a Belos iterative linear solver
+  //
+  // Belos provides multiple iterative linear solvers, with CG and
+  // GMRES being most popular. Based on a command line argument, we
+  // select the proper one.  Note that the C++ objects are templated
+  // on scalar, multivector and operator.  This is due to the traits
+  // interface in Belos.
   RCP<Belos::SolverManager<SC, MV, OP> > solver;
   if (solveType == "cg")
     solver = rcp(new Belos::PseudoBlockCGSolMgr<SC,MV,OP>(belosProblem, rcp(&belosList, false)));
   else if (solveType == "gmres")
     solver = rcp(new Belos::BlockGmresSolMgr   <SC,MV,OP>(belosProblem, rcp(&belosList, false)));
 
-  // Finally, perform the solve. We wrap it in try-catch as Belos indicates
-  // erros by throwing exceptions.
+  // Finally, perform the solve.  We wrap it in a try-catch block,
+  // since Belos indicates erros by throwing exceptions.
   Belos::ReturnType ret = Belos::Unconverged;
   try {
     ret = solver->solve();
