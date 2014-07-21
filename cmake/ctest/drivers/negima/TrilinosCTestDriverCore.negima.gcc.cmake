5d40d3a4b24a77ec8e3c5eac63d584fb04169116
Andrey Prokopenko <aprokop@sandia.gov>
2014-07-21 13:50:59 -0600

    Testing: negima: upgraded gcc to 4.9.1

--- cmake/ctest/drivers/negima/TrilinosCTestDriverCore.negima.gcc.cmake
+++ cmake/ctest/drivers/negima/TrilinosCTestDriverCore.negima.gcc.cmake
@@ -91,7 +91,7 @@ MACRO(TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER)
     "-DSuperLU_LIBRARY_NAMES=superlu_4.3"
     )
 
-  SET_DEFAULT(COMPILER_VERSION "GCC-4.8.0")
+  SET_DEFAULT(COMPILER_VERSION "GCC-4.9.1")
 
   #Ensuring that MPI is on for all parallel builds that might be run.
   IF(COMM_TYPE STREQUAL MPI)
@@ -108,8 +108,8 @@ MACRO(TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER)
 
     SET( EXTRA_SYSTEM_CONFIGURE_OPTIONS
       ${EXTRA_SYSTEM_CONFIGURE_OPTIONS}
-      "-DCMAKE_CXX_COMPILER:FILEPATH=/home/aprokop/local/opt/gcc-4.8.0/bin/g++"
-      "-DCMAKE_C_COMPILER:FILEPATH=/home/aprokop/local/opt/gcc-4.8.0/bin/gcc"
+      "-DCMAKE_CXX_COMPILER:FILEPATH=/home/aprokop/local/opt/gcc-4.9.1/bin/g++"
+      "-DCMAKE_C_COMPILER:FILEPATH=/home/aprokop/local/opt/gcc-4.9.1/bin/gcc"
       )
 
   ENDIF()
