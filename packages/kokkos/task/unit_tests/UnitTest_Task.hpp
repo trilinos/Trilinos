cd3d0d7cfa6d588672d7fd8bb12bd756971a12e2
Carter Edwards <hcedwar@sandia.gov>
2014-07-21 11:28:54 -0600

    Kokkos/Qthread : notes on task + execution policy

--- packages/kokkos/task/unit_tests/UnitTest_Task.hpp
+++ packages/kokkos/task/unit_tests/UnitTest_Task.hpp
@@ -53,17 +53,22 @@ namespace Test {
 
 //----------------------------------------------------------------------------
 
-template< class Device >
-struct FibChild : public Kokkos::task_serial<Device,long> {
+template< class ExecSpace >
+struct FibChild : public Kokkos::task_serial<ExecSpace,long> {
 
 #if 0
-  Kokkos::TaskPool< FibChild > fib_task_pool ;
+  Kokkos::TaskExecPolicy< ExecSpace > policy ;
 #endif
 
   const long n ;
   int has_nested ;
 
+#if 0
+  FibChild( const Kokkos::TaskExecPolicy<ExecSpace> & arg_policy , const long arg_n )
+    : policy(arg_policy), n( arg_n ), has_nested(0) {}
+#else
   FibChild( const long arg_n ) : n( arg_n ), has_nested(0) {}
+#endif
 
   inline
   void apply( long & result )
@@ -73,18 +78,13 @@ struct FibChild : public Kokkos::task_serial<Device,long> {
       }
       else {
         if ( has_nested ) {
-#if 1
-          const Kokkos::Future<long,Device> fib_1 = Kokkos::task_dependence(this,0);
-          const Kokkos::Future<long,Device> fib_2 = Kokkos::task_dependence(this,1);
-#else
-          const Kokkos::Future<long,Device> fib_1 = fib_task_pool.dependence(this,0);
-          const Kokkos::Future<long,Device> fib_2 = fib_task_pool.dependence(this,1);
-#endif
+          const Kokkos::Future<long,ExecSpace> fib_1 = Kokkos::task_dependence(this,0);
+          const Kokkos::Future<long,ExecSpace> fib_2 = Kokkos::task_dependence(this,1);
 
           result = fib_1.get() + fib_2.get();
         }
         else {
-          Kokkos::Future<long,Device> nested[2] ;
+          Kokkos::Future<long,ExecSpace> nested[2] ;
           // Spawn new children and respawn myself to sum their results:
           has_nested = 1 ;
 #if 1
@@ -92,17 +92,17 @@ struct FibChild : public Kokkos::task_serial<Device,long> {
           nested[1] = Kokkos::spawn( FibChild(n-2) );
           Kokkos::respawn( this , nested , nested + 2 );
 #else
-          nested[0] = fib_task_pool.spawn( n-1 );
-          nested[1] = fib_task_pool.spawn( n-2 );
-          fib_task_pool.respawn( this , nested , nested + 2 );
+          nested[0] = Kokkos::spawn( policy , FibChild(n-1) );
+          nested[1] = Kokkos::spawn( policy , FibChild(n-2) );
+          Kokkos::respawn( policy.depend(nested,2) , *this );
 #endif
         }
       }
     }
 };
 
-template< class Device >
-struct FibChild2 : public Kokkos::task_serial<Device,long> {
+template< class ExecSpace >
+struct FibChild2 : public Kokkos::task_serial<ExecSpace,long> {
 
   const long n ;
   int nested ;
@@ -113,7 +113,7 @@ struct FibChild2 : public Kokkos::task_serial<Device,long> {
   void apply( long & result )
     {
       if ( ! nested ) {
-        Kokkos::Future<long,Device> nest[2] ;
+        Kokkos::Future<long,ExecSpace> nest[2] ;
         if ( n < 2 ) {
           result = n ;
         }
@@ -138,8 +138,8 @@ struct FibChild2 : public Kokkos::task_serial<Device,long> {
         }
      }
      else {
-        const Kokkos::Future<long,Device> fib_a = Kokkos::task_dependence(this,0);
-        const Kokkos::Future<long,Device> fib_b = Kokkos::task_dependence(this,1);
+        const Kokkos::Future<long,ExecSpace> fib_a = Kokkos::task_dependence(this,0);
+        const Kokkos::Future<long,ExecSpace> fib_b = Kokkos::task_dependence(this,1);
 
         result = ( nested == 2 ) ? fib_a.get() + fib_b.get()
                                  : 3 * fib_a.get() + 2 * fib_b.get() ;
@@ -152,10 +152,16 @@ long eval_fib( long n )
   return n < 2 ? n : eval_fib(n-2) + eval_fib(n-1);
 }
 
-template< class Device >
+template< class ExecSpace >
 void test_fib( long n )
 {
-  Kokkos::Future<long,Device> f = Kokkos::spawn( FibChild<Device>(n) );
+#if 0
+  Kokkos::TaskExecPolicy< ExecSpace > policy ;
+
+  Kokkos::Future<long> f = Kokkos::spawn( policy , FibChild(policy,n) );
+#endif
+
+  Kokkos::Future<long,ExecSpace> f = Kokkos::spawn( FibChild<ExecSpace>(n) );
 
   Kokkos::wait( f );
 
@@ -166,10 +172,10 @@ void test_fib( long n )
   }
 }
 
-template< class Device >
+template< class ExecSpace >
 void test_fib2( long n )
 {
-  Kokkos::Future<long,Device> f = Kokkos::spawn( FibChild2<Device>(n) );
+  Kokkos::Future<long,ExecSpace> f = Kokkos::spawn( FibChild2<ExecSpace>(n) );
 
   Kokkos::wait( f );
 
@@ -182,37 +188,37 @@ void test_fib2( long n )
 
 //----------------------------------------------------------------------------
 
-template< class Device >
-struct Norm2 : public Kokkos::task_reduce<Device,double,size_t> {
+template< class ExecSpace >
+struct Norm2 : public Kokkos::task_reduce<ExecSpace,double,size_t> {
 
   const double * const m_x ;
 
-  Norm2( const int n , const double * x ) : Kokkos::task_reduce<Device,double,size_t>(n), m_x(x) {}
+  Norm2( const int n , const double * x ) : Kokkos::task_reduce<ExecSpace,double,size_t>(n), m_x(x) {}
 
   void operator()( int i , double & val ) const { val += m_x[i] * m_x[i] ; }
 
   void final( double & dst ) const { dst = std::sqrt( dst ); }
 };
 
-template< class Device >
+template< class ExecSpace >
 void test_norm2( const int n )
 {
   double * const x = new double[n];
 
   for ( int i = 0 ; i < n ; ++i ) x[i] = 1 ;
 
-  Kokkos::Future<double,Device> f = Kokkos::spawn( Norm2<Device>(n,x) );
+  Kokkos::Future<double,ExecSpace> f = Kokkos::spawn( Norm2<ExecSpace>(n,x) );
 
 #if 0
 
-  Kokkos::Future<double,Device> f =
-    Kokkos::spawn< Kokkos::task_reduce<double,Device,size_t> >
+  Kokkos::Future<double,ExecSpace> f =
+    Kokkos::spawn< Kokkos::task_reduce<double,ExecSpace,size_t> >
       ( n
       , [=]( int i , double & val ) { val += x[i] * x[i] ; } /* data parallel operator */
       , []( double & val ) { val = std::sqrt( val ); }       /* task serial operator */
       );
 
-  Kokkos::parallel_reduce<Device,double,int>( n , Norm2(x) , result );
+  Kokkos::parallel_reduce<ExecSpace,double,int>( n , Norm2(x) , result );
     // Compose task-functor from input functor type ...
 
 #endif
