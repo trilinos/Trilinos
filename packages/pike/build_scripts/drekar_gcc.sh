4e68e5328bb22ae5ecc84500c3f3e7b1100bf798
Roger Pawlowski <rppawlo@sandia.gov>
2014-07-23 12:48:38 -0400

    Pike: updated build script

--- packages/pike/build_scripts/drekar_gcc.sh
+++ packages/pike/build_scripts/drekar_gcc.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 rm -rf CMakeCache.txt CMakeFiles
 cmake \
--D CMAKE_INSTALL_PREFIX="/home/rppawlo/install/gnu4.8.1/pike" \
+-D CMAKE_INSTALL_PREFIX="/home/rppawlo/install/gnu4.8.2/pike" \
 -D Trilinos_EXTRA_REPOSITORIES="pike" \
 -D Trilinos_ENABLE_ALL_PACKAGES:BOOL=OFF \
 -D Trilinos_ENABLE_ALL_OPTIONAL_PACKAGES:BOOL=ON \
@@ -14,19 +14,19 @@ cmake \
 -D Pike_ENABLE_TESTS:BOOL=ON \
 -D Trilinos_ENABLE_TEUCHOS_TIME_MONITOR:BOOL=OFF \
 -D Pike_ENABLE_TEUCHOS_TIME_MONITOR:BOOL=ON \
--D TPL_BLAS_LIBRARIES:PATH="/home/rppawlo/install/gnu4.8.1/blas/libblas.a" \
--D TPL_LAPACK_LIBRARIES:PATH="/home/rppawlo/install/gnu4.8.1/lapack/liblapack.a" \
+-D TPL_BLAS_LIBRARIES:PATH="/home/rppawlo/install/gnu4.8.2/blas/libblas.a" \
+-D TPL_LAPACK_LIBRARIES:PATH="/home/rppawlo/install/gnu4.8.2/lapack/liblapack.a" \
 -D TPL_ENABLE_MPI:BOOL=ON \
--D MPI_BASE_DIR:PATH="/home/rppawlo/install/gnu4.8.1/openmpi" \
+-D MPI_BASE_DIR:PATH="/home/rppawlo/install/gnu4.8.2/openmpi" \
       -D TPL_ENABLE_Boost:BOOL=ON \
-      -D Boost_INCLUDE_DIRS:FILEPATH="/home/rppawlo/install/gnu4.8.1/boost/include" \
+      -D Boost_INCLUDE_DIRS:FILEPATH="/home/rppawlo/install/gnu4.8.2/boost/include" \
       -D TPL_ENABLE_BoostLib:BOOL=ON \
-      -D BoostLib_INCLUDE_DIRS:FILEPATH="/home/rppawlo/install/gnu4.8.1/boost/include" \
-      -D BoostLib_LIBRARY_DIRS:FILEPATH="/home/rppawlo/install/gnu4.8.1/boost/lib/" \
+      -D BoostLib_INCLUDE_DIRS:FILEPATH="/home/rppawlo/install/gnu4.8.2/boost/include" \
+      -D BoostLib_LIBRARY_DIRS:FILEPATH="/home/rppawlo/install/gnu4.8.2/boost/lib/" \
 -D MPI_EXEC_MAX_NUMPROCS:STRING=6 \
--D CMAKE_CXX_COMPILER:FILEPATH="/home/rppawlo/install/gnu4.8.1/openmpi/bin/mpiCC" \
--D CMAKE_C_COMPILER:FILEPATH="/home/rppawlo/install/gnu4.8.1/openmpi/bin/mpicc" \
--D CMAKE_Fortran_COMPILER:FILEPATH="/home/rppawlo/install/gnu4.8.1/openmpi/bin/mpif77" \
+-D CMAKE_CXX_COMPILER:FILEPATH="/home/rppawlo/install/gnu4.8.2/openmpi/bin/mpiCC" \
+-D CMAKE_C_COMPILER:FILEPATH="/home/rppawlo/install/gnu4.8.2/openmpi/bin/mpicc" \
+-D CMAKE_Fortran_COMPILER:FILEPATH="/home/rppawlo/install/gnu4.8.2/openmpi/bin/mpif77" \
 -D Trilinos_ENABLE_Fortran:BOOL=ON \
 -D CMAKE_CXX_FLAGS:STRING="-g -Wall" \
 -D CMAKE_C_FLAGS:STRING="" \
