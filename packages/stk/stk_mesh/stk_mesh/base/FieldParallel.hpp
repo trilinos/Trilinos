6f70ae631c9e29ac41b29ea0859673cf39588588
David Glaze <djglaze@sandia.gov>
2014-08-11 13:44:54 -0600

    Removed redundant parallel_reduce() function
    The parallel_reduce() function was removed in favor of the
    new-and-improved parallel_sum(), parallel_min(), and parallel_max(),
    to aspire to the goal of only one way to do things.  The
    parallel_reduce() function could only reduce a single field, and
    used the strange idiom of passing in the reduction operator coupled
    with the target field.  This previously used a very hairy recursive
    templated reduction operator that was shown to be part of the
    performance issues that lead to the rewrite of parallel_sum() and
    friends some time ago.  So, there's not much motivation left to keep
    this thing (and supporting functions) around.
    
    This change gives a ~3% overall speedup in the nalu edgeKsgsjetXflowML
    performance test (the "misc" timer category for each equation is roughly
    cut in half).
    
    (From Sierra)
    
    Build/Test Cases Summary
    Enabled Packages: STKMesh, STK, STKUnit_tests
    Disabled Packages: PyTrilinos,SEACASExo2mat,SEACASMat2exo,Optika,STKClassic,Sundance
    Enabled all Forward Packages
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_SS => passed: passed=11,notpassed=0 (9.43 min)
    3) SERIAL_RELEASE_SS => passed: passed=9,notpassed=0 (12.02 min)
    Other local commits for this build/test group: 478df2b, a2b43c8, 0535191

--- packages/stk/stk_mesh/stk_mesh/base/FieldParallel.hpp
+++ packages/stk/stk_mesh/stk_mesh/base/FieldParallel.hpp
@@ -11,6 +11,7 @@
 
 #include <stk_util/stk_config.h>
 #include <stk_mesh/base/Types.hpp>      // for EntityProc
+#include <stk_mesh/base/FieldTraits.hpp>  // for FieldTraits
 #include <stk_mesh/base/FieldBase.hpp>  // for FieldBase
 #include <stk_mesh/base/BulkData.hpp>
 #include <stk_util/parallel/Parallel.hpp>  // for ParallelMachine
@@ -21,8 +22,6 @@
 #include <vector>                       // for vector
 
 namespace stk { namespace mesh { class Ghosting; } }
-
-#include <stk_mesh/base/FieldParallel_helpers.hpp>
 
 namespace stk {
 namespace mesh {
@@ -51,73 +50,6 @@ void copy_owned_to_shared( const BulkData& mesh,
                            const std::vector< const FieldBase *> & fields )
 {
   communicate_field_data(*mesh.ghostings()[0], fields);
-}
-
-
-/** Communicate field data among shared entities.
- * This function is a helper function for the parallel_reduce/sum/max/min functions below.
- * This function sends field-data for each shared entity to each sharing processor. When this
- * function is finished, the communicated data is in the 'sparse' CommAll object, not unpacked yet.
- * The data is then unpacked by the calling parallel_* function which also performs the
- * sum/max/min operation on the data before storing it.
- */
-void communicate_field_data(
-  const BulkData & mesh ,
-  const unsigned field_count ,
-  const FieldBase * const * fields ,
-  CommAll & sparse );
-
-/** debugging function... do we need this?
- */
-void communicate_field_data_verify_read( CommAll & );
-
-//----------------------------------------------------------------------
-/** Parallel reduction of shared entities' field data.
- *
- *  example usage:
- *    parallel_reduce( mesh , sum( field ) );
- *
- *  where the operations are: sum, max, min
- */
-template< class OpField >
-inline
-void parallel_reduce( const BulkData & mesh ,
-                      const OpField  & op )
-{
-  const FieldBase * fields[1] = { & op.field };
-
-  CommAll sparse ;
-
-  communicate_field_data( mesh, 1, fields, sparse );
-
-  op( mesh, sparse );
-
-  // For debugging:
-  // communicate_field_data_verify_read( sparse );
-}
-
-/** Parallel reduction of shared entities' field data.
- *
- *  example usage:
- *    parallel_reduce( mesh , sum( fieldA ) , max( fieldB ) );
- */
-template< class OpField1 , class OpField2 >
-inline
-void parallel_reduce( const BulkData & mesh ,
-                      const OpField1 & op1 ,
-                      const OpField2 & op2 )
-{
-  const FieldBase * fields[2] = { & op1.field , & op2.field };
-
-  CommAll sparse ;
-
-  communicate_field_data( mesh, 2, fields, sparse );
-
-  op1( mesh , sparse );
-  op2( mesh , sparse );
-
-  // For debugging:
-  // communicate_field_data_verify_read( sparse );
 }
 
 //----------------------------------------------------------------------
