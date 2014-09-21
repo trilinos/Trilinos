0d69beaacbede6c6e42cbe94762f85d13a2e84fd
Nico Schlömer <nico.schloemer@gmail.com>
2014-09-21 20:38:17 +0200

    [TriUtils] whitespace changes; removed tabs
    This improves readability of the code and eases future editing.
    
    Build/Test Cases Summary
    Enabled Packages: Triutils
    Disabled Packages: Pamgen,Intrepid
    Enabled all Forward Packages
    0) MPI_DEBUG => passed: passed=712,notpassed=0 (17.65 min)
    1) SERIAL_RELEASE => passed: passed=659,notpassed=0 (4.39 min)

--- packages/triutils/src/Trilinos_Util_ReadTriples2Epetra.cpp
+++ packages/triutils/src/Trilinos_Util_ReadTriples2Epetra.cpp
@@ -1,12 +1,12 @@
 // @HEADER
 // ***********************************************************************
-// 
+//
 //                 TriUtils: Trilinos Utilities Package
 //                 Copyright (2011) Sandia Corporation
-// 
+//
 // Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
 // license for use of this work by or on behalf of the U.S. Government.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -34,8 +34,8 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
-// 
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
 // ***********************************************************************
 // @HEADER
 
@@ -56,40 +56,47 @@
 #include "Epetra_CrsMatrix.h"
 
 template<typename int_type>
-int Trilinos_Util_ReadTriples2Epetra_internal( char *data_file,
-				      bool symmetric, 
-				      const Epetra_Comm  &comm, 
-				      Epetra_Map *& map, 
-				      Epetra_CrsMatrix *& A, 
-				      Epetra_Vector *& x, 
-				      Epetra_Vector *& b,
-				      Epetra_Vector *&xexact,
-				      bool NonUniformMap=false,
-				      bool TimDavisHeader=false,
-				      bool ZeroBased=false ) {
+int Trilinos_Util_ReadTriples2Epetra_internal(
+    char *data_file,
+    bool symmetric,
+    const Epetra_Comm  &comm,
+    Epetra_Map *& map,
+    Epetra_CrsMatrix *& A,
+    Epetra_Vector *& x,
+    Epetra_Vector *& b,
+    Epetra_Vector *&xexact,
+    bool NonUniformMap=false,
+    bool TimDavisHeader=false,
+    bool ZeroBased=false
+    )
+{
   FILE *in_file ;
-  int_type N_rows = 0, nnz = 0; 
-  
-  const int BUFSIZE = 800 ; 
-  char buffer[BUFSIZE] ; 
+  int_type N_rows = 0, nnz = 0;
+
+  const int BUFSIZE = 800 ;
+  char buffer[BUFSIZE] ;
   std::vector<int> non_zeros;   // Number of non-zeros in each row
   Trilinos_Util_CountTriples( data_file, symmetric, non_zeros, N_rows, nnz, comm, TimDavisHeader, ZeroBased ) ;
 
 #if 0
-  cout << " Trilinos_Util_ReadTriples2Epetra.cpp::" << __LINE__ << "  N_rows = " << N_rows << endl << "non_zeros = " ; 
-  for (int_type i= 0; i<N_rows; i++ )  cout << non_zeros[i] ; 
-  cout << endl ; 
+  cout << " Trilinos_Util_ReadTriples2Epetra.cpp::" << __LINE__ << "  N_rows = " << N_rows << endl << "non_zeros = " ;
+  for (int_type i= 0; i<N_rows; i++ )  cout << non_zeros[i] ;
+  cout << endl ;
   comm.Barrier();
 #endif
- 
+
   int_type NumNonZeroRows=0;
   std::vector<int_type> MapIndices;
-  if(comm.MyPID() == 0)  { 
+  if(comm.MyPID() == 0)  {
     if ( NonUniformMap ) {
-      for (int_type i= 0; i<N_rows; i++ )  if( non_zeros[i] > 0 ) NumNonZeroRows++;
+      for (int_type i= 0; i<N_rows; i++ )
+        if( non_zeros[i] > 0 )
+          NumNonZeroRows++;
       MapIndices.resize(NumNonZeroRows);
       int_type IndexNumber =0;
-      for (int_type i= 0; i<N_rows; i++ ) if( non_zeros[i] > 0 ) MapIndices[IndexNumber++]=i;
+      for (int_type i= 0; i<N_rows; i++ )
+        if( non_zeros[i] > 0 )
+          MapIndices[IndexNumber++]=i;
     }
   }
   //
@@ -97,58 +104,60 @@ int Trilinos_Util_ReadTriples2Epetra_internal( char *data_file,
   //
   if ( NonUniformMap ) {
     //    int value[1];
-    //    values[0] = 
-    comm.Broadcast( &NumNonZeroRows, 1, 0 ) ; 
+    //    values[0] =
+    comm.Broadcast( &NumNonZeroRows, 1, 0 ) ;
     MapIndices.resize(NumNonZeroRows);
-    comm.Broadcast( &MapIndices[0], NumNonZeroRows, 0 ) ; 
+    comm.Broadcast( &MapIndices[0], NumNonZeroRows, 0 ) ;
   }
 
   std::vector<int_type> ptrs(N_rows+1) ; // Pointers into inds and vals for the start of each row
   std::vector<int_type> inds(nnz);     //  Column Indices
   std::vector<double> vals(nnz);  //  Matrix values
 
-  if(comm.MyPID() == 0)  { 
+  if(comm.MyPID() == 0)  {
     //  ptrs, inds and vals together constitute a compressed row storage of the matrix.
 
-
     in_file = fopen( data_file, "r");
-    assert (in_file != NULL) ;  // Checked in T_U_CountTriples() 
+    assert (in_file != NULL) ;  // Checked in T_U_CountTriples()
 
-    ptrs[0] = 0 ; 
-    for( int_type i=0; i< N_rows; i++ ) { 
-      ptrs[i+1] = ptrs[i] + non_zeros[i]; 
+    ptrs[0] = 0 ;
+    for( int_type i=0; i< N_rows; i++ ) {
+      ptrs[i+1] = ptrs[i] + non_zeros[i];
     }
 
     std::vector<int_type> iptrs = ptrs ; //  Current pointers into inds and vals for each row
 
-    if ( TimDavisHeader ) fgets( buffer, BUFSIZE, in_file ); // Throw away the Tim Davis Header Line 
-    while ( fgets( buffer, BUFSIZE, in_file ) ) { 
-      int_type i, j; 
-      double val ; 
+    // Throw away the Tim Davis Header Line
+    if ( TimDavisHeader )
+      fgets( buffer, BUFSIZE, in_file );
+
+    while ( fgets( buffer, BUFSIZE, in_file ) ) {
+      int_type i, j;
+      double val ;
       if(sizeof(int) == sizeof(int_type))
-        sscanf( buffer, "%d %d %lg", &i, &j, &val ) ; 
+        sscanf( buffer, "%d %d %lg", &i, &j, &val ) ;
       else if(sizeof(long long) == sizeof(int_type))
-        sscanf( buffer, "%lld %lld %lg", &i, &j, &val ) ; 
+        sscanf( buffer, "%lld %lld %lg", &i, &j, &val ) ;
       else
         assert(false);
       const int_type i_index = ( ZeroBased?i:i-1 );
       const int_type j_index = ( ZeroBased?j:j-1 );
 
-      int_type iptr = iptrs[i_index] ; 
+      int_type iptr = iptrs[i_index] ;
       iptrs[i_index]++ ;
-      vals[iptr] = val ; 
-      inds[iptr] = j_index ; 
+      vals[iptr] = val ;
+      inds[iptr] = j_index ;
       //
-      //  If this is a symmetric matrix, we need to enter the entry 
+      //  If this is a symmetric matrix, we need to enter the entry
       //  for the other triangular half
       //
       if (symmetric && i != j ) {
-	iptr = iptrs[j_index] ; 
-	iptrs[j_index]++;
-	vals[iptr] = val ; 
-	inds[iptr] = i_index ; 
+        iptr = iptrs[j_index] ;
+        iptrs[j_index]++;
+        vals[iptr] = val ;
+        inds[iptr] = i_index ;
       }
-    } 
+    }
     fclose(in_file);
   }
 
@@ -158,17 +167,17 @@ int Trilinos_Util_ReadTriples2Epetra_internal( char *data_file,
       if(NumNonZeroRows > std::numeric_limits<int>::max())
         throw "Triutils: Trilinos_Util_ReadTriples2Epetra_internal: NumNonZeroRows > std::numeric_limits<int>::max()";
       nlocal = static_cast<int>(NumNonZeroRows);
-	}
+    }
     map = new Epetra_Map(NumNonZeroRows, nlocal, &MapIndices[0], (int_type) 0, comm); // Create map with all elements on PE 0
-  } else { 
+  } else {
     if (comm.MyPID()==0) {
       if(N_rows > std::numeric_limits<int>::max())
         throw "Triutils: Trilinos_Util_ReadTriples2Epetra_internal: N_rows > std::numeric_limits<int>::max()";
       nlocal = static_cast<int>(N_rows);
-	}
+    }
     map = new Epetra_Map(N_rows, nlocal, (int_type) 0, comm); // Create map with all elements on PE 0
   }
-  
+
   A = new Epetra_CrsMatrix(Copy, *map, 0); // Construct matrix on PE 0
 
   if (comm.MyPID()==0)
@@ -183,14 +192,13 @@ int Trilinos_Util_ReadTriples2Epetra_internal( char *data_file,
   std::vector<double> hbb(N_rows);
   std::vector<double> hbxexact(N_rows);
 
-  
   x = new Epetra_Vector(Copy, *map, &hbx[0]);
   b = new Epetra_Vector(Copy, *map, &hbb[0]);
   xexact = new Epetra_Vector(Copy, *map, &hbxexact[0]);
 
   EPETRA_CHK_ERR( x->PutScalar( 0.0 ) );
-  EPETRA_CHK_ERR( xexact->Random( ) ) ; 
-  EPETRA_CHK_ERR( A->Multiply( false, *xexact, *b ) ); 
+  EPETRA_CHK_ERR( xexact->Random( ) ) ;
+  EPETRA_CHK_ERR( A->Multiply( false, *xexact, *b ) );
 
   return 0;
 }
@@ -198,18 +206,18 @@ int Trilinos_Util_ReadTriples2Epetra_internal( char *data_file,
 #ifndef EPETRA_NO_32BIT_GLOBAL_INDICES
 
 int Trilinos_Util_ReadTriples2Epetra( char *data_file,
-				      bool symmetric, 
-				      const Epetra_Comm  &comm, 
-				      Epetra_Map *& map, 
-				      Epetra_CrsMatrix *& A, 
-				      Epetra_Vector *& x, 
-				      Epetra_Vector *& b,
-				      Epetra_Vector *&xexact,
-				      bool NonUniformMap=false,
-				      bool TimDavisHeader=false,
-				      bool ZeroBased=false ) {
+    bool symmetric,
+    const Epetra_Comm  &comm,
+    Epetra_Map *& map,
+    Epetra_CrsMatrix *& A,
+    Epetra_Vector *& x,
+    Epetra_Vector *& b,
+    Epetra_Vector *&xexact,
+    bool NonUniformMap=false,
+    bool TimDavisHeader=false,
+    bool ZeroBased=false ) {
   return Trilinos_Util_ReadTriples2Epetra_internal<int>(data_file, symmetric, comm, map, A, x, b,
-	  xexact, NonUniformMap, TimDavisHeader, ZeroBased);
+      xexact, NonUniformMap, TimDavisHeader, ZeroBased);
 }
 
 #endif
@@ -217,18 +225,18 @@ int Trilinos_Util_ReadTriples2Epetra( char *data_file,
 #ifndef EPETRA_NO_64BIT_GLOBAL_INDICES
 
 int Trilinos_Util_ReadTriples2Epetra64( char *data_file,
-				      bool symmetric, 
-				      const Epetra_Comm  &comm, 
-				      Epetra_Map *& map, 
-				      Epetra_CrsMatrix *& A, 
-				      Epetra_Vector *& x, 
-				      Epetra_Vector *& b,
-				      Epetra_Vector *&xexact,
-				      bool NonUniformMap=false,
-				      bool TimDavisHeader=false,
-				      bool ZeroBased=false ) {
+    bool symmetric,
+    const Epetra_Comm  &comm,
+    Epetra_Map *& map,
+    Epetra_CrsMatrix *& A,
+    Epetra_Vector *& x,
+    Epetra_Vector *& b,
+    Epetra_Vector *&xexact,
+    bool NonUniformMap=false,
+    bool TimDavisHeader=false,
+    bool ZeroBased=false ) {
   return Trilinos_Util_ReadTriples2Epetra_internal<long long>(data_file, symmetric, comm, map, A, x, b,
-	  xexact, NonUniformMap, TimDavisHeader, ZeroBased);
+      xexact, NonUniformMap, TimDavisHeader, ZeroBased);
 }
 
 #endif
--- packages/triutils/src/Trilinos_Util_read_coo.cpp
+++ packages/triutils/src/Trilinos_Util_read_coo.cpp
@@ -1,12 +1,12 @@
 // @HEADER
 // ***********************************************************************
-// 
+//
 //                 TriUtils: Trilinos Utilities Package
 //                 Copyright (2011) Sandia Corporation
-// 
+//
 // Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
 // license for use of this work by or on behalf of the U.S. Government.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
@@ -34,26 +34,25 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact Michael A. Heroux (maherou@sandia.gov) 
-// 
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
 // ***********************************************************************
 // @HEADER
 
 #include "Trilinos_Util.h"
 
 void Trilinos_Util_read_coo(char *data_file, int MyPID,
-	      int *N_global, int *n_nonzeros,
-	      double **val, int **bindx,
-	      double **x, double **b, double **xexact)
-#undef DEBUG 
-     /*  read ASCII data file:
-	 line 1: N_global, number of entries (%d,%d)
-	 line 2-...: i,j,real (%d, %d, %f)
-     */
+    int *N_global, int *n_nonzeros,
+    double **val, int **bindx,
+    double **x, double **b, double **xexact)
+#undef DEBUG
+  /*  read ASCII data file:
+      line 1: N_global, number of entries (%d,%d)
+      line 2-...: i,j,real (%d, %d, %f)
+      */
 
 {
   FILE *data ;
-
 
   int i, n_entries, N_columns;
   int ii, jj ;
@@ -64,89 +63,80 @@ void Trilinos_Util_read_coo(char *data_file, int MyPID,
   double *val1;
 
   int MAXBLOCKSIZE = 1;
-  
-  if(MyPID == 0) 
-    { 
 
- 
-      data = fopen(data_file,"r") ;
- 
-      fscanf(data, "%d %d %d", N_global, &N_columns, &n_entries) ;
-      if (N_columns != *N_global)
-	perror("Matrix dimensions must be the same");
-      printf("Reading from file: %s\n",data_file);
-      printf("Number of equations = %d\n",*N_global);
-      printf("Number of entries   = %d\n",n_entries);
-    
+  if(MyPID == 0) {
+    data = fopen(data_file,"r") ;
 
-      *bindx = (int   *) calloc(n_entries+1,sizeof(int)) ;
-      *val = (double *) calloc(n_entries+1,sizeof(double)) ;
+    fscanf(data, "%d %d %d", N_global, &N_columns, &n_entries) ;
+    if (N_columns != *N_global)
+      perror("Matrix dimensions must be the same");
+    printf("Reading from file: %s\n",data_file);
+    printf("Number of equations = %d\n",*N_global);
+    printf("Number of entries   = %d\n",n_entries);
 
-      pntr1 = (int   *) calloc(n_entries+1,sizeof(int)) ;
-      indx1 = (int   *) calloc(n_entries+1,sizeof(int)) ;
-      val1 = (double *) calloc(n_entries+1,sizeof(double)) ;
-  
-      pntr = (int   *) calloc(n_entries+1,sizeof(int)) ;
+    *bindx = (int   *) calloc(n_entries+1,sizeof(int)) ;
+    *val = (double *) calloc(n_entries+1,sizeof(double)) ;
 
-      if ((pntr) == NULL) 
-	perror("Error: Not enough space to create matrix");
+    pntr1 = (int   *) calloc(n_entries+1,sizeof(int)) ;
+    indx1 = (int   *) calloc(n_entries+1,sizeof(int)) ;
+    val1 = (double *) calloc(n_entries+1,sizeof(double)) ;
 
-      while(!feof(data))
-	{
-	  fscanf(data, "%d %d %lf", &ii, &jj, &value) ;
-	  max_ii = Trilinos_Util_max(max_ii,ii);
-	  max_jj = Trilinos_Util_max(max_jj,jj);
+    pntr = (int   *) calloc(n_entries+1,sizeof(int)) ;
+
+    if ((pntr) == NULL)
+      perror("Error: Not enough space to create matrix");
+
+    while(!feof(data)) {
+      fscanf(data, "%d %d %lf", &ii, &jj, &value) ;
+      max_ii = Trilinos_Util_max(max_ii,ii);
+      max_jj = Trilinos_Util_max(max_jj,jj);
 #ifdef DEBUG
-	  printf("Entry %d, %d = %lf.\n",ii,jj,value);
+      printf("Entry %d, %d = %lf.\n",ii,jj,value);
 #endif
-	  (*bindx)[kk] = ii-1;
-	  pntr[kk] = jj-1;
-	  (*val)[kk] = value;
-	  kk++;
-	}
-      *n_nonzeros = kk-1;
-      *N_global = max_ii;
-      if (max_ii != max_jj) perror("Error: Number of rows and columns not equal");
+      (*bindx)[kk] = ii-1;
+      pntr[kk] = jj-1;
+      (*val)[kk] = value;
+      kk++;
+    }
+    *n_nonzeros = kk-1;
+    *N_global = max_ii;
+    if (max_ii != max_jj) perror("Error: Number of rows and columns not equal");
 
-      printf("Number of nonzeros = %d\n",*n_nonzeros);
+    printf("Number of nonzeros = %d\n",*n_nonzeros);
 
-      /* Convert real part in the following way:
-	 - Convert COO to CSR
-	 - CSR to CSC
-	 - CSC to CSR (columns are now in ascending order)
-	 - CSR to MSR
-      */
-      Trilinos_Util_coocsr(*N_global, *n_nonzeros, *val, *bindx, pntr, val1, indx1, pntr1);
+    /* Convert real part in the following way:
+       - Convert COO to CSR
+       - CSR to CSC
+       - CSC to CSR (columns are now in ascending order)
+       - CSR to MSR
+       */
+    Trilinos_Util_coocsr(*N_global, *n_nonzeros, *val, *bindx, pntr, val1, indx1, pntr1);
 
-      Trilinos_Util_csrcsc(*N_global, *N_global, 0, 0, val1, indx1, pntr1,*val, *bindx, pntr);
+    Trilinos_Util_csrcsc(*N_global, *N_global, 0, 0, val1, indx1, pntr1,*val, *bindx, pntr);
 
-      Trilinos_Util_csrcsc(*N_global, *N_global, 0, 0, *val,*bindx, pntr, val1, indx1, pntr1);
+    Trilinos_Util_csrcsc(*N_global, *N_global, 0, 0, *val,*bindx, pntr, val1, indx1, pntr1);
 
-      Trilinos_Util_csrmsr(*N_global, val1, indx1, pntr1, *val,*bindx, *val,*bindx);
+    Trilinos_Util_csrmsr(*N_global, val1, indx1, pntr1, *val,*bindx, *val,*bindx);
 
+    *b = (double   *) calloc((*N_global)*MAXBLOCKSIZE,sizeof(double)) ;
+    *x = (double   *) calloc((*N_global)*MAXBLOCKSIZE,sizeof(double)) ;
 
-      *b = (double   *) calloc((*N_global)*MAXBLOCKSIZE,sizeof(double)) ;
-      *x = (double   *) calloc((*N_global)*MAXBLOCKSIZE,sizeof(double)) ;
+    if ((*x) == NULL)
+      perror("Error: Not enough space to create matrix");
 
-      if ((*x) == NULL) 
-	perror("Error: Not enough space to create matrix");
-
-  
-      /* Set RHS to a random vector, initial guess to zero */
-      for (i=0;i<*N_global;i++)
-	{
-	  (*b)[i] = ((double) rand())/ ((double) RAND_MAX);
-	  (*x)[i] = 0.0;
-	}
+    /* Set RHS to a random vector, initial guess to zero */
+    for (i=0;i<*N_global;i++)
+    {
+      (*b)[i] = ((double) rand())/ ((double) RAND_MAX);
+      (*x)[i] = 0.0;
+    }
   }
-   
-      /* Release unneeded space */
 
+  /* Release unneeded space */
   free((void *) pntr);
   free((void *) val1);
   free((void *) indx1);
   free((void *) pntr1);
-  
-  
+
   /* end read_coo */
 }
