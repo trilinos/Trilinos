fdce4730b51a0790216b2a8ffdf9e9cd86ba8256
Mark Hoemmen <mhoemme@sandia.gov>
2014-10-02 09:17:01 -0600

    Tpetra: Updated Lesson 02 to use new Tpetra default template parameters.
    Commits a few weeks ago added new default template parameters to
    commonly used Tpetra objects.  These make Tpetra much less verbose for
    common cases.  They also can remove explicit dependencies on the (soon
    to be deprecated) KokkosClassic subpackage, for example by replacing
    KokkosClassic::DefaultNode::DefaultNodeType with
    Tpetra::Map<>::node_type.  This commit changes Lesson 02 (of the
    Tpetra tutorial) to rely on these new template parameters.

--- packages/tpetra/example/Lesson02-Map-Vector/lesson02_init_map_vec.cpp
+++ packages/tpetra/example/Lesson02-Map-Vector/lesson02_init_map_vec.cpp
@@ -64,7 +64,7 @@ exampleRoutine (const Teuchos::RCP<const Teuchos::Comm<int> >& comm,
   using Teuchos::rcp;
 
   // Print out the Tpetra software version information.
-  out << Tpetra::version() << endl << endl;
+  out << Tpetra::version () << endl << endl;
 
   //
   // The first thing you might notice that makes Tpetra objects
@@ -72,71 +72,60 @@ exampleRoutine (const Teuchos::RCP<const Teuchos::Comm<int> >& comm,
   // take several template parameters.  These template parameters give
   // Tpetra its features of being able to solve very large problems
   // (of more than 2 billion unknowns) and to exploit intranode
-  // parallelism.
+  // parallelism.  Most Tpetra objects come with default values of
+  // those template parameters.  In many cases, you might not have to
+  // specify _any_ of those values explicitly!  You can also control
+  // some of their default values, like that of the Node type, when
+  // configuring Trilinos.
   //
   // It's common to begin a Tpetra application with some typedefs to
   // make the code more concise and readable.  They also make the code
   // more maintainable, since you can change the typedefs without
-  // changing the rest of the program.
+  // changing the rest of the program.  You may also want to
+  // abbreviate the template parameters.  Standard abbreviations are
+  // "LO" for local_ordinal_type and "GO" for global_ordinal_type.
   //
 
   // The "Scalar" type is the type of the values stored in the Tpetra
   // objects.  Valid Scalar types include real or complex
   // (std::complex<T>) floating-point types, or more exotic objects
-  // with similar behavior.
-  typedef double scalar_type;
+  // with similar behavior.  We use the default type here, which we
+  // get from Vector.  "Vector<>" means that we let all template
+  // parameters' values revert to their defaults.
+  typedef Tpetra::Vector<>::scalar_type scalar_type;
 
   // The "LocalOrdinal" (LO) type is the type of "local" indices.
   // Both Epetra and Tpetra index local entries differently than
   // global entries.  Tpetra exploits this so that you can use a
   // shorter integer type for local indices.  This saves bandwidth
-  // when computing sparse matrix-vector products.
-  typedef int local_ordinal_type;
+  // when computing sparse matrix-vector products.  We use the default
+  // LO type here, which we get from Tpetra::Vector.  We could also
+  // get it from Tpetra::Map.
+  typedef Tpetra::Vector<>::local_ordinal_type local_ordinal_type;
 
   // The "GlobalOrdinal" (GO) type is the type of "global" indices.
-  typedef long global_ordinal_type;
+  // We use the default GO type here, which we get from
+  // Tpetra::Vector.  We could also get it from Tpetra::Map.
+  typedef Tpetra::Vector<>::global_ordinal_type global_ordinal_type;
 
   // The Kokkos "Node" type describes the type of shared-memory
   // parallelism that Tpetra will use _within_ an MPI process.  The
   // available Node types depend on Trilinos' build options and the
-  // availability of certain third-party libraries.  Here are a few
-  // examples:
-  //
-  // Kokkos::SerialNode: No parallelism
-  //
-  // Kokkos::TPINode: Uses a custom Pthreads wrapper
-  //
-  // Kokkos::TBBNode: Uses Intel's Threading Building Blocks
-  //
-  // Kokkos::ThrustNode: Uses Thrust, a C++ CUDA wrapper,
-  //   for GPU parallelism.
-  //
-  // Using a GPU-oriented Node means that Tpetra objects that store a
-  // lot of data (vectors and sparse matrices, for example) will store
-  // that data on the GPU, and operate on it there whenever possible.
-  //
-  // Kokkos::DefaultNode gives you a default Node type.  It may be
-  // different, depending on Trilinos' build options.  Currently, for
-  // example, building Trilinos with Pthreads enabled gives you
-  // Kokkos::TPINode by default.  That means your default Node is a
-  // parallel node!
-  typedef Kokkos::DefaultNode::DefaultNodeType node_type;
+  // availability of certain third-party libraries.  In almost all
+  // cases, the default setting will do.  You may set the default Node
+  // type when configuring Trilinos.  In this case, we access the
+  // default Node type using the typedef in Tpetra::Vector.  Almost
+  // all Tpetra classes have default template parameter values.
+  typedef Tpetra::Vector<>::node_type node_type;
 
   // Maps know how to convert between local and global indices, so of
   // course they are templated on the local and global Ordinal types.
   // They are also templated on the Kokkos Node type, because Tpetra
   // objects that use Tpetra::Map are.  It's important not to mix up
-  // Maps for different Kokkos Node types.
-  typedef Tpetra::Map<local_ordinal_type, global_ordinal_type, node_type> map_type;
-
-  // Get a pointer to the default Kokkos Node.  We'll need this when
-  // creating the Tpetra::Map objects.
-  //
-  // Currently, if you want a node of a different type, you have to
-  // instantiate it explicitly.  We are moving to a model whereby you
-  // template your entire program on the Node type, and the system
-  // gives you the appropriate Node instantiation.
-  RCP<node_type> node = Kokkos::DefaultNode::getDefaultNode ();
+  // Maps for different Kokkos Node types.  In this case, we use all
+  // default template parameters, which are the same as the
+  // corresponding template parameters of Vector.
+  typedef Tpetra::Map<> map_type;
 
   //////////////////////////////////////////////////////////////////////
   // Create some Tpetra Map objects
@@ -168,15 +157,12 @@ exampleRoutine (const Teuchos::RCP<const Teuchos::Comm<int> >& comm,
   const global_ordinal_type indexBase = 0;
 
   // Construct a Map that puts the same number of equations on each
-  // processor.  Pass in the Kokkos Node, so that this line of code
-  // will work with any Kokkos Node type.
-  //
-  // It's typical to create a const Map.  Maps should be considered
-  // immutable objects.  If you want a new data distribution, create a
-  // new Map.
+  // processor.  It's typical to create a const Map.  Maps should be
+  // considered immutable objects.  If you want a new data
+  // distribution, create a new Map.
   RCP<const map_type> contigMap =
     rcp (new map_type (numGlobalEntries, indexBase, comm,
-                       Tpetra::GloballyDistributed, node));
+                       Tpetra::GloballyDistributed));
 
   // contigMap is contiguous by construction.
   TEUCHOS_TEST_FOR_EXCEPTION(! contigMap->isContiguous(), std::logic_error,
@@ -203,8 +189,7 @@ exampleRoutine (const Teuchos::RCP<const Teuchos::Comm<int> >& comm,
     for (Array<global_ordinal_type>::size_type k = 0; k < numEltsPerProc; ++k)
       elementList[k] = myRank + k*numProcs;
 
-    cyclicMap = rcp (new map_type (numGlobalEntries, elementList, indexBase,
-                                   comm, node));
+    cyclicMap = rcp (new map_type (numGlobalEntries, elementList, indexBase, comm));
   }
 
   // If there's more than one MPI process in the communicator,
@@ -228,12 +213,13 @@ exampleRoutine (const Teuchos::RCP<const Teuchos::Comm<int> >& comm,
   // We have maps now, so we can create vectors.
   //////////////////////////////////////////////////////////////////////
 
-  // Since Tpetra::Vector takes four template parameters, its type
-  // is long.  It's helpful to use a typedef.  You may also want to
-  // abbreviate the template parameters.  Standard abbreviations
-  // are "LO" for local_ordinal_type and "GO" for global_ordinal_type.
-  typedef Tpetra::Vector<scalar_type, local_ordinal_type,
-    global_ordinal_type, node_type> vector_type;
+  // Since Tpetra::Vector takes four template parameters, its type is
+  // long.  Here, we use all default values for the template
+  // parameters, which helps with the length.  However, I still prefer
+  // to use a typedef for encapsulation, so that we only have to
+  // change one line of code if we decide to change the template
+  // parameters of Vector.
+  typedef Tpetra::Vector<> vector_type;
 
   // Create a Vector with the contiguous Map.  This version of the
   // constructor will fill in the vector with zeros.
@@ -304,9 +290,9 @@ main (int argc, char *argv[])
   Teuchos::oblackholestream blackHole;
   Teuchos::GlobalMPISession mpiSession (&argc, &argv, &blackHole);
   RCP<const Teuchos::Comm<int> > comm =
-    Tpetra::DefaultPlatform::getDefaultPlatform().getComm();
+    Tpetra::DefaultPlatform::getDefaultPlatform ().getComm ();
 
-  const int myRank = comm->getRank();
+  const int myRank = comm->getRank ();
   std::ostream& out = (myRank == 0) ? std::cout : blackHole;
 
   // We have a communicator and an output stream.
