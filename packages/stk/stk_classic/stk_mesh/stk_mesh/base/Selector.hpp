d173a40800d034fee7060a3d2157baaf24fde869
Nathan Crane <nkcrane@sandia.gov>
2014-10-03 09:47:30 -0600

    Whoops, modified the stk clasic ones, undo
    (From Sierra)

--- packages/stk/stk_classic/stk_mesh/stk_mesh/base/Selector.hpp
+++ packages/stk/stk_classic/stk_mesh/stk_mesh/base/Selector.hpp
@@ -282,7 +282,6 @@ Selector selectUnion( const PartVector& union_part_vector );
  * \relates Selector
  * */
 Selector selectIntersection( const PartVector& intersection_part_vector );
-Selector selectIntersection( const ConstPartVector& intersection_part_vector );
 
 /** \brief Return a selector for the union of the parts where field exists.
  * \relates Selector
