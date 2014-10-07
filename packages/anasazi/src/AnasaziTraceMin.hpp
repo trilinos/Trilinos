bc1ce35c58ea728f6b0fd3aad8d3eab571ed176d
Mark Hoemmen <mhoemme@sandia.gov>
2014-10-07 14:30:33 -0600

    Anasazi::TraceMin: Fixed build warning (variable set but not used)
    Build/Test Cases Summary
    Enabled Packages: KokkosCore, KokkosCompat, KokkosContainers, KokkosAlgorithms, KokkosLinAlg, KokkosMpiComm, Tpetra, Anasazi
    Disabled Packages: FEI,Moertel,MOOCHO,NOX,Rythmos,STK
    Enabled all Forward Packages
    0) MPI_DEBUG => Test case MPI_DEBUG was not run! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_KOKKOS_REFACTOR => passed: configure-only passed => Not ready to push! (3.12 min)
    3) SERIAL_RELEASE_KOKKOS_REFACTOR => passed: configure-only passed => Not ready to push! (3.13 min)
    Other local commits for this build/test group: bf2b385, 32bab6a, 7808784
    WARNING: Forced the push!

--- packages/anasazi/src/AnasaziTraceMin.hpp
+++ packages/anasazi/src/AnasaziTraceMin.hpp
@@ -60,9 +60,9 @@
 namespace Anasazi {
 namespace Experimental {
 /*! \class TraceMin
-  
-    \brief This class implements a TraceMIN iteration, a preconditioned iteration 
-    for solving linear symmetric positive definite eigenproblems.  
+
+    \brief This class implements a TraceMIN iteration, a preconditioned iteration
+    for solving linear symmetric positive definite eigenproblems.
 
     %TraceMin works by solving a constrained minimization problem
     \f[ \textrm{min trace}\left(Y^TKY\right) \textrm{ such that } Y^TMY = I\f]
@@ -76,94 +76,94 @@ namespace Experimental {
 
     A %TraceMin iteration consists of the following operations:
     -# Solve the saddle point problem
-          \f[\left[\begin{array}{lr} K & MX \\ X^TM & 0\end{array}\right] 
+          \f[\left[\begin{array}{lr} K & MX \\ X^TM & 0\end{array}\right]
           \left[\begin{array}{l} V \\ \tilde{L}\end{array}\right] =
           \left[\begin{array}{l} 0 \\ I\end{array}\right]\f]
-    -# Form a section out of the current subspace V such that \f$X^TKX = \Sigma\f$ and 
-       \f$X^TMX = I\f$, where \f$\Sigma\f$ is diagonal.  \f$\left(\Sigma,X\right)\f$ is 
+    -# Form a section out of the current subspace V such that \f$X^TKX = \Sigma\f$ and
+       \f$X^TMX = I\f$, where \f$\Sigma\f$ is diagonal.  \f$\left(\Sigma,X\right)\f$ is
        an approximation of the eigenpairs of smallest magnitude.
     -# Compute the new residual and check for convergence.
 
-    The saddle point problem need not be solved to a low relative residual and can be 
-    solved either by directly forming the Schur complement or by using a projected 
+    The saddle point problem need not be solved to a low relative residual and can be
+    solved either by directly forming the Schur complement or by using a projected
     Krylov subspace method to solve
     \f[ \left(PKP\right) \Delta = PMX \textrm{ with } P=I-BX\left(X^TB^2X\right)^{-1}X^TB\f]
-    Then V is constructed as \f$V=X-\Delta\f$.  If a preconditioner H is used with the 
-    projected Krylov method, it is applied as 
+    Then V is constructed as \f$V=X-\Delta\f$.  If a preconditioner H is used with the
+    projected Krylov method, it is applied as
     \f[F=\left[I-H^{-1}BX\left(X^TBH^{-1}BX\right)^{-1}X^TB\right]H^{-1}\f]
-    and we solve \f$FK\Delta = FMX\f$.  
-    (See A Parallel Implementation of the Trace Minimization Eigensolver by Eloy Romero 
+    and we solve \f$FK\Delta = FMX\f$.
+    (See A Parallel Implementation of the Trace Minimization Eigensolver by Eloy Romero
     and Jose E. Roman.)
 
     The convergence rate of %TraceMin is based on the distribution of eigenvalues.  If
     the eigenvalues are clustered far away from the origin, we have a slow rate of
-    convergence.  We can improve our convergence rate by taking advantage of Ritz 
-    shifts.  Instead of solving \f$Kx=\lambda Mx\f$, we solve 
+    convergence.  We can improve our convergence rate by taking advantage of Ritz
+    shifts.  Instead of solving \f$Kx=\lambda Mx\f$, we solve
     \f$\left(K-\sigma M\right) x = \left(\lambda - \sigma\right)Mx\f$.
-    
-    This method is described in <em>A Trace Minimization Algorithm for 
-    the Generalized Eigenvalue Problem</em>, Ahmed H. Sameh, John A. 
+
+    This method is described in <em>A Trace Minimization Algorithm for
+    the Generalized Eigenvalue Problem</em>, Ahmed H. Sameh, John A.
     Wisniewski, SIAM Journal on Numerical Analysis, 19(6), pp. 1243-1259
     (1982)
-        
+
     \ingroup anasazi_solver_framework
 
     \author Alicia Klinvex
 */
   template <class ScalarType, class MV, class OP>
-  class TraceMin : public TraceMinBase<ScalarType,MV,OP> { 
+  class TraceMin : public TraceMinBase<ScalarType,MV,OP> {
   public:
     //! @name Constructor/Destructor
-    //@{ 
-    
-    /*! \brief %TraceMin constructor with eigenproblem, solver utilities, and 
+    //@{
+
+    /*! \brief %TraceMin constructor with eigenproblem, solver utilities, and
      *  parameter list of solver options.
      *
      * This constructor takes pointers required by the eigensolver, in addition
-     * to a parameter list of options for the eigensolver. These options include 
+     * to a parameter list of options for the eigensolver. These options include
      * the following:
-     *   - \c "Block Size" - an \c int specifying the subspace dimension used 
-     *     by the algorithm. This can also be specified using the setBlockSize() 
+     *   - \c "Block Size" - an \c int specifying the subspace dimension used
+     *     by the algorithm. This can also be specified using the setBlockSize()
      *     method.
-     *   - \c "Maximum Iterations" - an \c int specifying the maximum number of 
+     *   - \c "Maximum Iterations" - an \c int specifying the maximum number of
      *     %TraceMin iterations.
-     *   - \c "Saddle Solver Type" - a \c string specifying the algorithm to use 
-     *     in solving the saddle point problem: "Schur Complement" or "Projected 
+     *   - \c "Saddle Solver Type" - a \c string specifying the algorithm to use
+     *     in solving the saddle point problem: "Schur Complement" or "Projected
      *     Krylov". Default: "Projected Krylov"
-     *      - \c "Schur Complement": We explicitly form the Schur complement and 
-     *        use it to solve the linear system.  This option may be faster, but 
-     *        it is less numerically stable and does not ensure orthogonality 
+     *      - \c "Schur Complement": We explicitly form the Schur complement and
+     *        use it to solve the linear system.  This option may be faster, but
+     *        it is less numerically stable and does not ensure orthogonality
      *        between the current iterate and the update.
      *      - \c "Projected Krylov": Use a projected iterative method to solve
-     *        the linear system. If %TraceMin was not given a preconditioner, it 
+     *        the linear system. If %TraceMin was not given a preconditioner, it
      *        will use MINRES.  Otherwise, it will use GMRES.
-     *   - \c "Shift Type" - a \c string specifying how to choose Ritz shifts: 
-     *        "No Shift", "Locked Shift", "Trace Leveled Shift", or "Original Shift". 
+     *   - \c "Shift Type" - a \c string specifying how to choose Ritz shifts:
+     *        "No Shift", "Locked Shift", "Trace Leveled Shift", or "Original Shift".
      *        Default: "Trace Leveled Shift"
-     *      - \c "No Shift": Do not perform Ritz shifts.  This option produces 
+     *      - \c "No Shift": Do not perform Ritz shifts.  This option produces
      *        guaranteed convergence but converges linearly.  Not recommended.
-     *      - \c "Locked Shift": Do not perform Ritz shifts until an eigenpair is 
-     *        locked.  Then, shift based on the largest converged eigenvalue.  
+     *      - \c "Locked Shift": Do not perform Ritz shifts until an eigenpair is
+     *        locked.  Then, shift based on the largest converged eigenvalue.
      *        This option is roughly as safe as "No Shift" but may be faster.
-     *      - \c "Trace Leveled Shift": Do not perform Ritz shifts until the trace 
-     *        of \f$X^TKX\f$ (i.e. the quantity being minimized has stagnated.  
-     *        Then, shift based on the strategy proposed in <em>The trace 
-     *        minimization method for the symmetric generalized eigenvalue problem.</em>  
+     *      - \c "Trace Leveled Shift": Do not perform Ritz shifts until the trace
+     *        of \f$X^TKX\f$ (i.e. the quantity being minimized has stagnated.
+     *        Then, shift based on the strategy proposed in <em>The trace
+     *        minimization method for the symmetric generalized eigenvalue problem.</em>
      *        Highly recommended.
-     *      - \c "Original Shift": The original shifting strategy proposed in 
-     *        "The trace minimization method for the symmetric generalized 
-     *        eigenvalue problem."  Compute shifts based on the Ritz values, 
-     *        residuals, and clustering of the eigenvalues.  May produce incorrect 
+     *      - \c "Original Shift": The original shifting strategy proposed in
+     *        "The trace minimization method for the symmetric generalized
+     *        eigenvalue problem."  Compute shifts based on the Ritz values,
+     *        residuals, and clustering of the eigenvalues.  May produce incorrect
      *        results for indefinite matrices or small subspace dimensions.
      */
-    TraceMin( const Teuchos::RCP<Eigenproblem<ScalarType,MV,OP> >    &problem, 
+    TraceMin( const Teuchos::RCP<Eigenproblem<ScalarType,MV,OP> >    &problem,
                    const Teuchos::RCP<SortManager<typename Teuchos::ScalarTraits<ScalarType>::magnitudeType> > &sorter,
                    const Teuchos::RCP<OutputManager<ScalarType> >         &printer,
                    const Teuchos::RCP<StatusTest<ScalarType,MV,OP> >      &tester,
                    const Teuchos::RCP<MatOrthoManager<ScalarType,MV,OP> > &ortho,
-                   Teuchos::ParameterList &params 
+                   Teuchos::ParameterList &params
                  );
- 
+
   private:
     //
     // Convenience typedefs
@@ -173,8 +173,8 @@ namespace Experimental {
     typedef OperatorTraits<ScalarType,MV,OP> OPT;
     typedef Teuchos::ScalarTraits<ScalarType> SCT;
     typedef typename SCT::magnitudeType MagnitudeType;
-    const MagnitudeType ONE;  
-    const MagnitudeType ZERO; 
+    const MagnitudeType ONE;
+    const MagnitudeType ZERO;
     const MagnitudeType NANVAL;
 
     // TraceMin specific methods
@@ -194,7 +194,7 @@ namespace Experimental {
   // Constructor
   template <class ScalarType, class MV, class OP>
   TraceMin<ScalarType,MV,OP>::TraceMin(
-        const Teuchos::RCP<Eigenproblem<ScalarType,MV,OP> >    &problem, 
+        const Teuchos::RCP<Eigenproblem<ScalarType,MV,OP> >    &problem,
         const Teuchos::RCP<SortManager<typename Teuchos::ScalarTraits<ScalarType>::magnitudeType> > &sorter,
         const Teuchos::RCP<OutputManager<ScalarType> >         &printer,
         const Teuchos::RCP<StatusTest<ScalarType,MV,OP> >      &tester,
@@ -205,7 +205,7 @@ namespace Experimental {
     ONE(Teuchos::ScalarTraits<MagnitudeType>::one()),
     ZERO(Teuchos::ScalarTraits<MagnitudeType>::zero()),
     NANVAL(Teuchos::ScalarTraits<MagnitudeType>::nan())
-  {     
+  {
   }
 
 
@@ -242,6 +242,11 @@ namespace Experimental {
       }
     }
 
+    // FIXME (mfh 07 Oct 2014) This variable is currently unused, but
+    // it would make sense to use it to check whether the block is
+    // rank deficient.
+    (void) rank;
+
     if(this->Op_ != Teuchos::null)
     {
 #ifdef ANASAZI_TEUCHOS_TIME_MONITOR
@@ -251,7 +256,7 @@ namespace Experimental {
       OPT::Apply(*this->Op_,*this->V_,*this->KV_);
     }
   }
-  
+
 }} // End of namespace Anasazi
 
 #endif
