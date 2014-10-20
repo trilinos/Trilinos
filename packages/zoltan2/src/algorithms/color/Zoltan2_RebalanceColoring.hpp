160f1cc6eca5f7d712194f1e8488060ec007f742
Erik Boman <egboman@sandia.gov>
2014-10-20 12:08:24 -0600

    Zoltan2: minor, in progress

--- packages/zoltan2/src/algorithms/color/Zoltan2_RebalanceColoring.hpp
+++ packages/zoltan2/src/algorithms/color/Zoltan2_RebalanceColoring.hpp
@@ -49,7 +49,7 @@
 // Sketch: Does not compile nor work yet!
 
 // Given a valid coloring of a graph, rebalance the colors such that
-// every color class has at least min_size vertices.
+// every color class has at least minSize vertices.
 // This function can be called as a post-processing after any initial
 // coloring.
    void rebalanceColoring(
@@ -57,15 +57,26 @@
      ArrayView<const lno_t> edgeIds,
      ArrayView<const lno_t> offsets,
      ArrayRCP<int> colors,
-     lno_t thres
+     const lno_t minSize
      )
    {
 #ifndef INCLUDE_ZOLTAN2_EXPERIMENTAL
     Z2_THROW_EXPERIMENTAL("Zoltan2 rebalanceColoring is experimental and not tested");
 #else
    // Count size of each color class.
-   // For each color i with < min_size vertices, pair it with
-   // a color j with > min_size vertices (e.g., the largest color class). 
+   // No vertex weights, may add in future.
+   lno_t maxColor = 0;
+   Teuchos::Array<lno_t> colorSize(nVtx,0);
+   for (lno_t i=0; i < nVtx; i++){
+     if (colors[i] > maxColor) 
+       maxColor = colors[i];
+   }
+   for (lno_t i=0; i < nVtx; i++){
+     colorSize[colors[i]]++;
+   }
+
+   // For each color i with < minSize vertices, pair it with
+   // a color j with > minSize vertices (e.g., the largest color class). 
    // Recolor vertices with color j to i.
 
 #endif
