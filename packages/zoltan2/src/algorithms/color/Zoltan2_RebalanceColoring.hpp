0fc54a624666d302cadf8569e59466d83a273622
Erik Boman <egboman@sandia.gov>
2014-10-20 15:01:42 -0600

    Zoltan2: Flesh out rebalancing to avoid small color classes.

--- packages/zoltan2/src/algorithms/color/Zoltan2_RebalanceColoring.hpp
+++ packages/zoltan2/src/algorithms/color/Zoltan2_RebalanceColoring.hpp
@@ -46,13 +46,12 @@
 #define _ZOLTAN2_REBALANCECOLORING_HPP_
 
 #include <Zoltan2_Standards.hpp>
-// Sketch: Does not compile nor work yet!
 
 // Given a valid coloring of a graph, rebalance the colors such that
 // every color class has at least minSize vertices.
 // This function can be called as a post-processing after any initial
 // coloring.
-   void rebalanceColoring(
+   int rebalanceColoring(
      const lno_t nVtx,
      ArrayView<const lno_t> edgeIds,
      ArrayView<const lno_t> offsets,
@@ -63,6 +62,8 @@
 #ifndef INCLUDE_ZOLTAN2_EXPERIMENTAL
     Z2_THROW_EXPERIMENTAL("Zoltan2 rebalanceColoring is experimental and not tested");
 #else
+   // Experimental: Not tested yet!
+   //
    // Count size of each color class.
    // No vertex weights, may add in future.
    lno_t maxColor = 0;
@@ -78,6 +79,38 @@
    // For each color i with < minSize vertices, pair it with
    // a color j with > minSize vertices (e.g., the largest color class). 
    // Recolor vertices with color j to i.
+   // Simple algorithm for now; optimize later.
+   bool done = false;
+   while (!done){
+     // Find smallest and biggest color class.
+     // Ignore color class 0 since it is special.
+     int minColorClass = -1;
+     int maxColorClass = -1;
+     int small = nVtx;
+     int big = 0;
+     for (int i=1; i <= maxColor; i++){
+       if (colorSize[i] < small) {
+         minColorClass = i;
+         small = colorSize[i];
+       }
+       if (colorSize[i] > big) {
+         maxColorClass = i;
+         big = colorSize[i];
+       }
+     }
+     // Move vertices from maxColorClass to minColorClass.
+     toMove = minSize - small;
+     if (toMove > 0){
+       for (lno_t i=0; i < nVtx; i++){
+         if ((colors[i] == maxColorClass) && (toMove > 0)){
+           colors[i] = minColorClass;
+           toMove--;
+         }
+       }
+     }
+     else
+       done = true; // All color classes are at least minSize.
+   }
 
 #endif
    }
