cdab971195371d7e2d97ef2a352dc32434d28841
Mark Hoemmen <mhoemme@sandia.gov>
2014-10-20 17:30:19 -0600

    Anasazi: Fixed Tpetra tests that were broken by recent commits.
    If it's not broken, don't fix it!  These tests only build if complex
    arithmetic is enabled: that is, Tpetra_INST_COMPLEX_DOUBLE must be ON.
    That requires Teuchos_ENABLE_COMPLEX to be ON as well.  Neither of
    these are ON by default, which would explain why you were able to
    push.
    
    I don't mind the occasional mistake, but I see no value in catching
    uncaught exceptions in a short little test.  It won't necessarily even
    prevent the test from hanging.  Remember that CTest already has a
    timeout for tests; it kills them automatically if they take too long.

--- packages/anasazi/tpetra/test/IRTR/cxx_main_complex.cpp
+++ packages/anasazi/tpetra/test/IRTR/cxx_main_complex.cpp
@@ -74,203 +74,200 @@ int main(int argc, char *argv[])
   GlobalMPISession mpisess(&argc,&argv,&std::cout);
 
   bool success = false;
-  try {
-    ST ONE  = SCT::one();
+  ST ONE  = SCT::one();
 
-    int info = 0;
-    int MyPID = 0;
+  int info = 0;
+  int MyPID = 0;
 
-    RCP<const Teuchos::Comm<int> > comm =
-      Tpetra::DefaultPlatform::getDefaultPlatform ().getComm ();
+  RCP<const Teuchos::Comm<int> > comm =
+    Tpetra::DefaultPlatform::getDefaultPlatform ().getComm ();
 
-    MyPID = rank(*comm);
+  MyPID = rank(*comm);
 
-    bool verbose = false;
-    bool debug = false;
-    bool skinny = true;
-    std::string filename("mhd1280b.cua");
-    std::string which("LR");
-    int nev = 4;
-    int blockSize = 4;
-    MT tol = 1.0e-6;
+  bool verbose = false;
+  bool debug = false;
+  bool skinny = true;
+  std::string filename("mhd1280b.cua");
+  std::string which("LR");
+  int nev = 4;
+  int blockSize = 4;
+  MT tol = 1.0e-6;
 
-    CommandLineProcessor cmdp(false,true);
-    cmdp.setOption("verbose","quiet",&verbose,"Print messages and results.");
-    cmdp.setOption("skinny","hefty",&skinny,"Use a skinny (low-mem) or hefty (higher-mem) implementation of IRTR.");
-    cmdp.setOption("debug","nodebug",&debug,"Print debugging information.");
-    cmdp.setOption("filename",&filename,"Filename for Harwell-Boeing test matrix.");
-    cmdp.setOption("sort",&which,"Targetted eigenvalues (SR or LR).");
-    cmdp.setOption("nev",&nev,"Number of eigenvalues to compute.");
-    cmdp.setOption("blockSize",&blockSize,"Block size for the algorithm.");
-    cmdp.setOption("tol",&tol,"Tolerance for convergence.");
-    if (cmdp.parse(argc,argv) != CommandLineProcessor::PARSE_SUCCESSFUL) {
-      return -1;
+  CommandLineProcessor cmdp(false,true);
+  cmdp.setOption("verbose","quiet",&verbose,"Print messages and results.");
+  cmdp.setOption("skinny","hefty",&skinny,"Use a skinny (low-mem) or hefty (higher-mem) implementation of IRTR.");
+  cmdp.setOption("debug","nodebug",&debug,"Print debugging information.");
+  cmdp.setOption("filename",&filename,"Filename for Harwell-Boeing test matrix.");
+  cmdp.setOption("sort",&which,"Targetted eigenvalues (SR or LR).");
+  cmdp.setOption("nev",&nev,"Number of eigenvalues to compute.");
+  cmdp.setOption("blockSize",&blockSize,"Block size for the algorithm.");
+  cmdp.setOption("tol",&tol,"Tolerance for convergence.");
+  if (cmdp.parse(argc,argv) != CommandLineProcessor::PARSE_SUCCESSFUL) {
+    return -1;
+  }
+  if (debug) verbose = true;
+  if (blockSize < nev) {
+    blockSize = nev;
+  }
+
+  if (MyPID == 0) {
+    cout << Anasazi::Anasazi_Version() << endl << endl;
+  }
+
+  // Get the data from the HB file
+  int dim,dim2,nnz;
+  int rnnzmax;
+  double *dvals;
+  int *colptr,*rowind;
+  nnz = -1;
+  if (MyPID == 0) {
+    info = readHB_newmat_double(filename.c_str(),&dim,&dim2,&nnz,&colptr,&rowind,&dvals);
+    // find maximum NNZ over all rows
+    vector<int> rnnz(dim,0);
+    for (int *ri=rowind; ri<rowind+nnz; ++ri) {
+      ++rnnz[*ri-1];
     }
-    if (debug) verbose = true;
-    if (blockSize < nev) {
-      blockSize = nev;
-    }
-
+    rnnzmax = *std::max_element(rnnz.begin(),rnnz.end());
+  }
+  else {
+    // address uninitialized data warnings
+    dvals = NULL;
+    colptr = NULL;
+    rowind = NULL;
+  }
+  Teuchos::broadcast(*comm,0,&info);
+  Teuchos::broadcast(*comm,0,&nnz);
+  Teuchos::broadcast(*comm,0,&dim);
+  Teuchos::broadcast(*comm,0,&rnnzmax);
+  if (info == 0 || nnz < 0) {
     if (MyPID == 0) {
-      cout << Anasazi::Anasazi_Version() << endl << endl;
+      cout << "Error reading '" << filename << "'" << endl
+           << "End Result: TEST FAILED" << endl;
     }
-
-    // Get the data from the HB file
-    int dim,dim2,nnz;
-    int rnnzmax;
-    double *dvals;
-    int *colptr,*rowind;
-    nnz = -1;
-    if (MyPID == 0) {
-      info = readHB_newmat_double(filename.c_str(),&dim,&dim2,&nnz,&colptr,&rowind,&dvals);
-      // find maximum NNZ over all rows
-      vector<int> rnnz(dim,0);
-      for (int *ri=rowind; ri<rowind+nnz; ++ri) {
-        ++rnnz[*ri-1];
+    return -1;
+  }
+  // create map
+  RCP<const Map<int> > map = rcp (new Map<int> (dim, 0, comm));
+  RCP<CrsMatrix<ST,int> > K = rcp(new CrsMatrix<ST,int>(map,rnnzmax));
+  if (MyPID == 0) {
+    // Convert interleaved doubles to complex values
+    // HB format is compressed column. CrsMatrix is compressed row.
+    const double *dptr = dvals;
+    const int *rptr = rowind;
+    for (int c=0; c<dim; ++c) {
+      for (int colnnz=0; colnnz < colptr[c+1]-colptr[c]; ++colnnz) {
+        K->insertGlobalValues(*rptr++ - 1,tuple(c),tuple(ST(dptr[0],dptr[1])));
+        dptr += 2;
       }
-      rnnzmax = *std::max_element(rnnz.begin(),rnnz.end());
-    }
-    else {
-      // address uninitialized data warnings
-      dvals = NULL;
-      colptr = NULL;
-      rowind = NULL;
-    }
-    Teuchos::broadcast(*comm,0,&info);
-    Teuchos::broadcast(*comm,0,&nnz);
-    Teuchos::broadcast(*comm,0,&dim);
-    Teuchos::broadcast(*comm,0,&rnnzmax);
-    if (info == 0 || nnz < 0) {
-      if (MyPID == 0) {
-        cout << "Error reading '" << filename << "'" << endl
-          << "End Result: TEST FAILED" << endl;
-      }
-      return -1;
-    }
-    // create map
-    RCP<const Map<int> > map = rcp (new Map<int> (dim, 0, comm));
-    RCP<CrsMatrix<ST,int> > K = rcp(new CrsMatrix<ST,int>(map,rnnzmax));
-    if (MyPID == 0) {
-      // Convert interleaved doubles to complex values
-      // HB format is compressed column. CrsMatrix is compressed row.
-      const double *dptr = dvals;
-      const int *rptr = rowind;
-      for (int c=0; c<dim; ++c) {
-        for (int colnnz=0; colnnz < colptr[c+1]-colptr[c]; ++colnnz) {
-          K->insertGlobalValues(*rptr++ - 1,tuple(c),tuple(ST(dptr[0],dptr[1])));
-          dptr += 2;
-        }
-      }
-    }
-    if (MyPID == 0) {
-      // Clean up.
-      free( dvals );
-      free( colptr );
-      free( rowind );
-    }
-    K->fillComplete();
-
-    // Create initial vectors
-    RCP<MV> ivec = rcp( new MV(map,blockSize) );
-    ivec->randomize ();
-
-    // Create eigenproblem
-    RCP<Anasazi::BasicEigenproblem<ST,MV,OP> > problem =
-      rcp( new Anasazi::BasicEigenproblem<ST,MV,OP>(K,ivec) );
-    //
-    // Inform the eigenproblem that the operator K is symmetric
-    problem->setHermitian(true);
-    //
-    // Set the number of eigenvalues requested
-    problem->setNEV( nev );
-    //
-    // Inform the eigenproblem that you are done passing it information
-    bool boolret = problem->setProblem();
-    if (boolret != true) {
-      if (MyPID == 0) {
-        cout << "Anasazi::BasicEigenproblem::SetProblem() returned with error." << endl
-          << "End Result: TEST FAILED" << endl;
-      }
-      return -1;
-    }
-
-
-    // Set verbosity level
-    int verbosity = Anasazi::Errors + Anasazi::Warnings + Anasazi::FinalSummary + Anasazi::TimingDetails;
-    if (verbose) {
-      verbosity += Anasazi::IterationDetails;
-    }
-    if (debug) {
-      verbosity += Anasazi::Debug;
-    }
-
-
-
-    // Eigensolver parameters
-    int maxIters = 450;
-    //
-    // Create parameter list to pass into the solver manager
-    ParameterList MyPL;
-    MyPL.set( "Skinny Solver", skinny);
-    MyPL.set( "Verbosity", verbosity );
-    MyPL.set( "Which", which );
-    MyPL.set( "Block Size", blockSize );
-    MyPL.set( "Maximum Iterations", maxIters );
-    MyPL.set( "Convergence Tolerance", tol );
-    //
-    // Create the solver manager
-    Anasazi::RTRSolMgr<ST,MV,OP> MySolverMgr(problem, MyPL);
-
-    // Solve the problem to the specified tolerances or length
-    Anasazi::ReturnType returnCode = MySolverMgr.solve();
-    success = (returnCode == Anasazi::Converged);
-
-    // Get the eigenvalues and eigenvectors from the eigenproblem
-    Anasazi::Eigensolution<ST,MV> sol = problem->getSolution();
-    RCP<MV> evecs = sol.Evecs;
-    int numev = sol.numVecs;
-
-    if (numev > 0) {
-      std::ostringstream os;
-      os.setf(std::ios::scientific, std::ios::floatfield);
-      os.precision(6);
-
-      // Compute the direct residual
-      std::vector<MT> normV( numev );
-      SerialDenseMatrix<int,ST> T(numev,numev);
-      for (int i=0; i<numev; i++) {
-        T(i,i) = sol.Evals[i].realpart;
-      }
-      RCP<MV> Kvecs = MVT::Clone( *evecs, numev );
-
-      OPT::Apply( *K, *evecs, *Kvecs );
-
-      MVT::MvTimesMatAddMv( -ONE, *evecs, T, ONE, *Kvecs );
-      MVT::MvNorm( *Kvecs, normV );
-
-      os << "Direct residual norms computed in Tpetra_IRTR_complex_test.exe" << endl
-        << std::setw(20) << "Eigenvalue" << std::setw(20) << "Residual  " << endl
-        << "----------------------------------------" << endl;
-      for (int i=0; i<numev; i++) {
-        if ( SCT::magnitude(sol.Evals[i].realpart) != SCT::zero() ) {
-          normV[i] = SCT::magnitude(normV[i]/sol.Evals[i].realpart);
-        }
-        os << std::setw(20) << sol.Evals[i].realpart << std::setw(20) << normV[i] << endl;
-        success = ( normV[i] < tol );
-      }
-      if (MyPID==0) {
-        cout << endl << os.str() << endl;
-      }
-    }
-
-    if (MyPID==0) {
-      if (success)
-        cout << "End Result: TEST PASSED" << endl;
-      else
-        cout << "End Result: TEST FAILED" << endl;
     }
   }
-  TEUCHOS_STANDARD_CATCH_STATEMENTS(verbose, std::cerr, success);
+  if (MyPID == 0) {
+    // Clean up.
+    free( dvals );
+    free( colptr );
+    free( rowind );
+  }
+  K->fillComplete();
+
+  // Create initial vectors
+  RCP<MV> ivec = rcp( new MV(map,blockSize) );
+  ivec->randomize ();
+
+  // Create eigenproblem
+  RCP<Anasazi::BasicEigenproblem<ST,MV,OP> > problem =
+    rcp( new Anasazi::BasicEigenproblem<ST,MV,OP>(K,ivec) );
+  //
+  // Inform the eigenproblem that the operator K is symmetric
+  problem->setHermitian(true);
+  //
+  // Set the number of eigenvalues requested
+  problem->setNEV( nev );
+  //
+  // Inform the eigenproblem that you are done passing it information
+  bool boolret = problem->setProblem();
+  if (boolret != true) {
+    if (MyPID == 0) {
+      cout << "Anasazi::BasicEigenproblem::SetProblem() returned with error." << endl
+           << "End Result: TEST FAILED" << endl;
+    }
+    return -1;
+  }
+
+
+  // Set verbosity level
+  int verbosity = Anasazi::Errors + Anasazi::Warnings + Anasazi::FinalSummary + Anasazi::TimingDetails;
+  if (verbose) {
+    verbosity += Anasazi::IterationDetails;
+  }
+  if (debug) {
+    verbosity += Anasazi::Debug;
+  }
+
+
+
+  // Eigensolver parameters
+  int maxIters = 450;
+  //
+  // Create parameter list to pass into the solver manager
+  ParameterList MyPL;
+  MyPL.set( "Skinny Solver", skinny);
+  MyPL.set( "Verbosity", verbosity );
+  MyPL.set( "Which", which );
+  MyPL.set( "Block Size", blockSize );
+  MyPL.set( "Maximum Iterations", maxIters );
+  MyPL.set( "Convergence Tolerance", tol );
+  //
+  // Create the solver manager
+  Anasazi::RTRSolMgr<ST,MV,OP> MySolverMgr(problem, MyPL);
+
+  // Solve the problem to the specified tolerances or length
+  Anasazi::ReturnType returnCode = MySolverMgr.solve();
+  success = (returnCode == Anasazi::Converged);
+
+  // Get the eigenvalues and eigenvectors from the eigenproblem
+  Anasazi::Eigensolution<ST,MV> sol = problem->getSolution();
+  RCP<MV> evecs = sol.Evecs;
+  int numev = sol.numVecs;
+
+  if (numev > 0) {
+    std::ostringstream os;
+    os.setf(std::ios::scientific, std::ios::floatfield);
+    os.precision(6);
+
+    // Compute the direct residual
+    std::vector<MT> normV( numev );
+    SerialDenseMatrix<int,ST> T(numev,numev);
+    for (int i=0; i<numev; i++) {
+      T(i,i) = sol.Evals[i].realpart;
+    }
+    RCP<MV> Kvecs = MVT::Clone( *evecs, numev );
+
+    OPT::Apply( *K, *evecs, *Kvecs );
+
+    MVT::MvTimesMatAddMv( -ONE, *evecs, T, ONE, *Kvecs );
+    MVT::MvNorm( *Kvecs, normV );
+
+    os << "Direct residual norms computed in Tpetra_IRTR_complex_test.exe" << endl
+       << std::setw(20) << "Eigenvalue" << std::setw(20) << "Residual  " << endl
+       << "----------------------------------------" << endl;
+    for (int i=0; i<numev; i++) {
+      if ( SCT::magnitude(sol.Evals[i].realpart) != SCT::zero() ) {
+        normV[i] = SCT::magnitude(normV[i]/sol.Evals[i].realpart);
+      }
+      os << std::setw(20) << sol.Evals[i].realpart << std::setw(20) << normV[i] << endl;
+      success = ( normV[i] < tol );
+    }
+    if (MyPID==0) {
+      cout << endl << os.str() << endl;
+    }
+  }
+
+  if (MyPID==0) {
+    if (success)
+      cout << "End Result: TEST PASSED" << endl;
+    else
+      cout << "End Result: TEST FAILED" << endl;
+  }
 
   return ( success ? EXIT_SUCCESS : EXIT_FAILURE );
 }
