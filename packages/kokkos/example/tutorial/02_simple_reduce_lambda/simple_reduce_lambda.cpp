c8c15897527678c15cbd37d3b6085abf8618cbba
Mark Hoemmen <mhoemme@sandia.gov>
2014-10-23 18:12:23 -0600

    KokkosExample: Improved Lesson 02 (simple reduce, functor & lambda).

--- packages/kokkos/example/tutorial/02_simple_reduce_lambda/simple_reduce_lambda.cpp
+++ packages/kokkos/example/tutorial/02_simple_reduce_lambda/simple_reduce_lambda.cpp
@@ -45,16 +45,36 @@
 
 #include <Kokkos_Core.hpp>
 #include <cstdio>
- 
-int main() {
-  Kokkos::initialize();
- 
-  int sum = 0;
-  Kokkos::parallel_reduce(10,[=] (int i, int &lsum) {
-   lsum+=i*i;
-  },sum);
-  printf("Sum of first %i square numbers is %i\n",9,sum);  
 
-  Kokkos::finalize();
+int main() {
+  Kokkos::initialize ();
+  const int n = 10;
+
+  // Compute the sum of squares of integers from 0 to n-1, in
+  // parallel, using Kokkos.  This time, use a lambda instead of a
+  // functor.  The lambda takes the same arguments as the functor's
+  // operator().
+  int sum = 0;
+  Kokkos::parallel_reduce (10, [=] (const int i, int& lsum) {
+    lsum += i*i;
+  }, sum);
+  printf ("Sum of squares of integers from 0 to %i, "
+          "computed in parallel, is %i\n", n - 1, sum);
+
+  // Compare to a sequential loop.
+  int seqSum = 0;
+  for (int i = 0; i < n; ++i) {
+    seqSum += i*i;
+  }
+  printf ("Sum of squares of integers from 0 to %i, "
+          "computed sequentially, is %i\n", n - 1, seqSum);
+
+  Kokkos::finalize ();
+
+  if (sum == seqSum) {
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
