de5bc65518b16a54f30df9920bb747f25071a3d9
Greg Sjaardema <gdsjaar@sandia.gov>
2014-11-19 07:50:15 -0700

    STK_UTIL: Support for building on 32-bit systems (size_t is 32 bit, not 64-bit) [Nico Schlomer]
    (From Sierra)

--- packages/stk/stk_util/stk_util/parallel/ParallelComm.cpp
+++ packages/stk/stk_util/stk_util/parallel/ParallelComm.cpp
@@ -1,23 +1,23 @@
 // Copyright (c) 2013, Sandia Corporation.
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-// 
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-// 
+//
 //     * Redistributions of source code must retain the above copyright
 //       notice, this list of conditions and the following disclaimer.
-// 
+//
 //     * Redistributions in binary form must reproduce the above
 //       copyright notice, this list of conditions and the following
 //       disclaimer in the documentation and/or other materials provided
 //       with the distribution.
-// 
+//
 //     * Neither the name of Sandia Corporation nor the names of its
 //       contributors may be used to endorse or promote products derived
 //       from this software without specific prior written permission.
-// 
+//
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -29,7 +29,7 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// 
+//
 
 #include <stdlib.h>
 #include <stdexcept>
@@ -37,7 +37,6 @@
 #include <iostream>
 #include <vector>
 
-#include <boost/static_assert.hpp> 
 #include <stk_util/parallel/ParallelComm.hpp>
 #include <stk_util/parallel/ParallelReduce.hpp>
 
@@ -1041,8 +1040,15 @@ bool comm_sizes( ParallelMachine comm ,
     send_buf[p_size]   = max_msg ;
     send_buf[p_size+1] = local_flag ;
 
-    BOOST_STATIC_ASSERT(sizeof(long long) == sizeof(size_t));
+#if __SIZEOF_INT__==__SIZEOF_SIZE_T__
+    result = MPI_Allreduce(p_send,p_recv,p_size,MPI_INT,MPI_SUM,comm);
+#elif __SIZEOF_LONG__==__SIZEOF_SIZE_T__
+    result = MPI_Allreduce(p_send,p_recv,p_size,MPI_LONG,MPI_SUM,comm);
+#elif __SIZEOF_LONG_LONG__==__SIZEOF_SIZE_T__
     result = MPI_Allreduce(p_send,p_recv,p_size,MPI_LONG_LONG,MPI_SUM,comm);
+#else
+#error No type matching __SIZEOF_SIZE_T__
+#endif
 
     if ( result != MPI_SUCCESS ) {
       // PARALLEL ERROR
