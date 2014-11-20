cd654f0796b141a9312f8a8ba570db4e7a520cd2
Mauro Perego <mperego@sandia.gov>
2014-11-20 11:56:37 -0700

    ROL::StdVector, remove const_casts non needed, added getter getNonConstVector().

--- packages/rol/src/vector/ROL_StdVector.hpp
+++ packages/rol/src/vector/ROL_StdVector.hpp
@@ -65,11 +65,10 @@ public:
   StdVector(const Teuchos::RCP<std::vector<Element> > & std_vec) : std_vec_(std_vec) {}
 
   void plus( const Vector<Real> &x ) {
-    StdVector &ex = Teuchos::dyn_cast<StdVector>(const_cast <Vector<Real> &>(x));
-    Teuchos::RCP<const std::vector<Element> > xvalptr = ex.getVector();
+    const StdVector &ex = Teuchos::dyn_cast<const StdVector>(x);
     int dimension  = (int)(this->std_vec_->size());
     for (int i=0; i<dimension; i++) {
-      (*this->std_vec_)[i] += (*xvalptr)[i];
+      (*this->std_vec_)[i] += (*ex.getVector())[i];
     }
   }
 
@@ -81,13 +80,12 @@ public:
   }
 
   Real dot( const Vector<Real> &x ) const {
-    StdVector & ex = Teuchos::dyn_cast<StdVector>(const_cast <Vector<Real> &>(x));
-    Teuchos::RCP<const std::vector<Element> > xvalptr = ex.getVector();
+    const StdVector & ex = Teuchos::dyn_cast<const StdVector>(x);
     int dimension  = (int)(this->std_vec_->size());
     Real val = 0;
 
     for (int i=0; i<dimension; i++) {
-      val += (*this->std_vec_)[i]*(*xvalptr)[i];
+      val += (*this->std_vec_)[i]*(*ex.getVector())[i];
     }
     return val;
   }
@@ -106,9 +104,13 @@ public:
     return this->std_vec_;
   }
 
+  Teuchos::RCP<std::vector<Element> > getNonConstVector() {
+    return this->std_vec_;
+  }
+
   Teuchos::RCP<Vector<Real> > basis( const int i ) const {
     Teuchos::RCP<StdVector> e = Teuchos::rcp( new StdVector( Teuchos::rcp(new std::vector<Element>(this->std_vec_->size(), 0.0)) ));
-    (const_cast <std::vector<Element> &> (*e->getVector()))[i]= 1.0;
+    (*e->getNonConstVector())[i]= 1.0;
     return e;
   }
 
@@ -120,17 +122,12 @@ namespace StdVector_Helper {
 
 template<class Real>
 Teuchos::RCP<const std::vector<Real> > constDownCast( const Vector<Real> &x ) {
-  return (Teuchos::dyn_cast<StdVector<Real> >(const_cast<Vector<Real> &>(x))).getVector();
+  return (Teuchos::dyn_cast<const StdVector<Real> >(x)).getVector();
 }
 
 template<class Real>
 Teuchos::RCP<std::vector<Real> > downCast( Vector<Real> &x ) {
-  return Teuchos::rcp_const_cast<std::vector<Real> >((Teuchos::dyn_cast<StdVector<Real> >(x)).getVector());
-}
-
-template<class Real>
-Teuchos::RCP<std::vector<Real> > downCast( const Vector<Real> &x ) {
-  return Teuchos::rcp_const_cast<std::vector<Real> >((Teuchos::dyn_cast<StdVector<Real> >(const_cast<Vector<Real> &>(x))).getVector());
+  return (Teuchos::dyn_cast<StdVector<Real> >(x)).getNonConstVector();
 }
 
 } // namespace StdVector_Helper
--- packages/rol/src/zoo/ROL_PoissonInversion.hpp
+++ packages/rol/src/zoo/ROL_PoissonInversion.hpp
@@ -496,13 +496,13 @@ namespace ZOO {
 
       // Cast hv and v vectors to std::vector.
       Teuchos::RCP<std::vector<Real> > hvp = ROL::StdVector_Helper::downCast(hv);
-      Teuchos::RCP<std::vector<Real> >  vp = ROL::StdVector_Helper::downCast(v);
+      std::vector<Real> vp(*ROL::StdVector_Helper::constDownCast(v));
 //      Teuchos::RCP<std::vector<Real> > hvp =
 //        Teuchos::rcp_const_cast<std::vector<Real> >((Teuchos::dyn_cast<StdVector<Real> >(hv)).getVector());
 //      Teuchos::RCP<std::vector<Real> > vp =
 //        Teuchos::rcp_const_cast<std::vector<Real> >((Teuchos::dyn_cast<StdVector<Real> >(const_cast<Vector<Real> &>(v))).getVector());
 
-      int dim = vp->size();
+      int dim = vp.size();
 
       // Compute dense Hessian.
       Teuchos::SerialDenseMatrix<int, Real> H(dim, dim);
@@ -538,7 +538,7 @@ namespace ZOO {
 
       // Apply dense inverse Hessian.
       Teuchos::SerialDenseVector<int, Real> hv_teuchos(Teuchos::View, &((*hvp)[0]), dim);
-      Teuchos::SerialDenseVector<int, Real> v_teuchos(Teuchos::View, &((*vp)[0]), dim);
+      const Teuchos::SerialDenseVector<int, Real> v_teuchos(Teuchos::View, &(vp[0]), dim);
       hv_teuchos.multiply(Teuchos::NO_TRANS, Teuchos::NO_TRANS, 1.0, invH, v_teuchos, 0.0);
     }
 
