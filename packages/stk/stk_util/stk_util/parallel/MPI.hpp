492488020934c68dfb8b7b83337d59e7be3b69f5
Greg Sjaardema <gdsjaar@sandia.gov>
2014-11-26 14:54:03 -0700

    Revert "STK_UTIL: Support for building on 32-bit systems (size_t is 32 bit, not 64-bit) [Nico Schlomer]"
    This causes issues on a redhat-5 system using the intel-12 compiler.
    Looking at the compiled source, it looks to be giving incorrect
    results for the __SIZEOF_??__ symbols.  Reverting until
    we have more time to figure out what is happening.
    
    After further examination, on the intel-12 compiler on sierra002, the __SIZEOF_??__
    symbols are not being defined, so __SIZEOF_INT__ == __SIZEOF_SIZE_T__ is
    true (0 == 0) which is incorrect.  Will need to determine where those symbols
    are defined.
    
    Note that with this revert, building on 32-bit systems is again broken
    (but hopefully running on rhel5 is unbroken)
    
    This reverts commit 9f614ba63dd19e22f0824a823f478eb5c8c0b59c.
    
    (From Sierra)

--- packages/stk/stk_util/stk_util/parallel/MPI.hpp
+++ packages/stk/stk_util/stk_util/parallel/MPI.hpp
@@ -1,23 +1,23 @@
 // Copyright (c) 2013, Sandia Corporation.
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-//
+// 
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
-//
+// 
 //     * Redistributions of source code must retain the above copyright
 //       notice, this list of conditions and the following disclaimer.
-//
+// 
 //     * Redistributions in binary form must reproduce the above
 //       copyright notice, this list of conditions and the following
 //       disclaimer in the documentation and/or other materials provided
 //       with the distribution.
-//
+// 
 //     * Neither the name of Sandia Corporation nor the names of its
 //       contributors may be used to endorse or promote products derived
 //       from this software without specific prior written permission.
-//
+// 
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
 // LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
@@ -29,11 +29,12 @@
 // THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-//
+// 
 
 #ifndef STK_UTIL_PARALLEL_MPI_hpp
 #define STK_UTIL_PARALLEL_MPI_hpp
 
+#include <boost/static_assert.hpp>
 #include <stk_util/stk_config.h>
 #if defined ( STK_HAS_MPI )
 
@@ -435,19 +436,8 @@ template <>
 struct Datatype<Loc<size_t, int> >
 {
   static MPI_Datatype type() {
-#if __SIZEOF_INT__==__SIZEOF_SIZE_T__
-    return MPI_2INT;
-#elif __SIZEOF_LONG__==__SIZEOF_SIZE_T__
-    return MPI_LONG_INT;
-#elif __SIZEOF_LONG_LONG__==__SIZEOF_SIZE_T__
-    return MPI_LONG_LONG_INT;
-#elif __SIZEOF_DOUBLE__==__SIZEOF_SIZE_T__
+    BOOST_STATIC_ASSERT(sizeof(double) == sizeof(size_t));
     return MPI_DOUBLE_INT;
-#elif __SIZEOF_FLOAT__==__SIZEOF_SIZE_T__
-    return MPI_FLOAT_INT;
-#else
-#error No type matching __SIZEOF_SIZE_T__
-#endif
   }
 };
 
