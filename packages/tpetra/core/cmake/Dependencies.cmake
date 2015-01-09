5fdede7b7d22885110465ee76b61169859afa858
crtrott <crtrott@sandia.gov>
2015-01-09 15:51:38 -0700

    Tpetra: makeing new Kokkos a required dependency of Tpetra
    This switches Teuchos_KokkosComm Teuchos_KokkosCompat Kokkos_Core Kokkos_Algorithms
    Kokkos_Containers Tpetra_Kernels to required dependencies of Tpetra.
    After switching KokkosRefactor ON by default you would get configure errors
    if you didn't explicitly enable Kokkos. This changes that since a required package
    is autoenabled. You can still turn of the instantion of the new node types by setting
    Tpetra_ENABLE_Kokkos_Refactor=OFF.
    Kokkos itself does not build much. It only builds some utility functions such as
    initialization, finalization and allocation. So it will not add much to build times.

--- packages/tpetra/core/cmake/Dependencies.cmake
+++ packages/tpetra/core/cmake/Dependencies.cmake
@@ -1,5 +1,5 @@
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
-  LIB_REQUIRED_PACKAGES Teuchos TpetraClassic
-  LIB_OPTIONAL_PACKAGES Epetra KokkosCore KokkosContainers KokkosAlgorithms TeuchosKokkosCompat TeuchosKokkosComm TpetraKernels TpetraTSQR
+  LIB_REQUIRED_PACKAGES Teuchos TpetraClassic KokkosCore KokkosContainers KokkosAlgorithms TeuchosKokkosCompat TeuchosKokkosComm TpetraKernels
+  LIB_OPTIONAL_PACKAGES Epetra TpetraTSQR
   LIB_OPTIONAL_TPLS MPI CUDA Thrust QD Cusp
 )
