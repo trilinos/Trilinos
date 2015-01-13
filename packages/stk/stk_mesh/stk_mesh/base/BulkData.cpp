50fdd4840ecef8e9bf6792a6e487761e578b8419
Patrick G. Xavier <pgxavie@sandia.gov>
2015-01-13 16:22:34 -0700

    stk_mesh BulkData::dump_all_mesh_info includes topology of relation target entities.
    (From Sierra)

--- packages/stk/stk_mesh/stk_mesh/base/BulkData.cpp
+++ packages/stk/stk_mesh/stk_mesh/base/BulkData.cpp
@@ -1570,7 +1570,11 @@ void BulkData::dump_all_mesh_info(std::ostream& out) const
             ConnectivityOrdinal const* ordinals = bucket->begin_ordinals(b_ord, r);
             const int num_conn         = bucket->num_connectivity(b_ord, r);
             for (int c_itr = 0; c_itr < num_conn; ++c_itr) {
-              out << "          " << print_entity_key(m_mesh_meta_data, entity_key(entities[c_itr])) << "[" << ordinals[c_itr] << "]" << std::endl;
+              out << "          " << print_entity_key(m_mesh_meta_data, entity_key(entities[c_itr])) << "[" << ordinals[c_itr] << "]  ";
+              if (r != stk::topology::NODE_RANK) {
+                out << this->bucket(entities[c_itr]).topology();
+              }
+              out << std::endl;
             }
           }
         }
