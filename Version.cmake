8c6bc7e01b130b8536c27b3960c43cd51eedc231
Brent Perschbacher <bmpersc@sandia.gov>
2015-01-22 12:11:58 -0700

    Initial commit for 11.14

--- Version.cmake
+++ Version.cmake
@@ -59,12 +59,12 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 11.13)
+SET(Trilinos_VERSION 11.14)
 SET(Trilinos_MAJOR_VERSION 11)
-SET(Trilinos_MAJOR_MINOR_VERSION 111300)
-SET(Trilinos_VERSION_STRING "11.13 (Dev)")
-SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
+SET(Trilinos_MAJOR_MINOR_VERSION 111400)
+SET(Trilinos_VERSION_STRING "11.14")
+SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
-SET(Trilinos_REPOSITORY_BRANCH "master" CACHE INTERNAL "")
-SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
+SET(Trilinos_REPOSITORY_BRANCH "trilinos-release-11-14-branch" CACHE INTERNAL "")
+SET(Trilinos_TESTING_TRACK "Nightly Release 11.14" CACHE INTERNAL "")
