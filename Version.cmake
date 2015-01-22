f5782da887c869c3aeaeb2a1103f273f311a9cae
Brent Perschbacher <bmpersc@sandia.gov>
2015-01-22 12:25:31 -0700

    Update version number to 11.15

--- Version.cmake
+++ Version.cmake
@@ -59,10 +59,10 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 11.13)
+SET(Trilinos_VERSION 11.15)
 SET(Trilinos_MAJOR_VERSION 11)
-SET(Trilinos_MAJOR_MINOR_VERSION 111300)
-SET(Trilinos_VERSION_STRING "11.13 (Dev)")
+SET(Trilinos_MAJOR_MINOR_VERSION 111500)
+SET(Trilinos_VERSION_STRING "11.15 (Dev)")
 SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
