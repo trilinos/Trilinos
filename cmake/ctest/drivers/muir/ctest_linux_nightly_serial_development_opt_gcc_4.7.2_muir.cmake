59479981a1555c46fd65cead598ad672f900207e
Brent Perschbacher <bmpersc@sandia.gov>
2015-01-29 11:08:55 -0700

    Muir: Update testing to CXX11
    Updated the testing that is run on muir to use the C++11 features to ensure most
    packages will build with them before the move to requiring it. For now I have
    set the builds to experimental since this is the first run with C++11 turned on.

--- cmake/ctest/drivers/muir/ctest_linux_nightly_serial_development_opt_gcc_4.7.2_muir.cmake
+++ cmake/ctest/drivers/muir/ctest_linux_nightly_serial_development_opt_gcc_4.7.2_muir.cmake
@@ -65,7 +65,7 @@ SET(BUILD_TYPE RELEASE)
 SET(BUILD_DIR_NAME SERIAL_OPT_DEV)
 SET(COMPILER_VERSION "GCC-4.7.2")
 SET(ENV{LD_LIBRARY_PATH} "/home/trilinos/compilers/gcc/support_libs/mpc-1.0.1/lib:/home/trilinos/compilers/gcc/support_libs/mpfr-3.1.2/lib:/home/trilinos/compilers/gcc/support_libs/gmp-5.1.1/lib:/home/trilinos/compilers/gcc/4.7.2/lib64:$ENV{LD_LIBRARY_PATH}")
-SET(CTEST_TEST_TYPE Nightly)
+SET(CTEST_TEST_TYPE Experimental)
 #SET(CTEST_TEST_TIMEOUT 900)
 
 SET(Trilinos_ENABLE_SECONDARY_STABLE_CODE ON)
