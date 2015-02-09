f794034fed80e3dd0a880e4b6f718861fdf1737b
Brett Clark <bwclark@sandia.gov>
2015-02-09 10:08:18 -0700

    Modified an error check in ex_put_partial_elem_conn() to look for "< 0" rather than "== -1" when looking up blocks by id. Added code to correctly subset and concatenate results when extracting iso surfaces.  Add code to output iso level set data when writing results.
    (From Sierra)

--- packages/seacas/libraries/exodus/cbind/src/ex_put_partial_elem_conn.c
+++ packages/seacas/libraries/exodus/cbind/src/ex_put_partial_elem_conn.c
@@ -79,7 +79,7 @@ int ex_put_partial_elem_conn (int  exoid,
   exerrval = 0; /* clear error code */
 
   /* Determine index of elem_blk_id in VAR_ID_EL_BLK array */
-  if ((elem_blk_id_ndx = ex_id_lkup(exoid, EX_ELEM_BLOCK, elem_blk_id)) == -1)
+  if ((elem_blk_id_ndx = ex_id_lkup(exoid, EX_ELEM_BLOCK, elem_blk_id)) < 0)
     {
       if (exerrval == EX_NULLENTITY) {
 	sprintf(errmsg,
