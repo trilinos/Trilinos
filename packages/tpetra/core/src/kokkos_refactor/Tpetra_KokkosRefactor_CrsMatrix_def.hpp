d197074e6a5ca7501a3c1f8f5b5867ebe266af36
Mark Hoemmen <mhoemme@sandia.gov>
2015-02-08 22:03:55 -0700

    Tpetra: Fixed minor build warning (signed / unsigned comparison).

--- packages/tpetra/core/src/kokkos_refactor/Tpetra_KokkosRefactor_CrsMatrix_def.hpp
+++ packages/tpetra/core/src/kokkos_refactor/Tpetra_KokkosRefactor_CrsMatrix_def.hpp
@@ -6633,8 +6633,8 @@ namespace Tpetra {
           break;
         }
         size_t tmpNumEnt = static_cast<LO> (valInTmp.size ());
-        if (tmpNumEnt < numEnt ||
-            static_cast<size_t> (indInTmp.size ()) < static_cast<size_type> (numEnt)) {
+        if (tmpNumEnt < static_cast<size_t> (numEnt) ||
+            static_cast<size_t> (indInTmp.size ()) < static_cast<size_t> (numEnt)) {
           tmpNumEnt = std::max (static_cast<size_t> (numEnt), tmpNumEnt * 2);
           valInTmp.resize (tmpNumEnt);
           indInTmp.resize (tmpNumEnt);
