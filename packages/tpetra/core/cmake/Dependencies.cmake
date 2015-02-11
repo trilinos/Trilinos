ec04395072a429748bddc4c11b771d7ec96b72f6
Mark Hoemmen <mhoemme@sandia.gov>
2015-02-11 16:17:53 -0700

    TpetraCore: Removed optional CUSP TPL.
    Tpetra doesn't currently need CUSP, so there is no need to depend on
    it.  If we ever want to use CUSP, we should make TpetraKernels depend
    on it, not TpetraCore.

--- packages/tpetra/core/cmake/Dependencies.cmake
+++ packages/tpetra/core/cmake/Dependencies.cmake
@@ -1,5 +1,5 @@
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   LIB_REQUIRED_PACKAGES Teuchos TpetraClassic KokkosCore KokkosContainers KokkosAlgorithms TeuchosKokkosCompat TeuchosKokkosComm TpetraKernels
   LIB_OPTIONAL_PACKAGES Epetra TpetraTSQR
-  LIB_OPTIONAL_TPLS MPI CUDA QD Cusp
+  LIB_OPTIONAL_TPLS MPI CUDA QD
 )
