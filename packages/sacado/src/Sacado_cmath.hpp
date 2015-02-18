94d55f0ccd46e2fd7650eff1f873dd04b9e420f6
Eric Phipps <etphipp@sandia.gov>
2015-02-18 16:48:56 -0700

    Sacado:  Disable definition of acosh() etc. when C++11 is enabled.
    Sacado provides a definition of acosh(), asinh(), and atanh() for when
    these functions are not provided by the standard library (a legacy from
    the original Fad/Rad).  There are configure tests to determine whether
    the standard library defines them and disables these definitions if they
    are defined.  However the tests don't always seem to work, so I am
    adding an additional check if C++11 is enabled, in which case they
    aren't defined.

--- packages/sacado/src/Sacado_cmath.hpp
+++ packages/sacado/src/Sacado_cmath.hpp
@@ -1,31 +1,29 @@
-// $Id$ 
-// $Source$ 
 // @HEADER
 // ***********************************************************************
-// 
+//
 //                           Sacado Package
 //                 Copyright (2006) Sandia Corporation
-// 
+//
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-// 
+//
 // This library is free software; you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation; either version 2.1 of the
 // License, or (at your option) any later version.
-//  
+//
 // This library is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-//  
+//
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 // USA
 // Questions? Contact David M. Gay (dmgay@sandia.gov) or Eric T. Phipps
 // (etphipp@sandia.gov).
-// 
+//
 // ***********************************************************************
 // @HEADER
 
@@ -33,25 +31,25 @@
 #define SACADO_CMATH_HPP
 
 #include <cmath>        // for most math functions
-#include <algorithm>	// for std::min and std::max
+#include <algorithm>    // for std::min and std::max
 #include "Sacado_ConfigDefs.h"
 
 // Define some math functions that aren't usually in cmath
 #define HAS_GXX_CMATH_11 defined(_GLIBCXX_USE_C99_MATH_TR1) && defined(__GXX_EXPERIMENTAL_CXX0X__)
-#if !( HAS_GXX_CMATH_11 || defined(HAS_C99_TR1_CMATH) || defined(USER_DISABLE_SACADO_TR1_CMATH) )
+#if !( HAS_GXX_CMATH_11 || defined(HAVE_SACADO_CXX11) || defined(HAS_C99_TR1_CMATH) || defined(USER_DISABLE_SACADO_TR1_CMATH) )
 namespace std {
-  inline float acosh(float x) { 
+  inline float acosh(float x) {
     return std::log(x + std::sqrt(x*x - float(1.0))); }
-  inline float asinh(float x) { 
+  inline float asinh(float x) {
     return std::log(x + std::sqrt(x*x + float(1.0))); }
-  inline float atanh(float x) { 
+  inline float atanh(float x) {
     return float(0.5)*std::log((float(1.0)+x)/(float(1.0)-x)); }
 
-  inline double acosh(double x) { 
+  inline double acosh(double x) {
     return std::log(x + std::sqrt(x*x - double(1.0))); }
-  inline double asinh(double x) { 
+  inline double asinh(double x) {
     return std::log(x + std::sqrt(x*x + double(1.0))); }
-  inline double atanh(double x) { 
+  inline double atanh(double x) {
     return double(0.5)*std::log((double(1.0)+x)/(double(1.0)-x)); }
 }
 #endif // HAS_C99_TR1_CMATH
