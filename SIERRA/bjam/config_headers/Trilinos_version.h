7d3fb8426207dab07b83bd35f382951787631761
Brent Perschbacher <bmpersc@sandia.gov>
2015-03-03 12:06:51 -0700

    Update version number to 11.14.1

--- SIERRA/bjam/config_headers/Trilinos_version.h
+++ SIERRA/bjam/config_headers/Trilinos_version.h
@@ -121,7 +121,7 @@
 * version 10.2.4 is designated 100204.  This preserves the comparability of
 * these version numbers with simple comparison operators used in #ifdef tests.
 */
-#define TRILINOS_MAJOR_MINOR_VERSION 111400
+#define TRILINOS_MAJOR_MINOR_VERSION 111401
 
 /* NOTE: These macros are given long int values to allow comparisons in
  * preprocessor #if statements.  For example, you can do comparisons with ==,
@@ -136,6 +136,6 @@
  *
  * NOTE: This string is to be used for outputting, not for comparison logic.
  */
-#define TRILINOS_VERSION_STRING "11.14"
+#define TRILINOS_VERSION_STRING "11.14.1"
 
 #endif /* TRILINOS_VERSION_H */
