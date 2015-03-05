b23a2ba1c724fc06f4a1754cc9cd664e1dbee819
Brent Perschbacher <bmpersc@sandia.gov>
2015-03-05 08:28:09 -0700

    Permanently disable PyTrilinos' interface to Tpetra
    Currently the Tpetra interface to Tpetra does not work and causes numerous
    compile errors when it is enabled. With the blessing of Bill Spotz I am forcing
    it to be off regardless of if it should be enabled for some reason.

--- cmake/CallbackSetupExtraOptions.cmake
+++ cmake/CallbackSetupExtraOptions.cmake
@@ -62,6 +62,9 @@ MACRO(TRIBITS_REPOSITORY_SETUP_EXTRA_OPTIONS)
       )
     SET(${PROJECT_NAME}_ENABLE_PyTrilinos OFF)
   ENDIF()
+  
+  #disabling PyTrilinos' use of Tpetra due to build issues.
+  SET(PyTrilinos_ENABLE_Tpetra OFF)
 
   IF (NOT EXISTS "${Trilinos_SOURCE_DIR}/packages/TriKota/Dakota")
     MESSAGE("-- " "  Setting ${PROJECT_NAME}_ENABLE_TriKota=OFF"
