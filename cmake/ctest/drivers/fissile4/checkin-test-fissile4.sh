95cd8fc10b4a715fdc6c8acba0ec783d7ef67e0b
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-03-09 20:01:28 -0400

    Add beginning of a GCC 4.8.3 dev env for Trilinos on CASL machines
    I will soon flesh this out to change over the nightly builds of Trilinos too.

---
+++ cmake/ctest/drivers/fissile4/checkin-test-fissile4.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+# Used to test Trilinos on any of the ORNL CASL Fissile/Spy machines
+#
+# This script requires that the VERA dev env be loaded by sourcing the script:
+#
+#  . /projects/vera/gcc-4.8.3/load_dev_env.[sh,csh]
+#
+# You can source this script either in your shell startup script
+# (e.g. .bash_profile) or you can source it manually whenever you need to set
+# up to build VERA software.
+#
+# NOTE: This script should not be directly modifed by typical CASL
+# developers except, perhaps to add new extra builds.
+
+EXTRA_ARGS=$@
+
+# The default location for this directory tree is:
+#
+#  Trilinos.base
+#    Trilinos    (your Trilinos soruce tree)
+#    BUILDS
+#      CHECKIN   (where you run this script from)
+#
+if [ "$TRILINOS_BASE_DIR" == "" ] ; then
+  TRILINOS_BASE_DIR=../..
+fi
+
+TRILINOS_BASE_DIR_ABS=$(readlink -f $TRILINOS_BASE_DIR)
+
+DRIVERS_BASE_DIR="$TRILINOS_BASE_DIR_ABS/Trilinos/cmake/ctest/drivers/fissile4"
+
+# Packages in Trilinos to disable (mostly for auotmated CI server)
+DISABLE_PACKAGES=CTrilinos,ForTrilinos,PyTrilinos,Didasko,Mesquite,Phdmesh,Pliris,Claps,Amesos2,STK,FEApp,TriKota,Optika
+
+# Check to make sure that the env has been loaded correctly
+if [ "$LOADED_TRIBITS_DEV_ENV" != "gcc-4.8.3" ] ; then
+  echo "Error, must source /projects/vera/gcc-4.8.3/load_dev_env.[sh,csh] before running checkin-test-vera.sh!"
+  exit 1
+fi
+
+echo "
+-DTrilinos_DISABLE_ENABLED_FORWARD_DEP_PACKAGES=ON
+" > COMMON.config
+
+#
+# Built-in Primary Tested (PT) --default-builds (DO NOT MODIFY)
+#
+
+echo "
+-DTrilinos_CONFIGURE_OPTIONS_FILE:FILEPATH='$DRIVERS_BASE_DIR/gcc-4.8.3-base-options.cmake,$DRIVERS_BASE_DIR/trilinos-tpls-gcc.4.8.3.cmake'
+" > MPI_DEBUG.config
+
+echo "
+-DTrilinos_CONFIGURE_OPTIONS_FILE:FILEPATH='$DRIVERS_BASE_DIR/gcc-4.8.3-base-options.cmake,$DRIVERS_BASE_DIR/trilinos-tpls-gcc.4.8.3.cmake'
+" > SERIAL_RELEASE.config
+
+#
+# Standard Secondary Tested (ST) --st-extra-builds (DO NOT MODIFY)
+#
+
+echo "
+-DTrilinos_CONFIGURE_OPTIONS_FILE:FILEPATH='$DRIVERS_BASE_DIR/gcc-4.8.3-base-options.cmake,$DRIVERS_BASE_DIR/trilinos-tpls-gcc.4.8.3.cmake'
+" > MPI_DEBUG_ST.config
+
+echo "
+-DTrilinos_CONFIGURE_OPTIONS_FILE:FILEPATH='$DRIVERS_BASE_DIR/gcc-4.8.3-base-options.cmake,$DRIVERS_BASE_DIR/trilinos-tpls-gcc.4.8.3.cmake'
+" > SERIAL_RELEASE_ST.config
+
+#
+# --extra-builds
+#
+
+echo "
+-DTrilinos_CONFIGURE_OPTIONS_FILE:FILEPATH='$DRIVERS_BASE_DIR/gcc-4.8.3-base-options.cmake,$DRIVERS_BASE_DIR/trilinos-tpls-gcc.4.8.3.cmake'
+-DCMAKE_BUILD_TYPE:STRING=RELEASE
+-DTrilinos_ENABLE_DEBUG:BOOL=OFF
+" > MPI_RELEASE.config
+
+#
+# Invocation
+#
+
+$TRILINOS_BASE_DIR/Trilinos/checkin-test.py \
+-j16 \
+--ctest-timeout=180 \
+--st-extra-builds=MPI_DEBUG_ST,SERIAL_RELEASE_ST \
+--disable-packages=$DISABLE_PACKAGES \
+--skip-case-no-email \
+$EXTRA_ARGS
+
+
+# --ctest-options="-E '(Piro_AnalysisDriver|Stokhos_Linear2D_Diffusion_GMRES_KLR|Panzer_STK_ResponseLibraryTest|MueLu_|Amesos2_|Rythmos_ImplicitRK_UnitTest_MPI_1|SEACASExodus_exodus_unit_tests|Intrepid_test_Discretization_Basis_HGRAD_TRI_Cn_FEM_Test_02_MPI_1|Intrepid_test_Discretization_Basis_HDIV_TET_In_FEM_Test_02_MPI_1|Intrepid_test_Discretization_Basis_HGRAD_TET_Cn_FEM_Test_02_MPI_1|Sundance_BesselTest2D_MPI_1|ThyraTpetraAdapters_TpetraThyraWrappersUnitTests_serial|Ifpack2_RILUKSingleProcessUnitTests)'"
+
+# NOTE: By default we use 16 processes which is 1/2 of the 32 processes on a
+# fissile 4 machine.  This way two people can build and test without taxing
+# the machine too much.
+
+# Here is the template for excluding tests that can be aded to
+# checkin-test.py call above
+#
+#   --ctest-options="-E '(Test1|Test2)'" \
---
+++ cmake/ctest/drivers/fissile4/trilinos-tpls-gcc.4.8.3.cmake
@@ -0,0 +1,59 @@
+#
+# Trilinos TPLs
+#
+
+# Give a default TPL_INSTALL_DIR if not set!
+IF (NOT TPL_INSTALL_DIR)
+  SET(TPL_INSTALL_DIR_DEFAULT_STATIC
+    "/projects/vera/gcc-4.8.3/tpls/opt_static")
+  IF ( (NOT BUILD_SHARED_LIBS) AND (EXISTS "${TPL_INSTALL_DIR_DEFAULT_STATIC}") )
+    SET(TPL_INSTALL_DIR_DEFAULT "${TPL_INSTALL_DIR_DEFAULT_STATIC}")
+  ELSE()
+    SET(TPL_INSTALL_DIR_DEFAULT "/projects/vera/gcc-4.8.3/tpls/opt")
+  ENDIF()
+  MESSAGE("-- " "TPL_INSTALL_DIR not set so setting by default to"
+    " ${TPL_INSTALL_DIR_DEFAULT}!")
+  SET(TPL_INSTALL_DIR  "${TPL_INSTALL_DIR_DEFAULT}"  CACHE  PATH
+    "Set by default in vera-tpls-std.cmake")
+ENDIF()
+
+# NOTE: Above we turn on all the TPLs, reguardless of packages are enabled.
+# If a VERA repo is not enabled that defines one of these TPLs, the enables
+# are no-ops.  This ensures that the same XXX_config.h files get generated.
+# For exmaple, Teuchos defines an optional dependence on QT.  However, if you
+# don't enable the SCALE packages, QT (which is required for SCALE) does not
+# get enabled and Teuchos_config.h does not define HAVE_TEUCHOS_QT.  This
+# causes the Teuchos_config.h file to keep changing with different package
+# enables.  If you don't want to enable one of these TPLs (because you know
+# you don't need it) you can just pass in -DTPL_ENABLE_<TPLNAME>=OFF and it
+# will override the above cache enable.
+
+SET(BLAS_LIBRARY_DIRS "${TPL_INSTALL_DIR}/lapack-3.3.1/lib" CACHE FILEPATH "")
+SET(LAPACK_LIBRARY_DIRS "${TPL_INSTALL_DIR}/lapack-3.3.1/lib" CACHE FILEPATH "")
+
+SET(Boost_DIR "${TPL_INSTALL_DIR}/boost-1.55.0")
+SET(Boost_INCLUDE_DIRS "${Boost_DIR}/include" CACHE FILEPATH "")
+
+SET(BoostLib_LIBRARY_NAMES "boost_system;boost_thread;boost_program_options" CACHE STRING  "")
+SET(BoostLib_INCLUDE_DIRS ${Boost_INCLUDE_DIRS}  CACHE FILEPATH "")
+SET(BoostLib_LIBRARY_DIRS "${Boost_DIR}/lib" CACHE FILEPATH "")
+
+SET(Zlib_INCLUDE_DIRS "${TPL_INSTALL_DIR}/zlib-1.2.7/include" CACHE FILEPATH "" )
+SET(Zlib_LIBRARY_DIRS "${TPL_INSTALL_DIR}/zlib-1.2.7/lib" CACHE FILEPATH "" )
+
+SET(HDF5_DIR "${TPL_INSTALL_DIR}/hdf5-1.8.10")
+SET(HDF5_LIB_DIR "${HDF5_DIR}/lib")
+SET(HDF5_LIBRARY_NAMES "hdf5_hl;hdf5_cpp;hdf5_fortran;hdf5;z" CACHE STRING "")
+SET(HDF5_INCLUDE_DIRS
+  "${HDF5_DIR}/include;${TPL_COMMON_DIR}/zlib-1.2.7/include"
+  CACHE FILEPATH "" )
+SET(HDF5_LIBRARY_DIRS
+  "${HDF5_LIB_DIR};${TPL_INSTALL_DIR}/zlib-1.2.7/lib"
+  CACHE FILEPATH "" )
+
+SET(QT_REQUIRED_VERSION 4.7.1 CACHE STRING "" )
+SET(QT_QMAKE_EXECUTABLE ${TPL_INSTALL_DIR}/qt-4.8.2/bin/qmake CACHE FILEPATH "" )
+
+# TriKota Hack!
+# Must force this varible in the Dakota configure process
+SET(BOOST_INCLUDEDIR ${Boost_INCLUDE_DIRS}  CACHE PATH  "" FORCE)
