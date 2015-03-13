d7b421adc38da890ced3a177c6aa74f723563f04
Siva Rajamanickam <srajama@sandia.gov>
2015-03-13 13:42:55 -0600

    ShyLU<core> : removing anasazi from required deps. Not sure why it was there  in the first place.

--- packages/shylu/core/cmake/Dependencies.cmake
+++ packages/shylu/core/cmake/Dependencies.cmake
@@ -1,4 +1,4 @@
-SET(LIB_REQUIRED_DEP_PACKAGES Epetra Isorropia AztecOO Belos Ifpack Amesos Teuchos Anasazi)
+SET(LIB_REQUIRED_DEP_PACKAGES Epetra Isorropia AztecOO Belos Ifpack Amesos Teuchos)
 SET(LIB_OPTIONAL_DEP_PACKAGES Tpetra Zoltan2 Amesos2)
 SET(TEST_REQUIRED_DEP_PACKAGES ML)
 SET(TEST_OPTIONAL_DEP_PACKAGES)
