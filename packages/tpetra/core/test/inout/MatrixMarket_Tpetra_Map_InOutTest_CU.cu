f73840190c1fcf7ee52d3c7215b9ee7478d102ba
Mark Hoemmen <mhoemme@sandia.gov>
2015-03-18 16:18:14 -0600

    Tpetra: Removed .cu test files (no longer used).

--- packages/tpetra/core/test/inout/MatrixMarket_Tpetra_Map_InOutTest_CU.cu
+++
@@ -1,9 +0,0 @@
-#include <Tpetra_ConfigDefs.hpp>
-#ifdef HAVE_TPETRACORE_TEUCHOSKOKKOSCOMPAT
-#include <KokkosCore_config.h>
-#ifdef KOKKOS_HAVE_CUDA
-#define KOKKOS_USE_CUDA_BUILD
-#include "inout/MatrixMarket_Tpetra_Map_InOutTest.cpp"
-#undef KOKKOS_USE_CUDA_BUILD
-#endif
-#endif
