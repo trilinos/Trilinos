e23abe5e488ebcd8343156bb00907c68cc0e4ed9
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-04-03 11:44:25 -0400

    Enable git rerere in all my local repos

--- sampleScripts/git-profiles/git-config-bartlettra.sh
+++ sampleScripts/git-profiles/git-config-bartlettra.sh
@@ -6,6 +6,8 @@ git config --global user.email bartlettra@ornl.gov
 git config --global color.ui true
 # Push only the current tracking branch (upstream in newer git versions)
 git config --global push.default tracking
+# Enable rerere by default
+git config --global rerere.enabled 1
 # Set up some shortcut commands
 _SCRIPT_DIR=`echo $0 | sed "s/\(.*\)\/.*\.sh/\1/g"`
 $_SCRIPT_DIR/../../cmake/tribits/common_tools/git/git-config-alias.sh
