3a41df68a6d3a2693154323f4784b0528081ad57
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-04-10 19:56:06 -0400

    Set CDash email to rppawlo@sandia.gov for now (PHI Kanban #3693)
    Since I doubt that the pike-regressions email address exists yet, we will set
    this to Roger's email.  We can change this later.

--- packages/pike/cmake/Dependencies.cmake
+++ packages/pike/cmake/Dependencies.cmake
@@ -3,5 +3,6 @@ TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
     #Core        core        PS REQUIRED
     BlackBox    blackbox    ST OPTIONAL
     Implicit    implicit    ST OPTIONAL
+  REGRESSION_EMAIL_LIST  rppawlo@sandia.gov 
   )
 # NOTE: The above subpackages are automatically listed as dependencies
