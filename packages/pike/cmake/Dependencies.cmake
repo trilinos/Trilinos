543521e7914d7eba4ab5409bb129bf998fd78462
Roscoe A. Bartlett <bartlettra@ornl.gov>
2015-04-10 19:53:09 -0400

    Change Pike subpackages to ST for now (PHI Kanban #3693)
    Until we get back test results, we don't want to make this PT or it will get
    tested in pre-push testing.

--- packages/pike/cmake/Dependencies.cmake
+++ packages/pike/cmake/Dependencies.cmake
@@ -1,7 +1,7 @@
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
     #Core        core        PS REQUIRED
-    BlackBox    blackbox    PS OPTIONAL
-    Implicit    implicit    PS OPTIONAL
+    BlackBox    blackbox    ST OPTIONAL
+    Implicit    implicit    ST OPTIONAL
   )
 # NOTE: The above subpackages are automatically listed as dependencies
