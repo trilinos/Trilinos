1e45d260b5bd00bbfa2d6f13d21da467f7a41fee
Tobias Wiesner <wiesner@lnm.mw.tum.de>
2015-04-16 12:27:57 +0200

    MueLu: add some more unit tests for uncoupled aggregation.

--- packages/muelu/test/unit_tests/Aggregates.cpp
+++ packages/muelu/test/unit_tests/Aggregates.cpp
@@ -50,6 +50,7 @@
 #include "MueLu_FactoryManagerBase.hpp"
 #include "MueLu_CoalesceDropFactory.hpp"
 #include "MueLu_CoupledAggregationFactory.hpp"
+#include "MueLu_UncoupledAggregationFactory.hpp"
 #include "MueLu_AmalgamationFactory.hpp"
 #include "MueLu_AmalgamationInfo.hpp"
 #include "MueLu_Aggregates.hpp"
@@ -60,8 +61,46 @@ namespace MueLuTests {
 
 #include "MueLu_UseShortNames.hpp"
 
-  // Little utility to generate aggregates.
-  RCP<Aggregates> gimmeAggregates(const RCP<Matrix> & A, RCP<AmalgamationInfo> & amalgInfo)
+  // Little utility to generate uncoupled aggregates.
+  RCP<Aggregates> gimmeUncoupledAggregates(const RCP<Matrix> & A, RCP<AmalgamationInfo> & amalgInfo, bool bPhase1 = true, bool bPhase2a = true, bool bPhase2b = true, bool bPhase3 = true)
+  {
+    Level level;
+    TestHelpers::TestFactory<SC,LO,GO,NO>::createSingleLevelHierarchy(level);
+    level.Set("A", A);
+
+    RCP<AmalgamationFactory> amalgFact = rcp(new AmalgamationFactory());
+    RCP<CoalesceDropFactory> dropFact = rcp(new CoalesceDropFactory());
+    dropFact->SetFactory("UnAmalgamationInfo", amalgFact);
+
+    std::cout << "Phase2a=" << bPhase2a << std::endl;
+
+    // Setup aggregation factory (use default factory for graph)
+    RCP<UncoupledAggregationFactory> aggFact = rcp(new UncoupledAggregationFactory());
+    aggFact->SetFactory("Graph", dropFact);
+    aggFact->SetParameter("aggregation: mode",Teuchos::ParameterEntry(std::string("new")));
+    aggFact->SetParameter("aggregation: max agg size",Teuchos::ParameterEntry(3));
+    aggFact->SetParameter("aggregation: min agg size",Teuchos::ParameterEntry(3));
+    aggFact->SetParameter("aggregation: max selected neighbors",Teuchos::ParameterEntry(0));
+    aggFact->SetParameter("aggregation: ordering",Teuchos::ParameterEntry(std::string("natural")));
+    aggFact->SetParameter("aggregation: enable phase 1",  Teuchos::ParameterEntry(bPhase1));
+    aggFact->SetParameter("aggregation: enable phase 2a", Teuchos::ParameterEntry(bPhase2a));
+    aggFact->SetParameter("aggregation: enable phase 2b", Teuchos::ParameterEntry(bPhase2b));
+    aggFact->SetParameter("aggregation: enable phase 3",  Teuchos::ParameterEntry(bPhase3));
+
+    level.Request("Aggregates", aggFact.get());
+    level.Request("UnAmalgamationInfo", amalgFact.get());
+
+    level.Request(*aggFact);
+    aggFact->Build(level);
+    RCP<Aggregates> aggregates = level.Get<RCP<Aggregates> >("Aggregates",aggFact.get()); // fix me
+    amalgInfo = level.Get<RCP<AmalgamationInfo> >("UnAmalgamationInfo",amalgFact.get()); // fix me
+    level.Release("UnAmalgamationInfo", amalgFact.get());
+    level.Release("Aggregates", aggFact.get());
+    return aggregates;
+  }  // gimmeUncoupledAggregates
+
+  // Little utility to generate coupled aggregates.
+  RCP<Aggregates> gimmeCoupledAggregates(const RCP<Matrix> & A, RCP<AmalgamationInfo> & amalgInfo)
   {
     Level level;
     TestHelpers::TestFactory<SC,LO,GO,NO>::createSingleLevelHierarchy(level);
@@ -89,15 +128,16 @@ namespace MueLuTests {
     level.Release("UnAmalgamationInfo", amalgFact.get());
     level.Release("Aggregates", aggFact.get());
     return aggregates;
-  }  // gimmeAggregates
+  }  // gimmeCoupledAggregates
 
   TEUCHOS_UNIT_TEST(Aggregates, JustAggregation)
   {
     out << "version: " << MueLu::Version() << std::endl;
     RCP<Matrix> A = TestHelpers::TestFactory<SC, LO, GO, NO>::Build1DPoisson(15);
     RCP<AmalgamationInfo> amalgInfo;
-    RCP<Aggregates> aggregates = gimmeAggregates(A, amalgInfo);
+    RCP<Aggregates> aggregates = gimmeCoupledAggregates(A, amalgInfo);
     TEST_EQUALITY(aggregates != Teuchos::null, true);
+    TEST_EQUALITY(aggregates->AggregatesCrossProcessors(),true);
   }
 
   ///////////////////////////////////////////////////////////////////////////
@@ -109,9 +149,11 @@ namespace MueLuTests {
     RCP<Matrix> A = TestHelpers::TestFactory<SC, LO, GO, NO>::Build1DPoisson(36);
     RCP<const Map> rowmap = A->getRowMap();
     RCP<AmalgamationInfo> amalgInfo;
-    RCP<Aggregates> aggregates = gimmeAggregates(A, amalgInfo);
+    RCP<Aggregates> aggregates = gimmeCoupledAggregates(A, amalgInfo);
     GO numAggs = aggregates->GetNumAggregates();
     RCP<const Teuchos::Comm<int> > comm = TestHelpers::Parameters::getDefaultComm();
+
+    TEST_EQUALITY(aggregates->AggregatesCrossProcessors(),true);
 
     ArrayRCP<LO> aggSizes = Teuchos::ArrayRCP<LO>(numAggs);
     ArrayRCP<LO> aggStart;
@@ -176,5 +218,256 @@ namespace MueLuTests {
 
   } //GetNumAggregates
 
+  TEUCHOS_UNIT_TEST(Aggregates, JustUncoupledAggregation)
+  {
+    out << "version: " << MueLu::Version() << std::endl;
+    RCP<Matrix> A = TestHelpers::TestFactory<SC, LO, GO, NO>::Build1DPoisson(15);
+    RCP<AmalgamationInfo> amalgInfo;
+    RCP<Aggregates> aggregates = gimmeUncoupledAggregates(A, amalgInfo);
+    TEST_EQUALITY(aggregates != Teuchos::null, true);
+    TEST_EQUALITY(aggregates->AggregatesCrossProcessors(),false);
+  }
 
+  ///////////////////////////////////////////////////////////////////////////
+
+  TEUCHOS_UNIT_TEST(Aggregates, GetNumUncoupledAggregates)
+  {
+    out << "version: " << MueLu::Version() << std::endl;
+
+    RCP<Matrix> A = TestHelpers::TestFactory<SC, LO, GO, NO>::Build1DPoisson(36);
+    RCP<const Map> rowmap = A->getRowMap();
+    RCP<AmalgamationInfo> amalgInfo;
+    RCP<Aggregates> aggregates = gimmeUncoupledAggregates(A, amalgInfo);
+    GO numAggs = aggregates->GetNumAggregates();
+    RCP<const Teuchos::Comm<int> > comm = TestHelpers::Parameters::getDefaultComm();
+
+    TEST_EQUALITY(aggregates->AggregatesCrossProcessors(),false);
+
+    ArrayRCP<LO> aggSizes = Teuchos::ArrayRCP<LO>(numAggs);
+    ArrayRCP<LO> aggStart;
+    ArrayRCP<GO> aggToRowMap;
+    amalgInfo->UnamalgamateAggregates(*aggregates, aggStart, aggToRowMap);
+    for (LO i = 0; i < numAggs; ++i)
+      aggSizes[i] = aggStart[i+1] - aggStart[i];
+
+    bool foundAggNotSize3=false;
+    for (int i=0; i<aggSizes.size(); ++i)
+      if (aggSizes[i] != 3) {
+        foundAggNotSize3=true;
+        break;
+      }
+
+    switch (comm->getSize()) {
+
+      case 1 :
+        TEST_EQUALITY(numAggs, 12);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      case 2:
+        TEST_EQUALITY(numAggs, 6);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      case 3:
+        TEST_EQUALITY(numAggs, 4);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      case 4:
+        TEST_EQUALITY(numAggs, 3);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      default:
+        std::string msg = "Only 1-4 MPI processes are supported.";
+        //throw(MueLu::Exceptions::NotImplemented(msg));
+        out << msg << std::endl;
+        break;
+    }
+
+    //ArrayRCP< ArrayRCP<GO> > aggToRowMap(numAggs);
+    int root = out.getOutputToRootOnly();
+    out.setOutputToRootOnly(-1);
+    for (int j=0; j<comm->getSize(); ++j) {
+      if (comm->getRank() == j) {
+        out << "++ pid " << j << " ++" << std::endl;
+        out << "   num local DOFs = " << rowmap->getNodeNumElements() << std::endl;
+        for (int i=0; i< numAggs; ++i) {
+          out << "   aggregate " << i << ": ";
+          for (int k=aggStart[i]; k< aggStart[i+1]; ++k)
+            out << aggToRowMap[k] << " ";
+          out << std::endl;
+        }
+      }
+      comm->barrier();
+    }
+    out.setOutputToRootOnly(root);
+
+  } //GetNumAggregates
+
+  TEUCHOS_UNIT_TEST(Aggregates, UncoupledPhase1)
+  {
+    out << "version: " << MueLu::Version() << std::endl;
+
+    RCP<Matrix> A = TestHelpers::TestFactory<SC, LO, GO, NO>::Build1DPoisson(36);
+    RCP<const Map> rowmap = A->getRowMap();
+    RCP<AmalgamationInfo> amalgInfo;
+    RCP<Aggregates> aggregates = gimmeUncoupledAggregates(A, amalgInfo,true,false,false,false);
+    GO numAggs = aggregates->GetNumAggregates();
+    RCP<const Teuchos::Comm<int> > comm = TestHelpers::Parameters::getDefaultComm();
+
+    TEST_EQUALITY(aggregates->AggregatesCrossProcessors(),false);
+
+    ArrayRCP<LO> aggSizes = Teuchos::ArrayRCP<LO>(numAggs);
+    ArrayRCP<LO> aggStart;
+    ArrayRCP<GO> aggToRowMap;
+    amalgInfo->UnamalgamateAggregates(*aggregates, aggStart, aggToRowMap);
+    for (LO i = 0; i < numAggs; ++i)
+      aggSizes[i] = aggStart[i+1] - aggStart[i];
+
+    bool foundAggNotSize3=false;
+    for (int i=0; i<aggSizes.size(); ++i)
+      if (aggSizes[i] != 3) {
+        foundAggNotSize3=true;
+        break;
+      }
+
+    switch (comm->getSize()) {
+
+      case 1 :
+        TEST_EQUALITY(numAggs, 12);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      case 2:
+        TEST_EQUALITY(numAggs, 6);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      case 3:
+        TEST_EQUALITY(numAggs, 4);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      case 4:
+        TEST_EQUALITY(numAggs, 3);
+        TEST_EQUALITY(foundAggNotSize3, false);
+        break;
+
+      default:
+        std::string msg = "Only 1-4 MPI processes are supported.";
+        //throw(MueLu::Exceptions::NotImplemented(msg));
+        out << msg << std::endl;
+        break;
+    }
+
+    //ArrayRCP< ArrayRCP<GO> > aggToRowMap(numAggs);
+    int root = out.getOutputToRootOnly();
+    out.setOutputToRootOnly(-1);
+    for (int j=0; j<comm->getSize(); ++j) {
+      if (comm->getRank() == j) {
+        out << "++ pid " << j << " ++" << std::endl;
+        out << "   num local DOFs = " << rowmap->getNodeNumElements() << std::endl;
+        for (int i=0; i< numAggs; ++i) {
+          out << "   aggregate " << i << ": ";
+          for (int k=aggStart[i]; k< aggStart[i+1]; ++k)
+            out << aggToRowMap[k] << " ";
+          out << std::endl;
+        }
+      }
+      comm->barrier();
+    }
+    out.setOutputToRootOnly(root);
+
+  } //UncoupledPhase1
+
+  TEUCHOS_UNIT_TEST(Aggregates, UncoupledPhase2)
+  {
+    out << "version: " << MueLu::Version() << std::endl;
+
+    RCP<Matrix> A = TestHelpers::TestFactory<SC, LO, GO, NO>::Build1DPoisson(36);
+    RCP<const Map> rowmap = A->getRowMap();
+    RCP<AmalgamationInfo> amalgInfo;
+    bool bSuccess = true;
+    TEUCHOS_TEST_THROW(gimmeUncoupledAggregates(A, amalgInfo,false,true,true,false),
+      MueLu::Exceptions::RuntimeError, out, bSuccess);
+    TEST_EQUALITY(bSuccess, true);
+  } //UncoupledPhase2
+
+  TEUCHOS_UNIT_TEST(Aggregates, UncoupledPhase3)
+  {
+    out << "version: " << MueLu::Version() << std::endl;
+
+    RCP<Matrix> A = TestHelpers::TestFactory<SC, LO, GO, NO>::Build1DPoisson(36);
+    RCP<const Map> rowmap = A->getRowMap();
+    RCP<AmalgamationInfo> amalgInfo;
+    RCP<Aggregates> aggregates = gimmeUncoupledAggregates(A, amalgInfo,false,false,false,true);
+    GO numAggs = aggregates->GetNumAggregates();
+    RCP<const Teuchos::Comm<int> > comm = TestHelpers::Parameters::getDefaultComm();
+
+    TEST_EQUALITY(aggregates->AggregatesCrossProcessors(),false);
+
+    ArrayRCP<LO> aggSizes = Teuchos::ArrayRCP<LO>(numAggs);
+    ArrayRCP<LO> aggStart;
+    ArrayRCP<GO> aggToRowMap;
+    amalgInfo->UnamalgamateAggregates(*aggregates, aggStart, aggToRowMap);
+    for (LO i = 0; i < numAggs; ++i)
+      aggSizes[i] = aggStart[i+1] - aggStart[i];
+
+    bool foundAggNotSize2=false;
+    for (int i=0; i<aggSizes.size(); ++i)
+      if (aggSizes[i] != 2) {
+        foundAggNotSize2=true;
+        break;
+      }
+
+    switch (comm->getSize()) {
+
+      case 1 :
+        TEST_EQUALITY(numAggs, 18);
+        TEST_EQUALITY(foundAggNotSize2, false);
+        break;
+
+      case 2:
+        TEST_EQUALITY(numAggs, 9);
+        TEST_EQUALITY(foundAggNotSize2, false);
+        break;
+
+      case 3:
+        TEST_EQUALITY(numAggs, 6);
+        TEST_EQUALITY(foundAggNotSize2, false);
+        break;
+
+      case 4:
+        TEST_EQUALITY(numAggs, 4);
+        TEST_EQUALITY(foundAggNotSize2, true);
+        break;
+
+      default:
+        std::string msg = "Only 1-4 MPI processes are supported.";
+        //throw(MueLu::Exceptions::NotImplemented(msg));
+        out << msg << std::endl;
+        break;
+    }
+
+    //ArrayRCP< ArrayRCP<GO> > aggToRowMap(numAggs);
+    int root = out.getOutputToRootOnly();
+    out.setOutputToRootOnly(-1);
+    for (int j=0; j<comm->getSize(); ++j) {
+      if (comm->getRank() == j) {
+        out << "++ pid " << j << " ++" << std::endl;
+        out << "   num local DOFs = " << rowmap->getNodeNumElements() << std::endl;
+        for (int i=0; i< numAggs; ++i) {
+          out << "   aggregate " << i << ": ";
+          for (int k=aggStart[i]; k< aggStart[i+1]; ++k)
+            out << aggToRowMap[k] << " ";
+          out << std::endl;
+        }
+      }
+      comm->barrier();
+    }
+    out.setOutputToRootOnly(root);
+
+  } //UncoupledPhase3
 } // namespace MueLuTests
