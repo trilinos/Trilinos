7af854d3f3fca546b83e7fce69517489b83f881e
Mark Hoemmen <mhoemme@sandia.gov>
2015-04-16 09:58:06 -0600

    TpetraKernels: Fixed build warnings (un/signed comparisons).
    The build warnings (signed / unsigned comparisons) were in the
    front-end input validation for the new local sparse matrix-vector
    multiply.  The build warnings didn't show up in my build, but they did
    show up on the Dashboard.

--- packages/tpetra/kernels/src/Kokkos_Sparse.hpp
+++ packages/tpetra/kernels/src/Kokkos_Sparse.hpp
@@ -98,8 +98,8 @@ spmv(const char mode[],
   // Check compatibility of dimensions at run time.
   if((mode[0]==NoTranspose[0])||(mode[0]==Conjugate[0])) {
     if ((x.dimension_1 () != y.dimension_1 ()) ||
-        (A.numCols() > x.dimension_0()) ||
-        (A.numRows() > y.dimension_0()) ){
+        (static_cast<size_t> (A.numCols ()) > static_cast<size_t> (x.dimension_0 ())) ||
+        (static_cast<size_t> (A.numRows ()) > static_cast<size_t> (y.dimension_0 ()))) {
       std::ostringstream os;
       os << "KokkosBlas::spmv: Dimensions do not match: "
          << ", A: " << A.numRows () << " x " << A.numCols()
@@ -111,8 +111,8 @@ spmv(const char mode[],
     }
   } else {
     if ((x.dimension_1 () != y.dimension_1 ()) ||
-        (A.numCols() > y.dimension_0()) ||
-        (A.numRows() > x.dimension_0()) ){
+        (static_cast<size_t> (A.numCols ()) > static_cast<size_t> (y.dimension_0 ())) ||
+        (static_cast<size_t> (A.numRows ()) > static_cast<size_t> (x.dimension_0()))) {
       std::ostringstream os;
       os << "KokkosBlas::spmv: Dimensions do not match (transpose): "
          << ", A: " << A.numRows () << " x " << A.numCols()
