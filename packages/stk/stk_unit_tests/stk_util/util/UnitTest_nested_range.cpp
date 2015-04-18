d82f686fc5ce62dd0dfccba193383ec5e4514b48
Kendall Hugh Pierson <khpiers@sandia.gov>
2015-04-18 08:06:22 -0600

    stk: Replace BOOST_FOREACH with range-for
     from the C++11 standard.
    
    (From Sierra)

--- packages/stk/stk_unit_tests/stk_util/util/UnitTest_nested_range.cpp
+++ packages/stk/stk_unit_tests/stk_util/util/UnitTest_nested_range.cpp
@@ -37,7 +37,6 @@
 #include <stk_util/util/nested_range.hpp>  // for nested_range
 #include <utility>                      // for pair, make_pair
 #include <vector>                       // for vector
-#include "boost/optional/optional.hpp"  // for operator!=
 
 
 namespace vector_vector_int {
@@ -76,66 +75,11 @@ TEST ( nested_range, basic)
   }
 
   range rng(a);
-rng.begin();
-//  BOOST_FOREACH(int i, rng) {
-//   std::cout<<i<<std::endl;
-//  }
+  rng.begin();
 
   pair_range prng(ap);
-prng.begin();
-//  BOOST_FOREACH(int i, prng) {
-//   std::cout<<i<<std::endl;
-//  }
+  prng.begin();
 
 }
-
-//namespace vector_vector_int {
-//
-//struct to_inner_range {
-//  typedef std::vector<int> result_type;
-//  typedef std::vector<int>* value_type;
-//
-//  result_type& operator()(value_type& r) const { return *r; }
-//  const result_type& operator()(const value_type& r) const { return *r; }
-//};
-//
-//typedef std::vector< std::vector<int>* > nested_ptr_type;
-//
-//typedef stk::util::nested_range< nested_ptr_type, std::vector<int>, to_inner_range > ptr_range;
-//
-//}
-//
-//TEST ( nested_range, nested_ptr)
-//{
-//  using namespace vector_vector_int;
-//
-//  const int OUTER = 10;
-//  const int INNER = 10;
-//
-//  nested_ptr_type a(OUTER);
-//
-//  {
-//    int count = 0;
-//    for (int i=0; i<OUTER; ++i) {
-//      a[i] = new std::vector<int>(INNER);
-//      for (int j=0; j<INNER; ++j) {
-//        (*a[i])[j] = ++count;
-//      }
-//    }
-//  }
-//
-//  BOOST_FOREACH(std::vector<int>* vecptr,a) {
-//    std::vector<int>& vec = *vecptr;
-//    BOOST_FOREACH(int i,vec) {
-//      std::cout<<i<<std::endl;
-//    }
-//  }
-//
-//  ptr_range rng(a,to_inner_range());
-//  BOOST_FOREACH(int i, rng) {
-//   std::cout<<i<<std::endl;
-//  }
-//
-//}
 
 #endif
