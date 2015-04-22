8e2dc69f2cd56d9ed8b7df2ce856b91d1ac25c61
David Glaze <djglaze@sandia.gov>
2015-04-22 08:13:32 -0600

    Removed usage of AllocatorMemoryUsage
    This is no longer used by anyone and just adds unneeded complexity.
    
    (From Sierra)

--- packages/stk/stk_mesh/stk_mesh/base/Types.hpp
+++ packages/stk/stk_mesh/stk_mesh/base/Types.hpp
@@ -64,9 +64,6 @@ namespace stk { namespace mesh { template <typename DataType = void> class Prope
 namespace stk {
 namespace mesh {
 
-// Tags used by tracking allocator
-struct FieldDataTag {};
-
 //----------------------------------------------------------------------
 /** \addtogroup stk_mesh_module
  *  \{
