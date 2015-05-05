f59555519cca6fa97df0e4da7828cfbc2d068a48
Brent Perschbacher <bmpersc@sandia.gov>
2015-05-05 15:53:30 -0600

    Promote ROL to secondary tested
    At the request of the ROL team it is being added to nightly testing.

--- PackagesList.cmake
+++ PackagesList.cmake
@@ -109,7 +109,7 @@ TRIBITS_REPOSITORY_DEFINE_PACKAGES(
   MOOCHO                packages/moocho                   PS
   Aristos               packages/aristos                  EX
   Stokhos               packages/stokhos                  SS
-  ROL                   packages/rol                      EX
+  ROL                   packages/rol                      SS
   Piro                  packages/piro                     SS
   Panzer                packages/panzer                   SS
   Sundance              packages/Sundance                 SS # Could be PS based on deps (BUG: 4669)
