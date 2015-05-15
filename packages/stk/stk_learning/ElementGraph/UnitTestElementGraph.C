c74802e89e9745ef18abefb4ee5690d46e8f6ca6
Manoj K Bhardwaj <mkbhard@sandia.gov>
2015-05-15 16:13:14 -0600

    comparing skin_mesh using element graph and not using it.
    (From Sierra)

--- packages/stk/stk_learning/ElementGraph/UnitTestElementGraph.C
+++ packages/stk/stk_learning/ElementGraph/UnitTestElementGraph.C
@@ -1,15 +1,22 @@
 #include <gtest/gtest.h>
+
 #include <vector>
 #include <algorithm>
+
 #include <stk_topology/topology.hpp>
 #include <stk_mesh/base/BulkData.hpp>
 #include <stk_mesh/base/MetaData.hpp>
 #include <stk_mesh/base/Comm.hpp>
 #include <stk_mesh/base/GetEntities.hpp>
-
+#include <stk_mesh/base/FEMHelpers.hpp>
 #include <stk_mesh/baseImpl/MeshImplUtils.hpp>
+#include <stk_mesh/base/SkinMesh.hpp>
 
 #include <stk_util/parallel/Parallel.hpp>
+#include <stk_util/environment/WallTime.hpp>
+#include <stk_util/environment/memory_util.hpp>
+
+#include <stk_io/IossBridge.hpp>
 
 #include <stk_unit_test_utils/ioUtils.hpp>
 #include <stk_unit_test_utils/getOption.h>
@@ -21,15 +28,15 @@ int check_connectivity(const std::vector<std::vector<int64_t> >& elem_graph, con
 {
     int side=-1;
 
-    if(element_id1 >=1 && element_id1 <=3 && element_id2 >=1 && element_id2 <=3)
+    if(element_id1 >=0 && element_id1 <=2 && element_id2 >=0 && element_id2 <=2)
     {
-        const std::vector<int64_t>& conn_elements = elem_graph[element_id1-1];
+        const std::vector<int64_t>& conn_elements = elem_graph[element_id1];
 
         std::vector<int64_t>::const_iterator iter = std::find(conn_elements.begin(), conn_elements.end(), element_id2);
         if ( iter != conn_elements.end() )
         {
             int64_t index = iter - conn_elements.begin();
-            side = via_side[element_id1-1][index];
+            side = via_side[element_id1][index];
         }
     }
 
@@ -38,29 +45,29 @@ int check_connectivity(const std::vector<std::vector<int64_t> >& elem_graph, con
 
 TEST(ElementGraph, check_graph_connectivity)
 {
-    // element1 --> element2 --> element3
+    // element0 --> element1 --> element2
     std::vector<std::vector<int64_t> > elem_graph = {
-            {2},
-            {1,3},
-            {2}
+            {1},
+            {0,2},
+            {1}
     };
 
     std::vector<std::vector<int64_t> > via_side = {
-            {5},
-            {2,6},
-            {4}
+            {4},
+            {1,5},
+            {3}
     };
 
+    EXPECT_EQ(4, check_connectivity(elem_graph, via_side, 0, 1));
+    EXPECT_EQ(1, check_connectivity(elem_graph, via_side, 1, 0));
     EXPECT_EQ(5, check_connectivity(elem_graph, via_side, 1, 2));
-    EXPECT_EQ(2, check_connectivity(elem_graph, via_side, 2, 1));
-    EXPECT_EQ(6, check_connectivity(elem_graph, via_side, 2, 3));
-    EXPECT_EQ(4, check_connectivity(elem_graph, via_side, 3, 2));
+    EXPECT_EQ(3, check_connectivity(elem_graph, via_side, 2, 1));
 
-    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 1, 3));
-    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 3, 1));
-    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 4, 1));
-    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 1, 4));
-    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 1, 1));
+    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 0, 2));
+    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 2, 0));
+    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 3, 0));
+    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 0, 3));
+    EXPECT_EQ(-1, check_connectivity(elem_graph, via_side, 0, 0));
 }
 
 std::vector<std::pair<int64_t, int64_t> > skin_mesh(const std::vector<std::vector<int64_t> >& elem_graph, const std::vector<std::vector<int64_t> > &via_side,
@@ -79,16 +86,16 @@ std::vector<std::pair<int64_t, int64_t> > skin_mesh(const std::vector<std::vecto
         elem_sides.assign(num_sides, -1);
         for(size_t j=0; j<internal_sides.size(); ++j)
         {
-            int64_t zeroBasedSideId = internal_sides[j] - 1;
+            int64_t zeroBasedSideId = internal_sides[j];
             elem_sides[zeroBasedSideId] = internal_sides[j];
         }
 
-        int64_t elementId = i+1;
+        int64_t elementId = i;
         for(size_t j=0; j<num_sides; ++j)
         {
             if (elem_sides[j] == -1)
             {
-                int64_t sideId = j+1;
+                int64_t sideId = j;
                 element_side_pairs.push_back(std::make_pair(elementId, sideId));
             }
         }
@@ -98,17 +105,17 @@ std::vector<std::pair<int64_t, int64_t> > skin_mesh(const std::vector<std::vecto
 
 TEST(ElementGraph, skin_mesh_using_graph)
 {
-    // element1 --> element2 --> element3
+    // element0 --> element1 --> element2
     std::vector<std::vector<int64_t> > elem_graph = {
-            {2},
-            {1,3},
-            {2}
+            {1},
+            {0,2},
+            {1}
     };
 
     std::vector<std::vector<int64_t> > via_side = {
-            {5},
-            {2,6},
-            {4}
+            {4},
+            {1,5},
+            {3}
     };
 
     std::vector<stk::topology> element_topologies{
@@ -120,20 +127,20 @@ TEST(ElementGraph, skin_mesh_using_graph)
     std::vector<std::pair<int64_t, int64_t> > element_side_pairs = skin_mesh(elem_graph, via_side, element_topologies);
 
     std::vector<std::pair<int64_t,int64_t> >gold_element_side_pairs{
-        {1,1},
+        {0,0},
+        {0,1},
+        {0,2},
+        {0,3},
+        {0,5},
+        {1,0},
         {1,2},
         {1,3},
         {1,4},
-        {1,6},
+        {2,0},
         {2,1},
-        {2,3},
+        {2,2},
         {2,4},
-        {2,5},
-        {3,1},
-        {3,2},
-        {3,3},
-        {3,5},
-        {3,6}
+        {2,5}
     };
 
     ASSERT_EQ(gold_element_side_pairs.size(), element_side_pairs.size());
@@ -145,7 +152,7 @@ TEST(ElementGraph, skin_mesh_using_graph)
     }
 }
 
-void set_local_ids_and_fill_element_entities(stk::mesh::BulkData& bulkData, stk::mesh::EntityVector& local_id_to_element_entity)
+void set_local_ids_and_fill_element_entities_and_topologies(stk::mesh::BulkData& bulkData, stk::mesh::EntityVector& local_id_to_element_entity, std::vector<stk::topology>& element_topologies)
 {
     const stk::mesh::BucketVector& elemBuckets = bulkData.get_buckets(stk::topology::ELEM_RANK, bulkData.mesh_meta_data().locally_owned_part());
     size_t local_id = 0;
@@ -155,6 +162,7 @@ void set_local_ids_and_fill_element_entities(stk::mesh::BulkData& bulkData, stk:
         for(size_t j=0; j<bucket.size(); ++j)
         {
             local_id_to_element_entity[local_id] = bucket[j];
+            element_topologies[local_id] = bucket.topology();
             bulkData.set_local_id(bucket[j], local_id);
             local_id++;
         }
@@ -205,6 +213,13 @@ void fill_graph(stk::mesh::BulkData& bulkData, std::vector<std::vector<int64_t> 
 TEST(ElementGraph, test_graph_creation_using_stk_mesh)
 {
     MPI_Comm comm = MPI_COMM_WORLD;
+    std::vector<double> wall_times;
+    wall_times.reserve(10);
+
+    std::vector<size_t> mem_usage;
+
+    wall_times.push_back(stk::wall_time());
+    mem_usage.push_back(stk::get_memory_usage_now());
 
     if(stk::parallel_machine_size(comm) == 1)
     {
@@ -215,9 +230,14 @@ TEST(ElementGraph, test_graph_creation_using_stk_mesh)
         stk::mesh::BulkData bulkData(meta, comm);
         std::ostringstream os;
         os << "generated:1x1x" << zdim;
+        bool check_results = true;
+
         std::string filename = os.str();
 
         stk::unit_test_util::fill_mesh_using_stk_io(filename, bulkData, comm);
+
+        wall_times.push_back(stk::wall_time());
+        mem_usage.push_back(stk::get_memory_usage_now());
 
         std::vector<unsigned> counts;
         stk::mesh::count_entities(bulkData.mesh_meta_data().locally_owned_part(), bulkData, counts);
@@ -225,14 +245,20 @@ TEST(ElementGraph, test_graph_creation_using_stk_mesh)
         EXPECT_EQ(zdim, numElems);
 
         stk::mesh::EntityVector local_id_to_element_entity(numElems, 0);
-        set_local_ids_and_fill_element_entities(bulkData, local_id_to_element_entity);
+        std::vector<stk::topology> element_topologies(numElems);
+        set_local_ids_and_fill_element_entities_and_topologies(bulkData, local_id_to_element_entity, element_topologies);
         size_t expectedNumElems = counts[stk::topology::ELEM_RANK];
-        ASSERT_EQ(expectedNumElems, local_id_to_element_entity.size());
-        std::vector<stk::mesh::EntityId> element_global_ids(numElems);
-        for(int i=0; i<numElems; ++i)
+
+        if ( check_results )
         {
-            stk::mesh::EntityId expectedId = static_cast<stk::mesh::EntityId>(i+1);
-            EXPECT_EQ(expectedId, bulkData.identifier(local_id_to_element_entity[i])) << "elem id for i=" << i << ": "<< bulkData.identifier(local_id_to_element_entity[i]);
+            ASSERT_EQ(expectedNumElems, local_id_to_element_entity.size());
+
+            std::vector<stk::mesh::EntityId> element_global_ids(numElems);
+            for(int i=0; i<numElems; ++i)
+            {
+                stk::mesh::EntityId expectedId = static_cast<stk::mesh::EntityId>(i+1);
+                EXPECT_EQ(expectedId, bulkData.identifier(local_id_to_element_entity[i])) << "elem id for i=" << i << ": "<< bulkData.identifier(local_id_to_element_entity[i]);
+            }
         }
 
         std::vector<std::vector<int64_t> > elem_graph(numElems);
@@ -240,36 +266,230 @@ TEST(ElementGraph, test_graph_creation_using_stk_mesh)
 
         fill_graph(bulkData, elem_graph, via_sides);
 
-        int64_t left_side_id = 4;
-        int64_t right_side_id = 5;
+        wall_times.push_back(stk::wall_time());
+        mem_usage.push_back(stk::get_memory_usage_now());
 
-        for(size_t i=0; i<elem_graph.size(); ++i)
+        if ( check_results )
         {
-            const std::vector<int64_t>& conn_elements = elem_graph[i];
-            if (i == 0)
+            int64_t left_side_id = 4;
+            int64_t right_side_id = 5;
+
+            for(size_t i=0; i<elem_graph.size(); ++i)
             {
-                ASSERT_EQ(1u, conn_elements.size());
-                EXPECT_EQ(1, conn_elements[0]);
-                EXPECT_EQ(right_side_id, via_sides[i][0]);
-            }
-            else if (i == elem_graph.size() - 1)
-            {
-                int64_t second_to_last_element_index = elem_graph.size() - 2;
-                ASSERT_EQ(1u, conn_elements.size());
-                EXPECT_EQ(second_to_last_element_index, conn_elements[0]);
-                EXPECT_EQ(left_side_id, via_sides[i][0]);
-            }
-            else
-            {
-                ASSERT_EQ(2u, conn_elements.size());
-                int64_t element_to_the_left = i-1;
-                int64_t element_to_the_right = i+1;
-                EXPECT_EQ(element_to_the_left, conn_elements[0]);
-                EXPECT_EQ(element_to_the_right, conn_elements[1]);
-                EXPECT_EQ(left_side_id, via_sides[i][0]);
-                EXPECT_EQ(right_side_id, via_sides[i][1]);
+                const std::vector<int64_t>& conn_elements = elem_graph[i];
+                if (i == 0)
+                {
+                    ASSERT_EQ(1u, conn_elements.size());
+                    EXPECT_EQ(1, conn_elements[0]);
+                    EXPECT_EQ(right_side_id, via_sides[i][0]);
+                }
+                else if (i == elem_graph.size() - 1)
+                {
+                    int64_t second_to_last_element_index = elem_graph.size() - 2;
+                    ASSERT_EQ(1u, conn_elements.size());
+                    EXPECT_EQ(second_to_last_element_index, conn_elements[0]);
+                    EXPECT_EQ(left_side_id, via_sides[i][0]);
+                }
+                else
+                {
+                    ASSERT_EQ(2u, conn_elements.size());
+                    int64_t element_to_the_left = i-1;
+                    int64_t element_to_the_right = i+1;
+                    EXPECT_EQ(element_to_the_left, conn_elements[0]);
+                    EXPECT_EQ(element_to_the_right, conn_elements[1]);
+                    EXPECT_EQ(left_side_id, via_sides[i][0]);
+                    EXPECT_EQ(right_side_id, via_sides[i][1]);
+                }
             }
         }
+    }
+
+    for(size_t i=0;i<wall_times.size();++i)
+    {
+        std::cerr << "Wall time: " << i << "\t" << wall_times[i] - wall_times[0] << std::endl;
+    }
+
+    for(size_t i=0;i<mem_usage.size();++i)
+     {
+         std::cerr << "Memory usage: " << i << "\t" << mem_usage[i] - mem_usage[0] << std::endl;
+     }
+}
+
+TEST(ElementGraph, test_create_skin_for_stk_mesh)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    std::vector<double> wall_times;
+    wall_times.reserve(10);
+
+    std::vector<size_t> mem_usage;
+
+    wall_times.push_back(stk::wall_time());
+    mem_usage.push_back(stk::get_memory_usage_now());
+
+    if(stk::parallel_machine_size(comm) == 1)
+    {
+        std::string dimension = unitTestUtils::getOption("--zdim", "3");
+        const int zdim = std::atoi(dimension.c_str());
+
+        unsigned spatialDim = 3;
+        stk::mesh::MetaData meta(spatialDim);
+        stk::mesh::Part& skin_part = meta.declare_part_with_topology("surface_5", stk::topology::QUAD_4);
+        stk::io::put_io_part_attribute(skin_part);
+        stk::mesh::BulkData bulkData(meta, comm);
+        std::ostringstream os;
+        os << "generated:" << zdim << "x" << zdim << "x" << zdim;
+        bool check_results = false;
+
+        std::string filename = os.str();
+
+        stk::unit_test_util::fill_mesh_using_stk_io(filename, bulkData, comm);
+
+        wall_times.push_back(stk::wall_time());
+        mem_usage.push_back(stk::get_memory_usage_now());
+
+        std::vector<unsigned> counts;
+        stk::mesh::count_entities(bulkData.mesh_meta_data().locally_owned_part(), bulkData, counts);
+        int numElems = counts[stk::topology::ELEM_RANK];
+        if ( check_results )
+        {
+            EXPECT_EQ(zdim, numElems);
+        }
+
+        stk::mesh::EntityVector local_id_to_element_entity(numElems, 0);
+        std::vector<stk::topology> element_topologies(numElems);
+        set_local_ids_and_fill_element_entities_and_topologies(bulkData, local_id_to_element_entity, element_topologies);
+        size_t expectedNumElems = counts[stk::topology::ELEM_RANK];
+
+        if ( check_results )
+        {
+            ASSERT_EQ(expectedNumElems, local_id_to_element_entity.size());
+
+            std::vector<stk::mesh::EntityId> element_global_ids(numElems);
+            for(int i=0; i<numElems; ++i)
+            {
+                stk::mesh::EntityId expectedId = static_cast<stk::mesh::EntityId>(i+1);
+                EXPECT_EQ(expectedId, bulkData.identifier(local_id_to_element_entity[i])) << "elem id for i=" << i << ": "<< bulkData.identifier(local_id_to_element_entity[i]);
+            }
+        }
+
+        std::vector<std::vector<int64_t> > elem_graph(numElems);
+        std::vector<std::vector<int64_t> > via_sides(numElems);
+
+        fill_graph(bulkData, elem_graph, via_sides);
+
+        wall_times.push_back(stk::wall_time());
+        mem_usage.push_back(stk::get_memory_usage_now());
+
+        std::vector<std::pair<int64_t, int64_t> > elem_side_pairs = skin_mesh(elem_graph, via_sides, element_topologies);
+
+        bulkData.modification_begin();
+
+        for(size_t face_index=0; face_index<elem_side_pairs.size(); ++face_index)
+        {
+            stk::mesh::Entity element = local_id_to_element_entity[elem_side_pairs[face_index].first];
+            stk::mesh::EntityId face_global_id = face_index + 1;
+            stk::mesh::declare_element_side(bulkData, face_global_id, element, elem_side_pairs[face_index].second, &skin_part);
+        }
+
+        bulkData.modification_end_for_entity_creation(stk::topology::FACE_RANK);
+
+        wall_times.push_back(stk::wall_time());
+        mem_usage.push_back(stk::get_memory_usage_now());
+
+        stk::unit_test_util::write_mesh_using_stk_io("out.exo", bulkData, bulkData.parallel());
+
+        if ( check_results )
+        {
+            int64_t left_side_id = 4;
+            int64_t right_side_id = 5;
+
+            for(size_t i=0; i<elem_graph.size(); ++i)
+            {
+                const std::vector<int64_t>& conn_elements = elem_graph[i];
+                if (i == 0)
+                {
+                    ASSERT_EQ(1u, conn_elements.size());
+                    EXPECT_EQ(1, conn_elements[0]);
+                    EXPECT_EQ(right_side_id, via_sides[i][0]);
+                }
+                else if (i == elem_graph.size() - 1)
+                {
+                    int64_t second_to_last_element_index = elem_graph.size() - 2;
+                    ASSERT_EQ(1u, conn_elements.size());
+                    EXPECT_EQ(second_to_last_element_index, conn_elements[0]);
+                    EXPECT_EQ(left_side_id, via_sides[i][0]);
+                }
+                else
+                {
+                    ASSERT_EQ(2u, conn_elements.size());
+                    int64_t element_to_the_left = i-1;
+                    int64_t element_to_the_right = i+1;
+                    EXPECT_EQ(element_to_the_left, conn_elements[0]);
+                    EXPECT_EQ(element_to_the_right, conn_elements[1]);
+                    EXPECT_EQ(left_side_id, via_sides[i][0]);
+                    EXPECT_EQ(right_side_id, via_sides[i][1]);
+                }
+            }
+        }
+    }
+
+    for(size_t i=0;i<wall_times.size();++i)
+    {
+        std::cerr << "Wall time: " << i << "\t" << wall_times[i] - wall_times[0] << std::endl;
+    }
+
+    for(size_t i=0;i<mem_usage.size();++i)
+     {
+         std::cerr << "Memory usage: " << i << "\t" << mem_usage[i] - mem_usage[0] << std::endl;
+     }
+}
+
+TEST(ElementGraph, test_skin_mesh)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    std::vector<double> wall_times;
+    wall_times.reserve(10);
+
+    std::vector<size_t> mem_usage;
+
+    wall_times.push_back(stk::wall_time());
+    mem_usage.push_back(stk::get_memory_usage_now());
+
+    if(stk::parallel_machine_size(comm) == 1)
+    {
+        std::string dimension = unitTestUtils::getOption("--zdim", "3");
+        const int zdim = std::atoi(dimension.c_str());
+
+        unsigned spatialDim = 3;
+        stk::mesh::MetaData meta(spatialDim);
+        stk::mesh::Part& skin_part = meta.declare_part_with_topology("surface_5", stk::topology::QUAD_4);
+        stk::io::put_io_part_attribute(skin_part);
+        stk::mesh::BulkData bulkData(meta, comm);
+        std::ostringstream os;
+
+        os << "generated:" << zdim << "x" << zdim << "x" << zdim;
+
+        std::string filename = os.str();
+        stk::unit_test_util::fill_mesh_using_stk_io(filename, bulkData, comm);
+
+        wall_times.push_back(stk::wall_time());
+        mem_usage.push_back(stk::get_memory_usage_now());
+
+        stk::mesh::PartVector parts(1, &skin_part);
+        stk::mesh::skin_mesh(bulkData, parts);
+
+        wall_times.push_back(stk::wall_time());
+        mem_usage.push_back(stk::get_memory_usage_now());
+    }
+
+    for(size_t i = 0; i < wall_times.size(); ++i)
+    {
+        std::cerr << "Wall time: " << i << "\t" << wall_times[i] - wall_times[0] << std::endl;
+    }
+
+    for(size_t i = 0; i < mem_usage.size(); ++i)
+    {
+        std::cerr << "Memory usage: " << i << "\t" << mem_usage[i] - mem_usage[0] << std::endl;
     }
 }
 
