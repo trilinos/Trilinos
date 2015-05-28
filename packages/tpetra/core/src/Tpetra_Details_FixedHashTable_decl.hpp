11474a4c2b477800bd9f5b5b21999235dc51e80a
Mark Hoemmen <mhoemme@sandia.gov>
2015-05-28 17:27:05 +0800

    Tpetra: Candidate fix for FixedHashTable CUDA build error.
    My previous commit explained the cause for CUDA build errors with
    Tpetra::Details::FixedHashTable.  This commit addresses those build
    errors.  It also includes a new feature: a run-time check for overflow
    when copying the arrays of offsets.  (This matters because different
    memory spaces may have different offset types.)

--- packages/tpetra/core/src/Tpetra_Details_FixedHashTable_decl.hpp
+++ packages/tpetra/core/src/Tpetra_Details_FixedHashTable_decl.hpp
@@ -51,6 +51,138 @@
 namespace Tpetra {
 namespace Details {
 
+//
+// Implementation details for FixedHashTable (see below).
+// Users should skip over this anonymous namespace.
+//
+namespace { // (anonymous)
+
+  // Overflow is impossible (the output can fit the input) if the
+  // output type is bigger than the input type, or if the types have
+  // the same size and (the output type is unsigned, or both types are
+  // signed).
+  //
+  // Implicit here is the assumption that both input and output types
+  // are integers.
+  template<class T1, class T2,
+           const bool T1_is_signed = std::is_signed<T1>::value,
+           const bool T2_is_signed = std::is_signed<T2>::value>
+  struct OutputCanFitInput {
+    static const bool value = sizeof (T1) > sizeof (T2) ||
+      (sizeof (T1) == sizeof (T2) &&
+       (std::is_unsigned<T1>::value || (std::is_signed<T1>::value && std::is_signed<T2>::value)));
+  };
+
+  // Kokkos parallel_reduce functor for copying offset ("ptr") arrays.
+  // Tpetra::Details::FixedHashTable uses this in its "copy"
+  // constructor for converting between different Device types.  All
+  // the action happens in the partial specializations for different
+  // values of outputCanFitInput.
+  template<class OutputViewType, class InputViewType,
+           const bool outputCanFitInput = OutputCanFitInput<typename OutputViewType::non_const_value_type,
+                                                            typename InputViewType::non_const_value_type>::value>
+  class CopyOffsets {};
+
+  // Specialization for when overflow is possible.
+  template<class OutputViewType, class InputViewType>
+  class CopyOffsets<OutputViewType, InputViewType, false> {
+  public:
+    typedef typename OutputViewType::execution_space execution_space;
+    typedef typename OutputViewType::size_type size_type;
+    typedef bool value_type;
+
+    typedef typename InputViewType::non_const_value_type input_value_type;
+    typedef typename OutputViewType::non_const_value_type output_value_type;
+
+    CopyOffsets (const OutputViewType& dst, const InputViewType& src) :
+      dst_ (dst),
+      src_ (src),
+      // We know that output_value_type cannot fit all values of
+      // input_value_type, so an input_value_type can fit all values
+      // of output_value_type.  This means we can convert from
+      // output_value_type to input_value_type.  This is how we test
+      // whether a given input_value_type value can fit in an
+      // output_value_type.
+      minDstVal_ (static_cast<input_value_type> (std::numeric_limits<output_value_type>::min ())),
+      maxDstVal_ (static_cast<input_value_type> (std::numeric_limits<output_value_type>::max ()))
+    {}
+
+    KOKKOS_INLINE_FUNCTION void
+    operator () (const size_type& i, value_type& noOverflow) const {
+      const input_value_type src_i = src_(i);
+      if (src_i < minDstVal_ || src_i > maxDstVal_) {
+        noOverflow = false;
+      }
+      dst_(i) = static_cast<output_value_type> (src_i);
+    }
+
+    KOKKOS_INLINE_FUNCTION void init (value_type& noOverflow) const {
+      noOverflow = true; // success (no overflow)
+    }
+
+    KOKKOS_INLINE_FUNCTION void
+    join (volatile value_type& result,
+          const volatile value_type& current) const {
+      result = result && current; // was there any overflow?
+    }
+
+  private:
+    OutputViewType dst_;
+    InputViewType src_;
+    input_value_type minDstVal_;
+    input_value_type maxDstVal_;
+  };
+
+  // Specialization for when overflow is impossible.
+  template<class OutputViewType, class InputViewType>
+  class CopyOffsets<OutputViewType, InputViewType, true> {
+  public:
+    typedef typename OutputViewType::execution_space execution_space;
+    typedef typename OutputViewType::size_type size_type;
+    typedef bool value_type;
+
+    CopyOffsets (const OutputViewType& dst, const InputViewType& src) :
+      dst_ (dst),
+      src_ (src)
+    {}
+
+    KOKKOS_INLINE_FUNCTION void
+    operator () (const size_type& i, value_type& /* noOverflow */) const {
+      // Overflow is impossible in this case, so there's no need to check.
+      dst_(i) = src_(i);
+    }
+
+    KOKKOS_INLINE_FUNCTION void init (value_type& noOverflow) const {
+      noOverflow = true; // success (no overflow)
+    }
+
+    KOKKOS_INLINE_FUNCTION void
+    join (volatile value_type& result,
+          const volatile value_type& current) const {
+      result = result && current; // was there any overflow?
+    }
+
+  private:
+    OutputViewType dst_;
+    InputViewType src_;
+  };
+
+  template<class OutputViewType, class InputViewType>
+  void copyOffsets (const OutputViewType& dst, const InputViewType& src) {
+    TEUCHOS_TEST_FOR_EXCEPTION
+      (dst.dimension_0 () != src.dimension_0 (), std::invalid_argument,
+       "copyOffsets: dst.dimension_0() = " << dst.dimension_0 ()
+       << " != src.dimension_0() = " << src.dimension_0 () << ".");
+    typedef CopyOffsets<OutputViewType, InputViewType> functor_type;
+    bool noOverflow = false;
+    Kokkos::parallel_reduce (dst.dimension_0 (), functor_type (dst, src), noOverflow);
+    TEUCHOS_TEST_FOR_EXCEPTION
+      (! noOverflow, std::runtime_error, "copyOffsets: One or more values in "
+       "src were too big (in the sense of integer overflow) to fit in dst.");
+  }
+
+} // namespace (anonymous)
+
 /// \class FixedHashTable
 /// \tparam KeyType The type of the hash table's keys.  This must be a
 ///   built-in signed or unsigned integer type.
@@ -142,8 +274,7 @@ public:
   ///   same KeyType and ValueType, but a different DeviceType.
   ///
   /// This constructor makes a deep copy of the input's data if
-  /// necessary.  Anything that it doesn't need to deep copy, it
-  /// shallow copies.
+  /// necessary.
   template<class InDeviceType>
   FixedHashTable (const FixedHashTable<KeyType, ValueType, InDeviceType>& src,
                   typename std::enable_if<! std::is_same<DeviceType, InDeviceType>::value, int>::type* = NULL)
@@ -152,19 +283,21 @@ public:
     typedef typename ptr_type::non_const_type nonconst_ptr_type;
     typedef typename val_type::non_const_type nonconst_val_type;
 
-    // FIXME (mfh 28 May 2015) Christian pointed out that different
-    // Devices may have different offset_type, because offset_type
-    // comes from the memory space's size_type typedef.  The proper
-    // fix is to write a deep copy functor.
+    // FIXME (mfh 28 May 2015) The code below _always_ copies.  This
+    // shouldn't be necessary if the input and output memory spaces
+    // are the same.  However, it is always correct.
+
+    // Different Devices may have different offset_type, because
+    // offset_type comes from the memory space's size_type typedef.
+    // That's why we use a specialized deep copy function here instead
+    // of Kokkos::deep_copy.
     nonconst_ptr_type ptr (ViewAllocateWithoutInitializing ("ptr"),
                            src.ptr_.dimension_0 ());
-
-    // NOTE (mfh 01 May 2015) deep_copy works here, because regardless
-    // of the DeviceType, all FixedHashTable types use the same array
-    // layout for their internal 1-D Views.
-    Kokkos::deep_copy (ptr, src.ptr_);
+    copyOffsets (ptr, src.ptr_);
     nonconst_val_type val (ViewAllocateWithoutInitializing ("val"),
                            src.val_.dimension_0 ());
+    // val and src.val_ have the same entry types, unlike (possibly)
+    // ptr and src.ptr_.  Thus, we can use Kokkos::deep_copy here.
     Kokkos::deep_copy (val, src.val_);
 
     this->ptr_ = ptr;
