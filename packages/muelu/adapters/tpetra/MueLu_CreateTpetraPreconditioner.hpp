70329d47e590d88e70619406dea174b1c82eece9
Emily Furst <eafurst@sandia.gov>
2015-06-17 16:45:17 +0000

    MueLu: added check for correct file type when creating TpetraPreconditioner.

--- packages/muelu/adapters/tpetra/MueLu_CreateTpetraPreconditioner.hpp
+++ packages/muelu/adapters/tpetra/MueLu_CreateTpetraPreconditioner.hpp
@@ -167,10 +167,13 @@ namespace MueLu {
   template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
   Teuchos::RCP<MueLu::TpetraOperator<Scalar,LocalOrdinal,GlobalOrdinal,Node> >
   CreateTpetraPreconditioner(const Teuchos::RCP<Tpetra::CrsMatrix  <Scalar, LocalOrdinal, GlobalOrdinal, Node> >& inA,
-                             const std::string& fileName,
+                             const std::string& XMLFileName,
                              const Teuchos::RCP<Tpetra::MultiVector<double, LocalOrdinal, GlobalOrdinal, Node> >& inCoords    = Teuchos::null,
                              const Teuchos::RCP<Tpetra::MultiVector<Scalar, LocalOrdinal, GlobalOrdinal, Node> >& inNullspace = Teuchos::null)
   {
+    if(XMLFileName.find("xml")==std::string::npos){
+      throw Exceptions::RuntimeError("file needs to be of type XML");
+    }
     Teuchos::ParameterList paramList;
     Teuchos::updateParametersFromXmlFileAndBroadcast(xmlFileName, Teuchos::Ptr<Teuchos::ParameterList>(&paramList), *inA->getComm());
 
@@ -196,7 +199,7 @@ namespace MueLu {
      /*need to transform size_t  into int*/
      inA->getAllValues(row_ptr, col_ind, data);
 
-    if(JSONFileName != NULL){
+    if(JSONFileName != NULL && JSONFileName.find("json") != std::string::npos){
        AMGX_SAFE_CALL(AMGX_config_create_from_file(&cfg, JSONFileName));
     }
     else{
