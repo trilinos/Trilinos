b4e9ebb1dfb8edacb2caaa26fa2ba0e383ab76db
Emily Furst <eafurst@sandia.gov>
2015-06-17 16:45:17 +0000

    MueLu: added check for correct file type when creating TpetraPreconditioner.

--- packages/muelu/adapters/tpetra/MueLu_CreateTpetraPreconditioner.hpp
+++ packages/muelu/adapters/tpetra/MueLu_CreateTpetraPreconditioner.hpp
@@ -155,10 +155,13 @@ namespace MueLu {
   template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
   Teuchos::RCP<MueLu::TpetraOperator<Scalar,LocalOrdinal,GlobalOrdinal,Node> >
   CreateTpetraPreconditioner(const Teuchos::RCP<Tpetra::CrsMatrix  <Scalar, LocalOrdinal, GlobalOrdinal, Node> >& inA,
-                             const std::string& xmlFileName,
+                             const std::string& XMLFileName,
                              const Teuchos::RCP<Tpetra::MultiVector<double, LocalOrdinal, GlobalOrdinal, Node> >& inCoords    = Teuchos::null,
                              const Teuchos::RCP<Tpetra::MultiVector<Scalar, LocalOrdinal, GlobalOrdinal, Node> >& inNullspace = Teuchos::null)
   {
+    if(XMLFileName.find("xml") == std::string::npos){
+      throw Exceptions::RuntimeError("file needs to be of type XML");
+    }
     Teuchos::ParameterList paramList;
     Teuchos::updateParametersFromXmlFileAndBroadcast(xmlFileName, Teuchos::Ptr<Teuchos::ParameterList>(&paramList), *inA->getComm());
 
