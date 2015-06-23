9c4a0b305b0e896e8e2300c60dbc3492d4f917b8
Eric C. Cyr <eccyr@sandia.gov>
2015-06-23 14:32:22 -0600

    Piro: Adding get_g_names to inverse mass matrix ME decorator
    This was recently added to the model evaluator, it was defaulted
    in some places but not others.

--- packages/piro/src/Piro_InvertMassMatrixDecorator_Def.hpp
+++ packages/piro/src/Piro_InvertMassMatrixDecorator_Def.hpp
@@ -154,6 +154,13 @@ Piro::InvertMassMatrixDecorator<Scalar>::get_g_space(int j) const
 }
 
 template<typename Scalar>
+Teuchos::RCP<const Teuchos::ArrayView<std::string> > 
+Piro::InvertMassMatrixDecorator<Scalar>::get_g_names(int j) const
+{
+  return model->get_g_names(j);
+}
+
+template<typename Scalar>
 Thyra::ModelEvaluatorBase::InArgs<Scalar>
 Piro::InvertMassMatrixDecorator<Scalar>::getNominalValues() const
 {
