38f522d7d4c96af6d54a91c79a2f1b50ad50a960
bddavid <davidson@txcorp.com>
2015-07-02 19:22:51 -0600

    Initial work on test driver
    - CMakeLists updated to build executable
    - driver doesn't do much yet, just compose property lists from file.

--- packages/zoltan2/test/driver/test_driver.cpp
+++ packages/zoltan2/test/driver/test_driver.cpp
@@ -43,8 +43,92 @@
 //
 // @HEADER
 
+// taking headers from existing driver template
+// will keep or remove as needed
+//#include <Zoltan2_TestHelpers.hpp>
+//#include <Zoltan2_Parameters.hpp>
+//#include <Zoltan2_PartitioningProblem.hpp>
+//#include <Zoltan2_PartitioningSolutionQuality.hpp>
+//#include <Zoltan2_BasicIdentifierAdapter.hpp>
+//#include <Zoltan2_BasicVectorAdapter.hpp>
+//#include <Zoltan2_XpetraCrsGraphAdapter.hpp>
+//#include <Zoltan2_XpetraCrsMatrixAdapter.hpp>
+//#include <Zoltan2_XpetraMultiVectorAdapter.hpp>
+//
+//#include <Teuchos_DefaultComm.hpp>
+//#include <Teuchos_XMLObject.hpp>
+//#include <Teuchos_FileInputSource.hpp>
+//
+//#include <Tpetra_MultiVector.hpp>
+//#include <Tpetra_CrsMatrix.hpp>
+
+#include <sstream>
+#include <string>
+#include <iostream>
+#include <vector>
+
+//using Teuchos::ParameterList;
+//using Teuchos::Comm;
+//using Teuchos::RCP;
+//using Teuchos::ArrayRCP;
+//using Teuchos::XMLObject;
+
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::exception;
+using std::ostringstream;
+using std::vector;
+
+#define ERRMSG(msg) if (rank == 0){ cerr << "FAIL: " << msg << endl; }
+#define EXC_ERRMSG(msg, e) \
+if (rank==0){ cerr << "FAIL: " << msg << endl << e.what() << endl;}
+
 int main(int argc, char *argv[])
 {
+//------------------------------------------------>>
+// (0) Set up MPI environment
+//------------------------------------------------>>
+//    Teuchos::GlobalMPISession session(&argc, &argv);
+//    RCP<const Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
+//    
+//    int rank = comm->getRank(); // get rank
+//    if(rank == 0){
+//        cout << "PASS" << endl;
+//        return 0;
+//    }
+//------------------------------------------------>>
+// (1) Get and read the input file
+// the input file defines tests to be run
+//------------------------------------------------>>
+//string inputFileName("driver.xml"); // assumes a default input file exists
+//if(argc > 1)
+//    inputFileName = argv[1]; // user has provided an input file
+//
+//Teuchos::FileInputSource inputFile(inputFileName);
+//XMLObject xmlInput;
+//
+//// Try to get xmlObject from inputfile
+//try{
+//    xmlInput = inputFile.getObject();
+//}
+//catch(exception &e)
+//{
+////        EXC_ERRMSG("Test Driver error: reading", e); // error reading input
+//}
 
-    return 0;
+
+//------------------------------------------------>>
+// (2) Get list of valid Zoltan2 Parameters
+//------------------------------------------------>>
+//    Teuchos::ParameterList zoltan2Parameters;
+//    Zoltan2::createAllParameters(zoltan2Parameters);
+
+//------------------------------------------------>>
+// (3) Loop over all tests and execute them
+//------------------------------------------------>>
+//
+
+return 0;
 }
