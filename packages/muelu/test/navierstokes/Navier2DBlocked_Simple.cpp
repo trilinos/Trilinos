7f3dab4d9836f802610b0f3169129299a38215a1
Andrey Prokopenko <aprokop@sandia.gov>
2015-07-15 13:37:17 -0600

    MueLu: shrinked the matrix data files for Navier-Stokes tests

--- packages/muelu/test/navierstokes/Navier2DBlocked_Simple.cpp
+++ packages/muelu/test/navierstokes/Navier2DBlocked_Simple.cpp
@@ -159,7 +159,7 @@ int main(int argc, char *argv[]) {
 
     GlobalOrdinal maxCoarseSize=1; //FIXME clp doesn't like long long int
 
-    int globalNumDofs = 8898;  // used for the maps
+    int globalNumDofs = 1500;  // used for the maps
     int nDofsPerNode = 3;      // used for generating the fine level null-space
 
     // build strided maps
