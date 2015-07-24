0f9bbb71476c972db25aca043ea13327f17f40f3
Bill Spotz <wfspotz@sandia.gov>
2015-07-24 16:36:36 -0600

    Domi: Added get methods for Epetra_IntVectors
    Added methods getEpetraIntVectorView() and getEpetraIntVectorCopy().
    These were needed by the new DAP typemaps in PyTrilinos for converting
    python objects that support the DistArray Protocol to
    Epetra_IntVectors.

--- packages/domi/src/Domi_MDVector.hpp
+++ packages/domi/src/Domi_MDVector.hpp
@@ -62,6 +62,7 @@
 #include "Teuchos_CommHelpers.hpp"
 
 #ifdef HAVE_EPETRA
+#include "Epetra_IntVector.h"
 #include "Epetra_Vector.h"
 #endif
 
@@ -597,6 +598,23 @@ public:
 
 #ifdef HAVE_EPETRA
 
+  /** \brief Return a view of this MDVector as an Epetra_IntVector
+   *
+   * The multiple dimensions of the MDVector will be flattened in
+   * order to be expressed as an Epetra_IntVector.  Currently, if the
+   * MDVector is non-contiguous, a Domi::MDMapNoncontiguous exception
+   * will be thrown, as Epetra_IntVectors are contiguous, and this is
+   * a view transform.  Non-contiguous data is the result of slicing a
+   * parent MDVector.  In this case, getEpetraIntVectorView() should
+   * be called on the parent, or getEpetraIntVectorCopy() should be
+   * called on this non-contiguous MDVector.
+   *
+   * The MDVector Scalar template must be the same type as an
+   * Epetra_IntVector, i.e. int, or a Domi::TypeError exception will
+   * be thrown.
+   */
+  Teuchos::RCP< Epetra_IntVector > getEpetraIntVectorView() const;
+
   /** \brief Return a view of this MDVector as an Epetra_Vector
    *
    * The multiple dimensions of the MDVector will be flattened in
@@ -635,6 +653,17 @@ public:
    * will be thrown.
    */
   Teuchos::RCP< Epetra_MultiVector > getEpetraMultiVectorView() const;
+
+  /** \brief Return a copy of this MDVector as an Epetra_IntVector
+   *
+   * The multiple dimensions of the MDVector will be flattened in
+   * order to be expressed as an Epetra_IntVector.
+   *
+   * The MDVector Scalar template must be the same type as an
+   * Epetra_IntVector, i.e. int, or a Domi::TypeError exception will
+   * be thrown.
+   */
+  Teuchos::RCP< Epetra_IntVector > getEpetraIntVectorCopy() const;
 
   /** \brief Return a copy of this MDVector as an Epetra_Vector
    *
@@ -1956,6 +1985,48 @@ isContiguous() const
 
 #ifdef HAVE_EPETRA
 
+// The getEpetraIntVectorView() method only makes sense for Scalar =
+// int, because Epetra_IntVectors store data buffers of type int only.
+// There is no convenient way to specialize just one (or a small
+// handfull of) methods, instead you have to specialize the whole
+// class.  So we allow getEpetraIntVectorView() to compile for any
+// Scalar, but we will throw an exception if Scalar is not int.
+
+template< class Scalar,
+          class Node >
+Teuchos::RCP< Epetra_IntVector >
+MDVector< Scalar, Node >::
+getEpetraIntVectorView() const
+{
+  // Throw an exception if Scalar is not int
+  const char * scalarType = typeid(Scalar).name();
+  TEUCHOS_TEST_FOR_EXCEPTION(
+    strncmp(scalarType, "int", 3) != 0,
+    TypeError,
+    "MDVector is of scalar type '" << scalarType << "', but Epetra_IntVector "
+    "requires scalar type 'int'");
+
+  // Throw an exception if this MDVector's MDMap is not contiguous
+  TEUCHOS_TEST_FOR_EXCEPTION(
+    !isContiguous(),
+    MDMapNoncontiguousError,
+    "This MDVector's MDMap is non-contiguous.  This can happen when you take "
+    "a slice of a parent MDVector.");
+
+  // Get the Epetra_Map that is equivalent to this MDVector's MDMap
+  Teuchos::RCP< const Epetra_Map > epetraMap = _mdMap->getEpetraMap(true);
+
+  // Return the result.  We are changing a Scalar* to a double*, which
+  // looks sketchy, but we have thrown an exception if Sca is not
+  // double, so everything is kosher.
+  void * buffer = (void*) _mdArrayView.getRawPtr();
+  return Teuchos::rcp(new Epetra_IntVector(View,
+                                           *epetraMap,
+                                           (int*) buffer));
+}
+
+////////////////////////////////////////////////////////////////////////
+
 // The getEpetraVectorView() method only makes sense for Scalar =
 // double, because Epetra_Vectors store data buffers of type double
 // only.  There is no convenient way to specialize just one (or a
@@ -1987,7 +2058,7 @@ getEpetraVectorView() const
   // Get the Epetra_Map that is equivalent to this MDVector's MDMap
   Teuchos::RCP< const Epetra_Map > epetraMap = _mdMap->getEpetraMap(true);
 
-  // Return the result.  We are changing a Sca* to a double*, which
+  // Return the result.  We are changing a Scalar* to a double*, which
   // looks sketchy, but we have thrown an exception if Sca is not
   // double, so everything is kosher.
   void * buffer = (void*) _mdArrayView.getRawPtr();
@@ -1996,11 +2067,7 @@ getEpetraVectorView() const
                                         (double*) buffer));
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_EPETRA
 
 // The getEpetraMultiVectorView() method only makes sense for Scalar =
 // double, because Epetra_MultiVectors store data buffers of type
@@ -2060,7 +2127,7 @@ getEpetraMultiVectorView() const
   // Get the Epetra_Map that is equivalent to newMdMap
   Teuchos::RCP< const Epetra_Map > epetraMap = newMdMap->getEpetraMap(true);
 
-  // Return the result.  We are changing a Sca* to a double*, which
+  // Return the result.  We are changing a Scalar* to a double*, which
   // looks sketchy, but we have thrown an exception if Sca is not
   // double, so everything is kosher.
   void * buffer = (void*) _mdArrayView.getRawPtr();
@@ -2071,11 +2138,34 @@ getEpetraMultiVectorView() const
                                              numVectors));
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
 
-#ifdef HAVE_EPETRA
+template< class Scalar,
+          class Node >
+Teuchos::RCP< Epetra_IntVector >
+MDVector< Scalar, Node >::
+getEpetraIntVectorCopy() const
+{
+  typedef typename MDArrayView< const Scalar >::iterator iterator;
+
+  // Get the Epetra_Map that is equivalent to this MDVector's MDMap
+  Teuchos::RCP< const Epetra_Map > epetraMap = _mdMap->getEpetraMap(true);
+
+  // Construct the result
+  Teuchos::RCP< Epetra_IntVector > result =
+    Teuchos::rcp(new Epetra_IntVector(*epetraMap));
+
+  // Copy the MDVector data buffer to the Epetra_IntVector, even if the
+  // MDVector is non-contiguous
+  int ii = 0;
+  for (iterator it = _mdArrayView.begin(); it != _mdArrayView.end(); ++it)
+    result->operator[](ii++) = (int) *it;
+
+  // Return the result
+  return result;
+}
+
+////////////////////////////////////////////////////////////////////////
 
 template< class Scalar,
           class Node >
@@ -2102,11 +2192,7 @@ getEpetraVectorCopy() const
   return result;
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_EPETRA
 
 template< class Scalar,
           class Node >
@@ -2178,11 +2264,7 @@ getTpetraVectorView() const
   return getTpetraVectorView< LocalOrdinal, LocalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template< class LocalOrdinal,
@@ -2194,11 +2276,7 @@ getTpetraVectorView() const
   return getTpetraVectorView< LocalOrdinal, GlobalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template< class LocalOrdinal,
@@ -2229,11 +2307,7 @@ getTpetraVectorView() const
                                                   _mdArrayView.arrayView()));
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template< class LocalOrdinal >
@@ -2244,11 +2318,7 @@ getTpetraVectorCopy() const
   return getTpetraVectorCopy< LocalOrdinal, LocalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template< class LocalOrdinal,
@@ -2260,11 +2330,7 @@ getTpetraVectorCopy() const
   return getTpetraVectorCopy< LocalOrdinal, GlobalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template< class LocalOrdinal,
@@ -2303,11 +2369,7 @@ getTpetraVectorCopy() const
   return result;
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template < class LocalOrdinal >
@@ -2321,11 +2383,7 @@ getTpetraMultiVectorView() const
   return getTpetraMultiVectorView< LocalOrdinal, LocalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template < class LocalOrdinal,
@@ -2340,11 +2398,7 @@ getTpetraMultiVectorView() const
   return getTpetraMultiVectorView< LocalOrdinal, GlobalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template < class LocalOrdinal,
@@ -2407,11 +2461,7 @@ getTpetraMultiVectorView() const
                                                        numVectors));
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template < class LocalOrdinal >
@@ -2425,11 +2475,7 @@ getTpetraMultiVectorCopy() const
   return getTpetraMultiVectorCopy< LocalOrdinal, LocalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template < class LocalOrdinal,
@@ -2444,11 +2490,7 @@ getTpetraMultiVectorCopy() const
   return getTpetraMultiVectorCopy< LocalOrdinal, GlobalOrdinal, Node >();
 }
 
-#endif
-
 ////////////////////////////////////////////////////////////////////////
-
-#ifdef HAVE_TPETRA
 
 template< class Scalar, class Node >
 template < class LocalOrdinal,
