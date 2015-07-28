22b4f5b0073fc1d18902efa6961c4e715066b4e5
Alan Williams <william@sandia.gov>
2015-07-28 16:44:58 -0600

    Option to do part de-induction in framework.  Useful for comparing results during transition
    Paired with Riley Wilson and Todd Coffey.
    
    (From Sierra)

--- packages/stk/stk_mesh/stk_mesh/base/BulkData.cpp
+++ packages/stk/stk_mesh/stk_mesh/base/BulkData.cpp
@@ -72,6 +72,7 @@
 #include "stk_util/util/NamedPair.hpp"
 #include "stk_util/util/PairIter.hpp"   // for PairIter
 #include "stk_util/util/SameType.hpp"   // for SameType, etc
+#include "stk_util/util/SortAndUnique.hpp"
 #include <stk_util/parallel/GenerateParallelUniqueIDs.hpp>
 #include <stk_mesh/base/ElemElemGraph.hpp>
 
@@ -5039,7 +5040,7 @@ void fill_add_parts_and_supersets(const PartVector & add_parts, PartVector &addP
             addPartsAndSupersets.push_back(supersets[j]);
         }
     }
-    sort_and_unique(addPartsAndSupersets);
+    stk::util::sort_and_unique(addPartsAndSupersets,PartLess());
 }
 
 void fill_remove_parts_and_subsets_minus_parts_in_add_parts_list(const PartVector & remove_parts,
@@ -5067,7 +5068,7 @@ void fill_remove_parts_and_subsets_minus_parts_in_add_parts_list(const PartVecto
             }
         }
     }
-    sort_and_unique(removePartsAndSubsetsMinusPartsInAddPartsList);
+    stk::util::sort_and_unique(removePartsAndSubsetsMinusPartsInAddPartsList,PartLess());
 }
 
 void BulkData::internal_verify_and_change_entity_parts( Entity entity,
@@ -5181,7 +5182,7 @@ void BulkData::internal_change_entity_parts(
 
         OrdinalVector newBucketPartList;
         internal_fill_new_part_list_and_removed_part_list(entity, add_parts, remove_parts, newBucketPartList, parts_removed);
-
+        stk::util::sort_and_unique(newBucketPartList);
         internal_move_entity_to_new_bucket(entity, newBucketPartList);
 
         EntityRank e_rank = entity_rank(entity);
@@ -6461,13 +6462,6 @@ bool is_node_connected_to_active_element_locally(stk::mesh::BulkData &mesh, stk:
     }
     return activeNode;
 }
-template<typename VECTOR>
-void sort_and_unique(VECTOR &vector)
-{
-    std::sort(vector.begin(), vector.end());
-    auto endIter = std::unique(vector.begin(), vector.end());
-    vector.resize(endIter - vector.begin());
-}
 } //emtpy namespace
 
 bool BulkData::modification_end_for_face_creation_and_deletion(const std::vector<sharing_info>& shared_modified,
@@ -6736,7 +6730,7 @@ void BulkData::de_induce_unranked_part_from_nodes(const stk::mesh::EntityVector 
             potentiallyDeactivatedNodes.push_back(nodes[nodeI]);
         }
     }
-    sort_and_unique(potentiallyDeactivatedNodes);
+    stk::util::sort_and_unique(potentiallyDeactivatedNodes);
 
     stk::mesh::EntityVector nodesToCommunicate;
     for (stk::mesh::Entity node : potentiallyDeactivatedNodes)
