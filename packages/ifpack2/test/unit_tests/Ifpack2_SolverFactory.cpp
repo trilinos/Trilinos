6f908316ba32f8f451c88b841073c9a8560f69b5
Mark Hoemmen <mhoemme@sandia.gov>
2015-08-07 14:26:17 -0600

    Ifpack2: Fixed build warning ((un)signed comparison) in test.

--- packages/ifpack2/test/unit_tests/Ifpack2_SolverFactory.cpp
+++ packages/ifpack2/test/unit_tests/Ifpack2_SolverFactory.cpp
@@ -48,7 +48,7 @@ namespace {
     if (rowMap->getNodeNumElements () != 0) {
       Teuchos::Array<SC> vals (1);
       Teuchos::Array<LO> inds (1);
-      for (size_t lclRow = rowMap->getMinLocalIndex ();
+      for (LO lclRow = rowMap->getMinLocalIndex ();
            lclRow <= rowMap->getMaxLocalIndex (); ++lclRow) {
         inds[0] = lclRow;
         vals[0] = STS::one ();
