01c63cd8e439a14a3dfc71c5f6844ca2f3e8960c
Gerrett Diamond <gdiamon@sandia.gov>
2015-08-10 09:26:45 -0600

    Added weight reading to ParMA algorithm

--- packages/zoltan2/test/partition/APFMeshAdapterTest.cpp
+++ packages/zoltan2/test/partition/APFMeshAdapterTest.cpp
@@ -44,7 +44,7 @@
 // @HEADER
 
 /*! \file APFMeshAdapterTest.cpp
-    \brief An example of partitioning a SCOREC mesh with RCB.
+    \brief An example of partitioning a SCOREC APF mesh
 
     \author Created by G. Diamond, K. Devine.
 
@@ -329,8 +329,6 @@ int main(int narg, char *arg[]) {
     zparams.set("LB_APPROACH","PARTITION");
     //params.set("compute_metrics","yes");
     adjacency="vertex";
-    primary="vertex";
-    adjacency="edge";
   }
   else if (action=="hg_ghost") {
     do_partitioning = true;
@@ -372,6 +370,7 @@ int main(int narg, char *arg[]) {
   
   const inputAdapter_t::scalar_t* weights=arr;
   ia.setWeights(ia.getPrimaryEntityType(),weights,1);
+  
 
   if (ghost_metric) {
     const baseMeshAdapter_t *base_ia = dynamic_cast<const baseMeshAdapter_t*>(&ia);
