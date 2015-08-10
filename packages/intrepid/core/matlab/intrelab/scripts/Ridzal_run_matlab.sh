8b237b3b88158472609697ea825495b3624f1c8a
avdelor <avdelor@sandia.gov>
2015-08-10 19:17:18 +0000

    Fixed cmake error associated with subpackage push

--- packages/intrepid/core/matlab/intrelab/scripts/Ridzal_run_matlab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-LD_PRELOAD=/home/dridzal/software/gcc/install_gcc-4.5.1/lib64/libstdc++.so.6:\
-/home/dridzal/software/gcc/install_gcc-4.5.1/lib64/libgcc_s.so.1:\
-/home/dridzal/software/gcc/install_gcc-4.5.1/lib64/libgfortran.so.3:\
-/usr/lib64/libblas.so.3:\
-/usr/lib64/liblapack.so.3 \
-LD_LIBRARY_PATH=/home/dridzal/research/Trilinos/BUILD/gcc-4.5.1_intrepid-ML_release/install/lib:$LD_LIBRARY_PATH \
-MATLABPATH=/home/dridzal/research/Trilinos/packages/intrepid/matlab/intrelab/install:/home/dridzal/research/Trilinos/packages/intrepid/matlab/intrelab/mesh \
-/usr/local/MATLAB/R2010b/bin/matlab -singleCompThread -nodesktop
--- packages/intrepid/core/matlab/intrelab/test/poisson_fem/poissonTest.m
+++
@@ -1,9 +0,0 @@
-function tests = poissonTest
-  tests = functiontests(localfunctions);
-end
-
-function testConvergence(testCase)
-  act_rate = conv_test;
-  exp_rate = 1.9;
-  verifyGreaterThanOrEqual(testCase,act_rate,exp_rate);
-end
