ef78bb1206d42714756ae6384d7077e7ee5c0eb0
Jonathan Hu <jhu@sandia.gov>
2015-08-10 16:21:29 -0700

    MueLu: add nullspace argument to SOC factory
    Build/Test Cases Summary
    Enabled Packages: MueLu
    Enabled all Forward Packages
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    2) SERIAL_SS_DEBUG => passed: passed=54,notpassed=0 (21.03 min)
    Other local commits for this build/test group: 5e26881, 0944009, b1d76ad, 742c099

--- packages/muelu/matlab/bin/Tests/Evolution/evolutionSoCFactory.m
+++ packages/muelu/matlab/bin/Tests/Evolution/evolutionSoCFactory.m
@@ -7,14 +7,20 @@
 %
 % Notes:  Dofs per node is hardwired to 1
 %
-function [Graph, DofsPerNode, Filtering] = evolutionSoC(A)
+function [Graph, DofsPerNode, Filtering] = evolutionSoC(A, B)
   fprintf('entering evolutionSoC\n');
+
+  [m,n] = size(A);
+  fprintf('size(A) = %d x %d\n',m,n);
+  [m,n] = size(B);
+  fprintf('size(B) = %d x %d\n',m,n);
 
   % for now, just return the matrix graph to see that it works
   Graph.edges = logical(A);
   Graph.boundaryNodes = int32([]);
   DofsPerNode = 1;
-  Filtering = false; % TODO If any connections are dropped, then Filtering must be true
+  Filtering = true; % TODO If any connections are dropped, then Filtering must be true
 
+  fprintf('enabling filtering\n');
   fprintf('leaving evolutionSoC\n');
 end 
