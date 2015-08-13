a83414b9b9abb196addfc5cd04c09fbfeee5087e
Drew Kouri <dpkouri@sandia.gov>
2015-08-13 09:13:15 -0600

    Changed the state/constraint norms to the scaled H1 norm.  Added a parameter to control nonlinearity of Burgers.  Made optimization tolerances/maximum number of iterations read in from xml.

--- packages/rol/example/burgers-control/example_07.cpp
+++ packages/rol/example/burgers-control/example_07.cpp
@@ -81,120 +81,169 @@ int main(int argc, char *argv[]) {
   // *** Example body.
 
   try {
-    // Initialize finite element class.
+    /*************************************************************************/
+    /************* INITIALIZE BURGERS FEM CLASS ******************************/
+    /*************************************************************************/
     int nx      = 128;   // Set spatial discretization.
     RealT alpha = 1.e-3; // Set penalty parameter.
     RealT nu    = 1e-2;  // Viscosity parameter.
+    RealT nl    = 1.0;   // Nonlinearity parameter (1 = Burgers, 0 = linear).
+    RealT u0    = 1.0;   // Dirichlet boundary condition at x=0.
+    RealT u1    = 0.0;   // Dirichlet boundary condition at x=1.
+    RealT f     = 0.0;   // Constant volumetric force.
+    RealT cH1   = 1.0;   // Scale for derivative term in H1 norm.
+    RealT cL2   = 0.0;   // Scale for mass term in H1 norm.
     Teuchos::RCP<BurgersFEM<RealT> > fem
-      = Teuchos::rcp(new BurgersFEM<RealT>(nx,nu));
+      = Teuchos::rcp(new BurgersFEM<RealT>(nx,nu,nl,u0,u1,f,cH1,cL2));
     fem->test_inverse_mass(*outStream);
-    // Initialize objective function.
-    Teuchos::RCP<std::vector<RealT> > ud_rcp = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
-    Teuchos::RCP<ROL::Vector<RealT> > ud = Teuchos::rcp(new L2VectorPrimal<RealT>(ud_rcp,fem));
+    fem->test_inverse_H1(*outStream);
+    /*************************************************************************/
+    /************* INITIALIZE SIMOPT OBJECTIVE FUNCTION **********************/
+    /*************************************************************************/
+    Teuchos::RCP<std::vector<RealT> > ud_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
+    Teuchos::RCP<ROL::Vector<RealT> > ud
+      = Teuchos::rcp(new L2VectorPrimal<RealT>(ud_rcp,fem));
     Objective_BurgersControl<RealT> obj(fem,ud,alpha);
-    // Initialize equality constraints
-    EqualityConstraint_BurgersControl<RealT> con(fem);
-    // Initialize equality constraints
-    std::vector<RealT> lo1(nx, 0.0), hi1(nx, 1.0);
-    //std::vector<RealT> lo1(nx, -1.e6), hi1(nx, 1.e6);
-    Teuchos::RCP<ROL::BoundConstraint<RealT> > bnd1 = Teuchos::rcp(new L2BoundConstraint<RealT>(lo1,hi1,fem));
-    //bnd1.deactivate();
-    //std::vector<RealT> lo2(nx+2, -0.1*ROL::ROL_OVERFLOW), hi2(nx+2, 0.1*ROL::ROL_OVERFLOW);
-    std::vector<RealT> lo2(nx+2,0.0), hi2(nx+2,2.0);
-    Teuchos::RCP<ROL::BoundConstraint<RealT> > bnd2 = Teuchos::rcp(new L2BoundConstraint<RealT>(lo2,hi2,fem));
+    /*************************************************************************/
+    /************* INITIALIZE SIMOPT EQUALITY CONSTRAINT *********************/
+    /*************************************************************************/
+    bool useEChessian = false;
+    EqualityConstraint_BurgersControl<RealT> con(fem, useEChessian);
+    /*************************************************************************/
+    /************* INITIALIZE BOUND CONSTRAINTS ******************************/
+    /*************************************************************************/
+    // INITIALIZE STATE CONSTRAINTS
+    std::vector<RealT> Ulo(nx, 0.0), Uhi(nx, 1.0);
+    //std::vector<RealT> Ulo(nx, -1.e8), Uhi(nx, 1.e8);
+    Teuchos::RCP<ROL::BoundConstraint<RealT> > Ubnd
+       = Teuchos::rcp(new H1BoundConstraint<RealT>(Ulo,Uhi,fem));
+    //Ubnd.deactivate();
+    // INITIALIZE CONTROL CONSTRAINTS
+    //std::vector<RealT> Zlo(nx+2, -1.e8), Zhi(nx+2, 1.e8);
+    std::vector<RealT> Zlo(nx+2,0.0), Zhi(nx+2,2.0);
+    Teuchos::RCP<ROL::BoundConstraint<RealT> > Zbnd
+      = Teuchos::rcp(new L2BoundConstraint<RealT>(Zlo,Zhi,fem));
     //bnd2.deactivate();
-    ROL::BoundConstraint_SimOpt<RealT> bnd(bnd1,bnd2);
-    // Initialize iteration vectors.
-    Teuchos::RCP<std::vector<RealT> > z_rcp  = Teuchos::rcp( new std::vector<RealT> (nx+2, 1.0) );
-    Teuchos::RCP<std::vector<RealT> > gz_rcp = Teuchos::rcp( new std::vector<RealT> (nx+2, 1.0) );
-    Teuchos::RCP<std::vector<RealT> > yz_rcp = Teuchos::rcp( new std::vector<RealT> (nx+2, 1.0) );
+    // INITIALIZE SIMOPT BOUND CONSTRAINTS
+    ROL::BoundConstraint_SimOpt<RealT> bnd(Ubnd,Zbnd);
+    /*************************************************************************/
+    /************* INITIALIZE VECTOR STORAGE *********************************/
+    /*************************************************************************/
+    // INITIALIZE CONTROL VECTORS
+    Teuchos::RCP<std::vector<RealT> > z_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx+2, 1.0) );
+    Teuchos::RCP<std::vector<RealT> > gz_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx+2, 1.0) );
+    Teuchos::RCP<std::vector<RealT> > yz_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx+2, 1.0) );
     for (int i=0; i<nx+2; i++) {
       (*z_rcp)[i]  = 10.0*(RealT)rand()/(RealT)RAND_MAX-5.0;
       (*yz_rcp)[i] = 10.0*(RealT)rand()/(RealT)RAND_MAX-5.0;
     }
-    Teuchos::RCP<ROL::Vector<RealT> > zp  = Teuchos::rcp(new L2VectorPrimal<RealT>(z_rcp,fem));
-    Teuchos::RCP<ROL::Vector<RealT> > gzp = Teuchos::rcp(new L2VectorDual<RealT>(gz_rcp,fem));
-    Teuchos::RCP<ROL::Vector<RealT> > yzp = Teuchos::rcp(new L2VectorPrimal<RealT>(yz_rcp,fem));
-
-    Teuchos::RCP<std::vector<RealT> > u_rcp  = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
-    Teuchos::RCP<std::vector<RealT> > gu_rcp = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
-    Teuchos::RCP<std::vector<RealT> > yu_rcp = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
+    Teuchos::RCP<ROL::Vector<RealT> > zp
+      = Teuchos::rcp(new L2VectorPrimal<RealT>(z_rcp,fem));
+    Teuchos::RCP<ROL::Vector<RealT> > gzp
+      = Teuchos::rcp(new L2VectorDual<RealT>(gz_rcp,fem));
+    Teuchos::RCP<ROL::Vector<RealT> > yzp
+      = Teuchos::rcp(new L2VectorPrimal<RealT>(yz_rcp,fem));
+    // INITIALIZE STATE VECTORS
+    Teuchos::RCP<std::vector<RealT> > u_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
+    Teuchos::RCP<std::vector<RealT> > gu_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
+    Teuchos::RCP<std::vector<RealT> > yu_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
     for (int i=0; i<nx; i++) {
       (*u_rcp)[i]  = 10.0*(RealT)rand()/(RealT)RAND_MAX-5.0;
       (*yu_rcp)[i] = 10.0*(RealT)rand()/(RealT)RAND_MAX-5.0;
     }
-    Teuchos::RCP<ROL::Vector<RealT> > up  = Teuchos::rcp(new L2VectorPrimal<RealT>(u_rcp,fem));
-    Teuchos::RCP<ROL::Vector<RealT> > gup = Teuchos::rcp(new L2VectorDual<RealT>(gu_rcp,fem));
-    Teuchos::RCP<ROL::Vector<RealT> > yup = Teuchos::rcp(new L2VectorPrimal<RealT>(yu_rcp,fem));
-
-    Teuchos::RCP<std::vector<RealT> > c_rcp = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
-    Teuchos::RCP<std::vector<RealT> > l_rcp = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
-    L2VectorDual<RealT> c(c_rcp,fem);
-    L2VectorPrimal<RealT> l(l_rcp,fem);
+    Teuchos::RCP<ROL::Vector<RealT> > up
+      = Teuchos::rcp(new H1VectorPrimal<RealT>(u_rcp,fem));
+    Teuchos::RCP<ROL::Vector<RealT> > gup
+      = Teuchos::rcp(new H1VectorDual<RealT>(gu_rcp,fem));
+    Teuchos::RCP<ROL::Vector<RealT> > yup
+      = Teuchos::rcp(new H1VectorPrimal<RealT>(yu_rcp,fem));
+    // INITIALIZE CONSTRAINT VECTORS
+    Teuchos::RCP<std::vector<RealT> > c_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
+    Teuchos::RCP<std::vector<RealT> > l_rcp
+      = Teuchos::rcp( new std::vector<RealT> (nx, 1.0) );
     for (int i=0; i<nx; i++) {
       (*l_rcp)[i] = (RealT)rand()/(RealT)RAND_MAX;
     }
-
+    H1VectorDual<RealT> c(c_rcp,fem);
+    H1VectorPrimal<RealT> l(l_rcp,fem);
+    // INITIALIZE SIMOPT VECTORS
     ROL::Vector_SimOpt<RealT> x(up,zp);
     ROL::Vector_SimOpt<RealT> g(gup,gzp);
     ROL::Vector_SimOpt<RealT> y(yup,yzp);
-
-    // Check derivatives.
+    /*************************************************************************/
+    /************* CHECK DERIVATIVES AND CONSISTENCY *************************/
+    /*************************************************************************/
+    // CHECK OBJECTIVE DERIVATIVES
     obj.checkGradient(x,g,y,true,*outStream);
     obj.checkHessVec(x,g,y,true,*outStream);
-
+    // CHECK EQUALITY CONSTRAINT DERIVATIVES
     con.checkApplyJacobian(x,y,c,true,*outStream);
     con.checkApplyAdjointHessian(x,*yup,y,g,true,*outStream);
-
+    // CHECK EQUALITY CONSTRAINT CONSISTENCY
     con.checkSolve(*up,*zp,c,true,*outStream);
     con.checkAdjointConsistencyJacobian_1(l,*yup,*up,*zp,true,*outStream);
     con.checkAdjointConsistencyJacobian_2(l,*yzp,*up,*zp,true,*outStream);
     con.checkInverseJacobian_1(c,*yup,*up,*zp,true,*outStream);
     con.checkInverseAdjointJacobian_1(c,*yup,*up,*zp,true,*outStream);
     *outStream << "\n";
-
+    // CHECK PENALTY OBJECTIVE DERIVATIVES
     ROL::MoreauYosidaPenalty<RealT> myPen(obj,bnd,x,10.0);
     myPen.checkGradient(x, y, true, *outStream);
     myPen.checkHessVec(x, g, y, true, *outStream);
-
-    // Optimization 
+    /*************************************************************************/
+    /************* RUN OPTIMIZATION ******************************************/
+    /*************************************************************************/
+    // READ IN XML INPUT
     std::string filename = "input.xml";
-    Teuchos::RCP<Teuchos::ParameterList> parlist_tr = Teuchos::rcp( new Teuchos::ParameterList() );
-    Teuchos::updateParametersFromXmlFile( filename, Teuchos::Ptr<Teuchos::ParameterList>(&*parlist_tr) );
-    // Define status test.
-    RealT gtol  = 1e-10;  // norm of gradient of Lagrangian tolerance
-    RealT ctol  = 1e-10;  // norm of constraint tolerance
-    RealT stol  = 1e-12;  // norm of step tolerance
-    int   maxit = 1000;   // maximum number of iterations
+    Teuchos::RCP<Teuchos::ParameterList> parlist
+      = Teuchos::rcp( new Teuchos::ParameterList() );
+    Teuchos::updateParametersFromXmlFile( filename,
+      Teuchos::Ptr<Teuchos::ParameterList>(&*parlist) );
+    // DEFINE STATUS TEST
+    RealT gtol  = parlist->get("Gradient Tolerance",1e-10);
+    RealT ctol  = parlist->get("Constraint Tolerance",1e-10);
+    RealT stol  = parlist->get("Step Tolerance",1.e-4*gtol);
+    int   maxit = parlist->get("Maximum Number of Iterations",1000);
     ROL::StatusTestSQP<RealT> status(gtol, ctol, stol, maxit);    
-    // Define step.
-    ROL::MoreauYosidaPenaltyStep<RealT> step(*parlist_tr);
-    // Define algorithm.
+    // DEFINE STEP
+    ROL::MoreauYosidaPenaltyStep<RealT> step(*parlist);
+    // DEFINE ALGORITHM
     ROL::DefaultAlgorithm<RealT> algo(step,status,false);
-    // Run Algorithm
+    // INITIALIZE STATE AND LAGRANGE MULTIPLIERS
     RealT zerotol = 0.0;
-    //z.scale(50.0);
     con.solve(*up,*zp,zerotol);
     obj.gradient_1(*gup,*up,*zp,zerotol);
     gup->scale(-1.0);
     con.applyInverseAdjointJacobian_1(l,*gup,*up,*zp,zerotol);
     gup->zero();
     c.zero();
+    // RUN OPTIMIZATION
     algo.run(x, g, l, c, obj, con, bnd, true, *outStream);
-
+    /*************************************************************************/
+    /************* PRINT CONTROL AND STATE TO SCREEN *************************/
+    /*************************************************************************/
     for ( int i = 0; i < nx+2; i++ ) {
-      std::cout << std::scientific << std::setprecision(10);
-      std::cout << std::setw(20) << std::left << (*z_rcp)[i];
+      *outStream << "\n";
+      *outStream << std::scientific << std::setprecision(10);
+      *outStream << std::setw(20) << std::left << (*z_rcp)[i];
       if ( i == 0 ) {
-        std::cout << std::setw(20) << std::left << 1.0;
+        *outStream << std::setw(20) << std::left << 1.0;
       }
       if ( i != 0 && i != nx+1 ) {
-        std::cout << std::setw(20) << std::left << (*u_rcp)[i-1];
+        *outStream << std::setw(20) << std::left << (*u_rcp)[i-1];
       }
       if ( i == nx+1 ) {
-        std::cout << std::setw(20) << std::left << 0.0;
+        *outStream << std::setw(20) << std::left << 0.0;
       }
-      std::cout << "\n";
+      *outStream << "\n";
     }
   }
   catch (std::logic_error err) {
