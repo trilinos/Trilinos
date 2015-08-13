7cf4844b437cbb6e93568246edd3395ca38fd3cc
Drew Kouri <dpkouri@sandia.gov>
2015-08-13 09:27:31 -0600

    Added mesh to output.

--- packages/rol/example/burgers-control/example_07.cpp
+++ packages/rol/example/burgers-control/example_07.cpp
@@ -233,6 +233,7 @@ int main(int argc, char *argv[]) {
     *outStream << "\n";
     for ( int i = 0; i < nx+2; i++ ) {
       *outStream << std::scientific << std::setprecision(10);
+      *outStream << std::setw(20) << std::left << (RealT)i/((RealT)nx+1.0);
       *outStream << std::setw(20) << std::left << (*z_rcp)[i];
       if ( i == 0 ) {
         *outStream << std::setw(20) << std::left << 1.0;
