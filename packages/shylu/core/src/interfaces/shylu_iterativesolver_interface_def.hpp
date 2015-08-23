4d8825048d0b519a2039d5c7eb947c2bd5f001bb
Mark Hoemmen <mhoemme@sandia.gov>
2015-08-23 16:55:38 -0600

    ShyLU: Fixed some build warnings (unused variables / typedefs)

--- packages/shylu/core/src/interfaces/shylu_iterativesolver_interface_def.hpp
+++ packages/shylu/core/src/interfaces/shylu_iterativesolver_interface_def.hpp
@@ -47,29 +47,29 @@ namespace ShyLU
 
     Teuchos::ParameterList subList = pList->sublist("Aztec Input");
     //Add additional sublist parameters for aztec
-    
-   
+
+
     e_problem.SetOperator(A);
     e_problem.SetRHS(x);
     e_problem.SetLHS(b);
-    
+
     //set Aztec paramters
     solver_aztec = new AztecOO(e_problem);
     solver_aztec->SetAztecOption(AZ_precond, AZ_none);
     solver_aztec->SetAztecOption(AZ_solver,  AZ_gmres);
     solver_aztec->SetAztecOption(AZ_max_iter, 1000);
-    
+
     //solve
     solver_aztec->Iterate(1000, 1e-5);
 
     return 0;
   }//end solveAztec
-  
+
   template <class Matrix, class Vector>
   int IterativeSolverInterface<Matrix,Vector>::solveBelos(Vector *b, Vector *x)
   {
 
-    typedef typename Matrix::scalar_type           scalar_type;   
+    typedef typename Matrix::scalar_type           scalar_type;
     typedef typename Matrix::local_ordinal_type    local_ordinal_type;
     typedef typename Matrix::global_ordinal_type   global_ordinal_type;
     typedef typename Matrix::node_type             node_type;
@@ -79,8 +79,8 @@ namespace ShyLU
     //Teuchos::RCP<Teuchos::ParameterList> belosList = Teuchos::parameterList();
 
     //Teuchos::RCP<Teuchos::ParameterList> subList = pList->sublist("Belos Input");
-   
-   
+
+
     Teuchos::RCP<Teuchos::ParameterList> pListRCP = Teuchos::rcp(pList,false);
     Teuchos::RCP<Teuchos::ParameterList> belosList = Teuchos::sublist(pListRCP, "Belos Input");
 
@@ -94,8 +94,8 @@ namespace ShyLU
         //solver_name = Teuchos::getParameter<string>(&belosList, "Solver");
         solver_name = belosList->get<string>("Solver");
       }
-        
-    
+
+
     //printf("start\n");
     //Add additional sublipst parameter
     //belosList.set( "Num blocks", 100);
@@ -103,7 +103,7 @@ namespace ShyLU
     // belosList->set( "Maximum Iterations", 1000);
     //belosList->set( "Maximum Restarts", 10);
     //belosList->set( "Convergence Tolerance" , 1e-5);
-    
+
     printf("load belos parameters\n");
     Teuchos::RCP<Belos::LinearProblem<scalar_type, Vector, OP> > belos_problem;
     belos_problem = Teuchos::rcp(new Belos::LinearProblem<scalar_type, Vector, OP>());
@@ -113,18 +113,26 @@ namespace ShyLU
     belos_problem->setLHS(Teuchos::rcp(b,false));
     belos_problem->setProblem();
 
-    //Based on input 
-  
+    //Based on input
+
     Belos::SolverFactory<scalar_type, Vector, OP> factory;
 
     Teuchos::RCP<Belos::SolverManager<scalar_type,Vector,OP> > solver_belos;
 
     solver_belos = factory.create(solver_name, belosList);
-    
+
     solver_belos->setProblem(belos_problem);
-    
-  
-    Belos::ReturnType ret = solver_belos->solve();
+
+
+    // FIXME (mfh 23 Aug 2015) Not using this return value results in
+    // a build warning.  You should use the return value from Belos to
+    // return something other than 0, in case the solve didn't succeed
+    // (didn't return Belos::Converged).  I don't know what ShyLU uses
+    // for a "didn't succeed, but not catastrophically so" error code,
+    // or I would put it in myself.
+    //
+    //Belos::ReturnType ret = solver_belos->solve();
+    (void) solver_belos->solve();
 
     return 0;
   }// end solveBelos
@@ -132,17 +140,20 @@ namespace ShyLU
    template <>
   int IterativeSolverInterface<Epetra_CrsMatrix,Epetra_MultiVector>::solveBelos(Epetra_MultiVector *b, Epetra_MultiVector *x)
   {
+    // FIXME (mfh 23 Aug 2015) Maybe you should throw an exception or
+    // something to signal that this routine is unimplemented, instead
+    // of just returning 0.
 
-    typedef double                                 scalar_type; 
-    typedef Teuchos::ScalarTraits<scalar_type>     scalar_type_traits;
-    typedef scalar_type_traits::magnitudeType      magnitude_type;
-    typedef Epetra_Operator                        OP;
+    //typedef double                                 scalar_type;
+    //typedef Teuchos::ScalarTraits<scalar_type>     scalar_type_traits;
+    //typedef scalar_type_traits::magnitudeType      magnitude_type; // unused
+    //typedef Epetra_Operator                        OP; // unused
 
     //Input list of belos
     //Teuchos::RCP<Teuchos::ParameterList> belosList = Teuchos::parameterList();
 
     //Teuchos::RCP<Teuchos::ParameterList> subList = pList->sublist("Belos Input");
-   
+
     /*
     Teuchos::RCP<Teuchos::ParameterList> belosList = Teuchos::rcp(pList)->sublist("Belos Input");
 
@@ -151,8 +162,8 @@ namespace ShyLU
       {
         solver_name = belosList->getEntry("Solver");
       }
-        
-    
+
+
     //printf("start\n");
     //Add additional sublipst parameter
     //belosList.set( "Num blocks", 100);
@@ -160,7 +171,7 @@ namespace ShyLU
     // belosList->set( "Maximum Iterations", 1000);
     //belosList->set( "Maximum Restarts", 10);
     //belosList->set( "Convergence Tolerance" , 1e-5);
-    
+
     printf("load belos parameters\n");
     Teuchos::RCP<Belos::LinearProblem<scalar_type, Vector, OP> > belos_problem;
     belos_problem = Teuchos::rcp(new Belos::LinearProblem<scalar_type, Vector, OP>());
@@ -170,17 +181,17 @@ namespace ShyLU
     belos_problem->setLHS(Teuchos::rcp(b,false));
     belos_problem->setProblem();
 
-    //Based on input 
-  
+    //Based on input
+
     Belos::SolverFactory<scalar_type, Vector, OP> factory;
 
     Teuchos::RCP<Belos::SolverManager<scalar_type,Vector,OP> > solver_belos;
 
     solver_belos = factory.create(solver_name, belosList);
-    
+
     solver_belos->setProblem(belos_problem);
-    
-  
+
+
     Belos::ReturnType ret = solver_belos->solve();
 
     */
@@ -192,7 +203,7 @@ namespace ShyLU
   int IterativeSolverInterface<Matrix,Vector>::solve(Vector *b, Vector* x)
   {
     string solverpackage = Teuchos::getParameter<string>(*pList, "Iterative Solver Package");
-    
+
     cout << "found package " << solverpackage << endl;
 
     if(solverpackage.compare("Aztec")==0)
@@ -212,7 +223,7 @@ namespace ShyLU
     return 0;
   }//end solve()
 
-  
+
 }//end namespace shylu
 
 #endif //end ifdef shylu_interative_fie
