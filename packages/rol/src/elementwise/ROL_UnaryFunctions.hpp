8bf40957624d1df4ad9788c336c62f98b8d0449b
Greg von Winckel <gvonwin@sandia.gov>
2015-08-26 12:19:12 -0600

    Added unary vector fill function, InteriorPointEqualityConstraint, and InteriorPointBoundConstraint using PartitionedVector

--- packages/rol/src/elementwise/ROL_UnaryFunctions.hpp
+++ packages/rol/src/elementwise/ROL_UnaryFunctions.hpp
@@ -41,95 +41,27 @@
 // ************************************************************************
 // @HEADER
 
-#ifndef ROL_ELEMENTWISE_FUNCTION_H
-#define ROL_ELEMENTWISE_FUNCTION_H
+#ifndef ROL_UNARYFUNCTIONS_H
+#define ROL_UNARYFUNCTIONS_H
 
-#include "Teuchos_CommHelpers.hpp"
+#include "ROL_Elementwise_Function.hpp"
 
 namespace ROL {
-
-template<class Real>
-class Vector;
-
 namespace Elementwise {
 
-// Interface class with function of a single argument
+// Used to set every element in a vector to a specific value
 template<class Real>
-class UnaryFunction {
+class Fill : public UnaryFunction<Real> {
 public:
-  virtual ~UnaryFunction() {}
-  virtual Real apply( const Real &x ) const = 0;
-};
-
-// Wrap a generic function/functor of a single argument
-template<class Real, class Func>
-class UnaryFunctionWrapper : public UnaryFunction<Real> {
-public:
-  UnaryFunctionWrapper( const Func &f ) : f_(f) {}
-  
+  Fill( const Real &value ) : value_(value) {}
   Real apply( const Real &x ) const {
-    return f_(x);
-  }
-private:
-  Func f_;
-};
+    return value_;
+  }  
+private:  
+  Real value_;
+}; // class Fill
 
-// Apply a Unary function to a ROL Vector
-template<class Real>
-void applyUnaryInPlace(ROL::Vector<Real> &x, const UnaryFunction<Real> &f) {
-  x.applyUnary(f);
 }
-
-// Wrap a generic single argument function or functor and and apply it to a ROL Vector
-template<class Real, class Func>
-void applyUnaryInPlace(ROL::Vector<Real> &x, Func f) {
-  UnaryFunctionWrapper<Real,Func> f_wrapped(f);
-  x.applyUnary(f_wrapped);
-}
-
-
-
-// Interface class with function of two arguments
-template<class Real>
-class BinaryFunction {
-public:
-  virtual ~BinaryFunction() {}
-  virtual Real apply( const Real &x, const Real &y ) const = 0;
-};
-
-
-// Wrap a generic function/functor of two arguments
-template<class Real, class Func>
-class BinaryFunctionWrapper : public BinaryFunction<Real> {
-public:
-  BinaryFunctionWrapper( const Func &f ) : f_(f) {}
-  
-  Real apply( const Real &x, const Real &y ) const {
-    return f_(x,y); 
-  }
-private:
-  Func f_;   
-};
-
-// Apply a Binary function to a ROL Vector
-template<class Real>
-void applyBinaryInPlace(ROL::Vector<Real> &x, const ROL::Vector<Real> &y, const BinaryFunction<Real> &f) {
-  x.applyBinary(f,y);
-}
-
-
-// Wrap a generic two argument function or functor and and apply it to a ROL Vector
-template<class Real, class Func>
-void applyBinaryInPlace(ROL::Vector<Real> &x, const ROL::Vector<Real> &y, Func f) {
-  BinaryFunctionWrapper<Real,Func> f_wrapped(f);
-  x.applyBinary(f_wrapped,y);
-}
-
-
-} // namespace Elementwise 
 } // namespace ROL
 
-#include <ROL_Elementwise_Reduce.hpp>
-#include <ROL_UnaryFunctions.hpp>
-
-#endif
+#endif // ROL_UNARYFUNCTIONS_H
--- packages/rol/src/step/interiorpoint/ROL_InteriorPoint.hpp
+++ packages/rol/src/step/interiorpoint/ROL_InteriorPoint.hpp
@@ -41,10 +41,14 @@
 #ifndef ROL_INTERIORPOINT_H
 #define ROL_INTERIORPOINT_H
 
+
 namespace ROL {
 
 
-// Treat slack variable as Simulation variable
+/** @ingroup func_group
+ *  \class ROL::InteriorPointObjective
+ *  \brief Adds barrier term to generic objective
+ */
 template <class Real>
 class InteriorPointObjective : public Objective<Real> {
 private:
@@ -61,11 +65,12 @@ private:
   Teuchos::RCP<Objective<Real> > barrier_;
 
 public:
-  InteriorPointObjective( Objective<Real> &obj, Objective<Real> &barrier, Real mu ) :
+  InteriorPointObjective( const Teuchos::RCP<Objective<Real> > &obj, 
+                          const Teuchos::RCP<Objective<Real> > &barrier, Real mu ) :
     mu_(mu) {
 
     obj_     = Teuchos::rcp(&obj, false);
-    barrier_ = Teuchos::rcp(&barrier,false);
+    barrier_ = Teuchos::rcp(&barrier, false);
   }
  
   void updatePenalty( Real mu ) {
@@ -122,9 +127,219 @@ public:
    
   }
 
-}; // InteriorPointObjective
+}; // class InteriorPointObjective
 
 
+/** @ingroup func_group
+ *  \class ROL::InteriorPointEqualityConstraint
+ *  \brief Treat inequality constraint as equality with slack variable
+ */
+
+template<class Real> 
+class InteriorPointEqualityConstraint : public EqualityConstraint<Real> {
+private:
+  typedef Vector<Real>            V;
+  typedef PartitionedVector<Real> PV;
+  typedef typename PV::size_type  size_type; 
+
+  const static size_type OPT   = 0;
+  const static size_type SLACK = 1;
+
+  const static size_type EQUAL = 0;
+  const static size_type INEQ  = 1;
+
+  Teuchos::RCP<EqualityConstraint<Real> > eqcon_;
+  Teuchos::RCP<EqualityConstraint<Real> > incon_;
+   
+public:
+
+  InteriorPointEqualityConstraint( const Teuchos::RCP<EqualityConstraint<Real> > &eqcon,
+                                   const Teuchos::RCP<EqualityConstraint<Real> > &incon ) :
+    eqcon_(eqcon), incon_(incon) {}
+
+  void value( Vector<Real> &c, const Vector<Real> &x, Real &tol ) {
+
+    const PV xpv = Teuchos::dyn_cast<const PV>(x);
+    PV cpv = Teuchos::dyn_cast<PV>(c); 
+
+    Teuchos::RCP<const V> xopt = xpv.get(OPT);    
+    Teuchos::RCP<const V> s    = xpv.get(SLACK);
+
+    Teuchos::RCP<V> ce = cpv.get(EQUAL);
+    Teuchos::RCP<V> ci = cpv.get(INEQ);
+
+    eqcon_->value(*ce, *xopt, tol);
+    incon_->value(*ci, *xopt, tol);
+    ci->axpy(-1.0,*s);
+  }
+
+  void applyJacobian( Vector<Real> &jv,
+                      const Vector<Real> &v,
+                      const Vector<Real> &x,
+                      Real &tol ) {
+    
+    const PV xpv = Teuchos::dyn_cast<const PV>(x);
+    const PV vpv = Teuchos::dyn_cast<const PV>(v);
+    PV jvpv = Teuchos::dyn_cast<PV>(jv); 
+
+    Teuchos::RCP<const V> xopt = xpv.get(OPT);    
+    Teuchos::RCP<const V> s    = xpv.get(SLACK);
+
+    Teuchos::RCP<const V> vopt = vpv.get(OPT);    
+    Teuchos::RCP<const V> vs   = vpv.get(SLACK);
+
+    Teuchos::RCP<V> jvopt = jvpv.get(OPT);
+    Teuchos::RCP<V> jvs   = jvpv.get(SLACK);
+
+    eqcon_->applyJacobian(*jvopt, *vopt, *xopt, tol);  
+    incon_->applyJacobian(*jvs, *vopt, *xopt, tol);
+    jvs->axpy(-1.0,vopt);
+  }
+
+  void applyAdjointJacobian( Vector<Real> &ajv,
+                             const Vector<Real> &v,
+                             const Vector<Real> &x,
+                             Real &tol ) {
+
+    const PV xpv = Teuchos::dyn_cast<const PV>(x);
+    const PV vpv = Teuchos::dyn_cast<const PV>(v);
+    PV ajvpv = Teuchos::dyn_cast<PV>(ajv); 
+
+    Teuchos::RCP<const V> xopt = xpv.get(OPT);    
+    Teuchos::RCP<const V> s    = xpv.get(SLACK);
+
+    Teuchos::RCP<const V> ve = vpv.get(EQUAL);    
+    Teuchos::RCP<const V> vi = vpv.get(INEQ);
+
+    Teuchos::RCP<V> ajve = ajvpv.get(EQUAL);
+    Teuchos::RCP<V> ajvi = ajvpv.get(INEQ);
+
+    eqcon_->applyAdjointJacobian(*ajve,*ve,*xopt,tol);
+    incon_->applyAdjointJacobian(*ajvi,*vi,*xopt,tol);
+    ajve->plus(*ajvi);
+    ajvi->set(*vi);
+    ajvi->scale(-1.0);
+  }
+
+  void applyAdjointHessian( Vector<Real> &ahuv,
+                            const Vector<Real> &u,
+                            const Vector<Real> &v,
+                            const Vector<Real> &x,
+                            Real &tol ) {
+  
+    const PV xpv = Teuchos::dyn_cast<const PV>(x);
+    const PV upv = Teuchos::dyn_cast<const PV>(u);
+    const PV vpv = Teuchos::dyn_cast<const PV>(v);
+    PV ahuvpv = Teuchos::dyn_cast<PV>(ahuv); 
+
+    Teuchos::RCP<const V> xopt = xpv.get(OPT);    
+    Teuchos::RCP<const V> s    = xpv.get(SLACK);
+
+    Teuchos::RCP<const V> vopt = vpv.get(OPT);    
+    
+    Teuchos::RCP<const V> ue   = upv.get(EQUAL);    
+    Teuchos::RCP<const V> ui   = upv.get(INEQ);
+    
+    Teuchos::RCP<V> ahuvopt = ahuvpv.get(OPT);
+    Teuchos::RCP<V> ahuvs   = ahuvpv.get(SLACK);
+
+    Teuchos::RCP<V> temp = ahuvopt->clone();
+    
+    eqcon_->applyAdjointHessian(*ahuvopt,*ue,*vopt,*xopt,tol);
+    incon_->applyAdjointHessian(*temp,*ui,*vopt,*xopt,tol);
+
+    ahuvopt->plus(temp);
+    ahuvs->zero();
+
+  }
+   
+}; // class InteriorPointEqualityConstraint
+
+
+/** @ingroup func_group
+ *  \class ROL::InteriorPointBoundConstraint
+ *  \brief Require positivity of slack variables
+ */
+
+template<class Real>
+class InteriorPointBoundConstraint : public BoundConstraint<Real> {
+private:
+  typedef Vector<Real>            V;
+  typedef PartitionedVector<Real> PV;
+  typedef typename PV::size_type  size_type; 
+
+  const static size_type OPT   = 0;
+  const static size_type SLACK = 1;
+
+  Teuchos::RCP<BoundConstraint<Real> > bc_;
+
+  Teuchos::RCP<Vector<Real> > lower_;
+  Teuchos::RCP<Vector<Real> > upper_;  
+
+public:
+
+  InteriorPointBoundConstraint( const Vector<Real> &x ) {
+
+    lower_ = x.clone();
+    upper_ = x.clone();
+
+    PV lowerpv = Teuchos::dyn_cast<PV>(lower_);
+    PV upperpv = Teuchos::dyn_cast<PV>(upper_);
+   
+    Teuchos::RCP<V> lower_opt   = lowerpv.get(OPT);
+    Teuchos::RCP<V> lower_slack = lowerpv.get(SLACK);
+
+    Teuchos::RCP<V> upper_opt   = upperpv.get(OPT);
+    Teuchos::RCP<V> upper_slack = upperpv.get(SLACK);
+
+    Elementwise::Fill<Real> setToMax(std::numeric_limits<Real>::max());
+    Elementwise::Fill<Real> setToMin(std::numeric_limits<Real>::lowest());
+    
+    lower_opt->applyUnary( setToMin );
+    lower_slack->zero();
+
+    upper_opt->applyUnary( setToMax );
+    upper_slack->applyUnary( setToMax );
+
+    bc_ = Teuchos::rcp( new BoundConstraint<Real>( lower_, upper_ ) );
+
+  }
+
+  void project( Vector<Real> &x ) {
+    bc_->project(x);
+  }
+
+  void pruneUpperActive( Vector<Real> &v, const Vector<Real> &x, Real eps ) {
+    bc_->pruneUpperActive( v, x, eps );
+  } 
+
+  void pruneUpperActive( Vector<Real> &v, const Vector<Real> &g, 
+                         const Vector<Real> &x, Real eps ) {
+    bc_->pruneUpperActive( v, g, x, eps );
+  }
+
+   void pruneLowerActive( Vector<Real> &v, const Vector<Real> &x, Real eps ) {
+    bc_->pruneLowerActive( v, x, eps );
+  } 
+
+  void pruneLowerActive( Vector<Real> &v, const Vector<Real> &g, 
+                         const Vector<Real> &x, Real eps ) {
+    bc_->pruneLowerActive( v, g, x, eps );
+  }
+ 
+  void setVectorToUpperBound( Vector<Real> &u ) {
+    u.set(*upper_);
+  }
+
+  void setVectorToLowerBound( Vector<Real> &l ) {
+    l.set(*lower_);
+  }
+
+  bool isFeasible( const Vector<Real> &v ) { 
+    return bc_->isFeasible(v);
+  }
+
+}; // class InteriorPointBoundConstraint
 
 
 }
