73a3f390ad0049ca0e96c2288adce1e5fa2dbdeb
cwilson <cwilson@sandia.gov>
2015-08-26 09:54:47 -0600

    Refactor for readability.
    (From Sierra)

--- packages/stk/stk_unit_test_utils/algorithmTimer.hpp
+++ packages/stk/stk_unit_test_utils/algorithmTimer.hpp
@@ -26,6 +26,62 @@ void initial_run_to_allocate_memory_and_stuff(const ALGORITHM &alg)
     alg();
 }
 
+int convert_bool_to_int(bool done)
+{
+    return done ? 1 : 0;
+}
+
+void update_max(double timeForOneRun, RunInfo &runInfo)
+{
+    if(timeForOneRun > runInfo.max)
+    {
+        runInfo.max = timeForOneRun;
+    }
+}
+void update_min(double timeForOneRun, RunInfo &runInfo)
+{
+    if(timeForOneRun < runInfo.min)
+    {
+        runInfo.min = timeForOneRun;
+    }
+}
+
+void update_run_info(double timeForOneRun, double duration, RunInfo &runInfo)
+{
+    runInfo.numRuns++;
+    runInfo.mean = duration / runInfo.numRuns;
+
+    update_max(timeForOneRun, runInfo);
+    update_min(timeForOneRun, runInfo);
+}
+
+template <typename ALGORITHM>
+double get_time_to_run_algorithm(const ALGORITHM &alg)
+{
+    double startTime = stk::cpu_time();
+    alg();
+    return stk::cpu_time() - startTime;
+}
+
+bool has_mean_converged_within_tolerance(const RunInfo &runInfo, const double lastMean, const size_t minRuns, const double tolerance)
+{
+    bool done = false;
+    double meanDiff = std::abs(runInfo.mean - lastMean);
+    if(runInfo.numRuns >= minRuns && runInfo.mean > 0.0 && meanDiff < tolerance)
+    {
+        done = true;
+    }
+    return done;
+}
+
+bool has_converged_on_all_procs(bool done, stk::ParallelMachine comm)
+{
+    int sendDone = convert_bool_to_int(done);
+    int recvDone = 0;
+    MPI_Allreduce(&sendDone, &recvDone, 1, MPI_INTEGER, MPI_MIN, comm);
+    return recvDone == 1;
+}
+
 template <typename ALGORITHM>
 RunInfo time_algorithm(const double tolerance, const size_t minRuns, stk::ParallelMachine comm, const ALGORITHM &alg)
 {
@@ -35,36 +91,18 @@ RunInfo time_algorithm(const double tolerance, const size_t minRuns, stk::Parall
 
     double lastMean = 0.0;
     double duration = 0.0;
-    int done = false;
-    while(!done)
+    bool isConverged = false;
+    while(!isConverged)
     {
-        double startTime = stk::cpu_time();
-        alg();
-        double timeForOneRun = stk::cpu_time() - startTime;
+        double timeForOneRun = get_time_to_run_algorithm(alg);
+
         duration += timeForOneRun;
-        runInfo.numRuns++;
+        update_run_info(timeForOneRun, duration, runInfo);
 
-        if(timeForOneRun > runInfo.max)
-        {
-            runInfo.max = timeForOneRun;
-        }
-        if(timeForOneRun < runInfo.min)
-        {
-            runInfo.min = timeForOneRun;
-        }
+        bool isConvergedOnThisProc = has_mean_converged_within_tolerance(runInfo, lastMean, minRuns, tolerance);
+        isConverged = has_converged_on_all_procs(isConvergedOnThisProc, comm);
 
-        runInfo.mean = duration / runInfo.numRuns;
-        double meanDiff = std::abs(runInfo.mean - lastMean);
         lastMean = runInfo.mean;
-        if(runInfo.numRuns >= minRuns && duration > 0.0 && meanDiff < tolerance)
-        {
-            done = true;
-        }
-
-        int sendDone = done ? 1 : 0;
-        int recvDone = 0;
-        MPI_Allreduce(&sendDone, &recvDone, 1, MPI_INTEGER, MPI_MIN, comm);
-        done = recvDone;
     }
     return runInfo;
 }
