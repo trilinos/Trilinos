526398bdb83d266b74e44ef26ebc944c6e6d9514
K Devine <kddevin@sandia.gov>
2015-08-28 14:48:44 -0600

    Added Zoltan_Get_Fn interface function for Paul Wolfenberger. ACME had been including zz_const.h and accessing Zoltan_Struct directly, just to get access to functions that had been registered. This addition allows them to get the functions that they need without including a file they shouldn't or accessing Zoltan_Struct directly.

--- packages/zoltan/src/zz/zz_set_fn.c
+++ packages/zoltan/src/zz/zz_set_fn.c
@@ -63,8 +63,9 @@ extern "C" {
  *  the compiler.
  *
  *  When new callback functions are added to Zoltan, they should be 
- *  added to the case statement in Zoltan_Set_Fn and should have separate
- *  registration functions specific to the new callback types.
+ *  added to the case statement in Zoltan_Set_Fn and Zoltan_Get_Fn
+ *  and should have separate registration functions specific to the 
+ *  new callback types.
  */
 /****************************************************************************/
 /****************************************************************************/
@@ -73,13 +74,13 @@ extern "C" {
 int Zoltan_Set_Fn(ZZ *zz, ZOLTAN_FN_TYPE fn_type, void (*fn)(void), void *data)
 {
 /*
- *  Function to initialize a given LB interface function.
+ *  Function to initialize a given Zoltan interface function.
  *  Input:
  *    zz                --  Pointer to a Zoltan structure.
  *    fn_type           --  Enum type indicating the function to be set.
  *    fn                --  Pointer to the function to be used in the
  *                          assignment.
- *    data              --  Pointer to data that the LB library will
+ *    data              --  Pointer to data that the Zoltan library will
  *                          pass as an argument to fn(). May be NULL.
  *  Output:
  *    zz                --  Appropriate field set to value in void *().
@@ -273,6 +274,221 @@ int ierr;
   default:
     sprintf(msg, "ZOLTAN_FN_TYPE %d is invalid.\n"
             "Value must be in range 0 to %d.", fn_type, ZOLTAN_MAX_FN_TYPES);
+    ZOLTAN_PRINT_ERROR(zz->Proc, yo, msg);
+    ierr = ZOLTAN_WARN;
+  }
+
+  return (ierr);
+}
+
+/****************************************************************************/
+/****************************************************************************/
+/****************************************************************************/
+
+int Zoltan_Get_Fn(ZZ *zz, ZOLTAN_FN_TYPE fn_type,
+                  void (**fn)(void), void **data)
+{
+/*
+ *  Function to retrieve a given Zoltan interface function.
+ *  Input:
+ *    zz                --  Pointer to a Zoltan structure.
+ *    fn_type           --  Enum type indicating the function to be retrieved.
+ *  Output:
+ *    fn                --  Pointer to the function retrieved.
+ *    data              --  Pointer to data associated with the retrieved 
+ *                          function.  May be NULL.
+ */
+
+char *yo = "Zoltan_Get_Fn";
+char msg[256];
+int ierr;
+
+  switch (fn_type) {
+  case ZOLTAN_PART_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Part;
+    *data = zz->Get_Part_Data;
+    break;
+  case ZOLTAN_PART_MULTI_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Part_Multi;
+    *data = zz->Get_Part_Multi_Data;
+    break;
+  case ZOLTAN_NUM_EDGES_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Edges;
+    *data = zz->Get_Num_Edges_Data;
+    break;
+  case ZOLTAN_NUM_EDGES_MULTI_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Edges_Multi;
+    *data = zz->Get_Num_Edges_Multi_Data;
+    break;
+  case ZOLTAN_EDGE_LIST_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Edge_List;
+    *data = zz->Get_Edge_List_Data;
+    break;
+  case ZOLTAN_EDGE_LIST_MULTI_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Edge_List_Multi;
+    *data = zz->Get_Edge_List_Multi_Data;
+    break;
+  case ZOLTAN_NUM_GEOM_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Geom;
+    *data = zz->Get_Num_Geom_Data;
+    break;
+  case ZOLTAN_GEOM_MULTI_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Geom_Multi;
+    *data = zz->Get_Geom_Multi_Data;
+    break;
+  case ZOLTAN_GEOM_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Geom;
+    *data = zz->Get_Geom_Data;
+    break;
+  case ZOLTAN_NUM_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Obj;
+    *data = zz->Get_Num_Obj_Data;
+    break;
+  case ZOLTAN_OBJ_LIST_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Obj_List;
+    *data = zz->Get_Obj_List_Data;
+    break;
+  case ZOLTAN_FIRST_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_First_Obj;
+    *data = zz->Get_First_Obj_Data;
+    break;
+  case ZOLTAN_NEXT_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Next_Obj;
+    *data = zz->Get_Next_Obj_Data;
+    break;
+  case ZOLTAN_NUM_BORDER_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Border_Obj;
+    *data = zz->Get_Num_Border_Obj_Data;
+    break;
+  case ZOLTAN_BORDER_OBJ_LIST_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Border_Obj_List;
+    *data = zz->Get_Border_Obj_List_Data;
+    break;
+  case ZOLTAN_FIRST_BORDER_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_First_Border_Obj;
+    *data = zz->Get_First_Border_Obj_Data;
+    break;
+  case ZOLTAN_NEXT_BORDER_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Next_Border_Obj;
+    *data = zz->Get_Next_Border_Obj_Data;
+    break;
+  case ZOLTAN_PRE_MIGRATE_PP_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Migrate.Pre_Migrate_PP;
+    *data = zz->Migrate.Pre_Migrate_PP_Data;
+    break;
+  case ZOLTAN_MID_MIGRATE_PP_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Migrate.Mid_Migrate_PP;
+    *data = zz->Migrate.Mid_Migrate_PP_Data;
+    break;
+  case ZOLTAN_POST_MIGRATE_PP_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Migrate.Post_Migrate_PP;
+    *data = zz->Migrate.Post_Migrate_PP_Data;
+    break;
+  case ZOLTAN_PRE_MIGRATE_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Migrate.Pre_Migrate;
+    *data = zz->Migrate.Pre_Migrate_Data;
+    break;
+  case ZOLTAN_MID_MIGRATE_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Migrate.Mid_Migrate;
+    *data = zz->Migrate.Mid_Migrate_Data;
+    break;
+  case ZOLTAN_POST_MIGRATE_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Migrate.Post_Migrate;
+    *data = zz->Migrate.Post_Migrate_Data;
+    break;
+  case ZOLTAN_OBJ_SIZE_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Obj_Size;
+    *data = zz->Get_Obj_Size_Data;
+    break;
+  case ZOLTAN_OBJ_SIZE_MULTI_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Obj_Size_Multi;
+    *data = zz->Get_Obj_Size_Multi_Data;
+    break;
+  case ZOLTAN_PACK_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Pack_Obj;
+    *data = zz->Pack_Obj_Data;
+    break;
+  case ZOLTAN_PACK_OBJ_MULTI_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Pack_Obj_Multi;
+    *data = zz->Pack_Obj_Multi_Data;
+    break;
+  case ZOLTAN_UNPACK_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Unpack_Obj;
+    *data = zz->Unpack_Obj_Data;
+    break;
+  case ZOLTAN_UNPACK_OBJ_MULTI_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Unpack_Obj_Multi;
+    *data = zz->Unpack_Obj_Multi_Data;
+    break;
+  case ZOLTAN_NUM_COARSE_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Coarse_Obj;
+    *data = zz->Get_Num_Coarse_Obj_Data;
+    break;
+  case ZOLTAN_COARSE_OBJ_LIST_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Coarse_Obj_List;
+    *data = zz->Get_Coarse_Obj_List_Data;
+    break;
+  case ZOLTAN_FIRST_COARSE_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_First_Coarse_Obj;
+    *data = zz->Get_First_Coarse_Obj_Data;
+    break;
+  case ZOLTAN_NEXT_COARSE_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Next_Coarse_Obj;
+    *data = zz->Get_Next_Coarse_Obj_Data;
+    break;
+  case ZOLTAN_NUM_CHILD_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Child;
+    *data = zz->Get_Num_Child_Data;
+    break;
+  case ZOLTAN_CHILD_LIST_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Child_List;
+    *data = zz->Get_Child_List_Data;
+    break;
+  case ZOLTAN_CHILD_WEIGHT_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Child_Weight;
+    *data = zz->Get_Child_Weight_Data;
+    break;
+  case ZOLTAN_HG_SIZE_CS_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_HG_Size_CS;
+    *data = zz->Get_HG_Size_CS_Data;
+    break;
+  case ZOLTAN_HG_CS_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_HG_CS;
+    *data = zz->Get_HG_CS_Data;
+    break;
+  case ZOLTAN_HG_SIZE_EDGE_WTS_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_HG_Size_Edge_Wts;
+    *data = zz->Get_HG_Size_Edge_Wts_Data;
+    break;
+  case ZOLTAN_HG_EDGE_WTS_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_HG_Edge_Wts;
+    *data = zz->Get_HG_Edge_Wts_Data;
+    break;
+  case ZOLTAN_NUM_FIXED_OBJ_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Num_Fixed_Obj;
+    *data = zz->Get_Num_Fixed_Obj_Data;
+    break;
+  case ZOLTAN_FIXED_OBJ_LIST_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Fixed_Obj_List;
+    *data = zz->Get_Fixed_Obj_List_Data;
+    break;
+  case ZOLTAN_HIER_NUM_LEVELS_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Hier_Num_Levels;
+    *data = zz->Get_Hier_Num_Levels_Data;
+    break;
+  case ZOLTAN_HIER_PART_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Hier_Part;
+    *data = zz->Get_Hier_Part_Data;
+    break;
+  case ZOLTAN_HIER_METHOD_FN_TYPE:
+    *fn = (ZOLTAN_VOID_FN *) zz->Get_Hier_Method;
+    *data = zz->Get_Hier_Method_Data;
+    break;
+  default:
+    sprintf(msg, "ZOLTAN_FN_TYPE %d is invalid.\n"
+            "Value must be in range 0 to %d.", fn_type, ZOLTAN_MAX_FN_TYPES);
+    *fn = NULL;
+    *data = NULL;
     ZOLTAN_PRINT_ERROR(zz->Proc, yo, msg);
     ierr = ZOLTAN_WARN;
   }
