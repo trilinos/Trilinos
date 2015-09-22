b78305895c1f01b1d2510ec34fcf260d2d378756
Greg von Winckel <gvonwin@sandia.gov>
2015-09-22 09:26:01 -0600

    Added a method for producing a heirarchical parameterlist using the new key names from a flat list using the old names.

--- packages/rol/src/zoo/ROL_ParameterListConverters.hpp
+++ packages/rol/src/zoo/ROL_ParameterListConverters.hpp
@@ -46,53 +46,190 @@
 #define ROL_PARAMETER_LIST_CONVERTERS_H
 
 #include "Teuchos_ParameterList.hpp"
+#include <map>
 
-/// Produces a flat list from a heirarchical list - specialization
-/// will be needed to handle the cases where the entry names differ in the
-/// two formats
-void addSublistToParameterList( Teuchos::ParameterList &outList, 
-                                const Teuchos::ParameterList &inList ) {
+
+namespace ROL {
+
+namespace StringList {
+
+/// Helper function for making vectors of strings
+std::vector<std::string> join( const std::string &s1,
+                               const std::string &s2 ) {
+  std::vector<std::string> v; 
+  v.push_back(s1);
+  v.push_back(s2);
+
+  return v;
+}
+
+/// Helper function for making vectors of strings
+std::vector<std::string> join( const std::string &s1,
+                               const std::string &s2,
+                               const std::string &s3 ) {
+  std::vector<std::string> v; 
+  v.push_back(s1);
+  v.push_back(s2);
+  v.push_back(s3);
+
+  return v;
+}
+
+/// Helper function for making vectors of strings
+std::vector<std::string> join( const std::string &s1,
+                               const std::string &s2,
+                               const std::string &s3,
+                               const std::string &s4 ) {
+  std::vector<std::string> v; 
+  v.push_back(s1);
+  v.push_back(s2);
+  v.push_back(s3);
+  v.push_back(s4);
+
+  return v;
+}
+
+/// Helper function for making vectors of strings
+std::vector<std::string> join( const std::string &s1,
+                               const std::string &s2,
+                               const std::string &s3,
+                               const std::string &s4,
+                               const std::string &s5 ) {
+  std::vector<std::string> v; 
+  v.push_back(s1);
+  v.push_back(s2);
+  v.push_back(s3);
+  v.push_back(s4);
+  v.push_back(s5);
+
+  return v;
+}
+
+} // namespace StringList
+
+template<class ParameterType>
+void setParameter( Teuchos::ParameterList &parlist, 
+                   const std::vector<std::string> &location,
+                   const std::vector<std::string>::iterator iter,
+                   ParameterType value ) {
+
+  if( iter == location.end()-1 ) { // Key name 
+    parlist.set(*iter,value); 
+  }
+  else { // sublist 
+    Teuchos::ParameterList &sublist = parlist.sublist(*iter);
+    setParameter(sublist,location,iter+1,value);
+  }
+
+
+}
+
+
+
+/// Produce a heirarchical parameter list using the new names from a flat list of the old names
+void tierParameterList( Teuchos::ParameterList &outList, 
+                        const Teuchos::ParameterList &inList ) {
+
   using namespace Teuchos;
-  typedef ParameterList::ConstIterator Iter;  
+  using StringList::join;
+
+  typedef std::string                  Str;
+  typedef std::vector<Str>             Vec;
+  typedef std::map<Str,Vec>            Map;
+  typedef ParameterList::ConstIterator IterPL;
+  typedef typename Vec::iterator       IterVec;
   
-  // Iterate over ParameterList
-  for(Iter it = inList.begin(); it != inList.end(); ++it) {
+  Map dict;
 
-    std::string key( inList.name(it) ); // Get name of item 
+  // Original flat list name                                  heirarchical list name 
+  dict["Descent Type"]                                      = join("Step","Descent Method","Type");
+  dict["Nonlinear CG Type"]                                 = join("Step","Descent Method","Nonlinear CG Type");
+  dict["Secant Type"]                                       = join("General","Secant","Type");
+  dict["Linesearch Type"]                                   = join("Step","Line Search","Type");
+  dict["Linesearch Curvature Condition"]                    = join("Step","Line Search","Curvature Condition","Type");
+  dict["Trust-Region Subproblem Solver Type"]               = join("Step","Trust Region","Subproblem Solver");
+  dict["Use Secant Preconditioning"]                        = join("General","Secant","Use as Preconditioner");
+  dict["Use Secant Hessian-Times-A-Vector"]                 = join("General","Secant","Use as Hessian");
+  dict["Maximum Secant Storage"]                            = join("General","Secant","Maximum Storage");
+  dict["Barzilai-Borwein Type"]                             = join("General","Secant","Barzilai-Borwein Type");
+  dict["Use Inexact Objective Function"]                    = join("General","Inexact Objective Function");
+  dict["Use Inexact Gradient"]                              = join("General","Inexact Gradient");
+  dict["Use Inexact Hessian-Times-A-Vector"]                = join("General","Inexact Hessian-Times-A-Vector");
+  dict["Maximum Number of Function Evaluations"]            = join("Step","Line Search","Function Evaluation Limit");
+  dict["Sufficient Decrease Parameter"]                     = join("Step","Line Search","Sufficient Decrease Tolerance");
+  dict["Curvature Conditions Parameter"]                    = join("Step","Line Search","Curvature Condition","General Parameter");
+  dict["Curvature Conditions Parameter: Generalized Wolfe"] = join("Step","Line Search","Curvature Condition","Generalized Wolfe Parameter");
+  dict["Bracketing Tolerance"]                              = join("Step","Line Search","Bracketing Tolerance");
+  dict["Backtracking Rate"]                                 = join("Step","Line Search","Backtracking Rate");
+  dict["Initial Linesearch Parameter"]                      = join("Step","Line Search","Initial Step Size");
+  dict["User Defined Linesearch Parameter"]                 = join("Step","Line Search","User Defined Initial Step Size");
+  dict["Accept Linesearch Minimizer"]                       = join("Step","Line Search","Accept Linesearch Minimizer");
+  dict["Accept Last Alpha"]                                 = join("Step","Line Search","Accept Last Alpha");
+  dict["Absolute Krylov Tolerance"]                         = join("General","Krylov","Absolute Tolerance");
+  dict["Relative Krylov Tolerance"]                         = join("General","Krylov","Relative Tolerance");
+  dict["Maximum Number of Krylov Iterations"]               = join("General","Krylov","Iteration Limit");
+  dict["Initial Trust-Region Radius"]                       = join("Step","Trust Region","Initial Radius");
+  dict["Maximum Trust-Region Radius"]                       = join("Step","Trust Region","Maximum Radius");
+  dict["Step Acceptance Parameter"]                         = join("Step","Trust Region","Step Acceptance Threshold");
+  dict["Radius Shrinking Threshold"]                        = join("Step","Trust Region","Radius Shrinking Threshold");
+  dict["Radius Growing Threshold"]                          = join("Step","Trust Region","Radius Growing Threshold");
+  dict["Radius Growing Rate"]                               = join("Step","Trust Region","Radius Growing Rate");
+  dict["Trust-Region Safeguard"]                            = join("Step","Trust Region","Safeguard Size");
+  dict["Gradient Update Tolerance Scaling"]                 = join("Step","Trust Region","Gradient Update Tolerance Scaling");
+  dict["Gradient Update Relative Tolerance"]                = join("Step","Trust Region","Gradient Update Relative Tolerance");
+  dict["Use Projected Gradient Criticality Measure"]        = join("General","Projected Gradient Criticality Measure");
+  dict["Gradient Tolerance"]                                = join("Status Test","Gradient Tolerance");
+  dict["Step Tolerance"]                                    = join("Status Test","Step Tolerance");
+  dict["Maximum Number of Iterations"]                      = join("Status Test","Iteration Limit");
+  dict["Variable Objective Function"]                       = join("General","Variable Objective Function");
+  dict["Scale for Epsilon Active Sets"]                     = join("General","Scale for Epsilon Active Sets");
+  dict["Value Update Tolerance Scaling"]                    = join("Step","Trust Region","Inexact","Value","Tolerance Scaling");
+  dict["Value Update Exponent"]                             = join("Step","Trust Region","Inexact","Value","Exponent");
+  dict["Value Update Forcing Sequence Initial Value"]       = join("Step","Trust Region","Inexact","Value","Forcing Sequence Initial Value");
+  dict["Value Update Forcing Sequence Update Frequency"]    = join("Step","Trust Region","Inexact","Value","Forcing Sequence Update Frequency");
+  dict["Value Update Forcing Sequence Reduction Factor"]    = join("Step","Trust Region","Inexact","Value","Forcing Sequence Reduction Factor");
 
-    if( inList.isSublist(key) ) {
+  // Iterate over parameter list
+  for(IterPL itpl = inList.begin(); itpl != inList.end(); ++itpl) {
 
-      ParameterList subList = inList.sublist(key);
-       
-      addSublistToParameterList(outList, subList);
+    // Get name of old key
+    Str key( inList.name(itpl) ); 
 
+    // Look up location/name of new key
+    Vec location = dict[key];
+    
+    // Skip if not found in map
+    if(location.size() == 0) {
+      break;
+    }
+
+    IterVec itvec = location.begin();
+
+    if( inList.isType<bool>(key) ) {
+      bool value = inList.get<bool>( key );
+      setParameter( outList, location, itvec, value );
+    }
+    else if( inList.isType<int>(key) ) {
+      int value = inList.get<int>( key );
+      setParameter( outList, location, itvec, value );
+    }
+    else if( inList.isType<double>(key) ) {
+      double value = inList.get<double>( key );
+      setParameter( outList, location, itvec, value );
+    }
+    else if( inList.isType<std::string>(key) ) {
+      std::string value = inList.get<std::string>( key );
+      setParameter( outList, location, itvec, value );
     }
     else {
-
-      if( inList.isType<bool>(key) ) {
-        bool value = inList.get<bool>( key );
-        outList.set( key, value );
-      }
-      else if( inList.isType<int>(key) ) {
-        int value = inList.get<int>( key );
-        outList.set( key, value );
-      }
-      else if( inList.isType<double>(key) ) {
-        double value = inList.get<double>( key );
-        outList.set( key, value );
-      }
-      else if( inList.isType<std::string>(key) ) {
-        std::string value = inList.get<std::string>( key );
-        outList.set( key, value );
-      }
-      else {
-        TEUCHOS_TEST_FOR_EXCEPTION( true, std::invalid_argument,
-                                    ">>>Error: Unhandled parameter type." );  
-      }
-    }   
+      TEUCHOS_TEST_FOR_EXCEPTION( true, std::invalid_argument,
+                                  ">>>Error: Unhandled parameter type." );  
+    }
+ 
+   
   }
 }
 
-/// TODO: Produce a heirarchical parameter list from a flat list
+} // namespace ROL
 
 #endif 
