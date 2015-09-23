ae095a7f5073262abef2d62c8f53c53ae6f24e20
Tobias Wiesner <tawiesn@sandia.gov>
2015-09-23 09:02:07 -0600

    MueLu: do not extract git sha1 string as version number for tutorial autogeneration script
    Need a better way to extract Trilinos version...

--- packages/muelu/doc/Tutorial/tex/prepareTexTutorial.py
+++ packages/muelu/doc/Tutorial/tex/prepareTexTutorial.py
@@ -129,12 +129,12 @@ if __name__ == '__main__':
 
   # here starts the preparation script
 
-  print bcolors.OKDARKGREEN + "Extract version number from git repository... " + bcolors.ENDC
-  # create version file
-  cmd = "rm version.txt"
-  runCommand(cmd)
-  cmd = "git log --pretty=format:'%h' -n 1 > version.txt"
-  runCommand(cmd)
+  #print bcolors.OKDARKGREEN + "Extract version number from git repository... " + bcolors.ENDC
+  ## create version file
+  #cmd = "rm version.txt"
+  #runCommand(cmd)
+  #cmd = "git log --pretty=format:'%h' -n 1 > version.txt"
+  #runCommand(cmd)
 
   print bcolors.OKDARKGREEN + "Split source files in src folder for inclusion in pdf... " + bcolors.ENDC
   # split cpp file for first example
