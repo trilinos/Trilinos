89b43c42a0490ebba939e2cf8ddd5407c48d0913
Nick Roehrig <nsroehr@sandia.gov>
2015-09-29 15:51:54 -0600

    refactoring observer
    (From Sierra)

--- packages/stk/stk_unit_tests/stk_mesh/UnitTestCommInfoObserver.cpp
+++ packages/stk/stk_unit_tests/stk_mesh/UnitTestCommInfoObserver.cpp
@@ -37,109 +37,127 @@ private:
     std::vector<bool> commInfoWasChangedByRank;
 };
 
-void ghost_element1_to_proc1(stk::mesh::BulkData &bulk, stk::mesh::Ghosting &ghost)
+class CommInfoObserverTest : public ::testing::Test
 {
-    stk::mesh::EntityProcVec entitiesToGhost;
-    if(bulk.parallel_rank() == 0)
+protected:
+    CommInfoObserverTest()
+    : meta(), bulk(meta, MPI_COMM_WORLD, stk::mesh::BulkData::NO_AUTO_AURA)
     {
-        stk::mesh::Entity element1 = bulk.get_entity(stk::topology::ELEM_RANK, 1);
-        int destProc = 1;
-        entitiesToGhost.push_back(stk::mesh::EntityProc(element1, destProc));
+        stk::unit_test_util::fill_mesh_using_stk_io("generated:1x1x4", bulk, MPI_COMM_WORLD);
+
+        observer = new CommInfoObserver();
+        bulk.register_observer(observer);
+
+        bulk.modification_begin();
+        ghost = &bulk.create_ghosting("Clyde");
+        bulk.modification_end();
     }
-    bulk.modification_begin();
-    bulk.change_ghosting(ghost, entitiesToGhost);
-    bulk.modification_end();
-}
 
-void expect_comm_info_changed_for_nodes_and_elements_only(const CommInfoObserver &observer)
-{
-    EXPECT_TRUE(observer.was_comm_info_changed(stk::topology::NODE_RANK));
-    EXPECT_TRUE(observer.was_comm_info_changed(stk::topology::ELEM_RANK));
-
-    EXPECT_TRUE(!observer.was_comm_info_changed(stk::topology::EDGE_RANK));
-    EXPECT_TRUE(!observer.was_comm_info_changed(stk::topology::FACE_RANK));
-    EXPECT_TRUE(!observer.was_comm_info_changed(stk::topology::CONSTRAINT_RANK));
-}
-
-void expect_comm_info_is_reset(CommInfoObserver &observer)
-{
-    for(stk::mesh::EntityRank rank = stk::topology::BEGIN_RANK; rank < stk::topology::END_RANK; rank++)
+    virtual ~CommInfoObserverTest()
     {
-        EXPECT_TRUE(!observer.was_comm_info_changed(rank));
+        delete observer;
     }
-}
 
-void stop_ghosting_of_element1_to_proc1(stk::mesh::BulkData &bulk, stk::mesh::Ghosting &ghost)
-{
-    std::vector<stk::mesh::EntityKey> entityKeysToStopGhosting;
-    if(bulk.parallel_rank() == 1)
+    void ghost_element1_to_proc1_and_check_for_comm_info_change()
     {
-        entityKeysToStopGhosting.push_back(stk::mesh::EntityKey(stk::topology::ELEM_RANK, 1));
-        for(int nodeId=1; nodeId <= 8; nodeId++)
+        ghost_element1_to_proc1();
+        expect_comm_info_changed_for_nodes_and_elements_only();
+    }
+
+    void expect_comm_info_changed_for_nodes_and_elements_only()
+    {
+        EXPECT_TRUE(observer->was_comm_info_changed(stk::topology::NODE_RANK));
+        EXPECT_TRUE(observer->was_comm_info_changed(stk::topology::ELEM_RANK));
+
+        EXPECT_TRUE(!observer->was_comm_info_changed(stk::topology::EDGE_RANK));
+        EXPECT_TRUE(!observer->was_comm_info_changed(stk::topology::FACE_RANK));
+        EXPECT_TRUE(!observer->was_comm_info_changed(stk::topology::CONSTRAINT_RANK));
+    }
+
+    void stop_ghosting_of_element1_to_proc1()
+    {
+        std::vector<stk::mesh::EntityKey> entityKeysToStopGhosting;
+        if(bulk.parallel_rank() == 1)
         {
-            entityKeysToStopGhosting.push_back(stk::mesh::EntityKey(stk::topology::NODE_RANK, nodeId));
+            entityKeysToStopGhosting.push_back(stk::mesh::EntityKey(stk::topology::ELEM_RANK, 1));
+            for(int nodeId=1; nodeId <= 8; nodeId++)
+            {
+                entityKeysToStopGhosting.push_back(stk::mesh::EntityKey(stk::topology::NODE_RANK, nodeId));
+            }
         }
-    }
-    bulk.modification_begin();
-    bulk.change_ghosting(ghost, {}, entityKeysToStopGhosting);
-    bulk.modification_end();
-}
-
-TEST(CommInfoObserver, addAndRemoveFromGhosting)
-{
-    MPI_Comm comm = MPI_COMM_WORLD;
-    if(stk::parallel_machine_size(comm) == 2)
-    {
-        stk::mesh::MetaData meta;
-        stk::mesh::BulkData bulk(meta, comm, stk::mesh::BulkData::NO_AUTO_AURA);
-        stk::unit_test_util::fill_mesh_using_stk_io("generated:1x1x4", bulk, comm);
-
-        CommInfoObserver observer;
-        bulk.register_observer(&observer);
-
         bulk.modification_begin();
-        stk::mesh::Ghosting &ghost = bulk.create_ghosting("Clyde");
+        bulk.change_ghosting(*ghost, {}, entityKeysToStopGhosting);
         bulk.modification_end();
-
-        ghost_element1_to_proc1(bulk, ghost);
-        expect_comm_info_changed_for_nodes_and_elements_only(observer);
-
-        observer.reset_comm_info_status();
-        expect_comm_info_is_reset(observer);
-
-        stop_ghosting_of_element1_to_proc1(bulk, ghost);
-
-        expect_comm_info_changed_for_nodes_and_elements_only(observer);
     }
-}
 
-TEST(CommInfoObserver, destroyGhosting)
-{
-    MPI_Comm comm = MPI_COMM_WORLD;
-    if(stk::parallel_machine_size(comm) == 2)
+    void reset_comm_info_status()
     {
-        stk::mesh::MetaData meta;
-        stk::mesh::BulkData bulk(meta, comm, stk::mesh::BulkData::NO_AUTO_AURA);
-        stk::unit_test_util::fill_mesh_using_stk_io("generated:1x1x4", bulk, comm);
+        observer->reset_comm_info_status();
+        expect_comm_info_is_reset();
+    }
 
-        CommInfoObserver observer;
-        bulk.register_observer(&observer);
-
-        bulk.modification_begin();
-        stk::mesh::Ghosting &ghost = bulk.create_ghosting("Clyde");
-        bulk.modification_end();
-
-        ghost_element1_to_proc1(bulk, ghost);
-        expect_comm_info_changed_for_nodes_and_elements_only(observer);
-
-        observer.reset_comm_info_status();
-        expect_comm_info_is_reset(observer);
-
+    void destroy_ghosting()
+    {
         bulk.modification_begin();
         bulk.destroy_all_ghosting();
         bulk.modification_end();
+    }
 
-        expect_comm_info_changed_for_nodes_and_elements_only(observer);
+private:
+    void ghost_element1_to_proc1()
+    {
+        stk::mesh::EntityProcVec entitiesToGhost;
+        if(bulk.parallel_rank() == 0)
+        {
+            stk::mesh::Entity element1 = bulk.get_entity(stk::topology::ELEM_RANK, 1);
+            int destProc = 1;
+            entitiesToGhost.push_back(stk::mesh::EntityProc(element1, destProc));
+        }
+        bulk.modification_begin();
+        bulk.change_ghosting(*ghost, entitiesToGhost);
+        bulk.modification_end();
+    }
+
+    void expect_comm_info_is_reset()
+    {
+        for(stk::mesh::EntityRank rank = stk::topology::BEGIN_RANK; rank < stk::topology::END_RANK; rank++)
+        {
+            EXPECT_TRUE(!observer->was_comm_info_changed(rank));
+        }
+    }
+
+private:
+    stk::mesh::MetaData meta;
+    stk::mesh::BulkData bulk;
+    CommInfoObserver *observer;
+    stk::mesh::Ghosting *ghost;
+};
+
+TEST_F(CommInfoObserverTest, addAndRemoveFromGhosting)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    if(stk::parallel_machine_size(comm) == 2)
+    {
+        ghost_element1_to_proc1_and_check_for_comm_info_change();
+
+        reset_comm_info_status();
+
+        stop_ghosting_of_element1_to_proc1();
+        expect_comm_info_changed_for_nodes_and_elements_only();
+    }
+}
+
+TEST_F(CommInfoObserverTest, destroyGhosting)
+{
+    MPI_Comm comm = MPI_COMM_WORLD;
+    if(stk::parallel_machine_size(comm) == 2)
+    {
+        ghost_element1_to_proc1_and_check_for_comm_info_change();
+
+        reset_comm_info_status();
+
+        destroy_ghosting();
+        expect_comm_info_changed_for_nodes_and_elements_only();
     }
 }
 
