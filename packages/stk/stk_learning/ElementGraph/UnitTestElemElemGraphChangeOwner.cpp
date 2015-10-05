7193fed761bcb50b60943715baf54e568ffb4015
cwilson <cwilson@sandia.gov>
2015-10-05 15:53:45 -0600

    IWYU for new file.
    (From Sierra)

--- packages/stk/stk_learning/ElementGraph/UnitTestElemElemGraphChangeOwner.cpp
+++ packages/stk/stk_learning/ElementGraph/UnitTestElemElemGraphChangeOwner.cpp
@@ -1,48 +1,32 @@
-#include <gtest/gtest.h>
 
-#include <vector>
-#include <algorithm>
-#include <stdlib.h>
-
-#include <stk_topology/topology.hpp>
-#include <stk_mesh/base/BulkData.hpp>
-#include <stk_mesh/base/MetaData.hpp>
-#include <stk_mesh/base/Comm.hpp>
-#include <stk_mesh/base/GetEntities.hpp>
-#include <stk_mesh/base/FEMHelpers.hpp>
-#include <stk_mesh/baseImpl/MeshImplUtils.hpp>
-#include <stk_mesh/base/FieldTraits.hpp>
-#include <stk_mesh/base/Field.hpp>
-
-#include <stk_mesh/base/SkinMesh.hpp>
-#include <stk_mesh/base/CreateFaces.hpp>
-#include <stk_mesh/base/ElemElemGraph.hpp>
-#include <stk_mesh/base/ElemElemGraphImpl.hpp>
-
-#include <stk_util/parallel/Parallel.hpp>
-#include <stk_util/parallel/ParallelVectorConcat.hpp>
-#include <stk_util/parallel/ParallelComm.hpp>
-#include <stk_util/environment/WallTime.hpp>
+#include <gtest/gtest.h>                // for AssertHelper, EXPECT_EQ, etc
+#include <stddef.h>                     // for size_t
+#include <ostream>                      // for basic_ostream::operator<<, etc
+#include <stdexcept>                    // for logic_error
+#include <stk_mesh/base/BulkData.hpp>   // for BulkData, etc
+#include <stk_mesh/base/ElemElemGraph.hpp>  // for change_entity_owner, etc
+#include <stk_mesh/base/ElemElemGraphImpl.hpp>  // for parallel_info
+#include <stk_mesh/base/FEMHelpers.hpp>  // for declare_element
+#include <stk_mesh/base/Field.hpp>      // for Field
+#include <stk_mesh/base/GetEntities.hpp>  // for count_entities
+#include <stk_mesh/base/MetaData.hpp>   // for MetaData, entity_rank_names, etc
+#include <stk_topology/topology.hpp>    // for topology, etc
+#include <stk_unit_test_utils/ioUtils.hpp>  // for fill_mesh_using_stk_io
+#include <stk_util/environment/WallTime.hpp>  // for wall_time
 #include <stk_util/environment/memory_util.hpp>
-#include <stk_util/parallel/CommSparse.hpp>
-#include <stk_util/environment/ReportHandler.hpp>
-
-#include <stk_io/IossBridge.hpp>
-#include <stk_io/StkMeshIoBroker.hpp>   // for StkMeshIoBroker
-
-#include <stk_unit_test_utils/ioUtils.hpp>
-#include <stk_unit_test_utils/getOption.h>
-
-#include "UnitTestElementDeathUtils.hpp"
+#include <stk_util/parallel/Parallel.hpp>  // for parallel_machine_rank, etc
+#include <string>                       // for string
+#include <utility>                      // for pair, make_pair
+#include <vector>                       // for vector
+#include "ElementGraphTester.hpp"       // for ElemElemGraphTester
+#include "gtest/gtest-message.h"        // for Message
+#include "mpi.h"                        // for MPI_COMM_WORLD, MPI_Comm, etc
+#include "stk_mesh/base/Bucket.hpp"     // for Bucket
+#include "stk_mesh/base/BulkDataInlinedMethods.hpp"
+#include "stk_mesh/base/Entity.hpp"     // for Entity
+#include "stk_mesh/base/Types.hpp"      // for PartVector, EntityId, etc
 #include "stk_unit_test_utils/unittestMeshUtils.hpp"
-#include <stk_unit_tests/stk_mesh/SetupKeyholeMesh.hpp>
-
-#include <stk_mesh/fixtures/QuadFixture.hpp>  // for QuadFixture
-#include <stk_mesh/fixtures/heterogeneous_mesh.hpp>
-#include <stk_mesh/fixtures/degenerate_mesh.hpp>
-
-#include "BulkDataElementGraphTester.hpp"
-#include "ElementGraphTester.hpp"
+namespace stk { namespace mesh { class Part; } }
 
 namespace {
 
