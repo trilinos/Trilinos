5077c911db174f3e03187fcbf8028d926add7592
cwilson <cwilson@sandia.gov>
2015-10-07 12:35:45 -0600

    Refactor SideIdPool out of element graph.
    (From Sierra)

---
+++ packages/stk/stk_mesh/stk_mesh/base/SideIdPool.hpp
@@ -0,0 +1,40 @@
+#ifndef STK_SIDE_ID_POOL_HPP
+#define STK_SIDE_ID_POOL_HPP
+
+#include <stk_mesh/base/Types.hpp>
+
+namespace stk { namespace mesh { class BulkData; } }
+
+namespace stk
+{
+namespace mesh
+{
+
+class SideIdPool
+{
+public:
+    SideIdPool(stk::mesh::BulkData &bulk);
+
+    void generate_initial_ids(unsigned numIdsNeeded);
+
+    stk::mesh::EntityId get_available_id();
+
+    void generate_additional_ids_collective(size_t num_additional_ids_needed);
+
+    void reset_suggested_side_id_iter(size_t numIdsNotReallyUsed);
+
+protected:
+    const stk::mesh::EntityIdVector &get_all_ids()
+    {
+        return mSuggestedIds;
+    }
+
+private:
+    stk::mesh::BulkData &mBulkData;
+    stk::mesh::EntityIdVector mSuggestedIds;
+    size_t mNumIdsUsed;
+};
+
+}}
+
+#endif
