94f3b2a9e321fb2f8349a88e25df04c6d0cd415e
Greg Sjaardema <gsjaardema@gmail.com>
2015-10-13 10:34:29 -0400

    SEACAS: Update cmake-related config files to use cmake to build standalone seacas instead of imake
    (From Sierra)

--- packages/seacas/libraries/ioss/cmake/Dependencies.cmake
+++ packages/seacas/libraries/ioss/cmake/Dependencies.cmake
@@ -1,8 +1,31 @@
+if(${CMAKE_PROJECT_NAME} STREQUAL "SEACAS")
+  SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
+    Ioss        src               PT  REQUIRED
+    Ioex        src/exodus        PT  OPTIONAL
+    Iofx        src/exo_fpp       PT  OPTIONAL
+    Iopx        src/exo_par       PT  OPTIONAL
+    Ioexo_fac   src/exo_fac       PT  OPTIONAL
+    Iogn        src/generated     PT  REQUIRED
+    Iopg        src/pamgen        PT  OPTIONAL
+    Iohb        src/heartbeat     PT  REQUIRED
+    Iotr        src/transform     PT  REQUIRED
+    Ionit       src/init          PT  REQUIRED
+    IoMain	src/main          PT  REQUIRED
+    IoUtest	src/utest         PT  REQUIRED
+    )
+
+  SET(LIB_OPTIONAL_DEP_PACKAGES Exodus)
+  SET(LIB_OPTIONAL_DEP_TPLS XDMF HDF5 Pamgen Zoltan MPI)
+
+else()
+  SET(LIB_OPTIONAL_DEP_PACKAGES SEACASExodus Pamgen Zoltan)
+  SET(LIB_OPTIONAL_DEP_TPLS XDMF HDF5)
+endif()
+
 SET(LIB_REQUIRED_DEP_PACKAGES)
-SET(LIB_OPTIONAL_DEP_PACKAGES SEACASExodus Pamgen Zoltan)
 SET(TEST_REQUIRED_DEP_PACKAGES)
 SET(TEST_OPTIONAL_DEP_PACKAGES)
 SET(LIB_REQUIRED_DEP_TPLS)
-SET(LIB_OPTIONAL_DEP_TPLS XDMF HDF5)
 SET(TEST_REQUIRED_DEP_TPLS)
 SET(TEST_OPTIONAL_DEP_TPLS)
+
