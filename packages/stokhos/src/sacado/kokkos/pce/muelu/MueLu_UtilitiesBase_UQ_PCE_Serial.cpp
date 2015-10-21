15bc4c1416b74ad946737b369ef095210a7a4cee
Eric Phipps <etphipp@sandia.gov>
2015-10-21 13:42:19 -0600

    Stokhos:  Update MueLu ETI files for Stokhos scalar types.
    Should fix build errors on the dashboard that have shown up the last few
    days.

--- packages/stokhos/src/sacado/kokkos/pce/muelu/MueLu_UtilitiesBase_UQ_PCE_Serial.cpp
+++ packages/stokhos/src/sacado/kokkos/pce/muelu/MueLu_UtilitiesBase_UQ_PCE_Serial.cpp
@@ -50,17 +50,17 @@
 #include "Stokhos_Tpetra_ETI_Helpers_UQ_PCE.hpp"
 #include "Stokhos_MueLu_UQ_PCE.hpp"
 
-#include "MueLu_Utilities_def.hpp"
+#include "MueLu_UtilitiesBase_def.hpp"
 
 #define MUELU_INST_S_LO_GO_N(S, LO, GO, N) \
-  template class MueLu::Utilities<S, LO, GO, N>;
+  template class MueLu::UtilitiesBase<S, LO, GO, N>;
 
 #define MUELU_INST_N(N) \
   INSTANTIATE_TPETRA_UQ_PCE_N(MUELU_INST_S_LO_GO_N, N)
 
 TPETRA_ETI_MANGLING_TYPEDEFS()
 
-INSTANTIATE_TPETRA_UQ_PCE_CUDA(MUELU_INST_N)
+INSTANTIATE_TPETRA_UQ_PCE_SERIAL(MUELU_INST_N)
 
 #endif
 
