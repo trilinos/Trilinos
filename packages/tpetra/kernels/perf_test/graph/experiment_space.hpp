c0775af12b29045a72ed0e301ee2de1b356527e2
Mehmet Deveci <mndevec@sandia.gov>
2015-11-04 16:20:46 -0700

    Tpetra: Kernels -- added graph coloring, gauss seidel implementation, and a PCG example.

---
+++ packages/tpetra/kernels/perf_test/graph/experiment_space.hpp
@@ -0,0 +1,52 @@
+//#define CUDA
+#define OPENMP
+
+
+#ifdef CUDA
+#define REPEAT 5
+typedef Kokkos::Cuda MyExecSpace;
+typedef Kokkos::Cuda MyMemorySpace;
+
+#ifdef CUDAPINNEDSPACE
+  typedef Kokkos::CudaHostPinnedSpace MyEdgeMemorySpace;
+#else
+  typedef Kokkos::Cuda MyEdgeMemorySpace;
+#endif
+
+#endif
+
+#ifdef OPENMP
+#define REPEAT 10
+typedef Kokkos::OpenMP MyExecSpace;
+typedef Kokkos::OpenMP MyMemorySpace;
+typedef Kokkos::OpenMP MyEdgeMemorySpace;
+
+typedef Kokkos::OpenMP TemporaryWorkSpace;
+typedef Kokkos::OpenMP PersistentWorkSpace;
+#endif
+
+
+
+typedef unsigned int idx;
+typedef double wt;
+typedef unsigned int color_type;
+typedef unsigned long long int color_type_eb;
+
+
+typedef Kokkos::View<idx *, MyMemorySpace> idx_array_type;
+typedef Kokkos::View<idx *, MyEdgeMemorySpace> idx_edge_array_type;
+typedef Kokkos::View<wt *, MyEdgeMemorySpace> value_array_type;
+
+typedef Kokkos::View<color_type_eb *, MyMemorySpace> color_eb_array_type;
+typedef Kokkos::View<color_type * , MyMemorySpace> color_array_type;
+
+typedef Kokkos::View<idx *, MyMemorySpace::array_layout, Kokkos::Serial, Kokkos::MemoryUnmanaged> um_array_type;
+typedef Kokkos::View<idx *, MyMemorySpace::array_layout, MyEdgeMemorySpace, Kokkos::MemoryUnmanaged> um_edge_array_type;
+
+
+typedef Kokkos::View<wt *, MyMemorySpace::array_layout, MyEdgeMemorySpace, Kokkos::MemoryUnmanaged> wt_um_edge_array_type;
+
+
+typedef Kokkos::View<wt *, MyMemorySpace::array_layout, Kokkos::Serial, Kokkos::MemoryUnmanaged> um_value_array_type;
+
+
