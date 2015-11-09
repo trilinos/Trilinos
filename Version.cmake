c21f3a0349408adad6a2b6d3b04efa8438a1c6ef
Brent Perschbacher <bmpersc@sandia.gov>
2015-11-09 12:42:39 -0700

    Update version to 12.4.2

--- Version.cmake
+++ Version.cmake
@@ -59,10 +59,10 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 12.4.1)
+SET(Trilinos_VERSION 12.4.2)
 SET(Trilinos_MAJOR_VERSION 12)
-SET(Trilinos_MAJOR_MINOR_VERSION 120401)
-SET(Trilinos_VERSION_STRING "12.4.1")
+SET(Trilinos_MAJOR_MINOR_VERSION 120402)
+SET(Trilinos_VERSION_STRING "12.4.2")
 SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
