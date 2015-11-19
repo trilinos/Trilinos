6230721e121d07b31af1c841dc87b058975bf629
Jonathan Hu <jhu@sandia.gov>
2015-11-19 12:31:05 -0800

    MueLu: remove scalar from 3-argument ETI header
    Thanks to Andrey for pointing this out.

--- packages/muelu/src/Utils/MueLu_ETI_3arg.hpp
+++ packages/muelu/src/Utils/MueLu_ETI_3arg.hpp
@@ -2,7 +2,7 @@
 #define MUELU_ETI_3ARGUMENT_HPP
 
 #if defined(HAVE_MUELU_EPETRA)  && !defined(HAVE_MUELU_TPETRA)
-  MUELU_ETI_GROUP(double,int,int,Kokkos::Serial);
+  MUELU_ETI_GROUP(int,int,Kokkos::Serial);
 #endif
 
 #if defined(HAVE_MUELU_EPETRA) && defined(HAVE_MUELU_TPETRA)
@@ -12,7 +12,7 @@
   TPETRA_ETI_MANGLING_TYPEDEFS()
   TPETRA_INSTANTIATE_LGN(MUELU_ETI_GROUP) 
 #if !defined(HAVE_TPETRA_INST_SERIAL) || !defined(HAVE_TPETRA_INST_INT_INT)
-    MUELU_ETI_GROUP(double,int,int,Kokkos::Serial);
+    MUELU_ETI_GROUP(int,int,Kokkos::Serial);
 # endif
 
 #endif
