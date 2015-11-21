02d9c6a447f5453579ff932fa10960cebbd6bd5b
Andrey Prokopenko <aprokop@sandia.gov>
2015-11-20 20:16:40 -0700

    MueLu: changed requirements for Stratimikos adapter
    We have two versions of a function to enable MueLu in Stratimikos
    (Stratimikos::enableMueLu): non-templated and templated. The first one
    is supposed to be used for Epetra, while the second one only for Tpetra.
    
    The issue, though, is that there is nothing preventing running the
    non-templated version for Tpetra, as that is a runtime decision.
    However, the non-templated version assumes a specific set of parameters:
    <double, int, int> with a serial node. This is exposed when trying to
    compile MueLu with Tpetra without that instantiation, resulting in
    undefined linkage with Tpetra's <double,int,int> objects.
    
    The change made throws an exception if Tpetra is enabled, but without
    that particular instantiation. That means that one is not able to run
    both Epetra through non-templated enableMueLu and Tpetra through templated
    enableMueLu at the same time.
    
    I am not convinced that this is a right way to fix the issue.

--- packages/muelu/adapters/stratimikos/Stratimikos_MueLuHelpers.cpp
+++ packages/muelu/adapters/stratimikos/Stratimikos_MueLuHelpers.cpp
@@ -47,6 +47,8 @@
 
 #include "Stratimikos_MueLuHelpers.hpp"
 
+#include "MueLu_ConfigDefs.hpp"
+
 #include "Thyra_MueLuPreconditionerFactory.hpp"
 #if defined(HAVE_MUELU_EXPERIMENTAL) && defined(HAVE_MUELU_TEKO)
 #include "Thyra_MueLuTpetraQ2Q1PreconditionerFactory.hpp"
@@ -60,21 +62,20 @@
 namespace Stratimikos {
 
   void enableMueLu(DefaultLinearSolverBuilder &builder, const std::string &stratName) {
+#if (defined(HAVE_MUELU_EPETRA) && !defined(HAVE_MUELU_TPETRA)) || \
+    (defined(HAVE_MUELU_TPETRA) && defined(HAVE_TPETRA_INST_INT_INT) && defined(HAVE_TPETRA_INST_SERIAL))
     const Teuchos::RCP<const Teuchos::ParameterList> precValidParams = Teuchos::sublist(builder.getValidParameters(), "Preconditioner Types");
 
     TEUCHOS_TEST_FOR_EXCEPTION(precValidParams->isParameter(stratName), std::logic_error,
                                "Stratimikos::enableMueLu cannot add \"" + stratName +"\" because it is already included in builder!");
 
     typedef Thyra::PreconditionerFactoryBase<double>                  Base;
-#if defined(HAVE_MUELU_TPETRA) && defined(HAVE_MUELU_TPETRA_INST_INT_INT)
-    typedef Thyra::MueLuPreconditionerFactory<double, int, int> Impl;
-    builder.setPreconditioningStrategyFactory(Teuchos::abstractFactoryStd<Base, Impl>(), stratName);
-#elif defined(HAVE_MUELU_EPETRA) && not defined(HAVE_MUELU_TPETRA)
     typedef Thyra::MueLuPreconditionerFactory<double, int, int> Impl;
     builder.setPreconditioningStrategyFactory(Teuchos::abstractFactoryStd<Base, Impl>(), stratName);
 #else
     TEUCHOS_TEST_FOR_EXCEPTION(true, std::logic_error,
-                               "Stratimikos::enableMueLu cannot add \"" + stratName +"\" because MueLu is compiled without support for GO=int.\n Add support for GO=int in Tpetra or compile only for Epetra.");
+      "Stratimikos::enableMueLu cannot add \"" + stratName +"\" because MueLu is compiled without support for "
+      "GO=int and/or NO=Serial.\n Add support for GO=int and NO=Serial in Tpetra or compile only for Epetra.");
 #endif
   }
 
