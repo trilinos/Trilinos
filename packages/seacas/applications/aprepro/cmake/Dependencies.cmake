f1bf057a470f6005400879b4b0e48ed69e71a3ca
Greg Sjaardema <gsjaardema@gmail.com>
2015-11-30 15:50:39 -0500

    SEACAS: Fix some bad trilinos build files in previous commit
    (From Sierra)

--- packages/seacas/applications/aprepro/cmake/Dependencies.cmake
+++ packages/seacas/applications/aprepro/cmake/Dependencies.cmake
@@ -1,8 +1,4 @@
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
-<<<<<<< Updated upstream
-  LIB_REQUIRED_PACKAGES SEACASSuplib
-=======
   LIB_REQUIRED_PACKAGES SEACASAprepro_lib SEACASSuplib
->>>>>>> Stashed changes
 )
 
