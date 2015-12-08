cbba372be762fb2d01fbe89524e70ccc4026d5b4
Eric Phipps <etphipp@sandia.gov>
2015-12-08 11:54:31 -0700

    Sacado:  Updates to ParameterAccessor/Registration
    A few additions to the Sacado ParameterAccessor/Registration classes
    that were helpful for Sparc.  Should be backwards compatible with
    previous usage.

--- packages/sacado/src/parameter/Sacado_ParameterRegistration.hpp
+++ packages/sacado/src/parameter/Sacado_ParameterRegistration.hpp
@@ -1,30 +1,29 @@
-// $Source$ 
 // @HEADER
 // ***********************************************************************
-// 
+//
 //                           Sacado Package
 //                 Copyright (2006) Sandia Corporation
-// 
+//
 // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
 // the U.S. Government retains certain rights in this software.
-// 
+//
 // This library is free software; you can redistribute it and/or modify
 // it under the terms of the GNU Lesser General Public License as
 // published by the Free Software Foundation; either version 2.1 of the
 // License, or (at your option) any later version.
-//  
+//
 // This library is distributed in the hope that it will be useful, but
 // WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 // Lesser General Public License for more details.
-//  
+//
 // You should have received a copy of the GNU Lesser General Public
 // License along with this library; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301
 // USA
 // Questions? Contact David M. Gay (dmgay@sandia.gov) or Eric T. Phipps
 // (etphipp@sandia.gov).
-// 
+//
 // ***********************************************************************
 // @HEADER
 
@@ -34,16 +33,17 @@
 #include <sstream>
 #include "Sacado_ParameterAccessor.hpp"
 #include "Sacado_Traits.hpp"
-
+#include "Sacado_ScalarParameterLibrary.hpp"
 
 namespace Sacado {
+
   /*!
    * @brief Parameter class for simple registration of a
    * parameter with a Parameter Library. Requires a parameter
    * name a ParameterAccessor object.
    */
   template <typename EvalType, typename EvalTypeTraits = DefaultEvalTypeTraits>
-  class ParameterRegistration : 
+  class ParameterRegistration :
     public Sacado::ScalarParameterEntry<EvalType, EvalTypeTraits > {
 
   //! Scalar type
@@ -52,9 +52,24 @@ namespace Sacado {
 
   public:
 
+    typedef ScalarParameterLibrary<EvalTypeTraits> ParamLib;
+
     //! Constructor: Registers the parameter with the Parameter Library
-    ParameterRegistration(const std::string &name_, ParameterAccessor<EvalType, EvalTypeTraits>* access_,
-                            Teuchos::RCP<ParamLib> paramLib)
+    ParameterRegistration(const std::string &name_,
+                          ParameterAccessor<EvalType, EvalTypeTraits>* access_,
+                          ParamLib& paramLib)
+      : access(access_), name(name_) {
+
+      if (!paramLib.isParameter(name))
+        paramLib.addParameterFamily(name, true, false);
+      if (!paramLib.template isParameterForType<EvalType>(name))
+        paramLib.template addEntry<EvalType>(name, Teuchos::rcp(this,false));
+    }
+
+    //! Constructor: Registers the parameter with the Parameter Library
+    ParameterRegistration(const std::string &name_,
+                          ParameterAccessor<EvalType, EvalTypeTraits>* access_,
+                          const Teuchos::RCP<ParamLib>& paramLib)
       : access(access_), name(name_) {
 
       if (paramLib != Teuchos::null) {
@@ -70,25 +85,27 @@ namespace Sacado {
     virtual ~ParameterRegistration() {}
 
     //! Set real parameter value
-    virtual void setRealValue(double value) { 
+    virtual void setRealValue(double value) {
       setValue(ScalarT(value)); }
 
     //! Set parameter values using ParameterAccessor
-    virtual void setValue(const ScalarT& value) { 
-      access->getValue(name) =  value; }
-    
+    virtual void setValue(const ScalarT& value) {
+      access->setValue(name, value);
+    }
+
     //! Get parameter value using ParameterAccessor
-    virtual const ScalarT& getValue() const { 
-      return access->getValue(name); 
-     }
-    
-  protected:  
-    
+    virtual const ScalarT& getValue() const {
+      return access->getValue(name);
+    }
+
+  protected:
+
     //! Pointer to source function
-     ParameterAccessor<EvalType, EvalTypeTraits>* access;
-     const std::string name;
+    ParameterAccessor<EvalType, EvalTypeTraits>* access;
+    const std::string name;
 
   };
+
 }
 
 #endif // SACADO_PARAMETERREGISTRATION_HPP
