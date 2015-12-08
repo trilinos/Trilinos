0f2898274eeb7548a0beaa45742b1e6ffa23306f
cwilson <cwilson@sandia.gov>
2015-12-07 17:32:58 -0700

    Move PerformanceTester to its own file.
    (From Sierra)

--- packages/stk/stk_performance_tests/stk_mesh/perfSkinMesh.cpp
+++ packages/stk/stk_performance_tests/stk_mesh/perfSkinMesh.cpp
@@ -16,47 +16,59 @@ size_t get_num_global_faces(const stk::mesh::BulkData &bulk)
     return meshCounts[stk::topology::FACE_RANK];
 }
 
-class SkinMeshPerformance : public stk::unit_test_util::MeshFixture, public stk::unit_test_util::PerformanceTester
+class SkinMeshPerformance : public stk::unit_test_util::PerformanceTester
 {
-protected:
-    SkinMeshPerformance() :
-            stk::unit_test_util::MeshFixture(),
-            stk::unit_test_util::PerformanceTester(get_comm()),
-            thingToSkin(get_meta().universal_part()),
-            skinPart(get_meta().declare_part("skinPart"))
+public:
+    SkinMeshPerformance(stk::mesh::BulkData &bulk) :
+            stk::unit_test_util::PerformanceTester(bulk.parallel()),
+            bulkData(bulk),
+            thingToSkin(bulk.mesh_meta_data().universal_part()),
+            skinPart(bulk.mesh_meta_data().declare_part("skinPart"))
     {
     }
+
+protected:
     virtual void run_algorithm_to_time()
     {
-        stk::mesh::skin_mesh(get_bulk(), thingToSkin, {&skinPart});
+        stk::mesh::skin_mesh(bulkData, thingToSkin, {&skinPart});
     }
     virtual size_t get_value_to_output_as_iteration_count()
     {
-        return get_num_global_faces(get_bulk());
+        return get_num_global_faces(bulkData);
     }
 
+    stk::mesh::BulkData &bulkData;
     stk::mesh::Selector thingToSkin;
     stk::mesh::Part &skinPart;
 };
 
-std::string get_mesh_spec()
+class SkinMeshPerformanceTest : public stk::unit_test_util::MeshFixture
 {
-    return unitTestUtils::getOption("-file", "NO_FILE_SPECIFIED");
-}
+protected:
+    void run_skin_mesh_perf_test()
+    {
+        SkinMeshPerformance perfTester(get_bulk());
+        perfTester.run_performance_test();
+    }
+    std::string get_mesh_spec()
+    {
+        return unitTestUtils::getOption("-file", "NO_FILE_SPECIFIED");
+    }
+};
 
-TEST_F(SkinMeshPerformance, read_mesh_with_auto_decomp)
+TEST_F(SkinMeshPerformanceTest, read_mesh_with_auto_decomp)
 {
     allocate_bulk(stk::mesh::BulkData::AUTO_AURA);
     stk::unit_test_util::read_from_serial_file_and_decompose(get_mesh_spec(), get_bulk(), "rcb");
 
-    run_performance_test();
+    run_skin_mesh_perf_test();
 }
 
-TEST_F(SkinMeshPerformance, read_mesh)
+TEST_F(SkinMeshPerformanceTest, read_mesh)
 {
     setup_mesh(get_mesh_spec(), stk::mesh::BulkData::AUTO_AURA);
 
-    run_performance_test();
+    run_skin_mesh_perf_test();
 }
 
 }
