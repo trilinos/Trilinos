981de976178e23e6f1d063cd8a24d00d25cf14ae
Eric Phipps <etphipp@sandia.gov>
2015-12-16 07:04:00 -0700

    Belos:  Fix some calls to BLAS::AXPY() for ensemble scalar type.
    The alpha parameter of BLAS::AXPY() is a general template parameter,
    which can cause a problem if that argument is passed as an expression
    for expression-template-based scalar types.  I fixed this by casting it
    back to the scalar type, which is alway safe and incurs no cost for
    builtin scalar types.

--- packages/belos/src/BelosBlockGmresIter.hpp
+++ packages/belos/src/BelosBlockGmresIter.hpp
@@ -818,7 +818,7 @@ class BlockGmresIter : virtual public GmresIteration<ScalarType,MV,OP> {
           sigma = blas.DOT( blockSize_, &(*R_)(i+1,i), 1, &(*R_)(i+1,curDim+j), 1);
           sigma += (*R_)(i,curDim+j);
           sigma *= beta[i];
-          blas.AXPY(blockSize_, -sigma, &(*R_)(i+1,i), 1, &(*R_)(i+1,curDim+j), 1);
+          blas.AXPY(blockSize_, ScalarType(-sigma), &(*R_)(i+1,i), 1, &(*R_)(i+1,curDim+j), 1);
           (*R_)(i,curDim+j) -= sigma;
         }
         //
@@ -853,7 +853,7 @@ class BlockGmresIter : virtual public GmresIteration<ScalarType,MV,OP> {
                             1, &(*z_)(curDim+j+1,i), 1);
           sigma += (*z_)(curDim+j,i);
           sigma *= beta[curDim+j];
-          blas.AXPY(blockSize_, -sigma, &(*R_)(curDim+j+1,curDim+j),
+          blas.AXPY(blockSize_, ScalarType(-sigma), &(*R_)(curDim+j+1,curDim+j),
                     1, &(*z_)(curDim+j+1,i), 1);
           (*z_)(curDim+j,i) -= sigma;
         }
