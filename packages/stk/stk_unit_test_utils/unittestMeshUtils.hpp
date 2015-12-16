edcc23241ca9ba6a6765f61beb0503c480e4bdb8
Alan Williams <william@sandia.gov>
2015-12-16 13:14:52 -0700

    remove a circular dependency between stk_unit_tests and stk_unit_test_utils.
    moved a function from stk_unit_test_utils into stk_unit_tests
    
    (From Sierra)

--- packages/stk/stk_unit_test_utils/unittestMeshUtils.hpp
+++ packages/stk/stk_unit_test_utils/unittestMeshUtils.hpp
@@ -10,7 +10,6 @@ namespace stk
 namespace unit_test_util
 {
 
-
 void put_mesh_into_part(stk::mesh::BulkData& bulkData, stk::mesh::Part& part);
 
 std::string get_name_of_generated_mesh(int xdim, int ydim, int zdim, const std::string &options);
@@ -18,9 +17,6 @@ std::string get_name_of_generated_mesh(int xdim, int ydim, int zdim, const std::
 void move_killed_elements_out_of_parts(stk::mesh::BulkData& bulkData,
                                   const stk::mesh::EntityVector& killedElements,
                                   const stk::mesh::PartVector& removeParts);
-
-
-void convert_quad_fixture_to_my_bulk_data_flavor(unsigned numX, unsigned numY, stk::mesh::BulkData* bulkData);
 
 } // namespace unit_test_util
 } // namespace stk
