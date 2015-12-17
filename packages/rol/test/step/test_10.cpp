8fe1b78d3583207694b3cd7d893e6c6441e051d5
Drew Kouri <dpkouri@sandia.gov>
2015-12-16 17:48:22 -0700

    ROL: Added the capability to transform a BoundConstraint into an InequalityConstraint.
    OptimizationProblem will do this transformation automatically if interior points is used.
    
    step/test_10.cpp tests this capability using HS38.

---
+++ packages/rol/test/step/test_10.cpp
@@ -0,0 +1,137 @@
+// @HEADER
+// ************************************************************************
+//
+//               Rapid Optimization Library (ROL) Package
+//                 Copyright (2014) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact lead developers:
+//              Drew Kouri   (dpkouri@sandia.gov) and
+//              Denis Ridzal (dridzal@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+
+/*! \file  test_10.cpp
+    \brief Test bound constrainted interior point.
+*/
+
+#define USE_HESSVEC 1
+
+#include "ROL_TestObjectives.hpp"
+#include "ROL_Algorithm.hpp"
+#include "Teuchos_oblackholestream.hpp"
+#include "Teuchos_GlobalMPISession.hpp"
+#include "Teuchos_XMLParameterListHelpers.hpp"
+
+#include <iostream>
+
+typedef double RealT;
+
+int main(int argc, char *argv[]) {
+
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv);
+
+  // This little trick lets us print to std::cout only if a (dummy) command-line argument is provided.
+  int iprint     = argc - 1;
+  Teuchos::RCP<std::ostream> outStream;
+  Teuchos::oblackholestream bhs; // outputs nothing
+  if (iprint > 0)
+    outStream = Teuchos::rcp(&std::cout, false);
+  else
+    outStream = Teuchos::rcp(&bhs, false);
+
+  int errorFlag  = 0;
+
+  // *** Test body.
+
+  try {
+    // Get Objective Function
+    Teuchos::RCP<ROL::Vector<RealT> > x0, z;
+    Teuchos::RCP<ROL::Objective<RealT> > obj;
+    Teuchos::RCP<ROL::BoundConstraint<RealT> > con;
+    ROL::getTestObjectives<RealT>(obj,con,x0,z,ROL::TESTOPTPROBLEM_HS38);
+    Teuchos::RCP<ROL::Vector<RealT> > x = x0->clone();;
+    x->set(*x0);
+    // Parse input
+    std::string filename = "input.xml";
+    Teuchos::RCP<Teuchos::ParameterList> parlist = Teuchos::rcp( new Teuchos::ParameterList() );
+    Teuchos::updateParametersFromXmlFile( filename, parlist.ptr() );
+    parlist->sublist("General").set("Inexact Hessian-Times-A-Vector",true);
+#if USE_HESSVEC
+    parlist->sublist("General").set("Inexact Hessian-Times-A-Vector",false);
+#endif
+
+    RealT mu = 0.1;            // Initial penalty parameter
+    RealT factor = 0.1;        // Penalty reduction factor
+
+    // Set solver parameters
+    parlist->sublist("Step").set("Type","Interior Point");
+    parlist->sublist("Step").sublist("Interior Point").set("Initial Barrier Penalty",mu);
+    parlist->sublist("Step").sublist("Interior Point").set("Minimium Barrier Penalty",1e-8);
+    parlist->sublist("Step").sublist("Interior Point").set("Barrier Penalty Reduction Factor",factor);
+    parlist->sublist("Step").sublist("Interior Point").set("Subproblem Iteration Limit",30);
+
+    parlist->sublist("Step").sublist("Composite Step").sublist("Optimality System Solver").set("Nominal Relative Tolerance",1.e-4);
+    parlist->sublist("Step").sublist("Composite Step").sublist("Optimality System Solver").set("Fix Tolerance",true);
+    parlist->sublist("Step").sublist("Composite Step").sublist("Tangential Subproblem Solver").set("Iteration Limit",20);
+    parlist->sublist("Step").sublist("Composite Step").sublist("Tangential Subproblem Solver").set("Relative Tolerance",1e-2);
+    parlist->sublist("Step").sublist("Composite Step").set("Output Level",0);
+
+    parlist->sublist("Status Test").set("Gradient Tolerance",1.e-12);
+    parlist->sublist("Status Test").set("Constraint Tolerance",1.e-8);
+    parlist->sublist("Status Test").set("Step Tolerance",1.e-8);
+    parlist->sublist("Status Test").set("Iteration Limit",100);
+
+    // Solve optimization problem with interior points
+    ROL::OptimizationProblem<RealT> optProb(obj,x,con,parlist);
+    ROL::Algorithm<RealT> algo("Interior Point",*parlist,false);
+    algo.run(optProb, true, *outStream);
+
+    // Compute Error
+    z->axpy(-1.0,*x);
+    *outStream << std::endl << "Norm of Error: " << z->norm() << std::endl;
+  }
+  catch (std::logic_error err) {
+    *outStream << err.what() << std::endl;
+    errorFlag = -1000;
+  }; // end try
+
+  if (errorFlag != 0)
+    std::cout << "End Result: TEST FAILED" << std::endl;
+  else
+    std::cout << "End Result: TEST PASSED" << std::endl;
+
+  return 0;
+
+}
+
