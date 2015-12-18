f626215f0666fe2472dea6b6b630579df8a8fd25
Mark Hoemmen <mhoemme@sandia.gov>
2015-12-17 23:36:23 -0700

    Ifpack2::Experimental::RBILUK: Removed unnecessary header include.

--- packages/ifpack2/src/Ifpack2_Experimental_RBILUK_def.hpp
+++ packages/ifpack2/src/Ifpack2_Experimental_RBILUK_def.hpp
@@ -49,9 +49,6 @@
 #include <Ifpack2_Experimental_RBILUK.hpp>
 #include <Ifpack2_Utilities.hpp>
 #include <Ifpack2_RILUK.hpp>
-// Need to include this if using the "classic" version of Tpetra,
-// since it may not get included in that case.
-#include <Kokkos_ArithTraits.hpp>
 
 namespace Ifpack2 {
 
