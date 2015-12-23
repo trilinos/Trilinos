55e9f0d4a1b39f7e49a13a8e0c456e7c2fe2f44f
Manoj K Bhardwaj <mkbhard@sandia.gov>
2015-12-23 11:25:57 -0700

    Changes for team Tupek and his DG element team.
    (From Sierra)

--- packages/stk/stk_learning/DGElement/UnitTestDisconnectMesh.cpp
+++ packages/stk/stk_learning/DGElement/UnitTestDisconnectMesh.cpp
@@ -6,6 +6,9 @@
 #include <stk_mesh/base/FieldBase.hpp>
 #include <stk_mesh/base/Field.hpp>
 #include <stk_mesh/baseImpl/elementGraph/ElemElemGraph.hpp>
+#include <stk_mesh/base/FEMHelpers.hpp>
+
+#include <stk_util/parallel/ParallelReduce.hpp>
 
 #include <stk_unit_test_utils/ioUtils.hpp>
 
@@ -13,7 +16,7 @@
 
 class DisconnectMesh : public DGTetFixture {};
 
-void createSerialSubMesh(const stk::mesh::MetaData &oldMeta,
+void disconnectMesh(const stk::mesh::MetaData &oldMeta,
                          stk::mesh::BulkData& oldBulkData,
                          stk::mesh::Selector subMeshSelector,
                          stk::mesh::MetaData &newMeta,
@@ -40,7 +43,7 @@ TEST_F(DisconnectMesh, tet)
 
     stk::mesh::MetaData newMetaData(get_meta().spatial_dimension());
     stk::mesh::BulkData newBulkData(newMetaData, get_bulk().parallel());
-    createSerialSubMesh(get_meta(), get_bulk(), get_meta().universal_part(), newMetaData, newBulkData);
+    disconnectMesh(get_meta(), get_bulk(), get_meta().universal_part(), newMetaData, newBulkData);
 
     stk::unit_test_util::write_mesh_using_stk_io("mike_new.g", newBulkData, newBulkData.parallel());
 }
@@ -156,11 +159,13 @@ void copyFieldDataToNewMesh(const stk::mesh::MetaData &oldMeta,
                             const stk::mesh::BulkData& oldBulkData,
                             const stk::mesh::Selector subMeshSelector,
                             const std::map<stk::mesh::Entity, stk::mesh::Entity> &oldToNewEntityMap,
-                            stk::mesh::MetaData &newMeta)
+                            stk::mesh::MetaData &newMeta,
+                            const stk::mesh::BulkData& newBulkData,
+                            const stk::mesh::EntityVector &node_in_old_mesh)
 {
     const stk::mesh::FieldVector &oldFields = oldMeta.get_fields();
     const stk::mesh::FieldVector &newFields = newMeta.get_fields();
-    for(stk::mesh::EntityRank rank = stk::topology::NODE_RANK; rank <= stk::topology::ELEMENT_RANK; rank++)
+    for(stk::mesh::EntityRank rank = stk::topology::EDGE_RANK; rank <= stk::topology::ELEMENT_RANK; rank++)
     {
         const stk::mesh::BucketVector &buckets = oldBulkData.get_buckets(rank, subMeshSelector);
         for(size_t i = 0; i < buckets.size(); i++)
@@ -182,9 +187,30 @@ void copyFieldDataToNewMesh(const stk::mesh::MetaData &oldMeta,
             }
         }
     }
+
+    const stk::mesh::BucketVector &buckets = newBulkData.get_buckets(stk::topology::NODE_RANK, subMeshSelector);
+    for(size_t i = 0; i < buckets.size(); i++)
+    {
+        stk::mesh::Bucket &bucket = *buckets[i];
+        for(size_t k = 0; k < newFields.size(); k++)
+        {
+            if(bucket.field_data_is_allocated(*newFields[k]))
+            {
+                for(size_t j = 0; j < bucket.size(); j++)
+                {
+                    stk::mesh::Entity newEntity = bucket[j];
+                    stk::mesh::Entity oldEntity = node_in_old_mesh[newBulkData.identifier(newEntity)-1];
+                    void *oldData = stk::mesh::field_data(*oldFields[k], oldEntity);
+                    void *newData = stk::mesh::field_data(*newFields[k], newEntity);
+                    memcpy(newData, oldData, stk::mesh::field_bytes_per_entity(*oldFields[k], oldEntity));
+                }
+            }
+        }
+    }
 }
 
-void createSerialSubMesh(const stk::mesh::MetaData &oldMeta,
+
+void disconnectMesh(const stk::mesh::MetaData &oldMeta,
                          stk::mesh::BulkData& oldBulkData,
                          stk::mesh::Selector subMeshSelector,
                          stk::mesh::MetaData &newMeta,
@@ -195,12 +221,70 @@ void createSerialSubMesh(const stk::mesh::MetaData &oldMeta,
 
     newMeta.commit();
 
+    const stk::mesh::PartVector &allOldparts = oldMeta.get_mesh_parts();
+    const stk::mesh::PartVector &allNewparts = newMeta.get_mesh_parts();
+    ThrowRequireMsg(allOldparts.size() == allNewparts.size(), "Error. Contact support.");
+
     std::map<stk::mesh::Entity, stk::mesh::Entity> oldToNewEntityMap;
 
+    stk::mesh::EntityVector node_in_old_mesh;
+    stk::mesh::EntityVector elements;
+    stk::mesh::get_selected_entities(oldMeta.locally_owned_part(), oldBulkData.buckets(stk::topology::ELEM_RANK), elements);
+    size_t num_nodes_needed = 0;
+    for(stk::mesh::Entity element : elements )
+    {
+        unsigned num_nodes_this_element = oldBulkData.num_nodes(element);
+        const stk::mesh::Entity* nodes = oldBulkData.begin_nodes(element);
+        num_nodes_needed += num_nodes_this_element;
+        for(unsigned int i=0;i<num_nodes_this_element;++i)
+        {
+            node_in_old_mesh.push_back(nodes[i]);
+        }
+    }
+
+    std::vector<size_t> num_nodes_needed_per_proc(oldBulkData.parallel_size(),0);
+    num_nodes_needed_per_proc[oldBulkData.parallel_rank()] = num_nodes_needed;
+    stk::all_reduce_sum(oldBulkData.parallel(), num_nodes_needed_per_proc.data(), num_nodes_needed_per_proc.data(), num_nodes_needed_per_proc.size());
+
+    size_t offset_this_proc = 0;
+    for(int i=0;i<oldBulkData.parallel_rank();++i)
+        offset_this_proc += num_nodes_needed_per_proc[i];
+
+    stk::mesh::EntityVector new_nodes(num_nodes_needed);
     newBulkData.modification_begin();
-    copySelectedEntitiesToNewMesh(oldBulkData, subMeshSelector, oldToNewEntityMap, newMeta, newBulkData);
-    copyRelationsToNewMesh(oldBulkData, subMeshSelector, oldToNewEntityMap, newBulkData);
+    for(size_t i=0;i<num_nodes_needed;++i)
+    {
+        const stk::mesh::PartVector parts = oldBulkData.bucket(node_in_old_mesh[i]).supersets();
+        new_nodes[i] = newBulkData.declare_entity(stk::topology::NODE_RANK, i+offset_this_proc+1, parts);
+    }
     newBulkData.modification_end();
 
-    copyFieldDataToNewMesh(oldMeta, oldBulkData, subMeshSelector, oldToNewEntityMap, newMeta);
+    newBulkData.modification_begin();
+    size_t node_counter = 0;
+    for(stk::mesh::Entity element : elements)
+    {
+        stk::mesh::PartVector parts = oldBulkData.bucket(element).supersets();
+
+        int index_root_part = -1;
+        for(size_t i=0;i<parts.size();++i)
+        {
+            if(stk::mesh::is_topology_root_part(*parts[i]))
+            {
+                index_root_part = i;
+                break;
+            }
+        }
+        ThrowRequireMsg(index_root_part !=-1, "Oops");
+
+        std::swap(parts[0], parts[index_root_part]);
+
+        stk::mesh::EntityIdVector node_ids(oldBulkData.num_nodes(element));
+        for(size_t i=0;i<node_ids.size();++i)
+            node_ids[i] = node_counter + i + offset_this_proc + 1;
+        node_counter += node_ids.size();
+        stk::mesh::declare_element(newBulkData, parts, oldBulkData.identifier(element), node_ids);
+    }
+    newBulkData.modification_end();
+
+    copyFieldDataToNewMesh(oldMeta, oldBulkData, subMeshSelector, oldToNewEntityMap, newMeta, newBulkData, node_in_old_mesh);
 }
