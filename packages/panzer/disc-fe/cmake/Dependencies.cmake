1a67741227ef8d9db484cbb2b0490005265342e4
avdelor <avdelor@sandia.gov>
2016-01-05 13:02:53 -0700

    Intrepid2: split intrepid and intrepid2 into different subpackages. Changed the names of all of the ifdefs using sed script added the copyright headers to any files that did not have it Fixed issue with teuchos rcp map to physical fram Updated panzer and phalanx to reflect intrepid split

--- packages/panzer/disc-fe/cmake/Dependencies.cmake
+++ packages/panzer/disc-fe/cmake/Dependencies.cmake
@@ -1,4 +1,4 @@
-SET(LIB_REQUIRED_DEP_PACKAGES TeuchosCore TeuchosParameterList TeuchosComm KokkosCore Sacado Phalanx IntrepidIntrepid2 ThyraCore ThyraTpetraAdapters ThyraEpetraAdapters ThyraEpetraExtAdapters Tpetra Epetra EpetraExt Zoltan PanzerCore PanzerDofMgr)
+SET(LIB_REQUIRED_DEP_PACKAGES TeuchosCore TeuchosParameterList TeuchosComm KokkosCore Sacado Phalanx Intrepid2 ThyraCore ThyraTpetraAdapters ThyraEpetraAdapters ThyraEpetraExtAdapters Tpetra Epetra EpetraExt Zoltan PanzerCore PanzerDofMgr)
 SET(LIB_OPTIONAL_DEP_PACKAGES)
 SET(TEST_REQUIRED_DEP_PACKAGES)
 SET(TEST_OPTIONAL_DEP_PACKAGES)
