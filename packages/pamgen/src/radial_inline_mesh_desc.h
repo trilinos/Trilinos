6a47b6340614483c51983515d9311512e792be8b
Mark Hoemmen <mhoemme@sandia.gov>
2016-01-11 06:20:54 +0800

    Pamgen: Fix build warning (struct prev. decl'd as a class)
    .../pamgen/src/Vector.h:11:1: warning: 'Vector' defined as a struct here but previously declared as a class [-Wmismatched-tags]
    struct Vector
    ^
    .../pamgen/src/radial_inline_mesh_desc.h:7:1: note: did you mean struct here?
    class Vector;
    ^~~~~
    struct
    
    I fixed this by changing the forward declaration (in the second
    header file) to 'struct' instead of 'class'.
    
    Build/Test Cases Summary
    Enabled Packages: Amesos2, Pamgen, TpetraCore, Triutils
    Disabled Packages: FEI,STK,PyTrilinos,Intrepid2
    0) MPI_DEBUG => passed: passed=82,notpassed=0 (0.36 min)
    1) SERIAL_RELEASE => FAILED: passed=129,notpassed=2 => Not ready to push! (0.18 min)
    Other local commits for this build/test group: d69fcaf, ebecac8, e37e7bf, ad0ca28, 68e77d5, f86d47a, 669ce89, 2ee9d70, 42e4735, d2f733a
    WARNING: Forced the push!

--- packages/pamgen/src/radial_inline_mesh_desc.h
+++ packages/pamgen/src/radial_inline_mesh_desc.h
@@ -4,7 +4,7 @@
 
 namespace PAMGEN_NEVADA {
 
-class Vector;
+struct Vector;
 class Radial_Inline_Mesh_Desc : public Inline_Mesh_Desc
 {
 public:
@@ -13,18 +13,18 @@ public:
   virtual ~Radial_Inline_Mesh_Desc(){};
   virtual long long Set_Up();
   virtual std::string Calc_Intervals();
-  virtual void calculateSize(long long & total_el_count, 
-			     long long & total_node_count, 
-			     long long & total_edge_count);
-//   virtual long long Calc_Coord_Vectors(); 
+  virtual void calculateSize(long long & total_el_count,
+                             long long & total_node_count,
+                             long long & total_edge_count);
+//   virtual long long Calc_Coord_Vectors();
   Vector calc_coords_periodic(double total_theta,
-			      long long i, 
-			      long long j, 
-			      long long k);
-  virtual void Populate_Coords(double * coords,   
-		       std::vector<long long> & global_node_vector, 
-		       std::map <long long, long long> & global_node_map,
-		       long long num_nodes);
+                              long long i,
+                              long long j,
+                              long long k);
+  virtual void Populate_Coords(double * coords,
+                       std::vector<long long> & global_node_vector,
+                       std::map <long long, long long> & global_node_map,
+                       long long num_nodes);
 };
 }// end namespace
 #endif
