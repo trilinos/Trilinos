2af5ff181eab1eabd9859f1414b5bef140c51d37
Andrew M. Bradley <ambradl@sandia.gov>
2016-01-19 17:21:38 -0700

    ShyLU: Bring HTS up to date for integration work.

--- packages/shylu/cmake/Dependencies.cmake
+++ packages/shylu/cmake/Dependencies.cmake
@@ -2,7 +2,7 @@ SET(SUBPACKAGES_DIRS_CLASSIFICATIONS_OPTREQS
   #SubPackageName       Directory         Class    Req/Opt
   #
   # New ShyLU subpackages:
-  GTS                   gts               EX       OPTIONAL
+  HTS                   hts               EX       OPTIONAL
   Tacho                 tacho             EX       OPTIONAL
   BDDC                  bddc              EX       OPTIONAL
   Basker                basker            EX       OPTIONAL
---
+++ packages/shylu/hts/src/shylu_hts.hpp
@@ -0,0 +1,8 @@
+#ifndef INCLUDE_SHYLU_HTS_HPP
+#define INCLUDE_SHYLU_HTS_HPP
+
+#include "ShyLUHTS_config.h"
+#include "shylu_hts_decl.hpp"
+#include "shylu_hts_impl_def.hpp"
+
+#endif
