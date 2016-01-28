96cc387e2780c86e80d950b8da5cc3a2a028d54f
Greg Sjaardema <gdsjaar@sandia.gov>
2016-01-28 13:56:44 -0700

    SEACAS: gen3d - Handle mirrored tri-wedge sidesets correctly
    (From Sierra)

--- packages/seacas/applications/gen3d/gen3d.f
+++ packages/seacas/applications/gen3d/gen3d.f
@@ -92,6 +92,10 @@ C   --   "User's Manual for GEN3D"
       CHARACTER*(MXSTLN) NAMECO(6)
 C      --NAMECO - the coordinate names
 
+C... String containing name of common element topology in model
+C    or 'MULTIPLE_TOPOLOGIES' if not common topology.
+      character*(MXSTLN) comtop
+
       CHARACTER CDUM
       logical l64bit
 
@@ -249,6 +253,8 @@ C ... Don't warn about no map stored in file
      &  IA(KNATR), IA(KLINK), KATRIB, *60)
       CALL MDSTAT (NERR, MEM)
       IF (NERR .GT. 0) GOTO 40
+
+      call CHKTOP(NELBLK, C(KNMLB), COMTOP)
 
       if (numnps .gt. 0) then
          call exgcns(ndbin, ia(kidns), ia(knnns), ia(kndnps),
@@ -540,8 +546,15 @@ C   --Write the node sets
 
 C   --Fixup sides sets if mirrored
       IF (XMIRR * YMIRR * ZMIRR .LT. 0.0) THEN
-         CALL MIRSS (IDESET(0,1), IDESET(0,2),
-     &        NESUR, IA(KISFRO), IA(KISBCK), IA(KLTSS3))
+        CALL MDRSRV ('IDXELB', KIDXELB, NELBLK+1)
+        CALL MDSTAT (NERR, MEM)
+        IF (NERR .GT. 0) GOTO 40
+        CALL MIRSS (IDESET(0,1), IDESET(0,2),
+     &    NESUR, IA(KISFRO), IA(KISBCK), IA(KLTES3), IA(KLTSS3),
+     *    COMTOP, C(KNMLB), IA(KNELB), IA(KIDXELB))
+        CALL MDDEL ('IDXELB')
+        CALL MDSTAT (NERR, MEM)
+        IF (NERR .GT. 0) GOTO 40
       END IF
 C     --Write the side sets
       
@@ -617,3 +630,23 @@ C     --Write the side sets
       call exginf(ndb, info, ierr)
       return
       end
+
+C...Check whether model contains elements of a single topology.
+C   This is currently used in the sideset mirroring code
+      subroutine chktop(nelblk, blktyp, comtop)
+
+      include 'exodusII.inc'
+      integer nelblk
+      character*(MXSTLN) blktyp(nelblk)
+      character*(MXSTLN) comtop
+      
+      comtop = blktyp(1)(:3)
+      do 10 i=2, nelblk
+         if (blktyp(i)(:3) .ne. comtop(:3)) then
+            comtop = 'MULTIPLE_TOPOLOGIES'
+            return
+         end if
+ 10   continue
+      return
+      end
+
