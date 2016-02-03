ff19ecb1e70ff2c1a4930108159b029e030c88a2
James M. Willenbring <jmwille@sandia.gov>
2016-02-03 15:28:09 -0700

    More XYCE build configuration changes.

--- cmake/ctest/drivers/rhel6-x86_64/TrilinosCTestDriverCore.rhel6-x86_64.gcc4.4.7.cmake
+++ cmake/ctest/drivers/rhel6-x86_64/TrilinosCTestDriverCore.rhel6-x86_64.gcc4.4.7.cmake
@@ -54,7 +54,7 @@
 # @HEADER
 
   
-INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../TrilinosCTestDriverCore.cmake")
+INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../../TrilinosCTestDriverCore.cmake")
 
 #
 # Platform/compiler specific options for trilinos-test using gcc
