bf1befdf242cc16277b1f348a298de87c32ba2a5
Tobias Wiesner <tawiesn@sandia.gov>
2016-02-05 14:15:22 -0700

    MueLu: get rid of the non-templated version of enableMueLu in the Stratimikos interface
    We use default template parameters <double, int, int, KokkosClassic::DefaultNode::DefaultNodeType>

--- packages/muelu/adapters/stratimikos/Stratimikos_MueLuHelpers.cpp
+++ packages/muelu/adapters/stratimikos/Stratimikos_MueLuHelpers.cpp
@@ -61,24 +61,6 @@
 
 namespace Stratimikos {
 
-  void enableMueLu(DefaultLinearSolverBuilder &builder, const std::string &stratName) {
-#if (defined(HAVE_MUELU_EPETRA) && !defined(HAVE_MUELU_TPETRA)) || \
-    (defined(HAVE_MUELU_TPETRA) && defined(HAVE_TPETRA_INST_INT_INT) && defined(HAVE_TPETRA_INST_SERIAL))
-    const Teuchos::RCP<const Teuchos::ParameterList> precValidParams = Teuchos::sublist(builder.getValidParameters(), "Preconditioner Types");
-
-    TEUCHOS_TEST_FOR_EXCEPTION(precValidParams->isParameter(stratName), std::logic_error,
-                               "Stratimikos::enableMueLu cannot add \"" + stratName +"\" because it is already included in builder!");
-
-    typedef Thyra::PreconditionerFactoryBase<double>                  Base;
-    typedef Thyra::MueLuPreconditionerFactory<double, int, int> Impl;
-    builder.setPreconditioningStrategyFactory(Teuchos::abstractFactoryStd<Base, Impl>(), stratName);
-#else
-    TEUCHOS_TEST_FOR_EXCEPTION(true, std::logic_error,
-      "Stratimikos::enableMueLu cannot add \"" + stratName +"\" because MueLu is compiled without support for "
-      "GO=int and/or NO=Serial.\n Add support for GO=int and NO=Serial in Tpetra or compile only for Epetra.");
-#endif
-  }
-
 #if defined(HAVE_MUELU_EXPERIMENTAL) && defined(HAVE_MUELU_TEKO)
 #if 0
   void enableMueLuTpetraQ2Q1(DefaultLinearSolverBuilder &builder, const std::string &stratName) {
