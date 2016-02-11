a3491ac6803ecc8e9fc0a5208dd7d88038235c42
Greg Sjaardema <gdsjaar@sandia.gov>
2016-02-11 08:03:27 -0700

    SEACAS: chaco - remove old copyright  notice
    (From Sierra)

--- packages/seacas/libraries/chaco/misc/make_subgoal.c
+++ packages/seacas/libraries/chaco/misc/make_subgoal.c
@@ -32,9 +32,6 @@
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  * 
  */
-/* This software was developed by Bruce Hendrickson and Robert Leland   *
- * at Sandia National Laboratories under US Department of Energy        *
- * contract DE-AC04-76DP00789 and is copyrighted by Sandia Corporation. */
 
 /* Use same ratios as original goals, but adjust based on set sizes. */
 /* Note: mesh stuff only works for division into two sets. */
