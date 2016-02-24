fdb03479a3f4e469ced3bfdb455ce8ce58ee425e
bddavid <davidson@txcorp.com>
2016-02-24 11:33:08 -0700

    Added debugging block for printing ordering solution to cout.

--- packages/zoltan2/test/driver/test_driver.cpp
+++ packages/zoltan2/test/driver/test_driver.cpp
@@ -323,7 +323,44 @@ void run(const UserInputForTests &uinput,
     reinterpret_cast<partitioning_problem_t *>(problem)->printMetrics(cout);
     cout << "PASSED." << endl;
   }
-  
+
+#define BDD
+#ifdef BDD 
+  if (problem_kind == "ordering") {
+    std:cout << "\nLet's examine the solution..." << std::endl;
+    auto solution = reinterpret_cast<ordering_problem_t *>(problem)->getSolution();
+    std::ostringstream sol;
+    sol << "Number of column blocks: " << solution->getSepColBlockCount() << std::endl;
+
+    if (solution->getPermutationSize() < 100) {
+      if (solution->havePerm()) {
+        sol << "permutation: {";
+        for (auto &x : solution->getPermutationRCPConst(false)) sol << " " << x;
+        sol << "}" << std::endl;
+      }
+     
+     if (solution->haveInverse()) { 
+        sol << "inverse permutation: {";
+        for (auto &x : solution->getPermutationRCPConst(true)) sol << " " << x;
+        sol << "}" << std::endl;
+     }
+      
+     if (solution->haveSepRange()) {
+        sol << "separator range: {";
+        for (auto &x : solution->getSepRangeRCPConst()) sol << " " << x;
+        sol << "}" << std::endl;
+     }
+     
+      if (solution->haveSepTree()) { 
+        sol << "separator tree: {";
+        for (auto &x : solution->getSepTreeRCPConst()) sol << " " << x;
+        sol << "}" << std::endl;
+      }
+    }
+
+    std::cout << sol.str() << std::endl;
+  }
+#endif
   // 4b. timers
 //  if(zoltan2_parameters.isParameter("timer_output_stream"))
 //    reinterpret_cast<partitioning_problem_t *>(problem)->printTimers();
