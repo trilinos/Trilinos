295cdaa642013c5f9a35b56e876b6c3d9d7b255a
Joshua Dennis Booth <jdbooth@sandia.gov>
2016-03-02 14:09:15 -0700

    Amesos2: (Basker--Experimental) Added Optional Parameters

--- packages/amesos2/src/Amesos2_Basker_def.hpp
+++ packages/amesos2/src/Amesos2_Basker_def.hpp
@@ -91,11 +91,16 @@ Basker<Matrix,Vector>::Basker(
   	"Kokkos node type not supported by experimental Basker Amesos2");
   typedef Kokkos::OpenMP Exe_Space;
   basker = new ::BaskerNS::Basker<local_ordinal_type, slu_type, Exe_Space>(); 
-  basker->Options.no_pivot  = true;
-  basker->Options.symmetric = false;
-  basker->Options.realloc   = false;
-  basker->Options.verbose   = false;
-  basker->Options.btf       = true;
+  basker->Options.no_pivot      = BASKER_TRUE;
+  basker->Options.symmetric     = BASKER_FALSE;
+  basker->Options.realloc       = BASKER_FALSE;
+  basker->Options.verbose       = BASKER_FALSE;
+  basker->Options.matching      = BASKER_TRUE;
+  basker->Options.matching_type = 0;
+  basker->Options.btf           = BASKER_TRUE;
+  basker->Options.amd_btf       = BASKER_TRUE;
+  basker->Options.amd_dom       = BASKER_TRUE;
+  basker->Options.transpose     = BASKER_FALSE;
   num_threads = Kokkos::OpenMP::max_hardware_threads();
 #endif  
 #endif
@@ -155,7 +160,10 @@ Basker<Matrix,Vector>::symbolicFactorization_impl()
                             rowind_.getRawPtr(), 
                             nzvals_.getRawPtr());
       //std::cout << "Symbolic Factorization Done" << std::endl; 
-      
+      TEUCHOS_TEST_FOR_EXCEPTION(info != 0,
+				 std::runtime_error,
+				 "Error in Basker Symbolic");
+ 
     }
 #endif
  
@@ -193,6 +201,11 @@ Basker<Matrix,Vector>::numericFactorization_impl()
                            colptr_.getRawPtr(), 
                            rowind_.getRawPtr(), 
                            nzvals_.getRawPtr());
+
+      TEUCHOS_TEST_FOR_EXCEPTION(info != 0, 
+				 std::runtime_error,
+				 "Error Basker Factor");
+
 #else
      
       info =basker.factor(this->globalNumRows_, this->globalNumCols_, this->globalNumNonZeros_, colptr_.getRawPtr(), rowind_.getRawPtr(), nzvals_.getRawPtr());
@@ -310,11 +323,55 @@ Basker<Matrix,Vector>::setParameters_impl(const Teuchos::RCP<Teuchos::ParameterL
   using Teuchos::ParameterEntryValidator;
 
   RCP<const Teuchos::ParameterList> valid_params = getValidParameters_impl();
-  /*To Do --- add support for parameters */
+
+#ifdef SHYLUBASKER
   if(parameterList->isParameter("num_threads"))
     {
       num_threads = parameterList->get<int>("num_threads");
     }
+  if(parameterList->isParameter("no_pivot"))
+    {
+      basker->Options.no_pivot = parameterList->get<bool>("no_pivot");
+    }
+  if(parameterList->isParameter("symmetric"))
+    {
+      basker->Options.symmetric = parameterList->get<bool>("symmetric");
+    }
+  if(parameterList->isParameter("realloc"))
+    {
+      basker->Options.realloc = parameterList->get<bool>("realloc");
+    }
+  if(parameterList->isParameter("verbose"))
+    {
+      basker->Options.verbose = parameterList->get<bool>("verbose");
+    }
+  if(parameterList->isParameter("matching"))
+    {
+      basker->Options.matching = parameterList->get<bool>("matching");
+    }
+  if(parameterList->isParameter("matching_type"))
+    {
+      basker->Options.matching_type =
+	(local_ordinal_type) parameterList->get<int>("matching_type");
+    }
+  if(parameterList->isParameter("btf"))
+    {
+      basker->Options.btf = parameterList->get<bool>("btf");
+    }
+  if(parameterList->isParameter("amd_btf"))
+    {
+      basker->Options.amd_btf = parameterList->get<bool>("amd_btf");
+    }
+  if(parameterList->isParameter("amd_dom"))
+    {
+      basker->Options.amd_dom = parameterList->get<bool>("amd_dom");
+    }
+  if(parameterList->isParameter("transpose"))
+    {
+      basker->Options.transpose = parameterList->get<bool>("transpose");
+    }
+#endif
+
 }
 
 template <class Matrix, class Vector>
@@ -330,8 +387,29 @@ Basker<Matrix,Vector>::getValidParameters_impl() const
   if( is_null(valid_params) )
     {
       Teuchos::RCP<Teuchos::ParameterList> pl = Teuchos::parameterList();
-      pl->set("num_threads", 1, "Number of threads");
-      //This is all I am supporting right this second in experimental
+      pl->set("num_threads", 1, 
+	      "Number of threads");
+      pl->set("no_pivot", true,
+	      "Should  not pivot");
+      pl->set("symmetric", false,
+	      "Should Symbolic assume symmetric nonzero pattern");
+      pl->set("realloc" , false, 
+	      "Should realloc space if not enough");
+      pl->set("verbose", false,
+	      "Give information (Not Supported)");
+      pl->set("matching", true,
+	      "Use WC matching (Not Supported)");
+      pl->set("matching_type", 0, 
+	      "Type of WC matching (Not Supported)");
+      pl->set("btf", true, 
+	      "Use BTF ordering");
+      pl->set("amd_btf", true,
+	      "Use AMD on BTF blocks (Not Supported)");
+      pl->set("amd_dom", true,
+	      "Use CAMD on ND blocks (Not Supported)");
+      pl->set("transpose", false,
+	      "Solve the transpose A");
+      valid_params = pl;
     }
   return valid_params;
 #else
