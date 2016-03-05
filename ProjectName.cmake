1bf003981337b3e67609fc8c08fca8361539dff1
Roscoe A. Bartlett <rabartl@sandia.gov>
2016-03-05 12:27:53 -0700

    Add MOOCHO as an inserted package (Trilinos #176)
    This should serve as a template for adding other pacakges as inserted
    packages.
    
    See trilinos/Trilinos#176

--- ProjectName.cmake
+++ ProjectName.cmake
@@ -2,3 +2,6 @@ SET(PROJECT_NAME Trilinos)
 
 SET(${PROJECT_NAME}_ENABLE_SECONDARY_TESTED_CODE  ON  CACHE  BOOL
   "Set by default in Trilinos ProjectName.cmake." )
+
+SET(${PROJECT_NAME}_GENERATE_REPO_VERSION_FILE  ON CACHE BOOL
+  "Set by default in Trilinos ProjectName.cmake." )
