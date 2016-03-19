ea98660e61031130acea936eb13c8007e30ce33b
Vitus Leung <vjleung@sandia.gov>
2016-03-18 20:28:09 -0600

    Zoltan2:  commented out some code

--- packages/zoltan2/test/helpers/Zoltan2_MetricAnalyzer.hpp
+++ packages/zoltan2/test/helpers/Zoltan2_MetricAnalyzer.hpp
@@ -108,7 +108,6 @@ public:
                               const RCP<const Comm<int>> &comm,
                               std::ostringstream &msg_stream) {
 
-    //ArrayRCP<const metric_t> metrics = (problem)->getMetrics();
       ArrayRCP<const metric_t> metrics = (metricObject)->getMetrics();
       
       string test_name;
