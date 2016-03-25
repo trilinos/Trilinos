cd64a93ba0c96d7a52fc34dbd42ea6a0ecfb3f09
Drew Kouri <dpkouri@sandia.gov>
2016-03-25 12:55:26 -0600

    ROL: Updated plot density script for 2D topology optimization example.

--- packages/rol/example/PDE-OPT/elasticitySIMP_topologyOptimization/plotdensity.m
+++ packages/rol/example/PDE-OPT/elasticitySIMP_topologyOptimization/plotdensity.m
@@ -1,10 +1,21 @@
 data_obj = importdata('density.txt', ' ', 2);  %% we need to skip the first two lines
 density = data_obj.data;
+nodes = load('nodes.txt');
 
 xsize = round(sqrt(size(density, 1)));
 ysize = xsize;
 
 density = reshape(density, xsize, ysize);
+Xnodes  = reshape(nodes(:,1), xsize+1, ysize+1);
+Ynodes  = reshape(nodes(:,2), xsize+1, ysize+1);
 
-imagesc(density.^3);
+Xnodes  = 0.5*(Xnodes(1:end-1,1:end-1)+Xnodes(2:end,2:end));
+Ynodes  = 0.5*(Ynodes(1:end-1,1:end-1)+Ynodes(2:end,2:end));
+
+surf(Xnodes,Ynodes,density.^3)
 colorbar
+view(2)
+axis('equal','tight');
+colormap('autumn')
+%imagesc(density.^3);
+%colorbar
