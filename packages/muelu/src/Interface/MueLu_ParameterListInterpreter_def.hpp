c9e639f3f966292d239926d3ea7bea6df9287c14
Andrey Prokopenko <aprokop@sandia.gov>
2016-03-29 11:03:46 -0600

    MueLu: switched matrix-matrix reuse to parameter lists
    The new reuse data on the each level is called "?? reuse data". Inside
    it will contain "graph" + extra information as needed, such as importers
    and hash tables.

--- packages/muelu/src/Interface/MueLu_ParameterListInterpreter_def.hpp
+++ packages/muelu/src/Interface/MueLu_ParameterListInterpreter_def.hpp
@@ -804,7 +804,7 @@ namespace MueLu {
       manager.SetFactory("P", P);
 
       if (reuseType == "tP" && !filteringChangesMatrix)
-        keeps.push_back(keep_pair("AP graph", P.get()));
+        keeps.push_back(keep_pair("AP reuse data", P.get()));
 
     } else if (multigridAlgo == "emin") {
       MUELU_SET_VAR_2LIST(paramList, defaultList, "emin: pattern", std::string, patternType);
@@ -970,8 +970,8 @@ namespace MueLu {
       manager.SetFactory("A", RAP);
 
       if (reuseType == "RP" || (reuseType == "tP" && !filteringChangesMatrix)) {
-        keeps.push_back(keep_pair("AP graph",  RAP.get()));
-        keeps.push_back(keep_pair("RAP graph", RAP.get()));
+        keeps.push_back(keep_pair("AP reuse data",  RAP.get()));
+        keeps.push_back(keep_pair("RAP reuse data", RAP.get()));
       }
     }
 
