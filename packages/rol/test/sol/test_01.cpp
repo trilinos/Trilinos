b0af3a5ed9622a259ee856e71757df00cb0592ee
Drew Kouri <dpkouri@sandia.gov>
2016-04-19 13:57:06 -0600

    ROL: Added Mean-Variance Quadrangle implementation.

--- packages/rol/test/sol/test_01.cpp
+++ packages/rol/test/sol/test_01.cpp
@@ -324,6 +324,15 @@ int main(int argc, char* argv[]) {
     setUpAndSolve(list,pObj,sampler,x,d,bnd,*outStream);
     printSolution(*x_rcp,*outStream);
     /**********************************************************************************************/
+    /************************* MIXED-QUANTILE QUADRANGLE ******************************************/
+    /**********************************************************************************************/
+    *outStream << "\nMEAN-VARIANCE QUADRANGLE RISK MEASURE\n";
+    list.sublist("SOL").set("Stochastic Optimization Type","Risk Averse"); 
+    list.sublist("SOL").sublist("Risk Measure").set("Name","Mean-Variance Quadrangle");
+    setRandomVector(*x_rcp);
+    setUpAndSolve(list,pObj,sampler,x,d,bnd,*outStream);
+    printSolution(*x_rcp,*outStream);
+    /**********************************************************************************************/
     /************************* QUANTILE-RANGE QUADRANGLE ******************************************/
     /**********************************************************************************************/
     *outStream << "\nQUANTILE-RADIUS QUADRANGLE RISK MEASURE\n";
