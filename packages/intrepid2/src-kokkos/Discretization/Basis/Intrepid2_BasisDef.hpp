66d6cdc63aa48f0c045f5c4da13aab2546220ded
Nathan Ellingwood <ndellin@sandia.gov>
2016-04-20 09:46:59 -0600

    Intrepid2 - Modifications for HCURL C1, test01

--- packages/intrepid2/src-kokkos/Discretization/Basis/Intrepid2_BasisDef.hpp
+++ packages/intrepid2/src-kokkos/Discretization/Basis/Intrepid2_BasisDef.hpp
@@ -49,6 +49,8 @@
 #ifndef __INTREPID2_BASIS_DEF_HPP__
 #define __INTREPID2_BASIS_DEF_HPP__
 
+#include <stdexcept>
+
 namespace Intrepid2 {
 
   //--------------------------------------------------------------------------------------------//
