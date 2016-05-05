8385d4cf18d47be463f57f8429688f6728d3998f
Roscoe A. Bartlett <rabartl@sandia.gov>
2016-05-04 22:47:11 -0400

    Allow Trilinos_REPOS_URL_BASE to be set in env (Trilinos #176)
    I tested this manually using:
    
    $ ./clone_extra_repos.py --no-op
    
    and
    
    $ env Trilinos_REPOS_URL_BASE=dummy_url.com ./clone_extra_repos.py --no-op
    
    and it looked correct.  The code that sets Trilinos_REPOS_URL_BASE if the env
    var is not set is exactly the same so it should work for the CTest/CDash
    driver scripts as well.
    
    Build/Test Cases Summary
    Enabled Packages: MOOCHO
    Disabled Packages: PyTrilinos,Pliris,Claps,STK,TriKota
    Enabled all Forward Packages
    0) MPI_DEBUG => Test case MPI_DEBUG was not run! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_ST => passed: passed=89,notpassed=0 (4.13 min)

--- cmake/ExtraRepositoriesList.cmake
+++ cmake/ExtraRepositoriesList.cmake
@@ -64,9 +64,14 @@
 #  https://tribits.org/doc/TribitsDevelopersGuide.html#tribits-project-define-extra-repositories
 #
 
-SET(Trilinos_REPOS_URL_BASE_DEFAULT git@github.com:trilinos/)
-SET(Trilinos_REPOS_URL_BASE  ${Trilinos_REPOS_URL_BASE_DEFAULT}
-  CACHE STRING "Base URL to Trilinos repos <url-base><repo-name>")
+IF (NOT "$ENV{Trilinos_REPOS_URL_BASE}" STREQUAL "")
+  SET(Trilinos_REPOS_URL_BASE  $ENV{Trilinos_REPOS_URL_BASE}
+    CACHE STRING "Set from env var Trilinos_REPOS_URL_BASE" FORCE)
+ELSE()
+  SET(Trilinos_REPOS_URL_BASE_DEFAULT git@github.com:trilinos/)
+  SET(Trilinos_REPOS_URL_BASE  ${Trilinos_REPOS_URL_BASE_DEFAULT}
+    CACHE STRING "Base URL to Trilinos repos <url-base><repo-name>")
+ENDIF()
 MARK_AS_ADVANCED(Trilinos_REPOS_URL_BASE)
 
 TRIBITS_PROJECT_DEFINE_EXTRA_REPOSITORIES(
