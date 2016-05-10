1706808cc51570588830c9c4d99d0b3ee098a46b
Mauro Perego <mperego@sandia.gov>
2016-05-09 23:35:23 -0600

    Intrepid2: Refactored HDIV basis functions for Wedge element and added corresponding test

--- packages/intrepid2/refactor/src/Discretization/Basis/Intrepid2_HDIV_WEDGE_I1_FEM.hpp
+++ packages/intrepid2/refactor/src/Discretization/Basis/Intrepid2_HDIV_WEDGE_I1_FEM.hpp
@@ -42,99 +42,172 @@
 
 /** \file   Intrepid_HDIV_WEDGE_I1_FEM.hpp
     \brief  Header file for the Intrepid2::HDIV_WEDGE_I1_FEM class.
-    \author Created by P. Bochev and D. Ridzal and K. Peterson.
- */
+    \author Created by P. Bochev and D. Ridzal
+*/
 
-#ifndef INTREPID2_HDIV_WEDGE_I1_FEM_HPP
-#define INTREPID2_HDIV_WEDGE_I1_FEM_HPP
+#ifndef __INTREPID2_HDIV_WEDGE_I1_FEM_HPP__
+#define __INTREPID2_HDIV_WEDGE_I1_FEM_HPP__
+
 #include "Intrepid2_Basis.hpp"
 
 namespace Intrepid2 {
-  
-/** \class  Intrepid2::Basis_HDIV_WEDGE_I1_FEM
-    \brief  Implementation of the default H(div)-compatible FEM basis of degree 1 on Wedge cell 
-  
-            Implements Raviart-Thomas basis of degree 1 on the reference Wedge cell. The basis has
-            cardinality 5 and spans an INCOMPLETE bi-linear polynomial space. Basis functions are dual 
-            to a unisolvent set of degrees-of-freedom (DoF) defined and enumerated as follows:
-  
-  \verbatim
-  ===================================================================================================
-  |         |           degree-of-freedom-tag table                    |                            |
-  |   DoF   |----------------------------------------------------------|       DoF definition       |
-  | ordinal |  subc dim    | subc ordinal | subc DoF ord |subc num DoF |                            |
-  |=========|==============|==============|==============|=============|============================|
-  |    0    |       2      |       0      |       0      |      1      |  L_0(u) = (u.n)(1/2,0,0)   |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    1    |       2      |       1      |       0      |      1      |  L_1(u) = (u.n)(1/2,1/2,0) |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    2    |       2      |       2      |       0      |      1      |  L_2(u) = (u.n)(0,1/2,0)   |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    3    |       2      |       3      |       0      |      1      |  L_3(u) = (u.n)(1/3,1/3,-1)|
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    4    |       2      |       4      |       0      |      1      |  L_4(u) = (u.n)(1/3,1/3,1) |
-  |=========|==============|==============|==============|=============|============================|
-  |   MAX   |  maxScDim=2  |  maxScOrd=4  |  maxDfOrd=0  |      -      |                            |
-  |=========|==============|==============|==============|=============|============================|
-  \endverbatim
-  
-    \remarks
-    \li     In the DoF functional \f${\bf n}\f$ is a face normal. Direction of face normals 
-            is determined by the right-hand rule applied to faces oriented by their vertex order
-            in the cell topology, from face vertex 0 to last face vertex, whereas their length is
-            set equal to face area (see http://mathworld.wolfram.com/Right-HandRule.html for definition 
-            of right-hand rule). For example, face 1 of all Wedge cells has vertex order {1,2,5,4} 
-            and its right-hand rule normal can be computed, e.g., by the vector product of edge 
-            tangents to edges {1,2} and {2,5}. On the reference Wedge the coordinates of face 1 
-            vertices are (1,0,-1), (0,1,-1), (0,1,1), and (1,0,1), the edge tangents are (-1,1,0) and 
-            (0,0,2) and the face normal direction is (-1,1,0) X (0,0,2) = (2,2,0). In this case the normal 
-            length already equals face area and no further normalization is needed.  
-  
-    \li     The length of the face normal equals the face area. As a result, the DoF functional 
-            is the value of the normal component of a vector field at the face center times the 
-            face area. The resulting basis is equivalent to a basis defined by using the face 
-            flux as a DoF functional. Note that faces 0 and 2 of reference Wedge<> cells have area 2, 
-            face 1 has area 2*Sqrt(2), and faces 3 and 4 have area 1/2.
-  
- */
-  
-template<class Scalar, class ArrayScalar> 
-class Basis_HDIV_WEDGE_I1_FEM : public Basis<Scalar, ArrayScalar> {
-private:
-  
-  /** \brief  Initializes <var>tagToOrdinal_</var> and <var>ordinalToTag_</var> lookup arrays.
-   */
-  void initializeTags();
-  
-public:
 
-  /** \brief  Constructor.
-    */
-  Basis_HDIV_WEDGE_I1_FEM();
-  
-    
-  /** \brief  Evaluation of a FEM basis on a <strong>reference Wedge</strong> cell. 
-    
-              Returns values of <var>operatorType</var> acting on FEM basis functions for a set of
-              points in the <strong>reference Wedge</strong> cell. For rank and dimensions of
-              I/O array arguments see Section \ref basis_md_array_sec.
-  
-      \param  outputValues      [out] - rank-3 or 4 array with the computed basis values
-      \param  inputPoints       [in]  - rank-2 array with dimensions (P,D) containing reference points  
-      \param  operatorType      [in]  - operator applied to basis functions    
-   */
-  void getValues(ArrayScalar &          outputValues,
-                 const ArrayScalar &    inputPoints,
-                 const EOperator        operatorType) const;
-  
-  
-  /**  \brief  FVD basis evaluation: invocation of this method throws an exception.
-   */
-  void getValues(ArrayScalar &          outputValues,
-                 const ArrayScalar &    inputPoints,
-                 const ArrayScalar &    cellVertices,
-                 const EOperator        operatorType = OPERATOR_VALUE) const;
-};
+  /** \class  Intrepid2::Basis_HDIV_WEDGE_I1_FEM
+      \brief  Implementation of the default H(grad)-compatible FEM basis of degree 1 on Wedge cell
+
+      Implements Lagrangian basis of degree 1 on the reference Wedge cell. The basis has
+      cardinality 6 and spans a COMPLETE bi-linear polynomial space. Basis functions are dual
+      to a unisolvent set of degrees-of-freedom (DoF) defined and enumerated as follows:
+
+      \verbatim
+      =================================================================================================
+      |         |           degree-of-freedom-tag table                    |                           |
+      |   DoF   |----------------------------------------------------------|      DoF definition       |
+      | ordinal |  subc dim    | subc ordinal | subc DoF ord |subc num DoF |                           |
+      |=========|==============|==============|==============|=============|===========================|
+      |    0    |       0      |       0      |       0      |      1      |   L_0(u) = u( 0, 0,-1)    |
+      |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |    1    |       0      |       1      |       0      |      1      |   L_1(u) = u( 1, 0,-1)    |
+      |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |    2    |       0      |       2      |       0      |      1      |   L_2(u) = u( 0, 1,-1)    |
+      |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |    3    |       0      |       3      |       0      |      1      |   L_3(u) = u( 0, 0, 1)    |
+      |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |    4    |       0      |       4      |       0      |      1      |   L_4(u) = u( 1, 0, 1)    |
+      |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |    5    |       0      |       5      |       0      |      1      |   L_5(u) = u( 0, 1, 1)    |
+      |=========|==============|==============|==============|=============|===========================|
+      |   MAX   |  maxScDim=0  |  maxScOrd=5  |  maxDfOrd=0  |      -      |                           |
+      |=========|==============|==============|==============|=============|===========================|
+      \endverbatim
+  */
+  template<typename ExecSpaceType = void,
+           typename outputValueType = double,
+           typename pointValueType = double>
+  class Basis_HDIV_WEDGE_I1_FEM : public Basis<ExecSpaceType,outputValueType,pointValueType> {
+  public:
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_1d_host ordinal_type_array_1d_host;
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_2d_host ordinal_type_array_2d_host;
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_3d_host ordinal_type_array_3d_host;
+
+    template<EOperator opType>
+    struct Serial {
+      template<typename outputValueValueType, class ...outputValueProperties,
+               typename inputPointValueType,  class ...inputPointProperties>
+      KOKKOS_INLINE_FUNCTION
+      static void
+      getValues( /**/  Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
+                 const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints );
+
+    };
+
+    template<typename outputValueViewType,
+             typename inputPointViewType,
+             EOperator opType>
+    struct Functor {
+      /**/  outputValueViewType _outputValues;
+      const inputPointViewType  _inputPoints;
+
+      KOKKOS_INLINE_FUNCTION
+      Functor( /**/  outputValueViewType outputValues_,
+               /**/  inputPointViewType  inputPoints_ )
+        : _outputValues(outputValues_), _inputPoints(inputPoints_) {}
+
+      KOKKOS_INLINE_FUNCTION
+      void operator()(const ordinal_type pt) const {
+        switch (opType) {
+        case OPERATOR_VALUE : {
+          auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
+          const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
+          Serial<opType>::getValues( output, input );
+          break;
+        }
+        case OPERATOR_DIV : {
+          auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt );
+          const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
+          Serial<opType>::getValues( output, input );
+          break;
+        }
+        default: {
+          INTREPID2_TEST_FOR_ABORT( opType != OPERATOR_VALUE &&
+                                    opType != OPERATOR_DIV ,
+                                    ">>> ERROR: (Intrepid2::Basis_HDIV_WEDGE_I1_FEM::Serial::getValues) operator is not supported");
+        }
+        }
+      }
+    };
+
+    class Internal {
+    private:
+      Basis_HDIV_WEDGE_I1_FEM *obj_;
+
+    public:
+      Internal(Basis_HDIV_WEDGE_I1_FEM *obj)
+        : obj_(obj) {}
+
+      /** \brief  Evaluation of a FEM basis on a <strong>reference Wedge</strong> cell.
+
+          Returns values of <var>operatorType</var> acting on FEM basis functions for a set of
+          points in the <strong>reference Wedge</strong> cell. For rank and dimensions of
+          I/O array arguments see Section \ref basis_md_array_sec.
+
+          \param  outputValues      [out] - rank-2 or 3 array with the computed basis values
+          \param  inputPoints       [in]  - rank-2 array with dimensions (P,D) containing reference points
+          \param  operatorType      [in]  - operator applied to basis functions
+      */
+      template<typename outputValueValueType, class ...outputValueProperties,
+               typename inputPointValueType,  class ...inputPointProperties>
+      void
+      getValues( /**/  Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
+                 const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints,
+                 const EOperator operatorType  = OPERATOR_VALUE ) const;
+
+
+      /** \brief  Returns spatial locations (coordinates) of degrees of freedom on a
+          <strong>reference Wedge</strong>.
+
+          \param  DofCoords      [out] - array with the coordinates of degrees of freedom,
+          dimensioned (F,D)
+      */
+      template<typename dofCoordValueType, class ...dofCoordProperties>
+      void
+      getDofCoords( Kokkos::DynRankView<dofCoordValueType,dofCoordProperties...> dofCoords ) const;
+
+    };
+    Internal impl_;
+
+    /** \brief  Constructor.
+     */
+    Basis_HDIV_WEDGE_I1_FEM();
+    Basis_HDIV_WEDGE_I1_FEM(const Basis_HDIV_WEDGE_I1_FEM &b)
+      : Basis<ExecSpaceType,outputValueType,pointValueType>(b),
+        impl_(this) {}
+
+    Basis_HDIV_WEDGE_I1_FEM& operator=(const Basis_HDIV_WEDGE_I1_FEM &b) {
+      if (this != &b) {
+        Basis<ExecSpaceType,outputValueType,pointValueType>::operator= (b);
+        // do not copy impl
+      }
+      return *this;
+    }
+
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::outputViewType outputViewType;
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::pointViewType  pointViewType;
+
+    virtual
+    void
+    getValues( /**/  outputViewType outputValues,
+               const pointViewType  inputPoints,
+               const EOperator operatorType = OPERATOR_VALUE ) const {
+      impl_.getValues( outputValues, inputPoints, operatorType );
+    }
+
+    virtual
+    void
+    getDofCoords( pointViewType dofCoords ) const {
+      impl_.getDofCoords( dofCoords );
+    }
+  };
 }// namespace Intrepid2
 
 #include "Intrepid2_HDIV_WEDGE_I1_FEMDef.hpp"
