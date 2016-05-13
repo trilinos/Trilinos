4d6b786c5dd31b2ff4f83a7e54e6d72e7c2c79a0
Tobias Wiesner <tawiesn@sandia.gov>
2016-05-13 12:31:34 -0600

    MueLu: fix Q2Q1 executable for recent changes in Xpetra::BlockedCrsOperator
    Build/Test Cases Summary
    Enabled Packages: Zoltan2
    Enabled all Forward Packages
    0) MPI_DEBUG => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Skipped configure, build, test due to no enabled packages! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG_ENIGMA => passed: passed=48,notpassed=0 (32.54 min)

--- packages/muelu/adapters/stratimikos/Thyra_MueLuTpetraQ2Q1PreconditionerFactory_def.hpp
+++ packages/muelu/adapters/stratimikos/Thyra_MueLuTpetraQ2Q1PreconditionerFactory_def.hpp
@@ -425,10 +425,10 @@ namespace Thyra {
     RCP<Matrix> filteredA = FilterMatrix(*A_11, *A_11,    dropTol);
     RCP<Matrix> filteredB = FilterMatrix(*BBt,  *BBt_abs, dropTol);
 
-    RCP<CrsMatrix> fA_11_crs = rcp_dynamic_cast<CrsMatrixWrap>(filteredA)->getCrsMatrix();
-    RCP<CrsMatrix> fA_12_crs = Teuchos::null;
-    RCP<CrsMatrix> fA_21_crs = Teuchos::null;
-    RCP<CrsMatrix> fA_22_crs = rcp_dynamic_cast<CrsMatrixWrap>(filteredB)->getCrsMatrix();
+    RCP<Matrix> fA_11_crs = rcp_dynamic_cast<CrsMatrixWrap>(filteredA);
+    RCP<Matrix> fA_12_crs = Teuchos::null;
+    RCP<Matrix> fA_21_crs = Teuchos::null;
+    RCP<Matrix> fA_22_crs = rcp_dynamic_cast<CrsMatrixWrap>(filteredB);
 
     // Build the large filtered matrix which requires strided maps
     std::vector<size_t> stridingInfo(1, 1);
