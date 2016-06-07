f96870f599f43a395fc979796442ef7b59d8e97c
Tobias Wiesner <tawiesn@sandia.gov>
2016-06-07 13:58:46 -0600

    MueLu: fix indentation

--- packages/muelu/src/Interface/YamlParameterList/MueLu_YamlParser_decl.hpp
+++ packages/muelu/src/Interface/YamlParameterList/MueLu_YamlParser_decl.hpp
@@ -1,55 +1,48 @@
 // @HEADER
-// //
-// // ***********************************************************************
-// //
-// //        MueLu: A package for multigrid based preconditioning
-// //                  Copyright 2012 Sandia Corporation
-// //
-// // Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
-// // the U.S. Government retains certain rights in this software.
-// //
-// // Redistribution and use in source and binary forms, with or without
-// // modification, are permitted provided that the following conditions are
-// // met:
-// //
-// // 1. Redistributions of source code must retain the above copyright
-// // notice, this list of conditions and the following disclaimer.
-// //
-// // 2. Redistributions in binary form must reproduce the above copyright
-// // notice, this list of conditions and the following disclaimer in the
-// // documentation and/or other materials provided with the distribution.
-// //
-// // 3. Neither the name of the Corporation nor the names of the
-// // contributors may be used to endorse or promote products derived from
-// // this software without specific prior written permission.
-// //
-// // THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
-// // EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-// // IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-// // PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
-// // CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-// // EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-// // PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-// // PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-// // LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-// // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-// // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-// //
-// // Questions? Contact
-// //                    Jonathan Hu       (jhu@sandia.gov)
-// //                    Andrey Prokopenko (aprokop@sandia.gov)
-// //                    Ray Tuminaro      (rstumin@sandia.gov)
-// //
-// // ***********************************************************************
-// //
-// // @HEADER
-// /*
-//  * MueLu_YamlParser_decl.hpp
-//  *
-//  *  Created on: May 23, 2016
-//  *      Author: bmkelle
-//  */
 //
+// ***********************************************************************
+//
+//        MueLu: A package for multigrid based preconditioning
+//                  Copyright 2012 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact
+//                    Jonathan Hu       (jhu@sandia.gov)
+//                    Andrey Prokopenko (aprokop@sandia.gov)
+//                    Ray Tuminaro      (rstumin@sandia.gov)
+//
+// ***********************************************************************
+//
+// @HEADER
 
 #ifndef MUELU_YAMLPARSER_DECL_H_
 #define MUELU_YAMLPARSER_DECL_H_
@@ -112,7 +105,7 @@ private:
   //load all k-v pairs within node into param list (checks if node is map, and handles nesting)
   //topLevel means to put sub-pairs directly into parent and not create named sublists
   void processMapNode(const YAML::Node& node, Teuchos::ParameterList& parent, bool topLevel = false);
-  void processKeyValueNode(const std::string& key, const YAML::Node& node, Teuchos::ParameterList& parent, bool topLevel = false); 
+  void processKeyValueNode(const std::string& key, const YAML::Node& node, Teuchos::ParameterList& parent, bool topLevel = false);
   template<typename T> Teuchos::Array<T> getYamlArray(const YAML::Node& node);
 };
 
