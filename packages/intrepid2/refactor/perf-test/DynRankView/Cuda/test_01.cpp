d717f8161478f00aa81aa7c435f49d32c164f0b7
Nathan Ellingwood <ndellin@sandia.gov>
2016-06-13 10:50:57 -0600

    Intrepid2: Fix DynRankView perf-test for cuda

--- packages/intrepid2/refactor/perf-test/DynRankView/Cuda/test_01.cpp
+++ packages/intrepid2/refactor/perf-test/DynRankView/Cuda/test_01.cpp
@@ -46,16 +46,50 @@
 */
 
 #include "Kokkos_Core.hpp"
+#include <impl/Kokkos_Timer.hpp>
 
+#include "Teuchos_CommandLineProcessor.hpp"
+
+#include "Intrepid2_Types.hpp"
 #include "test_01.hpp"
 
 int main(int argc, char *argv[]) {
 
-  const bool verbose = (argc-1) > 0;
-  Kokkos::initialize();
+  Teuchos::CommandLineProcessor clp;
+  clp.setDocString("Intrepid2::DynRankView_PerfTest01.\n");
 
-  const int r_val = Intrepid2::Test::RealSpaceTools_Test01<double,Kokkos::Cuda>(verbose);
+  int nworkset = 8;
+  clp.setOption("nworkset", &nworkset, "# of worksets");
+
+  int C = 1024;
+  clp.setOption("C", &C, "# of Cells in a workset");
+
+  int P = 9;
+  clp.setOption("P", &P, "# of Points in a Cell");
+
+  int D = 3;
+  clp.setOption("D", &D, "Dimension of point data (1 - 3)");
+
+  bool verbose = true;
+  clp.setOption("enable-verbose", "disable-verbose", &verbose, "Flag for verbose printing");
+
+  clp.recogniseAllOptions(true);
+  clp.throwExceptions(false);
+
+  Teuchos::CommandLineProcessor::EParseCommandLineReturn r_parse= clp.parse( argc, argv );
+
+  if (r_parse == Teuchos::CommandLineProcessor::PARSE_HELP_PRINTED) return 0;
+  if (r_parse != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL  ) return -1;
+
+  Kokkos::initialize();
+  
+  const int r_val = Intrepid2::Test::DynRankView_PerfTest01<double,Kokkos::Cuda>(nworkset,
+                                                                                   C,
+                                                                                   P,
+                                                                                   D,
+                                                                                   verbose);
   
   Kokkos::finalize();
+  
   return r_val;
 }
