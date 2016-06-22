f8a29a7425368b1e2a17bcee44c148bb813d5703
micheldemessieres <michel@demessieres.com>
2016-06-22 10:28:49 -0400

    Fix the allowed types for scalar, local ordinal, and global ordinal in InputTraits
    This sets the types to be as follows:
    
    scalar: double, float, int
    local ordinal: int, long, long long, ssize_t
    global ordinal: int, long, long long, ssize_t, unsigned int, unsigned long, unsigned long long, size_t
    
    Added a bunch of unit tests to verify things work/fail properly when using:
    int32_t, int64_t, uint32_t, uint34_t
    
    Also added unit tests to verify user defined types will also fail.
    There is a lot of redundancy in that and mostly it is to verify the std::is_same function works as expected.

--- packages/zoltan2/test/unit/input/InputTraitsGood.cpp
+++ packages/zoltan2/test/unit/input/InputTraitsGood.cpp
@@ -54,82 +54,157 @@ using Zoltan2::InputTraits;
 
 int main(int argc, char *argv[])
 {
+  // scalar ordinal (first slot) must be float, double, or int - this validates we would fail for any
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, signed int, long>                 END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<double, long>                            END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<int, int, long>                          END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<int32_t, int, long>                      END_CHECK
+
+  // local ordinal (second slot) must always be signed - this validates that we would fail for any unsigned setting
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, signed int, long>                 END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, signed long, long>                END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, signed long long, long>           END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, ssize_t, long>                    END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int32_t, long>                    END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int64_t, long>                    END_CHECK
+
+  // global ordinal (third slot) must be int, long, long long, ssize_t, unsigned int, unsigned long, unsigned long long, size_t
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, int>                         END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, long>                        END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, long long>                   END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, ssize_t>                     END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, unsigned int>                END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, unsigned long>               END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, unsigned long long>          END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, size_t>                      END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, int32_t>                     END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, int64_t>                     END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, uint32_t>                    END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, int, uint64_t>                    END_CHECK
+
+  // we also want to make sure that user typedefs will work just fine - lots of redundancy here just to make sure std::is_same is really working as we think
+  typedef signed int user_int_t;
+  typedef unsigned int user_unsigned_int_t;
+  typedef float user_float_t;
+  typedef double user_double_t;
+  typedef int32_t user_int32_t;
+  typedef uint32_t user_uint32_t;
+  typedef int64_t user_int64_t;
+  typedef uint64_t user_uint64_t;
+  typedef signed long user_long_t;
+  typedef signed long long user_long_long_t;
+  typedef unsigned long user_unsigned_long_t;
+  typedef unsigned long long user_unsigned_long_long_t;
+  typedef size_t user_size_t;
+  typedef ssize_t user_ssize_t;
+
+  // scalar ordinal (first slot) must be float, double, or int - this validates we would fail for any
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_long_t>                 END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_double_t, user_long_t>                            END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_int_t, user_int_t, user_long_t>                   END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_int32_t, user_int_t, user_long_t>                 END_CHECK
+
+  // local ordinal (second slot) must always be signed - this validates that we would fail for any unsigned setting
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_long_t>                 END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_long_t, user_long_t>                END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_long_long_t, user_long_t>           END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_ssize_t, user_long_t>               END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int32_t, user_long_t>               END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int64_t, user_long_t>               END_CHECK
+
+  // global ordinal (third slot) must be int, long, long long, ssize_t, unsigned int, unsigned long, unsigned long long, size_t
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_int_t>                  END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_long_t>                 END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_long_long_t>            END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_ssize_t>                END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_unsigned_int_t>         END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_unsigned_long_t>        END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_unsigned_long_long_t>   END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_size_t>                 END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_int32_t>                END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_int64_t>                END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_uint32_t>               END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<user_float_t, user_int_t, user_uint64_t>               END_CHECK
+
+  // this section takes each specific type and validates a few combinations
+  // note that some types may pass only two templaye parameters, not three - so it can be scalar + local + global or it can be just local + global.
+
   // validate Zoltan2::BasicUserTypes
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<double, signed int, long long>           END_CHECK
   BEGIN_CHECK    Zoltan2::BasicUserTypes<double, int, long long>                  END_CHECK
-  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, long, unsigned int>               END_CHECK
-  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, unsigned int, unsigned long>      END_CHECK
-  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, unsigned long, long>              END_CHECK
-  BEGIN_CHECK    Zoltan2::BasicUserTypes<double, long long, unsigned int>         END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, signed long, unsigned int>        END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<float, signed long long, long>           END_CHECK
+  BEGIN_CHECK    Zoltan2::BasicUserTypes<double, ssize_t, unsigned int>           END_CHECK
 
   // validate Xpetra::CrsMatrix
+  BEGIN_CHECK    Xpetra::CrsMatrix<double, signed int, long long>                 END_CHECK
   BEGIN_CHECK    Xpetra::CrsMatrix<double, int, long long>                        END_CHECK
-  BEGIN_CHECK    Xpetra::CrsMatrix<float, long, unsigned int>                     END_CHECK
-  BEGIN_CHECK    Xpetra::CrsMatrix<float, unsigned int, unsigned long>            END_CHECK
-  BEGIN_CHECK    Xpetra::CrsMatrix<float, unsigned long, long>                    END_CHECK
-  BEGIN_CHECK    Xpetra::CrsMatrix<double, long long, unsigned int>               END_CHECK
+  BEGIN_CHECK    Xpetra::CrsMatrix<float, signed long, unsigned int>              END_CHECK
+  BEGIN_CHECK    Xpetra::CrsMatrix<float, signed long long, long>                 END_CHECK
+  BEGIN_CHECK    Xpetra::CrsMatrix<double, ssize_t, unsigned int>                 END_CHECK
 
   // validate Tpetra::CrsMatrix
+  BEGIN_CHECK    Tpetra::CrsMatrix<double, signed int, long long>                 END_CHECK
   BEGIN_CHECK    Tpetra::CrsMatrix<double, int, long long>                        END_CHECK
-  BEGIN_CHECK    Tpetra::CrsMatrix<float, long, unsigned int>                     END_CHECK
-  BEGIN_CHECK    Tpetra::CrsMatrix<float, unsigned int, unsigned long>            END_CHECK
-  BEGIN_CHECK    Tpetra::CrsMatrix<float, unsigned long, long>                    END_CHECK
-  BEGIN_CHECK    Tpetra::CrsMatrix<double, long long, unsigned int>               END_CHECK
+  BEGIN_CHECK    Tpetra::CrsMatrix<float, signed long, unsigned int>              END_CHECK
+  BEGIN_CHECK    Tpetra::CrsMatrix<float, signed long long, long>                 END_CHECK
+  BEGIN_CHECK    Tpetra::CrsMatrix<double, ssize_t, unsigned int>                 END_CHECK
 
   // validate Xpetra::RowMatrix
+  BEGIN_CHECK    Xpetra::RowMatrix<double, signed int, long long>                 END_CHECK
   BEGIN_CHECK    Xpetra::RowMatrix<double, int, long long>                        END_CHECK
-  BEGIN_CHECK    Xpetra::RowMatrix<float, long, unsigned int>                     END_CHECK
-  BEGIN_CHECK    Xpetra::RowMatrix<float, unsigned int, unsigned long>            END_CHECK
-  BEGIN_CHECK    Xpetra::RowMatrix<float, unsigned long, long>                    END_CHECK
-  BEGIN_CHECK    Xpetra::RowMatrix<double, long long, unsigned int>               END_CHECK
+  BEGIN_CHECK    Xpetra::RowMatrix<float, signed long, unsigned int>              END_CHECK
+  BEGIN_CHECK    Xpetra::RowMatrix<float, signed long long, long>                 END_CHECK
+  BEGIN_CHECK    Xpetra::RowMatrix<double, ssize_t, unsigned int>                 END_CHECK
 
   // validate Tpetra::RowMatrix
+  BEGIN_CHECK    Tpetra::RowMatrix<double, signed int, long long>                 END_CHECK
   BEGIN_CHECK    Tpetra::RowMatrix<double, int, long long>                        END_CHECK
-  BEGIN_CHECK    Tpetra::RowMatrix<float, long, unsigned int>                     END_CHECK
-  BEGIN_CHECK    Tpetra::RowMatrix<float, unsigned int, unsigned long>            END_CHECK
-  BEGIN_CHECK    Tpetra::RowMatrix<float, unsigned long, long>                    END_CHECK
-  BEGIN_CHECK    Tpetra::RowMatrix<double, long long, unsigned int>               END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<float, signed long, unsigned int>              END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<float, signed long long, long>                 END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<double, ssize_t, unsigned int>                 END_CHECK
 
   // validate Xpetra::Vector
+  BEGIN_CHECK    Xpetra::Vector<double, signed int, long long>                    END_CHECK
   BEGIN_CHECK    Xpetra::Vector<double, int, long long>                           END_CHECK
-  BEGIN_CHECK    Xpetra::Vector<float, long, unsigned int>                        END_CHECK
-  BEGIN_CHECK    Xpetra::Vector<float, unsigned int, unsigned long>               END_CHECK
-  BEGIN_CHECK    Xpetra::Vector<float, unsigned long, long>                       END_CHECK
-  BEGIN_CHECK    Xpetra::Vector<double, long long, unsigned int>                  END_CHECK
+  BEGIN_CHECK    Xpetra::Vector<float, signed long, unsigned int>                 END_CHECK
+  BEGIN_CHECK    Xpetra::Vector<float, signed long long, long>                    END_CHECK
+  BEGIN_CHECK    Xpetra::Vector<double, ssize_t, unsigned int>                    END_CHECK
 
   // validate Tpetra::RowMatrix
-  BEGIN_CHECK    Tpetra::Vector<double, int, long long>                           END_CHECK
-  BEGIN_CHECK    Tpetra::Vector<float, long, unsigned int>                        END_CHECK
-  BEGIN_CHECK    Tpetra::Vector<float, unsigned int, unsigned long>               END_CHECK
-  BEGIN_CHECK    Tpetra::Vector<float, unsigned long, long>                       END_CHECK
-  BEGIN_CHECK    Tpetra::Vector<double, long long, unsigned int>                  END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<double, signed int, long long>                 END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<double, int, long long>                        END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<float, signed long, unsigned int>              END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<float, signed long long, long>                 END_CHECK
+  BEGIN_CHECK    Tpetra::RowMatrix<double, ssize_t, unsigned int>                 END_CHECK
 
   // validate Xpetra::MultiVector
+  BEGIN_CHECK    Xpetra::MultiVector<double, signed int, long long>               END_CHECK
   BEGIN_CHECK    Xpetra::MultiVector<double, int, long long>                      END_CHECK
-  BEGIN_CHECK    Xpetra::MultiVector<float, long, unsigned int>                   END_CHECK
-  BEGIN_CHECK    Xpetra::MultiVector<float, unsigned int, unsigned long>          END_CHECK
-  BEGIN_CHECK    Xpetra::MultiVector<float, unsigned long, long>                  END_CHECK
-  BEGIN_CHECK    Xpetra::MultiVector<double, long long, unsigned int>             END_CHECK
+  BEGIN_CHECK    Xpetra::MultiVector<float, signed long, unsigned int>            END_CHECK
+  BEGIN_CHECK    Xpetra::MultiVector<float, signed long long, long>               END_CHECK
+  BEGIN_CHECK    Xpetra::MultiVector<double, ssize_t, unsigned int>               END_CHECK
 
   // validate Tpetra::MultiVector
+  BEGIN_CHECK    Tpetra::MultiVector<double, signed int, long long>               END_CHECK
   BEGIN_CHECK    Tpetra::MultiVector<double, int, long long>                      END_CHECK
-  BEGIN_CHECK    Tpetra::MultiVector<float, long, unsigned int>                   END_CHECK
-  BEGIN_CHECK    Tpetra::MultiVector<float, unsigned int, unsigned long>          END_CHECK
-  BEGIN_CHECK    Tpetra::MultiVector<float, unsigned long, long>                  END_CHECK
-  BEGIN_CHECK    Tpetra::MultiVector<double, long long, unsigned int>             END_CHECK
+  BEGIN_CHECK    Tpetra::MultiVector<float, signed long, unsigned int>            END_CHECK
+  BEGIN_CHECK    Tpetra::MultiVector<float, signed long long, long>               END_CHECK
+  BEGIN_CHECK    Tpetra::MultiVector<double, ssize_t, unsigned int>               END_CHECK
 
   // validate Xpetra::CrsGraph
+  BEGIN_CHECK    Xpetra::CrsGraph<signed int, long long>                          END_CHECK
   BEGIN_CHECK    Xpetra::CrsGraph<int, long long>                                 END_CHECK
-  BEGIN_CHECK    Xpetra::CrsGraph<long, unsigned int>                             END_CHECK
-  BEGIN_CHECK    Xpetra::CrsGraph<unsigned int, unsigned long>                    END_CHECK
-  BEGIN_CHECK    Xpetra::CrsGraph<unsigned long, long>                            END_CHECK
-  BEGIN_CHECK    Xpetra::CrsGraph<long long, unsigned int>                        END_CHECK
+  BEGIN_CHECK    Xpetra::CrsGraph<signed long, unsigned int>                      END_CHECK
+  BEGIN_CHECK    Xpetra::CrsGraph<signed long long, long>                         END_CHECK
+  BEGIN_CHECK    Xpetra::CrsGraph<ssize_t, unsigned int>                          END_CHECK
 
   // validate Tpetra::CrsGraph
+  BEGIN_CHECK    Tpetra::CrsGraph<signed int, long long>                          END_CHECK
   BEGIN_CHECK    Tpetra::CrsGraph<int, long long>                                 END_CHECK
-  BEGIN_CHECK    Tpetra::CrsGraph<long, unsigned int>                             END_CHECK
-  BEGIN_CHECK    Tpetra::CrsGraph<unsigned int, unsigned long>                    END_CHECK
-  BEGIN_CHECK    Tpetra::CrsGraph<unsigned long, long>                            END_CHECK
-  BEGIN_CHECK    Tpetra::CrsGraph<long long, unsigned int>                        END_CHECK
+  BEGIN_CHECK    Tpetra::CrsGraph<signed long, unsigned int>                      END_CHECK
+  BEGIN_CHECK    Tpetra::CrsGraph<signed long long, long>                         END_CHECK
+  BEGIN_CHECK    Tpetra::CrsGraph<ssize_t, unsigned int>                          END_CHECK
 
   // set the PASS keyword
   std::cout << "Validated InputTraits - The test PASSED because it compiled" << std::endl;
