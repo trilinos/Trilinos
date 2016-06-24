86eb6e9d79f6e26026872265eb8dfe0b2532fff7
Roscoe A. Bartlett <rabartl@sandia.gov>
2016-06-24 07:08:20 -0600

    Disable STKUnit_tests_util_parallel_UnitTest_MPI_4 (Trilinos #441)
    This test has been failing for a long time.  See trilinos/Trilinos#441.

---
+++ cmake/RepositoryDependenciesSetup.cmake
@@ -0,0 +1,3 @@
+# Test has been failing for a long time (see Trilinos #441) 
+SET(STKUnit_tests_util_parallel_UnitTest_MPI_4_DISABLE  ON CACHE BOOL
+  "Set by default in Trilinos RepositoryDependenciesSetup.cmake." )
