8c3e67f61475612cbe634a709177b09f0841306b
Nathan Crane <nkcrane@sandia.gov>
2016-06-27 13:11:04 -0600

    Move bounding box class to 'move_to_stk' holding area
    (From STK/SEACAS)

--- packages/stk/stk_search/unit_tests/UnitTestUtils.hpp
+++ packages/stk/stk_search/unit_tests/UnitTestUtils.hpp
@@ -40,7 +40,7 @@
 #include <stk_search/BoundingBox.hpp>
 #include <stk_search/IdentProc.hpp>
 
-#include <Geom_AxisAlignedBB.h>
+#include <temp_move_to_stk_search/Geom_AxisAlignedBB.h>
 #include <Geom_Search.h>
 #include <search/ContactRangeSearch.h>
 #include <search/ContactCommunication.h>
