6e9940f041e1860ba93d01710fc2f512bf59a11b
Brent Perschbacher <bmpersc@sandia.gov>
2016-07-12 10:32:19 -0600

    Initial commit for 12.8

--- Version.cmake
+++ Version.cmake
@@ -59,12 +59,12 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 12.7)
+SET(Trilinos_VERSION 12.8)
 SET(Trilinos_MAJOR_VERSION 12)
-SET(Trilinos_MAJOR_MINOR_VERSION 120700)
-SET(Trilinos_VERSION_STRING "12.7 (Dev)")
-SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
+SET(Trilinos_MAJOR_MINOR_VERSION 120800)
+SET(Trilinos_VERSION_STRING "12.8")
+SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
-SET(Trilinos_REPOSITORY_BRANCH "develop" CACHE INTERNAL "")
-SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
+SET(Trilinos_REPOSITORY_BRANCH "trilinos-release-12-8-branch" CACHE INTERNAL "")
+SET(Trilinos_TESTING_TRACK "Nightly Release 12.8" CACHE INTERNAL "")
