dbaff0bc6fb3cc53c997bb834ad86498f1db1794
Andrey Prokopenko <aprokop@sandia.gov>
2016-07-12 14:18:20 -0600

    MueLu: kokkos: converted coalesce_drop kernel perf test to run on multiple nodes

--- packages/muelu/test/perf_tests_kokkos/KokkosKernels.cpp
+++ packages/muelu/test/perf_tests_kokkos/KokkosKernels.cpp
@@ -4,33 +4,14 @@
 #include <Kokkos_StaticCrsGraph.hpp>
 #include <impl/Kokkos_Timer.hpp>
 
-typedef Kokkos::DefaultExecutionSpace                   execution_space;
-typedef typename execution_space::memory_space          memory_space;
-typedef Kokkos::Device<execution_space, memory_space>   device_type;
-
-typedef double                                          scalar_type;
-typedef int                                             local_ordinal_type;
-
-typedef Kokkos::ArithTraits<scalar_type>                ATS;
-typedef typename ATS::mag_type                          magnitude_type;
-
-typedef Kokkos::StaticCrsGraph<local_ordinal_type,
-    Kokkos::LayoutLeft, device_type>                    local_graph_type;
-typedef typename local_graph_type::row_map_type         row_map_type;
-typedef typename local_graph_type::entries_type         entries_type;
-
-typedef Kokkos::View<const local_ordinal_type*,
-        device_type>                                    row_type;
-
-
-typedef Kokkos::CrsMatrix<scalar_type, local_ordinal_type,
-    execution_space, void,
-    typename local_graph_type::size_type>               local_matrix_type;
-typedef Kokkos::View<bool*, device_type>                boundary_nodes_type;
-
-typedef typename local_matrix_type::size_type           size_type;
-
+template<class scalar_type, class local_ordinal_type, class device_type>
 class LWGraph_kokkos {
+private:
+  typedef Kokkos::StaticCrsGraph<local_ordinal_type, Kokkos::LayoutLeft, device_type>   local_graph_type;
+  typedef Kokkos::View<const local_ordinal_type*, device_type>                          row_type;
+  typedef typename local_graph_type::size_type                                          size_type;
+  typedef Kokkos::View<bool*, device_type>                                              boundary_nodes_type;
+
 public:
   LWGraph_kokkos(const local_graph_type& graph) : graph_(graph) {
     minLocalIndex_ = 0;
@@ -83,7 +64,9 @@ private:
   size_type             maxNumRowEntries_;
 };
 
-local_matrix_type kernel_construct(local_ordinal_type numRows) {
+template<class scalar_type, class local_ordinal_type, class device_type>
+Kokkos::CrsMatrix<scalar_type, local_ordinal_type, device_type>
+kernel_construct(local_ordinal_type numRows) {
   local_ordinal_type numCols         = numRows;
   local_ordinal_type nnz             = 10*numRows;
 
@@ -114,35 +97,49 @@ local_matrix_type kernel_construct(local_ordinal_type numRows) {
     }
   }
 
+  typedef Kokkos::CrsMatrix<scalar_type, local_ordinal_type, device_type> local_matrix_type;
+
   return local_matrix_type("A", numRows, numCols, nnz, values, rowPtr, colInd, false/*pad*/);
 }
 
-void kernel_coalesce_drop(local_matrix_type A) {
+template<class scalar_type, class local_ordinal_type, class device_type>
+void kernel_coalesce_drop(Kokkos::CrsMatrix<scalar_type, local_ordinal_type, device_type> A) {
+  typedef Kokkos::CrsMatrix<scalar_type, local_ordinal_type, device_type>   local_matrix_type;
+  typedef Kokkos::ArithTraits<scalar_type>                                  ATS;
+  typedef typename ATS::mag_type                                            magnitude_type;
+  typedef Kokkos::View<bool*, device_type>                                  boundary_nodes_type;
+
   auto numRows = A.numRows();
 
   scalar_type eps = 0.05;
 
+  typedef Kokkos::RangePolicy<typename local_matrix_type::execution_space> RangePolicy;
+
   // Stage 0: detect Dirichlet rows
   boundary_nodes_type boundaryNodes("boundaryNodes", numRows);
-  Kokkos::parallel_for("MueLu:Utils::DetectDirichletRows", numRows, KOKKOS_LAMBDA(const local_ordinal_type row) {
-    auto rowView = A.row (row);
-    auto length  = rowView.length;
+  Kokkos::parallel_for("MueLu:Utils::DetectDirichletRows", RangePolicy(0, numRows),
+    KOKKOS_LAMBDA(const local_ordinal_type row) {
+      auto rowView = A.row (row);
+      auto length  = rowView.length;
 
-    boundaryNodes(row) = true;
-    for (decltype(length) colID = 0; colID < length; colID++)
-      if ((rowView.colidx(colID) != row) && (ATS::magnitude(rowView.value(colID)) > 1e-13)) {
-        boundaryNodes(row) = false;
-        break;
-      }
-  });
+      boundaryNodes(row) = true;
+      for (decltype(length) colID = 0; colID < length; colID++)
+        if ((rowView.colidx(colID) != row) && (ATS::magnitude(rowView.value(colID)) > 1e-13)) {
+          boundaryNodes(row) = false;
+          break;
+        }
+    });
 
   // Stage 1: calculate the number of remaining entries per row
+  typedef Kokkos::StaticCrsGraph<local_ordinal_type, Kokkos::LayoutLeft, device_type> local_graph_type;
+  typedef typename local_graph_type::row_map_type row_map_type;
+  typedef typename local_graph_type::entries_type entries_type;
+
   typename entries_type::non_const_type diag("ghosted", numRows);
   typename row_map_type::non_const_type rows("row_map", numRows+1);       // rows(0) = 0 automatically
 
-
   local_ordinal_type realnnz = 0;
-  Kokkos::parallel_reduce("kernel_cd:stage1_reduce", numRows,
+  Kokkos::parallel_reduce("kernel_cd:stage1_reduce", RangePolicy(0, numRows),
     KOKKOS_LAMBDA(const local_ordinal_type row, local_ordinal_type& nnz) {
       auto rowView = A.row (row);
       auto length  = rowView.length;
@@ -163,7 +160,7 @@ void kernel_coalesce_drop(local_matrix_type A) {
     }, realnnz);
 
   // parallel_scan (exclusive)
-  Kokkos::parallel_scan("kernel_cd:stage1_scan", numRows+1,
+  Kokkos::parallel_scan("kernel_cd:stage1_scan", RangePolicy(0, numRows+1),
     KOKKOS_LAMBDA(const local_ordinal_type i, local_ordinal_type& upd, const bool& final_result) {
       upd += rows(i);
       if (final_result)
@@ -174,7 +171,7 @@ void kernel_coalesce_drop(local_matrix_type A) {
   typename boundary_nodes_type::non_const_type bndNodes("boundaryNodes", numRows);
   typename entries_type::non_const_type        cols    ("entries",       realnnz);
   local_ordinal_type numDropped = 0;
-  Kokkos::parallel_reduce("kernel_cd:stage2_reduce", numRows,
+  Kokkos::parallel_reduce("kernel_cd:stage2_reduce", RangePolicy(0, numRows),
     KOKKOS_LAMBDA(const local_ordinal_type row, local_ordinal_type& dropped) {
       auto rowView = A.row (row);
       auto length = rowView.length;
@@ -208,11 +205,13 @@ void kernel_coalesce_drop(local_matrix_type A) {
 
   local_graph_type kokkosGraph(cols, rows);
 
-  LWGraph_kokkos graph(kokkosGraph);
+  LWGraph_kokkos<scalar_type, local_ordinal_type, device_type> graph(kokkosGraph);
+
   graph.SetBoundaryNodeMap(boundaryNodes);
 }
 
-int main(int argc, char **argv) {
+template<class scalar_type, class local_ordinal_type, class device_type>
+int main_(int argc, char **argv) {
   int n    = 100000;
   int loop = 10;
 
@@ -225,11 +224,10 @@ int main(int argc, char **argv) {
     }
   }
 
-  Kokkos::initialize(argc, argv);
+  typedef Kokkos::CrsMatrix<scalar_type, local_ordinal_type, device_type> local_matrix_type;
+  typedef typename device_type::execution_space execution_space;
 
-  srand(13721);
-
-  local_matrix_type A = kernel_construct(n);
+  local_matrix_type A = kernel_construct<scalar_type, local_ordinal_type, device_type>(n);
 
   execution_space::fence();
   Kokkos::Impl::Timer timer;
@@ -244,4 +242,49 @@ int main(int argc, char **argv) {
   printf("kernel_coalesce_drop: %.2e (s)\n", kernel_time / loop);
 
   execution_space::finalize();
+
+  return 0;
+}
+
+int main(int argc, char* argv[]) {
+
+  Kokkos::initialize(argc, argv);
+
+  srand(13721);
+
+  std::string node;
+  for (int i = 1; i < argc; i++)
+    if (strcmp(argv[i], "--node") == 0)
+      node = argv[++i];
+
+  std::cout << "node = " << node << std::endl;
+
+  if (node == "") {
+    return main_<double,int,Kokkos::DefaultExecutionSpace>(argc, argv);
+
+  } else if (node == "serial") {
+#ifdef KOKKOS_HAVE_SERIAL
+    return main_<double,int,Kokkos::Serial>(argc, argv);
+#else
+    throw std::runtime_error("Serial node type is disabled");
+#endif
+
+  } else if (node == "openmp") {
+#ifdef KOKKOS_HAVE_OPENMP
+    return main_<double,int,Kokkos::OpenMP>(argc, argv);
+#else
+    throw std::runtime_error("OpenMP node type is disabled");
+#endif
+
+  } else if (node == "cuda") {
+#ifdef KOKKOS_HAVE_CUDA
+    return main_<double,int,Kokkos::CUDA>(argc, argv);
+#else
+    throw std::runtime_error("CUDA node type is disabled");
+#endif
+  }
+
+  Kokkos::finalize();
+
+  return 0;
 }
