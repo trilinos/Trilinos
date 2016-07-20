55c5901c62191c7b0dd53658e3065f9bef376824
micheldemessieres <michel@demessieres.com>
2016-07-20 16:37:27 -0400

    Zoltan2: Converts tests to run as an internal CMake loop. #496
    Originally we were going to have different Jenkins tests.
    We decided it would be better to have it all in a single test.
    This runs a very extensive loop.
    It also has a setup for defining individual tests.
    From discussion it seems we may just define them that way.
    So this this is still in progress.

--- cmake/ctest/drivers/rhel6-x86_64/ctest_linux_nightly_jenkins_zoltan2.cmake
+++ cmake/ctest/drivers/rhel6-x86_64/ctest_linux_nightly_jenkins_zoltan2.cmake
@@ -53,6 +53,10 @@
 # ************************************************************************
 # @HEADER
 
+#
+# These options are some temporary stuff that will probably be removed later
+#
+
 # This will be modified later
 MESSAGE( "Turning CTEST_DO_UPDATES off to prevent attempt to access Sandia libraries." )
 SET(CTEST_DO_UPDATES FALSE)
@@ -62,19 +66,67 @@ SET(CTEST_DO_UPDATES FALSE)
 #SET(CTEST_DO_SUBMIT FALSE)
 #SET(CTEST_SUBMIT_CDASH_SUBPROJECTS_DEPS_FILE FALSE)
 
-# Setup binary location override - for custom setups but for normal cdash it can be unset
-IF( DEFINED ENV{OVERRIDE_BINARY_LOCATION} )
-  SET( CTEST_SOURCE_DIRECTORY ${CTEST_SCRIPT_DIRECTORY}/../../../../../Trilinos )
-  SET( CTEST_BINARY_DIRECTORY $ENV{OVERRIDE_BINARY_LOCATION} )
-  MESSAGE( "Custom build location setup set CTEST_SOURCE_DIRECTORY to $CTEST_SOURCE_DIRECTORY")
-  MESSAGE( "Custom build location setup set CTEST_BINARY_DIRECTORY to $CTEST_BINARY_DIRECTORY")
+#
+# Main macro for running a test called RunTest
+#
+
+MACRO( RunTest parallel epetra galeri pamgen scotchandparmetis loopLOandGOSetting loopScalarSetting )
+
+MESSAGE( "CALLING MACRO WITH ${parallel} ${epetra} ${galeri} ${pamgen} ${scotchandparmetis} ${loopLOandGOSetting} ${loopScalarSetting}" )
+
+# Pattern is set these false and then turn on the ones specific to the loop
+
+SET( Tpetra_INST_INT_INT_SETTING FALSE )
+SET( Tpetra_INST_INT_UNSIGNED_SETTING FALSE )
+SET( Tpetra_INST_INT_LONG_SETTING FALSE )
+SET( Tpetra_INST_INT_LONG_LONG_SETTING FALSE )
+SET( Tpetra_INST_INT_UNSIGNED_LONG_SETTING FALSE )
+
+SET( Tpetra_INST_DOUBLE_SETTING FALSE )
+SET( Tpetra_INST_FLOAT_SETTING FALSE )
+
+IF( ${parallel} STREQUAL "TRUE" )
+  SET( CUSTOM_PREFIX "par" )
+  SET(COMM_TYPE MPI )
+ELSE()
+  SET( CUSTOM_PREFIX "ser" )
+  SET(COMM_TYPE SERIAL )
 ENDIF()
+
+IF( ${loopLOandGOSetting} STREQUAL "INT_INT" )
+  SET( Tpetra_INST_INT_INT_SETTING TRUE )
+ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_UNSIGNED" )
+  SET( Tpetra_INST_INT_UNSIGNED_SETTING TRUE )
+ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_LONG" )
+  SET( Tpetra_INST_INT_LONG_SETTING TRUE )
+ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_LONG_LONG" )
+  SET( Tpetra_INST_INT_LONG_LONG_SETTING TRUE )
+ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_UNSIGNED_LONG" )
+  SET( Tpetra_INST_INT_UNSIGNED_LONG_SETTING TRUE )
+ENDIF()
+
+IF( ${loopScalarSetting} STREQUAL "DOUBLE" )
+  SET( Tpetra_INST_DOUBLE_SETTING TRUE )
+ELSEIF( loopScalarSetting STREQUAL "FLOAT" )
+  SET( Tpetra_INST_FLOAT_SETTING TRUE )
+ENDIF()
+
+# Currently just for Tech-X setup and not for normal Trilinos CDash testing
+IF( DEFINED ENV{OVERRIDE_BINARY_LOCATION_ROOT} )
+  SET( CUSTOM_BUILD_FOLDER_NAME "${CUSTOM_PREFIX}-LOGO-${loopLOandGOSetting}-Scalar-${loopScalarSetting}-Epetra-${epetra}-ScotchParmetis-${scotchandparmetis}-Gal-${galeri}-Pamgen-${pamgen}" )
+  SET( CTEST_BINARY_DIRECTORY "$ENV{OVERRIDE_BINARY_LOCATION_ROOT}/${CUSTOM_BUILD_FOLDER_NAME}" )
+  SET( CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/../../../../../Trilinos" )
+  MESSAGE( "Custom build location setup set CTEST_BINARY_DIRECTORY to ${CTEST_BINARY_DIRECTORY}")
+  MESSAGE( "Custom build location setup set CTEST_SOURCE_DIRECTORY to ${CTEST_SOURCE_DIRECTORY}")
+  # Not necessary but convenient - you can comment out the actual run TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER() the end and build all the directories quickly for testing setup
+  file(MAKE_DIRECTORY ${CTEST_BINARY_DIRECTORY})
+ENDIF()
+
 
 #
 # Set the options specific to this build case - must be BEFORE including the jenkins file
 #
 
-SET(COMM_TYPE $ENV{JENKINS_COMM_TYPE} )
 SET(BUILD_TYPE DEBUG)
 #SET(BUILD_DIR_NAME)
 SET(CTEST_PARALLEL_LEVEL 8)
@@ -90,28 +142,75 @@ SET(Trilinos_PACKAGES Zoltan2)
 SET(EXTRA_CONFIGURE_OPTIONS
 "-DZoltan2_ENABLE_Experimental:BOOL=$ENV{JENKINS_Zoltan2_ENABLE_Experimental}"
 "-DTeuchos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=$ENV{JENKINS_Teuchos_ENABLE_EXPLICIT_INSTANTIATION}"
-"-DTeuchos_ENABLE_LONG_LONG_INT:BOOL=$ENV{JENKINS_Teuchos_ENABLE_LONG_LONG_INT}"
 
-"-DTpetra_INST_INT_INT:BOOL=$ENV{JENKINS_Tpetra_INST_INT_INT}"
-"-DTpetra_INST_INT_UNSIGNED:BOOL=$ENV{JENKINS_Tpetra_INST_INT_UNSIGNED}"
-"-DTpetra_INST_INT_LONG:BOOL=$ENV{JENKINS_Tpetra_INST_INT_LONG}"
-"-DTpetra_INST_INT_LONG_LONG:BOOL=$ENV{JENKINS_Tpetra_INST_INT_LONG_LONG}"
-"-DTpetra_INST_INT_UNSIGNED_LONG:BOOL=$ENV{JENKINS_Tpetra_INST_INT_UNSIGNED_LONG}"
+# Not currently implemented
+# "-DTpetra_INST_COMPLEX_DOUBLE:BOOL=$ENV{JENKINS_Tpetra_INST_COMPLEX_DOUBLE}"
+# "-DTpetra_INST_COMPLEX_FLOAT:BOOL=$ENV{JENKINS_Tpetra_INST_COMPLEX_FLOAT}"
 
-"-DTpetra_INST_DOUBLE:BOOL=$ENV{JENKINS_Tpetra_INST_DOUBLE}"
-"-DTpetra_INST_FLOAT:BOOL=$ENV{JENKINS_Tpetra_INST_FLOAT}"
-"-DTpetra_INST_COMPLEX_DOUBLE:BOOL=$ENV{JENKINS_Tpetra_INST_COMPLEX_DOUBLE}"
-"-DTpetra_INST_COMPLEX_FLOAT:BOOL=$ENV{JENKINS_Tpetra_INST_COMPLEX_FLOAT}"
+# Need to decide about this
+"-DTeuchos_ENABLE_LONG_LONG_INT:BOOL=TRUE"
 
-"-DTPL_ENABLE_Scotch:BOOL=$ENV{JENKINS_TPL_ENABLE_Scotch}"
-"-DTrilinos_ENABLE_Galeri:BOOL=$ENV{JENKINS_Trilinos_ENABLE_Galeri}"
-"-DTrilinos_ENABLE_Pamgen:BOOL=$ENV{JENKINS_Trilinos_ENABLE_Pamgen}"
-"-DTPL_ENABLE_ParMETIS:BOOL=$ENV{JENKINS_TPL_ENABLE_ParMETIS}"
-"-DTrilinos_ENABLE_Epetra:BOOL=$ENV{JENKINS_Trilinos_ENABLE_Epetra}"
+"-DTpetra_INST_INT_INT:BOOL=${Tpetra_INST_INT_INT_SETTING}"
+"-DTpetra_INST_INT_UNSIGNED:BOOL=${Tpetra_INST_INT_UNSIGNED_SETTING}"
+"-DTpetra_INST_INT_LONG:BOOL=${Tpetra_INST_INT_LONG_SETTING}"
+"-DTpetra_INST_INT_LONG_LONG:BOOL=${Tpetra_INST_INT_LONG_LONG_SETTING}"
+"-DTpetra_INST_INT_UNSIGNED_LONG:BOOL=${Tpetra_INST_INT_UNSIGNED_LONG_SETTING}"
+
+"-DTpetra_INST_DOUBLE:BOOL=${Tpetra_INST_DOUBLE_SETTING}"
+"-DTpetra_INST_FLOAT:BOOL=${Tpetra_INST_FLOAT_SETTING}"
+
+"-DTPL_ENABLE_Scotch:BOOL=${scotchandparmetis}"
+"-DTPL_ENABLE_ParMETIS:BOOL=${scotchandparmetis}"
+
+"-DTrilinos_ENABLE_Galeri:BOOL=${galeri}"
+"-DTrilinos_ENABLE_Pamgen:BOOL=${pamgen}"
+"-DTrilinos_ENABLE_Epetra:BOOL=${epetra}"
 )
+
+MESSAGE( "EXTRA_CONFIGURE_OPTIONS was set to ${EXTRA_CONFIGURE_OPTIONS}" )
 
 #
 # Set the rest of the system-specific options and run the dashboard build/test
 #
 
 TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
+
+ENDMACRO( RunTest )
+
+
+MACRO( RunFullLoop )
+
+  MESSAGE( "Begin the loop of different tests." )
+
+  FOREACH( parallel TRUE )                       # Right now just parallel
+  FOREACH( epetra TRUE )                         # Right now just epetra on
+  FOREACH( galeri FALSE )                        # Right now just galeri off
+  FOREACH( pamgen FALSE )                        # Right now just pamgen off
+  FOREACH( scotchandparmetis FALSE TRUE )        # parmetis+scotch off (0) or on (1) - Right now both together
+  FOREACH( localglobal RANGE INT_INT INT_UNSIGNED INT_LONG INT_LONG_LONG INT_UNSIGNED_LONG )               # 5 settings for LO and GO
+  FOREACH( scalar DOUBLE FLOAT )                 # double or float - need to check on int status
+
+    RunTest( ${parallel} ${epetra} ${galeri} ${pamgen} ${scotchandparmetis} ${localglobal} ${scalar} )
+
+  ENDFOREACH( loopScalarSetting )
+  ENDFOREACH( loopLOandGOSetting )
+  ENDFOREACH( parmetisScotchSetting )
+  ENDFOREACH( pamgenSetting )
+  ENDFOREACH( galeriSetting )
+  ENDFOREACH( epetraSetting )
+  ENDFOREACH( parallelSetting )
+
+ENDMACRO( RunFullLoop )
+
+
+
+
+#        PARALLEL    EPETRA   GALERI      PAMGEN      SCOTCH&PARMETIS       LO-GO       Scalar
+#RunTest(  TRUE        TRUE     FALSE      FALSE          FALSE            INT_INT       DOUBLE   )
+#RunTest(  TRUE        TRUE     FALSE      FALSE          FALSE            INT_INT       FLOAT    )
+#RunTest(  TRUE        TRUE     FALSE      FALSE          FALSE            INT_LONG      FLOAT    )
+#RunTest(  TRUE        TRUE     TRUE       FALSE          TRUE             INT_LONG      FLOAT    )
+
+
+# This will take hours
+RunFullLoop()
