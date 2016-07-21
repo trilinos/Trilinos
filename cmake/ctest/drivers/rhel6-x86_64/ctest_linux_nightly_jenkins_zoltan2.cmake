7b3d1d661b0b23d802da36793d9a1b8cdcc775eb
micheldemessieres <michel@demessieres.com>
2016-07-21 15:21:43 -0400

    Zoltan2: Changed tests to be individual setups. #496
    Previously we considered doing a full loop.
    Now each test for Zoltan2 CDashis setup up individually.
    These will be modfied/extended.

--- cmake/ctest/drivers/rhel6-x86_64/ctest_linux_nightly_jenkins_zoltan2.cmake
+++ cmake/ctest/drivers/rhel6-x86_64/ctest_linux_nightly_jenkins_zoltan2.cmake
@@ -53,10 +53,6 @@
 # ************************************************************************
 # @HEADER
 
-#
-# These options are some temporary stuff that will probably be removed later
-#
-
 # This will be modified later
 MESSAGE( "Turning CTEST_DO_UPDATES off to prevent attempt to access Sandia libraries." )
 SET(CTEST_DO_UPDATES FALSE)
@@ -66,55 +62,29 @@ SET(CTEST_DO_UPDATES FALSE)
 #SET(CTEST_DO_SUBMIT FALSE)
 #SET(CTEST_SUBMIT_CDASH_SUBPROJECTS_DEPS_FILE FALSE)
 
-#
-# Main macro for running a test called RunTest
-#
+MACRO( RunTest testName )
 
-MACRO( RunTest parallel epetra galeri pamgen scotchandparmetis loopLOandGOSetting loopScalarSetting )
+MESSAGE( "Running subtest: ${testName}" )
+MESSAGE( "EXTRA_CONFIGURE_OPTIONS: ${EXTRA_CONFIGURE_OPTIONS}" )
 
-MESSAGE( "CALLING MACRO WITH ${parallel} ${epetra} ${galeri} ${pamgen} ${scotchandparmetis} ${loopLOandGOSetting} ${loopScalarSetting}" )
-
-# Pattern is set these false and then turn on the ones specific to the loop
-
-SET( Tpetra_INST_INT_INT_SETTING FALSE )
-SET( Tpetra_INST_INT_UNSIGNED_SETTING FALSE )
-SET( Tpetra_INST_INT_LONG_SETTING FALSE )
-SET( Tpetra_INST_INT_LONG_LONG_SETTING FALSE )
-SET( Tpetra_INST_INT_UNSIGNED_LONG_SETTING FALSE )
-
-SET( Tpetra_INST_DOUBLE_SETTING FALSE )
-SET( Tpetra_INST_FLOAT_SETTING FALSE )
-
-IF( ${parallel} STREQUAL "TRUE" )
-  SET( CUSTOM_PREFIX "par" )
-  SET(COMM_TYPE MPI )
-ELSE()
-  SET( CUSTOM_PREFIX "ser" )
-  SET(COMM_TYPE SERIAL )
+# Two hacks that need to be resolved
+# I did this so the setups for these tests would be 'natural' config settings - not sure why the orignal format worked with BUILD_TYPE and COMM_TYPE
+SET(COMM_TYPE SERIAL )
+IF( "${EXTRA_CONFIGURE_OPTIONS}" MATCHES "-DTPL_ENABLE_MPI=ON" OR 
+  "${EXTRA_CONFIGURE_OPTIONS}" MATCHES "-DTPL_ENABLE_MPI=TRUE" OR
+  "${EXTRA_CONFIGURE_OPTIONS}" MATCHES "-DTPL_ENABLE_MPI:BOOL=ON" OR
+  "${EXTRA_CONFIGURE_OPTIONS}" MATCHES "-DTPL_ENABLE_MPI:BOOL=TRUE" )
+    SET(COMM_TYPE MPI )
 ENDIF()
-
-IF( ${loopLOandGOSetting} STREQUAL "INT_INT" )
-  SET( Tpetra_INST_INT_INT_SETTING TRUE )
-ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_UNSIGNED" )
-  SET( Tpetra_INST_INT_UNSIGNED_SETTING TRUE )
-ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_LONG" )
-  SET( Tpetra_INST_INT_LONG_SETTING TRUE )
-ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_LONG_LONG" )
-  SET( Tpetra_INST_INT_LONG_LONG_SETTING TRUE )
-ELSEIF( ${loopLOandGOSetting} STREQUAL "INT_UNSIGNED_LONG" )
-  SET( Tpetra_INST_INT_UNSIGNED_LONG_SETTING TRUE )
-ENDIF()
-
-IF( ${loopScalarSetting} STREQUAL "DOUBLE" )
-  SET( Tpetra_INST_DOUBLE_SETTING TRUE )
-ELSEIF( loopScalarSetting STREQUAL "FLOAT" )
-  SET( Tpetra_INST_FLOAT_SETTING TRUE )
+SET(BUILD_TYPE RELEASE)
+IF( "${EXTRA_CONFIGURE_OPTIONS}" MATCHES "-DCMAKE_BUILD_TYPE:STRING=DEBUG" OR
+  "${EXTRA_CONFIGURE_OPTIONS}" MATCHES "-DCMAKE_BUILD_TYPE=DEBUG" )
+    SET(BUILD_TYPE DEBUG)
 ENDIF()
 
 # Currently just for Tech-X setup and not for normal Trilinos CDash testing
 IF( DEFINED ENV{OVERRIDE_BINARY_LOCATION_ROOT} )
-  SET( CUSTOM_BUILD_FOLDER_NAME "${CUSTOM_PREFIX}-LOGO-${loopLOandGOSetting}-Scalar-${loopScalarSetting}-Epetra-${epetra}-ScotchParmetis-${scotchandparmetis}-Gal-${galeri}-Pamgen-${pamgen}" )
-  SET( CTEST_BINARY_DIRECTORY "$ENV{OVERRIDE_BINARY_LOCATION_ROOT}/${CUSTOM_BUILD_FOLDER_NAME}" )
+  SET( CTEST_BINARY_DIRECTORY "$ENV{OVERRIDE_BINARY_LOCATION_ROOT}/${testName}" )
   SET( CTEST_SOURCE_DIRECTORY "${CTEST_SCRIPT_DIRECTORY}/../../../../../Trilinos" )
   MESSAGE( "Custom build location setup set CTEST_BINARY_DIRECTORY to ${CTEST_BINARY_DIRECTORY}")
   MESSAGE( "Custom build location setup set CTEST_SOURCE_DIRECTORY to ${CTEST_SOURCE_DIRECTORY}")
@@ -123,94 +93,149 @@ IF( DEFINED ENV{OVERRIDE_BINARY_LOCATION_ROOT} )
 ENDIF()
 
 
-#
-# Set the options specific to this build case - must be BEFORE including the jenkins file
-#
-
-SET(BUILD_TYPE DEBUG)
 #SET(BUILD_DIR_NAME)
 SET(CTEST_PARALLEL_LEVEL 8)
 SET(CTEST_TEST_TYPE Experimental)
 SET(CTEST_TEST_TIMEOUT 900)
 SET(CTEST_START_WITH_EMPTY_BINARY_DIRECTORY FALSE)
 
-# Generic driver script for building & testing Trilinos with Jenkins
 INCLUDE("${CTEST_SCRIPT_DIRECTORY}/../../TrilinosCTestDriverCore.generic.jenkins.cmake")
-
 SET(Trilinos_PACKAGES Zoltan2)
 
-SET(EXTRA_CONFIGURE_OPTIONS
-"-DZoltan2_ENABLE_Experimental:BOOL=$ENV{JENKINS_Zoltan2_ENABLE_Experimental}"
-"-DTeuchos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=$ENV{JENKINS_Teuchos_ENABLE_EXPLICIT_INSTANTIATION}"
-
-# Not currently implemented
-# "-DTpetra_INST_COMPLEX_DOUBLE:BOOL=$ENV{JENKINS_Tpetra_INST_COMPLEX_DOUBLE}"
-# "-DTpetra_INST_COMPLEX_FLOAT:BOOL=$ENV{JENKINS_Tpetra_INST_COMPLEX_FLOAT}"
-
-# Need to decide about this
-"-DTeuchos_ENABLE_LONG_LONG_INT:BOOL=TRUE"
-
-"-DTpetra_INST_INT_INT:BOOL=${Tpetra_INST_INT_INT_SETTING}"
-"-DTpetra_INST_INT_UNSIGNED:BOOL=${Tpetra_INST_INT_UNSIGNED_SETTING}"
-"-DTpetra_INST_INT_LONG:BOOL=${Tpetra_INST_INT_LONG_SETTING}"
-"-DTpetra_INST_INT_LONG_LONG:BOOL=${Tpetra_INST_INT_LONG_LONG_SETTING}"
-"-DTpetra_INST_INT_UNSIGNED_LONG:BOOL=${Tpetra_INST_INT_UNSIGNED_LONG_SETTING}"
-
-"-DTpetra_INST_DOUBLE:BOOL=${Tpetra_INST_DOUBLE_SETTING}"
-"-DTpetra_INST_FLOAT:BOOL=${Tpetra_INST_FLOAT_SETTING}"
-
-"-DTPL_ENABLE_Scotch:BOOL=${scotchandparmetis}"
-"-DTPL_ENABLE_ParMETIS:BOOL=${scotchandparmetis}"
-
-"-DTrilinos_ENABLE_Galeri:BOOL=${galeri}"
-"-DTrilinos_ENABLE_Pamgen:BOOL=${pamgen}"
-"-DTrilinos_ENABLE_Epetra:BOOL=${epetra}"
-)
-
 MESSAGE( "EXTRA_CONFIGURE_OPTIONS was set to ${EXTRA_CONFIGURE_OPTIONS}" )
-
-#
-# Set the rest of the system-specific options and run the dashboard build/test
-#
-
 TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
 
 ENDMACRO( RunTest )
 
+#
+# These are the specific tests we want to run
+#
 
-MACRO( RunFullLoop )
-
-  MESSAGE( "Begin the loop of different tests." )
-
-  FOREACH( parallel TRUE )                       # Right now just parallel
-  FOREACH( epetra TRUE )                         # Right now just epetra on
-  FOREACH( galeri FALSE )                        # Right now just galeri off
-  FOREACH( pamgen FALSE )                        # Right now just pamgen off
-  FOREACH( scotchandparmetis FALSE TRUE )        # parmetis+scotch off (0) or on (1) - Right now both together
-  FOREACH( localglobal RANGE INT_INT INT_UNSIGNED INT_LONG INT_LONG_LONG INT_UNSIGNED_LONG )               # 5 settings for LO and GO
-  FOREACH( scalar DOUBLE FLOAT )                 # double or float - need to check on int status
-
-    RunTest( ${parallel} ${epetra} ${galeri} ${pamgen} ${scotchandparmetis} ${localglobal} ${scalar} )
-
-  ENDFOREACH( loopScalarSetting )
-  ENDFOREACH( loopLOandGOSetting )
-  ENDFOREACH( parmetisScotchSetting )
-  ENDFOREACH( pamgenSetting )
-  ENDFOREACH( galeriSetting )
-  ENDFOREACH( epetraSetting )
-  ENDFOREACH( parallelSetting )
-
-ENDMACRO( RunFullLoop )
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_DOUBLE:BOOL=TRUE"
+  "-DTpetra_INST_INT_INT:BOOL=TRUE"
+)
+RunTest( par-int-int-double )
 
 
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_FLOAT:BOOL=TRUE"
+  "-DTpetra_INST_INT_INT:BOOL=TRUE"
+)
+RunTest( par-int-int-float )
 
 
-#        PARALLEL    EPETRA   GALERI      PAMGEN      SCOTCH&PARMETIS       LO-GO       Scalar
-#RunTest(  TRUE        TRUE     FALSE      FALSE          FALSE            INT_INT       DOUBLE   )
-#RunTest(  TRUE        TRUE     FALSE      FALSE          FALSE            INT_INT       FLOAT    )
-#RunTest(  TRUE        TRUE     FALSE      FALSE          FALSE            INT_LONG      FLOAT    )
-#RunTest(  TRUE        TRUE     TRUE       FALSE          TRUE             INT_LONG      FLOAT    )
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_FLOAT:BOOL=TRUE"
+  "-DTpetra_INST_INT_UNSIGNED:BOOL=TRUE"
+)
+RunTest( par-int-unsigned-float )
 
 
-# This will take hours
-RunFullLoop()
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_FLOAT:BOOL=TRUE"
+  "-DTpetra_INST_INT_LONG:BOOL=TRUE"
+)
+RunTest( par-int-long-float )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_FLOAT:BOOL=TRUE"
+  "-DTpetra_INST_INT_UNSIGNED_LONG:BOOL=TRUE"
+)
+RunTest( par-int-unsignedlong-float )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DCMAKE_CXX_FLAGS=-DTEST_STK_DATA_TYPES"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+)
+RunTest( par-stk )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+)
+RunTest( par-stk-off )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_DOUBLE:BOOL=TRUE"
+  "-DTeuchos_ENABLE_LONG_LONG_INT:BOOL=TRUE"
+  "-DTpetra_INST_INT_LONG_LONG:BOOL=TRUE"
+)
+RunTest( par-int-longlong-double )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=ON"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_DOUBLE:BOOL=TRUE"
+  "-DTpetra_INST_INT_INT:BOOL=TRUE"
+  "-DTPL_ENABLE_Scotch:BOOL=TRUE"
+  "-DTPL_ENABLE_ParMETIS:BOOL=TRUE"
+)
+RunTest( par-int-int-double-scotch-parmetis )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_DOUBLE:BOOL=TRUE"
+  "-DTpetra_INST_INT_INT:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Galeri:BOOL=TRUE"
+)
+RunTest( par-int-int-double-galeri )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_DOUBLE:BOOL=TRUE"
+  "-DTpetra_INST_INT_INT:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Pamgen:BOOL=TRUE"
+)
+RunTest( par-int-int-double-pamgen )
+
+
+SET( EXTRA_CONFIGURE_OPTIONS
+  "-DCMAKE_BUILD_TYPE:STRING=RELEASE"
+  "-DTPL_ENABLE_MPI=OFF"
+  "-DTrilinos_ENABLE_EXPLICIT_INSTANTIATION:BOOL=TRUE"
+  "-DTrilinos_ENABLE_Epetra:BOOL=TRUE"
+  "-DTpetra_INST_DOUBLE:BOOL=TRUE"
+  "-DTpetra_INST_INT_INT:BOOL=TRUE"
+)
+RunTest( ser-int-int-double )
