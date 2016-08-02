cebb4423b492190870c5c44895ddc69eb0006886
Drew Kouri <dpkouri@sandia.gov>
2016-08-02 13:35:15 -0600

    ROL: Made target evaluation dimension independent.

--- packages/rol/example/PDE-OPT/poisson/obj_poisson.hpp
+++ packages/rol/example/PDE-OPT/poisson/obj_poisson.hpp
@@ -59,7 +59,12 @@ private:
   Teuchos::RCP<Intrepid::FieldContainer<Real> > target_;
 
   Real targetFunc(const std::vector<Real> x) const {
-    return x[0]*x[0] + x[1]*x[1];
+    int size = x.size();
+    Real val(0);
+    for (int i = 0; i < size; ++i) {
+      val += x[i]*x[i];
+    }
+    return val;
   }
 
 public:
