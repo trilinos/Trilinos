2d14bd4b515d92e3106a2ce248ebff48daa942e5
Nick Roehrig <nsroehr@sandia.gov>
2016-08-03 15:24:37 -0600

    added doc test to show how to write a mesh.
    (From STK/SEACAS)

---
+++ packages/stk/stk_doc_tests/stk_io/howToWriteMesh.cpp
@@ -0,0 +1,39 @@
+#include <unistd.h>
+#include <gtest/gtest.h>
+#include <stk_mesh/base/MetaData.hpp>
+#include <stk_mesh/base/BulkData.hpp>
+#include <stk_mesh/base/Comm.hpp>
+#include <stk_io/StkMeshIoBroker.hpp>
+#include <stk_unit_test_utils/ioUtils.hpp>
+namespace
+{
+
+TEST(StkIoHowTo, WriteMesh)
+{
+    std::string filename = "output.exo";
+    {
+        stk::mesh::MetaData meta;
+        stk::mesh::BulkData bulk(meta, MPI_COMM_WORLD);
+        stk::unit_test_util::fill_mesh_using_stk_io("generated:1x1x4", bulk);
+
+        stk::io::StkMeshIoBroker stkIo;
+        stkIo.set_bulk_data(bulk);
+        size_t outputFileIndex = stkIo.create_output_mesh(filename, stk::io::WRITE_RESULTS);
+        stkIo.write_output_mesh(outputFileIndex);
+        stkIo.write_defined_output_fields(outputFileIndex);
+    }
+
+    {
+        stk::mesh::MetaData meta;
+        stk::mesh::BulkData bulk(meta, MPI_COMM_WORLD);
+        stk::unit_test_util::fill_mesh_using_stk_io(filename, bulk);
+
+        std::vector<size_t> entityCounts;
+        stk::mesh::comm_mesh_counts(bulk, entityCounts);
+        EXPECT_EQ(4u, entityCounts[stk::topology::ELEM_RANK]);
+    }
+
+    unlink(filename.c_str());
+}
+
+}
