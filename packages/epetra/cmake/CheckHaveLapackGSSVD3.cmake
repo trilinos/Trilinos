e4bb3ee47d72e20cb8ca93a66a1b4b463f292daf
micheldemessieres <michel@demessieres.com>
2016-08-16 14:24:52 -0400

    Epetra: Lapack testing of dggsvd3 alternative option. (#480)
    This is an alternate option for doing this.
    
    The previous commit uses a source check to probe.
    This version explicitly checks for mangle names.
    To resolve is exactly what we need to check for.
    This currently checks dggsvd3, dggsvd3_, DGGSVD3, and DGGSVD3_.

--- packages/epetra/cmake/CheckHaveLapackGSSVD3.cmake
+++ packages/epetra/cmake/CheckHaveLapackGSSVD3.cmake
@@ -1,6 +1,5 @@
 # LAPACK 3.6.0 deprecates dggsvd and sggsvd (Issue #480)
 # The new versions of dggsvd and sggsvd are called dggsvd3 and sggsvd3
-# This probes for the deprecated version
 #
 # dggsvd (JOBU, JOBV, JOBQ, M, N, P, K, L, A, LDA, B, LDB, ALPHA, BETA, U, LDU, V, LDV, Q, LDQ, WORK, IWORK, INFO)
 # sggsvd (JOBU, JOBV, JOBQ, M, N, P, K, L, A, LDA, B, LDB, ALPHA, BETA, U, LDU, V, LDV, Q, LDQ, WORK, IWORK, INFO)
@@ -13,94 +12,26 @@
 #   Information is at:
 #   http://www.netlib.org/lapack/explore-html/d6/db3/dggsvd3_8f_a4a187519e5c71da3b3f67c85e9baf0f2.html#a4a187519e5c71da3b3f67c85e9baf0f2
 
-INCLUDE(CheckCXXSourceRuns)
+INCLUDE(CheckFunctionExists)
 
 FUNCTION(CHECK_HAVE_LAPACK_GSSVD3 VARNAME)
 
   SET(CMAKE_REQUIRED_LIBRARIES ${TPL_LAPACK_LIBRARIES})
 
-  SET(SOURCE
-  "
+  CHECK_FUNCTION_EXISTS( dggsvd3   HAVE_dggsvd3 )
+  CHECK_FUNCTION_EXISTS( dggsvd3_  HAVE_dggsvd3_POST )
+  CHECK_FUNCTION_EXISTS( DGGSVD3   HAVE_DGGSVD3 )
+  CHECK_FUNCTION_EXISTS( DGGSVD3_  HAVE_DGGSVD3_POST )
 
-#define F77_BLAS_MANGLE${F77_BLAS_MANGLE}
+  IF( HAVE_dggsvd3 OR HAVE_dggsvd3_POST OR HAVE_DGGSVD3 OR HAVE_DGGSVD3_POST )
+    MESSAGE( "Found new version of lapack. dggsvd3 is available." )
 
-#define DGGSVD_F77   F77_BLAS_MANGLE(dggsvd3,DGGSVD3)
+    # Note calling CHECK_FUNCTION_EXISTS( dggsvd3_, ${VARNAME} ) is ok for scope
+    # but setting like this after several checks, it seems PARENT_SCOPE must be added
+    SET( ${VARNAME} 1 PARENT_SCOPE )
 
-extern \"C\" { void DGGSVD_F77(
-        const char * JOBU,
-        const char * JOBV,
-        const char * JOBQ,
-        const int * M,
-        const int * N,
-        const int * P,
-        int * K,
-        int * L,
-        double * A,
-        const int * LDA,
-        double * B,
-        const int * LDB,
-        double * ALPHA,
-        double * BETA,
-        double * U,
-        const int * LDU,
-        double * V,
-        const int * LDV,
-        double * Q,
-        const int * LDQ,
-        double * WORK,
-        const int * LWORK, // the new parameter LWORK
-        int * IWORK,
-        int * INFO); }
-
-int main()
-{
-  const char JOBU = 'N';
-  const char JOBV = 'N';
-  const char JOBQ = 'N';
-  const int M = 2;
-  const int N = 2;
-  const int P = 2;
-  int K = 0;
-  int L = 0;
-  double A[4] = { 1.0, 1.0, 1.0, 1.0 };
-  int LDA = 2;
-  double B[4] = { 1.0, 1.0, 1.0, 1.0 };
-  int LDB = 2;
-  double alpha[2] = { 1.0, 1.0 };
-  double beta[2] = { 1.0, 1.0 };
-  double U[4] = { 1.0, 1.0, 1.0, 1.0 };
-  const int LDU = 2;
-  double V[4] = { 1.0, 1.0, 1.0, 1.0 };
-  const int LDV = 2;
-  double Q[4] = { 1.0, 1.0, 1.0, 1.0 };
-  const int LDQ = 2;
-  double WORK[4] = { 1.0, 1.0, 1.0, 1.0 };
-  const int LWORK = 4;
-  int IWORK[2] = { 1, 1 };
-  int INFO = -9999; // will check for valid exit code 0
-
-  DGGSVD_F77(&JOBU, &JOBV, &JOBQ,
-    &M, &N, &P,
-    &K, &L,
-    A, &LDA, B, &LDB,
-    alpha, beta,
-    U, &LDU, V, &LDV, Q, &LDQ,
-    WORK,
-    &LWORK,   // the new parameter LWORK
-    IWORK, &INFO);
-
-  if( INFO == 0 )
-  {
-    return 0;
-  }
-  else
-  {
-    return 1;
-  }
-}
-  "
-  )
-
-  CHECK_CXX_SOURCE_RUNS("${SOURCE}" ${VARNAME})
+  ELSE()
+    MESSAGE( "Did not find new version of lapack. dggsvd3 is not available." )
+  ENDIF()
 
 ENDFUNCTION()
