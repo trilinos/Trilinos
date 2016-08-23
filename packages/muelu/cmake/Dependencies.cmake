cf5120bd4d2dc933447b29c47b9e44e35d2834a9
Andrey Prokopenko <aprokop@sandia.gov>
2016-08-23 15:48:29 -0600

    MueLu: added Pamgen as an optional dependency for tests

--- packages/muelu/cmake/Dependencies.cmake
+++ packages/muelu/cmake/Dependencies.cmake
@@ -2,7 +2,7 @@ SET(LIB_REQUIRED_DEP_PACKAGES Teuchos Xpetra)
 SET(LIB_OPTIONAL_DEP_PACKAGES Amesos Amesos2 Epetra EpetraExt Teko
                               Ifpack Ifpack2 ML Tpetra Zoltan Zoltan2 Stratimikos Thyra ThyraTpetraAdapters Isorropia KokkosCore KokkosContainers)
 SET(TEST_REQUIRED_DEP_PACKAGES )
-SET(TEST_OPTIONAL_DEP_PACKAGES AztecOO Galeri Belos)
+SET(TEST_OPTIONAL_DEP_PACKAGES AztecOO Galeri Belos Pamgen)
 SET(LIB_REQUIRED_DEP_TPLS BLAS LAPACK)
 SET(LIB_OPTIONAL_DEP_TPLS Boost MATLAB AmgX CGAL)
 SET(TEST_REQUIRED_DEP_TPLS)
