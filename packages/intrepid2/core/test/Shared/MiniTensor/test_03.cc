5d95be017b0c8ecfc7a654e671065e1a35a5dee6
Alejandro Mota <amota@sandia.gov>
2016-08-23 18:08:51 -0700

    Intrepid2: Fixed header.

--- packages/intrepid2/core/test/Shared/MiniTensor/test_03.cc
+++ packages/intrepid2/core/test/Shared/MiniTensor/test_03.cc
@@ -34,9 +34,7 @@
 // NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 // SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 //
-// Questions? Contact lead developers:
-//              Drew Kouri   (dpkouri@sandia.gov) and
-//              Denis Ridzal (dridzal@sandia.gov)
+// Questions: Alejandro Mota (amota@sandia.gov)
 //
 // ************************************************************************
 // @HEADER
