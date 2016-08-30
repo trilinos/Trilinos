867b9598ec81b665908b5b9b65c8bbfe575dfd00
Mark Hoemmen <mhoemme@sandia.gov>
2016-08-30 14:25:05 -0600

    Ifpack2: Integrate Mehmet's new test for multithreaded Gauss-Seidel
    @trilinos/ifpack2
    
    Build/Test Cases Summary
    Enabled Packages: TpetraKernels, Ifpack2
    Disabled Packages: FEI,PyTrilinos,Moertel,STK,SEACAS,ThreadPool,OptiPack,Rythmos,Intrepid
    0) MPI_DEBUG => Test case MPI_DEBUG was not run! => Does not affect push readiness! (-1.00 min)
    1) SERIAL_RELEASE => Test case SERIAL_RELEASE was not run! => Does not affect push readiness! (-1.00 min)
    2) MPI_DEBUG => passed: passed=39,notpassed=0 (1.46 min)
    3) SERIAL_RELEASE => passed: passed=33,notpassed=0 (6.77 min)

--- packages/ifpack2/test/unit_tests/Ifpack2_UnitTestSGSMT.cpp
+++ packages/ifpack2/test/unit_tests/Ifpack2_UnitTestSGSMT.cpp
@@ -47,79 +47,16 @@ HEADER
 #include "Teuchos_UnitTestHarness.hpp"
 #include "Ifpack2_UnitTestHelpers.hpp"
 #include "Ifpack2_Relaxation.hpp"
-
+#include "MatrixMarket_Tpetra.hpp"
 #include <KokkosKernels_GaussSeidel.hpp>
 
 namespace { // (anonymous)
 
 using Teuchos::RCP;
-using Teuchos::rcp;
-using Teuchos::rcp_const_cast;
-typedef Tpetra::global_size_t GST;
 typedef tif_utest::Node Node;
 using std::endl;
 
-TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(SGS_MT, OneSweep, Scalar, LocalOrdinal, GlobalOrdinal)
-{
-  using Teuchos::Comm;
-  using Teuchos::ParameterList;
-  using Teuchos::RCP;
-  using std::endl;
-  typedef LocalOrdinal LO;
-  typedef GlobalOrdinal GO;
-  typedef Tpetra::Map<LO, GO, Node> map_type;
-  typedef Tpetra::CrsMatrix<Scalar, LO, GO, Node> crs_matrix_type;
-  typedef Tpetra::RowMatrix<Scalar, LO, GO, Node> row_matrix_type;
-  typedef Tpetra::MultiVector<Scalar, LO, GO, Node> mv_type;
-  typedef Teuchos::ScalarTraits<Scalar> STS;
-
-  out << "Make sure that one sweep of threaded SGS doesn't crash" << endl;
-
-  RCP<const Comm<int> > comm = Tpetra::DefaultPlatform::getDefaultPlatform ().getComm ();
-  if (comm->getSize () == 1) {
-    out << "The unit test's (MPI) communicator only contains one process."
-        << endl << "This test only makes sense if the communicator contains "
-        << "multiple processes." << endl << "I'll let the test pass trivially."
-        << endl;
-    return;
-  }
-
-  const LO lclNumRows = 100;
-  const GST INVALID = Teuchos::OrdinalTraits<GST>::invalid ();
-  RCP<const map_type> rowMap (new map_type (INVALID, lclNumRows, 0, comm));
-  RCP<const crs_matrix_type> crsMatrix = tif_utest::create_test_matrix<Scalar,LO,GO,Node> (rowMap);
-
-  // We don't really need prec to be a pointer here, but it's
-  // convenient for putting the constructor call in a TEST_NOTHROW
-  // macro.  Otherwise the declaration of prec would be in an inner
-  // scope and we couldn't use it below.
-  RCP<Ifpack2::Relaxation<row_matrix_type> > prec;
-  {
-    TEST_NOTHROW( prec = rcp (new Ifpack2::Relaxation<row_matrix_type> (crsMatrix)) );
-  }
-
-  ParameterList params;
-  params.set ("relaxation: type", "MT Symmetric Gauss-Seidel");
-  prec->setParameters (params);
-  TEST_NOTHROW( prec->initialize () );
-  TEST_NOTHROW( prec->compute () );
-
-  mv_type X (rowMap, 2);
-  X.putScalar (STS::zero ());
-  mv_type B (rowMap, 2);
-  B.putScalar (STS::one ());
-
-  prec->apply (B, X);
-}
-
-// Check that multiple sweeps of Symmetric Gauss-Seidel (SGS) work correctly.
-//
-// The point is really to ensure that SGS does the Import (from the
-// domain Map input X to the column Map version of X) in the right
-// place, if an Import is needed.  Thus, this example uses a matrix
-// with a nontrivial Import (i.e., where the domain and column Maps
-// are not the same).
-TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(SGS_MT, MultipleSweeps, Scalar, LO, GO)
+TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(SGS_MT, JacobiComparison, Scalar, LO, GO)
 {
   using Teuchos::ArrayView;
   using Teuchos::Array;
@@ -127,260 +64,94 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(SGS_MT, MultipleSweeps, Scalar, LO, GO)
   using Teuchos::ParameterList;
   using std::endl;
   typedef Tpetra::CrsMatrix<Scalar, LO, GO, Node> crs_matrix_type;
-  typedef Tpetra::Import<LO, GO, Node> import_type;
   typedef Tpetra::Map<LO, GO, Node> map_type;
   typedef Tpetra::Vector<Scalar, LO, GO, Node> vec_type;
   typedef Tpetra::RowMatrix<Scalar, LO, GO, Node> row_matrix_type;
   typedef Teuchos::ScalarTraits<Scalar> STS;
-  typedef Teuchos::ScalarTraits<typename STS::magnitudeType> STM;
 
   Teuchos::OSTab tab0 (out);
-  out << "Test multiple threaded SGS sweeps with nontrivial Import" << endl;
+  out << "Test multiple threaded SGS sweeps compared w.r.t. Jacobi" << endl;
   Teuchos::OSTab tab1 (out);
 
   RCP<const Comm<int> > comm =
     Tpetra::DefaultPlatform::getDefaultPlatform ().getComm ();
-  const int myRank = comm->getRank ();
-  const int numProcs = comm->getSize ();
 
-  if (numProcs == 1) {
-    out << "The unit test's (MPI) communicator only contains one process."
-        << endl << "This test only makes sense if the communicator contains "
-        << "multiple processes." << endl << "I'll let the test pass trivially."
-        << endl;
-    return;
-  }
+  Tpetra::MatrixMarket::Reader<crs_matrix_type> crs_reader;
+  std::string file_name = "sherman1.mtx";
 
-  const GST gblNumRows = static_cast<GST> (numProcs * 2);
-  const GO indexBase = 0;
-  RCP<const map_type> rowMap (new map_type (gblNumRows, indexBase, comm));
+  RCP<const crs_matrix_type> A = crs_reader.readSparseFile(file_name, comm);
+
+  RCP<const map_type> rowMap = A->getRowMap();
   RCP<const map_type> domainMap = rowMap;
   RCP<const map_type> rangeMap = rowMap;
-  RCP<crs_matrix_type> A =
-    rcp (new crs_matrix_type (rowMap, 6, Tpetra::StaticProfile));
-
-  {
-    const size_t lclNumRows = rowMap->getNodeNumElements ();
-    const Scalar ONE = STS::one ();
-    const Scalar TWO = ONE + ONE;
-    const Scalar FOUR = TWO + TWO;
-    const Scalar EIGHT = FOUR + FOUR;
-    const Scalar TWELVE = EIGHT + FOUR;
-
-    if (myRank == 1) {
-      std::cout << ONE << " " << TWO << " " <<  FOUR << " " << EIGHT << " " << TWELVE << std::endl;
-
-    }
-    Array<Scalar> vals (6);
-    Array<GO> gblColInds (6);
-
-    for (LO lclRow = 0; lclRow < static_cast<LO> (lclNumRows); ++lclRow) {
-      const GO gblRow = rowMap->getGlobalElement (lclRow);
-      const Scalar diagVal = TWELVE;
-      size_t numEnt = 6;
-      if (gblRow == rowMap->getMinAllGlobalIndex ()) {
-        numEnt = 4;
-        vals[0] = diagVal;
-        vals[1] = ONE / TWO;
-        vals[2] = -ONE;
-        vals[3] = ONE;
-        gblColInds[0] = rowMap->getMinAllGlobalIndex ();
-        gblColInds[1] = gblColInds[0] + 1;
-        gblColInds[2] = gblColInds[0] + 2;
-        gblColInds[3] = gblColInds[0] + 3;
-
-        //std::cout << "-row: " << lclRow << " vals:" << vals[0] << " " << vals[1] << " " << vals[2] << " " << vals[3] << std::endl;
-      }
-      else if (gblRow == rowMap->getMinAllGlobalIndex () + 1) {
-        numEnt = 4;
-        vals[0] = -ONE / TWO;
-        vals[1] = diagVal / TWO;
-        vals[2] = ONE;
-        vals[3] = -ONE;
-        gblColInds[0] = rowMap->getMinAllGlobalIndex ();
-        gblColInds[1] = gblColInds[0] + 1;
-        gblColInds[2] = gblColInds[0] + 2;
-        gblColInds[3] = gblColInds[0] + 3;
-
-        //std::cout << "--row: " << lclRow << " vals:" << vals[0] << " " << vals[1] << " " << vals[2] << " " << vals[3] << std::endl;
-      }
-      else if (gblRow == rowMap->getMaxAllGlobalIndex () - 1) {
-        numEnt = 4;
-        vals[0] = -ONE;
-        vals[1] = ONE;
-        vals[2] = diagVal;
-        vals[3] = ONE / TWO;
-        gblColInds[0] = rowMap->getMaxAllGlobalIndex () - 3;
-        gblColInds[1] = rowMap->getMaxAllGlobalIndex () - 2;
-        gblColInds[2] = rowMap->getMaxAllGlobalIndex () - 1;
-        gblColInds[3] = rowMap->getMaxAllGlobalIndex ();
 
 
-        //std::cout << "---row: " << lclRow << " gsblRow:" << gblRow << " vals:" << vals[0] << " " << vals[1] << " " << vals[2] << " " << vals[3] << std::endl;
-      }
-      else if (gblRow == rowMap->getMaxAllGlobalIndex ()) {
-        numEnt = 4;
-        vals[0] = ONE;
-        vals[1] = -ONE;
-        vals[2] = -ONE / TWO;
-        vals[3] = diagVal / TWO;
-        gblColInds[0] = rowMap->getMaxAllGlobalIndex () - 3;
-        gblColInds[1] = rowMap->getMaxAllGlobalIndex () - 2;
-        gblColInds[2] = rowMap->getMaxAllGlobalIndex () - 1;
-        gblColInds[3] = rowMap->getMaxAllGlobalIndex ();
-        //std::cout << "----row: " << lclRow << " vals:" << vals[0] << " " << vals[1] << " " << vals[2] << " " << vals[3] << std::endl;
-      }
-      else if (gblRow % 2 == static_cast<GO> (0)) {
-        numEnt = 6;
-        vals[0] = -ONE;
-        vals[1] = ONE;
-        vals[2] = diagVal;
-        vals[3] = ONE / TWO;
-        vals[4] = -ONE;
-        vals[5] = ONE;
-        gblColInds[0] = gblRow - 2;
-        gblColInds[1] = gblRow - 1;
-        gblColInds[2] = gblRow;
-        gblColInds[3] = gblRow + 1;
-        gblColInds[4] = gblRow + 2;
-        gblColInds[5] = gblRow + 3;
-        //std::cout << "-----row: " << lclRow << " vals:" << vals[0] << " " << vals[1] << " " << vals[2] << " " << vals[3] << " " << vals[4] << " " << vals[5] << std::endl;
-      }
-      else { // gblRow % 2 != 0
-        numEnt = 6;
-        vals[0] = ONE;
-        vals[1] = -ONE;
-        vals[2] = -ONE / TWO;
-        vals[3] = diagVal;
-        vals[4] = ONE;
-        vals[5] = -ONE;
-        gblColInds[0] = gblRow - 3;
-        gblColInds[1] = gblRow - 2;
-        gblColInds[2] = gblRow - 1;
-        gblColInds[3] = gblRow;
-        gblColInds[4] = gblRow + 1;
-        gblColInds[5] = gblRow + 2;
+  vec_type X_wanted (domainMap);
+  vec_type Y_result (rangeMap);
 
-        //std::cout << "------row: " << lclRow << " vals:" << vals[0] << " " << vals[1] << " " << vals[2] << " " << vals[3] << " " << vals[4] << " " << vals[5] << std::endl;
-      }
+  X_wanted.randomize ();
+  A->apply(X_wanted, Y_result);
 
-      ArrayView<Scalar> valsView = vals (0, numEnt);
-      ArrayView<GO> gblColIndsView = gblColInds (0, numEnt);
-      A->insertGlobalValues (gblRow, gblColIndsView, valsView);
-    }
-  }
-  A->fillComplete (domainMap, rangeMap);
-
-  RCP<const map_type> gatherRowMap;
-  {
-    const size_t lclNumRows = (myRank == 0) ?
-      static_cast<size_t> (gblNumRows) :
-      static_cast<size_t> (0);
-    gatherRowMap = rcp (new map_type (gblNumRows, lclNumRows, indexBase, comm));
-  }
-
-  RCP<const map_type> gatherDomainMap = gatherRowMap;
-  RCP<const map_type> gatherRangeMap = gatherRowMap;
-
-  RCP<crs_matrix_type> A_gather = rcp (new crs_matrix_type (gatherRowMap, 6));
-  import_type import (rowMap, gatherRowMap);
-  A_gather->doImport (*A, import, Tpetra::INSERT);
-  A_gather->fillComplete (gatherDomainMap, gatherRangeMap);
-
-  vec_type X (domainMap);
-  vec_type Y (rangeMap);
-  vec_type X_gather (gatherDomainMap);
-  vec_type Y_gather (gatherRangeMap);
-  vec_type Y_diff (gatherRangeMap);
 
   // Test Symmetric Gauss-Seidel (SGS) with three sweeps.
   // Start by letting SGS set the starting solution to zero.
-  ParameterList params;
+  ParameterList params_mt_sgs;
 
-  params.set ("relaxation: type", "MT Symmetric Gauss-Seidel");
-  params.set ("relaxation: sweeps", 3);
-  params.set ("relaxation: zero starting solution", true);
+  params_mt_sgs.set ("relaxation: type", "MT Gauss-Seidel");
+  params_mt_sgs.set ("relaxation: sweeps", 10);
 
-  Ifpack2::Relaxation<row_matrix_type> prec (A);
-  prec.setParameters (params);
-  TEST_NOTHROW( prec.initialize () );
-  TEST_NOTHROW( prec.compute () );
+  params_mt_sgs.set ("relaxation: zero starting solution", true);
+  Ifpack2::Relaxation<row_matrix_type> prec_mt_sgs (A);
 
-  Ifpack2::Relaxation<row_matrix_type> gatherPrec (A_gather);
-  gatherPrec.setParameters (params);
-  TEST_NOTHROW( gatherPrec.initialize () );
-  TEST_NOTHROW( gatherPrec.compute () );
+  prec_mt_sgs.setParameters (params_mt_sgs);
+  TEST_NOTHROW( prec_mt_sgs.initialize () );
+  TEST_NOTHROW( prec_mt_sgs.compute () );
 
-  X.randomize ();
-  X_gather.doImport (X, import, Tpetra::REPLACE);
-  Y.randomize ();
-  X_gather.doImport (X, import, Tpetra::REPLACE);
 
-  prec.apply (X, Y);
+  vec_type X_seek (domainMap);
+  X_seek.putScalar (STS::zero ());
+  vec_type initial_X_diff(X_seek, Teuchos::Copy);
+  initial_X_diff.update (1, X_wanted, -1);
+  double initial_normInf = initial_X_diff.normInf ();
 
-  gatherPrec.apply (X_gather, Y_gather);
-  Y_diff.doImport (Y, import, Tpetra::REPLACE);
-  Y_diff.update (STS::one (), Y_gather, -STS::one ());
 
-  typename STS::magnitudeType normInf = Y_diff.normInf ();
-  TEST_EQUALITY(normInf, STM::zero ());
+  prec_mt_sgs.apply (Y_result, X_seek);
 
-  out << "Repeat test without setting starting solution to zero" << endl;
+  vec_type X_diff_mtgs(X_seek, Teuchos::Copy);
+  X_diff_mtgs.update (1, X_wanted, -1);
+  double normInf_mt_gs = X_diff_mtgs.normInf ();
 
-  params.set ("relaxation: zero starting solution", false);
+  ParameterList params_jacobi;
+  params_jacobi.set ("relaxation: type", "Jacobi");
+  params_jacobi.set ("relaxation: sweeps", 10);
+  params_jacobi.set ("relaxation: zero starting solution", true);
+  Ifpack2::Relaxation<row_matrix_type> prec_jacobi(A);
 
-  prec.setParameters (params);
-  TEST_NOTHROW( prec.initialize () );
-  TEST_NOTHROW( prec.compute () );
+  prec_jacobi.setParameters (params_jacobi);
+  TEST_NOTHROW( prec_jacobi.initialize () );
+  TEST_NOTHROW( prec_jacobi.compute () );
 
-  gatherPrec.setParameters (params);
-  TEST_NOTHROW( gatherPrec.initialize () );
-  TEST_NOTHROW( gatherPrec.compute () );
+  X_seek.putScalar (STS::zero ());
+  prec_jacobi.apply (Y_result, X_seek);
 
-  X.randomize ();
-  X_gather.doImport (X, import, Tpetra::REPLACE);
-  Y.randomize ();
-  X_gather.doImport (X, import, Tpetra::REPLACE);
+  vec_type X_diff_jacobi(X_seek, Teuchos::Copy);
+  X_diff_jacobi.update (1, X_wanted, -1);
+  double normInf_jacobi = X_diff_jacobi.normInf ();
 
-  prec.apply (X, Y);
-  gatherPrec.apply (X_gather, Y_gather);
+  int sgs_improve = initial_normInf > normInf_mt_gs;
+  int sgs_better_than_jacobi = normInf_mt_gs < normInf_jacobi;
 
-  Y_diff.doImport (Y, import, Tpetra::REPLACE);
-  Y_diff.update (STS::one (), Y_gather, -STS::one ());
-  normInf = Y_diff.normInf ();
-  TEST_EQUALITY( normInf, STM::zero () );
+  out << "Initial Norm:" << initial_normInf
+      << " MT GS Norm:" << normInf_mt_gs
+      << " Jacobi Norm:" << normInf_jacobi << std::endl;
+
+  TEST_EQUALITY( sgs_improve, 1 );
+  TEST_EQUALITY( sgs_better_than_jacobi, 1 );
 }
 
 
-// Macro used inside the unit test below.  It tests for global error,
-// and if so, prints each process' error message and quits the test
-// early.
-//
-// 'out' only prints on Process 0.  It's really not OK for other
-// processes to print to stdout, but it usually works and we need to
-// do it for debugging.
-#define IFPACK2RELAXATION_REPORT_GLOBAL_ERR( WHAT_STRING ) do { \
-  reduceAll<int, int> (*comm, REDUCE_MIN, lclSuccess, outArg (gblSuccess)); \
-  TEST_EQUALITY_CONST( gblSuccess, 1 ); \
-  if (gblSuccess != 1) { \
-    out << WHAT_STRING << " FAILED on one or more processes!" << endl; \
-    for (int p = 0; p < numProcs; ++p) { \
-      if (myRank == p && lclSuccess != 1) { \
-        std::cout << errStrm.str () << std::flush; \
-      } \
-      comm->barrier (); \
-      comm->barrier (); \
-      comm->barrier (); \
-    } \
-    std::cerr << "TEST FAILED; RETURNING EARLY" << endl; \
-    return; \
-  } \
-} while (false)
-
 #define UNIT_TEST_GROUP_SC_LO_GO( Scalar, LO, GO ) \
-    TEUCHOS_UNIT_TEST_TEMPLATE_3_INSTANT( SGS_MT, OneSweep, Scalar, LO, GO ) \
-    TEUCHOS_UNIT_TEST_TEMPLATE_3_INSTANT( SGS_MT, MultipleSweeps, Scalar, LO, GO )
-
+    TEUCHOS_UNIT_TEST_TEMPLATE_3_INSTANT( SGS_MT, JacobiComparison, Scalar, LO, GO )
 #include "Ifpack2_ETIHelperMacros.h"
 
 IFPACK2_ETI_MANGLING_TYPEDEFS()
