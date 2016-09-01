86e86e6250c85eeb1b3777cc58afecf366448c0c
Roger Pawlowski <rppawlo@sandia.gov>
2016-09-01 14:34:32 -0400

    Tempus: change includes for consistency

--- packages/tempus/src/Tempus_ResidualModelEvaluator.cpp
+++ packages/tempus/src/Tempus_ResidualModelEvaluator.cpp
@@ -1,7 +1,7 @@
 #include "Tempus_ExplicitTemplateInstantiation.hpp"
 
 #ifdef HAVE_TEMPUS_EXPLICIT_INSTANTIATION
-#include "Tempus_ResidualModelEvaluator_decl.hpp"
+#include "Tempus_ResidualModelEvaluator.hpp"
 #include "Tempus_ResidualModelEvaluator_impl.hpp"
 
 namespace Tempus {
