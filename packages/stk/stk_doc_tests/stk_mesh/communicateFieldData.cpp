d1019b66a7fee2d9ec5e36e9f9daf2ffd09dca96
cwilson <cwilson@sandia.gov>
2016-09-06 17:09:15 -0600

    Add doc test for parallel_sum.
    (From STK/SEACAS)

--- packages/stk/stk_doc_tests/stk_mesh/communicateFieldData.cpp
+++ packages/stk/stk_doc_tests/stk_mesh/communicateFieldData.cpp
@@ -53,15 +53,15 @@
 #include "stk_mesh/base/Types.hpp"      // for BucketVector
 #include "stk_topology/topology.hpp"    // for topology, etc
 
-class CommunicateFieldDataHowTo : public stk::unit_test_util::MeshFixture {};
+class ParallelHowTo : public stk::unit_test_util::MeshFixture {};
 
-//BEGIN
-TEST_F(CommunicateFieldDataHowTo, CommunicateMultipleGhostings)
+//BEGINCommuniateFieldData
+TEST_F(ParallelHowTo, communicateFieldDataForSharedAndAura)
 {
-    auto& temperatureField = get_meta().declare_field<stk::mesh::Field<double>>(stk::topology::NODE_RANK, "temperature");
+    auto& field = get_meta().declare_field<stk::mesh::Field<double>>(stk::topology::NODE_RANK, "temperature");
 
-    double initialTemperatureValue = 25.0;
-    stk::mesh::put_field_on_entire_mesh_with_initial_value(temperatureField, &initialTemperatureValue);
+    double initialValue = 25.0;
+    stk::mesh::put_field_on_entire_mesh_with_initial_value(field, &initialValue);
 
     setup_mesh("generated:8x8x8", stk::mesh::BulkData::AUTO_AURA);
 
@@ -70,12 +70,45 @@ TEST_F(CommunicateFieldDataHowTo, CommunicateMultipleGhostings)
 
     for(const stk::mesh::Bucket *bucket : notOwnedBuckets)
         for(stk::mesh::Entity node : *bucket)
-            *stk::mesh::field_data(temperatureField, node) = -1.2345;
+            *stk::mesh::field_data(field, node) = -1.2345;
 
-    stk::mesh::communicate_field_data(get_bulk(), {&temperatureField});
+    stk::mesh::communicate_field_data(get_bulk(), {&field});
 
     for(const stk::mesh::Bucket *bucket : notOwnedBuckets)
         for(stk::mesh::Entity node : *bucket)
-            EXPECT_EQ(initialTemperatureValue, *stk::mesh::field_data(temperatureField, node));
+            EXPECT_EQ(initialValue, *stk::mesh::field_data(field, node));
 }
-//END
+//ENDCommuniateFieldData
+
+//BEGINSum
+void expect_field_has_value(const stk::mesh::BucketVector& buckets,
+                            const stk::mesh::Field<double> &field,
+                            double value)
+{
+    for(const stk::mesh::Bucket *bucket : buckets)
+        for(stk::mesh::Entity node : *bucket)
+            EXPECT_EQ(value, *stk::mesh::field_data(field, node));
+}
+
+TEST_F(ParallelHowTo, computeParallelSum)
+{
+    auto& field = get_meta().declare_field<stk::mesh::Field<double>>(stk::topology::NODE_RANK, "temperature");
+
+    double initialValue = 25.0;
+    stk::mesh::put_field_on_entire_mesh_with_initial_value(field, &initialValue);
+
+    setup_mesh("generated:8x8x8", stk::mesh::BulkData::AUTO_AURA);
+
+    const stk::mesh::BucketVector& shared = get_bulk().get_buckets(stk::topology::NODE_RANK,
+                                                                   get_meta().globally_shared_part());
+    const stk::mesh::BucketVector& notShared = get_bulk().get_buckets(stk::topology::NODE_RANK,
+                                                                      !get_meta().globally_shared_part());
+    expect_field_has_value(shared, field, initialValue);
+    expect_field_has_value(notShared, field, initialValue);
+
+    stk::mesh::parallel_sum(get_bulk(), {&field});
+
+    expect_field_has_value(shared, field, 2*initialValue);
+    expect_field_has_value(notShared, field, initialValue);
+}
+//ENDSum
