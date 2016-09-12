2dca970f03f321a140c812c6cfaa0cdd87dd2d0c
Nick Roehrig <nsroehr@sandia.gov>
2016-09-12 11:13:52 -0600

    refactored for stk manual inclusion
    (From STK/SEACAS)

--- packages/stk/stk_doc_tests/stk_mesh/howToGetFields.cpp
+++ packages/stk/stk_doc_tests/stk_mesh/howToGetFields.cpp
@@ -42,47 +42,26 @@ namespace stk { namespace mesh { class Part; } }
 
 namespace {
 
+namespace SpatialDimension { const unsigned three = 3; }
 //BEGIN
 TEST(stkMeshHowTo, getFields)
 {
-    const unsigned spatialDimension = 3;
-    stk::mesh::MetaData metaData(spatialDimension, stk::mesh::entity_rank_names());
+    stk::mesh::MetaData metaData(SpatialDimension::three);
 
     typedef stk::mesh::Field<double> ScalarField;
     typedef stk::mesh::Field<double, stk::mesh::Cartesian3d> VectorField;
-    const stk::mesh::EntityRank node_rank = stk::topology::NODE_RANK;
-    const stk::mesh::EntityRank elem_rank = stk::topology::ELEM_RANK;
-
-    struct MyFields {
-        ScalarField * pressureField;
-        VectorField * displacementsField;
-    };
-    MyFields myFields;
 
     const std::string pressureFieldName = "pressure";
     const std::string displacementsFieldName = "displacements";
-    myFields.pressureField = &metaData.declare_field<ScalarField>(elem_rank, pressureFieldName);
-    myFields.displacementsField = &metaData.declare_field<VectorField>(node_rank, displacementsFieldName);
+    ScalarField *pressureField = &metaData.declare_field<ScalarField>(stk::topology::ELEM_RANK, pressureFieldName);
+    VectorField *displacementsField = &metaData.declare_field<VectorField>(stk::topology::NODE_RANK, displacementsFieldName);
     metaData.commit();
 
-    // get_field requires a string-compare search through all fields of the given EntityRank
+    EXPECT_EQ(pressureField, metaData.get_field<ScalarField>(stk::topology::ELEM_RANK, pressureFieldName));
+    EXPECT_EQ(pressureField, metaData.get_field(stk::topology::ELEM_RANK, pressureFieldName));
 
-    ScalarField* pressureField = metaData.get_field<ScalarField>(elem_rank, pressureFieldName);
-    ASSERT_TRUE( pressureField != NULL );
-    EXPECT_EQ( myFields.pressureField->mesh_meta_data_ordinal(), pressureField->mesh_meta_data_ordinal() );
-
-    stk::mesh::FieldBase* pressureFieldBase = metaData.get_field(elem_rank, pressureFieldName);
-    ASSERT_TRUE( pressureFieldBase != NULL );
-    EXPECT_EQ( myFields.pressureField->mesh_meta_data_ordinal(), pressureFieldBase->mesh_meta_data_ordinal() );
-
-    VectorField* displacementsField = metaData.get_field<VectorField>(node_rank, displacementsFieldName);
-    ASSERT_TRUE( displacementsField != NULL );
-    EXPECT_EQ( myFields.displacementsField->mesh_meta_data_ordinal(), displacementsField->mesh_meta_data_ordinal() );
-
-    stk::mesh::FieldBase* displacementsFieldBase = metaData.get_field(node_rank, displacementsFieldName);
-    ASSERT_TRUE( displacementsFieldBase != NULL );
-    EXPECT_EQ( myFields.displacementsField->mesh_meta_data_ordinal(), displacementsFieldBase->mesh_meta_data_ordinal() );
-
+    EXPECT_EQ(displacementsField, metaData.get_field<VectorField>(stk::topology::NODE_RANK, displacementsFieldName));
+    EXPECT_EQ(displacementsField, metaData.get_field(stk::topology::NODE_RANK, displacementsFieldName));
 }
 //END
 
