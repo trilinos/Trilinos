31637d35547f7096c1e1ecf60fb22854ff1f1cae
Nick Roehrig <nsroehr@sandia.gov>
2016-09-16 11:58:00 -0600

    performance test wrappers
    (From STK/SEACAS)

--- packages/stk/stk_learning/Kokkos/NgpFieldPerformanceTest.C
+++ packages/stk/stk_learning/Kokkos/NgpFieldPerformanceTest.C
@@ -3,6 +3,7 @@
 #include <stk_unit_test_utils/getOption.h>
 #include <stk_unit_test_utils/MeshFixture.hpp>
 #include <stk_unit_test_utils/GetMeshSpec.hpp>
+#include <stk_unit_test_utils/PerformanceTester.hpp>
 #include <stk_mesh/base/MetaData.hpp>
 #include <stk_mesh/base/BulkData.hpp>
 #include <stk_mesh/base/Bucket.hpp>
@@ -12,47 +13,52 @@
 #include <stk_mesh/base/GetEntities.hpp>
 #include <stk_util/stk_config.h>
 
-namespace
+namespace ngp_field_perf_test
 {
 
-void calculate_centroid(const ngp::Mesh &ngpMesh, const ngp::Field<double> &ngpCoords, const stk::mesh::Selector &sel, ngp::Field<double> &ngpCentroid)
+template <typename CoordFieldType>
+void calculate_centroid(const ngp::Mesh &ngpMesh, const CoordFieldType &ngpCoords, const stk::mesh::Selector &sel, ngp::Field<double> &ngpCentroid)
 {
     ngp::for_each_entity_run(ngpMesh, stk::topology::ELEM_RANK, sel, KOKKOS_LAMBDA(ngp::Mesh::MeshIndex elem)
     {
-       ngp::Mesh::ConnectedNodes nodes = ngpMesh.get_nodes(elem);
+        for(size_t count = 0; count < 1000; count++)
+        {
+            ngp::Mesh::ConnectedNodes nodes = ngpMesh.get_nodes(elem);
 
-       for(unsigned dim = 0; dim < 3; dim++)
-       {
-           for(size_t i = 0; i < nodes.size(); i++)
-           {
-               ngpCentroid.get(elem, dim) += ngpCoords.get(ngpMesh, nodes[i], dim);
-           }
-           ngpCentroid.get(elem, dim) /= nodes.size();
-       }
+            for(unsigned dim = 0; dim < 3; dim++)
+                ngpCentroid.get(elem, dim) = 0;
+
+            for(size_t i = 0; i < nodes.size(); i++)
+            {
+                stk::mesh::FastMeshIndex nodeIndex = ngpMesh.fast_mesh_index(nodes[i]);
+                for(unsigned dim = 0; dim < 3; dim++)
+                    ngpCentroid.get(elem, dim) += ngpCoords.get(nodeIndex, dim);
+            }
+
+            for(unsigned dim = 0; dim < 3; dim++)
+                ngpCentroid.get(elem, dim) /= nodes.size();
+        }
     });
 }
 
-class NgpFieldPerf : public stk::unit_test_util::MeshFixture {};
-
-TEST_F(NgpFieldPerf, fieldDataAccess)
+template <typename CoordFieldType>
+void calculate_centroid_using_coord_field(const stk::mesh::BulkData &bulk, stk::mesh::FieldBase &centroid)
 {
-    stk::mesh::Field<double, stk::mesh::Cartesian3d> &centroid = get_meta().declare_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >(stk::topology::ELEM_RANK, "centroid");
-    stk::mesh::put_field(centroid, get_meta().universal_part(), 3);
+    const stk::mesh::FieldBase& coords = *bulk.mesh_meta_data().coordinate_field();
+    CoordFieldType ngpCoords(bulk, coords);
+    ngp::Field<double> ngpCentroid(bulk, centroid);
+    ngp::Mesh ngpMesh(bulk);
 
-    std::string meshSpec = stk::unit_test_util::get_mesh_spec("-dim");
-    setup_mesh(meshSpec, stk::mesh::BulkData::NO_AUTO_AURA);
+    calculate_centroid(ngpMesh, ngpCoords, bulk.mesh_meta_data().locally_owned_part(), ngpCentroid);
 
-    const stk::mesh::FieldBase& coords = *get_meta().coordinate_field();
-    ngp::Field<double> ngpCoords(get_bulk(), coords);
-    ngp::Field<double> ngpCentroid(get_bulk(), centroid);
-    ngp::Mesh ngpMesh(get_bulk());
+    ngpCentroid.copy_device_to_host(bulk, centroid);
+}
 
-    calculate_centroid(ngpMesh, ngpCoords, get_meta().locally_owned_part(), ngpCentroid);
-
-    //verify_averaged_centroids_are_center_of_mesh(ngpCentroid)
-    double average[3] = {0, 0, 0};
+std::vector<double> get_centroid_average(stk::mesh::BulkData &bulk, stk::mesh::Field<double, stk::mesh::Cartesian3d> &centroid)
+{
+    std::vector<double> average = {0, 0, 0};
     size_t numElems = 0;
-    for(const stk::mesh::Bucket *bucket : get_bulk().buckets(stk::topology::ELEM_RANK))
+    for(const stk::mesh::Bucket *bucket : bulk.buckets(stk::topology::ELEM_RANK))
     {
         for(stk::mesh::Entity elem : *bucket)
         {
@@ -63,16 +69,61 @@ TEST_F(NgpFieldPerf, fieldDataAccess)
         }
     }
 
-    double meshCenter = unitTestUtils::get_command_line_option<double>("-dim", "20") / 2.0;
     for(size_t dim = 0; dim < 3; dim++)
-    {
         average[dim] /= numElems;
-        EXPECT_EQ(meshCenter, average[dim]);
-    }
+
+    return average;
 }
 
-TEST_F(NgpFieldPerf, constFieldDataAccess)
+class NgpFieldPerf : public stk::unit_test_util::MeshFixture
 {
+protected:
+    void declare_centroid_field()
+    {
+        centroid = &get_meta().declare_field<stk::mesh::Field<double, stk::mesh::Cartesian3d> >(stk::topology::ELEM_RANK, "centroid");
+        stk::mesh::put_field(*centroid, get_meta().universal_part(), 3);
+    }
+    void generate_mesh()
+    {
+        std::string meshSpec = stk::unit_test_util::get_mesh_spec("-dim");
+        setup_mesh(meshSpec, stk::mesh::BulkData::NO_AUTO_AURA);
+    }
+    void verify_averaged_centroids_are_center_of_mesh()
+    {
+        std::vector<double> average = get_centroid_average(get_bulk(), *centroid);
+        double meshCenter = unitTestUtils::get_command_line_option<double>("-dim", "20") / 2.0;
+        for(size_t dim = 0; dim < 3; dim++)
+            EXPECT_EQ(meshCenter, average[dim]);
+    }
+    template <typename CoordFieldType>
+    double time_field_data_access()
+    {
+        double startTime = stk::wall_time();
+        calculate_centroid_using_coord_field<CoordFieldType>(get_bulk(), *centroid);
+        double duration = stk::wall_time() - startTime;
+        verify_averaged_centroids_are_center_of_mesh();
+        return duration;
+    }
+    void warm_up_gpu()
+    {
+        time_field_data_access<ngp::Field<double>>();
+    }
+
+    stk::mesh::Field<double, stk::mesh::Cartesian3d> *centroid;
+};
+
+
+TEST_F(NgpFieldPerf, constFieldDataAccessIsFasterThanFieldDataAccess)
+{
+    declare_centroid_field();
+    generate_mesh();
+    warm_up_gpu();
+    double nonConstTime = time_field_data_access<ngp::Field<double>>();
+    double constTime = time_field_data_access<ngp::ConstField<double>>();
+    std::cerr << "non-const time: " << nonConstTime << ", const time: " << constTime << std::endl;
+#ifdef KOKKOS_HAVE_CUDA
+    EXPECT_LT(constTime, nonConstTime);
+#endif
 }
 
 }
