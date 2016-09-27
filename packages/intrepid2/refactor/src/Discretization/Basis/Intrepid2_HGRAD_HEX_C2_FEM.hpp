fe8dd032bf99b8eae02dae2cdf79536093006ae3
Kyungjoo Kim <kyukim@sandia.gov>
2016-09-27 10:48:23 -0600

    Intrepid2 - not using internal object for hex c2 element

--- packages/intrepid2/refactor/src/Discretization/Basis/Intrepid2_HGRAD_HEX_C2_FEM.hpp
+++ packages/intrepid2/refactor/src/Discretization/Basis/Intrepid2_HGRAD_HEX_C2_FEM.hpp
@@ -135,6 +135,77 @@ namespace Intrepid2 {
 
   */
 
+  namespace Impl {
+
+    class Basis_HGRAD_HEX_C2_FEM {
+    public:
+      template<EOperator opType>
+      struct Serial {
+        template<typename outputViewType,
+                 typename inputViewType>
+        KOKKOS_INLINE_FUNCTION
+        static void
+        getValues( /**/  outputViewType output,
+                   const inputViewType input );
+        
+      };
+      
+      template<typename ExecSpaceType, 
+               typename outputValueValueType, class ...outputValueProperties,
+               typename inputPointValueType,  class ...inputPointProperties>
+      static void
+      getValues( /**/  Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
+                 const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints,
+                 const EOperator operatorType);
+      
+      template<typename outputValueViewType,
+               typename inputPointViewType,
+               EOperator opType>
+      struct Functor {
+        /**/  outputValueViewType _outputValues;
+        const inputPointViewType  _inputPoints;
+        
+        KOKKOS_INLINE_FUNCTION
+        Functor( /**/  outputValueViewType outputValues_,
+                 /**/  inputPointViewType  inputPoints_ )
+          : _outputValues(outputValues_), _inputPoints(inputPoints_) {}
+        
+        KOKKOS_INLINE_FUNCTION
+        void operator()(const ordinal_type pt) const {
+          switch (opType) {
+          case OPERATOR_VALUE : {
+            auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt );
+            const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
+            Serial<opType>::getValues( output, input );
+            break;
+          }
+          case OPERATOR_GRAD :
+          case OPERATOR_D1 :
+          case OPERATOR_D2 :
+          case OPERATOR_D3 :
+          case OPERATOR_D4 :
+          case OPERATOR_MAX : {
+            auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
+            const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
+            Serial<opType>::getValues( output, input );
+            break;
+          }
+          default: {
+            INTREPID2_TEST_FOR_ABORT( opType != OPERATOR_VALUE &&
+                                      opType != OPERATOR_GRAD &&
+                                      opType != OPERATOR_D1 &&
+                                      opType != OPERATOR_D2 &&
+                                      opType != OPERATOR_D3 &&
+                                      opType != OPERATOR_D4 &&
+                                      opType != OPERATOR_MAX,
+                                      ">>> ERROR: (Intrepid2::Basis_HGRAD_HEX_C2_FEM::Serial::getValues) operator is not supported");
+          }
+          }
+        }
+      };
+    };
+  }
+      
   template<typename ExecSpaceType = void,
            typename outputValueType = double,
            typename pointValueType = double>
@@ -144,133 +215,47 @@ namespace Intrepid2 {
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_2d_host ordinal_type_array_2d_host;
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_3d_host ordinal_type_array_3d_host;
 
-    template<EOperator opType>
-    struct Serial {
-      template<typename outputValueValueType, class ...outputValueProperties,
-               typename inputPointValueType,  class ...inputPointProperties>
-      KOKKOS_INLINE_FUNCTION
-      static void
-      getValues( /**/  Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
-                 const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints );
-
-    };
-
-    template<typename outputValueViewType,
-             typename inputPointViewType,
-             EOperator opType>
-    struct Functor {
-      /**/  outputValueViewType _outputValues;
-      const inputPointViewType  _inputPoints;
-
-      KOKKOS_INLINE_FUNCTION
-      Functor( /**/  outputValueViewType outputValues_,
-               /**/  inputPointViewType  inputPoints_ )
-        : _outputValues(outputValues_), _inputPoints(inputPoints_) {}
-
-      KOKKOS_INLINE_FUNCTION
-      void operator()(const ordinal_type pt) const {
-        switch (opType) {
-        case OPERATOR_VALUE : {
-          auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt );
-          const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
-          Serial<opType>::getValues( output, input );
-          break;
-        }
-        case OPERATOR_GRAD :
-        case OPERATOR_D1 :
-        case OPERATOR_D2 :
-        case OPERATOR_D3 :
-        case OPERATOR_D4 :
-        case OPERATOR_MAX : {
-          auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
-          const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
-          Serial<opType>::getValues( output, input );
-          break;
-        }
-        default: {
-          INTREPID2_TEST_FOR_ABORT( opType != OPERATOR_VALUE &&
-                                    opType != OPERATOR_GRAD &&
-                                    opType != OPERATOR_D1 &&
-                                    opType != OPERATOR_D2 &&
-                                    opType != OPERATOR_D3 &&
-                                    opType != OPERATOR_D4 &&
-                                    opType != OPERATOR_MAX,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_HEX_C2_FEM::Serial::getValues) operator is not supported");
-        }
-        }
-      }
-    };
-
-    class Internal {
-    private:
-      Basis_HGRAD_HEX_C2_FEM *obj_;
-
-    public:
-      Internal(Basis_HGRAD_HEX_C2_FEM *obj)
-        : obj_(obj) {}
-
-      /** \brief  Evaluation of a FEM basis on a <strong>reference Hexahedron</strong> cell.
-
-          Returns values of <var>operatorType</var> acting on FEM basis functions for a set of
-          points in the <strong>reference Hexahedron</strong> cell. For rank and dimensions of
-          I/O array arguments see Section \ref basis_md_array_sec.
-
-          \param  outputValues      [out] - rank-2 or 3 array with the computed basis values
-          \param  inputPoints       [in]  - rank-2 array with dimensions (P,D) containing reference points
-          \param  operatorType      [in]  - operator applied to basis functions
-      */
-      template<typename outputValueValueType, class ...outputValueProperties,
-               typename inputPointValueType,  class ...inputPointProperties>
-      void
-      getValues( /**/  Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
-                 const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints,
-                 const EOperator operatorType  = OPERATOR_VALUE ) const;
-
-
-      /** \brief  Returns spatial locations (coordinates) of degrees of freedom on a
-          <strong>reference Quadrilateral</strong>.
-
-          \param  DofCoords      [out] - array with the coordinates of degrees of freedom,
-          dimensioned (F,D)
-      */
-      template<typename dofCoordValueType, class ...dofCoordProperties>
-      void
-      getDofCoords( Kokkos::DynRankView<dofCoordValueType,dofCoordProperties...> dofCoords ) const;
-
-    };
-    Internal impl_;
-
-
     /** \brief  Constructor.
      */
     Basis_HGRAD_HEX_C2_FEM();
-    Basis_HGRAD_HEX_C2_FEM(const Basis_HGRAD_HEX_C2_FEM &b)
-      : Basis<ExecSpaceType,outputValueType,pointValueType>(b),
-        impl_(this) {}
-
-    Basis_HGRAD_HEX_C2_FEM& operator=(const Basis_HGRAD_HEX_C2_FEM &b) {
-      if (this != &b) {
-        Basis<ExecSpaceType,outputValueType,pointValueType>::operator= (b);
-        // do not copy impl
-      }
-      return *this;
-    }
 
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::outputViewType outputViewType;
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::pointViewType  pointViewType;
-
+    
     virtual
     void
     getValues( /**/  outputViewType outputValues,
                const pointViewType  inputPoints,
                const EOperator operatorType = OPERATOR_VALUE ) const {
-      impl_.getValues( outputValues, inputPoints, operatorType );
+#ifdef HAVE_INTREPID2_DEBUG
+      // Verify arguments
+      Intrepid2::getValues_HGRAD_Args(outputValues,
+                                      inputPoints,
+                                      operatorType,
+                                      this->getBaseCellTopology(),
+                                      this->getCardinality() );
+#endif
+      Impl::Basis_HGRAD_HEX_C2_FEM::
+        getValues<ExecSpaceType>( outputValues,
+                                  inputPoints,
+                                  operatorType );
     }
 
     virtual
     void
     getDofCoords( pointViewType dofCoords ) const {
-      impl_.getDofCoords( dofCoords );
+#ifdef HAVE_INTREPID2_DEBUG
+      // Verify rank of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( dofCoords.rank() != 2, std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_HEX_C2_FEM::getDofCoords) rank = 2 required for dofCoords array");
+      // Verify 0th dimension of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( dofCoords.dimension(0) != obj_->basisCardinality_, std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_HEX_C2_FEM::getDofCoords) mismatch in number of dof and 0th dimension of dofCoords array");
+      // Verify 1st dimension of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( dofCoords.dimension(1) != obj_->basisCellTopology_.getDimension(), std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_HEX_C2_FEM::getDofCoords) incorrect reference cell (1st) dimension in dofCoords array");
+#endif
+      Kokkos::deep_copy(dofCoords, this->dofCoords_);
     }
   };
 }// namespace Intrepid2
