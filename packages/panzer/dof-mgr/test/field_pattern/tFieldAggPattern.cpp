dbfd8b24da81a581e55f4930abf1244b492ede42
Kyungjoo Kim <kyukim@sandia.gov>
2016-09-29 10:20:47 -0600

    Panzer - high order assembly passes test field agg

--- packages/panzer/dof-mgr/test/field_pattern/tFieldAggPattern.cpp
+++ packages/panzer/dof-mgr/test/field_pattern/tFieldAggPattern.cpp
@@ -156,10 +156,22 @@ namespace panzer {
   {
     out << note << std::endl;
 
+    const int poly_A = 3, poly_B = 5, poly_C = 1;
+    
     // basis to build patterns from
-    RCP<Intrepid2::Basis<PHX::Device,double,double> > basisA = rcp(new Intrepid2::Basis_HGRAD_QUAD_Cn_FEM<PHX::Device,double,double>(3));
-    RCP<Intrepid2::Basis<PHX::Device,double,double> > basisB = rcp(new Intrepid2::Basis_HGRAD_QUAD_Cn_FEM<PHX::Device,double,double>(5));
-    RCP<Intrepid2::Basis<PHX::Device,double,double> > basisC = rcp(new Intrepid2::Basis_HGRAD_QUAD_Cn_FEM<PHX::Device,double,double>(1));
+    RCP<Intrepid2::Basis<PHX::Device,double,double> > basisA = rcp(new Intrepid2::Basis_HGRAD_QUAD_Cn_FEM<PHX::Device,double,double>(poly_A));
+    RCP<Intrepid2::Basis<PHX::Device,double,double> > basisB = rcp(new Intrepid2::Basis_HGRAD_QUAD_Cn_FEM<PHX::Device,double,double>(poly_B));
+    RCP<Intrepid2::Basis<PHX::Device,double,double> > basisC = rcp(new Intrepid2::Basis_HGRAD_QUAD_Cn_FEM<PHX::Device,double,double>(poly_C));
+
+    const int ndof_A[] = { basisA->getDofTag(basisA->getDofOrdinal(0,0,0))(3),
+                           basisA->getDofTag(basisA->getDofOrdinal(1,0,0))(3),
+                           basisA->getDofTag(basisA->getDofOrdinal(2,0,0))(3) };
+    const int ndof_B[] = { basisB->getDofTag(basisB->getDofOrdinal(0,0,0))(3),
+                           basisB->getDofTag(basisB->getDofOrdinal(1,0,0))(3),
+                           basisB->getDofTag(basisB->getDofOrdinal(2,0,0))(3) };
+    const int ndof_C[] = { basisC->getDofTag(basisC->getDofOrdinal(0,0,0))(3),
+                           basisC->getDofTag(basisC->getDofOrdinal(1,0,0))(3),
+                           basisC->getDofTag(basisC->getDofOrdinal(2,0,0))(3) };
 
     RCP<const FieldPattern> patternA = rcp(new Intrepid2FieldPattern(basisA));
     RCP<const FieldPattern> patternB = rcp(new Intrepid2FieldPattern(basisB));
@@ -212,89 +224,80 @@ namespace panzer {
       for(int i=0;i<agg.getDimension();i++)
         TEST_EQUALITY(agg.getSubcellCount(i),patternB->getSubcellCount(i)); 
       // this cannot be compared anymore
+      // agg create a pattern based on the geometrical order while patternB is arbitrary given from intrepid
       // TEST_ASSERT(agg.equals(*patternB)); 
     }
 
-    // // test that single construction gives the same pattern
-    // {
-    //   std::vector<std::pair<int,RCP<const FieldPattern> > > patternM;
-    //   patternM.push_back(std::make_pair(3,patternB));
-    //   patternM.push_back(std::make_pair(7,patternA));
+    // test that single construction gives the same pattern
+    {
+      std::vector<std::pair<int,RCP<const FieldPattern> > > patternM;
+      patternM.push_back(std::make_pair(3,patternB));
+      patternM.push_back(std::make_pair(7,patternA));
 
-    //   FieldAggPattern agg(patternM); 
-    //   agg.print(out); // for debugging purposes
+      FieldAggPattern agg(patternM); 
+      agg.print(out); // for debugging purposes
 
-    //   TEST_ASSERT(patternB->sameGeometry(agg));
-    //   TEST_EQUALITY(agg.getDimension(),patternB->getDimension()); 
-    //   TEST_EQUALITY((int) agg.numFieldsPerId().size(),agg.getGeometricAggFieldPattern()->numberIds());
+      TEST_ASSERT(patternB->sameGeometry(agg));
+      TEST_EQUALITY(agg.getDimension(),patternB->getDimension()); 
+      TEST_EQUALITY((int) agg.numFieldsPerId().size(),agg.getGeometricAggFieldPattern()->numberIds());
 
-    //   const std::vector<int> & fieldsPerId = agg.numFieldsPerId();
-    //   const std::vector<int> & fieldIds = agg.fieldIds();
+      const std::vector<int> & fieldsPerId = agg.numFieldsPerId();
+      const std::vector<int> & fieldIds = agg.fieldIds();
 
-    //   int numberFields = 0;
-    //   for(std::size_t i=0;i<fieldsPerId.size();i++)
-    //     numberFields += fieldsPerId[i];
+      int numberFields = 0;
+      for(std::size_t i=0;i<fieldsPerId.size();i++)
+        numberFields += fieldsPerId[i];
 
-    //   TEST_EQUALITY((int) fieldIds.size(),numberFields);
-    //   TEST_EQUALITY((int) fieldsPerId.size(),agg.getGeometricAggFieldPattern()->numberIds());
+      TEST_EQUALITY((int) fieldIds.size(),numberFields);
+      TEST_EQUALITY((int) fieldsPerId.size(),agg.getGeometricAggFieldPattern()->numberIds());
 
-    //   int offset, dimension = 0;
-    //   RCP<const FieldPattern> geomPattern = agg.getGeometricAggFieldPattern();
+      RCP<const FieldPattern> geomPattern = agg.getGeometricAggFieldPattern();
 
-    //   // check out fieldsPerId
-    //   offset = 0;
-    //   dimension = 0;
-    //   for(int nodes=0;nodes<geomPattern->getSubcellCount(dimension);nodes++)
-    //     TEST_EQUALITY(fieldsPerId[offset+nodes],2); 
+      // check out fieldsPerId
+      for (int offset=0, dimension=0;dimension<3;++dimension) {
+        const int nsubcell = geomPattern->getSubcellCount(dimension);
+        for(int entities=0;entities<nsubcell;entities++) {
+          TEST_EQUALITY(fieldsPerId[offset],ndof_A[dimension]+ndof_B[dimension]); 
+        }
+        offset += nsubcell;
+      }
 
-    //   offset += geomPattern->getSubcellCount(dimension);
-    //   dimension = 1;
-    //   for(int edges=0;edges<geomPattern->getSubcellCount(dimension);edges++)
-    //     TEST_EQUALITY(fieldsPerId[offset+edges],1); 
+      // this tests length of fieldsPerId is not longer than expected
+      TEST_EQUALITY(geomPattern->getSubcellCount(0)+ 
+                    geomPattern->getSubcellCount(1)+ 
+                    geomPattern->getSubcellCount(2),
+                    (int) fieldsPerId.size());
 
-    //   // this tests length of fieldsPerId is not longer than expected
-    //   TEST_EQUALITY(offset+geomPattern->getSubcellCount(dimension),(int) fieldsPerId.size());
+      // check out fieldIds
+      for (int offset=0, dimension=0;dimension<3;++dimension) {
+        const int nsubcell = geomPattern->getSubcellCount(dimension);
+        for(int entities=0;entities<nsubcell;entities++) {
+          for (int ndof=0;ndof<ndof_B[dimension];++ndof,++offset) {
+            TEST_EQUALITY(fieldIds[offset],3);
+          }
+          for (int ndof=0;ndof<ndof_A[dimension];++ndof,++offset) {
+            TEST_EQUALITY(fieldIds[offset],7); 
+          }
+        }
+      }
 
-    //   // check out fieldIds
-    //   offset = 0;
-    //   dimension = 0;
-    //   for(int nodes=0;nodes<geomPattern->getSubcellCount(dimension);nodes++) {
-    //     TEST_EQUALITY(fieldIds[offset+2*nodes],3);
-    //     TEST_EQUALITY(fieldIds[offset+2*nodes+1],7);
-    //   }
- 
-    //   offset += 2*geomPattern->getSubcellCount(dimension);
-    //   dimension = 1;
-    //   for(int edges=0;edges<geomPattern->getSubcellCount(dimension);edges++)
-    //     TEST_EQUALITY(fieldIds[offset+edges],3);
+      // this tests length of fieldIds is not longer than expected
+      TEST_EQUALITY((ndof_A[0]+ndof_B[0])*geomPattern->getSubcellCount(0)+ 
+                    (ndof_A[1]+ndof_B[1])*geomPattern->getSubcellCount(1)+ 
+                    (ndof_A[2]+ndof_B[2])*geomPattern->getSubcellCount(2),
+                    (int) fieldIds.size());
 
-    //   // this tests length of fieldIds is not longer than expected
-    //   TEST_EQUALITY(offset+geomPattern->getSubcellCount(dimension),(int) fieldIds.size());
+      // check out subcell information
+      for (int index=0, dimension=0;dimension<3;++dimension) {
+        for(int nodes=0;nodes<geomPattern->getSubcellCount(dimension);++nodes) {
+          const std::vector<int> & indices = agg.getSubcellIndices(dimension,nodes);
+          TEST_EQUALITY((int) indices.size(),ndof_A[dimension]+ndof_B[dimension]);
+          for(std::size_t i=0;i<indices.size();i++,index++) 
+            TEST_EQUALITY(indices[i],index);
+        }
+      }
 
-    //   // check out subcell information
-    //   int index=0;
-    //   dimension = 0;
-    //   for(int nodes=0;nodes<geomPattern->getSubcellCount(dimension);++nodes) {
-    //     const std::vector<int> & indices = agg.getSubcellIndices(dimension,nodes);
-    //     TEST_EQUALITY((int) indices.size(),2);
-    //     for(std::size_t i=0;i<indices.size();i++,index++) 
-    //       TEST_EQUALITY(indices[i],index);
-    //   }
-
-    //   dimension = 1;
-    //   for(int edges=0;edges<geomPattern->getSubcellCount(dimension);++edges) {
-    //     const std::vector<int> & indices = agg.getSubcellIndices(dimension,edges);
-    //     TEST_EQUALITY((int) indices.size(),1);
-    //     for(std::size_t i=0;i<indices.size();i++,index++) 
-    //       TEST_EQUALITY(indices[i],index);
-    //   }
-
-    //   dimension = 2;
-    //   for(int cells=0;cells<geomPattern->getSubcellCount(dimension);++cells) {
-    //     const std::vector<int> & indices = agg.getSubcellIndices(dimension,cells);
-    //     TEST_EQUALITY((int) indices.size(),0);
-    //   }
-    // }
+    }
   }
 
   // // tests:
