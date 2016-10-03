b9921427dfc8ff56e2ef681cc6b1de1004bf1cbd
Greg Sjaardema <gdsjaar@sandia.gov>
2016-10-03 14:36:29 -0600

    IOSS: Add pyramid 18 and 19 element types
    (From STK/SEACAS)

--- packages/seacas/libraries/ioss/src/Ioss_Pyramid18.h
+++ packages/seacas/libraries/ioss/src/Ioss_Pyramid18.h
@@ -30,68 +30,55 @@
 // (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#include <Ioss_StandardElementTypes.h>
+#ifndef IOSS_Ioss_Pyramid18_h
+#define IOSS_Ioss_Pyramid18_h
 
-Ioss::Initializer::Initializer()
-{
-  // List all storage types here with a call to their factory method.
-  // This is Used to get the linker to pull in all needed libraries.
-  Ioss::Sphere::factory();
+#include <Ioss_CodeTypes.h>       // for IntVector
+#include <Ioss_ElementTopology.h> // for ElementTopology
 
-  Ioss::Edge2::factory();
-  Ioss::Edge3::factory();
+// STL Includes
 
-  Ioss::Bar2::factory();
-  Ioss::Bar3::factory();
-  Ioss::ShellLine2D2::factory();
-  Ioss::ShellLine2D3::factory();
+namespace Ioss {
+  class Pyramid18 : public Ioss::ElementTopology
+  {
 
-  Ioss::Hex8::factory();
-  Ioss::Hex20::factory();
-  Ioss::Hex27::factory();
+  public:
+    static constexpr auto name = "pyramid18";
 
-  Ioss::Node::factory();
+    static void factory();
+    ~Pyramid18() override;
 
-  Ioss::Pyramid5::factory();
-  Ioss::Pyramid13::factory();
-  Ioss::Pyramid14::factory();
-  Ioss::Pyramid18::factory();
-  Ioss::Pyramid19::factory();
+    ElementShape shape() const override { return ElementShape::PYRAMID; }
+    int          spatial_dimension() const override;
+    int          parametric_dimension() const override;
+    bool         is_element() const override { return true; }
+    int          order() const override;
 
-  Ioss::Quad4::factory();
-  Ioss::Quad8::factory();
-  Ioss::Quad9::factory();
+    int number_corner_nodes() const override;
+    int number_nodes() const override;
+    int number_edges() const override;
+    int number_faces() const override;
 
-  Ioss::Shell4::factory();
-  Ioss::Shell8::factory();
-  Ioss::Shell9::factory();
+    int number_nodes_edge(int edge = 0) const override;
+    int number_nodes_face(int face = 0) const override;
+    int number_edges_face(int face = 0) const override;
 
-  Ioss::Tet4::factory();
+    bool faces_similar() const override;
 
-  Ioss::Tet8::factory();
-  Ioss::Tet10::factory();
-  Ioss::Tet11::factory();
-  Ioss::Tet14::factory();
-  Ioss::Tet15::factory();
+    Ioss::IntVector edge_connectivity(int edge_number) const override;
+    Ioss::IntVector face_connectivity(int face_number) const override;
+    Ioss::IntVector element_connectivity() const override;
 
-  Ioss::Tri3::factory();
-  Ioss::Tri4::factory();
-  Ioss::Tri6::factory();
-  Ioss::Tri7::factory();
+    Ioss::IntVector face_edge_connectivity(int face_number) const override;
 
-  Ioss::TriShell3::factory();
-  Ioss::TriShell4::factory();
-  Ioss::TriShell6::factory();
-  Ioss::TriShell7::factory();
+    Ioss::ElementTopology *face_type(int face_number = 0) const override;
+    Ioss::ElementTopology *edge_type(int edge_number = 0) const override;
 
-  Ioss::Unknown::factory();
+  protected:
+    Pyramid18();
 
-  Ioss::Wedge6::factory();
-  Ioss::Wedge15::factory();
-  Ioss::Wedge16::factory();
-  Ioss::Wedge18::factory();
-  Ioss::Wedge20::factory();
-  Ioss::Wedge21::factory();
-
-  Ioss::Super::factory();
+  private:
+    Pyramid18(const Pyramid18 &); // Do not implement
+  };
 }
+#endif // IOSS_Ioss_Pyramid18_h
