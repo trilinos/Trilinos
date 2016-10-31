c9475c886ac2e74f1eedfc5aa64ccf2084ca82eb
Jason M. Gates <jmgate@sandia.gov>
2016-10-31 14:13:17 -0600

    Panzer:  Split up Owned and Ghosted Vectors (#615)
    The ReadOnlyVector_ and WriteVector_GlobalEvaluationData classes now
    have Kokkos::Views of the owned and ghosted vectors.  Previously ghosted
    vectors contained duplicates of all the information in the owned
    vectors; now these two vectors are non-overlapping so we avoid data
    duplication.  operator[]() routines were added to the
    GlobalEvaluationData classes to hide the logic of element access from
    users.  Previously one would call getGhostedVector() on the
    GlobalEvaluationData and then index into that; now simply index into the
    GlobalEvaluationData itself.
    
    The LinearObjFactory classes were updated with "version 2" routines to
    build these GlobalEvaluationDatas with non-overlapping owned and ghosted
    vectors.  Eventually the original routines will be replaced by these new
    ones, but not yet.
    
    The Gather*Epetra* classes have been updated to use this new syntax when
    working with GlobalEvaluationData containers.  When dealing with the
    older EpetraLinearObjContainers, they still use the old way of doing
    things.

--- packages/panzer/disc-fe/src/Panzer_EpetraVector_Write_GlobalEvaluationData.cpp
+++ packages/panzer/disc-fe/src/Panzer_EpetraVector_Write_GlobalEvaluationData.cpp
@@ -1,162 +1,279 @@
-#include "Panzer_EpetraVector_Write_GlobalEvaluationData.hpp"
+// @HEADER
+// ***********************************************************************
+//
+//           Panzer: A partial differential equation assembly
+//       engine for strongly coupled complex multiphysics systems
+//                 Copyright (2011) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Roger P. Pawlowski (rppawlo@sandia.gov) and
+// Eric C. Cyr (eccyr@sandia.gov)
+// ***********************************************************************
+// @HEADER
 
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Include Files
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// Epetra
 #include "Epetra_Export.h"
 
+// Panzer
+#include "Panzer_EpetraVector_Write_GlobalEvaluationData.hpp"
+
+// Thyra
+#include "Thyra_EpetraThyraWrappers.hpp"
+#include "Thyra_LinearOpBase.hpp"
+#include "Thyra_SpmdVectorBase.hpp"
+#include "Thyra_SpmdVectorSpaceBase.hpp"
 #include "Thyra_VectorBase.hpp"
 #include "Thyra_VectorStdOps.hpp"
-#include "Thyra_LinearOpBase.hpp"
-#include "Thyra_EpetraThyraWrappers.hpp"
 
-namespace panzer {
-
-using Teuchos::RCP;
-
-void
-EpetraVector_Write_GlobalEvaluationData::
-initialize(const RCP<const Epetra_Export>& exporter,
-           const RCP<const Epetra_Map>&    ghostedMap,
-           const RCP<const Epetra_Map>&    ownedMap)
+namespace panzer
 {
-  exporter_   = exporter;
-  ghostedMap_ = ghostedMap;
-  ownedMap_   = ownedMap;
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  initialize()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  void
+  EpetraVector_Write_GlobalEvaluationData::
+  initialize(
+    const Teuchos::RCP<const Epetra_Export>& exporter,
+    const Teuchos::RCP<const Epetra_Map>&    ghostedMap,
+    const Teuchos::RCP<const Epetra_Map>&    ownedMap)
+  {
+    using Teuchos::rcp;
+    using Thyra::create_VectorSpace;
 
-  // allocate the ghosted vector
-  ghostedVector_ = Teuchos::rcp(new Epetra_Vector(*ghostedMap_));
-
-  // build up the thyra conversion data structures
-  ghostedSpace_ = Thyra::create_VectorSpace(ghostedMap_);
-  ownedSpace_   = Thyra::create_VectorSpace(ownedMap_);
-
-  isInitialized_ = true;
-}
-
-void 
-EpetraVector_Write_GlobalEvaluationData::
-ghostToGlobal(int mem)
-{
-  TEUCHOS_TEST_FOR_EXCEPTION(ownedVector_ == Teuchos::null,std::logic_error,
-    "Owned vector has not been set, can't perform the halo exchange!");
-
-  // Initialize the ghosted data, zeroing out things, and filling in specified
-  // constants.
-  initializeData();
-  Teuchos::RCP<Epetra_Vector> ownedVector_ep = Thyra::get_Epetra_Vector(
-    *ownedMap_, ownedVector_);
-
-  // set different combine modes
-  Epetra_CombineMode cm = Add;
-  switch(getCombineMode()) {
-    case CM_Sum:
-      cm = Add;
-      break;
-    case CM_Min:
-      cm = Epetra_Min;
-      break;
-    case CM_Max:
-      cm = Epetra_Max;
-      break;
-    case CM_Insert:
-      cm = Insert;
-      break;
-  };
+    // Save the input.
+    exporter_   = exporter;
+    ghostedMap_ = ghostedMap;
+    ownedMap_   = ownedMap;
   
-  // Do the global distribution.
-  ownedVector_ep->Export(*ghostedVector_,*exporter_, cm);
-}
+    // Build up the Thyra conversion data structures.
+    ghostedSpace_ = create_VectorSpace(ghostedMap_);
+    ownedSpace_   = create_VectorSpace(ownedMap_);
+  
+    // Allocate the ghosted vector.
+    ghostedVector_ = rcp(new Epetra_Vector(*ghostedMap_));
+    isInitialized_ = true;
 
-void 
-EpetraVector_Write_GlobalEvaluationData::
-initializeData()
-{
-   TEUCHOS_TEST_FOR_EXCEPTION(!isInitialized_,std::logic_error,
-                              "EpetraVector_Write_GED has not been initialized, cannot call \"initializeData\"!");
+    // Get the Kokkos::View corresponding to the ghosted vector.
+    ghostedView_   =
+      panzer::kokkos_utils::getView<Epetra_Vector>(*getGhostedVector());
+  } // end of initialize()
+  
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  ghostToGlobal()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  void 
+  EpetraVector_Write_GlobalEvaluationData::
+  ghostToGlobal(
+    int mem)
+  {
+    using std::invalid_argument;
+    using std::logic_error;
+    using Teuchos::RCP;
+    using Thyra::get_Epetra_Vector;
+    TEUCHOS_TEST_FOR_EXCEPTION(ownedVector_.is_null(), logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::ghostToGlobal():  Owned "     \
+      "vector has not been set; can't perform the halo exchange!")
+  
+    // Initialize the ghosted data, zeroing out things, and filling in
+    // specified constants.
+    initializeData();
+    RCP<Epetra_Vector> ownedVector_ep = get_Epetra_Vector(*ownedMap_,
+      ownedVector_);
+  
+    // Set different combine modes.
+    Epetra_CombineMode cm = Add;
+    switch (getCombineMode())
+    {
+      case CM_Sum:
+        cm = Add;
+        break;
+      case CM_Min:
+        cm = Epetra_Min;
+        break;
+      case CM_Max:
+        cm = Epetra_Max;
+        break;
+      case CM_Insert:
+        cm = Insert;
+        break;
+      default:
+        TEUCHOS_TEST_FOR_EXCEPTION(true, invalid_argument,
+          "EpetraVector_Write_GlobalEvaluationData::ghostToGlobal():  "       \
+          "Invalid CombineMode.  Valid modes are CM_Sum, CM_Max, CM_Min, "    \
+          "and CM_Insert.")
+    }; // end switch (getCombineMode())
+    
+    // Do the global distribution.
+    ownedVector_ep->Export(*ghostedVector_, *exporter_, cm);
+  } // end of ghostToGlobal()
+  
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  initializeData()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  void 
+  EpetraVector_Write_GlobalEvaluationData::
+  initializeData()
+  {
+    using std::logic_error;
+    using Thyra::put_scalar;
+    TEUCHOS_TEST_FOR_EXCEPTION(not isInitialized_, logic_error,
+      "EpetraVector_Write_GlobalEvaluationData has not been initialized; "    \
+      "cannot call \"initializeData()\"!")
+    put_scalar(0.0, ownedVector_.ptr());
+  } // end of initializeData()
+  
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  setOwnedVector_Epetra()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  void 
+  EpetraVector_Write_GlobalEvaluationData::
+  setOwnedVector_Epetra(
+    const Teuchos::RCP<Epetra_Vector>& ownedVector)
+  {
+    using std::logic_error;
+    using Thyra::create_Vector;
+    TEUCHOS_TEST_FOR_EXCEPTION(not isInitialized_, logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::setOwnedVector_Epetra():  "   \
+      "This object hasn't yet been initialized.")
+    ownedVector_ = create_Vector(ownedVector, ownedSpace_);
+    ownedView_ = panzer::kokkos_utils::getView<Epetra_Vector>(*ownedVector_);
+  } // end of setOwnedVector_Epetra()
+  
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  getGhostedVector_Epetra()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  Teuchos::RCP<Epetra_Vector> 
+  EpetraVector_Write_GlobalEvaluationData::
+  getGhostedVector_Epetra() const
+  {
+    using std::logic_error;
+    TEUCHOS_TEST_FOR_EXCEPTION(not isInitialized_, logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::setGhostedVector_Epetra():  " \
+      "This object hasn't yet been initialized.")
+    TEUCHOS_TEST_FOR_EXCEPTION(ghostedVector_.is_null(), logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::setGhostedVector_Epetra():  " \
+      "The ghosted vector is just a null RCP.")
+    return ghostedVector_;
+  } // end of getGhostedVector_Epetra()
+  
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  setOwnedVector()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  void 
+  EpetraVector_Write_GlobalEvaluationData::
+  setOwnedVector(
+    const Teuchos::RCP<Thyra::VectorBase<double>>& ownedVector)
+  {
+    using std::logic_error;
+    TEUCHOS_TEST_FOR_EXCEPTION(not isInitialized_, logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::setOwnedVector():  This "     \
+      "object hasn't yet been initialized.")
+    ownedVector_ = ownedVector;
+    ownedView_ = panzer::kokkos_utils::getView<Epetra_Vector>(*ownedVector_);
+  } // end of setOwnedVector()
+  
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  getOwnedVector()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  Teuchos::RCP<Thyra::VectorBase<double>> 
+  EpetraVector_Write_GlobalEvaluationData::
+  getOwnedVector() const
+  {
+    using std::logic_error;
+    TEUCHOS_TEST_FOR_EXCEPTION(not isInitialized_, logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::getOwnedVector():  This "     \
+      "object hasn't yet been initialized.")
+    return ownedVector_;
+  } // end of getOwnedVector()
 
-   Thyra::put_scalar(0.0,ownedVector_.ptr());
-}
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  getGhostedVector()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  Teuchos::RCP<Thyra::VectorBase<double>> 
+  EpetraVector_Write_GlobalEvaluationData::
+  getGhostedVector() const
+  {
+    using std::logic_error;
+    using Thyra::create_Vector;
+    TEUCHOS_TEST_FOR_EXCEPTION(not isInitialized_, logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::getGhostedVector():  This "   \
+      "object hasn't yet been initialized.")
+    TEUCHOS_TEST_FOR_EXCEPTION(ghostedVector_.is_null(), logic_error,
+      "EpetraVector_Write_GlobalEvaluationData::getGhostedVector():  The "    \
+      "ghosted vector is just a null RCP.")
+    return create_Vector(ghostedVector_, ghostedSpace_);
+  } // end of getGhostedVector()
 
-void 
-EpetraVector_Write_GlobalEvaluationData::
-setOwnedVector_Epetra(const Teuchos::RCP<Epetra_Vector>& ownedVector)
-{
-  TEUCHOS_ASSERT(isInitialized_);
-  ownedVector_ = Thyra::create_Vector(ownedVector, ownedSpace_);
-}
+  /////////////////////////////////////////////////////////////////////////////
+  //
+  //  print()
+  //
+  /////////////////////////////////////////////////////////////////////////////
+  void
+  EpetraVector_Write_GlobalEvaluationData::
+  print(
+    std::ostream& os) const
+  {
+    using std::string;
+    const string tab("    ");
+    os << "\n";
+    os << tab << "EpetraVector_Write_GlobalEvaluationData\n"
+       << tab << "  init    = " << isInitialized_ << "\n"
+       << tab << "  owned   = " << ownedVector_   << "\n"
+       << tab << "  ghosted = " << ghostedVector_ << "\n";
+  } // end of print()
 
-Teuchos::RCP<Epetra_Vector> 
-EpetraVector_Write_GlobalEvaluationData::
-getGhostedVector_Epetra() const
-{
-  TEUCHOS_ASSERT(isInitialized_);
-  TEUCHOS_ASSERT(ghostedVector_!=Teuchos::null);
+} // end of namespace panzer
 
-  return ghostedVector_;
-}
-
-void 
-EpetraVector_Write_GlobalEvaluationData::
-setOwnedVector(const Teuchos::RCP<Thyra::VectorBase<double> >&
-	ownedVector)
-{
-  TEUCHOS_ASSERT(isInitialized_);
-  // ownedVector_ep_ = Thyra::get_Epetra_Vector(*ownedMap_, ownedVector);
-  ownedVector_ = ownedVector;
-/*
-  std::cout << "SETTING OWNED" << std::endl;
-  std::cout << Teuchos::describe(*ownedVector, Teuchos::VERB_EXTREME) << std::endl;
-  ownedVector_->Print(std::cout);
-  std::cout << std::endl;
-*/
-}
-
-Teuchos::RCP<Thyra::VectorBase<double> > 
-EpetraVector_Write_GlobalEvaluationData::
-getOwnedVector() const
-{
-  TEUCHOS_ASSERT(isInitialized_);
-  // return (ownedVector_==Teuchos::null) ? Teuchos::null :
-  // 	 Thyra::create_Vector(ownedVector_, ownedSpace_);
-  return ownedVector_;
-}
-
-Teuchos::RCP<Thyra::VectorBase<double> > 
-EpetraVector_Write_GlobalEvaluationData::
-getGhostedVector() const
-{
-  TEUCHOS_ASSERT(isInitialized_);
-  TEUCHOS_ASSERT(ghostedVector_!=Teuchos::null);
-
-  return Thyra::create_Vector(ghostedVector_,ghostedSpace_);
-}
-
-void
-EpetraVector_Write_GlobalEvaluationData::
-print(std::ostream & os) const
-{
-  const std::string tab = "    ";
-  os << "\n";
-  os << tab << "EpetraVector_Write_GlobalEvaluationData\n"
-     << tab << "  init    = " << isInitialized_ << "\n"
-     << tab << "  owned   = " << ownedVector_   << "\n"
-     << tab << "  ghosted = " << ghostedVector_ << "\n";
-
-  /*
-  os << "GHOSTED MAP\n";
-  ghostedMap_->Print(os);
-  os << "\n\n";
-
-  os << "GHOSTED Vector\n";
-  ghostedVector_->Print(os);
-  os << "\n\n";
-
-  os << "OWNED MAP\n";
-  ownedMap_->Print(os);
-  os << "\n\n";
-
-  os << "OWNED Vector\n";
-  ownedVector_->Print(os);
-  os << "\n\n";
-  */
-}
-
-}
+// end of Panzer_EpetraVector_Write_GlobalEvaluationData.cpp
--- packages/panzer/disc-fe/src/Panzer_EpetraVector_Write_GlobalEvaluationData.hpp
+++ packages/panzer/disc-fe/src/Panzer_EpetraVector_Write_GlobalEvaluationData.hpp
@@ -40,108 +40,247 @@
 // ***********************************************************************
 // @HEADER
 
-#ifndef __Panzer_EpetraVector_Write_GlobalEvaluationData_hpp__
-#define __Panzer_EpetraVector_Write_GlobalEvaluationData_hpp__
+#ifndef   __Panzer_EpetraVector_Write_GlobalEvaluationData_hpp__
+#define   __Panzer_EpetraVector_Write_GlobalEvaluationData_hpp__
 
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Include Files
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// Epetra
 #include "Epetra_Export.h"
-#include "Epetra_Vector.h"
 #include "Epetra_Map.h"
+#include "Epetra_Vector.h"
 
+// Panzer
+#include "Panzer_WriteVector_GlobalEvaluationData.hpp"
+
+// Teuchos
 #include "Teuchos_RCP.hpp"
 
+// Thyra
 #include "Thyra_VectorSpaceBase.hpp"
 #include "Thyra_VectorBase.hpp"
 
-#include "Panzer_WriteVector_GlobalEvaluationData.hpp"
+namespace panzer
+{
 
-namespace panzer {
+  /**
+   *  \brief This class provides a boundary exchange communication mechanism
+   *         for vectors.
+   *
+   *  \note This provides a "write" interface for parameters.
+   */
+  class EpetraVector_Write_GlobalEvaluationData
+    :
+    public WriteVector_GlobalEvaluationData
+  {
+    public:
 
-/** This class provides a boundary exchange communication mechanism for vectors.
-  * Note this provides a "write" interface for parameters 
-  */
-class EpetraVector_Write_GlobalEvaluationData : public WriteVector_GlobalEvaluationData {
-public:
-   //! Default constructor
-   EpetraVector_Write_GlobalEvaluationData()
-      : isInitialized_(false) { }
+      /**
+       *  \brief Default Constructor.
+       */
+      EpetraVector_Write_GlobalEvaluationData()
+        :
+        isInitialized_(false)
+      {
+      } // end of Default Constructor
 
-   EpetraVector_Write_GlobalEvaluationData(const EpetraVector_Write_GlobalEvaluationData & src)
-      : isInitialized_(false) { initialize(src.exporter_, src.ghostedMap_, src.ownedMap_); }
+      /**
+       *  \brief Copy Constructor.
+       *
+       *  \param[in] src The object to be copied.
+       */
+      EpetraVector_Write_GlobalEvaluationData(
+        const EpetraVector_Write_GlobalEvaluationData& src)
+        :
+        isInitialized_(false)
+      {
+        initialize(src.exporter_, src.ghostedMap_, src.ownedMap_);
+      } // end of Copy Constructor
 
-   /** Initialize this object with some Epetra communication objects. This method
-     * must be called before an object of this type can be used.
-     *
-     * \param[in] exporter Exporter for doing communication from the owned 
-     *                     to the ghosted vector.
-     * \param[in] ghostedMap Map describing the ghosted vector.
-     * \param[in] ownedMap Map describing the ghosted vector.
-     */
-   EpetraVector_Write_GlobalEvaluationData(const Teuchos::RCP<const Epetra_Export>& exporter,
-                                           const Teuchos::RCP<const Epetra_Map>&    ghostedMap,
-                                           const Teuchos::RCP<const Epetra_Map>&    ownedMap)
-      : isInitialized_(false) { initialize(exporter, ghostedMap, ownedMap); }
+      /**
+       *  \brief Initializing Constructor.
+       *
+       *  Initialize this object with some Epetra communication objects. This
+       *  method must be called before an object of this type can be used.
+       *
+       *  \param[in] exporter   Exporter for doing communication from the owned
+       *                        to the ghosted vector.
+       *  \param[in] ghostedMap Map describing the ghosted vector.
+       *  \param[in] ownedMap   Map describing the owned vector.
+       */
+      EpetraVector_Write_GlobalEvaluationData(
+        const Teuchos::RCP<const Epetra_Export>& exporter,
+        const Teuchos::RCP<const Epetra_Map>&    ghostedMap,
+        const Teuchos::RCP<const Epetra_Map>&    ownedMap)
+        :
+        isInitialized_(false)
+      {
+        initialize(exporter, ghostedMap, ownedMap);
+      } // end of Initializing Constructor
 
-   /** Initialize this object with some Epetra communication objects. This method
-     * must be called before an object of this type can be used.
-     *
-     * \param[in] exporter Exporter for doing communication from the owned 
-     *                     to the ghosted vector.
-     * \param[in] ghostedMap Map describing the ghosted vector.
-     * \param[in] ownedMap Map describing the ghosted vector.
-     */
-   void initialize(const Teuchos::RCP<const Epetra_Export>& exporter,
-                   const Teuchos::RCP<const Epetra_Map>&    ghostedMap,
-                   const Teuchos::RCP<const Epetra_Map>&    ownedMap);
+      /**
+       *  \brief Initialize this object with some `Epetra` communication
+       *         objects.
+       *
+       *  This method must be called before an object of this type can be used.
+       *
+       *  \param[in] exporter   Exporter for doing communication from the owned
+       *                        to the ghosted vector.
+       *  \param[in] ghostedMap Map describing the ghosted vector.
+       *  \param[in] ownedMap   Map describing the owned vector.
+       */
+      void
+      initialize(
+        const Teuchos::RCP<const Epetra_Export>& exporter,
+        const Teuchos::RCP<const Epetra_Map>&    ghostedMap,
+        const Teuchos::RCP<const Epetra_Map>&    ownedMap);
 
-   /** For this class, this method does the halo exchange for the 
-     * vector.
-     */
-   virtual void ghostToGlobal(int mem);
+      /**
+       *  \brief Communicate the ghosted data to the owned vector.
+       *
+       *  For this class, this method does the halo exchange for the vector.
+       *
+       *  \param[in] mem Not needed for this class, but part of the
+       *                 `GlobalEvaluationData` interface.
+       */
+      virtual void
+      ghostToGlobal(
+        int mem);
 
-   //! Clear out the ghosted vector 
-   virtual void initializeData(); 
+      /**
+       *  \brief Clear out the ghosted vector.
+       */
+      virtual void
+      initializeData();
 
-   //! Nothing to do (its read only)
-   virtual bool requiresDirichletAdjustment() const { return false; }
+      /**
+       *  \brief Determine if a Dirichlet adjustment is necessary.
+       *
+       *  \returns False.                                                        // JMG:  But why?                                
+       */
+      virtual bool
+      requiresDirichletAdjustment() const
+      {
+        return false;
+      } // end of requiresDirichletAdjustment()
 
-   //! Set the owned vector (Epetra version)
-   void setOwnedVector_Epetra(const Teuchos::RCP<Epetra_Vector>&
-      ownedVector);
+      /**
+       *  \brief Set the owned vector (`Epetra` version).
+       *
+       *  \param[in] ownedVector An `Epetra_Vector` that you would like to set
+       *                         as the owned vector.
+       */
+      void
+      setOwnedVector_Epetra(
+        const Teuchos::RCP<Epetra_Vector>& ownedVector);
 
-   //! Get the ghosted vector (Epetra version)
-   Teuchos::RCP<Epetra_Vector> getGhostedVector_Epetra() const;
+      /**
+       *  \brief Get the ghosted vector (`Epetra` version).
+       *
+       *  \returns The ghosted vector as an `Epetra_Vector`.
+       */
+      Teuchos::RCP<Epetra_Vector>
+      getGhostedVector_Epetra() const;
 
-   //! Set the owned vector (Thyra version)
-   void setOwnedVector(const Teuchos::RCP<Thyra::VectorBase<double> >&
-      ownedVector);
+      /**
+       *  \brief Set the owned vector (`Thyra` version).
+       *
+       *  \param[in] ownedVector A `Thyra::VectorBase<double>` that you would
+       *                         like to set as the owned vector.
+       */
+      void
+      setOwnedVector(
+        const Teuchos::RCP<Thyra::VectorBase<double>>& ownedVector);
 
-   //! Get the owned vector (Thyra version)
-   Teuchos::RCP<Thyra::VectorBase<double> > getOwnedVector() const;
+      /**
+       *  \brief Get the owned vector (`Thyra` version).
+       *
+       *  \returns The owned vector as a `Thyra::VectorBase<double>`.
+       */
+      Teuchos::RCP<Thyra::VectorBase<double>>
+      getOwnedVector() const;
 
-   //! Get the ghosted vector (Thyra version)
-   Teuchos::RCP<Thyra::VectorBase<double> > getGhostedVector() const;
+      /**
+       *  \brief Get the ghosted vector (`Thyra` version).
+       *
+       *  \returns The ghosted vector as a `Thyra::VectorBase<double>`.
+       */
+      Teuchos::RCP<Thyra::VectorBase<double>>
+      getGhostedVector() const;
 
-   //! Is this object initialized
-   virtual bool isInitialized() const { return isInitialized_; }
+      /**
+       *  \brief Is this object initialized?
+       *
+       *  \returns Whether or not this object is initialized.
+       */
+      virtual bool
+      isInitialized() const
+      {
+        return isInitialized_;
+      } // end of isInitialized()
 
-   //! Diagnostic function
-   void print(std::ostream & os) const;
+      /**
+       *  \brief Print the object.
+       *
+       *  This is a diagnostic function for debugging purposes.
+       *
+       *  \param[in,out] os The output stream to which the data should be
+       *                    printed.
+       */
+      void
+      print(
+        std::ostream& os) const;
 
-private:
-   bool isInitialized_;
+    private:
 
-   Teuchos::RCP<const Epetra_Map> ghostedMap_;
-   Teuchos::RCP<const Epetra_Map> ownedMap_;
+      /**
+       *  \brief A flag indicating whether or not the object has been
+       *         initialized.
+       */
+      bool isInitialized_;
 
-   Teuchos::RCP<const Thyra::VectorSpaceBase<double> > ghostedSpace_;
-   Teuchos::RCP<const Thyra::VectorSpaceBase<double> > ownedSpace_;
+      /**
+       *  \brief The map corresponding to the ghosted vector.
+       */
+      Teuchos::RCP<const Epetra_Map> ghostedMap_;
 
-   Teuchos::RCP<const Epetra_Export> exporter_;
-   Teuchos::RCP<Epetra_Vector>       ghostedVector_;
+      /**
+       *  \brief The map corresponding to the owned vector.
+       */
+      Teuchos::RCP<const Epetra_Map> ownedMap_;
 
-   Teuchos::RCP<Thyra::VectorBase<double> > ownedVector_;
-};
+      /**
+       *  \brief The vector space corresponding to the ghosted vector.
+       */
+      Teuchos::RCP<const Thyra::VectorSpaceBase<double>> ghostedSpace_;
 
-}
+      /**
+       *  \brief The vector space corresponding to the owned vector.
+       */
+      Teuchos::RCP<const Thyra::VectorSpaceBase<double>> ownedSpace_;
 
-#endif
+      /**
+       *  \brief The exporter used to communicate between the owned and ghosted
+       *         vectors.
+       */
+      Teuchos::RCP<const Epetra_Export> exporter_;
+
+      /**
+       *  \brief The ghosted vector.
+       */
+      Teuchos::RCP<Epetra_Vector> ghostedVector_;
+
+      /**
+       *  \brief The owned vector.
+       */
+      Teuchos::RCP<Thyra::VectorBase<double>> ownedVector_;
+
+  }; // end of class EpetraVector_Write_GlobalEvaluationData
+
+} // end of namespace panzer
+
+#endif // __Panzer_EpetraVector_Write_GlobalEvaluationData_hpp__
