9a2644d32283613e9088f0c5d84a3a42ffa64b1f
Kyungjoo Kim <kyukim@sandia.gov>
2016-11-01 17:40:11 -0600

    Intrepid2 - added new tests for hex high order assembly

--- packages/intrepid2/refactor/unit-test/Orientation/test_orientationtools_modify_basis_hex_hcurl.hpp
+++ packages/intrepid2/refactor/unit-test/Orientation/test_orientationtools_modify_basis_hex_hcurl.hpp
@@ -74,8 +74,8 @@ namespace Intrepid2 {
       *outStream << "-------------------------------------------------------------------------------" << "\n\n"; \
     }
     
-    template<typename QuadBasisType, typename DeviceSpaceType>
-    int OrientationToolsModifyBasis_QUAD_HCURL(const bool verbose) {
+    template<typename DeviceSpaceType>
+    int OrientationToolsModifyBasis_HEX_HCURL(const bool verbose) {
 
       Teuchos::RCP<std::ostream> outStream;
       Teuchos::oblackholestream bhs; // outputs nothing
@@ -99,43 +99,54 @@ namespace Intrepid2 {
       *outStream
         << "===============================================================================\n"
         << "|                                                                             |\n"
-        << "|            Unit Test (OrientationTools, getModifiedHcurl/Hdiv_In_Basis)     |\n"
+        << "|           Unit Test (OrientationTools, Hex, getModifiedHgradBasis)          |\n"
         << "|                                                                             |\n"
         << "===============================================================================\n";
 
       int errorFlag = 0;
-      const double tol = tolerence();
+      const double tol = 100*tolerence();
 
       typedef OrientationTools<DeviceSpaceType> ots;
       try {
 
         {
-          *outStream << "\n -- Testing Quadrilateral \n\n";
+          *outStream << "\n -- Testing Hexahedron \n\n";
 
-          const ordinal_type order = 3;
-          QuadBasisType cellBasis(order);
+          const ordinal_type order = 2;
 
-          *outStream << "\n --- " << cellBasis.getName() << "\n";
-
+          Basis_HCURL_HEX_In_FEM<DeviceSpaceType> cellBasis(order);
           const auto cellTopo = cellBasis.getBaseCellTopology();
           const ordinal_type ndofBasis = cellBasis.getCardinality();
-          
-          // 
-          // 9 12 13 16
-          // 4  3 11 15
-          // 5  2  8 14
-          // 1  6  7 10
-          ordinal_type refMesh[9][4] = { { 1, 6, 2, 5 },
-                                         { 6, 7, 8, 2 },
-                                         { 7,10,14, 8 },
-                                         { 5, 2, 3, 4 },
-                                         { 2, 8,11, 3 },
-                                         { 8,14,15,11 },
-                                         { 4, 3,12, 9 },
-                                         { 3,11,13,12 },
-                                         {11,15,16,13 } };
 
-          const ordinal_type numCells = 9, numVerts = 4, numEdges = 4;
+          ordinal_type refMesh[27][8] = { {   8 ,   33 ,   45 ,   10 ,   11 ,   37 ,   57 ,   15  },
+                                          {  33 ,   34 ,   46 ,   45 ,   37 ,   38 ,   58 ,   57  },
+                                          {  34 ,   21 ,   25 ,   46 ,   38 ,   23 ,   32 ,   58  },
+                                          {  10 ,   45 ,   43 ,    9 ,   15 ,   57 ,   59 ,   16  },
+                                          {  45 ,   46 ,   44 ,   43 ,   57 ,   58 ,   60 ,   59  },
+                                          {  46 ,   25 ,   26 ,   44 ,   58 ,   32 ,   31 ,   60  },
+                                          {   9 ,   43 ,   41 ,    5 ,   16 ,   59 ,   51 ,    7  },
+                                          {  43 ,   44 ,   42 ,   41 ,   59 ,   60 ,   52 ,   51  },
+                                          {  44 ,   26 ,   24 ,   42 ,   60 ,   31 ,   27 ,   52  },
+                                          {  11 ,   37 ,   57 ,   15 ,   12 ,   39 ,   61 ,   13  },
+                                          {  37 ,   38 ,   58 ,   57 ,   39 ,   40 ,   62 ,   61  },
+                                          {  38 ,   23 ,   32 ,   58 ,   40 ,   22 ,   30 ,   62  },
+                                          {  15 ,   57 ,   59 ,   16 ,   13 ,   61 ,   63 ,   14  },
+                                          {  57 ,   58 ,   60 ,   59 ,   61 ,   62 ,   64 ,   63  },
+                                          {  58 ,   32 ,   31 ,   60 ,   62 ,   30 ,   29 ,   64  },
+                                          {  16 ,   59 ,   51 ,    7 ,   14 ,   63 ,   49 ,    6  },
+                                          {  59 ,   60 ,   52 ,   51 ,   63 ,   64 ,   50 ,   49  },
+                                          {  60 ,   31 ,   27 ,   52 ,   64 ,   29 ,   28 ,   50  },
+                                          {  12 ,   39 ,   61 ,   13 ,    1 ,   35 ,   53 ,    3  },
+                                          {  39 ,   40 ,   62 ,   61 ,   35 ,   36 ,   54 ,   53  },
+                                          {  40 ,   22 ,   30 ,   62 ,   36 ,   18 ,   20 ,   54  },
+                                          {  13 ,   61 ,   63 ,   14 ,    3 ,   53 ,   55 ,    4  },
+                                          {  61 ,   62 ,   64 ,   63 ,   53 ,   54 ,   56 ,   55  },
+                                          {  62 ,   30 ,   29 ,   64 ,   54 ,   20 ,   19 ,   56  },
+                                          {  14 ,   63 ,   49 ,    6 ,    4 ,   55 ,   47 ,    2  },
+                                          {  63 ,   64 ,   50 ,   49 ,   55 ,   56 ,   48 ,   47  },
+                                          {  64 ,   29 ,   28 ,   50 ,   56 ,   19 ,   17 ,   48  } };
+          
+          const ordinal_type numCells = 27, numVerts = 8, numEdges = 12, numFaces = 6;
 
           // view to import refMesh from host          
           Kokkos::DynRankView<ordinal_type,Kokkos::LayoutRight,HostSpaceType> 
@@ -149,11 +160,11 @@ namespace Intrepid2 {
 
           auto elemOrtsHost = Kokkos::create_mirror_view(typename HostSpaceType::memory_space(), elemOrts);
           Kokkos::deep_copy(elemOrtsHost, elemOrts);
-          
+
           // cell specific modified basis 
           Kokkos::DynRankView<double,DeviceSpaceType> outValues("outValues", numCells, ndofBasis);
           Kokkos::DynRankView<double,DeviceSpaceType> refValues("refValues", numCells, ndofBasis);
-          
+
           auto refValuesHost = Kokkos::create_mirror_view(typename HostSpaceType::memory_space(), refValues);
           for (ordinal_type cell=0;cell<numCells;++cell)           
             for (ordinal_type bf=0;bf<ndofBasis;++bf) 
@@ -170,33 +181,43 @@ namespace Intrepid2 {
           Kokkos::deep_copy(outValuesHost, outValues);
 
           for (ordinal_type cell=0;cell<numCells;++cell) {
+            *outStream << "\n cell = " << cell << "\n";
+
             int flag = 0 ;
             std::stringstream s1, s2;
-            s1 << std::fixed << std::setprecision(0);
-            s2 << std::fixed << std::setprecision(0);
-
+            
+            const double ortVal[numEdges][2] = { {  1.0,-1.0 }, // bottom
+                                                 {  1.0,-1.0 },
+                                                 { -1.0, 1.0 },
+                                                 { -1.0, 1.0 },
+                                                 {  1.0,-1.0 }, // top
+                                                 {  1.0,-1.0 },
+                                                 { -1.0, 1.0 },
+                                                 { -1.0, 1.0 },
+                                                 {  1.0,-1.0 }, // vertical ones
+                                                 {  1.0,-1.0 },
+                                                 {  1.0,-1.0 },
+                                                 {  1.0,-1.0 } };
             ordinal_type orts[numEdges];
             elemOrtsHost(cell).getEdgeOrientation(orts, numEdges);
             
-            const double ortVal[4][2] = { {  1.0 , -1.0 },
-                                          {  1.0 , -1.0 },
-                                          { -1.0 ,  1.0 },
-                                          { -1.0 ,  1.0 } };
-
-            s1 << " :: edge(0000) = " ;
-            s2 << " :: edge(" << orts[0] << orts[1] << orts[2] << orts[3] << ") = ";
+            s1 << " :: edge(000000000000) = " ;
+            s2 << " :: edge(" 
+               << orts[0] << orts[1] << orts[2] << orts[3] 
+               << orts[4] << orts[5] << orts[6] << orts[7] 
+               << orts[8] << orts[9] << orts[10] << orts[11] 
+               << ") = ";
             for (ordinal_type edgeId=0;edgeId<numEdges;++edgeId) {
-              const ordinal_type ort = orts[edgeId];
+              const ordinal_type ort = orts[edgeId];   
               const ordinal_type ndof = cellBasis.getDofTag(cellBasis.getDofOrdinal(1, edgeId, 0))(3);
               for (ordinal_type i=0;i<ndof;++i) {
                 const double signVal = ortVal[edgeId][ort];
-                const ordinal_type ii = (signVal < 0 ? (ndof - i - 1) : i);
-
+                const ordinal_type ii = (signVal < 0 ? (ndof - i - 1) : i); 
+                
                 const ordinal_type refOrd = cellBasis.getDofOrdinal(1, edgeId, ii);
                 const ordinal_type outOrd = cellBasis.getDofOrdinal(1, edgeId, i);
                 s1 << std::setw(4) << refValuesHost(cell, outOrd);              
                 s2 << std::setw(4) << outValuesHost(cell, outOrd);              
-
 
                 flag += (std::abs(signVal*outValuesHost(cell, outOrd) - refValuesHost(cell, refOrd)) > tol);
               }
@@ -204,10 +225,45 @@ namespace Intrepid2 {
               s1 << " // ";
               s2 << " // ";
             }
+            *outStream << " - refValues = " << s1.str() << "\n"
+                       << " - outValues = " << s2.str() << "\n\n";
+            s1.str("");s2.str("");
 
-            *outStream << "\n cell = " << cell << "\n"
-                       << " - refValues = " << s1.str() << "\n"
-                       << " - outValues = " << s2.str() << "\n";
+            s1 << " :: face(000000) = " ;
+            s2 << " :: face(" << orts[0] << orts[1] << orts[2] << orts[3] << orts[4] << orts[5] << ") = ";
+            for (ordinal_type faceId=0;faceId<numFaces;++faceId) {
+              const ordinal_type ndof = cellBasis.getDofTag(cellBasis.getDofOrdinal(2, faceId, 0))(3);
+              for (ordinal_type i=0;i<ndof;++i) {
+                //const ordinal_type refOrd = cellBasis.getDofOrdinal(2, faceId, i);
+                const ordinal_type outOrd = cellBasis.getDofOrdinal(2, faceId, i);
+                s1 << std::setw(4) << refValuesHost(cell, outOrd);              
+                s2 << std::setw(4) << outValuesHost(cell, outOrd);              
+
+                // i dont know how to verify this consistency
+              }
+
+              s1 << " // ";
+              s2 << " // ";
+            }
+            *outStream << " - refValues = " << s1.str() << "\n"
+                       << " - outValues = " << s2.str() << "\n\n";
+            s1.str("");s2.str("");
+
+            s1 << " :: intr = " ;
+            s2 << " :: intr = " ;
+            {
+              const ordinal_type ndof = cellBasis.getDofTag(cellBasis.getDofOrdinal(3, 0, 0))(3);
+              for (ordinal_type i=0;i<ndof;++i) {
+                const ordinal_type ord = cellBasis.getDofOrdinal(3, 0, i);
+                s1 << std::setw(4) << refValuesHost(cell, ord);              
+                s2 << std::setw(4) << outValuesHost(cell, ord);              
+                flag += (std::abs(outValuesHost(cell, ord) - refValuesHost(cell, ord)) > tol);
+              }
+            }
+            *outStream << " - refValues = " << s1.str() << "\n"
+                       << " - outValues = " << s2.str() << "\n\n";
+            s1.str("");s2.str("");
+
             if (flag) {
               *outStream << "                      ^^^^^^^^^^^^ FAILURE\n";
               errorFlag += flag;
--- packages/intrepid2/refactor/unit-test/Orientation/test_orientationtools_modify_basis_hex_hgrad.hpp
+++ packages/intrepid2/refactor/unit-test/Orientation/test_orientationtools_modify_basis_hex_hgrad.hpp
@@ -75,7 +75,7 @@ namespace Intrepid2 {
     }
     
     template<typename DeviceSpaceType>
-    int OrientationToolsModifyBasis_QUAD_HGRAD(const bool verbose) {
+    int OrientationToolsModifyBasis_HEX_HGRAD(const bool verbose) {
 
       Teuchos::RCP<std::ostream> outStream;
       Teuchos::oblackholestream bhs; // outputs nothing
@@ -104,36 +104,49 @@ namespace Intrepid2 {
         << "===============================================================================\n";
 
       int errorFlag = 0;
-      const double tol = tolerence();
+      const double tol = 100*tolerence();
 
       typedef OrientationTools<DeviceSpaceType> ots;
       try {
 
         {
-          *outStream << "\n -- Testing Quadrilateral \n\n";
+          *outStream << "\n -- Testing Hexahedron \n\n";
 
-          const ordinal_type order = 4;
+          const ordinal_type order = 3;
 
-          Basis_HGRAD_QUAD_Cn_FEM<DeviceSpaceType> cellBasis(order);
+          Basis_HGRAD_HEX_Cn_FEM<DeviceSpaceType> cellBasis(order);
           const auto cellTopo = cellBasis.getBaseCellTopology();
           const ordinal_type ndofBasis = cellBasis.getCardinality();
 
+          ordinal_type refMesh[27][8] = { {   8 ,   33 ,   45 ,   10 ,   11 ,   37 ,   57 ,   15  },
+                                          {  33 ,   34 ,   46 ,   45 ,   37 ,   38 ,   58 ,   57  },
+                                          {  34 ,   21 ,   25 ,   46 ,   38 ,   23 ,   32 ,   58  },
+                                          {  10 ,   45 ,   43 ,    9 ,   15 ,   57 ,   59 ,   16  },
+                                          {  45 ,   46 ,   44 ,   43 ,   57 ,   58 ,   60 ,   59  },
+                                          {  46 ,   25 ,   26 ,   44 ,   58 ,   32 ,   31 ,   60  },
+                                          {   9 ,   43 ,   41 ,    5 ,   16 ,   59 ,   51 ,    7  },
+                                          {  43 ,   44 ,   42 ,   41 ,   59 ,   60 ,   52 ,   51  },
+                                          {  44 ,   26 ,   24 ,   42 ,   60 ,   31 ,   27 ,   52  },
+                                          {  11 ,   37 ,   57 ,   15 ,   12 ,   39 ,   61 ,   13  },
+                                          {  37 ,   38 ,   58 ,   57 ,   39 ,   40 ,   62 ,   61  },
+                                          {  38 ,   23 ,   32 ,   58 ,   40 ,   22 ,   30 ,   62  },
+                                          {  15 ,   57 ,   59 ,   16 ,   13 ,   61 ,   63 ,   14  },
+                                          {  57 ,   58 ,   60 ,   59 ,   61 ,   62 ,   64 ,   63  },
+                                          {  58 ,   32 ,   31 ,   60 ,   62 ,   30 ,   29 ,   64  },
+                                          {  16 ,   59 ,   51 ,    7 ,   14 ,   63 ,   49 ,    6  },
+                                          {  59 ,   60 ,   52 ,   51 ,   63 ,   64 ,   50 ,   49  },
+                                          {  60 ,   31 ,   27 ,   52 ,   64 ,   29 ,   28 ,   50  },
+                                          {  12 ,   39 ,   61 ,   13 ,    1 ,   35 ,   53 ,    3  },
+                                          {  39 ,   40 ,   62 ,   61 ,   35 ,   36 ,   54 ,   53  },
+                                          {  40 ,   22 ,   30 ,   62 ,   36 ,   18 ,   20 ,   54  },
+                                          {  13 ,   61 ,   63 ,   14 ,    3 ,   53 ,   55 ,    4  },
+                                          {  61 ,   62 ,   64 ,   63 ,   53 ,   54 ,   56 ,   55  },
+                                          {  62 ,   30 ,   29 ,   64 ,   54 ,   20 ,   19 ,   56  },
+                                          {  14 ,   63 ,   49 ,    6 ,    4 ,   55 ,   47 ,    2  },
+                                          {  63 ,   64 ,   50 ,   49 ,   55 ,   56 ,   48 ,   47  },
+                                          {  64 ,   29 ,   28 ,   50 ,   56 ,   19 ,   17 ,   48  } };
           
-          // 
-          // 9 12 13 16
-          // 4  3 11 15
-          // 5  2  8 14
-          // 1  6  7 10
-          ordinal_type refMesh[9][4] = { { 1, 6, 2, 5 },
-                                         { 6, 7, 8, 2 },
-                                         { 7,10,14, 8 },
-                                         { 5, 2, 3, 4 },
-                                         { 2, 8,11, 3 },
-                                         { 8,14,15,11 },
-                                         { 4, 3,12, 9 },
-                                         { 3,11,13,12 },
-                                         {11,15,16,13 } };
-          const ordinal_type numCells = 9, numVerts = 4, numEdges = 4;
+          const ordinal_type numCells = 27, numVerts = 8, numEdges = 12, numFaces = 6;
 
           // view to import refMesh from host          
           Kokkos::DynRankView<ordinal_type,Kokkos::LayoutRight,HostSpaceType> 
@@ -168,6 +181,8 @@ namespace Intrepid2 {
           Kokkos::deep_copy(outValuesHost, outValues);
 
           for (auto cell=0;cell<numCells;++cell) {
+            *outStream << "\n cell = " << cell << "\n";
+
             int flag = 0 ;
             std::stringstream s1, s2;
             
@@ -179,23 +194,38 @@ namespace Intrepid2 {
               s2 << std::setw(4) << outValuesHost(cell, ord); 
               flag += (std::abs(outValuesHost(cell, ord) - refValuesHost(cell, ord)) > tol);
             }
+            *outStream << " - refValues = " << s1.str() << "\n"
+                       << " - outValues = " << s2.str() << "\n\n";
+            s1.str("");s2.str("");
 
-            const ordinal_type reverse[numEdges][2] = { { 0,1 },
+            const ordinal_type reverse[numEdges][2] = { { 0,1 }, // bottom
                                                         { 0,1 },
                                                         { 1,0 },
-                                                        { 1,0 } };
+                                                        { 1,0 },
+                                                        { 0,1 }, // top
+                                                        { 0,1 },
+                                                        { 1,0 },
+                                                        { 1,0 },
+                                                        { 0,1 }, // vertical ones
+                                                        { 0,1 },
+                                                        { 0,1 },
+                                                        { 0,1 } };
             ordinal_type orts[numEdges];
             elemOrtsHost(cell).getEdgeOrientation(orts, numEdges);
-
-            s1 << " :: edge(0000) = " ;
-            s2 << " :: edge(" << orts[0] << orts[1] << orts[2] << orts[3] << ") = ";
+            
+            s1 << " :: edge(000000000000) = " ;
+            s2 << " :: edge(" 
+               << orts[0] << orts[1] << orts[2] << orts[3] 
+               << orts[4] << orts[5] << orts[6] << orts[7] 
+               << orts[8] << orts[9] << orts[10] << orts[11] 
+               << ") = ";
             for (auto edgeId=0;edgeId<numEdges;++edgeId) {
               const auto ndof = cellBasis.getDofTag(cellBasis.getDofOrdinal(1, edgeId, 0))(3);
               for (auto i=0;i<ndof;++i) {
                 const ordinal_type ii = (reverse[edgeId][orts[edgeId]] ? ndof-i-1 : i);
+
                 const auto refOrd = cellBasis.getDofOrdinal(1, edgeId, ii);
                 const auto outOrd = cellBasis.getDofOrdinal(1, edgeId, i);
-
                 s1 << std::setw(4) << refValuesHost(cell, outOrd);              
                 s2 << std::setw(4) << outValuesHost(cell, outOrd);              
 
@@ -205,22 +235,45 @@ namespace Intrepid2 {
               s1 << " // ";
               s2 << " // ";
             }
+            *outStream << " - refValues = " << s1.str() << "\n"
+                       << " - outValues = " << s2.str() << "\n\n";
+            s1.str("");s2.str("");
+
+            s1 << " :: face(000000) = " ;
+            s2 << " :: face(" << orts[0] << orts[1] << orts[2] << orts[3] << orts[4] << orts[5] << ") = ";
+            for (auto faceId=0;faceId<numFaces;++faceId) {
+              const auto ndof = cellBasis.getDofTag(cellBasis.getDofOrdinal(2, faceId, 0))(3);
+              for (auto i=0;i<ndof;++i) {
+                //const auto refOrd = cellBasis.getDofOrdinal(2, faceId, i);
+                const auto outOrd = cellBasis.getDofOrdinal(2, faceId, i);
+                s1 << std::setw(4) << refValuesHost(cell, outOrd);              
+                s2 << std::setw(4) << outValuesHost(cell, outOrd);              
+
+                // i dont know how to verify this consistency
+              }
+
+              s1 << " // ";
+              s2 << " // ";
+            }
+            *outStream << " - refValues = " << s1.str() << "\n"
+                       << " - outValues = " << s2.str() << "\n\n";
+            s1.str("");s2.str("");
 
             s1 << " :: intr = " ;
             s2 << " :: intr = " ;
             {
-              const auto ndof = cellBasis.getDofTag(cellBasis.getDofOrdinal(2, 0, 0))(3);
+              const auto ndof = cellBasis.getDofTag(cellBasis.getDofOrdinal(3, 0, 0))(3);
               for (auto i=0;i<ndof;++i) {
-                const auto ord = cellBasis.getDofOrdinal(2, 0, i);
+                const auto ord = cellBasis.getDofOrdinal(3, 0, i);
                 s1 << std::setw(4) << refValuesHost(cell, ord);              
                 s2 << std::setw(4) << outValuesHost(cell, ord);              
                 flag += (std::abs(outValuesHost(cell, ord) - refValuesHost(cell, ord)) > tol);
               }
             }
+            *outStream << " - refValues = " << s1.str() << "\n"
+                       << " - outValues = " << s2.str() << "\n\n";
+            s1.str("");s2.str("");
 
-            *outStream << "\n cell = " << cell << "\n"
-                       << " - refValues = " << s1.str() << "\n"
-                       << " - outValues = " << s2.str() << "\n";
             if (flag) {
               *outStream << "                      ^^^^^^^^^^^^ FAILURE\n";
               errorFlag += flag;
