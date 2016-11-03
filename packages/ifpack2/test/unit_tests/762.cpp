96d4c1700fed86b0ba6ed42f61d8280801cedfe2
Mark Hoemmen <mhoemme@sandia.gov>
2016-11-03 11:33:12 -0600

    Ifpack2: Refined #762 test
    @trilinos/ifpack2 The test now shows that the alpha=1, beta=0 case has
    no trouble, but the alpha=2, beta=0 case does.

--- packages/ifpack2/test/unit_tests/762.cpp
+++ packages/ifpack2/test/unit_tests/762.cpp
@@ -57,6 +57,7 @@ using Teuchos::outArg;
 using Teuchos::RCP;
 using Teuchos::REDUCE_MIN;
 using Teuchos::reduceAll;
+using std::cerr;
 using std::endl;
 
 typedef tif_utest::Node Node;
@@ -67,15 +68,23 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(Ifpack2, Issue762, Scalar, LO, GO)
   typedef Tpetra::CrsMatrix<Scalar,LO,GO,Node> crs_matrix_type;
   typedef Tpetra::MultiVector<Scalar,LO,GO,Node> MV;
   typedef Teuchos::ScalarTraits<Scalar> STS;
+  // Whether to print to cerr, for more immediate output ('out'
+  // doesn't actually print until the test returns, so it's not so
+  // helpful if the test segfaults).
+  const bool printToCerr = true;
+
   int lclSuccess = 1; // to be updated below
   int gblSuccess = 0; // output argument
   std::ostringstream errStrm;
 
-  using std::cerr;
-
-  //out << "Ifpack2: Test #762" << endl;
-  cerr << "Ifpack2: Test #762" << endl;
-  //Teuchos::OSTab tab1 (out);
+  if (printToCerr) {
+    cerr << "Ifpack2: Test #762" << endl;
+  }
+  else {
+    out << "Ifpack2: Test #762" << endl;
+  }
+  // This is a scope guard, so it's better to do this unconditionally.
+  Teuchos::OSTab tab1 (out);
 
   const global_size_t num_rows_per_proc = 5;
   RCP<const map_type> rowmap =
@@ -90,10 +99,14 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(Ifpack2, Issue762, Scalar, LO, GO)
     return; // that's the best we can do
   }
   if (comm->getSize () > 1) {
-    // out << "This test may only be run in serial "
-    //   "or with a single MPI process." << endl;
-    cerr << "This test may only be run in serial "
-      "or with a single MPI process." << endl;
+    if (printToCerr) {
+      cerr << "This test may only be run in serial "
+        "or with a single MPI process." << endl;
+    }
+    else {
+      out << "This test may only be run in serial "
+        "or with a single MPI process." << endl;
+    }
     return;
   }
 
@@ -102,7 +115,11 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(Ifpack2, Issue762, Scalar, LO, GO)
   RCP<const crs_matrix_type> crsmatrix;
   try {
     crsmatrix = tif_utest::create_test_matrix2<Scalar,LO,GO,Node>(rowmap);
-    cerr << "create_test_matrix2 returned!" << endl;
+    if (printToCerr) {
+      cerr << "create_test_matrix2 returned!" << endl;
+    }
+    // Don't print to 'out' here, since we only care about per-process
+    // printing in this case.
   }
   catch (std::exception& e) {
     success = false;
@@ -123,46 +140,123 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(Ifpack2, Issue762, Scalar, LO, GO)
     return;
   }
 
-  //out << "Creating test problem" << endl;
-  cerr << "Creating test problem" << endl;
+  if (printToCerr) {
+    cerr << "Creating test problem" << endl;
+  }
+  else {
+    out << "Creating test problem" << endl;
+  }
   MV x (rowmap, 2);
   MV y (rowmap, 2);
   x.putScalar (STS::one ());
 
-  // Now test alpha != 1 and beta == 0.
-  Scalar alpha = Teuchos::as<Scalar> (2.0);
-  Scalar beta = Teuchos::as<Scalar> (0.0);
-  // out << "Testing apply() for alpha = " << alpha
-  //     << " and beta = " << beta << endl;
+  {
+    // First test alpha == 1 and beta == 0.
+    const Scalar alpha = Teuchos::as<Scalar> (1.0);
+    const Scalar beta = Teuchos::as<Scalar> (0.0);
 
-  cerr << "Creating copies of x and y" << endl;
-  MV x_copy = Tpetra::createCopy (x);
-  MV y_copy = Tpetra::createCopy (y);
+    if (printToCerr) {
+      cerr << "Creating copies of x and y" << endl;
+    }
+    else {
+      out << "Creating copies of x and y" << endl;
+    }
+    MV x_copy = Tpetra::createCopy (x);
+    MV y_copy = Tpetra::createCopy (y);
 
-  // out << "Testing apply() for alpha = " << alpha
-  //     << " and beta = " << beta << endl;
-  cerr << "Testing apply() for alpha = " << alpha
-       << " and beta = " << beta << endl;
-  try {
-    //the problem  line
-    crsmatrix->apply (x_copy, y_copy, Teuchos::NO_TRANS, alpha, beta);
-    cerr << "apply returned!" << endl;
+    if (printToCerr) {
+      cerr << "Testing apply() for alpha = " << alpha
+           << " and beta = " << beta << endl;
+    }
+    else {
+      out << "Testing apply() for alpha = " << alpha
+          << " and beta = " << beta << endl;
+    }
+    try {
+      crsmatrix->apply (x_copy, y_copy, Teuchos::NO_TRANS, alpha, beta);
+      if (printToCerr) {
+        cerr << "apply (alpha = " << alpha << ", beta = " << beta << ") returned!"
+             << endl;
+      }
+      // Don't print to 'out' here, since we only care about per-process
+      // printing in this case.
+    }
+    catch (std::exception& e) {
+      lclSuccess = 0;
+      errStrm << "Process " << comm->getRank () << ": CrsMatrix::apply threw "
+        "an exception: " << e.what () << endl;
+    }
+    catch (...) {
+      lclSuccess = 0;
+      errStrm << "Process " << comm->getRank () << ": CrsMatrix::apply threw "
+        "an exception not a subclass of std::exception." << endl;
+    }
+    reduceAll<int, int> (*comm, REDUCE_MIN, lclSuccess, outArg (gblSuccess));
+    TEST_EQUALITY_CONST( gblSuccess, 1 );
+    if (gblSuccess != 1) {
+      Tpetra::Details::gathervPrint (out, errStrm.str (), *comm);
+      return;
+    }
   }
-  catch (std::exception& e) {
-    lclSuccess = 0;
-    errStrm << "Process " << comm->getRank () << ": CrsMatrix::apply threw "
-      "an exception: " << e.what () << endl;
-  }
-  catch (...) {
-    lclSuccess = 0;
-    errStrm << "Process " << comm->getRank () << ": CrsMatrix::apply threw "
-      "an exception not a subclass of std::exception." << endl;
-  }
-  reduceAll<int, int> (*comm, REDUCE_MIN, lclSuccess, outArg (gblSuccess));
-  TEST_EQUALITY_CONST( gblSuccess, 1 );
-  if (gblSuccess != 1) {
-    Tpetra::Details::gathervPrint (out, errStrm.str (), *comm);
-    return;
+
+  x.putScalar (STS::one ());
+  y.putScalar (STS::zero ());
+  {
+    // Now test alpha != 1 and beta == 0.
+    const Scalar alpha = Teuchos::as<Scalar> (2.0);
+    const Scalar beta = Teuchos::as<Scalar> (0.0);
+
+    if (printToCerr) {
+      cerr << "Creating copies of x and y" << endl;
+    }
+    else {
+      out << "Creating copies of x and y" << endl;
+    }
+    MV x_copy = Tpetra::createCopy (x);
+    MV y_copy = Tpetra::createCopy (y);
+
+    if (printToCerr) {
+      cerr << "Testing apply() for alpha = " << alpha
+           << " and beta = " << beta << endl;
+    }
+    else {
+      out << "Testing apply() for alpha = " << alpha
+          << " and beta = " << beta << endl;
+    }
+    try {
+      // This (alpha = 2, beta = 0, 2 columns) was the #762 case.
+      crsmatrix->apply (x_copy, y_copy, Teuchos::NO_TRANS, alpha, beta);
+      cerr << "apply (alpha = " << alpha << ", beta = " << beta << ") returned!"
+           << endl;
+      // Don't print to 'out' here, since we only care about per-process
+      // printing in this case.
+    }
+    catch (std::exception& e) {
+      lclSuccess = 0;
+      errStrm << "Process " << comm->getRank () << ": CrsMatrix::apply threw "
+        "an exception: " << e.what () << endl;
+    }
+    catch (...) {
+      lclSuccess = 0;
+      errStrm << "Process " << comm->getRank () << ": CrsMatrix::apply threw "
+        "an exception not a subclass of std::exception." << endl;
+    }
+    reduceAll<int, int> (*comm, REDUCE_MIN, lclSuccess, outArg (gblSuccess));
+    TEST_EQUALITY_CONST( gblSuccess, 1 );
+    if (gblSuccess != 1) {
+      Tpetra::Details::gathervPrint (out, errStrm.str (), *comm);
+      return;
+    }
+    else {
+      if (comm->getRank () == 0) {
+        if (printToCerr) {
+          cerr << "Yay, got through the test!" << endl;
+        }
+        else {
+          out << "Yay, got through the test!" << endl;
+        }
+      }
+    }
   }
 }
 
