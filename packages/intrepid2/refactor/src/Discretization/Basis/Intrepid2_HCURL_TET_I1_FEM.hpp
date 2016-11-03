4b0ed52d4f3aaae902af3bdeb7ca0947a77073e1
Kyungjoo Kim <kyukim@sandia.gov>
2016-11-02 20:45:09 -0600

    Intrpid2 - hcurl restructing

--- packages/intrepid2/refactor/src/Discretization/Basis/Intrepid2_HCURL_TET_I1_FEM.hpp
+++ packages/intrepid2/refactor/src/Discretization/Basis/Intrepid2_HCURL_TET_I1_FEM.hpp
@@ -43,171 +43,135 @@
 /** \file   Intrepid_HCURL_TET_I1_FEM.hpp
     \brief  Header file for the Intrepid2::HCURL_TET_I1_FEM class.
     \author Created by P. Bochev, D. Ridzal and K. Peterson.
- */
+            Kokkorized by Kyungjoo Kim
+*/
 
-#ifndef INTREPID2_HCURL_TET_I1_FEM_HPP
-#define INTREPID2_HCURL_TET_I1_FEM_HPP
-#include "Intrepid2_Basis.hpp"
+#ifndef __INTREPID2_HCURL_TET_I1_FEM_HPP__
+#define __INTREPID2_HCURL_TET_I1_FEM_HPP__
+
 
 namespace Intrepid2 {
-  
-/** \class  Intrepid2::Basis_HCURL_TET_I1_FEM
-    \brief  Implementation of the default H(curl)-compatible FEM basis of degree 1 on Tetrahedron cell 
-  
-            Implements Nedelec basis of degree 1 on the reference Tetrahedron cell. The basis has
-            cardinality 6 and spans an INCOMPLETE linear polynomial space. Basis functions are dual 
-            to a unisolvent set of degrees-of-freedom (DoF) defined and enumerated as follows:
-  
-  \verbatim
-  ===================================================================================================
-  |         |           degree-of-freedom-tag table                    |                            |
-  |   DoF   |----------------------------------------------------------|       DoF definition       |
-  | ordinal |  subc dim    | subc ordinal | subc DoF ord |subc num DoF |                            |
-  |=========|==============|==============|==============|=============|============================|
-  |    0    |       1      |       0      |       0      |      1      |  L_0(u) = (u.t)(0.5,0,0)   |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    1    |       1      |       1      |       0      |      1      |  L_1(u) = (u.t)(0.5,0.5,0) |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    2    |       1      |       2      |       0      |      1      |  L_2(u) = (u.t)(0,0.5,0)   |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    3    |       1      |       3      |       0      |      1      |  L_3(u) = (u.t)(0,0,0.5)   |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    4    |       1      |       4      |       0      |      1      |  L_4(u) = (u.t)(0.5,0,0.5) |
-  |---------|--------------|--------------|--------------|-------------|----------------------------|
-  |    5    |       1      |       5      |       0      |      1      |  L_5(u) = (u.t)(0,0.5,0.5) |
-  |=========|==============|==============|==============|=============|============================|
-  |   MAX   |  maxScDim=1  |  maxScOrd=5  |  maxDfOrd=0  |      -      |                            |
-  |=========|==============|==============|==============|=============|============================|
-  \endverbatim
-  
-    \remarks
-    \li     In the DoF functional \f${\bf t}\f$ is an edge tangent. Direction of edge  
-            tangents follows the vertex order of the edges in the cell topology and runs from 
-            edge vertex 0 to edge vertex 1, whereas their length is set equal to the edge length. For 
-            example, edge 4 of all Tetrahedron reference cells has vertex order {1,3}, i.e., its 
-            tangent runs from vertex 1 of the reference Tetrahedron to vertex 3 of that cell. On the 
-            reference Tetrahedron the coordinates of these vertices are (1,0,0) and (0,0,1), respectively. 
-            Therefore, the tangent to edge 4 is (0,0,1) - (1,0,0) = (-1, 0, 1). Because its length
-            already equals edge length, no further rescaling of the edge tangent is needed.
-  
-    \li     The length of the edge tangent equals the edge length. As a result, the DoF functional 
-            is the value of the tangent component of a vector field at the edge midpoint times the 
-            edge length. The resulting basis is equivalent to a basis defined by using the edge 
-            circulation as a DoF functional. Note that edges 0, 2 and 3 of reference Tetrahedron<> 
-            cells have unit lengths and edges 1, 4, and 5 have length Sqrt(2).  
-  
- */
-  
-template<typename ExecSpaceType = void,
-         typename outputValueType = double,
-         typename pointValueType = double>
-class Basis_HCURL_TET_I1_FEM : public Basis<ExecSpaceType,outputValueType,pointValueType> {
-public:
-  typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_1d_host ordinal_type_array_1d_host;
-  typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_2d_host ordinal_type_array_2d_host;
-  typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_3d_host ordinal_type_array_3d_host;
-    
-  template<EOperator opType>
-  struct Serial {
-    template<typename outputValueValueType, class ...outputValueProperties,
-             typename inputPointValueType,  class ...inputPointProperties>
-    KOKKOS_INLINE_FUNCTION
-    static void
-    getValues( /**/  Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
-               const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints );
-    
-  };
 
+  /** \class  Intrepid2::Basis_HCURL_TET_I1_FEM
+      \brief  Implementation of the default H(curl)-compatible FEM basis of degree 1 on Tetrahedron cell
 
-    template<typename outputValueViewType,
-             typename inputPointViewType,
-             EOperator opType>
-    struct Functor {
-      /**/  outputValueViewType _outputValues;
-      const inputPointViewType  _inputPoints;
-      
-      KOKKOS_INLINE_FUNCTION
-      Functor( /**/  outputValueViewType outputValues_,
-               /**/  inputPointViewType  inputPoints_ )
-        : _outputValues(outputValues_), _inputPoints(inputPoints_) {}
-      
-      KOKKOS_INLINE_FUNCTION
-      void operator()(const ordinal_type pt) const {
-        switch (opType) {
-        case OPERATOR_VALUE : {
-          auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
-          const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
-          Serial<opType>::getValues( output, input );
-          break;
-        }
-        case OPERATOR_CURL : {
-          auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
-          const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
-          Serial<opType>::getValues( output, input );
-          break;
-        }
-        default: {
-          INTREPID2_TEST_FOR_ABORT( opType != OPERATOR_VALUE &&
-                                    opType != OPERATOR_CURL,
-                                    ">>> ERROR: (Intrepid2::Basis_HCURL_TET_I1_FEM::Serial::getValues) operator is not supported");
-        }
-        }
-      }
-    };
+      Implements Nedelec basis of degree 1 on the reference Tetrahedron cell. The basis has
+      cardinality 6 and spans an INCOMPLETE linear polynomial space. Basis functions are dual
+      to a unisolvent set of degrees-of-freedom (DoF) defined and enumerated as follows:
 
-    class Internal {
-    private:
-      Basis_HCURL_TET_I1_FEM *obj_;
+      \verbatim
+      ===================================================================================================
+      |         |           degree-of-freedom-tag table                    |                            |
+      |   DoF   |----------------------------------------------------------|       DoF definition       |
+      | ordinal |  subc dim    | subc ordinal | subc DoF ord |subc num DoF |                            |
+      |=========|==============|==============|==============|=============|============================|
+      |    0    |       1      |       0      |       0      |      1      |  L_0(u) = (u.t)(0.5,0,0)   |
+      |---------|--------------|--------------|--------------|-------------|----------------------------|
+      |    1    |       1      |       1      |       0      |      1      |  L_1(u) = (u.t)(0.5,0.5,0) |
+      |---------|--------------|--------------|--------------|-------------|----------------------------|
+      |    2    |       1      |       2      |       0      |      1      |  L_2(u) = (u.t)(0,0.5,0)   |
+      |---------|--------------|--------------|--------------|-------------|----------------------------|
+      |    3    |       1      |       3      |       0      |      1      |  L_3(u) = (u.t)(0,0,0.5)   |
+      |---------|--------------|--------------|--------------|-------------|----------------------------|
+      |    4    |       1      |       4      |       0      |      1      |  L_4(u) = (u.t)(0.5,0,0.5) |
+      |---------|--------------|--------------|--------------|-------------|----------------------------|
+      |    5    |       1      |       5      |       0      |      1      |  L_5(u) = (u.t)(0,0.5,0.5) |
+      |=========|==============|==============|==============|=============|============================|
+      |   MAX   |  maxScDim=1  |  maxScOrd=5  |  maxDfOrd=0  |      -      |                            |
+      |=========|==============|==============|==============|=============|============================|
+      \endverbatim
 
+      \remarks
+      \li     In the DoF functional \f${\bf t}\f$ is an edge tangent. Direction of edge
+      tangents follows the vertex order of the edges in the cell topology and runs from
+      edge vertex 0 to edge vertex 1, whereas their length is set equal to the edge length. For
+      example, edge 4 of all Tetrahedron reference cells has vertex order {1,3}, i.e., its
+      tangent runs from vertex 1 of the reference Tetrahedron to vertex 3 of that cell. On the
+      reference Tetrahedron the coordinates of these vertices are (1,0,0) and (0,0,1), respectively.
+      Therefore, the tangent to edge 4 is (0,0,1) - (1,0,0) = (-1, 0, 1). Because its length
+      already equals edge length, no further rescaling of the edge tangent is needed.
+
+      \li     The length of the edge tangent equals the edge length. As a result, the DoF functional
+      is the value of the tangent component of a vector field at the edge midpoint times the
+      edge length. The resulting basis is equivalent to a basis defined by using the edge
+      circulation as a DoF functional. Note that edges 0, 2 and 3 of reference Tetrahedron<>
+      cells have unit lengths and edges 1, 4, and 5 have length Sqrt(2).
+
+  */
+
+  namespace Impl {
+
+    class Basis_HCURL_TET_I1_FEM {
     public:
-      Internal(Basis_HCURL_TET_I1_FEM *obj)
-        : obj_(obj) {}
+      template<EOperator opType>
+      struct Serial {
+        template<typename outputViewType,
+                 typename inputViewType>
+        KOKKOS_INLINE_FUNCTION
+        static void
+        getValues( /**/  outputViewType output,
+                   const inputViewType input );
 
-      /** \brief  FEM basis evaluation on a <strong>reference Tetrahedron</strong> cell. 
-          
-          Returns values of <var>operatorType</var> acting on FEM basis functions for a set of
-          points in the <strong>reference Tetrahedron</strong> cell. For rank and dimensions of 
-          I/O array arguments see Section \ref basis_md_array_sec .
-          
-          \param  outputValues      [out] - rank-2 or 3 array with the computed basis values
-          \param  inputPoints       [in]  - rank-2 array with dimensions (P,D) containing reference points  
-          \param  operatorType      [in]  - operator applied to basis functions    
-          
-          For rank and dimension specifications of <var>ArrayScalar</var> arguments see \ref basis_array_specs
-      */
-      template<typename outputValueValueType, class ...outputValueProperties,
+      };
+
+      template<typename ExecSpaceType,
+               typename outputValueValueType, class ...outputValueProperties,
                typename inputPointValueType,  class ...inputPointProperties>
-      void
+      static void
       getValues( /**/  Kokkos::DynRankView<outputValueValueType,outputValueProperties...> outputValues,
                  const Kokkos::DynRankView<inputPointValueType, inputPointProperties...>  inputPoints,
-                 const EOperator operatorType  = OPERATOR_VALUE ) const;
-      
-      /** \brief  Returns spatial locations (coordinates) of degrees of freedom on a
-          <strong>reference Triangle</strong>.
-          
-          \param  DofCoords      [out] - array with the coordinates of degrees of freedom,
-          dimensioned (F,D)
-      */
-      template<typename dofCoordValueType, class ...dofCoordProperties>
-      void
-      getDofCoords( Kokkos::DynRankView<dofCoordValueType,dofCoordProperties...> dofCoords ) const;
+                 const EOperator operatorType);
+
+      template<typename outputValueViewType,
+               typename inputPointViewType,
+               EOperator opType>
+      struct Functor {
+        /**/  outputValueViewType _outputValues;
+        const inputPointViewType  _inputPoints;
+
+        KOKKOS_INLINE_FUNCTION
+        Functor( /**/  outputValueViewType outputValues_,
+                 /**/  inputPointViewType  inputPoints_ )
+          : _outputValues(outputValues_), _inputPoints(inputPoints_) {}
+
+        KOKKOS_INLINE_FUNCTION
+        void operator()(const ordinal_type pt) const {
+          switch (opType) {
+          case OPERATOR_VALUE : {
+            auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
+            const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
+            Serial<opType>::getValues( output, input );
+            break;
+          }
+          case OPERATOR_CURL : {
+            auto       output = Kokkos::subdynrankview( _outputValues, Kokkos::ALL(), pt, Kokkos::ALL() );
+            const auto input  = Kokkos::subdynrankview( _inputPoints,                 pt, Kokkos::ALL() );
+            Serial<opType>::getValues( output, input );
+            break;
+          }
+          default: {
+            INTREPID2_TEST_FOR_ABORT( opType != OPERATOR_VALUE &&
+                                      opType != OPERATOR_CURL,
+                                      ">>> ERROR: (Intrepid2::Basis_HCURL_TET_I1_FEM::Serial::getValues) operator is not supported");
+          }
+          }
+        }
+      };
     };
-    Internal impl_;
+  }
+
+  template<typename ExecSpaceType = void,
+           typename outputValueType = double,
+           typename pointValueType = double>
+  class Basis_HCURL_TET_I1_FEM : public Basis<ExecSpaceType,outputValueType,pointValueType> {
+  public:
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_1d_host ordinal_type_array_1d_host;
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_2d_host ordinal_type_array_2d_host;
+    typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::ordinal_type_array_3d_host ordinal_type_array_3d_host;
 
     /** \brief  Constructor.
      */
     Basis_HCURL_TET_I1_FEM();
-    Basis_HCURL_TET_I1_FEM(const Basis_HCURL_TET_I1_FEM &b)
-      : Basis<ExecSpaceType,outputValueType,pointValueType>(b),
-        impl_(this) {}
-
-    Basis_HCURL_TET_I1_FEM& operator=(const Basis_HCURL_TET_I1_FEM &b) {
-      if (this != &b) {
-        Basis<ExecSpaceType,outputValueType,pointValueType>::operator= (b);
-        // do not copy impl
-      }
-      return *this;
-    }
 
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::outputViewType outputViewType;
     typedef typename Basis<ExecSpaceType,outputValueType,pointValueType>::pointViewType  pointViewType;
@@ -218,13 +182,35 @@ public:
     getValues( /**/  outputViewType outputValues,
                const pointViewType  inputPoints,
                const EOperator operatorType = OPERATOR_VALUE ) const {
-      impl_.getValues( outputValues, inputPoints, operatorType );
+#ifdef HAVE_INTREPID2_DEBUG
+      // Verify arguments
+      Intrepid2::getValues_HCURL_Args(outputValues,
+                                      inputPoints,
+                                      operatorType,
+                                      this->getBaseCellTopology(),
+                                      this->getCardinality() );
+#endif
+      Impl::Basis_HCURL_TET_I1_FEM::
+        getValues<ExecSpaceType>( outputValues,
+                                  inputPoints,
+                                  operatorType );
     }
 
     virtual
     void
     getDofCoords( scalarViewType dofCoords ) const {
-      impl_.getDofCoords( dofCoords );
+#ifdef HAVE_INTREPID2_DEBUG
+      // Verify rank of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( dofCoords.rank() != 2, std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HCURL_TET_I1_FEM::getDofCoords) rank = 2 required for dofCoords array");
+      // Verify 0th dimension of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( dofCoords.dimension(0) != this->basisCardinality_, std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HCURL_TET_I1_FEM::getDofCoords) mismatch in number of dof and 0th dimension of dofCoords array");
+      // Verify 1st dimension of output array.
+      INTREPID2_TEST_FOR_EXCEPTION( dofCoords.dimension(1) != this->basisCellTopology_.getDimension(), std::invalid_argument,
+                                    ">>> ERROR: (Intrepid2::Basis_HCURL_TET_I1_FEM::getDofCoords) incorrect reference cell (1st) dimension in dofCoords array");
+#endif
+      Kokkos::deep_copy(dofCoords, this->dofCoords_);
     }
 
     virtual
@@ -239,7 +225,7 @@ public:
       return true;
     }
 
-};
+  };
 }// namespace Intrepid2
 
 #include "Intrepid2_HCURL_TET_I1_FEMDef.hpp"
