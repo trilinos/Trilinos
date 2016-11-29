c3868119cdc5843a698536835693672e220060f1
Roscoe A. Bartlett <rabartl@sandia.gov>
2016-11-28 20:51:45 -0700

    Source /etc/bashrc to get module command (#482)
    Now I am just grasping at straws.  I can't understand why sourcing this in the
    trilinos_ci_server.sh script did not do the trick.

--- cmake/ctest/drivers/sems_ci/single_ci_iter.sh
+++ cmake/ctest/drivers/sems_ci/single_ci_iter.sh
@@ -8,6 +8,7 @@ echo "DRIVER_SCRIPT_DIR = '$DRIVER_SCRIPT_DIR'"
 TRILINOS_DIR=`readlink -f ${DRIVER_SCRIPT_DIR}/../../../..`
 echo "TRILINOS_DIR='${TRILINOS_DIR}'"
 
+source /etc/bashrc
 source $TRILINOS_DIR/cmake/load_ci_sems_dev_env.sh
 
 export CTEST_DASHBOARD_ROOT=$PWD
