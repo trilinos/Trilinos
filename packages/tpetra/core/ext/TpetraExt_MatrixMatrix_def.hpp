29d8d5e10dbcc8cba828d6ca878b18a51fdd8e61
Chris Siefert <csiefer@sandia.gov>
2016-12-06 16:06:38 -0700

    Tpetra: Fixing output errors in MMM timers

--- packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
+++ packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
@@ -1678,7 +1678,7 @@ void KernelWrappers<Scalar,LocalOrdinal,GlobalOrdinal,Kokkos::Compat::KokkosOpen
 #ifdef HAVE_TPETRA_MMM_TIMINGS
   std::string prefix_mmm = std::string("TpetraExt ") + label + std::string(": ");
   using Teuchos::TimeMonitor;
-  Teuchos::RCP<TimeMonitor> MM = rcp(new TimeMonitor(*(TimeMonitor::getNewTimer(prefix_mmm + std::string("MM Newmatrix OpenMP Wrapper")))));
+  Teuchos::RCP<TimeMonitor> MM = rcp(new TimeMonitor(*(TimeMonitor::getNewTimer(prefix_mmm + std::string("MMM Newmatrix OpenMP Wrapper")))));
 #endif
   //  printf("[%d] OpenMP kernel called\n",Aview.origMatrix->getRowMap()->getComm()->getRank());
 
