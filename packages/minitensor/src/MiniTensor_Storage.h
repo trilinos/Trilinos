4ed047ee641efc1d572cac4d2a986442f6f46ae9
Alejandro Mota <amota@sandia.gov>
2016-12-08 09:30:15 -0800

    Fix copyright date (#609)

--- packages/minitensor/src/MiniTensor_Storage.h
+++ packages/minitensor/src/MiniTensor_Storage.h
@@ -2,7 +2,7 @@
 // ************************************************************************
 //
 //                           Intrepid Package
-//                 Copyright (2007) Sandia Corporation
+//                 Copyright (2016) Sandia Corporation
 //
 // Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
 // license for use of this work by or on behalf of the U.S. Government.
