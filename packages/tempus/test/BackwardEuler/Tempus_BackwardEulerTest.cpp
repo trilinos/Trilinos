c6218f8eef210bf45c8a7009c7dcc5cab1727763
Curtis C. Ober <ccober@sandia.gov>
2017-01-19 11:16:03 -0700

    Tempus: Added Backward for van der Pol problem.
    Used a slightly stiffer epsilon (=0.1).
    
    FYI as epsilon decreases, larger timestep sizes get the initial
    xdot wrong and then it of course never recovers.  At some point it
    "jumps" on to the limit cycle, but of course at a time that does
    not match the initial condition.

--- packages/tempus/test/BackwardEuler/Tempus_BackwardEulerTest.cpp
+++ packages/tempus/test/BackwardEuler/Tempus_BackwardEulerTest.cpp
@@ -6,7 +6,8 @@
 #include "Tempus_IntegratorBasic.hpp"
 
 #include "../TestModels/SinCosModel.hpp"
-#include "CDR_Model.hpp"
+#include "../TestModels/CDR_Model.hpp"
+#include "../TestModels/VanDerPolModel.hpp"
 #include "../TestUtils/Tempus_ConvergenceTestUtils.hpp"
 
 #include "Stratimikos_DefaultLinearSolverBuilder.hpp"
@@ -292,6 +293,113 @@ TEUCHOS_UNIT_TEST(BackwardEuler, CDR)
                         static_cast<double>(num_elements);
       const double x_coord = left_end + static_cast<double>(n) * dx;
       ftmp << x_coord << "   " <<  Thyra::get_ele(x,n) << std::endl;
+    }
+    ftmp.close();
+  }
+
+  Teuchos::TimeMonitor::summarize();
+}
+
+// ************************************************************
+// ************************************************************
+TEUCHOS_UNIT_TEST(BackwardEuler, VanDerPol)
+{
+  std::vector<RCP<Thyra::VectorBase<double>>> solutions;
+  std::vector<double> StepSize;
+  std::vector<double> ErrorNorm;
+  const int nTimeStepSizes = 6;
+  double dt = 0.05;
+  double order = 0.0;
+  for (int n=0; n<nTimeStepSizes; n++) {
+
+    // Read params from .xml file
+    RCP<ParameterList> pList =
+      getParametersFromXmlFile("Tempus_BackwardEuler_VanDerPol.xml");
+
+    // Setup the VanDerPolModel
+    RCP<ParameterList> vdpm_pl = sublist(pList, "VanDerPolModel", true);
+    RCP<VanDerPolModel<double> > model =
+      Teuchos::rcp(new VanDerPolModel<double>(vdpm_pl));
+
+    // Set the step size
+    dt /= 2;
+    if (n == nTimeStepSizes-1) dt /= 10.0;
+
+    // Setup the Integrator and reset initial time step
+    RCP<ParameterList> pl = sublist(pList, "Tempus", true);
+    pl->sublist("Demo Integrator")
+       .sublist("Time Step Control").set("Initial Time Step", dt);
+    RCP<Tempus::IntegratorBasic<double> > integrator =
+      Tempus::integratorBasic<double>(pl, model);
+    order = integrator->getStepper()->getOrder();
+
+    // Integrate to timeMax
+    bool integratorStatus = integrator->advanceTime();
+    TEST_ASSERT(integratorStatus)
+
+    // Test if at 'Final Time'
+    double time = integrator->getTime();
+    double timeFinal =pl->sublist("Demo Integrator")
+      .sublist("Time Step Control").get<double>("Final Time");
+    double tol = 100.0 * std::numeric_limits<double>::epsilon();
+    TEST_FLOATING_EQUALITY(time, timeFinal, tol);
+
+    // Store off the final solution and step size
+    auto solution = Thyra::createMember(model->get_x_space());
+    Thyra::copy(*(integrator->getX()),solution.ptr());
+    solutions.push_back(solution);
+    StepSize.push_back(dt);
+
+    // Output finest temporal solution for plotting
+    // This only works for ONE MPI process
+    if ((n == 0) or (n == nTimeStepSizes-1)) {
+      std::string fname = "Tempus_BackwardEuler_VanDerPol-Ref.dat";
+      if (n == 0) fname = "Tempus_BackwardEuler_VanDerPol.dat";
+      std::ofstream ftmp(fname);
+      RCP<SolutionHistory<double> > solutionHistory =
+        integrator->getSolutionHistory();
+      int nStates = solutionHistory->getNumStates();
+      for (int i=0; i<nStates; i++) {
+        RCP<SolutionState<double> > solutionState = (*solutionHistory)[i];
+        RCP<Thyra::VectorBase<double> > x = solutionState->getX();
+        double ttime = solutionState->getTime();
+        ftmp << ttime << "   " << get_ele(*x, 0) << "   " << get_ele(*x, 1)
+             << std::endl;
+      }
+      ftmp.close();
+    }
+  }
+
+  // Calculate the error - use the most temporally refined mesh for
+  // the reference solution.
+  auto ref_solution = solutions[solutions.size()-1];
+  std::vector<double> StepSizeCheck;
+  for (std::size_t i=0; i < (solutions.size()-1); ++i) {
+    auto tmp = solutions[i];
+    Thyra::Vp_StV(tmp.ptr(), -1.0, *ref_solution);
+    const double L2norm = Thyra::norm_2(*tmp);
+    StepSizeCheck.push_back(StepSize[i]);
+    ErrorNorm.push_back(L2norm);
+  }
+
+  // Check the order and intercept
+  double slope = computeLinearRegressionLogLog<double>(StepSizeCheck,ErrorNorm);
+  std::cout << "  Stepper = BackwardEuler" << std::endl;
+  std::cout << "  =========================" << std::endl;
+  std::cout << "  Expected order: " << order << std::endl;
+  std::cout << "  Observed order: " << slope << std::endl;
+  std::cout << "  =========================" << std::endl;
+  TEST_FLOATING_EQUALITY( slope, order, 0.05 );
+  out << "\n\n ** Slope on Backward Euler Method = " << slope
+      << "\n" << std::endl;
+
+  // Write error data
+  {
+    std::ofstream ftmp("Tempus_BackwardEuler_VanDerPol-Error.dat");
+    double error0 = 0.8*ErrorNorm[0];
+    for (std::size_t n = 0; n < StepSizeCheck.size(); n++) {
+      ftmp << StepSizeCheck[n]  << "   " << ErrorNorm[n] << "   "
+           << error0*(StepSize[n]/StepSize[0]) << std::endl;
     }
     ftmp.close();
   }
