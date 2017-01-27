9abecfd3c2502a2c48ce38f06f89299e35674756
Dan Ibanez <dan.a.ibanez@gmail.com>
2017-01-27 13:16:57 -0700

    Stratimikos: allow compilation without Epetra
    allow an optional, not required, dependency
    on Epetra. it seems only the builder header
    needs #ifdef guards.
    since this changes the dependency graph,
    it is protected by
    
    Stratimikos_MAKE_EPETRA_OPTIONAL:BOOL
    
    which must explicitly be set to true
    for the changes in dependency status
    to change.
    this flag should remain until the next
    backwards-compatibility-breaking
    release of Trilinos.

--- packages/stratimikos/cmake/Dependencies.cmake
+++ packages/stratimikos/cmake/Dependencies.cmake
@@ -2,9 +2,15 @@
 # adapters will get enabled by default.  Note that Belos and ML do *not* have
 # a required dependence on Epetra but the Stratimikos Belos and ML adapters
 # need the Thyra/Epetra adapters.
-SET(LIB_REQUIRED_DEP_PACKAGES ThyraEpetraAdapters)
-SET(LIB_OPTIONAL_DEP_PACKAGES Amesos AztecOO Belos Ifpack ML EpetraExt)
-SET(TEST_REQUIRED_DEP_PACKAGES)
+IF (Stratimikos_MAKE_EPETRA_OPTIONAL)
+  SET(LIB_REQUIRED_DEP_PACKAGES)
+  SET(LIB_OPTIONAL_DEP_PACKAGES Amesos AztecOO Belos Ifpack ML EpetraExt ThyraEpetraAdapters)
+  SET(TEST_REQUIRED_DEP_PACKAGES ThyraEpetraAdapters)
+ELSE()
+  SET(LIB_REQUIRED_DEP_PACKAGES ThyraEpetraAdapters)
+  SET(LIB_OPTIONAL_DEP_PACKAGES Amesos AztecOO Belos Ifpack ML EpetraExt)
+  SET(TEST_REQUIRED_DEP_PACKAGES)
+ENDIF()
 SET(TEST_OPTIONAL_DEP_PACKAGES Triutils Ifpack2 ThyraTpetraAdapters)
 SET(LIB_REQUIRED_DEP_TPLS)
 SET(LIB_OPTIONAL_DEP_TPLS)
