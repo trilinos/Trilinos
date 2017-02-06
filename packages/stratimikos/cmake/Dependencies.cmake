a8c7cd1666d720bbb48d13eed3a54f8f0cb5555d
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-02-06 16:31:59 -0700

    Switch to use TRIBITS_PACKAGE_DEFINE_DEPENDENCIES()
    Just much cleaner to read.  All Trilinos packages should be converted over.

--- packages/stratimikos/cmake/Dependencies.cmake
+++ packages/stratimikos/cmake/Dependencies.cmake
@@ -2,13 +2,9 @@
 # adapters will get enabled by default.  Note that Belos and ML do *not* have
 # a required dependence on Epetra but the Stratimikos Belos and ML adapters
 # need the Thyra/Epetra adapters.
-SET(LIB_REQUIRED_DEP_PACKAGES)
-SET(LIB_OPTIONAL_DEP_PACKAGES Amesos AztecOO Belos Ifpack ML EpetraExt ThyraEpetraAdapters)
-SET(TEST_REQUIRED_DEP_PACKAGES ThyraEpetraAdapters)
-SET(TEST_OPTIONAL_DEP_PACKAGES Triutils Ifpack2 ThyraTpetraAdapters)
-SET(LIB_REQUIRED_DEP_TPLS)
-SET(LIB_OPTIONAL_DEP_TPLS)
-SET(TEST_REQUIRED_DEP_TPLS)
-SET(TEST_OPTIONAL_DEP_TPLS)
-
+TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
+  LIB_OPTIONAL_PACKAGES Amesos AztecOO Belos Ifpack ML EpetraExt ThyraEpetraAdapters
+  TEST_REQUIRED_PACKAGES ThyraEpetraAdapters
+  TEST_OPTIONAL_PACKAGES Triutils Ifpack2 ThyraTpetraAdapters
+  )
 # Note: EpetraExt is used by the AztecOO adapters
