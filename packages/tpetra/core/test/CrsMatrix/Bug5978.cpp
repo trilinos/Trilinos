89df42e81fbfa0d06abfd1c207ca12f849a95411
Tim Fuller <tjfulle@sandia.gov>
2017-02-09 16:19:09 -0700

    @trilinos/tpetra: Generalizes Crs Matrix tests that run for GO=int
    Summary: Several tests generalized to take LO,GO,NT as template parameters.
    Addresses: Issue #698
    
    Build/Test Cases Summary
    Enabled Packages: TpetraCore
    Disabled Packages: FEI,PyTrilinos,Moertel,STK,SEACAS,ThreadPool,OptiPack,Rythmos,Intrepid,ROL,Panzer
    0) MPI_RELEASE_DEBUG_SHARED_PT => Test case MPI_RELEASE_DEBUG_SHARED_PT was not run! => Does not affect push readiness! (-1.00 min)
    1) MPI_DEBUG => passed: passed=123,notpassed=0 (3.53 min)
    2) SERIAL_RELEASE => passed: passed=89,notpassed=0 (4.51 min)

---
+++ packages/tpetra/core/test/CrsMatrix/Bug5978.cpp
@@ -0,0 +1,185 @@
+/*
+// @HEADER
+// ***********************************************************************
+//
+//          Tpetra: Templated Linear Algebra Services Package
+//                 Copyright (2008) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+*/
+
+// This test is in response to bug documented in:
+// https://software.sandia.gov/bugzilla/show_bug.cgi?id=5978
+//
+// Since the bugzilla server is not generally visible, a relevant comment from
+// mhoemme describing the comment is the following:
+//
+// Ok, I think I get it.
+//
+// On both processes, the graph starts neither locally nor globally indexed. In
+// your test, only Process 0 calls insertLocalValues(), since Process 1 has zero
+// rows. The very first call to insertLocalValues() on Process 0 calls
+// allocateValues(LocalIndices, GraphNotYetAllocated). This in turn calls
+// allocateIndices(LocalIndices). This marks the graph as locally indexed, and
+// as _not_ globally indexed:
+//
+// indicesAreLocal_  = (lg == LocalIndices);
+// indicesAreGlobal_ = (lg == GlobalIndices);
+//
+// When your test calls fillComplete() on the matrix, indices have only been
+// allocated (i.e., allocateIndices() has been called) only on Process 0, not on
+// Process 1. Thus, in fillComplete, only Process 1 calls
+// allocateValues(GlobalIndices, GraphNotYetAllocated). This sets the locally
+// vs. globally indexed states to the opposite of what they are on Process 0.
+//
+// A comment above this line in fillComplete says "Allocate global, in case we
+// do not have a column Map yet." The problem is that we _do_ have a column Map:
+// your test gives it to the matrix's constructor.
+//
+// mhoemme indicates that the following commits provide fixes (but the commits
+// are not in the Trilinos repository):
+//
+// - a0120d54b64fa0bb4718fd5aa4430c6ad9612512
+// - 210b4176780172382181d05e815be92117d4324f
+//
+
+// Some Macro Magic to ensure that if CUDA and KokkosCompat is enabled
+// only the .cu version of this file is actually compiled
+#include <Tpetra_ConfigDefs.hpp>
+
+#include <Teuchos_UnitTestHarness.hpp>
+#include <Tpetra_ConfigDefs.hpp>
+#include <TpetraCore_ETIHelperMacros.h>
+#include <Tpetra_DefaultPlatform.hpp>
+#include <Tpetra_Map.hpp>
+#include <Tpetra_CrsMatrix.hpp>
+#include <Tpetra_TestingUtilities.hpp>
+
+// @mhoemme: When compiled, this test gives a lot of warnings coming from the
+// @mhoemme: Teuchos unit testing stuff.  I'm assuming because I don't return or call
+// @mhoemme: any kind of assert?  Does that make sense?  I'd like to remove all the
+// @mhoemme: warnings.
+
+namespace { // anonymous
+
+using std::endl;
+using Teuchos::RCP;
+using Teuchos::rcp;
+using Teuchos::Comm;
+using Teuchos::Array;
+using Teuchos::ArrayRCP;
+using Teuchos::OrdinalTraits;
+using Tpetra::Map;
+using Tpetra::CrsMatrix;
+using Tpetra::DefaultPlatform;
+
+TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(CrsMatrix, Bug5978, SC, LO, GO, NT)
+{
+  typedef Map<LO,GO,NT> MapType;
+  typedef CrsMatrix<SC, LO, GO, NT> MatrixType;
+
+  RCP<const Comm<int> > comm = DefaultPlatform::getDefaultPlatform().getComm();
+  const int numProc = comm->getSize ();
+  const int myRank = comm->getRank ();
+
+  if (numProc != 2) {
+    out << "This test must be run with exactly 2 MPI processes, but you ran "
+        << "it with " << numProc << " process" << (numProc != 1 ? "es" : "")
+        << "." << endl;
+    TEUCHOS_TEST_FOR_EXCEPTION(
+      true, std::runtime_error, "This test must be run with exactly "
+      "2 MPI processes, but you ran it with " << numProc << " process"
+      << (numProc != 1 ? "es" : "") << ".");
+  }
+  out << "Proc " << myRank << ": Running test" << endl;
+  comm->barrier ();
+
+  Array<GO> rowGIDs, colGIDs;
+  if (myRank == 0) {
+    rowGIDs.push_back (0);
+    rowGIDs.push_back (1);
+    colGIDs.push_back (0);
+    colGIDs.push_back (1);
+  }
+
+  out << "Proc " << myRank << ": Creating row and column Maps" << endl;
+  comm->barrier ();
+
+  RCP<NT> node = DefaultPlatform::getDefaultPlatform().getNode ();
+  const GO INVALID = OrdinalTraits<GO>::invalid ();
+  RCP<const MapType> rowMap =
+    rcp (new MapType (INVALID, rowGIDs (), 0, comm, node));
+  RCP<const MapType> colMap =
+    rcp (new MapType (INVALID, colGIDs (), 0, comm, node));
+
+  out << "Proc " << myRank << ": Creating matrix" << endl;
+  comm->barrier ();
+
+  ArrayRCP<size_t> count (rowGIDs.size ());
+
+  // mfh 16 Jan 2014: The explicit cast will hopefully prevent gcc
+  // 4.3.3 from confusing the non-template method assign(size_type,
+  // const T&) from the template method assign(Iter, Iter).
+  count.assign (rowGIDs.size (), static_cast<size_t> (1));
+  MatrixType A (rowMap, colMap, count);
+
+  out << "Proc " << myRank << ": Filling matrix" << endl;
+  comm->barrier ();
+
+  Array<LO> column (1);
+  Array<SC> entry (1, 7);
+  for (int i = 0; i < int (rowGIDs.size ()); ++i) {
+    column[0] = i;
+    A.insertLocalValues (i, column (), entry ());
+  }
+
+  out << "Proc " << myRank << ": Calling fillComplete" << endl;
+  comm->barrier ();
+
+  A.fillComplete (colMap, rowMap);
+
+  comm->barrier ();
+  out << "Proc " << myRank << ": Done with test" << endl;
+}
+
+#define UNIT_TEST_GROUP_SC_LO_GO_NO( SC, LO, GO, NT ) \
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(CrsMatrix, Bug5978, SC, LO, GO, NT)
+
+TPETRA_ETI_MANGLING_TYPEDEFS()
+
+TPETRA_INSTANTIATE_SLGN_NO_ORDINAL_SCALAR(UNIT_TEST_GROUP_SC_LO_GO_NO)
+
+} // namespace (anonymous)
--- packages/tpetra/core/test/CrsMatrix/Bug6171.cpp
+++ packages/tpetra/core/test/CrsMatrix/Bug6171.cpp
@@ -1,36 +1,91 @@
+/*
+// @HEADER
+// ***********************************************************************
+//
+//          Tpetra: Templated Linear Algebra Services Package
+//                 Copyright (2008) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+*/
+
+// This bug is described at
+// https://software.sandia.gov/bugzilla/show_bug.cgi?id=6171
+//
+// The relevant description of the problem this test exposed is:
+//
+// Vectors x and y start out containing 1's in all their rows. After a
+//
+//     CrsMatrix->apply(x, y, Teuchos::TRANS, alpha, beta)
+//
+// with alpha = 0 and beta = 1, some of the entries in y become zero when
+// none of these entries should have changed for the stated values of alpha and
+// beta. I've attached an example to demonstrate the problem.
+//
 // Some Macro Magic to ensure that if CUDA and KokkosCompat is enabled
 // only the .cu version of this file is actually compiled
 #include <Tpetra_ConfigDefs.hpp>
-
+#include <Teuchos_UnitTestHarness.hpp>
+#include <TpetraCore_ETIHelperMacros.h>
+#include <Tpetra_Map.hpp>
+#include <Tpetra_CrsMatrix.hpp>
+#include <Tpetra_TestingUtilities.hpp>
 
 #include <iostream>
 #include <vector>
 
-#include <Tpetra_CrsMatrix.hpp>
-#include <Tpetra_DefaultPlatform.hpp>
-#include <Tpetra_Map.hpp>
-#include <Teuchos_GlobalMPISession.hpp>
+namespace { // anonymous
 
-int main (int argc, char *argv[])
+using Teuchos::ArrayView;
+using Teuchos::RCP;
+using Teuchos::rcp;
+using Teuchos::Comm;
+using Teuchos::OrdinalTraits;
+using Tpetra::TestingUtilities::getDefaultComm;
+
+TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(CrsMatrix, Bug6171, SC, LO, GO, NT)
 {
-  using Teuchos::ArrayView;
-  using Teuchos::RCP;
-  using Teuchos::rcp;
 
-  typedef int LO; // Local Ordinal
-  typedef int GO; // Global Ordinal
-  typedef double ST; // data type
-  typedef Teuchos::ScalarTraits<ST>::magnitudeType MT;
+  typedef typename Teuchos::ScalarTraits<SC>::magnitudeType MagnitudeType;
   typedef Tpetra::global_size_t GST; // Map's constructor needs this
 
-  typedef Tpetra::DefaultPlatform::DefaultPlatformType::NodeType  NT;
-  typedef Tpetra::Map<LO,GO,NT>                                   map_type;
-  typedef Tpetra::CrsMatrix<ST,LO,GO,NT>                          crs_matrix_type;
-  typedef Tpetra::Vector<ST,LO,GO,NT>                             vec_type;
+  typedef Tpetra::Map<LO,GO,NT> MapType;
+  typedef Tpetra::CrsMatrix<SC,LO,GO,NT> CrsMatrixType;
+  typedef Tpetra::Vector<SC,LO,GO,NT> VectorType;
 
-  Teuchos::GlobalMPISession mpiSession (&argc, &argv, NULL);
-  RCP<const Teuchos::Comm<int> > comm =
-    Tpetra::DefaultPlatform::getDefaultPlatform ().getComm ();
+  RCP<const Comm<int> > comm = getDefaultComm();
   TEUCHOS_TEST_FOR_EXCEPTION(
     comm->getSize () != 2, std::logic_error,
     "This test must be run with exactly 2 MPI processes.");
@@ -38,8 +93,8 @@ int main (int argc, char *argv[])
   const int myPID = comm->getRank();
 
   std::vector<GO> globalIDs;
-  std::vector< std::vector<LO> > indices;
-  std::vector< std::vector<ST> > values;
+  std::vector< std::vector<GO> > indices;
+  std::vector< std::vector<SC> > values;
   if (myPID == 0) {
     globalIDs.push_back(0);
     globalIDs.push_back(1);
@@ -64,32 +119,32 @@ int main (int argc, char *argv[])
     indices[1].push_back(2); indices[1].push_back(4);
   }
 
-  const GST INVALID = Teuchos::OrdinalTraits<GST>::invalid ();
+  const GST INVALID = OrdinalTraits<GST>::invalid ();
   const GO indexBase = 0;
-  RCP<const map_type> rowMap =
-    rcp (new map_type (INVALID, ArrayView<GO> (globalIDs), indexBase, comm));
+  RCP<const MapType> rowMap =
+    rcp (new MapType (INVALID, ArrayView<GO> (globalIDs), indexBase, comm));
   TEUCHOS_TEST_FOR_EXCEPTION(
     ! rowMap->isOneToOne (), std::logic_error,
     "In this test, the row Map is supposed to be one to one.");
 
-  RCP<crs_matrix_type> matrix = rcp (new crs_matrix_type (rowMap, 0));
+  RCP<CrsMatrixType> matrix = rcp (new CrsMatrixType (rowMap, 0));
   for (size_t i = 0; i < static_cast<size_t> (globalIDs.size ()); ++i) {
     matrix->insertGlobalValues (globalIDs[i],
                                 ArrayView<const GO> (indices[i]),
-                                ArrayView<const ST> (values[i]));
+                                ArrayView<const SC> (values[i]));
   }
   matrix->fillComplete ();
 
-  vec_type x(rowMap), y(rowMap);
+  VectorType x(rowMap), y(rowMap);
   x.putScalar (1.0);
   y.putScalar (1.0);
-  const MT normBefore = y.norm2 ();
+  const MagnitudeType normBefore = y.norm2 ();
   if (myPID == 0) {
     std::cout << "norm of y before matrix->apply = " << normBefore << std::endl;
   }
-  ST alpha (0.0), beta (1.0);
+  SC alpha (0.0), beta (1.0);
   matrix->apply (x, y, Teuchos::TRANS, alpha, beta);
-  const MT normAfter = y.norm2 ();
+  const MagnitudeType normAfter = y.norm2 ();
   if (myPID == 0) {
     std::cout << "norm of y after matrix->apply = " << normAfter << std::endl;
   }
@@ -98,7 +153,13 @@ int main (int argc, char *argv[])
     normBefore != normAfter, std::logic_error,
     "normBefore = " << normBefore << " != normAfter = " << normAfter << ".");
 
-  std::cout << "End Result: TEST PASSED" << std::endl;
-  return EXIT_SUCCESS;
 }
 
+#define UNIT_TEST_GROUP_SC_LO_GO_NO( SC, LO, GO, NT )                   \
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(CrsMatrix, Bug6171, SC, LO, GO, NT)
+
+TPETRA_ETI_MANGLING_TYPEDEFS()
+
+TPETRA_INSTANTIATE_SLGN_NO_ORDINAL_SCALAR(UNIT_TEST_GROUP_SC_LO_GO_NO)
+
+} // anonymous
