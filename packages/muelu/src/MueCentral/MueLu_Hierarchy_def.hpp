9ead1a72ef7e131349fd5d3344970589b2e02d81
Chris Siefert <csiefer@sandia.gov>
2017-03-01 08:59:24 -0700

    MueLu: Code cleanup
    Build/Test Cases Summary
    Enabled Packages: MueLu, TpetraCore
    Enabled all Forward Packages
    0) MPI_RELEASE_DEBUG_SHARED_PT => passed: passed=1039,notpassed=0 (58.63 min)
    Other local commits for this build/test group: 2f7e208

--- packages/muelu/src/MueCentral/MueLu_Hierarchy_def.hpp
+++ packages/muelu/src/MueCentral/MueLu_Hierarchy_def.hpp
@@ -1042,11 +1042,9 @@ namespace MueLu {
 
       if (!A.is_null()) Xpetra::IO<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Write("A_" + toString(i) + suffix + ".m", *A);
       if (!P.is_null()) {
-	Teuchos::rcp_const_cast<CrsGraph>(P->getCrsGraph())->computeGlobalConstants(); 
 	Xpetra::IO<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Write("P_" + toString(i) + suffix + ".m", *P);
       }
       if (!R.is_null()) {
-	Teuchos::rcp_const_cast<CrsGraph>(R->getCrsGraph())->computeGlobalConstants(); 
 	Xpetra::IO<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Write("R_" + toString(i) + suffix + ".m", *R);
       }
     }
