cf85bf3f9e79bf7152b276f52aa20c4c51090b50
gvonwin <gvonwin@sandia.gov>
2017-03-01 17:10:26 -0700

    Changed alias names. Worked on clang, not gcc

--- packages/rol/src/vector/ROL_RieszVector.hpp
+++ packages/rol/src/vector/ROL_RieszVector.hpp
@@ -77,38 +77,38 @@ template <class Real>
 class RieszPrimalVector : public ElementwiseVector<Real> {
 
   template<typename T> using RCP = Teuchos::RCP<T>;
-
-  using Vector = Vector<Real>;
-  using RieszDualVector = RieszDualVector<Real>;
-  using LinearOperator = LinearOperator<Real>;
+ 
+  using V = Vector<Real>;
+  using DualVector = RieszDualVector<Real>;
+  using OP = LinearOperator<Real>;
 
 private:
 
-  const   RCP<Vector>          v_;
-  mutable RCP<RieszDualVector> dual_;
-  const   RCP<LinearOperator>  op_;
-  mutable Real                 tol_;
+  const   RCP<V>          v_;
+  mutable RCP<DualVector> dual_;
+  const   RCP<OP>         op_;
+  mutable Real            tol_;
 
   mutable bool isDualInitialized_;
 
   void initialize_dual( void ) const {
 
-    dual_ = Teuchos::rcp( new RieszDualVector(v_->clone(),op_,tol_) );   
+    dual_ = Teuchos::rcp( new DualVector(v_->clone(),op_,tol_) );   
     op_->apply(*(dual_->getVector()),*v_,tol_);
     isDualInitialized_ = true;
   }
 
 public:
 
-  RieszPrimalVector( const RCP<Vector>  &v, 
-                     const RCP<LinearOperator> &op,
+  RieszPrimalVector( const RCP<V>  &v, 
+                     const RCP<OP> &op,
                      Real tol=std::sqrt(ROL_EPSILON<Real>()) ) : 
     v_(v), op_(op), tol_(tol), isDualInitialized_(false) {  
   }   
 
   virtual ~RieszPrimalVector() {}
  
-  virtual Real dot( const Vector &x ) const {
+  virtual Real dot( const V &x ) const {
     if( !isDualInitialized_ ) {
       initialize_dual();
     }
@@ -117,11 +117,11 @@ public:
     return dual_->getVector()->dot(*(ex.getVector()));    
   }
 
-  virtual RCP<Vector> clone() const {
+  virtual RCP<V> clone() const {
     return Teuchos::rcp( new RieszPrimalVector( v_->clone(), op_, tol_ ) );   
   }
 
-  virtual const Vector & dual() const {
+  virtual const V & dual() const {
     if( !isDualInitialized_ ) {
       initialize_dual();
     }
@@ -132,7 +132,7 @@ public:
     v_->applyUnary(f);
   }
 
-  void applyBinary( const Elementwise::BinaryFunction<Real> &f, const Vector &x ) {
+  void applyBinary( const Elementwise::BinaryFunction<Real> &f, const V &x ) {
     const RieszPrimalVector &ex = Teuchos::dyn_cast<const RieszPrimalVector>(x);
     v_->applyBinary(f,*(ex.getVector()));
   } 
@@ -142,11 +142,11 @@ public:
   }
 
 
-  RCP<Vector> getVector( void ) { 
+  RCP<V> getVector( void ) { 
     return v_;
   }
 
-  RCP<const Vector> getVector( void ) const {
+  RCP<const V> getVector( void ) const {
     return v_;
   }
   
@@ -159,37 +159,37 @@ class RieszDualVector : public ElementwiseVector<Real> {
 
   template<typename T> using RCP = Teuchos::RCP<T>;
 
-  using Vector = Vector<Real>;
-  using RieszPrimalVector = RieszPrimalVector<Real>;
-  using LinearOperator = LinearOperator<Real>;
+  using V = Vector<Real>;
+  using PrimalVector = RieszPrimalVector<Real>;
+  using OP = LinearOperator<Real>;
 
 private:
 
-  const   RCP<Vector>             v_;
-  mutable RCP<RieszPrimalVector>  primal_;
-  const   RCP<LinearOperator>     op_;
-  mutable Real                    tol_;
+  const   RCP<V>            v_;
+  mutable RCP<PrimalVector>  primal_;
+  const   RCP<OP>            op_;
+  mutable Real               tol_;
 
   mutable bool isPrimalInitialized_;
 
   void initialize_primal( void ) const {
 
-    primal_ = Teuchos::rcp( new RieszPrimalVector(v_->clone(),op_,tol_) );   
+    primal_ = Teuchos::rcp( new PrimalVector(v_->clone(),op_,tol_) );   
     op_->applyInverse(*(primal_->getVector()),*v_,tol_);
     isPrimalInitialized_ = true;
   }
 
 public:
 
-  RieszDualVector( const RCP<Vector>  &v, 
-                   const RCP<LinearOperator> &op,
+  RieszDualVector( const RCP<V>  &v, 
+                   const RCP<OP> &op,
                    Real tol=std::sqrt(ROL_EPSILON<Real>()) ) : 
     v_(v), op_(op), tol_(tol), isPrimalInitialized_(false) {  
   }   
 
   virtual ~RieszDualVector() {}
  
-  virtual Real dot( const Vector &x ) const {
+  virtual Real dot( const V &x ) const {
     if( !isPrimalInitialized_ ) {
       initialize_primal();
     }
@@ -198,11 +198,11 @@ public:
     return primal_->getVector()->dot(*(ex.getVector()));    
   }
 
-  virtual RCP<Vector> clone() const {
+  virtual RCP<V> clone() const {
     return Teuchos::rcp( new RieszDualVector( v_->clone(), op_, tol_ ) );   
   }
 
-  virtual const Vector & dual() const {
+  virtual const V & dual() const {
     if( !isPrimalInitialized_ ) {
       initialize_primal();
     }
@@ -213,7 +213,7 @@ public:
     v_->applyUnary(f);
   }
 
-  void applyBinary( const Elementwise::BinaryFunction<Real> &f, const Vector &x ) {
+  void applyBinary( const Elementwise::BinaryFunction<Real> &f, const V &x ) {
     const RieszDualVector &ex = Teuchos::dyn_cast<const RieszDualVector>(x);
     v_->applyBinary(f,*(ex.getVector()));
   } 
@@ -223,16 +223,14 @@ public:
   }
 
 
-  RCP<Vector> getVector( void ) { 
+  RCP<V> getVector( void ) { 
     return v_;
   }
 
-  RCP<const Vector> getVector( void ) const {
+  RCP<const V> getVector( void ) const {
     return v_;
   }
   
-
-
 }; // class RieszDualVector
 
 
