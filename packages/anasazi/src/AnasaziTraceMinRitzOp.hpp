187e6901f73e94d3f8d28d2da9b6f4b8df60934f
William McLendon <wcmclen@sandia.gov>
2017-03-06 15:15:28 -0700

    Added BSD header to Anasazi source files that had no license/copyright header

--- packages/anasazi/src/AnasaziTraceMinRitzOp.hpp
+++ packages/anasazi/src/AnasaziTraceMinRitzOp.hpp
@@ -2,29 +2,45 @@
 // ***********************************************************************
 //
 //                 Anasazi: Block Eigensolvers Package
-//                 Copyright (2004) Sandia Corporation
+//                 Copyright 2004 Sandia Corporation
 //
-// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
-// license for use of this work by or on behalf of the U.S. Government.
+// Under terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
 //
-// This library is free software; you can redistribute it and/or modify
-// it under the terms of the GNU Lesser General Public License as
-// published by the Free Software Foundation; either version 2.1 of the
-// License, or (at your option) any later version.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
 //
-// This library is distributed in the hope that it will be useful, but
-// WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-// Lesser General Public License for more details.
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
 //
-// You should have received a copy of the GNU Lesser General Public
-// License along with this library; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
-// USA
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
 // Questions? Contact Michael A. Heroux (maherou@sandia.gov)
 //
 // ***********************************************************************
 // @HEADER
+//
+//                 Anasazi: Block Eigensolvers Package
+//
 
 /*! \file AnasaziTraceMinRitzOp.hpp
  *  Defines several operators for use with TraceMin
@@ -89,7 +105,7 @@ template <class Scalar, class MV, class OP>
 class TraceMinProjOp
 {
   typedef Anasazi::MultiVecTraits<Scalar,MV>    MVT;
-  const Scalar ONE; 
+  const Scalar ONE;
 
 public:
   // Constructors
@@ -105,7 +121,7 @@ public:
   // Called by MINRES when certain vectors converge
   void removeIndices(const std::vector<int>& indicesToRemove);
 
-private:  
+private:
   Teuchos::Array< Teuchos::RCP<const MV> > projVecs_;
   Teuchos::RCP<Anasazi::MatOrthoManager<Scalar,MV,OP> > orthman_;
   Teuchos::RCP<const OP> B_;
@@ -131,7 +147,7 @@ class TraceMinRitzOp : public TraceMinOp<Scalar,MV,OP>
 
   typedef Anasazi::MultiVecTraits<Scalar,MV>     MVT;
   typedef Anasazi::OperatorTraits<Scalar,MV,OP>  OPT;
-  const Scalar ONE;  
+  const Scalar ONE;
   const Scalar ZERO;
 
 public:
@@ -154,7 +170,7 @@ public:
   void getInnerTol(std::vector<Scalar>& tolerances) const { tolerances = tolerances_; };
 
   void setMaxIts(const int maxits) { maxits_ = maxits; };
-  
+
   int getMaxIts() const { return maxits_; };
 
   // applies A+\sigma B to a vector
@@ -165,7 +181,7 @@ public:
 
   void removeIndices(const std::vector<int>& indicesToRemove);
 
-private:  
+private:
   Teuchos::RCP<const OP> A_, B_;
   Teuchos::RCP<TraceMinRitzOp<Scalar,MV,OP> > Prec_;
 
@@ -208,7 +224,7 @@ public:
 
   void removeIndices(const std::vector<int>& indicesToRemove);
 
-private:  
+private:
   Teuchos::RCP< TraceMinRitzOp<Scalar,MV,OP> > Op_;
   Teuchos::RCP< TraceMinProjOp<Scalar,MV,OP> > projector_;
 
@@ -229,7 +245,7 @@ class TraceMinProjectedPrecOp : public TraceMinOp<Scalar,MV,OP>
 {
   typedef Anasazi::MultiVecTraits<Scalar,MV>     MVT;
   typedef Anasazi::OperatorTraits<Scalar,MV,OP>  OPT;
-  const Scalar ONE; 
+  const Scalar ONE;
 
 public:
   // constructors for standard/generalized EVP
@@ -242,7 +258,7 @@ public:
 
   void removeIndices(const std::vector<int>& indicesToRemove);
 
-private:  
+private:
   Teuchos::RCP<const OP> Op_;
   Teuchos::Array< Teuchos::RCP<const MV> > projVecs_;
 
@@ -264,7 +280,7 @@ class TraceMinProjRitzOpWithPrec : public TraceMinOp<Scalar,MV,OP>
 {
   typedef Anasazi::MultiVecTraits<Scalar,MV>                                MVT;
   typedef Anasazi::OperatorTraits<Scalar,MV,TraceMinRitzOp<Scalar,MV,OP> >  OPT;
-  const Scalar ONE; 
+  const Scalar ONE;
 
 public:
   // constructors for standard/generalized EVP
@@ -279,7 +295,7 @@ public:
 
   void removeIndices(const std::vector<int>& indicesToRemove);
 
-private:  
+private:
   Teuchos::RCP<TraceMinRitzOp<Scalar,MV,OP> > Op_;
   Teuchos::RCP<TraceMinProjectedPrecOp<Scalar,MV,OP> > Prec_;
 
@@ -554,9 +570,9 @@ ZERO(Teuchos::ScalarTraits<Scalar>::zero())
   linSolOp.release();
 
   // TODO: This should support left and right prec
-  if(Prec != Teuchos::null)    
+  if(Prec != Teuchos::null)
     Prec_ = Teuchos::rcp( new TraceMinRitzOp<Scalar,MV,OP>(Prec) );
-  
+
   // create my minres solver
   solver_ = Teuchos::rcp( new PseudoBlockMinres< Scalar,MV,TraceMinRitzOp<Scalar,MV,OP> >(linSolOp,Prec_) );
 }
@@ -647,7 +663,7 @@ void TraceMinRitzOp<Scalar,MV,OP>::ApplyInverse(const MV& X, MV& Y)
   solver_->setRHS(rcpX);
 
   // Solve the linear system
-  solver_->solve();  
+  solver_->solve();
 }
 
 
@@ -682,9 +698,9 @@ void TraceMinRitzOp<Scalar,MV,OP>::removeIndices(const std::vector<int>& indices
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////
 template <class Scalar, class MV, class OP>
-TraceMinProjRitzOp<Scalar,MV,OP>::TraceMinProjRitzOp(const Teuchos::RCP<TraceMinRitzOp<Scalar,MV,OP> >& Op, const Teuchos::RCP<const MV> projVecs, const Teuchos::RCP<Anasazi::MatOrthoManager<Scalar,MV,OP> > orthman) 
-{ 
-  Op_ = Op; 
+TraceMinProjRitzOp<Scalar,MV,OP>::TraceMinProjRitzOp(const Teuchos::RCP<TraceMinRitzOp<Scalar,MV,OP> >& Op, const Teuchos::RCP<const MV> projVecs, const Teuchos::RCP<Anasazi::MatOrthoManager<Scalar,MV,OP> > orthman)
+{
+  Op_ = Op;
 
   // Create the projector object
   projector_ = Teuchos::rcp( new TraceMinProjOp<Scalar,MV,OP>(projVecs, Op_->B_, orthman) );
@@ -701,7 +717,7 @@ TraceMinProjRitzOp<Scalar,MV,OP>::TraceMinProjRitzOp(const Teuchos::RCP<TraceMin
 template <class Scalar, class MV, class OP>
 TraceMinProjRitzOp<Scalar,MV,OP>::TraceMinProjRitzOp(const Teuchos::RCP<TraceMinRitzOp<Scalar,MV,OP> >& Op, const Teuchos::RCP<const MV> projVecs, const Teuchos::RCP<Anasazi::MatOrthoManager<Scalar,MV,OP> > orthman, Teuchos::Array<Teuchos::RCP<const MV> > auxVecs)
 {
-  Op_ = Op; 
+  Op_ = Op;
 
   // Create the projector object
   projector_ = Teuchos::rcp( new TraceMinProjOp<Scalar,MV,OP>(projVecs, Op_->B_, orthman, auxVecs) );
@@ -757,7 +773,7 @@ void TraceMinProjRitzOp<Scalar,MV,OP>::ApplyInverse(const MV& X, MV& Y)
   solver_->setRHS(PX);
 
   // Solve the linear system
-  solver_->solve();  
+  solver_->solve();
 }
 
 
@@ -853,9 +869,9 @@ void TraceMinProjRitzOpWithPrec<Scalar,MV,OP>::ApplyInverse(const MV& X, MV& Y)
 
   Teuchos::RCP<MV> rcpY = MVT::CloneViewNonConst(Y,indices);
   Teuchos::RCP<MV> rcpX = MVT::Clone(X,nvecs);
-  
+
   Prec_->Apply(X,*rcpX);
-  
+
   // Create the linear problem
   problem_->setProblem(rcpY,rcpX);
 
@@ -876,7 +892,7 @@ void TraceMinProjRitzOpWithPrec<Scalar,MV,OP>::ApplyInverse(const MV& X, MV& Y)
   solver_->setParameters(pl);
 
   // Solve the linear system
-  solver_->solve();  
+  solver_->solve();
 }
 
 
@@ -934,7 +950,7 @@ ONE (Teuchos::ScalarTraits<Scalar>::one())
 
     MVT::MvScale(*helperMV, dotprods);
   }
-  
+
   projVecs_.push_back(helperMV);
 }
 
@@ -985,26 +1001,26 @@ ONE(Teuchos::ScalarTraits<Scalar>::one())
     nvecs = MVT::GetNumberVecs(*projVecs);
     locProjVecs = MVT::CloneCopy(*projVecs);
   }
-  
+
   Teuchos::RCP<MV> helperMV = MVT::Clone(*projVecs,nvecs);
 
   // Compute Prec \ projVecs
   OPT::Apply(*Op_,*locProjVecs,*helperMV);
-  
+
   // Set the operator for the inner products
   B_ = orthman_->getOp();
   orthman_->setOp(Op_);
 
   // Normalize the vectors such that Y' Prec \ Y = I
   const int rank = orthman_->normalizeMat(*locProjVecs,Teuchos::null,helperMV);
-  
+
   projVecs_.push_back(helperMV);
 
 //  helperMV->describe(*(Teuchos::VerboseObjectBase::getDefaultOStream()),Teuchos::VERB_EXTREME);
 
-  // FIXME (mfh 08 Aug 2014) Write a named exception for this case.  
+  // FIXME (mfh 08 Aug 2014) Write a named exception for this case.
   TEUCHOS_TEST_FOR_EXCEPTION(
-          rank != nvecs, std::runtime_error, 
+          rank != nvecs, std::runtime_error,
     "Belos::TraceMinProjectedPrecOp: constructor: Loss of orthogonality detected.");
 
   orthman_->setOp(Teuchos::null);
@@ -1030,28 +1046,28 @@ void TraceMinProjectedPrecOp<Scalar,MV,OP>::Apply(const MV& X, MV& Y) const
     // Y = M\X - proj proj' X
     int nvecsP = MVT::GetNumberVecs(*projVecs_[0]);
     OPT::Apply(*Op_,X,Y);
-	
+
     Teuchos::RCP< Teuchos::SerialDenseMatrix<int,Scalar> > projX = Teuchos::rcp(new Teuchos::SerialDenseMatrix<int,Scalar>(nvecsP,nvecsX));
 
     MVT::MvTransMv(ONE, *projVecs_[0], X, *projX);
-	
+
     MVT::MvTimesMatAddMv(-ONE, *projVecs_[0], *projX, ONE, Y);
   }
   else
   {
     Teuchos::RCP<MV> MX = MVT::Clone(X,nvecsX);
     OPT::Apply(*Op_,X,*MX);
-	
+
     std::vector<Scalar> dotprods(nvecsX);
     MVT::MvDot(*projVecs_[0], X, dotprods);
-	
+
     Teuchos::RCP<MV> helper = MVT::CloneCopy(*projVecs_[0]);
     MVT::MvScale(*helper, dotprods);
     MVT::MvAddMv(ONE, *MX, -ONE, *helper, Y);
   }
 }
 
-  
+
 template <class Scalar, class MV, class OP>
 void TraceMinProjectedPrecOp<Scalar,MV,OP>::removeIndices(const std::vector<int>& indicesToRemove)
 {
