c150ecfa241e0d80b04cd17efc12b8e55afedb02
William C. McLendon III <wcmclen@sandia.gov>
2017-03-08 17:46:01 -0700

    fixed trailing whitespace issues so that source lines match c414e74 (prior to branch)
    Build/Test Cases Summary
    Enabled Packages: Anasazi
    Disabled Packages: PyTrilinos,Claps,TriKota
    Enabled all Forward Packages
    0) MPI_RELEASE_DEBUG_SHARED_PT => passed: passed=683,notpassed=0 (30.24 min)
    Other local commits for this build/test group: 7b767d1, 187e690, 7aa7167

--- packages/anasazi/src/ModalAnalysisSolvers/stat.h
+++ packages/anasazi/src/ModalAnalysisSolvers/stat.h
@@ -42,8 +42,8 @@ c     %--------------------------------%
 c     | See stat.doc for documentation |
 c     %--------------------------------%
 c
-c\SCCS Information: @(#)
-c FILE: stat.h   SID: 2.2   DATE OF SID: 11/16/95   RELEASE: 2
+c\SCCS Information: @(#) 
+c FILE: stat.h   SID: 2.2   DATE OF SID: 11/16/95   RELEASE: 2 
 c
       real       t0, t1, t2, t3, t4, t5
       save       t0, t1, t2, t3, t4, t5
