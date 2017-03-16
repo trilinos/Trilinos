c761a11ff4210a73b1892a5aae2190730ab71f88
Alejandro Mota <amota@sandia.gov>
2017-03-16 13:41:50 -0700

    MiniTensor, ROL: Fix error introduced by making some data members private and only accessible through accessors and mutators. Fix error in passing parameter to template that was caught by Clang but not GCC.
    [#1132]
    
    Build/Test Cases Summary
    Enabled Packages: MiniTensor, ROL
    Disabled Packages: PyTrilinos,Claps,TriKota
    Enabled all Forward Packages
    0) MPI_RELEASE_DEBUG_SHARED_PT => passed: passed=271,notpassed=0 (57.27 min)
    Other local commits for this build/test group: f001af9

--- packages/rol/adapters/minitensor/src/sol/ROL_MiniTensor_MiniSolver_Def.hpp
+++ packages/rol/adapters/minitensor/src/sol/ROL_MiniTensor_MiniSolver_Def.hpp
@@ -150,7 +150,8 @@ solve(
 
   initial_value = fn.value(soln);
   previous_value = initial_value;
-  failed = failed || fn.failed;
+  failed = failed || fn.get_failed();
+  if (fn.get_failed() == true) failure_message = fn.get_failure_message();
   initial_norm = minitensor::norm(resi);
 
   // Define algorithm.
@@ -222,7 +223,8 @@ solve(
 
   initial_value = fn.value(soln);
   previous_value = initial_value;
-  failed = failed || fn.failed;
+  failed = failed || fn.get_failed();
+  if (fn.get_failed() == true) failure_message = fn.get_failure_message();
   initial_norm = minitensor::norm(resi);
 
   // Define algorithm.
@@ -230,7 +232,7 @@ solve(
   obj(fn);
 
   // Define constraint
-  using MTCONSTR = typename std::conditional<eic.IS_EQUALITY,
+  using MTCONSTR = typename std::conditional<EIC::IS_EQUALITY,
       ROL::MiniTensor_EqualityConstraint<EIC, T, NC, N>,
       ROL::MiniTensor_InequalityConstraint<EIC, T, NC, N>>::type;
 
@@ -298,7 +300,8 @@ solve(
 
   initial_value = fn.value(soln);
   previous_value = initial_value;
-  failed = failed || fn.failed;
+  failed = failed || fn.get_failed();
+  if (fn.get_failed() == true) failure_message = fn.get_failure_message();
   initial_norm = minitensor::norm(resi);
 
   // Define algorithm.
@@ -316,7 +319,7 @@ solve(
   bound_constr(lo, hi);
 
   // Define equality/inequality constraint
-  using MTCONSTR = typename std::conditional<eic.IS_EQUALITY,
+  using MTCONSTR = typename std::conditional<EIC::IS_EQUALITY,
       ROL::MiniTensor_EqualityConstraint<EIC, T, NC, N>,
       ROL::MiniTensor_InequalityConstraint<EIC, T, NC, N>>::type;
 
