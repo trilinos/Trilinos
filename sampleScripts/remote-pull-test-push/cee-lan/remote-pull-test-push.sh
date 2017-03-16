dfa989f81e6ba4f7704f4956384beff6a5106f8d
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-03-16 16:24:18 -0600

    Generalize and improve remote pull/test/push server (#482)

--- sampleScripts/remote-pull-test-push/cee-lan/remote-pull-test-push.sh
+++ sampleScripts/remote-pull-test-push/cee-lan/remote-pull-test-push.sh
@@ -1,37 +1,41 @@
 #!/bin/bash -e
 #
 # Invoke a remote pull/test/push from the client machine (see below) using the
-# remote server <remote_pull_test_push_server>.
+# remote server <remote_pull_test_push_server> with Trilinos soruce tree on
+# the remote at <remote_trilinos_base_dir>/Trilinos.
 #
 # Call as:
 #
-#  cd <base_dir>/
-#  ./Trilinos/sampleScripts/remote-pull-test-push/cee-lan/remote-pull-test-push.sh \
-#     <remote_pull_test_push_server>
+#  $ cd <local_trilinos_base_dir>/
+#  $ ./Trilinos/sampleScripts/remote-pull-test-push/cee-lan/remote-pull-test-push.sh \
+#       <remote_pull_test_push_server>  <remote_trilinos_base_dir>
 #
-# where <base_dir> is the base directory that the Trilinos git repos exists under:
+# where <local_trilinos_base_dir> is the base directory for Trilinos on the
+# local machine (i.e. <local_trilinos_base_dir>/Trilinos).
 #
-#  <base_dir>/
-#    Trilinos/
-#
+ 
+echo
+echo "**************************************************************************"
+echo "*** Running remote pull/test/push using server $remote_pull_test_push_server"
+echo
 
-# Get constants and inputs
-
-basedir=$PWD
-
-remote_pull_test_push_sever_base_dir=/scratch/$USER/TRILINOS_PUSH_SERVER
+local_trilinos_base_dir=$PWD
 
 remote_pull_test_push_server=$1
 if [ "$remote_pull_test_push_server" == "" ] ; then
   echo "Error: Must pass in remote_pull_test_push_server!"
   exit 1
 fi
+echo "Remote server: '$remote_pull_test_push_server'"
 
-echo
-echo "**************************************************************************"
-echo "*** Running remote pull/test/push using server $remote_pull_test_push_server"
+remote_trilinos_base_dir=$2
+if [ "$remote_trilinos_base_dir" == "" ] ; then
+  echo "Error: Must pass in remote_trilinos_base_dir!"
+  exit 2
+fi
+echo "Remote base dir: '$remote_trilinos_base_dir'"
 
-cd $basedir/Trilinos/
+cd $local_trilinos_base_dir/Trilinos/
 local_branch_name=`git rev-parse --abbrev-ref HEAD`
 
 echo
@@ -40,7 +44,7 @@ echo "*** 1) Force push local branch '$local_branch_name' to 'intermediate-repo'
 echo "***"
 echo
 
-cd $basedir/Trilinos/
+cd $local_trilinos_base_dir/Trilinos/
 git push -f intermediate-repo $local_branch_name
 
 echo
@@ -50,20 +54,26 @@ echo "***"
 echo
 
 ssh -q $remote_pull_test_push_server \
-  "cd $remote_pull_test_push_sever_base_dir/Trilinos && git checkout develop && git reset --hard @{u}" 
+  "cd $remote_trilinos_base_dir/Trilinos && git checkout develop && git reset --hard @{u}" 
 
 echo
 echo "***"
-echo "*** 3) Doing non-blocking remote pull/tets/push on $remote_pull_test_push_server ...  See log file checkin-test-remote.log!"
+echo "*** 3) Doing non-blocking remote pull/tets/push on '$remote_pull_test_push_server' (se log file checkin-test-remote.log)"
 echo "***"
 
 
 # E) Run remote checkin-test.py script non-blocking 
 
-cd $basedir
+cd $local_trilinos_base_dir
 
 ssh -q $remote_pull_test_push_server \
-  "cd $remote_pull_test_push_sever_base_dir/Trilinos/CHECKIN && ./checkin-test-sems.sh --extra-pull-from=intermediate-repo:$local_branch_name --do-all --no-rebase --push" &> checkin-test-remote.log &
+  "cd $remote_trilinos_base_dir/Trilinos/CHECKIN && ./checkin-test-sems.sh --extra-pull-from=intermediate-repo:$local_branch_name --do-all --no-rebase --push" \
+  &> checkin-test-remote.log \
+  && echo && echo && echo "***" \
+  && echo "*** Final pull/test/push result from '$remote_pull_test_push_server':" \
+  && echo "***" && echo \
+  && tail -n10 checkin-test-remote.log &
 
 echo
-echo "You may now keep working on your local machine and wait for email notifications!"
+echo "You may now keep working on your local machine and wait for email notifications!  (final result will be printed when complete)"
+echo
