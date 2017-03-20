277864dd3e25c156d97fe42328d6604f68943bc1
Irina K. Tezaur <ikalash@sandia.gov>
2017-03-20 16:37:40 -0700

    Tempus: creating another test case for Newmark Beta scheme: a 2nd order ODE version of the SinCos example with f = L = 1, a = 0, b = 1 and phi = 0.

--- packages/tempus/test/Newmark/Tempus_NewmarkTest.cpp
+++ packages/tempus/test/Newmark/Tempus_NewmarkTest.cpp
@@ -14,6 +14,7 @@
 #include "Tempus_IntegratorBasic.hpp"
 
 #include "../TestModels/BallParabolicModel.hpp"
+#include "../TestModels/SinCosXdotdotModel.hpp"
 #include "../TestUtils/Tempus_ConvergenceTestUtils.hpp"
 
 #include "Stratimikos_DefaultLinearSolverBuilder.hpp"
@@ -45,7 +46,6 @@ using Tempus::SolutionState;
 
 
 
-// ************************************************************
 // ************************************************************
 TEUCHOS_UNIT_TEST(NewmarkImplicit, BallParabolic)
 {
@@ -132,6 +132,99 @@ TEUCHOS_UNIT_TEST(NewmarkImplicit, BallParabolic)
   TEST_FLOATING_EQUALITY( ErrorNorm[0], 0.0486418, 1.0e-4 );
 
   std::ofstream ftmp("Tempus_Newmark-Error_BallParabolic.dat");
+  double error0 = 0.8*ErrorNorm[0];
+  for (int n=0; n<nTimeStepSizes; n++) {
+    ftmp << StepSize[n]  << "   " << ErrorNorm[n] << "   "
+         << error0*(StepSize[n]/StepSize[0]) << std::endl;
+  }
+  ftmp.close();
+}
+
+// ************************************************************
+TEUCHOS_UNIT_TEST(NewmarkImplicit, SinCos)
+{
+  std::vector<double> StepSize;
+  std::vector<double> ErrorNorm;
+  const int nTimeStepSizes = 7;
+  double order = 0.0;
+  double dt = 0.2;
+  for (int n=0; n<nTimeStepSizes; n++) {
+
+    // Read params from .xml file
+    RCP<ParameterList> pList =
+      getParametersFromXmlFile("Tempus_Newmark_SinCos.xml");
+
+    //std::ofstream ftmp("PL.txt");
+    //pList->print(ftmp);
+    //ftmp.close();
+
+    // Setup the SinCosXdotdotModel
+    RCP<ParameterList> scm_pl = sublist(pList, "SinCosXdotdotModel", true);
+    RCP<SinCosXdotdotModel<double> > model =
+      Teuchos::rcp(new SinCosXdotdotModel<double>(scm_pl));
+
+
+    // Setup the Integrator and reset initial time step
+    RCP<ParameterList> pl = sublist(pList, "Tempus", true);
+    dt /= 2;
+    pl->sublist("Default Integrator")
+       .sublist("Time Step Control").set("Initial Time Step", dt);
+    RCP<Tempus::IntegratorBasic<double> > integrator =
+      Tempus::integratorBasic<double>(pl, model);
+    order = integrator->getStepper()->getOrder();
+
+    // Integrate to timeMax
+    bool integratorStatus = integrator->advanceTime();
+    TEST_ASSERT(integratorStatus)
+
+    // Test if at 'Final Time'
+    double time = integrator->getTime();
+    double timeFinal =pl->sublist("Default Integrator")
+       .sublist("Time Step Control").get<double>("Final Time");
+    TEST_FLOATING_EQUALITY(time, timeFinal, 1.0e-14);
+
+    // Time-integrated solution and the exact solution
+    RCP<Thyra::VectorBase<double> > x = integrator->getX();
+    RCP<const Thyra::VectorBase<double> > x_exact =
+      model->getExactSolution(time).get_x();
+
+    // Plot sample solution and exact solution
+    if (n == 0) {
+      std::ofstream ftmp("Tempus_Newmark_SinCos.dat");
+      RCP<SolutionHistory<double> > solutionHistory =
+        integrator->getSolutionHistory();
+      RCP<const Thyra::VectorBase<double> > x_exact_plot;
+      for (int i=0; i<solutionHistory->getNumStates(); i++) {
+        RCP<SolutionState<double> > solutionState = (*solutionHistory)[i];
+        double time = solutionState->getTime();
+        RCP<Thyra::VectorBase<double> > x_plot = solutionState->getX();
+        x_exact_plot = model->getExactSolution(time).get_x();
+        ftmp << time << "   "
+             << get_ele(*(x_plot), 0) << "   "
+             << get_ele(*(x_exact_plot), 0) << std::endl;
+      }
+      ftmp.close();
+    }
+
+    // Calculate the error
+    RCP<Thyra::VectorBase<double> > xdiff = x->clone_v();
+    Thyra::V_StVpStV(xdiff.ptr(), 1.0, *x_exact, -1.0, *(x));
+    StepSize.push_back(dt);
+    const double L2norm = Thyra::norm_2(*xdiff);
+    ErrorNorm.push_back(L2norm);
+  }
+
+  // Check the order and intercept
+  double slope = computeLinearRegressionLogLog<double>(StepSize, ErrorNorm);
+  std::cout << "  Stepper = Newmark" << std::endl;
+  std::cout << "  =========================" << std::endl;
+  std::cout << "  Expected order: " << order << std::endl;
+  std::cout << "  Observed order: " << slope << std::endl;
+  std::cout << "  =========================" << std::endl;
+  TEST_FLOATING_EQUALITY( slope, order, 0.01 );
+  TEST_FLOATING_EQUALITY( ErrorNorm[0], 0.0486418, 1.0e-4 );
+
+  std::ofstream ftmp("Tempus_Newmark-Error_SinCos.dat");
   double error0 = 0.8*ErrorNorm[0];
   for (int n=0; n<nTimeStepSizes; n++) {
     ftmp << StepSize[n]  << "   " << ErrorNorm[n] << "   "
---
+++ packages/tempus/test/TestModels/SinCosXdotdotModel_impl.hpp
@@ -0,0 +1,359 @@
+// @HEADER
+// ****************************************************************************
+//                Tempus: Copyright (2017) Sandia Corporation
+//
+// Distributed under BSD 3-clause license (See accompanying file Copyright.txt)
+// ****************************************************************************
+// @HEADER
+
+#ifndef TEMPUS_TEST_SINCOSXDOTDOT_MODEL_IMPL_HPP
+#define TEMPUS_TEST_SINCOSXDOTDOT_MODEL_IMPL_HPP
+
+#include "Teuchos_StandardParameterEntryValidators.hpp"
+
+#include "Thyra_DefaultSpmdVectorSpace.hpp"
+#include "Thyra_DetachedVectorView.hpp"
+#include "Thyra_DetachedMultiVectorView.hpp"
+#include "Thyra_DefaultSerialDenseLinearOpWithSolveFactory.hpp"
+#include "Thyra_DefaultMultiVectorLinearOpWithSolve.hpp"
+#include "Thyra_DefaultLinearOpSource.hpp"
+#include "Thyra_VectorStdOps.hpp"
+#include <iostream>
+
+
+namespace Tempus_Test {
+
+template<class Scalar>
+SinCosXdotdotModel<Scalar>::
+SinCosXdotdotModel(Teuchos::RCP<Teuchos::ParameterList> pList_)
+{
+  isInitialized_ = false;
+  out_ = Teuchos::VerboseObjectBase::getDefaultOStream();
+
+  //Set up space and initial guess for solution vector
+  vecLength_ = 1;
+  x_space_ = Thyra::defaultSpmdVectorSpace<Scalar>(vecLength_);
+  x_vec_ = createMember(x_space_);
+  Thyra::put_scalar(b_*std::sin(phi_), x_vec_.ptr());
+  x_dot_vec_ = createMember(x_space_);
+  Thyra::put_scalar(b_*std::cos(phi_), x_dot_vec_.ptr());
+  x_dot_dot_vec_ = createMember(x_space_);
+  Thyra::put_scalar(-b_*std::sin(phi_), x_dot_dot_vec_.ptr());
+
+  //Set up responses
+  numResponses_ = 1;
+  g_space_ = Thyra::defaultSpmdVectorSpace<Scalar>(numResponses_);
+
+  setParameterList(pList_);
+  *out_ << "\n Problem parameters: b, phi = " << b_ << ", " << phi_ << "\n";
+}
+
+template<class Scalar>
+Thyra::ModelEvaluatorBase::InArgs<Scalar>
+SinCosXdotdotModel<Scalar>::
+getExactSolution(double t) const
+{
+  using Thyra::VectorBase;
+  TEUCHOS_TEST_FOR_EXCEPTION( !isInitialized_, std::logic_error,
+      "Error, setupInOutArgs_ must be called first!\n");
+  Thyra::ModelEvaluatorBase::InArgs<Scalar> inArgs = inArgs_;
+  double exact_t = t;
+  inArgs.set_t(exact_t);
+  Teuchos::RCP<VectorBase<Scalar> > exact_x = createMember(x_space_);
+  { // scope to delete DetachedVectorView
+    Thyra::DetachedVectorView<Scalar> exact_x_view(*exact_x);
+    exact_x_view[0] = b_*std::sin(t+phi_);
+  }
+  inArgs.set_x(exact_x);
+  Teuchos::RCP<VectorBase<Scalar> > exact_x_dot = createMember(x_space_);
+  { // scope to delete DetachedVectorView
+    Thyra::DetachedVectorView<Scalar> exact_x_dot_view(*exact_x_dot);
+    exact_x_dot_view[0] = b_*std::cos(t+phi_); 
+  }
+  inArgs.set_x_dot(exact_x_dot);
+  Teuchos::RCP<VectorBase<Scalar> > exact_x_dot_dot = createMember(x_space_);
+  { // scope to delete DetachedVectorView
+    Thyra::DetachedVectorView<Scalar> exact_x_dot_dot_view(*exact_x_dot_dot);
+    exact_x_dot_dot_view[0] = -b_*std::sin(t+phi_); 
+  }
+  inArgs.set_x_dot_dot(exact_x_dot_dot);
+  return(inArgs);
+}
+
+template<class Scalar>
+Teuchos::RCP<const Thyra::VectorSpaceBase<Scalar> >
+SinCosXdotdotModel<Scalar>::
+get_x_space() const
+{
+  return x_space_;
+}
+
+
+template<class Scalar>
+Teuchos::RCP<const Thyra::VectorSpaceBase<Scalar> >
+SinCosXdotdotModel<Scalar>::
+get_f_space() const
+{
+  return x_space_;
+}
+
+
+template<class Scalar>
+Thyra::ModelEvaluatorBase::InArgs<Scalar>
+SinCosXdotdotModel<Scalar>::
+getNominalValues() const
+{
+  TEUCHOS_TEST_FOR_EXCEPTION( !isInitialized_, std::logic_error,
+      "Error, setupInOutArgs_ must be called first!\n");
+  return nominalValues_;
+}
+
+
+template<class Scalar>
+Teuchos::RCP<Thyra::LinearOpWithSolveBase<Scalar> >
+SinCosXdotdotModel<Scalar>::
+create_W() const
+{
+  Teuchos::RCP<const Thyra::LinearOpWithSolveFactoryBase<Scalar> > W_factory = this->get_W_factory();
+  Teuchos::RCP<Thyra::LinearOpBase<Scalar> > matrix = this->create_W_op();
+  Teuchos::RCP<Thyra::MultiVectorBase<Scalar> > matrix_mv = Teuchos::rcp_dynamic_cast<Thyra::MultiVectorBase<Scalar> >(matrix,true);
+  Thyra::DetachedMultiVectorView<Scalar> matrix_view( *matrix_mv );
+  //IKT: is it necessary for W to be non-singular when initialized?
+  matrix_view(0,0) = 1.0;
+  Teuchos::RCP<Thyra::LinearOpWithSolveBase<Scalar> > W =
+    Thyra::linearOpWithSolve<Scalar>(*W_factory, matrix );
+  return W;
+}
+
+
+template<class Scalar>
+Teuchos::RCP<Thyra::LinearOpBase<Scalar> >
+SinCosXdotdotModel<Scalar>::
+create_W_op() const
+{
+  Teuchos::RCP<Thyra::MultiVectorBase<Scalar> > matrix = Thyra::createMembers(x_space_, vecLength_);
+  return(matrix);
+}
+
+
+template<class Scalar>
+Teuchos::RCP<const Thyra::LinearOpWithSolveFactoryBase<Scalar> >
+SinCosXdotdotModel<Scalar>::
+get_W_factory() const
+{
+  Teuchos::RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar> > W_factory =
+    Thyra::defaultSerialDenseLinearOpWithSolveFactory<Scalar>();
+  return W_factory;
+}
+
+
+template<class Scalar>
+Thyra::ModelEvaluatorBase::InArgs<Scalar>
+SinCosXdotdotModel<Scalar>::
+createInArgs() const
+{
+  setupInOutArgs_();
+  return inArgs_;
+}
+
+
+// Private functions overridden from ModelEvaluatorDefaultBase
+
+
+template<class Scalar>
+Thyra::ModelEvaluatorBase::OutArgs<Scalar>
+SinCosXdotdotModel<Scalar>::
+createOutArgsImpl() const
+{
+  setupInOutArgs_();
+  return outArgs_;
+}
+
+
+template<class Scalar>
+void
+SinCosXdotdotModel<Scalar>::
+evalModelImpl(
+  const Thyra::ModelEvaluatorBase::InArgs<Scalar> &inArgs,
+  const Thyra::ModelEvaluatorBase::OutArgs<Scalar> &outArgs
+  ) const
+{
+  using Teuchos::RCP;
+  using Thyra::VectorBase;
+  TEUCHOS_TEST_FOR_EXCEPTION( !isInitialized_, std::logic_error,
+      "Error, setupInOutArgs_ must be called first!\n");
+
+  RCP<const VectorBase<Scalar> > x_in = inArgs.get_x();
+  if (!x_in.get()) {
+    TEUCHOS_TEST_FOR_EXCEPTION(true, std::logic_error,
+      "\n ERROR: SinCosXdotdotModel requires x as InArgs.\n");
+  }
+  Thyra::ConstDetachedVectorView<Scalar> x_in_view( *x_in );
+  //IKT, FIXME: check that subDim() is the write routine to get local length of a Thyra::ConstDetachedVectorView
+  auto myVecLength  = x_in_view.subDim();
+
+  RCP<const VectorBase<Scalar> > x_dot_in = inArgs.get_x_dot();
+  double alpha = inArgs.get_alpha();
+
+  RCP<const VectorBase<Scalar> > x_dotdot_in = inArgs.get_x_dot_dot();
+  double omega = inArgs.get_W_x_dot_dot_coeff();
+
+  //Parse OutArgs
+  RCP<VectorBase<Scalar> > f_out = outArgs.get_f();
+  RCP<VectorBase<Scalar> > g_out = outArgs.get_g(0);
+  const RCP<Thyra::LinearOpBase<Scalar> > W_out = outArgs.get_W_op();
+
+  //Populate residual and Jacobian
+  if (f_out != Teuchos::null) {
+    Thyra::DetachedVectorView<Scalar> f_out_view( *f_out );
+    if (x_dotdot_in != Teuchos::null) {
+      Thyra::ConstDetachedVectorView<Scalar> x_dotdot_in_view( *x_dotdot_in);
+      for (int i=0; i<myVecLength; i++) {
+        f_out_view[i] = x_dotdot_in_view[i];
+      }
+    }
+    if (x_dot_in != Teuchos::null) {
+      Thyra::ConstDetachedVectorView<Scalar> x_dot_in_view( *x_dot_in);
+      for (int i=0; i<myVecLength; i++) {
+        f_out_view[i] += x_dot_in_view[i];
+      }
+    }
+  }
+
+  // Note: W = alpha*df/dxdot + beta*df/dx + omega*df/dxdotdot
+  if (W_out != Teuchos::null) {
+    RCP<Thyra::MultiVectorBase<Scalar> > matrix = Teuchos::rcp_dynamic_cast<Thyra::MultiVectorBase<Scalar> >(W_out,true);
+    Thyra::DetachedMultiVectorView<Scalar> matrix_view( *matrix );
+    if (omega == 0.0) {
+      TEUCHOS_TEST_FOR_EXCEPTION(true, std::logic_error,
+        "\n ERROR: omega = 0 in SinCosXdotdotModel!\n");
+    }
+    matrix_view(0,0) = omega;
+    if (x_dot_in != Teuchos::null) {
+      matrix_view(0,0) += alpha;
+    }
+  }
+
+  //Calculated response(s) g
+  //g = mean value of x
+  if (g_out != Teuchos::null) {
+    Thyra::DetachedVectorView<Scalar> g_out_view(*g_out);
+    g_out_view[0] = Thyra::sum(*x_in)/vecLength_;
+  }
+}
+
+template<class Scalar>
+Teuchos::RCP<const Thyra::VectorSpaceBase<Scalar> >
+SinCosXdotdotModel<Scalar>::
+get_p_space(int l) const
+{
+  TEUCHOS_TEST_FOR_EXCEPTION(true, std::logic_error,
+                     "\n Error!  SinCosXdotdotModel::get_p_space() is not supported!\n"); 
+  return Teuchos::null;
+}
+
+template<class Scalar>
+Teuchos::RCP<const Teuchos::Array<std::string> >
+SinCosXdotdotModel<Scalar>::
+get_p_names(int l) const
+{
+  TEUCHOS_TEST_FOR_EXCEPTION(true, std::logic_error,
+                     "\n Error!  SinCosXdotdotModel::get_p_names() is not supported!\n"); 
+  return Teuchos::null;
+}
+
+template<class Scalar>
+Teuchos::RCP<const Thyra::VectorSpaceBase<Scalar> >
+SinCosXdotdotModel<Scalar>::
+get_g_space(int j) const
+{
+  TEUCHOS_TEST_FOR_EXCEPTION(j != 0, std::logic_error,
+                     "\n Error!  SinCosXdotdotModel::get_g_space() only " <<
+                     " supports 1 parameter vector.  Supplied index l = " <<
+                     j << "\n");
+  return g_space_;
+}
+
+// private
+
+template<class Scalar>
+void
+SinCosXdotdotModel<Scalar>::
+setupInOutArgs_() const
+{
+  if (isInitialized_) return;
+
+  //Set up InArgs
+  Thyra::ModelEvaluatorBase::InArgsSetup<Scalar> inArgs;
+  inArgs.setModelEvalDescription(this->description());
+  inArgs.set_Np(0);
+  inArgs.setSupports(Thyra::ModelEvaluatorBase::IN_ARG_x);
+  inArgs.setSupports(Thyra::ModelEvaluatorBase::IN_ARG_x_dot);
+  inArgs.setSupports(Thyra::ModelEvaluatorBase::IN_ARG_x_dot_dot);
+  inArgs.setSupports(Thyra::ModelEvaluatorBase::IN_ARG_t);
+  inArgs.setSupports(Thyra::ModelEvaluatorBase::IN_ARG_W_x_dot_dot_coeff);
+  inArgs.setSupports(Thyra::ModelEvaluatorBase::IN_ARG_alpha);
+  inArgs.setSupports(Thyra::ModelEvaluatorBase::IN_ARG_beta);
+  inArgs_ = inArgs;
+
+  //Set up OutArgs
+  Thyra::ModelEvaluatorBase::OutArgsSetup<Scalar> outArgs;
+  outArgs.setModelEvalDescription(this->description());
+  outArgs.set_Np_Ng(0, numResponses_);
+
+  outArgs.setSupports(Thyra::ModelEvaluatorBase::OUT_ARG_f);
+  outArgs.setSupports(Thyra::ModelEvaluatorBase::OUT_ARG_W_op);
+  //outArgs.setSupports(OUT_ARG_W,true);
+  //IKT, what is the following supposed to do??
+  //outArgs.set_W_properties( DerivativeProperties(
+  //    DERIV_LINEARITY_UNKNOWN, DERIV_RANK_FULL, true));
+  outArgs_ = outArgs;
+
+  // Set up nominal values
+  nominalValues_ = inArgs_;
+  nominalValues_.set_t(0.0);
+  nominalValues_.set_x(x_vec_);
+  nominalValues_.set_x_dot(x_dot_vec_);
+  nominalValues_.set_x_dot_dot(x_dot_dot_vec_);
+
+  isInitialized_ = true;
+
+}
+
+template<class Scalar>
+void
+SinCosXdotdotModel<Scalar>::
+setParameterList(Teuchos::RCP<Teuchos::ParameterList> const& paramList)
+{
+  using Teuchos::get;
+  using Teuchos::RCP;
+  using Teuchos::ParameterList;
+  RCP<ParameterList> tmpPL = Teuchos::rcp(new ParameterList("SinCosXdotdotModel"));
+  if (paramList != Teuchos::null) tmpPL = paramList;
+  tmpPL->validateParametersAndSetDefaults(*this->getValidParameters());
+  this->setMyParamList(tmpPL);
+  RCP<ParameterList> pl = this->getMyNonconstParamList();
+  b_ = get<Scalar>(*pl,"Coeff b");
+  phi_ = get<Scalar>(*pl,"Coeff phi");
+  setupInOutArgs_();
+}
+
+template<class Scalar>
+Teuchos::RCP<const Teuchos::ParameterList>
+SinCosXdotdotModel<Scalar>::
+getValidParameters() const
+{
+  static Teuchos::RCP<const Teuchos::ParameterList> validPL;
+  if (is_null(validPL)) {
+    Teuchos::RCP<Teuchos::ParameterList> pl = Teuchos::parameterList();
+    validPL = pl;
+    Teuchos::setDoubleParameter(
+        "Coeff b", 1.0, "Coefficient b in model", &*pl);
+    Teuchos::setDoubleParameter(
+        "Coeff phi", 0.0, "Coefficient phi in model", &*pl);
+  }
+  return validPL;
+}
+
+} // namespace Tempus_Test
+#endif // TEMPUS_TEST_SINCOSXDOTDOT_MODEL_IMPL_HPP
