bce0817291147f7e53c7f7ace72410a9ad3dfb71
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-03-22 10:31:02 -0600

    Remove obsolete scripts (#482)
    These are replaced by the single script
    
      cmake/std/sems/remote-pull-test-push.sh
    
    for an easier setup.

--- sampleScripts/users/rabartl/ceerws113-pull-test-push-server/remote-pull-test-push-server.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -e
-#
-# Script that gets called from a client machine using:
-#
-#   ssh -q <remotemachine> "~/remote-pull-test-push-server.sh ..."
-
-cd /scratch/$USER/Trilinos.base/BUILDS/CHECKIN
-./checkin-test-sems.sh --do-all --push "$@"
