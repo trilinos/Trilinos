8eb026ff8fcd712e16c2ee4acfb3ac9f1703ab21
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-03-30 20:46:31 -0600

    Fetch and merge in branch on remote before running checkin-test-sems.sh (#482)
    Turns out that are use cases where you really need to fetch and mege in the
    branch *before* running checkin-test-sems.sh on the remote machine.  For
    example, there was a use case where we needed an updated PackagesList.cmake
    file in order to elevate a package (Tempus) from ST to PT so that the
    checkin-test.py script would enable it.  But if the checking-test.py script
    runs before that change, then it does not see that upgrade from ST to PT and
    therefore will not enable the package, and will not push.
    
    Now the checkin-test-sems.sh script will run on the remote machine seeing all
    of the changes, which means it will run identically to if it were run locally
    (only it is more robust because it will not get tripped up by untracked
    ignored files that could be needed by the build or tests).
    
    These were use cases that I had not considered before but they are important
    use cases for better robustness.

--- cmake/std/sems/remote-pull-test-push.sh
+++ cmake/std/sems/remote-pull-test-push.sh
@@ -49,25 +49,34 @@ git push -f intermediate-repo $local_branch_name:$local_branch_name
 
 echo
 echo "***"
-echo "*** 2) Hard reset the 'develop' branch in Trilinos repo on $remote_pull_test_push_server"
+echo "*** 2) Hard reset the 'develop' branch and merge in the '$local_branch_name' branch in Trilinos repo on $remote_pull_test_push_server"
 echo "***"
 echo
 
 ssh -q $remote_pull_test_push_server \
-  "cd $remote_trilinos_base_dir/Trilinos && git checkout develop && git reset --hard @{u}" 
+  "cd $remote_trilinos_base_dir/Trilinos && git checkout develop && git reset --hard @{u} && git fetch intermediate-repo && git merge --no-ff intermediate-repo/$local_branch_name" 
+
+# NOTE: Above, we do the fetch and merge of the branch before running the
+# checkin-test.py script on the remote machine.  That is needed for some use
+# cases where the checkin-test.py needs to be run given the updated code or it
+# will not have the right result.  For example, when changing a package from
+# ST to PT and modifiying that package in the same push, the checkin-test.py
+# script needs to see the updated PackagesList.cmake file before it runs or it
+# will not allow the enable of the package.  (This actually happened with the
+# Tempus package.)
 
 echo
 echo "***"
-echo "*** 3) Doing non-blocking remote pull/tets/push on '$remote_pull_test_push_server' (se log file checkin-test-remote.log)"
+echo "*** 3) Doing non-blocking remote test/push on '$remote_pull_test_push_server' (see log file checkin-test-remote.log)"
 echo "***"
 
 cd $local_trilinos_base_dir
 
 ssh -q $remote_pull_test_push_server \
-  "cd $remote_trilinos_base_dir/Trilinos/CHECKIN && ./checkin-test-sems.sh --extra-pull-from=intermediate-repo:$local_branch_name --do-all --no-rebase --push" \
+  "cd $remote_trilinos_base_dir/Trilinos/CHECKIN && ./checkin-test-sems.sh --do-all --no-rebase --push" \
   &> checkin-test-remote.log \
   && echo && echo && echo "***" \
-  && echo "*** Final pull/test/push result from '$remote_pull_test_push_server':" \
+  && echo "*** Final test/push results from '$remote_pull_test_push_server':" \
   && echo "***" && echo \
   && tail -n10 checkin-test-remote.log &
 
