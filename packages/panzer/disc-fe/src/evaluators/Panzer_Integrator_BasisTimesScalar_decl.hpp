fe8070fd529b95015a22bf3a2519339a636d7fcc
Jason M. Gates <jmgate@sandia.gov>
2017-04-05 15:54:28 -0600

    Panzer:  Updating Integrator_BasisTimesScalar...
    and Integrator_GradBasisDotVector such that they can now evaluate a
    field (the old way of doing things) or contribute to a field instead
    (the new way).  The main constructors now ensure compile-time argument
    checking, though there are ParameterList constructors for backwards
    compatibility.  New code should not use the ParameterList constructors,
    as they will be going away eventually.

--- packages/panzer/disc-fe/src/evaluators/Panzer_Integrator_BasisTimesScalar_decl.hpp
+++ packages/panzer/disc-fe/src/evaluators/Panzer_Integrator_BasisTimesScalar_decl.hpp
@@ -40,61 +40,255 @@
 // ***********************************************************************
 // @HEADER
 
-#ifndef PANZER_EVALUATOR_BASISTIMESSCALAR_DECL_HPP
-#define PANZER_EVALUATOR_BASISTIMESSCALAR_DECL_HPP
+#ifndef   __Panzer_Integrator_BasisTimesScalar_decl_hpp__
+#define   __Panzer_Integrator_BasisTimesScalar_decl_hpp__
 
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Include Files
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// C++
 #include <string>
-#include "Panzer_Dimension.hpp"
-#include "Phalanx_Evaluator_Macros.hpp"
-#include "Phalanx_MDField.hpp"
+
+// Kokkos
 #include "Kokkos_DynRankView.hpp"
 
-#include "Panzer_Evaluator_Macros.hpp"
+// Panzer
+#include "Panzer_Evaluator_WithBaseImpl.hpp"
+#include "Panzer_EvaluatorStyle.hpp"
 
-namespace panzer {
+// Phalanx
+#include "Phalanx_Evaluator_Derived.hpp"
+#include "Phalanx_MDField.hpp"
 
-/** Computes the integral
-  * \f[
-    \int s(x) \phi(x) \, d x
-    \f]
-  * where \f$\phi\f$is the test function and \f$s\f$ is
-  * the scalar quantity. The parameter list passed into the construction
-  * is formatted as follows
-    \verbatim
-    <ParameterList>
-       <Parameter name="Residual Name" type="string" value=(required)/>
-       <Parameter name="Value Name" type="string" value="(required)"/>
-       <Parameter name="Basis" type="RCP<BasisIRLayout>" value=(required)/>
-       <Parameter name="IR" type="RCP<IntegrationRule>" value="(required)"/>
-       <Parameter name="Multiplier" type="double" value="(required)"/>
-       <Parameter name="Field Multipliers" type="RCP<const std::vector>" value=Null (default)/>
-    </ParameterList>
-    \endverbatim
-  */
-PANZER_EVALUATOR_CLASS(Integrator_BasisTimesScalar)
-  
-  PHX::MDField<ScalarT,Cell,BASIS> residual;
-    
-  PHX::MDField<ScalarT,Cell,IP> scalar;
+namespace panzer
+{
+  /**
+   *  \brief Computes \f$ Ma(x)b(x)\cdots\int s(x)\phi(x)\,dx \f$.
+   *
+   *  Evaluates the integral
+   *  \f[
+        Ma(x)b(x)\cdots\int s(x)\phi(x)\,dx,
+      \f]
+   *  where \f$ M \f$ is some constant, \f$ a(x) \f$, \f$ b(x) \f$, etc., are
+   *  some fields that depend on position, \f$ s \f$ is some scalar function,
+   *  and \f$ \phi \f$ is some basis.
+   */
+  template<typename EvalT, typename Traits>
+  class Integrator_BasisTimesScalar
+    :
+    public panzer::EvaluatorWithBaseImpl<Traits>,
+    public PHX::EvaluatorDerived<EvalT, Traits>
+  {
+    public:
 
-  std::vector<PHX::MDField<ScalarT,Cell,IP> > field_multipliers;
+      /**
+       *  \brief Main Constructor.
+       *
+       *  Creates an `Evaluator` to evaluate the integral
+       *  \f[
+            Ma(x)b(x)\cdots\int s(x)\phi(x)\,dx,
+          \f]
+       *  where \f$ M \f$ is some constant, \f$ a(x) \f$, \f$ b(x) \f$, etc.,
+       *  are some fields that depend on position, \f$ s \f$ is some scalar
+       *  function, and \f$ \phi \f$ is some basis.
+       *
+       *  \param[in] evalStyle  An `enum` declaring the behavior of this
+       *                        `Evaluator`, which is to either:
+       *                        - compute and contribute (`CONTRIBUTES`), or
+       *                        - compute and store (`EVALUATES`).
+       *  \param[in] resName    The name of either the contributed or evaluated
+       *                        field, depending on `evalStyle`.
+       *  \param[in] valName    The name of the scalar value being integrated
+       *                        (\f$ s \f$).
+       *  \param[in] basis      The basis that you'd like to use (\f$ \phi
+                                \f$).
+       *  \param[in] ir         The integration rule that you'd like to use.
+       *  \param[in] multiplier The scalar multiplier out in front of the
+       *                        integral you're computing (\f$ M \f$).  If not
+       *                        specified, this defaults to 1.
+       *  \param[in] fmNames    A list of names of fields that are multipliers
+       *                        out in front of the integral you're computing
+       *                        (\f$ a(x) \f$, \f$ b(x) \f$, etc.).  If not
+       *                        specified, this defaults to an empty `vector`.
+       *
+       *  \throws std::invalid_argument If any of the inputs are invalid.
+       *  \throws std::logic_error      If the `basis` supplied is not a scalar
+       *                                basis.
+       */
+      Integrator_BasisTimesScalar(
+        const panzer::EvaluatorStyle&   evalStyle,
+        const std::string&              resName,
+        const std::string&              valName,
+        const panzer::BasisIRLayout&    basis,
+        const panzer::IntegrationRule&  ir,
+        const double&                   multiplier = 1,
+        const std::vector<std::string>& fmNames    =
+          std::vector<std::string>());
 
-  std::size_t num_nodes;
+      /**
+       *  \brief `ParameterList` Constructor.
+       *
+       *  Creates an `Evaluator` to evaluate the integral
+       *  \f[
+            Ma(x)b(x)\cdots\int s(x)\phi(x)\,dx,
+          \f]
+       *  where \f$ M \f$ is some constant, \f$ a(x) \f$, \f$ b(x) \f$, etc.,
+       *  are some fields that depend on position, \f$ s \f$ is some scalar
+       *  function, and \f$ \phi \f$ is some basis.
+       *
+       *  \note This constructor exists to preserve the older way of creating
+       *        an `Evaluator` with a `ParameterList`; however, it is
+       *        _strongly_ advised that you _not_ use this `ParameterList`
+       *        Constructor, but rather that you favor the Main Constructor
+       *        with its compile-time argument checking instead.
+       *
+       *  \param[in] p A `ParameterList` of the form
+                       \code{.xml}
+                       <ParameterList>
+                         <Parameter name = "Residual Name"     type = "std::string"                         value = (required)    />
+                         <Parameter name = "Value Name"        type = "std::string"                         value = (required)    />
+                         <Parameter name = "Basis"             type = "RCP<panzer::BasisIRLayout>"          value = (required)    />
+                         <Parameter name = "IR"                type = "RCP<panzer::IntegrationRule>"        value = (required)    />
+                         <Parameter name = "Multiplier"        type = "double"                              value = (required)    />
+                         <Parameter name = "Field Multipliers" type = "RCP<const std::vector<std::string>>" value = null (default)/>
+                       </ParameterList>
+                       \endcode
+       *               where
+       *               - "Residual Name" is the name for the term this
+       *                 `Evaluator` is evaluating,
+       *               - "Value Name" is the name of the scalar value being
+       *                 integrated (\f$ s \f$),
+       *               - "Basis" is the basis that you'd like to use (\f$ \phi
+                         \f$),
+       *               - "IR" is the integration rule that you'd like to use,
+       *               - "Multiplier" is the scalar multiplier out in front of
+       *                 the integral you're computing (\f$ M \f$), and
+       *               - "Field Multipliers" is an optional list of names of
+       *                 fields that are multipliers out in front of the
+       *                 integral you're computing (\f$ a(x) \f$, \f$ b(x) \f$,
+       *                 etc.).
+       */
+      Integrator_BasisTimesScalar(
+        const Teuchos::ParameterList& p);
 
-  std::size_t num_qp;
+      /**
+       *  \brief Post-Registration Setup.
+       *
+       *  Sets the number of nodes and quadrature points, sets the basis index,
+       *  sets the contributed field data in the field manager (if applicable),
+       *  and then creates the `tmp_` `Kokkos::View`.
+       *
+       *  \param[in] sd Essentially a list of `Workset`s, which are collections
+       *                of cells (elements) that all live on a single process.
+       *  \param[in] fm The field manager, used in setting the field data for
+       *                the contributed field, if there is one.
+       */
+      void
+      postRegistrationSetup(
+        typename Traits::SetupData sd,
+        PHX::FieldManager<Traits>& fm);
 
-  double multiplier;
+      /**
+       *  \brief Evaluate Fields.
+       *
+       *  This actually performs the integration by looping over cells in the
+       *  `Workset`, and then over bases and integration points on the cell.
+       *
+       *  \param[in] workset The `Workset` on which you're going to do the
+       *                     integration.
+       */
+      void
+      evaluateFields(
+        typename Traits::EvalData workset);
 
-  std::string basis_name;
-  std::size_t basis_index;
+    private:
 
-  Kokkos::DynRankView<ScalarT,PHX::Device> tmp;
+      /**
+       *  \brief Get Valid Parameters.
+       *
+       *  Get all the parameters that we support such that the `ParameterList`
+       *  Constructor can do some validation of the input `ParameterList`.
+       *
+       *  \returns A `ParameterList` with all the valid parameters (keys) in
+       *           it.  The values tied to those keys are meaningless default
+       *           values.
+       */
+      Teuchos::RCP<Teuchos::ParameterList>
+      getValidParameters() const;
 
-private:
-  Teuchos::RCP<Teuchos::ParameterList> getValidParameters() const;
+      /**
+       *  \brief The scalar type.
+       */
+      typedef typename EvalT::ScalarT ScalarT;
 
-PANZER_EVALUATOR_CLASS_END
+      /**
+       *  \brief An `enum` determining the behavior of this `Evaluator`.
+       *
+       *  This `Evaluator` will compute the result of its integration and then:
+       *  - CONTRIBUTES:                contribute it to a specified residual,
+       *                                not saving anything; or
+       *  - EVALUATES:                  save it under a specified name for
+       *                                future use.
+       */
+      const panzer::EvaluatorStyle evalStyle_;
 
-}
+      /**
+       *  \brief A field to which we'll contribute, or in which we'll store,
+       *         the result of computing this integral.
+       */
+      PHX::MDField<ScalarT, panzer::Cell, panzer::BASIS> field_;
 
-#endif
+      /**
+       *  \brief A field representing the scalar function we're integrating
+       *         (\f$ s \f$).
+       */
+      PHX::MDField<const ScalarT, panzer::Cell, panzer::IP> scalar_;
+
+      /**
+       *  \brief The scalar multiplier out in front of the integral (\f$ M
+                 \f$).
+       */
+      double multiplier_;
+
+      /**
+       *  \brief The (possibly empty) list of fields that are multipliers out
+       *         in front of the integral (\f$ a(x) \f$, \f$ b(x) \f$, etc.).
+       */
+      std::vector<PHX::MDField<const ScalarT, panzer::Cell, panzer::IP>>
+      fieldMults_;
+
+      /**
+       *  \brief The number of nodes for each cell.
+       */
+      int numNodes_;
+
+      /**
+       *  \brief The number of quadrature points for each cell.
+       */
+      int numQP_;
+
+      /**
+       *  \brief The name of the basis we're using.
+       */
+      std::string basisName_;
+
+      /**
+       *  \brief The index in the `Workset` bases for our particular
+       *         `BasisIRLayout` name.
+       */
+      std::size_t basisIndex_;
+
+      /**
+       *  \brief A temporary `Kokkos::View` that we'll use in computing the
+       *         result  of this integral.
+       */
+      Kokkos::DynRankView<ScalarT, PHX::Device> tmp_;
+
+  }; // end of class Integrator_BasisTimesScalar
+
+} // end of namespace panzer
+
+#endif // __Panzer_Integrator_BasisTimesScalar_decl_hpp__
--- packages/panzer/disc-fe/src/evaluators/Panzer_Integrator_GradBasisDotVector_decl.hpp
+++ packages/panzer/disc-fe/src/evaluators/Panzer_Integrator_GradBasisDotVector_decl.hpp
@@ -40,42 +40,272 @@
 // ***********************************************************************
 // @HEADER
 
-#ifndef PANZER_EVALUATOR_GRADBASISDOTVECTOR_DECL_HPP
-#define PANZER_EVALUATOR_GRADBASISDOTVECTOR_DECL_HPP
+#ifndef   __Panzer_Integrator_GradBasisDotVector_decl_hpp__
+#define   __Panzer_Integrator_GradBasisDotVector_decl_hpp__
 
+///////////////////////////////////////////////////////////////////////////////
+//
+//  Include Files
+//
+///////////////////////////////////////////////////////////////////////////////
+
+// C++
 #include <string>
-#include "Panzer_Dimension.hpp"
-#include "Phalanx_Evaluator_Macros.hpp"
-#include "Phalanx_MDField.hpp"
+
+// Kokkos
 #include "Kokkos_DynRankView.hpp"
 
-#include "Panzer_Evaluator_Macros.hpp"
+// Panzer
+#include "Panzer_EvaluatorStyle.hpp"
+#include "Panzer_Evaluator_WithBaseImpl.hpp"
 
-namespace panzer {
-    
-PANZER_EVALUATOR_CLASS(Integrator_GradBasisDotVector)
-  
-  PHX::MDField<ScalarT,Cell,BASIS> residual;
-    
-  PHX::MDField<const ScalarT,Cell,IP,Dim> flux;
-    
-  std::vector<PHX::MDField<const ScalarT,Cell,IP> > field_multipliers;
+// Phalanx
+#include "Phalanx_Evaluator_Derived.hpp"
+#include "Phalanx_MDField.hpp"
 
-  std::size_t num_nodes;
+namespace panzer
+{
+  /**
+   *  \brief Computes \f$ Ma(x)b(x)\cdots\int\vec{s}(x)\cdot\nabla\phi(x)\,dx
+             \f$.
+   *
+   *  Evaluates the integral
+   *  \f[
+        Ma(x)b(x)\cdots\int\vec{s}(x)\cdot\nabla\phi(x)\,dx,
+      \f]
+   *  where \f$ M \f$ is some constant, \f$ a(x) \f$, \f$ b(x) \f$, etc., are
+   *  some fields that depend on position, \f$ \vec{s} \f$ is some
+   *  vector-valued function, and \f$ \phi \f$ is some basis.
+   */
+  template<typename EvalT, typename Traits>
+  class Integrator_GradBasisDotVector
+    :
+    public panzer::EvaluatorWithBaseImpl<Traits>,
+    public PHX::EvaluatorDerived<EvalT, Traits>
+  {
+    public:
 
-  std::size_t num_qp;
+      /**
+       *  \brief Main Constructor.
+       *
+       *  Creates an `Evaluator` to evaluate the integral
+       *  \f[
+            Ma(x)b(x)\cdots\int\vec{s}(x)\cdot\nabla\phi(x)\,dx,
+          \f]
+       *  where \f$ M \f$ is some constant, \f$ a(x) \f$, \f$ b(x) \f$, etc.,
+       *  are some fields that depend on position, \f$ \vec{s} \f$ is some
+       *  vector-valued function, and \f$ \phi \f$ is some basis.
+       *
+       *  \param[in] evalStyle  An `enum` declaring the behavior of this
+       *                        `Evaluator`, which is to either:
+       *                        - compute and contribute (`CONTRIBUTES`), or
+       *                        - compute and store (`EVALUATES`).
+       *  \param[in] resName    The name of either the contributed or evaluated
+       *                        field, depending on `evalStyle`.
+       *  \param[in] fluxName   The name of the vector-valued function being
+       *                        integrated (\f$ \vec{s} \f$).
+       *  \param[in] basis      The basis that you'd like to use (\f$ \phi
+                                \f$).
+       *  \param[in] ir         The integration rule that you'd like to use.
+       *  \param[in] multiplier The scalar multiplier out in front of the
+       *                        integral you're computing (\f$ M \f$).  If not
+       *                        specified, this defaults to 1.
+       *  \param[in] fmNames    A list of names of fields that are multipliers
+       *                        out in front of the integral you're computing
+       *                        (\f$ a(x) \f$, \f$ b(x) \f$, etc.).  If not
+       *                        specified, this defaults to an empty `vector`.
+       *  \param[in] vecDL      The vector data layout that you'd like to use.
+       *                        If not specified, this defaults to
+       *                        `Teuchos::null` and the vector data layout from
+       *                        the given `ir` is used.
+       *
+       *  \throws std::invalid_argument If any of the inputs are invalid.
+       *  \throws std::logic_error      If the `basis` supplied does not
+       *                                support the gradient operator, or if
+       *                                the dimension of the space exceeds the
+       *                                dimension of the vector data layout.
+       */
+      Integrator_GradBasisDotVector(
+        const panzer::EvaluatorStyle&        evalStyle,
+        const std::string&                   resName,
+        const std::string&                   fluxName,
+        const panzer::BasisIRLayout&         basis,
+        const panzer::IntegrationRule&       ir,
+        const double&                        multiplier = 1,
+        const std::vector<std::string>&      fmNames    =
+          std::vector<std::string>(),
+        const Teuchos::RCP<PHX::DataLayout>& vecDL      = Teuchos::null);
 
-  std::size_t num_dim;
+      /**
+       *  \brief `ParameterList` Constructor.
+       *
+       *  Creates an `Evaluator` to evaluate the integral
+       *  \f[
+            Ma(x)b(x)\cdots\int\vec{s}(x)\cdot\nabla\phi(x)\,dx,
+          \f]
+       *  where \f$ M \f$ is some constant, \f$ a(x) \f$, \f$ b(x) \f$, etc.,
+       *  are some fields that depend on position, \f$ \vec{s} \f$ is some
+       *  vector-valued function, and \f$ \phi \f$ is some basis.
+       *
+       *  \note This constructor exists to preserve the older way of creating
+       *        an `Evaluator` with a `ParameterList`; however, it is
+       *        _strongly_ advised that you _not_ use this `ParameterList`
+       *        Constructor, but rather that you favor either of the other
+       *        constructors with their compile-time argument checking instead.
+       *
+       *  \param[in] p A `ParameterList` of the form
+                       \code{.xml}
+                       <ParameterList>
+                         <Parameter name = "Residual Name"      type = "std::string"                         value = (required)    />
+                         <Parameter name = "Flux Name"          type = "std::string"                         value = (required)    />
+                         <Parameter name = "Basis"              type = "RCP<panzer::BasisIRLayout>"          value = (required)    />
+                         <Parameter name = "IR"                 type = "RCP<panzer::IntegrationRule>"        value = (required)    />
+                         <Parameter name = "Multiplier"         type = "double"                              value = (required)    />
+                         <Parameter name = "Field Multipliers"  type = "RCP<const std::vector<std::string>>" value = null (default)/>
+                         <Parameter name = "Vector Data Layout" type = "RCP<PHX::DataLayout>"                value = null (default)/>
+                       </ParameterList>
+                       \endcode
+       *               where
+       *               - "Residual Name" is the name for the term this
+       *                 `Evaluator` is evaluating,
+       *               - "Flux Name" is the name of the vector-valued function
+       *                 being integrated (\f$ \vec{s} \f$),
+       *               - "Basis" is the basis that you'd like to use (\f$ \phi
+                         \f$),
+       *               - "IR" is the integration rule that you'd like to use,
+       *               - "Multiplier" is the scalar multiplier out in front of
+       *                 the integral you're computing (\f$ M \f$), and
+       *               - "Field Multipliers" is an optional list of names of
+       *                 fields that are multipliers out in front of the
+       *                 integral you're computing (\f$ a(x) \f$, \f$ b(x) \f$,
+       *                 etc.).
+       *               - "Vector Data Layout" is the vector data layout that
+       *                 you'd like to use.
+       */
+      Integrator_GradBasisDotVector(
+        const Teuchos::ParameterList& p);
 
-  double multiplier;
+      /**
+       *  \brief Post-Registration Setup.
+       *
+       *  Sets the number of nodes and quadrature points, sets the basis index,
+       *  sets the contributed field data in the field manager (if applicable),
+       *  and then creates the `tmp_` `Kokkos::View`.
+       *
+       *  \param[in] sd Essentially a list of `Workset`s, which are collections
+       *                of cells (elements) that all live on a single process.
+       *  \param[in] fm The field manager, used in setting the field data for
+       *                the contributed field, if there is one.
+       */
+      void
+      postRegistrationSetup(
+        typename Traits::SetupData d,
+        PHX::FieldManager<Traits>& fm);
 
-  std::string basis_name;
-  std::size_t basis_index;
+      /**
+       *  \brief Evaluate Fields.
+       *
+       *  This actually performs the integration by looping over cells in the
+       *  `Workset`, then over bases and integration points on the cell, and
+       *  finally over the dimensions of the vector field.
+       *
+       *  \param[in] workset The `Workset` on which you're going to do the
+       *                     integration.
+       */
+      void
+      evaluateFields(
+        typename Traits::EvalData d);
 
-  Kokkos::DynRankView<ScalarT,PHX::Device> tmp;
+    private:
 
-PANZER_EVALUATOR_CLASS_END
+      /**
+       *  \brief Get Valid Parameters.
+       *
+       *  Get all the parameters that we support such that the `ParameterList`
+       *  Constructor can do some validation of the input `ParameterList`.
+       *
+       *  \returns A `ParameterList` with all the valid parameters (keys) in
+       *           it.  The values tied to those keys are meaningless default
+       *           values.
+       */
+      Teuchos::RCP<Teuchos::ParameterList>
+      getValidParameters() const;
 
-}
+      /**
+       *  \brief The scalar type.
+       */
+      typedef typename EvalT::ScalarT ScalarT;
 
-#endif
+      /**
+       *  \brief An `enum` determining the behavior of this `Evaluator`.
+       *
+       *  This `Evaluator` will compute the result of its integration and then:
+       *  - CONTRIBUTES:                contribute it to a specified residual,
+       *                                not saving anything; or
+       *  - EVALUATES:                  save it under a specified name for
+       *                                future use.
+       */
+      const panzer::EvaluatorStyle evalStyle_;
+
+      /**
+       *  \brief A field to which we'll contribute, or in which we'll store,
+       *         the result of computing this integral.
+       */
+      PHX::MDField<ScalarT, panzer::Cell, panzer::BASIS> field_;
+
+      /**
+       *  \brief A field representing the vector-valued function we're
+       *         integrating (\f$ \vec{s} \f$).
+       */
+      PHX::MDField<const ScalarT, panzer::Cell, panzer::IP, panzer::Dim> flux_;
+
+      /**
+       *  \brief The scalar multiplier out in front of the integral (\f$ M
+                 \f$).
+       */
+      double multiplier_;
+
+      /**
+       *  \brief The (possibly empty) list of fields that are multipliers out
+       *         in front of the integral (\f$ a(x) \f$, \f$ b(x) \f$, etc.).
+       */
+      std::vector<PHX::MDField<const ScalarT, panzer::Cell, panzer::IP>>
+      fieldMults_;
+
+      /**
+       *  \brief The number of nodes for each cell.
+       */
+      int numNodes_;
+
+      /**
+       *  \brief The number of quadrature points for each cell.
+       */
+      int numQP_;
+
+      /**
+       *  \brief The number of dimensions in the vector field \f$ \vec{s} \f$.
+       */
+      int numDim_;
+
+      /**
+       *  \brief The name of the basis we're using.
+       */
+      std::string basisName_;
+
+      /**
+       *  \brief The index in the `Workset` bases for our particular
+       *         `BasisIRLayout` name.
+       */
+      std::size_t basisIndex_;
+
+      /**
+       *  \brief A temporary `Kokkos::View` that we'll use in computing the
+       *         result  of this integral.
+       */
+      Kokkos::DynRankView<ScalarT,PHX::Device> tmp;
+
+  }; // end of class Integrator_GradBasisDotVector
+
+} // end of namespace panzer
+
+#endif // __Panzer_Integrator_GradBasisDotVector_decl_hpp__
