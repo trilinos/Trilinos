1534d00638a6bbcd80315c7f87cad53e3dcbed9c
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-04-05 19:20:29 -0600

    Upgrade for TriBITS <Project>_EXTRAREPOS_BRANCH (TriBITSPub/TriBITS#130)
    Need to set <Project>_EXTRAREPOS_BRANCH=master in order to accept next TriBITS
    update which can now checkout the extra repos on a branch.

--- Version.cmake
+++ Version.cmake
@@ -67,4 +67,9 @@ SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
 SET(Trilinos_REPOSITORY_BRANCH "develop" CACHE INTERNAL "")
+SET(Trilinos_EXTRAREPOS_BRANCH "master" CACHE INTERNAL "")
 SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
+
+# NOTE: Above, the extra repos for Trilinos don't have a 'develop' branch yet
+# so you have to run this with 'master'.  But on a release branch, these
+# should be change to all the same branch.
