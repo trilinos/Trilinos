66e7c069ca943a4487f8d2d8b2b9471515c2ccfc
William McLendon <wcmclen@sandia.gov>
2017-04-14 15:37:14 -0600

    cleaning up CMake Scripts

--- cmake/ctest/drivers/rhel6-x86_64/MPI_OPT_GCC_4.9.3/ctest_linux_nightly_mpi_development_debug_gcc_4.9.3_rhel6-x86_64.cmake
+++ cmake/ctest/drivers/rhel6-x86_64/MPI_OPT_GCC_4.9.3/ctest_linux_nightly_mpi_development_debug_gcc_4.9.3_rhel6-x86_64.cmake
@@ -70,11 +70,11 @@ SET(Trilinos_ENABLE_SECONDARY_TESTED_CODE ON)
 
 SET(EXTRA_EXCLUDE_PACKAGES MOOCHO Optika PyTrilinos Didasko)
 
-# 
-# SET(Trilinos_PACKAGES NOX      EpetraExt   TrilinosCouplings 
-#                       Ifpack   Isorropia   AztecOO 
-#                       Belos    Teuchos     Amesos 
-#                       Sacado   Zoltan      Epetra 
+#
+# SET(Trilinos_PACKAGES NOX      EpetraExt   TrilinosCouplings
+#                       Ifpack   Isorropia   AztecOO
+#                       Belos    Teuchos     Amesos
+#                       Sacado   Zoltan      Epetra
 #                       Triutils )
 
 SET(EXTRA_CONFIGURE_OPTIONS
@@ -84,22 +84,22 @@ SET(EXTRA_CONFIGURE_OPTIONS
   "-DTPL_ENABLE_Boost:BOOL=ON"
   "-DTPL_ENABLE_HDF5:BOOL=ON"
   "-DTPL_ENABLE_Netcdf:BOOL=ON"
-  
+
   "-DTPL_ENABLE_SuperLU:BOOL=ON"
-  
+
   "-DTrilinos_SHOW_DEPRECATED_WARNINGS=OFF"
-  
+
   "-DTrilinos_ENABLE_MOOCHO=OFF"
   "-DTrilinos_ENABLE_Optika=OFF"
   "-DTrilinos_ENABLE_Didasko=OFF"
-  
+
   "-DZoltan2_ENABLE_Experimental:BOOL=ON"
-    
+
   "-DCMAKE_CXX_FLAGS:STRING=-Wall -ansi -pedantic -Werror -Wno-unknown-pragmas -Wno-narrowing -Wno-pragmas -Wno-delete-non-virtual-dtor"
   )
 
-# Not enabled in 4.7.2 version from muir
-#  "-DTPL_ENABLE_BoostLib:BOOL=ON"
-#  "-DTPL_ENABLE_Zlib:BOOL=ON"
+#
+# Set the rest of the system-specific options and run the dashboard build/test
+#
 
 TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER()
