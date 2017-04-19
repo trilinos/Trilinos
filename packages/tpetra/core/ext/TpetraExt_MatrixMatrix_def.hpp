ff31d8681f911728338cd11e123da2878ddc5b73
Jonathan Hu <jhu@sandia.gov>
2017-04-19 13:59:01 -0700

    Tpetra: fix space allocation error in SPGEMM
    This is a fix proposed by Steve Thomas (NREL).  Steve discovered
    a bug in the sparse matrix matrix product in the space estimate
    for storage the product of two sparse matrices.  See issue #1248.
    
    Build/Test Cases Summary
    Enabled Packages: TpetraCore
    Disabled Packages: Stokhos,TrilinosCouplings
    Enabled all Forward Packages
    0) MPI_RELEASE_DEBUG_SHARED_PT => FAILED: passed=848,notpassed=1 => Not ready to push! (62.95 min)
    Other local commits for this build/test group: ebbd7f1
    WARNING: Forced the push!

--- packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
+++ packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
@@ -1275,9 +1275,9 @@ size_t C_estimate_nnz(CrsMatrixType & A, CrsMatrixType &B){
   // Follows the NZ estimate in ML's ml_matmatmult.c
   size_t Aest = 100, Best=100;
   if (A.getNodeNumEntries() > 0)
-    Aest = (A.getNodeNumRows() > 0)?  A.getNodeNumEntries()/A.getNodeNumEntries() : 100;
+    Aest = (A.getNodeNumRows() > 0)?  A.getNodeNumEntries()/A.getNodeNumRows() : 100;
   if (B.getNodeNumEntries() > 0)
-    Best = (B.getNodeNumRows() > 0) ? B.getNodeNumEntries()/B.getNodeNumEntries() : 100;
+    Best = (B.getNodeNumRows() > 0) ? B.getNodeNumEntries()/B.getNodeNumRows() : 100;
 
   size_t nnzperrow = (size_t)(sqrt((double)Aest) + sqrt((double)Best) - 1);
   nnzperrow *= nnzperrow;
