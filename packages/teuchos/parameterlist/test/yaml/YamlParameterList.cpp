7615b13140ce7b2d7cd41f0cb81c509481b3406c
Dan Ibanez <daibane@sandia.gov>
2017-04-26 14:24:25 -0600

    Teuchos: second try at first part of #1268

--- packages/teuchos/parameterlist/test/yaml/YamlParameterList.cpp
+++ packages/teuchos/parameterlist/test/yaml/YamlParameterList.cpp
@@ -134,15 +134,15 @@ namespace TeuchosTests
   {
     std::string xmlString =
       "  <ParameterList>\n"
-      "    <ParameterList name=\"Discretization\">\n"
-      "      <Parameter name=\"Node Set Associations\" type=\"TwoDArray(string)\" value=\"2x2:{1, 2, top, bottom}\"/>\n"
-      "    </ParameterList>\n"
       "    <ParameterList name=\"Problem\">\n"
       "      <ParameterList name=\"Neumann BCs\">\n"
       "        <ParameterList name=\"Time Dependent NBC on SS cyl_outside for DOF all set P\">\n"
       "          <Parameter name=\"BC Values\" type=\"TwoDArray(double)\" value=\"3x1:{ 0.0, 10.0, 20.0}\"/>\n"
       "        </ParameterList>\n"
       "      </ParameterList>\n"
+      "    </ParameterList>\n"
+      "    <ParameterList name=\"Discretization\">\n"
+      "      <Parameter name=\"Node Set Associations\" type=\"TwoDArray(string)\" value=\"2x2:{1, 2, top, bottom}\"/>\n"
       "    </ParameterList>\n"
       "  </ParameterList>\n";
     RCP<ParameterList> xmlParams = Teuchos::getParametersFromXmlString(xmlString);
@@ -153,12 +153,12 @@ namespace TeuchosTests
       "%YAML 1.1\n"
       "---\n"
       "ANONYMOUS:\n"
-      "  Discretization: \n"
-      "    Node Set Associations: [['1', '2'], [top, bottom]]\n"
       "  Problem: \n"
       "    Neumann BCs: \n"
       "      Time Dependent NBC on SS cyl_outside for DOF all set P: \n"
       "        BC Values: [[0], [10], [20]]\n"
+      "  Discretization: \n"
+      "    Node Set Associations: [['1', '2'], [top, bottom]]\n"
       "...\n";
     TEST_EQUALITY(yamlString, expectedYamlString);
     std::stringstream yamlInStream(yamlString);
@@ -167,7 +167,14 @@ namespace TeuchosTests
     std::stringstream yamlOutStream2;
     Teuchos::YAMLParameterList::writeYamlStream(yamlOutStream2, *yamlParams);
     std::string yamlString2 = yamlOutStream2.str();
-    TEST_EQUALITY(yamlString2, expectedYamlString);
+  /* There are issues with older versions of yaml-cpp not maintaining the order
+     of parameters in a list. see Trilinos issue #1268.
+     that is why we use the Unordered comparison instead of the pure text one. */
+  //TEST_EQUALITY(yamlString2, expectedYamlString);
+    std::stringstream yamlInStream2(yamlString2);
+    RCP<ParameterList> yamlParams2;
+    yamlParams2 = Teuchos::YAMLParameterList::parseYamlStream(yamlInStream2);
+    TEST_EQUALITY(Teuchos::haveSameValuesUnordered(*yamlParams, *yamlParams2), true);
   }
 } //namespace TeuchosTests
 
