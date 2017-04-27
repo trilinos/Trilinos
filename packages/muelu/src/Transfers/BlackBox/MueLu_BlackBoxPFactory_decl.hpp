cc03cec1d3a2204a0ad37b6b94f2ea8a0a712636
Luc Berger-Vergiat <lberge@sandia.gov>
2017-04-27 13:06:53 -0600

    MueLu: Adding Black Box transfer operators
    I am adding a Black Box factory class to compute the transfer operator.
    So far it only works for symmetric systems, but allows for a variable
    rate of coarsening.

---
+++ packages/muelu/src/Transfers/BlackBox/MueLu_BlackBoxPFactory_decl.hpp
@@ -0,0 +1,160 @@
+// @HEADER
+//
+// ***********************************************************************
+//
+//        MueLu: A package for multigrid based preconditioning
+//                  Copyright 2012 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact
+//                    Jonathan Hu       (jhu@sandia.gov)
+//                    Andrey Prokopenko (aprokop@sandia.gov)
+//                    Ray Tuminaro      (rstumin@sandia.gov)
+//
+// ***********************************************************************
+//
+// @HEADER
+#ifndef MUELU_BLACKBOXPFACTORY_DECL_HPP
+#define MUELU_BLACKBOXPFACTORY_DECL_HPP
+
+#include <Teuchos_SerialDenseVector.hpp>
+
+#include <Xpetra_MultiVector.hpp>
+#include <Xpetra_Matrix_fwd.hpp>
+
+#include "MueLu_ConfigDefs.hpp"
+#include "MueLu_PFactory.hpp"
+#include "MueLu_BlackBoxPFactory_fwd.hpp"
+
+#include "MueLu_Level_fwd.hpp"
+
+namespace MueLuTests {
+  // Forward declaration of friend tester class used to UnitTest BlackBoxPFactory
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+  class BlackBoxPFactoryTester;
+}
+
+namespace MueLu {
+
+/*!
+  @class BlackBoxPFactory
+  @ingroup MueLuTransferClasses
+  @brief Prolongator factory performing geometric coarsening.
+
+  The geometric algorithm assumes the underlying mesh is reasonably structured.
+  Any rate of coarsening can be applied, and the rate is automatically decrease along
+  an edge if the number of element is not divisible by the coarsening rate.
+  The coarsening rate is allowed to be different in all direction which means that
+  semi-coarsening can be achieved within this algorithm in 1 or 2 directions.
+  The main difficulty is to obtain the number of elements/nodes in each directions
+  to identify coarse nodes and fine nodes.
+
+  ## Input/output of BlackBoxPFactory ##
+
+  ### User parameters of SemiCoarsenPFactory ###
+  | Parameter   | type    | default   | master.xml | validated | requested | description                                                                      |
+  | ------------|---------|-----------|:----------:|:---------:|:---------:|----------------------------------------------------------------------------------|
+  | Coarsen     | string  | -1        |            |           |           | A string that specify the coarsening rate, if it is a single character, it will  |
+  |             |         |           |            |           |           | indicate a unique coarsening rate in each direction, if it is longer, it will be | 
+  |             |         |           |            |           |           | processed as a vector with 3 entries, one for each spatial direction             |
+  | A           | Factory | null      |            | *         | *         | Generating factory of the matrix A used during the prolongator smoothing process |
+  | Nullspace   | Factory | null      |            | *         | *         | Generating factory of the nullspace. The GeneralGeometricPFactory provides       |
+  |             |         |           |            |           |           | a coarse version of the given Nullspace.                                         |
+  | Coordinates | Factory | NoFactory |            | *         | *         | Generating factory for coorindates. The coordinates are expected to be provided  |
+  |             |         |           |            |           |           | on the finest level using the NoFactory mechanism. The coordinates are used to   |
+  |             |         |           |            |           |           | compute the coarsening stencil and coarse coordinates are generated for the next |
+  |             |         |           |            |           |           | level.                                                                           |
+
+
+
+  The * in the @c master.xml column denotes that the parameter is defined in the @c master.xml file.<br>
+  The * in the @c validated column means that the parameter is declared in the list of valid input parameters (see BlackBoxPFactory::GetValidParameters).<br>
+  The * in the @c requested column states that the data is requested as input with all dependencies (see BlackBoxPFactory::DeclareInput).
+
+  ### Variables provided by BlackBoxPFactory ###
+
+  After BlackBoxPFactory::Build the following data is available (if requested)
+
+  | Parameter         | generated by             | description
+  |-------------------|--------------------------|------------------------------------------------------------------------------------------------------------------|
+  | P                 | BlackBoxPFactory         | Prolongator                                                                                                      |
+  | Nullspace         | BlackBoxPFactory         | Coarse nullspace (the fine level nullspace information is coarsened using P to generate a coarse version         |
+  |                   |                          | of the nullspace. No scaling is applied.                                                                         |
+  | coarseCoordinates | BlackBoxPFactory         | coarseCoordinates are to be picked up on the coarse level by the coordinates transfer factory and renamed        |
+  |                   |                          | coordinates so that on coarser levels coordinates are available in case another factory needs them.              |
+
+*/
+  template <class Scalar = double, class LocalOrdinal = int, class GlobalOrdinal = LocalOrdinal, class Node = KokkosClassic::DefaultNode::DefaultNodeType>
+  class BlackBoxPFactory : public PFactory {
+#undef MUELU_BLACKBOXPFACTORY_SHORT
+#include "MueLu_UseShortNames.hpp"
+
+  public:
+
+    friend class MueLuTests::BlackBoxPFactoryTester<Scalar, LocalOrdinal, GlobalOrdinal, Node>;
+
+    //! @name Constructors/Destructors.
+    //@{
+
+    //! Constructor
+    BlackBoxPFactory() { }
+
+    //! Destructor.
+    virtual ~BlackBoxPFactory() { }
+    //@}
+
+    RCP<const ParameterList> GetValidParameterList() const;
+
+    //! Input
+    //@{
+
+    void DeclareInput(Level& fineLevel, Level& coarseLevel) const;
+
+    //@}
+
+    //! @name Build methods.
+    //@{
+
+    void Build (Level& fineLevel, Level& coarseLevel) const;
+    void BuildP(Level& fineLevel, Level& coarseLevel) const;
+
+    //@}
+
+  private:
+
+
+  }; //class BlackBoxPFactory
+
+} //namespace MueLu
+
+#define MUELU_BLACKBOXPFACTORY_SHORT
+#endif // MUELU_BLACKBOXPFACTORY_DECL_HPP
---
+++ packages/muelu/src/Transfers/BlackBox/MueLu_BlackBoxPFactory_def.hpp
@@ -0,0 +1,140 @@
+// @HEADER
+//
+// ***********************************************************************
+//
+//        MueLu: A package for multigrid based preconditioning
+//                  Copyright 2012 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact
+//                    Jonathan Hu       (jhu@sandia.gov)
+//                    Andrey Prokopenko (aprokop@sandia.gov)
+//                    Ray Tuminaro      (rstumin@sandia.gov)
+//
+// ***********************************************************************
+//
+// @HEADER
+#ifndef MUELU_BLACKBOXPFACTORY_DEF_HPP
+#define MUELU_BLACKBOXPFACTORY_DEF_HPP
+
+#include <stdlib.h>
+#include <iomanip>
+
+
+// #include <Teuchos_LAPACK.hpp>
+#include <Teuchos_SerialDenseMatrix.hpp>
+#include <Teuchos_SerialDenseVector.hpp>
+#include <Teuchos_SerialDenseSolver.hpp>
+
+#include <Xpetra_CrsMatrixWrap.hpp>
+#include <Xpetra_ImportFactory.hpp>
+#include <Xpetra_Matrix.hpp>
+#include <Xpetra_MapFactory.hpp>
+#include <Xpetra_MultiVectorFactory.hpp>
+#include <Xpetra_VectorFactory.hpp>
+
+#include <Xpetra_IO.hpp>
+
+#include "MueLu_FactoryManagerBase.hpp"
+#include "MueLu_BlackBoxPFactory_decl.hpp"
+
+#include "MueLu_MasterList.hpp"
+#include "MueLu_Monitor.hpp"
+
+namespace MueLu {
+
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+  RCP<const ParameterList> BlackBoxPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::GetValidParameterList() const {
+    RCP<ParameterList> validParamList = rcp(new ParameterList());
+
+    // Coarsen can come in two forms, either a single char that will be interpreted as an integer which is used as the coarsening
+    // rate in every spatial dimentions, or it can be a longer string that will then be interpreted as an array of integers.
+    // By default coarsen is set as "{2}", hence a coarsening rate of 2 in every spatial dimension is the default setting!
+    validParamList->set<std::string >           ("Coarsen",                 "{2}", "Coarsening rate in all spatial dimensions");
+    validParamList->set<RCP<const FactoryBase> >("A",                         Teuchos::null, "Generating factory of the matrix A");
+    validParamList->set<RCP<const FactoryBase> >("Nullspace",                 Teuchos::null, "Generating factory of the nullspace");
+    validParamList->set<RCP<const FactoryBase> >("Coordinates",               Teuchos::null, "Generating factory for coorindates");
+    validParamList->set<RCP<const FactoryBase> >("gNodesPerDim",               Teuchos::null, "Number of nodes per spatial dimmension provided by CoordinatesTransferFactory.");
+    validParamList->set<RCP<const FactoryBase> >("lNodesPerDim",               Teuchos::null, "Number of nodes per spatial dimmension provided by CoordinatesTransferFactory.");
+    validParamList->set<std::string >("axisPermutation",                    "", "Assuming a global (x,y,z) orientation, local might be (z,y,x). This vector gives a permutation from global to local orientation.");
+
+    return validParamList;
+  }
+
+  template <class Scalar,class LocalOrdinal, class GlobalOrdinal, class Node>
+  void BlackBoxPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::DeclareInput(Level& fineLevel, Level& coarseLevel) const {
+    Input(fineLevel, "A");
+    Input(fineLevel, "Nullspace");
+    Input(fineLevel, "Coordinates");
+    // Request the global number of nodes per dimensions
+    if(fineLevel.GetLevelID() == 0) {
+      if(fineLevel.IsAvailable("gNodesPerDim", NoFactory::get())) {
+        fineLevel.DeclareInput("gNodesPerDim", NoFactory::get(), this);
+      } else {
+        TEUCHOS_TEST_FOR_EXCEPTION(fineLevel.IsAvailable("gNodesPerDim", NoFactory::get()),
+                                   Exceptions::RuntimeError,
+                                   "gNodesPerDim was not provided by the user on level0!");
+      }
+    } else {
+      Input(fineLevel, "gNodesPerDim");
+    }
+
+    // Request the local number of nodes per dimensions
+    if(fineLevel.GetLevelID() == 0) {
+      if(fineLevel.IsAvailable("lNodesPerDim", NoFactory::get())) {
+        fineLevel.DeclareInput("lNodesPerDim", NoFactory::get(), this);
+      } else {
+        TEUCHOS_TEST_FOR_EXCEPTION(fineLevel.IsAvailable("lNodesPerDim", NoFactory::get()),
+                                   Exceptions::RuntimeError,
+                                   "lNodesPerDim was not provided by the user on level0!");
+      }
+    } else {
+      Input(fineLevel, "lNodesPerDim");
+    }
+  }
+
+  template <class Scalar,class LocalOrdinal, class GlobalOrdinal, class Node>
+  void BlackBoxPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Build(Level& fineLevel, Level& coarseLevel) const {
+    return BuildP(fineLevel, coarseLevel);
+
+  }
+
+  template <class Scalar,class LocalOrdinal, class GlobalOrdinal, class Node>
+  void BlackBoxPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::BuildP(Level& fineLevel, Level& coarseLevel) const {
+    FactoryMonitor m(*this, "Build", coarseLevel);
+
+  }
+
+} //namespace MueLu
+
+#define MUELU_BLACKBOXPFACTORY_SHORT
+#endif // MUELU_BLACKBOXPFACTORY_DEF_HPP
