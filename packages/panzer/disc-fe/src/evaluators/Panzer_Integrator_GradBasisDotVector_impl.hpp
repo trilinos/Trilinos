5b33a48e543e157882c8f925b3a0ad9f4bfc694e
Matt Bettencourt <mbetten@sandia.gov>
2017-04-28 11:33:28 -0600

    removed auto that was confusing clang

--- packages/panzer/disc-fe/src/evaluators/Panzer_Integrator_GradBasisDotVector_impl.hpp
+++ packages/panzer/disc-fe/src/evaluators/Panzer_Integrator_GradBasisDotVector_impl.hpp
@@ -268,7 +268,7 @@ namespace panzer
     using Teuchos::rcp;
 
     // Create a ParameterList with all the valid keys we support.
-    auto p = rcp(new ParameterList);
+    RCP<ParameterList> p = rcp(new ParameterList);
     p->set<string>("Residual Name", "?");
     p->set<string>("Flux Name", "?");
     RCP<BasisIRLayout> basis;
