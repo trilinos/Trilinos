79ae566d4521231873c45e013f7d0fd26fca9a8b
Tim Fuller <tjfulle@sandia.gov>
2017-05-22 10:08:41 -0600

    MueLu: Fix "pointer being freed was not allocated" error
    On Darwin (Mac OS X 10.11), with gcc 6.3, openmpi 1.10.3 the following runtime
    error occurs when linking against @tpetra/muelu:
    
    Pointer being freed was not allocated
    
    See issue #1347 for more details.
    
    This commit resolves the issue by removing the static member strings
    `defaultParams_` and `stringTemplate_`  from the different Facade classes
    and using their definitions used directly.

--- packages/muelu/src/Interface/FacadeClasses/gen_facade.sh
+++ packages/muelu/src/Interface/FacadeClasses/gen_facade.sh
@@ -11,57 +11,75 @@ function createFiles {
   templatestr=`xsltproc gen_template.xsl ${facade_xml}` 
   echo "The filekenner is " $filekenner
 
+  # Facade class files
+  facade_decl=MueLu_Facade_${filekenner}_decl.hpp
+  facade_def=MueLu_Facade_${filekenner}_def.hpp
+
   # delete old facade class files
-  rm -f MueLu_Facade_${filekenner}_decl.hpp
-  rm -f MueLu_Facade_${filekenner}_def.hpp
+  rm -f ${facade_decl}
+  rm -f ${facade_def}
 
-  # create new (empty) template files
-  cp MueLu_Facade_decl.tmpl MueLu_Facade_${filekenner}_decl.hpp
-  cp MueLu_Facade_def.tmpl MueLu_Facade_${filekenner}_def.hpp
-
-  # fix class names throughout all files
+  # create new (empty) template files and fix class names throughout all files
+  # Inplace use of sed (sed -i) is not supported with BSD sed.
   sedcommandstr=s/XYZNAMEXYZ/${filekenner}/g
-  sed -i $sedcommandstr MueLu_Facade_${filekenner}_decl.hpp
-  sed -i $sedcommandstr MueLu_Facade_${filekenner}_def.hpp
+  sed $sedcommandstr MueLu_Facade_decl.tmpl > ${facade_decl}
+  sed $sedcommandstr MueLu_Facade_def.tmpl > ${facade_def}
 
-  xsltproc gen_logic.xsl ${facade_xml} >> MueLu_Facade_${filekenner}_def.hpp
-  echo '    // end logical code' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '' >> MueLu_Facade_${filekenner}_def.hpp    
-  echo '    // loop over all input parameters' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '    for(Teuchos::ParameterList::ConstIterator it = inputParameters.begin(); it != inputParameters.end(); it++) {' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '      // form replacement string' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '      std::string par_name = inputParameters.name(it);' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '      std::stringstream ss;' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '      ss << "XXX" << par_name << "YYY";' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '      // update final string with parameters' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '      Teuchos::ParameterEntry par_entry = inputParameters.entry(it);' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '      this->ReplaceString(finalString,' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '              ss.str(), Teuchos::toString(par_entry.getAny()));' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '    }' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '    Teuchos::RCP<ParameterList> ret = Teuchos::getParametersFromXmlString(finalString);' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '    return ret;' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '  }' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '  // Note all parameters are of type string (we use it for string replacement)' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '  const std::string Facade'${filekenner}'<Scalar, LocalOrdinal, GlobalOrdinal, Node>::defaultParams_ =' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '"<ParameterList name=\"Input\">"' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '"<Parameter name=\"MueLu preconditioner\" type=\"string\" value=\"undefined\"/>"' >> MueLu_Facade_${filekenner}_def.hpp
-  xsltproc gen_defaults.xsl ${facade_xml} >> MueLu_Facade_${filekenner}_def.hpp
-  echo '"</ParameterList>"' >> MueLu_Facade_${filekenner}_def.hpp
-  echo ';' >> MueLu_Facade_${filekenner}_def.hpp
+  # Default input parameters
+  echo '    // obtain ParameterList with default input parameters for this facade class' >> ${facade_def}
+  echo '    // Note all parameters are of type string (we use it for string replacement)' >> ${facade_def}
+  echo '    std::string defaultString = ' >> ${facade_def}
+  echo '"<ParameterList name=\"Input\">"' >> ${facade_def}
+  echo '"<Parameter name=\"MueLu preconditioner\" type=\"string\" value=\"undefined\"/>"' >> ${facade_def}
+  xsltproc gen_defaults.xsl ${facade_xml} >> ${facade_def}
+  echo '"</ParameterList>"' >> ${facade_def}
+  echo ';' >> ${facade_def}
+  echo '    Teuchos::RCP<ParameterList> defaultList = Teuchos::getParametersFromXmlString(defaultString);' >> ${facade_def}
+  echo '    // validate user input parameters (and set defaults if necessary)' >> ${facade_def}
+  echo '    Teuchos::ParameterList inputParameters = paramList;' >> ${facade_def}
+  echo '    inputParameters.validateParametersAndSetDefaults(*defaultList);' >> ${facade_def}
+  echo '    TEUCHOS_TEST_FOR_EXCEPTION(inputParameters.get<std::string>("MueLu preconditioner") == "undefined", MueLu::Exceptions::RuntimeError, "Facade'${filekenner}': undefined MueLu preconditioner. Set the \"MueLu preconditioner\" parameter correctly in your input file.");' >> ${facade_def}
+  echo '' >> ${facade_def}
 
-  echo '  // template string for preconditioner layout (factory based parameters)' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '  const std::string Facade'${filekenner}'<Scalar, LocalOrdinal, GlobalOrdinal, Node>::stringTemplate_ =' >> MueLu_Facade_${filekenner}_def.hpp
-  xsltproc gen_template.xsl ${facade_xml} >> MueLu_Facade_${filekenner}_def.hpp
-  echo ';' >> MueLu_Facade_${filekenner}_def.hpp
+  # Template string
+  echo '    // create copy of template string which is updated with in-place string replacements' >> ${facade_def}
+  echo '    // template string for preconditioner layout (factory based parameters)' >> ${facade_def}
+  echo '    std::string finalString =' >> ${facade_def}
+  xsltproc gen_template.xsl ${facade_xml} >> ${facade_def}
+  echo ';' >> ${facade_def}
+  echo '' >> ${facade_def}
 
+  # Logical code
+  echo '    // logical code for more complicated distinctions' >> ${facade_def}
+  xsltproc gen_logic.xsl ${facade_xml} >> ${facade_def}
+  echo '' >> ${facade_def}
+  echo '    // end logical code' >> ${facade_def}
+  echo '' >> ${facade_def}
 
-  echo '} // end namespace MueLu' >> MueLu_Facade_${filekenner}_def.hpp
-  echo '#endif'                   >> MueLu_Facade_${filekenner}_def.hpp
+  # Loop over input parameters
+  echo '    // loop over all input parameters' >> ${facade_def}
+  echo '    for(Teuchos::ParameterList::ConstIterator it = inputParameters.begin(); it != inputParameters.end(); it++) {' >> ${facade_def}
+  echo '      // form replacement string' >> ${facade_def}
+  echo '      std::string par_name = inputParameters.name(it);' >> ${facade_def}
+  echo '      std::stringstream ss;' >> ${facade_def}
+  echo '      ss << "XXX" << par_name << "YYY";' >> ${facade_def}
+  echo '' >> ${facade_def}
+
+  # Update file string
+  echo '      // update final string with parameters' >> ${facade_def}
+  echo '      Teuchos::ParameterEntry par_entry = inputParameters.entry(it);' >> ${facade_def}
+  echo '      this->ReplaceString(finalString,' >> ${facade_def}
+  echo '              ss.str(), Teuchos::toString(par_entry.getAny()));' >> ${facade_def}
+  echo '    }' >> ${facade_def}
+  echo '' >> ${facade_def}
+
+  echo '    Teuchos::RCP<ParameterList> ret = Teuchos::getParametersFromXmlString(finalString);' >> ${facade_def}
+  echo '    return ret;' >> ${facade_def}
+  echo '  }' >> ${facade_def}
+  echo '' >> ${facade_def}
+
+  echo '} // end namespace MueLu' >> ${facade_def}
+  echo '#endif // PACKAGES_MUELU_SRC_INTERFACE_FACADECLASSES_'${filekenner}'_DEF_HPP_' >> ${facade_def}
 }
 
 # create facade files
