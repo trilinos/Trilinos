559f580ce5a86bc7aeab461bb149031c768398e3
Alex R. Toth <artoth@sandia.gov>
2017-06-19 09:51:53 -0600

    NOX: Updating thyra/tpetra tests

--- packages/nox/test/tpetra/Tpetra_OperatorTests.cpp
+++ packages/nox/test/tpetra/Tpetra_OperatorTests.cpp
@@ -10,6 +10,12 @@
 #include "Thyra_VectorStdOps.hpp"
 #include "Thyra_MultiVectorStdOps.hpp"
 #include "Thyra_TpetraThyraWrappers.hpp"
+#include "Thyra_StateFuncModelEvaluatorBase.hpp"
+#include "Stratimikos_DefaultLinearSolverBuilder.hpp"
+
+#include "NOX_Thyra.H"
+#include "NOX_Thyra_Vector.H"
+#include "NOX_Thyra_MultiVector.H"
 
 // Typedefs
 
@@ -28,6 +34,8 @@ typedef Thyra::VectorBase<Scalar> TVB;
 typedef Thyra::MultiVectorBase<Scalar> TMVB;
 typedef Thyra::LinearOpBase<Scalar> TLOB;
 typedef Thyra::TpetraOperatorVectorExtraction<Scalar,LO,GO,Node> TOVE;
+typedef NOX::Thyra::Vector NTV;
+typedef NOX::Thyra::MultiVector NTMV;
 typedef typename TMV::mag_type mag_type;
 
 #if defined HAVE_TPETRACORE_CUDA
@@ -37,29 +45,75 @@ typedef typename TMV::mag_type mag_type;
 #endif
 const std::size_t numLocalElements = NUM_LOCAL;
 
+// Model evaluator for returning precomputed Jacobian operator
+
+class JacobianEvaluator
+  : public Thyra::StateFuncModelEvaluatorBase<Scalar>
+{
+public:
+  JacobianEvaluator(const Teuchos::RCP<TLOB>& lop) :
+    lop_(lop)
+  {
+    typedef Thyra::ModelEvaluatorBase MEB;
+    MEB::InArgsSetup<Scalar> inArgs;
+    inArgs.setModelEvalDescription(this->description());
+    inArgs.setSupports(MEB::IN_ARG_x);
+    inArgs_ = inArgs;
+
+    MEB::OutArgsSetup<Scalar> outArgs;
+    outArgs.setModelEvalDescription(this->description());
+    outArgs.setSupports(MEB::OUT_ARG_f);
+    outArgs.setSupports(MEB::OUT_ARG_W_op);
+    outArgs_ = outArgs;
+
+    Teuchos::RCP<TVB> x0 = Thyra::createMember(lop_->domain());
+    Thyra::assign(x0.ptr(), ST::zero());
+    nominalValues_ = inArgs;
+    nominalValues_.set_x(x0);
+  }
+
+  Teuchos::RCP<const TVSB> get_x_space() const { return lop_->domain(); }
+  Teuchos::RCP<const TVSB> get_f_space() const { return lop_->range(); }
+  Thyra::ModelEvaluatorBase::InArgs<Scalar> getNominalValues() const { return nominalValues_; }
+  Teuchos::RCP<TLOB> create_W_op() const { return lop_; }
+  Thyra::ModelEvaluatorBase::InArgs<Scalar> createInArgs() const { return inArgs_; }
+
+private:
+  Thyra::ModelEvaluatorBase::OutArgs<Scalar> createOutArgsImpl() const { return outArgs_; }
+  void evalModelImpl(
+    const Thyra::ModelEvaluatorBase::InArgs<Scalar> &inArgs,
+    const Thyra::ModelEvaluatorBase::OutArgs<Scalar> &outArgs
+    ) const
+  { if (nonnull(outArgs.get_W_op())) outArgs.get_W_op() = lop_; }
+
+  Teuchos::RCP<TLOB> lop_;
+  Thyra::ModelEvaluatorBase::InArgs<Scalar> nominalValues_;
+  Thyra::ModelEvaluatorBase::InArgs<Scalar> inArgs_;
+  Thyra::ModelEvaluatorBase::OutArgs<Scalar> outArgs_;
+};
+
 // Tpetra matrix creation
 
 Teuchos::RCP<CRSM> createTpetraOp(const Teuchos::RCP<const Map>& map)
 {
-  Teuchos::RCP<CRSM> mat = Teuchos::rcp(new CRSM(map, 3));
+  Teuchos::RCP<CRSM> mat = Teuchos::rcp(new CRSM(map, 2));
 
   Scalar posOne = ST::one();
   Scalar negOne = -1.0*posOne;
-  Scalar two = 2.0*posOne;
   for (std::size_t i = 0; i < map->getNodeNumElements(); ++i) {
     GO row = map->getGlobalElement(static_cast<LO>(i));
     if (row == 0) {
       mat->insertGlobalValues(row,
-                              Teuchos::tuple(row, row+1),
-                              Teuchos::tuple(two, posOne));
+                              Teuchos::tuple(row+1),
+                              Teuchos::tuple(posOne));
     } else if (row == map->getGlobalNumElements()-1) {
       mat->insertGlobalValues(row,
-                              Teuchos::tuple(row-1, row),
-                              Teuchos::tuple(negOne, two));
+                              Teuchos::tuple(row-1),
+                              Teuchos::tuple(negOne));
     } else {
       mat->insertGlobalValues(row,
-                              Teuchos::tuple(row-1, row, row+1),
-                              Teuchos::tuple(negOne, two, posOne));
+                              Teuchos::tuple(row-1, row+1),
+                              Teuchos::tuple(negOne, posOne));
     }
   }
 
@@ -67,7 +121,7 @@ Teuchos::RCP<CRSM> createTpetraOp(const Teuchos::RCP<const Map>& map)
   return mat;
 }
 
-//Routines for checking solution
+// Routines for checking solution
 
 void checkMultiVectors(const Teuchos::RCP<TMV>& a,
                        const Teuchos::RCP<TMV>& b,
@@ -118,21 +172,34 @@ TEUCHOS_UNIT_TEST(Tpetra_OpTests, VecNoTrans)
   Teuchos::RCP<TV> y_tpetra = TOVE::getTpetraVector(y);
   Teuchos::RCP<TV> z_tpetra = TOVE::getTpetraVector(z);
 
+  Teuchos::RCP<NTV> y_nox = Teuchos::rcp(new NTV(y));
+  Teuchos::RCP<NTV> z_nox = Teuchos::rcp(new NTV(z));
+
   // Create operator objects
   Teuchos::RCP<CRSM> op_tpetra = createTpetraOp(map);
   Teuchos::RCP<TLOB> op = Thyra::tpetraLinearOp<Scalar,LO,GO,Node>(space, space, op_tpetra);
   TEUCHOS_TEST_EQUALITY(Thyra::opSupported(*op, Thyra::NOTRANS), true, out, success);
 
   // Apply op with Tpetra directly
-  Scalar alpha = static_cast<Scalar>(0.5);
-  Scalar beta = -1.0*one;
-  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::NO_TRANS, alpha, beta);
+  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::NO_TRANS);
 
-  // Apply op through Thyra interface
-  Thyra::apply(*op, Thyra::NOTRANS, *z, y.ptr(), alpha, beta);
+  // Apply op through NOX Group interface
+  Teuchos::RCP<JacobianEvaluator> model = Teuchos::rcp(new JacobianEvaluator(op));
+  // Need to pass a lows factory to the group to call computeJacobian
+  Stratimikos::DefaultLinearSolverBuilder builder;
+  Teuchos::RCP<Teuchos::ParameterList> p = Teuchos::parameterList();
+  p->set("Linear Solver Type", "Belos");
+  p->set("Preconditioner Type", "None");
+  builder.setParameterList(p);
+  Teuchos::RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar> >
+    lowsf = builder.createLinearSolveStrategy("");
+  Teuchos::RCP<NOX::Thyra::Group> group =
+    Teuchos::rcp(new NOX::Thyra::Group(*z_nox, model, op, lowsf, Teuchos::null, Teuchos::null));
+  group->computeJacobian();
+  group->applyJacobian(*z_nox, *y_nox);
 
   // Check for correct results
-  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(0.5)));
+  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(2.0)));
   auto ans = Teuchos::tuple(val);
   checkMultiVectors(x_tpetra, y_tpetra, ans, out, success);
 
@@ -161,6 +228,9 @@ TEUCHOS_UNIT_TEST(Tpetra_OpTests, MultiVecNoTrans)
   Teuchos::RCP<TMV> y_tpetra = TOVE::getTpetraMultiVector(y);
   Teuchos::RCP<TMV> z_tpetra = TOVE::getTpetraMultiVector(z);
 
+  Teuchos::RCP<NTMV> y_nox = Teuchos::rcp(new NTMV(y));
+  Teuchos::RCP<NTMV> z_nox = Teuchos::rcp(new NTMV(z));
+
   auto scales = Teuchos::tuple(one, 2.0*one, 3.0*one, 4.0*one, 5.0*one);
   x_tpetra->scale(scales);
   y_tpetra->scale(scales);
@@ -172,15 +242,25 @@ TEUCHOS_UNIT_TEST(Tpetra_OpTests, MultiVecNoTrans)
   TEUCHOS_TEST_EQUALITY(Thyra::opSupported(*op, Thyra::NOTRANS), true, out, success);
 
   // Apply op with Tpetra directly
-  Scalar alpha = static_cast<Scalar>(0.5);
-  Scalar beta = -1.0*one;
-  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::NO_TRANS, alpha, beta);
+  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::NO_TRANS);
 
-  // Apply op through Thyra interface
-  Thyra::apply(*op, Thyra::NOTRANS, *z, y.ptr(), alpha, beta);
+  // Apply op through NOX Group interface
+  Teuchos::RCP<JacobianEvaluator> model = Teuchos::rcp(new JacobianEvaluator(op));
+  // Need to pass a lows factory to the group to call computeJacobian
+  Stratimikos::DefaultLinearSolverBuilder builder;
+  Teuchos::RCP<Teuchos::ParameterList> p = Teuchos::parameterList();
+  p->set("Linear Solver Type", "Belos");
+  p->set("Preconditioner Type", "None");
+  builder.setParameterList(p);
+  Teuchos::RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar> >
+    lowsf = builder.createLinearSolveStrategy("");
+  Teuchos::RCP<NOX::Thyra::Group> group =
+    Teuchos::rcp(new NOX::Thyra::Group(dynamic_cast<NTV&>((*z_nox)[0]), model, op, lowsf, Teuchos::null, Teuchos::null));
+  group->computeJacobian();
+  group->applyJacobianMultiVector(*z_nox, *y_nox);
 
   // Check for correct results
-  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(0.5)));
+  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(2.0)));
   auto ans = Teuchos::tuple(val, 2.0*val, 3.0*val, 4.0*val, 5.0*val);
   checkMultiVectors(x_tpetra, y_tpetra, ans, out, success);
 }
@@ -207,21 +287,36 @@ TEUCHOS_UNIT_TEST(Tpetra_OpTests, VecTrans)
   Teuchos::RCP<TV> y_tpetra = TOVE::getTpetraVector(y);
   Teuchos::RCP<TV> z_tpetra = TOVE::getTpetraVector(z);
 
+  Teuchos::RCP<NTV> y_nox = Teuchos::rcp(new NTV(y));
+  Teuchos::RCP<NTV> z_nox = Teuchos::rcp(new NTV(z));
+
   // Create operator objects
   Teuchos::RCP<CRSM> op_tpetra = createTpetraOp(map);
   Teuchos::RCP<TLOB> op = Thyra::tpetraLinearOp<Scalar,LO,GO,Node>(space, space, op_tpetra);
   TEUCHOS_TEST_EQUALITY(Thyra::opSupported(*op, Thyra::TRANS), true, out, success);
 
   // Apply op with Tpetra directly
-  Scalar alpha = static_cast<Scalar>(0.5);
-  Scalar beta = -1.0*one;
-  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::TRANS, alpha, beta);
+  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::TRANS);
 
-  // Apply op through Thyra interface
-  Thyra::apply(*op, Thyra::TRANS, *z, y.ptr(), alpha, beta);
+  // Apply op through NOX Group interface
+  Teuchos::RCP<JacobianEvaluator> model = Teuchos::rcp(new JacobianEvaluator(op));
+  // Need to pass a lows factory to the group to call computeJacobian
+  Stratimikos::DefaultLinearSolverBuilder builder;
+  Teuchos::RCP<Teuchos::ParameterList> p = Teuchos::parameterList();
+  p->set("Linear Solver Type", "Belos");
+  p->set("Preconditioner Type", "None");
+  builder.setParameterList(p);
+  Teuchos::RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar> >
+    lowsf = builder.createLinearSolveStrategy("");
+  Teuchos::RCP<NOX::Thyra::Group> group =
+    Teuchos::rcp(new NOX::Thyra::Group(*z_nox, model, op, lowsf, Teuchos::null, Teuchos::null));
+  group->computeJacobian();
+  // Need this to get the group to call updateLOWS, or else shared_jacobian is unitialized
+  (void) group->getJacobian();
+  group->applyJacobianTranspose(*z_nox, *y_nox);
 
   // Check for correct results
-  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(0.5)));
+  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(2.0)));
   auto ans = Teuchos::tuple(val);
   checkMultiVectors(x_tpetra, y_tpetra, ans, out, success);
 }
@@ -249,6 +344,9 @@ TEUCHOS_UNIT_TEST(Tpetra_OpTests, MultiVecTrans)
   Teuchos::RCP<TMV> y_tpetra = TOVE::getTpetraMultiVector(y);
   Teuchos::RCP<TMV> z_tpetra = TOVE::getTpetraMultiVector(z);
 
+  Teuchos::RCP<NTMV> y_nox = Teuchos::rcp(new NTMV(y));
+  Teuchos::RCP<NTMV> z_nox = Teuchos::rcp(new NTMV(z));
+
   auto scales = Teuchos::tuple(one, 2.0*one, 3.0*one, 4.0*one, 5.0*one);
   x_tpetra->scale(scales);
   y_tpetra->scale(scales);
@@ -260,15 +358,27 @@ TEUCHOS_UNIT_TEST(Tpetra_OpTests, MultiVecTrans)
   TEUCHOS_TEST_EQUALITY(Thyra::opSupported(*op, Thyra::TRANS), true, out, success);
 
   // Apply op with Tpetra directly
-  Scalar alpha = static_cast<Scalar>(0.5);
-  Scalar beta = -1.0*one;
-  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::TRANS, alpha, beta);
+  op_tpetra->apply(*z_tpetra, *x_tpetra, Teuchos::TRANS);
 
-  // Apply op through Thyra interface
-  Thyra::apply(*op, Thyra::TRANS, *z, y.ptr(), alpha, beta);
+  // Apply op through NOX Group interface
+  Teuchos::RCP<JacobianEvaluator> model = Teuchos::rcp(new JacobianEvaluator(op));
+  // Need to pass a lows factory to the group to call computeJacobian even though it isn't used
+  Stratimikos::DefaultLinearSolverBuilder builder;
+  Teuchos::RCP<Teuchos::ParameterList> p = Teuchos::parameterList();
+  p->set("Linear Solver Type", "Belos");
+  p->set("Preconditioner Type", "None");
+  builder.setParameterList(p);
+  Teuchos::RCP<Thyra::LinearOpWithSolveFactoryBase<Scalar> >
+    lowsf = builder.createLinearSolveStrategy("");
+  Teuchos::RCP<NOX::Thyra::Group> group =
+    Teuchos::rcp(new NOX::Thyra::Group(dynamic_cast<NTV&>((*z_nox)[0]), model, op, lowsf, Teuchos::null, Teuchos::null));
+  group->computeJacobian();
+  // Need this to get the group to call updateLOWS, or else shared_jacobian is unitialized
+  (void) group->getJacobian();
+  group->applyJacobianTransposeMultiVector(*z_nox, *y_nox);
 
   // Check for correct results
-  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(0.5)));
+  mag_type val = static_cast<mag_type>(ST::squareroot(static_cast<Scalar>(2.0)));
   auto ans = Teuchos::tuple(val, 2.0*val, 3.0*val, 4.0*val, 5.0*val);
   checkMultiVectors(x_tpetra, y_tpetra, ans, out, success);
 }
