17ddb71b3f9b1024810bd49c62bc7f89e5282205
Michael Plooster <mplooster@elemtech.com>
2017-07-13 13:39:41 -0600

    Remove reference to local user account.

--- cmake/ctest/drivers/windows/task_driver_windows.bat
+++ cmake/ctest/drivers/windows/task_driver_windows.bat
@@ -7,7 +7,7 @@ set SEMS_DIR=C:\projects\sems\install\win-x86_64
 set NINJA_DIR=%SEMS_DIR%\utility\ninja\1.7.2
 set CTEST_EXE=%SEMS_DIR%\utility\cmake\3.8.1\bin\ctest.exe
 set GIT_EXE=%SEMS_DIR%\utility\git\2.13.0\cmd\git.exe
-set PATH=%NINJA_DIR%;C:\projects\sems\install\win-x86_64\compiler\Microsoft MPI\8.1.12438.1084\Bin\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\Microsoft SQL Server\130\Tools\Binn\;C:\Users\mgploos\.dnx\bin;C:\Program Files\Microsoft DNX\Dnvm\;C:\projects\sems\install\win-x86_64\utility\git\2.13.0\cmd;C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\
+set PATH=%NINJA_DIR%;C:\projects\sems\install\win-x86_64\compiler\Microsoft MPI\8.1.12438.1084\Bin\;C:\Windows\system32;C:\Windows;C:\Windows\System32\Wbem;C:\Windows\System32\WindowsPowerShell\v1.0\;C:\Program Files\Microsoft SQL Server\130\Tools\Binn\;C:\Program Files\Microsoft DNX\Dnvm\;C:\projects\sems\install\win-x86_64\utility\git\2.13.0\cmd;C:\Program Files (x86)\Windows Kits\10\Windows Performance Toolkit\
 set TRILINOS_REPOSITORY_LOCATION=git@github.com:trilinos/Trilinos.git
 
 set BASEDIR=%~1
