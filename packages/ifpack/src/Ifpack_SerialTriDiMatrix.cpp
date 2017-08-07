0e7b2e8c4a373ca1200a2ee3f1db853e8e0b941b
Mark Hoemmen <mhoemme@sandia.gov>
2017-08-06 18:11:31 -0600

    Ifpack_SerialTriDiMatrix: Fix unreachable statement warnings
    @trilinos/ifpack

--- packages/ifpack/src/Ifpack_SerialTriDiMatrix.cpp
+++ packages/ifpack/src/Ifpack_SerialTriDiMatrix.cpp
@@ -73,7 +73,7 @@ Ifpack_SerialTriDiMatrix::Ifpack_SerialTriDiMatrix(int NumRowCol,
     SetLabel("Epetra::SerialTriDiMatrix");
   }
   if(NumRowCol < 0)
-	throw ReportError("NumRows = " + toString(NumRowCol) + ". Should be >= 0", -1);
+        throw ReportError("NumRows = " + toString(NumRowCol) + ". Should be >= 0", -1);
 
   int errorcode = Shape(NumRowCol);
   if(errorcode != 0)
@@ -96,7 +96,7 @@ Ifpack_SerialTriDiMatrix::Ifpack_SerialTriDiMatrix(Epetra_DataAccess CV_in, doub
     SetLabel("Epetra::SerialTriDiMatrix");
   }
   if(A_in == 0)
-	throw ReportError("Null pointer passed as A parameter.", -3);
+        throw ReportError("Null pointer passed as A parameter.", -3);
   if(NumRowCol < 0)
     throw ReportError("NumRowCol = " + toString(NumRowCol) + ". Should be >= 0", -1);
 
@@ -145,21 +145,21 @@ Ifpack_SerialTriDiMatrix::Ifpack_SerialTriDiMatrix(const Ifpack_SerialTriDiMatri
 
 //=============================================================================
 int Ifpack_SerialTriDiMatrix::Reshape(int NumRows, int NumCols) {
-	if(NumRows < 0 || NumCols < 0)
-	  return(-1);
-	if(NumRows != NumCols)
-	  return(-1);
+        if(NumRows < 0 || NumCols < 0)
+          return(-1);
+        if(NumRows != NumCols)
+          return(-1);
 
-	double* A_tmp = 0;
-	const int newsize = (N_ == 1)? 1 : 4*(N_-1);
-	if(newsize > 0) {
-		// Allocate space for new matrix
-		A_tmp = new double[newsize];
-		for (int k = 0; k < newsize; k++)
-			A_tmp[k] = 0.0; // Zero out values
-		if (A_ != 0)
-		  CopyMat(A_, N_, A_tmp, NumRows); // Copy principal submatrix of A to new A
-		
+        double* A_tmp = 0;
+        const int newsize = (N_ == 1)? 1 : 4*(N_-1);
+        if(newsize > 0) {
+                // Allocate space for new matrix
+                A_tmp = new double[newsize];
+                for (int k = 0; k < newsize; k++)
+                        A_tmp[k] = 0.0; // Zero out values
+                if (A_ != 0)
+                  CopyMat(A_, N_, A_tmp, NumRows); // Copy principal submatrix of A to new A
+
   }
   CleanupData(); // Get rid of anything that might be already allocated
   N_ = NumCols;
@@ -177,8 +177,8 @@ int Ifpack_SerialTriDiMatrix::Reshape(int NumRows, int NumCols) {
 }
 //=============================================================================
 int Ifpack_SerialTriDiMatrix::Shape(int NumRowCol) {
-	if(NumRowCol < 0 || NumRowCol < 0)
-		return(-1);
+        if(NumRowCol < 0 || NumRowCol < 0)
+                return(-1);
 
   CleanupData(); // Get rid of anything that might be already allocated
   N_ = NumRowCol;
@@ -210,73 +210,73 @@ void Ifpack_SerialTriDiMatrix::CleanupData()
 {
   if (A_)
     delete [] A_;
-	A_ = DL_ = D_ = DU_ = DU2_ =  0;
-	A_Copied_ = false;
-	N_ = 0;
+        A_ = DL_ = D_ = DU_ = DU2_ =  0;
+        A_Copied_ = false;
+        N_ = 0;
 }
 //=============================================================================
 Ifpack_SerialTriDiMatrix& Ifpack_SerialTriDiMatrix::operator = (const Ifpack_SerialTriDiMatrix& Source) {
   if(this == &Source)
-		return(*this); // Special case of source same as target
-	if((CV_ == View) && (Source.CV_ == View) && (A_ == Source.A_))
-		return(*this); // Special case of both are views to same data.
+                return(*this); // Special case of source same as target
+        if((CV_ == View) && (Source.CV_ == View) && (A_ == Source.A_))
+                return(*this); // Special case of both are views to same data.
 
-	if(std::strcmp(Label(), Source.Label()) != 0)
-		throw ReportError("operator= type mismatch (lhs = " + std::string(Label()) +
+        if(std::strcmp(Label(), Source.Label()) != 0)
+                throw ReportError("operator= type mismatch (lhs = " + std::string(Label()) +
       ", rhs = " + std::string(Source.Label()) + ").", -5);
-	
-	if(Source.CV_ == View) {
-		if(CV_ == Copy) { // C->V only
-			CleanupData();
-			CV_ = View;
-		}
-		N_ = Source.N_;
-		A_ = Source.A_;
-		DL_ = Source.DL_;
-		D_ = Source.D_;
-		DU_ = Source.DU_;
-		DU2_ = Source.DU2_;
-	}
-	else {
-		if(CV_ == View) { // V->C
-			CV_ = Copy;
-			N_ = Source.N_;
-			const int newsize = 4*N_ - 4;
-			if(newsize > 0) {
-				A_ = new double[newsize];
-				DL_ = A_;
-				D_ = A_+(N_-1);
-				DU_ = D_+N_;
-				DU2_ = DU_+(N_-1);
-				A_Copied_ = true;
-			}
-			else {
-				A_ = 0;
-				DL_ = D_ = DU_ = DU2_ = 0;
-				A_Copied_ = false;
-			}
-		}
-		else { // C->C
-			if(Source.N_ == N_) { // we don't need to reallocate
-				N_ = Source.N_;
-			}
-			else { // we need to allocate more space (or less space)
-				CleanupData();
-				N_ = Source.N_;
-				const int newsize = (N_ == 1)? 1 : 4*(N_-1);
-				if(newsize > 0) {
-					A_ = new double[newsize];
-					DL_ = A_;
-					D_ = A_+(N_-1);
-					DU_ = D_+N_;
-					DU2_ = DU_+(N_-1);
-					A_Copied_ = true;
-				}
-			}
-		}
-		CopyMat(Source.A_, Source.N(), A_, N_); // V->C and C->C
-	}
-	
+
+        if(Source.CV_ == View) {
+                if(CV_ == Copy) { // C->V only
+                        CleanupData();
+                        CV_ = View;
+                }
+                N_ = Source.N_;
+                A_ = Source.A_;
+                DL_ = Source.DL_;
+                D_ = Source.D_;
+                DU_ = Source.DU_;
+                DU2_ = Source.DU2_;
+        }
+        else {
+                if(CV_ == View) { // V->C
+                        CV_ = Copy;
+                        N_ = Source.N_;
+                        const int newsize = 4*N_ - 4;
+                        if(newsize > 0) {
+                                A_ = new double[newsize];
+                                DL_ = A_;
+                                D_ = A_+(N_-1);
+                                DU_ = D_+N_;
+                                DU2_ = DU_+(N_-1);
+                                A_Copied_ = true;
+                        }
+                        else {
+                                A_ = 0;
+                                DL_ = D_ = DU_ = DU2_ = 0;
+                                A_Copied_ = false;
+                        }
+                }
+                else { // C->C
+                        if(Source.N_ == N_) { // we don't need to reallocate
+                                N_ = Source.N_;
+                        }
+                        else { // we need to allocate more space (or less space)
+                                CleanupData();
+                                N_ = Source.N_;
+                                const int newsize = (N_ == 1)? 1 : 4*(N_-1);
+                                if(newsize > 0) {
+                                        A_ = new double[newsize];
+                                        DL_ = A_;
+                                        D_ = A_+(N_-1);
+                                        DU_ = D_+N_;
+                                        DU2_ = DU_+(N_-1);
+                                        A_Copied_ = true;
+                                }
+                        }
+                }
+                CopyMat(Source.A_, Source.N(), A_, N_); // V->C and C->C
+        }
+
   return(*this);
 }
 
@@ -303,37 +303,37 @@ bool Ifpack_SerialTriDiMatrix::operator==(const Ifpack_SerialTriDiMatrix& rhs) c
 //=============================================================================
 Ifpack_SerialTriDiMatrix& Ifpack_SerialTriDiMatrix::operator+= ( const Ifpack_SerialTriDiMatrix & Source) {
     if (N() != Source.N())
-		throw ReportError("Column dimension of source = " + toString(Source.N()) +
-											" is different than column dimension of target = " + toString(N()), -2);
+                throw ReportError("Column dimension of source = " + toString(Source.N()) +
+                                                                                        " is different than column dimension of target = " + toString(N()), -2);
 
     CopyMat(Source.A(), Source.N(), A(), N(),true);
   return(*this);
 }
 //=============================================================================
 void Ifpack_SerialTriDiMatrix::CopyMat(const double* Source,
-				       int nrowcol,
+                                       int nrowcol,
                                        double* Target,
-				       int tN,
+                                       int tN,
                                        bool add)
 {
   int lmax = EPETRA_MIN(nrowcol,tN);
   if (add) {
     // do this in 4 passes
-    for(int j=0; j<lmax; ++j) {      
-      Target[(tN-1)+j] += Source[(nrowcol-1)+j];  //D      
+    for(int j=0; j<lmax; ++j) {
+      Target[(tN-1)+j] += Source[(nrowcol-1)+j];  //D
       if(j<tN-1) {
-	Target[j] += Source[j];  // DL
-	Target[(tN-1)+tN + j] += Source[(nrowcol-1)+ nrowcol + j]; // DU
+        Target[j] += Source[j];  // DL
+        Target[(tN-1)+tN + j] += Source[(nrowcol-1)+ nrowcol + j]; // DU
       }
       if(j<tN-2) Target[(tN-1)*2 + tN + j] += Source[ (nrowcol-1)*2 +nrowcol  + j]; // DU2
     }
   }
-  else {  
+  else {
     for(int j=0; j<lmax; ++j) {
-      Target[(tN-1)+j] = Source[(nrowcol-1)+j];  //D      
+      Target[(tN-1)+j] = Source[(nrowcol-1)+j];  //D
       if(j<tN-1) {
-	Target[j] = Source[j];  // DL
-	Target[(tN-1)+tN + j] = Source[(nrowcol-1)+ nrowcol + j]; // DU
+        Target[j] = Source[j];  // DL
+        Target[(tN-1)+tN + j] = Source[(nrowcol-1)+ nrowcol + j]; // DU
       }
       if(j<tN-2) Target[(tN-1)*2 + tN + j] = Source[ (nrowcol-1)*2 +nrowcol  + j]; // DU2
     }
@@ -346,11 +346,11 @@ double Ifpack_SerialTriDiMatrix::NormOne() const {
   double anorm = 0.0;
   double * ptr  = A_;
   double sum=0.0;
-  
+
   const int size = (N_ == 1)? 1 : 4*(N_-1);
 
   for (i=0; i<size; i++) sum += std::abs(*ptr++);
-  
+
   anorm = EPETRA_MAX(anorm, sum);
   UpdateFlops((double)size );
   return(anorm);
@@ -370,7 +370,7 @@ int Ifpack_SerialTriDiMatrix::Scale(double ScalarA) {
   const int size = (N_ == 1)? 1 : 4*(N_-1);
 
   for (i=0; i<size ; i++) { *ptr = ScalarA * (*ptr); ptr++; }
- 
+
   UpdateFlops((double)N_*(double)N_);
   return(0);
 
@@ -378,25 +378,24 @@ int Ifpack_SerialTriDiMatrix::Scale(double ScalarA) {
 
 // //=========================================================================
 int Ifpack_SerialTriDiMatrix::Multiply (char TransA, char TransB, double ScalarAB,
-					const Ifpack_SerialTriDiMatrix& A_in,
-					const Ifpack_SerialTriDiMatrix& B,
-					double ScalarThis ) {
+                                        const Ifpack_SerialTriDiMatrix& A_in,
+                                        const Ifpack_SerialTriDiMatrix& B,
+                                        double ScalarThis ) {
   throw ReportError("Ifpack_SerialTriDiMatrix::Multiply not implimented ",-2);
-  return(-1);
-
+  // return(-1); // unreachable
 }
 
 //=========================================================================
 
 int Ifpack_SerialTriDiMatrix::Random() {
-  
+
   Epetra_Util util;
   double* arrayPtr = A_;
   const int size = (N_ == 1)? 1 : 4*(N_-1);
-  for(int j = 0; j < size ; j++) {	  
-    *arrayPtr++ = util.RandomDouble();	
+  for(int j = 0; j < size ; j++) {
+    *arrayPtr++ = util.RandomDouble();
   }
-  
+
   return(0);
 }
 
@@ -409,10 +408,10 @@ void Ifpack_SerialTriDiMatrix::Print(std::ostream& os) const {
     for(int i=0 ; i < N_ ; ++i)  {
     for(int j=0 ; j < N_ ; ++j)  {
       if ( j >= i-1  && j <= i+1) {
-	os << (*this)(i,j)<<" ";
+        os << (*this)(i,j)<<" ";
       }
-      else 
-	os <<". ";
+      else
+        os <<". ";
     }
     os << std::endl;
     }
