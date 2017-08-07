f1deb71a7de7eb4aeefe47d5c7db7818cd4d348a
Mark Hoemmen <mhoemme@sandia.gov>
2017-08-06 21:53:35 -0600

    Tpetra: Make "BlankRowBugTest" test not depend on GO=int
    @trilinos/tpetra Tpetra's "Block" test depended arbitrarily on
    LocalOrdinal=int and GlobalOrdinal=int.  This commit changes the test
    to use the default LocalOrdinal and GlobalOrdinal types.  See #74.

--- packages/tpetra/core/test/BugTests/BlankRowBugTest.cpp
+++ packages/tpetra/core/test/BugTests/BlankRowBugTest.cpp
@@ -55,26 +55,31 @@ namespace {
 
   TEUCHOS_UNIT_TEST(CrsMatrix, BlankRowImport)
   {
-    typedef Tpetra::Map<int,int>                    Map;
-    typedef Tpetra::CrsMatrix<double,int,int> CrsMatrix;
-    typedef Tpetra::Import<int,int>              Import;
-    RCP<const Comm<int> > comm = Tpetra::DefaultPlatform::getDefaultPlatform().getComm();
+    typedef Tpetra::Map<>                     map_type;
+    typedef map_type::local_ordinal_type      LO;
+    typedef map_type::global_ordinal_type     GO;
+    typedef Tpetra::CrsMatrix<double, LO, GO> crs_matrix_type;
+    typedef Tpetra::Import<LO, GO>            import_type;
+
+    RCP<const Comm<int> > comm =
+      Tpetra::DefaultPlatform::getDefaultPlatform ().getComm ();
     // We run this test explicitly in MPI mode with 2 processors as described in
     // the CMakeLists.txt file. This is just asserting that fact.
     TEST_EQUALITY_CONST(comm->getSize(), 2)
 
     const int rank = comm->getRank();
-    RCP<const Map> destRowMap, sourceRowMap;
-    if (rank ==0) {
-      sourceRowMap = Tpetra::createNonContigMap<int,int>( tuple<int>(0), comm );
-    } else {
-      sourceRowMap = Tpetra::createNonContigMap<int,int>( tuple<int>(1), comm );
-    }
-    destRowMap   = Tpetra::createNonContigMap<int,int>( tuple<int>(0,1), comm );
-
-    RCP<CrsMatrix> srcMat = Tpetra::createCrsMatrix<double>(sourceRowMap);
+    RCP<const map_type> destRowMap, sourceRowMap;
     if (rank == 0) {
-      srcMat->insertGlobalValues(0, tuple<int>(0), tuple<double>(1.0) );
+      sourceRowMap = Tpetra::createNonContigMap<LO, GO> (tuple<GO> (0), comm);
+    }
+    else {
+      sourceRowMap = Tpetra::createNonContigMap<LO, GO> (tuple<GO> (1), comm);
+    }
+    destRowMap = Tpetra::createNonContigMap<LO, GO> (tuple<GO> (0, 1), comm);
+
+    RCP<crs_matrix_type> srcMat = Tpetra::createCrsMatrix<double>(sourceRowMap);
+    if (rank == 0) {
+      srcMat->insertGlobalValues (static_cast<GO> (0), tuple<GO> (0), tuple<double> (1.0));
     }
     srcMat->fillComplete();
     /*
@@ -82,23 +87,23 @@ namespace {
                 [  ] // proc 1
      */
     if (rank == 0) {
-      TEST_EQUALITY_CONST( srcMat->getNumEntriesInGlobalRow(0), 1 );
+      TEST_EQUALITY_CONST( srcMat->getNumEntriesInGlobalRow(0), static_cast<size_t> (1) );
     } else {
-      TEST_EQUALITY_CONST( srcMat->getNumEntriesInGlobalRow(1), 0 );
+      TEST_EQUALITY_CONST( srcMat->getNumEntriesInGlobalRow(1), static_cast<size_t> (0) );
     }
 
-    RCP<CrsMatrix> dstMat = Tpetra::createCrsMatrix<double>(destRowMap);
-    RCP<const Import> importer = Tpetra::createImport(sourceRowMap, destRowMap);
+    RCP<crs_matrix_type> dstMat = Tpetra::createCrsMatrix<double> (destRowMap);
+    RCP<const import_type> importer = Tpetra::createImport (sourceRowMap, destRowMap);
     // global row 1 in srcMat is empty: this is a null communication to dstMat
-    dstMat->doImport(*srcMat, *importer, Tpetra::INSERT);
+    dstMat->doImport (*srcMat, *importer, Tpetra::INSERT);
     /*
        dstMat_p0 = [1 ]
                    [  ]
        dstMat_p1 = [1 ]
                    [  ]
     */
-    TEST_EQUALITY_CONST( dstMat->getNumEntriesInGlobalRow(0), 1 );
-    TEST_EQUALITY_CONST( dstMat->getNumEntriesInGlobalRow(1), 0 );
+    TEST_EQUALITY_CONST( dstMat->getNumEntriesInGlobalRow(0), static_cast<size_t> (1) );
+    TEST_EQUALITY_CONST( dstMat->getNumEntriesInGlobalRow(1), static_cast<size_t> (0) );
   }
 
 }
