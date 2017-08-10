c9ae65f9f6c17aa6b7bbd4db3201216e0c2fd99e
Luc Berger-Vergiat <lberge@sandia.gov>
2017-08-10 10:11:08 -0600

    Xpetra: Replacing deprecated call in Xpetra_TpetraBlockCrsMatrix.hpp
    Creating a little interface with the new getLocalDiagOffsets form
    Tpetra_BlockCrsMatrix to avoid all the compile time warning associated
    with the use of the deprecated version of that function which uses a
    Teuchos::ArrayRCP.

--- packages/xpetra/src/CrsMatrix/Xpetra_TpetraBlockCrsMatrix.hpp
+++ packages/xpetra/src/CrsMatrix/Xpetra_TpetraBlockCrsMatrix.hpp
@@ -340,7 +340,31 @@ namespace Xpetra {
     //! Get offsets of the diagonal entries in the matrix.
     void getLocalDiagOffsets(Teuchos::ArrayRCP<size_t> &offsets) const {
       XPETRA_MONITOR("TpetraBlockCrsMatrix::getLocalDiagOffsets");
-      mtx_->getLocalDiagOffsets(offsets);
+
+      // The input ArrayRCP must always be a host pointer.  Thus, if
+      // device_type::memory_space is Kokkos::HostSpace, it's OK for us
+      // to write to that allocation directly as a Kokkos::View.
+      typedef typename Node::device_type device_type;
+      typedef typename device_type::memory_space memory_space;
+      if (std::is_same<memory_space, Kokkos::HostSpace>::value) {
+        // It is always syntactically correct to assign a raw host
+        // pointer to a device View, so this code will compile correctly
+        // even if this branch never runs.
+        typedef Kokkos::View<size_t*, device_type,
+                             Kokkos::MemoryUnmanaged> output_type;
+        output_type offsetsOut (offsets.getRawPtr(), offsets.size());
+        mtx_->getLocalDiagOffsets(offsetsOut);
+      }
+      else {
+        Kokkos::View<size_t*, device_type> offsetsTmp ("diagOffsets", offsets.size());
+        mtx_->getLocalDiagOffsets(offsetsTmp);
+        typedef Kokkos::View<size_t*, Kokkos::HostSpace,
+                             Kokkos::MemoryUnmanaged> output_type;
+        output_type offsetsOut(offsets.getRawPtr(), offsets.size());
+        Kokkos::deep_copy(offsetsOut, offsetsTmp);
+      }
+
+      // mtx_->getLocalDiagOffsets(offsets);
     }
 
     //! Get a copy of the diagonal entries owned by this node, with local row indices.
