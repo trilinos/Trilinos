f24f0d1ef69ee917a9d8d122ee21d09d21c6c1e9
Jonathan Hu <jhu@sandia.gov>
2017-08-14 11:06:56 -0700

    Xpetra: fix warnings under CUDA
    more unreachable code
    
    Build/Test Cases Summary
    Enabled Packages: Xpetra
    Disabled Packages: PyTrilinos,Claps,TriKota
    Enabled all Forward Packages
    0) MPI_RELEASE_DEBUG_SHARED_PT => FAILED: passed=584,notpassed=1 => Not ready to push! (305.40 min)
    WARNING: Forced the push!

--- packages/xpetra/src/CrsMatrix/Xpetra_TpetraBlockCrsMatrix.hpp
+++ packages/xpetra/src/CrsMatrix/Xpetra_TpetraBlockCrsMatrix.hpp
@@ -443,7 +443,7 @@ namespace Xpetra {
     local_matrix_type getLocalMatrix () const {
       throw std::runtime_error("Xpetra::TpetraBlockCrsMatrix does not support getLocalMatrix due to missing Kokkos::CrsMatrix in Tpetra's experimental implementation");
       local_matrix_type ret;
-      return ret; // make compiler happy
+      TEUCHOS_UNREACHABLE_RETURN(ret);
     }
 #endif
 #endif
@@ -1144,7 +1144,7 @@ namespace Xpetra {
     local_matrix_type getLocalMatrix () const {
       throw std::runtime_error("Xpetra::TpetraBlockCrsMatrix does not support getLocalMatrix due to missing Kokkos::CrsMatrix in Tpetra's experimental implementation");
       local_matrix_type ret;
-      return ret; // make compiler happy
+      TEUCHOS_UNREACHABLE_RETURN(ret);
     }
 #endif
 #endif
