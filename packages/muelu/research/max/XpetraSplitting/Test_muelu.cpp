8d29d0802c7c83545a4a535889bb2a58850b799e
Tobias Wiesner <tawiesn@sandia.gov>
2017-08-18 11:43:50 -0600

    MueLu: replace tabs by spaces
    Build/Test Cases Summary
    Enabled Packages: MueLu
    Disabled Packages: PyTrilinos,Claps,TriKota
    Enabled all Forward Packages
    0) MPI_RELEASE_DEBUG_SHARED_PT => passed: passed=430,notpassed=0 (237.59 min)
    Other local commits for this build/test group: 7a37f4a

--- packages/muelu/research/max/XpetraSplitting/Test_muelu.cpp
+++ packages/muelu/research/max/XpetraSplitting/Test_muelu.cpp
@@ -55,32 +55,32 @@ int main(int argc, char* argv[])
   typedef scalar_type         												Scalar;
   typedef local_ordinal_type  												LocalOrdinal;
   typedef global_ordinal_type 												GlobalOrdinal;
-	typedef KokkosClassic::DefaultNode::DefaultNodeType Node;
+  typedef KokkosClassic::DefaultNode::DefaultNodeType Node;
 
-	typedef Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node> 											Matrix;
-	typedef Xpetra::MatrixSplitting<Scalar, LocalOrdinal, GlobalOrdinal, Node>							MatrixSplitting;
-	typedef Xpetra::CrsMatrixWrap<Scalar, LocalOrdinal, GlobalOrdinal, Xpetra::EpetraNode> EpCrsMatrix;
+  typedef Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node> 											Matrix;
+  typedef Xpetra::MatrixSplitting<Scalar, LocalOrdinal, GlobalOrdinal, Node>							MatrixSplitting;
+  typedef Xpetra::CrsMatrixWrap<Scalar, LocalOrdinal, GlobalOrdinal, Xpetra::EpetraNode> EpCrsMatrix;
 
-  
-	TEUCHOS_TEST_FOR_EXCEPT_MSG(argc<2, "\nInvalid name for input matrix\n");
 
-	int numGlobalElements = 1;
+  TEUCHOS_TEST_FOR_EXCEPT_MSG(argc<2, "\nInvalid name for input matrix\n");
 
-	Teuchos::RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
+  int numGlobalElements = 1;
 
-	//Create Xpetra map
-	Teuchos::RCP<const Xpetra::Map<int,GlobalOrdinal,Xpetra::EpetraNode> > xpetraMap;
-	xpetraMap = Xpetra::MapFactory<int,GlobalOrdinal,Xpetra::EpetraNode>::Build(Xpetra::UseEpetra, numGlobalElements, 0, comm); 
+  Teuchos::RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
 
-	//Import matrix from an .mtx file into an Xpetra wrapper for an Epetra matrix
-	Teuchos::RCP<Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node> > xpetraMatrix = Xpetra::IO<Scalar,LocalOrdinal,GlobalOrdinal,Node>::Read(argv[1], Xpetra::UseEpetra, comm);
-	//Export matrix from an Xpetra wrapper into an .mtx file
-	Xpetra::IO<Scalar,LocalOrdinal,GlobalOrdinal,Node>::Write("A_write.mtx", *xpetraMatrix);
+  //Create Xpetra map
+  Teuchos::RCP<const Xpetra::Map<int,GlobalOrdinal,Xpetra::EpetraNode> > xpetraMap;
+  xpetraMap = Xpetra::MapFactory<int,GlobalOrdinal,Xpetra::EpetraNode>::Build(Xpetra::UseEpetra, numGlobalElements, 0, comm);
 
-	Teuchos::RCP<MatrixSplitting> xpetraMatrixSplitting;
-  
-	Teuchos::ParameterList xmlParams;
-	Teuchos::RCP<MueLu::Hierarchy<Scalar,LocalOrdinal,GlobalOrdinal,Node> > Hierarchy = MueLu::CreateXpetraPreconditioner( (Teuchos::RCP<Matrix>)xpetraMatrixSplitting, xmlParams );
+  //Import matrix from an .mtx file into an Xpetra wrapper for an Epetra matrix
+  Teuchos::RCP<Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node> > xpetraMatrix = Xpetra::IO<Scalar,LocalOrdinal,GlobalOrdinal,Node>::Read(argv[1], Xpetra::UseEpetra, comm);
+  //Export matrix from an Xpetra wrapper into an .mtx file
+  Xpetra::IO<Scalar,LocalOrdinal,GlobalOrdinal,Node>::Write("A_write.mtx", *xpetraMatrix);
+
+  Teuchos::RCP<MatrixSplitting> xpetraMatrixSplitting;
+
+  Teuchos::ParameterList xmlParams;
+  Teuchos::RCP<MueLu::Hierarchy<Scalar,LocalOrdinal,GlobalOrdinal,Node> > Hierarchy = MueLu::CreateXpetraPreconditioner( (Teuchos::RCP<Matrix>)xpetraMatrixSplitting, xmlParams );
 
 
 #ifdef HAVE_MPI
--- packages/muelu/research/max/XpetraSplitting/Xpetra_RegionAMG_decl.hpp
+++ packages/muelu/research/max/XpetraSplitting/Xpetra_RegionAMG_decl.hpp
@@ -70,127 +70,127 @@ namespace Xpetra{
 /*!
  * class RegionAMG
  *
- * This class inherits from Xpetra::Operator because the goal is to allow its usage 
+ * This class inherits from Xpetra::Operator because the goal is to allow its usage
  * as a preconditioner for a Belos-type linear solver
  *
  * RegionAMG is a class used to split a composite stiffness matrix into region matrices.
  * Each region matrix is associated with a geometric region of the discretized domain.
  * Therefore, the splitting operates in a domain decomposition fashion.
- * Since there are infinitely many ways to split a given composite matrix, 
+ * Since there are infinitely many ways to split a given composite matrix,
  * we explains here below the rationale behind the approach adopted in this class.
  *  */
 
-	template<class Scalar					= MultiVector<>::scalar_type,
-		class LocalOrdinal   				= typename MultiVector<Scalar>::local_ordinal_type,
-		class GlobalOrdinal  				= typename MultiVector<Scalar, LocalOrdinal>::global_ordinal_type,
-		class Node           				= typename MultiVector<Scalar, LocalOrdinal, GlobalOrdinal>::node_type>
-		class RegionAMG : Operator<Scalar, LocalOrdinal, GlobalOrdinal, Node> {
+template<class Scalar					= MultiVector<>::scalar_type,
+    class LocalOrdinal   				= typename MultiVector<Scalar>::local_ordinal_type,
+    class GlobalOrdinal  				= typename MultiVector<Scalar, LocalOrdinal>::global_ordinal_type,
+    class Node           				= typename MultiVector<Scalar, LocalOrdinal, GlobalOrdinal>::node_type>
+class RegionAMG : Operator<Scalar, LocalOrdinal, GlobalOrdinal, Node> {
 
-		typedef Map<LocalOrdinal, GlobalOrdinal, Node> map_type;
-		typedef MultiVector<Scalar, LocalOrdinal, GlobalOrdinal> multivector_type;
-		typedef Matrix< Scalar, LocalOrdinal, GlobalOrdinal, Node > matrix_type;
-		typedef MatrixSplitting<Scalar,LocalOrdinal,GlobalOrdinal,Node,Xpetra::UseTpetra, false> tpetra_splitting;
-		typedef MueLu::Hierarchy<Scalar,LocalOrdinal,GlobalOrdinal,Node> Hierarchy;
-		typedef MultiVectorFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node> mv_factory_type;
-		typedef Level<Scalar,LocalOrdinal,GlobalOrdinal,Node> level;
+  typedef Map<LocalOrdinal, GlobalOrdinal, Node> map_type;
+  typedef MultiVector<Scalar, LocalOrdinal, GlobalOrdinal> multivector_type;
+  typedef Matrix< Scalar, LocalOrdinal, GlobalOrdinal, Node > matrix_type;
+  typedef MatrixSplitting<Scalar,LocalOrdinal,GlobalOrdinal,Node,Xpetra::UseTpetra, false> tpetra_splitting;
+  typedef MueLu::Hierarchy<Scalar,LocalOrdinal,GlobalOrdinal,Node> Hierarchy;
+  typedef MultiVectorFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node> mv_factory_type;
+  typedef Level<Scalar,LocalOrdinal,GlobalOrdinal,Node> level;
 
-		public: 
+public:
 
-			//! Constructors
-			//@{
+  //! Constructors
+  //@{
 
-			RegionAMG()
-			{
-				std::cout<<"This version of constructor is not implemented yet"<<std::endl;
-			};
-			
-			//! This is the actual constructor we are interested in calling
-			RegionAMG( const char* , const char* , RCP<const Teuchos::Comm<int> >, Teuchos::ParameterList, GlobalOrdinal, GlobalOrdinal );
+  RegionAMG()
+{
+    std::cout<<"This version of constructor is not implemented yet"<<std::endl;
+};
 
-			//@}
+  //! This is the actual constructor we are interested in calling
+  RegionAMG( const char* , const char* , RCP<const Teuchos::Comm<int> >, Teuchos::ParameterList, GlobalOrdinal, GlobalOrdinal );
 
-			GlobalOrdinal GetNumLevels(){return num_levels_;}
+  //@}
 
-			//! Methods to extract Map information
-			//@{
-			//! For now, the domain Map coincides with the Domain Map of the composite matrix at the fine level
-			virtual RCP<const map_type > getDomainMap()const{return domainMap_;} 
+  GlobalOrdinal GetNumLevels(){return num_levels_;}
 
-			//! For now, the domain Map coincides with the Range Map of the composite matrix at the fine level
-			virtual RCP<const map_type > getRangeMap()const{return rangeMap_;}
+  //! Methods to extract Map information
+  //@{
+  //! For now, the domain Map coincides with the Domain Map of the composite matrix at the fine level
+  virtual RCP<const map_type > getDomainMap()const{return domainMap_;}
 
-			//@}
-			
-			//! Apply method
-			//@{
-			//!N.B.: The implementation still has to be finished
-			virtual void
-				apply (const multivector_type& X, multivector_type& Y, 
-				Teuchos::ETransp mode = Teuchos::NO_TRANS, 
-				Scalar alpha = Teuchos::ScalarTraits<Scalar>::one(), 
-				Scalar beta = Teuchos::ScalarTraits<Scalar>::zero())const; 
-			//@}
+  //! For now, the domain Map coincides with the Range Map of the composite matrix at the fine level
+  virtual RCP<const map_type > getRangeMap()const{return rangeMap_;}
 
-			//! hasTransposeApply should not be needed
-			virtual bool hasTransposeApply() const { return false; }
+  //@}
 
-		private:
+  //! Apply method
+  //@{
+  //!N.B.: The implementation still has to be finished
+  virtual void
+  apply (const multivector_type& X, multivector_type& Y,
+      Teuchos::ETransp mode = Teuchos::NO_TRANS,
+      Scalar alpha = Teuchos::ScalarTraits<Scalar>::one(),
+      Scalar beta = Teuchos::ScalarTraits<Scalar>::zero())const;
+  //@}
 
-			//! Private variables
-			//@{
+  //! hasTransposeApply should not be needed
+  virtual bool hasTransposeApply() const { return false; }
 
-			//Total number of levels in the hierarchy
-			GlobalOrdinal num_levels_ = -1;
+private:
 
-			//Total number of regions
-			GlobalOrdinal num_regions_ = -1;
+  //! Private variables
+  //@{
 
-			//Coarsening factor to transfer quantities across levels
-			int coarsening_factor_ = -1;
+  //Total number of levels in the hierarchy
+  GlobalOrdinal num_levels_ = -1;
 
-			RCP<const Teuchos::Comm<int> > comm_;
+  //Total number of regions
+  GlobalOrdinal num_regions_ = -1;
 
-			Teuchos::ParameterList muelu_;
+  //Coarsening factor to transfer quantities across levels
+  int coarsening_factor_ = -1;
 
-			RCP<const map_type > domainMap_;
-			RCP<const map_type > rangeMap_;
+  RCP<const Teuchos::Comm<int> > comm_;
 
-			//matrixSplitting associated with the composite matrix at the fine level
-			RCP<tpetra_splitting> matrixSplitting_;
+  Teuchos::ParameterList muelu_;
 
-			//Array of MueLu hierarchies (one for each region)
-			Array<RCP<Hierarchy> > regionHierarchies_;
+  RCP<const map_type > domainMap_;
+  RCP<const map_type > rangeMap_;
 
-			//Array of levels in the new hierarchy (each levels contains quantities associaed with every region for that level)
-			Array<RCP<level> > levels_;
+  //matrixSplitting associated with the composite matrix at the fine level
+  RCP<tpetra_splitting> matrixSplitting_;
 
-			//@}
+  //Array of MueLu hierarchies (one for each region)
+  Array<RCP<Hierarchy> > regionHierarchies_;
 
-			//@Private Methods
-			//
-			//This methods construct the Hierarchy using the quantities stored in the MueLu::Hierarchy objects
-			//There are as many MueLu::Hierarchy objects as the number of geometric regions partitioning the domain
-			void SetUpHierarchy();
+  //Array of levels in the new hierarchy (each levels contains quantities associaed with every region for that level)
+  Array<RCP<level> > levels_;
 
-			//This method is called in SetUpHierarchy and it is in chanrge of extracting quantities from MueLu::Hierarchy objects
-			//and store them inside Xpetra::Level objects
-			void DefineLevels();
+  //@}
 
-			//Method to create region input multivectors from the composite input multivector
-			void computeRegionX(const multivector_type& , Array<RCP<multivector_type> > )const;
+  //@Private Methods
+  //
+  //This methods construct the Hierarchy using the quantities stored in the MueLu::Hierarchy objects
+  //There are as many MueLu::Hierarchy objects as the number of geometric regions partitioning the domain
+  void SetUpHierarchy();
 
-			//Method to create composite output multivector from region output multivectors
-			void computeCompositeY(Array<RCP<const multivector_type> >, multivector_type& )const;
+  //This method is called in SetUpHierarchy and it is in chanrge of extracting quantities from MueLu::Hierarchy objects
+  //and store them inside Xpetra::Level objects
+  void DefineLevels();
 
-			//This method detects entries in region output mutlivectors associated with mesh nodes that lie on interregion interfaces
-			//and it scales them. The scaling factor is equal to the number of regions that the given degrees of freedom are shared by
-			void rescaleInterfaceEntries( Array<RCP<multivector_type> > )const;
+  //Method to create region input multivectors from the composite input multivector
+  void computeRegionX(const multivector_type& , Array<RCP<multivector_type> > )const;
 
-			virtual void regionToAllCoarsen(const level&, level& );
+  //Method to create composite output multivector from region output multivectors
+  void computeCompositeY(Array<RCP<const multivector_type> >, multivector_type& )const;
 
-			//@}
+  //This method detects entries in region output mutlivectors associated with mesh nodes that lie on interregion interfaces
+  //and it scales them. The scaling factor is equal to the number of regions that the given degrees of freedom are shared by
+  void rescaleInterfaceEntries( Array<RCP<multivector_type> > )const;
 
-	};
+  virtual void regionToAllCoarsen(const level&, level& );
+
+  //@}
+
+};
 
 } //namespace Xpetra
 
