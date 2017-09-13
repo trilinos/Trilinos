1f1903fa42d043518c22d4ee713ae712792634a1
Andrew M. Bradley <ambradl@sandia.gov>
2017-09-12 18:45:51 -0600

    Ifpack2: Fix LocalSparseTriangularSolver's use of HTS in complex case.
    Handle Kokkos::complex<T> as impl_scalar_type when std::complex<T> is
    scalar_type. In this case, it is done with a reinterpret_cast of the MV raw
    pointer.
    
    Build/Test Cases Summary
    Enabled Packages: Ifpack2
    Disabled Packages: PyTrilinos,Claps,TriKota
    Enabled all Forward Packages
    0) MPI_RELEASE_DEBUG_SHARED_PT => passed: passed=745,notpassed=0 (132.58 min)

--- packages/ifpack2/src/Ifpack2_LocalSparseTriangularSolver_def.hpp
+++ packages/ifpack2/src/Ifpack2_LocalSparseTriangularSolver_def.hpp
@@ -182,7 +182,13 @@ public:
     // Only does something if #rhs > current capacity.
     HTST::reset_max_nrhs(Timpl_.get(), X_view.dimension_1());
     // Switch alpha and beta because of HTS's opposite convention.
-    HTST::solve_omp(Timpl_.get(), X_view.data(), X_view.dimension_1(), Y_view.data(), beta, alpha);
+    HTST::solve_omp(Timpl_.get(),
+                    // For std/Kokkos::complex.
+                    reinterpret_cast<const scalar_type*>(X_view.data()),
+                    X_view.dimension_1(),
+                    // For std/Kokkos::complex.
+                    reinterpret_cast<scalar_type*>(Y_view.data()),
+                    beta, alpha);
 #endif
   }
 
