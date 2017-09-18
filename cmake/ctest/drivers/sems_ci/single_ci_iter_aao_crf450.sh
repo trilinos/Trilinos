f23a9de7596ec75f1f030fb5a7fbad6146e0955b
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-09-17 20:05:58 -0600

    Point to right aao CTest -S script (#1746)
    In my copy and paste, I forgot to point to the right ctest -S script.

--- cmake/ctest/drivers/sems_ci/single_ci_iter_aao_crf450.sh
+++ cmake/ctest/drivers/sems_ci/single_ci_iter_aao_crf450.sh
@@ -15,7 +15,6 @@ export PATH=/home/vera_env/common_tools/master-20170917-214d0ce/bin:$PATH
 
 export CTEST_DASHBOARD_ROOT=$PWD
 
-ctest -V -S $DRIVER_SCRIPT_DIR/ctest_linux_mpi_debug_shared_pt_ci.sems.cmake
+ctest -V -S $DRIVER_SCRIPT_DIR/ctest_linux_mpi_debug_shared_pt_ci_aao.sems.cmake
 
-# ToDo: Put back the simple CI logic!
-
+# ToDo: Put back the simple CI logic based on changed file!
