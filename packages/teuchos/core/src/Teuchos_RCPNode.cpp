ae329af50662e1a2e1fa6ca72d7b45c2e696c75d
Chris Siefert <csiefer@sandia.gov>
2017-09-20 10:58:52 -0600

    Teuchos: More int/size_t warning cleanup

--- packages/teuchos/core/src/Teuchos_RCPNode.cpp
+++ packages/teuchos/core/src/Teuchos_RCPNode.cpp
@@ -329,7 +329,7 @@ int RCPNodeTracer::numActiveRCPNodes()
 {
   // This list always exists, no matter debug or not so just access it.
   TEUCHOS_TEST_FOR_EXCEPT(0==rcp_node_list());
-  return rcp_node_list()->size();
+  return static_cast<int>(rcp_node_list()->size());
 }
 
 
