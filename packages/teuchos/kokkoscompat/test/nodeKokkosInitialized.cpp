c466d69d1727dae9348b2dd5b7312eda797adc41
Mark Hoemmen <mhoemme@sandia.gov>
2017-10-25 10:02:06 -0600

    Tpetra: Fix #1889
    @trilinos/tpetra Simplify how Node constructors initialize and
    finalize Kokkos.  In particular, get rid of the "count" and use
    std::call_once + atexit; this ensures the following:
    
      - Kokkos initialization will happen at most once
      - It won't matter whether users or Tpetra create Node instances
    
    It will also make the ultimate deprecation and removal of Node easier.
    
    This commit includes three tests that exercise different combinations
    of calling Kokkos::initialize (or not) vs. creating Node instances.  I
    have tested (build and tests) with all four Kokkos execution spaces
    that Tpetra supports: Cuda, OpenMP, Serial, and Threads.

---
+++ packages/teuchos/kokkoscompat/test/nodeKokkosInitialized.cpp
@@ -0,0 +1,79 @@
+#include "KokkosCompat_ClassicNodeAPI_Wrapper.hpp"
+#include "Kokkos_Core.hpp"
+#include <iostream>
+#include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
+
+// Test that Node does not initializes Kokkos if it is already
+// initialized, and does not attempt to double-finalize Kokkos
+// in this case.
+
+namespace { // (anonymous)
+
+// Is Kokkos initialized?
+//
+// https://github.com/kokkos/kokkos/issues/1184 means that this
+// function doesn't return something meaningful with Kokkos::Serial.
+bool isKokkosInitialized ()
+{
+  // When https://github.com/kokkos/kokkos/issues/1060 is fixed, use
+  // Kokkos::is_initialized here, instead of asking the default
+  // execution space whether it is initialized.
+  typedef Kokkos::DefaultExecutionSpace::execution_space
+    execution_space;
+  return execution_space::is_initialized ();
+}
+
+} // namespace (anonymous)
+
+int
+main (int argc, char* argv[])
+{
+  using std::cerr;
+  using std::endl;
+  typedef Kokkos::DefaultExecutionSpace execution_space;
+  typedef Kokkos::Compat::KokkosDeviceWrapperNode<execution_space>
+    node_type;
+
+  Kokkos::initialize (argc, argv);
+  if (! isKokkosInitialized ()) {
+    cerr << "FAILED: After calling Kokkos::initialize, "
+      "Kokkos is still not initialized!" << endl;
+    return EXIT_FAILURE;
+  }
+
+  // Create a node instance.  Make sure that this does not make Kokkos
+  // raise an exception, e.g., due to double initialization.
+  node_type node1;
+
+  if (! isKokkosInitialized ()) {
+    cerr << "FAILED: After calling Kokkos::initialize, "
+      "and after calling Node's constructor once, "
+      "Kokkos is still not initialized!" << endl;
+    return EXIT_FAILURE;
+  }
+  {
+    node_type node2; // inner scope, so its destructor gets invoked
+    if (! isKokkosInitialized ()) {
+      cerr << "FAILED: After calling Kokkos::initialize, "
+        "and after calling Node's constructor twice, "
+        "Kokkos is still not initialized!" << endl;
+      return EXIT_FAILURE;
+    }
+  }
+  if (! isKokkosInitialized ()) {
+    cerr << "FAILED: After calling Kokkos::initialize, "
+      "after calling Node's constructor twice, "
+      "and after calling Node's destructor once, "
+      "Kokkos is still not initialized!" << endl;
+    return EXIT_FAILURE;
+  }
+
+  Kokkos::finalize ();
+  // Node's destructor will be called after Kokkos::finalize.  We thus
+  // implicitly test whether this makes Kokkos raise an exception.
+  // Don't print PASSED!  That will make the test think that it
+  // passed.  We use FAIL_REGULAR_EXPRESSION (see CMakeLists.txt) so
+  // that the test fails if and only if it prints "FAILED:".
+  return EXIT_SUCCESS;
+}
+
---
+++ packages/teuchos/kokkoscompat/test/nodeKokkosNotInitializedInnerScope.cpp
@@ -0,0 +1,98 @@
+#include "KokkosCompat_ClassicNodeAPI_Wrapper.hpp"
+#include "Kokkos_Core.hpp"
+#include <iostream>
+#include <cstdlib> // EXIT_SUCCESS, EXIT_FAILURE
+#ifdef KOKKOS_ENABLE_SERIAL
+#  include <type_traits>
+#endif // KOKKOS_ENABLE_SERIAL
+
+// Test that Node initializes Kokkos if it is not already initialized.
+// It must also finalize Kokkos at exit, but the only portable way to
+// test that would be to run Valgrind and ensure no memory leaks.
+
+namespace { // (anonymous)
+
+// Is Kokkos initialized?
+//
+// https://github.com/kokkos/kokkos/issues/1184 means that this
+// function doesn't return something meaningful with Kokkos::Serial.
+bool isKokkosInitialized ()
+{
+  // When https://github.com/kokkos/kokkos/issues/1060 is fixed, use
+  // Kokkos::is_initialized here, instead of asking the default
+  // execution space whether it is initialized.
+  typedef Kokkos::DefaultExecutionSpace::execution_space
+    execution_space;
+  return execution_space::is_initialized ();
+}
+
+} // namespace (anonymous)
+
+int
+main (int argc, char* argv[])
+{
+  using std::cerr;
+  using std::endl;
+  typedef Kokkos::DefaultExecutionSpace execution_space;
+  typedef Kokkos::Compat::KokkosDeviceWrapperNode<execution_space>
+    node_type;
+#ifdef KOKKOS_ENABLE_SERIAL
+  constexpr bool defaultExecSpaceIsSerial =
+    std::is_same<execution_space, Kokkos::Serial>::value;
+#else
+  constexpr bool defaultExecSpaceIsSerial = false;
+#endif // KOKKOS_ENABLE_SERIAL
+
+  if (! defaultExecSpaceIsSerial) {
+    // https://github.com/kokkos/kokkos/issues/1184 means that this
+    // function doesn't return something meaningful with
+    // Kokkos::Serial.
+    if (isKokkosInitialized ()) {
+      cerr << "FAILED: "
+        "Before calling Node's constructor, "
+        "and without having called Kokkos::initialize ourselves, "
+        "Kokkos claims to be initialized!" << endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  {
+    // Create a node instance.  Make sure that this does not make
+    // Kokkos raise an exception, e.g., due to double initialization.
+    // Do this in an inner (within main()) scope, so we can test that
+    // case.  We'll have another test for where node instances are
+    // always at main() scope.
+    node_type node1;
+
+    if (! isKokkosInitialized ()) {
+      cerr << "FAILED: After calling Kokkos::initialize, "
+        "and after calling Node's constructor once, "
+        "Kokkos is still not initialized!" << endl;
+      return EXIT_FAILURE;
+    }
+    {
+      node_type node2; // inner scope, so its destructor gets invoked
+      if (! isKokkosInitialized ()) {
+        cerr << "FAILED: After calling Kokkos::initialize, "
+          "and after calling Node's constructor twice, "
+          "Kokkos is still not initialized!" << endl;
+        return EXIT_FAILURE;
+      }
+    }
+    if (! isKokkosInitialized ()) {
+      cerr << "FAILED: After calling Kokkos::initialize, "
+        "after calling Node's constructor twice, "
+        "and after calling Node's destructor once, "
+        "Kokkos is still not initialized!" << endl;
+      return EXIT_FAILURE;
+    }
+  }
+
+  // Don't print PASSED!  That will make the test think that it
+  // passed, even though things may still be happening (due to atexit)
+  // after main() exits.  We use FAIL_REGULAR_EXPRESSION (see
+  // CMakeLists.txt) so that the test fails if and only if it prints
+  // "FAILED:".
+  return EXIT_SUCCESS;
+}
+
