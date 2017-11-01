5bad881252fa1beab0679490672669a2eb11d3b0
Greg von Winckel <gvonwin@sandia.gov>
2017-11-01 11:53:37 -0600

    Added code to print results to file and python script for plotting

--- packages/rol/tutorial/rocket-simopt/Rocket.cpp
+++ packages/rol/tutorial/rocket-simopt/Rocket.cpp
@@ -1,9 +1,53 @@
+// @HEADER
+// ************************************************************************
+//
+//               Rapid Optimization Library (ROL) Package
+//                 Copyright (2014) Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000, there is a non-exclusive
+// license for use of this work by or on behalf of the U.S. Government.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact lead developers:
+//              Drew Kouri   (dpkouri@sandia.gov) and
+//              Denis Ridzal (dridzal@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+
 #include "Rocket.hpp"
 #include "ROL_OptimizationSolver.hpp"
 #include "ROL_Reduced_Objective_SimOpt.hpp"
-
 #include "Teuchos_XMLParameterListHelpers.hpp"
 #include <iostream>
+#include <fstream>
+#include <iomanip>
 
 int main( int argc, char* argv[] ) {
 
@@ -47,10 +91,15 @@ int main( int argc, char* argv[] ) {
   // Compute solution for constant burn rate
   con->update_2(*z);
   con->solve(*l, *u, *z, tol);
+
+  auto ucb_rcp = rcp( new vector(N) );
+  auto ucb = rcp( new ROL::StdVector<double>(ucb_rcp) );
+  ucb->set(*u);
+
   //  u->print(std::cout); 
   double htarg = w->dot(*u);  // Final height 
 
-  auto obj = rcp( new Rocket::Objective( N, T, mt, htarg, mu, w ) );
+  auto obj  = rcp( new Rocket::Objective( N, T, mt, htarg, mu, w ) );
   auto robj = rcp( new ROL::Reduced_Objective_SimOpt<double>( obj, con, u, z, l ) );
 
   // Full space problem
@@ -59,14 +108,52 @@ int main( int argc, char* argv[] ) {
   ROL::OptimizationProblem<double> opt( robj, z ); 
   ROL::OptimizationSolver<double> solver( opt, *parlist );
   solver.solve( std::cout );
-  
   con->update_2(*z);
   con->solve(*l, *u, *z, tol);
-  //  u->print(std::cout); 
-  std::cout << "target height = " << htarg << 
-               ", actual = " << w->dot(*u) << std::endl;
-  std::cout << "Initial fuel mass   = " << mf << std::endl;
-  std::cout << "Remaining fuel mass = " << mf-e->dot(*z) << std::endl;
 
+  std::ofstream os("Rocket.txt");
+  if( os.is_open() ) {
+  
+    os << std::setw(12) << "Time" 
+       << std::setw(18) << "Burn_Rate_(const)" 
+       << std::setw(16) << "Height_(const)" 
+       << std::setw(16) << "Burn_Rate_(opt)" 
+       << std::setw(16) << "Height_(opt)" << std::endl;
+
+    vector hcb(N,0);
+    vector hopt(N,0); 
+    
+    os << std::setw(12) << 0
+       << std::setw(18) << 0
+       << std::setw(16) << 0
+       << std::setw(16) << 0
+       << std::setw(16) << 0 << std::endl;
+
+    os << std::setw(12) << 0
+       << std::setw(18) << mf/T
+       << std::setw(16) << 0
+       << std::setw(16) << (*z_rcp)[0]
+       << std::setw(16) << 0 << std::endl;
+
+    hcb[0]  = 0.5*dt*(*ucb_rcp)[0];
+    hopt[0] = 0.5*dt*(*u_rcp)[0];
+
+    for( int i=1; i<N; ++i ) {
+      hcb[i]  = hcb[i-1]  + 0.5*dt*( (*ucb_rcp)[i] + (*ucb_rcp)[i-1] );
+      hopt[i] = hopt[i-1] + 0.5*dt*( (*u_rcp)[i]   + (*u_rcp)[i-1] );
+
+      os << std::setw(12) << i*dt
+         << std::setw(18) << mf/T
+         << std::setw(16) << hcb[i]
+         << std::setw(16) << (*z_rcp)[i-1]
+         << std::setw(16) << hopt[i] << std::endl;
+
+      os << std::setw(12) << i*dt
+         << std::setw(18) << mf/T
+         << std::setw(16) << hcb[i]
+         << std::setw(16) << (*z_rcp)[i]
+         << std::setw(16) << hopt[i] << std::endl;
+    }
+  }
   return 0;
 }
