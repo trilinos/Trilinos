c3930c894ac28dc76fcabe18b1239bc35d828ffa
Roscoe A. Bartlett <rabartl@sandia.gov>
2017-12-01 16:57:44 -0700

    Skip TriBITS setting C++11 and OpenMP flags when Kokkos flags are used (#1400)
    This required a small change to TriBITS to allow the OpenMP flags to be simply
    skipped.
    
    I also adde an if statement to avoid adding any of the Kokkos flags or
    changing any of this logic when KOKKOS_ARCH is equal to "None".  This is
    likely important to maintain backward compatiblity.

--- cmake/ProjectCompilerPostConfig.cmake
+++ cmake/ProjectCompilerPostConfig.cmake
@@ -1,4 +1,4 @@
-IF(${Trilinos_ENABLE_Kokkos})
+IF (${Trilinos_ENABLE_Kokkos})
 
   # This is where to generate the gen_kokkos.cmake and KokkosCore_config.h 
   # that we will use in the configuration
@@ -30,17 +30,32 @@ IF(${Trilinos_ENABLE_Kokkos})
   )
   include(${Kokkos_GEN_DIR}/kokkos_generated_settings.cmake)
 
-  # Convert CMakeList into string for CXX_FLAGS
-  set(CMAKE_CXX_FLAGSl "")
-  foreach(opt ${KOKKOS_CXX_FLAGS})
-    set(CMAKE_CXX_FLAGSl "${CMAKE_CXX_FLAGSl} ${opt}")
-  endforeach()
+  IF (NOT KOKKOS_ARCH STREQUAL "None")
 
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGSl}")
+    # Convert CMakeList into string for CXX_FLAGS
+    set(CMAKE_CXX_FLAGSl "")
+    foreach(opt ${KOKKOS_CXX_FLAGS})
+      set(CMAKE_CXX_FLAGSl "${CMAKE_CXX_FLAGSl} ${opt}")
+    endforeach()
+  
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CMAKE_CXX_FLAGSl}")
+  
+    # TODO -- need to remove the -lkokkos.  Check on LDFlags
+    #set(KOKKOS_LINK_DEPENDS libkokkos.a CACHE STRING "")
+    #set(KOKKOS_LIBS -lkokkos -ldl -lpthread CACHE STRING "")
+    #set(KOKKOS_LDFLAGS -L/scr_gabrielle/kruger/builds-ptsolve/trilinos/par2 --gcc-toolchain=/usr CACHE STRING "")
+  
+    MESSAGE("-- " "Skip adding flags for C++11 because Kokkos flags does that ...")
+    SET(${PROJECT_NAME}_CXX11_FLAGS " ")
+  
+    MESSAGE("-- " "Skip adding flags for OpenMP because Kokkos flags does that ...")
+    SET(OpenMP_CXX_FLAGS_OVERRIDE " ")
+  
+  ENDIF()
 
-  # TODO -- need to remove the -lkokkos.  Check on LDFlags
-  #set(KOKKOS_LINK_DEPENDS libkokkos.a CACHE STRING "")
-  #set(KOKKOS_LIBS -lkokkos -ldl -lpthread CACHE STRING "")
-  #set(KOKKOS_LDFLAGS -L/scr_gabrielle/kruger/builds-ptsolve/trilinos/par2 --gcc-toolchain=/usr CACHE STRING "")
+  # Above, It is important not to distrube the default configuraiton of
+  # Trilinos if KOKKOS_ARCH is not set.  But the implementation of the new
+  # Kokkos TriBITS CMake files requires kokkos_generated_settings.cmake be
+  # included.
 
 ENDIF()
