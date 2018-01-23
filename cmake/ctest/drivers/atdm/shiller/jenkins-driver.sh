2853beb3bfadeccce032c07eff821e7f7456e893
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-01-23 15:53:35 -0700

    Set timeout limit with default of 180 min (TRIL-171)

--- cmake/ctest/drivers/atdm/shiller/jenkins-driver.sh
+++ cmake/ctest/drivers/atdm/shiller/jenkins-driver.sh
@@ -1,6 +1,11 @@
 #!/bin/bash -l
 
+if [ "${SRUN_TIME_LIMIT_MIN}" == "" ] ; then
+  SRUN_TIME_LIMIT_MIN=180  # Default limit is 3 hours
+fi
+
 /usr/bin/srun -N 1 --constraint=k80 -J $JOB_NAME \
+  --time-min=${SRUN_TIME_LIMIT_MIN} \
   $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/shiller/srun-job.sh
 
 #   -e output.error
