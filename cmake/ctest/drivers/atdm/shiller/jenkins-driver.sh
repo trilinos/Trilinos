40c01359cd2318a2305f39d2bdc66a456550c20c
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-01-23 16:20:19 -0700

    Echo and run srun command (TRIL-171)

--- cmake/ctest/drivers/atdm/shiller/jenkins-driver.sh
+++ cmake/ctest/drivers/atdm/shiller/jenkins-driver.sh
@@ -4,6 +4,8 @@ if [ "${SRUN_TIME_LIMIT_MIN}" == "" ] ; then
   SRUN_TIME_LIMIT_MIN=180  # Default limit is 3 hours
 fi
 
+set -x
+
 /usr/bin/srun -N 1 --constraint=k80 -J $JOB_NAME \
   --time-min=${SRUN_TIME_LIMIT_MIN} \
   $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/shiller/srun-job.sh
