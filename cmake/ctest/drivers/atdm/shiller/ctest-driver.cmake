cdb17b842fab934a8242f25980c8204a114a12b1
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-01-24 06:49:51 -0700

    Fix some comments (TRIL-171)

--- cmake/ctest/drivers/atdm/shiller/ctest-driver.cmake
+++ cmake/ctest/drivers/atdm/shiller/ctest-driver.cmake
@@ -13,13 +13,13 @@ SET( CTEST_NOTES_FILES
   "${CMAKE_CURRENT_LIST_FILE}"
   )
 
-# Shiller and Hansen nodes have 16 cores which cant support 2 threads each.
+# Shiller and Hansen nodes have 16 cores which can support 2 threads each.
 # For building, we use all 32 threads.  For running tests, we assume to use
-# all of the threads as well.
+# all of the threads as well with 2 threads per core with an OpenMP build.
 SET(CTEST_BUILD_FLAGS "-j32 -k")
 SET(CTEST_PARALLEL_LEVEL "16")  # Should be set to 32 if using only 1 thread?
 
-# Extra configure options yet
+# No extra configure options set!
 SET(EXTRA_CONFIGURE_OPTIONS)
 
 # CDash Group
