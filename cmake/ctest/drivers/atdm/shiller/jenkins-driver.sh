bb225951c25eee4dbd8863f4933077e756d05de6
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-01-26 09:21:53 -0700

    Change srun --time-min to --time for timeout (TRIL-171)
    Now it really does timeout the build ased on the env var
    SRUN_CTEST_TIME_LIMIT_MINUTES will set the relative runtime.

--- cmake/ctest/drivers/atdm/shiller/jenkins-driver.sh
+++ cmake/ctest/drivers/atdm/shiller/jenkins-driver.sh
@@ -1,13 +1,13 @@
 #!/bin/bash -l
 
-if [ "${SRUN_TIME_LIMIT_MIN}" == "" ] ; then
-  SRUN_TIME_LIMIT_MIN=180  # Default limit is 3 hours
+if [ "${SRUN_CTEST_TIME_LIMIT_MINUTES}" == "" ] ; then
+  SRUN_CTEST_TIME_LIMIT_MINUTES=180  # Default limit is 3 hours
 fi
 
 set -x
 
 /usr/bin/srun -N 1 --constraint=k80 -J $JOB_NAME \
-  --time-min=${SRUN_TIME_LIMIT_MIN} \
+  --time=${SRUN_CTEST_TIME_LIMIT_MINUTES} \
   $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/shiller/srun-job.sh
 
 #   -e output.error
