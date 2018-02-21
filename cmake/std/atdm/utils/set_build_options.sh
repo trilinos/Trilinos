025518116cd9b4e0ae971e3aef5336dc16538292
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-02-21 09:18:14 -0700

    Update for EM-Plasma/BuildScripts at tril-171-baseline-20180215 (TRIL-171)
    This updates the rest of the ATDM configuration at the EM-Plasma/BuildScripts
    repo version:
    
        This was copied over directly from the
        EM-Plasma/BuildScripts/ALL/set_build_options.sh file at the tagged version:
    
          tag tril-171-baseline-20180215
          Tagger: Roscoe A. Bartlett <rabartl@sandia.gov>
          Date:   Thu Feb 15 12:20:56 2018 -0700
    
          Basdline for EMPIRE testing on 2018-02-15 (TRIL-171)
    
          commit 0d7578c1de8fcbd2e1eddcfc0154389aa7bd6324
          Merge: 3e306bd c9f97d4
          Author: Jason M. Gates <jmgate@sandia.gov>
          Date:   Wed Feb 14 09:14:56 2018 -0700
    
              Merge branch 'master' into develop
    
              Pull some commits accidentally made to the master branch into develop.
    
    In addition to applying these updates, I made a number of other changes/fixes
    as well:
    
    * Set TPL_ENABLE_CUDA and TPL_ENABLE_CUSPARSE correctly: When I created this
    file from BuildScripts/ALL/configure-trilinos.sh I forgot to set this var.
    
    * Fix up some vars to match EMPIRE build: After a careful comparison of the
    EMPIRE and ATDM configurations (by comparing cleaned up, sorted, and filtered
    CMakeCache.txt files), I think I have got the exact same options being used
    for at least the gnu-debug-openmp build that I worked with.
    
    * Allow DLib to be tentatively enabled: This is how the
    EM-Plasma/BuildScripts/ALL/configure-trilinos.sh script does it so this ATDM
    configuration should do that too.  I guess not every ATDM machine will have
    the dlib lib?
    
    * Remove Panzer/EMPIRE and install specific stuff: This stuff is not right for
    SPARC or other projects and it cluters up the scripts.  This info will need to
    be added in some other way once EMPIRE adopts this build.
    
    * Namespace env vars with ATDM_CONFIG_: Not good to have simple non-namespaced
      vars.  And you can just run 'set | grep ATDM_CONFIG_' to see what is set.
    
    * Allow enable of SEACASAprepo and other SEACAS subppackages that EMPIRE is
      enabling to be enabled when SEACAS is enabled.
    
    * Disable ForTrilinos explicitly: I am doing this to avoid the NOTE about it
      being disabled.
    
    * Set OMPI_CC and OPMI_FC for GNU and CUDA compilers: This is needed in case
      you load an Intel env first which sets OPMI_CC and OMPI_FC and then try to
      load GNU or CUDA after that.  This makes it easier to switch configurations
      without having to blow away your shell.

--- cmake/std/atdm/utils/set_build_options.sh
+++ cmake/std/atdm/utils/set_build_options.sh
@@ -1,38 +1,45 @@
-#!/bin/bash
-echo Setting default compiler and build options
-# Set the defaults
-export COMPILER=GNU
-export BUILD_TYPE=DEBUG
-export USE_OPENMP=OFF
-export USE_CUDA=OFF
-export USE_PTHREADS=OFF
-export PROJECT=PANZER
+# Assert this script is sourced, not run!
+called=$_
+if [ "$called" == "$0" ] ; then
+  echo "This script '$0' is being called.  Instead, it must be sourced!"
+  exit 1
+fi
 
-# Set the project name
-if [[ $JOB_NAME == *"Trilinos"* ]]; then export PROJECT=PANZER; fi
-if [[ $JOB_NAME == *"PIC"* ]];      then export PROJECT=EMPIRE-PIC; fi
+# Assert that JOB_NAME is set!
+if [ -z "$JOB_NAME" ] ; then
+  echo "Error, must set JOB_NAME in env!"
+  return
+fi
+
+echo "Setting default compiler and build options for JOB_NAME='${JOB_NAME}'"
+
+# Set the defaults
+export ATDM_CONFIG_COMPILER=GNU
+export ATDM_CONFIG_BUILD_TYPE=DEBUG
+export ATDM_CONFIG_USE_OPENMP=OFF
+export ATDM_CONFIG_USE_CUDA=OFF
+export ATDM_CONFIG_USE_PTHREADS=OFF
 
 # Set the compiler
-if [[ $JOB_NAME == *"gnu"* ]];   then COMPILER=GNU; fi
-if [[ $JOB_NAME == *"intel"* ]]; then COMPILER=INTEL; fi
-if [[ $JOB_NAME == *"cuda"* ]];  then COMPILER=CUDA; fi
-if [[ $JOB_NAME == *"clang"* ]]; then COMPILER=CLANG; fi
-
+if [[ $JOB_NAME == *"gnu"* ]];   then ATDM_CONFIG_COMPILER=GNU; fi
+if [[ $JOB_NAME == *"intel"* ]]; then ATDM_CONFIG_COMPILER=INTEL; fi
+if [[ $JOB_NAME == *"cuda"* ]];  then ATDM_CONFIG_COMPILER=CUDA; fi
+if [[ $JOB_NAME == *"clang"* ]]; then ATDM_CONFIG_COMPILER=CLANG; fi
 
 # Set the optimization level
 # Defaults to debug
-if [[ $JOB_NAME == *"opt"* ]]; then export BUILD_TYPE=RELEASE; fi
+if [[ $JOB_NAME == *"opt"* ]]; then export ATDM_CONFIG_BUILD_TYPE=RELEASE; fi
 
 # Set the node types default to serial
-NODE_TYPE=SERIAL
-if [[ $JOB_NAME == *"openmp"* ]]; then export USE_OPENMP=ON;   NODE_TYPE=OPENMP; fi
-if [[ $JOB_NAME == *"cuda"* ]];   then export USE_CUDA=ON;     NODE_TYPE=CUDA;   fi
-if [[ $JOB_NAME == *"pthread"* ]];then export USE_PTHREADS=ON; NODE_TYPE=THREAD; fi
-if [[ $JOB_NAME == *"serial"* ]];then  NODE_TYPE=SERIAL; fi
+ATDM_CONFIG_NODE_TYPE=SERIAL
+if [[ $JOB_NAME == *"openmp"* ]]; then export ATDM_CONFIG_USE_OPENMP=ON;   ATDM_CONFIG_NODE_TYPE=OPENMP; fi
+if [[ $JOB_NAME == *"cuda"* ]];   then export ATDM_CONFIG_USE_CUDA=ON;     ATDM_CONFIG_NODE_TYPE=CUDA;   fi
+if [[ $JOB_NAME == *"pthread"* ]];then export ATDM_CONFIG_USE_PTHREADS=ON; ATDM_CONFIG_NODE_TYPE=THREAD; fi
+if [[ $JOB_NAME == *"serial"* ]];then  ATDM_CONFIG_NODE_TYPE=SERIAL; fi
 
-if [[ $USE_OPENMP == "ON" && $USE_CUDA == "ON" ]]; then echo "Can't set more than one backend"; return 1; fi
-if [[ $USE_OPENMP == "ON" && $USE_PTHREADS == "ON" ]]; then echo "Can't set more than one backend"; return 1; fi
-if [[ $USE_OPENMP == "ON" && $NODE_TYPE == "SERIAL" ]]; then echo "Can't set more than one backend"; return 1; fi
-if [[ $USE_CUDA == "ON" && $USE_PTHREADS == "ON" ]]; then echo "Can't set more than one backend"; return 1; fi
-if [[ $USE_CUDA == "ON" && $NODE_TYPE == "SERIAL" ]]; then echo "Can't set more than one backend"; return 1; fi
-if [[ $USE_PTHREADS == "ON" && $NODE_TYPE == "SERIAL" ]]; then echo "Can't set more than one backend"; return 1; fi
+if [[ $ATDM_CONFIG_USE_OPENMP == "ON" && $ATDM_CONFIG_USE_CUDA == "ON" ]]; then echo "Can't set more than one backend"; return 1; fi
+if [[ $ATDM_CONFIG_USE_OPENMP == "ON" && $ATDM_CONFIG_USE_PTHREADS == "ON" ]]; then echo "Can't set more than one backend"; return 1; fi
+if [[ $ATDM_CONFIG_USE_OPENMP == "ON" && $ATDM_CONFIG_NODE_TYPE == "SERIAL" ]]; then echo "Can't set more than one backend"; return 1; fi
+if [[ $ATDM_CONFIG_USE_CUDA == "ON" && $ATDM_CONFIG_USE_PTHREADS == "ON" ]]; then echo "Can't set more than one backend"; return 1; fi
+if [[ $ATDM_CONFIG_USE_CUDA == "ON" && $ATDM_CONFIG_NODE_TYPE == "SERIAL" ]]; then echo "Can't set more than one backend"; return 1; fi
+if [[ $ATDM_CONFIG_USE_PTHREADS == "ON" && $ATDM_CONFIG_NODE_TYPE == "SERIAL" ]]; then echo "Can't set more than one backend"; return 1; fi
