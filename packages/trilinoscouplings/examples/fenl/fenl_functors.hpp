0a8102d98e69b4ecf6c8a7b93be7075af253d5be
Nathan Ellingwood <ndellin@sandia.gov>
2018-03-04 20:38:52 -0700

    muelu,nox,phalanx,sacado,basker,tpetra,trilinoscouplings
    Cast '1' increment to proper type in Kokkos::atomic_fetch_add; replace
    with Kokkos::atomic_increment when possible.

--- packages/trilinoscouplings/examples/fenl/fenl_functors.hpp
+++ packages/trilinoscouplings/examples/fenl/fenl_functors.hpp
@@ -248,8 +248,8 @@ public:
 
           if ( result.success() ) {
             // First time this pair was inserted
-            if ( row_node < row_count.dimension_0() ) { atomic_fetch_add( & row_count( row_node ) , 1 ); }
-            if ( col_node < row_count.dimension_0() && col_node != row_node ) { atomic_fetch_add( & row_count( col_node ) , 1 ); }
+            if ( row_node < row_count.dimension_0() ) { atomic_increment( & row_count( row_node ) ); }
+            if ( col_node < row_count.dimension_0() && col_node != row_node ) { atomic_increment( & row_count( col_node ) ); }
           }
           else if ( result.failed() ) {
             // Ran out of memory for insertion.
@@ -269,12 +269,14 @@ public:
       const unsigned col_node = key.second ;
 
       if ( row_node < row_count.dimension_0() ) {
-        const unsigned offset = graph.row_map( row_node ) + atomic_fetch_add( & row_count( row_node ) , 1 );
+        typedef typename std::remove_reference< decltype( row_count(0) ) >::type atomic_incr_type;
+        const unsigned offset = graph.row_map( row_node ) + atomic_fetch_add( & row_count( row_node ) , atomic_incr_type(1) );
         graph.entries( offset ) = col_node ;
       }
 
       if ( col_node < row_count.dimension_0() && col_node != row_node ) {
-        const unsigned offset = graph.row_map( col_node ) + atomic_fetch_add( & row_count( col_node ) , 1 );
+        typedef typename std::remove_reference< decltype( row_count(0) ) >::type atomic_incr_type;
+        const unsigned offset = graph.row_map( col_node ) + atomic_fetch_add( & row_count( col_node ) , atomic_incr_type(1) );
         graph.entries( offset ) = row_node ;
       }
     }
