58ceb6cc624ec3d2e5c949b0766f2350672b2cb4
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-03-07 06:33:26 -0700

    Must generate bsub script due to vars in comments (TRIL-171)
    This was likely not setting the timeout or the job name.  Now it should be.

--- cmake/ctest/drivers/atdm/ride/local-driver.sh
+++ cmake/ctest/drivers/atdm/ride/local-driver.sh
@@ -4,7 +4,20 @@ if [ "${BSUB_CTEST_TIME_LIMIT}" == "" ] ; then
   export BSUB_CTEST_TIME_LIMIT=04:00:00
 fi
 
+# We have to generate the file bsub-ctest-job.sh because it has values in
+# comments that we need to produce.  Just a regular bash script will not
+# evaluate $<var_name> in a comment.
+
+echo "#! /bin/bash
+#PBS -l nodes=1:ppn=64
+#PBS -l cput=$BSUB_CTEST_TIME_LIMIT
+#PBS -N $JOB_NAME
+
+$WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ctest-s-driver.sh
+" > bsub-ctest-job.sh
+
+chmod a+x bsub-ctest-job.sh
+
 set -x
 
-bsub -x -I \
-  $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ride/bsub-ctest-job.sh
+bsub -x -I $PWD/bsub-ctest-job.sh
