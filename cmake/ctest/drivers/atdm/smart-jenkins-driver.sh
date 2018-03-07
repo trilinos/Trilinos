a1e10aef147527b0f76e25853dac630abc529607
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-03-07 14:33:19 -0700

    Add 'date'command to see how long jobs wait in the queue (TRIL-171)

--- cmake/ctest/drivers/atdm/smart-jenkins-driver.sh
+++ cmake/ctest/drivers/atdm/smart-jenkins-driver.sh
@@ -5,6 +5,8 @@
 # This script is not system-specific so it can be usesd on all systems.
 #
 
+echo "Running $JOB_NAME.sh script in smart Jenkins driver"
+
 if [ "${JOB_NAME}" == ""  ] ; then
   echo "Error, must set JOB_NAME var before calling!"
   exit 1
@@ -17,6 +19,20 @@ fi
 
 source $WORKSPACE/Trilinos/cmake/std/atdm/utils/get_known_system_name.sh
 
+echo
+echo "Time when smart-jenkins-driver.sh was first called:"
+echo
+echo "  ==> `date`"
+echo
+
 set -x
 
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/$ATDM_CONFIG_KNOWN_SYSTEM_NAME/drivers/$JOB_NAME.sh
+
+echo
+echo "Time when smart-jenkins-driver.sh completed called:"
+echo
+echo "  ==> `date`"
+echo
+echo "DONE"
+echo
