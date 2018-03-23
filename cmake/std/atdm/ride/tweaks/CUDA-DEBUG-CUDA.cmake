e2c49a5eb264dafeb578b3a89cb5bc6960d6b9eb
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-03-23 08:54:59 -0600

    Disable seg faulting TeuchosNumerics_LAPACK_test_MPI_1 in  white/ride debug builds (#2410)
    This passes in the corresponding RELEASE builds so it is okay to disable this
    test in these DEBUG builds as we are not really loosing any real testing.  And
    note that almost all of the Belos and Anasazi and other tests that use this
    LAPACK interface pass in these DEBUG builds so disabling this one test is not
    too serious.  But if someone wants to try to debug it and then turn it back
    on, more power to them.
    
    Note that this is likey related to the mix-lanauage compiler defect reported
    in #1208.  That seems to be a strange compiler defect.

--- cmake/std/atdm/ride/tweaks/CUDA-DEBUG-CUDA.cmake
+++ cmake/std/atdm/ride/tweaks/CUDA-DEBUG-CUDA.cmake
@@ -1,1 +1,6 @@
+# #2410: This passes in the CUDA-RELESE-CUDA build so it is okay to disable in
+# this DEBUG build.  This is likey related to the mix-lanauage compiler defect
+# reported in #1208.
+ATDM_SET_ENABLE(TeuchosNumerics_LAPACK_test_MPI_1_DISABLE ON)
+
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/CUDA_COMMON_TWEAKS.cmake")
