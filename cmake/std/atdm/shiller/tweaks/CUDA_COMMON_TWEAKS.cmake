8f23641cd802cb945435761cbe4232485d2f5155
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-03-26 18:19:53 -0600

    Disable timing out test Anasazi_Epetra_BlockDavidson_auxtest_MPI_4 (#2455)
    This test appears to be hanging because it completes in just a few seconds in
    every other build.

--- cmake/std/atdm/shiller/tweaks/CUDA_COMMON_TWEAKS.cmake
+++ cmake/std/atdm/shiller/tweaks/CUDA_COMMON_TWEAKS.cmake
@@ -1,2 +1,5 @@
 # Disable test that runs over 30 min currently (#2446)
 ATDM_SET_ENABLE(PanzerAdaptersSTK_MixedPoissonExample-ConvTest-Hex-Order-3_DISABLE ON)
+
+# Disable test that times out after 10 minutes (#2455)
+ATDM_SET_ENABLE(Anasazi_Epetra_BlockDavidson_auxtest_MPI_4_DISABLE ON)
---
+++ cmake/std/atdm/shiller/tweaks/GNU-DEBUG-SERIAL.cmake
@@ -0,0 +1,2 @@
+# Disable test that times out after 10 minutes (#2455)
+ATDM_SET_ENABLE(Anasazi_Epetra_BlockDavidson_auxtest_MPI_4_DISABLE ON)
