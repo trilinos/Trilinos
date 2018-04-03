45206c27a4bbe9b727d123449ffd7d5f98c34c95
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-04-03 11:27:28 -0600

    Disable 3 Anasazi tests that randomly fail in debug builds on white/ride (#2473)
    These tests randomly fail with massive diffs.  Very strange behavior.  See
    Trilinos GitHub issue #2473 for history and more details.

--- cmake/std/atdm/ride/tweaks/CUDA-DEBUG-CUDA.cmake
+++ cmake/std/atdm/ride/tweaks/CUDA-DEBUG-CUDA.cmake
@@ -6,4 +6,9 @@ ATDM_SET_ENABLE(TeuchosNumerics_LAPACK_test_MPI_1_DISABLE ON)
 # This test segfaults in the 'debug' builds on this system (#2466)
 ATDM_SET_ENABLE(Belos_Tpetra_PseudoBlockCG_hb_test_MPI_4_DISABLE ON)
 
+# These tests randomly fail (see #2473)
+ATDM_SET_ENABLE(Anasazi_Epetra_ModalSolversTester_MPI_4_DISABLE ON)
+ATDM_SET_ENABLE(Anasazi_Epetra_OrthoManagerGenTester_0_MPI_4_DISABLE ON)
+ATDM_SET_ENABLE(Anasazi_Epetra_OrthoManagerGenTester_1_MPI_4_DISABLE ON)
+
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/CUDA_COMMON_TWEAKS.cmake")
