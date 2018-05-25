22729e396837a5f21c2f890334b164d5e78b3fd6
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-05-25 05:55:06 -0600

    Remove some env setup duplication and show cmake and ninja used (TRIL-171, TRIL-198, TRIL-208)
    Now with CMake and Ninja being found by prepending PATH, using 'module list'
    will no longer show the correct versions of CMake and Ninja.  Therefore, the
    updated script code now shows 'which cmake' and 'which ninja' after loading
    the env.
    
    Also, I broke out the common env setup code from these two ctest -S driver
    scripts into a new utils/setup_env.sh script to remove duplication.

---
+++ cmake/ctest/drivers/atdm/utils/setup_env.sh
@@ -0,0 +1,14 @@
+source ${WORKSPACE}/Trilinos/cmake/std/atdm/load-env.sh $JOB_NAME
+echo
+module list
+echo
+echo "cmake in path:"
+which cmake
+echo
+echo "ninja in path:"
+which ninja
+echo
+echo "ATDM config env vars:"
+set | grep ATDM_CONFIG_
+
+export Trilinos_REPOSITORY_LOCATION=https://github.com/trilinos/Trilinos.git
