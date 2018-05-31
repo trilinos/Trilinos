79042abd03257ea6cc6fdd2f47ad07dda6491a50
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-05-31 15:48:24 -0600

    Increase timeout for tests for build Trilinos-atdm-serrano-intel-opt-openmp-panzer (TRIL-200, #2699)
    For some reason, the running of the tests on 'serrano' timed out at 15 minutes
    for this build on 4/17/2-18 and 5/24/2018.  Most days it completes in less
    that 2 minutes but on 5/23/2018 it took 13m 7s in addition to timing out these
    two days.  That must have been a system issue but I am increasing the timeout
    some to give this a chance to finish on days like that.

--- cmake/ctest/drivers/atdm/serrano/drivers/Trilinos-atdm-serrano-intel-opt-openmp-panzer.sh
+++ cmake/ctest/drivers/atdm/serrano/drivers/Trilinos-atdm-serrano-intel-opt-openmp-panzer.sh
@@ -2,5 +2,5 @@
 if [ "${Trilinos_TRACK}" == "" ] ; then
   export Trilinos_TRACK=ATDM
 fi
-export SALLOC_CTEST_TIME_LIMIT_MINUTES=0:15:00
+export SALLOC_CTEST_TIME_LIMIT_MINUTES=0:30:00
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/serrano/local-driver.sh
