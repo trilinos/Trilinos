54b553c27c53f2c54e2dc2d53fb76eda55d625e8
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-05-31 14:58:18 -0400

    Send CUDA 9.0 builds to "Specialized" CDash Track/Group and remove old var (#2706, #2854)
    From a copy-and-paste these were hard-coded to send to the "ATDM" CDash
    Track/Group.  But these should not be promoted yet so I had the Jenkins jobs
    set CTEST_TEST_TYPE=Experimental which overrides that.  Now these will go to
    "Specializezd" by default, unless overridden in the Jenkins job.
    
    I also got rid of 'export ATDM_CONFIG_USE_MAKEFILES=ON' which is a non-op
    since the var name was changed to ATDM_CONFIG_USE_NINJA a while ago.

--- cmake/ctest/drivers/atdm/shiller/drivers/Trilinos-atdm-hansen-shiller-cuda-debug-panzer.sh
+++ cmake/ctest/drivers/atdm/shiller/drivers/Trilinos-atdm-hansen-shiller-cuda-debug-panzer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 export Trilinos_TRACK=ATDM
 export SRUN_CTEST_TIME_LIMIT_MINUTES=480 # 8 hour time limit
-export ATDM_CONFIG_USE_MAKEFILES=ON
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/shiller/local-driver.sh
