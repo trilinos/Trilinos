75fe8453ec2f725008cfb5c1ff69bf40f0a17177
Roger Pawlowski <rppawlo@sandia.gov>
2018-05-31 14:21:41 -0600

    Panzer: improve doxygen for projection utils #2265

--- packages/panzer/disc-fe/src/Panzer_L2Projection.hpp
+++ packages/panzer/disc-fe/src/Panzer_L2Projection.hpp
@@ -31,10 +31,8 @@ namespace panzer {
 
   /** \brief Unified set of tools for building objects for lumped and
       consistent L2 projects between bases. Currently only supports
-      scalar projections onto HGrad bases. It is design to be
-      extensible to all cases.
-
-      Need to add description of utilities.
+      projections onto HGrad bases from any type of source basis. It
+      is design to be extensible to all cases.
   */
   template<typename LO, typename GO>
   class L2Projection {
@@ -71,8 +69,7 @@ namespace panzer {
                const Teuchos::RCP<panzer::WorksetContainer>& worksetContainer = nullptr);
 
     /** \brief Allocates, fills and returns a mass matrix for L2
-        projection of a scalar and vector field(s) onto a scalar
-        basis.
+        projection onto a target basis.
 
         \returns Filled mass matrix in a Tpetra::CrsMatrix
     */
@@ -80,7 +77,12 @@ namespace panzer {
       buildMassMatrix();
 
     /** \brief Allocates, fills and returns a Tpetra::MultiVector
-        containing the inverse lumped mass matrix values.
+        containing the inverse lumped mass matrix values. This is
+        currently inefficient in that it uses the consistent mass
+        matrix to generate the row sums. This saves significant code
+        duplication at the temporary runtime cost of allocating the
+        mass matrix. The temporary matrix is deallocated on exit from
+        this function so it should not be an issue.
 
         \returns Filled inverse lumped mass matrix in a Tpetra::MultiVector (diagonal entries mass matrix)
     */
@@ -88,17 +90,20 @@ namespace panzer {
       buildInverseLumpedMassMatrix();
 
     /** \brief Allocates, fills and returns a rectangular matrix for
-        L2 projection of a scalar fields (or their gradients) or one
-        dimension of a vector field onto the target scalar basis. If
-        you wish to project all values of a vector field or the
-        gradients of a scalar field, then you will need three separate
-        RHS matrices to form the RHS for each independently.
+        L2 projection of a scalar field, one dimension of gradient
+        (for hgrad basis), or one dimension of a vector field onto the
+        target scalar basis. If you wish to project all values of a
+        vector field or all the gradients of a scalar field, then you
+        will need three separate RHS matrices to form the RHS for each
+        independently. The vectors must be independent Tpetra vectors
+        to solve multiple right hand sides with the linear solver.
 
         \param[in] (required) sourceDOFManager The source dof manger object
         \param[in] (required) ownedSourceMap The Tpetra Map for the owned source vector
         \param[in] (required) sourceFieldName The string name of the source field to project
         \param[in] (required) sourceBasisDescriptor The type of the basis for the source field
-        \param[in] (optional) vectorOrGradientDirectionIndex For vector fields, this is the vector index to project (x,y, or z coordinate direction). For scalar fields, the default value of -1 results in a projection of the scalar field value. If set to 0 or greater, it is assumed that the gradient of the HGrad field is projected and that this value is the dimension index for the particular gradient (x, y, or z coordinate direction).
+        \param[in] (optional) vectorOrGradientDirectionIndex For vector fields, this is the vector index to project (x,y, or z component). For scalar fields, the default value of -1 results in a projection of the scalar field value. If set to 0 or greater, it is assumed that the gradient of the HGrad field is projected and that this value is the dimension index for the particular gradient (x, y, or z component).
+
         \returns Alocated and filled Tpetra::CrsMatrix
     */
     Teuchos::RCP<Tpetra::CrsMatrix<double,LO,GO,Kokkos::Compat::KokkosDeviceWrapperNode<PHX::Device>>>
