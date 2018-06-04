b5056b71c7106791707335015cc3491a84bafce5
Joe Frye <jfrye@sandia.gov>
2018-06-04 14:35:04 -0600

    move all atdm/chama builds to specialized track TRIL-200

--- cmake/ctest/drivers/atdm/chama/drivers/Trilinos-atdm-chama-intel-debug-openmp.sh
+++ cmake/ctest/drivers/atdm/chama/drivers/Trilinos-atdm-chama-intel-debug-openmp.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+export Trilinos_TRACK=Specialized
+
+if [ "${Trilinos_TRACK}" == "" ] ; then
+  export Trilinos_TRACK=ATDM
+fi
+
 #export SALLOC_CTEST_TIME_LIMIT_MINUTES=1:00:00
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/chama/local-driver.sh
