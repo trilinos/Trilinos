1154819cbc6da5c187300b16031c5d2ebf837c2c
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-06-12 15:41:34 -0600

    Promote cuda-9.0 builds on 'hansen'/'shiller' to "ATDM" CDash Track/Group (#2826)
    The only failing test in the last few days was Belos_rcg_hb_MPI_4 but that is
    taken care of in #2919.  Therefore, these builds should be 100% clean once
    run.
    
    This concludes the effor to set up a CUDA 9.0 build on 'hansen'/'shiller' (see

--- cmake/ctest/drivers/atdm/shiller/drivers/Trilinos-atdm-hansen-shiller-cuda-9.0-debug.sh
+++ cmake/ctest/drivers/atdm/shiller/drivers/Trilinos-atdm-hansen-shiller-cuda-9.0-debug.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 if [ "${Trilinos_TRACK}" == "" ] ; then
-  export Trilinos_TRACK=Specialized
+  export Trilinos_TRACK=ATDM
 fi
 export SRUN_CTEST_TIME_LIMIT_MINUTES=480 # 8 hour time limit
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/shiller/local-driver.sh
