708d41c34f04e91949f7ad26f977475081e0b674
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-06-22 08:08:42 -0600

    Add missing GitHub issue number to disable comment (#2474)

--- cmake/std/atdm/mutrino/tweaks/INTEL-DEBUG-OPENMP.cmake
+++ cmake/std/atdm/mutrino/tweaks/INTEL-DEBUG-OPENMP.cmake
@@ -1,2 +1,2 @@
-# Disable test that is randomly failing in this build
+# Disable test that is randomly failing in this build (#2474)
 ATDM_SET_ENABLE(Piro_MatrixFreeDecorator_UnitTests_MPI_4_DISABLE ON)
