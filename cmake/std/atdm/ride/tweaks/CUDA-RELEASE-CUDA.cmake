33a933b004f88710274906fad612380049e1e82e
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-06-22 15:35:11 -0600

    Disable randomly failing tests Belos_pseudo_pcg_hb_[0,1]_MPI_4 in all builds on white/ride (#3007)

--- cmake/std/atdm/ride/tweaks/CUDA-RELEASE-CUDA.cmake
+++ cmake/std/atdm/ride/tweaks/CUDA-RELEASE-CUDA.cmake
@@ -1,5 +1,3 @@
-INCLUDE("${CMAKE_CURRENT_LIST_DIR}/CUDA_COMMON_TWEAKS.cmake")
-
 # Disable test that times out at 10 minutes (#2446)
 ATDM_SET_ENABLE(PanzerAdaptersSTK_main_driver_energy-ss-loca-eigenvalue_DISABLE ON)
 
@@ -9,3 +7,7 @@ ATDM_SET_ENABLE(Piro_MatrixFreeDecorator_UnitTests_MPI_4_DISABLE ON)
 # Disable randomly failing tests for this build (#2920)
 ATDM_SET_ENABLE(Belos_pseudo_stochastic_pcg_hb_0_MPI_4_DISABLE ON)
 ATDM_SET_ENABLE(Belos_pseudo_stochastic_pcg_hb_1_MPI_4_DISABLE ON)
+
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/CUDA_COMMON_TWEAKS.cmake")
+
+INCLUDE("${CMAKE_CURRENT_LIST_DIR}/ALL_COMMON_TWEAKS.cmake")
