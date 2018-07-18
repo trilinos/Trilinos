a0e152a3ec7e5f431f1b13b4e2eb21bb44e536aa
gsjaardema <gsjaardema@gmail.com>
2018-07-18 09:06:25 -0400

    SEACAS: rename grope to explore

--- packages/seacas/applications/explore/exp_prglob.f
+++ packages/seacas/applications/explore/exp_prglob.f
@@ -1,4 +1,4 @@
-C    Copyright(C) 2009 National Technology & Engineering Solutions of
+C    Copyright(C) 2008-2017 National Technology & Engineering Solutions of
 C    Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 C    NTESS, the U.S. Government retains certain rights in this software.
 C    
@@ -6,16 +6,17 @@ C    Redistribution and use in source and binary forms, with or without
 C    modification, are permitted provided that the following conditions are
 C    met:
 C    
-C        * Redistributions of source code must retain the above copyright
-C          notice, this list of conditions and the following disclaimer.
+C    * Redistributions of source code must retain the above copyright
+C       notice, this list of conditions and the following disclaimer.
 C    
-C        * Redistributions in binary form must reproduce the above
-C          copyright notice, this list of conditions and the following
-C          disclaimer in the documentation and/or other materials provided
-C          with the distribution.
-C        * Neither the name of NTESS nor the names of its
-C          contributors may be used to endorse or promote products derived
-C          from this software without specific prior written permission.
+C    * Redistributions in binary form must reproduce the above
+C      copyright notice, this list of conditions and the following
+C      disclaimer in the documentation and/or other materials provided
+C      with the distribution.
+C    
+C    * Neither the name of NTESS nor the names of its
+C      contributors may be used to endorse or promote products derived
+C      from this software without specific prior written permission.
 C    
 C    THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 C    "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
@@ -28,11 +29,12 @@ C    DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
 C    THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 C    (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 C    OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+C    
 C=======================================================================
       SUBROUTINE PRGLOB (OPTION, NOUT, NVARGL, LISGV, NAMEGV, VARGL)
 C=======================================================================
 
-C   --*** PRGLOB *** Display current database global variables
+C   --*** PRGLOB *** (EXPLORE) Display current database global variables
 C   --
 C   --PRGLOB displays the global data for a time step.
 C   --
@@ -44,7 +46,7 @@ C   --   LISGV - IN - the indices of the selected global variables
 C   --   NAMEGV - IN - the names of the global variables
 C   --   VARGL - IN - the global variables for the time step
 
-      include 'params.blk'
+      include 'exodusII.inc'
       CHARACTER*(*) OPTION
       INTEGER LISGV(0:*)
       CHARACTER*(*) NAMEGV(*)
@@ -52,9 +54,10 @@ C   --   VARGL - IN - the global variables for the time step
       INTEGER GETPRC, PRTLEN
       CHARACTER*128 FMT1, FMT
 
-      PRTLEN = 16
+      PRTLEN = GETPRC() + 7
       WRITE(FMT1,20) PRTLEN, PRTLEN-7
       CALL SQZSTR(FMT1, LFMT)
+      WRITE(FMT, 30) FMT1(:LFMT)
 
       LNAM = 0
       do i=1, lisgv(0)
@@ -62,8 +65,6 @@ C   --   VARGL - IN - the global variables for the time step
         lnam = max(l, lnam)
       end do
       
-      WRITE(FMT, 30) FMT1(:LFMT)
-
       IF (NOUT .GT. 0) THEN
         WRITE (NOUT, 10000)
       ELSE
