4756ca9dfc2dd5d153997676de0cfcc11bb39506
John Kaushagen <johnkaushagen@gmail.com>
2018-07-23 11:03:46 -0600

    fix chris comments

--- packages/trilinoscouplings/examples/scaling/TrilinosCouplings_TpetraIntrepidPoissonExample.cpp
+++ packages/trilinoscouplings/examples/scaling/TrilinosCouplings_TpetraIntrepidPoissonExample.cpp
@@ -276,7 +276,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
   int spaceDim = cellType.getDimension();
   int dim = 3;
 
-  
+
   // Build reference element edge to node map
   FieldContainer<int> refEdgeToNode(numEdgesPerElem,numNodesPerEdge);
   for (int i=0; i<numEdgesPerElem; i++){
@@ -407,12 +407,12 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
   double time = 0.0;
   if(inputList.isParameter("time"))
     time = inputList.get("time",time);
-  
+
   // Get sigma value for each block of elements from parameter list
   std::vector<double>  sigma(numElemBlk);
   std::vector<PG_RuntimeCompiler::Function> sigmaRTC(numElemBlk);
   std::vector<bool> useSigmaRTC(numElemBlk,false);
-  
+
   for(int b = 0; b < numElemBlk; b++){
     std::stringstream sigmaBlock, mysigmaRTC;
     sigmaBlock << "sigma" << b;
@@ -459,8 +459,8 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     }
   }
 
-  
-  
+
+
   // parallel info
   long long num_internal_nodes;
   long long num_border_nodes;
@@ -669,7 +669,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
   delete [] elements;
   elements = NULL;
 
- 
+
 /**********************************************************************************/
 /****************************** STATISTICS (Part I) *******************************/
 /**********************************************************************************/
@@ -687,7 +687,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     local_stat_max[i] = 0.0;
     local_stat_min[i] = std::numeric_limits<double>::max();
     local_stat_sum[i] = 0.0;
-  } 
+  }
 
   // Statistics
   double maxmin_ratio = 0;
@@ -715,10 +715,12 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
   int node3 = edgeToNode(edge, 0);
   int node4 = edgeToNode(edge, 1);
   double dist = 0;
+  static const int NUM_NODE_PAIRS = 4;
   int diag_nodes1[] = {0, 1, 2, 3};
   int diag_nodes2[] = {6, 7, 4, 5};
   int edge_nodes_1[] = {0, 0, 0, 1, 1, 2, 2, 3, 4, 4, 5, 6};
   int edge_nodes_2[] = {1, 3, 4, 2, 5, 3, 6, 7, 5, 7, 6, 7};
+  static const int NUM_EDGE_PAIRS = 6;
   int edge_opposites_1[] = { 0,  1, 2, 3, 4, 5};
   int edge_opposites_2[] = {11, 10, 6, 9, 7, 8};
   double edge_length_1 = 0;
@@ -736,7 +738,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     int hexnode7 = elemToNode(i, 7);
 
     // Now I have to get the node coordinates
-    for(int j=0; j<3; j++) {
+    for(int j=0; j<dim; j++) {
       hex_0[j] = nodeCoord(hexnode0, j);
       hex_1[j] = nodeCoord(hexnode1, j);
       hex_2[j] = nodeCoord(hexnode2, j);
@@ -750,7 +752,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     double pr1_norm = 0;
     double pr2_norm = 0;
     double pr3_norm = 0;
-    for(int j=0; j<3; j++){
+    for(int j=0; j<dim; j++){
       principle_axis_1[j] = hex_1[j] - hex_0[j] + hex_2[j] - hex_3[j] + hex_5[j] - hex_4[j] + hex_6[j] - hex_7[j];
       pr1_norm += principle_axis_1[j] * principle_axis_1[j];
       principle_axis_2[j] = hex_3[j] - hex_0[j] + hex_2[j] - hex_1[j] + hex_7[j] - hex_4[j] + hex_6[j] - hex_5[j];
@@ -763,7 +765,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     pr2_norm = sqrt(pr2_norm);
     pr3_norm = sqrt(pr3_norm);
 
-    for(int j=0; j<3; j++) {
+    for(int j=0; j<dim; j++) {
       principle_axis_1[j] = principle_axis_1[j] / pr1_norm;
       principle_axis_2[j] = principle_axis_2[j] / pr2_norm;
       principle_axis_3[j] = principle_axis_3[j] / pr3_norm;
@@ -798,9 +800,11 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     // 2 - det of cell Jacobian (later)
 
     // 3 - Stretch
-    diag_length_max = distance2(nodeCoord, elemToNode(i, 0), elemToNode(i, 6));
-    diag_length_min = distance2(nodeCoord, elemToNode(i, 0), elemToNode(i, 6));
-    for (int j=0; j<4; j++) {
+    diag_length_max = distance2(nodeCoord, elemToNode(i, diag_nodes1[0]),
+                                           elemToNode(i, diag_nodes2[0]));
+    diag_length_min = distance2(nodeCoord, elemToNode(i, diag_nodes1[0]),
+                                           elemToNode(i, diag_nodes2[0]));
+    for (int j=0; j<NUM_NODE_PAIRS; j++) {
       node1 = elemToNode(i, diag_nodes1[j]);
       node2 = elemToNode(i, diag_nodes2[j]);
       dist = distance2(nodeCoord, node1, node2);
@@ -812,19 +816,22 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     local_stat_max[3] = std::max(local_stat_max[3], stretch);
     local_stat_min[3] = std::min(local_stat_min[3], stretch);
     local_stat_sum[3] += stretch;
+
+    // 4 - Diagonal Ratio
     local_stat_max[4] = std::max(local_stat_max[4], diag_ratio);
     local_stat_min[4] = std::min(local_stat_min[4], diag_ratio);
     local_stat_sum[4] += diag_ratio;
 
-    node1 = elemToNode(i, 0);
-    node2 = elemToNode(i, 1);
-    node3 = elemToNode(i, 6);
-    node4 = elemToNode(i, 7);
+    // 5 - Inverse Taper
+    node1 = elemToNode(i, diag_nodes1[0]);
+    node2 = elemToNode(i, diag_nodes1[1]);
+    node3 = elemToNode(i, diag_nodes2[0]);
+    node4 = elemToNode(i, diag_nodes2[1]);
     edge_length_1 = distance2(nodeCoord, node1, node2);
     edge_length_2 = distance2(nodeCoord, node3, node4);
     double ratio = edge_length_1 / edge_length_2;
     ratio = std::min(ratio, 1/ratio);
-    for (int j=0; j<6; j++) {
+    for (int j=0; j<NUM_EDGE_PAIRS; j++) {
       node1 = elemToNode(i, edge_nodes_1[edge_opposites_1[j]]);
       node2 = elemToNode(i, edge_nodes_2[edge_opposites_1[j]]);
       node3 = elemToNode(i, edge_nodes_1[edge_opposites_2[j]]);
@@ -838,13 +845,13 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     local_stat_min[5] = std::min(local_stat_min[5], ratio);
     local_stat_sum[5] += ratio;
 
-    // Skew
+    // 6 - Skew
     double skew = 0;
     double skew1 = 0;
     double skew2 = 0;
     double skew3 = 0;
 
-    for(int j=0; j<3; j++) {
+    for(int j=0; j<dim; j++) {
       skew1 += principle_axis_1[j] * principle_axis_2[j];
       skew2 += principle_axis_1[j] * principle_axis_3[j];
       skew3 += principle_axis_1[j] * principle_axis_3[j];
@@ -1263,11 +1270,11 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
         weightedMeasureSigma(cellCounter,nPt) = worksetCubWeights(cellCounter,nPt) * sigmaVal(cell);
       }
       cellCounter++;
-    }   
-    
+    }
+
     // Multiply transformed (workset) gradients with weighted measure
     IntrepidFSTools::multiplyMeasure<ST> (worksetHGBGradsWeighted, // DF^{-T}(grad u)*J*w
-                                          weightedMeasureSigma, 
+                                          weightedMeasureSigma,
                                           worksetHGBGrads);
     // Compute the diffusive flux:
     IntrepidFSTools::tensorMultiplyDataField<ST> (worksetDiffusiveFlux, // A*(DF^{-T}(grad u)
@@ -1362,7 +1369,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     } // *** stop timer ***
   }// *** workset loop ***
 
- 
+
 /**********************************************************************************/
 /***************************** STATISTICS (Part III) ******************************/
 /**********************************************************************************/
@@ -1382,7 +1389,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
   problemStatistics.set("element edge ratio: max",global_stat_max[1]);
   problemStatistics.set("element edge ratio: mean",global_stat_sum[1] / numElemsGlobal);
 
-  // 2 - det of cell Jacobian (later)  
+  // 2 - det of cell Jacobian (later)
   problemStatistics.set("element det jacobian: min/mean",global_stat_min[2]/global_stat_sum[2]*numElemsGlobal);
   problemStatistics.set("element det jacobian: max/mean",global_stat_max[2]/global_stat_sum[2]*numElemsGlobal);
 
@@ -1396,7 +1403,7 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
   problemStatistics.set("Diagonal Ratio min", global_stat_min[4]);
   problemStatistics.set("Diagonal Ratio mean", global_stat_sum[4]/numElemsGlobal);
 
-  // 5 0 Inverse Taper
+  // 5 - Inverse Taper
   problemStatistics.set("Inverse Taper max", global_stat_max[5]);
   problemStatistics.set("Inverse Taper min", global_stat_min[5]);
   problemStatistics.set("Inverse Taper mean", global_stat_sum[5] / numElemsGlobal);
