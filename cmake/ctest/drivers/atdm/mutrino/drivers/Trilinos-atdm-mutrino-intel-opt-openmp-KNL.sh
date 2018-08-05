0df591fd6d9b9ccd91bd2695cc22665aa468886d
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-08-05 18:39:05 -0400

    Increase KNL testing timeout from 5 to 9 hours (TRIL-196) (#3236)
    The tests on the KNL build are taking an absurd amount of time but let's see
    if we can get them to complete in 9 hours!  AT the old timeout of 5 hours it
    the debug build ran 1504 out of 1801 tests so I am hopeful this will allow
    them to complete.
    
    This is only taking up one compute node on 'mutrino' so this is not such a
    crime at this point.

--- cmake/ctest/drivers/atdm/mutrino/drivers/Trilinos-atdm-mutrino-intel-opt-openmp-KNL.sh
+++ cmake/ctest/drivers/atdm/mutrino/drivers/Trilinos-atdm-mutrino-intel-opt-openmp-KNL.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
 if [ "${Trilinos_TRACK}" == "" ] ; then
   export Trilinos_TRACK=ATDM
 fi
-export SALLOC_CTEST_TIME_LIMIT_MINUTES=300 # 5 hour time limit
+
+if [ "${SALLOC_CTEST_TIME_LIMIT_MINUTES}" == "" ] ; then
+  export SALLOC_CTEST_TIME_LIMIT_MINUTES=540 # 9 hour time limit
+fi
+
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/mutrino/local-driver.sh
