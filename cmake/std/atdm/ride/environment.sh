a3363fac763d85d7fdc5e7aa7c1bb89951aaac68
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-08-08 08:35:49 -0600

    CUDA 9.2 build on white/ride and remove GPU KOKKOS_ARCH for GNU build (TRIL-215)
    Here I updated the default 'cuda' build to be a 'cuda-9.2' build.
    
    I also removed the GPU arch from KOKKOS_ARCH when doing a GPU build.  I don't
    think you want or need the GPU arch when not using CUDA.  I think having it
    might just confuse people.

--- cmake/std/atdm/ride/environment.sh
+++ cmake/std/atdm/ride/environment.sh
@@ -10,36 +10,57 @@ if [ "$ATDM_CONFIG_COMPILER" == "DEFAULT" ] ; then
   export ATDM_CONFIG_COMPILER=GNU
 fi
 
-if [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "DEFAULT" ]] ; then
-  export ATDM_CONFIG_KOKKOS_ARCH=Power8,Kepler37
-  export ATDM_CONFIG_QUEUE=rhel7F
-elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Power8" ]] ; then
-  export ATDM_CONFIG_KOKKOS_ARCH=Power8,Kepler37
-  export ATDM_CONFIG_QUEUE=rhel7F
-elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Kepler37" ]] ; then
-  export ATDM_CONFIG_KOKKOS_ARCH=Power8,$ATDM_CONFIG_KOKKOS_ARCH
-  export ATDM_CONFIG_QUEUE=rhel7F
-elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Kepler35" ]] ; then
-  export ATDM_CONFIG_KOKKOS_ARCH=Power8,$ATDM_CONFIG_KOKKOS_ARCH
-  export ATDM_CONFIG_QUEUE=rhel7T
-elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Pascal60" ]] ; then
-  export ATDM_CONFIG_KOKKOS_ARCH=Power8,$ATDM_CONFIG_KOKKOS_ARCH
-  export ATDM_CONFIG_QUEUE=rhel7G
+if [ "$ATDM_CONFIG_COMPILER" == "GNU" ]; then
+  if [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "DEFAULT" ]] ; then
+    export ATDM_CONFIG_KOKKOS_ARCH=Power8
+    export ATDM_CONFIG_QUEUE=rhel7F
+  elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Power8" ]] ; then
+    export ATDM_CONFIG_KOKKOS_ARCH=Power8
+    export ATDM_CONFIG_QUEUE=rhel7F
+  else
+    echo
+    echo "***"
+    echo "*** ERROR: KOKKOS_ARCH=$ATDM_CONFIG_KOKKOS_ARCH is not a valid option"
+    echo "*** for the compiler GNU.  Replace '$ATDM_CONFIG_KOKKOS_ARCH' in the"
+    echo "*** job name with 'Power8'"
+    echo "***"
+    return
+  fi
+elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA"* ]] ; then
+  if [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "DEFAULT" ]] ; then
+    export ATDM_CONFIG_KOKKOS_ARCH=Power8,Kepler37
+    export ATDM_CONFIG_QUEUE=rhel7F
+  elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Power8" ]] ; then
+    export ATDM_CONFIG_KOKKOS_ARCH=Power8,Kepler37
+    export ATDM_CONFIG_QUEUE=rhel7F
+  elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Kepler37" ]] ; then
+    export ATDM_CONFIG_KOKKOS_ARCH=Power8,$ATDM_CONFIG_KOKKOS_ARCH
+    export ATDM_CONFIG_QUEUE=rhel7F
+  elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Kepler35" ]] ; then
+    export ATDM_CONFIG_KOKKOS_ARCH=Power8,$ATDM_CONFIG_KOKKOS_ARCH
+    export ATDM_CONFIG_QUEUE=rhel7T
+  elif [[ "$ATDM_CONFIG_KOKKOS_ARCH" == "Pascal60" ]] ; then
+    export ATDM_CONFIG_KOKKOS_ARCH=Power8,$ATDM_CONFIG_KOKKOS_ARCH
+    export ATDM_CONFIG_QUEUE=rhel7G
+  else
+    echo
+    echo "***"
+    echo "*** ERROR: KOKKOS_ARCH=$ATDM_CONFIG_KOKKOS_ARCH is not a valid option"
+    echo "*** for the CUDA compiler.  Replace '$ATDM_CONFIG_KOKKOS_ARCH' in the"
+    echo "*** job name with one of the following options:"
+    echo "***"
+    echo "***   'Kepler35' Power8 with Kepler K-40 GPU"
+    echo "***   'Kepler37' Power8 with Kepler K-80 GPU (Default)"
+    echo "***   'Pascal60' Power8 with Pascal P-100 GPU"
+    echo "***"
+    return
+  fi
+else
+  echo "***"
+  echo "*** ERROR: COMPILER=$ATDM_CONFIG_COMPILER is not supported on this system!"
+  echo "***"
+  return
 fi
-
-if [ "$ATDM_CONFIG_KOKKOS_ARCH" != "Power8,Kepler35" ]  && \
-   [ "$ATDM_CONFIG_KOKKOS_ARCH" != "Power8,Kepler37" ]  && \
-   [ "$ATDM_CONFIG_KOKKOS_ARCH" != "Power8,Pascal60" ]; then
-     echo "***"
-     echo "*** ERROR: KOKKOS_ARCH=$ATDM_CONFIG_KOKKOS_ARCH is not a valid option on this system."
-     echo "*** Replace '$ATDM_CONFIG_KOKKOS_ARCH' in the job name with one of the following options:"
-     echo "*** 'Kepler35' Power8 with Kepler K-40 GPU"
-     echo "*** 'Kepler37' Power8 with Kepler K-80 GPU (Default)"
-     echo "*** 'Pascal60' Power8 with Pascal P-100 GPU"
-     echo "***"
-   return
-fi
-
 
 echo "Using white/ride compiler stack $ATDM_CONFIG_COMPILER to build $ATDM_CONFIG_BUILD_TYPE code with Kokkos node type $ATDM_CONFIG_NODE_TYPE and KOKKOS_ARCH=$ATDM_CONFIG_KOKKOS_ARCH"
 
@@ -57,38 +78,47 @@ else
   export ATDM_CONFIG_CTEST_PARALLEL_LEVEL=32
 fi
 
-if [ "$ATDM_CONFIG_COMPILER" == "GNU" ]; then
-    module load devpack/20180308/openmpi/2.1.2/gcc/7.2.0/cuda/9.0.176
+if [ "$ATDM_CONFIG_COMPILER" == "GNU" ] ; then
+
+  # Load the modules and set up env
+  module load devpack/20180308/openmpi/2.1.2/gcc/7.2.0/cuda/9.0.176
+  module swap openblas/0.2.20/gcc/7.2.0 netlib/3.8.0/gcc/7.2.0
+  export OMPI_CXX=`which g++`
+  export OMPI_CC=`which gcc`
+  export OMPI_FC=`which gfortran`
+  export ATDM_CONFIG_LAPACK_LIB="-L${LAPACK_ROOT}/lib;-llapack;-lgfortran;-lgomp"
+  export ATDM_CONFIG_BLAS_LIB="-L${BLAS_ROOT}/lib;-lblas;-lgfortran;-lgomp;-lm"
+
+elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA"* ]] ; then
+  if [[ "$ATDM_CONFIG_COMPILER" == "CUDA" ]] ; then
+    export ATDM_CONFIG_COMPILER=CUDA-9.2  # The default CUDA version currently
+  fi
+  if [[ "$ATDM_CONFIG_COMPILER" == "CUDA-9.2" ]] ; then
+    module load devpack/20180521/openmpi/3.1.0/gcc/7.2.0/cuda/9.2.88   
     module swap openblas/0.2.20/gcc/7.2.0 netlib/3.8.0/gcc/7.2.0
-    export OMPI_CXX=`which g++`
-    export OMPI_CC=`which gcc`
-    export OMPI_FC=`which gfortran`
-    export ATDM_CONFIG_LAPACK_LIB="-L${LAPACK_ROOT}/lib;-llapack;-lgfortran;-lgomp"
-    export ATDM_CONFIG_BLAS_LIB="-L${BLAS_ROOT}/lib;-lblas;-lgfortran;-lgomp;-lm"
-elif [ "$ATDM_CONFIG_COMPILER" == "CUDA" ]; then
-    export ATDM_CONFIG_CTEST_PARALLEL_LEVEL=16
-    module load devpack/20180308/openmpi/2.1.2/gcc/7.2.0/cuda/9.0.176
-    module unload gcc/7.2.0
-    module swap openblas/0.2.20/gcc/7.2.0 netlib/3.8.0/gcc/7.2.0
-    export OMPI_CXX=$ATDM_CONFIG_TRILNOS_DIR/packages/kokkos/bin/nvcc_wrapper
-    if [ ! -x "$OMPI_CXX" ]; then
-        echo "No nvcc_wrapper found"
-        return
-    fi
-    export OMPI_CC=`which gcc`
-    export OMPI_FC=`which gfortran`
-    export ATDM_CONFIG_LAPACK_LIB="-L${LAPACK_ROOT}/lib;-llapack;-lgfortran;-lgomp"
-    export ATDM_CONFIG_BLAS_LIB="-L${BLAS_ROOT}/lib;-lblas;-lgfortran;-lgomp;-lm"
-    export ATDM_CONFIG_USE_CUDA=ON
-    export CUDA_LAUNCH_BLOCKING=1
-    export CUDA_MANAGED_FORCE_DEVICE_ALLOC=1
-    export ATDM_CONFIG_CTEST_PARALLEL_LEVEL=8
-    # Avoids timeouts due to not running on separate GPUs (see #2446)
-else
+  else
+    echo
     echo "***"
-    echo "*** ERROR: COMPILER=$ATDM_CONFIG_COMPILER is not supported on this system!"
+    echo "*** ERROR: COMPILER=$ATDM_CONFIG_COMPILER is not a supported version of CUDA on this system!"
     echo "***"
     return
+  fi
+  export OMPI_CXX=$ATDM_CONFIG_TRILNOS_DIR/packages/kokkos/bin/nvcc_wrapper
+  if [ ! -x "$OMPI_CXX" ]; then
+      echo "No nvcc_wrapper found"
+      return
+  fi
+  export OMPI_CC=`which gcc`
+  export OMPI_FC=`which gfortran`
+  export ATDM_CONFIG_LAPACK_LIB="-L${LAPACK_ROOT}/lib;-llapack;-lgfortran;-lgomp"
+  export ATDM_CONFIG_BLAS_LIB="-L${BLAS_ROOT}/lib;-lblas;-lgfortran;-lgomp;-lm"
+  export ATDM_CONFIG_USE_CUDA=ON
+  export CUDA_LAUNCH_BLOCKING=1
+  export CUDA_MANAGED_FORCE_DEVICE_ALLOC=1
+  export ATDM_CONFIG_CTEST_PARALLEL_LEVEL=8
+  # Avoids timeouts due to not running on separate GPUs (see #2446)
+#else
+  # The else check for unsupported comiler is handled above when setting KOKKOS_ARCH
 fi
 
 export ATDM_CONFIG_USE_HWLOC=OFF
