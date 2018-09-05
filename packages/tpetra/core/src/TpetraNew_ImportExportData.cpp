e93362ffb6b634db06d9e086af120172fd088ca6
Mark Hoemmen <mhoemme@sandia.gov>
2018-09-05 12:39:44 -0600

    Tpetra: Add new TpetraNew::ImportExportData class

---
+++ packages/tpetra/core/src/TpetraNew_ImportExportData.cpp
@@ -0,0 +1,143 @@
+// @HEADER
+// ***********************************************************************
+//
+//          Tpetra: Templated Linear Algebra Services Package
+//                 Copyright (2008) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+
+#include "TpetraNew_ImportExportData.hpp"
+#include "TpetraNew_Map.hpp"
+#include "Teuchos_FancyOStream.hpp"
+#include "Teuchos_ParameterList.hpp"
+
+namespace TpetraNew {
+
+  ImportExportData::
+  ImportExportData (const Teuchos::RCP<const map_type>& source,
+                    const Teuchos::RCP<const map_type>& target) :
+    source_ (source),
+    target_ (target),
+    out_ (Teuchos::getFancyOStream (Teuchos::rcpFromRef (std::cerr))),
+    numSameIDs_ (0),
+    distributor_ (source->getComm (), out_),
+    isLocallyComplete_ (true) // Import/Export constructor may change this
+  {}
+
+  ImportExportData::
+  ImportExportData (const Teuchos::RCP<const map_type>& source,
+                    const Teuchos::RCP<const map_type>& target,
+                    const Teuchos::RCP<Teuchos::FancyOStream>& out) :
+    source_ (source),
+    target_ (target),
+    out_ (out),
+    numSameIDs_ (0),
+    distributor_ (source->getComm (), out_),
+    isLocallyComplete_ (true) // Import/Export constructor may change this
+  {}
+
+  ImportExportData::
+  ImportExportData (const Teuchos::RCP<const map_type>& source,
+                    const Teuchos::RCP<const map_type>& target,
+                    const Teuchos::RCP<Teuchos::ParameterList>& plist) :
+    source_ (source),
+    target_ (target),
+    out_ (Teuchos::getFancyOStream (Teuchos::rcpFromRef (std::cerr))),
+    numSameIDs_ (0),
+    distributor_ (source->getComm (), out_, plist),
+    isLocallyComplete_ (true) // Import/Export constructor may change this
+  {}
+
+  ImportExportData::
+  ImportExportData (const Teuchos::RCP<const map_type>& source,
+                    const Teuchos::RCP<const map_type>& target,
+                    const Teuchos::RCP<Teuchos::FancyOStream>& out,
+                    const Teuchos::RCP<Teuchos::ParameterList>& plist) :
+    source_ (source),
+    target_ (target),
+    out_ (out),
+    numSameIDs_ (0),
+    distributor_ (source->getComm (), out_, plist),
+    isLocallyComplete_ (true) // Import/Export constructor may change this
+  {}
+
+  Teuchos::RCP<ImportExportData>
+  ImportExportData::reverseClone ()
+  {
+    using Teuchos::ArrayView;
+
+    auto tData = Teuchos::rcp (new ImportExportData (target_, source_));
+
+    // Things that stay the same
+    tData->numSameIDs_       = numSameIDs_;
+
+    // Things that reverse
+    tData->distributor_      = *distributor_.getReverse();
+    tData->permuteToLIDs_    = permuteFromLIDs_;
+    tData->permuteFromLIDs_  = permuteToLIDs_;
+
+    // Remotes / exports (easy part)
+    tData->exportLIDs_       = remoteLIDs_;
+    tData->remoteLIDs_       = exportLIDs_;
+    tData->exportPIDs_.resize(tData->exportLIDs_.size());
+
+    // Remotes / exports (hard part) - extract the exportPIDs from the remotes of my distributor
+    size_t NumReceives                  = distributor_.getNumReceives();
+    ArrayView<const int> ProcsFrom      = distributor_.getProcsFrom();
+    ArrayView<const size_t> LengthsFrom = distributor_.getLengthsFrom();
+
+    // isLocallyComplete is a local predicate.
+    // It could be true in one direction but false in another.
+
+    bool isLocallyComplete = true; // by default
+    for (size_t i = 0, j = 0; i < NumReceives; ++i) {
+      const int pid = ProcsFrom[i];
+      if (pid == -1) {
+        isLocallyComplete = false;
+      }
+      for (size_t k = 0; k < LengthsFrom[i]; ++k) {
+        tData->exportPIDs_[j] = pid;
+        ++j;
+      }
+    }
+    tData->isLocallyComplete_ = isLocallyComplete;
+
+    return tData;
+  }
+
+} // namespace TpetraNew
+
+
---
+++ packages/tpetra/core/src/TpetraNew_ImportExportData.hpp
@@ -0,0 +1,224 @@
+// @HEADER
+// ***********************************************************************
+//
+//          Tpetra: Templated Linear Algebra Services Package
+//                 Copyright (2008) Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ************************************************************************
+// @HEADER
+
+#ifndef TPETRANEW_IMPORTEXPORTDATA_DECL_HPP
+#define TPETRANEW_IMPORTEXPORTDATA_DECL_HPP
+
+#include "TpetraNew_ImportExportData_fwd.hpp"
+#include "TpetraNew_Export_fwd.hpp"
+#include "TpetraNew_Import_fwd.hpp"
+#include "TpetraNew_Map.hpp"
+#include "Tpetra_Distributor.hpp"
+#include "Teuchos_Array.hpp"
+#include "Teuchos_RCP.hpp"
+
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+namespace Teuchos {
+class ParameterList; // forward declaration
+} // namespace Teuchos
+#endif // DOXYGEN_SHOULD_SKIP_THIS
+
+namespace TpetraNew {
+
+  /// \class ImportExportData
+  /// \brief Implementation detail of Import and Export.
+  ///
+  /// \warning This class is an implementation detail of Import and
+  ///   Export.  It may change or disappear at any time.  Tpetra users
+  ///   must not depend on this class.
+  ///
+  /// Import and Export both require the same data.  We use this class
+  /// as a container for those data.  They include incoming ("remote")
+  /// and outgoing ("export") local indices (LIDs), LIDs to permute on
+  /// the source and target of the Import or Export, and process ranks
+  /// ("image IDs") to which to send.
+  class ImportExportData {
+  public:
+    using map_type = ::TpetraNew::Map;    
+    using local_ordinal_type = map_type::local_ordinal_type;
+
+    /// \brief Constructor
+    ///
+    /// \param source [in] Source Map of the Import or Export
+    /// \param target [in] Target Map of the Import or Export
+    ImportExportData (const Teuchos::RCP<const map_type>& source,
+                      const Teuchos::RCP<const map_type>& target);
+
+    /// \brief Constructor with output stream.
+    ///
+    /// \param source [in] Source Map of the Import or Export
+    /// \param target [in] Target Map of the Import or Export
+    /// \param out [in/out] Output stream (for debugging output)
+    ImportExportData (const Teuchos::RCP<const map_type>& source,
+                      const Teuchos::RCP<const map_type>& target,
+                      const Teuchos::RCP<Teuchos::FancyOStream>& out);
+
+    /// \brief Constructor with ParameterList for Distributor
+    ///
+    /// \param source [in] Source Map of the Import or Export
+    /// \param target [in] Target Map of the Import or Export
+    /// \param plist [in/out] List of parameters for the Distributor
+    ImportExportData (const Teuchos::RCP<const map_type>& source,
+                      const Teuchos::RCP<const map_type>& target,
+                      const Teuchos::RCP<Teuchos::ParameterList>& plist);
+
+    /// \brief Constructor with output stream, and ParameterList for Distributor
+    ///
+    /// \param source [in] Source Map of the Import or Export
+    /// \param target [in] Target Map of the Import or Export
+    /// \param out [in/out] Output stream (for debugging output)
+    /// \param plist [in/out] List of parameters for the Distributor
+    ImportExportData (const Teuchos::RCP<const map_type>& source,
+                      const Teuchos::RCP<const map_type>& target,
+                      const Teuchos::RCP<Teuchos::FancyOStream>& out,
+                      const Teuchos::RCP<Teuchos::ParameterList>& plist);
+    //! Destructor
+    ~ImportExportData () = default;
+
+    /// \brief Copy the data, but reverse the direction of the
+    ///   transfer as well as reversing the Distributor.
+    ///
+    /// "Reverse the direction of the transfer" means that an Import
+    /// becomes an Export in the opposite direction, and vice versa.
+    Teuchos::RCP<ImportExportData> reverseClone ();
+
+    //! Source Map of the Import or Export
+    const Teuchos::RCP<const map_type> source_;
+
+    //! Target Map of the Import or Export
+    const Teuchos::RCP<const map_type> target_;
+
+    //! Output stream for debug output.
+    Teuchos::RCP<Teuchos::FancyOStream> out_;
+
+    /// \brief Index of target Map LIDs to which to permute.
+    ///
+    /// After the initial numSameIDs_ indices which are the same in
+    /// both the source and target Map, zero or more global indices
+    /// (GIDs) remain.  They exist in both the source and target Maps,
+    /// but are in a different order.  Therefore, they may have
+    /// different local indices (LIDs), and require permutation.
+    ///
+    /// For each remaining GIDs g in the target Map, if the source Map
+    /// also owns g, then permuteToLIDs_ gets the corresponding LID in
+    /// the target Map.
+    Teuchos::Array<local_ordinal_type> permuteToLIDs_;
+
+    /// \brief Index of source Map LIDs from which to permute.
+    ///
+    /// After the initial numSameIDs_ indices which are the same in
+    /// both the source and target Map, zero or more global indices
+    /// (GIDs) remain.  They exist in both the source and target Maps,
+    /// but are in a different order.  Therefore, they may have
+    /// different local indices (LIDs), and require permutation.
+    ///
+    /// For each remaining GID g in the target Map, if the source Map
+    /// also owns g, then permuteFromLIDs_ gets the corresponding LID
+    /// in the source Map.
+    Teuchos::Array<local_ordinal_type> permuteFromLIDs_;
+
+    /// \brief "Incoming" indices.
+    ///
+    /// This array holds the LIDs of the GIDs that are owned by the
+    /// target Map, but not by the source Map.  The target object of
+    /// the Import or Export will receive data for these LIDs from
+    /// other processes.
+    Teuchos::Array<local_ordinal_type> remoteLIDs_;
+
+    /// \brief "Outgoing" local indices.
+    ///
+    /// This array holds the LIDs of the GIDs that are owned by the
+    /// source Map, but not by the target Map.  The source object of
+    /// the Import or Export will send data from these LIDs to other
+    /// processes.
+    Teuchos::Array<local_ordinal_type> exportLIDs_;
+
+    //! Ranks of the processes to which the source object sends data.
+    Teuchos::Array<int> exportPIDs_;
+
+    /// \brief Number of initial identical indices.
+    ///
+    /// The number of initial indices (IDs) that are identical between
+    /// the source and target Maps.  This count stops at the first
+    /// different ID.
+    ///
+    /// Note that we didn't specify whether the IDs are global (GID)
+    /// or local (LID).  That is because if the two Maps start with
+    /// the same sequence of GIDs on the calling process, then those
+    /// GIDs map to the same LIDs on the calling process.  Thus, when
+    /// we say "ID" in the previous paragraph, we include both GID and
+    /// LID.
+    size_t numSameIDs_;
+
+    /// \brief Object that actually distributes (sends and receives) data.
+    ///
+    /// The Import or Export object that controls this
+    /// ImportExportData container is responsible for initializing the
+    /// Distributor.  The Distributor's constructor just gives it the
+    /// communicator; it does not complete initialization.
+    ::Tpetra::Distributor distributor_;
+
+    /// \brief Is this Export or Import locally complete?
+    ///
+    /// If this is an Export, then do all source Map indices on the
+    /// calling process exist on at least one process (not necessarily
+    /// this one) in the target Map?
+    ///
+    /// If this is an Import, then do all target Map indices on the
+    /// calling process exist on at least one process (not necessarily
+    /// this one) in the source Map?
+    ///
+    /// It's not necessarily an error for an Export or Import not to
+    /// be locally complete on one or more processes.  For example,
+    /// this may happen in the common use case of "restriction" --
+    /// that is, taking a subset of a large object.  Nevertheless, you
+    /// may find this predicate useful for figuring out whether you
+    /// set up your Maps in the way that you expect.
+    bool isLocallyComplete_;
+
+  private:
+    ImportExportData (const ImportExportData& rhs) = delete;
+    ImportExportData& operator= (const ImportExportData& rhs) = delete;
+  }; // class ImportExportData
+
+} // namespace TpetraNew
+
+#endif // TPETRANEW_IMPORTEXPORTDATA_DECL_HPP
