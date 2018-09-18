7f077afaac764a103deb5e5c4098131faac5e01c
Paul Wolfenbarger <prwolfe@sandia.gov>
2018-09-18 15:08:30 -0600

    Removes stk_classic from the Trilinos repository
    This will close #3444 as stk_classic is not longer supported.

--- packages/stk/stk_classic/stk_adapt/regression_tests/RegressionTestFileLoc.hpp
+++
@@ -1,8 +0,0 @@
-#if 1
-      const std::string path_sep = "._.";
-      const std::string input_files_loc="./input_files"+path_sep;
-      const std::string output_files_loc="./output_files"+path_sep;
-#else
-      const std::string input_files_loc="./input_files/";
-      const std::string output_files_loc="./output_files/";
-#endif
--- packages/stk/stk_classic/stk_percept/stk_percept/stk_mesh.hpp
+++
@@ -1,8 +0,0 @@
-#include <stk_mesh/base/Field.hpp>
-#include <stk_mesh/base/FieldBase.hpp>
-#include <stk_mesh/base/BulkData.hpp>
-#include <stk_mesh/base/Entity.hpp>
-#include <stk_mesh/fem/FEMMetaData.hpp>
-#include <stk_mesh/base/Part.hpp>
-
-#include <stk_mesh/fem/CoordinateSystems.hpp>
