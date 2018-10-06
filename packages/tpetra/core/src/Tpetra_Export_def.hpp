da157767dc420c0e4569f2614678ccf96feebb38
Daniel Arndt <daniel.arndt@iwr.uni-heidelberg.de>
2018-10-06 22:08:50 +0200

    Avoid bind1st and bind2nd

--- packages/tpetra/core/src/Tpetra_Export_def.hpp
+++ packages/tpetra/core/src/Tpetra_Export_def.hpp
@@ -545,7 +545,9 @@ namespace Classes {
         const size_type numInvalidExports =
           std::count_if (ExportData_->exportPIDs_().begin(),
                          ExportData_->exportPIDs_().end(),
-                         std::bind1st (std::equal_to<int>(), -1));
+                         [] (const int processor_id) {
+                           return processor_id == -1;
+                         });
         TEUCHOS_TEST_FOR_EXCEPTION_CLASS_FUNC
           (numInvalidExports == 0, std::logic_error, "Calling getRemoteIndexList "
            "on the target Map returned IDNotPresent, but none of the returned "
