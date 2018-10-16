de49d098228999ba261ee79228d1e29777443209
Roger Pawlowski <rppawlo@sandia.gov>
2018-10-16 14:41:05 -0400

    revert disable of panzer Curl test on waterman #3340

--- cmake/std/atdm/waterman/tweaks/CUDA_COMMON_TWEAKS.cmake
+++ cmake/std/atdm/waterman/tweaks/CUDA_COMMON_TWEAKS.cmake
@@ -1,6 +1,3 @@
 # This test fails consistently (#2751)
 ATDM_SET_ENABLE(PanzerAdaptersSTK_MixedPoissonExample-ConvTest-Hex-Order-3_DISABLE ON)
 
-# This test runs out of CUDA memory all on its own (#3340)
-ATDM_SET_ENABLE(PanzerAdaptersSTK_CurlLaplacianExample-ConvTest-Quad-Order-4_DISABLE ON)
-
