284dfd4ff8a248d9712511adf6d7d5fc192d1223
Chris Siefert <csiefer@sandia.gov>
2018-11-28 09:43:50 -0700

    Ctest: rocketman fixes

--- cmake/ctest/drivers/rocketman/ctest_linux_experimental_mpi_release_avatar_rocketman.cmake
+++ cmake/ctest/drivers/rocketman/ctest_linux_experimental_mpi_release_avatar_rocketman.cmake
@@ -88,7 +88,7 @@ SET(EXTRA_CONFIGURE_OPTIONS
   "-DTPL_ENABLE_X11=OFF"
   "-DAvatar_INCLUDE_DIRS=/home/csiefer/avatar/avatar/src;/home/csiefer/avatar/avatar/util/fclib-1.6.1/modules"
   "-DAvatar_LIBRARY_DIRS=/home/csiefer/avatar/build/src;/home/csiefer/avatar/build/util/fclib-1.6.1/modules"
-  "-DTPL_Avatar_LIBRARIES='/home/csiefer/avatar/build/src/libavatar.a /home/csiefer/avatar/build/util/fclib-1.6.1/modules/libfclib.a'"
+  "-DTPL_Avatar_LIBRARIES='/home/csiefer/avatar/build/src/libavatar.a;/home/csiefer/avatar/build/util/fclib-1.6.1/modules/libfclib.a'"
   "-DTrilinos_EXTRA_LINK_FLAGS:STRING='-Wl,-rpath,/home/csiefer/avatar/TPLs/lib -L/home/csiefer/avatar/TPLs/lib -lgsl -lgslcblas'"
 )
 
