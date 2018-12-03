a847842cece23a92616e1777fe0ef064971a5029
Roscoe A. Bartlett <rabartl@sandia.gov>
2018-12-03 08:30:35 -0700

    Update all_supported_builds.sh file to support ctest-s-local-test-driver.sh (TRIL-212)
    This matches the changes that I forgot to make in commit:
    
      commit ab6fbffd8617141792d18e8a98a12b167ae8bf0b
      Author: Roscoe A. Bartlett <rabartl@sandia.gov>
      Date:   Sat Nov 17 09:29:02 2018 -0700

--- cmake/std/atdm/waterman/all_supported_builds.sh
+++ cmake/std/atdm/waterman/all_supported_builds.sh
@@ -1,1 +1,9 @@
-export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=" gnu-release-debug-openmp-Power9-Volta70 gnu-opt-openmp-Power9-Volta70 cuda-9.2-release-debug-Power9-Volta70 cuda-9.2-debug-Power9-Volta70 cuda-9.2-opt-Power9-Volta70"
+export ATDM_CONFIG_CTEST_S_BUILD_NAME_PREFIX=Trilinos-atdm-waterman-
+
+export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=(
+  cuda-9.2-debug
+  cuda-9.2-opt
+  cuda-9.2-release-debug
+  gnu-debug-openmp
+  gnu-release-debug-openmp
+  )
