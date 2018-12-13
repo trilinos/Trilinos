68a42cfaec7c40dea7a0cbf841969a99e2d1c9f9
William McLendon <wcmclen@sandia.gov>
2018-12-13 14:56:44 -0700

    Tpetra: CrsGraph::swap() test checkpoint

--- packages/tpetra/core/test/CrsGraph/CrsGraph_UnitTests_Swap.cpp
+++ packages/tpetra/core/test/CrsGraph/CrsGraph_UnitTests_Swap.cpp
@@ -132,78 +132,45 @@ generate_crsgraph(Teuchos::RCP<comm_t>                      comm,
     typedef Tpetra::CrsGraph<LO, GO, Node> graph_t;
     typedef Tpetra::Map<LO, GO, Node>      map_t;
 
-    typedef typename std::pair<GO, GO>     pair_edge_t;
-    typedef typename std::pair<GO, size_t> pair_owner_t;
-
-    typedef typename std::vector<pair_edge_t> vec_edges_t;
-    typedef typename vec_edges_t::const_iterator vec_edges_const_iter_t;
-
-    typedef typename std::vector<pair_owner_t> vec_owners_t;
-    typedef typename vec_owners_t::const_iterator vec_owners_const_iter_t;
-
-    typedef typename std::unordered_map<GO, size_t> hash_row2pid_t;
-    //typedef typename std::set<GO> set_rows_t;
-    typedef typename std::map<GO, size_t> map_rows_t;   // row_id, num_entries
+    typedef typename std::map<GO, size_t> map_rows_t;      // row_id, num_entries
 
 
-    typedef typename std::vector<GO> vec_go_t;
+    typedef typename std::vector<GO>        vec_go_t;
     typedef typename std::map<GO, vec_go_t> map_row_to_cols_t;
 
     const size_t comm_rank = comm->getRank();
-
-    size_t num_gbl_elements = gbl_edges.size();
 
     map_row_to_cols_t gbl_rows;
     for(auto& e: gbl_edges)
     {
         if(gbl_rows.find(e.first) == gbl_rows.end())
         {
-            gbl_rows[e.first] = vec_go_t();
+            gbl_rows[ e.first ] = vec_go_t();
         }
-        gbl_rows[e.first].push_back(e.second);
+        gbl_rows[ e.first ].push_back(e.second);
     }
-
-
-    // Uniqueify the list of rows & get their counts
-    /*
-    map_rows_t gbl_rows;
-    for(auto & p: gbl_edges)
-    {
-        if(gbl_rows.find(p.first) != gbl_rows.end())
-        {
-            gbl_rows[p.first] = gbl_rows[p.first] + 1;
-        }
-        else
-        {
-            gbl_rows[p.first] = 1;
-        }
-    }
-    */
 
     map_rows_t gbl_row2pid;
-    for(auto & p: gbl_row_owners)
-    {
-        gbl_row2pid.insert(p);
-    }
+    for(auto& p: gbl_row_owners) { gbl_row2pid.insert(p); }
 
-    if(0==comm_rank)
+    // Print out some debugging information on what's in the graph
+    #if 0
+    std::cout << "p=0 | gbl_num_rows: " << gbl_rows.size() << std::endl;
+    for(auto& p: gbl_rows)
     {
-        std::cout << "p=0 | gbl_num_rows: " << gbl_rows.size() << std::endl;
-        for(auto& p: gbl_rows)
+        std::cout << "p=0 | gbl_row     : " << p.first << " (" << p.second.size() << ") ";
+        for(auto& j: p.second)
         {
-            std::cout << "p=0 | gbl_row     : " << p.first << " (" << p.second.size() << ") ";
-            for(auto& j: p.second)
-            {
-                std::cout << j << " ";
-            }
-            std::cout << std::endl;
+            std::cout << j << " ";
         }
-        for(auto& p: gbl_row2pid)
-            std::cout << "p=0 | gbl_row2pid : " << p.first << " => " << p.second << std::endl;
+        std::cout << std::endl;
     }
+    for(auto& p: gbl_row2pid)
+        std::cout << "p=0 | gbl_row2pid : " << p.first << " => " << p.second << std::endl;
+    #endif
 
-    GO gbl_num_rows = gbl_rows.size();  // the number of global rows
-    LO lcl_num_rows = 0;                // this will be updated later
+    GO gbl_num_rows = gbl_rows.size();      // the number of global rows
+    LO lcl_num_rows = 0;                    // this will be updated later
 
     // How many local rows do I have?
     for(auto& r: gbl_row2pid)
@@ -214,92 +181,82 @@ generate_crsgraph(Teuchos::RCP<comm_t>                      comm,
         }
     }
 
+    #if 0
     std::cout << "p=" << comm_rank << " | " << "lcl_num_rows = " << lcl_num_rows << std::endl;
+    #endif
 
     // Set up global ids
     std::vector<GO> global_ids;
-    for(auto & r: gbl_row2pid)
+    for(auto& r: gbl_row2pid)
     {
         if(comm_rank == r.second)
         {
             global_ids.push_back(r.first);
         }
     }
+    #if 0
     for(size_t i = 0; i < global_ids.size(); i++)
     {
         std::cout << "p=" << comm_rank << " | " << "global_ids[" << i << "] = " << global_ids[i] << std::endl;
     }
+    #endif
 
     // Create the Row Map
+    #if 0
     std::cout << "p=" << comm_rank << " | " << "row_map = map_t(" << gbl_num_rows << ", " << "global_ids.data(), "
               << lcl_num_rows << ", 0, comm)" << std::endl;
+    #endif
     RCP<const map_t> row_map(new map_t(gbl_num_rows, global_ids.data(), lcl_num_rows, 0, comm));
 
     Teuchos::ArrayRCP<size_t> num_ent_per_row(lcl_num_rows);
-    size_t idx=0;
+    size_t                    idx = 0;
     for(auto& r: gbl_rows)
     {
-        const GO     irow  = r.first;
-        const size_t row_pid  = gbl_row2pid.find(irow)->second;
-        if(row_pid==comm_rank)
+        const GO     irow    = r.first;
+        const size_t row_pid = gbl_row2pid.find(irow)->second;
+        if(row_pid == comm_rank)
         {
-            num_ent_per_row[idx++] = r.second.size();
+            num_ent_per_row[ idx++ ] = r.second.size();
         }
     }
 
+    #if 0
     std::cout << "p=" << comm_rank << " | lcl_num_rows = " << lcl_num_rows << std::endl;
     for(int i=0; i<lcl_num_rows; i++)
     {
         std::cout << "p=" << comm_rank << " | " << "num_ent_per_row[" << i << "] = " << num_ent_per_row[i] << std::endl;
     }
+    #endif
 
-    //std::cout << "p=" << comm_rank << " | " << ""
     RCP<graph_t> output_graph(new graph_t(row_map, num_ent_per_row, Tpetra::StaticProfile));
-
-    // todo: fill in the rest of it -- be careful to iterate over parts in the same order.
 
     for(auto& r: gbl_rows)
     {
-        const GO irow = r.first;
-        const size_t pid = gbl_row2pid.find(irow)->second;
-        if(pid==comm_rank)
+        const GO     irow = r.first;
+        const size_t pid  = gbl_row2pid.find(irow)->second;
+        if(pid == comm_rank)
         {
             std::vector<GO> gbl_inds;
             for(auto& v: r.second)
             {
-                std::cout << "p=" << comm_rank << " | " << "gbl_inds.push_back(" << v << ")" << std::endl;
+                // std::cout << "p=" << comm_rank << " | " << "gbl_inds.push_back(" << v << ")" << std::endl;
                 gbl_inds.push_back(v);
             }
 
+            #if 0
             std::cout << "p=" << comm_rank << " | " << "gbl_inds size = " << r.second.size() << std::endl;
             for(size_t i=0; i<gbl_inds.size(); i++)
             {
                 std::cout << "p=" << comm_rank << " | " << "gbl_inds[" << i << "] = " << gbl_inds[i] << std::endl;
             }
+            #endif
 
-            std::cout << "p=" << comm_rank << " | " << "insertGlobalIndices(" << irow << ", " << r.second.size() << ", gbl_inds.data())" << std::endl;
+            // std::cout << "p=" << comm_rank << " | " << "insertGlobalIndices(" << irow << ", " << r.second.size() << ",
+            // gbl_inds.data())" << std::endl;
             output_graph->insertGlobalIndices(irow, r.second.size(), gbl_inds.data());
         }
     }
 
-#if 0
-    if(0==comm_rank)
-    {
-        std::cout << "gbl_num_rows: " << gbl_rows.size() << std::endl;
-        for(auto& p: gbl_rows)
-        {
-            std::cout << "gbl_row     : " << p.first << " (" << p.second.size() << ") ";
-            for(auto& j: p.second)
-            {
-                std::cout << j << " ";
-            }
-            std::cout << std::endl;
-        }
-        for(auto& p: gbl_row2pid)
-            std::cout << "gbl_row2pid : " << p.first << " => " << p.second << std::endl;
-    }
-#endif
-
     RCP<const map_t> range_map = row_map;
 
     const GO         index_base = 0;
@@ -309,85 +266,6 @@ generate_crsgraph(Teuchos::RCP<comm_t>                      comm,
 
     return output_graph;
 }
-
-
-
-
-
-template<class LO, class GO, class Node, class comm_t>
-Teuchos::RCP<Tpetra::CrsGraph<LO, GO, Node>>
-gen_crsgraph_a(Teuchos::RCP<comm_t> comm)
-{
-    using Teuchos::Comm;
-
-    typedef Tpetra::CrsGraph<LO, GO, Node> graph_t;
-    typedef Tpetra::Map<LO, GO, Node>      map_t;
-
-    const int comm_rank = comm->getRank();
-
-    GO gbl_num_rows = 5;      // there are 5 global rows
-    LO lcl_num_rows = 0;      // this will be set later
-
-    std::vector<GO> global_ids;
-    if(0 == comm_rank)
-    {
-        lcl_num_rows = 3;
-        global_ids   = {0, 1, 3};
-    }
-    else if(1 == comm_rank)
-    {
-        lcl_num_rows = 2;
-        global_ids   = {7, 10};
-    }
-
-    RCP<const map_t> row_map(new map_t(gbl_num_rows, global_ids.data(), lcl_num_rows, 0, comm));
-
-    Teuchos::ArrayRCP<size_t> num_ent_per_row(lcl_num_rows);
-    if(0 == comm_rank)
-    {
-        num_ent_per_row[0] = 2;      // (0,0), (0,11)
-        num_ent_per_row[1] = 2;      // (1,3), (1,4)
-        num_ent_per_row[2] = 1;      // (3,2)
-    }
-    else if(1 == comm_rank)
-    {
-        num_ent_per_row[0] = 2;      // (7,5), (7,7)
-        num_ent_per_row[1] = 1;      // (10,6)
-    }
-    RCP<graph_t> output_graph(new graph_t(row_map, num_ent_per_row, Tpetra::StaticProfile));
-
-    std::vector<GO> gbl_inds(2);
-    if(0 == comm_rank)
-    {
-        gbl_inds[0] = 0;
-        gbl_inds[1] = 11;
-        output_graph->insertGlobalIndices(0, 2, gbl_inds.data());      // (0,0), (0,11)
-        gbl_inds[0] = 3;
-        gbl_inds[1] = 4;
-        output_graph->insertGlobalIndices(1, 2, gbl_inds.data());      // (1,3), (1,4)
-        gbl_inds[0] = 2;
-        output_graph->insertGlobalIndices(3, 1, gbl_inds.data());      // (3,2)
-    }
-    else if(1 == comm_rank)
-    {
-        gbl_inds[0] = 5;
-        gbl_inds[1] = 7;
-        output_graph->insertGlobalIndices(7, 2, gbl_inds.data());      // (7,5), (7,7)
-        gbl_inds[0] = 6;
-        output_graph->insertGlobalIndices(10, 1, gbl_inds.data());     // (10,6)
-    }
-
-    RCP<const map_t> range_map = row_map;
-
-    const GO         index_base = 0;
-    RCP<const map_t> domain_map(new map_t(12, index_base, comm));
-
-    output_graph->fillComplete(domain_map, range_map);
-
-    return output_graph;
-}
-
-
 
 
 
@@ -396,10 +274,10 @@ gen_crsgraph_a(Teuchos::RCP<comm_t> comm)
 //
 TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(CrsGraph, Swap, LO, GO, Node)
 {
+    using std::endl;
     using Teuchos::Comm;
     using Teuchos::outArg;
     using Teuchos::RCP;
-    using std::endl;
 
     typedef Teuchos::Comm<int>             comm_t;
     typedef Tpetra::CrsGraph<LO, GO, Node> graph_t;
@@ -407,11 +285,8 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(CrsGraph, Swap, LO, GO, Node)
     typedef typename std::pair<GO, GO>     pair_edge_t;
     typedef typename std::pair<GO, size_t> pair_owner_t;
 
-    typedef typename std::vector<pair_edge_t> vec_edges_t;
-    typedef typename vec_edges_t::const_iterator vec_edges_const_iter_t;
-
+    typedef typename std::vector<pair_edge_t>  vec_edges_t;
     typedef typename std::vector<pair_owner_t> vec_owners_t;
-    typedef typename vec_owners_t::const_iterator vec_owners_const_iter_t;
 
 
     auto initialComm = getDefaultComm();
@@ -433,107 +308,37 @@ TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(CrsGraph, Swap, LO, GO, Node)
 
         success = true;
 
+        vec_edges_t  vec_edges  = {pair_edge_t(0, 0), pair_edge_t(0, 11), pair_edge_t(1, 3), pair_edge_t(1, 4),
+                                   pair_edge_t(3, 2), pair_edge_t(7, 5),  pair_edge_t(7, 7), pair_edge_t(10, 6)};
+        vec_owners_t vec_owners = {pair_owner_t(0, 0), pair_owner_t(1, 0), pair_owner_t(3, 0), pair_owner_t(7, 1),
+                                   pair_owner_t(10, 1)};
 
-        #if 0
-        Teuchos::RCP<Tpetra::CrsGraph<LO, GO, Node>>
-        generate_crsgraph(Teuchos::RCP<comm_t>                      comm,
-                          const std::vector<std::pair<GO, GO>>&     edges,
-                          const std::vector<std::pair<GO, size_t>>& row_owners);
+         #if 0
+        for(auto& p: vec_edges)  out << "e: " << p.first << ", " << p.second << std::endl;
+        for(auto& p: vec_owners) out << "o: " << p.first << ", " << p.second << std::endl;
         #endif
 
-        vec_edges_t vec_edges = { pair_edge_t( 0, 0), pair_edge_t( 0,11), pair_edge_t( 1, 3)
-                                , pair_edge_t( 1, 4), pair_edge_t( 3, 2), pair_edge_t( 7, 5)
-                                , pair_edge_t( 7, 7), pair_edge_t(10, 6)
-                                };
-        vec_owners_t vec_owners = { pair_owner_t(0,0), pair_owner_t(1,0), pair_owner_t(3,0)
-                                  , pair_owner_t(7,1), pair_owner_t(10,1)
-                                  };
+        out << "Create graph_a" << std::endl;
+        RCP<graph_t> graph_a = generate_crsgraph<LO, GO, Node, comm_t>(comm, vec_edges, vec_owners);
+        graph_a->describe(out, Teuchos::VERB_DEFAULT);
 
-        #if 0
-        for(auto& p: vec_edges)
-            out << "e: " << p.first << ", " << p.second << std::endl;
-        for(auto& p: vec_owners)
-            out << "o: " << p.first << ", " << p.second << std::endl;
-        #endif
+        out << "Create graph_b" << std::endl;
+        RCP<graph_t> graph_b = generate_crsgraph<LO, GO, Node, comm_t>(comm, vec_edges, vec_owners);
+        graph_b->describe(out, Teuchos::VERB_DEFAULT);
 
-        RCP<graph_t> graph_a = generate_crsgraph<LO,GO,Node,comm_t>(comm, vec_edges, vec_owners);
-        graph_a->describe(out, Teuchos::VERB_EXTREME);
+        vec_edges.clear();
+        vec_owners.clear();
 
-        //RCP<graph_t> graph_a = generate_crsgraph<LO,GO,Node,comm_t>(comm, vec_edges, vec_owners);
-        //graph_a->describe(out, Teuchos::VERB_DEFAULT);
+        vec_edges  = {pair_edge_t(0, 0), pair_edge_t(0, 11), pair_edge_t(1, 3), pair_edge_t(1, 4),
+                      pair_edge_t(3, 2), pair_edge_t(7, 5), pair_edge_t(7, 7), pair_edge_t(10, 6)};
+        vec_owners = {pair_owner_t(0, 0), pair_owner_t(1, 0), pair_owner_t(3, 0), pair_owner_t(7, 1),
+                      pair_owner_t(10, 1)};
 
-        // RCP<graph_t> graph_a = gen_crsgraph<LO,GO,Node,comm_t>(comm, ... );
-
-        #if 0
-        RCP<graph_t> graph_b = gen_crsgraph_a< LO,GO,Node,comm_t>(comm);
-        graph_b->describe(out, Teuchos::VERB_EXTREME);
-        #endif
+        out << "Create graph_c" << std::endl;
+        RCP<graph_t> graph_c = generate_crsgraph<LO, GO, Node, comm_t>(comm, vec_edges, vec_owners);
+        graph_c->describe(out, Teuchos::VERB_DEFAULT);
     }
 }
-
-
-////
-#if 0
-TEUCHOS_UNIT_TEST_TEMPLATE_3_DECL(CrsGraph, WithColMap, LO, GO, Node)
-{
-    using Teuchos::Comm;
-    using Teuchos::outArg;
-    using Teuchos::RCP;
-    using Teuchos::REDUCE_MIN;
-    using Teuchos::reduceAll;
-    typedef Tpetra::CrsGraph<LO, GO, Node> GRAPH;
-    typedef Tpetra::Map<LO, GO, Node>      map_type;
-
-    const GST INVALID = Teuchos::OrdinalTraits<GST>::invalid();
-
-    RCP<const Comm<int>> comm     = getDefaultComm();
-    const int            numProcs = comm->getSize();
-    if(numProcs > 1)
-    {
-        const size_t        numLocal = 1;
-        RCP<const map_type> rmap     = rcp(new map_type(INVALID, numLocal, 0, comm));
-        RCP<const map_type> cmap     = rcp(new map_type(INVALID, numLocal, 0, comm));
-
-        // must allocate enough for all submitted indices.
-        RCP<GRAPH> G = rcp(new GRAPH(rmap, cmap, 2, StaticProfile));
-        TEST_EQUALITY_CONST(G->hasColMap(), true);
-        const GO myrowind = rmap->getGlobalElement(0);
-
-        // mfh 16 May 2013: insertGlobalIndices doesn't do column Map
-        // filtering anymore, so we have to test whether each of the
-        // column indices to insert is invalid.
-        if(cmap->isNodeGlobalElement(myrowind))
-        {
-            if(cmap->isNodeGlobalElement(myrowind + 1))
-            {
-                TEST_NOTHROW(G->insertGlobalIndices(myrowind, tuple<GO>(myrowind, myrowind + 1)));
-            }
-            else
-            {
-                TEST_NOTHROW(G->insertGlobalIndices(myrowind, tuple<GO>(myrowind)));
-            }
-        }
-        TEST_NOTHROW(G->fillComplete());
-        TEST_EQUALITY(G->getRowMap(), rmap);
-        TEST_EQUALITY(G->getColMap(), cmap);
-        TEST_EQUALITY(G->getNumEntriesInGlobalRow(myrowind), 1);
-    }
-    // All procs fail if any node fails
-    int lclSuccess = success ? 1 : 0;
-    int gblSuccess = 1;
-    reduceAll<int, int>(*comm, REDUCE_MIN, lclSuccess, outArg(gblSuccess));
-
-    if(gblSuccess == 1)
-    {
-        out << "Succeeded on all processes!" << endl;
-    }
-    else
-    {
-        out << "FAILED on at least one process!" << endl;
-    }
-    TEST_EQUALITY_CONST(gblSuccess, 1);
-}
-#endif
 
 
   //
