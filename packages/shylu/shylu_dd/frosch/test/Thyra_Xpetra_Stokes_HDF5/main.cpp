f12178704ba3d098737ed0a2f1f9c1cf381e37cc
Alexander Heinlein <alexander.heinlein@uni-koln.de>
2019-01-08 12:10:30 +0100

    Minor changes. Merging the changes of mayrmt.

--- packages/shylu/shylu_dd/frosch/test/Thyra_Xpetra_Stokes_HDF5/main.cpp
+++ packages/shylu/shylu_dd/frosch/test/Thyra_Xpetra_Stokes_HDF5/main.cpp
@@ -84,6 +84,7 @@
 
 // Xpetra include
 #include <Xpetra_CrsMatrixWrap.hpp>
+#include <Xpetra_DefaultPlatform.hpp>
 #ifdef HAVE_SHYLU_DDFROSCH_EPETRA
 #include <Xpetra_EpetraCrsMatrix.hpp>
 #endif
@@ -113,7 +114,7 @@ int main(int argc, char *argv[])
     oblackholestream blackhole;
     GlobalMPISession mpiSession(&argc,&argv,&blackhole);
     
-    RCP<const Comm<int> > CommWorld = Tpetra::getDefaultComm();
+    RCP<const Comm<int> > CommWorld = Xpetra::DefaultPlatform::getDefaultPlatform().getComm();
     
     CommandLineProcessor My_CLP;
     
