489985ce2d941f0f4e472d28f30951ed6522ed57
James M. Willenbring <jmwille@sandia.gov>
2019-01-11 13:03:56 -0700

    Fixing CUDA configuration

--- cmake/std/sems/PullRequestCuda9.2TestingEnv.sh
+++ cmake/std/sems/PullRequestCuda9.2TestingEnv.sh
@@ -8,11 +8,12 @@
 module load git/2.10.1
 module load devpack/20180521/openmpi/2.1.2/gcc/7.2.0/cuda/9.2.88
 module swap openblas/0.2.20/gcc/7.2.0 netlib/3.8.0/gcc/7.2.0
-export OMPI_CXX=`which g++`
+#export OMPI_CXX=`which g++`
+export OMPI_CXX=$WORKSPACE/Trilinos/packages/kokkos/bin/nvcc_wrapper
 export OMPI_CC=`which gcc`
 export OMPI_FC=`which gfortran`
+export NVCC_WRAPPER_DEFAULT_COMPILER=`which g++`
 export CUDA_LAUNCH_BLOCKING=1
-export NVCC_WRAPPER_DEFAULT_COMPILER=$WORKSPACE/Trilinos/packages/kokkos/bin/nvcc_wrapper
 
 # Use manually installed cmake and ninja to try to avoid module loading
 # problems (see TRIL-208)
