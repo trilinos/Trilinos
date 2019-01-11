8c634c5d11295c3ea90b2a1ff7c29195cf45a06f
James M. Willenbring <jmwille@sandia.gov>
2019-01-11 15:07:45 -0700

    Adding Netcdf library configuration.

--- cmake/std/PullRequestLinuxCuda9.2TestingSettings.cmake
+++ cmake/std/PullRequestLinuxCuda9.2TestingSettings.cmake
@@ -22,7 +22,8 @@ set (KOKKOS_ARCH Power8 CACHE STRING "Set by default for CUDA PR testing")
 set (TPL_BLAS_LIBRARIES "-L${BLAS_ROOT}/lib -lblas -lgfortran -lgomp -lm" CACHE STRING "Set by default for CUDA PR testing")
 set (TPL_LAPACK_LIBRARIES "-L${LAPACK_ROOT}/lib -llapack -lgfortran -lgomp" CACHE STRING "Set by default for CUDA PR testing")
 set (TPL_ENABLE_Scotch OFF CACHE BOOL "Set by default for CUDA PR testing")
-#Parmetis is available on ride and could be enabled for the CUDA PR build
+# Parmetis is available on ride and could be enabled for the CUDA PR build
 set (TPL_ENABLE_ParMETIS OFF CACHE BOOL "Set by default for CUDA PR testing")
+set (TPL_Netcdf_LIBRARIES "-L${BOOST_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${PNETCDF_ROOT}/lib;-L${HDF5_ROOT}/lib;${BOOST_ROOT}/lib/libboost_program_options.a;${BOOST_ROOT}/lib/libboost_system.a;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl" CACHE STRING "Set by default for CUDA PR testing")
 include("${CMAKE_CURRENT_LIST_DIR}/PullRequestLinuxCommonTestingSettings.cmake")
 
