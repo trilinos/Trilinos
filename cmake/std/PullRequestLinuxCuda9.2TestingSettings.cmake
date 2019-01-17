0fbb5ce01e1223c7602c20545dde8c19a944bb9b
James M. Willenbring <jmwille@sandia.gov>
2019-01-17 13:42:28 -0700

    Turning off secondary tested code.

--- cmake/std/PullRequestLinuxCuda9.2TestingSettings.cmake
+++ cmake/std/PullRequestLinuxCuda9.2TestingSettings.cmake
@@ -23,6 +23,7 @@ set (TPL_BLAS_LIBRARIES "-L${BLAS_ROOT}/lib -lblas -lgfortran -lgomp -lm" CACHE 
 set (TPL_LAPACK_LIBRARIES "-L${LAPACK_ROOT}/lib -llapack -lgfortran -lgomp" CACHE STRING "Set by default for CUDA PR testing")
 set (BUILD_SHARED_LIBS OFF CACHE BOOL "Set by default for CUDA PR testing")
 set (Tpetra_INST_SERIAL ON CACHE BOOL "Set by default for CUDA PR testing")
+set (Trilinos_ENABLE_SECONDARY_TESTED_CODE OFF CACHE BOOL "Set by default for CUDA PR testing")
 set (TPL_ENABLE_Scotch OFF CACHE BOOL "Set by default for CUDA PR testing")
 # Parmetis is available on ride and could be enabled for the CUDA PR build
 set (TPL_ENABLE_ParMETIS OFF CACHE BOOL "Set by default for CUDA PR testing")
