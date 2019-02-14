01742b423553ff2066b21bb51e10dee8db2f5164
Chris Siefert <csiefer@sandia.gov>
2019-02-14 15:19:58 -0700

    MueLu: Purging debug

--- packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_TentativePFactory_def.hpp
+++ packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_TentativePFactory_def.hpp
@@ -140,8 +140,6 @@ namespace MueLu {
     typedef Xpetra::MultiVector<coordinate_type,LO,GO,NO> CoordinateMultiVector;
     typedef Xpetra::MultiVectorFactory<coordinate_type,LO,GO,NO> CoordinateMultiVectorFactory;
 
-    fineLevel.print(std::cout,Debug);// CMS DEBUG
-
     RCP<Matrix>                A             = Get< RCP<Matrix> >               (fineLevel, "A");
     RCP<Aggregates>            aggregates    = Get< RCP<Aggregates> >           (fineLevel, "Aggregates");
     RCP<AmalgamationInfo>      amalgInfo     = Get< RCP<AmalgamationInfo> >     (fineLevel, "UnAmalgamationInfo");
