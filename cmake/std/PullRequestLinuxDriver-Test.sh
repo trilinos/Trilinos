6f7011161c006682f24e131a95297885967b1d7a
William McLendon <wcmclen@sandia.gov>
2019-02-19 11:05:59 -0700

    Framework: Updating PR_CDASH_TRACK param check

--- cmake/std/PullRequestLinuxDriver-Test.sh
+++ cmake/std/PullRequestLinuxDriver-Test.sh
@@ -300,7 +300,7 @@ fi
 CDASH_TRACK="Pull Request"
 
 # If PULLREQUEST_CDASH_TRACK is set and nonzero
-if [ ! -z ${PULLREQUEST_CDASH_TRACK} ]; then
+if [ ! -z "${PULLREQUEST_CDASH_TRACK}" ]; then
     CDASH_TRACK=${PULLREQUEST_CDASH_TRACK:?}
     echo -e "PULLREQUEST_CDASH_TRACK is set. Setting CDASH_TRACK=${PULLREQUEST_CDASH_TRACK:?}"
 else
