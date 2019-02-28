325d306877a53cbf64c1079c14e0fa9b3016b087
Joe Frye <jfrye@sandia.gov>
2019-02-28 10:17:14 -0700

    unload sems-python after loading sems-boost

--- cmake/std/atdm/sems-rhel6/environment.sh
+++ cmake/std/atdm/sems-rhel6/environment.sh
@@ -166,6 +166,7 @@ module load sems-netcdf/4.4.1/exo_parallel
 module load sems-hdf5/1.8.12/parallel
 module load sems-zlib/1.2.8/base
 module load sems-boost/1.59.0/base
+module unload sems-python/2.7.9 
 module load sems-superlu/4.3/base
 
 if [[ "${ATDM_CONFIG_SHARED_LIBS}" == "ON" ]] ; then
