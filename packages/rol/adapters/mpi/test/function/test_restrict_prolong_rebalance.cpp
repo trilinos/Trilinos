fbba96f03539a3350d61dfa674c561590deb4a0b
Eric C. Cyr <eccyr@sandia.gov>
2019-03-12 13:43:42 -0600

    ROL: Adding a SHA1 to a comment

--- packages/rol/adapters/mpi/test/function/test_restrict_prolong_rebalance.cpp
+++ packages/rol/adapters/mpi/test/function/test_restrict_prolong_rebalance.cpp
@@ -563,7 +563,7 @@ void testRestrictionProlong_OptVector(MPI_Comm comm, const ROL::Ptr<std::ostream
 
   /*
   // THIS NEEDS TO BE TESTED, BUT ITS COMPLICATED SO I'M SKIPPING FOR NOW: Code works as of 3/12/2019
-  //    see SHA1 84afc4dc4a56837387cdeea7955f7779554df608
+  //    see SHA1 28d50e83df19e8cb8dc3fc2e25573cdb8b062b03
 
   // check errors on the finest level
   for(size_t i=0;i<stamps_0->size();i++) {
