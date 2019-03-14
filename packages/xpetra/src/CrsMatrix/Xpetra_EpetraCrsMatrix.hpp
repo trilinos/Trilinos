19c934630af77218c2ab194a94b3587688b82e50
Christian Glusa <caglusa@sandia.gov>
2019-03-14 14:28:32 -0600

    Xpetra: Apply labels to wrapper and underlying object

--- packages/xpetra/src/CrsMatrix/Xpetra_EpetraCrsMatrix.hpp
+++ packages/xpetra/src/CrsMatrix/Xpetra_EpetraCrsMatrix.hpp
@@ -1098,7 +1098,10 @@ public:
 
   }
 
-  void setObjectLabel( const std::string &objectLabel ) {mtx_->SetLabel(objectLabel.c_str());}
+  void setObjectLabel( const std::string &objectLabel ) {
+    Teuchos::LabeledObject::setObjectLabel(objectLabel);
+    mtx_->SetLabel(objectLabel.c_str());
+  }
   //@}
 
   //! Deep copy constructor
@@ -2081,7 +2084,10 @@ public:
 
   }
 
-  void setObjectLabel( const std::string &objectLabel ) { mtx_->SetLabel(objectLabel.c_str());}
+  void setObjectLabel( const std::string &objectLabel ) {
+    Teuchos::LabeledObject::setObjectLabel(objectLabel);
+    mtx_->SetLabel(objectLabel.c_str());
+  }
   //@}
 
   //! Deep copy constructor
