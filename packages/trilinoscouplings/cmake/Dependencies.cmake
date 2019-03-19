3feaf2f6b4ca79d62174d0f6e092f3908baa90a3
Roscoe A. Bartlett <rabartl@sandia.gov>
2019-03-19 15:42:25 -0600

    Fix and narrow dependencies on STK (#4502)
    Only tests/exmaples depend on STK and then only the subpackages STKIO and
    STKMesh.  This will also avoid the multiple main() link errors occuring in

--- packages/trilinoscouplings/cmake/Dependencies.cmake
+++ packages/trilinoscouplings/cmake/Dependencies.cmake
@@ -1,8 +1,8 @@
 SET(LIB_REQUIRED_DEP_PACKAGES)
 SET(LIB_OPTIONAL_DEP_PACKAGES
-  EpetraExt Isorropia Amesos AztecOO Belos Ifpack ML MueLu NOX Zoltan STK Stokhos)
+  EpetraExt Isorropia Amesos AztecOO Belos Ifpack ML MueLu NOX Zoltan Stokhos)
 SET(TEST_REQUIRED_DEP_PACKAGES)
-SET(TEST_OPTIONAL_DEP_PACKAGES Amesos Isorropia Epetra EpetraExt Ifpack Intrepid Intrepid2 Pamgen AztecOO ML MueLu Zoltan Teko KokkosKernels Tpetra MueLu KokkosCore TeuchosKokkosCompat KokkosContainers TeuchosKokkosComm Stokhos TriKota Sacado Stratimikos)
+SET(TEST_OPTIONAL_DEP_PACKAGES Amesos Isorropia Epetra EpetraExt Ifpack Intrepid Intrepid2 Pamgen AztecOO ML MueLu Zoltan Teko KokkosKernels Tpetra MueLu KokkosCore TeuchosKokkosCompat KokkosContainers TeuchosKokkosComm Stokhos TriKota Sacado Stratimikos STKIO STKMesh)
 SET(LIB_REQUIRED_DEP_TPLS)
 SET(LIB_OPTIONAL_DEP_TPLS)
 SET(TEST_REQUIRED_DEP_TPLS)
