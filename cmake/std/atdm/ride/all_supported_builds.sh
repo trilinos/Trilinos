d99f8e0cd857d5e782849f10da1caa1ab5f092df
Roscoe A. Bartlett <rabartl@sandia.gov>
2019-04-17 07:29:10 -0600

    Remove CUDA 10.0 and GNU 7.4.0 stuff (TRIL-245)
    No need for CUDA 10.0, we need CUDA 10.1.  And CUDA 10.1 uses the GNU 7.2.0
    compiler.  There is no support for a GNU 7.4.0 compiler without CUDA 10.1 on
    this system at this point.  (NOTE: Some of the TPLs are still built with GNU
    7.4.0 but those are ABI compatible with GNU 7.2.0.)

--- cmake/std/atdm/ride/all_supported_builds.sh
+++ cmake/std/atdm/ride/all_supported_builds.sh
@@ -10,10 +10,4 @@ export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=(
   cuda-10.1-gnu-7.2.0-debug
   cuda-10.1-gnu-7.2.0-release
   cuda-10.1-gnu-7.2.0-release-debug
-  #cuda-10.0-gnu-7.4.0-debug
-  #cuda-10.0-gnu-7.4.0-release
-  #cuda-10.0-gnu-7.4.0-release-debug
-  #gnu-7.4.0-openmp-debug
-  #gnu-7.4.0-openmp-release
-  #gnu-7.4.0-openmp-release-debug
   )
--- cmake/std/atdm/ride/environment.sh
+++ cmake/std/atdm/ride/environment.sh
@@ -17,8 +17,6 @@ elif [[ "$ATDM_CONFIG_COMPILER" == "GNU"* ]]; then
     export ATDM_CONFIG_COMPILER=GNU-7.2.0
   elif [[ "$ATDM_CONFIG_COMPILER" == "GNU-7.2.0" ]] ; then
     export ATDM_CONFIG_COMPILER=GNU-7.2.0
-  elif [[ "$ATDM_CONFIG_COMPILER" == "GNU-7.4.0" ]] ; then
-    export ATDM_CONFIG_COMPILER=GNU-7.4.0
   else
     echo
     echo "***"
@@ -26,7 +24,6 @@ elif [[ "$ATDM_CONFIG_COMPILER" == "GNU"* ]]; then
     echo "*** Only GNU compilers supported on this system are:"
     echo "***   gnu (defaults to gnu-7.2.0)"
     echo "***   gnu-7.2.0 (default)"
-    echo "***   gnu-7.4.0"
     echo "***"
     return
   fi
@@ -37,11 +34,13 @@ elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA"* ]]; then
     export ATDM_CONFIG_COMPILER=CUDA-9.2_GNU-7.2.0
   elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-9.2_GNU-7.2.0" ]] ; then
     export ATDM_CONFIG_COMPILER=CUDA-9.2_GNU-7.2.0
-  elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10.0" ]] ; then
-    export ATDM_CONFIG_COMPILER=CUDA-10.0_GNU-7.4.0
-  elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10.0_GNU-7.4.0" ]] ; then
-    export ATDM_CONFIG_COMPILER=CUDA-10.0_GNU-7.4.0
   elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10.1_GNU-7.2.0" ]] ; then
+    export ATDM_CONFIG_COMPILER=CUDA-10.1_GNU-7.2.0
+  elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10.1_GNU-7.2.0" ]] ; then
+    export ATDM_CONFIG_COMPILER=CUDA-10.1_GNU-7.2.0
+  elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10.1" ]] ; then
+    export ATDM_CONFIG_COMPILER=CUDA-10.1_GNU-7.2.0
+  elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10" ]] ; then
     export ATDM_CONFIG_COMPILER=CUDA-10.1_GNU-7.2.0
   else
     echo
@@ -51,8 +50,7 @@ elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA"* ]]; then
     echo "***   cuda (defaults to cuda-9.2-gnu-7.2.0)"
     echo "***   cuda-9.2 (defaults to cuda-9.2-gnu-7.2.0)"
     echo "***   cuda-9.2-gnu-7.2.0 (default)"
-    echo "***   cuda-10.0 (defaults to cuda-10.0-gnu-7.7.0)"
-    echo "***   cuda-10.0-gnu-7.4.0"
+    echo "***   cuda-10.1 (defaults to cuda-10.1-gnu-7.2.0)"
     echo "***   cuda-10.1-gnu-7.2.0"
     echo "***"
     return
@@ -166,38 +164,10 @@ if [ "$ATDM_CONFIG_COMPILER" == "GNU-7.2.0" ] ; then
   export ATDM_CONFIG_LAPACK_LIBS="-L${LAPACK_ROOT}/lib;-llapack;-lgfortran;-lgomp"
   export ATDM_CONFIG_BLAS_LIBS="-L${BLAS_ROOT}/lib;-lblas;-lgfortran;-lgomp;-lm"
 
-elif [ "$ATDM_CONFIG_COMPILER" == "GNU-7.4.0" ] ; then
-
-  # Load the modules and set up env
-  module load devpack/20181218/openmpi/3.1.3/gcc/7.4.0/cuda/10.0.130
-  export OMPI_CXX=`which g++`
-  export OMPI_CC=`which gcc`
-  export OMPI_FC=`which gfortran`
-  export ATDM_CONFIG_LAPACK_LIBS="-L${LAPACK_ROOT}/lib;-llapack;-lgfortran;-lgomp"
-  export ATDM_CONFIG_BLAS_LIBS="-L${BLAS_ROOT}/lib;-lblas;-lgfortran;-lgomp;-lm"
-
 elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-9.2_GNU-7.2.0" ]] ; then
 
   module load devpack/20180521/openmpi/2.1.2/gcc/7.2.0/cuda/9.2.88
   module swap openblas/0.2.20/gcc/7.2.0 netlib/3.8.0/gcc/7.2.0
-  export OMPI_CXX=${ATDM_CONFIG_NVCC_WRAPPER}
-  if [ ! -x "$OMPI_CXX" ]; then
-      echo "No nvcc_wrapper found"
-      return
-  fi
-  export OMPI_CC=`which gcc`
-  export OMPI_FC=`which gfortran`
-  export ATDM_CONFIG_LAPACK_LIBS="-L${LAPACK_ROOT}/lib;-llapack;-lgfortran;-lgomp"
-  export ATDM_CONFIG_BLAS_LIBS="-L${BLAS_ROOT}/lib;-lblas;-lgfortran;-lgomp;-lm"
-  export ATDM_CONFIG_USE_CUDA=ON
-  export CUDA_LAUNCH_BLOCKING=1
-  export CUDA_MANAGED_FORCE_DEVICE_ALLOC=1
-  export ATDM_CONFIG_CTEST_PARALLEL_LEVEL=8
-  # ABove avoids timeouts due to not running on separate GPUs (see #2446)
-
-elif [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10.0_GNU-7.4.0" ]] ; then
-
-  module load devpack/20181218/openmpi/3.1.3/gcc/7.4.0/cuda/10.0.130
   export OMPI_CXX=${ATDM_CONFIG_NVCC_WRAPPER}
   if [ ! -x "$OMPI_CXX" ]; then
       echo "No nvcc_wrapper found"
