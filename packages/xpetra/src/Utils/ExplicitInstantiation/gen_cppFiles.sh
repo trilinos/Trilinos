39da36a9492195b7c3520638875559c36a7fb8f7
Jonathan Hu <jhu@sandia.gov>
2019-04-22 17:55:30 -0600

    Xpetra: first cut at ETI system
    This is modeled on MueLu's system.  I've converted
    part of the class Xpetra::TpetraCrsMatrix.

---
+++ packages/xpetra/src/Utils/ExplicitInstantiation/gen_cppFiles.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# internal field separator to figure out where to split words
+IFS=$'\n'
+
+classListDir=../ClassList/
+
+for i in LO-GO-NO SC-LO-GO-NO
+  do
+
+  classList=$classListDir/$i.classList
+  tmpl=$i.tmpl
+
+  for className in `cat $classList | grep -v ^\# | cut -d "-" -f1 | sed 's/ //'`
+    do
+
+    if ! grep -q -x $className $classListDir/EI-Exceptions.classList
+        then
+
+        condition=$(cat $classList | grep "^$className -" | cut -d "-" -f2-)
+
+        if [ -n "$condition" ]; then
+            conditionOpen1=$(echo "#include \\\"Xpetra_ConfigDefs.hpp\\\"")
+            conditionOpen2=$(echo $condition | sed 's/^[ ]*//' | sed 's/\&/\\\&/g')
+            conditionClose="#endif"
+        else
+            conditionOpen1=""
+            conditionOpen2=""
+            conditionClose=""
+        fi
+
+        cat $tmpl \
+            | sed "s/\$TMPL_CLASS/$className/g" \
+            | sed "s/\$TMPL_CONDITION_OPEN1/$conditionOpen1/g" \
+            | sed "s/\$TMPL_CONDITION_OPEN2/$conditionOpen2/g" \
+            | sed "s/\$TMPL_CONDITION_CLOSE/$conditionClose/g" \
+            > Xpetra_$className.cpp
+    fi
+
+  done
+
+done
