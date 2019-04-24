4eebda5faa42a9615f5f0b244a0af8b12772ff8f
Paul Wolfenbarger <prwolfe@sandia.gov>
2019-04-24 13:00:31 -0600

    Change gcc 7.2 PR testing to use python 3 and disable Modules tests
    This assures us that we have unit testing for python 3 as well as 2
    and disables a test that is known to fail in that condition. In fact
    it will fail until the Modules package is repaired.

--- cmake/std/sems/PullRequestGCC7.2.0TestingEnv.sh
+++ cmake/std/sems/PullRequestGCC7.2.0TestingEnv.sh
@@ -8,7 +8,7 @@ source /projects/sems/modulefiles/utils/sems-modules-init.sh
 
 module load sems-gcc/7.2.0
 module load sems-openmpi/1.10.1
-module load sems-python/2.7.9
+module load sems-python/3.5.2
 module load sems-git/2.10.1
 module load sems-boost/1.63.0/base
 module load sems-zlib/1.2.8/base
@@ -31,7 +31,6 @@ module load sems-cmake/3.10.3
 # available in main-line Ninja) to significantly speed up
 # compile and link times.
 module load atdm-env
-module load atdm-cmake/3.11.1
 module load atdm-ninja_fortran/1.7.2
 
 # add the OpenMP environment variable we need
