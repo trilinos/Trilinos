f26b647e7af8aa6887e9ed8da7e8fd0a058d968d
K. Devine <kddevin@sandia.gov>
2019-05-02 22:54:57 -0600

    teuchos deprecation:  updating downstream packages

--- packages/rol/adapters/teuchos/test/function/test_01.cpp
+++ packages/rol/adapters/teuchos/test/function/test_01.cpp
@@ -243,10 +243,10 @@ int main( int argc, char *argv[] ) {
       }
     }
 
-    *outStream << "\nA = " << *A;
-    *outStream << "\nb = " << *b;
-    *outStream << "\nC = " << *C;
-    *outStream << "\nd = " << *d;
+    *outStream << "\nA = "; A->print(*outStream);
+    *outStream << "\nb = "; b->print(*outStream);
+    *outStream << "\nC = "; C->print(*outStream);
+    *outStream << "\nd = "; d->print(*outStream);
     
 
     auto x = rcp( new Vector{Nopt,1} );
