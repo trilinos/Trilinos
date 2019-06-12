9512aab6d87b6d0985fd0ec2ee5dd31035cbc34b
Sidafa Conde <sconde@sandia.gov>
2019-06-11 23:10:17 -0600

    Tempus: observer - add stepper rk observer logging

---
+++ packages/tempus/src/Tempus_StepperRKObserverLogging_impl.hpp
@@ -0,0 +1,167 @@
+// @HEADER
+// ****************************************************************************
+//                Tempus: Copyright (2017) Sandia Corporation
+//
+// Distributed under BSD 3-clause license (See accompanying file Copyright.txt)
+// ****************************************************************************
+// @HEADER
+
+#ifndef Tempus_StepperRKObserverLogging_impl_hpp
+#define Tempus_StepperRKObserverLogging_impl_hpp
+
+#include "Tempus_StepperRKObserver.hpp"
+#include "Tempus_TimeStepControl.hpp"
+
+namespace Tempus {
+
+template<class Scalar>
+StepperRKObserverLogging<Scalar>::StepperRKObserverLogging()
+  : nameObserveBeginTakeStep_            ( "observeBeginTakeStep"),
+    nameObserveBeginStage_               ( "observeBeginStage"),
+    nameObserveBeforeImplicitExplicitly_ ( "observeBeforeImplicitExplicitly"),
+    nameObserveBeforeSolve_              ( "observeBeforeSolve"),
+    nameObserveAfterSolve_               ( "observeAfterSolve"),
+    nameObserveBeforeExplicit_           ( "observeBeforeExplicit"),
+    nameObserveEndStage_                 ( "observeEndStage"),
+    nameObserveEndTakeStep_              ( "observeEndTakeStep")
+{
+  counters_ = Teuchos::rcp(new std::map<std::string,int>);
+  order_ = Teuchos::rcp(new std::list<std::string>);
+  this->resetLogCounters();
+}
+
+template<class Scalar>
+StepperRKObserverLogging<Scalar>::~StepperRKObserverLogging(){}
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeBeginTakeStep(
+        Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+        Stepper<Scalar> & /* stepper */)
+{ 
+  
+  //asm("int $3");
+  //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  logCall(nameObserveBeginTakeStep_); 
+}
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeBeginStage(
+        Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+        Stepper<Scalar> & /* stepper */)
+{ 
+  //asm("int $3");
+  //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  
+  logCall(nameObserveBeginStage_); }
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeBeforeImplicitExplicitly(
+    Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+    Stepper<Scalar> & /* stepper */)
+{ 
+  //asm("int $3");
+  //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  
+  logCall(nameObserveBeforeImplicitExplicitly_); }
+
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeBeforeSolve(
+        Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+        Stepper<Scalar> & /* stepper */)
+{ 
+  //asm("int $3");
+  //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  
+  logCall(nameObserveBeforeSolve_); }
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeAfterSolve(
+        Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+        Stepper<Scalar> & /* stepper */)
+{
+  //asm("int $3");
+  //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  logCall(nameObserveAfterSolve_); }
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeBeforeExplicit(
+        Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+        Stepper<Scalar> & /* stepper */)
+{ 
+ //asm("int $3");
+ //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  
+  logCall(nameObserveBeforeExplicit_); }
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeEndStage(
+        Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+        Stepper<Scalar> & /* stepper */)
+{
+  
+  //asm("int $3");
+  //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  
+  logCall(nameObserveEndStage_); }
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::
+observeEndTakeStep(
+        Teuchos::RCP<SolutionHistory<Scalar> > /* sh */,
+        Stepper<Scalar> & /* stepper */)
+{ 
+  
+  //asm("int $3");
+  //std::cout << "SIDAFA: got here!!" << std::endl;
+
+  
+  logCall(nameObserveEndTakeStep_); }
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::resetLogCounters()
+{
+  (*counters_)[nameObserveBeginTakeStep_            ] = 0;
+  (*counters_)[nameObserveBeginStage_               ] = 0;
+  (*counters_)[nameObserveBeforeImplicitExplicitly_ ] = 0;
+  (*counters_)[nameObserveBeforeSolve_              ] = 0;
+  (*counters_)[nameObserveAfterSolve_               ] = 0;
+  (*counters_)[nameObserveBeforeExplicit_           ] = 0;
+  (*counters_)[nameObserveEndStage_                 ] = 0;
+  (*counters_)[nameObserveEndTakeStep_              ] = 0;
+  order_->clear();
+}
+
+template<class Scalar>
+Teuchos::RCP<const std::map<std::string,int> >
+StepperRKObserverLogging<Scalar>::getCounters()
+{ return counters_; }
+
+template<class Scalar>
+Teuchos::RCP<const std::list<std::string> >
+StepperRKObserverLogging<Scalar>::getOrder()
+{ return order_; }
+
+template<class Scalar>
+void StepperRKObserverLogging<Scalar>::logCall(const std::string call) const
+{
+  (*counters_)[call] += 1;
+  order_->push_back(call);
+}
+
+} // namespace Tempus
+#endif // Tempus_StepperRKObserverLogging_impl_hpp
