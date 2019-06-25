a50823455aa79044c5b2c64b31b058c183476b14
Nathan Ellingwood <ndellin@sandia.gov>
2019-06-25 17:11:25 -0600

    tacho: remove unused scheduler_name from unit test file

--- packages/shylu/shylu_node/tacho/unit-test/Tacho_Test.hpp
+++ packages/shylu/shylu_node/tacho/unit-test/Tacho_Test.hpp
@@ -1,27 +1,6 @@
 #ifndef __TACHO_TEST_HPP__
 #define __TACHO_TEST_HPP__
 
-#if defined(TACHO_USE_DEPRECATED_TASKSCHEDULER)
-template<typename T> using TaskSchedulerType = Kokkos::DeprecatedTaskScheduler<T>;
-static const char * scheduler_name = "DeprecatedTaskScheduler";
-#endif
-#if defined(TACHO_USE_DEPRECATED_TASKSCHEDULER_MULTIPLE)
-template<typename T> using TaskSchedulerType = Kokkos::DeprecatedTaskSchedulerMultiple<T>;
-static const char * scheduler_name = "DeprecatedTaskSchedulerMultiple";
-#endif
-#if defined(TACHO_USE_TASKSCHEDULER)
-template<typename T> using TaskSchedulerType = Kokkos::TaskScheduler<T>;
-static const char * scheduler_name = "TaskScheduler";
-#endif
-#if defined(TACHO_USE_TASKSCHEDULER_MULTIPLE)
-template<typename T> using TaskSchedulerType = Kokkos::TaskSchedulerMultiple<T>;
-static const char * scheduler_name = "TaskSchedulerMultiple";
-#endif
-#if defined(TACHO_USE_CHASELEV_TASKSCHEDULER)
-template<typename T> using TaskSchedulerType = Kokkos::ChaseLevTaskScheduler<T>;
-static const char * scheduler_name = "ChaseLevTaskScheduler";
-#endif
-
 
 #include "Tacho_TestCrsMatrixBase.hpp"
 #include "Tacho_TestGraph.hpp"
