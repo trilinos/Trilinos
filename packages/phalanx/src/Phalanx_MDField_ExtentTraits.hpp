d8f16cb33bfb6144a059e9ba65726dee57a0e512
Roger Pawlowski <rppawlo@sandia.gov>
2019-07-11 11:28:25 -0600

    Phalanx: add printing of static dimension ranks
    pre-removal of DimTags
    
    (cherry picked from commit 253ad3b689bb62c24a1bfcb377f718a31b2e4953)

--- packages/phalanx/src/Phalanx_MDField_ExtentTraits.hpp
+++ packages/phalanx/src/Phalanx_MDField_ExtentTraits.hpp
@@ -1,10 +1,21 @@
 #ifndef PHALANX_MDFIELD_EXTENT_TRAITS_HPP
 #define PHALANX_MDFIELD_EXTENT_TRAITS_HPP
 
+#include "Teuchos_TypeNameTraits.hpp"
 #include <type_traits>
+#include <string>
 
 namespace PHX {
-  template<typename I> struct is_extent : std::false_type {};
+
+  /// Identifies that a user defined struct is a dimension template parameter for MDFields. Users must specialize for true_types.
+  template<typename Extent> struct is_extent : std::false_type {};
+
+  /// Print statement for a user defined struct dimension template parameter for MDFields. Users can specialize this.
+  template<typename Extent> std::string print()
+  {return Teuchos::demangleName(typeid(Extent).name());}
+
 }
+
+
 
 #endif
