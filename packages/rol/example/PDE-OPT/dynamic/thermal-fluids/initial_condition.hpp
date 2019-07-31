633606a6e910299951aefdd30228ebe751d737da
Kyungjoo Kim <kyukim@sandia.gov>
2019-07-31 15:47:52 -0600

    ROL - change according to the deprecation in Tpetra.
      This commit mostly fixes the problem due to mismatch between
      Tpetra global ordinal type and "int" used in ROL examples.

--- packages/rol/example/PDE-OPT/dynamic/thermal-fluids/initial_condition.hpp
+++ packages/rol/example/PDE-OPT/dynamic/thermal-fluids/initial_condition.hpp
@@ -53,10 +53,12 @@
 template <class Real>
 class PotentialFlow {
 private:
+  typedef Tpetra::Map<>::global_ordinal_type GO; 
+
   const ROL::Ptr<FE<Real>> feVel_, fePrs_, feThr_;
   const ROL::Ptr<Intrepid::FieldContainer<Real>> cellNodes_;
   const ROL::Ptr<Intrepid::FieldContainer<int>> cellDofs_;
-  const Teuchos::Array<int> cellIds_;
+  const Teuchos::Array<GO> cellIds_;
   const ROL::Ptr<FieldHelper<Real>> fieldHelper_;
   Real cx_, cy_, r_;
 
@@ -66,7 +68,7 @@ public:
                 const ROL::Ptr<FE<Real>> &feThr,
                 const ROL::Ptr<Intrepid::FieldContainer<Real>> &cellNodes,
                 const ROL::Ptr<Intrepid::FieldContainer<int>> &cellDofs,
-                const Teuchos::Array<int> &cellIds,
+                const Teuchos::Array<GO> &cellIds,
                 const ROL::Ptr<FieldHelper<Real>> &fieldHelper,
                 ROL::ParameterList &parlist)
     : feVel_(feVel), fePrs_(fePrs), feThr_(feThr),
