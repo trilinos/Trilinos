395d2596272bf8c01eedce59ae44397b8f34d3ff
Roger Pawlowski <rppawlo@sandia.gov>
2019-08-16 13:03:10 -0600

    Phalanx: split out print from extent traits

--- packages/phalanx/src/Phalanx_ExtentTraits.hpp
+++ packages/phalanx/src/Phalanx_ExtentTraits.hpp
@@ -1,19 +1,12 @@
 #ifndef PHALANX_MDFIELD_EXTENT_TRAITS_HPP
 #define PHALANX_MDFIELD_EXTENT_TRAITS_HPP
 
-#include "Teuchos_TypeNameTraits.hpp"
+#include "Phalanx_Print.hpp"
 #include <type_traits>
-#include <string>
 
 namespace PHX {
-
   /// Identifies that a user defined struct is a dimension template parameter for MDFields. Users must specialize for true_types.
   template<typename Extent> struct is_extent : std::false_type {};
-
-  /// Print statement for a user defined struct dimension template parameter for MDFields. Users can specialize this.
-  template<typename Extent> std::string print()
-  {return Teuchos::demangleName(typeid(Extent).name());}
-
 }
 
 /** \brief  Macro for implementing the body of a simple ArrayDimTag
