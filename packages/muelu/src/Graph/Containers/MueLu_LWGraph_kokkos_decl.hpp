47f92d68a4224457ebfd91e3ca4adbf908e08a01
Luc Berger-Vergiat <lberge@sandia.gov>
2019-09-03 08:39:21 -0600

    MueLu: extending the interface of LWGraph to give direct access to rowPtrs and entries
    Accessing the underlying local data avoid having to use methods that transfer data to host which is not always the right thing to do.

--- packages/muelu/src/Graph/Containers/MueLu_LWGraph_kokkos_decl.hpp
+++ packages/muelu/src/Graph/Containers/MueLu_LWGraph_kokkos_decl.hpp
@@ -132,6 +132,16 @@ namespace MueLu {
       return graph_.row_map(GetNodeNumVertices());
     }
 
+    //! Return the row pointers of the local graph
+    KOKKOS_INLINE_FUNCTION typename local_graph_type::row_map_type getRowPtrs() const {
+      return graph_.row_map;
+    }
+
+    //! Return the list entries in the local graph
+    KOKKOS_INLINE_FUNCTION typename local_graph_type::entries_type getEntries() const {
+      return graph_.entries;
+    }
+
     //! Return the list of vertices adjacent to the vertex 'v'.
     // Unfortunately, C++11 does not support the following:
     //    auto getNeighborVertices(LO i) const -> decltype(rowView)
