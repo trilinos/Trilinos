aa9389fdaeaa348c0577a79620de2ca67979c0cd
Chris Siefert <csiefer@sandia.gov>
2019-09-09 15:26:01 -0600

    Tpetra: Adding Detials::inverseScaleBlockDiagonal()

--- packages/tpetra/core/src/Tpetra_Details_scaleBlockDiagonal.hpp
+++ packages/tpetra/core/src/Tpetra_Details_scaleBlockDiagonal.hpp
@@ -39,17 +39,23 @@
 // ************************************************************************
 // @HEADER
 
-#ifndef TPETRA_DETAILS_EXTRACTBLOCKDIAGONAL_HPP
-#define TPETRA_DETAILS_EXTRACTBLOCKDIAGONAL_HPP
+#ifndef TPETRA_DETAILS_SCALEBLOCKDIAGONAL_HPP
+#define TPETRA_DETAILS_SCALEBLOCKDIAGONAL_HPP
 
 #include "TpetraCore_config.h"
 #include "Tpetra_CrsMatrix.hpp"
-#include "Teuchos_RCP.hpp"
+#include "Teuchos_ScalarTraits.hpp"
 #include "Tpetra_Details_Behavior.hpp"
+#include "KokkosBatched_Util.hpp"
+#include "KokkosBatched_LU_Decl.hpp"
+#include "KokkosBatched_LU_Serial_Impl.hpp"
+#include "KokkosBatched_Trsm_Decl.hpp"
+#include "KokkosBatched_Trsm_Serial_Impl.hpp"
 
-/// \file Tpetra_Details_extractBlockDiagonal.hpp
-/// \brief Functions that allow for the extraction of a block diagonal
-/// from a Tpetra::CrsMatrix.
+
+/// \file Tpetra_Details_scaleBlockDiagonal.hpp
+/// \brief Functions that rescales a multivector (in-place) by a inverse of a block-diagonal matrix 
+/// (as implied by the given MultiVector
 /// \warning This file, and its contents, are implementation details
 ///   of Tpetra.  The file itself or its contents may disappear or
 ///   change at any time.
@@ -58,55 +64,48 @@ namespace Tpetra {
 namespace Details {
 
 
-template<class SparseMatrixType,
-         class MultiVectorType>
-void extractBlockDiagonal(const SparseMatrixType& A, MultiVectorType & diagonal) {
-  using local_map_type = typename SparseMatrixType::map_type::local_map_type;
+template<class MultiVectorType>        
+void inverseScaleBlockDiagonal(const MultiVectorType & blockDiagonal, MultiVectorType & multiVectorToBeScaled) {
   using SC             = typename MultiVectorType::scalar_type;
-  using LO             = typename SparseMatrixType::local_ordinal_type;
-  using KCRS           = typename SparseMatrixType::local_matrix_type;
-  using lno_view_t     = typename KCRS::StaticCrsGraphType::row_map_type::const_type;
-  using lno_nnz_view_t = typename KCRS::StaticCrsGraphType::entries_type::const_type;
-  using scalar_view_t  = typename KCRS::values_type::const_type;
+  using LO             = typename MultiVectorType::local_ordinal_type;
   using local_mv_type  = typename MultiVectorType::dual_view_type::t_dev;
-  using range_type     = Kokkos::RangePolicy<typename SparseMatrixType::node_type::execution_space, LO>;
+  using range_type     = Kokkos::RangePolicy<typename MultiVectorType::node_type::execution_space, LO>;
+  using namespace KokkosBatched;
+  SC SC_one = Teuchos::ScalarTraits<SC>::one();
 
   // Sanity checking: Map Compatibility (A's rowmap matches diagonal's map)
   if (Tpetra::Details::Behavior::debug() == true) {
-    TEUCHOS_TEST_FOR_EXCEPTION(!A.getRowMap()->isSameAs(*diagonal.getMap()),
-       std::runtime_error, "Tpetra::Details::extractBlockDiagonal was given incompatible maps");
+    TEUCHOS_TEST_FOR_EXCEPTION(!blockDiagonal.getMap()->isSameAs(*multiVectorToBeScaled.getMap()),
+       std::runtime_error, "Tpetra::Details::scaledBlockDiagonal was given incompatible maps");
   }
 
-  LO numrows   = diagonal.getLocalLength();
-  LO blocksize = diagonal.getNumVectors();
-  SC ZERO = Teuchos::ScalarTraits<typename MultiVectorType::scalar_type>::zero();
+  LO numrows   = blockDiagonal.getLocalLength();
+  LO blocksize = blockDiagonal.getNumVectors();
+  LO numblocks = numrows / blocksize;
 
   // Get Kokkos versions of objects
-  local_map_type rowmap  = A.getRowMap()->getLocalMap();
-  local_map_type colmap  = A.getRowMap()->getLocalMap();
-  local_mv_type diag     = diagonal.getLocalViewDevice();
-  const KCRS   Amat      = A.getLocalMatrix();
-  lno_view_t Arowptr     = Amat.graph.row_map;
-  lno_nnz_view_t Acolind = Amat.graph.entries;
-  scalar_view_t Avals    = Amat.values;
+  local_mv_type blockDiag = blockDiagonal.getLocalViewDevice();
+  local_mv_type toScale   = multiVectorToBeScaled.getLocalViewDevice();
+  
+  typedef Algo::Level3::Unblocked algo_type;
+  Kokkos::parallel_for("scaleBlockDiagonal",range_type(0,numblocks),KOKKOS_LAMBDA(const LO i){
+      std::pair<LO,LO> row_range = std::make_pair(i*blocksize,(i+1)*blocksize);
+      auto A = Kokkos::subview(blockDiag,row_range, Kokkos::ALL());
+      auto B = Kokkos::subview(toScale,  row_range, Kokkos::ALL());
 
-  Kokkos::parallel_for("Tpetra::extractBlockDiagonal",range_type(0,numrows),KOKKOS_LAMBDA(const LO i){
-      LO diag_col   = colmap.getLocalElement(rowmap.getGlobalElement(i));
-      LO blockStart = diag_col - (diag_col % blocksize);
-      LO blockStop  = blockStart + blocksize;
-      for(LO k=0; k<blocksize; k++)
-        diag(i,k)=ZERO;
+      // Factor
+      SerialLU<algo_type>::invoke(A);
 
-      for (size_t k = Arowptr(i); k < Arowptr(i+1); k++) {
-        LO col = Acolind(k);
-        if (blockStart <= col && col <= blockStop) {
-          diag(i,col-blockStart) = Avals(k);
-        }
-      }
-    });
+      // Solve L
+      SerialTrsm<Side::Left,Uplo::Lower,Trans::NoTranspose,Diag::Unit,algo_type>::invoke(SC_one,A,B);
+
+      // Solve U
+      SerialTrsm<Side::Left,Uplo::Upper,Trans::NoTranspose,Diag::NonUnit,algo_type>::invoke(SC_one,A,B);
+
+    });  
 }
 
 } // namespace Details
 } // namespace Tpetra
 
-#endif // TPETRA_DETAILS_EXTRACTBLOCKDIAGONAL_HPP
+#endif // TPETRA_DETAILS_SCALEBLOCKDIAGONAL_HPP
