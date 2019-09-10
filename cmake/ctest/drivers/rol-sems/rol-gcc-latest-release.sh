5956d346c7fb24abfea7ea007d3b2cdc20aa7f6d
Denis Ridzal <dridzal@sandia.gov>
2019-09-10 13:51:15 -0600

    Adding the testing infrastructure to the repo.

---
+++ cmake/ctest/drivers/rol-sems/rol-gcc-latest-release.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This is a poor-man's driver script
+
+DRIVER_SCRIPT_DIR=`echo $0 | sed "s/\(.*\)\/.*\.sh/\1/g"`
+echo "DRIVER_SCRIPT_DIR = '$DRIVER_SCRIPT_DIR'"
+
+TRILINOS_DIR=`readlink -f ${DRIVER_SCRIPT_DIR}/../../../..`
+echo "TRILINOS_DIR='${TRILINOS_DIR}'"
+
+source /etc/bashrc
+source $TRILINOS_DIR/cmake/load_sems_dev_env.sh "sems-gcc/6.1.0" "sems-openmpi/1.10.1"
+
+export CTEST_DASHBOARD_ROOT=$PWD
+
+ctest -V -S $DRIVER_SCRIPT_DIR/ctest-rol-mpi-release-gcc-latest.sems.cmake
