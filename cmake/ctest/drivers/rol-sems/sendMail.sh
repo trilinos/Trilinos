c437190c29b253e9b8a04f18134d37507704cc98
Denis Ridzal <dridzal@sandia.gov>
2019-09-10 15:40:59 -0600

    Modified the https link to the CDash results.

--- cmake/ctest/drivers/rol-sems/sendMail.sh
+++ cmake/ctest/drivers/rol-sems/sendMail.sh
@@ -5,7 +5,7 @@ RECIPIENTS=(
 )
 SUBJECT="Subject: ROL Test Summary $(date +%b-%d-%Y-at-%H:%M)\n\n"
 TEXT="To avoid complacency, you must follow this link:\n\n"
-LINK="https://testing-vm.sandia.gov/cdash/index.php?project=Trilinos&date=now&filtercount=1&field1=site&compare1=63&value1=optimizer"
+LINK="https://testing.sandia.gov/cdash/index.php?project=Trilinos&date=now&filtercount=1&field1=site&compare1=63&value1=optimizer"
 
 if [ "$1" -eq "1" ];then
   TEXT="There are test errors!!! Follow this link, pronto:\n\n"
