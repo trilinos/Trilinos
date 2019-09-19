fd78156078a8a1aa8f2479c7e25b2e2c54c61144
Chris Luchini <cbluchi@sandia.gov>
2019-09-18 18:32:50 -0600

    Fixes for trilinoscopuling build errors, and some TimeMonitor destructor usage

--- packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
+++ packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
@@ -771,11 +771,11 @@ public:
       " <= 0.  The block size must be positive.");
     {
       // These are rcp
-      const auto domainPointMap = getDomainMap();
+      const auto rcpDomainPointMap = getDomainMap();
       const auto colPointMap = Teuchos::rcp 
         (new typename BMV::map_type (BMV::makePointMap (*graph_.getColMap(), blockSize_)));
       *pointImporter_ = Teuchos::rcp 
-        (new typename crs_graph_type::import_type (domainPointMap, colPointMap));
+        (new typename crs_graph_type::import_type (rcpDomainPointMap, colPointMap));
     }
     {
       typedef typename crs_graph_type::local_graph_type::row_map_type row_map_type;
