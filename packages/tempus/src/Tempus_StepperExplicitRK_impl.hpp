c7233163e73a78ff89a3b849f1179fbb213cc216
Sidafa Conde <sconde@sandia.gov>
2019-09-25 11:42:56 -0600

    Tempus: fix seg fault rk stepper observer
    redundant declaration of rk stepper observer

--- packages/tempus/src/Tempus_StepperExplicitRK_impl.hpp
+++ packages/tempus/src/Tempus_StepperExplicitRK_impl.hpp
@@ -161,11 +161,12 @@ void StepperExplicitRK<Scalar>::setObserver(
      this->stepperObserver_  =
         Teuchos::rcp(new StepperRKObserverComposite<Scalar>());
 
-  if (( obs == Teuchos::null ) and (this->stepperObserver_->getSize() == 0) )
-     obs = Teuchos::rcp(new StepperRKObserver<Scalar>());
+  if (( obs == Teuchos::null ) and (this->stepperObserver_->getSize() == 0) ) {
+    obs = Teuchos::rcp(new StepperRKObserver<Scalar>());
 
-  this->stepperObserver_->addObserver(
-       Teuchos::rcp_dynamic_cast<StepperRKObserver<Scalar> > (obs, true) );
+    this->stepperObserver_->addObserver(
+        Teuchos::rcp_dynamic_cast<StepperRKObserver<Scalar> > (obs, true) );
+  }
 
 }
 
