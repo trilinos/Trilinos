b0b2789a174633babad4540ed2394cc26082b37a
Chris Siefert <csiefer@sandia.gov>
2019-10-29 11:45:06 -0600

    MueLu: makepr

--- packages/muelu/utils/misc/makepr.sh
+++ packages/muelu/utils/misc/makepr.sh
@@ -138,6 +138,8 @@ else
     ISSUE_BODY_TMPFILE=$(mktemp /tmp/issue_body.XXXXXX)
     # Open the issue
     echo "{\"title\": \"$TITLE_STRING\" , \"body\": \"$ISSUE_TEXT\"}"  > ${ISSUE_BODY_TMPFILE}
+    cat $ISSUE_BODY_TMPFILE
+    exit 1
     CMD=$(echo curl -i -H $h -d @${ISSUE_BODY_TMPFILE} https://api.github.com/repos/$fork/$repo/issues)
     eval $CMD >$TMPFILE 2> $TMPFILE
 
