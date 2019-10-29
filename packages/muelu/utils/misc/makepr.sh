da5500e34155b73cc2ee50f63d710d17c011cdfa
Chris Siefert <csiefer@sandia.gov>
2019-10-29 11:25:45 -0600

    MueLu: makepr

--- packages/muelu/utils/misc/makepr.sh
+++ packages/muelu/utils/misc/makepr.sh
@@ -116,7 +116,7 @@ SHA=`git rev-parse HEAD | cut -c1-7`
 REMOTE=$USER-$SHA
 
 # Push this branch to remote with a new name
-#git push origin $CBRANCH:$REMOTE
+git push origin $CBRANCH:$REMOTE
 
 if [[ -z $PR_FIRST_COMMENT ]]; then
   PR_FIRST_COMMENT="Auto-PR for SHA $SHA"
@@ -145,8 +145,6 @@ else
     $EDITOR_CMD $PR_TEXT_TMPFILE; 
     PR_BODY=`cat $PR_TEXT_TEMPFILE`
 fi
-
-exit 1
 
 echo "{\"title\": \"$TITLE_STRING\" , \"head\": \"$REMOTE\" ,\"base\": \"$mainBranch\", \"body\": \"$PR_BODY\"}" > ${PR_BODY_TMPFILE}
 token=$(cat $tokenfile)
