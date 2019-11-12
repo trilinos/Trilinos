b87eb77da0763a31d7f52e162151d0a668e974c6
Chris Siefert <csiefer@sandia.gov>
2019-11-12 16:19:28 -0700

    MueLu: Fixing merge issues

--- packages/muelu/test/maxwell/Maxwell3D.cpp
+++ packages/muelu/test/maxwell/Maxwell3D.cpp
@@ -566,7 +566,7 @@ int MainWrappers<double,LocalOrdinal,GlobalOrdinal,Node>::main_(Teuchos::Command
     bool        use_stacked_timer = false;              clp.setOption("stacked-timer", "no-stacked-timer", &use_stacked_timer, "use stacked timer");
 
 
-    std::string S_file, SM_file, M1_file, M0_file, M0inv_file, D0_file, coords_file, rhs_file="", nullspace_file="", material_file = "", Ms_file="", sol_file=""; Kn_file="";
+    std::string S_file, SM_file, M1_file, M0_file, M0inv_file, D0_file, coords_file, rhs_file="", nullspace_file="", material_file = "", Ms_file="", sol_file="", Kn_file="";
 
     S_file = "S.mat";
     SM_file = "";
