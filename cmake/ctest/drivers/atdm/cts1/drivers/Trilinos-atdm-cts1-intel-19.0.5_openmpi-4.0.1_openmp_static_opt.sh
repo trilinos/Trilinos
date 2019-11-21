db6378e85c64477d43bdf37d6e1085d0b97c0fcd
Forster, Jeffrey Edward <jeforst@sandia.gov>
2019-11-21 13:46:10 -0700

    updated cts1 driver script names to be usable with SPARC builds

---
+++ cmake/ctest/drivers/atdm/cts1/drivers/Trilinos-atdm-cts1-intel-19.0.5_openmpi-4.0.1_openmp_static_opt.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#export SALLOC_CTEST_TIME_LIMIT_MINUTES=1:00:00
+if [ "${Trilinos_TRACK}" == "" ] ; then
+  export Trilinos_TRACK=Experimental
+fi
+$WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/cts1/local-driver.sh
--- cmake/std/atdm/cts1/all_supported_builds.sh
+++ cmake/std/atdm/cts1/all_supported_builds.sh
@@ -2,6 +2,8 @@
 export ATDM_CONFIG_CTEST_S_BUILD_NAME_PREFIX=Trilinos-atdm-cts1-
 
 export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=(
-  intel-debug-openmp
-  intel-opt-openmp
+  cts1-intel-18.0.2_openmpi-2.0.3_openmpi_static_dbg
+  cts1-intel-18.0.2_openmpi-2.0.3_openmpi_static_opt
+  cts1-intel-19.0.5_openmpi-4.0.1_openmp_static_dbg
+  cts1-intel-19.0.5_openmpi-4.0.1_openmp_static_opt
   )
