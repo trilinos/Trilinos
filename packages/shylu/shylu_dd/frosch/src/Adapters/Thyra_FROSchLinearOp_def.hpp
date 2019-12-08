f7b0086c653fdbede1afa609ac7423982d7e9692
Brian Kelley <brian.honda11@gmail.com>
2019-12-08 12:37:43 -0700

    Zoltan2, ShyLU FROSch: fix -Wunused-variable

--- packages/shylu/shylu_dd/frosch/src/Adapters/Thyra_FROSchLinearOp_def.hpp
+++ packages/shylu/shylu_dd/frosch/src/Adapters/Thyra_FROSchLinearOp_def.hpp
@@ -131,8 +131,6 @@ namespace Thyra {
                                                 const SC alpha,
                                                 const SC beta) const
     {
-        const EOpTransp real_M_trans = real_trans(M_trans);
-
         FROSCH_ASSERT(getConstXpetraOperator()!=null,"XpetraLinearOp::applyImpl: internal Operator is null.");
         RCP< const Comm<int> > comm = getConstXpetraOperator()->getRangeMap()->getComm();
         //Transform to Xpetra MultiVector
@@ -147,6 +145,8 @@ namespace Thyra {
         }
         //Epetra NodeType
 #ifdef HAVE_SHYLU_DDFROSCH_EPETRA
+        const EOpTransp real_M_trans = real_trans(M_trans);
+
         if (this->bIsEpetra_) {
             const RCP<const VectorSpaceBase<double> > XY_domain = X_in.domain();
 
