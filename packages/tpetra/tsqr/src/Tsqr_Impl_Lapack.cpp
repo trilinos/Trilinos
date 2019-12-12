d4913a0d06f9a59d8353eadd71a4b1a040ded952
Mark Hoemmen <mhoemme@sandia.gov>
2019-12-12 16:11:26 -0700

    TSQR::Impl::Lapack: Use extern template instead of macros

--- packages/tpetra/tsqr/src/Tsqr_Impl_Lapack.cpp
+++ packages/tpetra/tsqr/src/Tsqr_Impl_Lapack.cpp
@@ -6,118 +6,124 @@
 namespace TSQR {
 namespace Impl {
 
-#define TSQR_IMPL_LAPACK_IMPL( Scalar ) \
-void Lapack<Scalar>:: \
-LARNV(const int idist, int seed[], const int n, \
-      value_type v[]) const \
-{ \
-  Teuchos::LAPACK<int, value_type> lapack; \
-  lapack.LARNV(idist, seed, n, v); \
-} \
-  \
-void Lapack<Scalar>:: \
-POTRF(const char UPLO, const int n, \
-      value_type A[], const int lda) const \
-{ \
-  Teuchos::LAPACK<int, value_type> lapack; \
-  int info = 0; \
-  lapack.POTRF(UPLO, n, A, lda, &info); \
-  if (info != 0) { \
-    std::ostringstream os; \
-    os << "LAPACK POTRF (Cholesky factorization) " \
-       << "failed with INFO = " << info << "."; \
-    throw std::logic_error (os.str ()); \
-  } \
-} \
-  \
-void Lapack<Scalar>:: \
-GESVD(const char JOBU, const char JOBVT, \
-      const int m, const int n, \
-      value_type A[], const int lda, \
-      magnitude_type S[], value_type U[], const int ldu, \
-      value_type V[], const int ldv, \
-      value_type WORK[], const int lwork, \
-      magnitude_type RWORK[]) const \
-{ \
-  Teuchos::LAPACK<int, value_type> lapack; \
-  int info = 0; \
-  lapack.GESVD(JOBU, JOBVT, m, n, A, lda, S, \
-               U, ldu, V, ldv, WORK, lwork, RWORK, &info); \
-  if (info != 0) { \
-    std::ostringstream os; \
-    os << "LAPACK GESVD (singular value decomposition) " \
-       << "failed with INFO = " << info << "."; \
-    throw std::logic_error (os.str ()); \
-  } \
-} \
-  \
-void Lapack<Scalar>:: \
-LARFG(const int n, value_type& alpha, value_type x[], \
-      const int incx, value_type& tau) const \
-{ \
-  Teuchos::LAPACK<int, value_type> lapack; \
-  lapack.LARFG(n, &alpha, x, incx, &tau); \
-} \
-  \
-void Lapack<Scalar>:: \
-compute_QR(const int m, const int n, value_type A[], const int lda, \
-           value_type TAU[], value_type WORK[], const int lwork) const \
-{ \
-  Teuchos::LAPACK<int, value_type> lapack; \
-  int info = 0; \
-  lapack.GEQRF(m, n, A, lda, TAU, WORK, lwork, &info); \
-  if (info != 0) { \
-    std::ostringstream os; \
-    os << "LAPACK GEQRF (QR factorization) failed with INFO = " \
-       << info << "."; \
-    throw std::logic_error (os.str()); \
-  } \
-} \
-  \
-void Lapack<Scalar>:: \
-apply_Q_factor(const char SIDE, const char TRANS, \
-               const int m, const int n, const int k, \
-               const value_type A[], const int lda, \
-               const value_type TAU[], \
-               value_type C[], const int ldc, \
-               value_type WORK[], const int lwork) const \
-{ \
-  Teuchos::LAPACK<int, value_type> lapack; \
-  int info = 0; \
-  value_type* A_nc = const_cast<value_type*>(A); \
-  lapack.UNMQR(SIDE, TRANS, m, n, k, A_nc, lda, TAU, C, ldc, WORK, \
-               lwork, &info); \
-  if (info != 0) { \
-    std::ostringstream os; \
-    os << "LAPACK UNMQR (apply Q factor from GEQRF) failed with " \
-       "INFO = " << info << "."; \
-    throw std::logic_error (os.str()); \
-  } \
-} \
-  \
-void Lapack<Scalar>:: \
-compute_explicit_Q(const int m, const int n, const int k, \
-                   value_type A[], const int lda, \
-                   const value_type TAU[], value_type WORK[], \
-                   const int lwork) const \
-{ \
-  Teuchos::LAPACK<int, value_type> lapack; \
-  int info = 0; \
-  lapack.UNGQR(m, n, k, A, lda, TAU, WORK, lwork, &info); \
-  if (info != 0) { \
-    std::ostringstream os; \
-    os << "LAPACK UNGQR (compute explicit Q factor from GEQRF) " \
-      "failed with INFO = " << info << "."; \
-    throw std::logic_error (os.str()); \
-  } \
+template<class Scalar>
+void Lapack<Scalar>::
+LARNV(const int idist, int seed[], const int n,
+      value_type v[]) const
+{
+  Teuchos::LAPACK<int, value_type> lapack;
+  lapack.LARNV(idist, seed, n, v);
 }
 
-TSQR_IMPL_LAPACK_IMPL( float )
-TSQR_IMPL_LAPACK_IMPL( double )
+template<class Scalar>
+void Lapack<Scalar>::
+POTRF(const char UPLO, const int n,
+      value_type A[], const int lda) const
+{
+  Teuchos::LAPACK<int, value_type> lapack;
+  int info = 0;
+  lapack.POTRF(UPLO, n, A, lda, &info);
+  if (info != 0) {
+    std::ostringstream os;
+    os << "LAPACK POTRF (Cholesky factorization) "
+       << "failed with INFO = " << info << ".";
+    throw std::logic_error (os.str ());
+  }
+}
+
+template<class Scalar>
+void Lapack<Scalar>::
+GESVD(const char JOBU, const char JOBVT,
+      const int m, const int n,
+      value_type A[], const int lda,
+      magnitude_type S[], value_type U[], const int ldu,
+      value_type V[], const int ldv,
+      value_type WORK[], const int lwork,
+      magnitude_type RWORK[]) const
+{
+  Teuchos::LAPACK<int, value_type> lapack;
+  int info = 0;
+  lapack.GESVD(JOBU, JOBVT, m, n, A, lda, S,
+               U, ldu, V, ldv, WORK, lwork, RWORK, &info);
+  if (info != 0) {
+    std::ostringstream os;
+    os << "LAPACK GESVD (singular value decomposition) "
+       << "failed with INFO = " << info << ".";
+    throw std::logic_error (os.str ());
+  }
+}
+
+template<class Scalar>
+void Lapack<Scalar>::
+LARFG(const int n, value_type& alpha, value_type x[],
+      const int incx, value_type& tau) const
+{
+  Teuchos::LAPACK<int, value_type> lapack;
+  lapack.LARFG(n, &alpha, x, incx, &tau);
+}
+
+template<class Scalar>
+void Lapack<Scalar>::
+compute_QR(const int m, const int n, value_type A[], const int lda,
+           value_type TAU[], value_type WORK[], const int lwork) const
+{
+  Teuchos::LAPACK<int, value_type> lapack;
+  int info = 0;
+  lapack.GEQRF(m, n, A, lda, TAU, WORK, lwork, &info);
+  if (info != 0) {
+    std::ostringstream os;
+    os << "LAPACK GEQRF (QR factorization) failed with INFO = "
+       << info << ".";
+    throw std::logic_error (os.str());
+  }
+}
+
+template<class Scalar>
+void Lapack<Scalar>::
+apply_Q_factor(const char SIDE, const char TRANS,
+               const int m, const int n, const int k,
+               const value_type A[], const int lda,
+               const value_type TAU[],
+               value_type C[], const int ldc,
+               value_type WORK[], const int lwork) const
+{
+  Teuchos::LAPACK<int, value_type> lapack;
+  int info = 0;
+  value_type* A_nc = const_cast<value_type*>(A);
+  lapack.UNMQR(SIDE, TRANS, m, n, k, A_nc, lda, TAU, C, ldc, WORK,
+               lwork, &info);
+  if (info != 0) {
+    std::ostringstream os;
+    os << "LAPACK UNMQR (apply Q factor from GEQRF) failed with "
+       "INFO = " << info << ".";
+    throw std::logic_error (os.str());
+  }
+}
+
+template<class Scalar>
+void Lapack<Scalar>::
+compute_explicit_Q(const int m, const int n, const int k,
+                   value_type A[], const int lda,
+                   const value_type TAU[], value_type WORK[],
+                   const int lwork) const
+{
+  Teuchos::LAPACK<int, value_type> lapack;
+  int info = 0;
+  lapack.UNGQR(m, n, k, A, lda, TAU, WORK, lwork, &info);
+  if (info != 0) {
+    std::ostringstream os;
+    os << "LAPACK UNGQR (compute explicit Q factor from GEQRF) "
+      "failed with INFO = " << info << ".";
+    throw std::logic_error (os.str());
+  }
+}
+
+template class Lapack<float>;
+template class Lapack<double>;
 
 #ifdef HAVE_TPETRATSQR_COMPLEX
-TSQR_IMPL_LAPACK_IMPL( std::complex<float> )
-TSQR_IMPL_LAPACK_IMPL( std::complex<double> )
+template class Lapack<std::complex<float>>;
+template class Lapack<std::complex<double>>;
 #endif // HAVE_TPETRATSQR_COMPLEX
 
 } // namespace Impl
--- packages/tpetra/tsqr/src/Tsqr_Impl_Lapack.hpp
+++ packages/tpetra/tsqr/src/Tsqr_Impl_Lapack.hpp
@@ -8,9 +8,6 @@
 namespace TSQR {
 namespace Impl {
 
-template<class Scalar>
-class Lapack {};
-
 // CombineNative needs LARFG, but it's not properly part of RawQR.
 // RawQR needs to be able to wrap lots of different functions,
 // including whatever cuSOLVER provides.  It doesn't make sense to
@@ -18,62 +15,61 @@ class Lapack {};
 // especially not when cuSOLVER already has all the needed QR
 // factorization and apply Q factor functions.
 
-#define TSQR_IMPL_LAPACK_DECL( Scalar ) \
-template<> \
-class Lapack<Scalar> : public RawQR<Scalar> { \
-public: \
-  using value_type = Scalar; \
-  using magnitude_type = decltype(std::abs(Scalar{})); \
-  \
-  ~Lapack() = default; \
-  \
-  void \
-  compute_QR(const int m, const int n, value_type A[], \
-             const int lda, value_type TAU[], value_type WORK[], \
-             const int lwork) const override; \
-  \
-  void \
-  apply_Q_factor(const char SIDE, const char TRANS, \
-                 const int m, const int n, const int k, \
-                 const value_type A[], const int lda, \
-                 const value_type TAU[], \
-                 value_type C[], const int ldc, \
-                 value_type WORK[], const int lwork) const override; \
-  \
-  void \
-  compute_explicit_Q(const int m, const int n, const int k, \
-                     value_type A[], const int lda, \
-                     const value_type TAU[], value_type WORK[], \
-                     const int lwork) const override; \
-  \
-  void \
-  GESVD(const char JOBU, const char JOBVT, \
-        const int m, const int n, \
-        value_type A[], const int lda, \
-        magnitude_type S[], value_type U[], const int ldu, \
-        value_type V[], const int ldv, \
-        value_type WORK[], const int lwork, \
-        magnitude_type RWORK[]) const; \
-  \
-  void \
-  LARFG(const int n, value_type& alpha, value_type x[], \
-        const int incx, value_type& tau) const; \
-  \
-  void \
-  POTRF(const char UPLO, const int n, \
-        value_type A[], const int lda) const; \
-  \
-  void \
-  LARNV(const int idist, int seed[], const int n, \
-        value_type v[]) const; \
+template<class Scalar>
+class Lapack : public RawQR<Scalar> {
+public:
+  using value_type = Scalar;
+  using magnitude_type = decltype(std::abs(Scalar{}));
+
+  ~Lapack() = default;
+
+  void
+  compute_QR(const int m, const int n, value_type A[],
+             const int lda, value_type TAU[], value_type WORK[],
+             const int lwork) const override;
+
+  void
+  apply_Q_factor(const char SIDE, const char TRANS,
+                 const int m, const int n, const int k,
+                 const value_type A[], const int lda,
+                 const value_type TAU[],
+                 value_type C[], const int ldc,
+                 value_type WORK[], const int lwork) const override;
+
+  void
+  compute_explicit_Q(const int m, const int n, const int k,
+                     value_type A[], const int lda,
+                     const value_type TAU[], value_type WORK[],
+                     const int lwork) const override;
+
+  void
+  GESVD(const char JOBU, const char JOBVT,
+        const int m, const int n,
+        value_type A[], const int lda,
+        magnitude_type S[], value_type U[], const int ldu,
+        value_type V[], const int ldv,
+        value_type WORK[], const int lwork,
+        magnitude_type RWORK[]) const;
+
+  void
+  LARFG(const int n, value_type& alpha, value_type x[],
+        const int incx, value_type& tau) const;
+
+  void
+  POTRF(const char UPLO, const int n,
+        value_type A[], const int lda) const;
+
+  void
+  LARNV(const int idist, int seed[], const int n,
+        value_type v[]) const;
 };
 
-TSQR_IMPL_LAPACK_DECL( float )
-TSQR_IMPL_LAPACK_DECL( double )
+extern template class Lapack<float>;
+extern template class Lapack<double>;
 
 #ifdef HAVE_TPETRATSQR_COMPLEX
-TSQR_IMPL_LAPACK_DECL( std::complex<float> )
-TSQR_IMPL_LAPACK_DECL( std::complex<double> )
+extern template class Lapack<std::complex<float>>;
+extern template class Lapack<std::complex<double>>;
 #endif // HAVE_TPETRATSQR_COMPLEX
 
 } // namespace Impl
