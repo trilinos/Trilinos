9e450bc6fdfda520a265842bb83acca840b095a5
Roscoe A. Bartlett <rabartl@sandia.gov>
2019-12-19 09:06:11 -0700

    Factoring atdm_run_script_on_comput_node into reusable file and other improvements (ATDV-212, #6467)
    This gets us closer to a single atdm_run_script_on_comput_node function that
    can be used on all platforms that use SLURM.
    
    Some other things:
    
    * Defined new ATDM_CONFIG vars ATDM_CONFIG_SPARC_TPL_BASE,
      ATDM_CONFIG_SBATCH_DEFUALT_TIMEOUT and ATDM_CONFIG_SBATCH_DEFUALT_ACCOUNT to
      allow these new files to reused.
    
    * Removed bad logic for "DEFAULT" compiler since a custom_builds.sh script is
      now being used.
    
    * Added the system-name to the echo command for the build being configured (so
      it says 'cts1' instead of just 'toss3').

--- cmake/std/atdm/common/toss3/environment_new.sh
+++ cmake/std/atdm/common/toss3/environment_new.sh
@@ -6,11 +6,7 @@
 #
 ################################################################################
 
-if [ "$ATDM_CONFIG_COMPILER" == "DEFAULT" ] ; then
-  export ATDM_CONFIG_COMPILER=INTEL
-fi
-
-echo "Using toss3 compiler stack $ATDM_CONFIG_COMPILER to build $ATDM_CONFIG_BUILD_TYPE code with Kokkos node type $ATDM_CONFIG_NODE_TYPE"
+echo "Using $ATDM_CONFIG_SYSTEM_NAME toss3 compiler stack $ATDM_CONFIG_COMPILER to build $ATDM_CONFIG_BUILD_TYPE code with Kokkos node type $ATDM_CONFIG_NODE_TYPE"
 
 export ATDM_CONFIG_ENABLE_SPARC_SETTINGS=ON
 export ATDM_CONFIG_USE_NINJA=ON
@@ -37,7 +33,7 @@ else
 fi
 
 # Common paths and modules for both intel-1{8,9}
-sparc_tpl_base=/projects/sparc/tpls/cts1-bdw
+sparc_tpl_base=${ATDM_CONFIG_SPARC_TPL_BASE}
 module load mkl/18.0.5.274
 module load cmake/3.12.2
 
@@ -116,104 +112,8 @@ export CXX=mpicxx
 export FC=mpif77
 export F90=mpif90
 
+# Define function atdm_run_script_on_compute_node
+source $ATDM_SCRIPT_DIR/common/define_run_on_slurm_compute_node_func.sh
+
 export ATDM_CONFIG_COMPLETED_ENV_SETUP=TRUE
 
-
-#
-# Run a script on the compute node send STDOUT and STDERR output to a file
-# while also echo output to the console.  The primary purpose is to run the
-# tests on the compute node.
-#
-# Usage:
-#
-#   atdm_run_script_on_comput_node <script_to_run> <output_file> \
-#     [<timeout>] [<account>]
-#
-# If <timeout> and/or <account> are not given, then defaults are provided that
-# work for the Jenkins driver process.
-#
-# In this case, sbatch is used to run the script but it also sends ouptut to
-# STDOUT in real-time while it is running in addition to writing to the
-# <outout_file>.  The job name for the sbatch script is taken from the env var
-# 'ATDM_CONFIG_BUILD_NAME'.  This works for local builds since ATDM_CONFIG_BUILD_NAME.
-#
-# Note that you can pass in the script to run with arguments such as with
-# "<some-script> <arg1> <arg2>" and it will work.  But note that this has to
-# be bash script that 'sbatch' can copy and run form a temp location and it
-# still has to work.  So the script has to use absolute directory paths, not
-# relative paths or asume sym links, etc.
-#
-function atdm_run_script_on_compute_node {
-
-  set +x
-
-  script_to_run=$1
-  output_file=$2
-  timeout_input=$3
-  account_input=$4
-
-  echo
-  echo "***"
-  echo "*** atdm_run_script_on_compute_node '${script_to_run}' '${output_file}' '${timeout_input}' '${account_input}'"
-  echo "***"
-  echo
-
-  if [ "${timeout_input}" == "" ] ; then
-    timeout=1:30:00
-  else
-    timeout=${timeout_input}
-  fi
-
-  if [ "${account_input}" == "" ] ; then
-    account=fy150090
-  else
-    account=${account_input}
-  fi
-
-  if [ -e $output_file ] ; then
-    echo "Remove existing file $output_file"
-    rm $output_file
-  fi
-  echo "Create empty file $output_file"
-  touch $output_file
-
-  echo
-  echo "Running '$script_to_run' using sbatch in the background ..."
-  set -x
-  sbatch --output=$output_file --wait -N1 --time=${timeout} \
-    -J $ATDM_CONFIG_BUILD_NAME --account=${account} ${script_to_run} &
-  SBATCH_PID=$!
-  set +x
-
-  echo
-  echo "Tailing output file $output_file in the background ..."
-  set -x
-  tail -f $output_file &
-  TAIL_BID=$!
-  set +x
-
-  echo
-  echo "Waiting for SBATCH_PID=$SBATCH_PID ..."
-  wait $SBATCH_PID
-
-  echo
-  echo "Kill TAIL_BID=$TAIL_BID"
-  kill -s 9 $TAIL_BID
-
-  echo
-  echo "Finished running ${script_to_run}!"
-  echo
-
-}
-
-export -f atdm_run_script_on_compute_node
-
-# NOTE: The above function is implemented in this way using 'sbatch' so that
-# we can avoid using 'salloc' which is belived to cause ORTE errors.  But we
-# still want to see live ouput from the script so that we can report it on
-# Jenkins.  Therefore, the above approach is to use 'sbatch' and write its
-# output to a known file-name.  Then, we use `tail -f` to print that file as
-# it gets filled in from the 'sbatch' command.  The 'sbatch' command is run
-# with --wait but is backgrouned to allow this to happen.  Then we wait for
-# the 'sbatch' command to complete and then we kill the 'tail -f' command.
-# That might seem overly complex but that gets the job done.
