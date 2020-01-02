1986eefc18075b40964bc35cd99ef8e818362d17
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-01-02 09:55:57 -0700

    Change name of SALLOC_CTEST_TIME_LIMIT_MINUTES to SLURM_CTEST_TIMEOUT and set to 1:20:00 (ATDV-212)
    The prior name was technically wrong (since we are using sbatch not salloc)
    and the timeout is in hh:mm:ss, not minutes!
    
    Also, I changed the default timeout from 1:30:00 to 1:20:00.  I did this for
    two reasons. First, all of the builds ran had the ctest suite finish in less
    than 50 min (even the 'dbg' builds).  Also, I want this to be different than
    the built-in timeout of 1:30:00 so I don't get confused again.

--- cmake/ctest/drivers/atdm/cts1/drivers/Trilinos-atdm-cts1-intel-18.0.2_openmpi-2.0.3_openmp_static_dbg.sh
+++ cmake/ctest/drivers/atdm/cts1/drivers/Trilinos-atdm-cts1-intel-18.0.2_openmpi-2.0.3_openmp_static_dbg.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#export SALLOC_CTEST_TIME_LIMIT_MINUTES=1:00:00
+#export SLURM_CTEST_TIMEOUT=1:00:00
 if [ "${Trilinos_TRACK}" == "" ] ; then
   export Trilinos_TRACK=Specialized
 fi
--- cmake/ctest/drivers/atdm/cts1/local-driver.sh
+++ cmake/ctest/drivers/atdm/cts1/local-driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 
-if [ "${SALLOC_CTEST_TIME_LIMIT_MINUTES}" == "" ] ; then
-  SALLOC_CTEST_TIME_LIMIT_MINUTES=1:30:00
+if [ "${SLURM_CTEST_TIMEOUT}" == "" ] ; then
+  SLURM_CTEST_TIMEOUT=1:20:00
   # This is just running tests, not the entire build!
 fi
 
@@ -18,4 +18,4 @@ set -x
 atdm_run_script_on_compute_node \
   $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ctest-s-driver-test.sh \
   $PWD/ctest-s-driver-test.out \
-  ${SALLOC_CTEST_TIME_LIMIT_MINUTES}
+  ${SLURM_CTEST_TIMEOUT}
