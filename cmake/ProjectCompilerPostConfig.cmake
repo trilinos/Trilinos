7f81b4e7eff824ac907320e1d5c74bb76ea54023
jjwilke <jjwilke@sandia.gov>
2020-01-13 10:41:43 -0700

    restore old behavior of Trilinos_CXX_COMPILER_FLAGS (mostly)

--- cmake/ProjectCompilerPostConfig.cmake
+++ cmake/ProjectCompilerPostConfig.cmake
@@ -1,9 +1,13 @@
 IF (${Trilinos_ENABLE_Kokkos})
-
   MESSAGE("-- " "Skip adding flags for C++11 because Kokkos flags does that ...")
   SET(${PROJECT_NAME}_CXX11_FLAGS " ")
   
   MESSAGE("-- " "Skip adding flags for OpenMP because Kokkos flags does that ...")
   SET(OpenMP_CXX_FLAGS_OVERRIDE " ")
 
+  # There is a top-level CMAKE_CXX_FLAGS. Warnings and other flags get added
+  # in the sub-scope of each individual package
+  # Grab those variables here so we know what was the original top-level flags
+  # and what are the CMAKE_CXX_FLAGS added afterwards for an individual package
+  SET(TRILINOS_TOPLEVEL_CXX_FLAGS ${CMAKE_CXX_FLAGS})
 ENDIF()
