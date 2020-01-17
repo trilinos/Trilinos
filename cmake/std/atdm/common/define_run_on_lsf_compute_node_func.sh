03d26355e0af5abb11199dd9f420e444eea2f109
Evan Harvey <eharvey@sandia.gov>
2020-01-17 08:58:18 -0700

    ATDM: Cleaned up lsf_compute_node_func (CDOFA-89)

--- cmake/std/atdm/common/define_run_on_lsf_compute_node_func.sh
+++ cmake/std/atdm/common/define_run_on_lsf_compute_node_func.sh
@@ -1,4 +1,3 @@
-#TODO: s/SBATCH/LSF
 function atdm_run_script_on_compute_node {
 
   set +x
@@ -6,28 +5,17 @@ function atdm_run_script_on_compute_node {
   script_to_run=$1
   output_file=$2
   timeout_input=$3
-  account_input=$4
 
   echo
   echo "***"
-  echo "*** atdm_run_script_on_compute_node '${script_to_run}' '${output_file}' '${timeout_input}' '${account_input}'"
+  echo "*** atdm_run_script_on_compute_node '${script_to_run}' '${output_file}' '${timeout_input}''"
   echo "***"
   echo
 
-#  if [[ "${timeout_input}" != "" ]] ; then
-#    timeout=${timeout_input}
-#  elif [[ "${ATDM_CONFIG_SBATCH_DEFAULT_TIMEOUT}" != "" ]] ; then
-#    timeout=${ATDM_CONFIG_SBATCH_DEFAULT_TIMEOUT}
-#  else
-    timeout=04:00
-#  fi
-
-  if [ "${account_input}" != "" ] ; then
-    account=${account_input}
-  elif [[ "${ATDM_CONFIG_SBATCH_DEFAULT_ACCOUNT}" != "" ]] ; then
-    timeout=${ATDM_CONFIG_SBATCH_DEFAULT_ACCOUNT}
+  if [[ "${timeout_input}" != "" ]] ; then
+    timeout=${timeout_input}
   else
-    account=fy150090
+    timeout=04:00
   fi
 
   if [ -e $output_file ] ; then
@@ -38,11 +26,10 @@ function atdm_run_script_on_compute_node {
   touch $output_file
 
   echo
-  echo "Running '$script_to_run' using sbatch in the background ..."
+  echo "Running '$script_to_run' using BSUB in the background ..."
   set -x
-  #echo "bsub -J ${JOBNAME}-Test1 -W 06:00 -Is -n 16 -q ${QUEUE} ./test_submitted_command &> test.output" &> test_command
   bsub -J $ATDM_CONFIG_BUILD_NAME -W ${timeout} -Is ${script_to_run} &> $output_file &
-  SBATCH_PID=$!
+  BSUB_PID=$!
   set +x
 
   echo
@@ -53,8 +40,8 @@ function atdm_run_script_on_compute_node {
   set +x
 
   echo
-  echo "Waiting for SBATCH_PID=$SBATCH_PID ..."
-  wait $SBATCH_PID
+  echo "Waiting for BSUB_PID=$BSUB_PID ..."
+  wait $BSUB_PID
 
   echo
   echo "Kill TAIL_BID=$TAIL_BID"
@@ -68,13 +55,12 @@ function atdm_run_script_on_compute_node {
 
 export -f atdm_run_script_on_compute_node
 
-# NOTE: The above function is implemented in this way using 'sbatch' so that
-# we can avoid using 'salloc' which is belived to cause ORTE errors.  But we
-# still want to see live ouput from the script so that we can report it on
+# NOTE: The above function is implemented in this way using 'BSUB' to improve
+# test run times (submitting a job for each test is slow). We  still want to
+# see live ouput from the script so that we can report it on
 # Jenkins and see it running in real-time on Jenins.  Therefore, the above
-# approach is to use 'sbatch' and write its output to a known file-name.
+# approach is to use 'BSUB' and write its output to a known file-name.
 # Then, we use `tail -f` to print that file as it gets filled in from the
-# 'sbatch' command.  The 'sbatch' command is run with --wait but is
-# backgrouned to allow this to happen.  Then we wait for the 'sbatch' command
-# to complete and then we kill the 'tail -f' command.  That might seem overly
-# complex but that gets the job done.
+# 'BSUB' command.  We wait for the 'BSUB' command  to complete and then we kill
+# the 'tail -f' command.  That might seem overly  complex but that gets the job
+# done.
