40df9119e270a58d7a20191444eca8af2b3eadc7
Nathan Ellingwood <ndellin@sandia.gov>
2020-01-23 20:54:52 -0700

    cmake/ProjectCompilerPostConfig.cmake
    Modification provided by @jjwilke for the Kokkos Ecosystem version 3.0
    Enables preservin existing cmake behavior following updates to the
    kokkos build system.

--- cmake/ProjectCompilerPostConfig.cmake
+++ cmake/ProjectCompilerPostConfig.cmake
@@ -6,4 +6,9 @@ IF (${Trilinos_ENABLE_Kokkos})
   MESSAGE("-- " "Skip adding flags for OpenMP because Kokkos flags does that ...")
   SET(OpenMP_CXX_FLAGS_OVERRIDE " ")
 
+  # There is a top-level CMAKE_CXX_FLAGS. Warnings and other flags get added
+  # in the sub-scope of each individual package
+  # Grab those variables here so we know what was the original top-level flags
+  # and what are the CMAKE_CXX_FLAGS added afterwards for an individual package
+  SET(TRILINOS_TOPLEVEL_CXX_FLAGS ${CMAKE_CXX_FLAGS})
 ENDIF()
