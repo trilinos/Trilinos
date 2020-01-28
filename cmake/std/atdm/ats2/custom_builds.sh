fa31b88d506d1c2a0d75fd7f7160b6a07010f7e9
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-01-28 09:10:37 -0700

    Make default 'gnu' and 'cuda' builds (CDOFA-72)
    The default 'cuda' build is the cuda-10 gnu-7.3.1 build since we don't support
    the xl build yet.

--- cmake/std/atdm/ats2/custom_builds.sh
+++ cmake/std/atdm/ats2/custom_builds.sh
@@ -7,25 +7,7 @@
 
 # Custom compiler selection logic
 
-if [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-gnu-7.3.1-spmpi-2019.06.24"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_gnu-7.3.1_spmpi-2019.06.24"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-gnu-7.3.1"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_gnu-7.3.1"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-gnu-7"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_gnu-7"* ]] \
-  ; then
-  export ATDM_CONFIG_COMPILER=CUDA-10.1.243_GNU-7.3.1_SPMPI-2019.06.24
-
-elif [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7.3.1-spmpi-2019.06.24"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7.3.1_spmpi-2019.06.24"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7.3.1"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"default" ]] \
-  ; then
-  export ATDM_CONFIG_COMPILER=GNU-7.3.1_SPMPI-2019.06.24
-
-elif [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-xl-2019.08.20-spmpi-2019.06.24"* ]] \
+if   [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-xl-2019.08.20-spmpi-2019.06.24"* ]] \
   || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_xl-2019.08.20_spmpi-2019.06.24"* ]] \
   || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-xl-2019.08.20"* ]] \
   || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_xl-2019.08.20"* ]] \
@@ -42,6 +24,29 @@ elif [[ $ATDM_CONFIG_BUILD_NAME == *"xl-2019.08.20-spmpi-2019.06.24"* ]] \
   ; then
   export ATDM_CONFIG_COMPILER=XL-2019.08.20_SPMPI-2019.06.24
 
+elif [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-gnu-7.3.1-spmpi-2019.06.24"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_gnu-7.3.1_spmpi-2019.06.24"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-gnu-7.3.1"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_gnu-7.3.1"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243-gnu-7"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243_gnu-7"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10.1.243"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda-10"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"cuda"* ]] \
+  ; then
+  export ATDM_CONFIG_COMPILER=CUDA-10.1.243_GNU-7.3.1_SPMPI-2019.06.24
+  # NOTE: Default 'cuda' must be last cuda listed!
+
+elif [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7.3.1-spmpi-2019.06.24"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7.3.1_spmpi-2019.06.24"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7.3.1"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu-7"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"gnu"* ]] \
+  || [[ $ATDM_CONFIG_BUILD_NAME == *"default" ]] \
+  ; then
+  export ATDM_CONFIG_COMPILER=GNU-7.3.1_SPMPI-2019.06.24
+  # NOTE: Defaut 'gnu' must be last 'gnu' listed!
+
 else
   echo
   echo "***"
@@ -49,10 +54,10 @@ else
   echo "***"
   echo "*** Supported compilers include:"
   echo "***"
-  echo "****  gnu-7.3.1_spmpi-2019.06.24   (default)"
-  echo "****  cuda-10.1.243_gnu-7.3.1_spmpi-2019.06.24"
-  echo "****  xl-2019.08.20_spmpi-2019.06.24 (disabled)"
-  echo "****  cuda-10.1.243-gnu-7.3.1-spmpi-2019.06.24 (disabled)"
+  echo "****  gnu-7.3.1_spmpi-2019.06.24                  (default, default gnu)"
+  echo "****  cuda-10.1.243_gnu-7.3.1_spmpi-2019.06.24    (default cuda)"
+  echo "****  xl-2019.08.20_spmpi-2019.06.24              (disabled)"
+  echo "****  cuda-10.1.243-gnu-7.3.1-spmpi-2019.06.24    (disabled)"
   echo "***"
   return
 
