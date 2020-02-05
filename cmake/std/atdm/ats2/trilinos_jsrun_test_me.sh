d4c10ee58962d43ea25c632aaad3d0c369d9412e
James Elliott <jjellio@sandia.gov>
2020-02-04 17:42:41 -0700

    Added safety check for the Tpetra ENV not being set
    1.  wrapper now defaults to non-cuda-aware MPI if the ENV is not set
    2.  fixed argument forwarding so they are get quoted

--- cmake/std/atdm/ats2/trilinos_jsrun_test_me.sh
+++ cmake/std/atdm/ats2/trilinos_jsrun_test_me.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "testing with cuda-aware unset!, should not see -gpu, look for -disable_gpu_hooks"
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI="" ./trilinos_jsrun -M -disable_gdr -p 2 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI="" ./trilinos_jsrun -M -disable_gdr -p 1 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI="" ./trilinos_jsrun -M -disable_gdr -p 1 hostname 1>/dev/null
+
 echo "testing with cuda-aware turned off!, should not see -gpu, look for -disable_gpu_hooks"
 ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=0 ./trilinos_jsrun -M -disable_gdr -p 2 hostname 1>/dev/null
 ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=0 ./trilinos_jsrun -M -disable_gdr -p 1 hostname 1>/dev/null
@@ -9,3 +14,18 @@ echo "testing with cuda-aware turned ON!, should see -gpu if NP>1 and  -disable_
 ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=1 ./trilinos_jsrun -M -disable_gdr -p 2 hostname 1>/dev/null
 ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=1 ./trilinos_jsrun -M -disable_gdr -p 1 hostname 1>/dev/null
 ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=1 ./trilinos_jsrun -M -disable_gdr -p 1 hostname 1>/dev/null
+
+echo "testing with cuda-aware unset!, should not see -gpu, look for -disable_gpu_hooks"
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI="" ./trilinos_jsrun  -p 2 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI="" ./trilinos_jsrun  -p 1 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI="" ./trilinos_jsrun  -p 1 hostname 1>/dev/null
+
+echo "testing with cuda-aware turned off!, should not see -gpu, look for -disable_gpu_hooks"
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=0 ./trilinos_jsrun  -p 2 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=0 ./trilinos_jsrun  -p 1 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=0 ./trilinos_jsrun  -p 1 hostname 1>/dev/null
+
+echo "testing with cuda-aware turned ON!, should see -gpu if NP>1 and  -disable_gpu_hooks when NP=1"
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=1 ./trilinos_jsrun  -p 2 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=1 ./trilinos_jsrun  -p 1 hostname 1>/dev/null
+ECHO_CMD=1 TPETRA_ASSUME_CUDA_AWARE_MPI=1 ./trilinos_jsrun  -p 1 hostname 1>/dev/null
