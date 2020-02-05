6743626d71216b315f3d0f7bbec325495c71d62d
Evan Harvey <eharvey@sandia.gov>
2020-02-05 08:12:06 -0700

    ATDM: Implement PR feedback (CDOFA-94)

--- cmake/std/atdm/load-env.sh
+++ cmake/std/atdm/load-env.sh
@@ -24,7 +24,7 @@ export atdm_config_get_abs_dir_path
 # Get the base dir for the sourced script
 ATDM_SCRIPT_DIR=$(readlink -f \
                 $(echo $BASH_SOURCE | sed "s/\(.*\)\/.*\.sh/\1/g"))
-echo "ATDM_SCRIPT_DIR = '$ATDM_SCRIPT_DIR'"
+#echo "ATDM_SCRIPT_DIR = '$ATDM_SCRIPT_DIR'"
 
 # Absolute path to this scripts dir
 export ATDM_CONFIG_SCRIPT_DIR=$ATDM_SCRIPT_DIR
