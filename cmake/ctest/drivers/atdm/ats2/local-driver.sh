9559c611195478f7a3b6ce630e67e03e43e1df68
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-02-14 16:19:12 -0700

    Create individual timeouts for full build&test and just test (CDOFA-94)
    I also remove some unnecassary verbose bash stuff loading the env

--- cmake/ctest/drivers/atdm/ats2/local-driver.sh
+++ cmake/ctest/drivers/atdm/ats2/local-driver.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -l
 
-if [ "${LSF_CTEST_TIMEOUT}" == "" ] ; then
-  LSF_CTEST_TIMEOUT=12:00
-  # This is just running tests, not the entire build!
+if [ "${LSF_CTEST_ALL_TIMEOUT}" == "" ] ; then
+  LSF_CTEST_ALL_TIMEOUT=12:00
+fi
+
+if [ "${LSF_CTEST_TEST_TIMEOUT}" == "" ] ; then
+  LSF_CTEST_TEST_TIMEOUT=4:00
 fi
 
 if [ "${Trilinos_CTEST_DO_ALL_AT_ONCE}" == "" ] ; then
   export Trilinos_CTEST_DO_ALL_AT_ONCE=TRUE
 fi
-
-set -x
 
 source $WORKSPACE/Trilinos/cmake/std/atdm/load-env.sh $JOB_NAME
 
@@ -18,7 +19,7 @@ set -x
 atdm_run_script_on_compute_node \
   $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ctest-s-driver.sh \
   $PWD/ctest-s-driver.out \
-  ${LSF_CTEST_TIMEOUT}
+  ${LSF_CTEST_ALL_TIMEOUT}
 
 if [ "${Trilinos_CTEST_RUN_CUDA_AWARE_MPI}" == "1" ]; then
   # Wait a little while before attempting another bsub allocation
@@ -33,5 +34,5 @@ if [ "${Trilinos_CTEST_RUN_CUDA_AWARE_MPI}" == "1" ]; then
   atdm_run_script_on_compute_node \
     $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ctest-s-driver.sh \
     $PWD/ctest-s-driver-cuda-aware-mpi.out \
-    ${LSF_CTEST_TIMEOUT}
+    ${LSF_CTEST_TEST_TIMEOUT}
 fi
