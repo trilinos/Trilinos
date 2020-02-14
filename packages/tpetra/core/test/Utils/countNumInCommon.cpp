298f5bbd7765592545e26a12a50d9c75b93b9af6
Mark Hoemmen <mhoemme@sandia.gov>
2020-02-13 21:23:53 -0700

    Tpetra: Add another unit test for countNumInCommon
    @trilinos/tpetra

--- packages/tpetra/core/test/Utils/countNumInCommon.cpp
+++ packages/tpetra/core/test/Utils/countNumInCommon.cpp
@@ -242,4 +242,67 @@ namespace { // (anonymous)
               expectedNumInCommon, expectedMergeSize);
   }
 
+  // This came from an application test problem, where I suspect that
+  // countNumInCommon may have been caught in an infinite loop.
+  TEUCHOS_UNIT_TEST( Utils, CountNumInCommon_regression )
+  {
+    std::vector<int> tgtColInds {{
+      91917, 91918, 91919, 91926, 91927, 91928, 92142, 92143,
+      92144, 92148, 92149, 92150, 92151, 92152, 92153, 92538,
+      92539, 92540, 92712, 92713, 92714, 95430, 95431, 95432,
+      95466, 95467, 95468, 95469, 95470, 95471, 95556, 95557,
+      95558, 95559, 95560, 95561, 95565, 95566, 95567, 95571,
+      95572, 95573, 95619, 95620, 95621, 95622, 95623, 95624,
+      95625, 95626, 95627, 95628, 95629, 95630, 95631, 95632,
+      95633, 95634, 95635, 95636
+    }};
+
+    std::vector<int> srcColInds {{
+      57090, 57091, 57092, 57093, 57094, 57095, 57102, 57103,
+      57104, 57117, 57118, 57119, 57120, 57121, 57122, 57123,
+      57124, 57125, 57129, 57130, 57131, 57183, 57184, 57185,
+      57186, 57187, 57188, 57195, 57196, 57197, 57198, 57199,
+      57200, 57204, 57205, 57206, 57207, 57208, 57209, 57222,
+      57223, 57224, 57225, 57226, 57227, 57228, 57229, 57230,
+      57234, 57235, 57236, 57237, 57238, 57239, 57240, 57241,
+      57242, 57243, 57244, 57245, 57246, 57247, 57248, 95421,
+      95422, 95423, 95430, 95431, 95432, 95436, 95437, 95438,
+      95457, 95458, 95459, 95463, 95464, 95465, 95466, 95467,
+      95468, 95469, 95470, 95471, 95472, 95473, 95474, 95556,
+      95557, 95558, 95559, 95560, 95561, 95565, 95566, 95567,
+      95571, 95572, 95573, 95619, 95620, 95621, 95622, 95623,
+      95624, 95625, 95626, 95627, 95628, 95629, 95630, 95631,
+      95632, 95633, 95634, 95635, 95636, 95754, 95755, 95756,
+      95757, 95758, 95759, 95835, 95836, 95837, 95838, 95839,
+      95840, 95841, 95842, 95843
+    }};
+
+    size_t numTgt = tgtColInds.size();
+    size_t numSrc = srcColInds.size();
+    size_t expectedUnionSize = 0;
+    {
+      std::vector<int> tgtColInds2(tgtColInds);
+      std::sort(tgtColInds2.begin(), tgtColInds2.end());
+      auto tgtEnd = std::unique(tgtColInds2.begin(), tgtColInds2.end());
+      numTgt = size_t(tgtEnd - tgtColInds2.begin());
+      TEST_EQUALITY( numTgt, tgtColInds.size() );
+
+      std::vector<int> srcColInds2(srcColInds);
+      std::sort(srcColInds2.begin(), srcColInds2.end());
+      auto srcEnd = std::unique(srcColInds2.begin(), srcColInds2.end());
+      numSrc = size_t(srcEnd - srcColInds2.begin());
+      TEST_EQUALITY( numSrc, srcColInds.size() );
+
+      std::vector<int> unionInds(numTgt + numSrc);
+      auto unionEnd = std::set_union(tgtColInds2.begin(), tgtEnd,
+                                     srcColInds2.begin(), srcEnd,
+                                     unionInds.begin());
+      expectedUnionSize = size_t(unionEnd - unionInds.begin());
+    }
+
+    const size_t expectedNumInCommon(39); // counted by hand
+    testLists(out, success, tgtColInds, srcColInds,
+              expectedNumInCommon, expectedUnionSize);
+  }
+
 } // namespace (anonymous)
