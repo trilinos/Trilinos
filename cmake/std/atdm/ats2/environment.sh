23eff8e9250cd02d5929c6fefa0483c45c4502d1
James Elliott <jjellio@sandia.gov>
2020-02-20 12:21:28 -0700

    The ATS2 ENV is changing TMPDIR, which is usually a reserved ENV.
    This causes a problem with jsrun RM connections.
    
    This patch renames that variable and prefixes it with Trilinos
    
    It isn't obvious this variable is used by the framework...
    If it is used, then where it is used needs to be updated as well.
    
    Fixes #6875

--- cmake/std/atdm/ats2/environment.sh
+++ cmake/std/atdm/ats2/environment.sh
@@ -171,10 +171,10 @@ if [[ "$ATDM_CONFIG_COMPILER" == "CUDA-10.1.243_"* ]]; then
   # CUDA Settings
   export CUDA_LAUNCH_BLOCKING=1
   export CUDA_MANAGED_FORCE_DEVICE_ALLOC=1
-  export TMPDIR=/tmp/$(whoami)
-  if [[ ! -d ${TMPDIR} ]] ; then
-    echo "Creating ${TMPDIR} for nvcc wrapper!"
-    mkdir ${TMPDIR}
+  export TRILINOS_NVCC_WRAPPER_TMPDIR=/tmp/$(whoami)
+  if [[ ! -d ${TRILINOS_NVCC_WRAPPER_TMPDIR} ]] ; then
+    echo "Creating ${TRILINOS_NVCC_WRAPPER_TMPDIR} for nvcc wrapper!"
+    mkdir ${TRILINOS_NVCC_WRAPPER_TMPDIR}
   fi
   # ATDM Settings
   export ATDM_CONFIG_USE_CUDA=ON
