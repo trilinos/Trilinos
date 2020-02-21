7f92a78133e08bf46b3bc5661adf2fc1d8dd070a
micheldemessieres <michel@demessieres.com>
2020-02-21 08:00:50 -0500

    Amesos2: Remove Amesos2_Tacho_Impl.hpp
    Do Tpetra ETI for Tacho using TPETRA_INSTANTIATE_SLGN_NO_ORDINAL_SCALAR.

--- packages/amesos2/src/Amesos2_Tacho.cpp
+++ packages/amesos2/src/Amesos2_Tacho.cpp
@@ -47,14 +47,20 @@
 #include "Amesos2_Tacho_decl.hpp"
 #include "Amesos2_Tacho_def.hpp"
 #include "Amesos2_ExplicitInstantiationHelpers.hpp"
+#include "TpetraCore_ETIHelperMacros.h"
 
 namespace Amesos2 {
 #ifdef HAVE_AMESOS2_EPETRA
   AMESOS2_SOLVER_EPETRA_INST(TachoSolver);
 #endif
 
-  #define AMESOS2_TPETRA_IMPL_SOLVER_NAME TachoSolver
-  #include "Amesos2_Tpetra_Impl.hpp"
+#define AMESOS2_TACHO_LOCAL_INSTANT(S,LO,GO,N) \
+template class Amesos2::TachoSolver<Tpetra::CrsMatrix<S, LO, GO, N>, \
+                                Tpetra::MultiVector<S, LO, GO, N> >;
+
+TPETRA_ETI_MANGLING_TYPEDEFS()
+
+TPETRA_INSTANTIATE_SLGN_NO_ORDINAL_SCALAR(AMESOS2_TACHO_LOCAL_INSTANT)
 
   #define AMESOS2_KOKKOS_IMPL_SOLVER_NAME TachoSolver
   #include "Amesos2_Kokkos_Impl.hpp"
