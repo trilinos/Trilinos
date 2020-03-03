293ca03438075a2a5b86cc2e73996d03f7bb2029
Chris Siefert <csiefer2@users.noreply.github.com>
2020-03-03 16:53:19 -0700

    Oops.

--- packages/trilinoscouplings/examples/scaling/TrilinosCouplings_TpetraIntrepidPoissonExample.cpp
+++ packages/trilinoscouplings/examples/scaling/TrilinosCouplings_TpetraIntrepidPoissonExample.cpp
@@ -1006,8 +1006,8 @@ makeMatrixAndRightHandSide (Teuchos::RCP<sparse_matrix_type>& A,
     TEUCHOS_FUNC_TIME_MONITOR_DIFF("Build matrix graph: 0-Total", graph_total);
 
     // Construct Tpetra::CrsGraph objects.
-    overlappedGraph = rcp (new sparse_graph_type (overlappedMapG, 0));
-    ownedGraph = rcp (new sparse_graph_type (globalMapG, 200));
+    overlappedGraph = rcp (new sparse_graph_type (overlappedMapG, 200));
+    ownedGraph = rcp (new sparse_graph_type (globalMapG, 0));
 
     // Define desired workset size and count how many worksets
     // there are on this process's mesh block.
