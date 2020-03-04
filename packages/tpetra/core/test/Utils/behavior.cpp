6a657f07df1a89a357e704f0a57d0f7f49359d99
Mark Hoemmen <mhoemme@sandia.gov>
2020-03-04 16:48:46 -0700

    Tpetra: Fix CUDA build warning in test

--- packages/tpetra/core/test/Utils/behavior.cpp
+++ packages/tpetra/core/test/Utils/behavior.cpp
@@ -190,7 +190,7 @@ namespace { // (anonymous)
 
         int threshold = std::stoi(std::string(varVal));
         const size_t threshold2 = Behavior::verbosePrintCountThreshold ();
-        TEST_EQUALITY( static_cast<const size_t>(threshold), threshold2 );
+        TEST_EQUALITY( static_cast<size_t>(threshold), threshold2 );
       }
     }
 
