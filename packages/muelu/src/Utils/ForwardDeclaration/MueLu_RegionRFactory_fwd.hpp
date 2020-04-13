44277262daa57c05dd06a07c21b3e3fc0d972589
Luc Berger-Vergiat <lberge@sandia.gov>
2020-04-13 17:13:23 -0600

    MueLu: region restriction operator factory
    This factory makes assumptions about the problem to allow for
    faster algorithms to compute the restriction operator.
    Implemented all the geometric cases independently to
    fill the restriction matrix. This should allow for
    good performance, hopefully faster than what smoothed
    aggregation does on a Laplace 3D problem.
    Just multiply the fine level nullspace by R to construct
    the coase nullpsace, injection could be performed instead.
    Also adding the logic in the unit test to check that
    the coarse nullspace can be pulled out from the level
    and inspected.
    Adding some logic so that coordinates are now propagated
    as the nullspace.
    Adding a regression test that runs both regionRFactory
    and GeometricInterpolationPFactory on the same mesh.
    It the compares the residual history of both and determines
    if there is any difference between them (tol=1.0e-12)

--- packages/muelu/src/Utils/ForwardDeclaration/MueLu_RegionRFactory_fwd.hpp
+++ packages/muelu/src/Utils/ForwardDeclaration/MueLu_RegionRFactory_fwd.hpp
@@ -43,67 +43,21 @@
 // ***********************************************************************
 //
 // @HEADER
-#ifndef MUELU_GENERICRFACTORY_DECL_HPP
-#define MUELU_GENERICRFACTORY_DECL_HPP
+#ifndef MUELU_REGIONRFACTORY_FWD_HPP
+#define MUELU_REGIONRFACTORY_FWD_HPP
 
-/*
- * MueLu_GenericRFactory.hpp
- *
- *  Created on: 20.09.2011
- *      Author: tobias
- */
 
-#include "MueLu_ConfigDefs.hpp"
-#include "MueLu_TwoLevelFactoryBase.hpp"
-#include "MueLu_GenericRFactory_fwd.hpp"
 
-#include "MueLu_Level_fwd.hpp"
-#include "MueLu_PFactory_fwd.hpp"
 
 namespace MueLu {
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+  class RegionRFactory;
+}
 
-  /*!
-    @class GenericRFactory class.
-    @brief Factory for building restriction operators using a prolongator factory
-  */
+#ifndef MUELU_REGIONRFACTORY_SHORT
+#define MUELU_REGIONRFACTORY_SHORT
+#endif
 
-  template <class Scalar = DefaultScalar,
-            class LocalOrdinal = DefaultLocalOrdinal,
-            class GlobalOrdinal = DefaultGlobalOrdinal,
-            class Node = DefaultNode>
-  class GenericRFactory : public TwoLevelFactoryBase {
-#undef MUELU_GENERICRFACTORY_SHORT
-#include "MueLu_UseShortNames.hpp"
 
-  public:
-    //! @name Constructors/Destructors.
-    //@{
 
-    //! Constructor.
-    GenericRFactory() { }
-
-    //! Destructor.
-    virtual ~GenericRFactory() { }
-    //@}
-
-    //! Input
-    //@{
-    RCP<const ParameterList> GetValidParameterList() const;
-
-    void DeclareInput(Level &fineLevel, Level &coarseLevel) const;
-
-    //@}
-
-    //! @name Build methods.
-    //@{
-
-    void Build(Level & fineLevel, Level & coarseLevel) const;
-
-    //@}
-
-  }; //class GenericRFactory
-
-} //namespace MueLu
-
-#define MUELU_GENERICRFACTORY_SHORT
-#endif // MUELU_GENERICRFACTORY_DECL_HPP
+#endif // MUELU_REGIONRFACTORY_FWD_HPP
