b5cb3bc67a6d7e393cb948fcfef6cdb39a4c2ec9
Irina K. Tezaur <ikalash@sandia.gov>
2020-04-16 19:30:58 -0700

    Removing "boost_signals" from REQUIRED_LIBS_NAMES in the file cmake/TPLs/FindTPLBoostAlbLib.cmake .
    boost_signals has been eliminated, and we no longer need it for Albany.

--- cmake/TPLs/FindTPLBoostAlbLib.cmake
+++ cmake/TPLs/FindTPLBoostAlbLib.cmake
@@ -56,6 +56,6 @@
 
 TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( BoostAlbLib
   REQUIRED_HEADERS boost/version.hpp boost/mpl/at.hpp
-  REQUIRED_LIBS_NAMES boost_signals boost_regex boost_filesystem boost_thread boost_wserialization boost_serialization boost_mpi boost_program_options boost_system
+  REQUIRED_LIBS_NAMES boost_regex boost_filesystem boost_thread boost_wserialization boost_serialization boost_mpi boost_program_options boost_system
   )
 
