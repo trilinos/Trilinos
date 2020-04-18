1fa8ebbf9239e5bf3415f099e731f0e0c40f7614
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-04-18 15:14:46 -0600

    ATDM: van1-tx2: Switch to atdm_match_any_buildname_keyword (#5939, CDOFA-27)
    Now 'arm' will not match non-keywords.
    
    I also added this to the ATDMTrilinosConfigTests package tests.

--- cmake/std/atdm/van1-tx2/custom_builds_unit_tests.sh
+++ cmake/std/atdm/van1-tx2/custom_builds_unit_tests.sh
@@ -10,29 +10,40 @@ ATDM_CONFIG_SCRIPT_DIR=`readlink -f ${CURRENT_SCRIPTS_DIR}/..`
 
 testAll() {
 
+  ATDM_CONFIG_SYSTEM_DIR=${ATDM_CONFIG_SCRIPT_DIR}/van1-tx2
+
   ATDM_CONFIG_BUILD_NAME=default
-  . ${ATDM_CONFIG_SCRIPT_DIR}/van1-tx2/custom_builds.sh
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
   ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} ARM-20.0_OPENMPI-4.0.2
 
   ATDM_CONFIG_BUILD_NAME=before-arm-20.0-openmpi-4.0.2_after
-  . ${ATDM_CONFIG_SCRIPT_DIR}/van1-tx2/custom_builds.sh
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
   ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} ARM-20.0_OPENMPI-4.0.2
 
   ATDM_CONFIG_BUILD_NAME=before-arm-20.0_openmpi-4.0.2-after
-  . ${ATDM_CONFIG_SCRIPT_DIR}/van1-tx2/custom_builds.sh
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
   ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} ARM-20.0_OPENMPI-4.0.2
 
-  ATDM_CONFIG_BUILD_NAME=arm-20.0
-  . ${ATDM_CONFIG_SCRIPT_DIR}/van1-tx2/custom_builds.sh
+  ATDM_CONFIG_BUILD_NAME=before_arm-20.0-after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
   ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} ARM-20.0_OPENMPI-4.0.2
 
-  ATDM_CONFIG_BUILD_NAME=arm-20
-  . ${ATDM_CONFIG_SCRIPT_DIR}/van1-tx2/custom_builds.sh
+  ATDM_CONFIG_BUILD_NAME=before_arm-20-afert
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
   ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} ARM-20.0_OPENMPI-4.0.2
 
-  ATDM_CONFIG_BUILD_NAME=arm
-  . ${ATDM_CONFIG_SCRIPT_DIR}/van1-tx2/custom_builds.sh
+  ATDM_CONFIG_BUILD_NAME=before-arm-after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
   ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} ARM-20.0_OPENMPI-4.0.2
+
+  ATDM_CONFIG_BUILD_NAME=BEFORE-ARM-AFTER
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} ARM-20.0_OPENMPI-4.0.2
+
+  # Make sure 'arms' does not match 'arm'
+  ATDM_CONFIG_BUILD_NAME=anything-arms
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} ${ATDM_CONFIG_COMPILER} DEFAULT
 
 }
 
