6aaa125ef84dffbc5f1339e868972b86f3ac22ca
Paul Kuberry <pakuber@sandia.gov>
2020-04-25 19:55:40 -0600

    Removed all MKL logic from Dependencies.cmake and CMakeLists.txt.

--- packages/compadre/cmake/Dependencies.cmake
+++ packages/compadre/cmake/Dependencies.cmake
@@ -1,18 +1,9 @@
-if (Compadre_USE_MKL)
+if (Compadre_USE_CUDA)
 tribits_package_define_dependencies(
   LIB_REQUIRED_PACKAGES
     KokkosCore KokkosContainers KokkosAlgorithms
   LIB_REQUIRED_TPLS
-    MKL
-  LIB_OPTIONAL_TPLS
-    MPI
-  )
-elseif (Compadre_USE_CUDA)
-tribits_package_define_dependencies(
-  LIB_REQUIRED_PACKAGES
-    KokkosCore KokkosContainers KokkosAlgorithms
-  LIB_REQUIRED_TPLS
-    CUSOLVER CUBLAS
+    CUDA CUSOLVER CUBLAS
   LIB_OPTIONAL_TPLS
     MPI
   )
