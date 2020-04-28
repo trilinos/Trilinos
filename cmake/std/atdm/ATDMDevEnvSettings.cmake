trilinos/Trilinos

commit 09e765db0adccbbc146cf777ec3d5d4bfb1f748b

CONFIG: Update NetCDF and HDF5 TPL version (#7197)
* CONFIG: Update NetCDF and HDF5 TPL version

* DO NOT MERGE See if this fixes builds

* CONFIG: Update sems netcdf module extension for new versions

* NETCDF: Remove BoostLib dependency

* NETCDF: See if this triggers use of curl

* NETCDF: Better way of specifying library list

* HDF5 -- See what HDF5 variables are set to...

* Undo some changes that were probably not needed

* NETCDF: Try older versions on gcc-4.8.4 build

* NETCDF: Try older versions on gcc-4.8.4 build

* NETCDF: Remove incorrect boost dependency

* Eliminate debug print

* Clean up the NetCDF library settings.

For SEMS, the pnetcdf library is in the SEMS_NETCDF_ROOT directory and
SEM_PNETCDF_ROOT is not defined (or PNETCDF_ROOT).  In all scripts checked
whether PNETCDF_ROOT was defined and if not, set it to value of NETCDF_ROOT

In cmake config files make sure root of libpnetcdf.{a|so} is PNETCDF_ROOT and
make sure PNETCDF_ROOT was set to some value.

* TPLs updated on test machine

Use HDF5-1.10.6 and NetCDF-4.7.3 now.

* Eliminate curl library dependency for cuda build

The SEMS installations of NetCDF included the DAP setting which requires -lcurl;

Installations on other systems did not include DAP, so don't need curl.  This is detected in the FindNetCDF.cmake, but that is currently not called for most builds...

* Mutrino NetCDF does not require curl library

* Ride NetCDF library does not require curl library

* shiller NetCDF does not require curl library

* van1-tx2 NetCDF library does not require curl library

* waterman NetCDF library does not require curl library

* Remove debug variable prints

* Update the non-unit test file also

After sems was updated on test machine, I updated the unit test file on 4.8.4 build, but not the other file that also needed updating...

* Curl library dependency not related to MPI

cmake/std/atdm/ATDMDevEnvSettings.cmake
@@ -430,8 +430,13 @@ ELSE()
   IF (NOT "$ENV{ATDM_CONFIG_NETCDF_LIBS}" STREQUAL "")
     ATDM_SET_CACHE(TPL_Netcdf_LIBRARIES "$ENV{ATDM_CONFIG_NETCDF_LIBS}" CACHE FILEPATH)
   ELSE()
+    IF ("$ENV{PNETCDF_ROOT}" STREQUAL "")
+      SET(PNETCDF_ROOT "$ENV{NETCDF_ROOT}")
+    ELSE()
+      SET(PNETCDF_ROOT "$ENV{PNETCDF_ROOT}")
+    ENDIF()
     ASSERT_DEFINED(ENV{ZLIB_ROOT})
-    ATDM_SET_CACHE(TPL_Netcdf_LIBRARIES "${TPL_BoostLib_LIBRARIES};$ENV{NETCDF_ROOT}/lib/libnetcdf.${ATDM_TPL_LIB_EXT};$ENV{NETCDF_ROOT}/lib/libpnetcdf.a;$ENV{HDF5_ROOT}/lib/libhdf5_hl.${ATDM_TPL_LIB_EXT};$ENV{HDF5_ROOT}/lib/libhdf5.${ATDM_TPL_LIB_EXT};$ENV{ZLIB_ROOT}/lib/libz.${ATDM_TPL_LIB_EXT};-ldl;-lcurl" CACHE FILEPATH)
+    ATDM_SET_CACHE(TPL_Netcdf_LIBRARIES "$ENV{NETCDF_ROOT}/lib/libnetcdf.${ATDM_TPL_LIB_EXT};${PNETCDF_ROOT}/lib/libpnetcdf.a;$ENV{HDF5_ROOT}/lib/libhdf5_hl.${ATDM_TPL_LIB_EXT};$ENV{HDF5_ROOT}/lib/libhdf5.${ATDM_TPL_LIB_EXT};$ENV{ZLIB_ROOT}/lib/libz.${ATDM_TPL_LIB_EXT};-ldl;-lcurl" CACHE FILEPATH)
   ENDIF()
 ENDIF()
 

cmake/std/atdm/sems-rhel6/environment.sh
@@ -169,11 +169,10 @@ else
 fi
 
 module load sems-openmpi/1.10.1
-module load sems-netcdf/4.4.1/exo_parallel
-module load sems-hdf5/1.8.12/parallel
+module load sems-netcdf/4.7.3/parallel
+module load sems-hdf5/1.10.6/parallel
 module load sems-zlib/1.2.8/base
-module load sems-boost/1.59.0/base
-module unload sems-python/2.7.9 
+module unload sems-python/2.7.9
 module load sems-superlu/4.3/base
 
 if [[ "${ATDM_CONFIG_SHARED_LIBS}" == "ON" ]] ; then
@@ -186,12 +185,17 @@ export ATDM_CONFIG_USE_HWLOC=OFF
 export HWLOC_LIBS=-lhwloc
 
 export ZLIB_ROOT=${SEMS_ZLIB_ROOT}
-export BOOST_ROOT=${SEMS_BOOST_ROOT}
 export HDF5_ROOT=${SEMS_HDF5_ROOT}
 export NETCDF_ROOT=${SEMS_NETCDF_ROOT}
+if [[ "${SEMS_PNETCDF_ROOT}" == "" ]] ; then
+  export PNETCDF_ROOT=${SEMS_NETCDF_ROOT}
+else
+  export PNETCDF_ROOT=${SEMS_PNETCDF_ROOT}
+fi
 
-export ATDM_CONFIG_HDF5_LIBS="${SEMS_HDF5_ROOT}/lib/libhdf5_hl.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_HDF5_ROOT}/lib/libhdf5.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_ZLIB_ROOT}/lib/libz.${ATDM_CONFIG_TPL_LIB_EXT};-ldl"
-export ATDM_CONFIG_NETCDF_LIBS="${SEMS_BOOST_ROOT}/lib/libboost_program_options.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_BOOST_ROOT}/lib/libboost_system.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_NETCDF_ROOT}/lib/libnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_NETCDF_ROOT}/lib/libpnetcdf.a;${SEMS_HDF5_ROOT}/lib/libhdf5_hl.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_HDF5_ROOT}/lib/libhdf5.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_ZLIB_ROOT}/lib/libz.${ATDM_CONFIG_TPL_LIB_EXT};-ldl;-lcurl"
+export ATDM_CONFIG_HDF5_LIBS="${HDF5_ROOT}/lib/libhdf5_hl.${ATDM_CONFIG_TPL_LIB_EXT};${HDF5_ROOT}/lib/libhdf5.${ATDM_CONFIG_TPL_LIB_EXT};${ZLIB_ROOT}/lib/libz.${ATDM_CONFIG_TPL_LIB_EXT};-ldl"
+
+export ATDM_CONFIG_NETCDF_LIBS="-L${NETCDF_ROOT}/lib;${NETCDF_ROOT}/lib/libnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${PNETCDF_ROOT}/lib/libpnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${ATDM_CONFIG_HDF5_LIBS};-lcurl"
 
 # NOTE: SEMS does not provide a *.a files for PNetCDF so we can't use them in
 # a shared lib build :-(
@@ -202,7 +206,7 @@ export MPICXX=`which mpicxx`
 export MPIF90=`which mpif90`
 
 export ATDM_CONFIG_MPI_PRE_FLAGS="--bind-to;none"
- 
+
 #
 # Set up default install-related stuff
 #

cmake/std/atdm/sems-rhel7/environment.sh
@@ -218,11 +218,11 @@ else
 fi
 
 module load sems-openmpi/1.10.1
-module load sems-netcdf/4.4.1/exo_parallel
-module load sems-hdf5/1.8.12/parallel
+module load sems-netcdf/4.7.3/parallel
+module load sems-hdf5/1.10.6/parallel
 module load sems-zlib/1.2.8/base
 module load sems-boost/1.59.0/base
-module unload sems-python/2.7.9 
+module unload sems-python/2.7.9
 module load sems-superlu/4.3/base
 
 if [[ "$ATDM_CONFIG_COMPILER" == "CUDA"* ]] && \
@@ -259,11 +259,18 @@ export ATDM_CONFIG_USE_HWLOC=OFF
 export HWLOC_LIBS=-lhwloc
 
 export BOOST_ROOT=${SEMS_BOOST_ROOT}
+
 export HDF5_ROOT=${SEMS_HDF5_ROOT}
 export NETCDF_ROOT=${SEMS_NETCDF_ROOT}
+if [[ "${SEMS_PNETCDF_ROOT}" == "" ]] ; then
+  export PNETCDF_ROOT=${NETCDF_ROOT}
+else
+  export PNETCDF_ROOT=${SEMS_PNETCDF_ROOT}
+fi
 
-export ATDM_CONFIG_HDF5_LIBS="-L${SEMS_HDF5_ROOT}/lib;${SEMS_HDF5_ROOT}/lib/libhdf5_hl.a;${SEMS_HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl"
-export ATDM_CONFIG_NETCDF_LIBS="-L${SEMS_BOOST_ROOT}/lib;-L${SEMS_NETCDF_ROOT}/lib;-L${SEMS_NETCDF_ROOT}/lib;-L${SEMS_PNETCDF_ROOT}/lib;-L${SEMS_HDF5_ROOT}/lib;${SEMS_BOOST_ROOT}/lib/libboost_program_options.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_BOOST_ROOT}/lib/libboost_system.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_NETCDF_ROOT}/lib/libnetcdf.a;${SEMS_NETCDF_ROOT}/lib/libpnetcdf.a;${SEMS_HDF5_ROOT}/lib/libhdf5_hl.a;${SEMS_HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl;-lcurl"
+export ATDM_CONFIG_HDF5_LIBS="${HDF5_ROOT}/lib/libhdf5_hl.${ATDM_CONFIG_TPL_LIB_EXT};${HDF5_ROOT}/lib/libhdf5.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_ZLIB_ROOT}/lib/libz.${ATDM_CONFIG_TPL_LIB_EXT};-ldl"
+
+export ATDM_CONFIG_NETCDF_LIBS="${SEMS_NETCDF_ROOT}/lib/libnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${PNETCDF_ROOT}/lib/libpnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${ATDM_CONFIG_HDF5_LIBS};-lcurl"
 
 # NOTE: SEMS does not provide the correct *.so files for NetCDF so we can't
 # use them in a shared lib build :-(

cmake/std/atdm/ats2/environment.sh
@@ -220,7 +220,6 @@ export ATDM_CONFIG_SPARC_TPL_BASE=/projects/sparc/tpls/ats2-${sparc_tpl_arch}
 sparc_tpl_base=${ATDM_CONFIG_SPARC_TPL_BASE}
 
 # Commont ROOT config variables
-export BOOST_ROOT=${sparc_tpl_base}/boost-1.65.1/00000000/${sparc_tpl_ext}
 export HDF5_ROOT=${sparc_tpl_base}/hdf5-1.10.5/00000000/${sparc_tpl_mpi_ext}
 export CGNS_ROOT=${sparc_tpl_base}/cgns-c09a5cd/27e5681f1b74c679b5dcb337ac71036d16c47977/${sparc_tpl_mpi_ext}
 export PNETCDF_ROOT=${sparc_tpl_base}/pnetcdf-1.10.0/6144dc67b2041e4093063a04e89fc1e33398bd09/${sparc_tpl_mpi_ext}
@@ -231,7 +230,7 @@ export SUPERLUDIST_ROOT=${sparc_tpl_base}/superlu_dist-5.4.0/a3121eaff44f7bf7d44
 
 export ATDM_CONFIG_USE_HWLOC=OFF
 export ATDM_CONFIG_HDF5_LIBS="-L${HDF5_ROOT}/lib;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl"
-export ATDM_CONFIG_NETCDF_LIBS="-L${BOOST_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${SEMS_PNETCDF_ROOT}/lib;-L${HDF5_ROOT}/lib;${BOOST_ROOT}/lib/libboost_program_options.a;${BOOST_ROOT}/lib/libboost_system.a;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl;-lcurl"
+export ATDM_CONFIG_NETCDF_LIBS="-L${NETCDF_ROOT}/lib;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${ATDM_CONFIG_HDF5_LIBS};-lcurl"
 
 if [[ "${ATDM_CONFIG_SUPERLUDIST_INCLUDE_DIRS}" == "" ]] ; then
   export ATDM_CONFIG_SUPERLUDIST_INCLUDE_DIRS=${SUPERLUDIST_ROOT}/include

cmake/std/atdm/cee-rhel6/environment.sh
@@ -217,7 +217,10 @@ atdm_config_add_libs_to_var ATDM_CONFIG_BOOST_LIBS ${BOOST_ROOT}/lib .a \
 
 export ATDM_CONFIG_HDF5_LIBS="-L${HDF5_ROOT}/lib;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl"
 
-export ATDM_CONFIG_NETCDF_LIBS="-L${BOOST_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${SEMS_PNETCDF_ROOT}/lib;-L${HDF5_ROOT}/lib;${BOOST_ROOT}/lib/libboost_program_options.a;${BOOST_ROOT}/lib/libboost_system.a;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl;-lcurl"
+if [[ "${PNETCDF_ROOT}" == "" ]] ; then
+  export PNETCDF_ROOT=${NETCDF_ROOT}
+fi
+export ATDM_CONFIG_NETCDF_LIBS="-L${NETCDF_ROOT}/lib;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${ATDM_CONFIG_HDF5_LIBS};-lcurl"
 
 # SuperLUDist
 if [[ "${ATDM_CONFIG_SUPERLUDIST_INCLUDE_DIRS}" == "" ]] ; then

cmake/std/atdm/examples/my-sems-rhel6-config/environment.sh
@@ -3,7 +3,7 @@
 
 echo "Using my custom SEMS RHEL6 env for compiler stack $ATDM_CONFIG_COMPILER to build $ATDM_CONFIG_BUILD_TYPE code with Kokkos node type $ATDM_CONFIG_NODE_TYPE"
 
-if [[ "${ATDM_CONFIG_COMPILER}" != "GNU" ]] ; then 
+if [[ "${ATDM_CONFIG_COMPILER}" != "GNU" ]] ; then
   echo "ATDM_CONFIG_COMPILER=${ATDM_CONFIG_COMPILER} not supported!  Only 'gnu' supported currently!"
 fi
 
@@ -18,11 +18,11 @@ module load sems-ninja_fortran/1.8.2
 
 module load sems-gcc/7.2.0
 module load sems-openmpi/1.10.1
-module load sems-netcdf/4.4.1/exo_parallel
-module load sems-hdf5/1.8.12/parallel
+module load sems-netcdf/4.7.3/parallel
+module load sems-hdf5/1.10.6/parallel
 module load sems-zlib/1.2.8/base
 module load sems-boost/1.59.0/base
-module unload sems-python/2.7.9 
+module unload sems-python/2.7.9
 module load sems-superlu/4.3/base
 
 # 2) Set up env vars that will get picked up by ATDMDevEnvSettings.cmake when

cmake/std/atdm/mutrino/environment.sh
@@ -76,7 +76,7 @@ module load cmake/3.14.6
 
 # No RPATH for static builds
 export ATDM_CONFIG_CMAKE_SKIP_INSTALL_RPATH=ON
-# ToDo: Make above contingent on 'static' or 'shared' 
+# ToDo: Make above contingent on 'static' or 'shared'
 
 # Use manually installed cmake and ninja to allow usage of ninja and
 # all-at-once mode
@@ -93,8 +93,11 @@ export ATDM_CONFIG_BLAS_LIBS="-mkl"
 
 export ATDM_CONFIG_USE_HWLOC=OFF
 
+if [[ "${PNETCDF_ROOT}" == "" ]] ; then
+  export PNETCDF_ROOT=${NETCDF_ROOT}
+fi
 export ATDM_CONFIG_HDF5_LIBS="-L${HDF5_ROOT}/lib;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl"
-export ATDM_CONFIG_NETCDF_LIBS="-L${BOOST_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${PNETCDF_ROOT}/lib;-L${HDF5_ROOT}/lib;${BOOST_ROOT}/lib/libboost_program_options.a;${BOOST_ROOT}/lib/libboost_system.a;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl;-lm"
+export ATDM_CONFIG_NETCDF_LIBS="-L${NETCDF_ROOT}/lib;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${ATDM_CONFIG_HDF5_LIBS};-lm"
 
 export ATDM_CONFIG_COMPLETED_ENV_SETUP=TRUE
 
@@ -153,7 +156,7 @@ function atdm_run_script_on_compute_node {
   else
     account=${account_input}
   fi
-  
+
   if [ -e $output_file ] ; then
     echo "Remove existing file $output_file"
     rm $output_file
@@ -168,11 +171,11 @@ function atdm_run_script_on_compute_node {
   else
    echo
    echo "***"
-   echo "*** ERROR: Invalid value ATDM_CONFIG_KOKKOS_ARCH=${ATDM_CONFIG_KOKKOS_ARCH} specified!" 
+   echo "*** ERROR: Invalid value ATDM_CONFIG_KOKKOS_ARCH=${ATDM_CONFIG_KOKKOS_ARCH} specified!"
    echo "***"
    return
   fi
- 
+
   echo
   echo "Running '$script_to_run' using sbatch in the background ..."
   set -x
@@ -180,22 +183,22 @@ function atdm_run_script_on_compute_node {
     --time=${timeout} -J $ATDM_CONFIG_BUILD_NAME ${script_to_run} &
   SBATCH_PID=$!
   set +x
-  
+
   echo
   echo "Tailing output file $output_file in the background ..."
   set -x
   tail -f $output_file &
   TAIL_BID=$!
   set +x
-  
+
   echo
   echo "Waiting for SBATCH_PID=$SBATCH_PID ..."
   wait $SBATCH_PID
-  
+
   echo
   echo "Kill TAIL_BID=$TAIL_BID"
   kill -s 9 $TAIL_BID
-  
+
   echo
   echo "Finished running ${script_to_run}!"
   echo

cmake/std/atdm/ride/environment.sh
@@ -217,8 +217,11 @@ fi
 
 export ATDM_CONFIG_USE_HWLOC=OFF
 
+if [[ "${PNETCDF_ROOT}" == "" ]] ; then
+  export PNETCDF_ROOT=${NETCDF_ROOT}
+fi
 export ATDM_CONFIG_HDF5_LIBS="-L${HDF5_ROOT}/lib;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl"
-export ATDM_CONFIG_NETCDF_LIBS="-L${BOOST_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${NETCDF_ROOT}/lib;-L${PNETCDF_ROOT}/lib;-L${HDF5_ROOT}/lib;${BOOST_ROOT}/lib/libboost_program_options.a;${BOOST_ROOT}/lib/libboost_system.a;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${HDF5_ROOT}/lib/libhdf5_hl.a;${HDF5_ROOT}/lib/libhdf5.a;-lz;-ldl"
+export ATDM_CONFIG_NETCDF_LIBS="-L${NETCDF_ROOT}/lib;-L${HDF5_ROOT}/lib;${NETCDF_ROOT}/lib/libnetcdf.a;${PNETCDF_ROOT}/lib/libpnetcdf.a;${ATDM_CONFIG_HDF5_LIBS}"
 
 # Use manually installed cmake and ninja to try to avoid module loading
 # problems (see TRIL-208)

commit cbbac67c79ef6b112ad14da3da8e96c318c09cf8

CONFIG: Fix pnetcdf and netcdf setting broke in #7197

cmake/std/atdm/ATDMDevEnvSettings.cmake
@@ -418,6 +418,7 @@ ENDIF()
 
 # Netcdf
 ATDM_SET_ENABLE(TPL_ENABLE_Netcdf ON)
+ATDM_SET_CACHE(NETCDF_ROOT "$ENV(NETCDF_ROOT)" CACHE FILEPATH)
 IF (ATDM_USE_SPARC_TPL_FIND_SETTINGS)
   # SPARC ATDM Trilinos configuration has SEACAS-customized FindNetCDF.cmake
   # module recurrsively find PNetCDF and HDF5 internally!

cmake/std/atdm/sems-rhel6/environment.sh
@@ -195,7 +195,7 @@ fi
 
 export ATDM_CONFIG_HDF5_LIBS="${HDF5_ROOT}/lib/libhdf5_hl.${ATDM_CONFIG_TPL_LIB_EXT};${HDF5_ROOT}/lib/libhdf5.${ATDM_CONFIG_TPL_LIB_EXT};${ZLIB_ROOT}/lib/libz.${ATDM_CONFIG_TPL_LIB_EXT};-ldl"
 
-export ATDM_CONFIG_NETCDF_LIBS="-L${NETCDF_ROOT}/lib;${NETCDF_ROOT}/lib/libnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${PNETCDF_ROOT}/lib/libpnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${ATDM_CONFIG_HDF5_LIBS};-lcurl"
+export ATDM_CONFIG_NETCDF_LIBS="-L${NETCDF_ROOT}/lib;${NETCDF_ROOT}/lib/libnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${PNETCDF_ROOT}/lib/libpnetcdf.a;${ATDM_CONFIG_HDF5_LIBS};-lcurl"
 
 # NOTE: SEMS does not provide a *.a files for PNetCDF so we can't use them in
 # a shared lib build :-(

cmake/std/atdm/sems-rhel7/environment.sh
@@ -270,7 +270,7 @@ fi
 
 export ATDM_CONFIG_HDF5_LIBS="${HDF5_ROOT}/lib/libhdf5_hl.${ATDM_CONFIG_TPL_LIB_EXT};${HDF5_ROOT}/lib/libhdf5.${ATDM_CONFIG_TPL_LIB_EXT};${SEMS_ZLIB_ROOT}/lib/libz.${ATDM_CONFIG_TPL_LIB_EXT};-ldl"
 
-export ATDM_CONFIG_NETCDF_LIBS="${SEMS_NETCDF_ROOT}/lib/libnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${PNETCDF_ROOT}/lib/libpnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${ATDM_CONFIG_HDF5_LIBS};-lcurl"
+export ATDM_CONFIG_NETCDF_LIBS="${SEMS_NETCDF_ROOT}/lib/libnetcdf.${ATDM_CONFIG_TPL_LIB_EXT};${PNETCDF_ROOT}/lib/libpnetcdf.a;${ATDM_CONFIG_HDF5_LIBS};-lcurl"
 
 # NOTE: SEMS does not provide the correct *.so files for NetCDF so we can't
 # use them in a shared lib build :-(