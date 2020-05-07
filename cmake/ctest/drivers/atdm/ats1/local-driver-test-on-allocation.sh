9b1ae7a576f31d54fdd75f9fea8be3bd49640c47
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-05-07 09:11:23 -0600

    Add print headers for what ctest -S script is being run (ATDV-347)

--- cmake/ctest/drivers/atdm/ats1/local-driver-test-on-allocation.sh
+++ cmake/ctest/drivers/atdm/ats1/local-driver-test-on-allocation.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+
+echo
+echo "======================================================================="
+echo "***"
+echo "*** Run tests on compute node '${SLURM_JOB_NODELIST}'"
+echo "***"
+echo
+
 export CTEST_SITE=${ATDM_CONFIG_CDASH_HOSTNAME}
 export CTEST_DO_NEW_START=FALSE
 export CTEST_DO_UPDATES=FALSE
