07a4b33bcb4c308a486c156d70a946e6340f98f4
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-05-07 08:39:12 -0600

    Small changes, perhaps one real fix (ATDV-347)
    * Moved, changed a few comments
    
    * Quoted and printed ATDM_CONFIG_SBATCH_EXTRA_ARGS (this might have been
      broken before)
    
    * Removed setting Trilinos_CTEST_DO_ALL_AT_ONCE=TRUE since that has been the
      default for a long time.  (Shows you how long ago the old 'mutrino' was
      first set up.)

--- cmake/ctest/drivers/atdm/ats1/local-driver.sh
+++ cmake/ctest/drivers/atdm/ats1/local-driver.sh
@@ -8,25 +8,20 @@ if [ "${SBATCH_TEST_TIME_LIMIT_MINUTES}" == "" ] ; then
   export SBATCH_TEST_TIME_LIMIT_MINUTES=840 # Default 14 hour time limit
 fi
 
-if [ "${Trilinos_CTEST_DO_ALL_AT_ONCE}" == "" ] ; then
-  export Trilinos_CTEST_DO_ALL_AT_ONCE=TRUE
-fi
-
-# Load environment on the "this" node
+# Load environment on the login node
 source $WORKSPACE/Trilinos/cmake/std/atdm/load-env.sh $JOB_NAME
 
-set -x
-
-atdm_config_sbatch_extra_args=$ATDM_CONFIG_SBATCH_EXTRA_ARGS
-unset ATDM_CONFIG_SBATCH_EXTRA_ARGS
 # Run configure, build on a haswell compute node
+atdm_config_sbatch_extra_args="$ATDM_CONFIG_SBATCH_EXTRA_ARGS"
+unset ATDM_CONFIG_SBATCH_EXTRA_ARGS
 atdm_run_script_on_compute_node \
   $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ats1/local-driver-build-on-allocation.sh \
   $PWD/ctest-s-driver.out \
   ${SBATCH_BUILD_TIME_LIMIT_MINUTES}
 
-export ATDM_CONFIG_SBATCH_EXTRA_ARGS=$atdm_config_sbatch_extra_args
- # Run tests on either a KNL or HSW compute node
+# Run tests on either a KNL or HSW compute node
+export ATDM_CONFIG_SBATCH_EXTRA_ARGS="$atdm_config_sbatch_extra_args"
+echo "ATDM_CONFIG_SBATCH_EXTRA_ARGS = '${ATDM_CONFIG_SBATCH_EXTRA_ARGS}'"
 atdm_run_script_on_compute_node \
   $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ats1/local-driver-test-on-allocation.sh \
   $PWD/ctest-s-driver.out \
