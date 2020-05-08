0283c590207e81ca7d1fb90fe0f3806825402635
Irina K. Tezaur <ikalash@sandia.gov>
2020-05-07 18:55:59 -0700

    Piro: filling in TempusIntegrator routines.

--- packages/piro/src/Piro_TempusIntegrator_Def.hpp
+++ packages/piro/src/Piro_TempusIntegrator_Def.hpp
@@ -89,63 +89,174 @@ template <typename Scalar>
 Teuchos::RCP<Tempus::Stepper<Scalar>> 
 Piro::TempusIntegrator<Scalar>::getStepper() const
 {
-  //IKT FIXME - fill in! 
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Teuchos::RCP<Tempus::Stepper<Scalar>> stepper; 
+  if (basicIntegrator_ != Teuchos::null) {
+    stepper = basicIntegrator_->getStepper(); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    stepper = fwdSensIntegrator_->getStepper(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    stepper = adjSensIntegrator_->getStepper(); 
+  }
+  return stepper; 
 }
 
 template <typename Scalar>
 bool 
 Piro::TempusIntegrator<Scalar>::advanceTime(const Scalar time_final)
 {
-  //IKT FIXME - fill in! 
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  bool out; 
+  if (basicIntegrator_ != Teuchos::null) {
+    out = basicIntegrator_->advanceTime(time_final); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    out = fwdSensIntegrator_->advanceTime(time_final); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    out = adjSensIntegrator_->advanceTime(time_final); 
+  }
+  return out; 
 }
 
 template <typename Scalar>
 Scalar 
 Piro::TempusIntegrator<Scalar>::getTime() const
 {
-//IKT FIXME - fill in!
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Scalar time; 
+  if (basicIntegrator_ != Teuchos::null) {
+    time = basicIntegrator_->getTime(); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    time = fwdSensIntegrator_->getTime(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    time = adjSensIntegrator_->getTime(); 
+  }
+  return time; 
 }
 
 template <typename Scalar>
 Teuchos::RCP<const Thyra::VectorBase<Scalar>> 
 Piro::TempusIntegrator<Scalar>::getX() const
 {
-  //IKT FIXME - fill in! 
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Teuchos::RCP<const Thyra::VectorBase<Scalar>> x; 
+  if (basicIntegrator_ != Teuchos::null) {
+    x = basicIntegrator_->getX(); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    x = fwdSensIntegrator_->getX(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    x = adjSensIntegrator_->getX(); 
+  }
+  return x; 
 }
  
 template <typename Scalar>
 Teuchos::RCP<const Tempus::SolutionHistory<Scalar>> 
 Piro::TempusIntegrator<Scalar>::getSolutionHistory() const
 {
-  //IKT FIXME - fill in!
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Teuchos::RCP<const Tempus::SolutionHistory<Scalar>> soln_history; 
+  if (basicIntegrator_ != Teuchos::null) {
+    soln_history = basicIntegrator_->getSolutionHistory(); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    soln_history = fwdSensIntegrator_->getSolutionHistory(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    soln_history = adjSensIntegrator_->getSolutionHistory(); 
+  }
+  return soln_history; 
 }
 
 template <typename Scalar>
 Teuchos::RCP<const Tempus::TimeStepControl<Scalar>> 
 Piro::TempusIntegrator<Scalar>::getTimeStepControl() const
 {
-  //IKT FIXME - fill in!
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Teuchos::RCP<const Tempus::TimeStepControl<Scalar>> ts_control; 
+  if (basicIntegrator_ != Teuchos::null) {
+    ts_control = basicIntegrator_->getTimeStepControl(); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    ts_control = fwdSensIntegrator_->getTimeStepControl(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    ts_control = adjSensIntegrator_->getTimeStepControl(); 
+  }
+  return ts_control; 
 }
 
 template <typename Scalar>
 void 
 Piro::TempusIntegrator<Scalar>::clearObservers()
 {
-  //IKT FIXME - fill in! 
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  if (basicIntegrator_ != Teuchos::null) {
+    basicIntegrator_->getObserver()->clearObservers();
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    fwdSensIntegrator_->getObserver()->clearObservers(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    adjSensIntegrator_->getObserver()->clearObservers(); 
+  }
 }
 
 template <typename Scalar>
 void 
 Piro::TempusIntegrator<Scalar>::setObserver(Teuchos::RCP<Tempus::IntegratorObserver<Scalar>> obs)
 {
-  //IKT FIXME - fill in!
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  if (basicIntegrator_ != Teuchos::null) {
+    basicIntegrator_->setObserver(obs); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    fwdSensIntegrator_->setObserver(obs); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    adjSensIntegrator_->setObserver(obs); 
+  }
 }
 
 template <typename Scalar>
 void 
 Piro::TempusIntegrator<Scalar>::initialize()
 {
-  //IKT FIXME - fill in!
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  if (basicIntegrator_ != Teuchos::null) {
+    basicIntegrator_->initialize();
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    fwdSensIntegrator_->initialize(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    adjSensIntegrator_->initialize(); 
+  }
 }
 
 template <typename Scalar>
@@ -158,12 +269,37 @@ Piro::TempusIntegrator<Scalar>::initializeSolutionHistory(Scalar t0,
     Teuchos::RCP< const Thyra::MultiVectorBase< Scalar > > DxdotDp0,
     Teuchos::RCP< const Thyra::MultiVectorBase< Scalar > > DxdotdotDp0)
 {
-  //IKT FIXME - fill in!
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  //IKT, Question: do forward and adjoint integrators need DxDp0, DxdotDp0, DxdotdotDp0??
+  if (basicIntegrator_ != Teuchos::null) {
+    basicIntegrator_->initializeSolutionHistory(t0, x0, xdot0, xdotdot0, DxDp0, DxdotDp0, DxdotdotDp0); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    fwdSensIntegrator_->initializeSolutionHistory(t0, x0, xdot0, xdotdot0, DxDp0, DxdotDp0, DxdotdotDp0);
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    adjSensIntegrator_->initializeSolutionHistory(t0, x0, xdot0, xdotdot0, DxDp0, DxdotDp0, DxdotdotDp0);
+  }
 }
 
 template <typename Scalar>
 Tempus::Status 
 Piro::TempusIntegrator<Scalar>::getStatus() const
 {
-  //IKT FIXME - fill in!
+#ifdef DEBUG_OUTPUT
+  *out_ << "DEBUG: " << __PRETTY_FUNCTION__ << "\n";
+#endif
+  Tempus::Status status; 
+  if (basicIntegrator_ != Teuchos::null) {
+    status = basicIntegrator_->getStatus(); 
+  }
+  if (fwdSensIntegrator_ != Teuchos::null) {
+    status = fwdSensIntegrator_->getStatus(); 
+  }
+  if (adjSensIntegrator_ != Teuchos::null) {
+    status = adjSensIntegrator_->getStatus(); 
+  }
+  return status; 
 }
