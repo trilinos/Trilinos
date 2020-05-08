6c0b41ec7c579068dcf1b65ba5903c87ef8aa58a
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-05-08 16:52:48 -0600

    ATDM: ats1: Don't run tests and don't build tests for 'dbg' builds (ATDV-183)
    Avoids problems with running tests for now until we can figure out the
    problem.  But submit zero tests to CDash so that they will not be flagged as
    msising.
    
    Also don't build the tests for the 'dbg' builds because those are very
    expensive builds.

--- cmake/ctest/drivers/atdm/ats1/local-driver.sh
+++ cmake/ctest/drivers/atdm/ats1/local-driver.sh
@@ -11,6 +11,19 @@ fi
 # Load environment on the login node
 source $WORKSPACE/Trilinos/cmake/std/atdm/load-env.sh $JOB_NAME
 
+# Don't run any tests for now until we can get them to run faster
+if [[ "${Trilinos_SKIP_CTEST_ADD_TEST}" == "" ]] ; then
+  export Trilinos_SKIP_CTEST_ADD_TEST=TRUE
+fi
+
+if [[ "${Trilinos_INNER_ENABLE_TESTS}" == "" ]] && \
+   [[ "${ATDM_CONFIG_BUILD_TYPE}" == "DEBUG" ]] \
+  ; then
+  # For debug builds, not build the test and example executables to speed
+  # things up for now.
+  export Trilinos_INNER_ENABLE_TESTS=OFF
+fi
+
 # Run configure, build on a haswell compute node
 atdm_config_sbatch_extra_args="$ATDM_CONFIG_SBATCH_EXTRA_ARGS"
 unset ATDM_CONFIG_SBATCH_EXTRA_ARGS
@@ -21,9 +34,9 @@ atdm_run_script_on_compute_node \
 
 # Run tests on either a KNL or HSW compute node
 # Per feedback in ATDV-347, disable running tests for now.
-#export ATDM_CONFIG_SBATCH_EXTRA_ARGS="$atdm_config_sbatch_extra_args"
-#echo "ATDM_CONFIG_SBATCH_EXTRA_ARGS = '${ATDM_CONFIG_SBATCH_EXTRA_ARGS}'"
-#atdm_run_script_on_compute_node \
-#  $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ats1/local-driver-test-on-allocation.sh \
-#  $PWD/ctest-s-driver.out \
-#  ${SBATCH_TEST_TIME_LIMIT_MINUTES} 
+export ATDM_CONFIG_SBATCH_EXTRA_ARGS="$atdm_config_sbatch_extra_args"
+echo "ATDM_CONFIG_SBATCH_EXTRA_ARGS = '${ATDM_CONFIG_SBATCH_EXTRA_ARGS}'"
+atdm_run_script_on_compute_node \
+  $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ats1/local-driver-test-on-allocation.sh \
+  $PWD/ctest-s-driver.out \
+  ${SBATCH_TEST_TIME_LIMIT_MINUTES}
