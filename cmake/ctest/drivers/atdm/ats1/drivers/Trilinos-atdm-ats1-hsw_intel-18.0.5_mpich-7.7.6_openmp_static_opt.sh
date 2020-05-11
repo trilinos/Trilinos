828aaae870ef466a8bbed5386bdc1f1f43f168d7
Evan Harvey <eharvey@sandia.gov>
2020-05-11 14:23:08 -0600

    ATDM/ats1: Enable tests for opt builds (ATDV-347)

--- cmake/ctest/drivers/atdm/ats1/drivers/Trilinos-atdm-ats1-hsw_intel-18.0.5_mpich-7.7.6_openmp_static_opt.sh
+++ cmake/ctest/drivers/atdm/ats1/drivers/Trilinos-atdm-ats1-hsw_intel-18.0.5_mpich-7.7.6_openmp_static_opt.sh
@@ -2,4 +2,5 @@
 if [ "${Trilinos_TRACK}" == "" ] ; then
   export Trilinos_TRACK=Experimental
 fi
+export Trilinos_SKIP_CTEST_ADD_TEST=FALSE
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ats1/local-driver.sh
