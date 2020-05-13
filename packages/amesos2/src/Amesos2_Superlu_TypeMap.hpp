762b94b0d56d7196de37a7c0e7a1c92338c73863
micheldemessieres <michel@demessieres.com>
2020-05-13 09:30:12 -0400

    Amesos2: Add copy loops for Kokkos::complex to SuperLU native type
    Added to protect from possible issues converting to the C type.

--- packages/amesos2/src/Amesos2_Superlu_TypeMap.hpp
+++ packages/amesos2/src/Amesos2_Superlu_TypeMap.hpp
@@ -66,6 +66,10 @@
 
 #include "Amesos2_TypeMap.hpp"
 
+/* The SuperLU comples headers file only need to be included if
+   complex has been enabled in Teuchos.  In addition we only need to
+   define the conversion and printing functions if complex has been
+   enabled. */
 namespace SLU {
 
 typedef int int_t;
@@ -74,9 +78,108 @@ extern "C" {
 
 #undef __SUPERLU_SUPERMATRIX
 #include "supermatrix.h"	// for Dtype_t declaration
+
+#ifdef HAVE_TEUCHOS_COMPLEX
+namespace C {
+#undef __SUPERLU_SCOMPLEX
+#undef SCOMPLEX_INCLUDE
+#include "slu_scomplex.h"     // single-precision complex data type definitions
+}
+
+namespace Z {
+#undef __SUPERLU_DCOMPLEX
+#undef DCOMPLEX_INCLUDE
+#include "slu_dcomplex.h"     // double-precision complex data type definitions
+}
+#endif // HAVE_TEUCHOS_COMPLEX
+
 } // end extern "C"
 
 } // end namespace SLU
+
+#ifdef HAVE_TEUCHOS_COMPLEX
+
+/* ==================== Conversion ==================== */
+namespace Teuchos {
+
+/**
+ * \defgroup slu_conversion Conversion definitions for SLU types.
+ *
+ * Define specializations of Teuchos::as<> for the SLU types.
+ * @{
+ */
+
+template <>
+class ValueTypeConversionTraits<SLU::C::complex, Kokkos::complex<float>>
+{
+public:
+  static SLU::C::complex convert( const Kokkos::complex<float> t ) {
+    SLU::C::complex ret;
+    ret.r = t.real();
+    ret.i = t.imag();
+    return( ret );
+  }
+
+  static SLU::C::complex safeConvert( const Kokkos::complex<float> t ) {
+    SLU::C::complex ret;
+    ret.r = t.real();
+    ret.i = t.imag();
+    return( ret );
+  }
+};
+
+template <>
+class ValueTypeConversionTraits<SLU::Z::doublecomplex, Kokkos::complex<double>>
+{
+public:
+  static SLU::Z::doublecomplex convert( const Kokkos::complex<double> t ) {
+    SLU::Z::doublecomplex ret;
+    ret.r = t.real();
+    ret.i = t.imag();
+    return( ret );
+  }
+
+  static SLU::Z::doublecomplex safeConvert( const Kokkos::complex<double> t ) {
+    SLU::Z::doublecomplex ret;
+    ret.r = t.real();
+    ret.i = t.imag();
+    return( ret );
+  }
+};
+
+// Also convert from SLU types
+
+template <>
+class ValueTypeConversionTraits<Kokkos::complex<float>, SLU::C::complex>
+{
+public:
+  static Kokkos::complex<float> convert( const SLU::C::complex t ) {
+    return ( Kokkos::complex<float>( t.r, t.i ) );
+  }
+
+  static Kokkos::complex<float> safeConvert( const SLU::C::complex t ) {
+    return ( Kokkos::complex<float>( t.r, t.i ) );
+  }
+};
+
+template <>
+class ValueTypeConversionTraits<Kokkos::complex<double>, SLU::Z::doublecomplex>
+{
+public:
+  static Kokkos::complex<double> convert( const SLU::Z::doublecomplex t ) {
+    return ( Kokkos::complex<double>( t.r, t.i ) );
+  }
+
+  static Kokkos::complex<double> safeConvert( const SLU::Z::doublecomplex t ) {
+    return ( Kokkos::complex<double>( t.r, t.i ) );
+  }
+};
+
+//@}  End Conversion group
+
+} // end namespace Teuchos
+
+#endif  // HAVE_TEUCHOS_COMPLEX
 
 namespace Amesos2 {
 
@@ -90,6 +193,7 @@ template <>
 struct TypeMap<Superlu,float>
 {
   static SLU::Dtype_t dtype;
+  typedef float convert_type;
   typedef float type;
   typedef float magnitude_type;
 };
@@ -99,6 +203,7 @@ template <>
 struct TypeMap<Superlu,double>
 {
   static SLU::Dtype_t dtype;
+  typedef double convert_type;
   typedef double type;
   typedef double magnitude_type;
 };
@@ -110,6 +215,7 @@ template <>
 struct TypeMap<Superlu,std::complex<float> >
 {
   static SLU::Dtype_t dtype;
+  typedef SLU::C::complex convert_type; // to create array before calling superlu
   typedef Kokkos::complex<float> type;
   typedef float magnitude_type;
 };
@@ -119,6 +225,7 @@ template <>
 struct TypeMap<Superlu,std::complex<double> >
 {
   static SLU::Dtype_t dtype;
+  typedef SLU::Z::doublecomplex convert_type; // to create array before calling superlu
   typedef Kokkos::complex<double> type;
   typedef double magnitude_type;
 };
@@ -128,6 +235,7 @@ template <>
 struct TypeMap<Superlu,Kokkos::complex<float> >
 {
   static SLU::Dtype_t dtype;
+  typedef SLU::C::complex convert_type; // to create array before calling superlu
   typedef Kokkos::complex<float> type;
   typedef float magnitude_type;
 };
@@ -137,6 +245,7 @@ template <>
 struct TypeMap<Superlu,Kokkos::complex<double> >
 {
   static SLU::Dtype_t dtype;
+  typedef SLU::Z::doublecomplex convert_type; // to create array before calling superlu
   typedef Kokkos::complex<double> type;
   typedef double magnitude_type;
 };
