205c03bed4faaec8c254f8e9a5000c4f17fb0f33
Drew P. Kouri <dpkouri@sandia.gov>
2020-05-26 08:43:35 -0600

    Split ReduceLinearConstraint into impl/def files.

--- packages/rol/src/function/ROL_ReduceLinearConstraint.hpp
+++ packages/rol/src/function/ROL_ReduceLinearConstraint.hpp
@@ -59,73 +59,35 @@
 
 namespace ROL {
 
-template <class Real>
+template<typename Real>
 class ReduceLinearConstraint {
 private:
   const Ptr<Constraint<Real>> lcon_;
   const Ptr<Vector<Real>>     x_;
-
-  Ptr<VectorController<Real>>  storage_;
-  Ptr<NullSpaceOperator<Real>> nsop_;
-
-  void feasible(const Ptr<const Vector<Real>> &c) {
-    Real tol = std::sqrt(ROL_EPSILON<Real>());
-    Ptr<Vector<Real>> ran = c->clone();
-    lcon_->value(*ran,*x_,tol);
-    Real cnorm = ran->norm();
-    if ( cnorm > static_cast<Real>(1e-4)*tol ) {
-      RangeSpaceOperator<Real> rsop(lcon_,x_,c);
-      Ptr<Vector<Real>> xzero = x_->clone(); xzero->zero();
-      lcon_->value(*ran,*xzero,tol);
-      ran->scale(static_cast<Real>(-1));
-      nsop_->apply(*xzero,*x_,tol);
-      rsop.apply(*x_,*ran,tol);
-      x_->plus(*xzero);
-      //throw Exception::NotImplemented(">>> ReduceLinearConstraint::feasible : Input x is not feasible!");
-    }
-  }
+  const Ptr<VectorController<Real>>  storage_;
+  const Ptr<NullSpaceOperator<Real>> nsop_;
 
 public:
   virtual ~ReduceLinearConstraint(void) {}
 
   ReduceLinearConstraint(const Ptr<Constraint<Real>>   &lcon,
                          const Ptr<Vector<Real>>       &x,
-                         const Ptr<const Vector<Real>> &c)
-    : lcon_(lcon), x_(x) {
-    nsop_ = makePtr<NullSpaceOperator<Real>>(lcon,x_,c);
-    feasible(c);
-    storage_ = makePtr<VectorController<Real>>();
-  }
+                         const Ptr<const Vector<Real>> &c);
 
-  const Ptr<Objective<Real>> transform(const Ptr<Objective<Real>> &obj) const {
-    return makePtr<AffineTransformObjective<Real>>(obj,nsop_,x_,storage_);
-  }
+  Ptr<Objective<Real>> transform(const Ptr<Objective<Real>> &obj) const;
+  Ptr<Constraint<Real>> transform(const Ptr<Constraint<Real>> &con) const;
+  Ptr<Constraint<Real>> getLinearConstraint(void) const;
+  Ptr<const Vector<Real>> getFeasibleVector(void) const;
+  void project(Vector<Real> &x, const Vector<Real> &y) const;
+  void project(const Ptr<Vector<Real>> &x, const Ptr<const Vector<Real>> &y) const;
 
-  const Ptr<Constraint<Real>> transform(const Ptr<Constraint<Real>> &con) const {
-    return makePtr<AffineTransformConstraint<Real>>(con,nsop_,x_,storage_);
-  }
-
-  const Ptr<Constraint<Real>> getLinearConstraint(void) const {
-    return lcon_;
-  }
-
-  const Ptr<Vector<Real>> getFeasibleVector(void) const {
-    return x_;
-  }
-
-  void project(Vector<Real>       &x,
-               const Vector<Real> &y) const {
-    Real tol = std::sqrt(ROL_EPSILON<Real>());
-    nsop_->apply(x,y,tol);
-  }
-
-  void project(const Ptr<Vector<Real>>       &x,
-               const Ptr<const Vector<Real>> &y) const {
-    project(*x,*y);
-  }
+private:
+  void feasible(const Ptr<const Vector<Real>> &c);
 
 }; // class ReduceLinearConstraint
 
 } // namespace ROL
+
+#include "ROL_ReduceLinearConstraint_Def.hpp"
 
 #endif
