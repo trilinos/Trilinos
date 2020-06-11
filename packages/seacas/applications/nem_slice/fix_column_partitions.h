56e9688c130fc482eb3c58914efc1069a085b58e
gsjaardema <gsjaardema@gmail.com>
2020-06-11 08:27:15 -0600

    Update copyright -- shorten in code and point to complete version

--- packages/seacas/applications/nem_slice/fix_column_partitions.h
+++ packages/seacas/applications/nem_slice/fix_column_partitions.h
@@ -1,4 +1,10 @@
-/* Copyright 2017 Los Alamos-2017 National Security, LLC */
+/*
+ * Copyright(C) 1999-2020 National Technology & Engineering Solutions
+ * of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
+ * NTESS, the U.S. Government retains certain rights in this software.
+ * 
+ * See packages/seacas/LICENSE for details
+ */
 
 #ifndef _ELB_FIX_COLUMN_PARTITIONS_H
 #define _ELB_FIX_COLUMN_PARTITIONS_H
