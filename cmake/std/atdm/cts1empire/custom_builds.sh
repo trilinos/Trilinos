034e18ae2f576719d3804d6ad201f508495d69c1
Evan Harvey <eharvey@sandia.gov>
2020-06-18 13:49:14 -0600

    ATDM/cts1empire: Fix custom_builds and add unit tests

--- cmake/std/atdm/cts1empire/custom_builds.sh
+++ cmake/std/atdm/cts1empire/custom_builds.sh
@@ -7,11 +7,12 @@
 
 # Custom compiler selection logic
 
-if [[ $ATDM_CONFIG_BUILD_NAME == *"intel-18.0.2-openmpi-4.0.1"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"intel-18.0.2_openmpi-4.0.1"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"intel-18.0.2"* ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"intel" ]] \
-  || [[ $ATDM_CONFIG_BUILD_NAME == *"default" ]] \
+if atdm_match_any_buildname_keyword \
+    intel-18.0.2-openmpi-4.0.1 \
+    intel-18.0.2_openmpi-4.0.1 \
+    intel-18.0.2 \
+    intel \
+    default \
   ; then
   export ATDM_CONFIG_COMPILER=INTEL-18.0.2_OPENMPI-4.0.1
 else
