d11ccea5e9171d58406e6dcc0431109e4d9377fe
mperego <mperego@sandia.gov>
2020-06-30 16:50:10 -0600

    Piro/ROL: Adding and testing Hessian support to thyra adapters (#7534)
    * Piro: modified gradient checks
          added Hessian checks
    
    * Piro/ROL: Thyra adapter, finite difference Hessian
         implemented second-order FD Hessian
    
    * Piro: modified Tpetra Analysis test A
          added Hessian derivatives
          modified test to make constraint Hessian non trivial.
    
    * ROL: Thyra adapters, added Hessian implementation
    
    * Piro: Using ROL Primal vector space
          for testing purposes
    
    * Piro: use the setHessianSupports function
    
    * Piro: remove double spaces
    
    * ROL: update the Doxygen to be consistent with the function arguments.
    
    * Piro: update Hessian related names
    
    * ROL: update Hessian related names
    
    * ROL: use finite diff. if derivatives are not supported
    
    * Piro: resolves a compatibility issue when Np > 1
    
    * Piro: resolves a compatibility issue when Np > 1
    
    Co-authored-by: kliegeois <kimliegeois@ymail.com>

--- packages/rol/adapters/thyra/src/function/ROL_ThyraProductME_Objective_SimOpt.hpp
+++ packages/rol/adapters/thyra/src/function/ROL_ThyraProductME_Objective_SimOpt.hpp
@@ -60,9 +60,9 @@ public:
 
   ThyraProductME_Objective_SimOpt(Thyra::ModelEvaluatorDefaultBase<double>& thyra_model_, int g_index_, const std::vector<int>& p_indices_,
       Teuchos::RCP<Teuchos::ParameterList> params_ = Teuchos::null, Teuchos::EVerbosityLevel verbLevel= Teuchos::VERB_HIGH) :
-    thyra_model(thyra_model_), g_index(g_index_), p_indices(p_indices_), params(params_),
-    out(Teuchos::VerboseObjectBase::getDefaultOStream()),
-    verbosityLevel(verbLevel) {
+        thyra_model(thyra_model_), g_index(g_index_), p_indices(p_indices_), params(params_),
+        out(Teuchos::VerboseObjectBase::getDefaultOStream()),
+        verbosityLevel(verbLevel) {
     computeValue = computeGradient1 = computeGradient2 = true;
     value_ = 0;
     rol_u_ptr = rol_z_ptr = Teuchos::null;
@@ -169,7 +169,7 @@ public:
 
     if(computeValue) {
       if(verbosityLevel >= Teuchos::VERB_HIGH)
-      *out << "ROL::ThyraProductME_Objective_SimOpt::gradient_1, Computing Value" << std::endl;
+        *out << "ROL::ThyraProductME_Objective_SimOpt::gradient_1, Computing Value" << std::endl;
       value_ = ::Thyra::get_ele(*thyra_g,0);
       computeValue = false;
     }
@@ -252,6 +252,322 @@ public:
     grad2_ptr_->set(g);
 
     computeGradient2 = false;
+  }
+
+
+  void hessVec_11( Vector<Real> &hv, const Vector<Real> &v,
+      const Vector<Real> &u,  const Vector<Real> &z, Real &/*tol*/ ) {
+
+#ifdef  HAVE_ROL_DEBUG
+    //u and z should be updated in the update functions before calling this function
+    TEUCHOS_ASSERT(!u_hasChanged(u));
+    TEUCHOS_ASSERT(!z_hasChanged(z));
+#endif
+
+    if(verbosityLevel >= Teuchos::VERB_MEDIUM)
+      *out << "ROL::ThyraProductME_Objective_SimOpt::hessVec_11" << std::endl;
+
+    Thyra::ModelEvaluatorBase::OutArgs<Real> outArgs = thyra_model.createOutArgs();
+    bool supports_deriv = outArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_hess_vec_prod_g_xx, g_index);
+
+    if(supports_deriv) { //use derivatives computed by model evaluator
+
+      const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
+
+      Teuchos::RCP<const  Thyra::ProductVectorBase<Real> > thyra_prodvec_p = Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<Real>>(thyra_p.getVector());
+      ThyraVector<Real>  & thyra_hv = dynamic_cast<ThyraVector<Real>&>(hv);
+
+      Thyra::ModelEvaluatorBase::InArgs<Real> inArgs = thyra_model.createInArgs();
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        inArgs.set_p(p_indices[i], thyra_prodvec_p->getVectorBlock(i));
+      }
+      inArgs.set_x(thyra_x.getVector());
+      inArgs.set_x_direction(thyra_v.getVector());
+
+      Teuchos::RCP< Thyra::VectorBase<Real> > multiplier_g = Thyra::createMember<Real>(thyra_model.get_g_multiplier_space(g_index));
+      Thyra::put_scalar(1.0, multiplier_g.ptr());
+      inArgs.set_g_multiplier(g_index, multiplier_g);
+
+      ROL_TEST_FOR_EXCEPTION( !supports_deriv, std::logic_error, "ROL::ThyraProductME_Objective: H_xx product vector is not supported");
+      outArgs.set_hess_vec_prod_g_xx(g_index, thyra_hv.getVector());
+
+      thyra_model.evalModel(inArgs, outArgs);
+
+    } else { //compute derivatives with 2nd-order finite differences
+
+      Real gtol = std::sqrt(ROL_EPSILON<Real>());
+      // Compute step length
+      Real h = std::cbrt(ROL_EPSILON<Real>());
+      if (v.norm() > h) {
+        h *= std::max(1.0,u.norm()/v.norm());
+      }
+      // Evaluate gradient of first component at (u+hv,z)
+      ROL::Ptr<Vector<Real> > unew = u.clone();
+      unew->set(u);
+      unew->axpy(h,v);
+      this->update(*unew,z);
+      hv.zero();
+      this->gradient_1(hv,*unew,z,gtol);
+      // Evaluate gradient of first component at (u-hv,z)
+      ROL::Ptr<Vector<Real> > g = hv.clone();
+      unew->axpy(-2.0*h,v);
+      this->update(*unew,z);
+      this->gradient_1(*g,*unew,z,gtol);
+      // Compute Newton quotient
+      hv.axpy(-1.0,*g);
+      hv.scale(0.5/h);
+    }
+  }
+
+  void hessVec_12( Vector<Real> &hv, const Vector<Real> &v,
+      const Vector<Real> &u, const Vector<Real> &z, Real &/*tol*/ ) {
+
+#ifdef  HAVE_ROL_DEBUG
+    //u and z should be updated in the update functions before calling this function
+    TEUCHOS_ASSERT(!u_hasChanged(u));
+    TEUCHOS_ASSERT(!z_hasChanged(z));
+#endif
+
+    if(verbosityLevel >= Teuchos::VERB_MEDIUM)
+      *out << "ROL::ThyraProductME_Objective_SimOpt::hessVec_12" << std::endl;
+
+    Thyra::ModelEvaluatorBase::OutArgs<Real> outArgs = thyra_model.createOutArgs();
+    bool supports_deriv = true;
+    for(std::size_t j=0; j<p_indices.size(); ++j)
+      supports_deriv =  supports_deriv && outArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_hess_vec_prod_g_xp, g_index, p_indices[j]);
+
+    if(supports_deriv) { //use derivatives computed by model evaluator
+      const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
+
+      Teuchos::RCP<const  Thyra::ProductVectorBase<Real> > thyra_prodvec_p = Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<Real>>(thyra_p.getVector());
+      Teuchos::RCP<const  Thyra::ProductVectorBase<Real> > thyra_prodvec_v = Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<Real>>(thyra_v.getVector());
+      ThyraVector<Real>  & thyra_hv = dynamic_cast<ThyraVector<Real>&>(hv);
+
+      Thyra::ModelEvaluatorBase::InArgs<Real> inArgs = thyra_model.createInArgs();
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        inArgs.set_p(p_indices[i], thyra_prodvec_p->getVectorBlock(i));
+        inArgs.set_p_direction(p_indices[i], thyra_prodvec_v->getVectorBlock(i));
+      }
+      inArgs.set_x(thyra_x.getVector());
+
+      Teuchos::RCP< Thyra::VectorBase<Real> > multiplier_g = Thyra::createMember<Real>(thyra_model.get_g_multiplier_space(g_index));
+      Thyra::put_scalar(1.0, multiplier_g.ptr());
+      inArgs.set_g_multiplier(g_index, multiplier_g);
+
+      std::vector<Teuchos::RCP< Thyra::MultiVectorBase<Real> > > hv_vec(p_indices.size());
+
+      hv_vec[0] = thyra_hv.getVector();
+      for(std::size_t j=1; j<p_indices.size(); ++j) {
+        hv_vec[j] = thyra_hv.getVector()->clone_v();
+      }
+
+      for(std::size_t j=0; j<p_indices.size(); ++j) {
+        bool supports_deriv =   outArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_hess_vec_prod_g_xp, g_index, p_indices[j]);
+        ROL_TEST_FOR_EXCEPTION( !supports_deriv, std::logic_error, "ROL::ThyraProductME_Objective_SimOpt: H_xp product vector is not supported");
+        outArgs.set_hess_vec_prod_g_xp(g_index,p_indices[j], hv_vec[j]);
+      }
+      thyra_model.evalModel(inArgs, outArgs);
+
+      for(std::size_t j=1; j<p_indices.size(); ++j)
+        hv_vec[0]->update(1.0, *hv_vec[j]);
+
+    } else { //compute derivatives with 2nd-order finite differences
+
+      Real gtol = std::sqrt(ROL_EPSILON<Real>());
+      // Compute step length
+      Real h = std::cbrt(ROL_EPSILON<Real>());
+      if (v.norm() > h) {
+        h *= std::max(1.0,u.norm()/v.norm());
+      }
+      // Evaluate gradient of first component at (u,z+hv)
+      ROL::Ptr<Vector<Real> > znew = z.clone();
+      znew->set(z);
+      znew->axpy(h,v);
+      this->update(u,*znew);
+      hv.zero();
+      this->gradient_1(hv,u,*znew,gtol);
+      // Evaluate gradient of first component at (u,z-hv)
+      ROL::Ptr<Vector<Real> > g = hv.clone();
+      znew->axpy(-2.0*h,v);
+      this->update(u,*znew);
+      this->gradient_1(*g,u,*znew,gtol);
+      // Compute Newton quotient
+      hv.axpy(-1.0,*g);
+      hv.scale(0.5/h);
+    }
+  }
+
+  void hessVec_21( Vector<Real> &hv, const Vector<Real> &v,
+      const Vector<Real> &u, const Vector<Real> &z, Real &/*tol*/ ) {
+
+#ifdef  HAVE_ROL_DEBUG
+    //u and z should be updated in the update functions before calling this function
+    TEUCHOS_ASSERT(!u_hasChanged(u));
+    TEUCHOS_ASSERT(!z_hasChanged(z));
+#endif
+
+    if(verbosityLevel >= Teuchos::VERB_MEDIUM)
+      *out << "ROL::ThyraProductME_Objective_SimOpt::hessVec_21" << std::endl;
+
+
+    Thyra::ModelEvaluatorBase::OutArgs<Real> outArgs = thyra_model.createOutArgs();
+
+    bool supports_deriv = true;
+    for(std::size_t i=0; i<p_indices.size(); ++i)
+      supports_deriv = supports_deriv &&  outArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_hess_vec_prod_g_px, g_index, p_indices[i]);
+
+
+    if(supports_deriv) { //use derivatives computed by model evaluator
+      const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
+
+      Teuchos::RCP<const  Thyra::ProductVectorBase<Real> > thyra_prodvec_p = Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<Real>>(thyra_p.getVector());
+      ThyraVector<Real>  & thyra_hv = dynamic_cast<ThyraVector<Real>&>(hv);
+
+      Teuchos::RCP< Thyra::ProductMultiVectorBase<Real> > prodvec_hv = Teuchos::rcp_dynamic_cast<Thyra::ProductMultiVectorBase<Real>>(thyra_hv.getVector());
+
+      Thyra::ModelEvaluatorBase::InArgs<Real> inArgs = thyra_model.createInArgs();
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        inArgs.set_p(p_indices[i], thyra_prodvec_p->getVectorBlock(i));
+      }
+      inArgs.set_x(thyra_x.getVector());
+      inArgs.set_x_direction(thyra_v.getVector());
+
+      Teuchos::RCP< Thyra::VectorBase<Real> > multiplier_g = Thyra::createMember<Real>(thyra_model.get_g_multiplier_space(g_index));
+      Thyra::put_scalar(1.0, multiplier_g.ptr());
+      inArgs.set_g_multiplier(g_index, multiplier_g);
+
+      Thyra::ModelEvaluatorBase::OutArgs<Real> outArgs = thyra_model.createOutArgs();
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        bool supports_deriv =   outArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_hess_vec_prod_g_px, g_index, p_indices[i]);
+        ROL_TEST_FOR_EXCEPTION( !supports_deriv, std::logic_error, "ROL::ThyraProductME_Objective_SimOpt: H_px product vector is not supported");
+        outArgs.set_hess_vec_prod_g_px(g_index,p_indices[i], prodvec_hv->getNonconstMultiVectorBlock(i));
+      }
+      thyra_model.evalModel(inArgs, outArgs);
+
+    } else { //compute derivatives with 2nd-order finite differences
+
+      Real gtol = std::sqrt(ROL_EPSILON<Real>());
+      // Compute step length
+      Real h = std::cbrt(ROL_EPSILON<Real>());;
+      if (v.norm() > h) {
+        h *= std::max(1.0,u.norm()/v.norm());
+      }
+      // Evaluate gradient of first component at (u+hv,z)
+      ROL::Ptr<Vector<Real> > unew = u.clone();
+      unew->set(u);
+      unew->axpy(h,v);
+      this->update(*unew,z);
+      hv.zero();
+      this->gradient_2(hv,*unew,z,gtol);
+      // Evaluate gradient of first component at (u-hv,z)
+      ROL::Ptr<Vector<Real> > g = hv.clone();
+      unew->axpy(-2.0*h,v);
+      this->update(*unew,z);
+      this->gradient_2(*g,*unew,z,gtol);
+      // Compute Newton quotient
+      hv.axpy(-1.0,*g);
+      hv.scale(0.5/h);
+    }
+  }
+
+  void hessVec_22( Vector<Real> &hv, const Vector<Real> &v,
+      const Vector<Real> &u,  const Vector<Real> &z, Real &/*tol*/ ) {
+
+#ifdef  HAVE_ROL_DEBUG
+    //u and z should be updated in the update functions before calling this function
+    TEUCHOS_ASSERT(!u_hasChanged(u));
+    TEUCHOS_ASSERT(!z_hasChanged(z));
+#endif
+
+    if(verbosityLevel >= Teuchos::VERB_MEDIUM)
+      *out << "ROL::ThyraProductME_Objective_SimOpt::hessVec_22" << std::endl;
+
+    Thyra::ModelEvaluatorBase::OutArgs<Real> outArgs = thyra_model.createOutArgs();
+    bool supports_deriv = true;
+    for(std::size_t i=0; i<p_indices.size(); ++i)
+      for(std::size_t j=0; j<p_indices.size(); ++j)
+        supports_deriv =  supports_deriv && outArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_hess_vec_prod_g_pp, g_index, p_indices[i], p_indices[j]);
+
+    if(supports_deriv) { //use derivatives computed by model evaluator
+      const ThyraVector<Real>  & thyra_p = dynamic_cast<const ThyraVector<Real>&>(z);
+      const ThyraVector<Real>  & thyra_x = dynamic_cast<const ThyraVector<Real>&>(u);
+      const ThyraVector<Real>  & thyra_v = dynamic_cast<const ThyraVector<Real>&>(v);
+
+      Teuchos::RCP<const  Thyra::ProductVectorBase<Real> > thyra_prodvec_p = Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<Real>>(thyra_p.getVector());
+      Teuchos::RCP<const  Thyra::ProductVectorBase<Real> > thyra_prodvec_v = Teuchos::rcp_dynamic_cast<const Thyra::ProductVectorBase<Real>>(thyra_v.getVector());
+      ThyraVector<Real>  & thyra_hv = dynamic_cast<ThyraVector<Real>&>(hv);
+
+      Teuchos::RCP< Thyra::ProductMultiVectorBase<Real> > prodvec_hv = Teuchos::rcp_dynamic_cast<Thyra::ProductMultiVectorBase<Real>>(thyra_hv.getVector());
+
+      Thyra::ModelEvaluatorBase::InArgs<Real> inArgs = thyra_model.createInArgs();
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        inArgs.set_p(p_indices[i], thyra_prodvec_p->getVectorBlock(i));
+        inArgs.set_p_direction(p_indices[i], thyra_prodvec_v->getVectorBlock(i));
+      }
+      inArgs.set_x(thyra_x.getVector());
+      Teuchos::RCP< Thyra::VectorBase<Real> > multiplier_g = Thyra::createMember<Real>(thyra_model.get_g_multiplier_space(g_index));
+      Thyra::put_scalar(1.0, multiplier_g.ptr());
+      inArgs.set_g_multiplier(g_index, multiplier_g);
+
+      std::vector<std::vector<Teuchos::RCP< Thyra::MultiVectorBase<Real> > > > hv_vec(p_indices.size());
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        hv_vec[i].resize(p_indices.size());
+        hv_vec[i][0] = prodvec_hv->getNonconstMultiVectorBlock(i);
+        for(std::size_t j=1; j<p_indices.size(); ++j) {
+          hv_vec[i][j] = hv_vec[i][0]->clone_mv();
+        }
+      }
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        for(std::size_t j=0; j<p_indices.size(); ++j) {
+          bool supports_deriv =   outArgs.supports(Thyra::ModelEvaluatorBase::OUT_ARG_hess_vec_prod_g_pp, g_index, p_indices[i], p_indices[j]);
+          ROL_TEST_FOR_EXCEPTION( !supports_deriv, std::logic_error, "ROL::ThyraProductME_Objective_SimOpt: H_pp product vector is not supported");
+
+          outArgs.set_hess_vec_prod_g_pp(g_index,p_indices[i], p_indices[j], hv_vec[i][j]);
+        }
+      }
+      thyra_model.evalModel(inArgs, outArgs);
+
+      for(std::size_t i=0; i<p_indices.size(); ++i) {
+        for(std::size_t j=1; j<p_indices.size(); ++j)
+          hv_vec[i][0]->update(1.0, *hv_vec[i][j]);
+      }
+    } else { //compute derivatives with 2nd-order finite differences
+
+      Real gtol = std::sqrt(ROL_EPSILON<Real>());
+      // Compute step length
+      Real h = std::cbrt(ROL_EPSILON<Real>());
+      if (v.norm() > h) {
+        h *= std::max(1.0,u.norm()/v.norm());
+      }
+      // Evaluate gradient of first component at (u,z+hv)
+      ROL::Ptr<Vector<Real> > znew = z.clone();
+      znew->set(z);
+      znew->axpy(h,v);
+      update(u,*znew);
+      hv.zero();
+      gradient_2(hv,u,*znew,gtol);
+      // Evaluate gradient of first component at (u,z-hv)
+      ROL::Ptr<Vector<Real> > g = hv.clone();
+      znew->axpy(-2.0*h,v);
+      update(u,*znew);
+      gradient_2(*g,u,*znew,gtol);
+      // Compute Newton quotient
+      hv.axpy(-1.0,*g);
+      hv.scale(0.5/h);
+    }
   }
 
   void update( const Vector<Real> &u, const Vector<Real> &z, bool /*flag*/ = true, int iter = -1) {
