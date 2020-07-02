4670cfd27443411dc0dd45d35f8ce273785da0e0
Evan Harvey <eharvey@sandia.gov>
2020-07-02 09:52:51 -0600

    ATDM/cee-rhel6: Restore old local-driver.sh (ATDV-272)

--- cmake/ctest/drivers/atdm/cee-rhel6/local-driver.sh
+++ cmake/ctest/drivers/atdm/cee-rhel6/local-driver.sh
@@ -1,18 +1,7 @@
 #!/bin/bash -l
 
-set +x
-
-source $WORKSPACE/Trilinos/cmake/std/atdm/load-env.sh $JOB_NAME
-
-if [[ "${Trilinos_SKIP_CTEST_ADD_TEST}" == "" ]] && \
-   [[ "${ATDM_CONFIG_USE_CUDA}" == "ON" ]] \
-  ; then
-  # For CUDA builds, do not run the the test and example executables to speed
-  # things up and to allow us to build and install on CEE machines that don't
-  # actaully have a GPU!
-  export Trilinos_SKIP_CTEST_ADD_TEST=ON
-  # NOTE: If building the tests and examples is too expensive, we could set
-  # Trilinos_INNER_ENABLE_TESTS=OFF.
+if [ "${Trilinos_CTEST_DO_ALL_AT_ONCE}" == "" ] ; then
+  export Trilinos_CTEST_DO_ALL_AT_ONCE=TRUE
 fi
 
 set -x
