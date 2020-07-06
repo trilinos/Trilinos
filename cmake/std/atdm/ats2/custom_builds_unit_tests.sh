7c24a8552173622dda8eac0a8ba245aef055cf75
Evan Harvey <eharvey@sandia.gov>
2020-07-06 11:32:51 -0600

    ATDM/ats2: Add unit tests for xl environments (CDOFA-92)

--- cmake/std/atdm/ats2/custom_builds_unit_tests.sh
+++ cmake/std/atdm/ats2/custom_builds_unit_tests.sh
@@ -12,11 +12,82 @@ else
   SHUNIT2_DIR=`readlink -f ${ATDM_CONFIG_SCRIPT_DIR}/../../../commonTools/test/shunit2`
 fi
 
+testXl() {
+  ATDM_CONFIG_BUILD_NAME=default-xl
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_xl-2020.03.18-spmpi-rolling_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_xl-2020.03.18_spmpi-rolling_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_xl-2020.03.18_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_xl-2020_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_xl_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  # This should not match anything and should be an error!
+  ATDM_CONFIG_BUILD_NAME=anything-xls-after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} DEFAULT ${ATDM_CONFIG_COMPILER}
+}
+
+#if atdm_match_any_buildname_keyword \
+#  cuda-10.1.243-xl-2020.03.18-spmpi-rolling \
+#  cuda-10.1.243-xl-2020.03.18_spmpi-rolling \
+#  cuda-10.1.243-xl-2020.03.18 \
+#  cuda-10.1.243-xl-2020 \
+#  cuda-xl \
+#  ; then
+#  export ATDM_CONFIG_COMPILER=CUDA-10.1.243-XL-2020.03.18_SPMPI-ROLLING
+#
+testXlCuda() {
+  ATDM_CONFIG_BUILD_NAME=default-cuda-xl
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} CUDA-10.1.243-XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_cuda-10.1.243-xl-2020.03.18-spmpi-rolling_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} CUDA-10.1.243-XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_cuda-10.1.243-xl-2020.03.18_spmpi-rolling_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} CUDA-10.1.243-XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_cuda-10.1.243-xl-2020.03.18_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} CUDA-10.1.243-XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_cuda-10.1.243-xl-2020_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} CUDA-10.1.243-XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  ATDM_CONFIG_BUILD_NAME=before_cuda-xl_after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} CUDA-10.1.243-XL-2020.03.18_SPMPI-ROLLING ${ATDM_CONFIG_COMPILER}
+
+  # This should not match anything and should be an error!
+  ATDM_CONFIG_BUILD_NAME=anything-cudas-xls-after
+  . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
+  ${_ASSERT_EQUALS_} DEFAULT ${ATDM_CONFIG_COMPILER}
+}
+
 #
 # Test compiler parsing
 #
 
-testAll() {
+testGnu() {
 
   ATDM_CONFIG_BUILD_NAME=default
   . ${ATDM_CONFIG_SCRIPT_DIR}/utils/set_build_options.sh
