8f31d1cd2774f8c18dca952795cb6c69c09ae653
Evan Harvey <eharvey@sandia.gov>
2020-07-08 13:03:58 -0600

    ATDM/ats2: Disable building and running tests for xl (CDOFA-92)

--- cmake/ctest/drivers/atdm/ats2/drivers/Trilinos-atdm-ats2-cuda-10.1.243-xl-2020.03.18_spmpi-rolling_static_opt.sh
+++ cmake/ctest/drivers/atdm/ats2/drivers/Trilinos-atdm-ats2-cuda-10.1.243-xl-2020.03.18_spmpi-rolling_static_opt.sh
@@ -2,4 +2,15 @@
 if [ "${Trilinos_TRACK}" == "" ] ; then
   export Trilinos_TRACK=Experimental
 fi
+
+if [[ "${Trilinos_INNER_ENABLE_TESTS}" == "" ]]; then
+  # For XL, do not build tests and examples
+  export Trilinos_INNER_ENABLE_TESTS=OFF
+fi
+
+if [[ "${Trilinos_SKIP_CTEST_ADD_TEST}" == "" ]] ; then
+  # For XL, do not run tests
+  export Trilinos_SKIP_CTEST_ADD_TEST=TRUE
+fi
+
 $WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ats2/local-driver.sh
