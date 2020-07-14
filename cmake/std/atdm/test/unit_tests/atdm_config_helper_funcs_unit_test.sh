38d8bf6b06f21db62e4db0ce35911ed8612b9074
Jason M. Gates <jmgate@sandia.gov>
2020-07-14 14:15:26 -0600

    Reset File to Match trilinos/Trilinos:develop
    Not sure how EM-Plasma/Trilinos:develop wound up diffing on this file,
    but we shouldn't be, so resetting it.

--- cmake/std/atdm/test/unit_tests/atdm_config_helper_funcs_unit_test.sh
+++ cmake/std/atdm/test/unit_tests/atdm_config_helper_funcs_unit_test.sh
@@ -14,60 +14,6 @@ fi
 
 source ${ATDM_UTIL_SCRIPT_ATDM_CONFIG_HELPER_FUNCS}
 #
-# Test atdm get_known_system_info script
-#
-test_atdm_get_known_system_info() {
-   # TODO: D.2 needs to be tested by setting these values:
-  SNLSYSTEM=
-  SEMS_PLATFORM=
-  ATDM_SYSTEM_NAME=
-  SNLCLUSTER=
-
-  # Get the good ATDM_KNOWN_SYSTEM_NAMES_LIST
-  ATDM_CONFIG_BUILD_NAME=unit_test
-  ATDM_CONFIG_DISABLE_WARNINGS=ON
-  source ${ATDM_UTIL_SCRIPT_GET_KNOWN_SYSTEM_INFO}
-
-  # Check that all known system names pass
-  for ATDM_CONFIG_BUILD_NAME in ${ATDM_KNOWN_SYSTEM_NAMES_LIST[@]}; do
-    ATDM_CONFIG_GET_KNOW_SYSTEM_INFO_REAL_HOSTNAME_OVERRIDE_FOR_UNIT_TESTING=$ATDM_CONFIG_BUILD_NAME
-    RET=$(source ${ATDM_UTIL_SCRIPT_GET_KNOWN_SYSTEM_INFO})
-    assertEquals "Hostname '$ATDM_CONFIG_BUILD_NAME' matches known ATDM host '$ATDM_CONFIG_BUILD_NAME' and system '$ATDM_CONFIG_BUILD_NAME'" "$RET"
-  done
-
-  # Set the bad ATDM_KNOWN_SYSTEM_NAMES
-  ATDM_KNOWN_SYSTEM_NAMES_LIST=(
-    dne-name1
-    dne-name2
-    dne-name3
-
-  )
-
-  # Check a few bad systems names for failure
-  for ATDM_CONFIG_BUILD_NAME in ${ATDM_KNOWN_SYSTEM_NAMES_LIST[@]}; do
-    ATDM_CONFIG_GET_KNOW_SYSTEM_INFO_REAL_HOSTNAME_OVERRIDE_FOR_UNIT_TESTING=$ATDM_CONFIG_BUILD_NAME
-    source ${ATDM_UTIL_SCRIPT_GET_KNOWN_SYSTEM_INFO}
-    assertEquals "" "$ATDM_SYSTEM_NAME"
-    assertEquals "$ATDM_CONFIG_BUILD_NAME" "$realHostname"
-  done
-
-  # Ensure that cts1empire is the default on cts1
-  ATDM_CONFIG_BUILD_NAME=default
-  ATDM_CONFIG_GET_KNOW_SYSTEM_INFO_REAL_HOSTNAME_OVERRIDE_FOR_UNIT_TESTING=$ATDM_CONFIG_BUILD_NAME
-  SNLSYSTEM=cts1
-  RET=$(source ${ATDM_UTIL_SCRIPT_GET_KNOWN_SYSTEM_INFO})
-  assertEquals "Hostname '$ATDM_CONFIG_BUILD_NAME' matches known ATDM host '$ATDM_CONFIG_BUILD_NAME' and system 'cts1empire'" "$RET"
-  
-  # Ensure that cts1 is still selected when it's in the build name
-  ATDM_CONFIG_BUILD_NAME=cts1-default
-  ATDM_CONFIG_GET_KNOW_SYSTEM_INFO_REAL_HOSTNAME_OVERRIDE_FOR_UNIT_TESTING=$ATDM_CONFIG_BUILD_NAME
-  SNLSYSTEM=cts1
-  RET=$(source ${ATDM_UTIL_SCRIPT_GET_KNOWN_SYSTEM_INFO})
-  assertEquals "Hostname '$ATDM_CONFIG_BUILD_NAME' matches known ATDM host '$ATDM_CONFIG_BUILD_NAME' and system 'cts1'" "$RET"
-}
-
-source ${ATDM_UTIL_SCRIPT_ATDM_CONFIG_HELPER_FUNCS}
-#
 # Test atdm utility functions
 #
 
