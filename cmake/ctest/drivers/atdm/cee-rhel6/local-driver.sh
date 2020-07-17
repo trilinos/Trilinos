88e774242240422c6160268fd671a53b7efb8d19
Roscoe A. Bartlett <rabartl@sandia.gov>
2020-07-17 13:03:01 -0600

    ATDM: Select set of mini packages using 'mini' keyword (ATDV-352)

--- cmake/ctest/drivers/atdm/cee-rhel6/local-driver.sh
+++ cmake/ctest/drivers/atdm/cee-rhel6/local-driver.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -l
 
-if [ "${Trilinos_CTEST_DO_ALL_AT_ONCE}" == "" ] ; then
-  export Trilinos_CTEST_DO_ALL_AT_ONCE=TRUE
+set +x
+
+# Need to load env so we define some vars
+source $WORKSPACE/Trilinos/cmake/std/atdm/load-env.sh $JOB_NAME
+
+# Make adjustments for mini build of Trilinos for SPARC
+if atdm_match_buildname_keyword mini ; then
+  echo "This is a mini build of Trilinos for SPARC!"
+  export ATDM_CONFIG_CONFIGURE_OPTIONS_FILES=cmake/std/atdm/ATDMDevEnv.cmake,cmake/std/atdm/apps/sparc/SPARC_MiniTrilinos_PACKAGES.cmake
 fi
 
 set -x
--- cmake/std/atdm/apps/sparc/SPARC_MiniTrilinos_PACKAGES.cmake
+++ cmake/std/atdm/apps/sparc/SPARC_MiniTrilinos_PACKAGES.cmake
@@ -1,8 +1,4 @@
 # Include this file in the driver that calls TRIBITS_CTEST_DRIVER() to set the
-# list of packages used by SPARC.
+# list of packages used by a mini build of SPARC.
 INCLUDE("${CMAKE_CURRENT_LIST_DIR}/SPARCMiniTrilinosPackagesList.cmake")
 SET(Trilinos_PACKAGES ${SPARC_MiniTrilinos_Packages})
-# NOTE: Above, even though SPARC_Trilinos_Packages may list a bunch of
-# packages that we don't want to test, the file ATDMDisables.cmake has
-# <Package>_ENABLE_TESTS set to OFF for all fo the packages that we don't want
-# to test.
