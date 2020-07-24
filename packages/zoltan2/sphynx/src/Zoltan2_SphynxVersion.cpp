08576e1edfd49a96e34ad131deb7341f1ea3b206
Seher Acer <sacer@sandia.gov>
2020-07-24 13:57:54 -0600

    Typo fix

--- packages/zoltan2/sphynx/src/Zoltan2_SphynxVersion.cpp
+++ packages/zoltan2/sphynx/src/Zoltan2_SphynxVersion.cpp
@@ -6,7 +6,7 @@
 //           Copyright 2020 National Technology & Engineering
 //                  Solutions of Sandia, LLC (NTESS).
 //
-// Under the terms of Contract Contract DE-NA0003525 with NTESS,
+// Under the terms of Contract DE-NA0003525 with NTESS,
 // the U.S. Government retains certain rights in this software.
 //
 // Redistribution and use in source and binary forms, with or without
