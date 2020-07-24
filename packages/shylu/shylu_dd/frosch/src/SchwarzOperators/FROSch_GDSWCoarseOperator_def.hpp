c4b46ce5c68eda5866e9aa8b2a3f317ec92b9a46
Friederike Röver <Friederike.Roever@math.tu-freiberg.de>
2020-07-24 16:29:57 +0200

    Check ZoltanDual

--- packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_GDSWCoarseOperator_def.hpp
+++ packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_GDSWCoarseOperator_def.hpp
@@ -140,7 +140,7 @@ namespace FROSch {
                                                     GOVecPtr dirichletBoundaryDofs,
                                                     ConstXMultiVectorPtr nodeList)
     {
-        FROSCH_TIMER_START_LEVELID(initializeTime,"GDSWCoarseOperator::initialize");
+        FROSCH_ASSERT(!this->DistributionList_->get("Type","linear").compare("ZoltanDual"),"GDSWCoarseOperator:: Distribution Type ZoltanDual only works for IPOUHarmonicCoarseOperator");
         buildCoarseSpace(dimension,dofsPerNode,repeatedNodesMap,repeatedDofMaps,dirichletBoundaryDofs,nodeList);
         this->assembleInterfaceCoarseSpace();
         this->buildCoarseSolveMap(this->AssembledInterfaceCoarseSpace_->getBasisMapUnique());
@@ -319,6 +319,7 @@ namespace FROSch {
         FROSCH_TIMER_START_LEVELID(resetCoarseSpaceBlockTime,"GDSWCoarseOperator::resetCoarseSpaceBlock");
         FROSCH_ASSERT(dofsMaps.size()==dofsPerNode,"dofsMaps.size()!=dofsPerNode");
         FROSCH_ASSERT(blockId<this->NumberOfBlocks_,"Block does not exist yet and can therefore not be reset.");
+        FROSCH_ASSERT(this->DistributionList_->get("Type","linear").compare("ZoltanDual"),"GDSWCoarseOperator:: Distribution Type ZoltanDual only works for IPOUHarmonicCoarseOperator");
 
         if (this->Verbose_) {
             cout << "\n\
--- packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_RGDSWCoarseOperator_def.hpp
+++ packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_RGDSWCoarseOperator_def.hpp
@@ -71,6 +71,7 @@ namespace FROSch {
         FROSCH_TIMER_START_LEVELID(resetCoarseSpaceBlockTime,"RGDSWCoarseOperator::resetCoarseSpaceBlock");
         FROSCH_ASSERT(dofsMaps.size()==dofsPerNode,"dofsMaps.size()!=dofsPerNode");
         FROSCH_ASSERT(blockId<this->NumberOfBlocks_,"Block does not exist yet and can therefore not be reset.");
+        FROSCH_ASSERT(this->DistributionList_->get("Type","linear").compare("ZoltanDual"),"GDSWCoarseOperator:: Distribution Type ZoltanDual only works for IPOUHarmonicCoarseOperator");
 
         if (this->Verbose_) {
             cout << "\n\
