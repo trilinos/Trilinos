16b1c82e2a1398bdd4a1e3a9807c0cb6fab2b424
micheldemessieres <michel@demessieres.com>
2020-08-11 13:25:50 -0400

    Sacado: Fence review

--- packages/sacado/src/KokkosExp_View_Fad_Contiguous.hpp
+++ packages/sacado/src/KokkosExp_View_Fad_Contiguous.hpp
@@ -374,7 +374,6 @@ struct SacadoViewFill<
       team_policy policy( (output.extent(0)+team_size-1)/team_size ,
                           team_size , stride );
       Kokkos::parallel_for( policy, *this );
-      execution_space().fence();
     }
 };
 #endif
