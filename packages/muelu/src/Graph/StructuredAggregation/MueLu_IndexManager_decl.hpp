d2ef204762a3440e96960987f47382eafdfbc39a
Luc Berger-Vergiat <lberge@sandia.gov>
2020-08-19 09:49:46 -0600

    MueLu: more modifications in StructuredAggregation for singleCoarsePoint
    This time focusing on modifying StructuredAggregation
    and AggregtionStructured classes to implement the single
    coarse point algorithm.
    Implemented two more unit-tests to check that both aggregates
    and CrsGraph code path are implementing the new logic correctly
    Next step will be to create a full test to check multi-level behavior.

--- packages/muelu/src/Graph/StructuredAggregation/MueLu_IndexManager_decl.hpp
+++ packages/muelu/src/Graph/StructuredAggregation/MueLu_IndexManager_decl.hpp
@@ -88,6 +88,7 @@ namespace MueLu {
 
     const RCP<const Teuchos::Comm<int> > comm_; ///< Communicator used by uncoupled aggregation
     const bool coupled_;                ///< Flag for coupled vs uncoupled aggregation mode, if true aggregation is coupled.
+    const bool singleCoarsePoint_;      ///< Flag telling us if can reduce dimensions to a single layer.
     const int numDimensions;            ///< Number of spacial dimensions in the problem
     const int interpolationOrder_;      ///< Interpolation order used by grid transfer operators using these aggregates.
 
@@ -129,7 +130,8 @@ namespace MueLu {
 
     IndexManager() = default;
 
-    IndexManager(const RCP<const Teuchos::Comm<int> > comm, const bool coupled, const int NumDimensions,
+    IndexManager(const RCP<const Teuchos::Comm<int> > comm, const bool coupled,
+                 const bool singleCoarsePoint, const int NumDimensions,
                  const int interpolationOrder, const Array<GO> GFineNodesPerDir,
                  const Array<LO> LFineNodesPerDir);
 
@@ -137,7 +139,7 @@ namespace MueLu {
 
     //! Sets basic parameters used to compute indices on the mesh.
     //! This method requires you to have set this->coarseRate and this->startIndices.
-    void computeMeshParameters(bool coarseSinglePoint = false);
+    void computeMeshParameters();
 
     virtual void computeGlobalCoarseParameters() = 0;
 
@@ -151,6 +153,8 @@ namespace MueLu {
                                     Array<GO>& coarseNodeFineGIDs) const = 0;
 
     bool isAggregationCoupled() const {return coupled_;}
+
+    bool isSingleCoarsePoint() const {return singleCoarsePoint_;}
 
     int getNumDimensions() const {return numDimensions;}
 
