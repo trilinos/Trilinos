d14dda8d24cdedb2e167d8aff958a81eebc724e3
William McLendon <wcmclen@sandia.gov>
2020-08-28 18:08:49 -0600

    Framework: PR Driver work - Debugging

--- cmake/std/PullRequestLinuxDriverMerge.py
+++ cmake/std/PullRequestLinuxDriverMerge.py
@@ -91,6 +91,8 @@ def merge_branch(source_url, source_branch, target_branch, sourceSHA):
     sourceSHA     = sourceSHA.strip()
 
     remote_list = subprocess.check_output(['git', 'remote', '-v'])
+    remote_list = remote_list.decode('utf-8')
+
     if 'source_remote' in remote_list:
         print('git remote exists, removing it', file=sys.stdout)
         subprocess.check_call(['git', 'remote', 'rm', 'source_remote'])
@@ -113,6 +115,7 @@ def merge_branch(source_url, source_branch, target_branch, sourceSHA):
     subprocess.check_call(['git', 'merge', '--no-edit', 'source_remote/' + source_branch]),
 
     actual_source_SHA = subprocess.check_output(['git', 'rev-parse', 'source_remote/' + source_branch])
+    actual_source_SHA = actual_source_SHA.decode('utf-8')
 
     actual_source_SHA = actual_source_SHA.strip()
 
