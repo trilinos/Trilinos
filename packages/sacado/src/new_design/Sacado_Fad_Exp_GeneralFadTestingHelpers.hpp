b17592c9ffdd46b0785f18d719085254a3afb8b3
kliegeois <kimliegeois@ymail.com>
2020-09-03 01:18:29 -0600

    Sacado: Overload the TestRelErr structure for Fad types

--- packages/sacado/src/new_design/Sacado_Fad_Exp_GeneralFadTestingHelpers.hpp
+++ packages/sacado/src/new_design/Sacado_Fad_Exp_GeneralFadTestingHelpers.hpp
@@ -68,22 +68,33 @@ namespace Sacado {
 
     template <typename Storage>
     typename GeneralFad<Storage>::scalar_type relErr( const GeneralFad<Storage> &s1, const GeneralFad<Storage> &s2 ) { return relErrFadImpl(s1.derived(),s2.derived()); }
+  }
+  }
+}
 
-    template<typename T1, typename T2, class Magnitude>
-    bool testRelErr(
+namespace Teuchos {
+  template <typename T1, typename T2>
+  struct TestRelErr<T1,T2,
+                    typename std::enable_if<Sacado::IsExpr<T1>::value ||
+                                            Sacado::IsExpr<T2>::value >::type >
+  {
+    typedef typename Sacado::ScalarType<T1>::type scalarType1;
+    typedef typename Sacado::ScalarType<T2>::type scalarType2;
+    typedef typename std::common_type<scalarType1,scalarType2>::type magnitudeType;
+    static bool eval(
       const std::string &v1_name,
-      const Expr<T1> &v1,
+      const T1 &v1,
       const std::string &v2_name,
-      const Expr<T2> &v2,
+      const T2 &v2,
       const std::string &maxRelErr_error_name,
-      const Magnitude &maxRelErr_error,
+      const magnitudeType &maxRelErr_error,
       const std::string &maxRelErr_warning_name,
-      const Magnitude &maxRelErr_warning,
-      const Teuchos::Ptr<std::ostream> &out
-      )
+      const magnitudeType &maxRelErr_warning,
+      const Ptr<std::ostream> &out
+    )
     {
       using std::endl;
-      typedef Teuchos::ScalarTraits<Magnitude> SMT;
+      typedef Teuchos::ScalarTraits<magnitudeType> SMT;
       const auto rel_err = Teuchos::relErr( v1.derived(), v2.derived() );
       const bool success = ( !SMT::isnaninf(rel_err) && !SMT::isnaninf(maxRelErr_error)
         && rel_err <= maxRelErr_error );
@@ -106,12 +117,7 @@ namespace Sacado {
       }
       return success;
     }
-  }
-  }
-}
-
-namespace Teuchos {
-  using Sacado::Fad::Exp::testRelErr;
+  };
 }
 
 #define TEUCHOS_TEST_FLOATING_NOT_EQUALITY( v1, v2, tol, out, success ) \
