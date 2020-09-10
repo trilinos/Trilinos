15b7317b53f38972a5bfc01effb33e0a861a0740
William McLendon <wcmclen@sandia.gov>
2020-09-10 12:28:10 -0600

    Framework: PR Overhaul Troubleshooting (2020-09-10)

--- cmake/std/pr_python_test_driver.sh
+++ cmake/std/pr_python_test_driver.sh
@@ -2,9 +2,16 @@
 
 echo "================================================================================"
 echo "="
-echo "= P Y T H O N   P A C K A G E   S E T U P"
+echo "=                   P Y T H O N   T E S T   D R I V E R"
 echo "="
 echo "================================================================================"
+echo ""
+echo "----------------------------------------"
+echo "-   P A C K A G E   S E T U P"
+echo "----------------------------------------"
+: "${1:?Param 1 ERROR: Python Executable not set or empty}"
+: "${2:?Param 2 ERROR: Pip Executable not set or empty}"
+
 python_exe=${1:?}
 pip_exe=${2:?}
 
@@ -44,11 +51,17 @@ pip_args=(
 echo -e ">>> ${pip_exe:?} install --user ${pip_args[@]}"
 ${pip_exe:?} install --user ${pip_args[@]}
 
-
+echo -e ""
+echo -e "----------------------------------------"
+echo -e "-   E X E C U T E   T E S T S"
+echo -e "----------------------------------------"
 echo -e ">>> ${python_exe:?} -m pytest --cov=trilinosprhelpers --cov-report term-missing"
 ${python_exe:?} -m pytest --cov=trilinosprhelpers --cov-report term-missing
 
 err=$?
+echo -e ""
+echo -e ">>> STATUS: ${err:?}"
+
 
 # Some Cleanup
 rm get-pip.py >& /dev/null
