897de58e8fca21aa4c8a352335274229036a92d1
Matthias Mayr <matthias.mayr@unibw.de>
2020-11-06 14:59:21 +0100

    MueLu: test helper creates 2x2 saddlepoint matrix
    - implement test helper to create a 2x2 saddle point matrix (based on
    meshtying of a Poisson operator) with a true zero block on the main
    diagonal
    - cover new test helper by unit test (Tpetra only, not working for
    Epetra)

--- packages/muelu/test/unit_tests/TestTestHelpers.cpp
+++ packages/muelu/test/unit_tests/TestTestHelpers.cpp
@@ -46,8 +46,10 @@
 #include <Teuchos_UnitTestHarness.hpp>
 #include <Teuchos_DefaultComm.hpp>
 
-#include "MueLu_TestHelpers.hpp"
-#include "MueLu_Version.hpp"
+#include <MueLu_TestHelpers.hpp>
+#include <MueLu_Version.hpp>
+
+#include <MueLu_Utilities.hpp>
 
 #include <Xpetra_Matrix.hpp>
 #include <Xpetra_StridedMapFactory.hpp>
@@ -87,10 +89,81 @@ namespace MueLuTests {
     }
   }
 
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(TestFactory, CreatePoisson2DMeshtyingMatrix_Obtain2x2SaddlePointSystem, Scalar, LocalOrdinal, GlobalOrdinal, Node)
+  {
+    MUELU_TEST_ONLY_FOR(Xpetra::UseTpetra);
+#   include "MueLu_UseShortNames.hpp"
+    MUELU_TESTING_SET_OSTREAM;
+    MUELU_TESTING_LIMIT_SCOPE(Scalar,GlobalOrdinal,NO);
+
+    out << "version: " << MueLu::Version() << std::endl;
+
+    const Scalar SC_zero = Teuchos::ScalarTraits<Scalar>::zero();
+    const Scalar SC_one = Teuchos::ScalarTraits<Scalar>::one();
+
+    const GO numNodesX = 3;
+    const GO numNodesY = 3;
+    RCP<const Matrix> matrix = TestHelpers::TestFactory<SC, LO, GO, NO>::BuildPoissonSaddlePointMatrix(numNodesX, numNodesY);
+    TEST_ASSERT(!matrix.is_null());
+
+    const size_t numSubDomains = 2;
+    const GO numNodesPrimal = numSubDomains * (numNodesX * numNodesY);
+    const GO numNodesDual = numNodesX; // Interface is oriented along the global x-axis
+
+    RCP<const BlockedCrsMatrix> blockedMatrix = Teuchos::rcp_dynamic_cast<const BlockedCrsMatrix>(matrix);
+    TEST_ASSERT(!blockedMatrix.is_null())
+    TEST_EQUALITY_CONST(blockedMatrix->Rows(), numSubDomains);
+    TEST_EQUALITY_CONST(blockedMatrix->Cols(), numSubDomains);
+
+    TEST_EQUALITY_CONST(blockedMatrix->getMatrix(0, 0)->getGlobalNumRows(), numNodesPrimal);
+    TEST_EQUALITY_CONST(blockedMatrix->getMatrix(0, 1)->getGlobalNumRows(), numNodesPrimal);
+    TEST_EQUALITY_CONST(blockedMatrix->getMatrix(1, 0)->getGlobalNumRows(), numNodesDual);
+
+    // Saddle-point system needs to have a zero block
+    TEST_ASSERT(blockedMatrix->getMatrix(1, 1).is_null());
+
+    TEST_EQUALITY_CONST(blockedMatrix->getGlobalNumRows(), numNodesPrimal + numNodesDual);
+
+    // Kinematic coupling: rows have entries [1, -1] or [-1, 1]
+    RCP<const Matrix> A10 = blockedMatrix->getMatrix(1, 0);
+    TEST_ASSERT(!A10.is_null());
+    for (LocalOrdinal lDualRow = 0; lDualRow < A10->getNodeNumRows(); ++lDualRow)
+    {
+      ArrayView<const LocalOrdinal> cols;
+      ArrayView<const Scalar> vals;
+      A10->getLocalRowView(lDualRow, cols, vals);
+
+      TEST_EQUALITY_CONST(cols.size(), 2);
+      TEST_EQUALITY_CONST(vals.size(), 2);
+      TEST_INEQUALITY_CONST(vals[0], SC_zero);
+      TEST_INEQUALITY_CONST(vals[1], SC_zero);
+      TEST_EQUALITY_CONST(vals[0] * vals[0], SC_one);
+      TEST_EQUALITY_CONST(vals[1] * vals[1], SC_one);
+      TEST_EQUALITY_CONST(vals[0] + vals[1], SC_zero);
+    }
+
+    // Coupling of fluxes: rows have entries [1] or [-1]
+    RCP<const Matrix> A01 = blockedMatrix->getMatrix(0, 1);
+    TEST_ASSERT(!A01.is_null());
+    for (LocalOrdinal lPrimalRow = 0; lPrimalRow < A01->getNodeNumRows(); ++lPrimalRow)
+    {
+      ArrayView<const LocalOrdinal> cols;
+      ArrayView<const Scalar> vals;
+      A01->getLocalRowView(lPrimalRow, cols, vals);
+
+      if (cols.size() > 0)
+      {
+        TEST_EQUALITY_CONST(cols.size(), 1);
+        TEST_EQUALITY_CONST(vals.size(), 1);
+        TEST_EQUALITY_CONST(vals[0] * vals[0], SC_one);
+      }
+    }
+  }
+
 #  define MUELU_ETI_GROUP(SC, LO, GO, Node) \
-      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(TestFactory, CreatePoisson1DMatrix, SC, LO, GO, Node)
+      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(TestFactory, CreatePoisson1DMatrix, SC, LO, GO, Node) \
+      TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(TestFactory, CreatePoisson2DMeshtyingMatrix_Obtain2x2SaddlePointSystem, SC, LO, GO, Node)
 
 #include <MueLu_ETI_4arg.hpp>
+
 }
-
-
