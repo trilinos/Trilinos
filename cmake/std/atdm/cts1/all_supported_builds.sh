87cddadb424503d328a2473f2a503108b0a01195
Henry Swantner <HRSwant@sandia.gov>
2020-11-11 03:13:49 -0700

    Remove Configurations marked as Non Needed

--- cmake/std/atdm/cts1/all_supported_builds.sh
+++ cmake/std/atdm/cts1/all_supported_builds.sh
@@ -4,6 +4,4 @@ export ATDM_CONFIG_CTEST_S_BUILD_NAME_PREFIX=Trilinos-atdm-
 export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=(
   cts1-intel-19.0.4_openmpi-4.0.3_openmp_static_dbg
   cts1-intel-19.0.4_openmpi-4.0.3_openmp_static_opt
-  cts1-intel-18.0.2_openmpi-4.0.3_openmp_static_dbg
-  cts1-intel-18.0.2_openmpi-4.0.3_openmp_static_opt
   )
--- cmake/std/atdm/spack-rhel/all_supported_builds.sh
+++ cmake/std/atdm/spack-rhel/all_supported_builds.sh
@@ -3,17 +3,9 @@
 export ATDM_CONFIG_CTEST_S_BUILD_NAME_PREFIX=Trilinos-atdm-
 
 export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=(
-  spack-rhel-clang-5.0.1-openmpi-1.10.2-openmp-shared-release-debug
-  spack-rhel-clang-5.0.1-openmpi-1.10.2-openmp-static-release-debug
-  spack-rhel-clang-5.0.1-openmpi-1.10.2-serial-shared-release-debug
-  spack-rhel-clang-5.0.1-openmpi-1.10.2-serial-static-release-debug
-  spack-rhel-gnu-7.2.0-opemmpi-1.10.1-openmp-shared-release-debug
-  spack-rhel-gnu-7.2.0-opemmpi-1.10.1-openmp-static-release-debug
-  spack-rhel-gnu-7.2.0-opemmpi-1.10.1-serial-shared-release-debug
-  spack-rhel-gnu-7.2.0-opemmpi-1.10.1-serial-static-release-debug
-  spack-rhel-gnu-7.2.0-opemmpi-2.1.2-openmp-shared-release-debug
-  spack-rhel-gnu-7.2.0-opemmpi-2.1.2-serial-shared-release-debug
   )
+
+#Is this entire directory needed with nothing left in supported builds?
 
 #  spack-rhel-gnu-7.2.0-opemmpi-1.10.1-openmp-shared-debug
 #  spack-rhel-gnu-7.2.0-opemmpi-1.10.1-openmp-static-release
