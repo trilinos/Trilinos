4bc69548de06e2df68bebc70e0b6ad3c8141012c
M. Mucahid Benlioglu <m.mucahidbenlioglu@gmail.com>
2020-11-11 00:06:57 -0500

    Add test for Sarma interface

---
+++ packages/zoltan2/test/core/partition/partition_sarma.cpp
@@ -0,0 +1,118 @@
+/*
+ *   Created by mbenlioglu on Nov 10, 2020.
+ */
+
+#include <Zoltan2_config.h>
+
+#include <Zoltan2_PartitioningProblem.hpp>
+#include <Zoltan2_PartitioningSolution.hpp>
+#include <Zoltan2_TestHelpers.hpp>
+#include <Zoltan2_XpetraCrsMatrixAdapter.hpp>
+
+using Teuchos::RCP;
+using Teuchos::rcp;
+
+typedef Zoltan2::BasicUserTypes<zscalar_t, zlno_t, zgno_t> myTypes_t;
+
+struct Parameters {
+    std::string alg = "pal";
+    sarma::Order order_type = sarma::Order::NAT;
+    std::string order_str = "nat";
+    Ordinal row_parts = 8, col_parts = 0;
+    Value max_load = 0;
+    int seed = 2147483647;
+    double sparsify = 1.0;
+    bool triangular = false, use_data = false;
+};
+
+auto testFromFile(const RCP<const Teuchos::Comm<int> > &comm, int nparts, std::string &filename, bool doRemap, Parameters &sarma) {
+    int me = comm->getRank();
+
+    UserInputForTests userInputForTests(testDataFilePath, filename, comm, false, true);
+    RCP<tcrsMatrix_t> matrix = userInputForTests.getUITpetraCrsMatrix();
+    RCP<const tcrsMatrix_t> matrixConst = rcp_const_cast<const tcrsMatrix_t>(matrix);
+
+
+    xCM_tCM_t matrixAdapter(matrixConst, 0);
+
+    #ifdef HAVE_ZOLTAN2_MPI
+    double begin = MPI_Wtime();
+    #endif
+
+    Teuchos::ParameterList params("test params");
+    params.set("debug_level", "basic_status");
+    params.set("num_global_parts", nparts);
+    params.set("algorithm", "sarma");
+    if (doRemap) params.set("remap_parts", true);
+    Teuchos::ParameterList &zparams = params.sublist("zoltan_parameters", false);
+    zparams.set("DEBUG_LEVEL", "0");
+
+    // Sarma params
+    Teuchos::ParameterList &sparams = params.sublist("sarma_parameters", false);
+    sparams.set("alg", "opal");
+//    sparams.set("row_parts", 4);
+
+
+    #ifdef HAVE_ZOLTAN2_MPI
+    Zoltan2::PartitioningProblem<xCM_tCM_t> problem(&matrixAdapter, &params, comm);
+    #else
+    Zoltan2::PartitioningProblem<inputAdapter_t> problem(&ia, &params);
+    #endif
+    if (me == 0) std::cout << "Problem constructed" << std::endl;
+
+    problem.solve();
+
+    #ifdef HAVE_ZOLTAN2_MPI
+    if (me == 0)
+        std::cout << "Run-time:\t" << MPI_Wtime() - begin << std::endl;
+    #endif
+    return problem.getSolution();
+}
+
+int main(int argc, char *argv[]) {
+    Tpetra::ScopeGuard tscope(&argc, &argv);
+    RCP<const Teuchos::Comm<int> > tcomm = Tpetra::getDefaultComm();
+
+    Parameters params;
+    params.alg = "opal";
+
+    int nParts = std::atoi(argv[2]);
+    bool doRemap = false;
+    std::string filename = argv[1];
+
+    // Run-time options
+    Teuchos::CommandLineProcessor cmdp(false, false);
+    cmdp.setOption("file", &filename);
+    cmdp.setOption("nparts", &nParts);
+    cmdp.setOption("remap", "no-remap", &doRemap);
+    cmdp.parse(argc, argv);
+
+    auto zsoln = testFromFile(tcomm, nParts, filename, doRemap, params);
+
+    // compare zoltan vs raw
+    std::ofstream null;
+    null.setstate(std::ios_base::badbit);
+    const auto algs = sarma::get_algorithm_map<Ordinal,Value>();
+    const auto parts = sarma::Run<Ordinal, Value>(algs.at(params.alg).first, null, filename + ".mtx", params.order_type, params.row_parts, params.col_parts, params.max_load, params.triangular,
+                                                   false, params.sparsify, algs.at(params.alg).second, params.use_data, params.seed);
+
+    const auto &p = parts.first;
+    const auto &q = parts.second;
+
+    const int *zparts = zsoln.getPartListView();
+    for (int i = 0; i < p.size(); ++i) {
+        if (zparts[i] != (int) p[i]) {
+            std::cerr << "Not same";
+            return EXIT_FAILURE;
+        }
+    }
+    for (int i = p.size(); i < p.size() + q.size(); ++i) {
+        if (zparts[i] != (int) q[i-p.size()]) {
+            std::cerr << "Not Same";
+            return EXIT_FAILURE;
+        }
+    }
+    std::cout << "PASSED\n";
+
+    return EXIT_SUCCESS;
+}
