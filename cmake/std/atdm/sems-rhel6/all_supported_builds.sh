89aba40ed0a5cd5778714f0fa0cdda4eb9b2d8d7
Henry Swantner <HRSwant@sandia.gov>
2020-11-11 12:13:12 -0700

    Remove entire directory for systems with no listed supported builds.

--- cmake/std/atdm/sems-rhel6/all_supported_builds.sh
+++
@@ -1,8 +0,0 @@
-# This script is sourced to return all of the supported builds
-
-export ATDM_CONFIG_CTEST_S_BUILD_NAME_PREFIX=Trilinos-atdm-
-
-export ATDM_CONFIG_ALL_SUPPORTED_BUILDS=(
-  )
-
-#Is this entire directory needed with nothing left in supported builds?
