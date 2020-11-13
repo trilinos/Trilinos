13e32b10b11522a97a33548dd66fc5836dba8d29
Chris Siefert <csiefer@sandia.gov>
2020-11-13 11:24:08 -0700

    Tpetra: applyDirichlet BC's helper routine --- UVM removal

--- packages/tpetra/core/src/Tpetra_applyDirichletBoundaryCondition.hpp
+++ packages/tpetra/core/src/Tpetra_applyDirichletBoundaryCondition.hpp
@@ -242,16 +242,26 @@ applyDirichletBoundaryConditionToLocalMatrixRows
   using NT = typename CrsMatrixType::node_type;
   using crs_matrix_type = ::Tpetra::CrsMatrix<SC, LO, GO, NT>;
   using execution_space = typename crs_matrix_type::execution_space;
-
-  using local_row_indices_type =
-    Kokkos::View<const LO*, Kokkos::AnonymousSpace>;
-  const local_row_indices_type lclRowInds_a (lclRowInds);
+  using memory_space = typename crs_matrix_type::device_type::memory_space;
 
   using Details::ApplyDirichletBoundaryConditionToLocalMatrixRows;
   using impl_type =
     ApplyDirichletBoundaryConditionToLocalMatrixRows<SC, LO, GO, NT>;
-  const bool runOnHost = true;
-  impl_type::run (execution_space (), A, lclRowInds_a, runOnHost);
+
+  // Only run on host if we can access the data
+  const bool runOnHost = Kokkos::Impl::SpaceAccessibility<Kokkos::Serial,memory_space>::accessible;
+  if(runOnHost) {
+    using local_row_indices_type = Kokkos::View<const LO*, Kokkos::AnonymousSpace>;
+    const local_row_indices_type lclRowInds_a (lclRowInds);
+    impl_type::run (execution_space (), A, lclRowInds_a, true);
+  }
+  else {
+    auto  lclRowInds_a = Kokkos::create_mirror_view_and_copy(execution_space(),lclRowInds);
+    impl_type::run (execution_space (), A, lclRowInds_a, false);
+  }
+
+
+
 }
 
 } // namespace Tpetra
