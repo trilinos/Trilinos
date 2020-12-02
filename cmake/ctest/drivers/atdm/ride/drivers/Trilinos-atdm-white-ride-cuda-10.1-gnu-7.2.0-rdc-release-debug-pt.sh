db06f4614985241536f761504d95b8bc4a4cc588
Henry Swantner <HRSwant@sandia.gov>
2020-12-02 04:08:44 -0700

    Remove unneeded systems from README & associated ctest dirs

--- cmake/ctest/drivers/atdm/ride/drivers/Trilinos-atdm-white-ride-cuda-10.1-gnu-7.2.0-rdc-release-debug-pt.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-if [ "${Trilinos_TRACK}" == "" ] ; then
-  export Trilinos_TRACK=Specialized
-fi
-if [ "${ATDM_CONFIG_CDASH_HOSTNAME}" == "ride" ] ; then
-  export EXCLUDE_NODES_FROM_BSUB="-R hname!=ride7"
-fi
-$WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/ride/local-driver.sh
--- cmake/ctest/drivers/atdm/sems-rhel6/drivers/Trilinos-atdm-sems-rhel6-gnu-6.1.0-serial-debug.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export Trilinos_TRACK=ATDM
-$WORKSPACE/Trilinos/cmake/ctest/drivers/atdm/sems-rhel6/local-driver.sh
