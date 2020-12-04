6035e97ead565c3e8efd31662087aa5b865e48e0
Drew P. Kouri <dpkouri@sandia.gov>
2020-12-04 14:42:54 -0700

    Added new projection algorithm for single linear constraints that uses Dai-Fletcher bracketing and Ridders' method to solve the dual optimality system.

--- packages/rol/src/function/polyproj/ROL_PolyhedralProjectionFactory.hpp
+++ packages/rol/src/function/polyproj/ROL_PolyhedralProjectionFactory.hpp
@@ -48,6 +48,7 @@
 #include "ROL_DaiFletcherProjection.hpp"
 #include "ROL_DykstraProjection.hpp"
 #include "ROL_SemismoothNewtonProjection.hpp"
+#include "ROL_RiddersProjection.hpp"
 
 namespace ROL {
 
@@ -57,11 +58,13 @@ namespace ROL {
     \arg    PPA_DAIFLETCHER describe
     \arg    PPA_DYKSTRA     describe
     \arg    PPA_NEWTON      describe
+    \arg    PPA_RIDDERS     describe
  */
 enum EPolyProjAlgo{
   PPA_DAIFLETCHER = 0,
   PPA_DYKSTRA,
   PPA_NEWTON,
+  PPA_RIDDERS,
   PPA_LAST
 };
 
@@ -71,6 +74,7 @@ inline std::string EPolyProjAlgoToString(EPolyProjAlgo alg) {
     case PPA_DAIFLETCHER: retString = "Dai-Fletcher";      break;
     case PPA_DYKSTRA:     retString = "Dysktra";           break;
     case PPA_NEWTON:      retString = "Semismooth Newton"; break;
+    case PPA_RIDDERS:     retString = "Ridders";           break;
     case PPA_LAST:        retString = "Last Type (Dummy)"; break;
     default:              retString = "INVALID EPolyProjAlgo";
   }
@@ -86,6 +90,7 @@ inline int isValidPolyProjAlgo(EPolyProjAlgo alg){
   return( (alg == PPA_DAIFLETCHER) ||
           (alg == PPA_DYKSTRA)     ||
           (alg == PPA_NEWTON)      ||
+          (alg == PPA_RIDDERS)     ||
           (alg == PPA_LAST)
         );
 }
@@ -133,6 +138,7 @@ inline Ptr<PolyhedralProjection<Real>> PolyhedralProjectionFactory(const Vector<
     case PPA_DAIFLETCHER: return makePtr<DaiFletcherProjection<Real>>(xprim,xdual,bnd,con,mul,res,list);      break;
     case PPA_DYKSTRA:     return makePtr<DykstraProjection<Real>>(xprim,xdual,bnd,con,mul,res,list);          break;
     case PPA_NEWTON:      return makePtr<SemismoothNewtonProjection<Real>>(xprim,xdual,bnd,con,mul,res,list); break;
+    case PPA_RIDDERS:     return makePtr<RiddersProjection<Real>>(xprim,xdual,bnd,con,mul,res,list);          break;
     default:              return nullPtr;
   }
 }
