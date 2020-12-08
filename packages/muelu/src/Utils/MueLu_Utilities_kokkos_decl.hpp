f3661856d5ab2ed0215cd57c7ecb3ab5fcffd998
Jonathan Hu <jhu@sandia.gov>
2020-12-08 11:25:22 -0700

    MueLu: remove disabled code

--- packages/muelu/src/Utils/MueLu_Utilities_kokkos_decl.hpp
+++ packages/muelu/src/Utils/MueLu_Utilities_kokkos_decl.hpp
@@ -386,14 +386,6 @@ namespace MueLu {
     static RCP<Vector> GetMatrixDiagonalInverse(const Matrix& A, Magnitude tol = Teuchos::ScalarTraits<SC>::eps()*100, const bool doLumped=false) {
       return UtilitiesBase::GetMatrixDiagonalInverse(A, tol, doLumped);
     }
-#if 0
-    static ArrayRCP<SC> GetLumpedMatrixDiagonal(const Matrix& A, const bool doReciprocal=false, Magnitude tol = Teuchos::ScalarTraits<Scalar>::eps()*100, Scalar tolReplacement = Teuchos::ScalarTraits<Scalar>::zero()) {
-      return UtilitiesBase::GetLumpedMatrixDiagonal(A, doReciprocal, tol, tolReplacement);
-    }
-    static RCP<Vector> GetLumpedMatrixDiagonal(RCP<const Matrix > A, const bool doReciprocal=false, Magnitude tol = Teuchos::ScalarTraits<Scalar>::eps()*100, Scalar tolReplacement = Teuchos::ScalarTraits<Scalar>::zero()) {
-      return UtilitiesBase::GetLumpedMatrixDiagonal(*A, doReciprocal, tol, tolReplacement);
-    }
-#endif
     static RCP<Vector> GetLumpedMatrixDiagonal(Matrix const &A, const bool doReciprocal=false, Magnitude tol = Teuchos::ScalarTraits<Scalar>::eps()*100, Scalar tolReplacement = Teuchos::ScalarTraits<Scalar>::zero()) {
       return UtilitiesBase::GetLumpedMatrixDiagonal(A, doReciprocal, tol, tolReplacement);
     }
