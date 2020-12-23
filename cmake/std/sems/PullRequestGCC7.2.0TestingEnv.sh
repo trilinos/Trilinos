873e531bcae17fba6a9a60536980dd718cbfaa6f
William McLendon <wcmclen@sandia.gov>
2020-12-23 13:20:18 -0700

    Framework: Update PR replication scripts to CMake 17.x
    This is for Issue #8520
    
    Preparing for the pending update to CMake's minimum version at the
    end of January 2021

--- cmake/std/sems/PullRequestGCC7.2.0TestingEnv.sh
+++ cmake/std/sems/PullRequestGCC7.2.0TestingEnv.sh
@@ -18,7 +18,7 @@ module load sems-parmetis/4.0.3/parallel
 module load sems-scotch/6.0.3/nopthread_64bit_parallel
 module load sems-superlu/4.3/base
 
-module load sems-cmake/3.10.3
+module load sems-cmake/3.17.1
 module load sems-ninja_fortran/1.8.2
 
 module load sems-git/2.10.1
