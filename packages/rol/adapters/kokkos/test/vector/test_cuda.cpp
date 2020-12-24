587bf1552f34da9f1bf6cd0d84d1f1c53e81da62
gvonwin <gvonwin@sandia.gov>
2020-12-23 20:07:25 -0700

    Modified ROL::KokkosVector to encapsulate a Kokkos::DualView on the host (CPU) and device (GPU). Standard vector tests passing when compiled using ROL_ENABLE_Kokkos, Kokkos_ENABLE_CUDA, Kokkos_ENABLE_CUDA_LAMBDA all set to ON and Kokkos_ARCH_PASCAL60, when compiling on machine with a Tesla P100 card. Compiled using GCC 7.2.0, NVCC 10.1.243, and OPENMPI-4.0.3. TODO: Add tests for ROL's elementwise functions, which are currently set to run in the host execution space. Additionally, replace the internal calls to Kokkos::deep_copy with Kokkos::DualView's modify() and sync() functions.

--- packages/rol/adapters/kokkos/test/vector/test_cuda.cpp
+++ packages/rol/adapters/kokkos/test/vector/test_cuda.cpp
@@ -2,12 +2,13 @@
 #include "ROL_StdVector.hpp"
 #include "ROL_Stream.hpp"
 #include "Teuchos_GlobalMPISession.hpp"
+#include "Kokkos_Core.hpp"
+#include "Kokkos_DualView.hpp"
 
 int main( int argc, char* argv[] ) {
 
-  using RealT  = float;
-  using Device = Kokkos::Cuda;
-  using Vector = ROL::KokkosVector<RealT,Device>;
+  using RealT   = float;
+  using VectorT = ROL::KokkosVector<RealT,Kokkos::Cuda>;
 
   Kokkos::initialize(argc,argv); {
 
@@ -22,20 +23,59 @@ int main( int argc, char* argv[] ) {
     int errorFlag = 0;
     auto errtol = ROL::ROL_THRESHOLD<RealT>();
 
-    int n = 1000;
+    int n = 100;
 
     try {
 
-      Vector x(n), y(n), z(n);
+      VectorT x(n), y(n), z(n);
       x.randomize();
       y.randomize();
       z.randomize();
 
+      *os << "\n\nCreated three random vectors of length " << n << std::endl;
+      *os << "||x|| = " << x.norm() << std::endl;
+      *os << "||y|| = " << y.norm() << std::endl;
+      *os << "||z|| = " << z.norm() << std::endl;
+
       // Standard Tests
       auto consistency = x.checkVector(y, z, true, *os );
       ROL::StdVector<RealT> checkvec( ROL::makePtrFromRef(consistency) );
+  
+      if( checkvec.norm() > std::sqrt(ROL::ROL_EPSILON<RealT>()) ) ++errorFlag;    
+      
+      // Basis tests
+      // Set to first basis vector
+      auto zp = x.basis(0);
+      auto znorm = zp->norm();
+      *os << "Norm or ROL::Vector z (first basis vector): " << znorm << "\n";
+      if( std::abs(znorm-1.0) > errtol ) {
+        *os << "---> POSSIBLE ERROR ABOVE!\n";
+        ++errorFlag;
+      }
+      
+      // set to middle basis vector
+      zp = x.basis(n/2);
+      znorm = zp->norm();
+      *os << "\nNorm of ROL::Vector z ('middle' basis vector): " << znorm << "\n";
+      if( std::abs(znorm-1.0) > errtol ) {
+        *os << "---> POSSIBLE ERROR ABOVE!\n";
+        ++errorFlag;
+      }
+ 
+      // set to last basis vector
+      zp = x.basis(n-1);
+      znorm = zp->norm();
+      *os << "\nNorm of ROL::Vector z ('middle' basis vector): " << znorm << "\n";
+      if( std::abs(znorm-1.0) > errtol ) {
+        *os << "---> POSSIBLE ERROR ABOVE!\n";
+        ++errorFlag;
+      }
+       
+      // Repeat the checkVector tests with a zero vector
+      x.scale(0.0);
+      consistency = x.checkVector(x,x,true,*os);
+      if( checkvec.norm() > 0.0 ) ++errorFlag;
 
-    
     } catch( std::exception& err ) {
       *os << err.what() << std::endl;
       errorFlag = -1000;
