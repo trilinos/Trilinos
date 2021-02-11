34af28c07b716be471f0b27406df80b1c0b3bb93
Kyungjoo Kim <kyukim@sandia.gov>
2021-02-10 17:19:47 -0700

    Intrepid2 - uvm removal testing on shared directory

--- packages/intrepid2/unit-test/Shared/PointTools/test_01.hpp
+++ packages/intrepid2/unit-test/Shared/PointTools/test_01.hpp
@@ -70,7 +70,7 @@ namespace Intrepid2 {
       };                                                                \
     }
     
-    template<typename ValueType, typename DeviceSpaceType>
+    template<typename ValueType, typename DeviceType>
     int PointTools_Test01(const bool verbose) {
       Teuchos::RCP<std::ostream> outStream;
       Teuchos::oblackholestream bhs; // outputs nothing
@@ -83,6 +83,7 @@ namespace Intrepid2 {
       Teuchos::oblackholestream oldFormatState;
       oldFormatState.copyfmt(std::cout);
 
+      using DeviceSpaceType = typename DeviceType::execution_space;
       typedef typename
         Kokkos::Impl::is_space<DeviceSpaceType>::host_mirror_space::execution_space HostSpaceType ;
 
