039d42b0a6d3cb40bf5e8cca4333d69057f0ebc8
Samuel Browne <sebrown@sandia.gov>
2021-02-18 10:44:03 -0700

    Add openmpi 4.0.5 toolchain for VAN1
    User Support Ticket(s) or Story Referenced: SPAR-969

--- cmake/std/atdm/van1-tx2/custom_builds.sh
+++ cmake/std/atdm/van1-tx2/custom_builds.sh
@@ -10,6 +10,11 @@ if atdm_match_any_buildname_keyword \
   ; then
   export ATDM_CONFIG_COMPILER=ARM-20.1_OPENMPI-4.0.3
 elif atdm_match_any_buildname_keyword \
+     arm-20.1-openmpi-4.0.5 \
+     arm-20.1_openmpi-4.0.5 \
+  ; then
+  export ATDM_CONFIG_COMPILER=ARM-20.1_OPENMPI-4.0.5
+elif atdm_match_any_buildname_keyword \
      arm-20.0-openmpi-4.0.2 \
      arm-20.0_openmpi-4.0.2 \
      arm-20.0 \
@@ -27,6 +32,7 @@ else
   echo "***"
   echo "****  arm-20.0-openmpi-4.0.2    (arm-20.0, default)"
   echo "****  arm-20.1-openmpi-4.0.3    (arm-20.1)"
+  echo "****  arm-20.1-openmpi-4.0.5"
   echo "***"
   return
 
