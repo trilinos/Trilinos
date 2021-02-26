a38e2282ff66f64ad5b190402b94d1db9971ff5e
K. Devine <kddevin@sandia.gov>
2021-02-26 12:42:28 -0700

    tpetra: separated BugTests.cpp into separate test files so that we can disable them separately (since they exercise different classes).

--- packages/tpetra/core/test/BugTests/Bug7234.cpp
+++ packages/tpetra/core/test/BugTests/Bug7234.cpp
@@ -44,10 +44,7 @@
 #include "Teuchos_UnitTestHarness.hpp"
 #include "Teuchos_Comm.hpp"
 #include "Tpetra_Core.hpp"
-#include "Tpetra_MatrixIO.hpp"
 #include "Tpetra_Map.hpp"
-#include "Tpetra_CrsGraph.hpp"
-#include "Tpetra_CrsMatrix.hpp"
 #include "Tpetra_Vector.hpp"
 #include "Tpetra_Details_Behavior.hpp"
 #include <memory>
@@ -63,8 +60,6 @@ namespace {
   using Teuchos::null;
   using Tpetra::Import;
   using Tpetra::Map;
-  using Tpetra::CrsGraph;
-  using Tpetra::CrsMatrix;
   using Tpetra::Vector;
   using Tpetra::global_size_t;
 
@@ -84,170 +79,11 @@ namespace {
         "Slack off of machine epsilon used to check test results" );
   }
 
-  RCP<const Comm<int> > getDefaultComm()
-  {
-    if (testMpi) {
-      return Tpetra::getDefaultComm();
-    }
-    return rcp(new Teuchos::SerialComm<int>());
-  }
-
   //
   // UNIT TESTS
   //
 
-  ////
-  TEUCHOS_UNIT_TEST( readHBMatrix, Bug5072_ReadOneRowMPI )
-  {
-    // failure reading 1x4 matrix under MPI
-    // typedef int                       LO;
-    // typedef int                       GO;
-    typedef Tpetra::Map<> map_type;
-    typedef map_type::local_ordinal_type LO;
-    typedef map_type::global_ordinal_type GO;
-    typedef Tpetra::CrsMatrix<> crs_matrix_type;
-    typedef typename crs_matrix_type::scalar_type SC;
-
-    // create a comm
-    RCP<const Comm<int> > comm = getDefaultComm();
-    const int myImageID = comm->getRank();
-    RCP<const crs_matrix_type> readMatrix, testMatrix;
-
-    {
-      // this is what the file looks like: 1 3 4 9
-      RCP<const map_type> rng = Tpetra::createUniformContigMap<LO, GO>(1,comm);
-      RCP<const map_type> dom = Tpetra::createUniformContigMap<LO, GO>(4,comm);
-      RCP<crs_matrix_type> A = Tpetra::createCrsMatrix<SC, LO, GO>(rng, 4);
-      if (myImageID == 0) {
-        A->insertGlobalValues( 0, Teuchos::tuple<GO>(0,1,2,3), Teuchos::tuple<SC>(1.0,3.0,4.0,9.0) );
-      }
-      A->fillComplete(dom,rng);
-      testMatrix = A;
-    }
-    {
-      RCP<crs_matrix_type> A;
-      Tpetra::Utils::readHBMatrix("addA2.hb", comm, A);
-      readMatrix = A;
-    }
-    // test that *readMatrix == *testMatrix
-    TEST_EQUALITY( testMatrix->getNodeNumRows(), readMatrix->getNodeNumRows() );
-    TEST_EQUALITY( testMatrix->getNodeNumCols(), readMatrix->getNodeNumCols() );
-    TEST_EQUALITY( testMatrix->getNodeNumEntries(), readMatrix->getNodeNumEntries() );
-    if (success) {
-      Teuchos::ArrayView<const LO>    rowinds1, rowinds2;
-      Teuchos::ArrayView<const SC> rowvals1, rowvals2;
-
-      const LO lclNumRows = testMatrix->getNodeNumRows ();
-      for (LO r = 0; r < lclNumRows; ++r) {
-        testMatrix->getLocalRowView(r, rowinds1, rowvals1);
-        readMatrix->getLocalRowView(r, rowinds2, rowvals2);
-        TEST_COMPARE_ARRAYS( rowinds1, rowinds2 );
-        TEST_COMPARE_ARRAYS( rowvals1, rowvals2 );
-      }
-    }
-    // All procs fail if any proc fails
-    int globalSuccess_int = -1;
-    reduceAll( *comm, Teuchos::REDUCE_SUM, success ? 0 : 1, outArg(globalSuccess_int) );
-    TEST_EQUALITY_CONST( globalSuccess_int, 0 );
-  }
-
-  ////
-  // TEUCHOS_UNIT_TEST( Map, Bug4756_UnsignedGlobalOrdinal )
-  // {
-  //   // test bug where unsigned LO clashes with signed GO
-  //   typedef unsigned int LO;
-  //   // this still has to be bigger than LO
-  //   typedef     long int GO;
-  //   // this test assumes that global_size_t (default: size_t) is larger than GO=="long int", which should be true on 64-bit builds.
-  //   // create a comm
-  //   RCP<const Comm<int> > comm = getDefaultComm();
-  //   const global_size_t numGlobal = comm->getSize();
-  //   RCP<const Map<LO,GO> > map = rcp(new Map<LO,GO>(numGlobal,0,comm) );
-  //   RCP<const CrsGraph<LO,GO> > graph = rcp(new CrsGraph<LO,GO>(map,0,Tpetra::StaticProfile) );
-  //   TEST_EQUALITY_CONST( graph != null, true );
-  //   // All procs fail if any proc fails
-  //   int globalSuccess_int = -1;
-  //   reduceAll( *comm, Teuchos::REDUCE_SUM, success ? 0 : 1, outArg(globalSuccess_int) );
-  //   TEST_EQUALITY_CONST( globalSuccess_int, 0 );
-  // }
-
-  TEUCHOS_UNIT_TEST( DistObject, Bug5129_OverlyStrictMapComparison )
-  {
-    // test bug where map test checks that maps are the same object, instead of checking that maps are equivalent
-
-    using std::endl;
-    // mfh 06 Aug 2017: There was no obvious reason why this test
-    // required LO=int and GO=int, nor did the original Bug 5129 bug
-    // report depend on specific LO or GO types, so I relaxed this
-    // requirement.  The test now uses the default LO and GO types.
-#if 1
-    typedef Tpetra::Map<>::local_ordinal_type LO;
-    typedef Tpetra::Map<>::global_ordinal_type GO;
-    typedef Tpetra::Vector<>::scalar_type SC;
-#else
-    typedef int LO;
-    // this still has to be bigger than LO
-    typedef int GO;
-#endif // 1
-
-    RCP<const Comm<int> > comm = getDefaultComm();
-    const bool verbose = Tpetra::Details::Behavior::verbose ();
-    std::unique_ptr<std::string> prefix;
-    if (verbose) {
-      std::ostringstream os;
-      os << "Proc " << comm->getRank () << ": Bug 5129 test: ";
-      prefix = std::unique_ptr<std::string> (new std::string (os.str ()));
-      os << endl;
-      std::cerr << os.str ();
-    }
-    const global_size_t numGlobal = comm->getSize()*10;
-
-    // create two separate, but identical, maps
-    if (verbose) {
-      std::ostringstream os;
-      os << *prefix << "Create Maps" << endl;
-      std::cerr << os.str ();
-    }
-
-    RCP<const Map<LO,GO> > mapImportIn  = Tpetra::createUniformContigMap<LO,GO>(numGlobal, comm);
-    RCP<const Map<LO,GO> > mapImportOut = Tpetra::createUniformContigMap<LO,GO>(numGlobal, comm);
-    RCP<const Map<LO,GO> > mapIn        = Tpetra::createUniformContigMap<LO,GO>(numGlobal, comm);
-    RCP<const Map<LO,GO> > mapOut       = Tpetra::createUniformContigMap<LO,GO>(numGlobal, comm);
-    TEST_EQUALITY_CONST( *mapImportIn  == *mapIn,  true );
-    TEST_EQUALITY_CONST( *mapImportOut == *mapOut, true );
-    TEST_EQUALITY_CONST( mapImportIn   == mapIn,  false );
-    TEST_EQUALITY_CONST( mapImportOut  == mapOut, false );
-    // create import, vectors from these maps
-
-    if (verbose) {
-      std::ostringstream os;
-      os << *prefix << "Create Import" << endl;
-      std::cerr << os.str ();
-    }
-    RCP<const Import<LO,GO> > import = Tpetra::createImport(mapImportIn, mapImportOut);
-
-    if (verbose) {
-      std::ostringstream os;
-      os << *prefix << "Create Vectors" << endl;
-      std::cerr << os.str ();
-    }
-    RCP<Vector<SC,LO,GO> > vecIn = Tpetra::createVector<SC>(mapIn);
-    RCP<Vector<SC,LO,GO> > vecOut = Tpetra::createVector<SC>(mapOut);
-
-    if (verbose) {
-      std::ostringstream os;
-      os << *prefix << "Call doImport" << endl;
-      std::cerr << os.str ();
-    }
-    // do the import; under the bug, this should throw an exception
-    TEST_NOTHROW( vecOut->doImport( *vecIn, *import, Tpetra::REPLACE ) )
-    // All procs fail if any proc fails
-    int globalSuccess_int = -1;
-    reduceAll( *comm, Teuchos::REDUCE_SUM, success ? 0 : 1, outArg(globalSuccess_int) );
-    TEST_EQUALITY_CONST( globalSuccess_int, 0 );
-  }
-
-  TEUCHOS_UNIT_TEST( DistObject, BlockedViews_7234 )
+  TEUCHOS_UNIT_TEST( MultiVector, BlockedViews_7234 )
   {
     // Test that replicates Trilinos issue #7234 
     // https://github.com/trilinos/Trilinos/issues/7234
@@ -273,6 +109,7 @@ namespace {
     const int rank = comm->getRank();
     const int size = comm->getSize();
 
+std::cout << rank << " KDDKDD HELLO ONE" << std::endl;
     // Build the array: GIDs are BLOCKED - owned first, then shared
     Teuchos::Array<GO> owned_gids;
     Teuchos::Array<GO> shared_gids;
@@ -286,6 +123,7 @@ namespace {
       owned_and_shared_gids.push_back(shared_gids[0]);
     }
 
+std::cout << rank << " KDDKDD HELLO TWO" << std::endl;
     out.setShowProcRank(true);
     out.setOutputToRootOnly(rank);
     out << "owned_map\n";
@@ -296,10 +134,12 @@ namespace {
       out << i << " " << owned_and_shared_gids[i] << std::endl;
     out.setOutputToRootOnly(0);
 
+std::cout << rank << " KDDKDD HELLO THREE" << std::endl;
     RCP<TpetraMap> owned_map = rcp(new TpetraMap(Teuchos::OrdinalTraits<GST>::invalid(),owned_gids,GO(0),comm));
     RCP<TpetraMap> shared_map = rcp(new TpetraMap(Teuchos::OrdinalTraits<GST>::invalid(),shared_gids,GO(0),comm));
     RCP<TpetraMap> owned_and_shared_map = rcp(new TpetraMap(Teuchos::OrdinalTraits<GST>::invalid(),owned_and_shared_gids,GO(0),comm));
 
+std::cout << rank << " KDDKDD HELLO FOUR" << std::endl;
     bool zeroOut = true;
     RCP<TpetraVec> owned_and_shared = rcp(new TpetraVec(owned_and_shared_map,zeroOut));
     RCP<TpetraExport> exporter = rcp(new TpetraExport(shared_map,owned_map));
@@ -310,26 +150,36 @@ namespace {
     RCP<TpetraVec> owned = owned_and_shared->offsetViewNonConst(owned_map,0);
     RCP<TpetraVec> shared = owned_and_shared->offsetViewNonConst(shared_map,owned->getLocalLength());
 
+std::cout << rank << " KDDKDD HELLO FIVE" << std::endl;
     owned->putScalar(1.0);
     shared->doImport(*owned, *importer, Tpetra::REPLACE);
 
-    auto host_shared = shared->getLocalViewHost(Tpetra::Access::ReadOnly);
+std::cout << rank << " KDDKDD HELLO SIX" << std::endl;
     const double tol = Teuchos::ScalarTraits<double>::eps() * 100.0;
+  {
+    auto host_shared = shared->getLocalViewHost(Tpetra::Access::ReadOnly);
     if (rank > 0) {
       TEST_FLOATING_EQUALITY(host_shared(0,0),1.0,tol);
     }
+  }
 
+  {
     auto host_owned_and_shared = owned_and_shared->getLocalViewHost(Tpetra::Access::ReadOnly);
     TEST_FLOATING_EQUALITY(host_owned_and_shared(0,0),1.0,tol);
     TEST_FLOATING_EQUALITY(host_owned_and_shared(1,0),1.0,tol);
     if (rank > 0) {
       TEST_FLOATING_EQUALITY(host_owned_and_shared(2,0),1.0,tol);
     }
+  }
 
+std::cout << rank << " KDDKDD HELLO SEVEN" << std::endl;
     // now test the export
     owned->doExport(*shared, *exporter, Tpetra::ADD);
 
+std::cout << rank << " KDDKDD HELLO EIGHT" << std::endl;
+  {
     auto host_owned = owned->getLocalViewHost(Tpetra::Access::ReadOnly);
+    auto host_owned_and_shared = owned_and_shared->getLocalViewHost(Tpetra::Access::ReadOnly);
     TEST_FLOATING_EQUALITY(host_owned(0,0),1.0,tol);
     TEST_FLOATING_EQUALITY(host_owned_and_shared(0,0),1.0,tol); // check owned entries only
     if (rank != size-1) {
@@ -340,5 +190,7 @@ namespace {
       TEST_FLOATING_EQUALITY(host_owned(1,0),1.0,tol);
       TEST_FLOATING_EQUALITY(host_owned_and_shared(1,0),1.0,tol); // check owned entries only
     }
+  }
+std::cout << rank << " KDDKDD HELLO NINE" << std::endl;
   }
 }
