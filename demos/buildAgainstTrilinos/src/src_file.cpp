d3869c8fe676075d6dce0fc3912c441cf4daf462
Roscoe A. Bartlett <rabartl@sandia.gov>
2021-03-10 07:19:10 -0700

    Modernize for newer CMake and fix compile error (#8498)
    This now builds without errors with Clang 10.0.
    
    * Require CMake 3.17.1 which is the same minimum now required by Trilinos
    
    * Updated the documentation to use Markdown and use raw CMake and no do-cmake
      script
    
    * Deleted unneccasary do-cmake script
    
    * Deleted usage of a 'Trilinos_PREFIX' var and just use built-in CMake var
      CMAKE_PREFIX_PATH directly as documented in CMake documentation for
      'find_package()'
    
    * Turned off warning from Kokkos about not suupporting C++ extensions
    
    * Fixed build error with the source code usage of Teuchos

--- demos/buildAgainstTrilinos/src/src_file.cpp
+++ demos/buildAgainstTrilinos/src/src_file.cpp
@@ -3,8 +3,9 @@
 
 int buildDemo::src_file(std::string& infile, Teuchos::Comm<int>& comm) {
 
+  using Teuchos::inOutArg;
   Teuchos::ParameterList p;
-  Teuchos::updateParametersFromXmlFileAndBroadcast(infile, &p, comm);
+  Teuchos::updateParametersFromXmlFileAndBroadcast(infile, inOutArg(p), comm);
 
   std::cout << "\nProcessor " << comm.getRank()
             << "  has param list : \n" << p << std::endl;
