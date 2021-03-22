efacbd2f1a5db5de47e709b13b0689f6e9a3ab45
Roger Pawlowski <rppawlo@sandia.gov>
2021-03-22 14:03:45 -0600

    Panzer: add exec space instances to hierarchic policy ctor
    for running with multiple cuda streams

--- packages/panzer/disc-fe/src/Panzer_HierarchicParallelism.hpp
+++ packages/panzer/disc-fe/src/Panzer_HierarchicParallelism.hpp
@@ -140,19 +140,17 @@ namespace panzer {
       return Kokkos::TeamPolicy<TeamPolicyProperties...>(league_size,team_size_,tmp_vector_size);
     }
 
-    /// Returns a TeamPolicy for hierarchic parallelism.
+    /// Returns a TeamPolicy for hierarchic parallelism using an exec_space instance (for cuda streams).
     template<typename ScalarT, typename ... TeamPolicyProperties, typename ExecSpace>
     Kokkos::TeamPolicy<ExecSpace, TeamPolicyProperties...> teamPolicy(ExecSpace exec_space, const int& league_size)
     {
       const int tmp_vector_size = this->template vectorSize<ScalarT>();
 
-      return Kokkos::TeamPolicy<ExecSpace, TeamPolicyProperties...>
-        (
-         exec_space,
-         league_size,
-         use_auto_team_size_ ? Kokkos::AUTO() : team_size_,
-         tmp_vector_size
-        );
+      if (use_auto_team_size_)
+        return Kokkos::TeamPolicy<TeamPolicyProperties...>(exec_space,league_size,Kokkos::AUTO(),
+                                                           tmp_vector_size);
+
+      return Kokkos::TeamPolicy<TeamPolicyProperties...>(exec_space,league_size,team_size_,tmp_vector_size);
     }
   };
 
