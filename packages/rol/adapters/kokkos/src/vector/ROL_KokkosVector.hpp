1e4bb16cf03486d9336a0e96f76c11129f7e11b7
Denis Ridzal <dridzal@sandia.gov>
2021-04-05 21:25:07 -0600

    Revert "Modified ROL::KokkosVector to encapsulate a Kokkos::DualView on the host (CPU) and device (GPU). Standard vector tests passing when compiled using ROL_ENABLE_Kokkos, Kokkos_ENABLE_CUDA, Kokkos_ENABLE_CUDA_LAMBDA all set to ON and Kokkos_ARCH_PASCAL60, when compiling on machine with a Tesla P100 card. Compiled using GCC 7.2.0, NVCC 10.1.243, and OPENMPI-4.0.3. TODO: Add tests for ROL's elementwise functions, which are currently set to run in the host execution space. Additionally, replace the internal calls to Kokkos::deep_copy with Kokkos::DualView's modify() and sync() functions."
    This reverts commit 587bf1552f34da9f1bf6cd0d84d1f1c53e81da62.

--- packages/rol/adapters/kokkos/src/vector/ROL_KokkosVector.hpp
+++ packages/rol/adapters/kokkos/src/vector/ROL_KokkosVector.hpp
@@ -46,159 +46,105 @@
 
 #include "ROL_Vector.hpp"
 #include "Kokkos_Core.hpp"
-#include "Kokkos_DualView.hpp"
+#include "Kokkos_Vector.hpp"
 
 /** @ingroup la_group
     \class ROL::KokkosVector
-    \brief Defines the ROL interface for Kokkos::View
+    \brief Defines the ROL interface for Kokkos Vector
 */
 
 namespace ROL {
 
 template<typename Real, typename Device>
-class KokkosVector 
-  : public ROL::Vector<Real> {
+class KokkosVector : public ROL::Vector<Real> {
 public:
 
-  using view_type = Kokkos::DualView<Real*,Device>;
-  using host_view_type = typename view_type::t_host;
-  using device_view_type = typename view_type::t_dev;
+  using container_type = Kokkos::vector<Real,Device>;
+  using size_type      = typename container_type::size_type;
 
-  KokkosVector( int n ) : data_("data",n) {} 
-  virtual ~KokkosVector() {}
+  KokkosVector( int n, Real fillValue = Real() ) : data_(n,fillValue) {} 
 
-  void copy_host_to_device() const {
-    Kokkos::deep_copy(data_.d_view,data_.h_view);
-  }
+  Real& operator[] ( int i ) { return data_[i]; }
+  const Real& operator[] ( int i ) const { return data_[i]; }
 
-  void copy_device_to_host() const {
-    Kokkos::deep_copy(data_.h_view,data_.d_view);
-  }
-
-  host_view_type   get_host_view()   const { return data_.view_host();   }
-  device_view_type get_device_view() const { return data_.view_device(); }
-
-  int dimension() const override { return static_cast<int>(data_.extent(0)); }
+  int dimension() const override { return data_.size(); }
 
   void zero() override {
-    device_view_type d = get_device_view();
-    Kokkos::parallel_for( data_.extent(0), KOKKOS_LAMBDA( const int i ) {
-      d(i) = 0;
+    Kokkos::parallel_for( data_.size(), KOKKOS_LAMBDA( const int i ) {
+      data_[i] = 0;
     });
   } // end zero
 
   void set( const Vector<Real>& x ) override {
-    device_view_type d  = get_device_view();
-    device_view_type xd = cast(x).get_device_view();
-    
-    Kokkos::parallel_for( dimension(), KOKKOS_LAMBDA( const int i ) {
-      d(i) = xd(i);
+    const auto& xk = cast(x);
+    Kokkos::parallel_for( data_.size(), KOKKOS_LAMBDA( const int i ) {
+      data_[i] = xk.data_[i];
     });
   } // end set
 
   void plus( const Vector<Real>& x ) override {
-    device_view_type d  = get_device_view();
-    device_view_type xd = cast(x).get_device_view();
-    Kokkos::parallel_for( dimension(), KOKKOS_LAMBDA( const int i ) {
-      d(i) += xd(i);
+    const auto& xk = cast(x);
+    Kokkos::parallel_for( data_.size(), KOKKOS_LAMBDA( const int i ) {
+      data_[i] += xk.data_[i];
     });
   } // end plus
 
   void axpy( const Real alpha, const Vector<Real>& x ) override {
-    device_view_type d  = get_device_view();
-    device_view_type xd = cast(x).get_device_view();
-    Kokkos::parallel_for( dimension(), KOKKOS_LAMBDA( const int i ) {
-      d(i) += alpha * xd(i);
+    const auto& xk = cast(x);
+    Kokkos::parallel_for( data_.size(), KOKKOS_LAMBDA( const int i ) {
+      data_[i] += alpha * xk.data_[i];
     });
   } // end axpy
 
   void setScalar( const Real alpha ) override {
-    device_view_type d  = get_device_view();
-    Kokkos::parallel_for( dimension(), KOKKOS_LAMBDA( const int i ) {
-      d(i) = alpha;
+    Kokkos::parallel_for( data_.size(), KOKKOS_LAMBDA( const int i ) {
+      data_[i] = alpha;
     });
   } // end setScalar
 
   void scale( const Real alpha ) override {
-    device_view_type d  = get_device_view();
-    Kokkos::parallel_for( dimension(), KOKKOS_LAMBDA( const int i ) {
-      d(i) *= alpha;
+    Kokkos::parallel_for( data_.size(), KOKKOS_LAMBDA( const int i ) {
+      data_[i] *= alpha;
     });
   } // end scale
 
   Real dot( const Vector<Real>& x ) const override { 
-    device_view_type d  = get_device_view();
-    device_view_type xd = cast(x).get_device_view();
+    const auto& xk = cast(x);
     Real result = 0;
-    Kokkos::parallel_reduce( dimension(), KOKKOS_LAMBDA( const int i, Real& lresult ) {
-      lresult += d(i) * xd(i);
+    Kokkos::parallel_reduce( data_.size(), KOKKOS_LAMBDA( const int i, Real& lresult ) {
+      lresult += data_[i] * xk.data_[i];
     }, result);
     return result;
   } // end dot
 
   Real norm() const override { 
-    device_view_type d  = get_device_view();
     Real result = 0;
-    Kokkos::parallel_reduce( dimension(), KOKKOS_LAMBDA( const int i, Real& lresult ) {
-      lresult += d(i) * d(i);
+    Kokkos::parallel_reduce( data_.size(), KOKKOS_LAMBDA( const int i, Real& lresult ) {
+      lresult += data_[i] * data_[i];
     }, result);
     return std::sqrt(result);
   } // end norm 
 
   Ptr<Vector<Real>> clone() const override {
-    return makePtr<KokkosVector>(dimension());
+    Ptr<Vector<Real>> xp = makePtr<KokkosVector>(data_.size());
+    return xp;
   } // end clone
 
-  Ptr<Vector<Real>> basis( int i ) const override {
-    auto bp = makePtr<KokkosVector>(dimension());
-    device_view_type bpd = bp->get_device_view();
-    Kokkos::parallel_for( dimension(), KOKKOS_LAMBDA( const int j ) {
-      bpd(j) = static_cast<Real>(i==j);
-    });
+  Ptr<Vector<Real>> basis( int i )  const override {
+    auto bp = makePtr<KokkosVector>(data_.size(),0);
+    (*bp)[i] = 1;
     return bp;
   } // end basis
 
   void randomize( const Real l = 0.0, const Real u = 1.0 ) override {
-    host_view_type h = get_host_view();
-    for( int i=0; i<dimension(); ++i ) {
+    for( size_type i=0; i<data_.size(); ++i ) {
       auto x = static_cast<Real>(rand())/static_cast<Real>(RAND_MAX);
-      data_.h_view(i) = u*x + l*(1-x);
+      data_[i] = u*x + l*(1-x);
     }
-    copy_host_to_device();
   } // end randomize
- 
-  /** 
-  * Elementwise functions must be executed on the host
-  */
-
-  void applyUnary( const Elementwise::UnaryFunction<Real>& f ) override {
-    host_view_type h = get_host_view();
-    copy_device_to_host();
-    for( int i=0; i<dimension(); ++i )  h(i) = f.apply(h(i));
-    copy_host_to_device();
-  }
-
-  void applyBinary( const Elementwise::BinaryFunction<Real>& f, const Vector<Real>& x ) override {
-    copy_device_to_host();
-    const auto& xkv = cast(x);
-    xkv.copy_device_to_host();
-    host_view_type h  = get_host_view();
-    host_view_type xh = cast(x).get_host_view();
-    for( int i=0; i<dimension(); ++i )  h(i) = f.apply(xh(i),h(i));
-    copy_host_to_device();
-  }
-
-  Real reduce( const Elementwise::ReductionOp<Real>& r ) const override {
-    copy_device_to_host();
-    host_view_type h = get_host_view();
-    Real result = r.initialValue();
-    for( int i=0; i<dimension(); ++i ) r.reduce(h(i),result);
-    return result;
-  } 
 
   void print( std::ostream& os ) const override {
-    copy_device_to_host();
-    for( int i=0; i<dimension(); ++i ) os << data_.h_view(i) << " ";
+    for( size_type i=0; i<data_.size(); ++i ) os << data_[i] << " ";
     os << std::endl;
   } // end print
 
@@ -212,7 +158,7 @@ public:
 
 private:
 
-  mutable view_type data_;
+   container_type data_;
 
 }; // KokkosVector
 
--- packages/rol/adapters/kokkos/test/vector/test_cuda.cpp
+++ packages/rol/adapters/kokkos/test/vector/test_cuda.cpp
@@ -2,13 +2,12 @@
 #include "ROL_StdVector.hpp"
 #include "ROL_Stream.hpp"
 #include "Teuchos_GlobalMPISession.hpp"
-#include "Kokkos_Core.hpp"
-#include "Kokkos_DualView.hpp"
 
 int main( int argc, char* argv[] ) {
 
-  using RealT   = float;
-  using VectorT = ROL::KokkosVector<RealT,Kokkos::Cuda>;
+  using RealT  = float;
+  using Device = Kokkos::Cuda;
+  using Vector = ROL::KokkosVector<RealT,Device>;
 
   Kokkos::initialize(argc,argv); {
 
@@ -23,59 +22,20 @@ int main( int argc, char* argv[] ) {
     int errorFlag = 0;
     auto errtol = ROL::ROL_THRESHOLD<RealT>();
 
-    int n = 100;
+    int n = 1000;
 
     try {
 
-      VectorT x(n), y(n), z(n);
+      Vector x(n), y(n), z(n);
       x.randomize();
       y.randomize();
       z.randomize();
 
-      *os << "\n\nCreated three random vectors of length " << n << std::endl;
-      *os << "||x|| = " << x.norm() << std::endl;
-      *os << "||y|| = " << y.norm() << std::endl;
-      *os << "||z|| = " << z.norm() << std::endl;
-
       // Standard Tests
       auto consistency = x.checkVector(y, z, true, *os );
       ROL::StdVector<RealT> checkvec( ROL::makePtrFromRef(consistency) );
-  
-      if( checkvec.norm() > std::sqrt(ROL::ROL_EPSILON<RealT>()) ) ++errorFlag;    
-      
-      // Basis tests
-      // Set to first basis vector
-      auto zp = x.basis(0);
-      auto znorm = zp->norm();
-      *os << "Norm or ROL::Vector z (first basis vector): " << znorm << "\n";
-      if( std::abs(znorm-1.0) > errtol ) {
-        *os << "---> POSSIBLE ERROR ABOVE!\n";
-        ++errorFlag;
-      }
-      
-      // set to middle basis vector
-      zp = x.basis(n/2);
-      znorm = zp->norm();
-      *os << "\nNorm of ROL::Vector z ('middle' basis vector): " << znorm << "\n";
-      if( std::abs(znorm-1.0) > errtol ) {
-        *os << "---> POSSIBLE ERROR ABOVE!\n";
-        ++errorFlag;
-      }
- 
-      // set to last basis vector
-      zp = x.basis(n-1);
-      znorm = zp->norm();
-      *os << "\nNorm of ROL::Vector z ('middle' basis vector): " << znorm << "\n";
-      if( std::abs(znorm-1.0) > errtol ) {
-        *os << "---> POSSIBLE ERROR ABOVE!\n";
-        ++errorFlag;
-      }
-       
-      // Repeat the checkVector tests with a zero vector
-      x.scale(0.0);
-      consistency = x.checkVector(x,x,true,*os);
-      if( checkvec.norm() > 0.0 ) ++errorFlag;
 
+    
     } catch( std::exception& err ) {
       *os << err.what() << std::endl;
       errorFlag = -1000;
