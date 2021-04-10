5ad4903059057d77bf47c6138108364ab12f7293
Brian Kelley <bmkelle@sandia.gov>
2021-04-09 19:52:10 -0600

    Tpetra: use new MV view interface in Bug8794 test

--- packages/tpetra/core/test/CrsMatrix/Bug8794.cpp
+++ packages/tpetra/core/test/CrsMatrix/Bug8794.cpp
@@ -92,7 +92,7 @@ TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(Bug8794, InsertDenseRows,
   // Initialize matrix and expected value of SpMV product
   {
     expected.putScalar(0.);
-    auto expectedData = expected.getDataNonConst();
+    auto expectedData = Kokkos::subview(expected.getLocalViewHost(Tpetra::Access::ReadWrite), Kokkos::ALL(), 0);
     for (size_t i = 0; i < map->getNodeNumElements(); i++) {
 
       GO gid = map->getGlobalElement(i);
@@ -153,9 +153,9 @@ TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(Bug8794, InsertDenseRows,
 
   // Initialize domain vector for SpMV
   {
-    auto xData = x.getDataNonConst();
+    auto xData = x.getLocalViewHost(Tpetra::Access::OverwriteAll);
     for (size_t i = 0; i < map->getNodeNumElements(); i++) 
-      xData[i] = map->getGlobalElement(i);
+      xData(i, 0) = map->getGlobalElement(i);
   }
 
   Amat.apply(x, y);
@@ -163,13 +163,13 @@ TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(Bug8794, InsertDenseRows,
   // Test product for correctness
   int ierr = 0;
   {
-    auto expectedData = expected.getData();
-    auto yData = y.getData();
+    auto expectedData = expected.getLocalViewHost(Tpetra::Access::ReadOnly);
+    auto yData = y.getLocalViewHost(Tpetra::Access::ReadOnly);
 
     for (size_t i = 0; i < map->getNodeNumElements(); i++) {
-      if (yData[i] != expectedData[i]) {
+      if (yData(i, 0) != expectedData(i, 0)) {
         std::cout << me << " of " << np << ": y[" << map->getGlobalElement(i)
-                  << "] " << yData[i] << " != " << expectedData[i]
+                  << "] " << yData(i, 0) << " != " << expectedData(i, 0)
                   << " expected" << std::endl;
         ierr++;
       }
