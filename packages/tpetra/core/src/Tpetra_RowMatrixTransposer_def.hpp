26b49bae3cc9a4024746613f6e440bb3e93c021b
Luc Berger <lberge@sandia.gov>
2021-06-08 23:01:31 -0600

    Update Tpetra_RowMatrixTransposer_def.hpp
    Removing unused variables `lclNumCols` and `nnz`

--- packages/tpetra/core/src/Tpetra_RowMatrixTransposer_def.hpp
+++ packages/tpetra/core/src/Tpetra_RowMatrixTransposer_def.hpp
@@ -145,9 +145,7 @@ createTransposeLocal (const Teuchos::RCP<Teuchos::ParameterList>& params)
       params->get (sortParamName, sortDefault);
   } ();
 
-  const LO lclNumCols (origMatrix_->getNodeNumCols ());
   const LO lclNumRows (origMatrix_->getNodeNumRows ());
-  const size_t nnz (origMatrix_->getNodeNumEntries ());
 
   RCP<const crs_matrix_type> crsMatrix =
     rcp_dynamic_cast<const crs_matrix_type> (origMatrix_);
