c6d9067c6e9119595a21dec894f8dd87f5b11122
Irina Tezaur <ikalash@camobap.ca.sandia.gov>
2021-06-09 20:11:18 -0700

    Ifpack2: correction to typo in long double file.

--- packages/ifpack2/adapters/thyra/RTOpPack_ROpGetSubVector_longdouble.cpp
+++ packages/ifpack2/adapters/thyra/RTOpPack_ROpGetSubVector_longdouble.cpp
@@ -1,14 +1,14 @@
 #include "Teuchos_ConfigDefs.hpp"
-#include "RTOpPack_ROpGetSubVector_decl.hpp"
+#include "RTOpPack_RTOpGetSubVector_decl.hpp"
 
 #if defined(HAVE_RTOP_EXPLICIT_INSTANTIATION) && defined(HAVE_TEUCHOS_LONG_DOUBLE)
 
-#include "RTOpPack_ROpGetSubVector_def.hpp"
+#include "RTOpPack_RTOpGetSubVector_def.hpp"
 #include "Teuchos_ExplicitInstantiationHelpers.hpp"
 
 namespace RTOpPack {
 
-TEUCHOS_CLASS_TEMPLATE_INSTANT_LONG_DOUBLE(ROpGetSubVector)
+TEUCHOS_CLASS_TEMPLATE_INSTANT_LONG_DOUBLE(RTOpGetSubVector)
 
 } // namespace RTOpPack
 
