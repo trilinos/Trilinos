30263fc46225217da1682b4aac38e4b84405e42c
Paul Wolfenbarger <prwolfe@sandia.gov>
2021-06-15 13:46:30 -0600

    New proxy setting to match the change Friday

--- cmake/std/PullRequestLinuxDriver.sh
+++ cmake/std/PullRequestLinuxDriver.sh
@@ -68,8 +68,8 @@ function bootstrap_modules() {
 print_banner "PullRequestLinuxDriver.sh"
 
 # Set up Sandia PROXY environment vars
-export https_proxy=http://wwwproxy.sandia.gov:80
-export http_proxy=http://wwwproxy.sandia.gov:80
+export https_proxy=http://proxy.sandia.gov:80
+export http_proxy=http://proxy.sandia.gov:80
 export no_proxy='localhost,localnets,127.0.0.1,169.254.0.0/16,forge.sandia.gov'
 
 
