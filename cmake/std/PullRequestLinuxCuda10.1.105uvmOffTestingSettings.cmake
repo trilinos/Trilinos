fb2eb489fd7cfa07c6198afe50ff166d5f9936b9
Matthew Bettencourt <mbetten@sandia.gov>
2021-07-24 08:23:06 -0600

    enable trilinos couplings testing without uvm

--- cmake/std/PullRequestLinuxCuda10.1.105uvmOffTestingSettings.cmake
+++ cmake/std/PullRequestLinuxCuda10.1.105uvmOffTestingSettings.cmake
@@ -139,7 +139,6 @@ set (Tpetra_ENABLE_CUDA_UVM OFF CACHE BOOL "Set by default for CUDA PR testing")
 # Turn off packages currently failing with UVM = OFF
 set (Trilinos_ENABLE_Panzer OFF CACHE BOOL "Turn off packages for non-UVM build")
 set (Trilinos_ENABLE_Stokhos OFF CACHE BOOL "Turn off packages for non-UVM build")
-set (Trilinos_ENABLE_TrilinosCouplings OFF CACHE BOOL "Turn off packages for non-UVM build")
 
 # Turn off tests currently failing with UVM = OFF
 # Packages with >5 failing tests
