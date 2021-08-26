91f52cdcd6bdb953aa3b528e8d79cf0e73b2cb38
Matt Bettencourt <mbetten@sandia.gov>
2021-08-26 13:15:23 -0600

    got poisson example UVM free

--- packages/panzer/adapters-stk/example/PoissonExample/Example_SimpleSource_impl.hpp
+++ packages/panzer/adapters-stk/example/PoissonExample/Example_SimpleSource_impl.hpp
@@ -82,14 +82,18 @@ template <typename EvalT,typename Traits>
 void SimpleSource<EvalT,Traits>::evaluateFields(typename Traits::EvalData workset)
 { 
   using panzer::index_t;
-  for (index_t cell = 0; cell < workset.num_cells; ++cell) {
-    for (int point = 0; point < source.extent_int(1); ++point) {
-      const double& x = workset.int_rules[ir_index]->ip_coordinates(cell,point,0);
-      const double& y = workset.int_rules[ir_index]->ip_coordinates(cell,point,1);
+  auto ip_coordinates = workset.int_rules[ir_index]->ip_coordinates.get_static_view();
+  auto source_v = source.get_static_view();
 
-      source(cell,point) = 8.0*M_PI*M_PI*std::sin(2.0*M_PI*x)*std::sin(2.0*M_PI*y);
-    }
-  }
+  Kokkos::parallel_for ("SimpleSource", workset.num_cells, KOKKOS_LAMBDA (const index_t cell) {
+      for (int point = 0; point < source_v.extent_int(1); ++point) {
+	const double& x = ip_coordinates(cell,point,0);
+	const double& y = ip_coordinates(cell,point,1);
+	
+	source_v(cell,point) = 8.0*M_PI*M_PI*std::sin(2.0*M_PI*x)*std::sin(2.0*M_PI*y);
+      }
+    });
+  Kokkos::fence();
 }
 
 //**********************************************************************
