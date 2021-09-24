654351d904f16e96f10d9e54eff2c472cf66614e
Roger Pawlowski <rppawlo@sandia.gov>
2021-09-24 07:27:39 -0600

    Phalanx: const to nonconst mdfield conversion utility

--- packages/phalanx/test/Field/NonConstDynRankViewFromConstMDField.cpp
+++ packages/phalanx/test/Field/NonConstDynRankViewFromConstMDField.cpp
@@ -1,8 +1,7 @@
 #include "Phalanx_ExtentTraits.hpp"
-#include "Phalanx_MDField.hpp"
 #include "Teuchos_UnitTestHarness.hpp"
 #include "Teuchos_TypeNameTraits.hpp"
-#include "Kokkos_DynRankView.hpp"
+#include "Phalanx_GetNonConstDynRankViewFromConstMDField.hpp"
 
 // This test demonstrates how to get a nonconst DynRankView from a
 // const MDField for double and FAD scalar types.
@@ -24,9 +23,6 @@ const int num_pts = 8;
 const int num_equations = 32;
 const int num_derivatives = 4;
 
-template<typename T> struct remove_low_level_const {using type = T;};
-template<typename T> struct remove_low_level_const<T const *> {using type = T*;};
-
 namespace {
   // function input for a,b,c are all CONST
   template<typename Scalar>
@@ -36,24 +32,11 @@ namespace {
                const_mdfield<Scalar>& b,
                const_mdfield<Scalar>& c)
   {
-    // Demonstrate getting a nonconst view from a const view
-    using data_type = decltype(c.get_static_view().data());
-    using nonconst_data_type = typename remove_low_level_const<data_type>::type;
-    std::cout << "\ndata_type          = " << Teuchos::demangleName(typeid(data_type).name()) << std::endl;  
-    std::cout << "nonconst_data_type = " << Teuchos::demangleName(typeid(nonconst_data_type).name()) << std::endl;  
-
-    // NOTE: the FAD types need the DevLayout for contiguous mapping
-    // on cuda. This is embedded in the MDField.
-    Kokkos::DynRankView<Scalar,typename PHX::DevLayout<Scalar>::type,Kokkos::MemoryUnmanaged> tmp_c;
-    if (Sacado::IsFad<Scalar>::value)
-      tmp_c = Kokkos::DynRankView<Scalar,typename PHX::DevLayout<Scalar>::type,Kokkos::MemoryUnmanaged>(const_cast<nonconst_data_type>(c.get_static_view().data()),c.extent(0),c.extent(1),c.extent(2),num_derivatives);
-    else
-      tmp_c = Kokkos::DynRankView<Scalar,typename PHX::DevLayout<Scalar>::type,Kokkos::MemoryUnmanaged>(const_cast<nonconst_data_type>(c.get_static_view().data()),c.extent(0),c.extent(1),c.extent(2));
-
-    std::cout << "ext_0=" << c.extent(0) << ", ext_1=" << c.extent(1) << ", ext_2=" << c.extent(2) << ", ext_3=" << c.extent(3) << std::endl;
-
     auto tmp_a = a.get_static_view();
     auto tmp_b = b.get_static_view();
+    // Demonstrate getting a nonconst view from a const view
+    auto tmp_c = PHX::getNonConstDynRankViewFromConstMDField<Scalar>(c);
+
     Kokkos::MDRangePolicy<exec_t,Kokkos::Rank<3>> policy({0,0,0},{num_cells,num_pts,num_equations});
     Kokkos::parallel_for("use non-const DynRankView from const View",policy,KOKKOS_LAMBDA (const int cell,const int pt,const int eq) {
       tmp_c(cell,pt,eq) = tmp_a(cell,pt,eq) + tmp_b(cell,pt,eq);
@@ -62,6 +45,7 @@ namespace {
   }
 }
 
+// Check double values to make sure layout hasn't been switched.
 TEUCHOS_UNIT_TEST(NonConstDynRankViewFromView,double) {
   using ScalarType = double; 
   non_const_mdfield<ScalarType> a("a","layout",num_cells,num_pts,num_equations);
@@ -80,6 +64,7 @@ TEUCHOS_UNIT_TEST(NonConstDynRankViewFromView,double) {
       }
 }
 
+// Check DFAD values to make sure layout hasn't been switched.
 TEUCHOS_UNIT_TEST(NonConstDynRankViewFromView,FAD) {
   using RealType = double;
   using ScalarType = Sacado::Fad::DFad<RealType>;
