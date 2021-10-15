2f7ab85d600edc59dc25199e6c38466aed4fc0b7
K. Devine <kddevin@sandia.gov>
2021-10-15 13:48:07 -0600

    tpetra:  unit test for #9583

--- packages/tpetra/core/test/MultiVector/Bug9583.cpp
+++ packages/tpetra/core/test/MultiVector/Bug9583.cpp
@@ -42,7 +42,7 @@
 */
 
 #include "Tpetra_Map.hpp"
-#include "Tpetra_Vector.hpp"
+#include "Tpetra_MultiVector.hpp"
 #include "Tpetra_Core.hpp"
 #include "Teuchos_UnitTestHarness.hpp"
 #include "TpetraCore_ETIHelperMacros.h"
@@ -50,92 +50,39 @@
 
 namespace { // (anonymous)
 
-  // Macro used inside the test(s) below.  It tests for global error,
-  // and if so, prints each process' error message and quits the test
-  // early.
-  //
-  // 'out' only prints on Process 0.  It's really not OK for other
-  // processes to print to stdout, but it usually works and we need to
-  // do it for debugging.
-#define TPETRA_MV_TEST_REPORT_GLOBAL_ERR( WHAT_STRING ) do {       \
-    reduceAll<int, int> (*comm, REDUCE_MIN, lclSuccess, outArg (gblSuccess)); \
-    TEST_EQUALITY_CONST( gblSuccess, 1 );                               \
-    if (gblSuccess != 1) {                                              \
-      out << WHAT_STRING << " FAILED on one or more processes!" << endl; \
-      for (int p = 0; p < numProcs; ++p) {                              \
-        if (myRank == p && lclSuccess != 1) {                           \
-          std::cout << errStrm.str () << std::flush;                    \
-        }                                                               \
-        comm->barrier ();                                               \
-        comm->barrier ();                                               \
-        comm->barrier ();                                               \
-      }                                                                 \
-      return;                                                           \
-    }                                                                   \
-  } while (false)
-
-  //
-  // UNIT TESTS
-  //
-
-  // Test for Bug 5474: getData and getDataNonConst should return
-  // Teuchos::null if the vector is locally empty.
-  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(Vector, Bug9583_1, S, LO, GO, NODE)
+  // Test for Bug #9583
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(MultiVector, Bug9583, S, LO, GO, NODE)
   {
-    using Teuchos::outArg;
     using Teuchos::RCP;
     using Teuchos::rcp;
-    using Teuchos::REDUCE_MIN;
-    using Teuchos::reduceAll;
-    using std::endl;
+
     typedef Tpetra::Map<LO, GO, NODE> map_type;
     typedef Tpetra::MultiVector<S, LO, GO, NODE> vec_type;
-    typedef Tpetra::global_size_t GST;
-
-    out << "Tpetra::Vector: Bug 9583 test (getData and getDataNonConst "
-      "should return Teuchos::null if the vector is locally empty)" << endl;
-
-    int lclSuccess = 1;
-    int gblSuccess = 1;
-    std::ostringstream errStrm; // for error collection
 
     RCP<const Teuchos::Comm<int> > comm = Tpetra::getDefaultComm ();
     const int myRank = comm->getRank ();
-    const int numProcs = comm->getSize ();
 
-    //
-    // Create a Map which has zero rows on a single processe
-    //
+    // Create a Map which has zero rows on ranks >= gblNumRows
     RCP<const map_type> rowMap;
-    const GST gblNumRows = 3;
+    const int gblNumRows = 3;
     const GO indexBase = 0;
 
     size_t numLocalElements = 1;
-    if(myRank == 3){
+    if(myRank >= gblNumRows) {
         numLocalElements = 0;
     }
+    rowMap = rcp (new map_type (gblNumRows, numLocalElements, indexBase, comm));
 
-    try {
-      rowMap = rcp (new map_type (gblNumRows, numLocalElements, indexBase, comm));
-    } catch (std::exception& e) {
-      lclSuccess = 0;
-      errStrm << "Process " << myRank << ": Map constructor threw exception: "
-              << e.what () << endl;
-    }
-    TPETRA_MV_TEST_REPORT_GLOBAL_ERR( "Map constructor threw exception" );
-
-    // Create a zero vector
+    // Create distributed zero vectors
     vec_type x (rowMap, 1, true);
-    TEST_EQUALITY( x.getNumVectors (), static_cast<size_t> (1) );
-
-    // Create a zero vector
     vec_type y (rowMap, 1, true);
-    TEST_EQUALITY( y.getNumVectors (), static_cast<size_t> (1) );
 
+    // Create a local vector z -- one entry that will be same on all ranks
     const size_t numElm = 1;
-    RCP<const map_type > rowMapLocal = Tpetra::createLocalMapWithNode<LO,GO,NODE>( numElm, comm);
-
+    RCP<const map_type > rowMapLocal = 
+        Tpetra::createLocalMapWithNode<LO,GO,NODE>( numElm, comm);
     vec_type z (rowMapLocal, 1, true);
+
     // Fill z with a non-zero number
     z.putScalar(static_cast<S>(999));
 
@@ -143,6 +90,8 @@ namespace { // (anonymous)
     z.multiply( Teuchos::TRANS, Teuchos::NO_TRANS, 1.0, x, y, 0.0);
 
     // Since x and y are zero vectors, we expect z to also be zero
+    // With bug #9583, z has value 999 on ranks >= gblNumRows
+    // With fix, z has value zero on all ranks.
     TEST_EQUALITY( z.getData(0)[0], static_cast<S>(0.0) )
   }
 
@@ -151,7 +100,7 @@ namespace { // (anonymous)
 //
 
 #define UNIT_TEST_GROUP( SCALAR, LO, GO, NODE ) \
-  TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT( Vector, Bug9583_1, SCALAR, LO, GO, NODE)
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(MultiVector, Bug9583, SCALAR, LO, GO, NODE)
 
   TPETRA_ETI_MANGLING_TYPEDEFS()
 
