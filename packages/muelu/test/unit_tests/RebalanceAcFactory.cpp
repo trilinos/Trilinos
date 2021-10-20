3494c770c4877a197940c4ea3e6dc8fefe212e12
meriadeg perrinel <meriadeg.perrinel@ng-analytics.com>
2021-10-20 16:17:57 +0200

    Created another build test for RebalanceAcFactory

--- packages/muelu/test/unit_tests/RebalanceAcFactory.cpp
+++ packages/muelu/test/unit_tests/RebalanceAcFactory.cpp
@@ -83,7 +83,7 @@
 #include <MueLu_DirectSolver.hpp>
 #include <MueLu_AmalgamationFactory.hpp>
 #include <MueLu_RepartitionHeuristicFactory.hpp>
-//#include <MueLu_SaPFactory.hpp>
+#include <MueLu_SaPFactory.hpp>
 //#include <MueLu_RebalanceAcFactory.hpp>
 //#include <MueLu_TrilinosSmoother.hpp>
 //#include <MueLu_CoupledAggregationFactory.hpp>
@@ -94,6 +94,10 @@
 #include <MueLu_IsorropiaInterface.hpp>
 #include <MueLu_RebalanceTransferFactory.hpp>
 #include <MueLu_RepartitionFactory.hpp>
+#include "MueLu_CoordinatesTransferFactory.hpp"
+#include "MueLu_TentativePFactory.hpp"
+#include "MueLu_TransPFactory.hpp"
+#include "MueLu_ZoltanInterface.hpp"
 
 namespace MueLuTests {
 
@@ -182,6 +186,8 @@ namespace MueLuTests {
     RCP<Galeri::Xpetra::Problem<Map,CrsMatrixWrap,MultiVector> > Pr =
         Galeri::Xpetra::BuildProblem<SC, LO, GO, Map, CrsMatrixWrap, MultiVector>(matrixParameters.GetMatrixType(), map, matrixParameters.GetParameterList());
     RCP<Matrix>  A = Pr->BuildMatrix();
+    int nDofsPerNode = 3;
+    A->SetFixedBlockSize(nDofsPerNode);   // 2 velocity dofs and 1 pressure dof per node.
 
     // build default null space
     LocalOrdinal numPDEs = 1;
@@ -196,9 +202,6 @@ namespace MueLuTests {
     level->setDefaultVerbLevel(Teuchos::VERB_HIGH);
     level->Set("A", A);
     level->Set("Nullspace",   nullspace);
-
-
-
 
     // Add Factory
     // build non-rebalanced transfer operators
@@ -277,10 +280,187 @@ namespace MueLuTests {
 
 
   }
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(RebalanceAcFactory, Build2, Scalar, LocalOrdinal, GlobalOrdinal, Node)
+  {
+    #include <MueLu_UseShortNames.hpp>
+    MUELU_TESTING_SET_OSTREAM;
+    MUELU_TESTING_LIMIT_SCOPE(Scalar,GlobalOrdinal,Node);
+    out << "version: " << MueLu::Version() << std::endl;
+
+    // - Smoothed-Aggregation
+    Scalar optSaDamping = 4./3;
+      int optMaxCoarseSize = 50;
+      LO optMinRowsPerProc = 2000;
+      double optNnzImbalance = 1.2;
+      Teuchos::CommandLineProcessor clp(false);
+LO  optMaxLevels     = 10;
+      // Default is Laplace1D with nx = 8748.
+      // It's a nice size for 1D and perfect aggregation. (6561 = 3^8)
+      //Nice size for 1D and perfect aggregation on small numbers of processors. (8748 = 4*3^7)
+      Galeri::Xpetra::Parameters<GO> matrixParameters(clp, 8748); // manage parameters of the test case
+      Xpetra::Parameters xpetraParameters(clp);                   // manage parameters of xpetra
+
+      // create coarsest smoother
+      RCP<SmootherPrototype> coarsestSmooProto;
+      std::string type = "";
+      Teuchos::ParameterList coarsestSmooList;
+  #if defined(HAVE_AMESOS_SUPERLU)
+      coarsestSmooProto = Teuchos::rcp( new DirectSolver("Superlu", coarsestSmooList) );
+  #else
+      coarsestSmooProto = Teuchos::rcp( new DirectSolver("Klu", coarsestSmooList) );
+  #endif
+      RCP<SmootherFactory> coarsestSmooFact = rcp(new SmootherFactory(coarsestSmooProto, Teuchos::null));
+
+
+      RCP<const Map> map;
+      RCP<Matrix> A;
+
+      RCP<MultiVector> coordinates;
+
+      // USER GUIDE // define communicator
+      RCP<const Teuchos::Comm<int> > comm = Teuchos::DefaultComm<int>::getComm();
+      Xpetra::UnderlyingLib lib = xpetraParameters.GetLib();
+
+      map = MapFactory::Build(lib, matrixParameters.GetNumGlobalElements(), 0, comm);
+      Teuchos::RCP<Galeri::Xpetra::Problem<Map,CrsMatrixWrap,MultiVector> > Pr =
+          Galeri::Xpetra::BuildProblem<SC,LO,GO,Map,CrsMatrixWrap,MultiVector>(matrixParameters.GetMatrixType(), map, matrixParameters.GetParameterList()); //TODO: Matrix vs. CrsMatrixWrap
+      A = Pr->BuildMatrix();
+      if (matrixParameters.GetMatrixType() == "Laplace1D") {
+        coordinates = Galeri::Xpetra::Utils::CreateCartesianCoordinates<SC, LO, GO, Map, MultiVector>("1D", map, matrixParameters.GetParameterList());
+      }
+      else if (matrixParameters.GetMatrixType() == "Laplace2D") {
+        coordinates = Galeri::Xpetra::Utils::CreateCartesianCoordinates<SC, LO, GO, Map, MultiVector>("2D", map, matrixParameters.GetParameterList());
+      }
+      else if (matrixParameters.GetMatrixType() == "Laplace3D") {
+        coordinates = Galeri::Xpetra::Utils::CreateCartesianCoordinates<SC, LO, GO, Map, MultiVector>("3D", map, matrixParameters.GetParameterList());
+      }
+
+      // USER GUIDE   // define near null space
+      RCP<MultiVector> nullspace = MultiVectorFactory::Build(map, 1);
+      nullspace->putScalar( (SC) 1.0);
+      // USER GUIDE   //
+//      Teuchos::Array<Teuchos::ScalarTraits<SC>::magnitudeType> norms(1);
+
+//      nullspace->norm1(norms);
+
+      // USER GUIDE   // create new hierarchy
+      RCP<MueLu::Hierarchy<SC, LO, GO, NO> > H;
+      // USER GUIDE   //
+
+
+    // USER GUIDE     // instantiate new Hierarchy object
+    H = rcp(new Hierarchy());
+    H->setDefaultVerbLevel(Teuchos::VERB_HIGH);
+    H->SetMaxCoarseSize((GO) optMaxCoarseSize);
+
+    // USER GUIDE     // create a fine level object
+    RCP<Level> Finest = H->GetLevel();
+    Finest->setDefaultVerbLevel(Teuchos::VERB_HIGH);
+    Finest->Set("A",           A);
+    Finest->Set("Nullspace",   nullspace);
+    Finest->Set("Coordinates", coordinates); //FIXME: XCoordinates, YCoordinate
+
+    FactoryManager M;
+    M.SetKokkosRefactor(false);
+    M.SetFactory("CoarseSolver", coarsestSmooFact);
+
+      // USER GUIDE       // declare some factories (potentially overwrite default factories)
+      RCP<PgPFactory> PFact = rcp(new PgPFactory());
+//      PFact->SetParameter("sa: damping factor", Teuchos::ParameterEntry(optSaDamping));
+
+      RCP<Factory>    RFact = rcp(new GenericRFactory());
+
+      RCP<RAPFactory> AFact = rcp(new RAPFactory());
+      AFact->setVerbLevel(Teuchos::VERB_HIGH);
+
+#if defined(HAVE_MPI) && defined(HAVE_MUELU_ZOLTAN)
+      // Repartitioning
+
+      // The Factory Manager will be configured to return the rebalanced versions of P, R, A by default.
+      // Everytime we want to use the non-rebalanced versions, we need to explicitly define the generating factory.
+      RFact->SetFactory("P", PFact);
+      //
+      AFact->SetFactory("P", PFact);
+      AFact->SetFactory("R", RFact);
+
+      // Transfer coordinates
+      RCP<CoordinatesTransferFactory> TransferCoordinatesFact = rcp(new CoordinatesTransferFactory());
+      AFact->AddTransferFactory(TransferCoordinatesFact); // FIXME REMOVE
+
+      // define rebalancing factory for coarse block matrix A(1,1)
+      RCP<AmalgamationFactory> rebAmalgFact = rcp(new AmalgamationFactory());
+      rebAmalgFact->SetFactory("A", AFact);
+
+      // Repartitioning (decides how many partitions are built)
+      RCP<Factory> RepartitionHeuristicFact = rcp(new RepartitionHeuristicFactory());
+      {
+        Teuchos::ParameterList paramList;
+        paramList.set("repartition: min rows per proc", optMinRowsPerProc);
+        paramList.set("repartition: max imbalance", optNnzImbalance);
+        RepartitionHeuristicFact->SetParameterList(paramList);
+      }
+      RepartitionHeuristicFact->SetFactory("A", AFact);
+
+      // create amalgamated "Partition"
+      RCP<MueLu::IsorropiaInterface<LO, GO, NO> > isoInterface = rcp(new MueLu::IsorropiaInterface<LO, GO, NO>());
+      isoInterface->SetFactory("A", AFact);
+      isoInterface->SetFactory("number of partitions", RepartitionHeuristicFact);
+      isoInterface->SetFactory("UnAmalgamationInfo", rebAmalgFact);
+
+      // create "Partition" by unamalgamtion
+      RCP<MueLu::RepartitionInterface<LO, GO, NO> > repInterface = rcp(new MueLu::RepartitionInterface<LO, GO, NO>());
+      repInterface->SetFactory("A", AFact);
+      repInterface->SetFactory("number of partitions", RepartitionHeuristicFact);
+      repInterface->SetFactory("AmalgamatedPartition", isoInterface);
+
+      // Repartitioning (creates "Importer" from "Partition")
+      RCP<Factory> RepartitionFact = rcp(new RepartitionFactory());
+      RepartitionFact->SetFactory("A", AFact);
+      RepartitionFact->SetFactory("number of partitions", RepartitionHeuristicFact);
+      RepartitionFact->SetFactory("Partition", repInterface);
+
+      // Reordering of the transfer operators
+      RCP<Factory> RebalancedPFact = rcp(new RebalanceTransferFactory());
+      RebalancedPFact->SetParameter("type", Teuchos::ParameterEntry(std::string("Interpolation")));
+      RebalancedPFact->SetFactory("P", PFact);
+      RebalancedPFact->SetFactory("Coordinates", TransferCoordinatesFact);
+      RebalancedPFact->SetFactory("Nullspace", M.GetFactory("Ptent")); // TODO
+
+      RCP<Factory> RebalancedRFact = rcp(new RebalanceTransferFactory());
+      RebalancedRFact->SetParameter("type", Teuchos::ParameterEntry(std::string("Restriction")));
+      RebalancedRFact->SetFactory("R", RFact);
+
+      // Compute Ac from rebalanced P and R
+      RCP<RebalanceAcFactory> RebalancedAFact = rcp(new RebalanceAcFactory());
+      RebalancedAFact->SetFactory("A", AFact);
+
+      // Configure FactoryManager
+      M.SetFactory("A", RebalancedAFact);
+      M.SetFactory("P", RebalancedPFact);
+      M.SetFactory("R", RebalancedRFact);
+      M.SetFactory("Nullspace",   RebalancedPFact);
+      M.SetFactory("Coordinates", RebalancedPFact);
+      M.SetFactory("Importer",    RepartitionFact);
+
+  #else
+          TEUCHOS_TEST_FOR_EXCEPT(true);
+  #endif
+      //
+      // Setup preconditioner
+      //
+
+      // USER GUIDE     // setup multigrid hierarchy
+      int startLevel = 0;
+      H->Setup(M, startLevel, optMaxLevels);
+//      RebalancedAFact->Build(*Finest.get(), *Finest.get());
+
+      // USER GUIDE     //
+  }
 
 #define MUELU_ETI_GROUP(Scalar, LO, GO, Node) \
   TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(RebalanceAcFactory, Constructor, Scalar, LO, GO, Node) \
-//  TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(RebalanceAcFactory, Build, Scalar, LO, GO, Node)
+  TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(RebalanceAcFactory, Build2, Scalar, LO, GO, Node)
+//    TEUCHOS_UNIT_TEST_TEMPLATE_4_INSTANT(RebalanceAcFactory, Build, Scalar, LO, GO, Node)
 
 #include <MueLu_ETI_4arg.hpp>
 
