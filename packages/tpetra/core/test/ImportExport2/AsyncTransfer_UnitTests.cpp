6e0ee32c24250a0de26a2c79d85076e23dcb7e77
Timothy A. Smith <tasmit@sandia.gov>
2021-10-25 10:56:45 -0600

    Tpetra: clean up includes/typedefs/setup functions for AsyncTransfer tests

--- packages/tpetra/core/test/ImportExport2/AsyncTransfer_UnitTests.cpp
+++ packages/tpetra/core/test/ImportExport2/AsyncTransfer_UnitTests.cpp
@@ -42,164 +42,41 @@
 #include <Tpetra_TestingUtilities.hpp>
 #include <Teuchos_UnitTestHarness.hpp>
 
+#include <Teuchos_as.hpp>
 #include <Teuchos_OrdinalTraits.hpp>
 #include <Teuchos_ScalarTraits.hpp>
-#include <Teuchos_VerboseObject.hpp>
-#include <Teuchos_as.hpp>
 #include <Teuchos_Tuple.hpp>
-#include "Tpetra_CrsGraph.hpp"
-#include "Tpetra_CrsMatrix.hpp"
-#include "Tpetra_MultiVector.hpp"
+
 #include "Tpetra_Core.hpp"
-#include "Tpetra_Distributor.hpp"
-#include "Tpetra_Map.hpp"
-#include "Tpetra_Util.hpp"
-#include "Tpetra_Import.hpp"
-#include "Tpetra_Import_Util.hpp"
-#include "Tpetra_Import_Util2.hpp"
-#include "Tpetra_Details_packCrsMatrix.hpp"
-#include "Tpetra_Details_unpackCrsMatrixAndCombine.hpp"
+#include "Tpetra_CrsMatrix.hpp"
 #include "Tpetra_Export.hpp"
-#include "Tpetra_RowMatrixTransposer.hpp"
-#include "TpetraExt_MatrixMatrix.hpp"
-#include "Tpetra_Details_Behavior.hpp"
-#include "Tpetra_Details_gathervPrint.hpp"
-#include <memory>
-#include <sstream>
+#include "Tpetra_Import.hpp"
+#include "Tpetra_Map.hpp"
+#include "Tpetra_MultiVector.hpp"
 
 namespace {
 
+  using std::endl;
+
   using Teuchos::as;
   using Teuchos::Array;
-  using Teuchos::ArrayRCP;
-  using Teuchos::ArrayView;
-  using Teuchos::arrayViewFromVector;
   using Teuchos::Comm;
   using Teuchos::FancyOStream;
-  using Teuchos::getFancyOStream;
-  using Teuchos::includesVerbLevel;
-  using Teuchos::OrdinalTraits;
-  using Teuchos::OSTab;
-  using Teuchos::outArg;
-  using Teuchos::ParameterList;
-  using Teuchos::parameterList;
   using Teuchos::RCP;
   using Teuchos::rcp;
-  using Teuchos::rcpFromRef;
-  using Teuchos::REDUCE_MAX;
-  using Teuchos::reduceAll;
+  using Teuchos::OrdinalTraits;
   using Teuchos::ScalarTraits;
   using Teuchos::tuple;
 
-  using Tpetra::ADD;
   using Tpetra::DistObject;
-  using Tpetra::createContigMap;
-  using Tpetra::CrsGraph;
   using Tpetra::CrsMatrix;
-  using Tpetra::MultiVector;
   using Tpetra::Export;
+  using Tpetra::getDefaultComm;
+  using Tpetra::global_size_t;
   using Tpetra::Import;
   using Tpetra::INSERT;
   using Tpetra::Map;
-  using Tpetra::REPLACE;
-  using Tpetra::StaticProfile;
-
-  using std::cerr;
-  using std::cout;
-  using std::ostream_iterator;
-  using std::endl;
-
-  using Node = Tpetra::Map<>::node_type;
-
-  // Command-line argument values (initially set to defaults).
-  bool testMpi = true;
-  double errorTolSlack = 1e+1;
-  std::string distributorSendType ("Send");
-  bool barrierBetween = true;
-  bool verbose = false;
-
-  TEUCHOS_STATIC_SETUP()
-  {
-    Teuchos::CommandLineProcessor &clp = Teuchos::UnitTestRepository::getCLP();
-    clp.addOutputSetupOptions(true);
-    clp.setOption(
-        "test-mpi", "test-serial", &testMpi,
-        "Test MPI (if available) or force test of serial.  In a serial build,"
-        " this option is ignored and a serial comm is always used." );
-    clp.setOption(
-        "error-tol-slack", &errorTolSlack,
-        "Slack off of machine epsilon used to check test results" );
-    // FIXME (mfh 02 Apr 2012) It would be better to ask Distributor
-    // for the list of valid send types, but setOption() needs a const
-    // char[], not an std::string.
-    clp.setOption ("distributor-send-type", &distributorSendType,
-                   "In MPI tests, the type of send operation that the Tpetra::"
-                   "Distributor will use.  Valid values include \"Isend\", "
-                   "\"Rsend\", \"Send\", and \"Ssend\".");
-    clp.setOption ("barrier-between", "no-barrier-between", &barrierBetween,
-                   "In MPI tests, whether Tpetra::Distributor will execute a "
-                   "barrier between posting receives and posting sends.");
-    clp.setOption ("verbose", "quiet", &verbose, "Whether to print verbose "
-                   "output.");
-  }
-
-  RCP<const Comm<int> >
-  getDefaultComm()
-  {
-    if (testMpi) {
-      return Tpetra::getDefaultComm ();
-    }
-    else {
-      return rcp (new Teuchos::SerialComm<int> ());
-    }
-  }
-
-  RCP<ParameterList>
-  getDistributorParameterList ()
-  {
-    static RCP<ParameterList> plist; // NOT THREAD SAFE, but that's OK here.
-    if (plist.is_null ()) {
-      plist = parameterList ("Tpetra::Distributor");
-      plist->set ("Send type", distributorSendType);
-      plist->set ("Barrier between receives and sends", barrierBetween);
-
-      if (verbose && getDefaultComm()->getRank() == 0) {
-        cout << "ParameterList for Distributor: " << *plist << endl;
-      }
-
-      if (verbose) {
-        // Tell Distributor to print verbose output.
-        Teuchos::VerboseObject<Tpetra::Distributor>::setDefaultVerbLevel (Teuchos::VERB_EXTREME);
-      }
-    }
-    return plist;
-  }
-
-  RCP<ParameterList> getImportParameterList () {
-    return getDistributorParameterList (); // For now.
-  }
-
-  RCP<ParameterList> getExportParameterList () {
-    return parameterList (* (getDistributorParameterList ())); // For now.
-  }
-
-  RCP<ParameterList> getCrsGraphParameterList () {
-    static RCP<ParameterList> plist; // NOT THREAD SAFE, but that's OK here.
-    if (plist.is_null ()) {
-      plist = parameterList ("Tpetra::CrsGraph");
-      plist->set ("Import", * (getImportParameterList ()));
-      plist->set ("Export", * (getExportParameterList ()));
-
-      if (verbose && getDefaultComm()->getRank() == 0) {
-        cout << "ParameterList for CrsGraph: " << *plist << endl;
-      }
-    }
-    return plist;
-  }
-
-  RCP<ParameterList> getCrsMatrixParameterList () {
-    return parameterList (* (getCrsGraphParameterList ())); // For now.
-  }
+  using Tpetra::MultiVector;
 
 
   //
@@ -214,7 +91,7 @@ namespace {
     using mv_type = MultiVector<Scalar, LO, GO>;
 
   public:
-    MultiVectorTransferFixture(Teuchos::FancyOStream& o, bool& s)
+    MultiVectorTransferFixture(FancyOStream& o, bool& s)
       : out(o),
         success(s),
         comm(getDefaultComm()),
@@ -252,7 +129,7 @@ namespace {
   private:
     void setupMaps(int collectRank) {
       const GO indexBase = 0;
-      const Tpetra::global_size_t INVALID = Teuchos::OrdinalTraits<Tpetra::global_size_t>::invalid();
+      const global_size_t INVALID = OrdinalTraits<global_size_t>::invalid();
 
       const size_t sourceNumLocalElements = 3;
       const size_t totalElements = numProcs*sourceNumLocalElements;
@@ -267,7 +144,7 @@ namespace {
       sourceMV->randomize();
 
       targetMV = rcp(new mv_type(targetMap, 1));
-      targetMV->putScalar(Teuchos::ScalarTraits<Scalar>::zero());
+      targetMV->putScalar(ScalarTraits<Scalar>::zero());
     }
 
     void compareMultiVectors(RCP<const mv_type> resultMV, RCP<const mv_type> referenceMV) {
@@ -280,7 +157,7 @@ namespace {
       }
     }
 
-    Teuchos::FancyOStream& out;
+    FancyOStream& out;
     bool& success;
 
     RCP<const Comm<int>> comm;
@@ -303,12 +180,12 @@ namespace {
   public:
     RCP<mv_type> generateWithClassicalCodePath(RCP<mv_type> sourceMV, RCP<const map_type> targetMap) const {
       RCP<const map_type> sourceMap = sourceMV->getMap();
-      Import<LO, GO> importer(sourceMap, targetMap, getImportParameterList());
+      Import<LO, GO> importer(sourceMap, targetMap);
       expertSetRemoteLIDsContiguous(importer, false);
       TEUCHOS_ASSERT(!importer.areRemoteLIDsContiguous());
 
       RCP<mv_type> referenceMV = rcp(new mv_type(targetMap, 1));
-      referenceMV->putScalar(Teuchos::ScalarTraits<Scalar>::zero());
+      referenceMV->putScalar(ScalarTraits<Scalar>::zero());
       referenceMV->doImport(*sourceMV, importer, INSERT);
       TEUCHOS_ASSERT(!referenceMV->importsAreAliased());
 
@@ -325,12 +202,12 @@ namespace {
   public:
     RCP<mv_type> generateWithClassicalCodePath(RCP<mv_type> sourceMV, RCP<const map_type> targetMap) const {
       RCP<const map_type> sourceMap = sourceMV->getMap();
-      Export<LO, GO> exporter(sourceMap, targetMap, getExportParameterList());
+      Export<LO, GO> exporter(sourceMap, targetMap);
       expertSetRemoteLIDsContiguous(exporter, false);
       TEUCHOS_ASSERT(!exporter.areRemoteLIDsContiguous());
 
       RCP<mv_type> referenceMV = rcp(new mv_type(targetMap, 1));
-      referenceMV->putScalar(Teuchos::ScalarTraits<Scalar>::zero());
+      referenceMV->putScalar(ScalarTraits<Scalar>::zero());
       referenceMV->doExport(*sourceMV, exporter, INSERT);
       TEUCHOS_ASSERT(!referenceMV->importsAreAliased());
 
@@ -346,7 +223,7 @@ namespace {
     using crs_type = CrsMatrix<Scalar, LO, GO>;
 
   public:
-    DiagonalCrsMatrixTransferFixture(Teuchos::FancyOStream& o, bool& s)
+    DiagonalCrsMatrixTransferFixture(FancyOStream& o, bool& s)
       : out(o),
         success(s),
         comm(getDefaultComm()),
@@ -387,7 +264,7 @@ namespace {
   private:
     void setupMaps() {
       const GO indexBase = 0;
-      const Tpetra::global_size_t INVALID = Teuchos::OrdinalTraits<Tpetra::global_size_t>::invalid();
+      const global_size_t INVALID = OrdinalTraits<global_size_t>::invalid();
 
       const size_t targetNumLocalElements = 3;
       const size_t totalElements = numProcs*targetNumLocalElements;
@@ -398,8 +275,8 @@ namespace {
     }
 
     void setupMatrices() {
-      sourceMat = rcp(new crs_type(sourceMap, 1, StaticProfile, getCrsMatrixParameterList()));
-      targetMat = rcp(new crs_type(targetMap, 1, StaticProfile, getCrsMatrixParameterList()));
+      sourceMat = rcp(new crs_type(sourceMap, 1));
+      targetMat = rcp(new crs_type(targetMap, 1));
 
       for (GO row = sourceMap->getMinGlobalIndex(); row <= sourceMap->getMaxGlobalIndex(); row++) {
         sourceMat->insertGlobalValues(row, tuple<GO>(row), tuple<Scalar>(row));
@@ -468,7 +345,7 @@ namespace {
       }
     }
 
-    Teuchos::FancyOStream& out;
+    FancyOStream& out;
     bool& success;
 
     RCP<const Comm<int>> comm;
@@ -491,7 +368,7 @@ namespace {
   public:
     RCP<crs_type> generateUsingAllInOne(RCP<crs_type> sourceMat, RCP<const map_type> targetMap) const {
       RCP<const map_type> sourceMap = sourceMat->getMap();
-      Import<LO, GO> importer(sourceMap, targetMap, getImportParameterList());
+      Import<LO, GO> importer(sourceMap, targetMap);
 
       Teuchos::ParameterList dummy;
       RCP<crs_type> referenceMat = Tpetra::importAndFillCompleteCrsMatrix<crs_type>(
@@ -509,7 +386,7 @@ namespace {
   public:
     RCP<crs_type> generateUsingAllInOne(RCP<crs_type> sourceMat, RCP<const map_type> targetMap) const {
       RCP<const map_type> sourceMap = sourceMat->getMap();
-      Export<LO, GO> exporter(sourceMap, targetMap, getExportParameterList());
+      Export<LO, GO> exporter(sourceMap, targetMap);
 
       Teuchos::ParameterList dummy;
       RCP<crs_type> referenceMat = Tpetra::exportAndFillCompleteCrsMatrix<crs_type>(
@@ -526,7 +403,7 @@ namespace {
     using crs_type = CrsMatrix<Scalar, LO, GO>;
 
   public:
-    LowerTriangularCrsMatrixTransferFixture(Teuchos::FancyOStream& o, bool& s)
+    LowerTriangularCrsMatrixTransferFixture(FancyOStream& o, bool& s)
       : out(o),
         success(s),
         comm(getDefaultComm()),
@@ -589,7 +466,7 @@ namespace {
   private:
     void setupMaps() {
       const GO indexBase = 0;
-      const Tpetra::global_size_t INVALID = Teuchos::OrdinalTraits<Tpetra::global_size_t>::invalid();
+      const global_size_t INVALID = OrdinalTraits<global_size_t>::invalid();
 
       const size_t sourceNumLocalElements = (myRank%2 == 0) ? 3 : 5;
       const size_t targetNumLocalElements = 4;
@@ -599,8 +476,8 @@ namespace {
     }
 
     void setupMatrices() {
-      sourceMat = rcp(new crs_type(sourceMap, 24, StaticProfile, getCrsMatrixParameterList()));
-      targetMat = rcp(new crs_type(targetMap, 24, StaticProfile, getCrsMatrixParameterList()));
+      sourceMat = rcp(new crs_type(sourceMap, 24));
+      targetMat = rcp(new crs_type(targetMap, 24));
 
       Array<GO> cols(1);
       Array<Scalar>  vals(1);
@@ -618,7 +495,7 @@ namespace {
       sourceMat->fillComplete();
     }
 
-    Teuchos::FancyOStream& out;
+    FancyOStream& out;
     bool& success;
 
     RCP<const Comm<int>> comm;
@@ -645,7 +522,7 @@ namespace {
 
   public:
     void operator()(DistObjectRCP source, DistObjectRCP target) const {
-      Import<LO, GO> importer(source->getMap(), target->getMap(), getImportParameterList());
+      Import<LO, GO> importer(source->getMap(), target->getMap());
       target->beginImport(*source, importer, INSERT);
       target->endImport(*source, importer, INSERT);
     }
@@ -711,7 +588,7 @@ namespace {
 
   public:
     void operator()(DistObjectRCP source, DistObjectRCP target) const {
-      Export<LO, GO> exporter(target->getMap(), source->getMap(), getExportParameterList());
+      Export<LO, GO> exporter(target->getMap(), source->getMap());
       target->beginImport(*source, exporter, INSERT);
       target->endImport(*source, exporter, INSERT);
     }
@@ -777,7 +654,7 @@ namespace {
 
   public:
     void operator()(DistObjectRCP source, DistObjectRCP target) const {
-      Export<LO, GO> exporter(source->getMap(), target->getMap(), getExportParameterList());
+      Export<LO, GO> exporter(source->getMap(), target->getMap());
       target->beginExport(*source, exporter, INSERT);
       target->endExport(*source, exporter, INSERT);
     }
@@ -843,7 +720,7 @@ namespace {
 
   public:
     void operator()(DistObjectRCP source, DistObjectRCP target) const {
-      Import<LO, GO> importer(target->getMap(), source->getMap(), getImportParameterList());
+      Import<LO, GO> importer(target->getMap(), source->getMap());
       target->beginExport(*source, importer, INSERT);
       target->endExport(*source, importer, INSERT);
     }
