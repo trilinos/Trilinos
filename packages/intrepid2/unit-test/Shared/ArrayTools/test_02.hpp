65e22e2a9299f9fc43d76ec566a68134deb2f621
Kyungjoo Kim <kyukim@sandia.gov>
2022-02-21 15:56:29 -0700

    Intrepid2 - eti of testing

--- packages/intrepid2/unit-test/Shared/ArrayTools/test_02.hpp
+++ packages/intrepid2/unit-test/Shared/ArrayTools/test_02.hpp
@@ -115,7 +115,12 @@ namespace Intrepid2 {
       typedef RealSpaceTools<DeviceType> rst;
       typedef ArrayTools<DeviceType> art; 
       typedef Kokkos::DynRankView<value_type,DeviceType> DynRankView;
+#if defined(INTREPID2_ENABLE_SACADO_ETI_TEST)
+#define NumDerivative 10
+#define ConstructWithLabel(obj, ...) obj(#obj, __VA_ARGS__, NumDerivative+1)
+#else
 #define ConstructWithLabel(obj, ...) obj(#obj, __VA_ARGS__)
+#endif 
       
       const value_type tol = tolerence()*10000.0;
       int errorFlag = 0;
