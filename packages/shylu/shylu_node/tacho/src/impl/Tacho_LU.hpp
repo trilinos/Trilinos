6f9857deff62f4a0767c9e85babdd2fc844bf607
Kyungjoo Kim <kyukim@sandia.gov>
2022-04-05 00:50:07 -0600

    Tacho - add LU

---
+++ packages/shylu/shylu_node/tacho/src/impl/Tacho_LU.hpp
@@ -0,0 +1,23 @@
+#ifndef __TACHO_LU_HPP__
+#define __TACHO_LU_HPP__
+
+/// \file Tacho_LU.hpp
+/// \brief Front interface for LU dense factorization
+/// \author Kyungjoo Kim (kyukim@sandia.gov)
+
+#include "Tacho_Util.hpp"
+
+namespace Tacho {
+  
+  ///
+  /// LU:
+  /// 
+  /// 
+  
+  /// various implementation for different uplo and algo parameters
+  template<typename ArgAlgo>
+  struct LU;
+  
+}
+
+#endif
