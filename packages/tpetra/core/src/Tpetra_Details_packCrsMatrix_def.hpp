192b8b0a08c86cee528fa96c5dfa04fefcbc515c
Chris Siefert <csiefer@sandia.gov>
2022-04-12 13:19:41 -0600

    Tpetra: Fixing issue

--- packages/tpetra/core/src/Tpetra_Details_packCrsMatrix_def.hpp
+++ packages/tpetra/core/src/Tpetra_Details_packCrsMatrix_def.hpp
@@ -863,11 +863,10 @@ packCrsMatrix (const CrsMatrix<ST, LO, GO, NT>& sourceMatrix,
                size_t& constantNumPackets)
 {
 
-  using device_type = typename local_matrix_type::device_type;
-  using execution_space = typename device_type::execution_space;
-
+  using device_type = typename CrsMatrix<ST,LO,GO,NT>::local_matrix_device_type::device_type;
   using buffer_device_type = typename DistObject<char, LO, GO, NT>::buffer_device_type;
   using host_exec_space = typename Kokkos::View<size_t*, device_type>::HostMirror::execution_space;
+  using device_exec_space = typename device_type::execution_space;
   using host_dev_type = Kokkos::Device<host_exec_space, Kokkos::HostSpace>;
 
   // Convert all Teuchos::Array to Kokkos::View
@@ -904,7 +903,7 @@ packCrsMatrix (const CrsMatrix<ST, LO, GO, NT>& sourceMatrix,
     (numPacketsPerLID.getRawPtr (),
      numPacketsPerLID.size ());
   // DEEP_COPY REVIEW - DEVICE-TO-HOST
-  Kokkos::deep_copy (execution_space(), num_packets_per_lid_h, num_packets_per_lid_d);
+  Kokkos::deep_copy (device_exec_space(), num_packets_per_lid_h, num_packets_per_lid_d);
 
   // FIXME (mfh 23 Aug 2017) If we're forced to use a DualView for
   // exports_dv above, then we have two host copies for exports_h.
@@ -918,7 +917,7 @@ packCrsMatrix (const CrsMatrix<ST, LO, GO, NT>& sourceMatrix,
   Kokkos::View<char*, host_dev_type> exports_h (exports.getRawPtr (),
                                                 exports.size ());
   // DEEP_COPY REVIEW - DEVICE-TO-HOST
-  Kokkos::deep_copy (execution_space(), exports_h, exports_dv.d_view);
+  Kokkos::deep_copy (device_exec_space(), exports_h, exports_dv.d_view);
 }
 
 template<typename ST, typename LO, typename GO, typename NT>
