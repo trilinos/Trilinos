1ec531e9842ee5cd2b1554670534fe255f5d580b
Aurya Javeed <asjavee@sandia.gov>
2022-04-13 16:01:22 -0600

    Revert "Removed the old Coleman-Li implementation"
    This reverts commit 48dcaa87568a910c69e2750d791dea2e238bd6f8.

--- packages/rol/src/step/trustregion/ROL_TrustRegion.hpp
+++ packages/rol/src/step/trustregion/ROL_TrustRegion.hpp
@@ -51,6 +51,7 @@
 #include "ROL_Types.hpp"
 #include "ROL_TrustRegionTypes.hpp"
 #include "ROL_TrustRegionModel.hpp"
+#include "ROL_ColemanLiModel.hpp"
 #include "ROL_KelleySachsModel.hpp"
 
 namespace ROL {
