997e4c4dabaf6cbc4bccb7d70ac1a2b0fd7e7af9
Aurya Javeed <asjavee@sandia.gov>
2022-04-27 17:28:15 -0600

    Reverted Coleman-Li changes to adapters/tpetra
    - I used git to revert these files. The packages/rol/adapters/tpetra
    directory should now be identical to what it is at f0317dc (the commit
    before changes from the coleman-li branch).

--- packages/rol/adapters/tpetra/src/function/ROL_TpetraBoundConstraint.hpp
+++ packages/rol/adapters/tpetra/src/function/ROL_TpetraBoundConstraint.hpp
@@ -131,60 +131,26 @@ namespace ROL {
 
         //----------------------------------------------------------------------
         //
-        // Helper functions
+        // Project x onto the bounds
         template<class Real, class V>
-        struct MinFunction {
-            KOKKOS_INLINE_FUNCTION
-            Real min(Real a,  Real b) const {
-                return (a < b) ? a : b;
-            }
-        };   // End helper functions
-
-        //----------------------------------------------------------------------
-        //
-        // Project x to the bounds
-        template<class Real, class V>
-        struct Project : MinFunction<Real, V> {
+        struct Project {
             typedef typename V::execution_space execution_space;
             V X_; // Optimization variable
             V L_; // Lower bounds
             V U_; // Upper bounds
-            Real eps_, tol_, min_diff_;
 
-            Project(V& X, const V& L, const V& U, Real eps = 0, Real tol = 0,
-                                                  Real min_diff = 0) :
-                X_(X), L_(L), U_(U), eps_(eps), tol_(tol), min_diff_(min_diff) {}
-
-            KOKKOS_INLINE_FUNCTION
-            Real max(Real a, Real b) const {
-                return -this->min(-a, -b);
-            }
+            Project(V& X, const V& L, const V& U) : X_(X), L_(L), U_(U) {}
 
             KOKKOS_INLINE_FUNCTION
             void operator() (const int i) const {
-                const Real zero(0), one(1);
-                Real val1, val2;
                 const int M = L_.extent(1);
                 for(int j=0;j<M;++j) {
-                    val1 = L_(i,j);
-                    if (eps_ != zero) {
-                        val1 = ((L_(i,j) < -tol_) ? (one-eps_)*L_(i,j)
-                             : ((L_(i,j) > +tol_) ? (one+eps_)*L_(i,j)
-                             :   L_(i,j)+eps_));
-                        val2 = L_(i,j)+eps_*min_diff_;
-                        val1 = this->min(val1,val2);
+                    if( X_(i,j)<L_(i,j) ) {
+                        X_(i,j) = L_(i,j);
                     }
-                    X_(i,j) = max(X_(i,j), val1);
-
-                    val1 = U_(i,j);
-                    if (eps_ != zero) {
-                        val1 = ((U_(i,j) < -tol_) ? (one+eps_)*U_(i,j)
-                             : ((U_(i,j) > +tol_) ? (one-eps_)*U_(i,j)
-                             :   U_(i,j)-eps_));
-                        val2 = U_(i,j)-eps_*min_diff_;
-                        val1 = max(val1,val2);
+                    else if( X_(i,j)>U_(i,j) ) {
+                        X_(i,j) = U_(i,j);
                     }
-                    X_(i,j) = this->min(X_(i,j), val1);
                 }
             }
         };   // End struct Project
@@ -358,120 +324,6 @@ namespace ROL {
             }
         }; // End struct GradPruneActive
 
-        //----------------------------------------------------------------------
-        //
-        // Build the scaling function for Coleman-Li.
-        template<class Real, class V>
-        struct BuildScalingFunction : MinFunction<Real,V> {
-            typedef typename V::execution_space execution_space;
-            V D_; // Scaling function
-            V X_; // Optimization variable
-            V G_; // Dual vector at which the scaling function is evaluated
-            V L_; // Lower bounds
-            V U_; // Upper bounds
-            Real kappa_, zeta_;
-
-            BuildScalingFunction(V &D, const V &X, const V &G,
-                                       const V &L, const V &U,
-                                       Real kappa, Real zeta) :
-                D_(D), X_(X), G_(G), L_(L), U_(U), kappa_(kappa), zeta_(zeta)
-                {}
-
-            KOKKOS_INLINE_FUNCTION
-            Real abs(Real a) const {
-                return sgn(a)*a;
-            }
-            KOKKOS_INLINE_FUNCTION
-            Real buildC(int i, int j) const {
-                return this->min(zeta_*(U_(i,j) - L_(i,j)), kappa_);
-            }
-            KOKKOS_INLINE_FUNCTION
-            Real sgn(Real a) const {
-                return (a < 0) ? -1 : (a > 0 ? 1 : 0);
-            }
-
-            KOKKOS_INLINE_FUNCTION
-            void operator() (const int i) const {
-                Real c, lodiff, updiff;
-                const int M = L_.extent(1);
-                for(int j=0;j<M;++j) {
-                    c = buildC(i,j);
-                    lodiff = X_(i,j) - L_(i,j);
-                    updiff = U_(i,j) - X_(i,j);
-                    if (-G_(i,j) > lodiff) {
-                        if (lodiff <= updiff) {
-                            D_(i,j) = this->min(abs(G_(i,j)),c);
-                            continue;
-                        }
-                    }
-                    if (+G_(i,j) > updiff) {
-                        if (updiff <= lodiff) {
-                            D_(i,j) = this->min(abs(G_(i,j)),c);
-                            continue;
-                        }
-                    }
-                    D_(i,j) = this->min(lodiff, this->min(updiff, c));
-                }
-            }
-        }; // End struct BuildScalingFunction
-
-        //----------------------------------------------------------------------
-        //
-        // Apply the inverse of the scaling function for Coleman-Li.
-        template<class Real, class V>
-        struct ApplyInverseScalingFunction : BuildScalingFunction<Real,V> {
-            V W_; // Primal vector being scaled
-
-            ApplyInverseScalingFunction(V &D, const V &W, const V &X, const V &G,
-                                              const V &L, const V &U,
-                                              Real kappa, Real zeta) :
-                BuildScalingFunction<Real,V>(D, X, G, L, U, kappa, zeta), W_(W) {};
-
-            KOKKOS_INLINE_FUNCTION
-            void operator() (const int i) const {
-                BuildScalingFunction<Real,V>::operator()(i);
-                const int M = this->L_.extent(1);
-                for(int j=0;j<M;++j) {
-                    this->D_(i,j) = this->W_(i,j)/this->D_(i,j);
-                }
-            }
-        }; // End struct ApplyInverseScalingFunction
-
-        //----------------------------------------------------------------------
-        //
-        // Apply the Jacobian of the scaling function for Coleman-Li.
-        template<class Real, class V>
-        struct ApplyScalingFunctionJacobian : BuildScalingFunction<Real,V> {
-            V W_; // Primal vector being scaled
-
-            ApplyScalingFunctionJacobian(V &D, const V &W, const V &X, const V &G,
-                                               const V &L, const V &U,
-                                               Real kappa, Real zeta) :
-                BuildScalingFunction<Real,V>(D, X, G, L, U, kappa, zeta), W_(W) {};
-
-            KOKKOS_INLINE_FUNCTION
-            void operator() (const int i) const {
-                BuildScalingFunction<Real,V>::operator()(i);
-                const Real zero(0), one(1);
-                Real indicator, d1prime;
-                const int M = this->L_.extent(1);
-                for(int j=0;j<M;++j) {
-                    indicator = (this->D_(i,j) < this->buildC(i,j)) ? one : zero;
-                    if (indicator == zero) {
-                        this->D_(i,j) = zero;
-                        continue;
-                    }
-                    d1prime = this->sgn(this->G_(i,j));
-                    if (d1prime == zero) {
-                        d1prime = one;
-                        if (this->U_(i,j) - this->X_(i,j) < this->X_(i,j) - this->L_(i,j))
-                            d1prime = -one;
-                    }
-                    this->D_(i,j) = d1prime*this->G_(i,j)*this->W_(i,j);
-                }
-            }
-        }; // End struct ApplyScalingFunctionJacobian
-
     } // End namespace KokkosStructs
 
 
@@ -480,6 +332,7 @@ namespace ROL {
 
     template <class Real, class LO, class GO, class Node>
     class TpetraBoundConstraint : public BoundConstraint<Real> {
+
 
         typedef Tpetra::MultiVector<Real,LO,GO,Node> MV;
         typedef ROL::Ptr<MV> MVP;
@@ -496,23 +349,21 @@ namespace ROL {
 
             ViewType l_;           // Kokkos view of Lower bounds
             ViewType u_;           // Kokkos view of Upper bounds
-            Real scale_;
-            Real feasTol_;
-            ROL::Ptr<const Teuchos::Comm<int> > comm_;
             Real min_diff_;
+            Real scale_;
+            ROL::Ptr<const Teuchos::Comm<int> > comm_;
 
-        ROL::Ptr<const MV> getVector( const ROL::Vector<Real>& x ) const {
+        ROL::Ptr<const MV> getVector( const ROL::Vector<Real>& x ) {
           return dynamic_cast<const TMV&>(x).getVector();
         }
 
-        ROL::Ptr<MV> getVector( ROL::Vector<Real>& x ) const {
+        ROL::Ptr<MV> getVector( ROL::Vector<Real>& x ) {
           return dynamic_cast<TMV&>(x).getVector();
         }
 
         public:
 
-            TpetraBoundConstraint(MVP lp, MVP up, Real scale = 1.0, 
-                                  Real feasTol = std::sqrt(ROL_EPSILON<Real>())) :
+            TpetraBoundConstraint(MVP lp, MVP up, Real scale = 1.0) :
                 gblDim_(lp->getGlobalLength()),
                 lclDim_(lp->getLocalLength()),
                 lp_(lp),
@@ -520,7 +371,6 @@ namespace ROL {
                 l_(lp->getLocalViewDevice()),
                 u_(up->getLocalViewDevice()),
                 scale_(scale),
-                feasTol_(feasTol),
                 comm_(lp->getMap()->getComm()) {
 
                 KokkosStructs::MinGap<Real,ViewType> findmin(l_,u_);
@@ -536,6 +386,7 @@ namespace ROL {
 
                 min_diff_ = 0.5*gblMinGap;
             }
+
 
             bool isFeasible( const Vector<Real> &x ) {
                 auto xp = getVector(x);
@@ -556,6 +407,7 @@ namespace ROL {
             }
 
             void project( Vector<Real> &x ) {
+
                 auto xp = getVector(x);
 
                 ViewType x_lcl = xp->getLocalViewDevice();
@@ -565,17 +417,6 @@ namespace ROL {
                 Kokkos::parallel_for(lclDim_,proj);
             }
 
-            void projectInterior( Vector<Real> &x ) {
-                auto xp = getVector(x);
-
-                ViewType x_lcl = xp->getLocalViewDevice();
-
-                Real tol = 100.0*ROL_EPSILON<Real>();
-
-                KokkosStructs::Project<Real,ViewType> projInterior(x_lcl,l_,u_,feasTol_,tol,min_diff_);
-
-                Kokkos::parallel_for(lclDim_,projInterior);
-            }
 
             void pruneLowerActive(Vector<Real> &v, const Vector<Real> &x, Real eps = Real(0)) {
                 auto xp = getVector(x);
@@ -623,7 +464,7 @@ namespace ROL {
                 auto xp = getVector(x);
                 auto gp = getVector(g);
                 auto vp = getVector(v);
-                Real epsn = std::min(scale_*xeps,this->min_diff_);
+               Real epsn = std::min(scale_*xeps,this->min_diff_);
 
                 ViewType x_lcl = xp->getLocalViewDevice();
                 ViewType g_lcl = gp->getLocalViewDevice();
@@ -662,42 +503,6 @@ namespace ROL {
                 KokkosStructs::GradPruneActive<Real,ViewType> prune(v_lcl,g_lcl,x_lcl,l_,u_,epsn,geps);
 
                 Kokkos::parallel_for(lclDim_,prune);
-            }
-
-            void applyInverseScalingFunction(Vector<Real> &dv, const Vector<Real> &v, const Vector<Real> &x, const Vector<Real> &g) const {
-                auto dvp = getVector(dv);
-                auto vp  = getVector(v);
-                auto xp  = getVector(x);
-                auto gp  = getVector(g);
-                Real kappa(1), zeta(0.5);
-
-                ViewType dv_lcl = dvp->getLocalViewDevice();
-                ViewType  v_lcl =  vp->getLocalViewDevice();
-                ViewType  x_lcl =  xp->getLocalViewDevice();
-                ViewType  g_lcl =  gp->getLocalViewDevice();
-
-                KokkosStructs::ApplyInverseScalingFunction<Real,ViewType>
-                    inverseScaling(dv_lcl,v_lcl,x_lcl,g_lcl,l_,u_,kappa,zeta);
-
-                Kokkos::parallel_for(lclDim_,inverseScaling);
-            }
-
-            void applyScalingFunctionJacobian(Vector<Real> &dv, const Vector<Real> &v, const Vector<Real> &x, const Vector<Real> &g) const {
-                auto dvp = getVector(dv);
-                auto vp  = getVector(v);
-                auto xp  = getVector(x);
-                auto gp  = getVector(g);
-                Real kappa(1), zeta(0.5);
-
-                ViewType dv_lcl = dvp->getLocalViewDevice();
-                ViewType  v_lcl =  vp->getLocalViewDevice();
-                ViewType  x_lcl =  xp->getLocalViewDevice();
-                ViewType  g_lcl =  gp->getLocalViewDevice();
-
-                KokkosStructs::ApplyScalingFunctionJacobian<Real,ViewType>
-                    scalingJacobian(dv_lcl,v_lcl,x_lcl,g_lcl,l_,u_,kappa,zeta);
-
-                Kokkos::parallel_for(lclDim_,scalingJacobian);
             }
       }; // End class TpetraBoundConstraint
 
