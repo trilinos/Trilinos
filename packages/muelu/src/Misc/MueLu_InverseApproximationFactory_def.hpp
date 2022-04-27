8daa1346df0c68f892dd1b4158bf16b83f7e6733
maxfirmbach <max@firmbach.com>
2022-04-27 13:45:08 +0200

    Add static sparse approximate inverse algorithm
    This commit adds a static sparse approximate
    inverse algorithm. Based on a static sparsity
    pattern, the inverse of a matrix A is calculated
    on the given pattern.
    
    - enhance the inverse approximation factory by
      including the static spai algorithm
    - add unit testing for the new inverse
      approximation option
    
    Related to #10437, #10484.

--- packages/muelu/src/Misc/MueLu_InverseApproximationFactory_def.hpp
+++ packages/muelu/src/Misc/MueLu_InverseApproximationFactory_def.hpp
@@ -57,6 +57,10 @@
 #include <Xpetra_BlockedCrsMatrix.hpp>
 #include <Xpetra_CrsMatrix.hpp>
 
+#include <Teuchos_SerialDenseVector.hpp>
+#include <Teuchos_SerialDenseMatrix.hpp>
+#include <Teuchos_SerialQRDenseSolver.hpp>
+
 #include "MueLu_Level.hpp"
 #include "MueLu_Monitor.hpp"
 #include "MueLu_Utilities.hpp"
@@ -93,7 +97,7 @@ namespace MueLu {
 
     // check which approximation type to use
     const std::string method = pL.get<std::string>("inverse: approximation type");
-    TEUCHOS_TEST_FOR_EXCEPTION(method != "diagonal" && method != "lumping", Exceptions::RuntimeError,
+    TEUCHOS_TEST_FOR_EXCEPTION(method != "diagonal" && method != "lumping" && method != "spai", Exceptions::RuntimeError,
                                "MueLu::InverseApproximationFactory::Build: Approximation type can be 'diagonal' or 'lumping'.");
 
     RCP<Matrix> A = Get<RCP<Matrix> >(currentLevel, "A");
@@ -115,10 +119,79 @@ namespace MueLu {
       const RCP<const Vector> D = (!fixing ? Utilities::GetInverse(diag) : Utilities::GetInverse(diag, 1e-4, one));
       Ainv = MatrixFactory::Build(D);
     }
+    else if(method=="spai") {
+      Ainv = GetSparseInverse(A, A->getCrsGraph());
+    }
 
     GetOStream(Statistics1) << "Approximate inverse calculated by: " << method << "." << std::endl;
+    GetOStream(Statistics1) << "Ainv has " << Ainv->getGlobalNumRows() << "x" << Ainv->getGlobalNumCols() << " rows and columns." << std::endl;
 
     Set(currentLevel, "Ainv", Ainv);
+  }
+
+  template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+  RCP<Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>>
+  InverseApproximationFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::GetSparseInverse(const RCP<Matrix>& Aorg, const RCP<const CrsGraph>& sparsityPattern) const {
+
+    // construct the inverse matrix with the given sparsity pattern
+    RCP<Matrix> Ainv = MatrixFactory::Build(sparsityPattern);
+    Ainv->resumeFill();
+
+    // gather missing rows from other procs to generate an overlapping map
+    RCP<Import> rowImport = ImportFactory::Build(sparsityPattern->getRowMap(), sparsityPattern->getColMap());
+    RCP<Matrix> A = MatrixFactory::Build(Aorg, *rowImport);
+
+    // loop over all rows of the inverse sparsity pattern (this can be done in parallel)
+    for(size_t k=0; k<sparsityPattern->getLocalNumRows(); k++) {
+
+      // 1. get column indices Ik of local row k
+      ArrayView<const LO> Ik;
+      sparsityPattern->getLocalRowView(k, Ik);
+
+      // 2. get all local A(Ik,:) rows
+      Array<ArrayView<const GO>> J(Ik.size());
+      Array<ArrayView<const SC>> Ak(Ik.size());
+      Array<GO> Jk;
+      for (size_t i = 0; i < Ik.size(); i++) {
+        A->getLocalRowView(Ik[i], J[i], Ak[i]);
+        for (size_t j = 0; j < J[i].size(); j++)
+          Jk.append(J[i][j]);
+      }
+      // set of unique column indices Jk
+      std::sort(Jk.begin(), Jk.end());
+      Jk.erase(std::unique(Jk.begin(), Jk.end()), Jk.end());
+      // create map
+      std::map<GO, GO> G;
+      for (size_t i = 0; i < Jk.size(); i++) G.insert(std::pair<GO, GO>(Jk[i], i));
+
+      // 3. merge rows together
+      Teuchos::SerialDenseMatrix<LO, SC> localA(Jk.size(), Ik.size(), true);
+      for (size_t i = 0; i < Ik.size(); i++) {
+        for (size_t j = 0; j < J[i].size(); j++) {
+          localA(G.at(J[i][j]), i) = Ak[i][j];
+        }
+      }
+
+      // 4. get direction-vector
+      // diagonal needs an entry!
+      Teuchos::SerialDenseVector<LO, SC> ek(Jk.size(), true);
+      ek[std::find(Jk.begin(), Jk.end(), k) - Jk.begin()] = 1.0;
+
+      // 5. solve linear system for x
+      Teuchos::SerialDenseVector<LO, SC> localX(Ik.size());
+      Teuchos::SerialQRDenseSolver<LO, SC> qrSolver;
+      qrSolver.setMatrix(Teuchos::rcp(&localA, false));
+      qrSolver.setVectors(Teuchos::rcp(&localX, false), Teuchos::rcp(&ek, false));
+      qrSolver.solve();
+
+      // 6. set calculated row into Ainv
+      ArrayView<const SC> Mk(localX.values(), localX.length());
+      Ainv->replaceLocalValues(k, Ik, Mk);
+
+    }
+    Ainv->fillComplete();
+
+    return Ainv;
   }
 
 } // namespace MueLu
