eaf5212672bbef15e6ca18b5a257e3e841041c18
Henry Swantner <HRSwant@sandia.gov>
2022-05-20 15:54:25 -0600

    test all formerly disabled tests

--- cmake/std/sems/PullRequestIntel19.0.5TestingEnv.sh
+++ cmake/std/sems/PullRequestIntel19.0.5TestingEnv.sh
@@ -12,9 +12,11 @@ module purge
 
 source /projects/sems/modulefiles/utils/sems-modules-init.sh
 
+module load sems-intel/19.0.5
+
 module load sems-git/2.11.1
 module load sems-openmpi/4.0.5
-module load sems-boost/1.74.0
+module load sems-boost/1.70.0
 module load sems-zlib/1.2.11
 module load sems-hdf5/1.10.7
 module load sems-netcdf-c/4.7.3
@@ -27,8 +29,8 @@ module load sems-scotch-int64/6.0.3
 module load sems-superlu/4.3
 module load sems-cmake/3.18.4
 
-module load sems-intel/19.0.5
 module load sems-mpich/3.2.1
+module load sems-ninja/1.10.1
 
 # add the OpenMP environment variable we need
 export OMP_NUM_THREADS=2
