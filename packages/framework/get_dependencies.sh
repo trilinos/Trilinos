261d8d3ee87c48cc4c440d6a03a1457906d26e36
Evan Harvey <eharvey@sandia.gov>
2022-06-06 13:42:25 -0600

    packages/framework/ini-files: Added

--- packages/framework/get_dependencies.sh
+++ packages/framework/get_dependencies.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash 
 ini_file_option=$1
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)"
 
@@ -19,14 +19,22 @@ if [[ ! -z "$(ls ./srn-ini-files)" && "$ini_file_option" == "--srn" ]]; then
     cd GenConfig/deps/LoadEnv/ini_files
     ln -sf ${script_dir}/srn-ini-files/trilinos/framework/environment-specs.ini
     ln -sf ${script_dir}/srn-ini-files/trilinos/framework/supported-systems.ini
+    ln -sf ${script_dir}/ini-files/supported-envs.ini
 elif [[ ! -z "$(ls ./son-ini-files)" && "$ini_file_option" == "--son" ]]; then
     cd GenConfig/deps/LoadEnv/ini_files
     ln -sf ${script_dir}/son-ini-files/trilinos/framework/environment-specs.ini
     ln -sf ${script_dir}/son-ini-files/trilinos/framework/supported-systems.ini
+    ln -sf ${script_dir}/ini-files/supported-envs.ini
 else
-    echo "ERROR: Invalid ini_file_option=$ini_file_option"
+    echo "ERROR: Unable to clone ini files repo or invalid ini_file_option=$ini_file_option"
+    echo "Please ensure you have access to the ini files repo"
+    echo ""
     echo "Usage: ./get_dependencies.sh [--srn,--son]"
     exit 1
 fi
+
+cd ../../../ini_files
+ln -sf ${script_dir}/ini-files/config-specs.ini
+ln -sf ${script_dir}/ini-files/supported-config-flags.ini
 
 popd
