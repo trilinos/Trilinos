b45091e273ea545d3a367ff8e3df34f822e03702
Roscoe A. Bartlett <rabartl@sandia.gov>
2022-06-14 20:49:37 -0600

    Switch to raw git repo clone and update instead of submodules
    This uses a simple clone and update function to get the GenConfig files in a
    way that does not mess with the base Trilinos git repo.
    
    NOTE: You can view the state of these subrepos using:
    
    $ gitdist dist-repo-status
    
    (or 'gitdist-status' if alias have been defined).

--- packages/framework/get_dependencies.sh
+++ packages/framework/get_dependencies.sh
@@ -6,15 +6,48 @@ pushd $PWD
 
 cd ${script_dir}
 
-# Update GenConfig submodules
-# NOTE: Add --remote below to pull in the latest tip of the GenConfig tracked branch
-git submodule update --init --recursive ${script_dir}/GenConfig
+function tril_genconfig_clone_or_update_repo() {
+  git_url=$1
+  sub_dir=$2
+  has_submodules=$3
 
-# Update ini file submodules
-git submodule update --init --remote ${script_dir}/srn-ini-files || true
-git submodule update --init --remote ${script_dir}/son-ini-files || true
+  if [[ -e ${sub_dir} ]] ; then
+    cd ${sub_dir}
+    git pull
+    cd -
+  else
+    git clone ${git_url} ${sub_dir}
+  fi
 
-# Point to srn ini files if they exist
+  if [[ "${has_submodules}" == "has-submodules" ]] ; then
+    cd ${sub_dir}
+    git submodule update --init --recursive
+    cd -
+  elif [[ "${has_submodules}" != "" ]] ; then
+    echo "ERROR: argument '${has_submodules}' not allowed!  Only 'has-submodules' or ''!"
+    exit 1
+  fi
+}
+
+# Clone or update the repos
+
+tril_genconfig_clone_or_update_repo \
+  git@gitlab-ex.sandia.gov:trilinos-devops-consolidation/code/GenConfig.git \
+  GenConfig  has-submodules
+
+if [[ "$ini_file_option" == "--srn" ]] ; then
+  tril_genconfig_clone_or_update_repo \
+    git@cee-gitlab.sandia.gov:trilinos-project/srn-ini-files.git \
+    srn-ini-files
+fi
+
+if [[ "$ini_file_option" == "--son" ]] ; then
+  tril_genconfig_clone_or_update_repo \
+    git@gitlab-ex.sandia.gov:trilinos-project/son-ini-files.git \
+    son-ini-files
+fi
+
+# Set up symlinks to the desired *.ini files
 if [[ ! -z "$(ls ./srn-ini-files)" && "$ini_file_option" == "--srn" ]]; then
     cd GenConfig/deps/LoadEnv/ini_files
     ln -sf ${script_dir}/srn-ini-files/trilinos/framework/environment-specs.ini
