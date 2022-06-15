7115007a82ccd1d1a2a6d2e0c50549e068abf729
Kyungjoo Kim <kyukim@sandia.gov>
2022-06-15 10:08:39 -0600

    Tacho - cleanup

--- packages/shylu/shylu_node/tacho/src/impl/Tacho_Herk.hpp
+++ packages/shylu/shylu_node/tacho/src/impl/Tacho_Herk.hpp
@@ -20,56 +20,6 @@ struct HerkAlgorithm {
   using type = ActiveAlgorithm::type;
 };
 
-// /// task construction for the above chol implementation
-// /// Herk<ArgUplo,ArgTrans,ArgAlgo> ::invoke(_sched, member, _alpha, _A, _beta, _C);
-// template<typename SchedulerType,
-//          typename ScalarType,
-//          typename DenseMatrixViewType,
-//          typename ArgUplo,
-//          typename ArgTrans,
-//          typename ArgAlgo>
-// struct TaskFunctor_Herk {
-// public:
-//   typedef SchedulerType scheduler_type;
-//   typedef typename scheduler_type::member_type member_type;
-
-//   typedef ScalarType scalar_type;
-
-//   typedef DenseMatrixViewType dense_block_type;
-//   typedef typename dense_block_type::future_type future_type;
-//   typedef typename future_type::value_type value_type;
-
-// private:
-//   scalar_type _alpha, _beta;
-//   dense_block_type _A, _C;
-
-// public:
-//   KOKKOS_INLINE_FUNCTION
-//   TaskFunctor_Herk() = delete;
-
-//   KOKKOS_INLINE_FUNCTION
-//   TaskFunctor_Herk(const scalar_type alpha,
-//                    const dense_block_type &A,
-//                    const scalar_type beta,
-//                    const dense_block_type &C)
-//     : _alpha(alpha),
-//       _beta(beta),
-//       _A(A),
-//       _C(C) {}
-
-//   KOKKOS_INLINE_FUNCTION
-//   void operator()(member_type &member, value_type &r_val) {
-//     const int ierr = Herk<ArgUplo,ArgTrans,ArgAlgo>
-//       ::invoke(member, _alpha, _A, _beta, _C);
-
-//     Kokkos::single(Kokkos::PerThread(member),
-//                    [&, ierr] () { // Value capture is a workaround for cuda + gcc-7.2 compiler bug w/c++14
-//                      _C.set_future();
-//                      r_val = ierr;
-//                    });
-//   }
-// };
-
 } // namespace Tacho
 
 #endif
