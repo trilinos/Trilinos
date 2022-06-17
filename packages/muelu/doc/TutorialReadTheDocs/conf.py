fe01d2d5e1894d3c98796c05ff525b54e455dbcc
Matthias Mayr <matthias.mayr@unibw.de>
2022-06-17 09:37:50 +0200

    MueLu: set sphinx RTD theme for tutorial

--- packages/muelu/doc/TutorialReadTheDocs/conf.py
+++ packages/muelu/doc/TutorialReadTheDocs/conf.py
@@ -32,7 +32,7 @@ release = "2022"
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    sphinx_rtd_theme,
+    "sphinx_rtd_theme",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
