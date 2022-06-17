06b51e0082d569d5390ff3dd4dd018ae3fe64e6b
Evan Harvey <eharvey@sandia.gov>
2022-06-17 12:34:12 -0600

    packages/framework/get_dependencies.sh: Add suggestions

--- packages/framework/get_dependencies.sh
+++ packages/framework/get_dependencies.sh
@@ -7,33 +7,39 @@ pushd $PWD
 cd ${script_dir}
 
 function tril_genconfig_clone_or_update_repo() {
+  pushd $PWD
   git_url=$1
   sub_dir=$2
   has_submodules=$3
+  head_sha=$4
 
   if [[ -e ${sub_dir} ]] ; then
     cd ${sub_dir}
-    git pull
-    cd - > /dev/null
+    git fetch
   else
     git clone ${git_url} ${sub_dir}
+    cd ${sub_dir}
+  fi
+
+  if [[ ! -z ${head_sha} ]]; then
+    git checkout -f ${head_sha}
   fi
 
   if [[ "${has_submodules}" == "has-submodules" ]] ; then
-    cd ${sub_dir}
-    git submodule update --init --recursive
+    git submodule update --force --init --recursive
     cd - > /dev/null
   elif [[ "${has_submodules}" != "" ]] ; then
     echo "ERROR: argument '${has_submodules}' not allowed!  Only 'has-submodules' or ''!"
     exit 1
   fi
+  popd > /dev/null
 }
 
 # Clone or update the repos
 
 tril_genconfig_clone_or_update_repo \
   git@gitlab-ex.sandia.gov:trilinos-devops-consolidation/code/GenConfig.git \
-  GenConfig  has-submodules
+  GenConfig  has-submodules b47cbdb1145a9d13f5bc8d4e7e691bab1dcdb60d
 
 if [[ "$ini_file_option" == "--srn" ]] ; then
   tril_genconfig_clone_or_update_repo \
@@ -48,28 +54,27 @@ if [[ "$ini_file_option" == "--son" ]] ; then
 fi
 
 # Set up symlinks to the desired *.ini files
-cd GenConfig/deps/LoadEnv/ini_files
+cd ${script_dir}/GenConfig/deps/LoadEnv/ini_files
 if [[ -d ${script_dir}/srn-ini-files ]] && [[ "$ini_file_option" == "--srn" ]]; then
     echo "Link files from srn-ini-files"
     ln -sf ${script_dir}/srn-ini-files/trilinos/framework/environment-specs.ini
     ln -sf ${script_dir}/srn-ini-files/trilinos/framework/supported-systems.ini
-    ln -sf ${script_dir}/ini-files/supported-envs.ini
 elif [[ -d ${script_dir}/son-ini-files ]] && [[ "$ini_file_option" == "--son" ]]; then
     echo "Link files from son-ini-files"
     ln -sf ${script_dir}/son-ini-files/trilinos/framework/environment-specs.ini
     ln -sf ${script_dir}/son-ini-files/trilinos/framework/supported-systems.ini
-    ln -sf ${script_dir}/ini-files/supported-envs.ini
 else
     echo "Link files from ini-files"
     [ -e ${script_dir}/ini-files/environment-specs.ini ] && ln -sf ${script_dir}/ini-files/environment-specs.ini
     [ -e ${script_dir}/ini-files/supported-systems.ini ] && ln -sf ${script_dir}/ini-files/supported-systems.ini
-    ln -sf ${script_dir}/ini-files/supported-envs.ini
 fi
+ln -sf ${script_dir}/ini-files/supported-envs.ini
 cd - > /dev/null
-cd GenConfig/ini_files
 
+cd ${script_dir}/GenConfig/ini_files
 ln -sf ${script_dir}/ini-files/config-specs.ini
 ln -sf ${script_dir}/ini-files/supported-config-flags.ini
+
 # Print summary of ini file settings
 cd ${script_dir}
 echo "You selected the following LoadEnv ini files:"
@@ -77,6 +82,6 @@ ls -lat GenConfig/deps/LoadEnv/ini_files
 echo ""
 echo "You selected the following GenConfig ini files:"
 ls -lat GenConfig/ini_files
-echo "If these symlinks do not point to the desired ini files, please re-run:
+echo "If these symlinks do not point to the desired ini files, please re-run:"
 echo "    $0 [--son, --srn]"
 popd > /dev/null
