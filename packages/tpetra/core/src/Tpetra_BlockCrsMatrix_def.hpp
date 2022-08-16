9b2d48b90f32bcdffd8c993c70e0496aeadbb9b5
Chris Siefert <csiefer@sandia.gov>
2022-08-16 14:07:14 -0600

    Tpetra: reverting to scratch space

--- packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
+++ packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
@@ -2772,19 +2772,23 @@ public:
     errorDuringUnpack () = 0;
     {
       using policy_type = Kokkos::TeamPolicy<host_exec>;
-      const auto policy = policy_type (numImportLIDs, 1, 1);
+      const auto policy = policy_type (numImportLIDs, 1, 1)
+        .set_scratch_size (0, Kokkos::PerTeam (sizeof (GO) * maxRowNumEnt +
+                                               sizeof (LO) * maxRowNumEnt +
+                                               numBytesPerValue * maxRowNumScalarEnt));
+      using host_scratch_space = typename host_exec::scratch_memory_space;
+
       using pair_type = Kokkos::pair<size_t, size_t>;
       Kokkos::parallel_for
         ("Tpetra::BlockCrsMatrix::unpackAndCombine: unpack", policy,
          [=] (const typename policy_type::member_type& member) {
           const size_t i = member.league_rank();
-
-          Kokkos::View<GO*, host_exec> gblColInds
-            ("", maxRowNumEnt);
-          Kokkos::View<LO*, host_exec> lclColInds
-            ("", maxRowNumEnt);
-          Kokkos::View<impl_scalar_type*, host_exec> vals
-            ("", maxRowNumScalarEnt);
+          Kokkos::View<GO*, host_scratch_space> gblColInds
+            (member.team_scratch (0), maxRowNumEnt);
+          Kokkos::View<LO*, host_scratch_space> lclColInds
+            (member.team_scratch (0), maxRowNumEnt);
+          Kokkos::View<impl_scalar_type*, host_scratch_space> vals
+            (member.team_scratch (0), maxRowNumScalarEnt);
 
           const size_t offval = offset(i);
           const LO lclRow = importLIDsHost(i);
