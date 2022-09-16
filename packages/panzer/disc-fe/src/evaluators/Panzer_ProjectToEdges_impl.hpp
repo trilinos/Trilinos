ee5818043c5f809d03444751cb1f9e2cd10213b8
Roger Pawlowski <rppawlo@sandia.gov>
2022-09-16 13:42:02 -0600

    Panzer: move project to edges to device

--- packages/panzer/disc-fe/src/evaluators/Panzer_ProjectToEdges_impl.hpp
+++ packages/panzer/disc-fe/src/evaluators/Panzer_ProjectToEdges_impl.hpp
@@ -53,6 +53,7 @@
 
 #include "Panzer_PureBasis.hpp"
 #include "Panzer_CommonArrayFactories.hpp"
+#include "Panzer_HierarchicParallelism.hpp"
 #include "Kokkos_ViewFactory.hpp"
 
 #include "Teuchos_FancyOStream.hpp"
@@ -62,28 +63,27 @@ panzer::ProjectToEdges<EvalT, Traits>::
 ProjectToEdges(
   const Teuchos::ParameterList& p)
 { 
-  dof_name = (p.get< std::string >("DOF Name"));
+  dof_name_ = (p.get< std::string >("DOF Name"));
 
   if(p.isType< Teuchos::RCP<PureBasis> >("Basis"))
-    basis = p.get< Teuchos::RCP<PureBasis> >("Basis");
+    basis_ = p.get< Teuchos::RCP<PureBasis> >("Basis");
   else
-    basis = p.get< Teuchos::RCP<const PureBasis> >("Basis");
+    basis_ = p.get< Teuchos::RCP<const PureBasis> >("Basis");
 
-  Teuchos::RCP<PHX::DataLayout> basis_layout  = basis->functional;
-  Teuchos::RCP<PHX::DataLayout> vector_layout = basis->functional_grad;
+  Teuchos::RCP<PHX::DataLayout> basis_layout  = basis_->functional;
+  Teuchos::RCP<PHX::DataLayout> vector_layout = basis_->functional_grad;
 
   // some sanity checks
-  TEUCHOS_ASSERT(basis->isVectorBasis());
+  TEUCHOS_ASSERT(basis_->isVectorBasis());
 
-  result = PHX::MDField<ScalarT,Cell,BASIS>(dof_name,basis_layout);
-  this->addEvaluatedField(result);
+  result_ = PHX::MDField<ScalarT,Cell,BASIS>(dof_name_,basis_layout);
+  this->addEvaluatedField(result_);
 
-  tangents = PHX::MDField<const ScalarT,Cell,BASIS,Dim>(dof_name+"_Tangents",vector_layout);
-  this->addDependentField(tangents);
+  tangents_ = PHX::MDField<const ScalarT,Cell,BASIS,Dim>(dof_name_+"_Tangents",vector_layout);
+  this->addDependentField(tangents_);
 
-  vector_values.resize(1);
-  vector_values[0] = PHX::MDField<const ScalarT,Cell,BASIS,Dim>(dof_name+"_Vector",vector_layout);
-  this->addDependentField(vector_values[0]);
+  vector_values_ = PHX::MDField<const ScalarT,Cell,BASIS,Dim>(dof_name_+"_Vector",vector_layout);
+  this->addDependentField(vector_values_);
 
   this->setName("Project To Edges");
 }
@@ -94,11 +94,11 @@ void panzer::ProjectToEdges<EvalT, Traits>::
 postRegistrationSetup(typename Traits::SetupData  d, 
 		      PHX::FieldManager<Traits>& /* fm */)
 {
-  num_edges = vector_values[0].extent(1);
-  num_dim = vector_values[0].extent(2);
+  num_edges_ = vector_values_.extent(1);
+  num_dim_ = vector_values_.extent(2);
 
-  TEUCHOS_ASSERT(vector_values[0].extent(1) == tangents.extent(1));
-  TEUCHOS_ASSERT(vector_values[0].extent(2) == tangents.extent(2));
+  TEUCHOS_ASSERT(vector_values_.extent(1) == tangents_.extent(1));
+  TEUCHOS_ASSERT(vector_values_.extent(2) == tangents_.extent(2));
 }
 
 // **********************************************************************
@@ -112,17 +112,24 @@ evaluateFields(typename Traits::EvalData workset)
   // HVol is constant
 
   //TODO: make this work w/ high order basis
-  const int intDegree = basis->order();
+  const int intDegree = basis_->order();
   TEUCHOS_ASSERT(intDegree == 1);
 
-  //TODO: use parallel for over cells
-  for (index_t cell = 0; cell < workset.num_cells; ++cell) {
-    for (int p = 0; p < num_edges; ++p) {
+  auto result = result_.get_static_view();
+  auto vector_values = vector_values_.get_static_view();
+  auto tangents = tangents_.get_static_view();
+  auto num_edges = num_edges_;
+  auto num_dim = num_dim_;
+
+  auto policy = panzer::HP::inst().teamPolicy<ScalarT,PHX::exec_space>(workset.num_cells);
+  Kokkos::parallel_for("panzer::ProjectToEdges",policy,KOKKOS_LAMBDA(const Kokkos::TeamPolicy<PHX::exec_space>::member_type& team) {
+    const auto cell = team.league_rank();
+    Kokkos::parallel_for(Kokkos::TeamThreadRange(team,0,num_edges),[&] (const int p) {
       result(cell,p) = ScalarT(0.0);
       for (int dim = 0; dim < num_dim; ++dim)
-        result(cell,p) += vector_values[0](cell,p,dim) * tangents(cell,p,dim);
-    }
-  }
+        result(cell,p) += vector_values(cell,p,dim) * tangents(cell,p,dim);
+    });
+  });
 }
 
 #endif
