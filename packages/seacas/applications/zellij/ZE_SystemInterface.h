fe19d6b8da0b31059b2b6c3e8b2a6bb5b222d40b
Greg Sjaardema <gsjaardema@gmail.com>
2022-10-05 08:22:06 -0600

    Revert "Snapshot changes (partial)"
    This reverts commit d6628b0e8c4b2004f0e379c04fdd35e8eff754be.

--- packages/seacas/applications/zellij/ZE_SystemInterface.h
+++ packages/seacas/applications/zellij/ZE_SystemInterface.h
@@ -1,4 +1,4 @@
-// Copyright(C) 2021, 2022 National Technology & Engineering Solutions
+// Copyright(C) 2021 National Technology & Engineering Solutions
 // of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
 // NTESS, the U.S. Government retains certain rights in this software.
 //
@@ -7,12 +7,9 @@
 #define ZE_SystemInterface_h
 
 #include "Ioss_GetLongOpt.h" // for GetLongOption
-#include <array>
-#include <string> // for string
+#include <string>            // for string
 
 //! \file
-
-using vector3d = std::array<double, 3>;
 
 enum class Minimize { NONE = 0, UNIT = 1, OUTPUT = 2, ALL = 3 };
 
@@ -48,8 +45,7 @@ public:
   bool     equivalence_nodes() const { return equivalenceNodes_; }
   Minimize minimize_open_files() const { return minimizeOpenFiles_; }
 
-  double   scale_factor() const { return scaleFactor_; }
-  vector3d offset() const { return offset_; }
+  double scale_factor() const { return scaleFactor_; }
 
   int skip() const { return skip_; }
   int repeat() const { return repeat_; }
@@ -68,7 +64,6 @@ private:
   std::string   decompMethod_{"HSFC"};
   std::string   sidesetSurfaces_{};
   std::string   sidesetNames_{};
-  vector3d      offset_{0.0, 0.0, 0.0};
   double        scaleFactor_{1.0};
   int           myRank_{0};
   int           debugLevel_{0};
