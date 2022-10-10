d45fe35a07d4c8e215831bfe8409ecc80e83500e
Greg Sjaardema <gsjaardema@gmail.com>
2022-10-10 11:56:18 -0600

    IOSS: Fix compiler warning on cuda build

--- packages/seacas/libraries/ioss/src/text_mesh/Iotm_TextMeshDataTypes.h
+++ packages/seacas/libraries/ioss/src/text_mesh/Iotm_TextMeshDataTypes.h
@@ -218,7 +218,7 @@ namespace Iotm {
 
       const std::vector<double> &operator[](const EntityId nodeId) const
       {
-        auto it(m_nodalCoords.find(nodeId));
+        auto it = m_nodalCoords.find(nodeId);
 
         if (it == m_nodalCoords.end()) {
           std::ostringstream errmsg;
