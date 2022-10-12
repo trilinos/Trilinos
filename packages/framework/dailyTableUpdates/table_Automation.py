f8fa7281167773de27ee3de24c8ae0aaca567bb9
dave <djohn10@sandia.gov>
2022-10-12 16:16:09 -0600

    refactoring code -> PR#11105 comments

--- packages/framework/dailyTableUpdates/table_Automation.py
+++ packages/framework/dailyTableUpdates/table_Automation.py
@@ -19,14 +19,10 @@ SYNOPSIS
 #STATUS: [0] == success, [1] == warning [3] == failure -> Pull Request(s) and Master Merge Status codes.
 
 """
-# from dateutil.relativedelta import relativedelta, FR
 from time import sleep as pause
 from datetime import timedelta
-# from datetime import datetime
 from datetime import date
-import subprocess
 import sys
-import os
 
 STOP = 0b11
 ONE_OFF = -0b1
@@ -37,10 +33,6 @@ try:
 except ImportError:
   print("\n\nHere are a list of needed dependencies ... \n\n\n")
   pause(STOP)
-#   os.system('cls' if os.name == 'nt' else 'clear')
-#   os.system('python3 -m ensurepip')
-#   os.system('python3 -m pip install pytablewriter')
-#   os.system('cls' if os.name == 'nt' else 'clear')
   print("\t*** Package(s) that need to be installed ***\n \n\tpip3 install python-dateutil " 
         + "\n\tpip3 install pytablewriter \n\n\n\t*** Install and try running again ***\n\n\n")
   pause(STOP)  
@@ -61,8 +53,6 @@ def main():
     except:
         print("View needed dependencies above\n\n")
         
-    
-    
     try:
         number_of_pr_merged = stat_container[1]
         number_of_failed_pr = stat_container[2]
@@ -76,7 +66,6 @@ def main():
         NUMBER_OF_PRs_MERGED = "["+number_of_pr_merged+"]"+"(https://github.com/trilinos/Trilinos/pulls?q=is%3Apr+merged%3A"+str(yesterday)+"T12%3A00%3A00-07%3A00.."+str(today)+"T12%3A00%3A00-07%3A00+base%3Adevelop)"
         NUMBER_OF_FAILED_PRs = "["+number_of_failed_pr+"]"+"(https://github.com/trilinos/Trilinos/pulls?q=is%3Apr+updated%3A"+str(yesterday)+"T12%3A00%3A00-07%3A00.."+str(today)+"T12%3A00%3A00-07%3A00+base%3Adevelop+status%3Afailure+)"
         NUMBER_OF_PRs_WAITING = "["+number_of_waiting_pr+"]"+"(https://github.com/trilinos/Trilinos/pulls?q=is%3Apr+is%3Aopen+updated%3A"+str(yesterday)+"T12%3A00%3A00-07%3A00.."+str(today)+"T12%3A00%3A00-07%3A00+base%3Adevelop+status%3Apending+)"
-        # NUMBER_SUCCESSFUL_MM = "["+number_of_successful_mm+"]"+"(https://github.com/trilinos/Trilinos/pulls?q=is%3Apr+merged%3A"+str(yesterday)+"T12%3A00%3A00-07%3A00.."+str(today)+"T12%3A00%3A00-07%3A00+base%3Amaster+)"
         NUMBER_SUCCESSFUL_MM = "["+number_of_successful_mm+"]"+"(https://github.com/trilinos/Trilinos/pulls?q=is%3Apr+merged%3A"+str(last_Friday)+"T12%3A00%3A00-07%3A00.."+str(today)+"T12%3A00%3A00-07%3A00+base%3Amaster+)"
         JIRA_TICKETS = "[TrilFrame-"+jira_ticket_number+"]"+"(https://sems-atlassian-son.sandia.gov/jira/browse/TRILFRAME-"+str(jira_ticket_number)+")"
     except:
@@ -91,27 +80,8 @@ def main():
           )
     except:
         print("Not enough arguments ... Example: table_Automation.py 0 4 6 4 72 0 4 435")
-    #change the output stream to terminal <- OPTIONAL IMPORT ABOVE
-    # writer.stream = io.StringIO()
-    # writer.write_table()
-    # print(writer.stream.getvalue())
-
-    # change the output stream to a file
     try:
         print(writer)
-    
-        # TODO -> CREATE SUBPROCESS TO CAPTURE OUTPUT AND PIPE TO <MDV> 
-        # pause(STOP)
-        # subprocess.call(["| "])
-        # os.system(writer + "| mdv ")
-        
-        
-        
-        # with open("trilinos_Table_Updates.md", "w") as f:
-            # writer.stream = f
-            # writer.write_table()
-            # or you can use dump method to file if you just output a table to a file
-            # writer.dump("sample.md")
     except:
         print("Can not write to file, missing arguements ... Example: table_Automation.py 0 4 6 4 72 0 4 435")
     
