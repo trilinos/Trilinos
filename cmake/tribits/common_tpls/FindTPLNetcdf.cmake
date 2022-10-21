170962b45bf13d2d9944e8ab9818e82a193bede1
Dan Ibanez <daibane@sandia.gov>
2022-10-20 22:03:18 -0600

    Netcdf_ENABLE_MODERN mode for TriBITS Netcdf TPL
    when this CMake cache variable is set to a true
    value, the TriBITS Netcdf TPL code will only look
    for externally installed CMake config files for
    CMake packages called netCDF and possibly hdf5.
    Such files are installed by the latest versions
    of NetCDF and HDF5.
    This effectively allows loading NetCDF and its
    HDF5 dependency using the most modern CMake
    approach.
    
    This is needed to resolve TriBITSPub/TriBITS#533

--- cmake/tribits/common_tpls/FindTPLNetcdf.cmake
+++ cmake/tribits/common_tpls/FindTPLNetcdf.cmake
@@ -44,98 +44,158 @@
 # in order to trigger the right behavior in the function
 # tribits_tpl_find_include_dirs_and_libraries().
 #
-
-set(REQUIRED_HEADERS netcdf.h)
-set(REQUIRED_LIBS_NAMES netcdf)
-
-if (TPL_ENABLE_MPI)
-  set(REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES} pnetcdf)
+if (${CMAKE_VERSION} GREATER "3.13")
+     cmake_policy(SET CMP0074 NEW)
 endif()
 
-#
-# Second, search for Netcdf components (if allowed) using the standard
-# find_package(NetCDF ...).
-#
-tribits_tpl_allow_pre_find_package(Netcdf  Netcdf_ALLOW_PREFIND)
-if (Netcdf_ALLOW_PREFIND)
+set(Netcdf_ALLOW_MODERN FALSE CACHE BOOL "Allow finding Netcdf as a modern CMake config file with exported targets (and only this way)")
 
-  message("-- Using find_package(Netcdf ...) ...")
+if (Netcdf_ALLOW_MODERN)
 
-  set(CMAKE_MODULE_PATH
-    "${CMAKE_MODULE_PATH}"
-    "${CMAKE_CURRENT_LIST_DIR}/find_modules"
-    "${CMAKE_CURRENT_LIST_DIR}/utils"
-     )
-
-  find_package(NetCDF)
-
-  if (NetCDF_FOUND)
-    set(DOCSTR "List of semi-colon separated paths to look for the TPL Netcdf")
-    set(TPL_Netcdf_Enables_Netcdf4 ${NetCDF_NEEDS_HDF5} CACHE BOOL
+  set(minimum_modern_netCDF_version 4.7.4)
+  message("-- Netcdf_ALLOW_MODERN=${Netcdf_ALLOW_MODERN}")
+  message("-- Using find_package(netCDF ${minimum_modern_netCDF_version} CONFIG) ...")
+  find_package(netCDF ${minimum_modern_netCDF_version} CONFIG)
+  if (netCDF_FOUND)
+    message("-- Found netCDF_CONFIG=${netCDF_CONFIG}")
+    message("-- Generating Netcdf::all_libs and NetcdfConfig.cmake")
+    # instead of using tribits_extpkg_create_imported_all_libs_target_and_config_file,
+    # we will do what it does ourselves because we need to bring in two
+    # inner find packages (netCDF and hdf5) because netCDF does not properly
+    # do find_dependency(hdf5)
+    add_library(Netcdf::all_libs  INTERFACE  IMPORTED  GLOBAL)
+    target_link_libraries(Netcdf::all_libs  INTERFACE  netCDF::netcdf)
+    set(configFileStr "")
+    string(APPEND configFileStr
+      "include(CMakeFindDependencyMacro)\n" )
+    string(APPEND configFileStr
+      "set(netCDF_DIR \"${netCDF_DIR}\")\n" )
+    string(APPEND configFileStr
+      "find_dependency(netCDF ${minimum_modern_netCDF_version} CONFIG)\n"
+      )
+    if (netCDF_HAS_HDF5)
+      find_package(hdf5 CONFIG REQUIRED)
+      string(APPEND configFileStr
+        "set(hdf5_DIR \"${hdf5_DIR}\")\n" )
+      string(APPEND configFileStr
+        "find_dependency(hdf5 CONFIG)\n"
+        )
+    endif()
+    string(APPEND configFileStr
+      "add_library(Netcdf::all_libs  INTERFACE  IMPORTED  GLOBAL)\n"
+      )
+    string(APPEND configFileStr
+      "target_link_libraries(Netcdf::all_libs  INTERFACE  netCDF::netcdf)\n")
+    set(buildDirExternalPkgsDir
+      "${${PROJECT_NAME}_BINARY_DIR}/${${PROJECT_NAME}_BUILD_DIR_EXTERNAL_PKGS_DIR}")
+    set(tplConfigFile
+      "${buildDirExternalPkgsDir}/Netcdf/NetcdfConfig.cmake")
+    file(WRITE "${tplConfigFile}" "${configFileStr}")
+    # the following gets set by tribits_tpl_find_include_dirs_and_libraries
+    # and it is not well-documented that you need to set it yourself if not using
+    # tribits_tpl_find_include_dirs_and_libraries
+    set(TPL_Netcdf_NOT_FOUND FALSE)
+    message("-- TPL_Netcdf_PARALLEL = netCDF_HAS_PARALLEL = ${netCDF_HAS_PARALLEL}")
+    set(TPL_Netcdf_PARALLEL ${netCDF_HAS_PARALLEL})
+    message("-- TPL_Netcdf_Enables_Netcdf4 = netCDF_HAS_NC4 = ${netCDF_HAS_NC4}")
+    set(TPL_Netcdf_Enables_Netcdf4 ${netCDF_HAS_NC4} CACHE BOOL
       "True if netcdf enables netcdf-4")
-    set(TPL_Netcdf_Enables_PNetcdf ${NetCDF_NEEDS_PNetCDF} CACHE BOOL
-      "True if netcdf enables pnetcdf")
-    set(TPL_Netcdf_PARALLEL ${NetCDF_PARALLEL} CACHE INTERNAL
-      "True if netcdf compiled with parallel enabled")
-    set(TPL_Netcdf_LIBRARY_DIRS ${_hdf5_LIBRARY_SEARCH_DIRS} CACHE PATH
-      "${DOCSTR} library files")
-    set(TPL_Netcdf_LIBRARIES ${NetCDF_LIBRARIES} CACHE PATH
-      "List of semi-colon separated library names (not 'lib' or extension).")
-    set(TPL_Netcdf_INCLUDE_DIRS ${NetCDF_INCLUDE_DIRS} CACHE PATH
-      "${DOCSTR} header files.")
   endif()
+
 else()
-  # Curl library is only required if DAP is enabled; should detect inside
-  # FindNetCDF.cmake, but that is not being called... SEMS has DAP enabled;
-  # many HPC systems don't, but they override these settings...
-  find_program(NC_CONFIG "nc-config")
-  if (NC_CONFIG)
-    execute_process(COMMAND "nc-config --has-dap2"
-                    OUTPUT_VARIABLE NETCDF_HAS_DAP2)
-    execute_process(COMMAND "nc-config --has-dap4"
-                    OUTPUT_VARIABLE NETCDF_HAS_DAP4)
+
+  set(REQUIRED_HEADERS netcdf.h)
+  set(REQUIRED_LIBS_NAMES netcdf)
+
+  if (TPL_ENABLE_MPI)
+    set(REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES} pnetcdf)
   endif()
-  if ((NOT NC_CONFIG) OR NETCDF_HAS_DAP2 OR NETCDF_HAS_DAP4)
-    set(REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES} curl)
+
+  #
+  # Second, search for Netcdf components (if allowed) using the standard
+  # find_package(NetCDF ...).
+  #
+  tribits_tpl_allow_pre_find_package(Netcdf  Netcdf_ALLOW_PREFIND)
+  if (Netcdf_ALLOW_PREFIND)
+
+    message("-- Using find_package(Netcdf ...) ...")
+
+    set(CMAKE_MODULE_PATH
+      "${CMAKE_MODULE_PATH}"
+      "${CMAKE_CURRENT_LIST_DIR}/find_modules"
+      "${CMAKE_CURRENT_LIST_DIR}/utils"
+       )
+
+    find_package(NetCDF)
+
+    if (NetCDF_FOUND)
+      set(DOCSTR "List of semi-colon separated paths to look for the TPL Netcdf")
+      set(TPL_Netcdf_Enables_Netcdf4 ${NetCDF_NEEDS_HDF5} CACHE BOOL
+        "True if netcdf enables netcdf-4")
+      set(TPL_Netcdf_Enables_PNetcdf ${NetCDF_NEEDS_PNetCDF} CACHE BOOL
+        "True if netcdf enables pnetcdf")
+      set(TPL_Netcdf_PARALLEL ${NetCDF_PARALLEL} CACHE INTERNAL
+        "True if netcdf compiled with parallel enabled")
+      set(TPL_Netcdf_LIBRARY_DIRS ${_hdf5_LIBRARY_SEARCH_DIRS} CACHE PATH
+        "${DOCSTR} library files")
+      set(TPL_Netcdf_LIBRARIES ${NetCDF_LIBRARIES} CACHE PATH
+        "List of semi-colon separated library names (not 'lib' or extension).")
+      set(TPL_Netcdf_INCLUDE_DIRS ${NetCDF_INCLUDE_DIRS} CACHE PATH
+        "${DOCSTR} header files.")
+    endif()
+  else()
+    # Curl library is only required if DAP is enabled; should detect inside
+    # FindNetCDF.cmake, but that is not being called... SEMS has DAP enabled;
+    # many HPC systems don't, but they override these settings...
+    find_program(NC_CONFIG "nc-config")
+    if (NC_CONFIG)
+      execute_process(COMMAND "nc-config --has-dap2"
+                      OUTPUT_VARIABLE NETCDF_HAS_DAP2)
+      execute_process(COMMAND "nc-config --has-dap4"
+                      OUTPUT_VARIABLE NETCDF_HAS_DAP4)
+    endif()
+    if ((NOT NC_CONFIG) OR NETCDF_HAS_DAP2 OR NETCDF_HAS_DAP4)
+      set(REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES} curl)
+    endif()
   endif()
+
+  #
+  # Third, call tribits_tpl_find_include_dirs_and_libraries()
+  #
+  tribits_tpl_find_include_dirs_and_libraries( Netcdf
+    REQUIRED_HEADERS ${REQUIRED_HEADERS}
+    REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES}
+    )
+  # NOTE: If find_package(Netcdf ...) was called and successfully found Netcdf,
+  # then tribits_tpl_find_include_dirs_and_libraries() will use the already-set
+  # variables TPL_Netcdf_INCLUDE_DIRS and TPL_Netcdf_LIBRARIES and then print
+  # them out (and set some other standard variables as well).  This is the final
+  # "hook" into the TriBITS TPL system.
+
+  # If the `find_package(NetCDF)` is not run, then this may not be set
+  # Need to determine how this is set in the library that is being used...
+
+  if ("${TPL_Netcdf_PARALLEL}" STREQUAL "")
+     assert_defined(TPL_Netcdf_INCLUDE_DIRS)
+     find_path(meta_path
+        NAMES "netcdf_meta.h"
+        HINTS ${TPL_Netcdf_INCLUDE_DIRS}
+        NO_DEFAULT_PATH)
+
+     if (meta_path)
+        # Search meta for NC_HAS_PARALLEL setting...
+        # Note that there is both NC_HAS_PARALLEL and NC_HAS_PARALLEL4, only want first...
+        file(STRINGS "${meta_path}/netcdf_meta.h" netcdf_par_string REGEX "NC_HAS_PARALLEL ")
+        string(REGEX MATCH "[01]" netcdf_par_val "${netcdf_par_string}")
+        if (netcdf_par_val EQUAL 1)
+           set(TPL_Netcdf_PARALLEL True CACHE INTERNAL
+               "True if netcdf compiled with parallel enabled")
+        endif()
+     endif()
+     if ("${TPL_Netcdf_PARALLEL}" STREQUAL "")
+        set(TPL_Netcdf_PARALLEL False CACHE INTERNAL
+            "True if netcdf compiled with parallel enabled")
+     endif()
+  endif()
+  message(STATUS "TPL_Netcdf_PARALLEL is ${TPL_Netcdf_PARALLEL}")
 endif()
-
-#
-# Third, call tribits_tpl_find_include_dirs_and_libraries()
-#
-tribits_tpl_find_include_dirs_and_libraries( Netcdf
-  REQUIRED_HEADERS ${REQUIRED_HEADERS}
-  REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES}
-  )
-# NOTE: If find_package(Netcdf ...) was called and successfully found Netcdf,
-# then tribits_tpl_find_include_dirs_and_libraries() will use the already-set
-# variables TPL_Netcdf_INCLUDE_DIRS and TPL_Netcdf_LIBRARIES and then print
-# them out (and set some other standard variables as well).  This is the final
-# "hook" into the TriBITS TPL system.
-
-# If the `find_package(NetCDF)` is not run, then this may not be set
-# Need to determine how this is set in the library that is being used...
-
-if ("${TPL_Netcdf_PARALLEL}" STREQUAL "")
-   assert_defined(TPL_Netcdf_INCLUDE_DIRS)
-   find_path(meta_path
-      NAMES "netcdf_meta.h"
-      HINTS ${TPL_Netcdf_INCLUDE_DIRS}
-      NO_DEFAULT_PATH)
-
-   if (meta_path)
-      # Search meta for NC_HAS_PARALLEL setting...
-      # Note that there is both NC_HAS_PARALLEL and NC_HAS_PARALLEL4, only want first...
-      file(STRINGS "${meta_path}/netcdf_meta.h" netcdf_par_string REGEX "NC_HAS_PARALLEL ")
-      string(REGEX MATCH "[01]" netcdf_par_val "${netcdf_par_string}")
-      if (netcdf_par_val EQUAL 1)
-         set(TPL_Netcdf_PARALLEL True CACHE INTERNAL
-             "True if netcdf compiled with parallel enabled")
-      endif()
-   endif()
-   if ("${TPL_Netcdf_PARALLEL}" STREQUAL "")
-      set(TPL_Netcdf_PARALLEL False CACHE INTERNAL
-          "True if netcdf compiled with parallel enabled")
-   endif()
-endif()
-message(STATUS "TPL_Netcdf_PARALLEL is ${TPL_Netcdf_PARALLEL}")
