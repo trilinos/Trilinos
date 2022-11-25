2cde27acebf314f76f158a01b8e073c66b9d761a
Greg Sjaardema <gsjaardema@gmail.com>
2022-11-25 22:57:38 +0100

    CMAKE: Clean up cmake-related files.

--- packages/seacas/libraries/ioss/cmake/Dependencies.cmake
+++ packages/seacas/libraries/ioss/cmake/Dependencies.cmake
@@ -1,12 +1,12 @@
-if(CMAKE_PROJECT_NAME STREQUAL "Seacas")
+if (CMAKE_PROJECT_NAME STREQUAL "Seacas")
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   LIB_OPTIONAL_PACKAGES SEACASExodus Zoltan
-  LIB_OPTIONAL_TPLS HDF5 Pamgen CGNS ParMETIS Faodel Cereal DLlib Pthread ADIOS2 Catalyst2 GTest Kokkos DataWarp fmt
+  LIB_OPTIONAL_TPLS HDF5 Pamgen CGNS ParMETIS Faodel Cereal DLlib Pthread ADIOS2 Catalyst2 ${SEACAS_GTest_TPL_name} Kokkos DataWarp fmt
 )
 else()
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   LIB_OPTIONAL_PACKAGES SEACASExodus Pamgen Zoltan Kokkos
-  LIB_OPTIONAL_TPLS HDF5 CGNS ParMETIS Faodel Cereal DLlib Pthread DataWarp ADIOS2 Catalyst2 GTest
+  LIB_OPTIONAL_TPLS HDF5 CGNS ParMETIS Faodel Cereal DLlib Pthread DataWarp ADIOS2 Catalyst2 ${SEACAS_GTest_TPL_name}
 )
 endif()
 
