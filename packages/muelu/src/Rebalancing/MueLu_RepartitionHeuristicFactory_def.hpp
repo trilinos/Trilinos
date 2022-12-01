d4315a8afb798b1fef42f5bf18b279f136e89170
Graham Harper <grahambenharper@gmail.com>
2022-12-01 15:59:19 -0700

    MueLu: update use of ExecSpace::concurrency() for the non-static case

--- packages/muelu/src/Rebalancing/MueLu_RepartitionHeuristicFactory_def.hpp
+++ packages/muelu/src/Rebalancing/MueLu_RepartitionHeuristicFactory_def.hpp
@@ -132,7 +132,7 @@ namespace MueLu {
 #if defined(KOKKOS_ENABLE_OPENMP)
     using execution_space = typename Node::device_type::execution_space;
     if (std::is_same<execution_space, Kokkos::OpenMP>::value)
-      thread_per_mpi_rank = execution_space::concurrency();
+      thread_per_mpi_rank = execution_space().concurrency();
 #endif
 
     if (minRowsPerThread > 0)
