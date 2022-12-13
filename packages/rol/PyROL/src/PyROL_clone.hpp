5829cb2dc8c5a6ce8fa944080b6b5835e550cb49
kliegeois <kimliegeois@ymail.com>
2022-12-12 22:18:37 -0700

    work

--- packages/rol/PyROL/src/PyROL_clone.hpp
+++ packages/rol/PyROL/src/PyROL_clone.hpp
@@ -1,16 +1,17 @@
 #include "Teuchos_RCP.hpp"
+#include "pybind11/pybind11.h"
 
 template <typename A, typename trampoline_A>
-class Teuchos::RCP<A> customClone(const trampoline_A *ptr_to_this, std::string const &function_name) {
+Teuchos::RCP<A> customClone(const trampoline_A *ptr_to_this, std::string const &function_name) {
     pybind11::gil_scoped_acquire gil;
-    pybind11::function overload = pybind11::get_overload(static_cast<const A *>(ptr_to_this), function_name);
+    pybind11::function overload = pybind11::get_overload(static_cast<const A *>(ptr_to_this), function_name.c_str());
     if (overload) {
         auto o = overload.operator()<pybind11::return_value_policy::reference>();
         auto self = pybind11::cast(ptr_to_this);
-        auto cloned = self.attr(function_name)();
+        auto cloned = self.attr(function_name.c_str())();
 
         auto keep_python_state_alive = Teuchos::rcp<pybind11::object>(new pybind11::object(cloned));
-        auto ptr = cloned.cast<trampoline_A*>();
+        auto ptr = pybind11::cast<trampoline_A *>(cloned);
 
         // aliasing shared_ptr: points to `A_trampoline* ptr` but refcounts the Python object
         return Teuchos::RCP<A>(keep_python_state_alive, ptr);		
