fe9e4355dc826b40d057cb90ce442689a9137ae0
Christian Glusa <caglusa@sandia.gov>
2022-12-14 10:35:37 -0700

    MueLu: Remove Epetra part 2

--- packages/muelu/cmake/Dependencies.cmake
+++ packages/muelu/cmake/Dependencies.cmake
@@ -1,10 +1,9 @@
 SET(LIB_REQUIRED_DEP_PACKAGES Teuchos Xpetra KokkosCore KokkosContainers KokkosKernels)
-SET(LIB_OPTIONAL_DEP_PACKAGES Amesos Amesos2 AvatarT Belos Epetra EpetraExt Teko
-                              Ifpack Ifpack2 Intrepid2 ML Tpetra
-                              Zoltan Zoltan2Core Stratimikos Thyra ThyraTpetraAdapters
-                              Isorropia)
+SET(LIB_OPTIONAL_DEP_PACKAGES Amesos2 AvatarT Belos Teko
+                              Ifpack2 Intrepid2 Tpetra
+                              Zoltan2Core Stratimikos Thyra ThyraTpetraAdapters)
 SET(TEST_REQUIRED_DEP_PACKAGES Galeri)
-SET(TEST_OPTIONAL_DEP_PACKAGES AztecOO Pamgen)
+SET(TEST_OPTIONAL_DEP_PACKAGES Pamgen)
 SET(LIB_REQUIRED_DEP_TPLS BLAS LAPACK)
 SET(LIB_OPTIONAL_DEP_TPLS Boost MATLAB AmgX CGAL ViennaCL MKL Avatar CUSPARSE MAGMASparse mlpack)
 SET(TEST_REQUIRED_DEP_TPLS)
