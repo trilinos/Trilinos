3accd1f7ddae13779c2a75482a1357975f3f342c
Drew Kouri <dpkouri@sandia.gov>
2023-01-06 16:33:56 -0700

    Updated the algorithm factory.

--- packages/rol/src/algorithm/TypeP/ROL_TypeP_AlgorithmFactory.hpp
+++ packages/rol/src/algorithm/TypeP/ROL_TypeP_AlgorithmFactory.hpp
@@ -49,6 +49,7 @@
 #include "ROL_TypeP_iPianoAlgorithm.hpp"
 #include "ROL_TypeP_QuasiNewtonAlgorithm.hpp"
 #include "ROL_TypeP_TrustRegionAlgorithm.hpp"
+#include "ROL_TypeP_InexactNewtonAlgorithm.hpp"
 #include "ROL_Types.hpp"
 
 namespace ROL {
@@ -57,29 +58,26 @@ namespace TypeP {
 /** \enum   ROL::EAlgorithmP
     \brief  Enumeration of bound constrained algorithm types.
 
-    \arg    ALGORITHM_P_PROXIMALGRADIENT    describe
+    \arg    ALGORITHM_P_LINESEARCH          describe
+    \arg    ALGORITHM_P_TRUSTREGION         describe
     \arg    ALGORITHM_P_SPECTRALGRADIENT    describe
     \arg    ALGORITHM_P_IPIANO              describe
-    \arg    ALGORITHM_P_QUASINEWTON         describe
-    \arg    ALGORITHM_P_TRUSTREGION         describe
  */
 enum EAlgorithmP{
-  ALGORITHM_P_PROXIMALGRADIENT = 0,
+  ALGORITHM_P_LINESEARCH = 0,
+  ALGORITHM_P_TRUSTREGION,
   ALGORITHM_P_SPECTRALGRADIENT,
   ALGORITHM_P_IPIANO,
-  ALGORITHM_P_QUASINEWTON,
-  ALGORITHM_P_TRUSTREGION,
   ALGORITHM_P_LAST
 };
 
 inline std::string EAlgorithmPToString(EAlgorithmP alg) {
   std::string retString;
   switch(alg) {
-    case ALGORITHM_P_PROXIMALGRADIENT:    retString = "Proximal Gradient";      break;
+    case ALGORITHM_P_LINESEARCH:          retString = "Line Search";            break;
+    case ALGORITHM_P_TRUSTREGION:         retString = "Trust Region";           break;
     case ALGORITHM_P_SPECTRALGRADIENT:    retString = "Spectral Gradient";      break;
     case ALGORITHM_P_IPIANO:              retString = "iPiano";                 break;
-    case ALGORITHM_P_QUASINEWTON:         retString = "Quasi-Newton";           break;
-    case ALGORITHM_P_TRUSTREGION:         retString = "Trust Region";           break;
     case ALGORITHM_P_LAST:                retString = "Last Type (Dummy)";      break;
     default:                              retString = "INVALID EAlgorithmP";
   }
@@ -92,11 +90,10 @@ inline std::string EAlgorithmPToString(EAlgorithmP alg) {
     \return 1 if the argument is a valid AlgorithmP; 0 otherwise.
   */
 inline int isValidAlgorithmP(EAlgorithmP alg){
-  return( (alg == ALGORITHM_P_PROXIMALGRADIENT)    ||
+  return( (alg == ALGORITHM_P_LINESEARCH)          ||
+          (alg == ALGORITHM_P_TRUSTREGION)         ||
           (alg == ALGORITHM_P_SPECTRALGRADIENT)    ||
           (alg == ALGORITHM_P_IPIANO)              ||
-          (alg == ALGORITHM_P_QUASINEWTON)         ||
-          (alg == ALGORITHM_P_TRUSTREGION)         ||
           (alg == ALGORITHM_P_LAST)
         );
 }
@@ -123,23 +120,32 @@ inline EAlgorithmP operator--(EAlgorithmP &type, int) {
 
 inline EAlgorithmP StringToEAlgorithmP(std::string s) {
   s = removeStringFormat(s);
-  for ( EAlgorithmP alg = ALGORITHM_P_PROXIMALGRADIENT; alg < ALGORITHM_P_LAST; alg++ ) {
+  for ( EAlgorithmP alg = ALGORITHM_P_LINESEARCH; alg < ALGORITHM_P_LAST; alg++ ) {
     if ( !s.compare(removeStringFormat(EAlgorithmPToString(alg))) ) {
       return alg;
     }
   }
-  return ALGORITHM_P_SPECTRALGRADIENT;
+  return ALGORITHM_P_TRUSTREGION;
 }
 
 template<typename Real>
 inline Ptr<Algorithm<Real>> AlgorithmFactory(ParameterList &parlist) {
   EAlgorithmP ealg = StringToEAlgorithmP(parlist.sublist("Step").get("Type","Trust Region"));
   switch(ealg) {
-    case ALGORITHM_P_PROXIMALGRADIENT:    return makePtr<ProxGradientAlgorithm<Real>>(parlist);
+    case ALGORITHM_P_LINESEARCH:
+    {
+      std::string desc
+        = parlist.sublist("Step").sublist("Line Search").sublist("Descent Method").get("Type","Newton-Krylov");
+      if (desc=="Newton-Krylov" || desc=="Newton")
+        return makePtr<InexactNewtonAlgorithm<Real>>(parlist);
+      else if (desc=="Quasi-Newton Method" || desc = "Quasi-Newton")
+        return makePtr<QuasiNewtonAlgorithm<Real>>(parlist);
+      else
+        return makePtr<ProxGradientAlgorithm<Real>>(parlist);
+    }
+    case ALGORITHM_P_TRUSTREGION:         return makePtr<TrustRegionAlgorithm<Real>>(parlist);
     case ALGORITHM_P_SPECTRALGRADIENT:    return makePtr<SpectralGradientAlgorithm<Real>>(parlist);
     case ALGORITHM_P_IPIANO:              return makePtr<iPianoAlgorithm<Real>>(parlist);
-    case ALGORITHM_P_QUASINEWTON:         return makePtr<QuasiNewtonAlgorithm<Real>>(parlist);
-    case ALGORITHM_P_TRUSTREGION:         return makePtr<TrustRegionAlgorithm<Real>>(parlist);
     default:                              return nullPtr;
   }
 }
