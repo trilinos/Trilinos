79123e300a3e982b04f5d1907941c9955d551db3
Samuel Browne <sebrown@sandia.gov>
2023-03-09 09:26:54 -0700

    Remove confusing comment
    It was referencing changes that have not happend (I can't find any other
    repositories with release branch names), so remove the instructions.
    
    User Support Ticket(s) or Story Referenced: TRILFRAME-557

--- Version.cmake
+++ Version.cmake
@@ -69,7 +69,3 @@ SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a releas
 SET(Trilinos_REPOSITORY_BRANCH "trilinos-release-14-0-branch" CACHE INTERNAL "")
 SET(Trilinos_EXTRAREPOS_BRANCH "master" CACHE INTERNAL "")
 SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
-
-# NOTE: Above, the extra repos for Trilinos don't have a 'develop' branch yet
-# so you have to run this with 'master'.  But on a release branch, these
-# should be change to all the same branch.
