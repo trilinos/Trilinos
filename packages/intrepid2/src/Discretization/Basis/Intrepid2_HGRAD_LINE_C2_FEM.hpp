da2dcb3e66a60fdc6e17f67e0d825f0f37b9dd4d
Mauro Perego <mperego@sandia.gov>
2023-03-15 12:39:07 -0600

    Intrepid2: Adding missing basis HGRAD_LINE_C2 and related test
    HGRAD_LINE_C2 orders the dofs according to Shards::Line<3> topology.
    It is required as subcell basis of other HGRAD_C2 functions.

--- packages/intrepid2/src/Discretization/Basis/Intrepid2_HGRAD_LINE_C2_FEM.hpp
+++ packages/intrepid2/src/Discretization/Basis/Intrepid2_HGRAD_LINE_C2_FEM.hpp
@@ -40,24 +40,22 @@
 // ************************************************************************
 // @HEADER
 
-/** \file   Intrepid2_HGRAD_LINE_C1_FEM.hpp
-    \brief  Header file for the Intrepid2::Basis_HGRAD_LINE_C1_FEM class.
-    \author Created by P. Bochev and D. Ridzal.
-            Kokkorized by Kyungjoo Kim
+/** \file   Intrepid2_HGRAD_LINE_C2_FEM.hpp
+    \brief  Header file for the Intrepid2::Basis_HGRAD_LINE_C2_FEM class.
 */
 
-#ifndef __INTREPID2_HGRAD_LINE_C1_FEM_HPP__
-#define __INTREPID2_HGRAD_LINE_C1_FEM_HPP__
+#ifndef __INTREPID2_HGRAD_LINE_C2_FEM_HPP__
+#define __INTREPID2_HGRAD_LINE_C2_FEM_HPP__
 
 #include "Intrepid2_Basis.hpp"
 
 namespace Intrepid2 {
 
-  /** \class  Intrepid2::Basis_HGRAD_LINE_C1_FEM
-      \brief  Implementation of the default H(grad)-compatible FEM basis of degree 1 on Line cell.
+  /** \class  Intrepid2::Basis_HGRAD_LINE_C2_FEM
+      \brief  Implementation of the default H(grad)-compatible FEM basis of degree 2 on Line cell.
 
-      Implements Lagrangian basis of degree 1 on the reference Line cell. The basis has
-      cardinality 2 and spans a COMPLETE linear polynomial space. Basis functions are dual
+      Implements Lagrangian basis of degree 2 on the reference Line cell. The basis has
+      cardinality 3 and spans a COMPLETE linear polynomial space. Basis functions are dual
       to a unisolvent set of degrees-of-freedom (DoF) defined and enumerated as follows:
 
       \verbatim
@@ -69,8 +67,10 @@ namespace Intrepid2 {
       |    0    |       0      |       0      |       0      |      1      |   L_0(u) = u(-1)          |
       |---------|--------------|--------------|--------------|-------------|---------------------------|
       |    1    |       0      |       1      |       0      |      1      |   L_1(u) = u(1)           |
+      |---------|--------------|--------------|--------------|-------------|---------------------------|
+      |    2    |       1      |       0      |       0      |      1      |   L_2(u) = u(0)           |
       |=========|==============|==============|==============|=============|===========================|
-      |   MAX   |  maxScDim=0  |  maxScOrd=1  |  maxDfOrd=0  |      -      |                           |
+      |   MAX   |  maxScDim=1  |  maxScOrd=1  |  maxDfOrd=0  |      -      |                           |
       |=========|==============|==============|==============|=============|===========================|
       \endverbatim
   */
@@ -78,13 +78,13 @@ namespace Intrepid2 {
   namespace Impl {
 
     /**
-      \brief See Intrepid2::Basis_HGRAD_LINE_C1_FEM
+      \brief See Intrepid2::Basis_HGRAD_LINE_C2_FEM
     */
-    class Basis_HGRAD_LINE_C1_FEM {
+    class Basis_HGRAD_LINE_C2_FEM {
     public:
-      typedef struct Line<2> cell_topology_type;
+      typedef struct Line<3> cell_topology_type;
       /**
-        \brief See Intrepid2::Basis_HGRAD_LINE_C1_FEM
+        \brief See Intrepid2::Basis_HGRAD_LINE_C2_FEM
       */
       template<EOperator opType>
       struct Serial {
@@ -106,7 +106,7 @@ namespace Intrepid2 {
                  const EOperator operatorType);
 
       /**
-        \brief See Intrepid2::Basis_HGRAD_LINE_C1_FEM
+        \brief See Intrepid2::Basis_HGRAD_LINE_C2_FEM
       */
       template<typename outputValueViewType,
                typename inputPointViewType,
@@ -140,7 +140,7 @@ namespace Intrepid2 {
             INTREPID2_TEST_FOR_ABORT( opType != OPERATOR_VALUE &&
                                       opType != OPERATOR_GRAD &&
                                       opType != OPERATOR_MAX,
-                                      ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C1_FEM::Serial::getValues) operator is not supported");
+                                      ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C2_FEM::Serial::getValues) operator is not supported");
 
           }
           }
@@ -152,7 +152,7 @@ namespace Intrepid2 {
   template<typename DeviceType = void,
            typename outputValueType = double,
            typename pointValueType = double>
-  class Basis_HGRAD_LINE_C1_FEM
+  class Basis_HGRAD_LINE_C2_FEM
     : public Basis<DeviceType,outputValueType,pointValueType> {
   public:
     using BasisBase = Basis<DeviceType,outputValueType,pointValueType>;
@@ -167,7 +167,7 @@ namespace Intrepid2 {
       
     /** \brief  Constructor.
      */
-    Basis_HGRAD_LINE_C1_FEM();
+    Basis_HGRAD_LINE_C2_FEM();
 
     using BasisBase::getValues;
 
@@ -184,7 +184,7 @@ namespace Intrepid2 {
                                       this->getBaseCellTopology(),
                                       this->getCardinality() );
 #endif
-      Impl::Basis_HGRAD_LINE_C1_FEM::
+      Impl::Basis_HGRAD_LINE_C2_FEM::
         getValues<DeviceType>( outputValues,
                                   inputPoints,
                                   operatorType );
@@ -196,13 +196,13 @@ namespace Intrepid2 {
 #ifdef HAVE_INTREPID2_DEBUG
       // Verify rank of output array.
       INTREPID2_TEST_FOR_EXCEPTION( dofCoords.rank() != 2, std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C1_FEM::getDofCoords) rank = 2 required for dofCoords array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C2_FEM::getDofCoords) rank = 2 required for dofCoords array");
       // Verify 0th dimension of output array.
       INTREPID2_TEST_FOR_EXCEPTION( static_cast<ordinal_type>(dofCoords.extent(0)) != this->basisCardinality_, std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C1_FEM::getDofCoords) mismatch in number of dof and 0th dimension of dofCoords array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C2_FEM::getDofCoords) mismatch in number of dof and 0th dimension of dofCoords array");
       // Verify 1st dimension of output array.
       INTREPID2_TEST_FOR_EXCEPTION( dofCoords.extent(1) != this->basisCellTopology_.getDimension(), std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C1_FEM::getDofCoords) incorrect reference cell (1st) dimension in dofCoords array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C2_FEM::getDofCoords) incorrect reference cell (1st) dimension in dofCoords array");
 #endif
       Kokkos::deep_copy(dofCoords, this->dofCoords_);
     }
@@ -213,10 +213,10 @@ namespace Intrepid2 {
 #ifdef HAVE_INTREPID2_DEBUG
       // Verify rank of output array.
       INTREPID2_TEST_FOR_EXCEPTION( dofCoeffs.rank() != 1, std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C1_FEM::getdofCoeffs) rank = 1 required for dofCoeffs array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C2_FEM::getdofCoeffs) rank = 1 required for dofCoeffs array");
       // Verify 0th dimension of output array.
       INTREPID2_TEST_FOR_EXCEPTION( static_cast<ordinal_type>(dofCoeffs.extent(0)) != this->getCardinality(), std::invalid_argument,
-                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C1_FEM::getdofCoeffs) mismatch in number of dof and 0th dimension of dofCoeffs array");
+                                    ">>> ERROR: (Intrepid2::Basis_HGRAD_LINE_C2_FEM::getdofCoeffs) mismatch in number of dof and 0th dimension of dofCoeffs array");
 #endif
       Kokkos::deep_copy(dofCoeffs, 1.0);
     }
@@ -224,18 +224,18 @@ namespace Intrepid2 {
     virtual
     const char*
     getName() const override {
-      return "Intrepid2_HGRAD_LINE_C1_FEM";
+      return "Intrepid2_HGRAD_LINE_C2_FEM";
     }
 
     BasisPtr<typename Kokkos::HostSpace::device_type,outputValueType,pointValueType>
     getHostBasis() const override{
-      return Teuchos::rcp(new Basis_HGRAD_LINE_C1_FEM<typename Kokkos::HostSpace::device_type,outputValueType,pointValueType>());
+      return Teuchos::rcp(new Basis_HGRAD_LINE_C2_FEM<typename Kokkos::HostSpace::device_type,outputValueType,pointValueType>());
     }
 
   };
 
 }// namespace Intrepid2
 
-#include "Intrepid2_HGRAD_LINE_C1_FEMDef.hpp"
+#include "Intrepid2_HGRAD_LINE_C2_FEMDef.hpp"
 
 #endif
