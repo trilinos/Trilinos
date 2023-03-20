9bcec1b95dd19430e2800124fc1c4e0fa42f966c
iyamazaki <ic.yamazaki@gmail.com>
2023-03-19 23:32:49 -0600

    Tacho : catch invalid pivots in teamed factorization

--- packages/shylu/shylu_node/tacho/src/impl/Tacho_TeamFunctor_FactorizeLDL.hpp
+++ packages/shylu/shylu_node/tacho/src/impl/Tacho_TeamFunctor_FactorizeLDL.hpp
@@ -53,6 +53,8 @@ private:
   size_type_array _buf_ptr;
   value_type_array _buf;
 
+  int *_rval;
+
 public:
   KOKKOS_INLINE_FUNCTION
   TeamFunctor_FactorizeLDL() = delete;
@@ -60,8 +62,8 @@ public:
   KOKKOS_INLINE_FUNCTION
   TeamFunctor_FactorizeLDL(const supernode_info_type &info, const ordinal_type_array &compute_mode,
                            const ordinal_type_array &level_sids, const ordinal_type_array &piv,
-                           const value_type_array &diag, const value_type_array buf)
-      : _info(info), _compute_mode(compute_mode), _level_sids(level_sids), _piv(piv), _diag(diag), _buf(buf) {}
+                           const value_type_array &diag, const value_type_array buf, int *rval)
+      : _info(info), _compute_mode(compute_mode), _level_sids(level_sids), _piv(piv), _diag(diag), _buf(buf), _rval(rval) {}
 
   inline void setRange(const ordinal_type pbeg, const ordinal_type pend) {
     _pbeg = pbeg;
@@ -82,13 +84,19 @@ public:
     using TrsmAlgoType = typename TrsmAlgorithm::type;
     using GemmAlgoType = typename GemmAlgorithm::type;
 
+    int err = 0;
     const ordinal_type m = s.m, n = s.n, n_m = n - m;
     if (m > 0) {
       UnmanagedViewType<value_type_matrix> ATL(s.u_buf, m, m);
       Symmetrize<Uplo::Upper, Algo::Internal>::invoke(member, ATL);
       member.team_barrier();
-      LDL<Uplo::Lower, LDL_AlgoType>::invoke(member, ATL, P, W);
+      err = LDL<Uplo::Lower, LDL_AlgoType>::invoke(member, ATL, P, W);
       member.team_barrier();
+      if (err != 0) {
+        Kokkos::atomic_add(_rval, 1);
+        return;
+      }
+
       LDL<Uplo::Lower, LDL_AlgoType>::modify(member, ATL, P, D);
       member.team_barrier();
 
@@ -122,6 +130,7 @@ public:
     using TrsmAlgoType = typename TrsmAlgorithm::type;
     using GemmAlgoType = typename GemmAlgorithm::type;
 
+    int err = 0;
     const value_type one(1), minus_one(-1), zero(0);
     const ordinal_type m = s.m, n = s.n, n_m = n - m;
     if (m > 0) {
@@ -130,8 +139,12 @@ public:
       Symmetrize<Uplo::Upper, Algo::Internal>::invoke(member, ATL);
       member.team_barrier();
 
-      LDL<Uplo::Lower, LDL_AlgoType>::invoke(member, ATL, P, W);
+      err = LDL<Uplo::Lower, LDL_AlgoType>::invoke(member, ATL, P, W);
       member.team_barrier();
+      if (err != 0) {
+        Kokkos::atomic_add(_rval, 1);
+        return;
+      }
 
       LDL<Uplo::Lower, LDL_AlgoType>::modify(member, ATL, P, D);
       member.team_barrier();
@@ -178,6 +191,7 @@ public:
     using TrsmAlgoType = typename TrsmAlgorithm::type;
     using GemmAlgoType = typename GemmAlgorithm::type;
 
+    int err = 0;
     const value_type one(1), minus_one(-1), zero(0);
     const ordinal_type m = s.m, n = s.n, n_m = n - m;
     if (m > 0) {
@@ -186,8 +200,12 @@ public:
       Symmetrize<Uplo::Upper, Algo::Internal>::invoke(member, ATL);
       member.team_barrier();
 
-      LDL<Uplo::Lower, LDL_AlgoType>::invoke(member, ATL, P, W);
+      err = LDL<Uplo::Lower, LDL_AlgoType>::invoke(member, ATL, P, W);
       member.team_barrier();
+      if (err != 0) {
+        Kokkos::atomic_add(_rval, 1);
+        return;
+      }
 
       LDL<Uplo::Lower, LDL_AlgoType>::modify(member, ATL, P, D);
 
