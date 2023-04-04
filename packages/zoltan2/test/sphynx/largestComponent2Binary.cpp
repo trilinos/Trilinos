f428e4bd743c1118a0ed3b417b4805938809ca7a
Jennifer Loe <jloe@sandia.gov>
2023-04-03 18:22:30 -0600

    Add comments to code.

--- packages/zoltan2/test/sphynx/largestComponent2Binary.cpp
+++ packages/zoltan2/test/sphynx/largestComponent2Binary.cpp
@@ -1,10 +1,59 @@
-// Seher Acer, 2019
+// @HEADER
+//
+// ***********************************************************************
+//
+//   Zoltan2: A package of combinatorial algorithms for scientific computing
+//                  Copyright 2012 Sandia Corporation
+//
+// Under the terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Seher Acer        (sacer@sandia.gov)
+//                    Erik Boman        (egboman@sandia.gov)
+//                    Siva Rajamanickam (srajama@sandia.gov)
+// ***********************************************************************
+//
+// @HEADER
+//
+/////////////////////////////////////////////////////////////////////////////
+// Written by Seher Acer, 2019
 // This code reads a mtx file and writes a binary file in CRS format:
 // [nrows nnzs rowPtr[0 nrows] colInd[0 nnzs]
 // 1) It symmetrizes the input, regardless of it is already symmetric or not
 // 2) It removes the diagonal entries
 // 3) The column indices are sorted in increasing order for each row
 // 4) Index base for the output file is 0
+/////////////////////////////////////////////////////////////////////////////
+// The corresponding file reader exists in "readMatrixFromBinaryFile.hpp"
+// Note: This is research code. We do not guarantee it works in all cases. 
+/////////////////////////////////////////////////////////////////////////////
 
 #include <iostream>
 #include <fstream>
