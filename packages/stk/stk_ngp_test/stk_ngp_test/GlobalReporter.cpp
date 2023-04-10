05ff29185cc225b28da3bbc39ddd1910bd8710b5
Daniel Arndt <arndtd@ornl.gov>
2023-04-10 11:44:51 -0400

    Fix preprocessor if-branch in macro

--- packages/stk/stk_ngp_test/stk_ngp_test/GlobalReporter.cpp
+++ packages/stk/stk_ngp_test/stk_ngp_test/GlobalReporter.cpp
@@ -34,12 +34,16 @@ sycl::ext::oneapi::experimental::device_global<
 
 NGP_TEST_INLINE ReporterBase*& getDeviceReporterOnDevice()
 {
-  KOKKOS_IF_ON_DEVICE((
 #ifndef KOKKOS_ENABLE_SYCL
+  KOKKOS_IF_ON_DEVICE((
     __device__ static ReporterBase* deviceReporterOnDevice = nullptr;
-#endif
     return deviceReporterOnDevice;
   ))
+#else
+  KOKKOS_IF_ON_DEVICE((
+    return deviceReporterOnDevice;
+  ))
+#endif
   KOKKOS_IF_ON_HOST((
     static ReporterBase* deviceReporterOnDevice = nullptr;
     return deviceReporterOnDevice;
