065f78ef16c0a04cd16be174a117664ff1463117
Brian Kelley <bmkelle@sandia.gov>
2023-04-26 14:50:49 -0600

    Tpetra: fix #11820
    In #11679, the WDV "sync path" was disabled when in a host-parallel region.
    However, it turns out that Serial().in_parallel() always returns false, so
    parallel_ regions running on Serial would suffer the overhead of WDV tracking.
    This brings back the explicit enable/disable of WDV tracking.
    This means a few more lines of boilerplate, but it fixes perf regression #11820,
    and in fact makes fillComplete in FEAssembly 20% faster than before #11679.

--- packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
+++ packages/tpetra/core/src/Tpetra_BlockCrsMatrix_def.hpp
@@ -2612,6 +2612,7 @@ public:
         // The following parallel_for needs const access to the local values of src.
         // (the local graph is also accessed on host, but this does not use WDVs).
         getValuesHost();
+        Details::disableWDVTracking();
         Kokkos::parallel_for
           (policy,
            [=](const typename policy_type::member_type &member) {
@@ -2659,6 +2660,7 @@ public:
               }
             }
           }); // for each LID (of a row) to send
+        Details::enableWDVTracking();
       }
     } // if totalNumEntries > 0
 
@@ -2883,6 +2885,7 @@ public:
 
       //The following parallel_for modifies values on host while unpacking.
       getValuesHostNonConst();
+      Details::disableWDVTracking();
       Kokkos::parallel_for
         ("Tpetra::BlockCrsMatrix::unpackAndCombine: unpack", policy,
          [=] (const typename policy_type::member_type& member) {
@@ -2999,6 +3002,7 @@ public:
             return;
           }
         }); // for each import LID i
+      Details::enableWDVTracking();
     }
 
     if (errorDuringUnpack () != 0) {
