4bad0e0b7a2fcd54394132e64cac360ef0378aca
Jonathan Hu <jhu@sandia.gov>
2023-05-31 11:39:00 -0700

    Framework: removing obsolete nightly build scripts
    Addresses #11933.

--- cmake/ctest/drivers/pu241/gcc-4.5.1-serial-release-ps-options.cmake
+++
@@ -1,6 +0,0 @@
-#
-# checkin-test-fissile4.sh PS SERIAL_RELEASE build
-#
-
-INCLUDE(${CMAKE_CURRENT_LIST_DIR}/gcc-4.5.1-serial-options.cmake)
-INCLUDE(${CMAKE_CURRENT_LIST_DIR}/gcc-4.5.1-release-options.cmake)
