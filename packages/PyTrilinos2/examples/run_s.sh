f00663cbf32029dbc9eac2ca345b84d963e0ae3b
kliegeois <kimliegeois@ymail.com>
2023-06-07 17:07:39 -0600

    clean the PR and merge develop

--- packages/PyTrilinos2/examples/run_s.sh
+++
@@ -1,3 +0,0 @@
-export PYTHONPATH=/home/knliege/local/trilinos/pytrilinos_test/lib/python3.8/site-packages:$PYTHONPATH
-export LD_LIBRARY_PATH=/home/knliege/local/trilinos/pytrilinos_test/lib:$LD_LIBRARY_PATH
-mpirun -np 4 --map-by ppr:2:socket python CG.py
