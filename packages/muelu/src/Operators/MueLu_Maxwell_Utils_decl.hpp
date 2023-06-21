442d7c2e67d90d26fca836355bf506920721c571
Chris Siefert <csiefer@sandia.gov>
2023-06-21 12:25:26 -0600

    MueLu: Adding a const

--- packages/muelu/src/Operators/MueLu_Maxwell_Utils_decl.hpp
+++ packages/muelu/src/Operators/MueLu_Maxwell_Utils_decl.hpp
@@ -127,7 +127,7 @@ namespace MueLu {
 
     // Performs an RAP
     static RCP<Xpetra::Matrix<Scalar,LocalOrdinal,GlobalOrdinal,Node> >
-    PtAPWrapper(RCP<Matrix>& A,RCP<Matrix>& P, Teuchos::ParameterList &params, std::string & label);
+    PtAPWrapper(const RCP<Matrix>& A,const RCP<Matrix>& P, Teuchos::ParameterList &params, std::string & label);
 
   };
 
