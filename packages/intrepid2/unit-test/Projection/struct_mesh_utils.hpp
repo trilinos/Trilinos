728532f2b1d52193811bf019b04d52796e8c629a
Mauro Perego <mperego@sandia.gov>
2023-07-06 10:40:07 -0600

    Remove shadow warnings

--- packages/intrepid2/unit-test/Projection/struct_mesh_utils.hpp
+++ packages/intrepid2/unit-test/Projection/struct_mesh_utils.hpp
@@ -207,8 +207,7 @@ namespace Test {
     // Get nodal coordinates
     vertexCoords = ViewType("vertexCoords", numVertices, dim);
     auto hVertexCoord = Kokkos::create_mirror_view(vertexCoords);
-    ordinal_type ivertex = 0;
-    for (ordinal_type k=0; k<NZ+1; k++) {
+    for (ordinal_type ivertex=0, k=0; k<NZ+1; k++) {
       for (ordinal_type j=0; j<NY+1; j++) {
         for (ordinal_type i=0; i<NX+1; i++) {
           hVertexCoord(ivertex,0) = leftX + (double)i*hx;
@@ -330,8 +329,7 @@ namespace Test {
     // Get nodal coordinates
     vertexCoords = ViewType("vertexCoords", numVertices, dim);
     auto hVertexCoord = Kokkos::create_mirror_view(vertexCoords);
-    ordinal_type ivertex = 0;
-    for (ordinal_type j=0; j<NY+1; j++) {
+    for (ordinal_type ivertex = 0, j=0; j<NY+1; j++) {
       for (ordinal_type i=0; i<NX+1; i++) {
         hVertexCoord(ivertex,0) = leftX + (double)i*hx;
         hVertexCoord(ivertex,1) = leftY + (double)j*hy;
@@ -412,8 +410,7 @@ namespace Test {
     // Get nodal coordinates
     vertexCoords = ViewType("vertexCoords", numVertices, dim);
     auto hVertexCoord = Kokkos::create_mirror_view(vertexCoords);
-    ordinal_type ivertex = 0;
-    for (ordinal_type j=0; j<NY+1; j++) {
+    for (ordinal_type ivertex = 0, j=0; j<NY+1; j++) {
       for (ordinal_type i=0; i<NX+1; i++) {
         hVertexCoord(ivertex,0) = leftX + (double)i*hx;
         hVertexCoord(ivertex,1) = leftY + (double)j*hy;
