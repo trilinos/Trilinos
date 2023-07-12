cb4090def796ab4ecdc5aa099329c4b8534d40a5
Roscoe A. Bartlett <rabartl@sandia.gov>
2023-07-12 06:19:27 -0600

    Update logic for enabling TrilinosInstallTests in CI testing (#12024)
    * Now TrilinosInstallTests will be enabled if any Trilinos package gets
      enabled.
    
    * Removed dependence of TrilinosInstallTests on Tpetra.
    
    This should give more consistent installation testing with Trilinos.  Prior to
    this commit, only changes to Tpetra and packages upstream from Tpetra would
    trigger the enable of the TrilinosInstallTests package and the running of
    these tests.  Now, any change to any Trilinos packge will trigger some
    installation testing.
    
    See new TrilinosInstallTests/READMME.md file for details.

--- packages/TrilinosInstallTests/cmake/Dependencies.cmake
+++ packages/TrilinosInstallTests/cmake/Dependencies.cmake
@@ -1,8 +1,2 @@
-tribits_package_define_dependencies(
-  LIB_OPTIONAL_PACKAGES  Tpetra)
-# NOTE: By declaring a dependence on Tpetra, if that package or any package
-# upstream from it changes, then it will trigger the enable of this package
-# and the running of installation tests and the demo example.  But since
-# TrilinosInstallTests does not have any libs or execs of its own (all of its
-# work is done in tests), there is no link-time dependency here in the build
-# dir.
+tribits_package_define_dependencies()
+# See discussion of the CI testing process in the file ./../README.md
