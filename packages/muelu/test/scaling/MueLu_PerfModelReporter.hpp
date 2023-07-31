39669fe949d2db5ce5216612d4393dccbce34d4c
Chris Siefert <csiefer@sandia.gov>
2023-07-31 13:11:09 -0600

    MueLu: Mods to get proc specific perf info

--- packages/muelu/test/scaling/MueLu_PerfModelReporter.hpp
+++ packages/muelu/test/scaling/MueLu_PerfModelReporter.hpp
@@ -174,9 +174,8 @@ void report_spmv_performance_models(const Teuchos::RCP<const Matrix> & A, int nr
     double memory_traffic = (double)SPMV_object_size[i] *(double)SPMV_num_objects[i];
     gb_per_sec[i] = convert_time_to_bandwidth_gbs(avg_time,1,memory_traffic);
     
-    if(verbose && rank == 0) {
-
-      std::cout<<prefix<< "Local: "<<SPMV_test_names[i] << " # Scalars = "<<memory_traffic/sizeof(SC) << " time per call = "<<avg_time*1e6 << " us. GB/sec = "<<gb_per_sec[i]<<std::endl;
+    if(verbose) {
+      std::cout<<prefix<<rank<< "Local: "<<SPMV_test_names[i] << " # Scalars = "<<memory_traffic/sizeof(SC) << " time per call = "<<avg_time*1e6 << " us. GB/sec = "<<gb_per_sec[i]<<std::endl;
     }
   }
   
@@ -211,6 +210,12 @@ void report_spmv_performance_models(const Teuchos::RCP<const Matrix> & A, int nr
     minimum_local_composite_time +=  spmv_memory_bytes[i] / (GB * gb_per_sec[i]);
 
   double minimum_local_all_time = spmv_memory_bytes[NUM_TIMERS-1] / (GB * gb_per_sec[NUM_TIMERS-1]);
+
+  if(verbose) {
+    std::cout<<prefix<<rank<< "Local: composite      = "<<minimum_local_composite*1e6<<" us.\n"
+             <<prefix<<rank<< "Local: all            = "<<minimum_all_composite*1e6<<" us.\n";
+  }
+
 
   /***************************************************************************/
   // *** Calculate Remote part of the SPMV ***
@@ -281,21 +286,26 @@ void report_spmv_performance_models(const Teuchos::RCP<const Matrix> & A, int nr
         halo_time = PM.halopong_device_lookup(avg_size_per_msg);
       }
 
-      if(verbose && rank == 0) {
-         std::cout<<prefix<<"****** Remote Time Model Results ******"<<std::endl;
-         std::cout<<prefix<< "Remote: same      = "<<same_time*1e6<<" us.\n"
-                  <<prefix<< "Remote: permutes  = "<<permute_time*1e6<<" us.\n"
-                  <<prefix<< "Remote: exports   = "<<export_time*1e6<<" us.\n"
-                  <<prefix<< "Remote: remotes   = "<<remote_time*1e6<<" us.\n"
-                  <<prefix<< "Remote: sends len = "<<total_send_length<<" time = "<<send_time*1e6<<" us.\n"
-                  <<prefix<< "Remote: recvs len = "<<total_recv_length<<" time  = "<<recv_time*1e6<<" us.\n"
-                  <<prefix<< "Remote: halo avg  = "<<(size_t)avg_size_per_msg<<" time  = "<<halo_time*1e6<<" us.\n"<<std::endl;
-      }
 
       // NOTE: For now we'll do comm time as the larger of send/recv.  Not sure this is
       // really the optimal thing to do, but we'll start here.
       time_communicate_ping = std::max(send_time,recv_time);
       time_communicate_halo = halo_time;
+
+      if(verbose) {
+        std::cout<<prefix<<rank<<"****** Remote Time Model Results ******"<<std::endl;
+        std::cout<<prefix<<rank<< "Remote: same      = "<<same_time*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: permutes  = "<<permute_time*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: exports   = "<<export_time*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: remotes   = "<<remote_time*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: sends len = "<<total_send_length<<" time = "<<send_time*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: recvs len = "<<total_recv_length<<" time  = "<<recv_time*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: pack/unpack OOP = "<< time_pack_unpack_outofplace*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: pack/unpack INP = "<< time_pack_unpack_inplace*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: ping avg  = "<<(size_t)avg_size_per_msg<<" time  = "<<time_communicate_ping*1e6<<" us.\n"
+                 <<prefix<<rank<< "Remote: halo avg  = "<<(size_t)avg_size_per_msg<<" time  = "<<time_communicate_halo*1e6<<" us.\n"<<std::endl;
+      }
+      
     }
   }
   double minimum_time_in_place_ping     = time_communicate_ping + time_pack_unpack_inplace;
@@ -305,18 +315,10 @@ void report_spmv_performance_models(const Teuchos::RCP<const Matrix> & A, int nr
 
 
   /***************************************************************************/
-  if(rank == 0)
-    std::cout << "\n\n========================================================\n"
-              <<prefix<< "Minimum time model (composite) : " << minimum_local_composite_time << std::endl
-              <<prefix<< "Minimum time model (all)       : " << minimum_local_all_time << std::endl
-              <<prefix<< "Pack/unpack in-place           : " << time_pack_unpack_inplace << std::endl
-              <<prefix<< "Pack/unpack out-of-place       : " << time_pack_unpack_outofplace << std::endl
-              <<prefix<< "Communication time (ping)      : " << time_communicate_ping << std::endl
-              <<prefix<< "Communication time (halo)      : " << time_communicate_halo << std::endl;
-  
+ 
 
   // Get global average/max time sums
-  constexpr int NUM_TIMES=10;
+  constexpr int NUM_TIMES=12;
   double avg_times[NUM_TIMES];
   double max_times[NUM_TIMES];
   {
@@ -328,8 +330,9 @@ void report_spmv_performance_models(const Teuchos::RCP<const Matrix> & A, int nr
     double h_ooplace = minimum_time_out_of_place_halo;
 
     double all_times_local[NUM_TIMES] = {
-                 comp,comp+p_inplace,comp+p_ooplace,comp+h_inplace,comp+h_ooplace,
-                 alls,alls+p_inplace,alls+p_ooplace,alls+h_inplace,alls+h_ooplace};
+      comp,comp+p_inplace,comp+p_ooplace,comp+h_inplace,comp+h_ooplace, //0-4
+      alls,alls+p_inplace,alls+p_ooplace,alls+h_inplace,alls+h_ooplace,//5-9
+      time_communicate_ping,time_communicate_halo };//10,11
 
     if(nproc > 1) {
       Teuchos::reduceAll(*comm, Teuchos::REDUCE_SUM, NUM_TIMES, &all_times_local[0], &avg_times[0]);
@@ -341,6 +344,17 @@ void report_spmv_performance_models(const Teuchos::RCP<const Matrix> & A, int nr
       for(int i=0; i<NUM_TIMES; i++)
         avg_times[i] = max_times[i] = all_times_local[i];
     }
+
+    if(rank == 0)
+      std::cout << "\n\n========================================================\n"
+                <<prefix<< "Minimum time model (composite) : " << avg_times[0] << "\n"
+                <<prefix<< "Minimum time model (all)       : " << avg_times[5] << "\n"
+                <<prefix<< "Pack/unpack in-place           : " << avg_times[1] - avg_times[0] - avg_times[10]<< "\n" // comp+ping_pack_inplace - comp - ping
+                <<prefix<< "Pack/unpack out-of-place       : " << avg_times[2] - avg_times[0] - avg_times[10]<< "\n" // comp+ping_pack_ooplace - comp - ping
+                <<prefix<< "Communication time (ping)      : " << avg_times[10] << "\n"
+                <<prefix<< "Communication time (halo)      : " << avg_times[11] << std::endl;
+    
+
   }
 
   // Iterate through all of the "MV" timers
