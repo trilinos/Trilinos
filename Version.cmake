62dd0bcdb570ab34b33bfd300419a64279040400
Caleb Leandre Jackson <caljack@ascicgpu17.sandia.gov>
2023-08-01 09:16:53 -0600

    trilinos-develop-14.4-branch-update

--- Version.cmake
+++ Version.cmake
@@ -59,10 +59,10 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 14.3)
+SET(Trilinos_VERSION 14.4)
 SET(Trilinos_MAJOR_VERSION 14)
-SET(Trilinos_MAJOR_MINOR_VERSION 140300)
-SET(Trilinos_VERSION_STRING "14.3 (Dev)")
+SET(Trilinos_MAJOR_MINOR_VERSION 140400)
+SET(Trilinos_VERSION_STRING "14.4 (Dev)")
 SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
