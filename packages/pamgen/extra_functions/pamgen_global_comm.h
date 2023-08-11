9934499545417e7fa6c0316469fa4c722e0be948
Arkadiusz Szczepkowicz <arek.szczepkowicz@ng-analytics.com>
2023-08-11 16:25:00 +0200

    Pamgen: Remove use of hardcoded MPI_COMM_WORLD

---
+++ packages/pamgen/extra_functions/pamgen_global_comm.h
@@ -0,0 +1,28 @@
+
+#ifndef PAMGEN_GLOBAL_COMM_HPP
+#define PAMGEN_GLOBAL_COMM_HPP
+
+#ifdef HAVE_MPI
+
+#include <mpi.h>
+#include <mutex>
+
+namespace PAMGEN_NEVADA {
+
+static std::mutex mpi_mutex;
+static MPI_Comm Global_MPI_Comm = MPI_COMM_WORLD; // CHECK: ALLOW MPI_COMM_WORLD
+
+inline void initialize_global_comm(MPI_Comm comm) {
+    std::lock_guard<std::mutex> guard(mpi_mutex);
+    Global_MPI_Comm = comm;
+}
+
+inline MPI_Comm get_global_comm() {
+    std::lock_guard<std::mutex> guard(mpi_mutex);
+    return Global_MPI_Comm;
+}
+
+}
+
+#endif // HAVE_MPI
+#endif // PAMGEN_GLOBAL_COMM_HPP
