6b577a2d57bac760c2cfba7beec3f5637bd2e897
Heather Maria Switzer <hmswitz@s1051541.srn.sandia.gov>
2023-08-16 08:35:44 -0600

    trying again

---
+++ packages/anasazi/tpetra/test/Randomized/rand_driver.cpp
@@ -0,0 +1,231 @@
+// @HEADER
+// ***********************************************************************
+//
+//                 Anasazi: Block Eigensolvers Package
+//                 Copyright 2004 Sandia Corporation
+//
+// Under terms of Contract DE-AC04-94AL85000 with Sandia Corporation,
+// the U.S. Government retains certain rights in this software.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// 1. Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright
+// notice, this list of conditions and the following disclaimer in the
+// documentation and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the Corporation nor the names of the
+// contributors may be used to endorse or promote products derived from
+// this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY SANDIA CORPORATION "AS IS" AND ANY
+// EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+// PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL SANDIA CORPORATION OR THE
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// Questions? Contact Michael A. Heroux (maherou@sandia.gov)
+//
+// ***********************************************************************
+// @HEADER
+//
+
+#include "AnasaziConfigDefs.hpp"
+#include "AnasaziTypes.hpp"
+#include "AnasaziTpetraAdapter.hpp"
+#include "AnasaziBasicEigenproblem.hpp"
+#include "AnasaziRandomizedSolMgr.hpp"
+
+#include <Teuchos_CommandLineProcessor.hpp>
+
+#include <Tpetra_Core.hpp>
+#include <Tpetra_CrsMatrix.hpp>
+#include <MatrixMarket_Tpetra.hpp>
+
+template <class ST>
+void print(std::vector<ST> const &a) {
+  std::cout << "The vector elements are : ";
+
+  for(int i=0; i < a.size(); i++)
+    std::cout << a.at(i) << ' ';
+}
+
+int main(int argc, char *argv[])
+{
+  using Teuchos::RCP;
+  using Teuchos::rcp;
+  using Teuchos::tuple;
+  using Tpetra::CrsMatrix;
+  using Tpetra::Map;
+  using std::cout;
+  using std::endl;
+
+  typedef double                              ST;
+  typedef Teuchos::ScalarTraits<ST>          SCT;
+  typedef SCT::magnitudeType                  MT;
+  typedef Tpetra::MultiVector<ST>             MV;
+  typedef Tpetra::Operator<ST>                OP;
+  typedef Anasazi::MultiVecTraits<ST,MV>     MVT;
+  typedef Anasazi::OperatorTraits<ST,MV,OP>  OPT;
+  const ST ONE  = SCT::one();
+
+  Tpetra::ScopeGuard tpetraScope (&argc, &argv);
+  RCP<const Teuchos::Comm<int> > comm = Tpetra::getDefaultComm ();
+
+  const int MyPID = comm->getRank ();
+
+  bool testFailed;
+  bool verbose = true;
+  bool debug = false;
+  std::string ortho("ICGS");
+  std::string filename("simple.mtx");
+  int nev = 4;
+  int nsteps = 3;
+  int blockSize = 6;
+  MT tol = 1.0e-2;
+  int resFreq = 0;
+  int orthoFreq = 0;
+
+  Teuchos::CommandLineProcessor cmdp(false,true);
+  cmdp.setOption("verbose","quiet",&verbose,"Print messages and results.");
+  cmdp.setOption("debug","nodebug",&debug,"Print debugging information.");
+  cmdp.setOption("ortho",&ortho,"Orthogonalization method (DGKS, ICGS, or SVQB)");
+  cmdp.setOption("nev",&nev,"Number of eigenvalues to compute.");
+  cmdp.setOption("nsteps",&nsteps,"Number of times to apply A. ('q' parameter)");
+  cmdp.setOption("blockSize",&blockSize,"Block size for the algorithm.");
+  cmdp.setOption("tol",&tol,"Tolerance for convergence.");
+  cmdp.setOption("filename",&filename,"Filename for Matrix Market test matrix.");
+  cmdp.setOption("orthoFreq",&orthoFreq,"How many iterations should pass before reorthogonalizing the basis (not including when computing residual norms).");
+  cmdp.setOption("resFreq",&resFreq,"How many iterations should pass before computing the residuals (Orthogonalization of the basis is done here).");
+  if (cmdp.parse(argc,argv) != Teuchos::CommandLineProcessor::PARSE_SUCCESSFUL) {
+    return -1;
+  }
+  if ( blockSize < nev ){
+    if(MyPID == 0) std::cout << "Block size must be greater than or equal to num evals. Increasing block size to " << nev << std::endl;
+    blockSize = nev;
+  }
+
+  RCP<CrsMatrix<ST>> A = Tpetra::MatrixMarket::Reader<CrsMatrix<ST> >::readSparseFile(filename,comm);
+  RCP<const Tpetra::Map<> > map = A->getDomainMap();
+
+  // Create initial vectors
+  RCP<MV> randVecs = rcp(new MV(map,blockSize));
+  randVecs->randomize();
+
+  // Create eigenproblem
+  RCP<Anasazi::BasicEigenproblem<ST,MV,OP> > problem =
+    rcp (new Anasazi::BasicEigenproblem<ST,MV,OP> (A, randVecs));
+  problem->setNEV (nev);
+  // Inform the eigenproblem that you are done passing it information
+  bool boolret = problem->setProblem ();
+  if (! boolret) {
+    if (MyPID == 0) {
+      cout << "Anasazi::BasicEigenproblem::SetProblem() returned with error." << endl
+           << "End Result: TEST FAILED" << endl;
+    }
+    return -1;
+  }
+
+  // Set verbosity level
+  int verbosity = Anasazi::Errors + Anasazi::Warnings + Anasazi::FinalSummary + Anasazi::TimingDetails;
+  if (verbose) {
+    verbosity += Anasazi::IterationDetails;
+  }
+  if (debug) {
+    verbosity += Anasazi::Debug;
+  }
+
+  // Eigensolver parameters
+  //
+  // Create parameter list to pass into the solver manager
+  Teuchos::ParameterList MyPL;
+  MyPL.set( "Verbosity", verbosity );
+  MyPL.set( "Block Size", blockSize );
+  MyPL.set( "Maximum Iterations", nsteps );
+  MyPL.set( "Convergence Tolerance", tol );
+  MyPL.set( "Orthogonalization", ortho ); 
+  MyPL.set( "Orthogonalization Frequency", orthoFreq ); 
+  MyPL.set( "Residual Frequency", orthoFreq ); 
+  //
+  // Create the solver manager
+  Anasazi::Experimental::RandomizedSolMgr<ST,MV,OP> MySolverMgr(problem, MyPL);
+  std::cout << "DEBUG: Created solver manager. Calling solve." << std::endl;
+  //int numItsSlvr = MySolverMgr.getNumIters();
+  //std::cout << "DEBUG: Num iters is : " << numItsSlvr << std::endl;
+
+  // Solve the problem to the specified tolerances or length
+  Anasazi::ReturnType returnCode = MySolverMgr.solve();
+  testFailed = false;
+  if (returnCode != Anasazi::Converged) {
+    testFailed = true;
+  }
+
+  // Extract evects/evals from solution
+  Anasazi::Eigensolution<ST,MV> sol = problem->getSolution();
+  RCP<MV> evecs = sol.Evecs;
+  int numev = sol.numVecs;
+
+  std::cout << "Verifying Eigenvector residuals" << std::endl;
+  // Verify Evec residuals by hand. 
+  if (numev > 0) {
+    std::ostringstream os;
+    os.setf(std::ios::scientific, std::ios::floatfield);
+    os.precision(6);
+
+    // Compute the direct residual
+    std::vector<MT> normV( numev );
+    Teuchos::SerialDenseMatrix<int,ST> T (numev, numev);
+    //std::cout << "About to try to access the evals. Bet it crashes here. Haha." << std::endl;
+    for (int i = 0; i < numev; ++i) {
+      T(i,i) = sol.Evals[i].realpart;
+    }
+    //std::cout << "If you see this, it didn't really crash there." << std::endl;
+    RCP<MV> Avecs = MVT::Clone( *evecs, numev );
+
+    OPT::Apply( *A, *evecs, *Avecs );
+
+    MVT::MvTimesMatAddMv( -ONE, *evecs, T, ONE, *Avecs );
+    MVT::MvNorm( *Avecs, normV );
+
+    os << "Direct residual norms computed in Tpetra_RandSolve_test.exe" << endl
+       << std::setw(20) << "Eigenvalue" << std::setw(20) << "Residual  " << endl
+       << "----------------------------------------" << endl;
+    for (int i=0; i<numev; i++) {
+      if ( SCT::magnitude(sol.Evals[i].realpart) != SCT::zero() ) {
+        normV[i] = SCT::magnitude(normV[i]/sol.Evals[i].realpart);
+      }
+      os << std::setw(20) << sol.Evals[i].realpart << std::setw(20) << normV[i] << endl;
+      if ( normV[i] > tol ) {
+        testFailed = true;
+      }
+    }
+    if (MyPID==0) {
+      cout << endl << os.str() << endl;
+    }
+  }
+
+  if (testFailed) {
+    if (MyPID==0) {
+      cout << "End Result: TEST FAILED" << endl;
+    }
+    return -1;
+  }
+  //
+  // Default return value
+  //
+  if (MyPID==0) {
+    cout << "End Result: TEST PASSED" << endl;
+  }
+  return 0;
+
+}
