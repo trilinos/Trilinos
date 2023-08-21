b4f3ecb4b382376672008c71724197c9c20c78aa
Brian Kelley <bmkelle@sandia.gov>
2023-08-21 10:22:26 -0600

    Fix incorrect total - computeOffsetsFromConstantCounts

--- packages/tpetra/core/src/Tpetra_Details_computeOffsets.hpp
+++ packages/tpetra/core/src/Tpetra_Details_computeOffsets.hpp
@@ -189,12 +189,17 @@ public:
        const CountType count)
   {
     const SizeType numOffsets (offsets.extent (0));
+    if(numOffsets == SizeType(0))
+    {
+      // Special case that is possible with zero rows
+      return 0;
+    }
     using range_type = Kokkos::RangePolicy<ExecutionSpace, SizeType>;
     range_type range (execSpace, 0, numOffsets);
     using functor_type =
       ComputeOffsetsFromConstantCount<OffsetType, CountType, SizeType>;
     functor_type functor (offsets, count);
-    const OffsetType total = numOffsets*count;
+    const OffsetType total = (numOffsets - 1) * count;
     const char funcName[] =
       "Tpetra::Details::computeOffsetsFromConstantCount";
     Kokkos::parallel_for (funcName, range, functor);
