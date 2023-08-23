50b78e92b690ca269541cada89883c2faafd6c90
kliegeois <kimliegeois@ymail.com>
2023-08-23 10:34:07 -0600

    Check if the estimation of the number of files generated by binder is sufficiently large; if not, throw an error

---
+++ packages/PyTrilinos2/src/checkNumberFiles.cmake
@@ -0,0 +1,16 @@
+# Designed to be run as a script with "cmake -P"
+#
+# Set variable NUMBER_FILE when running this.
+
+if(NOT NUMBER_FILE)
+    message(FATAL_ERROR "NUMBER_FILE must be specified")
+endif()
+
+if(EXISTS ${CMAKE_CURRENT_BINARY_DIR}/../binder/PyTrilinos2_${NUMBER_FILE}.cpp)
+    MATH(EXPR INDEX "${NUMBER_FILE}+1")
+    while (EXISTS ${CMAKE_CURRENT_BINARY_DIR}/../binder/PyTrilinos2_${INDEX}.cpp)
+        MATH(EXPR INDEX "${INDEX}+1")
+    endwhile()
+    MATH(EXPR INDEX "${INDEX}-1")
+    message(FATAL_ERROR "File PyTrilinos2_${NUMBER_FILE}.cpp exists; please rerun the configuration with PyTrilinos2_BINDER_NUM_FILES at least equal to ${INDEX}.")
+endif()
