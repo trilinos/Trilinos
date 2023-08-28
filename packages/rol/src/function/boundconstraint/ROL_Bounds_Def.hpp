d86ba507a3a54e39747085a5e2a6ff9899db44de
Drew Kouri <dpkouri@sandia.gov>
2023-08-28 08:29:14 -0600

    Changed variables names to avoid "hiding".

--- packages/rol/src/function/boundconstraint/ROL_Bounds_Def.hpp
+++ packages/rol/src/function/boundconstraint/ROL_Bounds_Def.hpp
@@ -117,14 +117,14 @@ void Bounds<Real>::projectInterior( Vector<Real> &x ) {
     public:
       LowerFeasible(const Real eps, const Real diff)
         : eps_(eps), diff_(diff) {}
-      Real apply( const Real &x, const Real &y ) const {
+      Real apply( const Real &xc, const Real &yc ) const {
         const Real tol = static_cast<Real>(100)*ROL_EPSILON<Real>();
         const Real one(1);
-        Real val = ((y <-tol) ? y*(one-eps_)
-                 : ((y > tol) ? y*(one+eps_)
-                 : y+eps_));
-        val = std::min(y+eps_*diff_, val);
-        return x < val ? val : x;
+        Real val = ((yc <-tol) ? yc*(one-eps_)
+                 : ((yc > tol) ? yc*(one+eps_)
+                 : yc+eps_));
+        val = std::min(yc+eps_*diff_, val);
+        return xc < val ? val : xc;
       }
     };
     x.applyBinary(LowerFeasible(feasTol_,min_diff_), *lower_);
@@ -138,14 +138,14 @@ void Bounds<Real>::projectInterior( Vector<Real> &x ) {
     public:
       UpperFeasible(const Real eps, const Real diff)
         : eps_(eps), diff_(diff) {}
-      Real apply( const Real &x, const Real &y ) const {
+      Real apply( const Real &xc, const Real &yc ) const {
         const Real tol = static_cast<Real>(100)*ROL_EPSILON<Real>();
         const Real one(1);
-        Real val = ((y <-tol) ? y*(one+eps_)
-                 : ((y > tol) ? y*(one-eps_)
-                 : y-eps_));
-        val = std::max(y-eps_*diff_, val);
-        return x > val ? val : x;
+        Real val = ((yc <-tol) ? yc*(one+eps_)
+                 : ((yc > tol) ? yc*(one-eps_)
+                 : yc-eps_));
+        val = std::max(yc-eps_*diff_, val);
+        return xc > val ? val : xc;
       }
     };
     x.applyBinary(UpperFeasible(feasTol_,min_diff_), *upper_);
