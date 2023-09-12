28d33c62cf306bd1be684aa2f0c05f761fd3d892
Greg Sjaardema <gsjaardema@gmail.com>
2023-09-12 12:38:22 -0600

    SLICE: Bring up-to-date with seacas refactoring/fixes

---
+++ packages/seacas/applications/slice/SL_Decompose.h
@@ -0,0 +1,23 @@
+// Copyright(C) 1999-2023 National Technology & Engineering Solutions
+// of Sandia, LLC (NTESS).  Under the terms of Contract DE-NA0003525 with
+// NTESS, the U.S. Government retains certain rights in this software.
+//
+// See packages/seacas/LICENSE for details
+
+#include <Ioss_ChainGenerator.h>
+#include <Ioss_Region.h>
+#include <SL_SystemInterface.h>
+#include <vector>
+
+#pragma once
+template <typename INT>
+void decompose_elements(const Ioss::Region &region, SystemInterface &interFace,
+                        std::vector<int> &elem_to_proc, IOSS_MAYBE_UNUSED INT dummy);
+
+template <typename INT>
+void line_decomp_modify(const Ioss::chain_t<INT> &element_chains, std::vector<int> &elem_to_proc,
+                        int proc_count);
+
+template <typename INT>
+void output_decomposition_statistics(const std::vector<INT> &elem_to_proc, int proc_count,
+                                     size_t number_elements);
