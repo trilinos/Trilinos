78ff5fdc966a0e931db38ed49270be4209fa9e70
Samuel Browne <sebrown@sandia.gov>
2023-09-13 13:52:55 -0600

    Fix git log command
    Switched source and target....

--- commonTools/test/utilities/check-lfs-oids.sh
+++ commonTools/test/utilities/check-lfs-oids.sh
@@ -7,7 +7,7 @@ set -o pipefail
 target_branch=${1}
 source_branch=${2}
 
-for commit in $(git log --format=%H ${target_branch} --not ${source_branch})
+for commit in $(git log --format=%H ${source_branch} --not ${target_branch})
 do
     echo "Processing commit ${commit}"
     git diff -U0 --ignore-all-space ${commit}~1 ${commit} | grep "^\+oid sha256" \
