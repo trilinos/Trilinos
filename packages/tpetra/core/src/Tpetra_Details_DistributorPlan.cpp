5e079214037deb05902470e938b31fd884d85cb1
Carl Pearson <cwpears@sandia.gov>
2023-10-03 11:53:11 -0600

    Tpetra_Details_DistributorPlan.cpp: uninitialized, lifetime, consistent strings
    * typos
    * initialize error to zero, common
    * strings
    * Tpetra_Details_DistributorPlan.cpp: Fix MPIX_Comm lifetime, don't keep a pointer to stack variable
    * remove redundant initializeMpiAdvance()

--- packages/tpetra/core/src/Tpetra_Details_DistributorPlan.cpp
+++ packages/tpetra/core/src/Tpetra_Details_DistributorPlan.cpp
@@ -58,12 +58,12 @@ DistributorSendTypeEnumToString (EDistributorSendType sendType)
   else if (sendType == DISTRIBUTOR_ALLTOALL) {
     return "Alltoall";
   }
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
   else if (sendType == DISTRIBUTOR_MPIADVANCE_ALLTOALL) {
-    return "MpiAdvance Alltoall";
+    return "MpiAdvanceAlltoall";
   }
   else if (sendType == DISTRIBUTOR_MPIADVANCE_NBRALLTOALLV) {
-    return "MpiAdvance Neighborhood Alltoallv";
+    return "MpiAdvanceNbralltoallv";
   }
 #endif
   else {
@@ -93,10 +93,9 @@ DistributorHowInitializedEnumToString (EDistributorHowInitialized how)
   }
 }
 
-// TODO null out the mpi advance communicator
 DistributorPlan::DistributorPlan(Teuchos::RCP<const Teuchos::Comm<int>> comm)
   : comm_(comm),
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
     mpixComm_(Teuchos::null),
 #endif
     howInitialized_(DISTRIBUTOR_NOT_INITIALIZED),
@@ -109,10 +108,9 @@ DistributorPlan::DistributorPlan(Teuchos::RCP<const Teuchos::Comm<int>> comm)
     totalReceiveLength_(0)
 { }
 
-// TODO: add mpi advance comm
 DistributorPlan::DistributorPlan(const DistributorPlan& otherPlan)
   : comm_(otherPlan.comm_),
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
     mpixComm_(otherPlan.mpixComm_),
 #endif
     howInitialized_(DISTRIBUTOR_INITIALIZED_BY_COPY),
@@ -132,13 +130,6 @@ DistributorPlan::DistributorPlan(const DistributorPlan& otherPlan)
     startsFrom_(otherPlan.startsFrom_),
     indicesFrom_(otherPlan.indicesFrom_)
 { }
-
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
-DistributorPlan::~DistributorPlan() {
-  int err = MPIX_Comm_free(*(mpixComm_.get()));
-  TEUCHOS_ASSERT(err == 0);
-}
-#endif
 
 size_t DistributorPlan::createFromSends(const Teuchos::ArrayView<const int>& exportProcIDs) {
   using Teuchos::outArg;
@@ -416,8 +407,8 @@ size_t DistributorPlan::createFromSends(const Teuchos::ArrayView<const int>& exp
   // Invert map to see what msgs are received and what length
   computeReceives();
 
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
-  initializeMpiAvance();
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
+  initializeMpiAdvance();
 #endif
 
   // createFromRecvs() calls createFromSends(), but will set
@@ -430,13 +421,7 @@ size_t DistributorPlan::createFromSends(const Teuchos::ArrayView<const int>& exp
 void DistributorPlan::createFromRecvs(const Teuchos::ArrayView<const int>& remoteProcIDs)
 {
   createFromSends(remoteProcIDs);
-
   *this = *getReversePlan();
-
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
-  initializeMpiAvance();
-#endif
-
   howInitialized_ = Details::DISTRIBUTOR_INITIALIZED_BY_CREATE_FROM_RECVS;
 }
 
@@ -450,12 +435,7 @@ void DistributorPlan::createFromSendsAndRecvs(const Teuchos::ArrayView<const int
   // for each processor id. A version of this with lengthsTo and lengthsFrom
   // should be made.
 
-  #if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
-  initializeMpiAvance();
-  #endif
-
   howInitialized_ = Tpetra::Details::DISTRIBUTOR_INITIALIZED_BY_CREATE_FROM_SENDS_N_RECVS;
-
 
   int myProcID = comm_->getRank ();
   int numProcs = comm_->getSize();
@@ -628,8 +608,8 @@ void DistributorPlan::createFromSendsAndRecvs(const Teuchos::ArrayView<const int
   indicesFrom_.clear ();
   numReceives_-=sendMessageToSelf_;
   
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
-  initializeMpiAvance();
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
+  initializeMpiAdvance();
 #endif
 }
 
@@ -678,9 +658,9 @@ void DistributorPlan::createReversePlan() const
   reversePlan_->startsFrom_ = startsTo_;
   reversePlan_->indicesFrom_ = indicesTo_;
 
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
   // is there a smarter way to do this
-  reversePlan_->initializeMpiAvance();
+  reversePlan_->initializeMpiAdvance();
 #endif
 }
 
@@ -942,7 +922,7 @@ Teuchos::Array<std::string> distributorSendTypes()
   sendTypes.push_back ("Isend");
   sendTypes.push_back ("Send");
   sendTypes.push_back ("Alltoall");
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
   sendTypes.push_back ("MpiAdvanceAlltoall");
   sendTypes.push_back ("MpiAdvanceNbralltoallv");
 #endif
@@ -964,7 +944,7 @@ DistributorPlan::getValidParameters() const
   sendTypeEnums.push_back (Details::DISTRIBUTOR_ISEND);
   sendTypeEnums.push_back (Details::DISTRIBUTOR_SEND);
   sendTypeEnums.push_back (Details::DISTRIBUTOR_ALLTOALL);
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
   sendTypeEnums.push_back (Details::DISTRIBUTOR_MPIADVANCE_ALLTOALL);
   sendTypeEnums.push_back (Details::DISTRIBUTOR_MPIADVANCE_NBRALLTOALLV);
 #endif
@@ -979,8 +959,16 @@ DistributorPlan::getValidParameters() const
   return Teuchos::rcp_const_cast<const ParameterList> (plist);
 }
 
-#if defined(HAVE_TPETRA_CORE_MPI_ADVANCE)
-void DistributorPlan::initializeMpiAvance() {
+#if defined(HAVE_TPETRACORE_MPI_ADVANCE)
+
+// Used by Teuchos::RCP to clean up an owned MPIX_Comm*
+struct MpixCommDeallocator {
+  void free(MPIX_Comm **comm) const {
+    MPIX_Comm_free(*comm);
+  }
+};
+
+void DistributorPlan::initializeMpiAdvance() {
 
   // assert the mpix communicator is null. if this is not the case we will figure out why
   TEUCHOS_ASSERT(mpixComm_.is_null());
@@ -988,17 +976,27 @@ void DistributorPlan::initializeMpiAvance() {
   // use the members to initialize the graph for neightborhood mode, or just the MPIX communicator for non-neighborhood mode
   Teuchos::RCP<const Teuchos::MpiComm<int> > mpiComm = Teuchos::rcp_dynamic_cast<const Teuchos::MpiComm<int> >(comm_);
   Teuchos::RCP<const Teuchos::OpaqueWrapper<MPI_Comm> > rawComm = mpiComm->getRawMpiComm();
-  int err;
+  int err = 0;
   if (sendType_ == DISTRIBUTOR_MPIADVANCE_ALLTOALL) {
-    err = MPIX_Comm_init(mpixComm_.get(), (*rawComm)());
+    MPIX_Comm **mpixComm = new(MPIX_Comm*);
+    err = MPIX_Comm_init(mpixComm, (*rawComm)());
+    mpixComm_ = Teuchos::RCP(mpixComm,
+      MpixCommDeallocator(),
+      true /*take ownership*/
+    );
   }
   else if (sendType_ == DISTRIBUTOR_MPIADVANCE_NBRALLTOALLV) {
     int numRecvs = (int)(numReceives_ + (sendMessageToSelf_ ? 1 : 0));
     int *sourceRanks = procsFrom_.data();
-    int *sourceWeights = static_cast<int*>(lengthsFrom_.data()); // assume lengths good for weights
+    
+    // int *sourceWeights = static_cast<int*>(lengthsFrom_.data());// lengthsFrom_ may not be int
+    const int *sourceWeights = MPI_UNWEIGHTED;
     int numSends = (int)(numSendsToOtherProcs_ + (sendMessageToSelf_ ? 1 : 0));
     int *destRanks = procIdsToSendTo_.data();
-    int *destWeights = static_cast<int*>(lengthsTo_.data()); // assume lengths good for weights
+
+    // int *destWeights = static_cast<int*>(lengthsTo_.data()); // lengthsTo_ may not be int
+    const int *destWeights = MPI_UNWEIGHTED; // lengthsTo_ may not be int
+
     err = MPIX_Dist_graph_create_adjacent((*rawComm)(), numRecvs, sourceRanks, sourceWeights, numSends, destRanks, destWeights, MPI_INFO_NULL, false, mpixComm_.get());
   }
 
