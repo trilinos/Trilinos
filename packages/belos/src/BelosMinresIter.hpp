fc83b961b0e4aca17fd4a73812768e60d4952ba6
Heidi Thornquist <hkthorn@sandia.gov>
2023-10-23 09:39:38 -0600

    Fixes MINRES solver for Kokkos.  Now the MINRES test passes on ascicgpu030.
    Signed-off-by: Heidi Thornquist <hkthorn@sandia.gov>

--- packages/belos/src/BelosMinresIter.hpp
+++ packages/belos/src/BelosMinresIter.hpp
@@ -277,7 +277,8 @@ class MinresIter : virtual public MinresIteration<ScalarType,MV,OP,DM> {
   Teuchos::RCP< MV > W2_;
 
   /// Coefficient in the MINRES iteration
-  Teuchos::RCP<DM> beta1_;
+  ScalarType beta1_;
+  Teuchos::RCP<DM> tmpDM;
 
 };
 
@@ -394,21 +395,23 @@ class MinresIter : virtual public MinresIteration<ScalarType,MV,OP,DM> {
 
     // beta1_ = b'*y;
 
-    beta1_ = DMT::Create(1,1);
-    MVT::MvTransMv( one, *newstate.Y, *Y_, *beta1_ );
+    tmpDM = DMT::Create(1,1);
+    MVT::MvTransMv( one, *newstate.Y, *Y_, *tmpDM);
+    DMT::SyncDeviceToHost(*tmpDM);
+    beta1_ = DMT::ValueConst(*tmpDM,0,0);
 
-    TEUCHOS_TEST_FOR_EXCEPTION( SCT::real(DMT::ValueConst(*beta1_,0,0)) < m_zero,
+    TEUCHOS_TEST_FOR_EXCEPTION( SCT::real(beta1_) < m_zero,
                         std::invalid_argument,
                         "The preconditioner is not positive definite." );
 
-    if( SCT::magnitude(DMT::ValueConst(*beta1_,0,0)) == m_zero )
+    if( SCT::magnitude(beta1_) == m_zero )
     {
         // X = 0
         Teuchos::RCP<MV> cur_soln_vec = lp_->getCurrLHSVec();
         MVT::MvInit( *cur_soln_vec );
     }
 
-    DMT::Value(*beta1_,0,0) = SCT::squareroot( DMT::ValueConst(*beta1_,0,0) );
+    beta1_ = SCT::squareroot( beta1_ );
 
     // The solver is initialized
     initialized_ = true;
@@ -433,10 +436,8 @@ class MinresIter : virtual public MinresIteration<ScalarType,MV,OP,DM> {
     const MagnitudeType m_zero = SMT::zero();
 
     // Allocate memory for scalars.
-    Teuchos::RCP<DM> alpha = DMT::Create(1,1); 
-    Teuchos::RCP<DM> beta = DMT::Create(1,1); 
-    DMT::Assign(*beta,*beta1_); 
-    phibar_ = Teuchos::ScalarTraits<ScalarType>::magnitude( DMT::ValueConst(*beta1_,0,0) );
+    ScalarType alpha, beta = beta1_;
+    phibar_ = Teuchos::ScalarTraits<ScalarType>::magnitude( beta1_ );
 
     // Initialize a few variables.
     ScalarType oldBeta = zero;
@@ -474,19 +475,21 @@ class MinresIter : virtual public MinresIteration<ScalarType,MV,OP,DM> {
 
       // Normalize previous vector.
       //   v = y / beta(0,0);
-      MVT::MvAddMv (one / DMT::ValueConst(*beta,0,0), *Y_, zero, *Y_, *V);
+      MVT::MvAddMv (one / beta, *Y_, zero, *Y_, *V);
 
       // Apply operator.
       lp_->applyOp (*V, *Y_);
 
       if (iter_ > 1)
-        MVT::MvAddMv (one, *Y_, -DMT::ValueConst(*beta,0,0)/oldBeta, *R1_, *Y_);
+        MVT::MvAddMv (one, *Y_, -beta/oldBeta, *R1_, *Y_);
 
       // alpha := dot(V, Y_)
-      MVT::MvTransMv (one, *V, *Y_, *alpha);
+      MVT::MvTransMv (one, *V, *Y_, *tmpDM);
+      DMT::SyncDeviceToHost(*tmpDM);
+      alpha = DMT::ValueConst(*tmpDM,0,0);
 
       // y := y - alpha/beta r2
-      MVT::MvAddMv (one, *Y_, -DMT::ValueConst(*alpha,0,0)/DMT::ValueConst(*beta,0,0), *R2_, *Y_);
+      MVT::MvAddMv (one, *Y_, -alpha/beta, *R2_, *Y_);
 
       // r1 = r2;
       // r2 = y;
@@ -511,8 +514,10 @@ class MinresIter : virtual public MinresIteration<ScalarType,MV,OP,DM> {
       }
 
       // Get new beta.
-      oldBeta = DMT::ValueConst(*beta,0,0);
-      MVT::MvTransMv( one, *R2_, *Y_, *beta );
+      oldBeta = beta;
+      MVT::MvTransMv( one, *R2_, *Y_, *tmpDM);
+      DMT::SyncDeviceToHost(*tmpDM);
+      beta = DMT::ValueConst(*tmpDM,0,0);
 
       // Intercept beta <= 0.
       //
@@ -524,12 +529,12 @@ class MinresIter : virtual public MinresIteration<ScalarType,MV,OP,DM> {
       // algebra library to compute a posteriori rounding error bounds
       // for the inner product, and then changing
       // Belos::MultiVecTraits to make this information available).
-      TEUCHOS_TEST_FOR_EXCEPTION( SCT::real(DMT::ValueConst(*beta,0,0)) < m_zero,
+      TEUCHOS_TEST_FOR_EXCEPTION( SCT::real(beta) < m_zero,
                           MinresIterateFailure,
                           "Belos::MinresIter::iterate(): Encountered negative "
-			  "value " << DMT::ValueConst(*beta,0,0) << " for r2^H*M*r2 at itera"
+			  "value " << beta << " for r2^H*M*r2 at itera"
 			  "tion " << iter_ << ": MINRES cannot continue." );
-      DMT::Value(*beta,0,0) = SCT::squareroot( DMT::ValueConst(*beta,0,0) );
+      beta = SCT::squareroot( beta );
 
       // Apply previous rotation Q_{k-1} to get
       //
@@ -537,13 +542,13 @@ class MinresIter : virtual public MinresIteration<ScalarType,MV,OP,DM> {
       //    [gbar_k  dbar_{k+1} ]   [-sn cs][alpha_k beta_{k+1}].
       //
       oldeps = epsln;
-      delta  = cs*dbar + sn*DMT::ValueConst(*alpha,0,0);
-      gbar   = sn*dbar - cs*DMT::ValueConst(*alpha,0,0);
-      epsln  =           sn*DMT::ValueConst(*beta,0,0);
-      dbar   =         - cs*DMT::ValueConst(*beta,0,0);
+      delta  = cs*dbar + sn*alpha;
+      gbar   = sn*dbar - cs*alpha;
+      epsln  =           sn*beta;
+      dbar   =         - cs*beta;
 
       // Compute the next plane rotation Q_k.
-      this->symOrtho(gbar, DMT::ValueConst(*beta,0,0), &cs, &sn, &gamma);
+      this->symOrtho(gbar, beta, &cs, &sn, &gamma);
 
       phi    = cs * phibar_; // phi_k
       phibar_ = Teuchos::ScalarTraits<ScalarType>::magnitude( sn * phibar_ ); // phibar_{k+1}
