cab0a96404e51df7cb8a74a8e1511eafa1878a94
Christian Glusa <caglusa@sandia.gov>
2023-11-03 08:06:20 -0600

    Geminga: Attempt to silence chatty MPI

--- cmake/ctest/drivers/geminga/TrilinosCTestDriverCore.geminga.gcc-cuda.cmake
+++ cmake/ctest/drivers/geminga/TrilinosCTestDriverCore.geminga.gcc-cuda.cmake
@@ -148,6 +148,7 @@ MACRO(TRILINOS_SYSTEM_SPECIFIC_CTEST_DRIVER)
         ${EXTRA_SYSTEM_CONFIGURE_OPTIONS}
         "-DTPL_ENABLE_MPI:BOOL=ON"
             "-DMPI_BASE_DIR:PATH=$ENV{OPENMPI_ROOT}"
+            "-DMPI_EXEC_POST_NUMPROCS_FLAGS:STRING=--bind-to\\\;socket\\\;--map-by\\\;socket\\\;-quiet"
        )
 
   ENDIF()
