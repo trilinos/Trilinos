61c50515e590b9e2211300867b2eaf027ba36429
Aurya Javeed <asjavee@sandia.gov>
2023-11-17 15:45:52 -0700

    Add HS41

--- packages/rol/pyrol/test/algorithm/zoo/__init__.py
+++ packages/rol/pyrol/test/algorithm/zoo/__init__.py
@@ -1,5 +1,6 @@
 from .TestProblem import TestProblem
+from .HS41 import HS41
 from .HS42 import HS42
 from .Rosenbrock import Rosenbrock
 
-__all__ = ["HS42", "Rosenbrock"]
+__all__ = ["HS41", "HS42", "Rosenbrock"]
