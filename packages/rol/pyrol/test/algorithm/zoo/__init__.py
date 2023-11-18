79a35f730389aa0b753ece01806279c80564086d
Aurya Javeed <asjavee@sandia.gov>
2023-11-18 00:59:45 -0700

    Implement TypeB tests

--- packages/rol/pyrol/test/algorithm/zoo/__init__.py
+++ packages/rol/pyrol/test/algorithm/zoo/__init__.py
@@ -1,6 +1,7 @@
 from .TestProblem import TestProblem
+from .HS02 import HS02
 from .HS41 import HS41
 from .HS42 import HS42
 from .Rosenbrock import Rosenbrock
 
-__all__ = ["HS41", "HS42", "Rosenbrock"]
+__all__ = ["HS02", "HS41", "HS42", "Rosenbrock"]
