dd4c55a7dd462de079e4f70f6951fec7bfeff3f3
Aurya Javeed <asjavee@sandia.gov>
2023-11-27 01:37:27 -0700

    Hack together a cleaner PyROL import

--- packages/rol/pyrol/python/__init__.py
+++ packages/rol/pyrol/python/__init__.py
@@ -1,27 +1,50 @@
+import importlib
+from types import SimpleNamespace
+
+from . getTypeName import getTypeName, getDefaultScalarType, ROL_classes, ROL_members
+
+
 __version__ = '0.1.0'
 def version():
     return 'PyROL version: ' + __version__
 
 
-import importlib
-from . getTypeName import getTypeName, getDefaultScalarType, ROL_classes, ROL_members
-
 def getWrapper(classname):
     def wrapper(scalarType=getDefaultScalarType()):
         actualClass = getTypeName(classname, scalarType)
         return actualClass
-
     return wrapper
 
+
 defaultScalarType = getDefaultScalarType()
+
+supported_objects = {"Bounds", "Constraint", "LinearOperator",
+                     "LinearConstraint", "Objective", "Problem", "Solver", 
+                     "Vector", "getCout", "getParametersFromXmlFile"}
+
+unsupported = SimpleNamespace()
+
 for classnameLong in ROL_members:
-    class_obj, isClass = ROL_members[classnameLong]
+    class_obj, _ = ROL_members[classnameLong]
     pos = classnameLong.find('_'+defaultScalarType+'_t')
     if pos <= 0:
-        locals().update({classnameLong: class_obj})
+        if classnameLong in supported_objects:
+            locals().update({classnameLong: class_obj})
+        else:
+            setattr(unsupported, classnameLong, class_obj)
         continue
+
     classname = classnameLong[:pos]
 
+    if classname in supported_objects:
+        locals().update({classname: getTypeName(classname, defaultScalarType)})
+        locals().update({classname+'_forScalar': getWrapper(classname)})
+    else:
+        setattr(unsupported, classname, getTypeName(classname, defaultScalarType))
+        setattr(unsupported, classname + '_forScalar', getWrapper(classname))
 
-    locals().update({classname: getTypeName(classname, defaultScalarType)})
-    locals().update({classname+'_forScalar': getWrapper(classname)})
+locals().update({'unsupported': unsupported})
+
+del importlib, SimpleNamespace, getTypeName, getDefaultScalarType, ROL_classes, ROL_members
+del class_obj, classname, classnameLong, getWrapper, pos
+
