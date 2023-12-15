bc36ff2c25f1874e52404a813036d9dfdf018a6b
Heidi Thornquist <hkthorn@sandia.gov>
2023-12-15 13:57:35 -0700

    Finished converting GMRES-variants to dense matrix traits
    The pseudo-block and polynomial-preconditioned GMRES variants
    are integrated with dense matrix traits.  All GMRES variants have
    been cleaned up to remove default templates where appropriate.

--- packages/belos/src/BelosBlockFGmresIter.hpp
+++ packages/belos/src/BelosBlockFGmresIter.hpp
@@ -57,7 +57,6 @@
 #include "BelosOperatorTraits.hpp"
 #include "BelosMultiVecTraits.hpp"
 #include "BelosDenseMatTraits.hpp"
-#include "BelosTeuchosDenseAdapter.hpp"
 
 #include "Teuchos_BLAS.hpp"
 #include "Teuchos_ScalarTraits.hpp"
@@ -81,7 +80,7 @@
 
 namespace Belos {
 
-template<class ScalarType, class MV, class OP, class DM = Teuchos::SerialDenseMatrix<int, ScalarType>>
+template<class ScalarType, class MV, class OP, class DM>
 class BlockFGmresIter : virtual public GmresIteration<ScalarType,MV,OP,DM> {
 
   public:
