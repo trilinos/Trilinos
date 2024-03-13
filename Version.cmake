0f23508b18caed9d9289a28dd3018a501b5b767a
Joe Frye <jfrye@sandia.gov>
2024-03-13 10:01:15 -0600

    Update to version 15.1.1

--- Version.cmake
+++ Version.cmake
@@ -59,10 +59,10 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 15.1.0)
+SET(Trilinos_VERSION 15.1.1)
 SET(Trilinos_MAJOR_VERSION 15)
-SET(Trilinos_MAJOR_MINOR_VERSION 150100)
-SET(Trilinos_VERSION_STRING "15.1.0")
+SET(Trilinos_MAJOR_MINOR_VERSION 150101)
+SET(Trilinos_VERSION_STRING "15.1.1")
 SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
