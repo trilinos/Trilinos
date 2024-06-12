11266971a80971367e0e14950674034f675f9c45
Kim Liegeois <kimliegeois@ymail.com>
2024-06-12 12:53:26 -0600

    Use std::stable_sort instead of insertion sort

--- packages/tpetra/core/src/Tpetra_Util.hpp
+++ packages/tpetra/core/src/Tpetra_Util.hpp
@@ -57,6 +57,7 @@
 #include "Teuchos_OrdinalTraits.hpp"
 #include "Teuchos_TypeNameTraits.hpp"
 #include "Teuchos_Utils.hpp"
+#include <numeric>
 #include <algorithm>
 #include <iterator>
 #include <memory>
@@ -476,75 +477,89 @@ namespace Tpetra {
    }
 
   /**
-   * \brief Sort the first array using insertion sort, and apply the resulting
+   * \brief Compute the permutation of the ordering that 
+   * sorts the array using a stable sort.
+   */
+  template<typename IT1>
+  std::vector<typename std::iterator_traits<IT1>::difference_type> sort_indexes(IT1 first, IT1 last) {
+
+    typedef typename std::iterator_traits<IT1>::difference_type DT;
+
+    DT length = last - first;
+    std::vector<DT> idx(length);
+    std::iota(idx.begin(), idx.end(), 0);
+
+    std::stable_sort(idx.begin(), idx.end(),
+        [&first](size_t i1, size_t i2) {return first[i1] < first[i2];});
+
+    return idx;
+  }
+
+  /**
+   * \brief Apply a permutation of the ordering of an array in place.
+   */
+  template<typename IT1, typename IT2>
+  void
+  apply_permutation(
+      IT1 first,
+      IT1 last,
+      IT2 indices)
+  {
+    typedef typename std::iterator_traits<IT1>::difference_type DT;
+    typedef typename std::iterator_traits<IT1>::value_type T;
+
+    DT n = last - first;
+
+    Kokkos::View<T*, Kokkos::HostSpace> tmp(Kokkos::view_alloc(Kokkos::WithoutInitializing, "tmp"), n);
+
+    Kokkos::parallel_for("apply_permutation_1",
+                        Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, n),
+                        KOKKOS_LAMBDA(const int j) {
+                            tmp(j) = first[indices[j]];
+    });
+    Kokkos::parallel_for("apply_permutation_2",
+                        Kokkos::RangePolicy<Kokkos::DefaultHostExecutionSpace>(0, n),
+                        KOKKOS_LAMBDA(const int j) {
+                            first[j] = tmp(j);
+    });
+  }
+
+  /**
+   * \brief Sort the first array using std sort, and apply the resulting
    * permutation to the second array.
    *
    * All of the input argument iterators are random access iterators.
    */
   template<class IT1, class IT2>
-  void in_sort2(const IT1& first1,
+  void std_sort2(const IT1& first1,
     const IT1& last1,
     const IT2& first2,
     const IT2& last2)
    {
-        typedef typename std::iterator_traits<IT1>::difference_type DT;
-        DT n = last1 - first1;
-        DT i = 0;
-        
-        while (i < n)
-        {
-            auto x1 = first1[i];
-            auto x2 = first2[i];
-            DT j = i;
-            while (j >= 0 && first1[j-1] > x1)
-            {
-                first1[j] = first1[j-1];
-                first2[j] = first2[j-1];
-                --j;
-            }
-            first1[j] = x1;
-            first2[j] = x2;
-            ++i;
-        }
+    auto indices = sort_indexes(first1, last1);
+    apply_permutation(first1, last1, indices);
+    apply_permutation(first2, last2, indices);
   }
 
   /**
-   * \brief Sort the first array using insertion sort, and apply the resulting
+   * \brief Sort the first array using std sort, and apply the resulting
    * permutation to the second and third arrays.
    *
    * All of the input argument iterators are random access iterators.
    */
   template<class IT1, class IT2, class IT3>
-  void in_sort3(
+  void std_sort3(
     const IT1& first1,
     const IT1& last1,
     const IT2& first2,
-    const IT2& /* last2 */,
+    const IT2& last2,
     const IT3& first3,
-    const IT3& /* last3 */)
+    const IT3& last3)
    {
-        typedef typename std::iterator_traits<IT1>::difference_type DT;
-        DT n = last1 - first1;
-        DT i = 0;
-        
-        while (i < n)
-        {
-            auto x1 = first1[i];
-            auto x2 = first2[i];
-            auto x3 = first3[i];
-            DT j = i;
-            while (j >= 0 && first1[j-1] > x1)
-            {
-                first1[j] = first1[j-1];
-                first2[j] = first2[j-1];
-                first3[j] = first3[j-1];
-                --j;
-            }
-            first1[j] = x1;
-            first2[j] = x2;
-            first3[j] = x3;
-            ++i;
-        }
+    auto indices = sort_indexes(first1, last1);
+    apply_permutation(first1, last1, indices);
+    apply_permutation(first2, last2, indices);
+    apply_permutation(first3, last3, indices);
    }
 
   } //end namespace SortDetails
@@ -577,7 +592,7 @@ namespace Tpetra {
     if(SortDetails::isAlreadySorted(first1, last1)){
       return;
     }
-    SortDetails::in_sort2(first1, last1, first2, first2+(last1-first1));
+    SortDetails::std_sort2(first1, last1, first2, first2+(last1-first1));
 #ifdef HAVE_TPETRA_DEBUG
     if(!SortDetails::isAlreadySorted(first1, last1)){
       std::cout << "Trouble: sort() did not sort !!" << std::endl;
@@ -678,7 +693,7 @@ namespace Tpetra {
     if(SortDetails::isAlreadySorted(first1, last1)){
       return;
     }
-    SortDetails::in_sort3(first1, last1, first2, first2+(last1-first1), first3,
+    SortDetails::std_sort3(first1, last1, first2, first2+(last1-first1), first3,
                     first3+(last1-first1));
 
 #ifdef HAVE_TPETRA_DEBUG
