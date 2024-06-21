f7b5bc3a18f6a7f84c0df42c0d56faab2c893fc2
Curtis C. Ober <ccober@sandia.gov>
2024-06-21 16:43:08 -0600

    TriBITS: Update License and Copyright
    Update the License and Copyright files to
     * Reflect NTESS and new contract
     * Utilize SPDX identifiers within files
     * Make copyright and license files consistent across
       Trilinos packages (i.e., LICENSE and COPYRIGHT)
    
    Signed-off-by: Curtis C. Ober <ccober@sandia.gov>

--- cmake/tribits/win_interface/include/strings.h
+++ cmake/tribits/win_interface/include/strings.h
@@ -1,5 +1,14 @@
+// @HEADER
+// *****************************************************************************
+//            TriBITS: Tribal Build, Integrate, and Test System
+//
+// Copyright 2013-2016 NTESS and the TriBITS contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+// @HEADER
+
 // On windows stricmp and strnicmp
 // are strcasecmp and strncasecmp, and are
 // include in other header files
 #define strcasecmp stricmp
-#define strncasecmp strnicmp 
+#define strncasecmp strnicmp
