174449871205d8595588dbfb37ce3ae3dae71642
Christian Glusa <caglusa@sandia.gov>
2024-06-28 14:54:56 -0600

    MueLu: Clean up includes

--- packages/muelu/src/Interface/MueLu_ParameterListInterpreter_def.hpp
+++ packages/muelu/src/Interface/MueLu_ParameterListInterpreter_def.hpp
@@ -61,6 +61,7 @@
 
 #include "MueLu_AggregationExportFactory.hpp"
 #include "MueLu_AggregateQualityEstimateFactory.hpp"
+#include "MueLu_AmalgamationFactory.hpp"
 #include "MueLu_BrickAggregationFactory.hpp"
 #include "MueLu_ClassicalMapFactory.hpp"
 #include "MueLu_ClassicalPFactory.hpp"
@@ -89,6 +90,7 @@
 #include "MueLu_RebalanceAcFactory.hpp"
 #include "MueLu_RebalanceTransferFactory.hpp"
 #include "MueLu_RepartitionFactory.hpp"
+#include "MueLu_RepartitionHeuristicFactory.hpp"
 #include "MueLu_ReitzingerPFactory.hpp"
 #include "MueLu_SaPFactory.hpp"
 #include "MueLu_ScaledNullspaceFactory.hpp"
