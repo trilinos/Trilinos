a6ca30bd925c0168732c065381aa87e82fe6db51
Curtis C. Ober <ccober@sandia.gov>
2024-07-12 11:20:06 -0600

    Pamgen: Update License and Copyright
    Update the License and Copyright files to
     * Reflect NTESS and new contract
     * Utilize SPDX identifiers within files
     * Make copyright and license files consistent across Trilinos packages (i.e., LICENSE and COPYRIGHT)
    
    Signed-off-by: Curtis C. Ober <ccober@sandia.gov>

--- packages/pamgen/example/getopts.h
+++ packages/pamgen/example/getopts.h
@@ -1,3 +1,12 @@
+// @HEADER
+// *****************************************************************************
+//                     Pamgen Package
+//
+// Copyright 2004 NTESS and the Pamgen contributors.
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// *****************************************************************************
+// @HEADER
+
 /*
  * Copyright (c) 2001 to 2004 Steve Mertz <steve@dragon-ware.com>
  * All rights reserved.
--- packages/pamgen/parser/token_stream.C
+++ packages/pamgen/parser/token_stream.C
@@ -1,4 +1,11 @@
-// $Id$
+// @HEADER
+// *****************************************************************************
+//                     Pamgen Package
+//
+// Copyright 2004 NTESS and the Pamgen contributors.
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// *****************************************************************************
+// @HEADER
 
 #include "token_stream.h"
 #include "InputBlock.h"
