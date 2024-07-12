ec74a82de1f9105f1bc23574c5b690bb3c215b6e
Curtis Ober <ccober@sandia.gov>
2024-07-12 16:52:36 -0600

    Update Phalanx_any.hpp
    Restoring original file as it is a booth file.
    
    Signed-off-by: Curtis C. Ober <ccober@sandia.gov>

--- packages/phalanx/src/Phalanx_any.hpp
+++ packages/phalanx/src/Phalanx_any.hpp
@@ -1,12 +1,3 @@
-// @HEADER
-// *****************************************************************************
-//        Phalanx: A Partial Differential Equation Field Evaluation 
-//       Kernel for Flexible Management of Complex Dependency Chains
-//
-// Copyright 2008 NTESS and the Phalanx contributors.
-// SPDX-License-Identifier: BSD-3-Clause
-// *****************************************************************************
-// @HEADER
 
 // *******************************************************************
 // This file is a modified copy of the Boost any object. It was moved
