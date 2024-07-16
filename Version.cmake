a51ed9584c109daeef2ac498a8f7ab7e081cda44
Justin M. LaPre <jmlapre@sandia.gov>
2024-07-16 15:42:38 +0000

    update for Trilinos 16.0.0

--- Version.cmake
+++ Version.cmake
@@ -4,11 +4,11 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 15.2.0)
-SET(Trilinos_MAJOR_VERSION 15)
-SET(Trilinos_MAJOR_MINOR_VERSION 150200)
-SET(Trilinos_VERSION_STRING "15.2.0-dev")
-SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
+SET(Trilinos_VERSION 16.0.0)
+SET(Trilinos_MAJOR_VERSION 16)
+SET(Trilinos_MAJOR_MINOR_VERSION 160000)
+SET(Trilinos_VERSION_STRING "16.0.0")
+SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
 SET(Trilinos_REPOSITORY_BRANCH "develop" CACHE INTERNAL "")
