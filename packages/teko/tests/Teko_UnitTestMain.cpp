9f30aa6b739446e436434a5ce181f9940f3540ff
Curtis C. Ober <ccober@sandia.gov>
2024-07-18 08:48:51 -0600

    Teko: Update License and Copyright
    Update the License and Copyright files to
    
     * Reflect NTESS and new contract
     * Utilize SPDX identifiers within files
     * Make copyright and license files consistent across Trilinos packages (i.e., LICENSE and COPYRIGHT)
    
    Signed-off-by: Curtis C. Ober <ccober@sandia.gov>

--- packages/teko/tests/Teko_UnitTestMain.cpp
+++ packages/teko/tests/Teko_UnitTestMain.cpp
@@ -1,3 +1,12 @@
+// @HEADER
+// *****************************************************************************
+//      Teko: A package for block and physics based preconditioning
+//
+// Copyright 2010 NTESS and the Teko contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+// @HEADER
+
 #include "Teuchos_UnitTestRepository.hpp"
 #include "Teuchos_GlobalMPISession.hpp"
 #include "Kokkos_Core.hpp"
