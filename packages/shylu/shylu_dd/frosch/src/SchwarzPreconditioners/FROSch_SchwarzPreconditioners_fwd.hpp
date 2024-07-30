4e57e3626ae9a9fda95c3ac51df0fa933d350695
Sebastian Kinnewig <Sebastian@Kinnewig.org>
2024-07-30 15:40:51 +0200

    Add the GeometricOneLevelPreconditioner.
    Signed-off-by: Sebastian Kinnewig <sebastian@kinnewig.org>

--- packages/shylu/shylu_dd/frosch/src/SchwarzPreconditioners/FROSch_SchwarzPreconditioners_fwd.hpp
+++ packages/shylu/shylu_dd/frosch/src/SchwarzPreconditioners/FROSch_SchwarzPreconditioners_fwd.hpp
@@ -28,6 +28,12 @@ namespace FROSch {
               class LO,
               class GO,
               class NO>
+    class GeometricOneLevelPreconditioner;
+
+    template <class SC,
+              class LO,
+              class GO,
+              class NO>
     class OneLevelPreconditioner;
 
     template <class SC,
