4c1b7a5a6b9720894d0de802bef1d3e791191e16
Greg Sjaardema <gdsjaar@sandia.gov>
2024-08-23 09:15:37 -0600

    IOSS: Remove fmt includes in external headers

--- packages/seacas/libraries/ioss/src/Ioss_StructuredBlock.h
+++ packages/seacas/libraries/ioss/src/Ioss_StructuredBlock.h
@@ -12,7 +12,6 @@
 #include "Ioss_NodeBlock.h"
 #include "Ioss_Property.h"
 #include "Ioss_ZoneConnectivity.h"
-#include <fmt/ostream.h>
 #include <array>
 #include <cassert>
 #include <iosfwd>
@@ -350,10 +349,3 @@ namespace Ioss {
   };
 } // namespace Ioss
 
-#if FMT_VERSION >= 90000
-namespace fmt {
-  template <> struct formatter<Ioss::BoundaryCondition> : ostream_formatter
-  {
-  };
-} // namespace fmt
-#endif
