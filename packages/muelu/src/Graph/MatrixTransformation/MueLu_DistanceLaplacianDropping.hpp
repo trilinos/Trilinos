2447ac8280dd9163f4c775b27e47b90080634d73
Christian Glusa <caglusa@sandia.gov>
2024-10-04 09:51:00 -0600

    MueLu: Add material dropping to distance Laplacian
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>
    Signed-off-by: Greg Sjaardema <gdsjaar@sandia.gov>

--- packages/muelu/src/Graph/MatrixTransformation/MueLu_DistanceLaplacianDropping.hpp
+++ packages/muelu/src/Graph/MatrixTransformation/MueLu_DistanceLaplacianDropping.hpp
@@ -10,6 +10,11 @@
 #ifndef MUELU_DISTANCELAPLACIANDROPPING_HPP
 #define MUELU_DISTANCELAPLACIANDROPPING_HPP
 
+#include "Kokkos_Macros.hpp"
+#include "KokkosBatched_LU_Decl.hpp"
+#include "KokkosBatched_LU_Serial_Impl.hpp"
+#include "KokkosBatched_Trsv_Decl.hpp"
+#include "KokkosBatched_Trsv_Serial_Impl.hpp"
 #include "MueLu_DroppingCommon.hpp"
 #include "Kokkos_Core.hpp"
 #include "Kokkos_ArithTraits.hpp"
@@ -25,14 +30,16 @@ namespace MueLu::DistanceLaplacian {
 @brief Computes the unscaled distance Laplacian.
 */
 template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
-class DistanceFunctor {
+class UnweightedDistanceFunctor {
  private:
   using matrix_type        = Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>;
   using local_matrix_type  = typename matrix_type::local_matrix_type;
   using scalar_type        = typename local_matrix_type::value_type;
   using local_ordinal_type = LocalOrdinal;
   using ATS                = Kokkos::ArithTraits<scalar_type>;
-  using magnitudeType      = typename ATS::magnitudeType;
+  using impl_scalar_type   = typename ATS::val_type;
+  using implATS            = Kokkos::ArithTraits<impl_scalar_type>;
+  using magnitudeType      = typename implATS::magnitudeType;
   using magATS             = Kokkos::ArithTraits<magnitudeType>;
   using coords_type        = Xpetra::MultiVector<magnitudeType, LocalOrdinal, GlobalOrdinal, Node>;
   using local_coords_type  = typename coords_type::dual_view_type_const::t_dev;
@@ -44,7 +51,7 @@ class DistanceFunctor {
   local_coords_type ghostedCoords;
 
  public:
-  DistanceFunctor(matrix_type& A, Teuchos::RCP<coords_type>& coords_) {
+  UnweightedDistanceFunctor(matrix_type& A, Teuchos::RCP<coords_type>& coords_) {
     coordsMV      = coords_;
     auto importer = A.getCrsGraph()->getImporter();
     if (!importer.is_null()) {
@@ -67,6 +74,218 @@ class DistanceFunctor {
       d += s * s;
     }
     return d;
+  }
+};
+
+template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+class ScalarMaterialDistanceFunctor {
+ private:
+  using matrix_type         = Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>;
+  using local_matrix_type   = typename matrix_type::local_matrix_type;
+  using scalar_type         = typename local_matrix_type::value_type;
+  using local_ordinal_type  = LocalOrdinal;
+  using ATS                 = Kokkos::ArithTraits<scalar_type>;
+  using impl_scalar_type    = typename ATS::val_type;
+  using implATS             = Kokkos::ArithTraits<impl_scalar_type>;
+  using magnitudeType       = typename implATS::magnitudeType;
+  using magATS              = Kokkos::ArithTraits<magnitudeType>;
+  using coords_type         = Xpetra::MultiVector<magnitudeType, LocalOrdinal, GlobalOrdinal, Node>;
+  using local_coords_type   = typename coords_type::dual_view_type_const::t_dev;
+  using material_type       = Xpetra::MultiVector<Scalar, LocalOrdinal, GlobalOrdinal, Node>;
+  using local_material_type = typename material_type::dual_view_type_const::t_dev;
+
+  Teuchos::RCP<coords_type> coordsMV;
+  Teuchos::RCP<coords_type> ghostedCoordsMV;
+
+  local_coords_type coords;
+  local_coords_type ghostedCoords;
+
+  Teuchos::RCP<material_type> materialMV;
+  Teuchos::RCP<material_type> ghostedMaterialMV;
+
+  local_material_type material;
+  local_material_type ghostedMaterial;
+
+ public:
+  ScalarMaterialDistanceFunctor(matrix_type& A, Teuchos::RCP<coords_type>& coords_, Teuchos::RCP<material_type>& material_) {
+    coordsMV      = coords_;
+    materialMV    = material_;
+    auto importer = A.getCrsGraph()->getImporter();
+    if (!importer.is_null()) {
+      ghostedCoordsMV = Xpetra::MultiVectorFactory<magnitudeType, LocalOrdinal, GlobalOrdinal, Node>::Build(importer->getTargetMap(), coordsMV->getNumVectors());
+      ghostedCoordsMV->doImport(*coordsMV, *importer, Xpetra::INSERT);
+      coords        = coordsMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+      ghostedCoords = ghostedCoordsMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+
+      ghostedMaterialMV = Xpetra::MultiVectorFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Build(importer->getTargetMap(), materialMV->getNumVectors());
+      ghostedMaterialMV->doImport(*materialMV, *importer, Xpetra::INSERT);
+      material        = materialMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+      ghostedMaterial = ghostedMaterialMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+    } else {
+      coords        = coordsMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+      ghostedCoords = coords;
+
+      material        = materialMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+      ghostedMaterial = material;
+    }
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  magnitudeType distance2(const local_ordinal_type row, const local_ordinal_type col) const {
+    // || x_row - x_col ||_S^2
+    // where
+    // S = 1/material(row) * Identity
+    magnitudeType d     = magATS::zero();
+    magnitudeType d_row = magATS::zero();
+    magnitudeType d_col = magATS::zero();
+    magnitudeType s;
+
+    for (size_t j = 0; j < coords.extent(1); ++j) {
+      s = coords(row, j) - ghostedCoords(col, j);
+      d += s * s;
+    }
+
+    d_row = d / implATS::magnitude(ghostedMaterial(row, 0));
+    d_col = d / implATS::magnitude(ghostedMaterial(col, 0));
+
+    return Kokkos::max(d_row, d_col);
+  }
+};
+
+template <class local_ordinal_type, class material_vector_type, class material_matrix_type>
+class TensorInversion {
+ private:
+  material_vector_type material_vector;
+  material_matrix_type material_matrix;
+
+ public:
+  TensorInversion(material_vector_type& material_vector_, material_matrix_type& material_matrix_)
+    : material_vector(material_vector_)
+    , material_matrix(material_matrix_) {}
+
+  KOKKOS_FORCEINLINE_FUNCTION
+  void operator()(local_ordinal_type i) const {
+    for (size_t j = 0; j < material_matrix.extent(1); ++j) {
+      for (size_t k = 0; k < material_matrix.extent(2); ++k) {
+        material_matrix(i, j, k) = material_vector(i, j * material_matrix.extent(1) + k);
+      }
+    }
+    auto matrix_material = Kokkos::subview(material_matrix, i, Kokkos::ALL(), Kokkos::ALL());
+    KokkosBatched::SerialLU<KokkosBatched::Algo::LU::Unblocked>::invoke(matrix_material);
+  }
+};
+
+template <class Scalar, class LocalOrdinal, class GlobalOrdinal, class Node>
+class TensorMaterialDistanceFunctor {
+ private:
+  using matrix_type        = Xpetra::Matrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>;
+  using local_matrix_type  = typename matrix_type::local_matrix_type;
+  using scalar_type        = typename local_matrix_type::value_type;
+  using local_ordinal_type = LocalOrdinal;
+  using ATS                = Kokkos::ArithTraits<scalar_type>;
+  using impl_scalar_type   = typename ATS::val_type;
+  using implATS            = Kokkos::ArithTraits<impl_scalar_type>;
+  using magnitudeType      = typename implATS::magnitudeType;
+  using magATS             = Kokkos::ArithTraits<magnitudeType>;
+  using coords_type        = Xpetra::MultiVector<magnitudeType, LocalOrdinal, GlobalOrdinal, Node>;
+  using local_coords_type  = typename coords_type::dual_view_type_const::t_dev;
+  using material_type      = Xpetra::MultiVector<Scalar, LocalOrdinal, GlobalOrdinal, Node>;
+  using memory_space       = typename local_matrix_type::memory_space;
+
+  using local_material_type = Kokkos::View<impl_scalar_type***, memory_space>;
+  using local_dist_type     = Kokkos::View<impl_scalar_type**, memory_space>;
+
+  Teuchos::RCP<coords_type> coordsMV;
+  Teuchos::RCP<coords_type> ghostedCoordsMV;
+
+  local_coords_type coords;
+  local_coords_type ghostedCoords;
+
+  local_material_type material;
+
+  local_dist_type lcl_dist;
+
+  const scalar_type one = ATS::one();
+
+ public:
+  TensorMaterialDistanceFunctor(matrix_type& A, Teuchos::RCP<coords_type>& coords_, Teuchos::RCP<material_type>& material_) {
+    coordsMV = coords_;
+
+    auto importer = A.getCrsGraph()->getImporter();
+    if (!importer.is_null()) {
+      ghostedCoordsMV = Xpetra::MultiVectorFactory<magnitudeType, LocalOrdinal, GlobalOrdinal, Node>::Build(importer->getTargetMap(), coordsMV->getNumVectors());
+      ghostedCoordsMV->doImport(*coordsMV, *importer, Xpetra::INSERT);
+      coords        = coordsMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+      ghostedCoords = ghostedCoordsMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+    } else {
+      coords        = coordsMV->getDeviceLocalView(Xpetra::Access::ReadOnly);
+      ghostedCoords = coords;
+    }
+
+    {
+      Teuchos::RCP<material_type> ghostedMaterial;
+      if (!importer.is_null()) {
+        ghostedMaterial = Xpetra::MultiVectorFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Build(importer->getTargetMap(), material_->getNumVectors());
+        ghostedMaterial->doImport(*material_, *importer, Xpetra::INSERT);
+      } else {
+        ghostedMaterial = material_;
+      }
+
+      using execution_space = typename Node::execution_space;
+      using range_type      = Kokkos::RangePolicy<LocalOrdinal, execution_space>;
+
+      local_ordinal_type dim = std::sqrt(material_->getNumVectors());
+      auto lclMaterial       = ghostedMaterial->getDeviceLocalView(Xpetra::Access::ReadOnly);
+      material               = local_material_type("material", lclMaterial.extent(0), dim, dim);
+      lcl_dist               = local_dist_type("material", lclMaterial.extent(0), dim);
+      TensorInversion<local_ordinal_type, typename material_type::dual_view_type::t_dev_const_um, local_material_type> functor(lclMaterial, material);
+      Kokkos::parallel_for("MueLu:TensorMaterialDistanceFunctor::inversion", range_type(0, lclMaterial.extent(0)), functor);
+    }
+  }
+
+  KOKKOS_INLINE_FUNCTION
+  magnitudeType distance2(const local_ordinal_type row, const local_ordinal_type col) const {
+    // || x_row - x_col ||_S^2
+    // where
+    // S = inv(material(col))
+
+    // row material
+    impl_scalar_type d_row = implATS::zero();
+    {
+      auto matrix_row_material = Kokkos::subview(material, row, Kokkos::ALL(), Kokkos::ALL());
+      auto dist                = Kokkos::subview(lcl_dist, row, Kokkos::ALL());
+
+      for (size_t j = 0; j < coords.extent(1); ++j) {
+        dist(j) = coords(row, j) - ghostedCoords(col, j);
+      }
+
+      KokkosBatched::SerialTrsv<KokkosBatched::Uplo::Lower, KokkosBatched::Trans::NoTranspose, KokkosBatched::Diag::Unit, KokkosBatched::Algo::Trsv::Unblocked>::invoke(one, matrix_row_material, dist);
+      KokkosBatched::SerialTrsv<KokkosBatched::Uplo::Upper, KokkosBatched::Trans::NoTranspose, KokkosBatched::Diag::NonUnit, KokkosBatched::Algo::Trsv::Unblocked>::invoke(one, matrix_row_material, dist);
+
+      for (size_t j = 0; j < coords.extent(1); ++j) {
+        d_row += dist(j) * (coords(row, j) - ghostedCoords(col, j));
+      }
+    }
+
+    // column material
+    impl_scalar_type d_col = implATS::zero();
+    {
+      auto matrix_col_material = Kokkos::subview(material, col, Kokkos::ALL(), Kokkos::ALL());
+      auto dist                = Kokkos::subview(lcl_dist, row, Kokkos::ALL());
+
+      for (size_t j = 0; j < coords.extent(1); ++j) {
+        dist(j) = coords(row, j) - ghostedCoords(col, j);
+      }
+
+      KokkosBatched::SerialTrsv<KokkosBatched::Uplo::Lower, KokkosBatched::Trans::NoTranspose, KokkosBatched::Diag::Unit, KokkosBatched::Algo::Trsv::Unblocked>::invoke(one, matrix_col_material, dist);
+      KokkosBatched::SerialTrsv<KokkosBatched::Uplo::Upper, KokkosBatched::Trans::NoTranspose, KokkosBatched::Diag::NonUnit, KokkosBatched::Algo::Trsv::Unblocked>::invoke(one, matrix_col_material, dist);
+
+      for (size_t j = 0; j < coords.extent(1); ++j) {
+        d_col += dist(j) * (coords(row, j) - ghostedCoords(col, j));
+      }
+    }
+
+    return Kokkos::max(implATS::magnitude(d_row), implATS::magnitude(d_col));
   }
 };
 
