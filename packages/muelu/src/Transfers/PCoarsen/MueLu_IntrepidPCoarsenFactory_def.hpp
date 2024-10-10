c9665abd97ca39c05d74fe74566ac34a54d9858c
Nate Roberts <nvrober@sandia.gov>
2024-10-10 17:54:26 -0500

    MueLu: tentative fix for GenerateRepresentativeBasisNodes() in MueLu_IntrepidPCoarsenFactory_def.hpp, along with enablement of higher-order tests in IntrepidPCoarsenFactory.cpp.
    Still building the tests right now; will see if they pass with this change.

--- packages/muelu/src/Transfers/PCoarsen/MueLu_IntrepidPCoarsenFactory_def.hpp
+++ packages/muelu/src/Transfers/PCoarsen/MueLu_IntrepidPCoarsenFactory_def.hpp
@@ -526,7 +526,7 @@ void GenerateRepresentativeBasisNodes(const Basis &basis, const SCFieldContainer
 
     // 2nd pass: Find all values w/i threshhold of target
     for (size_t j = 0; j < numFieldsHi; j++) {
-      if (Teuchos::ScalarTraits<SC>::magnitude(vmax - LoValues_host(i, j)) < threshold * vmax)
+      if (Teuchos::ScalarTraits<SC>::magnitude(vmax - Teuchos::ScalarTraits<SC>::magnitude(LoValues_host(i, j))) < threshold * vmax)
         representative_node_candidates[i].push_back(j);
     }
   }
