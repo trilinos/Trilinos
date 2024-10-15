b2a7764bba60b3d19b1ff35f2e2a56593e5f2b77
Samuel E. Browne <sebrown@sandia.gov>
2024-10-15 10:31:18 -0600

    Correct ON vs OFF handling
    Needed parenthesis to pass the full option AND value, instead of
    switching the option with the value.
    
    Signed-off-by: Samuel E. Browne <sebrown@sandia.gov>

--- packages/framework/pr_tools/trilinosprhelpers/TrilinosPRConfigurationStandard.py
+++ packages/framework/pr_tools/trilinosprhelpers/TrilinosPRConfigurationStandard.py
@@ -77,7 +77,7 @@ class TrilinosPRConfigurationStandard(TrilinosPRConfigurationBase):
                f"-Dpackage_enables:FILEPATH={self.arg_filename_packageenables}",
                f"-Dsubprojects_file:FILEPATH={self.arg_filename_subprojects}",
                f"-DCTEST_DROP_SITE:STRING={self.arg_ctest_drop_site}",
-                "-DUSE_EXPLICIT_TRILINOS_CACHEFILE:BOOL=" + "ON" if self.arg_use_explicit_cachefile else "OFF",
+                "-DUSE_EXPLICIT_TRILINOS_CACHEFILE:BOOL=" + ("ON" if self.arg_use_explicit_cachefile else "OFF"),
              ]
 
         if self.arg_extra_configure_args:
