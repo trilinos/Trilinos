6cc5fb76407ed45cdc0ad13bb1021bba5aa069fc
Mauro Perego <mperego@sandia.gov>
2024-12-06 16:16:12 -0700

    Piro: remove Stokhos dependency and related tests and files
    Stokhos capability in Piro is based on the Epetra stack and is no longer supported.
    
    Signed-off-by: Mauro Perego <mperego@sandia.gov>

--- packages/piro/cmake/Dependencies.cmake
+++ packages/piro/cmake/Dependencies.cmake
@@ -1,5 +1,5 @@
 SET(LIB_REQUIRED_DEP_PACKAGES   Teuchos Stratimikos ThyraCore Tpetra Teko)
-SET(LIB_OPTIONAL_DEP_PACKAGES   NOX Tempus Stokhos
+SET(LIB_OPTIONAL_DEP_PACKAGES   NOX Tempus
     ROL Ifpack2 MueLu ThyraEpetraAdapters ThyraEpetraExtAdapters Epetra EpetraExt)
 SET(TEST_REQUIRED_DEP_PACKAGES ThyraTpetraAdapters MPI)
 SET(TEST_OPTIONAL_DEP_PACKAGES)
