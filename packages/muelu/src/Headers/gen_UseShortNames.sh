468ceda14effae33f4a58c33722a78f84a1c6aaf
Christian Glusa <caglusa@sandia.gov>
2024-12-10 08:08:40 -0700

    MueLu: Change gen_UseShortNames.sh to generate source files with headers
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/muelu/src/Headers/gen_UseShortNames.sh
+++ packages/muelu/src/Headers/gen_UseShortNames.sh
@@ -6,7 +6,16 @@
 
 classListDir=../Utils/ClassList/
 
-echo "// Type definitions for templated classes (generally graph-related) that do not require a scalar." > MueLu_UseShortNamesOrdinal.hpp
+echo "// @HEADER" > MueLu_UseShortNamesOrdinal.hpp
+echo "// *****************************************************************************" >> MueLu_UseShortNamesOrdinal.hpp
+echo "//        MueLu: A package for multigrid based preconditioning" >> MueLu_UseShortNamesOrdinal.hpp
+echo "//" >> MueLu_UseShortNamesOrdinal.hpp
+echo "// Copyright 2012 NTESS and the MueLu contributors." >> MueLu_UseShortNamesOrdinal.hpp
+echo "// SPDX-License-Identifier: BSD-3-Clause" >> MueLu_UseShortNamesOrdinal.hpp
+echo "// *****************************************************************************" >> MueLu_UseShortNamesOrdinal.hpp
+echo "// @HEADER" >> MueLu_UseShortNamesOrdinal.hpp
+echo "" >> MueLu_UseShortNamesOrdinal.hpp
+echo "// Type definitions for templated classes (generally graph-related) that do not require a scalar." >> MueLu_UseShortNamesOrdinal.hpp
 echo >> MueLu_UseShortNamesOrdinal.hpp
 echo "#include <Xpetra_UseShortNamesOrdinal.hpp>" >> MueLu_UseShortNamesOrdinal.hpp
 echo >> MueLu_UseShortNamesOrdinal.hpp
@@ -27,8 +36,16 @@ done
 #
 # Scalar
 #
-
-echo "// New definition of types using the types Scalar, LocalOrdinal, GlobalOrdinal, Node of the current context." > MueLu_UseShortNamesScalar.hpp
+echo "// @HEADER" > MueLu_UseShortNamesScalar.hpp
+echo "// *****************************************************************************" >> MueLu_UseShortNamesScalar.hpp
+echo "//        MueLu: A package for multigrid based preconditioning" >> MueLu_UseShortNamesScalar.hpp
+echo "//" >> MueLu_UseShortNamesScalar.hpp
+echo "// Copyright 2012 NTESS and the MueLu contributors." >> MueLu_UseShortNamesScalar.hpp
+echo "// SPDX-License-Identifier: BSD-3-Clause" >> MueLu_UseShortNamesScalar.hpp
+echo "// *****************************************************************************" >> MueLu_UseShortNamesScalar.hpp
+echo "// @HEADER" >> MueLu_UseShortNamesScalar.hpp
+echo "" >> MueLu_UseShortNamesScalar.hpp
+echo "// New definition of types using the types Scalar, LocalOrdinal, GlobalOrdinal, Node of the current context." >> MueLu_UseShortNamesScalar.hpp
 echo >> MueLu_UseShortNamesScalar.hpp
 echo "#include <Xpetra_UseShortNamesScalar.hpp>" >> MueLu_UseShortNamesScalar.hpp
 echo >> MueLu_UseShortNamesScalar.hpp
