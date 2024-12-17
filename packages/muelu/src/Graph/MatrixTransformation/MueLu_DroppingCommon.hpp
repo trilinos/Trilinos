c87813dd041b3eac8e0cc5d6b61fef1711653838
Christian Glusa <caglusa@sandia.gov>
2024-12-17 12:13:36 -0700

    MueLu CoalesceDrop_kokkos: Move serial sort to DroppingCommon
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/muelu/src/Graph/MatrixTransformation/MueLu_DroppingCommon.hpp
+++ packages/muelu/src/Graph/MatrixTransformation/MueLu_DroppingCommon.hpp
@@ -402,6 +402,37 @@ class SymmetrizeFunctor {
   }
 };
 
+template <class view_type, class comparator_type>
+KOKKOS_INLINE_FUNCTION void serialHeapSort(view_type& v, comparator_type comparator) {
+  auto N       = v.extent(0);
+  size_t start = N / 2;
+  size_t end   = N;
+  while (end > 1) {
+    if (start > 0)
+      start = start - 1;
+    else {
+      end       = end - 1;
+      auto temp = v(0);
+      v(0)      = v(end);
+      v(end)    = temp;
+    }
+    size_t root = start;
+    while (2 * root + 1 < end) {
+      size_t child = 2 * root + 1;
+      if ((child + 1 < end) and (comparator(v(child), v(child + 1))))
+        ++child;
+
+      if (comparator(v(root), v(child))) {
+        auto temp = v(root);
+        v(root)   = v(child);
+        v(child)  = temp;
+        root      = child;
+      } else
+        break;
+    }
+  }
+}
+
 }  // namespace Misc
 
 }  // namespace MueLu
