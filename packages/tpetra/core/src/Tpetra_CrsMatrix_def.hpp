4bf45065c01c124f103b35dd4cd890850a94adf2
Christian Glusa <caglusa@sandia.gov>
2025-01-21 15:21:53 -0700

    Tpetra CrsMatrix: Fix warning
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/tpetra/core/src/Tpetra_CrsMatrix_def.hpp
+++ packages/tpetra/core/src/Tpetra_CrsMatrix_def.hpp
@@ -9539,7 +9539,7 @@ CrsMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>::
            local_inds_device_value_t tend = tgtLocalRowPtrsDevice(sourceLID + 1);
            local_inds_device_value_t numInTgtRow = (tend - tstart);
 
-           KOKKOS_ASSERT(tstart < tvals.extent(0));
+           KOKKOS_ASSERT(static_cast<size_t>(tstart) < tvals.extent(0));
            impl_scalar_type *tgtRowVals = reinterpret_cast<impl_scalar_type *>(&tvals(tstart));
            const local_inds_device_value_t *tgtColInds = &tgtLocalColIndsDevice(tstart);
 
