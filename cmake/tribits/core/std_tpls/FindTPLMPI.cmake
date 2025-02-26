3229d78be10f7202e9a1ca7f3f93fa6bc775223a
Maarten Arnst <maarten.arnst@uliege.be>
2025-02-26 16:13:52 +0100

    Allow find_package(MPI) when MPI_USE_COMPILER_WRAPPERS is OFF
    Signed-off-by: Maarten Arnst <maarten.arnst@uliege.be>

--- cmake/tribits/core/std_tpls/FindTPLMPI.cmake
+++ cmake/tribits/core/std_tpls/FindTPLMPI.cmake
@@ -20,7 +20,22 @@ if(WIN32 AND TPL_ENABLE_MPI)
   global_set(TPL_MPI_LIBRARIES ${MPI_LIBRARIES})
 endif()
 
-tribits_tpl_find_include_dirs_and_libraries(MPI)
+tribits_tpl_allow_pre_find_package(MPI MPI_ALLOW_PREFIND)
+if(MPI_ALLOW_PREFIND)
+
+  find_package(MPI)
+
+  if(MPI_C_FOUND AND MPI_CXX_FOUND)
+    tribits_extpkg_create_imported_all_libs_target_and_config_file(
+      MPI
+      INNER_FIND_PACKAGE_NAME MPI
+      IMPORTED_TARGETS_FOR_ALL_LIBS MPI::MPI_C MPI::MPI_CXX)
+  endif()
+endif()
+
+if(NOT TARGET MPI::all_libs)
+  tribits_tpl_find_include_dirs_and_libraries(MPI)
+endif()
 
 # NOTE: Above, we need to generate the MPI::all_libs target and the
 # MPIConfig.cmake file that will also provide the MPI::all_libs target.
