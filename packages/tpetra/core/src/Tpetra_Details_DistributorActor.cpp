4046b8285acf980a492bfd799fed771bfb9c0034
Christian Glusa <caglusa@sandia.gov>
2025-03-17 10:56:49 -0600

    Tpetra: Maintain recv and send requests separately
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/tpetra/core/src/Tpetra_Details_DistributorActor.cpp
+++ packages/tpetra/core/src/Tpetra_Details_DistributorActor.cpp
@@ -23,7 +23,8 @@ namespace Details {
 
   DistributorActor::DistributorActor(const DistributorActor& otherActor)
     : mpiTag_(otherActor.mpiTag_),
-      requests_(otherActor.requests_)
+    requestsRecv_(otherActor.requestsRecv_),
+    requestsSend_(otherActor.requestsSend_)
   {
 #ifdef HAVE_TPETRA_DISTRIBUTOR_TIMINGS
     makeTimers();
@@ -32,21 +33,64 @@ namespace Details {
 
   void DistributorActor::doWaits(const DistributorPlan& plan) {
 #ifdef HAVE_TPETRA_DISTRIBUTOR_TIMINGS
-    Teuchos::TimeMonitor timeMon (*timer_doWaits_);
+    Teuchos::TimeMonitor timeMon (*timer_doWaitsRecv_);
 #endif // HAVE_TPETRA_DISTRIBUTOR_TIMINGS
 
-    if (requests_.size() > 0) {
-      Teuchos::waitAll(*plan.getComm(), requests_());
+    if (requestsRecv_.size() > 0) {
+      Teuchos::waitAll(*plan.getComm(), requestsRecv_());
 
       // Restore the invariant that requests_.size() is the number of
       // outstanding nonblocking communication requests.
-      requests_.resize(0);
+      requestsRecv_.resize(0);
+    }
+
+#ifdef HAVE_TPETRA_DISTRIBUTOR_TIMINGS
+    Teuchos::TimeMonitor timeMon (*timer_doWaitsSend_);
+#endif // HAVE_TPETRA_DISTRIBUTOR_TIMINGS
+
+    if (requestsSend_.size() > 0) {
+      Teuchos::waitAll(*plan.getComm(), requestsSend_());
+
+      // Restore the invariant that requests_.size() is the number of
+      // outstanding nonblocking communication requests.
+      requestsSend_.resize(0);
+    }
+  }
+
+  void DistributorActor::doWaitsRecv(const DistributorPlan& plan) {
+#ifdef HAVE_TPETRA_DISTRIBUTOR_TIMINGS
+    Teuchos::TimeMonitor timeMon (*timer_doWaitsRecv_);
+#endif // HAVE_TPETRA_DISTRIBUTOR_TIMINGS
+
+    if (requestsRecv_.size() > 0) {
+      Teuchos::waitAll(*plan.getComm(), requestsRecv_());
+
+      // Restore the invariant that requests_.size() is the number of
+      // outstanding nonblocking communication requests.
+      requestsRecv_.resize(0);
+    }
+  }
+
+  void DistributorActor::doWaitsSend(const DistributorPlan& plan) {
+#ifdef HAVE_TPETRA_DISTRIBUTOR_TIMINGS
+    Teuchos::TimeMonitor timeMon (*timer_doWaitsSend_);
+#endif // HAVE_TPETRA_DISTRIBUTOR_TIMINGS
+
+    if (requestsSend_.size() > 0) {
+      Teuchos::waitAll(*plan.getComm(), requestsSend_());
+
+      // Restore the invariant that requests_.size() is the number of
+      // outstanding nonblocking communication requests.
+      requestsSend_.resize(0);
     }
   }
 
   bool DistributorActor::isReady() const {
     bool result = true;
-    for (auto& request : requests_) {
+    for (auto& request : requestsRecv_) {
+      result &= request->isReady();
+    }
+    for (auto& request : requestsSend_) {
       result &= request->isReady();
     }
     return result;
@@ -54,8 +98,11 @@ namespace Details {
 
 #ifdef HAVE_TPETRA_DISTRIBUTOR_TIMINGS
   void DistributorActor::makeTimers () {
-    timer_doWaits_ = Teuchos::TimeMonitor::getNewTimer (
-                           "Tpetra::Distributor: doWaits");
+    timer_doWaitsRecv_ = Teuchos::TimeMonitor::getNewTimer (
+                           "Tpetra::Distributor: doWaitsRecv");
+
+    timer_doWaitsSend_ = Teuchos::TimeMonitor::getNewTimer (
+                           "Tpetra::Distributor: doWaitsSend");
 
     timer_doPosts3KV_ = Teuchos::TimeMonitor::getNewTimer (
                            "Tpetra::Distributor: doPosts(3) KV");
