6173377d4d6741cc7237e1ab2c1f149690d3c00c
Chris Siefert <csiefer@sandia.gov>
2025-03-24 09:26:55 -0600

    MueLu: Hail Clang!
    Signed-off-by: Chris Siefert <csiefer@sandia.gov>

--- packages/muelu/src/Utils/MueLu_KokkosTuningInterface.cpp
+++ packages/muelu/src/Utils/MueLu_KokkosTuningInterface.cpp
@@ -17,8 +17,6 @@
 #include "MueLu_BaseClass.hpp"
 #include "MueLu_Exceptions.hpp"
 
-
-
 // ***********************************************************************
 /* Notional Parameterlist Structure
    "kokkos tuning: muelu parameter mapping"
@@ -37,13 +35,12 @@
   The output variable should be calculated automatically based on the types above
  */
 
-
 namespace MueLu {
 
-  // FIXME: Will probably need to bump this
-  namespace KokkosTuningParams {
-    const int MAX_VALID_PARAMS=10;
-  };
+// FIXME: Will probably need to bump this
+namespace KokkosTuningParams {
+const int MAX_VALID_PARAMS = 10;
+};
 
 // ***********************************************************************
 RCP<const Teuchos::ParameterList> KokkosTuningInterface::GetValidParameterList() const {
@@ -54,25 +51,22 @@ RCP<const Teuchos::ParameterList> KokkosTuningInterface::GetValidParameterList()
   Teuchos::Array<std::string> ar_dummy;
   std::string s_dummy;
 
-
   // Input variables for Kokkos tuning
   validParamList.set<Teuchos::Array<std::string>>("input variables", ar_dummy, "Names of the input variables for Kokkos tuning");
 
-  for(int i=0;i<KokkosTuningParams::MAX_VALID_PARAMS; i++) {
-    std:: ostringstream oss;
-    oss<< "param" <<i;
+  for (int i = 0; i < KokkosTuningParams::MAX_VALID_PARAMS; i++) {
+    std::ostringstream oss;
+    oss << "param" << i;
     const std::string name = oss.str();
 
     // FIXME: Not validating parameter sublists at present
-    validParamList.set<Teuchos::ParameterList>(name,pl_dummy,"Parameter-specific sublist");
+    validParamList.set<Teuchos::ParameterList>(name, pl_dummy, "Parameter-specific sublist");
   }
 
-  topValidParamList->set<Teuchos::ParameterList>("kokkos tuning: muelu parameter mapping",validParamList,"Sublist for Kokkos tuning of MueLu");
+  topValidParamList->set<Teuchos::ParameterList>("kokkos tuning: muelu parameter mapping", validParamList, "Sublist for Kokkos tuning of MueLu");
 
   return topValidParamList;
 }
-
-
 
 // ***********************************************************************
 void KokkosTuningInterface::Setup() {
@@ -83,11 +77,10 @@ void KokkosTuningInterface::Setup() {
   UnpackMueLuMapping();
 }
 
-
 // ***********************************************************************
 void KokkosTuningInterface::UnpackMueLuMapping() {
   const Teuchos::ParameterList& pL = params_.get<Teuchos::ParameterList>("kokkos tuning: muelu parameter mapping");
-  namespace KTE = Kokkos::Tools::Experimental;
+  namespace KTE                    = Kokkos::Tools::Experimental;
 
   /********************************/
   /* Process the output variables */
@@ -95,53 +88,52 @@ void KokkosTuningInterface::UnpackMueLuMapping() {
   out_variables.clear();
   out_names.clear();
 
-  for(int i=0;i<KokkosTuningParams::MAX_VALID_PARAMS; i++) {
-    std:: ostringstream oss;
-    oss<< "param" <<i;
+  for (int i = 0; i < KokkosTuningParams::MAX_VALID_PARAMS; i++) {
+    std::ostringstream oss;
+    oss << "param" << i;
     const std::string name = oss.str();
 
     if (pL.isSublist(name)) {
       const Teuchos::ParameterList& sublist = pL.sublist(name);
-      std::string muelu_param = sublist.get<std::string>("muelu parameter");
+      std::string muelu_param               = sublist.get<std::string>("muelu parameter");
 
       // Infer types from initial guess
-      if(sublist.isType<int>("initial guess")) {
+      if (sublist.isType<int>("initial guess")) {
         // Discrete range
-        int guess = sublist.get<int>("initial guess");
-        const Teuchos::Array<int> & range = sublist.get<Teuchos::Array<int> >("discrete range");
-        TEUCHOS_TEST_FOR_EXCEPTION(range.size() !=3, Exceptions::RuntimeError, "MueLu::KokkosTuningInterface: 'discrete range' needs to be (low, high, step)");
+        int guess                        = sublist.get<int>("initial guess");
+        const Teuchos::Array<int>& range = sublist.get<Teuchos::Array<int>>("discrete range");
+        TEUCHOS_TEST_FOR_EXCEPTION(range.size() != 3, Exceptions::RuntimeError, "MueLu::KokkosTuningInterface: 'discrete range' needs to be (low, high, step)");
 
         // Set the VariableInfo
         KTE::VariableInfo out_info;
-        out_info.type = KTE::ValueType::kokkos_value_int64;
-        out_info.category = KTE::StatisticalCategory::kokkos_value_interval;
+        out_info.type          = KTE::ValueType::kokkos_value_int64;
+        out_info.category      = KTE::StatisticalCategory::kokkos_value_interval;
         out_info.valueQuantity = KTE::CandidateValueType::kokkos_value_range;
 
         // Unlike the ordinal lists, the ranges get copied into Kokkos
         // TODO: Add support for open/closed ranges
-        out_info.candidates = KTE::make_candidate_range((int64_t)range[0],(int64_t)range[1],(int64_t)range[2],false,false);
-        size_t var_id = KTE::declare_output_type(muelu_param,out_info);
-        out_variables.push_back(KTE::make_variable_value(var_id,int64_t(guess)));
+        out_info.candidates = KTE::make_candidate_range((int64_t)range[0], (int64_t)range[1], (int64_t)range[2], false, false);
+        size_t var_id       = KTE::declare_output_type(muelu_param, out_info);
+        out_variables.push_back(KTE::make_variable_value(var_id, int64_t(guess)));
 
         out_typenames.push_back("int");
-      }
-      else if(sublist.isType<double>("initial guess")) {
+      } else if (sublist.isType<double>("initial guess")) {
         // Continuous range
-        double guess = sublist.get<double>("initial guess");
-        const Teuchos::Array<double> & range = sublist.get<Teuchos::Array<double> >("continuous range");
-        TEUCHOS_TEST_FOR_EXCEPTION(range.size() !=3, Exceptions::RuntimeError, "MueLu::KokkosTuningInterface: 'continuous range' needs to be (low, high, step)");
+        double guess                        = sublist.get<double>("initial guess");
+        const Teuchos::Array<double>& range = sublist.get<Teuchos::Array<double>>("continuous range");
+        TEUCHOS_TEST_FOR_EXCEPTION(range.size() != 3, Exceptions::RuntimeError, "MueLu::KokkosTuningInterface: 'continuous range' needs to be (low, high, step)");
 
         // Set the VariableInfo
         KTE::VariableInfo out_info;
-        out_info.type = KTE::ValueType::kokkos_value_double;
-        out_info.category = KTE::StatisticalCategory::kokkos_value_interval;
+        out_info.type          = KTE::ValueType::kokkos_value_double;
+        out_info.category      = KTE::StatisticalCategory::kokkos_value_interval;
         out_info.valueQuantity = KTE::CandidateValueType::kokkos_value_range;
 
         // Unlike the ordinal lists, the ranges get copied into Kokkos
         // TODO: Add support for open/closed ranges
-        out_info.candidates = KTE::make_candidate_range(range[0],range[1],range[2],false,false);
-        size_t var_id = KTE::declare_output_type(muelu_param,out_info);
-        out_variables.push_back(KTE::make_variable_value(var_id,guess));
+        out_info.candidates = KTE::make_candidate_range(range[0], range[1], range[2], false, false);
+        size_t var_id       = KTE::declare_output_type(muelu_param, out_info);
+        out_variables.push_back(KTE::make_variable_value(var_id, guess));
 
         out_typenames.push_back("double");
       }
@@ -153,9 +145,9 @@ void KokkosTuningInterface::UnpackMueLuMapping() {
       // Stash the parameter name
       out_names.push_back(muelu_param);
 
-    }// end if pL.isSublist
+    }  // end if pL.isSublist
 
-  }// end for
+  }  // end for
 
   // Sanity check
   TEUCHOS_TEST_FOR_EXCEPTION(out_names.size() != out_variables.size(), Exceptions::RuntimeError, "MueLu::KokkosTuningInterface: Error in option processing");
@@ -166,17 +158,15 @@ void KokkosTuningInterface::UnpackMueLuMapping() {
   /********************************/
   in_variables.clear();
 
-  const Teuchos::Array<std::string> & inputs = pL.get<Teuchos::Array<std::string> >("input variables");
+  const Teuchos::Array<std::string>& inputs = pL.get<Teuchos::Array<std::string>>("input variables");
 
-  for (int i=0; i< (int)inputs.size(); i++) {
+  for (int i = 0; i < (int)inputs.size(); i++) {
     // NOTE: The string name is copied in here (unlike double/int) so we don't need to cache.
     KTE::VariableInfo in_info;
-    in_info.type = KTE::ValueType::kokkos_value_string;
-    size_t var_id = KTE::declare_input_type(inputs[i].c_str(),in_info);
-    in_variables.push_back(KTE::make_variable_value(var_id,inputs[i].c_str()));
+    in_info.type  = KTE::ValueType::kokkos_value_string;
+    size_t var_id = KTE::declare_input_type(inputs[i].c_str(), in_info);
+    in_variables.push_back(KTE::make_variable_value(var_id, inputs[i].c_str()));
   }
-
-
 }
 
 // ***********************************************************************
@@ -190,7 +180,7 @@ std::vector<std::string> KokkosTuningInterface::SplitString(const std::string& b
   while (end != std::string::npos) {
     tokens.push_back(base_string.substr(start, end - start));
     start = end + delimiter.length();
-    end = base_string.find(delimiter, start);
+    end   = base_string.find(delimiter, start);
   }
 
   // And the final token...
@@ -206,11 +196,11 @@ void KokkosTuningInterface::SetMueLuParameters(size_t kokkos_context_id, Teuchos
 
   if (comm_->getRank() == 0) {
     // Only Rank 0 calls KokkosTuning
-    GetOStream(Runtime0) << "MueLu::KokkosTuningInterface: Tuning " << out_variables.size() << " parameters" <<std::endl;
+    GetOStream(Runtime0) << "MueLu::KokkosTuningInterface: Tuning " << out_variables.size() << " parameters" << std::endl;
 
     // Set input variable
     if (IsPrint(Runtime1))
-      GetOStream(Runtime1) <<"Adding "<<in_variables.size()<<" input variables"<<std::endl;
+      GetOStream(Runtime1) << "Adding " << in_variables.size() << " input variables" << std::endl;
 
     KTE::set_input_values(kokkos_context_id, in_variables.size(), in_variables.data());
 
@@ -219,32 +209,31 @@ void KokkosTuningInterface::SetMueLuParameters(size_t kokkos_context_id, Teuchos
 
     // Diagnostic output
     if (IsPrint(Runtime1)) {
-      GetOStream(Runtime1) << "Tuned Parameters: "<<std::endl;
-      for(int i=0; i<(int)out_variables.size(); i++) {
+      GetOStream(Runtime1) << "Tuned Parameters: " << std::endl;
+      for (int i = 0; i < (int)out_variables.size(); i++) {
         if (out_typenames[i] == "int")
-          GetOStream(Runtime1) << "- " << out_names[i] << ": " <<  out_variables[i].value.int_value<<std::endl;
+          GetOStream(Runtime1) << "- " << out_names[i] << ": " << out_variables[i].value.int_value << std::endl;
         else if (out_typenames[i] == "double")
-          GetOStream(Runtime1) << "- " << out_names[i] << ": " <<  out_variables[i].value.double_value<<std::endl;
+          GetOStream(Runtime1) << "- " << out_names[i] << ": " << out_variables[i].value.double_value << std::endl;
       }
     }
 
     // Unpack the tuned values
-    for(int i=0; i<(int)out_names.size(); i++) {
-
+    for (int i = 0; i < (int)out_names.size(); i++) {
       // Because we'll want to set parameters inside sublists we'll allow the "muelu parameter" option to specify sublists with '||' as a nesting delimiter
       // That's really, really unlikely to be part of a parameter list item name, so we'll go with it.
-      Teuchos::ParameterList *activeList = &tunedParams;
-      std::vector<std::string> treeWalk = SplitString(out_names[i],"||");
+      Teuchos::ParameterList* activeList = &tunedParams;
+      std::vector<std::string> treeWalk  = SplitString(out_names[i], "||");
 
       // Walk down all but the last guy
-      for(int j=0;j<(int) treeWalk.size()-1; j++) {
+      for (int j = 0; j < (int)treeWalk.size() - 1; j++) {
         activeList = &(activeList->sublist(treeWalk[j]));
       }
 
-      std::string activeName=treeWalk[treeWalk.size()-1];
+      std::string activeName = treeWalk[treeWalk.size() - 1];
 
       if (out_typenames[i] == "int")
-        activeList->set(activeName, (int) out_variables[i].value.int_value);
+        activeList->set(activeName, (int)out_variables[i].value.int_value);
       else if (out_typenames[i] == "double")
         activeList->set(activeName, out_variables[i].value.double_value);
       else {
@@ -255,6 +244,5 @@ void KokkosTuningInterface::SetMueLuParameters(size_t kokkos_context_id, Teuchos
 
   Teuchos::updateParametersAndBroadcast(outArg(tunedParams), outArg(mueluParams), *comm_, 0, overwrite);
 }
-
 
 }  // namespace MueLu
