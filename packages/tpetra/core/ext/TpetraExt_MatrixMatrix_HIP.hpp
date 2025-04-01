5c4ec62f3170d1f173ff069293934e63c9b6e642
Chris Siefert <csiefer2@users.noreply.github.com>
2025-04-01 10:11:47 -0600

    Update packages/tpetra/core/ext/TpetraExt_MatrixMatrix_HIP.hpp
    Co-authored-by: Luc Berger <lberge@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>

--- packages/tpetra/core/ext/TpetraExt_MatrixMatrix_HIP.hpp
+++ packages/tpetra/core/ext/TpetraExt_MatrixMatrix_HIP.hpp
@@ -218,7 +218,7 @@ void KernelWrappers<Scalar,LocalOrdinal,GlobalOrdinal,Tpetra::KokkosCompat::Kokk
 
     {
       Tpetra::Details::ProfilingRegion MM2("TpetraExt: MMM: Newmatrix KokkosKernels symbolic non-int");
-      KokkosSparse::Experimental::spgemm_symbolic(&kh,AnumRows,BnumRows,BnumCols,Amat.graph.row_map,Amat.graph.entries,false,Bmerged.graph.row_map,Bmerged.graph.entries,false,row_mapC);
+      KokkosSparse::spgemm_symbolic(&kh,AnumRows,BnumRows,BnumCols,Amat.graph.row_map,Amat.graph.entries,false,Bmerged.graph.row_map,Bmerged.graph.entries,false,row_mapC);
     }
 
     size_t c_nnz_size = kh.get_spgemm_handle()->get_c_nnz();
