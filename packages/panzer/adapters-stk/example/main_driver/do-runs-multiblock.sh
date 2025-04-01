fbfd155e6bf7535558778d701f376c6aa0deeddf
Roger Pawlowski <rppawlo@sandia.gov>
2025-04-01 14:26:53 -0600

    Panzer: update input file for bc ic issue
    Signed-off-by: Roger Pawlowski <rppawlo@sandia.gov>

--- packages/panzer/adapters-stk/example/main_driver/do-runs-multiblock.sh
+++ packages/panzer/adapters-stk/example/main_driver/do-runs-multiblock.sh
@@ -1,4 +1,5 @@
 export IOSS_PROPERTIES="DECOMPOSITION_METHOD=rcb"
+#export IOSS_PROPERTIES="DECOMPOSITION_METHOD=rib"
 mpirun -np 3 ./PanzerAdaptersSTK_main_driver.exe --i=energy-ss-tp-multiblock-ic-bc-issue.xml
 mpirun -np 2 ./PanzerAdaptersSTK_main_driver.exe --i=energy-ss-tp-multiblock-ic-bc-issue.xml
 mpirun -np 1 ./PanzerAdaptersSTK_main_driver.exe --i=energy-ss-tp-multiblock-ic-bc-issue.xml
