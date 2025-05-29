431fb1e68ac82fe4198a6d11f38328e9e7bba77b
Greg Sjaardema <gsjaardema@gmail.com>
2025-05-29 13:52:35 -0600

    ALGEBRA: Compilation fixes for gcc-15
    Signed-off-by: Greg Sjaardema <gsjaardema@gmail.com>

--- packages/seacas/applications/algebra/ag_myamod.f
+++ packages/seacas/applications/algebra/ag_myamod.f
@@ -4,14 +4,14 @@ C    NTESS, the U.S. Government retains certain rights in this software.
 C
 C    See packages/seacas/LICENSE for details
 C=======================================================================
-      REAL FUNCTION MYAMOD (PARM1, PARM2)
+      REAL FUNCTION FUNCAMOD (PARM1, PARM2)
 C=======================================================================
 
-C   --*** MYAMOD *** (ALGEBRA) amod function
+C   --*** FUNCAMOD *** (ALGEBRA) amod function
 C   --
 C   --   PARM1, PARM2 - IN - the maximum parameters
 
-      MYAMOD = MOD(PARM1, PARM2)
+      FUNCAMOD = MOD(PARM1, PARM2)
 
       RETURN
       END
--- packages/seacas/applications/algebra/ag_mysum.f
+++ packages/seacas/applications/algebra/ag_mysum.f
@@ -4,19 +4,19 @@ C    NTESS, the U.S. Government retains certain rights in this software.
 C
 C    See packages/seacas/LICENSE for details
 C=======================================================================
-      REAL FUNCTION MYSUM (PARM1, PARM2)
+      REAL FUNCTION FUNCSUM (PARM1, PARM2)
 C=======================================================================
 
-C   --*** MYSUM *** (ALGEBRA) Addition function
+C   --*** FUNCSUM *** (ALGEBRA) Addition function
 C   --   Written by Amy Gilkey - revised 07/24/87
 C   --
-C   --MYSUM returns the sum of two real numbers.  This is a function so that
+C   --FUNCSUM returns the sum of two real numbers.  This is a function so that
 C   --it can be passed to DOFNC2.
 C   --
 C   --Parameters:
 C   --   PARM1, PARM2 - IN - the numbers to be summed
 
-      MYSUM = PARM1 + PARM2
+      FUNCSUM = PARM1 + PARM2
 
       RETURN
       END
