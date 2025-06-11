6b67a46053da5013b57a8a520c14f8b9ae053913
Carl Pearson <cwpears@sandia.gov>
2025-06-11 11:44:47 -0600

    Add Details::distributorSendTypes, validate defaultSendType before putting in plist
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

--- packages/tpetra/core/src/Tpetra_Details_DistributorPlan.hpp
+++ packages/tpetra/core/src/Tpetra_Details_DistributorPlan.hpp
@@ -61,8 +61,17 @@ DistributorSendTypeEnumToString (EDistributorSendType sendType);
 EDistributorSendType
 DistributorSendTypeStringToEnum (const std::string_view s);
 
+/// \brief Valid string values for Distributor's "Send type" parameter.
+Teuchos::Array<std::string> distributorSendTypes ();
+
 /// \brief Valid enum values of distributor send types.
 Teuchos::Array<EDistributorSendType> distributorSendTypeEnums ();
+
+/// \brief Valid string values of distributor send types.
+Teuchos::Array<std::string> distributorSendTypes ();
+
+/// \brief Return the provided argument. Throw if it's not a valid send type.
+const std::string &validSendTypeOrThrow (const std::string &s);
 
 /// \brief Enum indicating how and whether a Distributor was initialized.
 ///
