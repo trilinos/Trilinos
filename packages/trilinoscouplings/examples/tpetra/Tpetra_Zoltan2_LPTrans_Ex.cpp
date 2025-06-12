0817acfc4018c309bb5d33eee107f1b75cc13c35
Ernesto Prudencio <eeprude@sandia.gov>
2025-06-11 21:15:37 -0600

    Detailing the example code.

--- packages/trilinoscouplings/examples/tpetra/Tpetra_Zoltan2_LPTrans_Ex.cpp
+++ packages/trilinoscouplings/examples/tpetra/Tpetra_Zoltan2_LPTrans_Ex.cpp
@@ -8,10 +8,15 @@
 // @HEADER
 
 //--------------------------------------------------------------------
-// This file is a self-contained example of creating an Epetra_CrsGraph
-//and Epetra_CrsMatrix object, and using Isorropia through the EpetraExt
-//transform interface to rebalance them.
-// NOTE:  This is a modified version of the matrix_1.cpp example in Isorropia.
+// This file is a self-contained example of rebalancing the contents of
+// a matrix and a vector among different MPI nodes. The matrix is formed,
+// on purpose, in an unbalanced way.
+//
+// Originally, this example used EpetraExt + Isorropia for rebalance. It
+// was a modified version of the matrix_1.cpp example in Isorropia.
+//
+// This example now uses Tpetra + Zoltan2. It is a modified version of
+// the zoltan2/example/graph/graph.cpp example in Zoltan2.
 //--------------------------------------------------------------------
 
 #ifdef HAVE_MPI
@@ -25,12 +30,13 @@
 
 #endif // ifdef HAVE_MPI
 
-#include <Tpetra_Map_decl.hpp>
+//#include <Tpetra_Map_decl.hpp>
 #include <Tpetra_CrsMatrix_decl.hpp>
-#include <Tpetra_MultiVector_decl.hpp>
+//#include <Tpetra_MultiVector_decl.hpp>
 
 #include <Zoltan2_PartitioningProblem.hpp>
 #include <Zoltan2_XpetraCrsMatrixAdapter.hpp>
+#include <Zoltan2_XpetraMultiVectorAdapter.hpp>
 
 #include <Teuchos_ParameterList.hpp>
 #include <Teuchos_RCP.hpp>
@@ -43,45 +49,25 @@ void runExample() {
   int localProc(0);
   MPI_Comm_rank(MPI_COMM_WORLD, &localProc);
 
-  //Now create a balanced copy of the matrix using the Tpetra
-  //transform interface to Isorropia.
-  //NOTE: By default, Isorropia will use Zoltan for the
-  //repartitioning, if Isorropia was configured with Zoltan support.
-  //(i.e., --enable-isorropia-zoltan flag to configure, plus Zoltan include
-  //paths and library directives)
-  //If Isorropia was not configured with Zoltan support, then a simple
-  //built-in linear partitioner will be used to make sure the number
-  //of nonzeros on each processor is equal or close to equal.
-
   using Map_t      = Tpetra::Map<>;
   using localId_t  = Map_t::local_ordinal_type;
   using globalId_t = Map_t::global_ordinal_type;
   using scalar_t   = Tpetra::Details::DefaultTypes::scalar_type;
+  using Vector_t   = Tpetra::Vector<scalar_t, localId_t, globalId_t>;
   using Matrix_t   = Tpetra::CrsMatrix<scalar_t, localId_t, globalId_t>;
-  //Teuchos::RCP<Matrix_t> origMatrix;
 
-  std::string method("scotch");    // Partitioning method
-
-  Teuchos::ParameterList param;
-  param.set("partitioning_approach", "partition");
-  param.set("algorithm", method);
-
-  // Create a Tpetra::CrsMatrix with rows spread un-evenly over processors.
-  if (localProc == 0) {
-    std::cout << "Creating Tpetra::CrsMatrix with un-even distribution..."
-              << std::endl;
-  }
-  MPI_Barrier(MPI_COMM_WORLD);
-
+  // ****************************************************************
+  // Step 1/6: create a Tpetra::CrsMatrix with rows spread un-evenly
+  //           over processors.
+  // ****************************************************************
   Teuchos::RCP< Teuchos::MpiComm<int> > comm = Teuchos::rcp( new Teuchos::MpiComm<int>(MPI_COMM_WORLD) );
-  localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ local_num_rows = 200;
-  localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ nnz_per_row = local_num_rows/4+1;
-  globalId_t/*Tpetra::CrsMatrix<double>::global_ordinal_type*/ global_num_rows = numProcs*local_num_rows;
+  localId_t  local_num_rows  = 200;
+  localId_t  nnz_per_row     = local_num_rows/4+1;
+  globalId_t global_num_rows = numProcs*local_num_rows;
 
   int mid_proc = numProcs/2;
   bool num_procs_even = numProcs%2==0 ? true : false;
-
-  localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ adjustment = local_num_rows/2;
+  localId_t adjustment = local_num_rows/2;
 
   // Adjust local_num_rows so that it's not equal on all procs.
   if (localProc < mid_proc) {
@@ -101,16 +87,14 @@ void runExample() {
 
   // Now we're ready to create a row-map.
   Teuchos::RCP< Map_t > rowmap = Teuchos::rcp( new Map_t(global_num_rows, local_num_rows, 0, comm) );
-  Teuchos::RCP< Matrix_t/*Tpetra::CrsMatrix<double>*/ > originalMatrix = Teuchos::rcp( new Matrix_t/*Tpetra::CrsMatrix<double>*/(rowmap, nnz_per_row) );
+  Teuchos::RCP< Matrix_t > originalMatrix = Teuchos::rcp( new Matrix_t(rowmap, nnz_per_row) );
 
-  //std::cout << "originalMatrix = " << originalMatrix << std::endl;
-
-  std::vector<globalId_t/*Tpetra::CrsMatrix<double>::global_ordinal_type*/> indices(nnz_per_row);
+  std::vector<globalId_t> indices(nnz_per_row);
   std::vector<double> coefs(nnz_per_row);
 
-  for (localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ i(0); i < local_num_rows; ++i) {
-    globalId_t/*Tpetra::CrsMatrix<double>::global_ordinal_type*/ global_row = rowmap->getGlobalElement(i);
-    globalId_t/*Tpetra::CrsMatrix<double>::global_ordinal_type*/ first_col = global_row - nnz_per_row/2;
+  for (localId_t i(0); i < local_num_rows; ++i) {
+    globalId_t global_row = rowmap->getGlobalElement(i);
+    globalId_t first_col = global_row - nnz_per_row/2;
 
     if (first_col < 0) {
       first_col = 0;
@@ -119,47 +103,66 @@ void runExample() {
       first_col = global_num_rows - nnz_per_row;
     }
 
-    for (localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ j(0); j < nnz_per_row; ++j) {
+    for (localId_t j(0); j < nnz_per_row; ++j) {
       indices[j] = first_col + j;
       coefs[j] = 1.0;
     }
 
-    originalMatrix->insertGlobalValues(global_row, nnz_per_row,
-                                  &coefs[0], &indices[0]);
+    originalMatrix->insertGlobalValues(global_row,
+                                       nnz_per_row,
+                                       &coefs[0],
+                                       &indices[0]);
   }
 
   originalMatrix->fillComplete();
 
-  // Create Tpetra::MultiVectors for the lhs and rhs of the linear problem.
-  Teuchos::RCP< Tpetra::MultiVector<double> > lhs = Teuchos::rcp( new Tpetra::MultiVector<double>(originalMatrix->getMap(), 1) );
-  Teuchos::RCP< Tpetra::MultiVector<double> > rhs = Teuchos::rcp( new Tpetra::MultiVector<double>(originalMatrix->getMap(), 1) );
-  rhs->putScalar( 1.0 );
+  // ****************************************************************
+  // Step 2/6: instantiate a partitioning problem and solve it.
+  // ****************************************************************
+  using MatrixAdapter_t = Zoltan2::XpetraCrsMatrixAdapter<Matrix_t>;
+  MatrixAdapter_t matrixAdapter(originalMatrix);
 
-  // Create the linear problem with the original partitioning.
-  //Tpetra::LinearProblem< double
-  //                     , localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/
-  //                     , Tpetra::CrsMatrix<double>::global_ordinal_type
-  //                     , Tpetra::CrsMatrix<double>::node_type
-  //                     > problem( crsmatrix, lhs, rhs );
+  std::string partitioningMethod("scotch");
+  Teuchos::ParameterList param;
+  param.set("partitioning_approach", "partition");
+  param.set("algorithm", partitioningMethod);
 
-  // Create the new linear problem and perform the balanced partitioning.
-  // NOTE:  The balanced linear system will be in transformedProblem after fwd() is called.
-  //        It is not necessary for the RCP to manage the transformed problem.
+  Zoltan2::PartitioningProblem<MatrixAdapter_t> partitioningProblem(&matrixAdapter, &param);
+  partitioningProblem.solve();
 
-  typedef Zoltan2::XpetraCrsMatrixAdapter<Matrix_t> MatrixAdapter_t;
-  MatrixAdapter_t adapter(originalMatrix);
+  // ****************************************************************
+  // Step 3/6: create a random vector compatible (distribution wise)
+  //           with the original matrix. The original matrix will
+  //           multiply this vector in the step 6/6 below.
+  // ****************************************************************
+  Teuchos::RCP<Vector_t> originalVector;
+  originalVector  = Tpetra::createVector<scalar_t,localId_t,globalId_t>( originalMatrix->getDomainMap() );
+  originalVector->randomize();
 
-  Zoltan2::PartitioningProblem<MatrixAdapter_t> problem2(&adapter, &param);
-  problem2.solve();
-
+  // ****************************************************************
+  // Step 4/6: balance the matrix and the vector
+  // ****************************************************************
   Teuchos::RCP<Matrix_t> balancedMatrix;
-  adapter.applyPartitioningSolution(*originalMatrix, balancedMatrix,
-                                    problem2.getSolution());
+  matrixAdapter.applyPartitioningSolution(*originalMatrix,
+                                          balancedMatrix,
+                                          partitioningProblem.getSolution());
 
-  localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ originalLocalNumRows = originalMatrix->getLocalNumRows();
-  localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ balancedLocalNumRows = balancedMatrix->getLocalNumRows();
-  localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ originalLocalNNZ     = originalMatrix->getLocalNumEntries();
-  localId_t/*Tpetra::CrsMatrix<double>::local_ordinal_type*/ balancedLocalNNZ     = balancedMatrix->getLocalNumEntries();
+  using MultiVectorAdapter_t = Zoltan2::XpetraMultiVectorAdapter<Vector_t>;
+  MultiVectorAdapter_t vectorAdapter(originalVector);
+
+  Teuchos::RCP<Vector_t> balancedVector;
+  vectorAdapter.applyPartitioningSolution(*originalVector,
+                                          balancedVector,
+                                          partitioningProblem.getSolution());
+
+  // ****************************************************************
+  // Step 5/6: compare the matrix balancing among MPI nodes, before
+  //           and after solving the partitionaing problem.
+  // ****************************************************************
+  localId_t originalLocalNumRows = originalMatrix->getLocalNumRows();
+  localId_t balancedLocalNumRows = balancedMatrix->getLocalNumRows();
+  localId_t originalLocalNNZ     = originalMatrix->getLocalNumEntries();
+  localId_t balancedLocalNNZ     = balancedMatrix->getLocalNumEntries();
 
   MPI_Barrier(MPI_COMM_WORLD);
   for (int p(0); p < numProcs; ++p) {
@@ -177,6 +180,25 @@ void runExample() {
               << std::endl;
   }
   MPI_Barrier(MPI_COMM_WORLD);
+
+  // ****************************************************************
+  // Step 6/6: check sparse matvec
+  // ****************************************************************
+  Teuchos::RCP<Vector_t> originalMatVec = Tpetra::createVector<scalar_t,localId_t,globalId_t>( originalMatrix->getRangeMap() );
+  originalMatrix->apply(*originalVector, *originalMatVec);
+  scalar_t originalNorm = originalMatVec->norm2();
+
+  Teuchos::RCP<Vector_t> balancedMatVec = Tpetra::createVector<scalar_t,localId_t,globalId_t>( balancedMatrix->getRangeMap() );
+  balancedMatrix->apply(*balancedVector, *balancedMatVec);
+  scalar_t balancedNorm = balancedMatVec->norm2();
+
+  if (localProc == 0) {
+    std::cout << "||originalMatVec||_2 = " << originalNorm
+              << ", ||balancedMatVec||_2 = " << balancedNorm
+              << std::endl;
+  }
+  MPI_Barrier(MPI_COMM_WORLD);
+  
 }
 #endif // ifdef HAVE_MPI
 
