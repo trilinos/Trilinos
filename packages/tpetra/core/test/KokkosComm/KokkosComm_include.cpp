d4b76760827b3d2bc5488d36f4d5b33116e8414d
Carl Pearson <cwpears@sandia.gov>
2025-06-18 12:28:52 -0600

    tpetra: detect presence of KokkosComm package
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

---
+++ packages/tpetra/core/test/KokkosComm/KokkosComm_include.cpp
@@ -0,0 +1,15 @@
+// @HEADER
+// *****************************************************************************
+//          Tpetra: Templated Linear Algebra Services Package
+//
+// Copyright 2008 NTESS and the Tpetra contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+// @HEADER
+
+#include <KokkosComm/KokkosComm.hpp>
+
+int main(int argc, char* argv[])
+{
+  return 0;
+}
