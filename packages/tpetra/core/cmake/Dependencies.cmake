47dbfa522f5e656d7204c33f1c9eb7a72503aaeb
Samuel E. Browne <sebrown@sandia.gov>
2025-07-30 15:16:38 -0600

    Remove all dependencies logic for deprecated packages
    Scrub dependencies on deprecated packages from any
    non-deprecated package's `Dependencies.cmake` file.
    
    This helps prevent issues where TriBITS could try to enable a
    nonexistant package or complain about one during configure.
    
    Signed-off-by: Samuel E. Browne <sebrown@sandia.gov>

--- packages/tpetra/core/cmake/Dependencies.cmake
+++ packages/tpetra/core/cmake/Dependencies.cmake
@@ -1,5 +1,5 @@
 TRIBITS_PACKAGE_DEFINE_DEPENDENCIES(
   LIB_REQUIRED_PACKAGES Teuchos Kokkos TeuchosKokkosCompat TeuchosKokkosComm KokkosKernels
-  LIB_OPTIONAL_PACKAGES Epetra TpetraTSQR TeuchosNumerics
+  LIB_OPTIONAL_PACKAGES TpetraTSQR TeuchosNumerics
   LIB_OPTIONAL_TPLS MPI CUDA QD quadmath mpi_advance
 )
