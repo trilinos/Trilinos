d06b8cd7896e24dc909768bf3dfd32bf26d2f502
Greg Sjaardema <gsjaardema@gmail.com>
2025-08-19 10:36:46 -0600

    IOSS Changes
    * More robust parallel read error detection and reporting of non-consistent files
    * Remove unused macro
    
    Signed-off-by: Greg Sjaardema <gsjaardema@gmail.com>

--- packages/seacas/libraries/ioss/src/Ioss_Utils.h
+++ packages/seacas/libraries/ioss/src/Ioss_Utils.h
@@ -35,18 +35,22 @@ namespace Ioss {
   enum class ElementShape : unsigned int;
 } // namespace Ioss
 
-[[noreturn]] inline void IOSS_ERROR(const std::ostringstream &errmsg)
+[[noreturn]] inline void IOSS_ERROR(const std::string &errmsg)
 {
-  throw std::runtime_error((errmsg).str());
+#if defined(SEACAS_HAVE_MPI)
+  int parallelSize = 1;
+  MPI_Comm_size(MPI_COMM_WORLD, &parallelSize); // CHECK: ALLOW MPI_COMM_WORLD
+  if (parallelSize > 1) {
+    std::cerr << "ERROR: " << errmsg << "\n";
+    MPI_Abort(MPI_COMM_WORLD, 1); // CHECK: ALLOW MPI_COMM_WORLD
+    // MPI_Abort should not return, but if it does, we throw the exception...
+    // This helps quiet the compiler also.
+  }
+#endif
+  throw std::runtime_error(errmsg);
 }
 
-[[noreturn]] inline void IOSS_ERROR(const std::string &errmsg) { throw std::runtime_error(errmsg); }
-
-#ifdef NDEBUG
-#define IOSS_ASSERT_USED(x) (void)x
-#else
-#define IOSS_ASSERT_USED(x)
-#endif
+[[noreturn]] inline void IOSS_ERROR(const std::ostringstream &errmsg) { IOSS_ERROR(errmsg.str()); }
 
 // We have been relying on the assumption that calling `.data()` on an empty vector
 // will return `nullptr`.  However, according to cppreference (based on the standard):
