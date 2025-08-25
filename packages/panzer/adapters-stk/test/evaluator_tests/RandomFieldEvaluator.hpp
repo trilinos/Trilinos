69463c09f17c4d1444e3ec3fd7d557daa41561de
Roger Pawlowski <rppawlo@sandia.gov>
2025-08-25 09:21:37 -0600

    Panzer: Fixes for the new Kokkos::View implementation
    Signed-off-by: Roger Pawlowski <rppawlo@sandia.gov>
    Signed-off-by: Christian Trott <crtrott@sandia.gov>

--- packages/panzer/adapters-stk/test/evaluator_tests/RandomFieldEvaluator.hpp
+++ packages/panzer/adapters-stk/test/evaluator_tests/RandomFieldEvaluator.hpp
@@ -16,6 +16,7 @@
 #include "Phalanx_MDField.hpp"
 #include "Panzer_IntegrationRule.hpp"
 #include "Panzer_Workset_Utilities.hpp"
+#include "Kokkos_Random.hpp"
 
 template <typename ScalarT>
 class PointEvaluation {
@@ -26,68 +27,38 @@ public:
 
 template<typename EvalT, typename Traits>
 class RandomFieldEvaluator
-  :
-  public PHX::EvaluatorWithBaseImpl<Traits>,
-  public PHX::EvaluatorDerived<EvalT, Traits>
+  : public PHX::EvaluatorWithBaseImpl<Traits>,
+    public PHX::EvaluatorDerived<EvalT, Traits>
 {
   public:
+  void evaluateFields(typename Traits::EvalData d);
 
-    RandomFieldEvaluator(
-      const Teuchos::ParameterList& p);
-
-    void
-    postRegistrationSetup(
-      typename Traits::SetupData d,
-      PHX::FieldManager<Traits>& fm);
-
-    void
-    evaluateFields(
-      typename Traits::EvalData d);
-
-  private:
-
-    using ScalarT = typename EvalT::ScalarT;
-
-  PHX::MDField<ScalarT> field;
-
+private:
+  using ScalarT = typename EvalT::ScalarT;
+  PHX::MDField<ScalarT> field_;
+  Kokkos::Random_XorShift64_Pool<PHX::Device> random_pool_;
+  
 public:
   RandomFieldEvaluator(const std::string & name,
                        const Teuchos::RCP<PHX::DataLayout> & dl)
-     : field(name,dl) { this->addEvaluatedField(field); }
-}; // end of class RandomFieldEvaluator
-
-
-//**********************************************************************
-template<typename EvalT, typename Traits>
-RandomFieldEvaluator<EvalT, Traits>::
-RandomFieldEvaluator(
-  const Teuchos::ParameterList& p)
-{
-   TEUCHOS_ASSERT(false); // don't do this
-}
+    : field_(name,dl),random_pool_(12345)
+  { this->addEvaluatedField(field_); }
+};
 
 //**********************************************************************
 template<typename EvalT, typename Traits>
 void
 RandomFieldEvaluator<EvalT, Traits>::
-postRegistrationSetup(
-  typename Traits::SetupData  /* sd */,
-  PHX::FieldManager<Traits>&  fm)
+evaluateFields(typename Traits::EvalData workset)
 {
-  this->utils.setFieldData(field,fm);
-}
-
-//**********************************************************************
-template<typename EvalT, typename Traits>
-void
-RandomFieldEvaluator<EvalT, Traits>::
-evaluateFields(
-  typename Traits::EvalData  /* workset */)
-{
-  auto field_h = Kokkos::create_mirror_view(field.get_view());
-   for(int i=0;i<static_cast<int>(field.size());i++)
-      field_h[i] = double(std::rand())/double(RAND_MAX);
-   Kokkos::deep_copy(field.get_view(),field_h);
+  auto field = field_;
+  auto random_pool = random_pool_;
+  
+  Kokkos::parallel_for("panzer::RandomFieldEvalautor",workset.num_cells,KOKKOS_LAMBDA(const int i){
+    auto generator = random_pool.get_state();
+    field(i) = generator.drand(0.0,1.0);
+    random_pool.free_state(generator);
+  });
 }
 
 //**********************************************************************
