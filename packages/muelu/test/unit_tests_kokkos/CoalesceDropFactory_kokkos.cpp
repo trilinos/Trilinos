4158f8c7e1ea4fa4ae0bf3553ca498db8f342614
Nathan Ellingwood <ndellin@sandia.gov>
2025-08-28 16:38:09 -0600

    muelu: update HostMirror->host_mirror_type
    Compatibility update when disabling KOKKOS_ENABLE_DEPRECATED_CODE_4
    See https://github.com/kokkos/kokkos/pull/8232
    
    The deprecation of HostMirror is present on kokkos@develop (version > 4.7.99)
    
    Signed-off-by: Nathan Ellingwood <ndellin@sandia.gov>

--- packages/muelu/test/unit_tests_kokkos/CoalesceDropFactory_kokkos.cpp
+++ packages/muelu/test/unit_tests_kokkos/CoalesceDropFactory_kokkos.cpp
@@ -2637,7 +2637,11 @@ TEUCHOS_UNIT_TEST_TEMPLATE_4_DECL(CoalesceDropFactory_kokkos, 2x2, Scalar, Local
   }
 
   using TF                = TestHelpers_kokkos::TestFactory<SC, LO, GO, NO>;
+#if KOKKOS_VERSION > 40799
+  using local_matrix_type = typename Matrix::local_matrix_type::host_mirror_type;
+#else
   using local_matrix_type = typename Matrix::local_matrix_type::HostMirror;
+#endif
 
   std::vector<Teuchos::ParameterList> params;
   std::vector<local_matrix_type> expectedFilteredMatrices;
