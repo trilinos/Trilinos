8eaf8f007952ac2b82b1b4232200ac6e7c51220d
Christian Trott <crtrott@sandia.gov>
2025-08-28 09:46:49 -0600

    Add missing test file for Sacado
    Signed-off-by: Christian Trott <crtrott@sandia.gov>

---
+++ packages/sacado/test/UnitTests/Fad_KokkosTests_ViewCornerCases.cpp
@@ -0,0 +1,48 @@
+// @HEADER
+// *****************************************************************************
+//                           Sacado Package
+//
+// Copyright 2006 NTESS and the Sacado contributors.
+// SPDX-License-Identifier: LGPL-2.1-or-later
+// *****************************************************************************
+// @HEADER
+
+#include "Teuchos_UnitTestHarness.hpp"
+#include "Teuchos_UnitTestRepository.hpp"
+#include "Teuchos_GlobalMPISession.hpp"
+
+#include "Kokkos_Core.hpp"
+#include "Sacado.hpp"
+
+// Tests special size alignment for SFad on Cuda is correct
+TEUCHOS_UNIT_TEST(Kokkos_View_CornerCases, ViewOfUnmanagedView)
+{
+  using fad_t = Sacado::Fad::SFad<double, 3>;
+  using inner_t = Kokkos::View<fad_t*, Kokkos::HostSpace, Kokkos::MemoryTraits<Kokkos::Unmanaged>>;
+  using outer_t = Kokkos::View<inner_t*, Kokkos::HostSpace>;
+
+  Kokkos::View<fad_t*, Kokkos::HostSpace> data("data", 9, 4);
+  TEUCHOS_TEST_EQUALITY(data.use_count(), 1, out, success);
+  {
+    outer_t A("A", 10);
+ 
+    for(int i=0 ; i<10; i++) A(i) = data;
+    TEUCHOS_TEST_EQUALITY(data.use_count(), 1, out, success);
+  }
+  TEUCHOS_TEST_EQUALITY(data.use_count(), 1, out, success);
+  data = Kokkos::View<fad_t*, Kokkos::HostSpace>();
+  TEUCHOS_TEST_EQUALITY(data.use_count(), 0, out, success);
+}
+
+int main( int argc, char* argv[] ) {
+  Teuchos::GlobalMPISession mpiSession(&argc, &argv);
+
+  // Initialize serial
+  Kokkos::initialize(argc,argv);
+
+  int res = Teuchos::UnitTestRepository::runUnitTestsFromMain(argc, argv);
+
+  Kokkos::finalize();
+
+  return res;
+}
