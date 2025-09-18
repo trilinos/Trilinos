b90cf3d7dfa41569e4484b122b88867be62b6b07
Christian Glusa <caglusa@sandia.gov>
2025-09-18 17:16:25 -0600

    Ifpack2: Silence warning
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/ifpack2/src/Ifpack2_BlockHelper.hpp
+++ packages/ifpack2/src/Ifpack2_BlockHelper.hpp
@@ -280,7 +280,7 @@ struct ImplType {
   ///
   /// kokkos arithmetic traits of scalar_type
   ///
-  typedef typename Kokkos::Details::ArithTraits<scalar_type>::val_type impl_scalar_type;
+  typedef typename Kokkos::ArithTraits<scalar_type>::val_type impl_scalar_type;
   typedef typename Kokkos::ArithTraits<impl_scalar_type>::mag_type magnitude_type;
 
   typedef typename BlockTridiagScalarType<impl_scalar_type>::type btdm_scalar_type;
