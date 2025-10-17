1fd6995470ecda0f9750938733b263b12c4e8c65
Nate Roberts <nvrober@sandia.gov>
2025-10-17 10:49:01 -0500

    More simplification.  Seg fault still occurs.
    Signed-off-by: Nate Roberts <nvrober@sandia.gov>

--- packages/intrepid2/unit-test/MonolithicExecutable/DistillPR14546SegFault.cpp
+++ packages/intrepid2/unit-test/MonolithicExecutable/DistillPR14546SegFault.cpp
@@ -51,9 +51,6 @@ void callIntegrate(const TransformedBasisValues<Scalar,DeviceType> vectorDataLef
                    const TensorData<Scalar,DeviceType> cellMeasures, const TransformedBasisValues<Scalar,DeviceType> vectorDataRight,
                    Teuchos::FancyOStream &out, bool &success)
 {
-  const double relTol = 1e-12;
-  const double absTol = 1e-12;
-  
   using IntegrationTools = Intrepid2::IntegrationTools<DeviceType>;
   
   auto integralsBaseline  = IntegrationTools::allocateIntegralData(vectorDataLeft, cellMeasures, vectorDataRight);
@@ -68,26 +65,26 @@ TEUCHOS_UNIT_TEST( PR14546, Distill14546SegFault )
 {
   using DataScalar  = double;
   using DeviceType = DefaultTestDeviceType;
+  using D  = Data<DataScalar,DeviceType>;
+  using TD = TensorData<DataScalar,DeviceType>;
+  using VD = VectorData<DataScalar,DeviceType>;
   
   const int spaceDim = 2;
   
-  const int numCells  = 2;
+  const int numCells  = 1;
   const int numComponentPoints = 1;
-  const int numFields1 = 2;
-  const int numFields2 = 3;
+  const int numFields1 = 1;
+  const int numFields2 = 1;
   const int numFieldsPerFamilyLeft  = numFields1 * numFields2;
   const int numFieldsPerFamilyRight = numFields2 * numFields2;
   
   auto fieldComponentDataView1 = getFixedRankView<DataScalar>("field component data 1", numFields1);
   auto fieldComponentDataViewHost1 = Kokkos::create_mirror_view(fieldComponentDataView1);
   fieldComponentDataViewHost1(0) = 1.0;
-  fieldComponentDataViewHost1(1) = 3.0;
   
   auto fieldComponentDataView2 = getFixedRankView<DataScalar>("field component data 2", numFields2);
   auto fieldComponentDataViewHost2 = Kokkos::create_mirror_view(fieldComponentDataView2);
   fieldComponentDataViewHost2(0) = 1.0/2.0;
-  fieldComponentDataViewHost2(1) = 1.0/3.0;
-  fieldComponentDataViewHost2(2) = 1.0/4.0;
   
   Kokkos::deep_copy(fieldComponentDataView1, fieldComponentDataViewHost1);
   Kokkos::deep_copy(fieldComponentDataView2, fieldComponentDataViewHost2);
@@ -95,12 +92,12 @@ TEUCHOS_UNIT_TEST( PR14546, Distill14546SegFault )
   const int fieldComponentDataRank = 2;
   Kokkos::Array<int,fieldComponentDataRank> fieldComponentExtents {numFields1,numComponentPoints};
   Kokkos::Array<DataVariationType,fieldComponentDataRank> fieldComponentVariationTypes {GENERAL,GENERAL};
-  Data<DataScalar,DeviceType> fieldComponentData1(fieldComponentDataView1,fieldComponentExtents,fieldComponentVariationTypes);
+  D fieldComponentData1(fieldComponentDataView1,fieldComponentExtents,fieldComponentVariationTypes);
   fieldComponentExtents[0] = numFields2;
-  Data<DataScalar,DeviceType> fieldComponentData2(fieldComponentDataView2,fieldComponentExtents,fieldComponentVariationTypes);
+  D fieldComponentData2(fieldComponentDataView2,fieldComponentExtents,fieldComponentVariationTypes);
     
-  TensorData<DataScalar,DeviceType>  tensorDataLeft(std::vector< Data<DataScalar,DeviceType> >{fieldComponentData1,fieldComponentData2});
-  TensorData<DataScalar,DeviceType> tensorDataRight(std::vector< Data<DataScalar,DeviceType> >{fieldComponentData2,fieldComponentData2});
+  TD  tensorDataLeft(std::vector<D>{fieldComponentData1,fieldComponentData2});
+  TD tensorDataRight(std::vector<D>{fieldComponentData2,fieldComponentData2});
   
   auto identityMatrixView = getFixedRankView<DataScalar>("identity matrix", spaceDim, spaceDim);
   auto identityMatrixViewHost = getHostCopy(identityMatrixView);
@@ -118,30 +115,31 @@ TEUCHOS_UNIT_TEST( PR14546, Distill14546SegFault )
   Kokkos::Array<int,4> transformationExtents {numCells, numPoints, spaceDim, spaceDim};
   Kokkos::Array<DataVariationType,4> transformationVariationType {CONSTANT, CONSTANT, GENERAL, GENERAL};
   
-  Data<DataScalar,DeviceType> explicitIdentityMatrix(identityMatrixView, transformationExtents, transformationVariationType);
+  D explicitIdentityMatrix(identityMatrixView, transformationExtents, transformationVariationType);
   
   const int numFamilies = 2;
-  Kokkos::Array<TensorData<DataScalar,DeviceType>, spaceDim > firstFamilyLeft  {tensorDataLeft,TensorData<DataScalar,DeviceType>()};
-  Kokkos::Array<TensorData<DataScalar,DeviceType>, spaceDim > secondFamilyLeft {TensorData<DataScalar,DeviceType>(),tensorDataLeft};
-  Kokkos::Array< Kokkos::Array<TensorData<DataScalar,DeviceType>, spaceDim>, numFamilies> vectorComponentsLeft {firstFamilyLeft, secondFamilyLeft};
+  TD nullTD;
+  Kokkos::Array<TD, spaceDim > firstFamilyLeft  {tensorDataLeft,nullTD};
+  Kokkos::Array<TD, spaceDim > secondFamilyLeft {nullTD,tensorDataLeft};
+  Kokkos::Array< Kokkos::Array<TD, spaceDim>, numFamilies> vectorComponentsLeft {firstFamilyLeft, secondFamilyLeft};
   
-  VectorData<DataScalar,DeviceType> vectorDataLeft(vectorComponentsLeft);
+  VD vectorDataLeft(vectorComponentsLeft);
   TEST_EQUALITY(numFieldsPerFamilyLeft, vectorDataLeft.numFieldsInFamily(0));
   TEST_EQUALITY(numFieldsPerFamilyLeft, vectorDataLeft.numFieldsInFamily(1));
   
-  Kokkos::Array<TensorData<DataScalar,DeviceType>, spaceDim > firstFamilyRight  {tensorDataRight,TensorData<DataScalar,DeviceType>()};
-  Kokkos::Array<TensorData<DataScalar,DeviceType>, spaceDim > secondFamilyRight {TensorData<DataScalar,DeviceType>(),tensorDataRight};
-  Kokkos::Array< Kokkos::Array<TensorData<DataScalar,DeviceType>, spaceDim>, numFamilies> vectorComponentsRight {firstFamilyRight, secondFamilyRight};
+  Kokkos::Array<TD, spaceDim > firstFamilyRight  {tensorDataRight,nullTD};
+  Kokkos::Array<TD, spaceDim > secondFamilyRight {nullTD,tensorDataRight};
+  Kokkos::Array< Kokkos::Array<TD, spaceDim>, numFamilies> vectorComponentsRight {firstFamilyRight, secondFamilyRight};
   
-  VectorData<DataScalar,DeviceType> vectorDataRight(vectorComponentsRight);
+  VD vectorDataRight(vectorComponentsRight);
   TEST_EQUALITY(numFieldsPerFamilyRight, vectorDataRight.numFieldsInFamily(0));
   TEST_EQUALITY(numFieldsPerFamilyRight, vectorDataRight.numFieldsInFamily(1));
   
   TransformedBasisValues<DataScalar,DeviceType>  transformedUnitVectorDataLeft(explicitIdentityMatrix,vectorDataLeft);
   TransformedBasisValues<DataScalar,DeviceType> transformedUnitVectorDataRight(explicitIdentityMatrix,vectorDataRight);
   
-  Data<DataScalar,DeviceType> constantCellMeasuresData(1.0, Kokkos::Array<int,2>{numCells,numPoints});
-  TensorData<DataScalar,DeviceType> constantCellMeasures(constantCellMeasuresData);
+  D constantCellMeasuresData(1.0, Kokkos::Array<int,2>{numCells,numPoints});
+  TD constantCellMeasures(constantCellMeasuresData);
   
   callIntegrate(transformedUnitVectorDataLeft, constantCellMeasures, transformedUnitVectorDataRight, out, success);
 }
