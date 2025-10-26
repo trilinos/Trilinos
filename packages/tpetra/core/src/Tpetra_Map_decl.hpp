f149e25f47f79cd5d9a0b4e908b3b55c4f014984
Brian Kelley <bmkelle@sandia.gov>
2025-10-26 15:25:34 -0600

    Ifpack2/Tpetra: fix #14529 and re-do of #14337
    Work around parallel_scan issue; move BTD symbolic phase
    to device again. And make Tpetra::Map::lazyPushToHost private again.
    
    Signed-off-by: Brian Kelley <bmkelle@sandia.gov>

--- packages/tpetra/core/src/Tpetra_Map_decl.hpp
+++ packages/tpetra/core/src/Tpetra_Map_decl.hpp
@@ -1092,14 +1092,9 @@ class Map : public Teuchos::Describable {
       const global_ordinal_type indexBase,
       const Teuchos::RCP<const Teuchos::Comm<int>>& comm);
 
- public:
   /// \brief Push the device data to host, if needed
-  ///
-  /// \warning lazyPushToHost is SUBJECT TO CHANGE and is for EXPERT USERS ONLY.
-  /// We STRONGLY advise against its use.
   void lazyPushToHost() const;
 
- private:
   //! The communicator over which this Map is distributed.
   Teuchos::RCP<const Teuchos::Comm<int>> comm_;
 
