39317a6b5eaa4943ed3990df943686c818b544f1
Chris Siefert <csiefer2@users.noreply.github.com>
2025-11-07 10:02:45 -0700

    Xpetra/MueLu: Moving IteratorOps to the Mueverse (#14659)
    * MueLu/Xpetra: Moving IteratorOps to the Mueverse
    
    Signed-off-by: Chris Siefert <csiefer@sandia.gov>
    
    * MueLu: Fixes as requested by @jhux2
    
    Signed-off-by: Chris Siefert <csiefer@sandia.gov>
    
    * MueLu: Fixes as requested by @jhux2
    
    Signed-off-by: Chris Siefert <csiefer@sandia.gov>
    
    * MueLu: Fixes as requested by @jhux2
    
    Signed-off-by: Chris Siefert <csiefer@sandia.gov>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * Update packages/muelu/src/Utils/MueLu_IteratorOps.cpp
    
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    
    * MueLu: All hail Clang
    
    Signed-off-by: Chris Siefert <csiefer@sandia.gov>
    
    ---------
    
    Signed-off-by: Chris Siefert <csiefer@sandia.gov>
    Signed-off-by: Chris Siefert <csiefer2@users.noreply.github.com>
    Co-authored-by: Jonathan Hu <jhu@sandia.gov>

--- packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_SaPFactory_def.hpp
+++ packages/muelu/src/Transfers/Smoothed-Aggregation/MueLu_SaPFactory_def.hpp
@@ -18,7 +18,6 @@
 #include "MueLu_SaPFactory_decl.hpp"
 
 #include <Xpetra_Matrix.hpp>
-#include <Xpetra_IteratorOps.hpp>
 
 #include "MueLu_FactoryManagerBase.hpp"
 #include "MueLu_Level.hpp"
@@ -26,6 +25,7 @@
 #include "MueLu_Monitor.hpp"
 #include "MueLu_PerfUtils.hpp"
 #include "MueLu_TentativePFactory.hpp"
+#include "MueLu_IteratorOps.hpp"
 #include "MueLu_Utilities.hpp"
 
 #include <sstream>
@@ -210,9 +210,9 @@ void SaPFactory<Scalar, LocalOrdinal, GlobalOrdinal, Node>::BuildP(Level& fineLe
       TEUCHOS_TEST_FOR_EXCEPTION(!std::isfinite(Teuchos::ScalarTraits<SC>::magnitude(omega)), Exceptions::RuntimeError, "Prolongator damping factor needs to be finite.");
 
       {
-        SubFactoryMonitor m3(*this, "Xpetra::IteratorOps::Jacobi", coarseLevel);
+        SubFactoryMonitor m3(*this, "MueLu::IteratorOps::Jacobi", coarseLevel);
         // finalP = Ptent + (I - \omega D^{-1}A) Ptent
-        finalP = Xpetra::IteratorOps<Scalar, LocalOrdinal, GlobalOrdinal, Node>::Jacobi(omega, *invDiag, *A, *Ptent, finalP, GetOStream(Statistics2), std::string("MueLu::SaP-") + toString(coarseLevel.GetLevelID()), APparams);
+        finalP = MueLu::IteratorOps<SC, LO, GO, NO>::Jacobi(omega, *invDiag, *A, *Ptent, finalP, GetOStream(Statistics2), std::string("MueLu::SaP-") + toString(coarseLevel.GetLevelID()), APparams);
         if (enforceConstraints) {
           if (!pL.get<bool>("use kokkos refactor")) {
             if (A->GetFixedBlockSize() == 1)
