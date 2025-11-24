338847390a8cb57a3a6f329a1ddeecc8dabe8b19
Samuel E. Browne <sebrown@sandia.gov>
2025-11-24 14:09:35 -0700

    Update version(s) for 16.2.0 release
    Signed-off-by: Samuel E. Browne <sebrown@sandia.gov>

--- Version.cmake
+++ Version.cmake
@@ -7,11 +7,11 @@
 SET(Trilinos_VERSION 16.2.0)
 SET(Trilinos_MAJOR_VERSION 16)
 SET(Trilinos_MAJOR_MINOR_VERSION 160200)
-SET(Trilinos_VERSION_STRING "16.2.0-dev")
-SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT ON) # Change to 'OFF' for a release
+SET(Trilinos_VERSION_STRING "16.2.0")
+SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
-SET(Trilinos_REPOSITORY_BRANCH "develop" CACHE INTERNAL "")
+SET(Trilinos_REPOSITORY_BRANCH "trilinos-release-16-2-branch" CACHE INTERNAL "")
 SET(Trilinos_EXTRAREPOS_BRANCH "master" CACHE INTERNAL "")
 SET(Trilinos_TESTING_TRACK "" CACHE INTERNAL "")
 
