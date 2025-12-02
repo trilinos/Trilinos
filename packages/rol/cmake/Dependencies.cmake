8b76b61b22dc86d3a26ee41265dbf3844252c748
Aurya Javeed <asjavee@sandia.gov>
2025-12-02 08:45:03 -0700

    Snapshot of rol.git from commit 57311a591bfe08db960f03e2a1cb572b044d3f16
    From repository at git@github.com:sandialabs/rol.git
    
    At commit:
    commit 57311a591bfe08db960f03e2a1cb572b044d3f16
    Author: Aurya Javeed <asjavee@sandia.gov>
    Date:   Mon Dec 1 20:11:31 2025 -0700
    
        Account for deprecated Trilinos packages
    
    Signed-off-by: Aurya Javeed <asjavee@sandia.gov>

--- packages/rol/cmake/Dependencies.cmake
+++ packages/rol/cmake/Dependencies.cmake
@@ -1,7 +1,30 @@
-#SET(SUBPACKAGES_DIRS_CLASSIFICATION_OPTREQS # SubPackageName Directory Class Req/Opt # xrol experimental EX OPTIONAL #)      
+#SET(SUBPACKAGES_DIRS_CLASSIFICATION_OPTREQS
+  # SubPackageName       Directory       Class       Req/Opt
+#  xrol                   experimental    EX          OPTIONAL
+#)
 
 SET(LIB_REQUIRED_DEP_PACKAGES Teuchos)
-SET(LIB_OPTIONAL_DEP_PACKAGES Belos Tpetra Thyra Sacado Intrepid2 MiniTensor Shards Amesos2 Ifpack2 MueLu Tempus)
+SET(LIB_OPTIONAL_DEP_PACKAGES
+    Belos
+    Tpetra
+    Thyra
+    Sacado
+    Intrepid2
+    MiniTensor
+    Shards
+    Amesos2
+    Ifpack2
+    MueLu
+    Tempus
+)
+IF(Trilinos_MAJOR_MINOR_VERSION LESS 160200)
+  LIST(APPEND LIB_OPTIONAL_DEP_PACKAGES
+       Epetra
+       Intrepid
+       Amesos
+       TriKota
+  )
+ENDIF()
 SET(TEST_REQUIRED_DEP_PACKAGES)
 SET(TEST_OPTIONAL_DEP_PACKAGES Gtest)
 SET(LIB_REQUIRED_DEP_TPLS)
