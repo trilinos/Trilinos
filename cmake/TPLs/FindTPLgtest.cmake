da2a9da4d4ee67524f7f4ef89dc82da2a53a6b33
Samuel E. Browne <sebrown@sandia.gov>
2025-12-02 06:57:24 -0700

    Move to modern `find_package()` for GTest
    Also add `googletest` as a possible library name for the TriBITS find
    system to try and pull in GTest in more modern ways.
    
    Signed-off-by: Samuel E. Browne <sebrown@sandia.gov>

--- cmake/TPLs/FindTPLgtest.cmake
+++ cmake/TPLs/FindTPLgtest.cmake
@@ -1,5 +1,26 @@
+set(REQUIRED_HEADERS gtest.h)
+set(REQUIRED_LIBS_NAMES "gtest googletest")
+set(IMPORTED_TARGETS_FOR_ALL_LIBS GTest::gtest)
 
-TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( gtest
-  REQUIRED_HEADERS gtest/gtest.h 
-  REQUIRED_LIBS_NAMES gtest
+tribits_tpl_allow_pre_find_package(gtest GTEST_ALLOW_PREFIND)
+
+if (GTEST_ALLOW_PREFIND)
+  message("-- Using find_package(GTest ...) ...")
+  find_package(GTest)
+  if (GTest_FOUND)
+    message("-- Found GTest_DIR='${GTest_DIR}'")
+    message("-- Generating gtest::all_libs and gtestConfig.cmake")
+    tribits_extpkg_create_imported_all_libs_target_and_config_file(gtest
+      INNER_FIND_PACKAGE_NAME GTest
+      IMPORTED_TARGETS_FOR_ALL_LIBS  ${IMPORTED_TARGETS_FOR_ALL_LIBS})
+  endif()
+endif()
+
+if (NOT TARGET gtest::all_libs)
+
+  TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES(gtest
+    REQUIRED_HEADERS ${REQUIRED_HEADERS}
+    REQUIRED_LIBS_NAMES ${REQUIRED_LIBS_NAMES}
   )
+
+endif()
