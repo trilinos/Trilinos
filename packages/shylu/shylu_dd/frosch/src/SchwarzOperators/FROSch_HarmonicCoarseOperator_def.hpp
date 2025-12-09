8ae2aac05bdf76c55fe1376123e3334e268a56f1
iyamazaki <iyamaza@sandia.gov>
2025-12-09 09:25:20 -0700

    FROSch: initial try to convert indicesGammaDofsAll to Kokkos::view..
    Signed-off-by: iyamazaki <iyamaza@sandia.gov>

--- packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_HarmonicCoarseOperator_def.hpp
+++ packages/shylu/shylu_dd/frosch/src/SchwarzOperators/FROSch_HarmonicCoarseOperator_def.hpp
@@ -58,19 +58,29 @@ namespace FROSch {
         }
 
         // Extract submatrices
-        GOVec indicesGammaDofsAll(0);
-        GOVec indicesIDofsAll(0);
-        LO tmp = 0;
+        GOView indicesGammaDofsAll("indicesGammaDofsAll", 0);
+        LO count = 0;
+        for (UN i=0; i<NumberOfBlocks_; i++) {
+            count += GammaDofs_[i].size();
+        }
+        Kokkos::resize(indicesGammaDofsAll, count);
+        count = 0;
 
+        LO tmp = 0;
+        GOVec indicesIDofsAll(0);
+        auto indicesGammaDofsAll_h = Kokkos::create_mirror_view(indicesGammaDofsAll);
         for (UN i=0; i<NumberOfBlocks_; i++) {
             for (UN j=0; j<GammaDofs_[i].size(); j++) {
-                indicesGammaDofsAll.push_back(tmp+GammaDofs_[i][j]);
+                indicesGammaDofsAll_h(count+j) = tmp+GammaDofs_[i][j];
             }
+            count += GammaDofs_[i].size();
             for (UN j=0; j<IDofs_[i].size(); j++) {
                 indicesIDofsAll.push_back(tmp+IDofs_[i][j]);
             }
             tmp += GammaDofs_[i].size()+IDofs_[i].size();
         }
+        //Copy back to device !!
+        Kokkos::deep_copy(indicesGammaDofsAll, indicesGammaDofsAll_h);
 
         XMatrixPtr kII;
         XMatrixPtr kIGamma;
@@ -81,7 +91,9 @@ namespace FROSch {
 
         //Detect linear dependencies
         if (!this->ParameterList_->get("Skip DetectLinearDependencies",false)) {
-            LOVecPtr linearDependentVectors = detectLinearDependencies(indicesGammaDofsAll(),this->K_->getRowMap(),this->K_->getRangeMap(),repeatedMap,this->ParameterList_->get("Phi: Dropping Threshold",1.e-8),this->ParameterList_->get("Phi: Orthogonalization Threshold",1.e-12));
+            LOVecPtr linearDependentVectors = detectLinearDependencies(indicesGammaDofsAll,this->K_->getRowMap(),this->K_->getRangeMap(),repeatedMap,
+                                                                       this->ParameterList_->get("Phi: Dropping Threshold",1.e-8),
+                                                                       this->ParameterList_->get("Phi: Orthogonalization Threshold",1.e-12));
             // cout << this->MpiComm_->getRank() << " " << linearDependentVectors.size() << endl;
             AssembledInterfaceCoarseSpace_->zeroOutBasisVectors(linearDependentVectors());
         }
@@ -89,7 +101,7 @@ namespace FROSch {
         // Build the saddle point harmonic extensions
         XMultiVectorPtr localCoarseSpaceBasis;
         if (AssembledInterfaceCoarseSpace_->getBasisMap()->getLocalNumElements()) {
-            localCoarseSpaceBasis = computeExtensions(repeatedMatrix->getRowMap(),indicesGammaDofsAll(),indicesIDofsAll(),kII,kIGamma);
+            localCoarseSpaceBasis = computeExtensions(repeatedMatrix->getRowMap(),indicesGammaDofsAll,indicesIDofsAll(),kII,kIGamma);
 
             coarseSpace->addSubspace(AssembledInterfaceCoarseSpace_->getBasisMap(),AssembledInterfaceCoarseSpace_->getBasisMapUnique(),localCoarseSpaceBasis);
         } else {
@@ -131,7 +143,7 @@ namespace FROSch {
         RCP<ParameterList> coarseSpaceList = sublist(sublist(this->ParameterList_,"Blocks"),blockIdString.c_str());
         bool useForCoarseSpace = coarseSpaceList->get("Use For Coarse Space",true);
 
-	if (useForCoarseSpace) {
+        if (useForCoarseSpace) {
             // Das könnte man noch ändern
             NumberOfBlocks_++;
 
@@ -300,7 +312,8 @@ namespace FROSch {
                 }
             }
         }
-        return MapFactory<LO,GO,NO>::Build(DofsMaps_[0][0]->lib(),-1,mapVector(),0,this->MpiComm_);
+        GO INVALID = Teuchos::OrdinalTraits<GO>::invalid();
+        return MapFactory<LO,GO,NO>::Build(DofsMaps_[0][0]->lib(),INVALID,mapVector(),0,this->MpiComm_);
     }
 
     template <class SC,class LO,class GO,class NO>
@@ -445,7 +458,7 @@ namespace FROSch {
             }
             // If necessary, discard additional rotations
             UN rotationsToDiscard = discardRotations - numZeroRotations;
-            if (rotationsToDiscard<0) {
+            if (discardRotations < numZeroRotations) {
                 FROSCH_WARNING("FROSch::HarmonicCoarseOperator",this->Verbose_,"More rotations have been discarded than expected.");
             } else if (rotationsToDiscard>0) {
                 UN it=0;
@@ -463,7 +476,7 @@ namespace FROSch {
     }
 
     template <class SC,class LO,class GO,class NO>
-    typename HarmonicCoarseOperator<SC,LO,GO,NO>::LOVecPtr HarmonicCoarseOperator<SC,LO,GO,NO>::detectLinearDependencies(GOVecView indicesGammaDofsAll,
+    typename HarmonicCoarseOperator<SC,LO,GO,NO>::LOVecPtr HarmonicCoarseOperator<SC,LO,GO,NO>::detectLinearDependencies(GOView indicesGammaDofsAll,
                                                                                                                          ConstXMapPtr rowMap,
                                                                                                                          ConstXMapPtr rangeMap,
                                                                                                                          ConstXMapPtr repeatedMap,
@@ -481,7 +494,7 @@ namespace FROSch {
         if (numMVRows > 0 && numCols > 0) {
 
             XMatrixPtr phiGamma;
-#if defined(HAVE_XPETRA_KOKKOS_REFACTOR) && defined(HAVE_XPETRA_TPETRA)
+#if defined(HAVE_XPETRA_TPETRA)
             if (rowMap->lib() == UseTpetra)
             {
                 using crsmat_type  = typename Matrix<SC,LO,GO,NO>::local_matrix_type;
@@ -501,7 +514,6 @@ namespace FROSch {
                 auto localMVBasis = asembledBasis->getLocalViewDevice(Tpetra::Access::ReadOnly);
 
                 // Array for scaling the columns of PhiGamma (1/norm(PhiGamma(:,i)))
-                using execution_space = typename Map<LO,GO,NO>::local_map_type::execution_space;
                 using SCView = Kokkos::View<SC*, execution_space>;
                 SCView scale(Kokkos::ViewAllocateWithoutInitializing("FROSch::HarmonicCoarseOperator::detectLinearDependencies::scale"), numCols);
                 detectLinearDependenciesFunctor<GOVecView, SCView, local_map_type, local_mv_type, rowptr_type, indices_type, values_type>
@@ -513,7 +525,7 @@ namespace FROSch {
                 // count nnz
                 rowptr_type Rowptr (Kokkos::ViewAllocateWithoutInitializing("Rowptr"), numRows+1);
                 Kokkos::deep_copy(Rowptr, 0);
-                detectLinearDependenciesFunctor<GOVecView, SCView, local_map_type, local_mv_type, rowptr_type, indices_type, values_type>
+                detectLinearDependenciesFunctor<GOView, SCView, local_map_type, local_mv_type, rowptr_type, indices_type, values_type>
                     count_functor(numMVRows, numCols, localMVBasis, tresholdDropping, indicesGammaDofsAll,
                                   localRowMap, localRepeatedMap, Rowptr);
                 Kokkos::RangePolicy<CountNnzTag, execution_space> policy_count (0, numMVRows);
@@ -529,7 +541,7 @@ namespace FROSch {
                 Kokkos::fence();
 
                 // make it into offsets
-                KokkosKernels::Impl::kk_inclusive_parallel_prefix_sum<rowptr_type, execution_space>
+                KokkosKernels::Impl::kk_inclusive_parallel_prefix_sum<execution_space>
                     (1+numRows, Rowptr);
                 Kokkos::fence();
 
@@ -538,7 +550,7 @@ namespace FROSch {
                 values_type  Values  (Kokkos::ViewAllocateWithoutInitializing("Values"),  nnz);
 
                 // fill in all the nz entries
-                detectLinearDependenciesFunctor<GOVecView, SCView, local_map_type, local_mv_type, rowptr_type, indices_type, values_type>
+                detectLinearDependenciesFunctor<GOView, SCView, local_map_type, local_mv_type, rowptr_type, indices_type, values_type>
                     fill_functor(numMVRows, numCols, scale, localMVBasis, tresholdDropping, indicesGammaDofsAll,
                                  localRowMap, localRepeatedMap, Rowptr, Indices, Values);
                 Kokkos::RangePolicy<FillNzEntriesTag, execution_space> policy_fill (0, numMVRows);
@@ -572,8 +584,11 @@ namespace FROSch {
                 phiGamma = MatrixFactory<SC,LO,GO,NO>::Build(rowMap,basisMap->getLocalNumElements());
                 LO iD;
                 SC valueTmp;
+                //Copy indices to CPU, first !!
+                auto indicesGammaDofsAll_h = Kokkos::create_mirror_view(indicesGammaDofsAll);
+                Kokkos::deep_copy(indicesGammaDofsAll_h, indicesGammaDofsAll);
                 for (UN i=0; i<asembledBasis->getLocalLength(); i++) {
-                    iD = repeatedMap->getGlobalElement(indicesGammaDofsAll[i]);
+                    iD = repeatedMap->getGlobalElement(indicesGammaDofsAll_h(i));
                     if (rowMap->getLocalElement(iD)!=-1) { // This should prevent duplicate entries on the interface
                         GOVec indices;
                         SCVec values;
@@ -737,7 +752,7 @@ namespace FROSch {
 
     template <class SC,class LO,class GO,class NO>
     typename HarmonicCoarseOperator<SC,LO,GO,NO>::XMultiVectorPtr HarmonicCoarseOperator<SC,LO,GO,NO>::computeExtensions(ConstXMapPtr localMap,
-                                                                                                                         GOVecView indicesGammaDofsAll,
+                                                                                                                         GOView indicesGammaDofsAll,
                                                                                                                          GOVecView indicesIDofsAll,
                                                                                                                          XMatrixPtr kII,
                                                                                                                          XMatrixPtr kIGamma)
@@ -772,6 +787,10 @@ namespace FROSch {
         XMultiVectorPtr mVtmp = MultiVectorFactory<SC,LO,GO,NO>::Build(kII->getRowMap(),AssembledInterfaceCoarseSpace_->getBasisMap()->getLocalNumElements());
         XMultiVectorPtr mVPhiI = MultiVectorFactory<SC,LO,GO,NO>::Build(kII->getRowMap(),AssembledInterfaceCoarseSpace_->getBasisMap()->getLocalNumElements());
 
+        //Copy to host !!
+        auto indicesGammaDofsAll_h = Kokkos::create_mirror_view(indicesGammaDofsAll);
+        Kokkos::deep_copy(indicesGammaDofsAll_h, indicesGammaDofsAll);
+
         //Build mVPhiGamma
         XMultiVectorPtr mVPhiGamma = MultiVectorFactory<SC,LO,GO,NO>::Build(kIGamma->getDomainMap(),AssembledInterfaceCoarseSpace_->getBasisMap()->getLocalNumElements());
         if (AssembledInterfaceCoarseSpace_->hasAssembledBasis()) {
@@ -779,7 +798,7 @@ namespace FROSch {
                 ConstSCVecPtr assembledInterfaceCoarseSpaceData = AssembledInterfaceCoarseSpace_->getAssembledBasis()->getData(i);
                 for (UN j=0; j<AssembledInterfaceCoarseSpace_->getAssembledBasis()->getLocalLength(); j++) {
                     mVPhiGamma->replaceLocalValue(j,i,assembledInterfaceCoarseSpaceData[j]);
-                    mVPhi->replaceLocalValue(indicesGammaDofsAll[j],i,assembledInterfaceCoarseSpaceData[j]);
+                    mVPhi->replaceLocalValue(indicesGammaDofsAll_h(j),i,assembledInterfaceCoarseSpaceData[j]);
                 }
             }
         }
@@ -854,9 +873,6 @@ namespace FROSch {
 
             #if defined(HAVE_XPETRA_TPETRA)
             if (mVPhi->getMap()->lib() == UseTpetra) {
-                using XMap2            = typename SchwarzOperator<SC,LO,GO,NO>::XMap;
-                using execution_space = typename XMap2::local_map_type::execution_space;
-
                 // explicitly copying indicesIDofsAll in Teuchos::Array to Kokkos::View on "device"
                 using GOIndViewHost = Kokkos::View<GO*, Kokkos::LayoutLeft, Kokkos::HostSpace, Kokkos::MemoryUnmanaged>;
                 using GOIndView     = Kokkos::View<GO*, execution_space>;
@@ -1002,7 +1018,7 @@ namespace FROSch {
         FROSCH_DETAILTIMER_START_LEVELID(buildElementNodeListTime,"CoarseOperator::buildElementNodeList");
 
         Teuchos::ArrayView<const GO> elements =  KRowMap_->getLocalElementList();
-        UN maxNumElements = -1;
+        UN maxNumElements = 0;
         UN numElementsLocal = elements.size();
 
         reduceAll(*this->MpiComm_,Teuchos::REDUCE_MAX,numElementsLocal,Teuchos::ptr(&maxNumElements));
@@ -1086,7 +1102,7 @@ namespace FROSch {
 
         //UN maxNumElements = -1;
         //get the maximum number of neighbors for a subdomain
-        MaxNumNeigh_ = -1;
+        MaxNumNeigh_ = 0;
         UN numElementsLocal = entries.size();
         reduceAll(*this->MpiComm_,Teuchos::REDUCE_MAX,numElementsLocal,Teuchos::ptr(&MaxNumNeigh_));
         this->SubdomainConnectGraph_ = Xpetra::CrsGraphFactory<LO,GO,NO>::Build(graphMap,MaxNumNeigh_);
@@ -1157,14 +1173,10 @@ namespace FROSch {
 
             // -------------------------------------------------------
             // symbolic for computeExtensions to factor kII
-            GOVec indicesGammaDofsAll(0);
             GOVec indicesIDofsAll(0);
             LO tmp = 0;
 
             for (UN i=0; i<NumberOfBlocks_; i++) {
-                for (UN j=0; j<GammaDofs_[i].size(); j++) {
-                    indicesGammaDofsAll.push_back(tmp+GammaDofs_[i][j]);
-                }
                 for (UN j=0; j<IDofs_[i].size(); j++) {
                     indicesIDofsAll.push_back(tmp+IDofs_[i][j]);
                 }
