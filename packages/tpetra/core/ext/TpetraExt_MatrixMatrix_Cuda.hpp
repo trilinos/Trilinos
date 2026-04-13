8ef2e2d1fc3a211386dba3c4edc6e38fef5ae6e0
Carl Pearson <cwpears@sandia.gov>
2026-04-13 15:23:24 +0000

    Tpetra: Enable time monitor for CUDA and HIP
    Signed-off-by: Carl Pearson <cwpears@sandia.gov>

--- packages/tpetra/core/ext/TpetraExt_MatrixMatrix_Cuda.hpp
+++ packages/tpetra/core/ext/TpetraExt_MatrixMatrix_Cuda.hpp
@@ -18,7 +18,7 @@ namespace MMdetails {
 
 template <>
 struct KokkosKernelsSPGEMMBackend<Tpetra::KokkosCompat::KokkosCudaWrapperNode> {
-  static constexpr bool use_time_monitor = false;
+  static constexpr bool use_time_monitor = true;
   static std::string parameter_prefix() { return "cuda"; }
   static std::string algorithm_label() { return "Cuda"; }
   static std::string wrapper_label() { return "CudaWrapper"; }
