991aa4ccdc751149000b67909828f2cd828f3961
Heidi Thornquist <hkthorn@sandia.gov>
2026-04-22 16:20:29 -0600

    Fix comments to replace Epetra with Tpetra
    The TraceMin examples and tests have comments indicating that Epetra
    is being used when Tpetra is being used.  This commit cleans that up.
    
    Signed-off-by: Heidi Thornquist <hkthorn@sandia.gov>

--- packages/anasazi/tpetra/test/TraceMinDavidson/cxx_main_standard_noprec.cpp
+++ packages/anasazi/tpetra/test/TraceMinDavidson/cxx_main_standard_noprec.cpp
@@ -311,7 +311,7 @@ int main(int argc, char *argv[]) {
   MyPL.set("Which", which);
 
   //
-  // Create an Epetra_MultiVector for an initial vector to start the solver.
+  // Create an Tpetra::MultiVector for an initial vector to start the solver.
   // Note:  This needs to have the same number of columns as the blocksize.
   // We are giving it random entries.
   //
