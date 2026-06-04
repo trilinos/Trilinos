810620368132fa8d2ead54f15e2fe2bfb815372b
Christian Glusa <caglusa@sandia.gov>
2026-06-04 17:30:08 -0600

    Intrepid2: ETI cubatures
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

---
+++ packages/intrepid2/src/Discretization/Integration/Intrepid2_CubatureDirectLineGaussJacobi20_ETI.cpp
@@ -0,0 +1,17 @@
+// @HEADER
+// *****************************************************************************
+//                           Intrepid2 Package
+//
+// Copyright 2007 NTESS and the Intrepid2 contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+// @HEADER
+
+/** \file   Intrepid2_CubatureDirectLineGaussJacobi20_ETI.cpp
+    \brief  \brief ETI instantiation for Intrepid2 CubatureDirectLineGaussJacobi20 class.
+    \author Created by N.V. Roberts.
+ */
+
+#include "Intrepid2_CubatureDirectLineGaussJacobi20.hpp"
+
+INTREPID2_ETI_DEVICE(INTREPID2_CUBATUREDIRECTLINEGAUSSJACOBI20_INSTANT);
---
+++ packages/intrepid2/src/Discretization/Integration/Intrepid2_CubatureTensor_ETI.cpp
@@ -0,0 +1,17 @@
+// @HEADER
+// *****************************************************************************
+//                           Intrepid2 Package
+//
+// Copyright 2007 NTESS and the Intrepid2 contributors.
+// SPDX-License-Identifier: BSD-3-Clause
+// *****************************************************************************
+// @HEADER
+
+/** \file   Intrepid2_CubatureTensor_ETI.cpp
+    \brief  \brief ETI instantiation for Intrepid2 CubatureTensor class.
+    \author Created by N.V. Roberts.
+ */
+
+#include "Intrepid2_CubatureTensor.hpp"
+
+INTREPID2_ETI_DEVICE(INTREPID2_CUBATURETENSOR_INSTANT);
