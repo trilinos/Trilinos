0223820e51019759dcd96286408c904d4c883063
Christian Glusa <caglusa@sandia.gov>
2026-06-12 16:01:33 -0600

    Tpetra: Fixes for ASAN
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
+++ packages/tpetra/core/ext/TpetraExt_MatrixMatrix_def.hpp
@@ -2326,7 +2326,7 @@ void KernelWrappers<Scalar, LocalOrdinal, GlobalOrdinal, Node, LocalOrdinalViewT
           LO Bkj = Bcolind[j];
           LO Cij = Bcol2Ccol[Bkj];
 
-          const bool badInsert = c_status[Cij] < OLD_ip || c_status[Cij] >= CSR_ip;
+          const bool badInsert = (Cij == LO_INVALID) || (c_status[Cij] < OLD_ip) || (c_status[Cij] >= CSR_ip);
           if (!badInsert)
             Cvals[c_status[Cij]] += Aval * Bvals[j];
           else if (throwOnInsert)
@@ -2342,7 +2342,7 @@ void KernelWrappers<Scalar, LocalOrdinal, GlobalOrdinal, Node, LocalOrdinalViewT
           LO Ikj = Icolind[j];
           LO Cij = Icol2Ccol[Ikj];
 
-          const bool badInsert = c_status[Cij] < OLD_ip || c_status[Cij] >= CSR_ip;
+          const bool badInsert = (Cij == LO_INVALID) || (c_status[Cij] < OLD_ip) || (c_status[Cij] >= CSR_ip);
           if (!badInsert)
             Cvals[c_status[Cij]] += Aval * Ivals[j];
           else if (throwOnInsert)
