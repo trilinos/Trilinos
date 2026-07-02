11f67369107cc1c72bd81546474b2c448cde7818
Konstantin Pieper <pieperk@ornl.gov>
2026-07-01 23:46:52 -0400

    create a StepperExponential base class
    Signed-off-by: Konstantin Pieper <pieperk@ornl.gov>

--- packages/tempus/src/Tempus_StepperEPI.cpp
+++ packages/tempus/src/Tempus_StepperEPI.cpp
@@ -15,8 +15,13 @@ namespace Tempus {
   TEMPUS_INSTANTIATE_TEMPLATE_CLASS(StepperEPI)
 
   // Nonmember constructor
-  template Teuchos::RCP<StepperEPI<double> >
-  createStepperEPI(
+  template Teuchos::RCP<StepperExponential_EPI2<double> >
+  createStepperExponential_EPI2(
+      const Teuchos::RCP<const Thyra::ModelEvaluator<double> >& model,
+      Teuchos::RCP<Teuchos::ParameterList> pl);
+
+  template Teuchos::RCP<StepperExponential_EPI3<double> >
+  createStepperExponential_EPI3(
     const Teuchos::RCP<const Thyra::ModelEvaluator<double> >& model,
     Teuchos::RCP<Teuchos::ParameterList> pl);
 
