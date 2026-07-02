3196e5f39a0655f51da0a3c941de6af6eb854b07
Jeff Haack <haack@lanl.gov>
2026-07-02 10:42:05 -0600

    change preproc logic
    Signed-off-by: Jeff Haack <haack@lanl.gov>

--- packages/ifpack2/src/Ifpack2_Hypre_def.hpp
+++ packages/ifpack2/src/Ifpack2_Hypre_def.hpp
@@ -20,7 +20,7 @@
 #include "Teuchos_DefaultMpiComm.hpp"
 #include "HYPRE_IJ_mv.h"
 #include "HYPRE_parcsr_ls.h"
-#if HAVE_IFPACK2_HYPRE_UNDERSCORE_KRYLOV_H
+#if defined(HAVE_IFPACK2_HYPRE_UNDERSCORE_KRYLOV_H)
 #include "_hypre_krylov.h"
 #else
 #include "krylov.h"
