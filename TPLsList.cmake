4ff1fdda33fcd9282236c711c206a89cccad77e3
Jeff Haack <haack@lanl.gov>
2026-07-10 13:23:34 -0700

    change to modern cmake consumption of exported hypre target
    Signed-off-by: Jeff Haack <haack@lanl.gov>

--- TPLsList.cmake
+++ TPLsList.cmake
@@ -61,7 +61,6 @@ TRIBITS_REPOSITORY_DEFINE_TPLS(
   AMD             "cmake/TPLs/"    TS
   CSparse         "cmake/TPLs/"    EX
   HYPRE           "${${PROJECT_NAME}_TRIBITS_DIR}/common_tpls/"   EX
-  Umpire          "${${PROJECT_NAME}_TRIBITS_DIR}/common_tpls/"   EX
   PETSC           "${${PROJECT_NAME}_TRIBITS_DIR}/common_tpls/"   ST
   BLACS           "cmake/TPLs/"    ST
   SCALAPACK       "cmake/TPLs/"    ST
--- cmake/tribits/common_tpls/FindTPLHYPRE.cmake
+++ cmake/tribits/common_tpls/FindTPLHYPRE.cmake
@@ -1,4 +1,7 @@
-TRIBITS_TPL_FIND_INCLUDE_DIRS_AND_LIBRARIES( HYPRE
-  REQUIRED_HEADERS HYPRE.h HYPRE_config.h
-  REQUIRED_LIBS_NAMES HYPRE
-  )
+find_package(HYPRE)
+
+if(HYPRE_FOUND)
+	tribits_extpkg_create_imported_all_libs_target_and_config_file( HYPRE
+		INNER_FIND_PACKAGE_NAME  HYPRE
+        IMPORTED_TARGETS_FOR_ALL_LIBS  HYPRE::HYPRE )
+endif()
