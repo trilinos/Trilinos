5192de9a694a7ac7ccc9ea9181c913b16d2f5caa
Christian Glusa <caglusa@sandia.gov>
2026-07-15 21:25:25 -0600

    remove old scripts
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- sampleScripts/git-profiles/git-config-dridzal.sh
+++
@@ -1,9 +0,0 @@
-# Set your username
-eg config --global user.name "Denis Ridzal"
-# Set your email address
-eg config --global user.email dridzal@sandia.gov
-# Use colorized output when it makes sense
-eg config --global color.ui true
-# Set up some shortcut commands
-_SCRIPT_DIR=`echo $0 | sed "s/\(.*\)\/.*\.sh/\1/g"`
-$_SCRIPT_DIR/../../cmake/tribits/common_tools/git/git-config-alias.sh
