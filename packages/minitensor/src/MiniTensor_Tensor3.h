2451846cd8c1d17eaf4635101c79cdbe67182c95
Alejandro Mota <amota@sandia.gov>
2026-07-29 17:18:58 -0700

    MiniTensor: merge .i.h and .t.h sub-headers into their parent headers
    Every module was split into a .h with declarations, a .i.h with inline
    definitions, and a .t.h with template definitions, a convention inherited
    from older codes. Both sub-files were unconditionally included at the end
    of the parent header, so the split served no compilation purpose, and the
    nonstandard suffixes confuse tooling that does not recognize them as C++
    headers. Inline each sub-file's content at its former include site, in
    order, so the resulting translation unit is identical. Sub-file license
    banners and include guards are dropped; nothing else changes. No
    downstream code includes the sub-files directly.
    
    Also change the one angle-bracket include of MiniTensor.h in
    MiniTensor_Solvers.h to the quoted form used everywhere else.
    
    Signed-off-by: Alejandro Mota <amota@sandia.gov>

--- packages/minitensor/src/MiniTensor_Tensor3.h
+++ packages/minitensor/src/MiniTensor_Tensor3.h
@@ -411,7 +411,774 @@ operator<<(std::ostream & os, Tensor3<T, N> const & A);
 
 } // namespace minitensor
 
-#include "MiniTensor_Tensor3.i.h"
-#include "MiniTensor_Tensor3.t.h"
+namespace minitensor {
+
+//
+// 3rd-order tensor constructor with NaNs
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::Tensor3() :
+TensorBase<T, Store>::TensorBase()
+{
+  set_dimension(N);
+  return;
+}
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::Tensor3(Index const dimension) :
+TensorBase<T, Store>::TensorBase(dimension, ORDER)
+{
+  return;
+}
+
+//
+// 3rd-order tensor constructor with a specified value
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::Tensor3(Filler const value) :
+TensorBase<T, Store>::TensorBase(N, ORDER, value)
+{
+  return;
+}
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::Tensor3(Index const dimension, Filler const value) :
+TensorBase<T, Store>::TensorBase(dimension, ORDER, value)
+{
+  return;
+}
+
+//
+//  Create 3rd-order tensor from array
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::Tensor3(T const * data_ptr) :
+TensorBase<T, Store>::TensorBase(N, ORDER, data_ptr)
+{
+  return;
+}
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::Tensor3(Index const dimension, T const * data_ptr) :
+TensorBase<T, Store>::TensorBase(dimension, ORDER, data_ptr)
+{
+  return;
+}
+
+//
+// Copy constructor
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::Tensor3(Tensor3<T, N> const & A) :
+TensorBase<T, Store>::TensorBase(A)
+{
+  return;
+}
+
+//
+// 3rd-order tensor simple destructor
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>::~Tensor3()
+{
+  return;
+}
+
+//
+// Get dimension
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Index
+Tensor3<T, N>::get_dimension() const
+{
+  return TensorBase<T, Store>::get_dimension(ORDER);
+}
+
+//
+// Set dimension
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+void
+Tensor3<T, N>::set_dimension(Index const dimension)
+{
+  TensorBase<T, Store>::set_dimension(dimension, ORDER);
+  return;
+}
+
+//
+// 3rd-order tensor addition
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+operator+(Tensor3<S, N>const & A, Tensor3<T, N> const & B)
+{
+  Tensor3<typename Promote<S, T>::type, N>
+  C(A.get_dimension());
+
+  add(A, B, C);
+
+  return C;
+}
+
+//
+// 3rd-order tensor subtraction
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+operator-(Tensor3<S, N> const & A, Tensor3<T, N> const & B)
+{
+  Tensor3<typename Promote<S, T>::type, N>
+  C(A.get_dimension());
+
+  subtract(A, B, C);
+
+  return C;
+}
+
+//
+// 3rd-order tensor minus
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N>
+operator-(Tensor3<T, N> const & A)
+{
+  Tensor3<T, N>
+  B(A.get_dimension());
+
+  minus(A, B);
+
+  return B;
+}
+
+//
+// 3rd-order tensor equality
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+bool
+operator==(Tensor3<T, N> const & A, Tensor3<T, N> const & B)
+{
+  return equal(A, B);
+}
+
+//
+// 3rd-order tensor inequality
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+bool
+operator!=(Tensor3<T, N> const & A, Tensor3<T, N> const & B)
+{
+  return not_equal(A, B);
+}
+
+//
+// Scalar 3rd-order tensor product
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+typename lazy_disable_if< order_1234<S>, apply_tensor3< Promote<S,T>, N>>::type
+operator*(S const & s, Tensor3<T, N> const & A)
+{
+  Tensor3<typename Promote<S, T>::type, N>
+  B(A.get_dimension());
+
+  scale(A, s, B);
+
+  return B;
+}
+
+//
+// 3rd-order tensor scalar product
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+typename lazy_disable_if< order_1234<S>, apply_tensor3< Promote<S,T>, N>>::type
+operator*(Tensor3<T, N> const & A, S const & s)
+{
+  Tensor3<typename Promote<S, T>::type, N>
+  B(A.get_dimension());
+
+  scale(A, s, B);
+
+  return B;
+}
+
+//
+// 3rd-order tensor scalar division
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+operator/(Tensor3<T, N> const & A, S const & s)
+{
+  Tensor3<typename Promote<S, T>::type, N>
+  B(A.get_dimension());
+
+  divide(A, s, B);
+
+  return B;
+}
+
+//
+// 3rd-order scalar tensor division
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+operator/(S const & s, Tensor3<T, N> const & A)
+{
+  Tensor3<typename Promote<S, T>::type, N>
+  B(A.get_dimension());
+
+  split(A, s, B);
+
+  return B;
+}
+
+//
+// Indexing for constant 3rd order tensor
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+T const &
+Tensor3<T, N>::operator()(Index const i, Index const j, Index const k) const
+{
+  Tensor3<T, N> const &
+  self = (*this);
+
+  Index const
+  dimension = self.get_dimension();
+
+  return self[(i * dimension + j) * dimension + k];
+}
+
+//
+// 3rd-order tensor indexing
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+T &
+Tensor3<T, N>::operator()(Index const i, Index const j, Index const k)
+{
+  Tensor3<T, N> &
+  self = (*this);
+
+  Index const
+  dimension = self.get_dimension();
+
+  return self[(i * dimension + j) * dimension + k];
+}
+
+// Local utility functions
+namespace {
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+void ones_in_diagonal(Tensor3<T, N> & A)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  switch (dimension) {
+
+  default:
+    for (Index i = 0; i < dimension; ++i) {
+      A(i, i, i) = 1.0;
+    }
+    break;
+
+  case 3:
+    A(0, 0, 0) = 1.0;
+    A(1, 1, 1) = 1.0;
+    A(2, 2, 2) = 1.0;
+    break;
+
+  case 2:
+    A(0, 0, 0) = 1.0;
+    A(1, 1, 1) = 1.0;
+    break;
+
+  }
+
+  return;
+}
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+void fill_levi_civita(Tensor3<T, N> & A)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index j = 0; j < dimension; ++j) {
+      for (Index k = 0; k < dimension; ++k) {
+        A(i, j, k) = levi_civita<T>(i, j, k);
+      }
+    }
+  }
+
+  return;
+}
+
+} // anonymous namespace
+
+//
+// Levi-Civita symbol
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N> const
+levi_civita_3()
+{
+  Tensor3<T, N>
+  A(N, Filler::ZEROS);
+
+  fill_levi_civita(A);
+
+  return A;
+}
+
+template<typename T>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, DYNAMIC> const
+levi_civita_3(Index const dimension)
+{
+  Tensor3<T, DYNAMIC>
+  A(dimension, Filler::ZEROS);
+
+  fill_levi_civita(A);
+
+  return A;
+}
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N> const
+levi_civita_3(Index const dimension)
+{
+  if (N != DYNAMIC) assert(dimension == N);
+
+  Tensor3<T, DYNAMIC>
+  A(dimension, Filler::ZEROS);
+
+  fill_levi_civita(A);
+
+  return A;
+}
+// Permutation symbol
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N> const
+permutation_3()
+{
+  return levi_civita_3<T, N>();
+}
+
+template<typename T>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, DYNAMIC> const
+permutation_3(Index const dimension)
+{
+  return levi_civita_3<T>(dimension);
+}
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N> const
+permutation_3(Index const dimension)
+{
+  return levi_civita_3<T, N>(dimension);
+}
+
+//
+// Alternating symbol
+//
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N> const
+alternator_3()
+{
+  return levi_civita_3<T, N>();
+}
+
+template<typename T>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, DYNAMIC> const
+alternator_3(Index const dimension)
+{
+  return levi_civita_3<T>(dimension);
+}
+
+template<typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<T, N> const
+alternator_3(Index const dimension)
+{
+  return levi_civita_3<T, N>(dimension);
+}
+
+} // namespace minitensor
+namespace minitensor {
+
+//
+// \return \f$ B = A : u := B_i = A_{ijk} u_{jk} \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Vector<typename Promote<S, T>::type, N>
+dotdot(Tensor3<T, N> const & A, Tensor<S, N> const & u)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(u.get_dimension() == dimension);
+
+  Vector<typename Promote<S, T>::type, N>
+  B(dimension);
+
+  for (Index i = 0; i < dimension; ++i) {
+
+    typename Promote<S, T>::type
+    s = 0.0;
+
+    for (Index j = 0; j < dimension; ++j) {
+      for (Index k = 0; k < dimension; ++k) {
+        s += A(i,j,k) * u(j,k);
+      }
+    }
+    B(i) = s;
+  }
+
+  return B;
+}
+
+//
+// \return \f$ B = A \cdot u := B_{ij} = A_{ijp} u_p \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor<typename Promote<S, T>::type, N>
+dot(Tensor3<T, N> const & A, Vector<S, N> const & u)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(u.get_dimension() == dimension);
+
+  Tensor<typename Promote<S, T>::type, N>
+  B(N);
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index j = 0; j < dimension; ++j) {
+
+      typename Promote<S, T>::type
+      s = 0.0;
+
+      for (Index p = 0; p < dimension; ++p) {
+        s += A(i,j,p) * u(p);
+      }
+      B(i,j) = s;
+    }
+  }
+
+  return B;
+}
+
+//
+// \return \f$ B = u \cdot A := B_{ij} = u_p A{pij} \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor<typename Promote<S, T>::type, N>
+dot(Vector<S, N> const & u, Tensor3<T, N> const & A)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(u.get_dimension() == dimension);
+
+  Tensor<typename Promote<S, T>::type, N>
+  B(dimension);
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index j = 0; j < dimension; ++j) {
+
+      typename Promote<S, T>::type
+      s = 0.0;
+
+      for (Index p = 0; p < dimension; ++p) {
+        s += u(p) * A(p,i,j);
+      }
+      B(i,j) = s;
+    }
+  }
+
+  return B;
+}
+
+
+//
+// \return \f$ B = A \cdot u := B_{ij} = A_{ipj} u_p \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor<typename Promote<S, T>::type, N>
+dot2(Tensor3<T, N> const & A, Vector<S, N> const & u)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(u.get_dimension() == dimension);
+
+  Tensor<typename Promote<S, T>::type, N>
+  B(dimension);
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index j = 0; j < dimension; ++j) {
+
+      typename Promote<S, T>::type
+      s = 0.0;
+
+      for (Index p = 0; p < dimension; ++p) {
+        s += A(i,p,j) * u(p);
+      }
+      B(i,j) = s;
+    }
+  }
+
+  return B;
+}
+
+//
+// \return \f$ B = u \cdot A := B_{ij} = u_p A_{ipj} \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor<typename Promote<S, T>::type, N>
+dot2(Vector<S, N> const & u, Tensor3<T, N> const & A)
+{
+  return dot2(A, u);
+}
+
+//
+// \return \f$ C = A \cdot B := C_{ijk} = A_{ijp} B_{pk} \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+dot(Tensor3<T, N> const & A, Tensor<S, N> const & B)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(B.get_dimension() == dimension);
+
+  Tensor3<typename Promote<S, T>::type, N>
+  C(dimension);
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index k = 0; k < dimension; ++k) {
+      for (Index j = 0; j < dimension; ++j) {
+
+        typename Promote<S, T>::type
+        s = 0.0;
+
+        for (Index p = 0; p < dimension; ++p) {
+          s += A(i,j,p) * B(p,k);
+        }
+        C(i,j,k) = s;
+      }
+    }
+  }
+
+  return C;
+}
+
+//
+// \return \f$ C = A \cdot B := C_{ijk} = A_{ip} B_{pjk} \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+dot(Tensor<S, N> const & A, Tensor3<T, N> const & B)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(B.get_dimension() == dimension);
+
+  Tensor3<typename Promote<S, T>::type, N>
+  C(dimension);
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index k = 0; k < dimension; ++k) {
+      for (Index j = 0; j < dimension; ++j) {
+
+        typename Promote<S, T>::type
+        s = 0.0;
+
+        for (Index p = 0; p < dimension; ++p) {
+          s += A(i,p) * B(p,j,k);
+        }
+        C(i,j,k) = s;
+      }
+    }
+  }
+
+  return C;
+}
+
+//
+// \return \f$ C = A \cdot B := C_{ijk} = A_{ipj} B_{pk} \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+dot2(Tensor3<T, N> const & A, Tensor<S, N> const & B)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(B.get_dimension() == dimension);
+
+  Tensor3<typename Promote<S, T>::type, N>
+  C(dimension);
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index k = 0; k < dimension; ++k) {
+      for (Index j = 0; j < dimension; ++j) {
+
+        typename Promote<S, T>::type
+        s = 0.0;
+
+        for (Index p = 0; p < dimension; ++p) {
+          s += A(i,p,j) * B(p,k);
+        }
+        C(i,j,k) = s;
+      }
+    }
+  }
+
+  return C;
+}
+
+
+//
+// \return \f$ C = A \cdot B := C_{ijk} = A_{ip} B_{jpk} \f$
+//
+template<typename S, typename T, Index N>
+KOKKOS_INLINE_FUNCTION
+Tensor3<typename Promote<S, T>::type, N>
+dot2(Tensor<S, N> const & A, Tensor3<T, N> const & B)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  assert(B.get_dimension() == dimension);
+
+  Tensor3<typename Promote<S, T>::type, N>
+  C(dimension);
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index k = 0; k < dimension; ++k) {
+      for (Index j = 0; j < dimension; ++j) {
+
+        typename Promote<S, T>::type
+        s = 0.0;
+
+        for (Index p = 0; p < dimension; ++p) {
+          s += A(i,p) * B(j,p,k);
+        }
+        C(i,j,k) = s;
+      }
+    }
+  }
+
+  return C;
+}
+
+
+//
+// 3rd-order tensor input
+// \param A 3rd-order tensor
+// \param is input stream
+// \return is input stream
+//
+template<typename T, Index N>
+std::istream &
+operator>>(std::istream & is, Tensor3<T, N> & A)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  for (Index i = 0; i < dimension; ++i) {
+    for (Index j = 0; j < dimension; ++j) {
+      for (Index k = 0; k < dimension; ++k) {
+        is >> A(i,j,k);
+      }
+    }
+  }
+
+  return is;
+}
+
+//
+// 3rd-order tensor output
+// \param A 3rd-order tensor
+// \param os output stream
+// \return os output stream
+//
+template<typename T, Index N>
+std::ostream &
+operator<<(std::ostream & os, Tensor3<T, N> const & A)
+{
+  Index const
+  dimension = A.get_dimension();
+
+  if (dimension == 0) {
+    return os;
+  }
+
+  os << std::scientific << std::setprecision(17);
+
+  for (Index i = 0; i < dimension; ++i) {
+
+    for (Index j = 0; j < dimension; ++j) {
+
+      os << std::setw(24) << A(i,j,0);
+
+      for (Index k = 1; k < dimension; ++k) {
+        os << "," << std::setw(24) << A(i,j,k);
+      }
+
+      os << std::endl;
+
+    }
+
+    os << std::endl;
+    os << std::endl;
+
+  }
+
+  return os;
+}
+
+} // namespace minitensor
 
 #endif //MiniTensor_Tensor3_h
