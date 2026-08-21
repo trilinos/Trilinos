24a43b8bc3887fa2890c37c5cc4e279f1bd57e38
Nate Roberts <nvrober@sandia.gov>
2026-08-20 21:04:35 -0500

    Revert the previous C++17-compatibility edit to Util::max because Trilinos now requires C++20; restore the original requires-clause form.
    Signed-off-by: Nate Roberts <nvrober@sandia.gov>

--- packages/intrepid2/src/Shared/Intrepid2_Utils.hpp
+++ packages/intrepid2/src/Shared/Intrepid2_Utils.hpp
@@ -247,7 +247,8 @@ namespace Intrepid2 {
       return (a > b ? a : b);
     }
 
-    template <typename... Args, typename = std::enable_if_t<(std::is_same_v<T, Args> && ...)>>
+    template <typename... Args>
+    requires (std::is_same_v<T, Args> && ...)
     KOKKOS_FORCEINLINE_FUNCTION
     static T max(const T a, const T b, Args... args) {
         // Recursively find the max of everything after the first argument
