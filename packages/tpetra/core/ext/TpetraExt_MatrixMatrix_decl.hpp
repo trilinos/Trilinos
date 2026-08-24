2e8d9fbcbe8b3c262d583dfe18b242a6226b154e
Christian Glusa <caglusa@sandia.gov>
2026-08-24 12:59:03 -0600

    Tpetra: Deduplicate jacobi_A_B_reuse_kernel_wrapper
    Signed-off-by: Christian Glusa <caglusa@sandia.gov>

--- packages/tpetra/core/ext/TpetraExt_MatrixMatrix_decl.hpp
+++ packages/tpetra/core/ext/TpetraExt_MatrixMatrix_decl.hpp
@@ -499,6 +499,24 @@ void host_mult_A_B_reuse(
     const std::string& label,
     const Teuchos::RCP<Teuchos::ParameterList>& params);
 
+template <class Scalar,
+          class LocalOrdinal,
+          class GlobalOrdinal,
+          class Node,
+          class LocalOrdinalViewType>
+void host_jacobi_A_B_reuse(typename Teuchos::ScalarTraits<Scalar>::magnitudeType omega,
+                           const Vector<Scalar, LocalOrdinal, GlobalOrdinal, Node>& Dinv,
+                           CrsMatrixStruct<Scalar, LocalOrdinal, GlobalOrdinal, Node>& Aview,
+                           CrsMatrixStruct<Scalar, LocalOrdinal, GlobalOrdinal, Node>& Bview,
+                           const LocalOrdinalViewType& targetMapToOrigRow_dev,
+                           const LocalOrdinalViewType& targetMapToImportRow_dev,
+                           const LocalOrdinalViewType& Bcol2Ccol_dev,
+                           const LocalOrdinalViewType& Icol2Ccol_dev,
+                           CrsMatrix<Scalar, LocalOrdinal, GlobalOrdinal, Node>& C,
+                           Teuchos::RCP<const Import<LocalOrdinal, GlobalOrdinal, Node> > Cimport,
+                           const std::string& label,
+                           const Teuchos::RCP<Teuchos::ParameterList>& params);
+
 // MMM Kernel wrappers struct
 // Because C++ doesn't support partial template specialization of functions.
 template <class Scalar,
