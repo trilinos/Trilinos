bd2a89ee26c01b8734daed9d3bc460e23cb608b4
Anderson Chauphan <achauph@sandia.gov>
2026-08-27 17:56:06 -0500

    Update patch release Version.cmake
    Signed-off-by: Anderson Chauphan <achauph@sandia.gov>

--- Version.cmake
+++ Version.cmake
@@ -4,10 +4,10 @@
 # for release mode and set the version.
 #
 
-SET(Trilinos_VERSION 17.2.0)
+SET(Trilinos_VERSION 17.2.1)
 SET(Trilinos_MAJOR_VERSION 17)
-SET(Trilinos_MAJOR_MINOR_VERSION 170200)
-SET(Trilinos_VERSION_STRING "17.2.0")
+SET(Trilinos_MAJOR_MINOR_VERSION 170201)
+SET(Trilinos_VERSION_STRING "17.2.1")
 SET(Trilinos_ENABLE_DEVELOPMENT_MODE_DEFAULT OFF) # Change to 'OFF' for a release
 
 # Used by testing scripts and should not be used elsewhere
